73233cc7ad3b3020f0475b71df81b3b2ab4c1ceb
asciian <asciian@outlook.jp>
2018-03-29 19:07:40 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-sanchez.rb to Formula/font-sanchez.rb

---
+++ Formula/font-sanchez.rb
@@ -0,0 +1,11 @@
+class FontSanchez < Formula
+  head "https://github.com/google/fonts/trunk/ofl/sanchez", using: :svn, revision: "50", trust_cert: true
+  desc "Sanchez"
+  homepage "https://www.google.com/fonts/specimen/Sanchez"
+  def install
+    (share/"fonts").install "Sanchez-Italic.ttf"
+    (share/"fonts").install "Sanchez-Regular.ttf"
+  end
+  test do
+  end
+end
