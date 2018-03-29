186aba34bb0a6c36abe55889aab179bc463237e5
asciian <asciian@outlook.jp>
2018-03-29 18:56:47 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-asul.rb to Formula/font-asul.rb

---
+++ Formula/font-asul.rb
@@ -0,0 +1,11 @@
+class FontAsul < Formula
+  head "https://github.com/google/fonts/trunk/ofl/asul", using: :svn, revision: "50", trust_cert: true
+  desc "Asul"
+  homepage "https://www.google.com/fonts/specimen/Asul"
+  def install
+    (share/"fonts").install "Asul-Bold.ttf"
+    (share/"fonts").install "Asul-Regular.ttf"
+  end
+  test do
+  end
+end
