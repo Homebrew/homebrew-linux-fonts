81e412ba4dc7f9cd3ea55b8dca7e4845e5d546f0
asciian <asciian@outlook.jp>
2018-03-29 18:56:23 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-andada.rb to Formula/font-andada.rb

---
+++ Formula/font-andada.rb
@@ -0,0 +1,13 @@
+class FontAndada < Formula
+  head "https://github.com/google/fonts/trunk/ofl/andada", using: :svn, revision: "50", trust_cert: true
+  desc "Andada"
+  homepage "https://www.google.com/fonts/specimen/Andada"
+  def install
+    (share/"fonts").install "Andada-Bold.ttf"
+    (share/"fonts").install "Andada-BoldItalic.ttf"
+    (share/"fonts").install "Andada-Italic.ttf"
+    (share/"fonts").install "Andada-Regular.ttf"
+  end
+  test do
+  end
+end
