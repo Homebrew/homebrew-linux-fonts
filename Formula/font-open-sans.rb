1b5ee69585cee8e08f6d2a4a0cc048a4b53edc3c
asciian <asciian@outlook.jp>
2018-03-29 19:05:59 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-open-sans.rb to Formula/font-open-sans.rb

---
+++ Formula/font-open-sans.rb
@@ -0,0 +1,19 @@
+class FontOpenSans < Formula
+  head "https://github.com/google/fonts/trunk/apache/opensans", using: :svn, revision: "48", trust_cert: true
+  desc "Open+Sans"
+  homepage "https://www.google.com/fonts/specimen/Open+Sans"
+  def install
+    (share/"fonts").install "OpenSans-Bold.ttf"
+    (share/"fonts").install "OpenSans-BoldItalic.ttf"
+    (share/"fonts").install "OpenSans-ExtraBold.ttf"
+    (share/"fonts").install "OpenSans-ExtraBoldItalic.ttf"
+    (share/"fonts").install "OpenSans-Italic.ttf"
+    (share/"fonts").install "OpenSans-Light.ttf"
+    (share/"fonts").install "OpenSans-LightItalic.ttf"
+    (share/"fonts").install "OpenSans-Regular.ttf"
+    (share/"fonts").install "OpenSans-Semibold.ttf"
+    (share/"fonts").install "OpenSans-SemiboldItalic.ttf"
+  end
+  test do
+  end
+end
