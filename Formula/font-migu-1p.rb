f561b46804799e51c820055528c4eea4f2515544
asciian <asciian@outlook.jp>
2018-03-29 19:03:25 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-migu-1p.rb to Formula/font-migu-1p.rb

---
+++ Formula/font-migu-1p.rb
@@ -0,0 +1,13 @@
+class FontMigu1p < Formula
+  version "20150712"
+  sha256 "9406399eeb94bb98f0844c2cd6bc94c390d994e6705af56e550d27f2a30b4bd5"
+  url "http://dl.osdn.jp/mix-mplus-ipa/63545/migu-1p-#{version}.zip"
+  desc "Migu 1P"
+  homepage "http://mix-mplus-ipa.osdn.jp/migu/#migu1p"
+  def install
+    (share/"fonts").install "migu-1p-#{version}/migu-1p-bold.ttf"
+    (share/"fonts").install "migu-1p-#{version}/migu-1p-regular.ttf"
+  end
+  test do
+  end
+end
