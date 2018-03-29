1f35bcf4a860ec760164f6ff7a7d7f1f69a205a3
asciian <asciian@outlook.jp>
2018-03-29 18:59:03 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-doulos-sil.rb to Formula/font-doulos-sil.rb

---
+++ Formula/font-doulos-sil.rb
@@ -0,0 +1,12 @@
+class FontDoulosSil < Formula
+  version "5.000"
+  sha256 "0b309c3db813a98ce884c0bd25c7f5c0bd96bbffd076459e39298812ca22472e"
+  url "http://software.sil.org/downloads/r/doulos/DoulosSIL-#{version}.zip"
+  desc "Doulos SIL"
+  homepage "http://software.sil.org/doulos/"
+  def install
+    (share/"fonts").install "DoulosSIL-#{version}/DoulosSIL-R.ttf"
+  end
+  test do
+  end
+end
