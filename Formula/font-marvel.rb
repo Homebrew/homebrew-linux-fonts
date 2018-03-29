f370ba47053e76e24670999ab338d8560f9e3a31
asciian <asciian@outlook.jp>
2018-03-29 19:02:57 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-marvel.rb to Formula/font-marvel.rb

---
+++ Formula/font-marvel.rb
@@ -0,0 +1,13 @@
+class FontMarvel < Formula
+  head "https://github.com/google/fonts/trunk/ofl/marvel", using: :svn, revision: "50", trust_cert: true
+  desc "Marvel"
+  homepage "https://www.google.com/fonts/specimen/Marvel"
+  def install
+    (share/"fonts").install "Marvel-Bold.ttf"
+    (share/"fonts").install "Marvel-BoldItalic.ttf"
+    (share/"fonts").install "Marvel-Italic.ttf"
+    (share/"fonts").install "Marvel-Regular.ttf"
+  end
+  test do
+  end
+end
