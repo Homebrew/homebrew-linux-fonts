24f584bf51d80f381af1293ff4d3ec0cb4b0123b
asciian <asciian@outlook.jp>
2018-03-29 18:59:11 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-economica.rb to Formula/font-economica.rb

---
+++ Formula/font-economica.rb
@@ -0,0 +1,13 @@
+class FontEconomica < Formula
+  head "https://github.com/google/fonts/trunk/ofl/economica", using: :svn, revision: "50", trust_cert: true
+  desc "Economica"
+  homepage "https://www.google.com/fonts/specimen/Economica"
+  def install
+    (share/"fonts").install "Economica-Bold.ttf"
+    (share/"fonts").install "Economica-BoldItalic.ttf"
+    (share/"fonts").install "Economica-Italic.ttf"
+    (share/"fonts").install "Economica-Regular.ttf"
+  end
+  test do
+  end
+end
