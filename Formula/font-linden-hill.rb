8b888b105a3f35b42e6d625aa8d8bffc10bf7458
asciian <asciian@outlook.jp>
2018-03-29 19:02:33 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-linden-hill.rb to Formula/font-linden-hill.rb

---
+++ Formula/font-linden-hill.rb
@@ -0,0 +1,11 @@
+class FontLindenHill < Formula
+  head "https://github.com/google/fonts/trunk/ofl/lindenhill", using: :svn, revision: "50", trust_cert: true
+  desc "Linden Hill"
+  homepage "https://www.google.com/fonts/specimen/Linden%20Hill"
+  def install
+    (share/"fonts").install "LindenHill-Italic.ttf"
+    (share/"fonts").install "LindenHill-Regular.ttf"
+  end
+  test do
+  end
+end
