4f53258f8ccb81878c329f5cf54a84e2aa8c8171
asciian <asciian@outlook.jp>
2018-03-29 19:06:54 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-puritan.rb to Formula/font-puritan.rb

---
+++ Formula/font-puritan.rb
@@ -0,0 +1,13 @@
+class FontPuritan < Formula
+  head "https://github.com/google/fonts/trunk/ofl/puritan", using: :svn, revision: "50", trust_cert: true
+  desc "Puritan"
+  homepage "https://www.google.com/fonts/specimen/Puritan"
+  def install
+    (share/"fonts").install "Puritan-Bold.ttf"
+    (share/"fonts").install "Puritan-BoldItalic.ttf"
+    (share/"fonts").install "Puritan-Italic.ttf"
+    (share/"fonts").install "Puritan-Regular.ttf"
+  end
+  test do
+  end
+end
