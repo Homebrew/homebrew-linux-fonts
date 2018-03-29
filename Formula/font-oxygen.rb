243259982baf6623e5dcd12b3497b32c3484992d
asciian <asciian@outlook.jp>
2018-03-29 19:06:15 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-oxygen.rb to Formula/font-oxygen.rb

---
+++ Formula/font-oxygen.rb
@@ -0,0 +1,12 @@
+class FontOxygen < Formula
+  head "https://github.com/google/fonts/trunk/ofl/oxygen", using: :svn, revision: "50", trust_cert: true
+  desc "Oxygen"
+  homepage "https://www.google.com/fonts/specimen/Oxygen"
+  def install
+    (share/"fonts").install "Oxygen-Bold.ttf"
+    (share/"fonts").install "Oxygen-Light.ttf"
+    (share/"fonts").install "Oxygen-Regular.ttf"
+  end
+  test do
+  end
+end
