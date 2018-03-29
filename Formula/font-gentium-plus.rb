0ffdcb51189dfb2654117819fe68aea8b3af1661
asciian <asciian@outlook.jp>
2018-03-29 19:00:09 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-gentium-plus.rb to Formula/font-gentium-plus.rb

---
+++ Formula/font-gentium-plus.rb
@@ -0,0 +1,13 @@
+class FontGentiumPlus < Formula
+  version "5.000"
+  sha256 "335911f17bd2de4e43742e1d0367cfeff19a90abf7ed604f100a42705042e154"
+  url "http://software.sil.org/downloads/r/gentium/GentiumPlus-#{version}.zip"
+  desc "Gentium"
+  homepage "http://software.sil.org/gentium/"
+  def install
+    (share/"fonts").install "GentiumPlus-5.000/GentiumPlus-I.ttf"
+    (share/"fonts").install "GentiumPlus-5.000/GentiumPlus-R.ttf"
+  end
+  test do
+  end
+end
