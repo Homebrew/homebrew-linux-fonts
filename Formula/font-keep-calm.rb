f6e39cb5a4e76eeddc93d52c78fee5856d229b2f
asciian <asciian@outlook.jp>
2018-03-29 19:01:56 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-keep-calm.rb to Formula/font-keep-calm.rb

---
+++ Formula/font-keep-calm.rb
@@ -0,0 +1,10 @@
+class FontKeepCalm < Formula
+  head "http://dl.1001fonts.com/keep-calm.zip"
+  desc "Keep Calm"
+  homepage "http://www.1001fonts.com/keep-calm-font.html"
+  def install
+    (share/"fonts").install "Keep Calm Medium – Personal Use/KeepCalm-Medium.ttf"
+  end
+  test do
+  end
+end
