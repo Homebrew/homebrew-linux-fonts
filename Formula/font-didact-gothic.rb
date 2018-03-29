663761035dea2022e6124124a51234ea981e6acf
asciian <asciian@outlook.jp>
2018-03-29 18:58:55 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-didact-gothic.rb to Formula/font-didact-gothic.rb

---
+++ Formula/font-didact-gothic.rb
@@ -0,0 +1,12 @@
+class FontDidactGothic < Formula
+  version "20110429"
+  sha256 "b98f9e091b6337a86f4e0f69c94c31905e1e287782853f472e176b4e28d3f81f"
+  url "https://github.com/google/fonts/raw/master/ofl/didactgothic/DidactGothic-Regular.ttf"
+  desc "Didact Gothic"
+  homepage "https://www.google.com/fonts/specimen/Didact%20Gothic"
+  def install
+    (share/"fonts").install "DidactGothic.ttf"
+  end
+  test do
+  end
+end
