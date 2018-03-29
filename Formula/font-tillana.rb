bef5340d5407039e0b0c619b3710313262896445
asciian <asciian@outlook.jp>
2018-03-29 19:08:55 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-tillana.rb to Formula/font-tillana.rb

---
+++ Formula/font-tillana.rb
@@ -0,0 +1,16 @@
+class FontTillana < Formula
+  version "2.002"
+  sha256 "0a7965cffee3765ac59dd3beae43f30ace6f1e5cbd9806d86aa26fd90a5ac7c4"
+  url "https://github.com/itfoundry/tillana/releases/download/v#{version}/tillana-#{version.to_s.gsub(".", "_")}.zip"
+  desc "Tillana"
+  homepage "https://github.com/itfoundry/tillana"
+  def install
+    (share/"fonts").install "Tillana-Bold.otf"
+    (share/"fonts").install "Tillana-ExtraBold.otf"
+    (share/"fonts").install "Tillana-Medium.otf"
+    (share/"fonts").install "Tillana-Regular.otf"
+    (share/"fonts").install "Tillana-SemiBold.otf"
+  end
+  test do
+  end
+end
