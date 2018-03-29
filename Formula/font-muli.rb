ae2a5c50dc68052bff978a5a574ebabf3c5b319f
asciian <asciian@outlook.jp>
2018-03-29 19:03:56 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-muli.rb to Formula/font-muli.rb

---
+++ Formula/font-muli.rb
@@ -0,0 +1,23 @@
+class FontMuli < Formula
+  head "https://github.com/google/fonts/trunk/ofl/muli", using: :svn, trust_cert: true
+  desc "Muli"
+  homepage "https://www.google.com/fonts/specimen/Muli"
+  def install
+    (share/"fonts").install "Muli-Black.ttf"
+    (share/"fonts").install "Muli-BlackItalic.ttf"
+    (share/"fonts").install "Muli-Bold.ttf"
+    (share/"fonts").install "Muli-BoldItalic.ttf"
+    (share/"fonts").install "Muli-ExtraBold.ttf"
+    (share/"fonts").install "Muli-ExtraBoldItalic.ttf"
+    (share/"fonts").install "Muli-ExtraLight.ttf"
+    (share/"fonts").install "Muli-ExtraLightItalic.ttf"
+    (share/"fonts").install "Muli-Italic.ttf"
+    (share/"fonts").install "Muli-Light.ttf"
+    (share/"fonts").install "Muli-LightItalic.ttf"
+    (share/"fonts").install "Muli-Regular.ttf"
+    (share/"fonts").install "Muli-SemiBold.ttf"
+    (share/"fonts").install "Muli-SemiBoldItalic.ttf"
+  end
+  test do
+  end
+end
