583ea435e7d569354d54bced5a7fe0f7ad377bbd
asciian <asciian@outlook.jp>
2018-03-29 19:07:30 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-rufina.rb to Formula/font-rufina.rb

---
+++ Formula/font-rufina.rb
@@ -0,0 +1,11 @@
+class FontRufina < Formula
+  head "https://github.com/google/fonts/trunk/ofl/rufina", using: :svn, revision: "50", trust_cert: true
+  desc "Rufina"
+  homepage "https://www.google.com/fonts/specimen/Rufina"
+  def install
+    (share/"fonts").install "Rufina-Bold.ttf"
+    (share/"fonts").install "Rufina-Regular.ttf"
+  end
+  test do
+  end
+end
