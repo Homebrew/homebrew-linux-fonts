9e4901a1019daa4cf485fcd6fc31d6b8a272e43d
asciian <asciian@outlook.jp>
2018-03-29 19:07:33 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-rupakara.rb to Formula/font-rupakara.rb

---
+++ Formula/font-rupakara.rb
@@ -0,0 +1,15 @@
+class FontRupakara < Formula
+  version "1.0.4"
+  sha256 "d6ad49842444846ad0ef2ea40d471533993a3137a7aceacbb28b5a8d89aaddc0"
+  url "http://www.evertype.com/fonts/rupakara/rupakara.zip"
+  desc "Rupakara"
+  homepage "http://www.evertype.com/fonts/rupakara/"
+  def install
+    (share/"fonts").install "rupakara-#{version}/Rupakara-Bold.ttf"
+    (share/"fonts").install "rupakara-#{version}/Rupakara-BoldOblique.ttf"
+    (share/"fonts").install "rupakara-#{version}/Rupakara-Oblique.ttf"
+    (share/"fonts").install "rupakara-#{version}/Rupakara.ttf"
+  end
+  test do
+  end
+end
