7c37cee4a7c5a6404810317a4f5861f7cc08e692
asciian <asciian@outlook.jp>
2018-03-29 18:58:23 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-corben.rb to Formula/font-corben.rb

---
+++ Formula/font-corben.rb
@@ -0,0 +1,11 @@
+class FontCorben < Formula
+  head "https://github.com/google/fonts/trunk/ofl/corben", using: :svn, revision: "50", trust_cert: true
+  desc "Corben"
+  homepage "https://www.google.com/fonts/specimen/Corben"
+  def install
+    (share/"fonts").install "Corben-Bold.ttf"
+    (share/"fonts").install "Corben-Regular.ttf"
+  end
+  test do
+  end
+end
