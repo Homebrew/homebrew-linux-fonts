7706ad458182bf8865c3937d8c61a58d545037e7
asciian <asciian@outlook.jp>
2018-03-29 19:01:08 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-im-fell-french-canon.rb to Formula/font-im-fell-french-canon.rb

---
+++ Formula/font-im-fell-french-canon.rb
@@ -0,0 +1,11 @@
+class FontImFellFrenchCanon < Formula
+  head "https://github.com/google/fonts/trunk/ofl/imfellfrenchcanon", using: :svn, revision: "50", trust_cert: true
+  desc "IM Fell French Canon"
+  homepage "https://www.google.com/fonts/specimen/IM%20Fell%20French%20Canon"
+  def install
+    (share/"fonts").install "IMFeFCit28P.ttf"
+    (share/"fonts").install "IMFeFCrm28P.ttf"
+  end
+  test do
+  end
+end
