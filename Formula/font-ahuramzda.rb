5fab09311ce2c10c70af3065f29df59244568257
asciian <asciian@outlook.jp>
2018-03-29 18:56:04 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-ahuramzda.rb to Formula/font-ahuramzda.rb

---
+++ Formula/font-ahuramzda.rb
@@ -0,0 +1,12 @@
+class FontAhuramzda < Formula
+  version "1.000"
+  sha256 "5a8afb0b24ceeb98f1ef121406ceecb124f2a300c5ee7877a7ff6abdd697b160"
+  url "http://openfontlibrary.org/assets/downloads/ahuramazda/b2c0eeb9186f389749746f075b5a1abf/ahuramazda.zip"
+  desc "Ahuramzda"
+  homepage "http://openfontlibrary.org/font/ahuramazda/"
+  def install
+    (share/"fonts").install "Ahuramazda-Avestan-Font-1.0/ahuramazda.ttf"
+  end
+  test do
+  end
+end
