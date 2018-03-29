625a7ba9f855754d04fc889c073a2dd0471d550d
asciian <asciian@outlook.jp>
2018-03-29 19:09:39 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-wenquanyi-micro-hei.rb to Formula/font-wenquanyi-micro-hei.rb

---
+++ Formula/font-wenquanyi-micro-hei.rb
@@ -0,0 +1,12 @@
+class FontWenquanyiMicroHei < Formula
+  version "0.2.0-beta"
+  sha256 "2802ac8023aa36a66ea6e7445854e3a078d377ffff42169341bd237871f7213e"
+  url "https://downloads.sourceforge.net/wqy/wqy-microhei-#{version}.tar.gz"
+  desc "WenQuanYi Micro Hei"
+  homepage "http://wenq.org/wqy2/index.cgi?MicroHei"
+  def install
+    (share/"fonts").install "wqy-microhei/wqy-microhei.ttc"
+  end
+  test do
+  end
+end
