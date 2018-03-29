d789dda244bbaec1d87d51e93c2c213ca44b6056
asciian <asciian@outlook.jp>
2018-03-29 19:03:37 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-monoid-nerd-font-mono.rb to Formula/font-monoid-nerd-font-mono.rb

---
+++ Formula/font-monoid-nerd-font-mono.rb
@@ -0,0 +1,15 @@
+class FontMonoidNerdFontMono < Formula
+  version "1.2.0"
+  sha256 "57d16688a7b77f920472fb44e35dfae4cd4c8a3b718e1c622a84519f39231c5c"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Monoid.zip"
+  desc "Monoid Nerd Font (Monoid)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    (share/"fonts").install "Monoid Retina Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Monoid Regular Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Monoid Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Monoid Italic Nerd Font Complete Mono.ttf"
+  end
+  test do
+  end
+end
