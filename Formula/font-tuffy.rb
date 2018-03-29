2820e9edf290e7f19e8ff14c8395f3afd8074662
asciian <asciian@outlook.jp>
2018-03-29 19:09:04 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-tuffy.rb to Formula/font-tuffy.rb

---
+++ Formula/font-tuffy.rb
@@ -0,0 +1,15 @@
+class FontTuffy < Formula
+  version "20120614"
+  sha256 "c6f9eb7a878e270b96fbb5dae7783be29710b4405c76e856de60dcfc96386e0b"
+  url "http://tulrich.com/fonts/tuffy-#{version}.tar.gz"
+  desc "Tuffy"
+  homepage "http://tulrich.com/fonts/"
+  def install
+    (share/"fonts").install "tuffy-#{version}/Tuffy.otf"
+    (share/"fonts").install "tuffy-#{version}/Tuffy_Bold.otf"
+    (share/"fonts").install "tuffy-#{version}/Tuffy_Bold_Italic.otf"
+    (share/"fonts").install "tuffy-#{version}/Tuffy_Italic.otf"
+  end
+  test do
+  end
+end
