71032446823a6f0a0f28bbedeff8356276ee63ca
asciian <asciian@outlook.jp>
2018-03-29 19:00:10 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-geo.rb to Formula/font-geo.rb

---
+++ Formula/font-geo.rb
@@ -0,0 +1,11 @@
+class FontGeo < Formula
+  head "https://github.com/google/fonts/trunk/ofl/geo", using: :svn, revision: "50", trust_cert: true
+  desc "Geo"
+  homepage "https://www.google.com/fonts/specimen/Geo"
+  def install
+    (share/"fonts").install "Geo-Oblique.ttf"
+    (share/"fonts").install "Geo-Regular.ttf"
+  end
+  test do
+  end
+end
