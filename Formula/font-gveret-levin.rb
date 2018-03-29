364949217422b34c79f0c266963be15c14df5278
asciian <asciian@outlook.jp>
2018-03-29 19:00:35 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-gveret-levin.rb to Formula/font-gveret-levin.rb

---
+++ Formula/font-gveret-levin.rb
@@ -0,0 +1,10 @@
+class FontGveretLevin < Formula
+  head "https://alefalefalef.co.il/wp-content/themes/aaa/fonts/free/GveretLevinAlefAlefAlef.zip"
+  desc "Gveret Levin"
+  homepage "https://alefalefalef.co.il/resources/פונטים-בחינם/"
+  def install
+    (share/"fonts").install "otf/GveretLevinAlefAlefAlef-Regular.otf"
+  end
+  test do
+  end
+end
