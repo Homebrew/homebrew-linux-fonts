9d2cab2313544cdfa19adcfcb75946f81885fd73
asciian <asciian@outlook.jp>
2018-03-29 18:56:25 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-andika.rb to Formula/font-andika.rb

---
+++ Formula/font-andika.rb
@@ -0,0 +1,12 @@
+class FontAndika < Formula
+  version "5.000"
+  sha256 "604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507"
+  url "http://software.sil.org/downloads/r/andika/Andika-#{version}.zip"
+  desc "Andika"
+  homepage "http://software.sil.org/andika/"
+  def install
+    (share/"fonts").install "Andika-#{version}/Andika-R.ttf"
+  end
+  test do
+  end
+end
