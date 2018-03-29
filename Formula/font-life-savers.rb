ca22d670f8827d1e039c5c32d37084d71bde6ad3
asciian <asciian@outlook.jp>
2018-03-29 19:02:29 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-life-savers.rb to Formula/font-life-savers.rb

---
+++ Formula/font-life-savers.rb
@@ -0,0 +1,11 @@
+class FontLifeSavers < Formula
+  head "https://github.com/google/fonts/trunk/ofl/lifesavers", using: :svn, revision: "50", trust_cert: true
+  desc "Life Savers"
+  homepage "https://www.google.com/fonts/specimen/Life%20Savers"
+  def install
+    (share/"fonts").install "LifeSavers-Bold.ttf"
+    (share/"fonts").install "LifeSavers-Regular.ttf"
+  end
+  test do
+  end
+end
