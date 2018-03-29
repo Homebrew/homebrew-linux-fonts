d17b800f61191d9497916cfb6927bf5fe820973c
asciian <asciian@outlook.jp>
2018-03-29 19:03:50 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-mplus-nerd-font-mono.rb to Formula/font-mplus-nerd-font-mono.rb

---
+++ Casks/font-mplus-nerd-font-mono.rb
@@ -0,0 +1,16 @@
+cask 'font-mplus-nerd-font-mono' do
+  version '1.2.0'
+  sha256 'acd1401bfd0de8804db02c782e01e03ccf9fc9fe78d056d73f7f352b07269230'
+
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/MPlus.zip"
+  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+  name 'mplus Nerd Font (MPlus)'
+  homepage 'https://github.com/ryanoasis/nerd-fonts'
+
+  font 'M+ 1m bold Nerd Font Complete Mono.ttf'
+  font 'M+ 1m medium Nerd Font Complete Mono.ttf'
+  font 'M+ 1m regular Nerd Font Complete Mono.ttf'
+  font 'M+ 1m thin Nerd Font Complete Mono.ttf'
+  font 'M+ 1m light Nerd Font Complete Mono.ttf'
+end
