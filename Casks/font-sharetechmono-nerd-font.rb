85b50a7d3e1e6eefd23d191f8473356532d20dae
asciian <asciian@outlook.jp>
2018-03-29 19:07:56 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-sharetechmono-nerd-font.rb to Formula/font-sharetechmono-nerd-font.rb

---
+++ Casks/font-sharetechmono-nerd-font.rb
@@ -0,0 +1,12 @@
+cask 'font-sharetechmono-nerd-font' do
+  version '1.2.0'
+  sha256 '0840c2ff6bbc887c716f57991a101ad9a2a7cb304572e7f964fbe9e36a08350d'
+
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ShareTechMono.zip"
+  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+  name 'ShureTechMono Nerd Font (ShareTechMono)'
+  homepage 'https://github.com/ryanoasis/nerd-fonts'
+
+  font 'Shure Tech Mono Nerd Font Complete.ttf'
+end
