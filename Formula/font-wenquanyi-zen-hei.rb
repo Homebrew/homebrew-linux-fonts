f62e606864b3d8aaac7f9881d5625b830f22688e
asciian <asciian@outlook.jp>
2018-03-29 19:09:39 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-wenquanyi-zen-hei.rb to Formula/font-wenquanyi-zen-hei.rb

---
+++ Formula/font-wenquanyi-zen-hei.rb
@@ -0,0 +1,12 @@
+class FontWenquanyiZenHei < Formula
+  version "0.9.45"
+  sha256 "e4b7e306475bf9427d1757578f0e4528930c84c44eaa3f167d4c42f110ee75d6"
+  url "https://downloads.sourceforge.net/wqy/wqy-zenhei-#{version}.tar.gz"
+  desc "WenQuanYi Zen Hei"
+  homepage "http://wenq.org/wqy2/index.cgi?ZenHei"
+  def install
+    (share/"fonts").install "wqy-zenhei/wqy-zenhei.ttc"
+  end
+  test do
+  end
+end
