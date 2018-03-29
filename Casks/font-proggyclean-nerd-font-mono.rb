9de375f45988530b0ad4ba9f95459c38da014de5
asciian <asciian@outlook.jp>
2018-03-29 19:06:49 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-proggyclean-nerd-font-mono.rb to Formula/font-proggyclean-nerd-font-mono.rb

---
+++ Casks/font-proggyclean-nerd-font-mono.rb
@@ -0,0 +1,14 @@
+cask 'font-proggyclean-nerd-font-mono' do
+  version '1.2.0'
+  sha256 '81accf645939031c23dadb79eae8fef8a1b0851cb121353ab20f0405ca5da905'
+
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ProggyClean.zip"
+  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+  name 'ProggyCleanTTCE Nerd Font,ProggyCleanTT CE (ProggyClean)'
+  homepage 'https://github.com/ryanoasis/nerd-fonts'
+
+  font 'ProggyCleanTT CE Nerd Font Complete Mono.ttf'
+  font 'ProggyCleanTT Nerd Font Complete Mono.ttf'
+  font 'ProggyCleanTTSZ Nerd Font Complete Mono.ttf'
+end
