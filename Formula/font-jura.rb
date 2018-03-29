8d43903ba77363f36472e1cab28a4c760dacb7f2
asciian <asciian@outlook.jp>
2018-03-29 19:01:46 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-jura.rb to Formula/font-jura.rb

---
+++ Formula/font-jura.rb
@@ -0,0 +1,13 @@
+class FontJura < Formula
+  head "https://github.com/google/fonts/trunk/ofl/jura", using: :svn, revision: "50", trust_cert: true
+  desc "Jura"
+  homepage "https://www.google.com/fonts/specimen/Jura"
+  def install
+    (share/"fonts").install "Jura-DemiBold.ttf"
+    (share/"fonts").install "Jura-Light.ttf"
+    (share/"fonts").install "Jura-Medium.ttf"
+    (share/"fonts").install "Jura-Regular.ttf"
+  end
+  test do
+  end
+end
