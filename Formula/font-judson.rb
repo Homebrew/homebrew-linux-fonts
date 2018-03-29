b6ef93c014d8704fbf1407a1501bf383723048ad
asciian <asciian@outlook.jp>
2018-03-29 19:01:41 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-judson.rb to Formula/font-judson.rb

---
+++ Formula/font-judson.rb
@@ -0,0 +1,12 @@
+class FontJudson < Formula
+  head "https://github.com/google/fonts/trunk/ofl/judson", using: :svn, revision: "50", trust_cert: true
+  desc "Judson"
+  homepage "https://www.google.com/fonts/specimen/Judson"
+  def install
+    (share/"fonts").install "Judson-Bold.ttf"
+    (share/"fonts").install "Judson-Italic.ttf"
+    (share/"fonts").install "Judson-Regular.ttf"
+  end
+  test do
+  end
+end
