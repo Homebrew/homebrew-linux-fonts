17b1187072da7bf7a258895ca8bcea64ae64e627
asciian <asciian@outlook.jp>
2018-03-29 19:08:57 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-tinos-nerd-font-mono.rb to Formula/font-tinos-nerd-font-mono.rb

---
+++ Formula/font-tinos-nerd-font-mono.rb
@@ -0,0 +1,15 @@
+class FontTinosNerdFontMono < Formula
+  version "1.2.0"
+  sha256 "b8e90e6a3cdaff3fff2f928295d4000f07eb75fd5fa6fa72a88304891ee0cc1c"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Tinos.zip"
+  desc "Tinos Nerd Font Mono (Tinos)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    (share/"fonts").install "Tinos Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Tinos Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Tinos Bold Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Tinos Bold Nerd Font Complete Mono.ttf"
+  end
+  test do
+  end
+end
