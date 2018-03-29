6840f42018bb1be1aa9a7a2da5c357dc01141ea5
asciian <asciian@outlook.jp>
2018-03-29 19:00:28 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-gorditas.rb to Formula/font-gorditas.rb

---
+++ Formula/font-gorditas.rb
@@ -0,0 +1,11 @@
+class FontGorditas < Formula
+  head "https://github.com/google/fonts/trunk/ofl/gorditas", using: :svn, revision: "50", trust_cert: true
+  desc "Gorditas"
+  homepage "https://www.google.com/fonts/specimen/Gorditas"
+  def install
+    (share/"fonts").install "Gorditas-Bold.ttf"
+    (share/"fonts").install "Gorditas-Regular.ttf"
+  end
+  test do
+  end
+end
