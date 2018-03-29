2c1bc485071a9d432ac0c914b60b319c2c377140
asciian <asciian@outlook.jp>
2018-03-29 18:58:31 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-coustard.rb to Formula/font-coustard.rb

---
+++ Formula/font-coustard.rb
@@ -0,0 +1,11 @@
+class FontCoustard < Formula
+  head "https://github.com/google/fonts/trunk/ofl/coustard", using: :svn, revision: "50", trust_cert: true
+  desc "Coustard"
+  homepage "https://www.google.com/fonts/specimen/Coustard"
+  def install
+    (share/"fonts").install "Coustard-Black.ttf"
+    (share/"fonts").install "Coustard-Regular.ttf"
+  end
+  test do
+  end
+end
