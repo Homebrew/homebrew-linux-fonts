e36d9a927c6db378ed97f46177b7ef4f20775d2d
asciian <asciian@outlook.jp>
2018-03-29 19:08:33 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-sue-ellen-francisco.rb to Formula/font-sue-ellen-francisco.rb

---
+++ Formula/font-sue-ellen-francisco.rb
@@ -0,0 +1,12 @@
+class FontSueEllenFrancisco < Formula
+  version "1.002"
+  sha256 "71790cdeb69ec4081bea5ca4721c5510f31fd441afcd66e83bdd881b14ddd3e8"
+  url "https://github.com/google/fonts/raw/master/ofl/sueellenfrancisco/SueEllenFrancisco.ttf"
+  desc "Sue Ellen Francisco"
+  homepage "https://www.google.com/fonts/specimen/Sue%20Ellen%20Francisco"
+  def install
+    (share/"fonts").install "SueEllenFrancisco.ttf"
+  end
+  test do
+  end
+end
