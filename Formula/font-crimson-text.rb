829c026396e23df8066400cdcbf075599fe3bbd1
asciian <asciian@outlook.jp>
2018-03-29 18:58:35 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-crimson-text.rb to Formula/font-crimson-text.rb

---
+++ Formula/font-crimson-text.rb
@@ -0,0 +1,15 @@
+class FontCrimsonText < Formula
+  head "https://github.com/google/fonts/trunk/ofl/crimsontext", using: :svn, revision: "50", trust_cert: true
+  desc "Crimson Text"
+  homepage "https://www.google.com/fonts/specimen/Crimson%20Text"
+  def install
+    (share/"fonts").install "CrimsonText-Bold.ttf"
+    (share/"fonts").install "CrimsonText-BoldItalic.ttf"
+    (share/"fonts").install "CrimsonText-Italic.ttf"
+    (share/"fonts").install "CrimsonText-Regular.ttf"
+    (share/"fonts").install "CrimsonText-Semibold.ttf"
+    (share/"fonts").install "CrimsonText-SemiboldItalic.ttf"
+  end
+  test do
+  end
+end
