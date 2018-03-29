92709578a02705e06b1cc25a9833608846112fda
asciian <asciian@outlook.jp>
2018-03-29 18:59:07 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-dukor.rb to Formula/font-dukor.rb

---
+++ Formula/font-dukor.rb
@@ -0,0 +1,13 @@
+class FontDukor < Formula
+  version "1.0.7"
+  sha256 "15933e2f71c4db80f55189ee05957e87af26e517333b33df7b2c4b528a2848f4"
+  url "http://www.evertype.com/fonts/vai/dukorfont.zip"
+  desc "Dukor"
+  homepage "http://www.evertype.com/fonts/vai/"
+  def install
+    (share/"fonts").install "Dukor-#{version}/Dukor.ttf"
+    (share/"fonts").install "Dukor-#{version}/DukorItalic.ttf"
+  end
+  test do
+  end
+end
