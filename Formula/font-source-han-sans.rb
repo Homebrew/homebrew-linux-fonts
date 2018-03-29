296709235de2d2f0acee4f3c209c7aac0688c429
asciian <asciian@outlook.jp>
2018-03-29 19:08:16 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-source-han-sans.rb to Formula/font-source-han-sans.rb

---
+++ Formula/font-source-han-sans.rb
@@ -0,0 +1,12 @@
+class FontSourceHanSans < Formula
+  version "1.004"
+  sha256 "b684c9b659ec4998e72d22ff5ed3f531d31d506ae00a48ec72b9846be23e7c10"
+  url "https://github.com/adobe-fonts/source-han-sans/raw/5f5311e71cb628321cc0cffb51fb38d862b726aa/SuperOTC/SourceHanSans.ttc.zip"
+  desc "Source Han Sans"
+  homepage "https://github.com/adobe-fonts/source-han-sans"
+  def install
+    (share/"fonts").install "SourceHanSans.ttc"
+  end
+  test do
+  end
+end
