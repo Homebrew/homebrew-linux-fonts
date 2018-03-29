8aa08e6747a09140b8a523a1a1e196b3e23fb2a4
asciian <asciian@outlook.jp>
2018-03-29 18:56:56 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-averia-serif-libre.rb to Formula/font-averia-serif-libre.rb

---
+++ Formula/font-averia-serif-libre.rb
@@ -0,0 +1,15 @@
+class FontAveriaSerifLibre < Formula
+  head "https://github.com/google/fonts/trunk/ofl/averiaseriflibre", using: :svn, revision: "50", trust_cert: true
+  desc "Averia Serif Libre"
+  homepage "https://www.google.com/fonts/specimen/Averia%20Serif%20Libre"
+  def install
+    (share/"fonts").install "AveriaSerifLibre-Bold.ttf"
+    (share/"fonts").install "AveriaSerifLibre-BoldItalic.ttf"
+    (share/"fonts").install "AveriaSerifLibre-Italic.ttf"
+    (share/"fonts").install "AveriaSerifLibre-Light.ttf"
+    (share/"fonts").install "AveriaSerifLibre-LightItalic.ttf"
+    (share/"fonts").install "AveriaSerifLibre-Regular.ttf"
+  end
+  test do
+  end
+end
