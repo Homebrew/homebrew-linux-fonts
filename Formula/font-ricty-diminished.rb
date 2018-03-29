8c8d9da847c14dd1b8f12e453d4240d410dcf62b
asciian <asciian@outlook.jp>
2018-03-29 19:07:14 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-ricty-diminished.rb to Formula/font-ricty-diminished.rb

---
+++ Formula/font-ricty-diminished.rb
@@ -0,0 +1,19 @@
+class FontRictyDiminished < Formula
+  version "4.0.1"
+  sha256 "b32ba2877651df31914797cc4938b98d726d4777ecb2064c9dbbb620efcff24f"
+  url "http://www.rs.tus.ac.jp/yyusa/ricty_diminished/ricty_diminished-#{version}.tar.gz"
+  desc "Ricty Diminished"
+  homepage "http://www.rs.tus.ac.jp/yyusa/ricty_diminished.html"
+  def install
+    (share/"fonts").install "RictyDiminished-Bold.ttf"
+    (share/"fonts").install "RictyDiminished-BoldOblique.ttf"
+    (share/"fonts").install "RictyDiminished-Oblique.ttf"
+    (share/"fonts").install "RictyDiminished-Regular.ttf"
+    (share/"fonts").install "RictyDiminishedDiscord-Bold.ttf"
+    (share/"fonts").install "RictyDiminishedDiscord-BoldOblique.ttf"
+    (share/"fonts").install "RictyDiminishedDiscord-Oblique.ttf"
+    (share/"fonts").install "RictyDiminishedDiscord-Regular.ttf"
+  end
+  test do
+  end
+end
