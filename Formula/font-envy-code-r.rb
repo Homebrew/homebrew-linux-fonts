897297c7e1870c692f9c41006d587febc4d8646c
asciian <asciian@outlook.jp>
2018-03-29 18:59:19 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-envy-code-r.rb to Formula/font-envy-code-r.rb

---
+++ Formula/font-envy-code-r.rb
@@ -0,0 +1,14 @@
+class FontEnvyCodeR < Formula
+  version "PR7"
+  sha256 "9f7e9703aaf21110b4e1a54d954d57d4092727546348598a5a8e8101e4597aff"
+  url "http://download.damieng.com/fonts/original/EnvyCodeR-#{version}.zip"
+  desc "Envy Code R"
+  homepage "http://damieng.com/blog/tag/envy-code-r"
+  def install
+    (share/"fonts").install "Envy Code R PR7/Envy Code R Bold.ttf"
+    (share/"fonts").install "Envy Code R PR7/Envy Code R Italic.ttf"
+    (share/"fonts").install "Envy Code R PR7/Envy Code R.ttf"
+  end
+  test do
+  end
+end
