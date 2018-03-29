b90cf26eab96b07576800ec78c626e1e094d81d2
asciian <asciian@outlook.jp>
2018-03-29 18:56:17 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-almendra-sc.rb to Formula/font-almendra-sc.rb

---
+++ Formula/font-almendra-sc.rb
@@ -0,0 +1,13 @@
+class FontAlmendraSc < Formula
+  head "https://github.com/google/fonts/trunk/ofl/almendrasc", using: :svn, revision: "50", trust_cert: true
+  desc "Almendra SC"
+  homepage "https://www.google.com/fonts/specimen/Almendra%20SC"
+  def install
+    (share/"fonts").install "AlmendraSC-Bold.ttf"
+    (share/"fonts").install "AlmendraSC-BoldItalic.ttf"
+    (share/"fonts").install "AlmendraSC-Italic.ttf"
+    (share/"fonts").install "AlmendraSC-Regular.ttf"
+  end
+  test do
+  end
+end
