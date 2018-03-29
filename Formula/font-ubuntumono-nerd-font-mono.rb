8bf59175a7920c86edc39763807a81aead514b35
asciian <asciian@outlook.jp>
2018-03-29 19:09:10 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-ubuntumono-nerd-font-mono.rb to Formula/font-ubuntumono-nerd-font-mono.rb

---
+++ Formula/font-ubuntumono-nerd-font-mono.rb
@@ -0,0 +1,13 @@
+class FontUbuntumonoNerdFontMono < Formula
+  version "1.2.0"
+  sha256 "9b8bacbabf4b7486fd68068507b208f6d2a017a6909521d19aeeb6f3026f3f6d"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/UbuntuMono.zip"
+  desc "UbuntuMono Nerd Font (UbuntuMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    (share/"fonts").install "Ubuntu Mono Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Ubuntu Mono derivative Powerline Nerd Font Complete Mono.ttf"
+  end
+  test do
+  end
+end
