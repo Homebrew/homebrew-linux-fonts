7800db640ae67f622d895132c5be2d5f81f0c860
asciian <asciian@outlook.jp>
2018-03-29 18:56:50 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-aurulentsansmono-nerd-font-mono.rb to Formula/font-aurulentsansmono-nerd-font-mono.rb

---
+++ Casks/font-aurulentsansmono-nerd-font-mono.rb
@@ -0,0 +1,12 @@
+cask 'font-aurulentsansmono-nerd-font-mono' do
+  version '1.2.0'
+  sha256 'e72de2e8a8d22afa7ce3aafb819a9a11487c4fc2165e317a1a54b39f471cf5f4'
+
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
+  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+  name 'AurulentSansMono Nerd Font (AurulentSansMono)'
+  homepage 'https://github.com/ryanoasis/nerd-fonts'
+
+  font 'AurulentSansMono-Regular Nerd Font Complete Mono.otf'
+end
