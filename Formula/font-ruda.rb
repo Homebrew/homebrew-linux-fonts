7501ba0e237baca6598aa6da4533143e2b8bc300
asciian <asciian@outlook.jp>
2018-03-29 19:07:29 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-ruda.rb to Formula/font-ruda.rb

---
+++ Formula/font-ruda.rb
@@ -0,0 +1,12 @@
+class FontRuda < Formula
+  head "https://github.com/google/fonts/trunk/ofl/ruda", using: :svn, revision: "50", trust_cert: true
+  desc "Ruda"
+  homepage "https://www.google.com/fonts/specimen/Ruda"
+  def install
+    (share/"fonts").install "Ruda-Black.ttf"
+    (share/"fonts").install "Ruda-Bold.ttf"
+    (share/"fonts").install "Ruda-Regular.ttf"
+  end
+  test do
+  end
+end
