05e05d0366f405e073641af0a2951c30e625d3c0
asciian <asciian@outlook.jp>
2018-03-29 18:56:19 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-amaranth.rb to Formula/font-amaranth.rb

---
+++ Formula/font-amaranth.rb
@@ -0,0 +1,13 @@
+class FontAmaranth < Formula
+  head "https://github.com/google/fonts/trunk/ofl/amaranth", using: :svn, revision: "50", trust_cert: true
+  desc "Amaranth"
+  homepage "https://www.google.com/fonts/specimen/Amaranth"
+  def install
+    (share/"fonts").install "Amaranth-Bold.ttf"
+    (share/"fonts").install "Amaranth-BoldItalic.ttf"
+    (share/"fonts").install "Amaranth-Italic.ttf"
+    (share/"fonts").install "Amaranth-Regular.ttf"
+  end
+  test do
+  end
+end
