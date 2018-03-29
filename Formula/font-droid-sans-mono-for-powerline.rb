9c1fc578f446b34a93af518724901be02600d91e
asciian <asciian@outlook.jp>
2018-03-29 18:59:05 +0900

    Convert Caskroom/homebrew-fonts/Casks/font-droid-sans-mono-for-powerline.rb to Formula/font-droid-sans-mono-for-powerline.rb

---
+++ Formula/font-droid-sans-mono-for-powerline.rb
@@ -0,0 +1,10 @@
+class FontDroidSansMonoForPowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/DroidSansMono", using: :svn, trust_cert: true
+  desc "Droid Sans Mono for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/DroidSansMono"
+  def install
+    (share/"fonts").install "Droid Sans Mono for Powerline.otf"
+  end
+  test do
+  end
+end
