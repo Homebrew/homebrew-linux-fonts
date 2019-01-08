369a510eaf659cff46c54cacb751e399fe72a38f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-short-stack.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-short-stack.rb
@@ -0,0 +1,9 @@
+class FontShortStack < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/shortstack/ShortStack-Regular.ttf"
+  desc "Short Stack"
+  homepage "https://www.google.com/fonts/specimen/Short+Stack"
+  def install
+    
+  test do
+  end
+end
