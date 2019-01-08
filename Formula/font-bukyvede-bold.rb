e46beb20ebffe3093e7f96265c95ef92523fa54e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bukyvede-bold.rb from 7e47753cb3caf68dedf6e83c6112f824376afae8

---
+++ Formula/font-bukyvede-bold.rb
@@ -0,0 +1,9 @@
+class FontBukyvedeBold < Formula
+  head "http://kodeks.uni-bamberg.de/aksl/media/BukyVede-Bold.ttf"
+  desc "BukyVede Bold"
+  homepage "http://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm"
+  def install
+    
+  test do
+  end
+end
