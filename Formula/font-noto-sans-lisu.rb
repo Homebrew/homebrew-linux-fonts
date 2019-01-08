b6b4d0f3163b2c4e5ac80f89f84d7d8d51725e70
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:47 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-lisu.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-lisu.rb
@@ -0,0 +1,9 @@
+class FontNotoSansLisu < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLisu-unhinted.zip"
+  desc "Noto Sans Lisu"
+  homepage "https://www.google.com/get/noto/#sans-lisu"
+  def install
+    
+  test do
+  end
+end
