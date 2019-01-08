eaf8a5d2d043ff298e092f67971a758de0dfeeb2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-yellowtail.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-yellowtail.rb
@@ -0,0 +1,9 @@
+class FontYellowtail < Formula
+  head "https://github.com/google/fonts/raw/master/apache/yellowtail/Yellowtail-Regular.ttf"
+  desc "Yellowtail"
+  homepage "https://www.google.com/fonts/specimen/Yellowtail"
+  def install
+    
+  test do
+  end
+end
