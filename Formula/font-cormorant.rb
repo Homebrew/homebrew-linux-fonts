2cf10a4e5a6e11c9ca266e59fdc87680a8f35661
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cormorant.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-cormorant.rb
@@ -0,0 +1,11 @@
+class FontCormorant < Formula
+  version "3.504"
+  sha256 "0915ccd7d396a50c2f4e83846717c0fe3afda1a0da8e6800d5382b24647c5f45"
+  url "https://github.com/CatharsisFonts/Cormorant/releases/download/v#{version}/Cormorant_Install_v#{version}.zip"
+  desc "Cormorant"
+  homepage "https://github.com/CatharsisFonts/Cormorant/"
+  def install
+    
+  test do
+  end
+end
