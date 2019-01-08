c7b5396d0104a7b1f840586b2f2f7a48a4514fec
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-macondo-swash-caps.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-macondo-swash-caps.rb
@@ -0,0 +1,9 @@
+class FontMacondoSwashCaps < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/macondoswashcaps/MacondoSwashCaps-Regular.ttf"
+  desc "Macondo Swash Caps"
+  homepage "https://www.google.com/fonts/specimen/Macondo+Swash+Caps"
+  def install
+    
+  test do
+  end
+end
