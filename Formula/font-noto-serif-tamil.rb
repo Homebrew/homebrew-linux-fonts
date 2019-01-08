0b335aae4100eb1d650e72cf0ba682da1f358aaf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:29 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-tamil.rb from 6612748208449843385714429f09395a19960b12

---
+++ Formula/font-noto-serif-tamil.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifTamil < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifTamil-unhinted.zip"
+  desc "Noto Serif Tamil"
+  homepage "https://www.google.com/get/noto/#serif-taml"
+  def install
+    
+  test do
+  end
+end
