420e80d9739ab77762e7735b386fdf1d10b82ea0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:47 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ranchers.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-ranchers.rb
@@ -0,0 +1,9 @@
+class FontRanchers < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/ranchers/Ranchers-Regular.ttf"
+  desc "Ranchers"
+  homepage "https://www.google.com/fonts/specimen/Ranchers"
+  def install
+    
+  test do
+  end
+end
