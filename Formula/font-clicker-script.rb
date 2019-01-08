872be07a34320f3ba392b5a702ada229a54f7d36
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-clicker-script.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-clicker-script.rb
@@ -0,0 +1,9 @@
+class FontClickerScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/clickerscript/ClickerScript-Regular.ttf"
+  desc "Clicker Script"
+  homepage "https://www.google.com/fonts/specimen/Clicker+Script"
+  def install
+    
+  test do
+  end
+end
