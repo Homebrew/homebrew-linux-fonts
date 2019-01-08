d1114dfadf7e9cc8ee22fc45cd6e5b45f4ffe2cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-julee.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-julee.rb
@@ -0,0 +1,9 @@
+class FontJulee < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/julee/Julee-Regular.ttf"
+  desc "Julee"
+  homepage "https://www.google.com/fonts/specimen/Julee"
+  def install
+    
+  test do
+  end
+end
