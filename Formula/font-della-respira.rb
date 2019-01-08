10d3cb2581eca62a7955cf34fd156923eeebcc6a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-della-respira.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-della-respira.rb
@@ -0,0 +1,9 @@
+class FontDellaRespira < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/dellarespira/DellaRespira-Regular.ttf"
+  desc "Della Respira"
+  homepage "https://www.google.com/fonts/specimen/Della+Respira"
+  def install
+    
+  test do
+  end
+end
