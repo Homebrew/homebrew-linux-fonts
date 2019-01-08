7b486c74439094ec7b4983d2ab0a85ed17d7b848
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-crete-round.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-crete-round.rb
@@ -0,0 +1,10 @@
+class FontCreteRound < Formula
+  head "https://github.com/google/fonts/trunk/ofl/creteround", using: :svn, revision: "50", trust_cert: true
+  desc "Crete Round"
+  homepage "https://www.google.com/fonts/specimen/Crete%20Round"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
