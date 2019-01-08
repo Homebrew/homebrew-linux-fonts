97b50b116f334e6bf10ff6907ff372f25f633347
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-koulen.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-koulen.rb
@@ -0,0 +1,9 @@
+class FontKoulen < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/koulen/Koulen.ttf"
+  desc "Koulen"
+  homepage "https://www.google.com/fonts/specimen/Koulen"
+  def install
+    
+  test do
+  end
+end
