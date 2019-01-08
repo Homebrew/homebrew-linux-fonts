c12d24329a36a5802c332ba5ac811897c71cec42
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gentium-book-basic.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-gentium-book-basic.rb
+++ Formula/font-gentium-book-basic.rb
@@ -1,7 +1,7 @@
 class FontGentiumBookBasic < Formula
   version "1.102"
   sha256 "2f1a2c5491d7305dffd3520c6375d2f3e14931ee35c6d8ae1e8f098bf1a7b3cc"
-  url "https://software.sil.org/downloads/r/gentium/GentiumBasic_#{version.to_s.gsub(".", "")}.zip"
+  head "https://software.sil.org/downloads/r/gentium/GentiumBasic_#{version.no_dots}.zip"
   desc "Gentium Book Basic"
   homepage "https://software.sil.org/gentium/"
   def install
