9e59988abbda674da6dd5b8cd348989a4d1ae388
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nexa.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-nexa.rb
@@ -0,0 +1,9 @@
+class FontNexa < Formula
+  head "https://fontfabric.com/downfont/nexa.zip"
+  desc "Nexa"
+  homepage "https://fontfabric.com/nexa-font/"
+  def install
+    
+  test do
+  end
+end
