003392271b863c2e15d2db14514503ced49ea7a5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fairfax.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-fairfax.rb
@@ -0,0 +1,9 @@
+class FontFairfax < Formula
+  head "https://www.kreativekorp.com/swdownload/fonts/relaybm/fairfax.zip", user_agent: :fake
+  desc "Fairfax"
+  homepage "https://www.kreativekorp.com/software/fonts/constructium.shtml"
+  def install
+    
+  test do
+  end
+end
