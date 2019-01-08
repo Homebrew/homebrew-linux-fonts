fe0f399b134df7263c453eff9c2a1c411efec456
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-unifrakturmaguntia.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-unifrakturmaguntia.rb
@@ -0,0 +1,9 @@
+class FontUnifrakturmaguntia < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/unifrakturmaguntia/UnifrakturMaguntia-Book.ttf"
+  desc "UnifrakturMaguntia"
+  homepage "https://www.google.com/fonts/specimen/UnifrakturMaguntia"
+  def install
+    
+  test do
+  end
+end
