4d4a6d5d583d5fdd3af53bfd556e7c452894fc76
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:29 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bitstreamverasansmono-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-bitstreamverasansmono-nerd-font.rb
@@ -0,0 +1,11 @@
+class FontBitstreamverasansmonoNerdFont < Formula
+  version "2.0.0"
+  sha256 "285c7c08ee2d651cf1f0bdb8a83b787adcab2449ac09b04c23aa4ae17a50d84b"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BitstreamVeraSansMono.zip"
+  desc "BitstreamVeraSansMono Nerd Font (BitstreamVeraSansMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
