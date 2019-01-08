923c7a5ea88ece51b7935c8d7d0d7cf11091bd40
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:47 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lily-script-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-lily-script-one.rb
@@ -0,0 +1,9 @@
+class FontLilyScriptOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/lilyscriptone/LilyScriptOne-Regular.ttf"
+  desc "Lily Script One"
+  homepage "https://www.google.com/fonts/specimen/Lily+Script+One"
+  def install
+    
+  test do
+  end
+end
