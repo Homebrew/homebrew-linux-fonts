ece70ceba6730414cefa519a8683735476fccbab
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:28 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-titillium-web.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-titillium-web.rb
@@ -0,0 +1,10 @@
+class FontTitilliumWeb < Formula
+  head "https://github.com/google/fonts/trunk/ofl/titilliumweb", using: :svn, revision: "50", trust_cert: true
+  desc "Titillium Web"
+  homepage "https://www.google.com/fonts/specimen/Titillium%20Web"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
