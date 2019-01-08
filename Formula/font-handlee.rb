bbc85db32fbb873d44323327b243e73f20c2370c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-handlee.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-handlee.rb
@@ -0,0 +1,9 @@
+class FontHandlee < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/handlee/Handlee-Regular.ttf"
+  desc "Handlee"
+  homepage "https://www.google.com/fonts/specimen/Handlee"
+  def install
+    
+  test do
+  end
+end
