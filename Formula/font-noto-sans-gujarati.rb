49cb0465ec280bb5b493439e611c354ee67b086b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-gujarati.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-gujarati.rb
@@ -0,0 +1,9 @@
+class FontNotoSansGujarati < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGujarati-unhinted.zip"
+  desc "Noto Sans Gujarati"
+  homepage "https://www.google.com/get/noto/#sans-gujr"
+  def install
+    
+  test do
+  end
+end
