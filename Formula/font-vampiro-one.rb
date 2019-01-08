9ff56662e731af403ec8169884e0d7b6b7ae5d02
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-vampiro-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-vampiro-one.rb
@@ -0,0 +1,9 @@
+class FontVampiroOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/vampiroone/VampiroOne-Regular.ttf"
+  desc "Vampiro One"
+  homepage "https://www.google.com/fonts/specimen/Vampiro+One"
+  def install
+    
+  test do
+  end
+end
