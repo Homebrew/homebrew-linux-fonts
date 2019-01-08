eed78cd36a360fb8c7438138f9fc3121f1fa6ec7
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:58:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-proggyclean-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-proggyclean-nerd-font-mono.rb
@@ -0,0 +1,14 @@
+class FontProggycleanNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "cf98665e543bc47bf669baaecd573b9ba35184b21c69bbe6e04450c7fb3ffbf2"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ProggyClean.zip"
+  desc "ProggyCleanTTCE Nerd Font,ProggyCleanTT CE (ProggyClean)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    (share/"fonts").install "ProggyCleanTT CE Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "ProggyCleanTT Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "ProggyCleanTTSZ Nerd Font Complete Mono.ttf"
+  end
+  test do
+  end
+end
