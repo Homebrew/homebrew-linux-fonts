60f3beddca4ce01e0a996d1e5edb911de1c1ced6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tuffy.rb from 0577a469f888bf3aa220f0422f13364c35ce5f38

---
+++ Formula/font-tuffy.rb
@@ -0,0 +1,11 @@
+class FontTuffy < Formula
+  version "20120614"
+  sha256 "c6f9eb7a878e270b96fbb5dae7783be29710b4405c76e856de60dcfc96386e0b"
+  url "http://tulrich.com/fonts/tuffy-#{version}.tar.gz"
+  desc "Tuffy"
+  homepage "http://tulrich.com/fonts/"
+  def install
+    
+  test do
+  end
+end
