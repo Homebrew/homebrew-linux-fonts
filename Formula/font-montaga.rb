8851f9eae545e80dc322e963e2ff8948563b0d2c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-montaga.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-montaga.rb
@@ -0,0 +1,9 @@
+class FontMontaga < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/montaga/Montaga-Regular.ttf"
+  desc "Montaga"
+  homepage "https://www.google.com/fonts/specimen/Montaga"
+  def install
+    
+  test do
+  end
+end
