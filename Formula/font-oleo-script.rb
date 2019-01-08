d3cad3b6086ae643d32e6ae64f17d1d7f350f604
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oleo-script.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-oleo-script.rb
@@ -0,0 +1,10 @@
+class FontOleoScript < Formula
+  head "https://github.com/google/fonts/trunk/ofl/oleoscript", using: :svn, revision: "50", trust_cert: true
+  desc "Oleo Script"
+  homepage "https://www.google.com/fonts/specimen/Oleo%20Script"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
