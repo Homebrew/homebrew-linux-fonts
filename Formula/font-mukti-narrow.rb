162e4397be2d5e57cde76c98ad856b8a0dff0fb1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mukti-narrow.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-mukti-narrow.rb
@@ -0,0 +1,9 @@
+class FontMuktiNarrow < Formula
+  head "https://www.omicronlab.com/download/fonts/muktinarrow.ttf"
+  desc "Mukti Narrow"
+  homepage "https://www.omicronlab.com/bangla-fonts.html"
+  def install
+    
+  test do
+  end
+end
