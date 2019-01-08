1a0501edc83b8696297f185221271445bb90279d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sail.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sail.rb
@@ -0,0 +1,9 @@
+class FontSail < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sail/Sail-Regular.ttf"
+  desc "Sail"
+  homepage "https://www.google.com/fonts/specimen/Sail"
+  def install
+    
+  test do
+  end
+end
