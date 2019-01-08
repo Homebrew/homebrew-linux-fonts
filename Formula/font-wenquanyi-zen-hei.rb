2036645005a94206fd3aeb6447f3fa5886c37122
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wenquanyi-zen-hei.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-wenquanyi-zen-hei.rb
@@ -0,0 +1,11 @@
+class FontWenquanyiZenHei < Formula
+  version "0.9.45"
+  sha256 "e4b7e306475bf9427d1757578f0e4528930c84c44eaa3f167d4c42f110ee75d6"
+  url "https://downloads.sourceforge.net/wqy/wqy-zenhei-#{version}.tar.gz"
+  desc "WenQuanYi Zen Hei"
+  homepage "https://wenq.org/wqy2/index.cgi?ZenHei"
+  def install
+    
+  test do
+  end
+end
