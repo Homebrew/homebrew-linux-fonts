4cc7c87e834607a2b20c54c6644b932975ce99f0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spicy-rice.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-spicy-rice.rb
@@ -0,0 +1,9 @@
+class FontSpicyRice < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/spicyrice/SpicyRice-Regular.ttf"
+  desc "Spicy Rice"
+  homepage "https://www.google.com/fonts/specimen/Spicy+Rice"
+  def install
+    
+  test do
+  end
+end
