06747981b971d4c78535a7afac122783b4567850
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-cjk-kr.rb from 45cd631147cee78c9cf83f81cc0be2270a59d7eb

---
+++ Formula/font-noto-serif-cjk-kr.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifCjkKr < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKkr-hinted.zip"
+  desc "Noto Serif CJK KR"
+  homepage "https://www.google.com/get/noto/#serif-kore"
+  def install
+    
+  test do
+  end
+end
