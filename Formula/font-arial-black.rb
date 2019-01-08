b85c673c95f4567c0be7877b97f24bcc6ece8d65
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:53:22 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arial-black.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-arial-black.rb
@@ -0,0 +1,12 @@
+class FontArialBlack < Formula
+  version "2.35"
+  sha256 "a425f0ffb6a1a5ede5b979ed6177f4f4f4fdef6ae7c302a7b7720ef332fec0a8"
+  url "https://downloads.sourceforge.net/corefonts/arialb32.exe"
+  desc "Arial Black"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  def install
+    (share/"fonts").install "AriBlk.TTF"
+  end
+  test do
+  end
+end
