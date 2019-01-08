f60628583d2dc0cdd0fbcd609401127fb3316f22
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-metamorphous.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-metamorphous.rb
@@ -0,0 +1,9 @@
+class FontMetamorphous < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/metamorphous/Metamorphous-Regular.ttf"
+  desc "Metamorphous"
+  homepage "https://www.google.com/fonts/specimen/Metamorphous"
+  def install
+    
+  test do
+  end
+end
