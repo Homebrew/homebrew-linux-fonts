9e4c58f1ed8a387485781a7886eb2fa2d8a04651
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-swanky-and-moo-moo.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-swanky-and-moo-moo.rb
@@ -0,0 +1,9 @@
+class FontSwankyAndMooMoo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/swankyandmoomoo/SwankyandMooMoo.ttf"
+  desc "Swanky and Moo Moo"
+  homepage "https://www.google.com/fonts/specimen/Swanky+and+Moo+Moo"
+  def install
+    
+  test do
+  end
+end
