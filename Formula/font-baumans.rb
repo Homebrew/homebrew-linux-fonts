5690bca12fb00c405a64fae98fc327986ba6d036
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-baumans.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-baumans.rb
@@ -0,0 +1,9 @@
+class FontBaumans < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/baumans/Baumans-Regular.ttf"
+  desc "Baumans"
+  homepage "https://www.google.com/fonts/specimen/Baumans"
+  def install
+    
+  test do
+  end
+end
