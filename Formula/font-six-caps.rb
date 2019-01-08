957cc6107f274b587bd0e41975b681c1230888aa
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-six-caps.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-six-caps.rb
@@ -0,0 +1,9 @@
+class FontSixCaps < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sixcaps/SixCaps.ttf"
+  desc "Six Caps"
+  homepage "https://www.google.com/fonts/specimen/Six+Caps"
+  def install
+    
+  test do
+  end
+end
