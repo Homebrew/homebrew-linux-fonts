830542e696239e8123de605b0d9ec22d857b440e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-londrina-shadow.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-londrina-shadow.rb
@@ -0,0 +1,9 @@
+class FontLondrinaShadow < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/londrinashadow/LondrinaShadow-Regular.ttf"
+  desc "Londrina Shadow"
+  homepage "https://www.google.com/fonts/specimen/Londrina+Shadow"
+  def install
+    
+  test do
+  end
+end
