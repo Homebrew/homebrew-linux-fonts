384d2a42294cee61df8ae7e420ca29936d718dff
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-courgette.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-courgette.rb
@@ -0,0 +1,9 @@
+class FontCourgette < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/courgette/Courgette-Regular.ttf"
+  desc "Courgette"
+  homepage "https://www.google.com/fonts/specimen/Courgette"
+  def install
+    
+  test do
+  end
+end
