2f423f1dcf302c0e6ba501aaa3f1076a5c041bad
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-han-serif-el-m.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-source-han-serif-el-m.rb
@@ -0,0 +1,18 @@
+class FontSourceHanSerifElM < Formula
+  version "1.001R"
+  sha256 "3d23f70a6d134fd4206a920c4d54376f2b68393beabf60af1d56a451d03eae26"
+  url "https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_EL-M.zip"
+  desc "Source Han Serif EL-M"
+  homepage "https://github.com/adobe-fonts/source-han-serif"
+  def install
+    
+  def creavat; <<~EOS
+    #{token} only installs the ExtraLight, Light, Regular and Medium weights.
+    To install SemiBold, Bold, and Heavy:
+
+      brew cask install font-source-han-serif-sb-h
+  EOS
+  end
+  test do
+  end
+end
