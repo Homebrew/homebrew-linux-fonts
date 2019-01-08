b4649c4e5c30aac164a8eb5b75dc1454cabe7683
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-league-gothic.rb from 4e29f53c5521477ec4d75c0265210f615bb65ca9

---
+++ Formula/font-league-gothic.rb
@@ -0,0 +1,9 @@
+class FontLeagueGothic < Formula
+  head "http://files.theleagueofmoveabletype.com/downloads/theleagueof-league-gothic-64c3ede.zip"
+  desc "League Gothic"
+  homepage "https://www.theleagueofmoveabletype.com/league-gothic"
+  def install
+    
+  test do
+  end
+end
