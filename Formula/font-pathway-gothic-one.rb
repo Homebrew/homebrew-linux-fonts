26c5062d6d362907e1b688876339d628e1c64527
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pathway-gothic-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-pathway-gothic-one.rb
@@ -0,0 +1,9 @@
+class FontPathwayGothicOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/pathwaygothicone/PathwayGothicOne-Regular.ttf"
+  desc "Pathway Gothic One"
+  homepage "https://www.google.com/fonts/specimen/Pathway+Gothic+One"
+  def install
+    
+  test do
+  end
+end
