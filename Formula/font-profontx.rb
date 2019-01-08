b73f290185971edb3d66936a4af1aca8a6c6c6b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-profontx.rb from e9524946e2e56c46bc191bf1a8f445489222ecaf

---
+++ Formula/font-profontx.rb
@@ -0,0 +1,9 @@
+class FontProfontx < Formula
+  head "http://faisal.com/software/profontx/ProFontX.zip"
+  desc "ProFontX"
+  homepage "http://faisal.com/software/profontx/"
+  def install
+    
+  test do
+  end
+end
