887518ae0d31b130489ee42b1a6710c18481c352
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-capriola.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-capriola.rb
@@ -0,0 +1,9 @@
+class FontCapriola < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/capriola/Capriola-Regular.ttf"
+  desc "Capriola"
+  homepage "https://www.google.com/fonts/specimen/Capriola"
+  def install
+    
+  test do
+  end
+end
