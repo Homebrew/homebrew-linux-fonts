8825441a767daf8c3552715cd6b059563f9ed94d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-work-sans.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-work-sans.rb
@@ -0,0 +1,11 @@
+class FontWorkSans < Formula
+  version "1.6"
+  sha256 "247bd98900d52fc1016098449dd906472a2c85e956b62736f04929d8922f7fcf"
+  url "https://github.com/weiweihuanghuang/Work-Sans/archive/v#{version}.zip"
+  desc "Work Sans"
+  homepage "https://weiweihuanghuang.github.io/Work-Sans/"
+  def install
+    
+  test do
+  end
+end
