966fc5853731986d9d7bb7d086397c00b0579a3c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-constructium.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-constructium.rb
@@ -0,0 +1,9 @@
+class FontConstructium < Formula
+  head "https://www.kreativekorp.com/swdownload/fonts/xlang/constructium.zip", user_agent: :fake
+  desc "Constructium"
+  homepage "https://www.kreativekorp.com/software/fonts/constructium.shtml"
+  def install
+    
+  test do
+  end
+end
