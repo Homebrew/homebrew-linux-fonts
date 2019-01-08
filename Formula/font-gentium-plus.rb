3b34c0164eb370e37d60251242da5fccf0d58399
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gentium-plus.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-gentium-plus.rb
+++ Formula/font-gentium-plus.rb
@@ -1,7 +1,7 @@
 class FontGentiumPlus < Formula
   version "5.000"
   sha256 "335911f17bd2de4e43742e1d0367cfeff19a90abf7ed604f100a42705042e154"
-  url "https://software.sil.org/downloads/r/gentium/GentiumPlus-#{version}.zip"
+  head "https://software.sil.org/downloads/r/gentium/GentiumPlus-#{version}.zip"
   desc "Gentium"
   homepage "https://software.sil.org/gentium/"
   def install
