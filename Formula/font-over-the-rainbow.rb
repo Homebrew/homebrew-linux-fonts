509899e3ea3876d4fa0e778541cf1f2ce1e7b5f6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-over-the-rainbow.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-over-the-rainbow.rb
@@ -0,0 +1,9 @@
+class FontOverTheRainbow < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/overtherainbow/OvertheRainbow.ttf"
+  desc "Over the Rainbow"
+  homepage "https://www.google.com/fonts/specimen/Over+the+Rainbow"
+  def install
+    
+  test do
+  end
+end
