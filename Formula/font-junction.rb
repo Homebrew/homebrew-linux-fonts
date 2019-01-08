a4ef18c726c055c94ea7658a5a4e2855461d47e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-junction.rb from 03f29544d6de653b32e5f2752fd0cb6a973ae576

---
+++ Formula/font-junction.rb
@@ -0,0 +1,9 @@
+class FontJunction < Formula
+  head "https://github.com/theleagueof/junction/archive/master.zip"
+  desc "Junction"
+  homepage "https://www.theleagueofmoveabletype.com/junction"
+  def install
+    
+  test do
+  end
+end
