247bd179f671984345773cff26df852e166f5fb9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-double-pica-sc.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-im-fell-double-pica-sc.rb
@@ -0,0 +1,9 @@
+class FontImFellDoublePicaSc < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/imfelldoublepicasc/IMFeDPsc28P.ttf"
+  desc "IM Fell Double Pica SC"
+  homepage "https://www.google.com/fonts/specimen/IM+Fell+Double+Pica+SC"
+  def install
+    
+  test do
+  end
+end
