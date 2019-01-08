f4777b852c846ac55fa5d8979d137d018f14b32a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-meddon.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-meddon.rb
@@ -0,0 +1,9 @@
+class FontMeddon < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/meddon/Meddon.ttf"
+  desc "Meddon"
+  homepage "https://www.google.com/fonts/specimen/Meddon"
+  def install
+    
+  test do
+  end
+end
