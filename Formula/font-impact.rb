b1f8191fd348609976c8ee645dfd4de06a9fae7c
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:54 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-impact.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-impact.rb
+++ Formula/font-impact.rb
@@ -1,7 +1,7 @@
 class FontImpact < Formula
   version "2.35"
   sha256 "6061ef3b7401d9642f5dfdb5f2b376aa14663f6275e60a51207ad4facf2fccfb"
-  head "https://downloads.sourceforge.net/corefonts/impact32.exe"
+  url "https://downloads.sourceforge.net/corefonts/impact32.exe"
   desc "Impact"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
   def install
