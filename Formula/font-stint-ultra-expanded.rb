1e24903645da3b3f613d244a1ba45b37b8cbc234
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stint-ultra-expanded.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-stint-ultra-expanded.rb
@@ -0,0 +1,9 @@
+class FontStintUltraExpanded < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/stintultraexpanded/StintUltraExpanded-Regular.ttf"
+  desc "Stint Ultra Expanded"
+  homepage "https://www.google.com/fonts/specimen/Stint+Ultra+Expanded"
+  def install
+    
+  test do
+  end
+end
