e883983b569d12418d37f045c66b7d723b1e9a01
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:54 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolatalgc-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-inconsolatalgc-nerd-font-mono.rb
+++ Formula/font-inconsolatalgc-nerd-font-mono.rb
@@ -1,7 +1,7 @@
 class FontInconsolatalgcNerdFontMono < Formula
   version "2.0.0"
   sha256 "e764c3fc484088dd12d93688f4be8efa7e89b8e87ff5be2e9473ab2f37a60c40"
-  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataLGC.zip"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataLGC.zip"
   desc "InconsolataLGC Nerd Font Mono (InconsolataLGC)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
