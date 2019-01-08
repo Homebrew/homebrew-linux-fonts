5133de655baa3ef0ea85b84c632d3dcb695b7de3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-prosto-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-prosto-one.rb
@@ -0,0 +1,9 @@
+class FontProstoOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/prostoone/ProstoOne-Regular.ttf"
+  desc "Prosto One"
+  homepage "https://www.google.com/fonts/specimen/Prosto+One"
+  def install
+    
+  test do
+  end
+end
