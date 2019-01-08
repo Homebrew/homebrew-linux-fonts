4dcfe5326cc7187a53afe5d71ec5bc11a3baaf5a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-vidaloka.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-vidaloka.rb
@@ -0,0 +1,9 @@
+class FontVidaloka < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/vidaloka/Vidaloka-Regular.ttf"
+  desc "Vidaloka"
+  homepage "https://www.google.com/fonts/specimen/Vidaloka"
+  def install
+    
+  test do
+  end
+end
