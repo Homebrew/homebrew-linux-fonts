b0ece890145293be339a81ce29dd770d21acd31d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kreon.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-kreon.rb
@@ -0,0 +1,10 @@
+class FontKreon < Formula
+  head "https://github.com/google/fonts/trunk/ofl/kreon", using: :svn, revision: "50", trust_cert: true
+  desc "Kreon"
+  homepage "https://www.google.com/fonts/specimen/Kreon"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
