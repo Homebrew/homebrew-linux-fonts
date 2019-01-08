437ea55839aab963b9563474f4a64cb563e28339
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ruda.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-ruda.rb
@@ -0,0 +1,10 @@
+class FontRuda < Formula
+  head "https://github.com/google/fonts/trunk/ofl/ruda", using: :svn, revision: "50", trust_cert: true
+  desc "Ruda"
+  homepage "https://www.google.com/fonts/specimen/Ruda"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
