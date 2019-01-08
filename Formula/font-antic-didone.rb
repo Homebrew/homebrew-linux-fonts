5524043baf882d7f636cbf76ba1248647cfcb6d2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-antic-didone.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-antic-didone.rb
@@ -0,0 +1,9 @@
+class FontAnticDidone < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/anticdidone/AnticDidone-Regular.ttf"
+  desc "Antic Didone"
+  homepage "https://www.google.com/fonts/specimen/Antic+Didone"
+  def install
+    
+  test do
+  end
+end
