57dfa9877fcd6d5b2f8d1fc4d195a52f99e16b6b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:47 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolatago-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-inconsolatago-nerd-font.rb
@@ -0,0 +1,11 @@
+class FontInconsolatagoNerdFont < Formula
+  version "2.0.0"
+  sha256 "4a3860cb82de6e5e00dc13871269073a0367ccb75bc8d0f5d33d303e7d1e4986"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataGo.zip"
+  desc "InconsolataGo Nerd Font (InconsolataGo)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
