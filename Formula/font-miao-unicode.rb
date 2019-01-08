dc752e55076f99c74e5e0dcd73b1559852489674
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:57:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-miao-unicode.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-miao-unicode.rb
@@ -0,0 +1,10 @@
+class FontMiaoUnicode < Formula
+  head "https://github.com/phjamr/MiaoUnicode/blob/master/MiaoUnicode-Regular.ttf?raw=true"
+  desc "MiaoUnicode"
+  homepage "https://phjamr.github.io/miao.html"
+  def install
+    (share/"fonts").install "MiaoUnicode-Regular.ttf"
+  end
+  test do
+  end
+end
