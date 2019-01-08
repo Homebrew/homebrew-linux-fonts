9f9179d212926cec8d23befbd5d9dcb37e3d4735
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-malayalam.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-malayalam.rb
@@ -0,0 +1,9 @@
+class FontNotoSansMalayalam < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMalayalam-unhinted.zip"
+  desc "Noto Sans Malayalam"
+  homepage "https://www.google.com/get/noto/#sans-mlym"
+  def install
+    
+  test do
+  end
+end
