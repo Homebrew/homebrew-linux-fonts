1764dba314e6142ea6ce346e4a5b998a0ec587ca
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gnu-unifont.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-gnu-unifont.rb
@@ -0,0 +1,11 @@
+class FontGnuUnifont < Formula
+  version "10.0.06"
+  sha256 "669a9764fb640c832a8ebfbd2c351be744b55c6f65b40f1fb203144309659ac0"
+  url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.ttf"
+  desc "GNU Unifont"
+  homepage "http://unifoundry.com/unifont.html"
+  def install
+    
+  test do
+  end
+end
