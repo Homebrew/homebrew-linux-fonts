f6526498e8194075c03302393504573889ee7ba8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kenia.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-kenia.rb
@@ -0,0 +1,9 @@
+class FontKenia < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kenia/Kenia-Regular.ttf"
+  desc "Kenia"
+  homepage "https://www.google.com/fonts/specimen/Kenia"
+  def install
+    
+  test do
+  end
+end
