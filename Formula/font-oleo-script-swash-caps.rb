1d661b28e614b2c0e010145c386b3de77c07cf96
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oleo-script-swash-caps.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-oleo-script-swash-caps.rb
@@ -0,0 +1,10 @@
+class FontOleoScriptSwashCaps < Formula
+  head "https://github.com/google/fonts/trunk/ofl/oleoscriptswashcaps", using: :svn, revision: "50", trust_cert: true
+  desc "Oleo Script Swash Caps"
+  homepage "https://www.google.com/fonts/specimen/Oleo%20Script%20Swash%20Caps"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
