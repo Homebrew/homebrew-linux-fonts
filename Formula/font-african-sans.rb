22b359138c172eef55e731a792cb363da46225e6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-african-sans.rb from afef902e060f8dd380745639d335ecb2a74db92b

---
+++ Formula/font-african-sans.rb
@@ -0,0 +1,11 @@
+class FontAfricanSans < Formula
+  version "9.380"
+  sha256 "66176e5517ace047affc23ca7b7137018c907eee00b113666a671f31254ac7a4"
+  url "http://www.languagegeek.com/font/AfricanSans.zip"
+  desc "African Sans"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
