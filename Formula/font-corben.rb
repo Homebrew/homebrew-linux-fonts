8e163880b5cefee2406936d9a4165c38fda88b31
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-corben.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-corben.rb
@@ -0,0 +1,10 @@
+class FontCorben < Formula
+  head "https://github.com/google/fonts/trunk/ofl/corben", using: :svn, revision: "50", trust_cert: true
+  desc "Corben"
+  homepage "https://www.google.com/fonts/specimen/Corben"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
