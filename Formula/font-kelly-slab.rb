be0ab6752863d55cf73ef087c47dc24115888533
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kelly-slab.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-kelly-slab.rb
@@ -0,0 +1,9 @@
+class FontKellySlab < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kellyslab/KellySlab-Regular.ttf"
+  desc "Kelly Slab"
+  homepage "https://www.google.com/fonts/specimen/Kelly+Slab"
+  def install
+    
+  test do
+  end
+end
