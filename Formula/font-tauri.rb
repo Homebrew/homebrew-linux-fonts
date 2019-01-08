2aff3ba37dbd178ea1a8be28ca24a3b0fc42e0e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tauri.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-tauri.rb
@@ -0,0 +1,9 @@
+class FontTauri < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/tauri/Tauri-Regular.ttf"
+  desc "Tauri"
+  homepage "https://www.google.com/fonts/specimen/Tauri"
+  def install
+    
+  test do
+  end
+end
