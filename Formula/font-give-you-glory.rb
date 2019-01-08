4a07e847c40537f128f12d5586b1462d836db835
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-give-you-glory.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-give-you-glory.rb
@@ -0,0 +1,9 @@
+class FontGiveYouGlory < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/giveyouglory/GiveYouGlory.ttf"
+  desc "Give You Glory"
+  homepage "https://www.google.com/fonts/specimen/Give+You+Glory"
+  def install
+    
+  test do
+  end
+end
