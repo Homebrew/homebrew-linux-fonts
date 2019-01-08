d8ad6ac7f2c3b374dc2b28a43d00f05f4742f75e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-egyptian-hieroglyphs.rb from 91ec7fe0323c8467fb529c8559b961db2070ea2c

---
+++ Formula/font-noto-sans-egyptian-hieroglyphs.rb
@@ -0,0 +1,9 @@
+class FontNotoSansEgyptianHieroglyphs < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansEgyptianHieroglyphs-unhinted.zip"
+  desc "Noto Sans Egyptian Hieroglyphs"
+  homepage "https://www.google.com/get/noto/#sans-egyp"
+  def install
+    
+  test do
+  end
+end
