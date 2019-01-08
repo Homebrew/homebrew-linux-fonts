4eff2ab5612b004dc24391abae31426236ead338
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-abril-fatface.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-abril-fatface.rb
@@ -0,0 +1,9 @@
+class FontAbrilFatface < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/abrilfatface/AbrilFatface-Regular.ttf"
+  desc "Abril Fatface"
+  homepage "https://www.google.com/fonts/specimen/Abril%20Fatface"
+  def install
+    
+  test do
+  end
+end
