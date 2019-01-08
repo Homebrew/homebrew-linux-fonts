f5d9397bb6f214ebfa7653910b82e1bf9ac7a8a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-materialdesignicons-webfont.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-materialdesignicons-webfont.rb
@@ -0,0 +1,10 @@
+class FontMaterialdesigniconsWebfont < Formula
+  head "https://github.com/templarian/materialdesign-webfont/trunk/fonts", using: :svn, revision: "50", trust_cert: true
+  desc "Material Design Icons Webfont"
+  homepage "https://materialdesignicons.com/"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
