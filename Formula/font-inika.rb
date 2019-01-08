a6525ab0a6bca9c311cc0fe22975d773f560a26f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inika.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-inika.rb
@@ -0,0 +1,10 @@
+class FontInika < Formula
+  head "https://github.com/google/fonts/trunk/ofl/inika", using: :svn, revision: "50", trust_cert: true
+  desc "Inika"
+  homepage "https://www.google.com/fonts/specimen/Inika"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
