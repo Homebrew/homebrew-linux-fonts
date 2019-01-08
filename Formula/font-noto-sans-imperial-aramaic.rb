6f330b087f9531e66093314546d6f2b02c92f7bb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-imperial-aramaic.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-imperial-aramaic.rb
@@ -0,0 +1,9 @@
+class FontNotoSansImperialAramaic < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansImperialAramaic-unhinted.zip"
+  desc "Noto Sans Imperial Aramaic"
+  homepage "https://www.google.com/get/noto/#sans-armi"
+  def install
+    
+  test do
+  end
+end
