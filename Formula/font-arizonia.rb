733e29ed2fa10b085d695a81a459bd79c614d7a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arizonia.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-arizonia.rb
@@ -0,0 +1,9 @@
+class FontArizonia < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/arizonia/Arizonia-Regular.ttf"
+  desc "Arizonia"
+  homepage "https://www.google.com/fonts/specimen/Arizonia"
+  def install
+    
+  test do
+  end
+end
