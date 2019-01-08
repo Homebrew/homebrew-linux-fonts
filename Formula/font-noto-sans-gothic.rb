d3962620cf9ab1a41b85874ccac8c77db54eb65d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-gothic.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-gothic.rb
@@ -0,0 +1,9 @@
+class FontNotoSansGothic < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGothic-unhinted.zip"
+  desc "Noto Sans Gothic"
+  homepage "https://www.google.com/get/noto/#sans-goth"
+  def install
+    
+  test do
+  end
+end
