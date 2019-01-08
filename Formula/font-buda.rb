6e8b25b5608fdf308c0aed5e675afd40e0549e90
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-buda.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-buda.rb
@@ -0,0 +1,9 @@
+class FontBuda < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/buda/Buda-Light.ttf"
+  desc "Buda"
+  homepage "https://www.google.com/fonts/specimen/Buda"
+  def install
+    
+  test do
+  end
+end
