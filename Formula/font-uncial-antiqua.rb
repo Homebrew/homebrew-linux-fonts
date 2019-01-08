03539e2b8738674bd10b8aa0df781c2885965b3d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-uncial-antiqua.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-uncial-antiqua.rb
@@ -0,0 +1,9 @@
+class FontUncialAntiqua < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/uncialantiqua/UncialAntiqua-Regular.ttf"
+  desc "Uncial Antiqua"
+  homepage "https://www.google.com/fonts/specimen/Uncial+Antiqua"
+  def install
+    
+  test do
+  end
+end
