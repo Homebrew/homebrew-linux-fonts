09fb2dfa5cf3ceb87f2221bd9751fd7fc3be6018
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rokkitt.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-rokkitt.rb
@@ -0,0 +1,10 @@
+class FontRokkitt < Formula
+  head "https://github.com/google/fonts/trunk/ofl/rokkitt", using: :svn, revision: "50", trust_cert: true
+  desc "Rokkitt"
+  homepage "https://www.google.com/fonts/specimen/Rokkitt"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
