5bbd37e8c8d12bbe34cb65030abe477408cb3fef
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bayon.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bayon.rb
@@ -0,0 +1,9 @@
+class FontBayon < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/bayon/Bayon-Regular.ttf"
+  desc "Bayon"
+  homepage "https://www.google.com/fonts/specimen/Bayon"
+  def install
+    
+  test do
+  end
+end
