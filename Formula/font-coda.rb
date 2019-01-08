7c69b4f19af33c6451a71e6b06edfb99a3a37d52
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-coda.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-coda.rb
@@ -0,0 +1,10 @@
+class FontCoda < Formula
+  head "https://github.com/google/fonts/trunk/ofl/coda", using: :svn, revision: "50", trust_cert: true
+  desc "Coda"
+  homepage "https://www.google.com/fonts/specimen/Coda"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
