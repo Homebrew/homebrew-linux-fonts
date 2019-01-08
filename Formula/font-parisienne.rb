cba252b7a79898294b2893cd16829d842c5868df
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-parisienne.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-parisienne.rb
@@ -0,0 +1,9 @@
+class FontParisienne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/parisienne/Parisienne-Regular.ttf"
+  desc "Parisienne"
+  homepage "https://www.google.com/fonts/specimen/Parisienne"
+  def install
+    
+  test do
+  end
+end
