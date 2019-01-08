23f4c75321692821002a4e3af6e57886c9b8e2ff
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-envy-code-r.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-envy-code-r.rb
@@ -0,0 +1,11 @@
+class FontEnvyCodeR < Formula
+  version "PR7"
+  sha256 "9f7e9703aaf21110b4e1a54d954d57d4092727546348598a5a8e8101e4597aff"
+  url "http://download.damieng.com/fonts/original/EnvyCodeR-#{version}.zip"
+  desc "Envy Code R"
+  homepage "https://damieng.com/blog/tag/envy-code-r"
+  def install
+    
+  test do
+  end
+end
