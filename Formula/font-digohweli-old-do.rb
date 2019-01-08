e9b39cd822712332cb97ba535096651173b66ba8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-digohweli-old-do.rb from 730e93df24396808b4273cdae4dc7ca63315ac4d

---
+++ Formula/font-digohweli-old-do.rb
@@ -0,0 +1,9 @@
+class FontDigohweliOldDo < Formula
+  head "http://www.languagegeek.com/font/OldDODigohweli.zip"
+  desc "Digohweli Old DO"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
