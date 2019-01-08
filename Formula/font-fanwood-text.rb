4cb549ecde56ca090c2279062e4a2000f11767a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fanwood-text.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-fanwood-text.rb
@@ -0,0 +1,10 @@
+class FontFanwoodText < Formula
+  head "https://github.com/google/fonts/trunk/ofl/fanwoodtext", using: :svn, revision: "50", trust_cert: true
+  desc "Fanwood Text"
+  homepage "https://www.google.com/fonts/specimen/Fanwood%20Text"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
