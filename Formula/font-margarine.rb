2ae244d1042a8465ea8bf4ac8cac659d85b9fcb8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-margarine.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-margarine.rb
@@ -0,0 +1,9 @@
+class FontMargarine < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/margarine/Margarine-Regular.ttf"
+  desc "Margarine"
+  homepage "https://www.google.com/fonts/specimen/Margarine"
+  def install
+    
+  test do
+  end
+end
