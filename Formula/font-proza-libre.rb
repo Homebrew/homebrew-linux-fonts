26fa2495b37280f1fc9a4b12cad035654f7f2bda
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-proza-libre.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-proza-libre.rb
@@ -0,0 +1,11 @@
+class FontProzaLibre < Formula
+  version "1.0"
+  sha256 "cbd3bb929d905330ad9e2d4b2efc3edf5c351eb5b4a238bd87367e74836fa9c9"
+  url "https://github.com/jasperdewaard/Proza-Libre/archive/#{version}.zip"
+  desc "Proza Libre"
+  homepage "https://github.com/jasperdewaard/Proza-Libre"
+  def install
+    
+  test do
+  end
+end
