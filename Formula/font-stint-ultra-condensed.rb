1499714644fbe00ab2819b5bb619a8c86f0194c0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stint-ultra-condensed.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-stint-ultra-condensed.rb
@@ -0,0 +1,9 @@
+class FontStintUltraCondensed < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/stintultracondensed/StintUltraCondensed-Regular.ttf"
+  desc "Stint Ultra Condensed"
+  homepage "https://www.google.com/fonts/specimen/Stint+Ultra+Condensed"
+  def install
+    
+  test do
+  end
+end
