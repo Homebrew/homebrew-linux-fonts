907f13ed073fa14a0a84b5aea31ac474b5e86bc5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sirin-stencil.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sirin-stencil.rb
@@ -0,0 +1,9 @@
+class FontSirinStencil < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sirinstencil/SirinStencil-Regular.ttf"
+  desc "Sirin Stencil"
+  homepage "https://www.google.com/fonts/specimen/Sirin+Stencil"
+  def install
+    
+  test do
+  end
+end
