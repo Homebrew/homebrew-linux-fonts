aacb8f5f572d8a180e3bf7fb048cea35ed986ccf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-crafty-girls.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-crafty-girls.rb
@@ -0,0 +1,9 @@
+class FontCraftyGirls < Formula
+  head "https://github.com/google/fonts/raw/master/apache/craftygirls/CraftyGirls-Regular.ttf"
+  desc "Crafty Girls"
+  homepage "https://www.google.com/fonts/specimen/Crafty+Girls"
+  def install
+    
+  test do
+  end
+end
