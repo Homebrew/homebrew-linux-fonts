ae1f7a365d2d6d21bf3adbd3f085ec9f775723fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-varela.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-varela.rb
@@ -0,0 +1,10 @@
+class FontVarela < Formula
+  head "https://github.com/google/fonts/trunk/ofl/varela", using: :svn, revision: "796", trust_cert: true
+  desc "Varela"
+  homepage "https://www.google.com/fonts/specimen/Varela"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
