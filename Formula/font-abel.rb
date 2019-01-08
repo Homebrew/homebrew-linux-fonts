3d11565b25a07b7088f14e6992806ca3d1765bdc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-abel.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-abel.rb
@@ -0,0 +1,9 @@
+class FontAbel < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/abel/Abel-Regular.ttf"
+  desc "Abel"
+  homepage "https://www.google.com/fonts/specimen/Abel"
+  def install
+    
+  test do
+  end
+end
