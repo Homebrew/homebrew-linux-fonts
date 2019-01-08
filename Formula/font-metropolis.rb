c644c1a88575b47f8e3ccedd43500f6f9acd8a17
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-metropolis.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-metropolis.rb
@@ -0,0 +1,11 @@
+class FontMetropolis < Formula
+  version "9"
+  sha256 "ddc8cd6b0f24b7f1cefb7d2819a1ddf7340c4868eba7df17b5fe4123081169ad"
+  url "https://github.com/chrismsimpson/Metropolis/archive/r#{version}.zip"
+  desc "Metropolis"
+  homepage "https://github.com/chrismsimpson/Metropolis"
+  def install
+    
+  test do
+  end
+end
