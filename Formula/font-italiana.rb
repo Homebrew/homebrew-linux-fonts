4f54e2718402d3f3e0067f65f662b06e2361d2ea
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-italiana.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-italiana.rb
@@ -0,0 +1,9 @@
+class FontItaliana < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/italiana/Italiana-Regular.ttf"
+  desc "Italiana"
+  homepage "https://www.google.com/fonts/specimen/Italiana"
+  def install
+    
+  test do
+  end
+end
