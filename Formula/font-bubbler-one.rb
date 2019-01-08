197a571d8a0074c486adcfc2d62e4c5466a436a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bubbler-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bubbler-one.rb
@@ -0,0 +1,9 @@
+class FontBubblerOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/bubblerone/BubblerOne-Regular.ttf"
+  desc "Bubbler One"
+  homepage "https://www.google.com/fonts/specimen/Bubbler+One"
+  def install
+    
+  test do
+  end
+end
