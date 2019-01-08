c6933f13e1cdb1b7a72267f1bbdf530e71287017
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-courier-new.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-courier-new.rb
@@ -0,0 +1,11 @@
+class FontCourierNew < Formula
+  version "2.82"
+  sha256 "bb511d861655dde879ae552eb86b134d6fae67cb58502e6ff73ec5d9151f3384"
+  url "https://downloads.sourceforge.net/corefonts/courie32.exe"
+  desc "Courier New"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  def install
+    
+  test do
+  end
+end
