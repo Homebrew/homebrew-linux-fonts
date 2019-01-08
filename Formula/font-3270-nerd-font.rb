e012454005c9d07ebd3f480f9389d20ed18d54b0
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-3270-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-3270-nerd-font.rb
+++ Formula/font-3270-nerd-font.rb
@@ -1,7 +1,7 @@
 class Font3270NerdFont < Formula
   version "2.0.0"
   sha256 "d4b34b2d6c7cb758d8d6c7213a08a732a3759da123904e316df08e38d421641b"
-  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/3270.zip"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/3270.zip"
   desc "3270Medium Nerd Font (3270)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
