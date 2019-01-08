5d7bdd287b41091e7b375335feafbafe862cdda7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-asset.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-asset.rb
@@ -0,0 +1,9 @@
+class FontAsset < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/asset/Asset-Regular.ttf"
+  desc "Asset"
+  homepage "https://www.google.com/fonts/specimen/Asset"
+  def install
+    
+  test do
+  end
+end
