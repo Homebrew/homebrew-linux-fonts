9f52160640b0ff787678c1d7fbfb34e807c2dc09
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cutive-mono.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-cutive-mono.rb
@@ -0,0 +1,9 @@
+class FontCutiveMono < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/cutivemono/CutiveMono-Regular.ttf"
+  desc "Cutive Mono"
+  homepage "https://www.google.com/fonts/specimen/Cutive%20Mono"
+  def install
+    
+  test do
+  end
+end
