5a310bca105b0cac0156102f1eaeee9617f9dab7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-amiri.rb from 63cebaa9738e367dd01d47b396d44bece28f6a97

---
+++ Formula/font-amiri.rb
@@ -0,0 +1,11 @@
+class FontAmiri < Formula
+  version "0.111"
+  sha256 "1fbfccced6348b5db2c1c21d5b319cd488e14d055702fa817a0f6cb83d882166"
+  url "https://github.com/alif-type/amiri-font/releases/download/#{version}/Amiri-#{version}.zip"
+  desc "Amiri"
+  homepage "https://www.amirifont.org/"
+  def install
+    
+  test do
+  end
+end
