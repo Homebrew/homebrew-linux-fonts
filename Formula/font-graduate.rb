ee7f1cb5d5a8d9a4ebef52cd0af1cbf40ed105e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-graduate.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-graduate.rb
@@ -0,0 +1,9 @@
+class FontGraduate < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/graduate/Graduate-Regular.ttf"
+  desc "Graduate"
+  homepage "https://www.google.com/fonts/specimen/Graduate"
+  def install
+    
+  test do
+  end
+end
