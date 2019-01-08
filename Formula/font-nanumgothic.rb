3481cb4ddba50f3e1eec4f1be008a8a15beec1a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nanumgothic.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-nanumgothic.rb
@@ -0,0 +1,10 @@
+class FontNanumgothic < Formula
+  head "https://github.com/google/fonts/trunk/ofl/nanumgothic", using: :svn, revision: "50", trust_cert: true
+  desc "Nanum Gothic"
+  homepage "https://www.google.com/fonts/earlyaccess"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
