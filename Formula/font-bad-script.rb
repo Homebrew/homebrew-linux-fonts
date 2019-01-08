a63eaf1bfaac5e47dfe4490754d1d95b069914df
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bad-script.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bad-script.rb
@@ -0,0 +1,9 @@
+class FontBadScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/badscript/BadScript-Regular.ttf"
+  desc "Bad Script"
+  homepage "https://www.google.com/fonts/specimen/Bad+Script"
+  def install
+    
+  test do
+  end
+end
