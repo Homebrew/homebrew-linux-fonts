464a23fd2b401bb7790de7c9042d0dddccf2f0b6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lora.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-lora.rb
@@ -0,0 +1,10 @@
+class FontLora < Formula
+  head "https://github.com/google/fonts/trunk/ofl/lora", using: :svn, revision: "50", trust_cert: true
+  desc "Lora"
+  homepage "https://www.google.com/fonts/specimen/Lora"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
