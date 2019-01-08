1b83c73dab8a7a5fda8576c85fe163514030da50
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tinos-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-tinos-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontTinosNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "cfe1306602e7839f0898bf237ffa2cf1f5d686ea74d3619c39cf417ce2942426"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Tinos.zip"
+  desc "Tinos Nerd Font Mono (Tinos)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
