96612e276ccc491c9086172ea699ec6001ec72d7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-andron-scriptor-web.rb from e74dded934a091b16c11fe52345890626151a90d

---
+++ Formula/font-andron-scriptor-web.rb
@@ -0,0 +1,11 @@
+class FontAndronScriptorWeb < Formula
+  version "3.0"
+  sha256 "19071b0d58e0d53c852d3f8e3b056205046236ccc33d8c185a9d4a0044644930"
+  url "https://folk.uib.no/hnooh/mufi/fonts/Andron/AND_SCR_WEB_#{version}.zip"
+  desc "Andron Scriptor Web"
+  homepage "https://folk.uib.no/hnooh/mufi/fonts/"
+  def install
+    
+  test do
+  end
+end
