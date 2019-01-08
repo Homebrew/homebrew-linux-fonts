97068b928357b1e7a31d2d861451cece043b54cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-open-sans.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-open-sans.rb
@@ -0,0 +1,10 @@
+class FontOpenSans < Formula
+  head "https://github.com/google/fonts/trunk/apache/opensans", using: :svn, revision: "48", trust_cert: true
+  desc "Open+Sans"
+  homepage "https://www.google.com/fonts/specimen/Open+Sans"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
