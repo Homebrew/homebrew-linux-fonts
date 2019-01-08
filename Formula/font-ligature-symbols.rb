37b2d3bc1ead3ffbdf354d8c7bdfa06d7c3cfabc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ligature-symbols.rb from 878f0106cf9fb7df7d3f8fa9ab33c14d2cc3a1ef

---
+++ Formula/font-ligature-symbols.rb
@@ -0,0 +1,9 @@
+class FontLigatureSymbols < Formula
+  head "http://kudakurage.com/ligature_symbols/LigatureSymbols.zip"
+  desc "Ligature Symbols"
+  homepage "http://kudakurage.com/ligature_symbols/"
+  def install
+    
+  test do
+  end
+end
