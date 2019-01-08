0c53a899ad2f3d016c630fd2dd8d40b54d788d92
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quicksand.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-quicksand.rb
@@ -0,0 +1,10 @@
+class FontQuicksand < Formula
+  head "https://github.com/google/fonts/trunk/ofl/quicksand", using: :svn, revision: "50", trust_cert: true
+  desc "Quicksand"
+  homepage "https://www.google.com/fonts/specimen/Quicksand"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
