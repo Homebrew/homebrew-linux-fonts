c4bc2a5ddff9fee6261e23b5f3e38a938b263a32
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tai-le.rb from 429fe80b45ad0fcc4027e2cc38e6908c61e6d571

---
+++ Formula/font-noto-sans-tai-le.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTaiLe < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiLe-unhinted.zip"
+  desc "Noto Sans Tai Le"
+  homepage "https://www.google.com/get/noto/#sans-tale"
+  def install
+    
+  test do
+  end
+end
