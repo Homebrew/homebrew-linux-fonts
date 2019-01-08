7a71e13a2f07dd5d87746826f98d7f138a80f0d6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:28 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-prociono.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-prociono.rb
@@ -0,0 +1,9 @@
+class FontProciono < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/prociono/Prociono-Regular.ttf"
+  desc "Prociono"
+  homepage "https://www.google.com/fonts/specimen/Prociono"
+  def install
+    
+  test do
+  end
+end
