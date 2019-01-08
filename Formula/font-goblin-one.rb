26332e5c73922c87a2bc93a632c60c9ac32892c2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-goblin-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-goblin-one.rb
@@ -0,0 +1,9 @@
+class FontGoblinOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/goblinone/GoblinOne.ttf"
+  desc "Goblin One"
+  homepage "https://www.google.com/fonts/specimen/Goblin+One"
+  def install
+    
+  test do
+  end
+end
