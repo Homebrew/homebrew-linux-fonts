31199c1b79af7ed8bb7607619ac4ed37d6cf15f1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-geostar.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-geostar.rb
@@ -0,0 +1,9 @@
+class FontGeostar < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/geostar/Geostar-Regular.ttf"
+  desc "Geostar"
+  homepage "https://www.google.com/fonts/specimen/Geostar"
+  def install
+    
+  test do
+  end
+end
