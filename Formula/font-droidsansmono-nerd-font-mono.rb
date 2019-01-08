0e7d123402f1b8460f139d8372592c4c126f3f86
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:54:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-droidsansmono-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-droidsansmono-nerd-font-mono.rb
@@ -0,0 +1,12 @@
+class FontDroidsansmonoNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "b48ace2486e2503aa829adcb4d5814567d23e46a99af4a79b21c2c00bb13bfda"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DroidSansMono.zip"
+  desc "DroidSansMono Nerd Font (DroidSansMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    (share/"fonts").install "Droid Sans Mono Nerd Font Complete Mono.otf"
+  end
+  test do
+  end
+end
