7c12a0f28e17e346c8584bf8f54a017960f041af
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:54 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hermit-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-hermit-nerd-font-mono.rb
+++ Formula/font-hermit-nerd-font-mono.rb
@@ -1,7 +1,7 @@
 class FontHermitNerdFontMono < Formula
   version "2.0.0"
   sha256 "5c656e844dfaf14355e5e607c738dabc903e1985a375bb907f1a200956774a18"
-  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hermit.zip"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hermit.zip"
   desc "Hurmit Nerd Font (Hermit)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
