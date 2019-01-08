f90ac1e3911846f9c36e3ebcdcc989806ab74a3a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-josefin-slab.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-josefin-slab.rb
@@ -0,0 +1,10 @@
+class FontJosefinSlab < Formula
+  head "https://github.com/google/fonts/trunk/ofl/josefinslab", using: :svn, revision: "50", trust_cert: true
+  desc "Josefin Slab"
+  homepage "https://www.google.com/fonts/specimen/Josefin%20Slab"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
