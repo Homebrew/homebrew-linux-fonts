a48e6f7e41705f3df2f12425efa1dfbd6679d243
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:53 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-firamono-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-firamono-nerd-font-mono.rb
+++ Formula/font-firamono-nerd-font-mono.rb
@@ -1,7 +1,7 @@
 class FontFiramonoNerdFontMono < Formula
   version "2.0.0"
   sha256 "1eb587b0985d61a9323e0cc979ce16cf820013f0dd235c3a5df979029f6e6e34"
-  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraMono.zip"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraMono.zip"
   desc "FuraMono Nerd Font (FiraMono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
