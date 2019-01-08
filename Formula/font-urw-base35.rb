08a889200afe754bb022373988378e64f4806676
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-urw-base35.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-urw-base35.rb
@@ -0,0 +1,11 @@
+class FontUrwBase35 < Formula
+  version "20170801"
+  sha256 "4505042c8859166f5bff77e33907e244b66eb4e04b56646e14e0a97e5757cd21"
+  url "https://github.com/ArtifexSoftware/urw-base35-fonts/archive/#{version}.zip"
+  desc "URW++ base 35"
+  homepage "https://github.com/ArtifexSoftware/urw-base35-fonts"
+  def install
+    
+  test do
+  end
+end
