16d92a9e2926a16826c3f47c301a44a6cfadd6c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-encodesans-semiexpanded.rb from 4b855fd673d71fdc6cb6a25766a751f68319e2ec

---
+++ Formula/font-encodesans-semiexpanded.rb
@@ -0,0 +1,10 @@
+class FontEncodesansSemiexpanded < Formula
+  head "https://github.com/google/fonts/trunk/ofl/encodesanssemiexpanded", using: :svn, trust_cert: true
+  desc "Encode Sans Semi Expanded"
+  homepage "https://fonts.google.com/specimen/Encode+Sans+Semi+Expanded"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
