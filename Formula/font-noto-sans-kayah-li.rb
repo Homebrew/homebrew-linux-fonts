254b0a0cc27ca84285017b1a48b938614e368f37
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-kayah-li.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-kayah-li.rb
@@ -0,0 +1,9 @@
+class FontNotoSansKayahLi < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKayahLi-unhinted.zip"
+  desc "Noto Sans Kayah Li"
+  homepage "https://www.google.com/get/noto/#sans-kali"
+  def install
+    
+  test do
+  end
+end
