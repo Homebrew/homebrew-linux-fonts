29c8f5105251196b6ede8d005df0b071cee91376
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka.rb from 0cb3945b3ca2f6dd1181bd8f92e00842716aff1b

---
+++ Formula/font-iosevka.rb
@@ -0,0 +1,11 @@
+class FontIosevka < Formula
+  version "2.0.2"
+  sha256 "03010cc668725e3f40e72fc8c48e4d76f4617714cf2c0e69a7dfdc0a59ef48a8"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
+  desc "Iosevka"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    
+  test do
+  end
+end
