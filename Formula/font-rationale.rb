c0024cde0464c89ac21e0cdb5e09217ddd4842de
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rationale.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-rationale.rb
@@ -0,0 +1,9 @@
+class FontRationale < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/rationale/Rationale-Regular.ttf"
+  desc "Rationale"
+  homepage "https://www.google.com/fonts/specimen/Rationale"
+  def install
+    
+  test do
+  end
+end
