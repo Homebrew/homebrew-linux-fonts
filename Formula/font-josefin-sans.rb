db0760277edd6c155ca1479a928405def1c88808
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-josefin-sans.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-josefin-sans.rb
@@ -0,0 +1,10 @@
+class FontJosefinSans < Formula
+  head "https://github.com/google/fonts/trunk/ofl/josefinsans", using: :svn, revision: "50", trust_cert: true
+  desc "Josefin Sans"
+  homepage "https://www.google.com/fonts/specimen/Josefin%20Sans"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
