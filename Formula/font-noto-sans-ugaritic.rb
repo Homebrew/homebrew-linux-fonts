0724fe008a2ebc9b0229f652d2c27707ca447abe
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-ugaritic.rb from 45cd631147cee78c9cf83f81cc0be2270a59d7eb

---
+++ Formula/font-noto-sans-ugaritic.rb
@@ -0,0 +1,9 @@
+class FontNotoSansUgaritic < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansUgaritic-unhinted.zip"
+  desc "Noto Sans Ugaritic"
+  homepage "https://www.google.com/get/noto/#sans-ugar"
+  def install
+    
+  test do
+  end
+end
