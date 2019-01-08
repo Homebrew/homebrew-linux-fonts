3e28a300516eefe64250470e3df9d8591c4311f7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-eb-garamond.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-eb-garamond.rb
@@ -0,0 +1,11 @@
+class FontEbGaramond < Formula
+  version "0.016"
+  sha256 "a0b77b405f55c10cff078787ef9d2389a9638e7604d53aa80207fe62e104c378"
+  url "https://bitbucket.org/georgd/eb-garamond/downloads/EBGaramond-#{version}.zip"
+  desc "EB Garamond"
+  homepage "https://github.com/georgd/EB-Garamond"
+  def install
+    
+  test do
+  end
+end
