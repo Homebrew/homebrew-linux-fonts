73554fb822d01508cdbcdc84aa21c380ce175fee
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nova-square.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-nova-square.rb
@@ -0,0 +1,9 @@
+class FontNovaSquare < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/novasquare/NovaSquare.ttf"
+  desc "Nova Square"
+  homepage "https://www.google.com/fonts/specimen/Nova%20Square"
+  def install
+    
+  test do
+  end
+end
