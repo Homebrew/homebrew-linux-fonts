b1472a71b4f7dd991bcf26efa15f6030ee00c3b7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-norican.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-norican.rb
@@ -0,0 +1,9 @@
+class FontNorican < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/norican/Norican-Regular.ttf"
+  desc "Norican"
+  homepage "https://www.google.com/fonts/specimen/Norican"
+  def install
+    
+  test do
+  end
+end
