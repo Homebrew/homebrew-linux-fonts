9774ba16b962b1a6e32fc274ef276903a877152a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pt-sans.rb from 5698597dbd89487ab557cc2086b32fdb4d145b45

---
+++ Formula/font-pt-sans.rb
@@ -0,0 +1,9 @@
+class FontPtSans < Formula
+  head "https://company.paratype.com/system/attachments/629/original/ptsans.zip"
+  desc "PT Sans"
+  homepage "https://www.paratype.com/public/"
+  def install
+    
+  test do
+  end
+end
