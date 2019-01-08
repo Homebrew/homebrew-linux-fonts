c9c0edb3e9a562ce19d2d2859c5e7c7c2a933bc4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-thai.rb from 45cd631147cee78c9cf83f81cc0be2270a59d7eb

---
+++ Formula/font-noto-sans-thai.rb
@@ -0,0 +1,9 @@
+class FontNotoSansThai < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansThai-unhinted.zip"
+  desc "Noto Sans Thai"
+  homepage "https://www.google.com/get/noto/#sans-thai"
+  def install
+    
+  test do
+  end
+end
