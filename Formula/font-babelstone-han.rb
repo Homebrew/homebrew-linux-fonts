95ced7b03843b1ee07a4954001032a4954f9f41e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-babelstone-han.rb from a426e3452592befbc14865de022394e767d9a788

---
+++ Formula/font-babelstone-han.rb
@@ -0,0 +1,9 @@
+class FontBabelstoneHan < Formula
+  head "http://www.babelstone.co.uk/Fonts/Download/BabelStoneHan.zip"
+  desc "BabelStone Han"
+  homepage "http://babelstone.co.uk/Fonts/Han.html"
+  def install
+    
+  test do
+  end
+end
