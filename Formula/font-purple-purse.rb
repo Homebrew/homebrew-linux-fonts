9c053a0aa949ba0ca61ea0cf3214d68678d34a0a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-purple-purse.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-purple-purse.rb
@@ -0,0 +1,9 @@
+class FontPurplePurse < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/purplepurse/PurplePurse-Regular.ttf"
+  desc "Purple Purse"
+  homepage "https://www.google.com/fonts/specimen/Purple+Purse"
+  def install
+    
+  test do
+  end
+end
