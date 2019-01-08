0ee33a993b014842dc3a4914a3ba6b219f29f677
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-meslo-lg.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-meslo-lg.rb
@@ -0,0 +1,11 @@
+class FontMesloLg < Formula
+  version "1.2.1"
+  sha256 "d0bcb7668dda8fa1a0f8162d626adb434c32854e243b5bd52a717cf569af08d0"
+  url "https://github.com/andreberg/Meslo-Font/raw/master/dist/v#{version}/Meslo%20LG%20v#{version}.zip"
+  desc "Meslo LG"
+  homepage "https://github.com/andreberg/Meslo-Font"
+  def install
+    
+  test do
+  end
+end
