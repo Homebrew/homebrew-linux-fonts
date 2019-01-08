e3852e78f96fb21e52752020b5deb7785e8f2b46
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-awesome-terminal-fonts.rb from cb406b6bd3096ad5c16e4e1c1edcc3badf75a4a5

---
+++ Formula/font-awesome-terminal-fonts.rb
@@ -0,0 +1,9 @@
+class FontAwesomeTerminalFonts < Formula
+  head "https://github.com/gabrielelana/awesome-terminal-fonts/archive/patching-strategy.tar.gz"
+  desc "Awesome Terminal Fonts"
+  homepage "https://github.com/gabrielelana/awesome-terminal-fonts"
+  def install
+    
+  test do
+  end
+end
