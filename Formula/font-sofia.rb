622f0a6f24387727107ca3d4d894ddd2371cc084
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sofia.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sofia.rb
@@ -0,0 +1,9 @@
+class FontSofia < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sofia/Sofia-Regular.ttf"
+  desc "Sofia"
+  homepage "https://www.google.com/fonts/specimen/Sofia"
+  def install
+    
+  test do
+  end
+end
