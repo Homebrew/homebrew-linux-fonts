398333ec4d77c114eb6394d3a76a399a69fb41bf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-devicons.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-devicons.rb
@@ -0,0 +1,11 @@
+class FontDevicons < Formula
+  version "1.8.0"
+  sha256 "fc0baa260f54832c059d1d9eab3798ae758d1a1cf0c1695e9883aab85d9a4308"
+  url "https://github.com/vorillaz/devicons/archive/#{version}.zip"
+  desc "Devicons"
+  homepage "https://vorillaz.github.io/devicons/"
+  def install
+    
+  test do
+  end
+end
