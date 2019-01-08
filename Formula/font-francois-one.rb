a954b9787a0ffaab3b26b2210a6936bbf9336093
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-francois-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-francois-one.rb
@@ -0,0 +1,9 @@
+class FontFrancoisOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/francoisone/FrancoisOne-Regular.ttf"
+  desc "Francois One"
+  homepage "https://www.google.com/fonts/specimen/Francois+One"
+  def install
+    
+  test do
+  end
+end
