23712302f6c8208f32f1c465673f5e624c6ffb40
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sniglet.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-sniglet.rb
@@ -0,0 +1,10 @@
+class FontSniglet < Formula
+  head "https://github.com/google/fonts/trunk/ofl/sniglet", using: :svn, revision: "50", trust_cert: true
+  desc "Sniglet"
+  homepage "https://www.google.com/fonts/specimen/Sniglet"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
