755c7324d189056727ccba62717317c7a15353d4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rancho.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-rancho.rb
@@ -0,0 +1,9 @@
+class FontRancho < Formula
+  head "https://github.com/google/fonts/raw/master/apache/rancho/Rancho-Regular.ttf"
+  desc "Rancho"
+  homepage "https://www.google.com/fonts/specimen/Rancho"
+  def install
+    
+  test do
+  end
+end
