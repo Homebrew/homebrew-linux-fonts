128d88b972b1e657bc7828b083755072c23b4ab1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-baron.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-baron.rb
@@ -0,0 +1,9 @@
+class FontBaron < Formula
+  head "https://www.fontfabric.com/downfont/baron.zip"
+  desc "Baron"
+  homepage "https://www.fontfabric.com/baron-free-font/"
+  def install
+    
+  test do
+  end
+end
