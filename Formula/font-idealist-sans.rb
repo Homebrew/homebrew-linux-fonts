e7db2b15e5f8ad360f642ce3a4684cf96ee7ea41
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-idealist-sans.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-idealist-sans.rb
@@ -0,0 +1,9 @@
+class FontIdealistSans < Formula
+  head "https://fontfabric.com/downfont/idealist.zip"
+  desc "Idealist"
+  homepage "https://fontfabric.com/idealist-sans/"
+  def install
+    
+  test do
+  end
+end
