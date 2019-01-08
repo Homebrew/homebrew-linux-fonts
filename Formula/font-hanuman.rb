505fd43c611306e70996a840445be34300b87b5b
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:55:35 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hanuman.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-hanuman.rb
@@ -0,0 +1,12 @@
+class FontHanuman < Formula
+  head "https://github.com/google/fonts/trunk/ofl/hanuman", using: :svn, revision: "50", trust_cert: true
+  desc "Hanuman"
+  homepage "https://www.google.com/fonts/specimen/Hanuman"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "Hanuman.ttf"
+    (share/"fonts").install "Hanumanb.ttf"
+  end
+  test do
+  end
+end
