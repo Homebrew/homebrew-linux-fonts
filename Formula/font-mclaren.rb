12feb5d2d11ce7e2e08563e237b6fcdc535c75ef
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mclaren.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-mclaren.rb
@@ -0,0 +1,9 @@
+class FontMclaren < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/mclaren/McLaren-Regular.ttf"
+  desc "McLaren"
+  homepage "https://www.google.com/fonts/specimen/McLaren"
+  def install
+    
+  test do
+  end
+end
