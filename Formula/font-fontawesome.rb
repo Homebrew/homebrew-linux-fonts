fc87a540cb4fe95e26c2785195a228e544e2b874
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fontawesome.rb from cc6d7d2a262c58e3b270c63ca725ba3596aac709

---
+++ Formula/font-fontawesome.rb
@@ -0,0 +1,11 @@
+class FontFontawesome < Formula
+  version "5.5.0"
+  sha256 "18e94c42dd01c86bd45b3dad287266247e59362fe545ff748bde2ba45b192dac"
+  url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip"
+  desc "Font Awesome"
+  homepage "http://fontawesome.io/"
+  def install
+    
+  test do
+  end
+end
