1305b4feefaa9a9627172d220696fdee22094508
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-brill.rb from 82b13c722b57af1ab3714567372a6e1f81d1b1ab

---
+++ Formula/font-brill.rb
@@ -0,0 +1,11 @@
+class FontBrill < Formula
+  version "2.06"
+  sha256 "b58eeb26ea43d9a020ac9891a395ff7364bac7215d0714910561b675e4947166"
+  url "https://brill.com/fileasset/downloads_static/static_typefacedownload_typefacepackage.zip"
+  desc "Brill"
+  homepage "https://brill.com/page/BrillFont/"
+  def install
+    
+  test do
+  end
+end
