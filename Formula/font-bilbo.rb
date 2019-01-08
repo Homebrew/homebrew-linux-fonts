e35ea60bb5b60794355c3b2201ea8083a1ca120b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bilbo.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bilbo.rb
@@ -0,0 +1,9 @@
+class FontBilbo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/bilbo/Bilbo-Regular.ttf"
+  desc "Bilbo"
+  homepage "https://www.google.com/fonts/specimen/Bilbo"
+  def install
+    
+  test do
+  end
+end
