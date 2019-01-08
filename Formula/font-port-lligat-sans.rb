0055421ace736cd059aeb0b95d0aa54049b6d066
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-port-lligat-sans.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-port-lligat-sans.rb
@@ -0,0 +1,9 @@
+class FontPortLligatSans < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/portlligatsans/PortLligatSans-Regular.ttf"
+  desc "Port Lligat Sans"
+  homepage "https://www.google.com/fonts/specimen/Port+Lligat+Sans"
+  def install
+    
+  test do
+  end
+end
