7c614867ddf734828cea0d691878c77e14331252
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oskidenea.rb from abe9aaed7d00e3a2229fae0cd5cedbf37b04afdf

---
+++ Formula/font-oskidenea.rb
@@ -0,0 +1,9 @@
+class FontOskidenea < Formula
+  head "http://www.languagegeek.com/font/OskiDeneA.zip"
+  desc "OskiDeneA"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
