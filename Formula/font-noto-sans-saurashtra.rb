ead703fb392d7473a946ad2bd728e30e0d35b9b0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-saurashtra.rb from 7eff405e323aa4047b9815ba7e3a316e52a51c46

---
+++ Formula/font-noto-sans-saurashtra.rb
@@ -0,0 +1,9 @@
+class FontNotoSansSaurashtra < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSaurashtra-unhinted.zip"
+  desc "Noto Sans Saurashtra"
+  homepage "https://www.google.com/get/noto/#sans-saur"
+  def install
+    
+  test do
+  end
+end
