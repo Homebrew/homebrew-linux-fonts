ba290526b096c6a18ad27251e3e831e448642448
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iceland.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-iceland.rb
@@ -0,0 +1,9 @@
+class FontIceland < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/iceland/Iceland-Regular.ttf"
+  desc "Iceland"
+  homepage "https://www.google.com/fonts/specimen/Iceland"
+  def install
+    
+  test do
+  end
+end
