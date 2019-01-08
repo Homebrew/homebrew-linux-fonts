1796f3877d6dfa1c2cd877571e3b227286e7ecd5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pinyon-script.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-pinyon-script.rb
@@ -0,0 +1,9 @@
+class FontPinyonScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/pinyonscript/PinyonScript-Regular.ttf"
+  desc "Pinyon Script"
+  homepage "https://www.google.com/fonts/specimen/Pinyon+Script"
+  def install
+    
+  test do
+  end
+end
