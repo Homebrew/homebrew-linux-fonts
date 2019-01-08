7c18d2bf8ce3ed01d459f22464fbe8219ebf45f2
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mononoki.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-mononoki.rb
+++ Formula/font-mononoki.rb
@@ -1,7 +1,7 @@
 class FontMononoki < Formula
   version "1.2"
   sha256 "cf29ea7663dbfe94feaedaf5c0be4f4ae6a5b98c1293e0e772e69c28ac5dc658"
-  head "https://github.com/madmalik/mononoki/releases/download/#{version}/mononoki.zip"
+  url "https://github.com/madmalik/mononoki/releases/download/#{version}/mononoki.zip"
   desc "Mononoki"
   homepage "https://madmalik.github.io/mononoki/"
   def install
