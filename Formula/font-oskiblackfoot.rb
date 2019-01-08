5feb90682391921a94e1c8b4c38bde926aa9545c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oskiblackfoot.rb from 749eec05f19b7449b62236c79be11036b5ff7282

---
+++ Formula/font-oskiblackfoot.rb
@@ -0,0 +1,9 @@
+class FontOskiblackfoot < Formula
+  head "http://www.languagegeek.com/font/oskiblackfoot.zip"
+  desc "OskiBlackfoot"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
