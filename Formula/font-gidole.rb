d2f90092d86bbf115719fc6ee4149bae9d6f1e06
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gidole.rb from b9f81982b538c9f7f0ef58c4a3ac586a15009990

---
+++ Formula/font-gidole.rb
@@ -0,0 +1,9 @@
+class FontGidole < Formula
+  head "https://github.com/gidole/Gidole-Typefaces/raw/master/gidole.zip"
+  desc "Gidole"
+  homepage "https://gidole.github.io/"
+  def install
+    
+  test do
+  end
+end
