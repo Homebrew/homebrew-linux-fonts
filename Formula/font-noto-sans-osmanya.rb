e6e5eaacd2b31011cad9f60c1308804f9f9ee650
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-osmanya.rb from 7eff405e323aa4047b9815ba7e3a316e52a51c46

---
+++ Formula/font-noto-sans-osmanya.rb
@@ -0,0 +1,9 @@
+class FontNotoSansOsmanya < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOsmanya-unhinted.zip"
+  desc "Noto Sans Osmanya"
+  homepage "https://www.google.com/get/noto/#sans-osma"
+  def install
+    
+  test do
+  end
+end
