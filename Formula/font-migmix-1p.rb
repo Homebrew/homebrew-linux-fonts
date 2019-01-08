ca6afd4e1f571d574ea1feb8267e33549eb167aa
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-migmix-1p.rb from cb406b6bd3096ad5c16e4e1c1edcc3badf75a4a5

---
+++ Formula/font-migmix-1p.rb
@@ -0,0 +1,11 @@
+class FontMigmix1p < Formula
+  version "20150712"
+  sha256 "d71aa59146c600bc2f22de87495fe0127741fbb692736be0e1fe454e128c9d76"
+  url "http://dl.osdn.jp/mix-mplus-ipa/63544/migmix-1p-#{version}.zip"
+  desc "MigMix 1P"
+  homepage "http://mix-mplus-ipa.osdn.jp/migmix/#migmix1p"
+  def install
+    
+  test do
+  end
+end
