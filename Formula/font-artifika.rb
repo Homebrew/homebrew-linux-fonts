d8e80944842a9693f019cadedd68f9b86872b162
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-artifika.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-artifika.rb
@@ -0,0 +1,9 @@
+class FontArtifika < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/artifika/Artifika-Regular.ttf"
+  desc "Artifika"
+  homepage "https://www.google.com/fonts/specimen/Artifika"
+  def install
+    
+  test do
+  end
+end
