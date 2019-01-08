6c781044719307bd0cfe9af514e95957b63f021a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-latin-modern.rb from bf533b1fff6f7c7fad978dd6d000717114a5f67f

---
+++ Formula/font-latin-modern.rb
@@ -0,0 +1,11 @@
+class FontLatinModern < Formula
+  version "2.004"
+  sha256 "5b0236051d3728be6616f1b274e3b910473875b5a3ef9382f0ef00384ddb161b"
+  url "http://www.gust.org.pl/projects/e-foundry/latin-modern/download/lm#{version}otf.zip"
+  desc "Latin Modern"
+  homepage "http://www.gust.org.pl/projects/e-foundry/latin-modern"
+  def install
+    
+  test do
+  end
+end
