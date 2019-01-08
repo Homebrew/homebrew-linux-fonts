decc107525e5edb6ab390a105985711d1c2954ce
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alegreya.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-alegreya.rb
+++ Formula/font-alegreya.rb
@@ -1,7 +1,7 @@
 class FontAlegreya < Formula
   version "2.007"
   sha256 "5a18133a815b6ff7ca20e1a50a7c536d32a5d3775502cf3f9ae16a06740b7703"
-  url "https://github.com/huertatipografica/Alegreya/archive/v#{version}.zip"
+  head "https://github.com/huertatipografica/Alegreya/archive/v#{version}.zip"
   desc "Alegreya"
   homepage "https://github.com/huertatipografica/Alegreya"
   def install
