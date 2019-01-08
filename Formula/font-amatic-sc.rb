a5a957bb4acda0a0ee60fd5e174f8c0a3608e5a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-amatic-sc.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-amatic-sc.rb
@@ -0,0 +1,10 @@
+class FontAmaticSc < Formula
+  head "https://github.com/google/fonts/trunk/ofl/amaticsc", using: :svn, revision: "50", trust_cert: true
+  desc "Amatic SC"
+  homepage "https://www.google.com/fonts/specimen/Amatic%20SC"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
