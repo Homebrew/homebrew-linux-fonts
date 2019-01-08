ffeaff716718c00a8c96773bee3d9f5835853388
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:28 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bitstream-vera.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-bitstream-vera.rb
@@ -0,0 +1,11 @@
+class FontBitstreamVera < Formula
+  version "1.10"
+  sha256 "1b0ba0f7af2e1d05f64e259d351965a2cb2673104a057ce715a06969c478f6cc"
+  url "https://ftp.gnome.org/pub/GNOME/sources/ttf-bitstream-vera/1.10/ttf-bitstream-vera-1.10.zip"
+  desc "Bitstream Vera"
+  homepage "https://www.gnome.org/fonts/"
+  def install
+    
+  test do
+  end
+end
