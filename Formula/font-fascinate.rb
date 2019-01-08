8483f661c93dcf6fbd48aabcade110d77841b2a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fascinate.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-fascinate.rb
@@ -0,0 +1,9 @@
+class FontFascinate < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/fascinate/Fascinate-Regular.ttf"
+  desc "Fascinate"
+  homepage "https://www.google.com/fonts/specimen/Fascinate"
+  def install
+    
+  test do
+  end
+end
