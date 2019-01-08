b2247446d499ce0688bb8170185c26075e990714
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-webdings.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-webdings.rb
@@ -0,0 +1,9 @@
+class FontWebdings < Formula
+  head "https://downloads.sourceforge.net/corefonts/webdin32.exe"
+  desc "Webdings"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  def install
+    
+  test do
+  end
+end
