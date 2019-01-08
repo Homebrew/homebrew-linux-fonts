8a8278af50a8ef402260ec8ece1d40acad4899bb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arvo.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-arvo.rb
@@ -0,0 +1,10 @@
+class FontArvo < Formula
+  head "https://github.com/google/fonts/trunk/ofl/arvo", using: :svn, revision: "50", trust_cert: true
+  desc "Arvo"
+  homepage "https://www.google.com/fonts/specimen/Arvo"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
