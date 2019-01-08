c4728dd9ceeabc33ed4fd2336f04e2fbe50a5ebf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-bengali.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-sans-bengali.rb
@@ -0,0 +1,9 @@
+class FontNotoSansBengali < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBengali-unhinted.zip"
+  desc "Noto Sans Bengali"
+  homepage "https://www.google.com/get/noto/#sans-beng"
+  def install
+    
+  test do
+  end
+end
