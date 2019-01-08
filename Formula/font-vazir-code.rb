d2297fa7deb4733c0056880b06358b6dbd941ec1
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:35:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-vazir-code.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-vazir-code.rb
+++ Formula/font-vazir-code.rb
@@ -1,7 +1,7 @@
 class FontVazirCode < Formula
   version "1.0.3"
   sha256 "8a0bcc3e0ab9f086aeb3d8d2827825dc5b582f34562ad4c0b6f79242a692249a"
-  head "https://github.com/rastikerdar/vazir-code-font/releases/download/v#{version}/vazir-code-font-v#{version}.zip"
+  url "https://github.com/rastikerdar/vazir-code-font/releases/download/v#{version}/vazir-code-font-v#{version}.zip"
   desc "Vazir Code"
   homepage "https://rastikerdar.github.io/vazir-code-font/"
   def install
