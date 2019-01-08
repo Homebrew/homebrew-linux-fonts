57935210c51b6c59068369c12f2b2bac86582e0a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-emblema-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-emblema-one.rb
@@ -0,0 +1,9 @@
+class FontEmblemaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/emblemaone/EmblemaOne-Regular.ttf"
+  desc "Emblema One"
+  homepage "https://www.google.com/fonts/specimen/Emblema+One"
+  def install
+    
+  test do
+  end
+end
