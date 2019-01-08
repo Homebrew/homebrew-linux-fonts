3b5cdf99125fb215b25db34dd58622560bc0511b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fredoka-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-fredoka-one.rb
@@ -0,0 +1,9 @@
+class FontFredokaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/fredokaone/FredokaOne-Regular.ttf"
+  desc "Fredoka One"
+  homepage "https://www.google.com/fonts/specimen/Fredoka+One"
+  def install
+    
+  test do
+  end
+end
