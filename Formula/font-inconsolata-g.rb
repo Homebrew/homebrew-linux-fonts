1cbd133c818aa005e13a65f42909498f6ed563f0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-g.rb from 65eee2ec79a4894d7da2b342c93624c7e0bdbbdc

---
+++ Formula/font-inconsolata-g.rb
@@ -0,0 +1,9 @@
+class FontInconsolataG < Formula
+  head "http://www.fantascienza.net/leonardo/ar/inconsolatag/inconsolata-g_font.zip"
+  desc "Inconsolata-g"
+  homepage "https://leonardo-m.livejournal.com/77079.html"
+  def install
+    
+  test do
+  end
+end
