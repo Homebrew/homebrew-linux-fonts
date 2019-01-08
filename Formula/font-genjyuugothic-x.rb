7d61813b8bdb91035c1a1844522372a2a2e12223
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-genjyuugothic-x.rb from cb406b6bd3096ad5c16e4e1c1edcc3badf75a4a5

--- Formula/font-genjyuugothic-x.rb
+++ Formula/font-genjyuugothic-x.rb
@@ -1,7 +1,7 @@
 class FontGenjyuugothicX < Formula
   version "20150607,8.644"
   sha256 "e4a0ea11b8155056ad2b678c8501b2e76dd99b8c8eb5363d396fe7c3079201b3"
-  url "http://osdn.dl.osdn.jp/users/#{version.to_s.sub(/.*,/, "").sub(/\..*/, "")}/#{version.to_s.sub(/.*,/, "").gsub(".", "")}/genjyuugothic-x-#{version.to_s.sub(/,.*/, "")}.zip"
+  head "http://osdn.dl.osdn.jp/users/#{version.after_comma.major}/#{version.after_comma.no_dots}/genjyuugothic-x-#{version.before_comma}.zip"
   desc "Gen Jyuu GothicX"
   homepage "http://jikasei.me/font/genjyuu/"
   def install
