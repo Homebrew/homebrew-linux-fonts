b059f255b2da09b5b6de0fff5560dd4d9634fb33
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sevillana.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sevillana.rb
@@ -0,0 +1,9 @@
+class FontSevillana < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sevillana/Sevillana-Regular.ttf"
+  desc "Sevillana"
+  homepage "https://www.google.com/fonts/specimen/Sevillana"
+  def install
+    
+  test do
+  end
+end
