81f1c6c66584914b91af04487a35528d50058aee
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-caesar-dressing.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-caesar-dressing.rb
@@ -0,0 +1,9 @@
+class FontCaesarDressing < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/caesardressing/CaesarDressing-Regular.ttf"
+  desc "Caesar Dressing"
+  homepage "https://www.google.com/fonts/specimen/Caesar+Dressing"
+  def install
+    
+  test do
+  end
+end
