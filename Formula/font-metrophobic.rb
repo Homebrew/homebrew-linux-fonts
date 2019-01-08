50434234f9f9ca744a636b569b7ec6392524f84f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-metrophobic.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-metrophobic.rb
@@ -0,0 +1,9 @@
+class FontMetrophobic < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/metrophobic/Metrophobic-Regular.ttf"
+  desc "Metrophobic"
+  homepage "https://www.google.com/fonts/specimen/Metrophobic"
+  def install
+    
+  test do
+  end
+end
