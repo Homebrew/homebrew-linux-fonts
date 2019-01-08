4ff563b88969d50414cb05207b42e45a5f60fed6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-conakry.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-conakry.rb
@@ -0,0 +1,9 @@
+class FontConakry < Formula
+  head "https://www.evertype.com/fonts/nko/ConakryFont.zip"
+  desc "Conakry"
+  homepage "https://www.evertype.com/fonts/nko/"
+  def install
+    
+  test do
+  end
+end
