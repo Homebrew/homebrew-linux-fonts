2604ebdc31f8cfc6ddd83b843042886fc4269b1f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marvel.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-marvel.rb
@@ -0,0 +1,10 @@
+class FontMarvel < Formula
+  head "https://github.com/google/fonts/trunk/ofl/marvel", using: :svn, revision: "50", trust_cert: true
+  desc "Marvel"
+  homepage "https://www.google.com/fonts/specimen/Marvel"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
