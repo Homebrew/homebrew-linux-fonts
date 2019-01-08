37f45094a7f954fec5bdc05e0a3a24fe52d7027a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-irish-grover.rb from c74a667a92fd8e3d1b5746f8ef125e9c7a45895e

---
+++ Formula/font-irish-grover.rb
@@ -0,0 +1,9 @@
+class FontIrishGrover < Formula
+  head "https://github.com/google/fonts/raw/master/apache/irishgrover/IrishGrover-Regular.ttf"
+  desc "Irish Grover"
+  homepage "https://www.google.com/fonts/specimen/Irish+Grover"
+  def install
+    
+  test do
+  end
+end
