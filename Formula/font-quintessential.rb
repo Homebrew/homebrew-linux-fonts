ae063db69b7ee94cddd91a14333d85f4475a0939
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quintessential.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-quintessential.rb
@@ -0,0 +1,9 @@
+class FontQuintessential < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/quintessential/Quintessential-Regular.ttf"
+  desc "Quintessential"
+  homepage "https://www.google.com/fonts/specimen/Quintessential"
+  def install
+    
+  test do
+  end
+end
