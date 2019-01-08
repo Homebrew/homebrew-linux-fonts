14fe7c7bfcbbe2f3f5b74475bba904c3c50e206f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-accordance.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-accordance.rb
@@ -0,0 +1,9 @@
+class FontAccordance < Formula
+  head "https://www.accordancefiles1.com/products/Accordance_Font.zip"
+  desc "Accordance"
+  homepage "https://www.accordancebible.com/Accordance-Font"
+  def install
+    
+  test do
+  end
+end
