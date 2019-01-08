ebfd3bfb3cfaac0229995059fc43e856714f01fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-permanent-marker.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-permanent-marker.rb
@@ -0,0 +1,9 @@
+class FontPermanentMarker < Formula
+  head "https://github.com/google/fonts/raw/master/apache/permanentmarker/PermanentMarker-Regular.ttf"
+  desc "Permanent Marker"
+  homepage "https://www.google.com/fonts/specimen/Permanent+Marker"
+  def install
+    
+  test do
+  end
+end
