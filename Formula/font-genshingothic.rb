e40242f88e4b6bc86e4a82cdf9d2b03860aebc14
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-genshingothic.rb from cb406b6bd3096ad5c16e4e1c1edcc3badf75a4a5

--- Formula/font-genshingothic.rb
+++ Formula/font-genshingothic.rb
@@ -1,7 +1,7 @@
 class FontGenshingothic < Formula
   version "20150607,8.637"
   sha256 "b8e00f00a6e2517bfe75ceb2a732b596fe002457b89c05c181d6b71373aada58"
-  url "http://osdn.dl.osdn.jp/users/#{version.to_s.sub(/.*,/, "").sub(/\..*/, "")}/#{version.to_s.sub(/.*,/, "").gsub(".", "")}/genshingothic-#{version.to_s.sub(/,.*/, "")}.zip"
+  head "http://osdn.dl.osdn.jp/users/#{version.after_comma.major}/#{version.after_comma.no_dots}/genshingothic-#{version.before_comma}.zip"
   desc "Gen Shin Gothic"
   homepage "http://jikasei.me/font/genshin/"
   def install
