3734d29096b7c820520996cf82ae95a2e44fa803
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cherry-swash.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-cherry-swash.rb
@@ -0,0 +1,10 @@
+class FontCherrySwash < Formula
+  head "https://github.com/google/fonts/trunk/ofl/cherryswash", using: :svn, revision: "50", trust_cert: true
+  desc "Cherry Swash"
+  homepage "https://www.google.com/fonts/specimen/Cherry%20Swash"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
