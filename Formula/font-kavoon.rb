dd421c0876d1ec514fa1ce854f8875592fdd11ec
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kavoon.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-kavoon.rb
@@ -0,0 +1,9 @@
+class FontKavoon < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kavoon/Kavoon-Regular.ttf"
+  desc "Kavoon"
+  homepage "https://www.google.com/fonts/specimen/Kavoon"
+  def install
+    
+  test do
+  end
+end
