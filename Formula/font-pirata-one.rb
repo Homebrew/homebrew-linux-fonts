b71954b32ca7a27e3e6306dff96bbf829503e6d3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pirata-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-pirata-one.rb
@@ -0,0 +1,9 @@
+class FontPirataOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/pirataone/PirataOne-Regular.ttf"
+  desc "Pirata One"
+  homepage "https://www.google.com/fonts/specimen/Pirata+One"
+  def install
+    
+  test do
+  end
+end
