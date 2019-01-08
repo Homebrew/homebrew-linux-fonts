64fbe4ab3e09b37831c76478ce8427f750bb5503
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-dw-pica-sc.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-im-fell-dw-pica-sc.rb
@@ -0,0 +1,9 @@
+class FontImFellDwPicaSc < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/imfelldwpicasc/IMFePIsc28P.ttf"
+  desc "IM Fell DW Pica"
+  homepage "https://www.google.com/fonts/specimen/IM+Fell+DW+Pica"
+  def install
+    
+  test do
+  end
+end
