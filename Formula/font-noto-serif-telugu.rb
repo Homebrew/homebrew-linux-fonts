936cc37f890231d7a682bc6e9a8d73438fe92622
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-telugu.rb from 6612748208449843385714429f09395a19960b12

---
+++ Formula/font-noto-serif-telugu.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifTelugu < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifTelugu-unhinted.zip"
+  desc "Noto Serif Telugu"
+  homepage "https://www.google.com/get/noto/#serif-telu"
+  def install
+    
+  test do
+  end
+end
