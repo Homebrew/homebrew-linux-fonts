e33df6b051d2a5e93f24a223fbe54486dd6f1217
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-overlock.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-overlock.rb
@@ -0,0 +1,10 @@
+class FontOverlock < Formula
+  head "https://github.com/google/fonts/trunk/ofl/overlock", using: :svn, revision: "50", trust_cert: true
+  desc "Overlock"
+  homepage "https://www.google.com/fonts/specimen/Overlock"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
