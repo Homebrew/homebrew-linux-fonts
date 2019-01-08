dd5fcd70139c775aa3d3c0dfad6d735179e06b89
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-terminus-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-terminus-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontTerminusNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "3dd17846e4749415ab2ff06e6af254cfde627feda202cda14efc93e87adc4e9b"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Terminus.zip"
+  desc "TerminessTTF Nerd Font (Terminus)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
