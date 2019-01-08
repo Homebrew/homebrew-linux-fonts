432b65d02c11a356403c44f6da88a8e36bcdb3a2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-yiddishkeit.rb from 03ab7088079c366d36740c3d1146af1569e8e041

---
+++ Formula/font-yiddishkeit.rb
@@ -0,0 +1,9 @@
+class FontYiddishkeit < Formula
+  head "https://alefalefalef.co.il/wp-content/fonts/free/YiddishkeitAlefAlefAlef-Bold.zip"
+  desc "Yiddishkeit"
+  homepage "https://alefalefalef.co.il/resources/פונטים-בחינם/"
+  def install
+    
+  test do
+  end
+end
