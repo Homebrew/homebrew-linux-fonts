62bcaf7725d8e1f5fc3e16fbbdf39d51aa225677
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quivira.rb from 48d8f7d587b52e737969f05795e1838dc7f7cc12

---
+++ Formula/font-quivira.rb
@@ -0,0 +1,9 @@
+class FontQuivira < Formula
+  head "http://www.quivira-font.com/files/Quivira.otf"
+  desc "Quivira"
+  homepage "http://www.quivira-font.com/downloads.php"
+  def install
+    
+  test do
+  end
+end
