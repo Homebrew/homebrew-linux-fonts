fdeb67df479fb6f54f34195d1fdc9c06a47b2b03
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-moulpali.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-moulpali.rb
@@ -0,0 +1,9 @@
+class FontMoulpali < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/moulpali/Moulpali.ttf"
+  desc "Moulpali"
+  homepage "https://www.google.com/fonts/specimen/Moulpali"
+  def install
+    
+  test do
+  end
+end
