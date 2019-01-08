76b0c2395c0a2d01298826d0e0b053f81b6f233d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-fira-mono.rb
@@ -0,0 +1,11 @@
+class FontFiraMono < Formula
+  version "3.206,4.202"
+  sha256 "d86269657387f144d77ba12011124f30f423f70672e1576dc16f918bb16ddfe4"
+  url "https://github.com/mozilla/Fira/archive/#{version.to_s.sub(/.*,/, "")}.tar.gz"
+  desc "Fira Mono"
+  homepage "https://mozilla.github.io/Fira/"
+  def install
+    
+  test do
+  end
+end
