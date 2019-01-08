41d5b395acc3a44a9dedadf625c3fd722e2c1eae
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:58:35 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oswald.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-oswald.rb
@@ -0,0 +1,16 @@
+class FontOswald < Formula
+  head "https://github.com/google/fonts/trunk/ofl/oswald", using: :svn, revision: "1208", trust_cert: true
+  desc "Oswald"
+  homepage "https://www.google.com/fonts/specimen/Oswald"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "Oswald-Bold.ttf"
+    (share/"fonts").install "Oswald-ExtraLight.ttf"
+    (share/"fonts").install "Oswald-Light.ttf"
+    (share/"fonts").install "Oswald-Medium.ttf"
+    (share/"fonts").install "Oswald-Regular.ttf"
+    (share/"fonts").install "Oswald-SemiBold.ttf"
+  end
+  test do
+  end
+end
