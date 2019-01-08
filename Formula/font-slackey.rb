ed8ecb9c9713bf3438f31a2e55aa0d07cb14e853
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-slackey.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-slackey.rb
@@ -0,0 +1,9 @@
+class FontSlackey < Formula
+  head "https://github.com/google/fonts/raw/master/apache/slackey/Slackey-Regular.ttf"
+  desc "Slackey"
+  homepage "https://www.google.com/fonts/specimen/Slackey"
+  def install
+    
+  test do
+  end
+end
