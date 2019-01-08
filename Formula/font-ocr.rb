6d350cf43a5bca2b925c2d99230477c0fa44d880
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ocr.rb from af18fb8c73df08b267cfe77176d3d58fc1566a17

---
+++ Formula/font-ocr.rb
@@ -0,0 +1,11 @@
+class FontOcr < Formula
+  version "0.2"
+  sha256 "39289c641520265ecedbade99f01600f316f8196ec57f71c8402d3ba09438666"
+  url "http://cznic.dl.osdn.jp/tsukurimashou/56948/ocr-#{version}.zip"
+  desc "OCR"
+  homepage "http://ansuz.sooke.bc.ca/page/fonts#ocra"
+  def install
+    
+  test do
+  end
+end
