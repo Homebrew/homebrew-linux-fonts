4ede412c6dfe8a08e546eddf2bc9ba6c34a495b9
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:59:48 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-han-serif.rb from 5dbd44fa52481319a4ede707a0b5bbe889331827

---
+++ Formula/font-source-han-serif.rb
@@ -0,0 +1,13 @@
+class FontSourceHanSerif < Formula
+  version "1.001R"
+  sha256 "85cc634fa228286ca307803bbb4ca61f61fa821b3ed573f4f07c6f0c064426b5"
+  url "https://github.com/adobe-fonts/source-han-serif/releases/download/1.001R/SourceHanSerif.ttc"
+  desc "Source Han Serif"
+  homepage "https://github.com/adobe-fonts/source-han-serif"
+  depends_on macos: ">= :mountain_lion"
+  def install
+    (share/"fonts").install "SourceHanSerif.ttc"
+  end
+  test do
+  end
+end
