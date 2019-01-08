dabba65d355c1ea9607f7012927ec13b3545542d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-galindo.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-galindo.rb
@@ -0,0 +1,9 @@
+class FontGalindo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/galindo/Galindo-Regular.ttf"
+  desc "Galindo"
+  homepage "https://www.google.com/fonts/specimen/Galindo"
+  def install
+    
+  test do
+  end
+end
