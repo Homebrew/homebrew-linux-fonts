3c66898a700f119ca018271518f4e79ead7eebef
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monda.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-monda.rb
@@ -0,0 +1,10 @@
+class FontMonda < Formula
+  head "https://github.com/google/fonts/trunk/ofl/monda", using: :svn, revision: "50", trust_cert: true
+  desc "Monda"
+  homepage "https://www.google.com/fonts/specimen/Monda"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
