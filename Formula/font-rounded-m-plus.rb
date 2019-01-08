249b93eacc2316b173583f78a837c1c4b7854c45
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rounded-m-plus.rb from c99fb34dbf053e375133073dc3b5fb4715e93d67

---
+++ Formula/font-rounded-m-plus.rb
@@ -0,0 +1,11 @@
+class FontRoundedMPlus < Formula
+  version "20150529"
+  sha256 "e746736c8ded99fe9a9dd72a241ec59435eaa282a18e7ac33a26dc0578c06ff7"
+  url "http://cznic.dl.osdn.jp/users/8/8569/rounded-mplus-#{version}.7z"
+  desc "Rounded M+"
+  homepage "http://jikasei.me/font/rounded-mplus/"
+  def install
+    
+  test do
+  end
+end
