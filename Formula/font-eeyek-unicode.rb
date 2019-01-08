71466052e4b76b58db8780de38435ee7b5fcaab9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-eeyek-unicode.rb from e3ba499db3e57f9d7f2bfe85eaf3a757f838a74f

---
+++ Formula/font-eeyek-unicode.rb
@@ -0,0 +1,9 @@
+class FontEeyekUnicode < Formula
+  head "http://tabish.freeshell.org/eeyek/eeyek.zip"
+  desc "Eeyek Unicode"
+  homepage "http://tabish.freeshell.org/eeyek/download.html"
+  def install
+    
+  test do
+  end
+end
