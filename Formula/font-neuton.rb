baebf6ed6f446086f2210db34506a518fddca657
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-neuton.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-neuton.rb
@@ -0,0 +1,10 @@
+class FontNeuton < Formula
+  head "https://github.com/google/fonts/trunk/ofl/neuton", using: :svn, revision: "50", trust_cert: true
+  desc "Neuton"
+  homepage "https://www.google.com/fonts/specimen/Neuton"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
