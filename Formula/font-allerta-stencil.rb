48ec48f3150976f9295f11c180134afa3b39e80d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-allerta-stencil.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-allerta-stencil.rb
@@ -0,0 +1,9 @@
+class FontAllertaStencil < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/allertastencil/AllertaStencil-Regular.ttf"
+  desc "Allerta Stencil"
+  homepage "https://www.google.com/fonts/specimen/Allerta+Stencil"
+  def install
+    
+  test do
+  end
+end
