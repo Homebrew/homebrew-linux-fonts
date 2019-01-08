3a5af6272de5d4cb3ffedf37c2caa848326e274c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-joti-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-joti-one.rb
@@ -0,0 +1,9 @@
+class FontJotiOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/jotione/JotiOne-Regular.ttf"
+  desc "Joti One"
+  homepage "https://www.google.com/fonts/specimen/Joti+One"
+  def install
+    
+  test do
+  end
+end
