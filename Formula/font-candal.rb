6db92db21478cd0d414f2c9f424c7596fb2f1842
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-candal.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-candal.rb
@@ -0,0 +1,9 @@
+class FontCandal < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/candal/Candal.ttf"
+  desc "Candal"
+  homepage "https://www.google.com/fonts/specimen/Candal"
+  def install
+    
+  test do
+  end
+end
