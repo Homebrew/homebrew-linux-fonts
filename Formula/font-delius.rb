b1e27f1fa7005711f1280adf1492a9d0c616ea29
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-delius.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-delius.rb
@@ -0,0 +1,9 @@
+class FontDelius < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/delius/Delius-Regular.ttf"
+  desc "Delius"
+  homepage "https://www.google.com/fonts/specimen/Delius"
+  def install
+    
+  test do
+  end
+end
