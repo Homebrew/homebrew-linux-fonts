f72e246e2aefc9e8a7853fa394d77ee592d311a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-imprima.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-imprima.rb
@@ -0,0 +1,9 @@
+class FontImprima < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/imprima/Imprima-Regular.ttf"
+  desc "Imprima"
+  homepage "https://www.google.com/fonts/specimen/Imprima"
+  def install
+    
+  test do
+  end
+end
