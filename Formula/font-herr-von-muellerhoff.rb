3724bc6bc0f3cdaf9afdfc032bfedb3eea04fe74
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-herr-von-muellerhoff.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-herr-von-muellerhoff.rb
@@ -0,0 +1,9 @@
+class FontHerrVonMuellerhoff < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/herrvonmuellerhoff/HerrVonMuellerhoff-Regular.ttf"
+  desc "Herr Von Muellerhoff"
+  homepage "https://www.google.com/fonts/specimen/Herr+Von+Muellerhoff"
+  def install
+    
+  test do
+  end
+end
