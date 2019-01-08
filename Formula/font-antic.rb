e81a23515d24d61ae6a724c7245e809c377fc63f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-antic.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-antic.rb
@@ -0,0 +1,9 @@
+class FontAntic < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/antic/Antic-Regular.ttf"
+  desc "Antic"
+  homepage "https://www.google.com/fonts/specimen/Antic"
+  def install
+    
+  test do
+  end
+end
