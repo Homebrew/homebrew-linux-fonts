141d09b43f7b05b3b0fc1e3b64760ef3a855f981
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fascinate-inline.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-fascinate-inline.rb
@@ -0,0 +1,9 @@
+class FontFascinateInline < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/fascinateinline/FascinateInline-Regular.ttf"
+  desc "Fascinate Inline"
+  homepage "https://www.google.com/fonts/specimen/Fascinate+Inline"
+  def install
+    
+  test do
+  end
+end
