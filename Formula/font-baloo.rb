153152a8035f0d50555a9404e6b0f73a9dc938aa
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-baloo.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-baloo.rb
+++ Formula/font-baloo.rb
@@ -1,7 +1,7 @@
 class FontBaloo < Formula
   version "1.443"
   sha256 "86db29e7463475fd0ce93376fff7eefb3a787525b8113bca9662b9cdc1c3a139"
-  url "https://github.com/EkType/Baloo/releases/download/#{version}/Baloo_#{version}.zip"
+  head "https://github.com/EkType/Baloo/releases/download/#{version}/Baloo_#{version}.zip"
   desc "Baloo"
   homepage "https://github.com/EkType/Baloo"
   def install
