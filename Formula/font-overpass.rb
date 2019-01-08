d450b6d502c3bdf2010f05118433a0781d9e9dc9
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-overpass.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-overpass.rb
+++ Formula/font-overpass.rb
@@ -1,7 +1,7 @@
 class FontOverpass < Formula
   version "3.0.2"
   sha256 "10d2186ad1e1e628122f2e4ea0bbde16438e34a0068c35190d41626d89bb64e4"
-  head "https://github.com/RedHatBrand/Overpass/releases/download/#{version}/overpass-desktop-fonts.zip"
+  url "https://github.com/RedHatBrand/Overpass/releases/download/#{version}/overpass-desktop-fonts.zip"
   desc "Overpass"
   homepage "https://overpassfont.org/"
   def install
