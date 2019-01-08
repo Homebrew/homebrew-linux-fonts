fbb2c924e3995e9c9f8d3251147cb1c123cd3316
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gravitas-one.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-gravitas-one.rb
@@ -0,0 +1,10 @@
+class FontGravitasOne < Formula
+  head "https://github.com/google/fonts/trunk/ofl/gravitasone", using: :svn, revision: "50", trust_cert: true
+  desc "Gravitas One"
+  homepage "https://www.google.com/fonts/specimen/Gravitas%20One"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
