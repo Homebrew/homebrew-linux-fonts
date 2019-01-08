517dd509d58c5bad7c1ee91741d5818cbe879ff4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-kharoshthi.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-kharoshthi.rb
@@ -0,0 +1,9 @@
+class FontNotoSansKharoshthi < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKharoshthi-unhinted.zip"
+  desc "Noto Sans Kharoshthi"
+  homepage "https://www.google.com/get/noto/#sans-khar"
+  def install
+    
+  test do
+  end
+end
