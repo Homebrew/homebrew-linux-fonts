8f68cf7f9e6ddc9233d503223bf63c7feb485eb6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-londrina-sketch.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-londrina-sketch.rb
@@ -0,0 +1,9 @@
+class FontLondrinaSketch < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/londrinasketch/LondrinaSketch-Regular.ttf"
+  desc "Londrina Sketch"
+  homepage "https://www.google.com/fonts/specimen/Londrina+Sketch"
+  def install
+    
+  test do
+  end
+end
