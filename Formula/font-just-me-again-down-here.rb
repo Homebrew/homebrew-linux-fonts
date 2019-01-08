b23437924645a50c7ceeced0eb4d83fd2fa6616d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-just-me-again-down-here.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-just-me-again-down-here.rb
@@ -0,0 +1,9 @@
+class FontJustMeAgainDownHere < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/justmeagaindownhere/JustMeAgainDownHere.ttf"
+  desc "Just Me Again Down Here"
+  homepage "https://www.google.com/fonts/specimen/Just+Me+Again+Down+Here"
+  def install
+    
+  test do
+  end
+end
