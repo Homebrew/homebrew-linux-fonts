592bfd6456fc7bdcacbe3f0f581ae7eea3ac2044
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-offside.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-offside.rb
@@ -0,0 +1,9 @@
+class FontOffside < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/offside/Offside-Regular.ttf"
+  desc "Offside"
+  homepage "https://www.google.com/fonts/specimen/Offside"
+  def install
+    
+  test do
+  end
+end
