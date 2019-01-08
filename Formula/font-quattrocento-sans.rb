4f9d5927bf6ad9a3fd1d03ad6cab2a023aa82949
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quattrocento-sans.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-quattrocento-sans.rb
@@ -0,0 +1,10 @@
+class FontQuattrocentoSans < Formula
+  head "https://github.com/google/fonts/trunk/ofl/quattrocentosans", using: :svn, revision: "50", trust_cert: true
+  desc "Quattrocento Sans"
+  homepage "https://www.google.com/fonts/specimen/Quattrocento%20Sans"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
