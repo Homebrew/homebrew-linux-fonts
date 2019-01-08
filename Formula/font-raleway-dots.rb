a71d6745dbaa9bbd8abc91099c3b7424b2263241
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-raleway-dots.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-raleway-dots.rb
@@ -0,0 +1,9 @@
+class FontRalewayDots < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/ralewaydots/RalewayDots-Regular.ttf"
+  desc "Raleway Dots"
+  homepage "https://www.google.com/fonts/specimen/Raleway+Dots"
+  def install
+    
+  test do
+  end
+end
