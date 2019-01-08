3c09d9b4ff9774aa3e6b322ab70cdce943692927
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quantico.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-quantico.rb
@@ -0,0 +1,10 @@
+class FontQuantico < Formula
+  head "https://github.com/google/fonts/trunk/ofl/quantico", using: :svn, revision: "50", trust_cert: true
+  desc "Quantico"
+  homepage "https://www.google.com/fonts/specimen/Quantico"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
