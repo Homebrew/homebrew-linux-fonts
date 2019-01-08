d63bf12f21b5617892c1101365bb42cd3f47afcf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-keep-calm.rb from 39df46afee88e73a56a60146b2cd3049973b1444

---
+++ Formula/font-keep-calm.rb
@@ -0,0 +1,9 @@
+class FontKeepCalm < Formula
+  head "https://dl.1001fonts.com/keep-calm.zip"
+  desc "Keep Calm"
+  homepage "https://www.1001fonts.com/keep-calm-font.html"
+  def install
+    
+  test do
+  end
+end
