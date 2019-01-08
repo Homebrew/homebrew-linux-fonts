099063be9b09f66f60592c7da3e41f90075cca2b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-neucha.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-neucha.rb
@@ -0,0 +1,9 @@
+class FontNeucha < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/neucha/Neucha.ttf"
+  desc "Neucha"
+  homepage "https://www.google.com/fonts/specimen/Neucha"
+  def install
+    
+  test do
+  end
+end
