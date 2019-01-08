c3afab7d1ef7dd7b6be838fc8b131c372e809223
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quattrocento.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-quattrocento.rb
@@ -0,0 +1,10 @@
+class FontQuattrocento < Formula
+  head "https://github.com/google/fonts/trunk/ofl/quattrocento", using: :svn, revision: "50", trust_cert: true
+  desc "Quattrocento"
+  homepage "https://www.google.com/fonts/specimen/Quattrocento"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
