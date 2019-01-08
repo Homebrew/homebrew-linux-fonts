10ea685f40360ed8bfeaa8b53aa119082eaadbf4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noticia-text.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-noticia-text.rb
@@ -0,0 +1,10 @@
+class FontNoticiaText < Formula
+  head "https://github.com/google/fonts/trunk/ofl/noticiatext", using: :svn, revision: "50", trust_cert: true
+  desc "Noticia Text"
+  homepage "https://www.google.com/fonts/specimen/Noticia%20Text"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
