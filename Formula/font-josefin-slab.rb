36aef29720fc3f237011a6282674c628661c562c
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:56:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-josefin-slab.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-josefin-slab.rb
@@ -0,0 +1,20 @@
+class FontJosefinSlab < Formula
+  head "https://github.com/google/fonts/trunk/ofl/josefinslab", using: :svn, revision: "50", trust_cert: true
+  desc "Josefin Slab"
+  homepage "https://www.google.com/fonts/specimen/Josefin%20Slab"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "JosefinSlab-Bold.ttf"
+    (share/"fonts").install "JosefinSlab-BoldItalic.ttf"
+    (share/"fonts").install "JosefinSlab-Italic.ttf"
+    (share/"fonts").install "JosefinSlab-Light.ttf"
+    (share/"fonts").install "JosefinSlab-LightItalic.ttf"
+    (share/"fonts").install "JosefinSlab-Regular.ttf"
+    (share/"fonts").install "JosefinSlab-SemiBold.ttf"
+    (share/"fonts").install "JosefinSlab-SemiBoldItalic.ttf"
+    (share/"fonts").install "JosefinSlab-Thin.ttf"
+    (share/"fonts").install "JosefinSlab-ThinItalic.ttf"
+  end
+  test do
+  end
+end
