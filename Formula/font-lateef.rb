d4cb0a1d3b29b46be6ef492ec5f48591b3591425
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lateef.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-lateef.rb
@@ -0,0 +1,9 @@
+class FontLateef < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/lateef/LateefRegOT.ttf"
+  desc "Lateef"
+  homepage "https://www.google.com/fonts/specimen/Lateef"
+  def install
+    
+  test do
+  end
+end
