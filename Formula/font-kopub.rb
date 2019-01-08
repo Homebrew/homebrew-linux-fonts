2389248f56bb89aa8c51b074e9f0cfee983caae6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kopub.rb from cab3c4540da64cb216b772fd6b5620760dfd26ed

---
+++ Formula/font-kopub.rb
@@ -0,0 +1,9 @@
+class FontKopub < Formula
+  head "http://www.kopus.org/Download_font.aspx?font_id=4&name=KOPUB_OTF_FONTS.zip"
+  desc "KoPub"
+  homepage "http://www.kopus.org/Biz/electronic/Font.aspx"
+  def install
+    
+  test do
+  end
+end
