b5dbe03e0703c281f5353eb8afeb2df1d0becc7c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-warnes.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-warnes.rb
@@ -0,0 +1,9 @@
+class FontWarnes < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/warnes/Warnes-Regular.ttf"
+  desc "Warnes"
+  homepage "https://www.google.com/fonts/specimen/Warnes"
+  def install
+    
+  test do
+  end
+end
