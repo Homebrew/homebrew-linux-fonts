2e630642f30b1e8030555e3f950b18f49b0e0c88
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-prince-valiant.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-prince-valiant.rb
@@ -0,0 +1,9 @@
+class FontPrinceValiant < Formula
+  head "https://www.1001freefonts.com/d/3801/prince_valiant.zip"
+  desc "Prince Valiant"
+  homepage "https://www.1001freefonts.com/prince_valiant.font"
+  def install
+    
+  test do
+  end
+end
