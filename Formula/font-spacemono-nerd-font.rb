de5a6badd995db2ab7ecbc3ca837ccd4546f5c4f
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:59:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spacemono-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-spacemono-nerd-font.rb
@@ -0,0 +1,15 @@
+class FontSpacemonoNerdFont < Formula
+  version "2.0.0"
+  sha256 "c41c8b4d9a634a8662aaa70d4541a534e82a13fbcc40c14149a01cda32925648"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/SpaceMono.zip"
+  desc "SpaceMono Nerd Font (SpaceMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    (share/"fonts").install "Space Mono Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Space Mono Bold Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Space Mono Bold Nerd Font Complete.ttf"
+    (share/"fonts").install "Space Mono Nerd Font Complete.ttf"
+  end
+  test do
+  end
+end
