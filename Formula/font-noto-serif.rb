970ebd53b5a2c9f978a5e01be13655f44b1a7805
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif.rb from 7ba99eb0d220c5dcbd47bbd6186bede514f0d71a

---
+++ Formula/font-noto-serif.rb
@@ -0,0 +1,9 @@
+class FontNotoSerif < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerif-unhinted.zip"
+  desc "Noto Serif"
+  homepage "https://www.google.com/get/noto/#serif-lgc"
+  def install
+    
+  test do
+  end
+end
