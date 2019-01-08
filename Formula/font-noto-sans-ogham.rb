4dd11216d47101450d785dd18f3db4a4b0bcde70
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-ogham.rb from 7eff405e323aa4047b9815ba7e3a316e52a51c46

---
+++ Formula/font-noto-sans-ogham.rb
@@ -0,0 +1,9 @@
+class FontNotoSansOgham < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOgham-unhinted.zip"
+  desc "Noto Sans Ogham"
+  homepage "https://www.google.com/get/noto/#sans-ogam"
+  def install
+    
+  test do
+  end
+end
