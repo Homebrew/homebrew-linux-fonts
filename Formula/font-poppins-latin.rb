9afe7e9a2da5c4f7b00ecc1c7e536390417a9321
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-poppins-latin.rb from 9571a7374af76c2fd522d17f964f503e3ae925f5

---
+++ Formula/font-poppins-latin.rb
@@ -0,0 +1,11 @@
+class FontPoppinsLatin < Formula
+  version "5.001"
+  sha256 "cd4bdded93013fc9f123485b8fbc6113f62a7ff77492cc97cba6901cf6493738"
+  url "https://github.com/itfoundry/Poppins/raw/master/products/PoppinsLatin-#{version}-Latin-OTF.zip"
+  desc "PoppinsLatin"
+  homepage "https://github.com/itfoundry/poppins"
+  def install
+    
+  test do
+  end
+end
