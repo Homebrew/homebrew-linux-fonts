20ae91f927bdf60cbf759646321b3a3d6799ce5b
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:58:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-profont-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-profont-nerd-font.rb
@@ -0,0 +1,12 @@
+class FontProfontNerdFont < Formula
+  version "2.0.0"
+  sha256 "a82de4b5d5322bbe21ba3aed4520d7f2f3da72280a4caf005fb3d2787b36d325"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ProFont.zip"
+  desc "ProFontIIx Nerd Font (ProFont)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    (share/"fonts").install "ProFont IIx Nerd Font Complete.ttf"
+  end
+  test do
+  end
+end
