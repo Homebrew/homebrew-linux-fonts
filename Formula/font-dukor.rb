145886021c93272c4d2621825b6a7a07eb1f92c6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dukor.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-dukor.rb
@@ -0,0 +1,11 @@
+class FontDukor < Formula
+  version "1.0.7"
+  sha256 "15933e2f71c4db80f55189ee05957e87af26e517333b33df7b2c4b528a2848f4"
+  url "https://www.evertype.com/fonts/vai/dukorfont.zip"
+  desc "Dukor"
+  homepage "https://www.evertype.com/fonts/vai/"
+  def install
+    
+  test do
+  end
+end
