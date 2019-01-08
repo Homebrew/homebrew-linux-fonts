933da0347dc7984fb931cb2ef780820c0735c508
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cookie.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-cookie.rb
@@ -0,0 +1,9 @@
+class FontCookie < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/cookie/Cookie-Regular.ttf"
+  desc "Cookie"
+  homepage "https://www.google.com/fonts/specimen/Cookie"
+  def install
+    
+  test do
+  end
+end
