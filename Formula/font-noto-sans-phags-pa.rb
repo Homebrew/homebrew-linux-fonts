dd6def163be840677f251a91221156f43b81132b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-phags-pa.rb from 7eff405e323aa4047b9815ba7e3a316e52a51c46

---
+++ Formula/font-noto-sans-phags-pa.rb
@@ -0,0 +1,9 @@
+class FontNotoSansPhagsPa < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansPhagsPa-unhinted.zip"
+  desc "Noto Sans Phags Pa"
+  homepage "https://www.google.com/get/noto/#sans-phag"
+  def install
+    
+  test do
+  end
+end
