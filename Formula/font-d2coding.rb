7b2d5fcf98a2fb200b5cac77526aebdb83c7f970
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-d2coding.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-d2coding.rb
@@ -0,0 +1,11 @@
+class FontD2coding < Formula
+  version "1.3.2,20180524"
+  sha256 "0f1c9192eac7d56329dddc620f9f1666b707e9c8ed38fe1f988d0ae3e30b24e6"
+  url "https://github.com/naver/d2codingfont/releases/download/VER#{version.to_s.sub(/,.*/, "")}/D2Coding-Ver#{version.to_s.sub(/,.*/, "")}-#{version.to_s.sub(/.*,/, "")}.zip"
+  desc "D2 Coding"
+  homepage "https://github.com/naver/d2codingfont"
+  def install
+    
+  test do
+  end
+end
