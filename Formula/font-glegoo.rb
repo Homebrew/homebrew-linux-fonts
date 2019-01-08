b89ca31d4f9b0c16660c9ee79f1c734fc6994a56
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-glegoo.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-glegoo.rb
@@ -0,0 +1,10 @@
+class FontGlegoo < Formula
+  head "https://github.com/google/fonts/trunk/ofl/glegoo", using: :svn, trust_cert: true
+  desc "Glegoo"
+  homepage "https://www.google.com/fonts/specimen/Glegoo"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
