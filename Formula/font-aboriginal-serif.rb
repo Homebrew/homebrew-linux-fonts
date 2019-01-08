9ca45483aadd469a58058da6e9008570dea62cc1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aboriginal-serif.rb from ead100dbdc5928caccf79d6ff068d65a785d38d0

---
+++ Formula/font-aboriginal-serif.rb
@@ -0,0 +1,9 @@
+class FontAboriginalSerif < Formula
+  head "http://www.languagegeek.com/font/AboriginalSerif.zip"
+  desc "Aboriginal Serif"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
