751e140c2a98084df4fd4be9cefb41f388836461
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-belgrano.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-belgrano.rb
@@ -0,0 +1,9 @@
+class FontBelgrano < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/belgrano/Belgrano-Regular.ttf"
+  desc "Belgrano"
+  homepage "https://www.google.com/fonts/specimen/Belgrano"
+  def install
+    
+  test do
+  end
+end
