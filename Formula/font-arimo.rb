99fe3e011e10fdc19ca3e175d47b9fe47239dfd8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arimo.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-arimo.rb
@@ -0,0 +1,10 @@
+class FontArimo < Formula
+  head "https://github.com/google/fonts/trunk/apache/arimo", using: :svn, revision: "50", trust_cert: true
+  desc "Arimo"
+  homepage "https://www.google.com/fonts/specimen/Arimo"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
