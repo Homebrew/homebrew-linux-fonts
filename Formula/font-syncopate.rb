1367b39fed1e6345eb64e5cb6720e55e4ed7c955
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-syncopate.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-syncopate.rb
@@ -0,0 +1,10 @@
+class FontSyncopate < Formula
+  head "https://github.com/google/fonts/trunk/apache/syncopate", using: :svn, revision: "50", trust_cert: true
+  desc "Syncopate"
+  homepage "https://www.google.com/fonts/specimen/Syncopate"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
