d242aaa14837b7597386630bdfe814e9546eb056
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:54 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-georgia.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-georgia.rb
+++ Formula/font-georgia.rb
@@ -1,7 +1,7 @@
 class FontGeorgia < Formula
   version "2.05"
   sha256 "2c2c7dcda6606ea5cf08918fb7cd3f3359e9e84338dc690013f20cd42e930301"
-  head "https://downloads.sourceforge.net/corefonts/georgi32.exe"
+  url "https://downloads.sourceforge.net/corefonts/georgi32.exe"
   desc "Georgia"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
   def install
