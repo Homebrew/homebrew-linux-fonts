ed59cf7ab2b6cac31b98659e6352b971615c6184
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gilbert.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-gilbert.rb
@@ -0,0 +1,11 @@
+class FontGilbert < Formula
+  version "1.004,alpha"
+  sha256 "0cea7456bdc704c9b68c023a6bb40590e327da3d84f8ff5511c6460a712987a9"
+  url "https://github.com/Fontself/TypeWithPride/releases/download/#{version.to_s.sub(/,.*/, "")}/Gilbert_#{version.to_s.sub(/,.*/, "")}_#{version.to_s.sub(/.*,/, "")}.zip"
+  desc "Gilbert"
+  homepage "https://typewithpride.com/"
+  def install
+    
+  test do
+  end
+end
