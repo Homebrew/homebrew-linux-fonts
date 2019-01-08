3473a24c8e9e8d85589ef09843f90a0b55902853
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ia-writer-duospace.rb from da262c5b7b603a3d1bb81095fa04381951605e18

---
+++ Formula/font-ia-writer-duospace.rb
@@ -0,0 +1,9 @@
+class FontIaWriterDuospace < Formula
+  head "https://codeload.github.com/iaolo/iA-Fonts/zip/master"
+  desc "iA Writer Duospace"
+  homepage "https://github.com/iaolo/iA-Fonts"
+  def install
+    
+  test do
+  end
+end
