56e37d6ac55f0cfd20f053f1091c544ce0c673f2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-eater.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-eater.rb
@@ -0,0 +1,9 @@
+class FontEater < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/eater/Eater-Regular.ttf"
+  desc "Eater"
+  homepage "https://www.google.com/fonts/specimen/Eater"
+  def install
+    
+  test do
+  end
+end
