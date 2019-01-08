88b6fb2ea47fd46ef37c65836d56e7c5790009c0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mouse-memoirs.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-mouse-memoirs.rb
@@ -0,0 +1,9 @@
+class FontMouseMemoirs < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/mousememoirs/MouseMemoirs-Regular.ttf"
+  desc "Mouse Memoirs"
+  homepage "https://www.google.com/fonts/specimen/Mouse+Memoirs"
+  def install
+    
+  test do
+  end
+end
