e7f0dda6e4e078f4e20ade538e7aa5a8096aa344
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-euphoria-script.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-euphoria-script.rb
@@ -0,0 +1,9 @@
+class FontEuphoriaScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/euphoriascript/EuphoriaScript-Regular.ttf"
+  desc "Euphoria Script"
+  homepage "https://www.google.com/fonts/specimen/Euphoria+Script"
+  def install
+    
+  test do
+  end
+end
