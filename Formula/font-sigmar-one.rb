a36b96e5aba744f41c619b70d759ca896d82cdea
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sigmar-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sigmar-one.rb
@@ -0,0 +1,9 @@
+class FontSigmarOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sigmarone/SigmarOne-Regular.ttf"
+  desc "Sigmar One"
+  homepage "https://www.google.com/fonts/specimen/Sigmar+One"
+  def install
+    
+  test do
+  end
+end
