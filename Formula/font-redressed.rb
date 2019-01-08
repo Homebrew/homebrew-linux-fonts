7dfa8f618e1e4422b5b8981105cb716df3faefb1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-redressed.rb from 64b2df26f8a8d13d2d4efd36ea52c62dfd325034

---
+++ Formula/font-redressed.rb
@@ -0,0 +1,9 @@
+class FontRedressed < Formula
+  head "https://github.com/google/fonts/raw/master/apache/redressed/Redressed-Regular.ttf"
+  desc "Redressed"
+  homepage "https://www.google.com/fonts/specimen/Redressed"
+  def install
+    
+  test do
+  end
+end
