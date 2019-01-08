ddba9ff7cd15a4872b9cf768ddf3f13e71679bb2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bokor.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bokor.rb
@@ -0,0 +1,9 @@
+class FontBokor < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/bokor/Bokor-Regular.ttf"
+  desc "Bokor"
+  homepage "https://www.google.com/fonts/specimen/Bokor"
+  def install
+    
+  test do
+  end
+end
