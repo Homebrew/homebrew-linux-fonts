ab4dfe8030624b2637009dd293aa17553aa3013a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bubblegum-sans.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bubblegum-sans.rb
@@ -0,0 +1,9 @@
+class FontBubblegumSans < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/bubblegumsans/BubblegumSans-Regular.ttf"
+  desc "Bubblegum Sans"
+  homepage "https://www.google.com/fonts/specimen/Bubblegum+Sans"
+  def install
+    
+  test do
+  end
+end
