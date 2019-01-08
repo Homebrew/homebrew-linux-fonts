335dc2d9afd64d61e470f1b3c2a7b71411dbc27e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-piedra.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-piedra.rb
@@ -0,0 +1,9 @@
+class FontPiedra < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/piedra/Piedra-Regular.ttf"
+  desc "Piedra"
+  homepage "https://www.google.com/fonts/specimen/Piedra"
+  def install
+    
+  test do
+  end
+end
