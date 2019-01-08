fced9d6fdfa34459e8e416734e610e522e71a228
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-codystar.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-codystar.rb
@@ -0,0 +1,10 @@
+class FontCodystar < Formula
+  head "https://github.com/google/fonts/trunk/ofl/codystar", using: :svn, revision: "50", trust_cert: true
+  desc "Codystar"
+  homepage "https://www.google.com/fonts/specimen/Codystar"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
