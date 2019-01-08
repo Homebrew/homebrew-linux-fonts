9aa924c3b3cc390e30f8d4aec526550a83b067b6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aladin.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-aladin.rb
@@ -0,0 +1,9 @@
+class FontAladin < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/aladin/Aladin-Regular.ttf"
+  desc "Aladin"
+  homepage "https://www.google.com/fonts/specimen/Aladin"
+  def install
+    
+  test do
+  end
+end
