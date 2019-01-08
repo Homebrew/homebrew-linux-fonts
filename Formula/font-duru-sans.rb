cddb3c64af921159b55be2703d1c47f75abf85ae
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-duru-sans.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-duru-sans.rb
@@ -0,0 +1,9 @@
+class FontDuruSans < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/durusans/DuruSans-Regular.ttf"
+  desc "Duru Sans"
+  homepage "https://www.google.com/fonts/specimen/Duru+Sans"
+  def install
+    
+  test do
+  end
+end
