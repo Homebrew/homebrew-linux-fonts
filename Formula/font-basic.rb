1d68749fd4b9fe6f5a9345348381fc23eda9401a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-basic.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-basic.rb
@@ -0,0 +1,9 @@
+class FontBasic < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/basic/Basic-Regular.ttf"
+  desc "Basic"
+  homepage "https://www.google.com/fonts/specimen/Basic"
+  def install
+    
+  test do
+  end
+end
