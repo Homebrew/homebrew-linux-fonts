926dca51cb99bb7873ddfe4a49560718f2f57ced
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:59:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sintony.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-sintony.rb
@@ -0,0 +1,12 @@
+class FontSintony < Formula
+  head "https://github.com/google/fonts/trunk/ofl/sintony", using: :svn, revision: "50", trust_cert: true
+  desc "Sintony"
+  homepage "https://www.google.com/fonts/specimen/Sintony"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "Sintony-Bold.ttf"
+    (share/"fonts").install "Sintony-Regular.ttf"
+  end
+  test do
+  end
+end
