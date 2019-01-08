ee29c343ac09b9f5464226c505a2656d1bc7596b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-vibur.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-vibur.rb
@@ -0,0 +1,9 @@
+class FontVibur < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/vibur/Vibur-Regular.ttf"
+  desc "Vibur"
+  homepage "https://www.google.com/fonts/specimen/Vibur"
+  def install
+    
+  test do
+  end
+end
