17ef3855efb0d7c0399eba627274b2a29dfea939
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rosario.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-rosario.rb
@@ -0,0 +1,10 @@
+class FontRosario < Formula
+  head "https://github.com/google/fonts/trunk/ofl/rosario", using: :svn, revision: "50", trust_cert: true
+  desc "Rosario"
+  homepage "https://www.google.com/fonts/specimen/Rosario"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
