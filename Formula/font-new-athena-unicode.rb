7862b02badc8323f6ab67f34065e77efe72bc57f
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-new-athena-unicode.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-new-athena-unicode.rb
+++ Formula/font-new-athena-unicode.rb
@@ -1,7 +1,7 @@
 class FontNewAthenaUnicode < Formula
   version "405"
   sha256 "ef7cdccec31ea61391333762dd5542994189585f0f5384dcb1194701a2a9a3cf"
-  url "https://apagreekkeys.org/fonts/NAU#{version}styles.zip"
+  head "https://apagreekkeys.org/fonts/NAU#{version}styles.zip"
   desc "New Athena Unicode"
   homepage "https://apagreekkeys.org/NAUdownload.html"
   def install
