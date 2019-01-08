6338603c8ceaeacf805c3fae517ef16bb6c9e0d4
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-comic-neue.rb from 1cc0e7baa704ecf92a292089804f29ebc30dd257

--- Formula/font-comic-neue.rb
+++ Formula/font-comic-neue.rb
@@ -1,7 +1,7 @@
 class FontComicNeue < Formula
   version "2.2"
   sha256 "d67c7de093b3555a525a29db1999b7759723208f514f5fca117201d63b94b5b6"
-  head "http://comicneue.com/comic-neue-#{version}.zip"
+  url "http://comicneue.com/comic-neue-#{version}.zip"
   desc "Comic Neue"
   homepage "http://comicneue.com/"
   def install
