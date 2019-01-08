52c81d6f33ff8ea173b78b19a39226b843640722
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ledger.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-ledger.rb
@@ -0,0 +1,9 @@
+class FontLedger < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/ledger/Ledger-Regular.ttf"
+  desc "Ledger"
+  homepage "https://www.google.com/fonts/specimen/Ledger"
+  def install
+    
+  test do
+  end
+end
