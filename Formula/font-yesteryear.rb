7ae159d940fa0af9d74901212b5974ce2542e6b9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-yesteryear.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-yesteryear.rb
@@ -0,0 +1,9 @@
+class FontYesteryear < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/yesteryear/Yesteryear-Regular.ttf"
+  desc "Yesteryear"
+  homepage "https://www.google.com/fonts/specimen/Yesteryear"
+  def install
+    
+  test do
+  end
+end
