da72172ec59f362b0b7fc9f2794e0aa81c74e0b8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-deseret.rb from 91ec7fe0323c8467fb529c8559b961db2070ea2c

---
+++ Formula/font-noto-sans-deseret.rb
@@ -0,0 +1,9 @@
+class FontNotoSansDeseret < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansDeseret-unhinted.zip"
+  desc "Noto Sans Deseret"
+  homepage "https://www.google.com/get/noto/#sans-dsrt"
+  def install
+    
+  test do
+  end
+end
