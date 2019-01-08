aa67d565d4897f8c0d71af34611733e6b18b2cd2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pt-serif.rb from 0be6c290b298ae6beddecdab00c586fad0628800

---
+++ Formula/font-pt-serif.rb
@@ -0,0 +1,9 @@
+class FontPtSerif < Formula
+  head "https://company.paratype.com/system/attachments/634/original/ptserif.zip"
+  desc "PT Serif"
+  homepage "https://www.paratype.com/public/"
+  def install
+    
+  test do
+  end
+end
