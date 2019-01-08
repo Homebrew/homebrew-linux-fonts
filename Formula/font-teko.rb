e68ccea5d502d0d4f0318dda9ec8a67542928cd6
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:23 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-teko.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-teko.rb
+++ Formula/font-teko.rb
@@ -1,7 +1,7 @@
 class FontTeko < Formula
   version "2.000"
   sha256 "189cfd7d5f75ef206da039795574a0e66cf2daa3d1b31405c0fda7d70e6ac802"
-  url "https://github.com/itfoundry/teko/releases/download/v#{version}/teko-#{version.to_s.gsub(".", "_")}.zip"
+  head "https://github.com/itfoundry/teko/releases/download/v#{version}/teko-#{version.dots_to_underscores}.zip"
   desc "Teko"
   homepage "https://github.com/itfoundry/teko"
   def install
