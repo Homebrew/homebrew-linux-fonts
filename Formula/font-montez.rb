7dfa8f696c98e57e476253d0e847917bccb2b543
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-montez.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-montez.rb
@@ -0,0 +1,9 @@
+class FontMontez < Formula
+  head "https://github.com/google/fonts/raw/master/apache/montez/Montez-Regular.ttf"
+  desc "Montez"
+  homepage "https://www.google.com/fonts/specimen/Montez"
+  def install
+    
+  test do
+  end
+end
