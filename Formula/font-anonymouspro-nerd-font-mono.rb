7474d55043d565b74f97662c547aa3a9a31ba21e
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-anonymouspro-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-anonymouspro-nerd-font-mono.rb
+++ Formula/font-anonymouspro-nerd-font-mono.rb
@@ -1,7 +1,7 @@
 class FontAnonymousproNerdFontMono < Formula
   version "2.0.0"
   sha256 "a5ff0e9d8564594fafc0f3698ecaf08b38f87d140550736038fab0bc2b57b018"
-  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AnonymousPro.zip"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AnonymousPro.zip"
   desc "AnonymicePowerline Nerd Font (AnonymousPro)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
