12f0c35203baf18ea8803c427d88a3d8a7ebee1e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hammersmith-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-hammersmith-one.rb
@@ -0,0 +1,9 @@
+class FontHammersmithOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/hammersmithone/HammersmithOne-Regular.ttf"
+  desc "Hammersmith One"
+  homepage "https://www.google.com/fonts/specimen/Hammersmith+One"
+  def install
+    
+  test do
+  end
+end
