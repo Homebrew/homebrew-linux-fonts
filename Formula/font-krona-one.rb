881d061efcfa152c4a3665d53fcf7323fef83d58
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:28 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-krona-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-krona-one.rb
@@ -0,0 +1,9 @@
+class FontKronaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kronaone/KronaOne-Regular.ttf"
+  desc "Krona One"
+  homepage "https://www.google.com/fonts/specimen/Krona+One"
+  def install
+    
+  test do
+  end
+end
