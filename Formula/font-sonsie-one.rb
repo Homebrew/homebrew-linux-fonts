e381fea55d69933255f7278e96240f8e195b2e23
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sonsie-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sonsie-one.rb
@@ -0,0 +1,9 @@
+class FontSonsieOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sonsieone/SonsieOne-Regular.ttf"
+  desc "Sonsie One"
+  homepage "https://www.google.com/fonts/specimen/Sonsie+One"
+  def install
+    
+  test do
+  end
+end
