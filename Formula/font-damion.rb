a826e5dbcc09d93396b969ec69621953e3f2456d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-damion.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-damion.rb
@@ -0,0 +1,9 @@
+class FontDamion < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/damion/Damion-Regular.ttf"
+  desc "Damion"
+  homepage "https://www.google.com/fonts/specimen/Damion"
+  def install
+    
+  test do
+  end
+end
