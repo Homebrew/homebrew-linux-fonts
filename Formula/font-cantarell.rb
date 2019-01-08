577655439f976cfbd2eda06ecd4cd86557f207cc
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:53:56 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cantarell.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-cantarell.rb
@@ -0,0 +1,14 @@
+class FontCantarell < Formula
+  head "https://github.com/google/fonts/trunk/ofl/cantarell", using: :svn, revision: "50", trust_cert: true
+  desc "Cantarell"
+  homepage "https://www.google.com/fonts/specimen/Cantarell"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "Cantarell-Bold.ttf"
+    (share/"fonts").install "Cantarell-BoldOblique.ttf"
+    (share/"fonts").install "Cantarell-Oblique.ttf"
+    (share/"fonts").install "Cantarell-Regular.ttf"
+  end
+  test do
+  end
+end
