c2e6bbd06ccc43495a4112b52c5be571580f1e19
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-faster-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-faster-one.rb
@@ -0,0 +1,9 @@
+class FontFasterOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/fasterone/FasterOne-Regular.ttf"
+  desc "Faster One"
+  homepage "https://www.google.com/fonts/specimen/Faster+One"
+  def install
+    
+  test do
+  end
+end
