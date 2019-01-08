24d60258c400c0d79e267d8dbad48857cf664160
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-smokum.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-smokum.rb
@@ -0,0 +1,9 @@
+class FontSmokum < Formula
+  head "https://github.com/google/fonts/raw/master/apache/smokum/Smokum-Regular.ttf"
+  desc "Smokum"
+  homepage "https://www.google.com/fonts/specimen/Smokum"
+  def install
+    
+  test do
+  end
+end
