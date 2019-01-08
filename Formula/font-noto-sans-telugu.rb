44dfe79152163aeb69934e634c31e3c4a104554f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-telugu.rb from 429fe80b45ad0fcc4027e2cc38e6908c61e6d571

---
+++ Formula/font-noto-sans-telugu.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTelugu < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTelugu-unhinted.zip"
+  desc "Noto Sans Telugu"
+  homepage "https://www.google.com/get/noto/#sans-telu"
+  def install
+    
+  test do
+  end
+end
