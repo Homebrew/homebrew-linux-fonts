ecec2663cec4bc47c7bb0f90f6d2c645f8a8ed8d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aguafina-script.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-aguafina-script.rb
@@ -0,0 +1,9 @@
+class FontAguafinaScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/aguafinascript/AguafinaScript-Regular.ttf"
+  desc "Aguafina Script"
+  homepage "https://www.google.com/fonts/specimen/Aguafina+Script"
+  def install
+    
+  test do
+  end
+end
