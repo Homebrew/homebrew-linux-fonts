951119b685365bc18f4d0dead1fb893bdaa6857e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-amaranth.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-amaranth.rb
@@ -0,0 +1,10 @@
+class FontAmaranth < Formula
+  head "https://github.com/google/fonts/trunk/ofl/amaranth", using: :svn, revision: "50", trust_cert: true
+  desc "Amaranth"
+  homepage "https://www.google.com/fonts/specimen/Amaranth"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
