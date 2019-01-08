aa6c142b35dc058ce5908c0ed24fc12a7da80422
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:54 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hermit.rb from 192b61037fc0f36861b3983c79e5f8c50c5fbc9c

--- Formula/font-hermit.rb
+++ Formula/font-hermit.rb
@@ -1,7 +1,7 @@
 class FontHermit < Formula
   version "1.21"
   sha256 "2966e83f012e6a31a861e3afdcb89d157f12b307f2367303d58be364d4308b85"
-  head "https://pcaro.es/d/otf-hermit-#{version}.tar.gz"
+  url "https://pcaro.es/d/otf-hermit-#{version}.tar.gz"
   desc "Hermit"
   homepage "https://pcaro.es/p/hermit/"
   def install
