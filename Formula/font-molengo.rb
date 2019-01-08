b0ca6330ddfdd6c1bccd89a849cf7be168d7794c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-molengo.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-molengo.rb
@@ -0,0 +1,9 @@
+class FontMolengo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/molengo/Molengo-Regular.ttf"
+  desc "Molengo"
+  homepage "https://www.google.com/fonts/specimen/Molengo"
+  def install
+    
+  test do
+  end
+end
