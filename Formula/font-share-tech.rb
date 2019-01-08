a277d9dacceb9dfb55b072053e10ecc2a34af645
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:28 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-share-tech.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-share-tech.rb
@@ -0,0 +1,9 @@
+class FontShareTech < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sharetech/ShareTech-Regular.ttf"
+  desc "Share Tech"
+  homepage "https://www.google.com/fonts/specimen/Share%20Tech"
+  def install
+    
+  test do
+  end
+end
