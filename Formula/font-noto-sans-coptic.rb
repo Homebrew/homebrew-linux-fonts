b96a1149aa4da02210416314a9211eb6a75d1d3f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-coptic.rb from 91ec7fe0323c8467fb529c8559b961db2070ea2c

---
+++ Formula/font-noto-sans-coptic.rb
@@ -0,0 +1,9 @@
+class FontNotoSansCoptic < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCoptic-unhinted.zip"
+  desc "Noto Sans Coptic"
+  homepage "https://www.google.com/get/noto/#sans-copt"
+  def install
+    
+  test do
+  end
+end
