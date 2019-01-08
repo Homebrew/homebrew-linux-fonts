dff5af28b96169ad36ed266ea7d415a1a64e06e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sarpanch.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-sarpanch.rb
@@ -0,0 +1,11 @@
+class FontSarpanch < Formula
+  version "2.000"
+  sha256 "5da058f7a7686ea5ad9d2811b2063343a580bf4da3d415a947d2c37d6c5bcf07"
+  url "https://github.com/itfoundry/sarpanch/releases/download/v#{version}/sarpanch-#{version.to_s.gsub(".", "_")}.zip"
+  desc "Sarpanch"
+  homepage "https://github.com/itfoundry/sarpanch"
+  def install
+    
+  test do
+  end
+end
