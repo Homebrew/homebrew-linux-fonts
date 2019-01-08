2b1cc6395e0dd7434172a5ecfce5e03628fa8b5d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-snippet.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-snippet.rb
@@ -0,0 +1,9 @@
+class FontSnippet < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/snippet/Snippet.ttf"
+  desc "Snippet"
+  homepage "https://www.google.com/fonts/specimen/Snippet"
+  def install
+    
+  test do
+  end
+end
