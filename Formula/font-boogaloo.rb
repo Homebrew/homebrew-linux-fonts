ea2a674dbb3bae46329f776e8120ccb557d8505d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-boogaloo.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-boogaloo.rb
@@ -0,0 +1,9 @@
+class FontBoogaloo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/boogaloo/Boogaloo-Regular.ttf"
+  desc "Boogaloo"
+  homepage "https://www.google.com/fonts/specimen/Boogaloo"
+  def install
+    
+  test do
+  end
+end
