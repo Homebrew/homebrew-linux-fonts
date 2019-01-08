51b03af5ce2f8daebb332fd4a5cf58431e288006
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mplus-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-mplus-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontMplusNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "dc3aa81b0274d5feb36e86c68c79e25c44cbafee1b1a10dcc01acedb45a0403e"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/MPlus.zip"
+  desc "mplus Nerd Font (MPlus)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
