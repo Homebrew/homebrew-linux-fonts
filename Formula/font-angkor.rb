a41c640c3a0684dbbacb6cddccb1b8b4bb817bcf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-angkor.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-angkor.rb
@@ -0,0 +1,9 @@
+class FontAngkor < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/angkor/Angkor-Regular.ttf"
+  desc "Angkor"
+  homepage "https://www.google.com/fonts/specimen/Angkor"
+  def install
+    
+  test do
+  end
+end
