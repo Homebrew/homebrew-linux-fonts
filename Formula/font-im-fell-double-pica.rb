b7670031296fa34cb12b14c319a83b008f7f6940
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-double-pica.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-im-fell-double-pica.rb
@@ -0,0 +1,10 @@
+class FontImFellDoublePica < Formula
+  head "https://github.com/google/fonts/trunk/ofl/imfelldoublepica", using: :svn, revision: "50", trust_cert: true
+  desc "IM Fell Double Pica"
+  homepage "https://www.google.com/fonts/specimen/IM%20Fell%20Double%20Pica"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
