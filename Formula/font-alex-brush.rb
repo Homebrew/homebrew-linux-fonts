f6762753213c0a9260dff4ed5127c282d16b49e6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alex-brush.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-alex-brush.rb
@@ -0,0 +1,9 @@
+class FontAlexBrush < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/alexbrush/AlexBrush-Regular.ttf"
+  desc "Alex Brush"
+  homepage "https://www.google.com/fonts/specimen/Alex+Brush"
+  def install
+    
+  test do
+  end
+end
