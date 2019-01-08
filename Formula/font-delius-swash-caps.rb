f7f0ffb1b7924d37ccf9d377ef18e537a6edcb18
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-delius-swash-caps.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-delius-swash-caps.rb
@@ -0,0 +1,9 @@
+class FontDeliusSwashCaps < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/deliusswashcaps/DeliusSwashCaps-Regular.ttf"
+  desc "Delius Swash Caps"
+  homepage "https://www.google.com/fonts/specimen/Delius+Swash+Caps"
+  def install
+    
+  test do
+  end
+end
