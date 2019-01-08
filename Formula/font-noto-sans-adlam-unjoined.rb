a07b814c1d970049c4071fe2542998b2c7700785
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-adlam-unjoined.rb from a877d71f000c4cd26ee9ddccfec7d06085e2aa0a

---
+++ Formula/font-noto-sans-adlam-unjoined.rb
@@ -0,0 +1,9 @@
+class FontNotoSansAdlamUnjoined < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAdlamUnjoined-unhinted.zip"
+  desc "Noto Sans Adlam Unjoined"
+  homepage "https://www.google.com/get/noto/#sans-adlm-unjoined"
+  def install
+    
+  test do
+  end
+end
