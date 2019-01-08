243d69274b7f17acb4ce06735d6f8b80611545d8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-code-pro.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-source-code-pro.rb
@@ -0,0 +1,11 @@
+class FontSourceCodePro < Formula
+  version "2.030R-ro-1.050R-it"
+  sha256 "da2ac159497d31b0c6d9daa8fc390fb8252e75b4a9805ace6a2c9cccaed4932e"
+  url "https://github.com/adobe-fonts/source-code-pro/archive/#{version.to_s.sub('ro-', 'ro/')}.zip"
+  desc "Source Code Pro"
+  homepage "https://adobe-fonts.github.io/source-code-pro/"
+  def install
+    
+  test do
+  end
+end
