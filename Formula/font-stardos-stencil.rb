048e7678a57e04109c4876cb2e987f1727e00cf8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stardos-stencil.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-stardos-stencil.rb
@@ -0,0 +1,10 @@
+class FontStardosStencil < Formula
+  head "https://github.com/google/fonts/trunk/ofl/stardosstencil", using: :svn, revision: "50", trust_cert: true
+  desc "Stardos Stencil"
+  homepage "https://www.google.com/fonts/specimen/Stardos%20Stencil"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
