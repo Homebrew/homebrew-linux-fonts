3c3a78f600eb9e9699267df8c9b81cbe7717c8ab
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-calligraffitti.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-calligraffitti.rb
@@ -0,0 +1,9 @@
+class FontCalligraffitti < Formula
+  head "https://github.com/google/fonts/raw/master/apache/calligraffitti/Calligraffitti-Regular.ttf"
+  desc "Calligraffitti"
+  homepage "https://www.google.com/fonts/specimen/Calligraffitti"
+  def install
+    
+  test do
+  end
+end
