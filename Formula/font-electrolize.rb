b7fc36a3e31a81509eda9877529279dbaa795f7e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-electrolize.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-electrolize.rb
@@ -0,0 +1,9 @@
+class FontElectrolize < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/electrolize/Electrolize-Regular.ttf"
+  desc "Electrolize"
+  homepage "https://www.google.com/fonts/specimen/Electrolize"
+  def install
+    
+  test do
+  end
+end
