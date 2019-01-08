54f79462cd8bccaf36d09f5a56d9694b263ae66a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-henny-penny.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-henny-penny.rb
@@ -0,0 +1,9 @@
+class FontHennyPenny < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/hennypenny/HennyPenny-Regular.ttf"
+  desc "Henny Penny"
+  homepage "https://www.google.com/fonts/specimen/Henny+Penny"
+  def install
+    
+  test do
+  end
+end
