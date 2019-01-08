f3319e32dfef8f2e6db02f475e0b22b42021f371
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-blockzone.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-blockzone.rb
@@ -0,0 +1,11 @@
+class FontBlockzone < Formula
+  version "1.004"
+  sha256 "2e815598363619f36726a9535963766529091ba11021c06925ecf018f603d0c9"
+  url "https://github.com/ansilove/BlockZone/archive/#{version}.zip"
+  desc "BlockZone"
+  homepage "https://github.com/ansilove/BlockZone"
+  def install
+    
+  test do
+  end
+end
