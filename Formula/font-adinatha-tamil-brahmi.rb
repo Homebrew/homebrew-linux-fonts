841e17af1962909ee3491536e0ae089d225671c7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-adinatha-tamil-brahmi.rb from ad5b894f7c4be5d2faaefeaa50632bc2213c1a52

---
+++ Formula/font-adinatha-tamil-brahmi.rb
@@ -0,0 +1,9 @@
+class FontAdinathaTamilBrahmi < Formula
+  head "http://www.virtualvinodh.com/download/Adinatha-Tamil-Brahmi.zip"
+  desc "Adinatha Tamil Brahmi"
+  homepage "http://www.virtualvinodh.com/wp/tamil-brahmi-font/"
+  def install
+    
+  test do
+  end
+end
