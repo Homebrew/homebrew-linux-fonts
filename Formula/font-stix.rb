7c06713c5fc580dfb44bb90ba73e888d357489af
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stix.rb from 47fa7a19836a8f1aee4808b69600265a6a2abbda

---
+++ Formula/font-stix.rb
@@ -0,0 +1,11 @@
+class FontStix < Formula
+  version "2.0.0"
+  sha256 "667e3187a22cc63ba0de7083fc69c7920737cd44a44ff5eb2921cbd188531aa9"
+  url "https://github.com/stipub/stixfonts/archive/#{version}.tar.gz"
+  desc "STIX"
+  homepage "https://stixfonts.org/"
+  def install
+    
+  test do
+  end
+end
