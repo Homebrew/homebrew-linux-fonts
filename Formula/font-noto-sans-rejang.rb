64237e2bf4e304d5249e0a1191d46a0a3f8430af
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-rejang.rb from 7eff405e323aa4047b9815ba7e3a316e52a51c46

---
+++ Formula/font-noto-sans-rejang.rb
@@ -0,0 +1,9 @@
+class FontNotoSansRejang < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRejang-unhinted.zip"
+  desc "Noto Sans Rejang"
+  homepage "https://www.google.com/get/noto/#sans-rjng"
+  def install
+    
+  test do
+  end
+end
