c8bb6e9d3a0fb65a44cc1ed0890641448489e8fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-allura.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-allura.rb
@@ -0,0 +1,9 @@
+class FontAllura < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/allura/Allura-Regular.ttf"
+  desc "Allura"
+  homepage "https://www.google.com/fonts/specimen/Allura"
+  def install
+    
+  test do
+  end
+end
