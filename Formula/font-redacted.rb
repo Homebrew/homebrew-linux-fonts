ab5d15800c7635573e339838801f4779efaf85e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-redacted.rb from ad5b894f7c4be5d2faaefeaa50632bc2213c1a52

---
+++ Formula/font-redacted.rb
@@ -0,0 +1,9 @@
+class FontRedacted < Formula
+  head "https://github.com/christiannaths/Redacted-Font/archive/old-sources.zip"
+  desc "Redacted"
+  homepage "https://github.com/christiannaths/Redacted-Font"
+  def install
+    
+  test do
+  end
+end
