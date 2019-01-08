82331ffdb2769f93b1ab268d93d044ae3f2bb2d8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-butcherman.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-butcherman.rb
@@ -0,0 +1,9 @@
+class FontButcherman < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/butcherman/Butcherman-Regular.ttf"
+  desc "Butcherman"
+  homepage "https://www.google.com/fonts/specimen/Butcherman"
+  def install
+    
+  test do
+  end
+end
