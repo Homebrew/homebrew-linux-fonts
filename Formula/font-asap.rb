d14cea7ee9625601b1eb3dd390d237ee308b201a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-asap.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-asap.rb
@@ -0,0 +1,10 @@
+class FontAsap < Formula
+  head "https://github.com/google/fonts/trunk/ofl/asap", using: :svn, revision: "50", trust_cert: true
+  desc "Asap"
+  homepage "https://www.google.com/fonts/specimen/Asap"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
