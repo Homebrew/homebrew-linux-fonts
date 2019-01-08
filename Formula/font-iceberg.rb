6b8d591b989ba70af484ede816f1af7af9f9f001
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iceberg.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-iceberg.rb
@@ -0,0 +1,9 @@
+class FontIceberg < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/iceberg/Iceberg-Regular.ttf"
+  desc "Iceberg"
+  homepage "https://www.google.com/fonts/specimen/Iceberg"
+  def install
+    
+  test do
+  end
+end
