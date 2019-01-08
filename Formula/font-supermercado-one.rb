edc3e916e785f9d070c9614b9ec9cc8fa585448d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-supermercado-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-supermercado-one.rb
@@ -0,0 +1,9 @@
+class FontSupermercadoOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/supermercadoone/SupermercadoOne-Regular.ttf"
+  desc "Supermercado One"
+  homepage "https://www.google.com/fonts/specimen/Supermercado+One"
+  def install
+    
+  test do
+  end
+end
