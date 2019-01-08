14f4b5280cf1e9eff10af8ce78d9fc538e0d2710
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-schola.rb from 053c7ba28ff020559d0d84e709b83e78afea1809

---
+++ Formula/font-tex-gyre-schola.rb
@@ -0,0 +1,11 @@
+class FontTexGyreSchola < Formula
+  version "2.005"
+  sha256 "24063368bfdc1046439e290299157621a437294ecbb39a938d2ddb2afa3daaf8"
+  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/schola/qcs#{version}otf.zip"
+  desc "TeX Gyre Schola"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/schola"
+  def install
+    
+  test do
+  end
+end
