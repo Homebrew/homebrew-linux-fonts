f6f1e10bdaa14fa9ac72c2946073055a1e2fce0f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-serif-pro.rb from edc81c4bb4852c4bacf86c0480663bb47c20a2af

---
+++ Formula/font-source-serif-pro.rb
@@ -0,0 +1,11 @@
+class FontSourceSerifPro < Formula
+  version "2.007R-ro%2F1.007R-it"
+  sha256 "46d9b5114e3e86b24769729e2fe09288b6a94c2d4f28a7e39ef572fbe2bec2da"
+  url "https://github.com/adobe-fonts/source-serif-pro/archive/#{version}.zip"
+  desc "Source Serif Pro"
+  homepage "https://adobe-fonts.github.io/source-serif-pro/"
+  def install
+    
+  test do
+  end
+end
