0ad8d37c7d5f4f8f8409f2f23ebd70d87c4cd01d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dashicons.rb from d774dc13402cb23d4713da1b263bf1dbee568385

---
+++ Formula/font-dashicons.rb
@@ -0,0 +1,9 @@
+class FontDashicons < Formula
+  head "https://github.com/WordPress/dashicons/raw/master/icon-font/fonts/dashicons.ttf"
+  desc "Dashicons"
+  homepage "https://developer.wordpress.org/resource/dashicons"
+  def install
+    
+  test do
+  end
+end
