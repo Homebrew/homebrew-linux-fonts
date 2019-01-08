2d89e7d156cbcda6195284f9468958ec12557117
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-carme.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-carme.rb
@@ -0,0 +1,9 @@
+class FontCarme < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/carme/Carme-Regular.ttf"
+  desc "Carme"
+  homepage "https://www.google.com/fonts/specimen/Carme"
+  def install
+    
+  test do
+  end
+end
