7ce4135821af2d5e9c988f9f480c47fed8f20419
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cabin-sketch.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-cabin-sketch.rb
@@ -0,0 +1,10 @@
+class FontCabinSketch < Formula
+  head "https://github.com/google/fonts/trunk/ofl/cabinsketch", using: :svn, revision: "50", trust_cert: true
+  desc "Cabin Sketch"
+  homepage "https://www.google.com/fonts/specimen/Cabin%20Sketch"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
