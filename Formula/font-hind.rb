f0c10b0d4f69f97dc48c9d48c95d50ef2aaba4f1
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:54 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hind.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-hind.rb
+++ Formula/font-hind.rb
@@ -1,7 +1,7 @@
 class FontHind < Formula
   version "2.000"
   sha256 "8748ce1fa0db67d0c782d7824a9fdcf0b8544b9d063db477dff9733774571193"
-  head "https://github.com/itfoundry/hind/releases/download/v2.000/hind-#{version.dots_to_underscores}.zip"
+  url "https://github.com/itfoundry/hind/releases/download/v2.000/hind-#{version.to_s.gsub(".", "_")}.zip"
   desc "Hind"
   homepage "https://github.com/itfoundry/hind"
   def install
