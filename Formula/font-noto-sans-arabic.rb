850769aa59dbf06aa3da36ba9e69f10bb56e2ead
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-arabic.rb from a877d71f000c4cd26ee9ddccfec7d06085e2aa0a

---
+++ Formula/font-noto-sans-arabic.rb
@@ -0,0 +1,9 @@
+class FontNotoSansArabic < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansArabic-unhinted.zip"
+  desc "Noto Sans Arabic"
+  homepage "https://www.google.com/get/noto/#sans-arab"
+  def install
+    
+  test do
+  end
+end
