52d89d9277d5da44bc9c96a46e7af169509d68d9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mr-bedfort.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-mr-bedfort.rb
@@ -0,0 +1,9 @@
+class FontMrBedfort < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/mrbedfort/MrBedfort-Regular.ttf"
+  desc "Mr Bedfort"
+  homepage "https://www.google.com/fonts/specimen/Mr+Bedfort"
+  def install
+    
+  test do
+  end
+end
