f28b8dfb9947f2e065aaa1539035788a56fb3e2c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kite-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-kite-one.rb
@@ -0,0 +1,9 @@
+class FontKiteOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kiteone/KiteOne-Regular.ttf"
+  desc "Kite One"
+  homepage "https://www.google.com/fonts/specimen/Kite+One"
+  def install
+    
+  test do
+  end
+end
