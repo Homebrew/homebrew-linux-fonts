d8cf3603dd455ee14e04c77beedd04e5ed9c155b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-squada-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-squada-one.rb
@@ -0,0 +1,9 @@
+class FontSquadaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/squadaone/SquadaOne-Regular.ttf"
+  desc "Squada One"
+  homepage "https://www.google.com/fonts/specimen/Squada+One"
+  def install
+    
+  test do
+  end
+end
