d5650a3c01906c70b87c999c7af42560e80eaf1c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pitabek.rb from 065a6c8537b91d79f6f9f022e58d4c1a8eb9b7eb

---
+++ Formula/font-pitabek.rb
@@ -0,0 +1,9 @@
+class FontPitabek < Formula
+  head "http://www.languagegeek.com/font/pitabek.zip"
+  desc "Pitabek"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
