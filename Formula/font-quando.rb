d95b5404510cfc9ffefc77af57cd46e3a0bd4925
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quando.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-quando.rb
@@ -0,0 +1,9 @@
+class FontQuando < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/quando/Quando-Regular.ttf"
+  desc "Quando"
+  homepage "https://www.google.com/fonts/specimen/Quando"
+  def install
+    
+  test do
+  end
+end
