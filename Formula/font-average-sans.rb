918e7ed588f64c03a23e2cde76b8e8047a204cca
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-average-sans.rb from 2d563bcd79d494fd5a7d8b5b9fc8f5648411ed0a

--- Formula/font-average-sans.rb
+++ Formula/font-average-sans.rb
@@ -1,7 +1,7 @@
 class FontAverageSans < Formula
   version "1.002"
   sha256 "f978bb0eb5ad93b02f3a8e4ef0ed525a1192b4076e8a8d0bb37b2546a297762e"
-  head "https://github.com/google/fonts/raw/master/ofl/averagesans/AverageSans-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/averagesans/AverageSans-Regular.ttf"
   desc "Average Sans"
   homepage "https://www.google.com/fonts/specimen/Average%20Sans"
   def install
