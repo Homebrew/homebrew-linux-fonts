3dd5b30c60faf8bacc5b7414701b355f62b0ed3b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-opendyslexic.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-opendyslexic.rb
@@ -0,0 +1,11 @@
+class FontOpendyslexic < Formula
+  version "20160623-Stable"
+  sha256 "3b4a84f573d3f5d75e198bbf362ff6fa812b33d6559dce651e44df455be929cc"
+  url "https://codeload.github.com/antijingoist/open-dyslexic/zip/#{version}"
+  desc "OpenDyslexic"
+  homepage "https://github.com/antijingoist/open-dyslexic"
+  def install
+    
+  test do
+  end
+end
