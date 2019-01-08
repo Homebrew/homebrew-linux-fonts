f74cafc4d9e6e11f641bdd0e713f79e4791cffcd
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lustria.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-lustria.rb
@@ -0,0 +1,9 @@
+class FontLustria < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/lustria/Lustria-Regular.ttf"
+  desc "Lustria"
+  homepage "https://www.google.com/fonts/specimen/Lustria"
+  def install
+    
+  test do
+  end
+end
