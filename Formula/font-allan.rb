ad6d2fc406f375dfbe441e76c67358da78ada814
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-allan.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-allan.rb
@@ -0,0 +1,10 @@
+class FontAllan < Formula
+  head "https://github.com/google/fonts/trunk/ofl/allan", using: :svn, revision: "50", trust_cert: true
+  desc "Allan"
+  homepage "https://www.google.com/fonts/specimen/Allan"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
