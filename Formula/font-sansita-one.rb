5320f7f1965cb5516f400b511236be08ce311c03
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sansita-one.rb from 291b1d470cc6116a851a6d729a0e668788c98700

---
+++ Formula/font-sansita-one.rb
@@ -0,0 +1,9 @@
+class FontSansitaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sansitaone/SansitaOne-Regular.ttf"
+  desc "Sansita One"
+  homepage "https://www.google.com/fonts/specimen/Sansita+One"
+  def install
+    
+  test do
+  end
+end
