259406aabdf0455a9d6ea1466aef8ea33192049c
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-museo.rb from 152917c2c252e8486b20252f73c04cd4e52999a8

--- Formula/font-museo.rb
+++ Formula/font-museo.rb
@@ -1,7 +1,7 @@
 class FontMuseo < Formula
   version "2.002"
   sha256 "c4c40653bc288aa123f38d6832c77498ba30e483caa16991f570cca625362344"
-  head "http://www.abstractfonts.com/download/14890?option=zip"
+  url "http://www.abstractfonts.com/download/14890?option=zip"
   desc "Museo"
   homepage "http://www.abstractfonts.com/font/14890"
   def install
