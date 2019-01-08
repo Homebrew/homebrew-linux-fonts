d8f02809d7d958e5e41475b79d1e8c07635445a3
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-namdhinggo-sil.rb from fcd1a55e57483dc627633b1a2623a79cb86782b8

--- Formula/font-namdhinggo-sil.rb
+++ Formula/font-namdhinggo-sil.rb
@@ -1,7 +1,7 @@
 class FontNamdhinggoSil < Formula
   version "1.004"
   sha256 "8bd69ee93687f2b3fcb705b0c8867c8ff573edcaf9a5c51a08a8ca1c1ddc966b"
-  head "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=NamdhinggoSIL#{version}&filename=NamdhinggoSIL#{version}.zip"
+  url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=NamdhinggoSIL#{version}&filename=NamdhinggoSIL#{version}.zip"
   desc "Namdhinggo SIL"
   homepage "https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=NamdhinggoSIL"
   def install
