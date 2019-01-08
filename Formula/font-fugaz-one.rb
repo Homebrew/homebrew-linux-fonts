029c10fb31695da855b1eba9d51c7299454a868a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fugaz-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-fugaz-one.rb
@@ -0,0 +1,9 @@
+class FontFugazOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/fugazone/FugazOne-Regular.ttf"
+  desc "Fugaz One"
+  homepage "https://www.google.com/fonts/specimen/Fugaz+One"
+  def install
+    
+  test do
+  end
+end
