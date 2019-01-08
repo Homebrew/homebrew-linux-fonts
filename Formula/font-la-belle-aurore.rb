58c29b62a521265674f67c27ab5bc93df71353c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:29 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-la-belle-aurore.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-la-belle-aurore.rb
@@ -0,0 +1,9 @@
+class FontLaBelleAurore < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/labelleaurore/LaBelleAurore.ttf"
+  desc "La Belle Aurore"
+  homepage "https://www.google.com/fonts/specimen/La+Belle+Aurore"
+  def install
+    
+  test do
+  end
+end
