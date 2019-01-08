bf1f9e9489adb7574fb40c442182ee289da293d0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aegean.rb from 49764000d914cc0534db2f022557adc6943bb3fe

---
+++ Formula/font-aegean.rb
@@ -0,0 +1,9 @@
+class FontAegean < Formula
+  head "http://users.teilar.gr/~g1951d/Aegean.zip"
+  desc "Aegean"
+  homepage "http://users.teilar.gr/~g1951d/"
+  def install
+    
+  test do
+  end
+end
