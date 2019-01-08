c443901c55d87311bcdbafb987fae33346eec7fd
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rosarivo.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-rosarivo.rb
@@ -0,0 +1,10 @@
+class FontRosarivo < Formula
+  head "https://github.com/google/fonts/trunk/ofl/rosarivo", using: :svn, revision: "50", trust_cert: true
+  desc "Rosarivo"
+  homepage "https://www.google.com/fonts/specimen/Rosarivo"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
