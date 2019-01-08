a3d52637a5508cba91fbb4a0b45d06b89746a8da
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-ubuntu-nerd-font.rb
+++ Formula/font-ubuntu-nerd-font.rb
@@ -1,7 +1,7 @@
 class FontUbuntuNerdFont < Formula
   version "2.0.0"
   sha256 "1c63e03728d89bbc6df6d357d156b5adb1c90bde247933c7cb429e6482ad22bb"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Ubuntu.zip"
+  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Ubuntu.zip"
   desc "Ubuntu Nerd Font (Ubuntu)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
