71f145fc92e763c5a5d230195f8f92658fbb9f6f
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:22 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-qataban.rb from 2c94e7a12a23af48a8ef54b5cf06b402bfe8c72c

--- Formula/font-qataban.rb
+++ Formula/font-qataban.rb
@@ -1,7 +1,7 @@
 class FontQataban < Formula
   version "1.0"
   sha256 "9f3243f1caa97cd8740c87ea892e8dc15d4efac0546df08ce1df2bc6f5fbdf32"
-  url "http://www.alanwood.net/downloads/qataban.zip"
+  head "http://www.alanwood.net/downloads/qataban.zip"
   desc "Qataban"
   homepage "http://www.alanwood.net/downloads/index.html"
   def install
