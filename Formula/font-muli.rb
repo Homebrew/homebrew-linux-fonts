f48c4e3f9451385669f4870d5480d85f7721708f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-muli.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-muli.rb
@@ -0,0 +1,10 @@
+class FontMuli < Formula
+  head "https://github.com/google/fonts/trunk/ofl/muli", using: :svn, trust_cert: true
+  desc "Muli"
+  homepage "https://www.google.com/fonts/specimen/Muli"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
