23e993d86767144cfa5425ca2c46fa9f6d942a2e
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:56:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-masinahikan-dene.rb from 07c6bd41138a782b4fde5540b0b95693b15ee808

---
+++ Formula/font-masinahikan-dene.rb
@@ -0,0 +1,12 @@
+class FontMasinahikanDene < Formula
+  head "http://www.languagegeek.com/font/MasinahikanDene.zip"
+  desc "Masinahikan Dene"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    (share/"fonts").install "MasinahikanDene_h.ttf"
+    (share/"fonts").install "MasinahikanDene_h_Bold.ttf"
+    (share/"fonts").install "MasinahikanDene_h_Semi.ttf"
+  end
+  test do
+  end
+end
