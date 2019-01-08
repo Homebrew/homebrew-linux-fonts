bff13691bcc9ee179d0ffba6870e4beaf77e58fb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-g-for-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-inconsolata-g-for-powerline.rb
@@ -0,0 +1,10 @@
+class FontInconsolataGForPowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/Inconsolata-g", using: :svn, trust_cert: true
+  desc "Inconsolata-g for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/Inconsolata-g"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
