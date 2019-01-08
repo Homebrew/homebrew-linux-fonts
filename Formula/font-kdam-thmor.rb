86d30be4235ad2004e87e9ba7daf8d4dc2d719a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kdam-thmor.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-kdam-thmor.rb
@@ -0,0 +1,9 @@
+class FontKdamThmor < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kdamthmor/KdamThmor-Regular.ttf"
+  desc "Kdam Thmor"
+  homepage "https://www.google.com/fonts/specimen/Kdam+Thmor"
+  def install
+    
+  test do
+  end
+end
