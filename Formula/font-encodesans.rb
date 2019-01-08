f148a03610ac1d19a9d44938ef74033a659091f0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-encodesans.rb from 4b855fd673d71fdc6cb6a25766a751f68319e2ec

---
+++ Formula/font-encodesans.rb
@@ -0,0 +1,10 @@
+class FontEncodesans < Formula
+  head "https://github.com/google/fonts/trunk/ofl/encodesans", using: :svn, trust_cert: true
+  desc "Encode Sans"
+  homepage "https://fonts.google.com/specimen/Encode+Sans"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
