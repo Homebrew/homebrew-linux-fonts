e009dc146b499e79303896424bab173380de5892
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-love-ya-like-a-sister.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-love-ya-like-a-sister.rb
@@ -0,0 +1,9 @@
+class FontLoveYaLikeASister < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/loveyalikeasister/LoveYaLikeASister.ttf"
+  desc "Love Ya Like A Sister"
+  homepage "https://www.google.com/fonts/specimen/Love+Ya+Like+A+Sister"
+  def install
+    
+  test do
+  end
+end
