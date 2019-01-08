43ddcac1a19e25524714e033d63202e67578f129
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-comfortaa.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-comfortaa.rb
@@ -0,0 +1,10 @@
+class FontComfortaa < Formula
+  head "https://github.com/google/fonts/trunk/ofl/comfortaa", using: :svn, revision: "50", trust_cert: true
+  desc "Comfortaa"
+  homepage "https://www.google.com/fonts/specimen/Comfortaa"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
