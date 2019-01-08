7451bbff138081fb68e7cae50c0b21ed337d04a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-smythe.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-smythe.rb
@@ -0,0 +1,9 @@
+class FontSmythe < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/smythe/Smythe-Regular.ttf"
+  desc "Smythe"
+  homepage "https://www.google.com/fonts/specimen/Smythe"
+  def install
+    
+  test do
+  end
+end
