6b31a91388ff0cb1eab4c61079cb084de141488c
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:55:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ia-writer-duospace.rb from da262c5b7b603a3d1bb81095fa04381951605e18

---
+++ Formula/font-ia-writer-duospace.rb
@@ -0,0 +1,13 @@
+class FontIaWriterDuospace < Formula
+  head "https://codeload.github.com/iaolo/iA-Fonts/zip/master"
+  desc "iA Writer Duospace"
+  homepage "https://github.com/iaolo/iA-Fonts"
+  def install
+    (share/"fonts").install "iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Bold.otf"
+    (share/"fonts").install "iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-BoldItalic.otf"
+    (share/"fonts").install "iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Regular.otf"
+    (share/"fonts").install "iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Italic.otf"
+  end
+  test do
+  end
+end
