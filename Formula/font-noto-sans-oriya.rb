f21ed0b6ca1a4c8aee2484944a6aa595d70134a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-oriya.rb from 7eff405e323aa4047b9815ba7e3a316e52a51c46

---
+++ Formula/font-noto-sans-oriya.rb
@@ -0,0 +1,9 @@
+class FontNotoSansOriya < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOriya-unhinted.zip"
+  desc "Noto Sans Oriya"
+  homepage "https://www.google.com/get/noto/#sans-orya"
+  def install
+    
+  test do
+  end
+end
