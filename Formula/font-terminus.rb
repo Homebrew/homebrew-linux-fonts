61933d98de056c2a100e225fad709f84c0e05c2d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-terminus.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-terminus.rb
@@ -0,0 +1,11 @@
+class FontTerminus < Formula
+  version "4.40.1"
+  sha256 "c3cb690c2935123035a0b1f3bfdd9511c282dab489cd423e161a47c592edf188"
+  url "https://files.ax86.net/terminus-ttf/files/#{version}/terminus-ttf-#{version}.zip"
+  desc "Terminus TTF"
+  homepage "https://files.ax86.net/terminus-ttf/"
+  def install
+    
+  test do
+  end
+end
