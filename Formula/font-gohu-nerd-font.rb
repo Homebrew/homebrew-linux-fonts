17f93270ba536170292504768c55e7f386800b3e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gohu-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-gohu-nerd-font.rb
@@ -0,0 +1,11 @@
+class FontGohuNerdFont < Formula
+  version "1.2.0"
+  sha256 "3c2cc94365093054b7b32638dfa1cba0ea09e02a8abd28395612de4c03fd1f4b"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Gohu.zip"
+  desc "GohuFontBold Nerd Font (Gohu)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
