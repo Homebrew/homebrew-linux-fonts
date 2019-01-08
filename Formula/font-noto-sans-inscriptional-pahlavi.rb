705bcd4a4158400186a021223419196d737e9aec
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-inscriptional-pahlavi.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-inscriptional-pahlavi.rb
@@ -0,0 +1,9 @@
+class FontNotoSansInscriptionalPahlavi < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansInscriptionalPahlavi-unhinted.zip"
+  desc "Noto Sans Inscriptional Pahlavi"
+  homepage "https://www.google.com/get/noto/#sans-phli"
+  def install
+    
+  test do
+  end
+end
