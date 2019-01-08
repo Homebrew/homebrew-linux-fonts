a06a0d945527b8b7d77a0c957e7fb62fd3ec7df7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oldenburg.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-oldenburg.rb
@@ -0,0 +1,9 @@
+class FontOldenburg < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/oldenburg/Oldenburg-Regular.ttf"
+  desc "Oldenburg"
+  homepage "https://www.google.com/fonts/specimen/Oldenburg"
+  def install
+    
+  test do
+  end
+end
