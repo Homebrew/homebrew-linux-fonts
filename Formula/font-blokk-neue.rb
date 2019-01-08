a48893df880ed8313e749093f69cc32dc4df339e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-blokk-neue.rb from f8b9b559eb8992f95d5f1cc68f98cfc6d7822416

---
+++ Formula/font-blokk-neue.rb
@@ -0,0 +1,9 @@
+class FontBlokkNeue < Formula
+  head "http://www.blokkfont.com/cdn/v2/BLOKKNeue-Regular.zip"
+  desc "BLOKK Neue"
+  homepage "http://blokkfont.com/"
+  def install
+    
+  test do
+  end
+end
