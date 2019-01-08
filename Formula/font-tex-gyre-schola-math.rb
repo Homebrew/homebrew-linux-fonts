6e0cc0de809ee987059c5434f2cf3d267002fada
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-schola-math.rb from ae069a537e694d476b567118c3c6c390bf25b74c

---
+++ Formula/font-tex-gyre-schola-math.rb
@@ -0,0 +1,11 @@
+class FontTexGyreScholaMath < Formula
+  version "1.533"
+  sha256 "53560861144138e25f89f1f487126d21c81c5086364ffcf2c8e5e46e37ebbe00"
+  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyreschola-math-1533.zip"
+  desc "Schola Math"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tg-math"
+  def install
+    
+  test do
+  end
+end
