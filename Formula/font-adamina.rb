3d062732253184427942516d1fcd73cd02a5c44f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-adamina.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-adamina.rb
@@ -0,0 +1,9 @@
+class FontAdamina < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/adamina/Adamina-Regular.ttf"
+  desc "Adamina"
+  homepage "https://www.google.com/fonts/specimen/Adamina"
+  def install
+    
+  test do
+  end
+end
