aa283d4e1c3a02caeadead7632712e7a6f0096b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-junge.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-junge.rb
@@ -0,0 +1,9 @@
+class FontJunge < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/junge/Junge-Regular.ttf"
+  desc "Junge"
+  homepage "https://www.google.com/fonts/specimen/Junge"
+  def install
+    
+  test do
+  end
+end
