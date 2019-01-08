92d6972770f2a8b5b480144bdd8ee45105f0cbe4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-unica-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-unica-one.rb
@@ -0,0 +1,9 @@
+class FontUnicaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/unicaone/UnicaOne-Regular.ttf"
+  desc "Unica One"
+  homepage "https://www.google.com/fonts/specimen/Unica+One"
+  def install
+    
+  test do
+  end
+end
