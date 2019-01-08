00e00c5f129949b9d515121aa69c06cd940d4023
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-balthazar.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-balthazar.rb
@@ -0,0 +1,9 @@
+class FontBalthazar < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/balthazar/Balthazar-Regular.ttf"
+  desc "Balthazar"
+  homepage "https://www.google.com/fonts/specimen/Balthazar"
+  def install
+    
+  test do
+  end
+end
