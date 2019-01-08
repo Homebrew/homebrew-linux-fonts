837b983aae6c85b1ab1df434f022683de5c1635b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-coming-soon.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-coming-soon.rb
@@ -0,0 +1,9 @@
+class FontComingSoon < Formula
+  head "https://github.com/google/fonts/raw/master/apache/comingsoon/ComingSoon-Regular.ttf"
+  desc "Coming Soon"
+  homepage "https://www.google.com/fonts/specimen/Coming%20Soon"
+  def install
+    
+  test do
+  end
+end
