8cd742bdc8482469cad397ffbe27f7254fdce03d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-almendra-display.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-almendra-display.rb
@@ -0,0 +1,9 @@
+class FontAlmendraDisplay < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/almendradisplay/AlmendraDisplay-Regular.ttf"
+  desc "Almendra Display"
+  homepage "https://www.google.com/fonts/specimen/Almendra+Display"
+  def install
+    
+  test do
+  end
+end
