26e96353f9540c8bf98527eeb40250a6c06ae287
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mononoki-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-mononoki-nerd-font-mono.rb
+++ Formula/font-mononoki-nerd-font-mono.rb
@@ -1,7 +1,7 @@
 class FontMononokiNerdFontMono < Formula
   version "2.0.0"
   sha256 "715813f9bdeddfa35a39681dedf4a61cf1b6cfe8e06b9b3ca19c391cb308a589"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Mononoki.zip"
+  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Mononoki.zip"
   desc "mononoki Nerd Font (Mononoki)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
