e5c30ae408ac9bad42218b236cfac25fba8f1dbd
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rozha-one.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-rozha-one.rb
@@ -0,0 +1,11 @@
+class FontRozhaOne < Formula
+  version "2.000"
+  sha256 "9048def2a602b51466f6f7528a0e83fbc73abd72aadb87d70fba3887e6101c7c"
+  url "https://github.com/itfoundry/rozhaone/releases/download/v#{version}/rozhaone-#{version.to_s.gsub(".", "_")}.zip"
+  desc "Rozha One"
+  homepage "https://github.com/itfoundry/rozhaone"
+  def install
+    
+  test do
+  end
+end
