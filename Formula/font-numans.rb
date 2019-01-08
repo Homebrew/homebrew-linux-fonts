6c627b3e35e6976e6467692da1cdce7174567af6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-numans.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-numans.rb
@@ -0,0 +1,9 @@
+class FontNumans < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/numans/Numans-Regular.ttf"
+  desc "Numans"
+  homepage "https://www.google.com/fonts/specimen/Numans"
+  def install
+    
+  test do
+  end
+end
