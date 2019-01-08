0c78223b9d8c64f53d2940fd35469a7d20547382
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-signika-negative.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-signika-negative.rb
@@ -0,0 +1,10 @@
+class FontSignikaNegative < Formula
+  head "https://github.com/google/fonts/trunk/ofl/signikanegative", using: :svn, revision: "50", trust_cert: true
+  desc "Signika Negative"
+  homepage "https://www.google.com/fonts/specimen/Signika%20Negative"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
