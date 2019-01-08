f798d34111c21c37137b2eaf93662f0bb1dfcd54
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mystery-quest.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-mystery-quest.rb
@@ -0,0 +1,9 @@
+class FontMysteryQuest < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/mysteryquest/MysteryQuest-Regular.ttf"
+  desc "Mystery Quest"
+  homepage "https://www.google.com/fonts/specimen/Mystery+Quest"
+  def install
+    
+  test do
+  end
+end
