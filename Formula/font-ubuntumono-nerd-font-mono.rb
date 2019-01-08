7c93975ae2ec5f091b91daed9d8499be210b57fc
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 12:00:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntumono-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-ubuntumono-nerd-font-mono.rb
@@ -0,0 +1,12 @@
+class FontUbuntumonoNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "23b9fb5e683ae7ca81306a0c1da842a4d4cabb8bbec28ed5a7d604de688b2b8e"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/UbuntuMono.zip"
+  desc "UbuntuMono Nerd Font (UbuntuMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    (share/"fonts").install "Ubuntu Mono Nerd Font Complete Mono.ttf"
+  end
+  test do
+  end
+end
