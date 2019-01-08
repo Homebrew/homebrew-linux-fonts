0781c2e19dbe60ed400dd9903d3c85a9d2318dba
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-berkshire-swash.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-berkshire-swash.rb
@@ -0,0 +1,9 @@
+class FontBerkshireSwash < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/berkshireswash/BerkshireSwash-Regular.ttf"
+  desc "Berkshire Swash"
+  homepage "https://www.google.com/fonts/specimen/Berkshire+Swash"
+  def install
+    
+  test do
+  end
+end
