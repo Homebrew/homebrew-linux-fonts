72072f9969d3b72a7015c0d5f6a4601db7536a73
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-anton.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-anton.rb
@@ -0,0 +1,9 @@
+class FontAnton < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/anton/Anton-Regular.ttf"
+  desc "Anton"
+  homepage "https://www.google.com/fonts/specimen/Anton"
+  def install
+    
+  test do
+  end
+end
