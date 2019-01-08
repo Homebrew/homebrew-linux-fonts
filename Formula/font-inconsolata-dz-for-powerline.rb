921b472c0fa9a00c304402f4b7fd51527babfbcb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-dz-for-powerline.rb from 534bc6c25e26357a55b24d972735d607b9c030ff

---
+++ Formula/font-inconsolata-dz-for-powerline.rb
@@ -0,0 +1,9 @@
+class FontInconsolataDzForPowerline < Formula
+  head "https://raw.github.com/powerline/fonts/master/InconsolataDz/Inconsolata-dz%20for%20Powerline.otf"
+  desc "Inconsolata-dz for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/InconsolataDz"
+  def install
+    
+  test do
+  end
+end
