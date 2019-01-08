f9a894fe15d7383a6a1ec19c266ba3ada3d8d9a7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spectral.rb from 1ec199340fed1af60f1788a36e6e532450d45424

---
+++ Formula/font-spectral.rb
@@ -0,0 +1,9 @@
+class FontSpectral < Formula
+  head "https://github.com/productiontype/Spectral/archive/master.zip"
+  desc "Spectral"
+  homepage "https://github.com/productiontype/Spectral"
+  def install
+    
+  test do
+  end
+end
