c859d11b15e5862316788c2e8ef13622ed107579
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-trirong.rb from cc1109830245d7a3361fd2626787383a987405a2

---
+++ Formula/font-trirong.rb
@@ -0,0 +1,9 @@
+class FontTrirong < Formula
+  head "https://github.com/cadsondemak/trirong/archive/master.zip"
+  desc "Trirong"
+  homepage "https://cadsondemak.github.io/trirong/"
+  def install
+    
+  test do
+  end
+end
