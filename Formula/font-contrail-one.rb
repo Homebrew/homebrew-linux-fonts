b0fc2f874b95dca3e89b85878fdb3d8017b8fbae
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-contrail-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-contrail-one.rb
@@ -0,0 +1,9 @@
+class FontContrailOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/contrailone/ContrailOne-Regular.ttf"
+  desc "Contrail One"
+  homepage "https://www.google.com/fonts/specimen/Contrail+One"
+  def install
+    
+  test do
+  end
+end
