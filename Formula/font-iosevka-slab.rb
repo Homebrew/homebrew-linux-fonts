f6e549cc5b3e135fc507880d0c2d7a02fce65991
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-slab.rb from 52eb64934bb15ce1448b89017965ef6bc714f94e

---
+++ Formula/font-iosevka-slab.rb
@@ -0,0 +1,11 @@
+class FontIosevkaSlab < Formula
+  version "2.0.2"
+  sha256 "f6f16f1d9615e95cce1b04f4ce9833b252badaca605d97fde22884bfcec088dd"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
+  desc "Iosevka Slab"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    
+  test do
+  end
+end
