a60e675fd73e8568626ef71233becdee548cc9e6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-racing-sans-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-racing-sans-one.rb
@@ -0,0 +1,9 @@
+class FontRacingSansOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/racingsansone/RacingSansOne-Regular.ttf"
+  desc "Racing Sans One"
+  homepage "https://www.google.com/fonts/specimen/Racing+Sans+One"
+  def install
+    
+  test do
+  end
+end
