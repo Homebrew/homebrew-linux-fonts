bd908faba0d4043c1046ca38811228cdfb46319f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mfizz.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-mfizz.rb
@@ -0,0 +1,11 @@
+class FontMfizz < Formula
+  version "2.4.1"
+  sha256 "c447c9d00a7a37b78af4e86ff8f787473fd6e9812b6b5033eb23dc62ffc5e044"
+  url "https://github.com/fizzed/font-mfizz/releases/download/v#{version}/font-mfizz-#{version}.zip"
+  desc "Font Mfizz"
+  homepage "http://fizzed.com/oss/font-mfizz"
+  def install
+    
+  test do
+  end
+end
