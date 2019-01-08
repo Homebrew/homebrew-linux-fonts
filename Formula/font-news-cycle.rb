5b9ec02af51ff90a308299e651cf569e1e3d774a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-news-cycle.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-news-cycle.rb
@@ -0,0 +1,10 @@
+class FontNewsCycle < Formula
+  head "https://github.com/google/fonts/trunk/ofl/newscycle", using: :svn, revision: "50", trust_cert: true
+  desc "News Cycle"
+  homepage "https://www.google.com/fonts/specimen/News%20Cycle"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
