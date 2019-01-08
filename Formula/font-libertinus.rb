0b94a0e4f6728272e8de7eb2cb7cdf5a72c54a7d
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-libertinus.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-libertinus.rb
+++ Formula/font-libertinus.rb
@@ -1,7 +1,7 @@
 class FontLibertinus < Formula
   version "6.6"
   sha256 "ad772e5272f53a6f548ce1472651f4215eac0064e586fe7259001c634bdf0fb0"
-  head "https://github.com/libertinus-fonts/libertinus/archive/v#{version}.tar.gz"
+  url "https://github.com/libertinus-fonts/libertinus/archive/v#{version}.tar.gz"
   desc "Libertinus"
   homepage "https://github.com/libertinus-fonts/libertinus"
   def install
