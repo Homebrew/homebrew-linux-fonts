e748d5df37070c615923acba481d578b17e3ce1b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-han-code-jp.rb from d89319af38a80fb8a46456f4efaf74e842a1f9f6

---
+++ Formula/font-source-han-code-jp.rb
@@ -0,0 +1,11 @@
+class FontSourceHanCodeJp < Formula
+  version "2.011"
+  sha256 "0f98beb0a381d9d19067f9b58f65d2dd65c808a3012ac9cb9866e575ad6d2e7f"
+  url "https://github.com/adobe-fonts/source-han-code-jp/archive/#{version}R.zip"
+  desc "Source Han Code JP"
+  homepage "https://github.com/adobe-fonts/source-han-code-jp"
+  def install
+    
+  test do
+  end
+end
