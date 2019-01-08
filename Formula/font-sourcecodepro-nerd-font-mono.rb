acfb55d649cb46b6b45340fea95004c31b9764b1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sourcecodepro-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-sourcecodepro-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontSourcecodeproNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "f8e0cc0aceefa97a2c3f256fbc9a460038059ef0a193f02960f644daddfdfbbb"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/SourceCodePro.zip"
+  desc "SauceCodePro Nerd Font (SourceCodePro)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
