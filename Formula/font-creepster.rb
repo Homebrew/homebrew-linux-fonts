ae6f486baad4c18f52f4aa75601af79f86a51c08
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-creepster.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-creepster.rb
@@ -0,0 +1,9 @@
+class FontCreepster < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/creepster/Creepster-Regular.ttf"
+  desc "Creepster"
+  homepage "https://www.google.com/fonts/specimen/Creepster"
+  def install
+    
+  test do
+  end
+end
