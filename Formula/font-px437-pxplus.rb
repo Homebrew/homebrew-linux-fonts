c0efe080762b93b44a92d833f730d51e99cdc02c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-px437-pxplus.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-px437-pxplus.rb
@@ -0,0 +1,13 @@
+class FontPx437Pxplus < Formula
+  version "1.0"
+  sha256 "7666cf23176e34ea03a218b5c1500f4ad729d97150ab7bdb7cf2adf4c99a9a7a"
+  url "https://int10h.org/oldschool-pc-fonts/download/ultimate_oldschool_pc_font_pack_v#{version}.zip"
+  desc "Px437"
+  desc "PxPlus"
+  desc "Ultimate Oldschool PC Font Pack"
+  homepage "https://int10h.org/oldschool-pc-fonts/"
+  def install
+    
+  test do
+  end
+end
