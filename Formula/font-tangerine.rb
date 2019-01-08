a10ca987e55ae369955df33b6e5df3f6134ef552
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tangerine.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-tangerine.rb
@@ -0,0 +1,10 @@
+class FontTangerine < Formula
+  head "https://github.com/google/fonts/trunk/ofl/tangerine", using: :svn, revision: "50", trust_cert: true
+  desc "Tangerine"
+  homepage "https://www.google.com/fonts/specimen/Tangerine"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
