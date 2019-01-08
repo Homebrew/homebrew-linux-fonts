4893c53ff3634160852aef5d27bf46a467c3a0a2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-limbu.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-limbu.rb
@@ -0,0 +1,9 @@
+class FontNotoSansLimbu < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLimbu-unhinted.zip"
+  desc "Noto Sans Limbu"
+  homepage "https://www.google.com/get/noto/#sans-limb"
+  def install
+    
+  test do
+  end
+end
