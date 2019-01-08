5c986760763826cf96081e366aae948d0a51393d
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:25 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-pagella.rb from 42cebe1020217520cf1da4d1d1e9ea9767d8a605

--- Formula/font-tex-gyre-pagella.rb
+++ Formula/font-tex-gyre-pagella.rb
@@ -1,7 +1,7 @@
 class FontTexGyrePagella < Formula
   version "2.501"
   sha256 "e16078d19860d68a54bcaedc00e35c2a81b396cdc36842700f1d556f89cf8ef2"
-  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl#{version.to_s.gsub(".", "_")}otf.zip"
+  head "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl#{version.dots_to_underscores}otf.zip"
   desc "TeX Gyre Pagella"
   homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella"
   def install
