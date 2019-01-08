1c3cf8be60b7d19b0470e3cedb906b281b4782d6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aclonica.rb from 25c13705705a0563de663f06a9f81890d09f588b

---
+++ Formula/font-aclonica.rb
@@ -0,0 +1,9 @@
+class FontAclonica < Formula
+  head "https://github.com/google/fonts/raw/master/apache/aclonica/Aclonica-Regular.ttf"
+  desc "Aclonica"
+  homepage "https://www.google.com/fonts/specimen/Aclonica"
+  def install
+    
+  test do
+  end
+end
