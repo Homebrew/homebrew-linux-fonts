3c5151a50d65f48ec51520380d7d774c3e283f5d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-just-another-hand.rb from 8111609852d305b5d18b49764cd609d405a4b7df

---
+++ Formula/font-just-another-hand.rb
@@ -0,0 +1,9 @@
+class FontJustAnotherHand < Formula
+  head "https://github.com/google/fonts/raw/master/apache/justanotherhand/JustAnotherHand-Regular.ttf"
+  desc "Just Another Hand"
+  homepage "https://www.google.com/fonts/specimen/Just+Another+Hand"
+  def install
+    
+  test do
+  end
+end
