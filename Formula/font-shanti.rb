fe76d76e97dbb824538feaea8fd5520cab902d3e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-shanti.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-shanti.rb
@@ -0,0 +1,9 @@
+class FontShanti < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/shanti/Shanti-Regular.ttf"
+  desc "Shanti"
+  homepage "https://www.google.com/fonts/specimen/Shanti"
+  def install
+    
+  test do
+  end
+end
