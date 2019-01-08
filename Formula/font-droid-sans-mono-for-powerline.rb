2627a2d83572b7a5373a1437144be062a890762c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-droid-sans-mono-for-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-droid-sans-mono-for-powerline.rb
@@ -0,0 +1,10 @@
+class FontDroidSansMonoForPowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/DroidSansMono", using: :svn, trust_cert: true
+  desc "Droid Sans Mono for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/DroidSansMono"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
