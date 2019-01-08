78e207a9c5459dd36be2dc54fb0f2bb302ba03cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-adventor.rb from 88600e3ac0a5a9e27896e0c82af4f76fd633ef37

---
+++ Formula/font-tex-gyre-adventor.rb
@@ -0,0 +1,11 @@
+class FontTexGyreAdventor < Formula
+  version "2.501"
+  sha256 "9e619eb1c8af5cb55240f8bb865453562a2efd9059dee39d085fb71f7a00f7a2"
+  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor/qag#{version.to_s.gsub(".", "_")}otf.zip"
+  desc "TeX Gyre Adventor"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor"
+  def install
+    
+  test do
+  end
+end
