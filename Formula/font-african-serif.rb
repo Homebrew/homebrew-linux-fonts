a60f406b6f0f0c530af582b5359f4e9282f56f43
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-african-serif.rb from 8beb2f950ff43e62fe11dfc1861178a7a86948bf

--- Formula/font-african-serif.rb
+++ Formula/font-african-serif.rb
@@ -1,7 +1,7 @@
 class FontAfricanSerif < Formula
   version "9.380"
   sha256 "fe352b95cf8a6e949d40a6b6f018b4fd6321c16dd8c5c40b35be3a69feb582f5"
-  head "http://www.languagegeek.com/font/AfricanSerif.zip"
+  url "http://www.languagegeek.com/font/AfricanSerif.zip"
   desc "African Serif"
   homepage "http://www.languagegeek.com/font/fontdownload.html"
   def install
