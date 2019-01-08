c5c530b885cef42c322f8533eb940962a5e9a068
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-esteban.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-esteban.rb
@@ -0,0 +1,9 @@
+class FontEsteban < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/esteban/Esteban-Regular.ttf"
+  desc "Esteban"
+  homepage "https://www.google.com/fonts/specimen/Esteban"
+  def install
+    
+  test do
+  end
+end
