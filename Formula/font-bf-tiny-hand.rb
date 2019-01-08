b3338345fb17d9e2c75217d0a03aee7c3234805e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bf-tiny-hand.rb from aac982845cfd7a976b97889278f77ed6d4a13e16

---
+++ Formula/font-bf-tiny-hand.rb
@@ -0,0 +1,11 @@
+class FontBfTinyHand < Formula
+  version "1.0"
+  sha256 "b103175ac44cd3247e1ad2c3a8715d60caee60152e18fc69d849e7c88d1ce0bf"
+  url "https://www.dropbox.com/s/irvgp4813izfbso/BFTinyHand-Regular.zip?dl=1"
+  desc "BF Tiny Hand"
+  homepage "https://www.buzzfeed.com/benking/tiny-hand-will-be-your-new-comic-sans"
+  def install
+    
+  test do
+  end
+end
