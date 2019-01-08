b76b359d61c7af5a57a4065f0c4d161fb2af7df2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-benchnine.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-benchnine.rb
@@ -0,0 +1,10 @@
+class FontBenchnine < Formula
+  head "https://github.com/google/fonts/trunk/ofl/benchnine", using: :svn, revision: "50", trust_cert: true
+  desc "BenchNine"
+  homepage "https://www.google.com/fonts/specimen/BenchNine"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
