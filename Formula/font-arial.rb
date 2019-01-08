175e498c64043eaf4a31993eea8df39bb10fa1ee
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arial.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-arial.rb
@@ -0,0 +1,11 @@
+class FontArial < Formula
+  version "2.82"
+  sha256 "85297a4d146e9c87ac6f74822734bdee5f4b2a722d7eaa584b7f2cbf76f478f6"
+  url "https://downloads.sourceforge.net/corefonts/arial32.exe"
+  desc "Arial"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  def install
+    
+  test do
+  end
+end
