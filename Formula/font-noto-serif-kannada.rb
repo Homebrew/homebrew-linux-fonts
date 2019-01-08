89785ee5cdbd1103e2b6e82b5f91322973f2209e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-kannada.rb from 6612748208449843385714429f09395a19960b12

---
+++ Formula/font-noto-serif-kannada.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifKannada < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifKannada-unhinted.zip"
+  desc "Noto Serif Kannada"
+  homepage "https://www.google.com/get/noto/#serif-knda"
+  def install
+    
+  test do
+  end
+end
