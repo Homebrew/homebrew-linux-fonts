2b2564d1056c73b44489c755c3715d19d073a00c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-coda-caption.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-coda-caption.rb
@@ -0,0 +1,9 @@
+class FontCodaCaption < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/codacaption/CodaCaption-ExtraBold.ttf"
+  desc "Coda Caption"
+  homepage "https://www.google.com/fonts/specimen/Coda+Caption"
+  def install
+    
+  test do
+  end
+end
