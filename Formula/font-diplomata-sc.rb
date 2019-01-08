8bbe12302c1a6a42513208e6ae894bfed50faf2b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-diplomata-sc.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-diplomata-sc.rb
@@ -0,0 +1,9 @@
+class FontDiplomataSc < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/diplomatasc/DiplomataSC-Regular.ttf"
+  desc "Diplomata SC"
+  homepage "https://www.google.com/fonts/specimen/Diplomata+SC"
+  def install
+    
+  test do
+  end
+end
