325ffe71db582e3e9200855bf0f161d3e52d8325
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:53 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-firacode-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-firacode-nerd-font.rb
+++ Formula/font-firacode-nerd-font.rb
@@ -1,7 +1,7 @@
 class FontFiracodeNerdFont < Formula
   version "2.0.0"
   sha256 "09894d24bf3d61493dba052187a9200497135a4b885cb837bcb637ad2e62070f"
-  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraCode.zip"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraCode.zip"
   desc "FuraCode Nerd Font (FiraCode)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
