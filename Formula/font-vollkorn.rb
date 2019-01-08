c0ae1791fbb26cde1240a77928b4f915b0440a9f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-vollkorn.rb from 6a75abe985e579028be7b6368b5c85137c851617

---
+++ Formula/font-vollkorn.rb
@@ -0,0 +1,11 @@
+class FontVollkorn < Formula
+  version "4.105"
+  sha256 "e650ea9d967c2af1e9b8d34e1118721728bcd73ccfd3220da4264661301e8af5"
+  url "http://vollkorn-typeface.com/download/vollkorn-#{version.to_s.gsub(".", "-")}.zip"
+  desc "Vollkorn"
+  homepage "http://vollkorn-typeface.com/"
+  def install
+    
+  test do
+  end
+end
