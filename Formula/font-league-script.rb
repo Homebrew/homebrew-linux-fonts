e44b03c0b90ba48873c6d826250735b07bc1ada1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-league-script.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-league-script.rb
@@ -0,0 +1,9 @@
+class FontLeagueScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/leaguescript/LeagueScript.ttf"
+  desc "League Script"
+  homepage "https://www.google.com/fonts/specimen/League+Script"
+  def install
+    
+  test do
+  end
+end
