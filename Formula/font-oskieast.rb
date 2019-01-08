0097a09c1edf826e2fe82bb4826f8061c94d1b99
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oskieast.rb from 1a4077ba599fabe33b0731882303a6f7b8370f0b

---
+++ Formula/font-oskieast.rb
@@ -0,0 +1,9 @@
+class FontOskieast < Formula
+  head "http://www.languagegeek.com/font/oskieast.zip"
+  desc "OskiEast"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
