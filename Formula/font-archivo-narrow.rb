aef2fbff289690d2a5bde3191d73e281ebae7d26
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-archivo-narrow.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-archivo-narrow.rb
@@ -0,0 +1,10 @@
+class FontArchivoNarrow < Formula
+  head "https://github.com/google/fonts/trunk/ofl/archivonarrow", using: :svn, revision: "50", trust_cert: true
+  desc "Archivo Narrow"
+  homepage "https://www.google.com/fonts/specimen/Archivo%20Narrow"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
