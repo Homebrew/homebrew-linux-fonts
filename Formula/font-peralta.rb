17fb2cfff4afeb9a2d335083f7c6b38150dded16
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-peralta.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-peralta.rb
@@ -0,0 +1,9 @@
+class FontPeralta < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/peralta/Peralta-Regular.ttf"
+  desc "Peralta"
+  homepage "https://www.google.com/fonts/specimen/Peralta"
+  def install
+    
+  test do
+  end
+end
