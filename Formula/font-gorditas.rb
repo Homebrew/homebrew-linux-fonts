3dc39dbf0ed0b338cee2e6bdeb2739f15cf1a3f8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gorditas.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-gorditas.rb
@@ -0,0 +1,10 @@
+class FontGorditas < Formula
+  head "https://github.com/google/fonts/trunk/ofl/gorditas", using: :svn, revision: "50", trust_cert: true
+  desc "Gorditas"
+  homepage "https://www.google.com/fonts/specimen/Gorditas"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
