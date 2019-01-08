32178681ef119dc3df0c559077f490a1f51bd57f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stoke.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-stoke.rb
@@ -0,0 +1,10 @@
+class FontStoke < Formula
+  head "https://github.com/google/fonts/trunk/ofl/stoke", using: :svn, revision: "50", trust_cert: true
+  desc "Stoke"
+  homepage "https://www.google.com/fonts/specimen/Stoke"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
