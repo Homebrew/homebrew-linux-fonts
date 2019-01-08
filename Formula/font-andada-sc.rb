86e2bc1cf503389fd2120c44895a2c8a174693a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-andada-sc.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-andada-sc.rb
@@ -0,0 +1,10 @@
+class FontAndadaSc < Formula
+  head "https://github.com/google/fonts/trunk/ofl/andadasc", using: :svn, revision: "50", trust_cert: true
+  desc "Andada SC"
+  homepage "https://www.fontsquirrel.com/fonts/andada"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
