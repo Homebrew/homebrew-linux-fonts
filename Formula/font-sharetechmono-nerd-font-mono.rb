a6ad8a68612fa81f7d0f89b7a7277447977e6764
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sharetechmono-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-sharetechmono-nerd-font-mono.rb
+++ Formula/font-sharetechmono-nerd-font-mono.rb
@@ -1,7 +1,7 @@
 class FontSharetechmonoNerdFontMono < Formula
   version "2.0.0"
   sha256 "0d697cf592e46d6c36e017a84e58a05343e3b4c2ea71e40671de3930c1582b9f"
-  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ShareTechMono.zip"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ShareTechMono.zip"
   desc "ShureTechMono Nerd Font (ShareTechMono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
