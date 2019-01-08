5eaa8d9c4d398ffd973646e274c8e9ef061f813e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-antic-slab.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-antic-slab.rb
@@ -0,0 +1,9 @@
+class FontAnticSlab < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/anticslab/AnticSlab-Regular.ttf"
+  desc "Antic Slab"
+  homepage "https://www.google.com/fonts/specimen/Antic+Slab"
+  def install
+    
+  test do
+  end
+end
