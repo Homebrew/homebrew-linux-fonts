d6bf06fe99575f7e1dd9f2a13e62f8da89078908
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-content.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-content.rb
@@ -0,0 +1,10 @@
+class FontContent < Formula
+  head "https://github.com/google/fonts/trunk/ofl/content", using: :svn, revision: "50", trust_cert: true
+  desc "Content"
+  homepage "https://www.google.com/fonts/specimen/Content"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
