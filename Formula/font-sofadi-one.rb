7755a2f23a76df5d363c86d02d54162ab984d105
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sofadi-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sofadi-one.rb
@@ -0,0 +1,9 @@
+class FontSofadiOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sofadione/SofadiOne-Regular.ttf"
+  desc "Sofadi One"
+  homepage "https://www.google.com/fonts/specimen/Sofadi+One"
+  def install
+    
+  test do
+  end
+end
