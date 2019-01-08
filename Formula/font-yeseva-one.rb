7d0a2c7a174ebfdad74599a75d7a8fd523597d46
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-yeseva-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-yeseva-one.rb
@@ -0,0 +1,9 @@
+class FontYesevaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/yesevaone/YesevaOne-Regular.ttf"
+  desc "Yeseva One"
+  homepage "https://www.google.com/fonts/specimen/Yeseva+One"
+  def install
+    
+  test do
+  end
+end
