5ab532326a9dddd9413690eda3ce3459ede22306
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-homemade-apple.rb from ea393d78428f7dde851657021c1ef5774afc6749

---
+++ Formula/font-homemade-apple.rb
@@ -0,0 +1,9 @@
+class FontHomemadeApple < Formula
+  head "https://github.com/google/fonts/raw/master/apache/homemadeapple/HomemadeApple-Regular.ttf"
+  desc "Homemade Apple"
+  homepage "https://www.google.com/fonts/specimen/Homemade+Apple"
+  def install
+    
+  test do
+  end
+end
