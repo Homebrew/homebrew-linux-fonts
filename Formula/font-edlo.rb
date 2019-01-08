39549a0e0edc9dddd1966959987ee6b90b721f72
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-edlo.rb from fde78a2412a73b75a54b925f81f6283661616279

---
+++ Formula/font-edlo.rb
@@ -0,0 +1,9 @@
+class FontEdlo < Formula
+  head "https://github.com/ehamiter/Edlo/raw/master/edlo.ttf"
+  desc "Edlo"
+  homepage "https://ehamiter.github.io/Edlo/"
+  def install
+    
+  test do
+  end
+end
