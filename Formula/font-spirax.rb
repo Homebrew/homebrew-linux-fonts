74fb6ec1160d07a14495f54c6441eef27cb14267
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spirax.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-spirax.rb
@@ -0,0 +1,9 @@
+class FontSpirax < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/spirax/Spirax-Regular.ttf"
+  desc "Spirax"
+  homepage "https://www.google.com/fonts/specimen/Spirax"
+  def install
+    
+  test do
+  end
+end
