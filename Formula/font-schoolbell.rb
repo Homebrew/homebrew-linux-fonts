d6047168ecef91a7f63774f32947f1513fb28b49
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-schoolbell.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-schoolbell.rb
@@ -0,0 +1,9 @@
+class FontSchoolbell < Formula
+  head "https://github.com/google/fonts/raw/master/apache/schoolbell/Schoolbell-Regular.ttf"
+  desc "Schoolbell"
+  homepage "https://www.google.com/fonts/specimen/Schoolbell"
+  def install
+    
+  test do
+  end
+end
