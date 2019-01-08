3101b255c9ed2e098b5ee0acb5be5ecd07e6767c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-unna.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-unna.rb
@@ -0,0 +1,9 @@
+class FontUnna < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/unna/Unna-Regular.ttf"
+  desc "Unna"
+  homepage "https://www.google.com/fonts/specimen/Unna"
+  def install
+    
+  test do
+  end
+end
