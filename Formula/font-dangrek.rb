ea8ead7778adfa4ef509e583eed9cb1563092a8c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dangrek.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-dangrek.rb
@@ -0,0 +1,9 @@
+class FontDangrek < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/dangrek/Dangrek.ttf"
+  desc "Dangrek"
+  homepage "https://www.google.com/fonts/specimen/Dangrek"
+  def install
+    
+  test do
+  end
+end
