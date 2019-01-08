f27730fd63ef785195619bb2cb00de0a9cf98306
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:47 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-montserrat.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-montserrat.rb
@@ -0,0 +1,11 @@
+class FontMontserrat < Formula
+  version "7.200"
+  sha256 "81b2c8f0148d6acf62b59f78869ad7eff28adcd9d19cddec4777d482df5dcd71"
+  url "https://github.com/JulietaUla/Montserrat/archive/v#{version}.zip"
+  desc "Montserrat"
+  homepage "https://github.com/JulietaUla/Montserrat"
+  def install
+    
+  test do
+  end
+end
