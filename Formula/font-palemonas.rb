4a881f790a1dc9ff2b2457c41b809c251a961173
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-palemonas.rb from e0c52ad1cdfdf9a218652c2a6e20c15bd18b5f34

---
+++ Formula/font-palemonas.rb
@@ -0,0 +1,11 @@
+class FontPalemonas < Formula
+  version "3.1"
+  sha256 "342dbc5c62787c0a966a43f9da4dbc06171d1dc72925f8a0f355441a8b9ca2b6"
+  url "http://www.vlkk.lt/media/public/file/Palemonas/Palemonas-#{version}.zip"
+  desc "Palemonas"
+  homepage "http://www.vlkk.lt/palemonas"
+  def install
+    
+  test do
+  end
+end
