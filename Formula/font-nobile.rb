306784034d1391b5c16aee34a6cfdaca4a9efcb2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nobile.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-nobile.rb
@@ -0,0 +1,10 @@
+class FontNobile < Formula
+  head "https://github.com/google/fonts/trunk/ofl/nobile", using: :svn, revision: "50", trust_cert: true
+  desc "Nobile"
+  homepage "https://www.google.com/fonts/specimen/Nobile"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
