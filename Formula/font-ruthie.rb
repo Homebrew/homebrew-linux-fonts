8c9f0f4b215f2c76a4337de4d401030c7adf42fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ruthie.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-ruthie.rb
@@ -0,0 +1,9 @@
+class FontRuthie < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/ruthie/Ruthie-Regular.ttf"
+  desc "Ruthie"
+  homepage "https://www.google.com/fonts/specimen/Ruthie"
+  def install
+    
+  test do
+  end
+end
