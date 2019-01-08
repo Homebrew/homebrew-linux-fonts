7e370e7e79c5ec427be2a0155a65a091bfe14660
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-display.rb from 6612748208449843385714429f09395a19960b12

---
+++ Formula/font-noto-serif-display.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifDisplay < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifDisplay-unhinted.zip"
+  desc "Noto Serif Display"
+  homepage "https://www.google.com/get/noto/#serif-lgc-display"
+  def install
+    
+  test do
+  end
+end
