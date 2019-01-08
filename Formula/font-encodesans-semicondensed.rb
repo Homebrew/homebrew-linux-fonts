73a91e5555708ee722842b8f31fe3ee42b39d870
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-encodesans-semicondensed.rb from 4b855fd673d71fdc6cb6a25766a751f68319e2ec

---
+++ Formula/font-encodesans-semicondensed.rb
@@ -0,0 +1,10 @@
+class FontEncodesansSemicondensed < Formula
+  head "https://github.com/google/fonts/trunk/ofl/encodesanssemicondensed", using: :svn, trust_cert: true
+  desc "Encode Sans Semi Condensed"
+  homepage "https://fonts.google.com/specimen/Encode+Sans+Semi+Condensed"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
