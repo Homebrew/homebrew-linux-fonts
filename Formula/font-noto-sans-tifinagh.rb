bc6bec2a72b62ddac3570d4ac4b802fb11ccd495
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tifinagh.rb from 45cd631147cee78c9cf83f81cc0be2270a59d7eb

---
+++ Formula/font-noto-sans-tifinagh.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTifinagh < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTifinagh-unhinted.zip"
+  desc "Noto Sans Tifinagh"
+  homepage "https://www.google.com/get/noto/#sans-tfng"
+  def install
+    
+  test do
+  end
+end
