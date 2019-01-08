ef6c99e2c23716050fefe8b4aed384d222300e87
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-paytone-one.rb from 3a390793d35509f158240c775f3dfaa05900770a

---
+++ Formula/font-paytone-one.rb
@@ -0,0 +1,9 @@
+class FontPaytoneOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/paytoneone/PaytoneOne-Regular.ttf"
+  desc "Paytone One"
+  homepage "https://www.google.com/fonts/specimen/Paytone+One"
+  def install
+    
+  test do
+  end
+end
