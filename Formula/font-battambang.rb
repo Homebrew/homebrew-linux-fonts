a30a5a63ddb98c04dec608b144c89767a0ade8cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-battambang.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-battambang.rb
@@ -0,0 +1,10 @@
+class FontBattambang < Formula
+  head "https://github.com/google/fonts/trunk/ofl/battambang", using: :svn, revision: "50", trust_cert: true
+  desc "Battambang"
+  homepage "https://www.google.com/fonts/specimen/Battambang"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
