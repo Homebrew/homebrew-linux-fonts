dd9195fa4a43a49ff47c20894f8a85769d6570dd
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pacifico.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-pacifico.rb
@@ -0,0 +1,9 @@
+class FontPacifico < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/pacifico/Pacifico-Regular.ttf"
+  desc "Pacifico"
+  homepage "https://www.google.com/fonts/specimen/Pacifico"
+  def install
+    
+  test do
+  end
+end
