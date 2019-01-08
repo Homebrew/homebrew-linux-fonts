2a104028669cb015a119b695f890939c0654de5d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu-mono-derivative-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-ubuntu-mono-derivative-powerline.rb
@@ -0,0 +1,10 @@
+class FontUbuntuMonoDerivativePowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/UbuntuMono", using: :svn, revision: "53", trust_cert: true
+  desc "Ubuntu Mono derivative Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/UbuntuMono"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
