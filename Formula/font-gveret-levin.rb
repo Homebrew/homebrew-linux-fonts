3db3927c40f3da65b0567bb698922fd05de98cee
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gveret-levin.rb from 3edf156cc8535d8350841c428f99ba68ae5f9318

---
+++ Formula/font-gveret-levin.rb
@@ -0,0 +1,9 @@
+class FontGveretLevin < Formula
+  head "https://alefalefalef.co.il/wp-content/fonts/free/GveretLevinAlefAlefAlef.zip"
+  desc "Gveret Levin"
+  homepage "https://alefalefalef.co.il/resources/פונטים-בחינם/"
+  def install
+    
+  test do
+  end
+end
