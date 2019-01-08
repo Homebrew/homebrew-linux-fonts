1ad4f9420fc2ed30a0dc1dc2c353ce9402d1d184
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-great-primer.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-im-fell-great-primer.rb
@@ -0,0 +1,10 @@
+class FontImFellGreatPrimer < Formula
+  head "https://github.com/google/fonts/trunk/ofl/imfellgreatprimer", using: :svn, revision: "50", trust_cert: true
+  desc "IM Fell Great Primer"
+  homepage "https://www.google.com/fonts/specimen/IM%20Fell%20Great%20Primer"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
