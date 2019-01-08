8baa14e1e3373e36c4053e1df20cd534a9aa7e50
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kayases.rb from d7166b29e63746b3227548f1ec8a41c2d96898d9

---
+++ Formula/font-kayases.rb
@@ -0,0 +1,9 @@
+class FontKayases < Formula
+  head "http://www.languagegeek.com/font/Kayases.zip"
+  desc "Kayases"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
