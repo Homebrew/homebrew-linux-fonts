8f7f946c0b65294e1bb7d8f8a856af5491d9f905
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aldrich.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-aldrich.rb
@@ -0,0 +1,9 @@
+class FontAldrich < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/aldrich/Aldrich-Regular.ttf"
+  desc "Aldrich"
+  homepage "https://www.google.com/fonts/specimen/Aldrich"
+  def install
+    
+  test do
+  end
+end
