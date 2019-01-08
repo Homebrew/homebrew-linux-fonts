b41e47f105a8315d5c6578b65c22ee0060aa1d82
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arimo-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-arimo-nerd-font-mono.rb
+++ Formula/font-arimo-nerd-font-mono.rb
@@ -1,7 +1,7 @@
 class FontArimoNerdFontMono < Formula
   version "2.0.0"
   sha256 "5b23a4607604cfecc7a4551fab86df86acd49c5ce8dfb93d51d195e6b7f0a09e"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Arimo.zip"
+  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Arimo.zip"
   desc "Arimo Nerd Font Mono (Arimo)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
