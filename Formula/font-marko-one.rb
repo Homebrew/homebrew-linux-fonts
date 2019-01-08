e90f0b476e5d4b7480ed2b359b838c70b3a6cf41
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marko-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-marko-one.rb
@@ -0,0 +1,9 @@
+class FontMarkoOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/markoone/MarkoOne-Regular.ttf"
+  desc "Marko One"
+  homepage "https://www.google.com/fonts/specimen/Marko+One"
+  def install
+    
+  test do
+  end
+end
