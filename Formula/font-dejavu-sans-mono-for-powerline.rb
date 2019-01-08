84cd51b981da7c764625f8b88078f55be0949f84
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dejavu-sans-mono-for-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-dejavu-sans-mono-for-powerline.rb
@@ -0,0 +1,10 @@
+class FontDejavuSansMonoForPowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/DejaVuSansMono", using: :svn, trust_cert: true
+  desc "DejaVu Sans Mono for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/DejaVuSansMono"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
