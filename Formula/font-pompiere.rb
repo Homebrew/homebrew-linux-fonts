94bd01573b70e356f843b242471e8605506f935a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pompiere.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-pompiere.rb
@@ -0,0 +1,9 @@
+class FontPompiere < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/pompiere/Pompiere-Regular.ttf"
+  desc "Pompiere"
+  homepage "https://www.google.com/fonts/specimen/Pompiere"
+  def install
+    
+  test do
+  end
+end
