3c6fbd28dc67557c7d2c8aadc57eff1f0c0e7804
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-saira.rb from 577374024b27a8115cd45f677d37b660bc665722

---
+++ Formula/font-saira.rb
@@ -0,0 +1,10 @@
+class FontSaira < Formula
+  head "https://github.com/google/fonts/trunk/ofl/saira", using: :svn, trust_cert: true
+  desc "Saira"
+  homepage "https://fonts.google.com/specimen/Saira"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
