7b3bb5299a535fe86646ee1473a1c3c4baaf0060
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-modern-antiqua.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-modern-antiqua.rb
@@ -0,0 +1,9 @@
+class FontModernAntiqua < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/modernantiqua/ModernAntiqua-Regular.ttf"
+  desc "Modern Antiqua"
+  homepage "https://www.google.com/fonts/specimen/Modern+Antiqua"
+  def install
+    
+  test do
+  end
+end
