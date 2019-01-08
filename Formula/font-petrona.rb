6c7446405d8458ca7c04c09d5b08e765be0011fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-petrona.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-petrona.rb
@@ -0,0 +1,9 @@
+class FontPetrona < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/petrona/Petrona-Regular.ttf"
+  desc "Petrona"
+  homepage "https://www.google.com/fonts/specimen/Petrona"
+  def install
+    
+  test do
+  end
+end
