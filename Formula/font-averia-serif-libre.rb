715adb6ba0b074b950b91595c7c0e3e32daf5633
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-averia-serif-libre.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-averia-serif-libre.rb
@@ -0,0 +1,10 @@
+class FontAveriaSerifLibre < Formula
+  head "https://github.com/google/fonts/trunk/ofl/averiaseriflibre", using: :svn, revision: "50", trust_cert: true
+  desc "Averia Serif Libre"
+  homepage "https://www.google.com/fonts/specimen/Averia%20Serif%20Libre"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
