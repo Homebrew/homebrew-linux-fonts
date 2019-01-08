ee4b8120fe23a57c67ddb1e50dd122c4f31a230f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gafata.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-gafata.rb
@@ -0,0 +1,9 @@
+class FontGafata < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/gafata/Gafata-Regular.ttf"
+  desc "Gafata"
+  homepage "https://www.google.com/fonts/specimen/Gafata"
+  def install
+    
+  test do
+  end
+end
