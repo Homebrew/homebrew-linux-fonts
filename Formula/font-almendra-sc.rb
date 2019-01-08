4dd031639bddba81885d9d346eca8ad0df8915c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-almendra-sc.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-almendra-sc.rb
@@ -0,0 +1,10 @@
+class FontAlmendraSc < Formula
+  head "https://github.com/google/fonts/trunk/ofl/almendrasc", using: :svn, revision: "50", trust_cert: true
+  desc "Almendra SC"
+  homepage "https://www.google.com/fonts/specimen/Almendra%20SC"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
