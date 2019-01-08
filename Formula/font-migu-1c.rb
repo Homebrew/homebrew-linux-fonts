a904fb60f2ebf543ed3fc2466c0e9a069cc56fc8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:29 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-migu-1c.rb from cb406b6bd3096ad5c16e4e1c1edcc3badf75a4a5

---
+++ Formula/font-migu-1c.rb
@@ -0,0 +1,11 @@
+class FontMigu1c < Formula
+  version "20150712"
+  sha256 "62aba11af4e5343b5437c866e3747366d084b63885539c92636222d2978999f1"
+  url "http://dl.osdn.jp/mix-mplus-ipa/63545/migu-1c-#{version}.zip"
+  desc "Migu 1C"
+  homepage "http://mix-mplus-ipa.osdn.jp/migu/#migu1c"
+  def install
+    
+  test do
+  end
+end
