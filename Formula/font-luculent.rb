a5ae569e21dbe78b0229a9d0e3f871d70d45ee60
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-luculent.rb from 45816c0b352beecb6270abd109a863436931bd12

---
+++ Formula/font-luculent.rb
@@ -0,0 +1,9 @@
+class FontLuculent < Formula
+  head "http://eastfarthing.com/luculent/luculent.tar.xz"
+  desc "Luculent"
+  homepage "http://eastfarthing.com/luculent/"
+  def install
+    
+  test do
+  end
+end
