2daa8c6717401c11d8eb776c14d410d8b2ccc0b8
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:53 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fjalla-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

--- Formula/font-fjalla-one.rb
+++ Formula/font-fjalla-one.rb
@@ -1,7 +1,7 @@
 class FontFjallaOne < Formula
   version "1.001"
   sha256 "2c4a287ae4d7e6dbf6e45b6264612d02130147dcd5434cf168a1ba83055225bf"
-  head "https://github.com/google/fonts/raw/a30ff88e1a02d70aaf2589dfc3cef18a4128c495/ofl/fjallaone/FjallaOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/a30ff88e1a02d70aaf2589dfc3cef18a4128c495/ofl/fjallaone/FjallaOne-Regular.ttf"
   desc "Fjalla One"
   homepage "https://www.google.com/fonts/specimen/Fjalla%20One"
   def install
