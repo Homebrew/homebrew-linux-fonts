542cc6f91c4482db8f2868163c8137201364829c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-glober.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-glober.rb
@@ -0,0 +1,9 @@
+class FontGlober < Formula
+  head "https://fontfabric.com/downfont/glober.zip"
+  desc "Gobler"
+  homepage "https://fontfabric.com/glober-free-font/"
+  def install
+    
+  test do
+  end
+end
