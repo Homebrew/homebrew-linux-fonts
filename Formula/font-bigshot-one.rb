8a4b4a858766cd28ed2818fa038b4f806e76df3a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bigshot-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bigshot-one.rb
@@ -0,0 +1,9 @@
+class FontBigshotOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/bigshotone/BigshotOne-Regular.ttf"
+  desc "Bigshot One"
+  homepage "https://www.google.com/fonts/specimen/Bigshot+One"
+  def install
+    
+  test do
+  end
+end
