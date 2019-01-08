4c42d9b5b4536851dcf1b540b465b79aa5b3b7ab
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sarina.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sarina.rb
@@ -0,0 +1,9 @@
+class FontSarina < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sarina/Sarina-Regular.ttf"
+  desc "Sarina"
+  homepage "https://www.google.com/fonts/specimen/Sarina"
+  def install
+    
+  test do
+  end
+end
