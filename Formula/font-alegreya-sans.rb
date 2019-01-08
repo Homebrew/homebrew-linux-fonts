1f0dfccf056cc6483ada408977cd9c243ed34517
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alegreya-sans.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-alegreya-sans.rb
+++ Formula/font-alegreya-sans.rb
@@ -1,7 +1,7 @@
 class FontAlegreyaSans < Formula
   version "2.008"
   sha256 "ff8d1290eae47df0d1f710083690aa7f46508acfb2f1d6bce04b1355ac56dbab"
-  head "https://github.com/huertatipografica/Alegreya-Sans/archive/v#{version}.zip"
+  url "https://github.com/huertatipografica/Alegreya-Sans/archive/v#{version}.zip"
   desc "Alegreya-Sans"
   homepage "https://github.com/huertatipografica/Alegreya-Sans"
   def install
