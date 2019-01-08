0fa57a226745dc1d9e78e0fcecfb0a7b275af0fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dancing-script.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-dancing-script.rb
@@ -0,0 +1,10 @@
+class FontDancingScript < Formula
+  head "https://github.com/google/fonts/trunk/ofl/dancingscript", using: :svn, revision: "50", trust_cert: true
+  desc "Dancing Script"
+  homepage "https://www.google.com/fonts/specimen/Dancing%20Script"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
