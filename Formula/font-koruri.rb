e5b70d0ef5dfe80f6ec161fcf8dd636dc0439220
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-koruri.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-koruri.rb
@@ -0,0 +1,11 @@
+class FontKoruri < Formula
+  version "20161105,66647"
+  sha256 "78e674e1b884189d60e378897179bec5164fa917c76adb53aa7931fc19a40074"
+  url "http://dl.osdn.jp/koruri/#{version.to_s.sub(/.*,/, "")}/Koruri-#{version.to_s.sub(/,.*/, "")}.tar.xz"
+  desc "Koruri"
+  homepage "http://koruri.lindwurm.biz/"
+  def install
+    
+  test do
+  end
+end
