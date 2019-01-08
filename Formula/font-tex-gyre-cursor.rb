07ed1891ca8e2019549398d36b94553dc37b4655
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-cursor.rb from e477038ad1ead3c086fe5e21806c38329799a3bb

---
+++ Formula/font-tex-gyre-cursor.rb
@@ -0,0 +1,11 @@
+class FontTexGyreCursor < Formula
+  version "2.004"
+  sha256 "ae8db1c134ec5c1b8c3999116b0610a5ad1c6f47520c3b4712b9bc914458dce9"
+  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/cursor/qcr#{version}otf.zip"
+  desc "TeX Gyre Cursor"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/cursor"
+  def install
+    
+  test do
+  end
+end
