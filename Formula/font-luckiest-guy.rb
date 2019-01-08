cbf7225d76f9e66f89b4ead16013a9249254c220
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-luckiest-guy.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-luckiest-guy.rb
@@ -0,0 +1,9 @@
+class FontLuckiestGuy < Formula
+  head "https://github.com/google/fonts/raw/master/apache/luckiestguy/LuckiestGuy-Regular.ttf"
+  desc "Luckiest Guy"
+  homepage "https://www.google.com/fonts/specimen/Luckiest+Guy"
+  def install
+    
+  test do
+  end
+end
