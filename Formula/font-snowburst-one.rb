704c4966347255e9b9d2525d5caed4e661495348
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-snowburst-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-snowburst-one.rb
@@ -0,0 +1,9 @@
+class FontSnowburstOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/snowburstone/SnowburstOne-Regular.ttf"
+  desc "Snowburst One"
+  homepage "https://www.google.com/fonts/specimen/Snowburst+One"
+  def install
+    
+  test do
+  end
+end
