1804194ffcdc7e9df84135c72740e959d8cbadff
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-linear-b.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-linear-b.rb
@@ -0,0 +1,9 @@
+class FontNotoSansLinearB < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLinearB-unhinted.zip"
+  desc "Noto Sans Linear B"
+  homepage "https://www.google.com/get/noto/#sans-linb"
+  def install
+    
+  test do
+  end
+end
