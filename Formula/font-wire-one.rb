533229c13fcbae9b533413a085d4fac8ea3ff270
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wire-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-wire-one.rb
@@ -0,0 +1,9 @@
+class FontWireOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/wireone/WireOne.ttf"
+  desc "Wire One"
+  homepage "https://www.google.com/fonts/specimen/Wire+One"
+  def install
+    
+  test do
+  end
+end
