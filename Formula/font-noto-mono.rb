376e46c3554396eb5948cf1c1a295c4d9f767dcc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-mono.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-mono.rb
@@ -0,0 +1,9 @@
+class FontNotoMono < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoMono-hinted.zip"
+  desc "Noto Mono"
+  homepage "https://www.google.com/get/noto/#mono-mono"
+  def install
+    
+  test do
+  end
+end
