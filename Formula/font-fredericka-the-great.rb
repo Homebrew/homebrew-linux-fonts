1be140761ee2e5360ef6d27b72f15b60bf718eb9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fredericka-the-great.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-fredericka-the-great.rb
@@ -0,0 +1,9 @@
+class FontFrederickaTheGreat < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/frederickathegreat/FrederickatheGreat-Regular.ttf"
+  desc "Fredericka the Great"
+  homepage "https://www.google.com/fonts/specimen/Fredericka+the+Great"
+  def install
+    
+  test do
+  end
+end
