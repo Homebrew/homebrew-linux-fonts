a55194d482a60216b2b8ac11d53d51ef0d347ebb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-roboto.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-roboto.rb
@@ -0,0 +1,10 @@
+class FontRoboto < Formula
+  head "https://github.com/google/fonts/trunk/apache/roboto", using: :svn, trust_cert: true
+  desc "Roboto"
+  homepage "https://www.google.com/fonts/specimen/Roboto"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
