24c970ab2ed212ccfc917f33f0b4e4b47d48ffe4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-preahvihear.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-preahvihear.rb
@@ -0,0 +1,9 @@
+class FontPreahvihear < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/preahvihear/Preahvihear.ttf"
+  desc "Preahvihear"
+  homepage "https://www.google.com/fonts/specimen/Preahvihear"
+  def install
+    
+  test do
+  end
+end
