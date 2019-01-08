b8f39440471228d3f2b8a1d61fa8904edc69c5d0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-foundation-icons.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-foundation-icons.rb
@@ -0,0 +1,9 @@
+class FontFoundationIcons < Formula
+  head "https://zurb.com/playground/uploads/upload/upload/288/foundation-icons.zip"
+  desc "Foundation Icons"
+  homepage "https://zurb.com/playground/foundation-icon-fonts-3"
+  def install
+    
+  test do
+  end
+end
