1e5c05d3be09644a6a89f03d9c65f5cdb9d1e63c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oxygen-mono.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-oxygen-mono.rb
@@ -0,0 +1,11 @@
+class FontOxygenMono < Formula
+  version "0.201"
+  sha256 "dcc671da93f7fc31f2e7a4a7c058c815c1eaec6a3d27792e18edff4d3bf539f0"
+  url "https://github.com/google/fonts/raw/master/ofl/oxygenmono/OxygenMono-Regular.ttf"
+  desc "Oxygen Mono"
+  homepage "https://www.google.com/fonts/specimen/Oxygen%20Mono"
+  def install
+    
+  test do
+  end
+end
