28f6ef1d9fcaa9f12907a200d5625d4a9cf84868
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wonder-unit-sans.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-wonder-unit-sans.rb
@@ -0,0 +1,11 @@
+class FontWonderUnitSans < Formula
+  version "1.0.0"
+  sha256 "4dbc800fafbe9ebdbdd6fd095c52258b30890ee41b75ebbcd491a50a31c0c82e"
+  url "https://github.com/wonderunit/font-wonder-unit/archive/#{version}.zip"
+  desc "Wonder Unit Sans"
+  homepage "https://github.com/wonderunit/font-wonder-unit"
+  def install
+    
+  test do
+  end
+end
