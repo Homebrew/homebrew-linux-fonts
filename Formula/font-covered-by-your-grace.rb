e315b9b4e990d36b67f521ccf720e118bb9dd8ef
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-covered-by-your-grace.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-covered-by-your-grace.rb
@@ -0,0 +1,9 @@
+class FontCoveredByYourGrace < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/coveredbyyourgrace/CoveredByYourGrace.ttf"
+  desc "Covered By Your Grace"
+  homepage "https://www.google.com/fonts/specimen/Covered+By+Your+Grace"
+  def install
+    
+  test do
+  end
+end
