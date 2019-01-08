77ffb90086d70f0d720975b5a7db224b58af55a0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-naskh-arabic.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-naskh-arabic.rb
@@ -0,0 +1,9 @@
+class FontNotoNaskhArabic < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoNaskhArabic-unhinted.zip"
+  desc "Noto Naskh Arabic"
+  homepage "https://www.google.com/get/noto/#naskh-arab"
+  def install
+    
+  test do
+  end
+end
