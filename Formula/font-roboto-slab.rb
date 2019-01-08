84381c18eb7a23d724a5e6dca0ab9a85938b014e
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:59:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-roboto-slab.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-roboto-slab.rb
@@ -0,0 +1,14 @@
+class FontRobotoSlab < Formula
+  head "https://github.com/google/fonts/trunk/apache/robotoslab", using: :svn, trust_cert: true
+  desc "Roboto Slab"
+  homepage "https://www.google.com/fonts/specimen/Roboto%20Slab"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "RobotoSlab-Bold.ttf"
+    (share/"fonts").install "RobotoSlab-Light.ttf"
+    (share/"fonts").install "RobotoSlab-Regular.ttf"
+    (share/"fonts").install "RobotoSlab-Thin.ttf"
+  end
+  test do
+  end
+end
