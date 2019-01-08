277ba18d09e9f352a77d35c2e03770901418ea96
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-merriweather.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-merriweather.rb
@@ -0,0 +1,10 @@
+class FontMerriweather < Formula
+  head "https://github.com/google/fonts/trunk/ofl/merriweather", using: :svn, revision: "1208", trust_cert: true
+  desc "Merriweather"
+  homepage "https://www.google.com/fonts/specimen/Merriweather"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
