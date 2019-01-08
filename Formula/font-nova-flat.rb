0c7681cebc1c6ab2d1f3a28e9d2ce1e19e3cbcb2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nova-flat.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-nova-flat.rb
@@ -0,0 +1,9 @@
+class FontNovaFlat < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/novaflat/NovaFlat.ttf"
+  desc "Nova Flat"
+  homepage "https://www.google.com/fonts/specimen/Nova%20Flat"
+  def install
+    
+  test do
+  end
+end
