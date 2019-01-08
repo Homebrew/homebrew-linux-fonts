d34172837a4404f16c829fab0661ed3805b07733
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-questrial.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-questrial.rb
@@ -0,0 +1,9 @@
+class FontQuestrial < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/questrial/Questrial-Regular.ttf"
+  desc "Questrial"
+  homepage "https://www.google.com/fonts/specimen/Questrial"
+  def install
+    
+  test do
+  end
+end
