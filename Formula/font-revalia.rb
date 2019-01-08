356065467fa893554c9658f7588e96430fb4669f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-revalia.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-revalia.rb
@@ -0,0 +1,9 @@
+class FontRevalia < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/revalia/Revalia-Regular.ttf"
+  desc "Revalia"
+  homepage "https://www.google.com/fonts/specimen/Revalia"
+  def install
+    
+  test do
+  end
+end
