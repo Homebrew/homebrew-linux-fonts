e19be96538e17de9d0dcd32713885f12df83bcdc
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-m-plus.rb from 45816c0b352beecb6270abd109a863436931bd12

--- Formula/font-m-plus.rb
+++ Formula/font-m-plus.rb
@@ -1,7 +1,7 @@
 class FontMPlus < Formula
   version "063"
   sha256 "149a5c97c35624d79ffb3cbbdd56559319085229acaf72b49b56adc5ede0979c"
-  url "http://dl.osdn.jp/mplus-fonts/62344/mplus-TESTFLIGHT-#{version}.tar.xz"
+  head "http://dl.osdn.jp/mplus-fonts/62344/mplus-TESTFLIGHT-#{version}.tar.xz"
   desc "M+ FONTS"
   homepage "https://mplus-fonts.osdn.jp/about-en.html"
   def install
