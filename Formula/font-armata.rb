624c672411d43315227b3a51ae28a31cc71a5de0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-armata.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-armata.rb
@@ -0,0 +1,9 @@
+class FontArmata < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/armata/Armata-Regular.ttf"
+  desc "Armata"
+  homepage "https://www.google.com/fonts/specimen/Armata"
+  def install
+    
+  test do
+  end
+end
