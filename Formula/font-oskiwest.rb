07d2970c12a68b40d8b0df5e95aebeb37190fb2e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oskiwest.rb from 231246d8926c56f3cf93e965dcb505ff9ac22dba

---
+++ Formula/font-oskiwest.rb
@@ -0,0 +1,9 @@
+class FontOskiwest < Formula
+  head "http://www.languagegeek.com/font/oskiwest.zip"
+  desc "OskiWest"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
