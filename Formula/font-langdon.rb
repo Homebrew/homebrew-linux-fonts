213e74d54fe55ed88f8f40c98493ed238c7135b0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-langdon.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-langdon.rb
@@ -0,0 +1,9 @@
+class FontLangdon < Formula
+  head "https://www.ffonts.net/Langdon.font.zip"
+  desc "Langdon"
+  homepage "https://www.ffonts.net/Langdon.font"
+  def install
+    
+  test do
+  end
+end
