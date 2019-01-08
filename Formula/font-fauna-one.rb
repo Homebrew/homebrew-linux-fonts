24d6262e49c98b2f2a7f9400308ce3f1321a9450
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fauna-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-fauna-one.rb
@@ -0,0 +1,9 @@
+class FontFaunaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/faunaone/FaunaOne-Regular.ttf"
+  desc "Fauna One"
+  homepage "https://www.google.com/fonts/specimen/Fauna+One"
+  def install
+    
+  test do
+  end
+end
