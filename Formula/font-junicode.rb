05a9fbf331e0648f3ea1ed82c7006859fcbf25b7
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-junicode.rb from 45ec98f49c391f0091cfa20efd5ce7ada84c13b3

--- Formula/font-junicode.rb
+++ Formula/font-junicode.rb
@@ -1,7 +1,7 @@
 class FontJunicode < Formula
   version "1.002"
   sha256 "c199d96c8424be60fcab8d00d2eee39ea8ae632cfd5e710cbbd70626d6a729e7"
-  head "https://downloads.sourceforge.net/junicode/junicode-#{version}.zip"
+  url "https://downloads.sourceforge.net/junicode/junicode-#{version}.zip"
   desc "Junicode"
   homepage "http://junicode.sourceforge.net/"
   def install
