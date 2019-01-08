4867454dfb78d5ed9dc66bc3251b83dc28d5556e
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:57:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-montserrat-subrayada.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-montserrat-subrayada.rb
@@ -0,0 +1,12 @@
+class FontMontserratSubrayada < Formula
+  head "https://github.com/google/fonts/trunk/ofl/montserratsubrayada", using: :svn, revision: "50", trust_cert: true
+  desc "Montserrat Subrayada"
+  homepage "https://www.google.com/fonts/specimen/Montserrat%20Subrayada"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "MontserratSubrayada-Bold.ttf"
+    (share/"fonts").install "MontserratSubrayada-Regular.ttf"
+  end
+  test do
+  end
+end
