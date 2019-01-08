368ae6f2087b89d2f551eae0c50491e638bdfaf8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rajdhani.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-rajdhani.rb
@@ -0,0 +1,11 @@
+class FontRajdhani < Formula
+  version "2.000"
+  sha256 "2dd0b14ed9989319fde8518e4b596467ae635571ffe7733d7968201d8cdeb9cf"
+  url "https://github.com/itfoundry/rajdhani/releases/download/v#{version}/rajdhani-#{version.to_s.gsub(".", "_")}.zip"
+  desc "Rajdhani"
+  homepage "https://github.com/itfoundry/rajdhani"
+  def install
+    
+  test do
+  end
+end
