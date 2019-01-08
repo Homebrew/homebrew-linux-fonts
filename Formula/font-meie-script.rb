e5b3e04515cd6d8ebc3b9ed49c952f19fad60613
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-meie-script.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-meie-script.rb
@@ -0,0 +1,9 @@
+class FontMeieScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/meiescript/MeieScript-Regular.ttf"
+  desc "Meie Script"
+  homepage "https://www.google.com/fonts/specimen/Meie+Script"
+  def install
+    
+  test do
+  end
+end
