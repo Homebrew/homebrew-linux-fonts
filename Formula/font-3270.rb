ee9589047027b9f7606efb92522d6df1cf2ac107
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-3270.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-3270.rb
+++ Formula/font-3270.rb
@@ -1,7 +1,7 @@
 class Font3270 < Formula
   version "1.2.23,d250fd9"
   sha256 "9cf7235088b3afe7827b24fec534a569b5cab15c26e0b5b6956cc2af0db837b6"
-  url "https://github.com/rbanffy/3270font/releases/download/v#{version.to_s.sub(/,.*/, "")}/3270_fonts_#{version.to_s.sub(/.*,/, "")}.zip"
+  head "https://github.com/rbanffy/3270font/releases/download/v#{version.before_comma}/3270_fonts_#{version.after_comma}.zip"
   desc "IBM 3270"
   homepage "https://github.com/rbanffy/3270font"
   def install
