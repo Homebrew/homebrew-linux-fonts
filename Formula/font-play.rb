f7472a822346ec29a8a5c31ba4a5d9060e66ed49
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-play.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-play.rb
@@ -0,0 +1,10 @@
+class FontPlay < Formula
+  head "https://github.com/google/fonts/trunk/ofl/play", using: :svn, revision: "50", trust_cert: true
+  desc "Play"
+  homepage "https://www.google.com/fonts/specimen/Play"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
