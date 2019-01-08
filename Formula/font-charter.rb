64bb9e7f893180d4f58485a58778c525b1d08ec2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-charter.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-charter.rb
@@ -0,0 +1,9 @@
+class FontCharter < Formula
+  head "https://practicaltypography.com/fonts/charter.zip"
+  desc "Charter"
+  homepage "https://practicaltypography.com/charter.html"
+  def install
+    
+  test do
+  end
+end
