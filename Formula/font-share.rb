36f2ac432c5ab736cb2edbffabfc76882cf22d6b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:29 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-share.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-share.rb
@@ -0,0 +1,10 @@
+class FontShare < Formula
+  head "https://github.com/google/fonts/trunk/ofl/share", using: :svn, revision: "50", trust_cert: true
+  desc "Share"
+  homepage "https://www.google.com/fonts/specimen/Share"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
