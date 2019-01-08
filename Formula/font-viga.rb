7405b8aa88b6b96862f1e77c6267bdb5f261f789
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-viga.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-viga.rb
@@ -0,0 +1,9 @@
+class FontViga < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/viga/Viga-Regular.ttf"
+  desc "Viga"
+  homepage "https://www.google.com/fonts/specimen/Viga"
+  def install
+    
+  test do
+  end
+end
