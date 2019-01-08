a58db304fc8458222ca6e323064e279ccf0e8d32
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fantasquesansmono-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-fantasquesansmono-nerd-font-mono.rb
+++ Formula/font-fantasquesansmono-nerd-font-mono.rb
@@ -1,7 +1,7 @@
 class FontFantasquesansmonoNerdFontMono < Formula
   version "2.0.0"
   sha256 "7274bae9949a6f5100f6c5248c9b9649bd64ba3f9f1e9b9dd53a71f8fdd6d9c0"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FantasqueSansMono.zip"
+  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FantasqueSansMono.zip"
   desc "FantasqueSansMono Nerd Font (FantasqueSansMono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
