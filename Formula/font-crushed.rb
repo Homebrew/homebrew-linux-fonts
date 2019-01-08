ee404c88b69ad4794328d7ba2eaac7b1928ed179
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-crushed.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-crushed.rb
@@ -0,0 +1,9 @@
+class FontCrushed < Formula
+  head "https://github.com/google/fonts/raw/master/apache/crushed/Crushed-Regular.ttf"
+  desc "Crushed"
+  homepage "https://www.google.com/fonts/specimen/Crushed"
+  def install
+    
+  test do
+  end
+end
