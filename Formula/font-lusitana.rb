56f44eb93e64e6656a85334c6c3012e8ff199080
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lusitana.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-lusitana.rb
@@ -0,0 +1,10 @@
+class FontLusitana < Formula
+  head "https://github.com/google/fonts/trunk/ofl/lusitana", using: :svn, revision: "50", trust_cert: true
+  desc "Lusitana"
+  homepage "https://www.google.com/fonts/specimen/Lusitana"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
