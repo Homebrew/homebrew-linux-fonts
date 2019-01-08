6b8e3241b336b730f5b74d1b90053bac32ad79b7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-miltonian-tattoo.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-miltonian-tattoo.rb
@@ -0,0 +1,9 @@
+class FontMiltonianTattoo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/miltoniantattoo/MiltonianTattoo-Regular.ttf"
+  desc "Miltonian Tattoo"
+  homepage "https://www.google.com/fonts/specimen/Miltonian+Tattoo"
+  def install
+    
+  test do
+  end
+end
