8c037c06547367ff374bc40ee29fb05d6619f5cd
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hasklig.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-hasklig.rb
@@ -0,0 +1,11 @@
+class FontHasklig < Formula
+  version "1.1"
+  sha256 "98eccec22bc0ae3838a099a911230f62197e681b6a6c30e5149c52f5fc6a95c3"
+  url "https://github.com/i-tu/Hasklig/releases/download/#{version}/Hasklig-#{version}.zip"
+  desc "Hasklig"
+  homepage "https://github.com/i-tu/Hasklig"
+  def install
+    
+  test do
+  end
+end
