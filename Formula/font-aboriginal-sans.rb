15758f481ce82ce224e72a224442a264a8da967d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aboriginal-sans.rb from bf533b1fff6f7c7fad978dd6d000717114a5f67f

---
+++ Formula/font-aboriginal-sans.rb
@@ -0,0 +1,9 @@
+class FontAboriginalSans < Formula
+  head "http://www.languagegeek.com/font/AboriginalSans.zip"
+  desc "LanguageGeek Aboriginal Sans Serif"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
