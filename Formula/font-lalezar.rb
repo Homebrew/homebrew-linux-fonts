62fdbc2a544c3800d9884333c5b47b0103e8525b
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lalezar.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-lalezar.rb
+++ Formula/font-lalezar.rb
@@ -1,7 +1,7 @@
 class FontLalezar < Formula
   version "1.003"
   sha256 "e2c758bd395b08d5cb440167d35b264d1393c26bab6854eab484672c09f10f42"
-  head "https://github.com/BornaIz/Lalezar/archive/#{version}.zip"
+  url "https://github.com/BornaIz/Lalezar/archive/#{version}.zip"
   desc "Lalezar"
   homepage "https://github.com/BornaIz/Lalezar"
   def install
