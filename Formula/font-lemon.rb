d4b6d145472c0011fa2dbda00315278fc1c8eea9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lemon.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-lemon.rb
@@ -0,0 +1,9 @@
+class FontLemon < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/lemon/Lemon-Regular.ttf"
+  desc "Lemon"
+  homepage "https://www.google.com/fonts/specimen/Lemon"
+  def install
+    
+  test do
+  end
+end
