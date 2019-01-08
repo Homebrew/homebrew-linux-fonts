2a5e194b58ae20a8df5fc0e22450ce2c0befb8ae
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-anonymice-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-anonymice-powerline.rb
@@ -0,0 +1,10 @@
+class FontAnonymicePowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/AnonymousPro", using: :svn, trust_cert: true
+  desc "Anonymice Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/AnonymousPro"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
