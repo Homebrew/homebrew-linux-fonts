ddbca05dda0a8f4b035e4855462add4bb57ad961
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-emilys-candy.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-emilys-candy.rb
@@ -0,0 +1,9 @@
+class FontEmilysCandy < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/emilyscandy/EmilysCandy-Regular.ttf"
+  desc "Emilys Candy"
+  homepage "https://www.google.com/fonts/specimen/Emilys+Candy"
+  def install
+    
+  test do
+  end
+end
