5a94383f5ccf767e04e03c4e60b36c74ebb0154e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-eagle-lake.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-eagle-lake.rb
@@ -0,0 +1,9 @@
+class FontEagleLake < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/eaglelake/EagleLake-Regular.ttf"
+  desc "Eagle Lake"
+  homepage "https://www.google.com/fonts/specimen/Eagle+Lake"
+  def install
+    
+  test do
+  end
+end
