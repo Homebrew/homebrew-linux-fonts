323611135638b7bfe8b0c3f38ab71df14530b445
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dejavu-sans.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-dejavu-sans.rb
@@ -0,0 +1,11 @@
+class FontDejavuSans < Formula
+  version "2.37"
+  sha256 "7576310b219e04159d35ff61dd4a4ec4cdba4f35c00e002a136f00e96a908b0a"
+  url "https://downloads.sourceforge.net/dejavu/dejavu-fonts-ttf-#{version}.zip"
+  desc "DejaVu"
+  homepage "https://sourceforge.net/projects/dejavu/"
+  def install
+    
+  test do
+  end
+end
