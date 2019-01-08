24d8a2ce142ec915c64e3ce48bfa1ae6656e209e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ropa-sans.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-ropa-sans.rb
@@ -0,0 +1,10 @@
+class FontRopaSans < Formula
+  head "https://github.com/google/fonts/trunk/ofl/ropasans", using: :svn, revision: "50", trust_cert: true
+  desc "Ropa Sans"
+  homepage "https://www.google.com/fonts/specimen/Ropa%20Sans"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
