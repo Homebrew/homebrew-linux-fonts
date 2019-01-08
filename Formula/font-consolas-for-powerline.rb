76cf917f6e7c8ee714bddfc1bfca88b0d601a94b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-consolas-for-powerline.rb from ba8fe2e21719f602a04121d0a0b1aca06e581b9d

---
+++ Formula/font-consolas-for-powerline.rb
@@ -0,0 +1,9 @@
+class FontConsolasForPowerline < Formula
+  head "https://github.com/eugeii/consolas-powerline-vim/archive/master.zip"
+  desc "Consolas for Powerline"
+  homepage "https://github.com/eugeii/consolas-powerline-vim"
+  def install
+    
+  test do
+  end
+end
