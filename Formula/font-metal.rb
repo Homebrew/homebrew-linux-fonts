72a4384ceac010cb16f0c14e4d03552bdeef5fae
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-metal.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-metal.rb
@@ -0,0 +1,9 @@
+class FontMetal < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/metal/Metal.ttf"
+  desc "Metal"
+  homepage "https://www.google.com/fonts/specimen/Metal"
+  def install
+    
+  test do
+  end
+end
