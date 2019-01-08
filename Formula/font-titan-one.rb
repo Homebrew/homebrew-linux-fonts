40cfd6cdd1ff791bc3750bb846c6061001b87b15
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:28 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-titan-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-titan-one.rb
@@ -0,0 +1,9 @@
+class FontTitanOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/titanone/TitanOne-Regular.ttf"
+  desc "Titan One"
+  homepage "https://www.google.com/fonts/specimen/Titan+One"
+  def install
+    
+  test do
+  end
+end
