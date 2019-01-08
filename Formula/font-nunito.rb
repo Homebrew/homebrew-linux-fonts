c604fc15bd4772243ae8cc53c19c02027d81cf62
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nunito.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-nunito.rb
@@ -0,0 +1,10 @@
+class FontNunito < Formula
+  head "https://github.com/google/fonts/trunk/ofl/nunito", using: :svn, revision: "50", trust_cert: true
+  desc "Nunito"
+  homepage "https://www.google.com/fonts/specimen/Nunito"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
