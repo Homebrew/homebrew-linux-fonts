f54c4ccb7de15a44c0a2f8fc5acb499b9ac66186
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-batak.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-sans-batak.rb
@@ -0,0 +1,9 @@
+class FontNotoSansBatak < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBatak-unhinted.zip"
+  desc "Noto Sans Batak"
+  homepage "https://www.google.com/get/noto/#sans-batk"
+  def install
+    
+  test do
+  end
+end
