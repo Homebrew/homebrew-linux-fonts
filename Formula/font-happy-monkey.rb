cebfbbe1d95958795f19c8f34e30f073bd24b69b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-happy-monkey.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-happy-monkey.rb
@@ -0,0 +1,9 @@
+class FontHappyMonkey < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/happymonkey/HappyMonkey-Regular.ttf"
+  desc "Happy Monkey"
+  homepage "https://www.google.com/fonts/specimen/Happy+Monkey"
+  def install
+    
+  test do
+  end
+end
