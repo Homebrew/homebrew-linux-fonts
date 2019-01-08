742066bf4a8b496b606d81ce8afaa18c115a0ed4
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:56:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-josefin-sans.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-josefin-sans.rb
@@ -0,0 +1,20 @@
+class FontJosefinSans < Formula
+  head "https://github.com/google/fonts/trunk/ofl/josefinsans", using: :svn, revision: "50", trust_cert: true
+  desc "Josefin Sans"
+  homepage "https://www.google.com/fonts/specimen/Josefin%20Sans"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "JosefinSans-Bold.ttf"
+    (share/"fonts").install "JosefinSans-BoldItalic.ttf"
+    (share/"fonts").install "JosefinSans-Italic.ttf"
+    (share/"fonts").install "JosefinSans-Light.ttf"
+    (share/"fonts").install "JosefinSans-LightItalic.ttf"
+    (share/"fonts").install "JosefinSans-Regular.ttf"
+    (share/"fonts").install "JosefinSans-SemiBold.ttf"
+    (share/"fonts").install "JosefinSans-SemiBoldItalic.ttf"
+    (share/"fonts").install "JosefinSans-Thin.ttf"
+    (share/"fonts").install "JosefinSans-ThinItalic.ttf"
+  end
+  test do
+  end
+end
