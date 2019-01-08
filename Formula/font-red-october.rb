3af27cd221fc0e2b9fba38d38f1a3f4511b00eb1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-red-october.rb from dad5e642c86b1c936b14d420f3d9ef02c8156cde

---
+++ Formula/font-red-october.rb
@@ -0,0 +1,9 @@
+class FontRedOctober < Formula
+  head "https://dl.dafont.com/dl/?f=red_october"
+  desc "Red October"
+  homepage "https://www.dafont.com/red-october.font"
+  def install
+    
+  test do
+  end
+end
