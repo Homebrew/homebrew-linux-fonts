c102152608d3fff2dc651ba45e55e85cde5a7b97
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-georgian.rb from 45cd631147cee78c9cf83f81cc0be2270a59d7eb

---
+++ Formula/font-noto-serif-georgian.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifGeorgian < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifGeorgian-unhinted.zip"
+  desc "Noto Serif Georgian"
+  homepage "https://www.google.com/get/noto/#serif-geor"
+  def install
+    
+  test do
+  end
+end
