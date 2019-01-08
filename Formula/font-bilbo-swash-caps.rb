fd269953e02c8e3ea939136919fec6ed7a9757e7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bilbo-swash-caps.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bilbo-swash-caps.rb
@@ -0,0 +1,9 @@
+class FontBilboSwashCaps < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/bilboswashcaps/BilboSwashCaps-Regular.ttf"
+  desc "Bilbo Swash Caps"
+  homepage "https://www.google.com/fonts/specimen/Bilbo+Swash+Caps"
+  def install
+    
+  test do
+  end
+end
