c0cfe7f098d6fd229532292375aa2e0db807f359
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fantasque-sans-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-fantasque-sans-mono.rb
@@ -0,0 +1,11 @@
+class FontFantasqueSansMono < Formula
+  version "1.7.2"
+  sha256 "f3c712d02b3f1f78a2ba1e5be95f1366e75f910b22b7b9242449b2bd43d1f194"
+  url "https://github.com/belluzj/fantasque-sans/releases/download/v#{version}/FantasqueSansMono-Normal.zip"
+  desc "Fantasque Sans Mono"
+  homepage "https://github.com/belluzj/fantasque-sans"
+  def install
+    
+  test do
+  end
+end
