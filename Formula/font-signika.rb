e30635fbbbb714830567d5fee0d9dda6cea45fac
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-signika.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-signika.rb
@@ -0,0 +1,9 @@
+class FontSignika < Formula
+  head "https://fontfabric.com/downfont/signika.zip"
+  desc "Signika"
+  homepage "https://fontfabric.com/signika-font/"
+  def install
+    
+  test do
+  end
+end
