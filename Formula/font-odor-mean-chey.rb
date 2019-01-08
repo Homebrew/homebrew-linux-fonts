1c8a4d5de0ab74403b0bb4d53cf793a7410af412
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-odor-mean-chey.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-odor-mean-chey.rb
@@ -0,0 +1,9 @@
+class FontOdorMeanChey < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/odormeanchey/OdorMeanChey.ttf"
+  desc "Odor Mean Chey"
+  homepage "https://www.google.com/fonts/specimen/Odor+Mean+Chey"
+  def install
+    
+  test do
+  end
+end
