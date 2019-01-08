3d2924c91d49e6d6aa1ad55c18bfa6f65871d694
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aubrey.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-aubrey.rb
@@ -0,0 +1,9 @@
+class FontAubrey < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/aubrey/Aubrey-Regular.ttf"
+  desc "Aubrey"
+  homepage "https://www.google.com/fonts/specimen/Aubrey"
+  def install
+    
+  test do
+  end
+end
