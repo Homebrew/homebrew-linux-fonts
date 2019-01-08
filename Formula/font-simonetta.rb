c6d4eca255674f8334fb39ef7f5ce0f316ac13d4
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:59:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-simonetta.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-simonetta.rb
@@ -0,0 +1,14 @@
+class FontSimonetta < Formula
+  head "https://github.com/google/fonts/trunk/ofl/simonetta", using: :svn, revision: "50", trust_cert: true
+  desc "Simonetta"
+  homepage "https://www.google.com/fonts/specimen/Simonetta"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "Simonetta-Black.ttf"
+    (share/"fonts").install "Simonetta-BlackItalic.ttf"
+    (share/"fonts").install "Simonetta-Italic.ttf"
+    (share/"fonts").install "Simonetta-Regular.ttf"
+  end
+  test do
+  end
+end
