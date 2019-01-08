748485ec26bd9315d76096d5fed67667aa16e403
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-siemreap.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-siemreap.rb
@@ -0,0 +1,9 @@
+class FontSiemreap < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/siemreap/Siemreap.ttf"
+  desc "Siemreap"
+  homepage "https://www.google.com/fonts/specimen/Siemreap"
+  def install
+    
+  test do
+  end
+end
