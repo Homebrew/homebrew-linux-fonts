67da7eac6f4b161bfb483fdc02bd478527376263
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-waiting-for-the-sunrise.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-waiting-for-the-sunrise.rb
@@ -0,0 +1,9 @@
+class FontWaitingForTheSunrise < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/waitingforthesunrise/WaitingfortheSunrise.ttf"
+  desc "Waiting for the Sunrise"
+  homepage "https://www.google.com/fonts/specimen/Waiting+for+the+Sunrise"
+  def install
+    
+  test do
+  end
+end
