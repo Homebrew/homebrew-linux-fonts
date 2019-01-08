1bd9c8501183a6ed9cd48448f5e67a18be342cda
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-voltaire.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-voltaire.rb
@@ -0,0 +1,9 @@
+class FontVoltaire < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/voltaire/Voltaire-Regular.ttf"
+  desc "Voltaire"
+  homepage "https://www.google.com/fonts/specimen/Voltaire"
+  def install
+    
+  test do
+  end
+end
