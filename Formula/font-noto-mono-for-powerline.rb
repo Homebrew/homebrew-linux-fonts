1558055199b9eb4378742c1377a411857f57cd88
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:57:32 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-mono-for-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-noto-mono-for-powerline.rb
@@ -0,0 +1,11 @@
+class FontNotoMonoForPowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/NotoMono", using: :svn, trust_cert: true
+  desc "Noto Mono for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/NotoMono"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "Noto Mono for Powerline.ttf"
+  end
+  test do
+  end
+end
