78acd3e25d355e90e0724f279b39986a394be8ed
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-english.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-im-fell-english.rb
@@ -0,0 +1,10 @@
+class FontImFellEnglish < Formula
+  head "https://github.com/google/fonts/trunk/ofl/imfellenglish", using: :svn, revision: "50", trust_cert: true
+  desc "IM Fell English"
+  homepage "https://www.google.com/fonts/specimen/IM%20Fell%20English"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
