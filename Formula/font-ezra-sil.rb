90c5b84efa2368b81fd1149774d1c3dea0dac18f
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ezra-sil.rb from f023c758ae521f3dab57518fc4dc160e7856b114

--- Formula/font-ezra-sil.rb
+++ Formula/font-ezra-sil.rb
@@ -1,7 +1,7 @@
 class FontEzraSil < Formula
   version "2.51"
   sha256 "fd2894531aab32ff7d6845e3d0b4c0401c91f64ba637d13624dfc17a3e815831"
-  url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=EzraSIL#{version.to_s.gsub(".", "")}.zip&filename=EzraSIL#{version.to_s.gsub(".", "")}.zip"
+  head "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=EzraSIL#{version.no_dots}.zip&filename=EzraSIL#{version.no_dots}.zip"
   desc "Ezra SIL"
   homepage "https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=EzraSIL_Home"
   def install
