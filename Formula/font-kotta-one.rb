aa0920c29fbe886a8627aac73f4669da3e950d74
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kotta-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-kotta-one.rb
@@ -0,0 +1,9 @@
+class FontKottaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kottaone/KottaOne-Regular.ttf"
+  desc "Kotta One"
+  homepage "https://www.google.com/fonts/specimen/Kotta+One"
+  def install
+    
+  test do
+  end
+end
