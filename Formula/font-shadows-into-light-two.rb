9a224ad701a42f7a2baec63974babae324d960e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-shadows-into-light-two.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-shadows-into-light-two.rb
@@ -0,0 +1,9 @@
+class FontShadowsIntoLightTwo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/shadowsintolighttwo/ShadowsIntoLightTwo-Regular.ttf"
+  desc "Shadows Into Light Two"
+  homepage "https://www.google.com/fonts/specimen/Shadows+Into+Light+Two"
+  def install
+    
+  test do
+  end
+end
