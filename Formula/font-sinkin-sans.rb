71de8f727d40845677e2e11750f04e7affd4ed58
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sinkin-sans.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-sinkin-sans.rb
@@ -0,0 +1,9 @@
+class FontSinkinSans < Formula
+  head "https://www.fontsquirrel.com/fonts/download/sinkin-sans"
+  desc "Sinkin Sans"
+  homepage "https://www.fontsquirrel.com/fonts/sinkin-sans"
+  def install
+    
+  test do
+  end
+end
