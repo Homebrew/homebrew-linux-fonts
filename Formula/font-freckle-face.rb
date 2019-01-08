3ecc9bef8156450c7908c7542fcf9733c7b82385
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-freckle-face.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-freckle-face.rb
@@ -0,0 +1,9 @@
+class FontFreckleFace < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/freckleface/FreckleFace-Regular.ttf"
+  desc "Freckle Face"
+  homepage "https://www.google.com/fonts/specimen/Freckle+Face"
+  def install
+    
+  test do
+  end
+end
