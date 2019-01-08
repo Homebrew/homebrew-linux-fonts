66ad912d9868b264aba7be27fc239722141b701a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-croissant-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-croissant-one.rb
@@ -0,0 +1,9 @@
+class FontCroissantOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/croissantone/CroissantOne-Regular.ttf"
+  desc "Croissant One"
+  homepage "https://www.google.com/fonts/specimen/Croissant+One"
+  def install
+    
+  test do
+  end
+end
