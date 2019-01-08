bcbb71ff92339ebb8f4b8f28a8cface1389857f3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-bonum-math.rb from ae069a537e694d476b567118c3c6c390bf25b74c

---
+++ Formula/font-tex-gyre-bonum-math.rb
@@ -0,0 +1,11 @@
+class FontTexGyreBonumMath < Formula
+  version "1.005"
+  sha256 "8f8dc6f52ff838201f581f20b4ab634508e6d4b1e2745fe5d6b7732e1df73290"
+  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrebonum-math-1005.zip"
+  desc "Bonum Math"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tg-math"
+  def install
+    
+  test do
+  end
+end
