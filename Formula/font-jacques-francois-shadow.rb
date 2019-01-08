7aca54e5addbe6784e69e75242fe4fa0b106c880
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jacques-francois-shadow.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-jacques-francois-shadow.rb
@@ -0,0 +1,9 @@
+class FontJacquesFrancoisShadow < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/jacquesfrancoisshadow/JacquesFrancoisShadow-Regular.ttf"
+  desc "Jacques Francois Shadow"
+  homepage "https://www.google.com/fonts/specimen/Jacques+Francois+Shadow"
+  def install
+    
+  test do
+  end
+end
