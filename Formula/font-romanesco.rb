290f8290a912e6badc99c5077904eef08c85d593
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-romanesco.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-romanesco.rb
@@ -0,0 +1,9 @@
+class FontRomanesco < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/romanesco/Romanesco-Regular.ttf"
+  desc "Romanesco"
+  homepage "https://www.google.com/fonts/specimen/Romanesco"
+  def install
+    
+  test do
+  end
+end
