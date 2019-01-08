0b85121fa6b6848a7e7a3212aba697c1fd5e6aa4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-hebrew.rb from 6612748208449843385714429f09395a19960b12

---
+++ Formula/font-noto-serif-hebrew.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifHebrew < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifHebrew-unhinted.zip"
+  desc "Noto Serif Hebrew"
+  homepage "https://www.google.com/get/noto/#serif-hebr"
+  def install
+    
+  test do
+  end
+end
