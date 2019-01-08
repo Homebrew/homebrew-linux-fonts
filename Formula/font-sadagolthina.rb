aad708d7baf8f09b498f1bf3a12343c04a8daa68
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sadagolthina.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-sadagolthina.rb
@@ -0,0 +1,11 @@
+class FontSadagolthina < Formula
+  version "1.0.2"
+  sha256 "aa8b42ccf0cc74a72bb50ab95a45904e6e032e000e09d0c07d93fbee3870c112"
+  url "https://www.evertype.com/fonts/gothic/sadagolthina.zip"
+  desc "Sadagolthina"
+  homepage "https://www.evertype.com/fonts/gothic/"
+  def install
+    
+  test do
+  end
+end
