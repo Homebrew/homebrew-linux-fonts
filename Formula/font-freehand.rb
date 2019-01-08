cb8a06d3a40a6433a1b81acd68438b0bc7f651a0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-freehand.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-freehand.rb
@@ -0,0 +1,9 @@
+class FontFreehand < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/freehand/Freehand.ttf"
+  desc "Freehand"
+  homepage "https://www.google.com/fonts/specimen/Freehand"
+  def install
+    
+  test do
+  end
+end
