8a0a7f66c68d983df5ddccce206b9355ab8dfcab
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rambla.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-rambla.rb
@@ -0,0 +1,10 @@
+class FontRambla < Formula
+  head "https://github.com/google/fonts/trunk/ofl/rambla", using: :svn, revision: "50", trust_cert: true
+  desc "Rambla"
+  homepage "https://www.google.com/fonts/specimen/Rambla"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
