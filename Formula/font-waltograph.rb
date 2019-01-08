455eb74c928af017f100e312ce2af892ee0f3819
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-waltograph.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-waltograph.rb
@@ -0,0 +1,11 @@
+class FontWaltograph < Formula
+  version "4.2"
+  sha256 "f10b3eb3b084c326847c61f432f7f9b02895f15accade06d3ba8ac90f844c3d9"
+  url "https://mickeyavenue.com/fonts/download/waltograph.zip"
+  desc "Waltograph"
+  homepage "https://mickeyavenue.com/fonts/waltograph/"
+  def install
+    
+  test do
+  end
+end
