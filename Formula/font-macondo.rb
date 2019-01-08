16103d26426dfac277605595acddf0614fa25f71
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-macondo.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-macondo.rb
@@ -0,0 +1,9 @@
+class FontMacondo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/macondo/Macondo-Regular.ttf"
+  desc "Macondo"
+  homepage "https://www.google.com/fonts/specimen/Macondo"
+  def install
+    
+  test do
+  end
+end
