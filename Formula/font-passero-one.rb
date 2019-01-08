974d51529c588502a07d4ef0f7ed66f1d613bcc0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-passero-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-passero-one.rb
@@ -0,0 +1,9 @@
+class FontPasseroOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/passeroone/PasseroOne-Regular.ttf"
+  desc "Passero One"
+  homepage "https://www.google.com/fonts/specimen/Passero+One"
+  def install
+    
+  test do
+  end
+end
