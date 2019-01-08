4aea9ee0828946c893120e7e7a490412f31b97cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-merienda-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-merienda-one.rb
@@ -0,0 +1,9 @@
+class FontMeriendaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/meriendaone/MeriendaOne-Regular.ttf"
+  desc "Merienda One"
+  homepage "https://www.google.com/fonts/specimen/Merienda+One"
+  def install
+    
+  test do
+  end
+end
