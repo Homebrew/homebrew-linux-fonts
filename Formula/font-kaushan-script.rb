8b89f082907913df3ea7ea35d5a62e05698ec5d1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kaushan-script.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-kaushan-script.rb
@@ -0,0 +1,9 @@
+class FontKaushanScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kaushanscript/KaushanScript-Regular.ttf"
+  desc "Kaushan Script"
+  homepage "https://www.google.com/fonts/specimen/Kaushan+Script"
+  def install
+    
+  test do
+  end
+end
