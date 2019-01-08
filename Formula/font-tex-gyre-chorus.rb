1f6222360154c5880015df64fda0b2b1485c4dc8
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:59 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-chorus.rb from 02c394749324b5d01d3f7705c40b7e5ae0877ae6

--- Formula/font-tex-gyre-chorus.rb
+++ Formula/font-tex-gyre-chorus.rb
@@ -1,7 +1,7 @@
 class FontTexGyreChorus < Formula
   version "2.003"
   sha256 "fbd905a504e6f86df9c38b42c6ed58681a10e6debae6afa6308bc031695cdbd8"
-  head "http://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus/qzc#{version}otf.zip"
+  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus/qzc#{version}otf.zip"
   desc "TeX Gyre Chorus"
   homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus"
   def install
