c7a5ec5d70226ffa80d0224b6d224b17540a27ef
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-federant.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-federant.rb
@@ -0,0 +1,9 @@
+class FontFederant < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/federant/Federant-Regular.ttf"
+  desc "Federant"
+  homepage "https://www.google.com/fonts/specimen/Federant"
+  def install
+    
+  test do
+  end
+end
