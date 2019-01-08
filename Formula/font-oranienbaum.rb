202a100567391c7b0db2f5901eacc0e77aea7aa1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oranienbaum.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-oranienbaum.rb
@@ -0,0 +1,9 @@
+class FontOranienbaum < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/oranienbaum/Oranienbaum-Regular.ttf"
+  desc "Oranienbaum"
+  homepage "https://www.google.com/fonts/specimen/Oranienbaum"
+  def install
+    
+  test do
+  end
+end
