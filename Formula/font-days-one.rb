33b607bae3e91e633a4bb2ad45d5de447800a7ff
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-days-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-days-one.rb
@@ -0,0 +1,9 @@
+class FontDaysOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/daysone/DaysOne-Regular.ttf"
+  desc "Days One"
+  homepage "https://www.google.com/fonts/specimen/Days+One"
+  def install
+    
+  test do
+  end
+end
