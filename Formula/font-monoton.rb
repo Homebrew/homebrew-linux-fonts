60d29a4f296016d90fd9858dde2048c633162a89
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monoton.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-monoton.rb
@@ -0,0 +1,9 @@
+class FontMonoton < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/monoton/Monoton-Regular.ttf"
+  desc "Monoton"
+  homepage "https://www.google.com/fonts/specimen/Monoton"
+  def install
+    
+  test do
+  end
+end
