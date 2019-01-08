d5bfe7ef7cd5d8dce1baa4eee2e45b30f61a0f51
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-iosevka-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontIosevkaNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "a67d9e2d66146e7f38ef0d19cda5d7f81cb769ea1e6716b545b30412ae8823a0"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Iosevka.zip"
+  desc "Iosevka Nerd Font (Iosevka)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
