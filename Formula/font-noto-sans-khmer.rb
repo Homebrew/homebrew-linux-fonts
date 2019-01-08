1911f2597e2224b894ba4d8904e00e3a34381e31
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-khmer.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-khmer.rb
@@ -0,0 +1,9 @@
+class FontNotoSansKhmer < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKhmer-unhinted.zip"
+  desc "Noto Sans Khmer"
+  homepage "https://www.google.com/get/noto/#sans-khmr"
+  def install
+    
+  test do
+  end
+end
