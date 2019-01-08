50a9c9534718b5b8602a0b45fdaacca06aa9a525
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-skranji.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-skranji.rb
@@ -0,0 +1,10 @@
+class FontSkranji < Formula
+  head "https://github.com/google/fonts/trunk/ofl/skranji", using: :svn, revision: "50", trust_cert: true
+  desc "Skranji"
+  homepage "https://www.google.com/fonts/specimen/Skranji"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
