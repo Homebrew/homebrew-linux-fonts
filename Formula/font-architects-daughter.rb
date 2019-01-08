1adafe9edbbd0c256ad828eb6a1ef318fdee17fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-architects-daughter.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-architects-daughter.rb
@@ -0,0 +1,9 @@
+class FontArchitectsDaughter < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/architectsdaughter/ArchitectsDaughter-Regular.ttf"
+  desc "Architects Daughter"
+  homepage "https://www.google.com/fonts/specimen/Architects+Daughter"
+  def install
+    
+  test do
+  end
+end
