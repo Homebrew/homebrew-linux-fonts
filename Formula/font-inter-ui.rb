236f6a06c53c131494200949e242e69b906bd5ff
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inter-ui.rb from d469e5e25b7f47b00bd09f9e39460279a5824053

---
+++ Formula/font-inter-ui.rb
@@ -0,0 +1,11 @@
+class FontInterUi < Formula
+  version "3.0"
+  sha256 "c9adea61ca421234b21aa85e6d3c0b9e28a6040e835f8a5b48b6de72072bdbfa"
+  url "https://github.com/rsms/inter/releases/download/v#{version}/Inter-UI-#{version}.zip"
+  desc "Inter UI"
+  homepage "https://rsms.me/inter/"
+  def install
+    
+  test do
+  end
+end
