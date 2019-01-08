ee3d5dd4a9573ab1e70afd5d07bf314767194051
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-headland-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-headland-one.rb
@@ -0,0 +1,9 @@
+class FontHeadlandOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/headlandone/HeadlandOne-Regular.ttf"
+  desc "Headland One"
+  homepage "https://www.google.com/fonts/specimen/Headland+One"
+  def install
+    
+  test do
+  end
+end
