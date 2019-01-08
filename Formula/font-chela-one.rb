dd454a0412a59469be79f17d7473002f0055cd69
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chela-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-chela-one.rb
@@ -0,0 +1,9 @@
+class FontChelaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/chelaone/ChelaOne-Regular.ttf"
+  desc "Chela One"
+  homepage "https://www.google.com/fonts/specimen/Chela+One"
+  def install
+    
+  test do
+  end
+end
