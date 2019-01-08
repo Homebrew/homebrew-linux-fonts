9f4fca3f4580ef174df94f9c1808e36e45e50599
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-thabit.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-thabit.rb
@@ -0,0 +1,10 @@
+class FontThabit < Formula
+  head "https://github.com/google/fonts/trunk/ofl/thabit", using: :svn, revision: "50", trust_cert: true
+  desc "Thabit"
+  homepage "https://www.arabeyes.org/"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
