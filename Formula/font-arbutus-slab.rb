a028747769007f3e9d7a40ec934ddca6d60b6a7e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arbutus-slab.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-arbutus-slab.rb
@@ -0,0 +1,9 @@
+class FontArbutusSlab < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/arbutusslab/ArbutusSlab-Regular.ttf"
+  desc "Arbutus Slab"
+  homepage "https://www.google.com/fonts/specimen/Arbutus+Slab"
+  def install
+    
+  test do
+  end
+end
