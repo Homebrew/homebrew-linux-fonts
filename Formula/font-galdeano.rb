6c9013a3fd95293550a196a7b24aaa34156e277f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-galdeano.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-galdeano.rb
@@ -0,0 +1,9 @@
+class FontGaldeano < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/galdeano/Galdeano-Regular.ttf"
+  desc "Galdeano"
+  homepage "https://www.google.com/fonts/specimen/Galdeano"
+  def install
+    
+  test do
+  end
+end
