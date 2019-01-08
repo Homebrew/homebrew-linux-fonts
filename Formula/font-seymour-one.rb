86b59161133aabb7e4441d18df1e8c501ec1dd3a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-seymour-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-seymour-one.rb
@@ -0,0 +1,9 @@
+class FontSeymourOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/seymourone/SeymourOne-Regular.ttf"
+  desc "Seymour One"
+  homepage "https://www.google.com/fonts/specimen/Seymour+One"
+  def install
+    
+  test do
+  end
+end
