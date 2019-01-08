1b2bb5b04150760009f066a5e2bfdc88ebf5c6e6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wellfleet.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-wellfleet.rb
@@ -0,0 +1,9 @@
+class FontWellfleet < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/wellfleet/Wellfleet-Regular.ttf"
+  desc "Wellfleet"
+  homepage "https://www.google.com/fonts/specimen/Wellfleet"
+  def install
+    
+  test do
+  end
+end
