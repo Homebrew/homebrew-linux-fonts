decdd73212b8460236115e32c8ba918cfff7c1a9
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sarasa-gothic.rb from e5a8cad38b151a9d2787fdb3837d271eb07f5b77

--- Formula/font-sarasa-gothic.rb
+++ Formula/font-sarasa-gothic.rb
@@ -1,7 +1,7 @@
 class FontSarasaGothic < Formula
   version "0.6.0"
   sha256 "5deb785be27abed5d2b3b671f9c4eb800a168d5e10505044b06903e6393f20ae"
-  head "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
+  url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   desc "Sarasa Gothic"
   homepage "https://github.com/be5invis/Sarasa-Gothic"
   def install
