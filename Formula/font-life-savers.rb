b9fda9c890c473943aa6c37b6dfa6f62b5469bec
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-life-savers.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-life-savers.rb
@@ -0,0 +1,10 @@
+class FontLifeSavers < Formula
+  head "https://github.com/google/fonts/trunk/ofl/lifesavers", using: :svn, revision: "50", trust_cert: true
+  desc "Life Savers"
+  homepage "https://www.google.com/fonts/specimen/Life%20Savers"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
