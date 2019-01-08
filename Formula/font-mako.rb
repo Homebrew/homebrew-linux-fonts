79fdb9e3aa44a2e8679a874bf3895f701e58f8c1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mako.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-mako.rb
@@ -0,0 +1,9 @@
+class FontMako < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/mako/Mako-Regular.ttf"
+  desc "Mako"
+  homepage "https://www.google.com/fonts/specimen/Mako"
+  def install
+    
+  test do
+  end
+end
