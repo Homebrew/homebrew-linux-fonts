7a885762aee25629e365a3df0df5bbeb8bac940c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-symbols.rb from 429fe80b45ad0fcc4027e2cc38e6908c61e6d571

---
+++ Formula/font-noto-sans-symbols.rb
@@ -0,0 +1,9 @@
+class FontNotoSansSymbols < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSymbols-unhinted.zip"
+  desc "Noto Sans Symbols"
+  homepage "https://www.google.com/get/noto/#sans-zsym"
+  def install
+    
+  test do
+  end
+end
