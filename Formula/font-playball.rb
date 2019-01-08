1b9a4e76b79869ae07c605fadb248574f5647990
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-playball.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-playball.rb
@@ -0,0 +1,9 @@
+class FontPlayball < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/playball/Playball-Regular.ttf"
+  desc "Playball"
+  homepage "https://www.google.com/fonts/specimen/Playball"
+  def install
+    
+  test do
+  end
+end
