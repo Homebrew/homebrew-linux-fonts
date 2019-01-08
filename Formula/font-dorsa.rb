9fa9a75937da79ff6ff243eca44b7ca765bd12be
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dorsa.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-dorsa.rb
@@ -0,0 +1,9 @@
+class FontDorsa < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/dorsa/Dorsa-Regular.ttf"
+  desc "Dorsa"
+  homepage "https://www.google.com/fonts/specimen/Dorsa"
+  def install
+    
+  test do
+  end
+end
