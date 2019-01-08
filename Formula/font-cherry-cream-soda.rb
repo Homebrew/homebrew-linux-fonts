adaf3615cfc1414a7f7452c2ffe59dba3080d7e8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cherry-cream-soda.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-cherry-cream-soda.rb
@@ -0,0 +1,9 @@
+class FontCherryCreamSoda < Formula
+  head "https://github.com/google/fonts/raw/master/apache/cherrycreamsoda/CherryCreamSoda-Regular.ttf"
+  desc "Cherry Cream Soda"
+  homepage "https://www.google.com/fonts/specimen/Cherry+Cream+Soda"
+  def install
+    
+  test do
+  end
+end
