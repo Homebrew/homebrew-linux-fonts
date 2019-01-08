7506e59212a4ed02c99c692bd3a2c79e322c5534
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-libre-caslon-text.rb from f33456dd2e07fbab7cfeb9d86e59cc0f0f9a4b9c

---
+++ Formula/font-libre-caslon-text.rb
@@ -0,0 +1,9 @@
+class FontLibreCaslonText < Formula
+  head "https://github.com/impallari/Libre-Caslon-Text/archive/master.zip"
+  desc "Libre Caslon Text"
+  homepage "http://www.impallari.com/projects/overview/libre-caslon-display-and-text"
+  def install
+    
+  test do
+  end
+end
