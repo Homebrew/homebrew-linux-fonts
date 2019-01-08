1afbb85a52c7368879ae8910fc0141e0c55f8c28
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-holtwood-one-sc.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-holtwood-one-sc.rb
@@ -0,0 +1,9 @@
+class FontHoltwoodOneSc < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/holtwoodonesc/HoltwoodOneSC.ttf"
+  desc "Holtwood One SC"
+  homepage "https://www.google.com/fonts/specimen/Holtwood+One+SC"
+  def install
+    
+  test do
+  end
+end
