86cf1d41e7dcbc9c2128add668a350b1783f06a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-meetei-mayek.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-meetei-mayek.rb
@@ -0,0 +1,9 @@
+class FontNotoSansMeeteiMayek < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMeeteiMayek-unhinted.zip"
+  desc "Noto Sans Meetei Mayek"
+  homepage "https://www.google.com/get/noto/#sans-mtei"
+  def install
+    
+  test do
+  end
+end
