25bdebaa53b1cdf96e8e5c84b9c6d2ad06720f77
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-cjk-jp.rb from 91ec7fe0323c8467fb529c8559b961db2070ea2c

---
+++ Formula/font-noto-sans-cjk-jp.rb
@@ -0,0 +1,9 @@
+class FontNotoSansCjkJp < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip"
+  desc "Noto Sans CJK JP"
+  homepage "https://www.google.com/get/noto/#sans-jpan"
+  def install
+    
+  test do
+  end
+end
