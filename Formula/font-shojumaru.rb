a7d96092754d4ca8eb0ba58d4c251f3ee7f69b16
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-shojumaru.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-shojumaru.rb
@@ -0,0 +1,9 @@
+class FontShojumaru < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/shojumaru/Shojumaru-Regular.ttf"
+  desc "Shojumaru"
+  homepage "https://www.google.com/fonts/specimen/Shojumaru"
+  def install
+    
+  test do
+  end
+end
