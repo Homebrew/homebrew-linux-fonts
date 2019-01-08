02e3b99f46e9d711533d147314cf3d2084df1d5c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-patua-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-patua-one.rb
@@ -0,0 +1,9 @@
+class FontPatuaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/patuaone/PatuaOne-Regular.ttf"
+  desc "Patua One"
+  homepage "https://www.google.com/fonts/specimen/Patua+One"
+  def install
+    
+  test do
+  end
+end
