477ccce9a8d9188cfadc9b1bfaed1cf4efe60c05
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-trocchi.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-trocchi.rb
@@ -0,0 +1,9 @@
+class FontTrocchi < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/trocchi/Trocchi-Regular.ttf"
+  desc "Trocchi"
+  homepage "https://www.google.com/fonts/specimen/Trocchi"
+  def install
+    
+  test do
+  end
+end
