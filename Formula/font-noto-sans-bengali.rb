c29c8c16325fcceaf340b884d484adb3550aa409
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:57:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-bengali.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-sans-bengali.rb
@@ -0,0 +1,27 @@
+class FontNotoSansBengali < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBengali-unhinted.zip"
+  desc "Noto Sans Bengali"
+  homepage "https://www.google.com/get/noto/#sans-beng"
+  def install
+    (share/"fonts").install "NotoSansBengali-Black.ttf"
+    (share/"fonts").install "NotoSansBengali-Bold.ttf"
+    (share/"fonts").install "NotoSansBengali-ExtraBold.ttf"
+    (share/"fonts").install "NotoSansBengali-ExtraLight.ttf"
+    (share/"fonts").install "NotoSansBengali-Light.ttf"
+    (share/"fonts").install "NotoSansBengali-Medium.ttf"
+    (share/"fonts").install "NotoSansBengali-Regular.ttf"
+    (share/"fonts").install "NotoSansBengali-SemiBold.ttf"
+    (share/"fonts").install "NotoSansBengali-Thin.ttf"
+    (share/"fonts").install "NotoSansBengaliUI-Black.ttf"
+    (share/"fonts").install "NotoSansBengaliUI-Bold.ttf"
+    (share/"fonts").install "NotoSansBengaliUI-ExtraBold.ttf"
+    (share/"fonts").install "NotoSansBengaliUI-ExtraLight.ttf"
+    (share/"fonts").install "NotoSansBengaliUI-Light.ttf"
+    (share/"fonts").install "NotoSansBengaliUI-Medium.ttf"
+    (share/"fonts").install "NotoSansBengaliUI-Regular.ttf"
+    (share/"fonts").install "NotoSansBengaliUI-SemiBold.ttf"
+    (share/"fonts").install "NotoSansBengaliUI-Thin.ttf"
+  end
+  test do
+  end
+end
