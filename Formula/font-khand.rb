90680e73208f1aa736541cce46c87a57eb506f7b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-khand.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-khand.rb
@@ -0,0 +1,11 @@
+class FontKhand < Formula
+  version "2.000"
+  sha256 "668910b4cd3487a5bbf93f6dd7259914c1596eec7fef28b08877abdbb7775226"
+  url "https://github.com/itfoundry/khand/releases/download/v#{version}/khand-#{version.to_s.gsub(".", "_")}.zip"
+  desc "Khand"
+  homepage "https://github.com/itfoundry/khand"
+  def install
+    
+  test do
+  end
+end
