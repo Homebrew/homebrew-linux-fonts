312329c7114db110dc62fc0853b4d9c63e7e307b
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:23 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-han-serif-sb-h.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-source-han-serif-sb-h.rb
+++ Formula/font-source-han-serif-sb-h.rb
@@ -1,7 +1,7 @@
 class FontSourceHanSerifSbH < Formula
   version "1.001R"
   sha256 "e7c7c0c02e43f7cc6b1dd54e5e3a313b434123a796c8b64459f95174a6021645"
-  url "https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_SB-H.zip"
+  head "https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_SB-H.zip"
   desc "Source Han Serif SB-H"
   homepage "https://github.com/adobe-fonts/source-han-serif"
   def install
