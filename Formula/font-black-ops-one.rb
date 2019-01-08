5c2bf1cff92df9067bc2d6ca1881b7ddde7a5a7f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-black-ops-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-black-ops-one.rb
@@ -0,0 +1,9 @@
+class FontBlackOpsOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/blackopsone/BlackOpsOne-Regular.ttf"
+  desc "Black Ops One"
+  homepage "https://www.google.com/fonts/specimen/Black+Ops+One"
+  def install
+    
+  test do
+  end
+end
