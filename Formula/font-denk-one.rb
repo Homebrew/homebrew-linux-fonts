ff584b558dccd3a3a69cf76f66501e3318cefef0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-denk-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-denk-one.rb
@@ -0,0 +1,9 @@
+class FontDenkOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/denkone/DenkOne-Regular.ttf"
+  desc "Denk One"
+  homepage "https://www.google.com/fonts/specimen/Denk+One"
+  def install
+    
+  test do
+  end
+end
