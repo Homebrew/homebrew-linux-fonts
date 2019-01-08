958c6fa35eafbfcae74bb004b9306dd6af5467c3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:47 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cambo.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-cambo.rb
@@ -0,0 +1,9 @@
+class FontCambo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/cambo/Cambo-Regular.ttf"
+  desc "Cambo"
+  homepage "https://www.google.com/fonts/specimen/Cambo"
+  def install
+    
+  test do
+  end
+end
