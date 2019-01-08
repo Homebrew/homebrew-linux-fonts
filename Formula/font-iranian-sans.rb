f018d6ea03a75df43f637712cf0ccc93968538e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iranian-sans.rb from f2a7ecd1347a7f8a7ac9f41603c3b8fad8fe3f43

---
+++ Formula/font-iranian-sans.rb
@@ -0,0 +1,9 @@
+class FontIranianSans < Formula
+  head "https://fontlibrary.org/assets/downloads/iranian-sans/2619cbcea5e87e4bb3d77b2e2a94466f/iranian-sans.zip"
+  desc "Iranian Sans"
+  homepage "https://fontlibrary.org/en/font/iranian-sans"
+  def install
+    
+  test do
+  end
+end
