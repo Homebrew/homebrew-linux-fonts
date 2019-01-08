d6182bc677e07d384361879270d0518d837c0ee4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-londrina-solid.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-londrina-solid.rb
@@ -0,0 +1,9 @@
+class FontLondrinaSolid < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/londrinasolid/LondrinaSolid-Regular.ttf"
+  desc "Londrina Solid"
+  homepage "https://www.google.com/fonts/specimen/Londrina+Solid"
+  def install
+    
+  test do
+  end
+end
