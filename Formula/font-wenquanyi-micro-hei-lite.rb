4793c38ed86d8f5c9b5f7529c6dd26ea4b6ba7da
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wenquanyi-micro-hei-lite.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-wenquanyi-micro-hei-lite.rb
@@ -0,0 +1,11 @@
+class FontWenquanyiMicroHeiLite < Formula
+  version "0.2.0-beta"
+  sha256 "bf67c2a38ef9919489afae2364496d2eeba2e6799823292d9290f857f3e78abc"
+  url "https://downloads.sourceforge.net/wqy/wqy-microhei-lite-#{version}.tar.gz"
+  desc "WenQuanYi Micro Hei Lite"
+  homepage "https://wenq.org/wqy2/index.cgi?MicroHei"
+  def install
+    
+  test do
+  end
+end
