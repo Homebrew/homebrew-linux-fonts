a7594554f55b50b74f96ddf0909a87298eed9ded
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-metal-mania.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-metal-mania.rb
@@ -0,0 +1,9 @@
+class FontMetalMania < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/metalmania/MetalMania-Regular.ttf"
+  desc "Metal Mania"
+  homepage "https://www.google.com/fonts/specimen/Metal+Mania"
+  def install
+    
+  test do
+  end
+end
