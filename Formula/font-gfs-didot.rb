a31aca0ce9358ba6167cdde2f94bd04afaf0f157
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gfs-didot.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-gfs-didot.rb
@@ -0,0 +1,9 @@
+class FontGfsDidot < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/gfsdidot/GFSDidot-Regular.ttf"
+  desc "GFS Didot"
+  homepage "https://www.google.com/fonts/specimen/GFS+Didot"
+  def install
+    
+  test do
+  end
+end
