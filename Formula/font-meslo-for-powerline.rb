e02f33ec37610cc53c26248e9305aec08d605007
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-meslo-for-powerline.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-meslo-for-powerline.rb
+++ Formula/font-meslo-for-powerline.rb
@@ -1,7 +1,7 @@
 class FontMesloForPowerline < Formula
   version "2015-12-04"
   sha256 "b6673e0e28441cc04065f8e5c79960aeaa220dae52351852b37e6cf3a03bb8af"
-  head "https://github.com/powerline/fonts/archive/#{version}.zip"
+  url "https://github.com/powerline/fonts/archive/#{version}.zip"
   desc "Meslo for Powerline"
   homepage "https://github.com/powerline/fonts"
   def install
