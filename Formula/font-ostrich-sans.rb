564507ee6186cc2a00c427a37312bfeeb0bd199c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ostrich-sans.rb from 062e596974855a7d4232a99cded5c827b69af959

---
+++ Formula/font-ostrich-sans.rb
@@ -0,0 +1,9 @@
+class FontOstrichSans < Formula
+  head "https://github.com/theleagueof/ostrich-sans/archive/master.zip"
+  desc "Ostrich Sans"
+  homepage "https://www.theleagueofmoveabletype.com/ostrich-sans"
+  def install
+    
+  test do
+  end
+end
