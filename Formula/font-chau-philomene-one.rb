9adea9e0cd8305f02d059b514c7c74dfa5bfa7ac
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chau-philomene-one.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-chau-philomene-one.rb
@@ -0,0 +1,10 @@
+class FontChauPhilomeneOne < Formula
+  head "https://github.com/google/fonts/trunk/ofl/chauphilomeneone", using: :svn, revision: "50", trust_cert: true
+  desc "Chau Philomene One"
+  homepage "https://www.google.com/fonts/specimen/Chau%20Philomene%20One"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
