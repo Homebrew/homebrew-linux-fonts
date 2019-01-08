89778abf402379ed7dbbf549d457606b39ed375f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iranian-serif.rb from 449ce67bc841419c681d2eb8e1f3771beac1970a

---
+++ Formula/font-iranian-serif.rb
@@ -0,0 +1,9 @@
+class FontIranianSerif < Formula
+  head "https://fontlibrary.org/assets/downloads/iranian-serif/3bf122e0d984a0a116a95491c835ec49/iranian-serif.zip"
+  desc "Iranian Serif"
+  homepage "https://fontlibrary.org/en/font/iranian-serif"
+  def install
+    
+  test do
+  end
+end
