580b42bcb0cb4800b4c0802316388f42e6e30f98
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-charis-sil.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-charis-sil.rb
+++ Formula/font-charis-sil.rb
@@ -1,7 +1,7 @@
 class FontCharisSil < Formula
   version "5.000"
   sha256 "5e3e5473b30363008c289cc87e2aa584a0916087a63a3f689defa8e0cee09bfd"
-  head "https://software.sil.org/downloads/r/charis/CharisSIL-#{version}.zip"
+  url "https://software.sil.org/downloads/r/charis/CharisSIL-#{version}.zip"
   desc "Charis SIL"
   homepage "https://software.sil.org/charis/download/"
   def install
