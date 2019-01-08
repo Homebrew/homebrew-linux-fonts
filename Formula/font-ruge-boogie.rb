a9f2315702384a80f62f2e21996d7817901247cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ruge-boogie.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-ruge-boogie.rb
@@ -0,0 +1,9 @@
+class FontRugeBoogie < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/rugeboogie/RugeBoogie-Regular.ttf"
+  desc "Ruge Boogie"
+  homepage "https://www.google.com/fonts/specimen/Ruge+Boogie"
+  def install
+    
+  test do
+  end
+end
