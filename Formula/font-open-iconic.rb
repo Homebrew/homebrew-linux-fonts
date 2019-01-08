006d01bbad9e7188f6db12a3260ac0802ade9cd3
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:21 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-open-iconic.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-open-iconic.rb
+++ Formula/font-open-iconic.rb
@@ -1,7 +1,7 @@
 class FontOpenIconic < Formula
   version "1.1.1"
   sha256 "8acf49f08ae5a069935b48e6be20349c4e9f43fcfc773ea0aba5b972b5b3743c"
-  url "https://codeload.github.com/iconic/open-iconic/zip/#{version}"
+  head "https://codeload.github.com/iconic/open-iconic/zip/#{version}"
   desc "Open Iconic"
   homepage "https://useiconic.com/open/"
   def install
