390ed5058ce761e02429cc0aa472221be5fad8f7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-lepcha.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-lepcha.rb
@@ -0,0 +1,9 @@
+class FontNotoSansLepcha < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLepcha-unhinted.zip"
+  desc "Noto Sans Lepcha"
+  homepage "https://www.google.com/get/noto/#sans-lepc"
+  def install
+    
+  test do
+  end
+end
