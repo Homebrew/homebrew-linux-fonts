18fd845ed0250428a103060baba748ad5831fed7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-thaana.rb from 45cd631147cee78c9cf83f81cc0be2270a59d7eb

---
+++ Formula/font-noto-sans-thaana.rb
@@ -0,0 +1,9 @@
+class FontNotoSansThaana < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansThaana-unhinted.zip"
+  desc "Noto Sans Thaana"
+  homepage "https://www.google.com/get/noto/#sans-thaa"
+  def install
+    
+  test do
+  end
+end
