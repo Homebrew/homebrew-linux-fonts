721c172ffea4a7a69abc37ae03f6f63b011e417b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-maiden-orange.rb from 45ae2083e5bef65af081dfad49a1ccd849a2cccd

---
+++ Formula/font-maiden-orange.rb
@@ -0,0 +1,9 @@
+class FontMaidenOrange < Formula
+  head "https://github.com/google/fonts/raw/master/apache/maidenorange/MaidenOrange-Regular.ttf"
+  desc "Maiden Orange"
+  homepage "https://www.google.com/fonts/specimen/Maiden+Orange"
+  def install
+    
+  test do
+  end
+end
