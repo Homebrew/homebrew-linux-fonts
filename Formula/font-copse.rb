d72d9831dbd1266ee0833648344f32df7e7550e7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-copse.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-copse.rb
@@ -0,0 +1,9 @@
+class FontCopse < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/copse/Copse-Regular.ttf"
+  desc "Copse"
+  homepage "https://www.google.com/fonts/specimen/Copse"
+  def install
+    
+  test do
+  end
+end
