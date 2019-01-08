1f63bb91683229735f4d388da231f32d5e4e155c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-times-new-roman.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-times-new-roman.rb
@@ -0,0 +1,9 @@
+class FontTimesNewRoman < Formula
+  head "https://downloads.sourceforge.net/corefonts/times32.exe"
+  desc "Times New Roman"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  def install
+    
+  test do
+  end
+end
