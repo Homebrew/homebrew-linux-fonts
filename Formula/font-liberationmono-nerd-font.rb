dcd4fb58fb8b2279f25b86ccb98bf90287712560
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-liberationmono-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-liberationmono-nerd-font.rb
+++ Formula/font-liberationmono-nerd-font.rb
@@ -1,7 +1,7 @@
 class FontLiberationmonoNerdFont < Formula
   version "2.0.0"
   sha256 "c4c705a351483ce096d4ebb2af2a45364d193b295cb870cfe0f38e8b3a7ac136"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/LiberationMono.zip"
+  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/LiberationMono.zip"
   desc "LiterationMono Nerd Font (LiberationMono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
