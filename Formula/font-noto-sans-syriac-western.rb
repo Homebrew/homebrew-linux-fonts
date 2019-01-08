23053cc659dd9fac2ff8736ed034693c87f8f24a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-syriac-western.rb from 429fe80b45ad0fcc4027e2cc38e6908c61e6d571

---
+++ Formula/font-noto-sans-syriac-western.rb
@@ -0,0 +1,9 @@
+class FontNotoSansSyriacWestern < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacWestern-unhinted.zip"
+  desc "Noto Sans Syriac Western"
+  homepage "https://www.google.com/get/noto/#sans-syrc-western"
+  def install
+    
+  test do
+  end
+end
