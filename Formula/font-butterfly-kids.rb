341cbf5c6b3f135ab8b10f239cf1dbb191dcf2d9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-butterfly-kids.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-butterfly-kids.rb
@@ -0,0 +1,9 @@
+class FontButterflyKids < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/butterflykids/ButterflyKids-Regular.ttf"
+  desc "Butterfly Kids"
+  homepage "https://www.google.com/fonts/specimen/Butterfly+Kids"
+  def install
+    
+  test do
+  end
+end
