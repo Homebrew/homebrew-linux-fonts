f4c56d4c255eb3ed5524e1039d8b105d896621b7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-englebert.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-englebert.rb
@@ -0,0 +1,9 @@
+class FontEnglebert < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/englebert/Englebert-Regular.ttf"
+  desc "Englebert"
+  homepage "https://www.google.com/fonts/specimen/Englebert"
+  def install
+    
+  test do
+  end
+end
