63013793d1240ffd1f4045959286ba8b41fc23d4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-orienta.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-orienta.rb
@@ -0,0 +1,9 @@
+class FontOrienta < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/orienta/Orienta-Regular.ttf"
+  desc "Orienta"
+  homepage "https://www.google.com/fonts/specimen/Orienta"
+  def install
+    
+  test do
+  end
+end
