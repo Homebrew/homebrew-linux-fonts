3d9c763a10b278e4337debbcfb528dcb8f01eb39
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-walter-turncoat.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-walter-turncoat.rb
@@ -0,0 +1,9 @@
+class FontWalterTurncoat < Formula
+  head "https://github.com/google/fonts/raw/master/apache/walterturncoat/WalterTurncoat-Regular.ttf"
+  desc "Walter Turncoat"
+  homepage "https://www.google.com/fonts/specimen/Walter+Turncoat"
+  def install
+    
+  test do
+  end
+end
