29c21117cd9b80de7dd387d14602c564608fa6c3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ruluko.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-ruluko.rb
@@ -0,0 +1,9 @@
+class FontRuluko < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/ruluko/Ruluko-Regular.ttf"
+  desc "Ruluko"
+  homepage "https://www.google.com/fonts/specimen/Ruluko"
+  def install
+    
+  test do
+  end
+end
