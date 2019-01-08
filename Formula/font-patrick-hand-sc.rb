10e97ac29b4e3b4d50b2ab7b53a891f6ffaa3617
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-patrick-hand-sc.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-patrick-hand-sc.rb
@@ -0,0 +1,9 @@
+class FontPatrickHandSc < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/patrickhandsc/PatrickHandSC-Regular.ttf"
+  desc "Patrick Hand SC"
+  homepage "https://www.google.com/fonts/specimen/Patrick+Hand+SC"
+  def install
+    
+  test do
+  end
+end
