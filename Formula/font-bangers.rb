52c8b159b451ed60ec2393c93eddecf1cef2ca22
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bangers.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bangers.rb
@@ -0,0 +1,9 @@
+class FontBangers < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/bangers/Bangers-Regular.ttf"
+  desc "Bangers"
+  homepage "https://www.google.com/fonts/specimen/Bangers"
+  def install
+    
+  test do
+  end
+end
