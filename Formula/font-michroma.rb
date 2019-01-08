39a21bae17a1052b9fbde3a0c7d16a9eb47b56c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-michroma.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-michroma.rb
@@ -0,0 +1,9 @@
+class FontMichroma < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/michroma/Michroma.ttf"
+  desc "Michroma"
+  homepage "https://www.google.com/fonts/specimen/Michroma"
+  def install
+    
+  test do
+  end
+end
