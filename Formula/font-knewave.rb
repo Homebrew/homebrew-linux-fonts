acbc6167540b6af5b522a3a1f47592530c571fa3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-knewave.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-knewave.rb
@@ -0,0 +1,9 @@
+class FontKnewave < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/knewave/Knewave-Regular.ttf"
+  desc "Knewave"
+  homepage "https://www.google.com/fonts/specimen/Knewave"
+  def install
+    
+  test do
+  end
+end
