9e1b4ed0af63f3c5b7d6cc4d8bc78938f8adcd8b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rubik.rb from 81137b934002c44cf153ed763223b130d85e5cc3

---
+++ Formula/font-rubik.rb
@@ -0,0 +1,9 @@
+class FontRubik < Formula
+  head "https://github.com/googlefonts/rubik/archive/master.zip"
+  desc "Rubik"
+  homepage "https://www.google.com/fonts/specimen/Rubik"
+  def install
+    
+  test do
+  end
+end
