06d5a9e6053c63ca080d75d5361d6ccaf1e59bcb
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kawkab-mono.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-kawkab-mono.rb
+++ Formula/font-kawkab-mono.rb
@@ -1,7 +1,7 @@
 class FontKawkabMono < Formula
   version "0.501"
   sha256 "11c06f57dddefaf0166d74caaa072865ab6ff8d34076e7ec5d2c20edda145666"
-  url "https://github.com/aiaf/kawkab-mono/releases/download/v#{version}/kawkab-mono-#{version}.zip"
+  head "https://github.com/aiaf/kawkab-mono/releases/download/v#{version}/kawkab-mono-#{version}.zip"
   desc "Kawkab Mono"
   homepage "https://makkuk.com/kawkab-mono"
   def install
