2887af71b6b096f2356cb646b3074f1b71a46821
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-magra.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-magra.rb
@@ -0,0 +1,10 @@
+class FontMagra < Formula
+  head "https://github.com/google/fonts/trunk/ofl/magra", using: :svn, revision: "50", trust_cert: true
+  desc "Magra"
+  homepage "https://www.google.com/fonts/specimen/Magra"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
