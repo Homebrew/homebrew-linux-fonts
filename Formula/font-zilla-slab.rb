526b959b25b6dfb77c86e7c4842af057d1617c26
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-zilla-slab.rb from 667fd2939fb228d85b1b88271fef3f1a85ac121f

--- Formula/font-zilla-slab.rb
+++ Formula/font-zilla-slab.rb
@@ -1,7 +1,7 @@
 class FontZillaSlab < Formula
   version "1.002"
   sha256 "62490dc19cd17e2951fe88ba3e662089ca14077634cacf1f12926374281dcf42"
-  url "https://github.com/mozilla/zilla-slab/releases/download/v#{version}/Zilla-Slab-Fonts-v#{version}.zip"
+  head "https://github.com/mozilla/zilla-slab/releases/download/v#{version}/Zilla-Slab-Fonts-v#{version}.zip"
   desc "Zilla Slab"
   homepage "https://github.com/mozilla/zilla-slab"
   def install
