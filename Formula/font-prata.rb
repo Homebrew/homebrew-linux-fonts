278c2bf66d3fb3de6fc824f97f53453592b88c19
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-prata.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-prata.rb
@@ -0,0 +1,9 @@
+class FontPrata < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/prata/Prata-Regular.ttf"
+  desc "Prata"
+  homepage "https://www.google.com/fonts/specimen/Prata"
+  def install
+    
+  test do
+  end
+end
