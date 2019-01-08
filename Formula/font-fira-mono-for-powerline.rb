edd87cc584dc9cbec5df1cd36ef1b843acdb4e90
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-mono-for-powerline.rb from 3a16d70a9f03bbda73c1a306c778297f33ca5b50

---
+++ Formula/font-fira-mono-for-powerline.rb
@@ -0,0 +1,10 @@
+class FontFiraMonoForPowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/FiraMono", using: :svn, trust_cert: true
+  desc "Fira Mono for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/FiraMono"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
