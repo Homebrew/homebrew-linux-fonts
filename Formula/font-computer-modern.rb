7790286eb33b272f4b198593e5ef78f334c01903
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-computer-modern.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-computer-modern.rb
@@ -0,0 +1,11 @@
+class FontComputerModern < Formula
+  version "0.7.0"
+  sha256 "2609c14450f42d0bcd40203900afcb1d693521a9b24a18c65e14b6b0585ff150"
+  url "https://downloads.sourceforge.net/cm-unicode/cm-unicode-#{version}-ttf.tar.xz"
+  desc "Computer Modern"
+  homepage "http://cm-unicode.sourceforge.net/"
+  def install
+    
+  test do
+  end
+end
