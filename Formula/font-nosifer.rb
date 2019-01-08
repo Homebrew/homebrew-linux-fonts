cdc67f6287c4a3dbf32cec772c42b807a5eec06c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nosifer.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-nosifer.rb
@@ -0,0 +1,9 @@
+class FontNosifer < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/nosifer/Nosifer-Regular.ttf"
+  desc "Nosifer"
+  homepage "https://www.google.com/fonts/specimen/Nosifer"
+  def install
+    
+  test do
+  end
+end
