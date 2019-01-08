fe501944b9a57904a18df3758bfbea778e3f6ce9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-armenian.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-sans-armenian.rb
@@ -0,0 +1,9 @@
+class FontNotoSansArmenian < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansArmenian-unhinted.zip"
+  desc "Noto Sans Armenian"
+  homepage "https://www.google.com/get/noto/#sans-armn"
+  def install
+    
+  test do
+  end
+end
