ef6c0d4df7a4d4f847c2cdcf1314429af2a7a126
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bonbon.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-bonbon.rb
@@ -0,0 +1,9 @@
+class FontBonbon < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/bonbon/Bonbon-Regular.ttf"
+  desc "Bonbon"
+  homepage "https://www.google.com/fonts/specimen/Bonbon"
+  def install
+    
+  test do
+  end
+end
