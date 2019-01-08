eb5797fbbaf0818d2150ebe342ec2f7c9f6021da
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-diplomata.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-diplomata.rb
@@ -0,0 +1,9 @@
+class FontDiplomata < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/diplomata/Diplomata-Regular.ttf"
+  desc "Diplomata"
+  homepage "https://www.google.com/fonts/specimen/Diplomata"
+  def install
+    
+  test do
+  end
+end
