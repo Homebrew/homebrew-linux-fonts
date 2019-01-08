e2b67c4e5209cca830499d258bcdf0b2d3e8ec59
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nokora.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-nokora.rb
@@ -0,0 +1,10 @@
+class FontNokora < Formula
+  head "https://github.com/google/fonts/trunk/apache/nokora", using: :svn, revision: "50", trust_cert: true
+  desc "Nokora"
+  homepage "https://www.google.com/fonts/specimen/Nokora"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
