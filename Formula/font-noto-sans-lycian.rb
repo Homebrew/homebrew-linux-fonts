97d9d21c77cd9ccc20f0d2b34ed29809a64f6d2e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-lycian.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-lycian.rb
@@ -0,0 +1,9 @@
+class FontNotoSansLycian < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLycian-unhinted.zip"
+  desc "Noto Sans Lycian"
+  homepage "https://www.google.com/get/noto/#sans-lyci"
+  def install
+    
+  test do
+  end
+end
