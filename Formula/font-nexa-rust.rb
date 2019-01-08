e3ead03ef529f76498ddfd60926577e44c66a4f4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nexa-rust.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-nexa-rust.rb
@@ -0,0 +1,9 @@
+class FontNexaRust < Formula
+  head "https://fontfabric.com/downfont/nexarust.zip"
+  desc "Nexa Rust"
+  homepage "https://fontfabric.com/nexa-rust-free-font/"
+  def install
+    
+  test do
+  end
+end
