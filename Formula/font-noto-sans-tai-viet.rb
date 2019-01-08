6a4735e49c7659ae6d78eee8d91c464574ee113d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tai-viet.rb from 429fe80b45ad0fcc4027e2cc38e6908c61e6d571

---
+++ Formula/font-noto-sans-tai-viet.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTaiViet < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiViet-unhinted.zip"
+  desc "Noto Sans Tai Viet"
+  homepage "https://www.google.com/get/noto/#sans-tavt"
+  def install
+    
+  test do
+  end
+end
