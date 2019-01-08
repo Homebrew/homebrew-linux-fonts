e3d77512e307e879850674afcced804af6975dbf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-kaithi.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-kaithi.rb
@@ -0,0 +1,9 @@
+class FontNotoSansKaithi < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKaithi-unhinted.zip"
+  desc "Noto Sans Kaithi"
+  homepage "https://www.google.com/get/noto/#sans-kthi"
+  def install
+    
+  test do
+  end
+end
