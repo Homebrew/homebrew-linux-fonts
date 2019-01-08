4a40a1080e33f4c684acb2ba302914de9093b9f2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-code.rb from 2f2095f2816b8e8fdce633865f8ecfd9b9dc9e67

---
+++ Formula/font-fira-code.rb
@@ -0,0 +1,11 @@
+class FontFiraCode < Formula
+  version "1.206"
+  sha256 "433e9e059e53ae1e94c674819b73e49d65bd76e3c6ec70f42d2324062ea3210b"
+  url "https://github.com/tonsky/FiraCode/releases/download/#{version}/FiraCode_#{version}.zip"
+  desc "Fira Code"
+  homepage "https://github.com/tonsky/FiraCode"
+  def install
+    
+  test do
+  end
+end
