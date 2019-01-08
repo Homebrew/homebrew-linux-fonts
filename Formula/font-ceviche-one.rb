2edd48a6c75e7076ccf0346376e753a6ef61404f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ceviche-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-ceviche-one.rb
@@ -0,0 +1,9 @@
+class FontCevicheOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/cevicheone/CevicheOne-Regular.ttf"
+  desc "Ceviche One"
+  homepage "https://www.google.com/fonts/specimen/Ceviche+One"
+  def install
+    
+  test do
+  end
+end
