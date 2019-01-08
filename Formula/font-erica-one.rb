d9dd6363c5594feec4d4f83ad9c92b40643c0619
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-erica-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-erica-one.rb
@@ -0,0 +1,9 @@
+class FontEricaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/ericaone/EricaOne-Regular.ttf"
+  desc "Erica One"
+  homepage "https://www.google.com/fonts/specimen/Erica+One"
+  def install
+    
+  test do
+  end
+end
