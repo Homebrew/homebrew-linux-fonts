01c014d0ec459c09ebc9c19797fc32bf298851c4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-geo.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-geo.rb
@@ -0,0 +1,10 @@
+class FontGeo < Formula
+  head "https://github.com/google/fonts/trunk/ofl/geo", using: :svn, revision: "50", trust_cert: true
+  desc "Geo"
+  homepage "https://www.google.com/fonts/specimen/Geo"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
