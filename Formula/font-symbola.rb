b8eb51d40b4a908b913b7a3eab1c8d480930a95d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-symbola.rb from 11bd78a0a65d611a9ef5305b3605489c67ff102f

---
+++ Formula/font-symbola.rb
@@ -0,0 +1,9 @@
+class FontSymbola < Formula
+  head "http://users.teilar.gr/~g1951d/Symbola.zip"
+  desc "Symbola"
+  homepage "http://users.teilar.gr/~g1951d/"
+  def install
+    
+  test do
+  end
+end
