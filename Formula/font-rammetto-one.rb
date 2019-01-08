73f08fdff310905d66d8a49a7408c90d9e27c146
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rammetto-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-rammetto-one.rb
@@ -0,0 +1,9 @@
+class FontRammettoOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/rammettoone/RammettoOne-Regular.ttf"
+  desc "Rammetto One"
+  homepage "https://www.google.com/fonts/specimen/Rammetto+One"
+  def install
+    
+  test do
+  end
+end
