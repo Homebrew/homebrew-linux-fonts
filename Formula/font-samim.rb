7850a3aaaf9777b51e625ab9324ba3e6aef8527d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-samim.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-samim.rb
@@ -0,0 +1,11 @@
+class FontSamim < Formula
+  version "2.0.1"
+  sha256 "2d03590a09edc23fc5336edb2a9b67586bbccda14b2320d26a06baf107f51684"
+  url "https://github.com/rastikerdar/samim-font/releases/download/v#{version}/samim-font-v#{version}.zip"
+  desc "Samim"
+  homepage "https://rastikerdar.github.io/samim-font"
+  def install
+    
+  test do
+  end
+end
