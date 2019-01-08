fa4fef9d76c24250af6daf605eb8ffd26eddbfa7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sanchez.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-sanchez.rb
@@ -0,0 +1,10 @@
+class FontSanchez < Formula
+  head "https://github.com/google/fonts/trunk/ofl/sanchez", using: :svn, revision: "50", trust_cert: true
+  desc "Sanchez"
+  homepage "https://www.google.com/fonts/specimen/Sanchez"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
