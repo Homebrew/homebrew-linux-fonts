ce9cde8151e9a1db23c04ce354690aaff67a6b64
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-n-ko.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-n-ko.rb
@@ -0,0 +1,9 @@
+class FontNotoSansNKo < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNKo-unhinted.zip"
+  desc "Noto Sans NKo"
+  homepage "https://www.google.com/get/noto/#sans-nkoo"
+  def install
+    
+  test do
+  end
+end
