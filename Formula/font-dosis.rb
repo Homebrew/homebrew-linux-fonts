bad429373b5476e31f4f2074f628a404b199babb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dosis.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-dosis.rb
@@ -0,0 +1,10 @@
+class FontDosis < Formula
+  head "https://github.com/google/fonts/trunk/ofl/dosis", using: :svn, revision: "50", trust_cert: true
+  desc "Dosis"
+  homepage "https://www.google.com/fonts/specimen/Dosis"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
