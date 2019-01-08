28c56ebb5dc7896c710d7c88fbd44b425eea372c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-indie-flower.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-indie-flower.rb
@@ -0,0 +1,9 @@
+class FontIndieFlower < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/indieflower/IndieFlower-Regular.ttf"
+  desc "Indie Flower"
+  homepage "https://www.google.com/fonts/specimen/Indie+Flower"
+  def install
+    
+  test do
+  end
+end
