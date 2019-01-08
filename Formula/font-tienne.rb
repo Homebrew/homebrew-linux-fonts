42c08d42f52c50db1771e1bbfa7a973c67161dce
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tienne.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-tienne.rb
@@ -0,0 +1,10 @@
+class FontTienne < Formula
+  head "https://github.com/google/fonts/trunk/ofl/tienne", using: :svn, revision: "50", trust_cert: true
+  desc "Tienne"
+  homepage "https://www.google.com/fonts/specimen/Tienne"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
