928d2cc0c4339258d66c33f9c4f18e8dd65bbdbc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-inscriptional-parthian.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-inscriptional-parthian.rb
@@ -0,0 +1,9 @@
+class FontNotoSansInscriptionalParthian < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansInscriptionalParthian-unhinted.zip"
+  desc "Noto Sans Inscriptional Parthian"
+  homepage "https://www.google.com/get/noto/#sans-prti"
+  def install
+    
+  test do
+  end
+end
