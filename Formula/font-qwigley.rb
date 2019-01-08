c30a7efaeef02e08b9dd93ed2f91ae1869bd65d5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-qwigley.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-qwigley.rb
@@ -0,0 +1,9 @@
+class FontQwigley < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/qwigley/Qwigley-Regular.ttf"
+  desc "Qwigley"
+  homepage "https://www.google.com/fonts/specimen/Qwigley"
+  def install
+    
+  test do
+  end
+end
