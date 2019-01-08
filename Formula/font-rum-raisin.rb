d14f8f024767cce274fbffca5c101111dfe72132
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rum-raisin.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-rum-raisin.rb
@@ -0,0 +1,9 @@
+class FontRumRaisin < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/rumraisin/RumRaisin-Regular.ttf"
+  desc "Rum Raisin"
+  homepage "https://www.google.com/fonts/specimen/Rum+Raisin"
+  def install
+    
+  test do
+  end
+end
