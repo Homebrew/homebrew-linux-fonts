48fbc96c0c5eb94df6f5b11f4df05ab90e1f7193
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-anatolian-hieroglyphs.rb from a877d71f000c4cd26ee9ddccfec7d06085e2aa0a

---
+++ Formula/font-noto-sans-anatolian-hieroglyphs.rb
@@ -0,0 +1,9 @@
+class FontNotoSansAnatolianHieroglyphs < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAnatolianHieroglyphs-unhinted.zip"
+  desc "Noto Sans Anatolian Hieroglyphs"
+  homepage "https://www.google.com/get/noto/#sans-hluw"
+  def install
+    
+  test do
+  end
+end
