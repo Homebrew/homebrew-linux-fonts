fbf316d2fea7b729d24044cabb7659ab6cfc0110
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-economica.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-economica.rb
@@ -0,0 +1,10 @@
+class FontEconomica < Formula
+  head "https://github.com/google/fonts/trunk/ofl/economica", using: :svn, revision: "50", trust_cert: true
+  desc "Economica"
+  homepage "https://www.google.com/fonts/specimen/Economica"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
