39f2811099dfbbd9dd783252e65696b57e0607db
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tulpen-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-tulpen-one.rb
@@ -0,0 +1,9 @@
+class FontTulpenOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/tulpenone/TulpenOne-Regular.ttf"
+  desc "Tulpen One"
+  homepage "https://www.google.com/fonts/specimen/Tulpen+One"
+  def install
+    
+  test do
+  end
+end
