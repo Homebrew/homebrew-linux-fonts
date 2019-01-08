988a725c47a0f2b2520074cc9f4af21be0667d44
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:56 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-migu-1m.rb from cb406b6bd3096ad5c16e4e1c1edcc3badf75a4a5

--- Formula/font-migu-1m.rb
+++ Formula/font-migu-1m.rb
@@ -1,7 +1,7 @@
 class FontMigu1m < Formula
   version "20150712"
   sha256 "d4c38664dd57bc5927abe8f4fbea8f06a8ece3fea49ea02354d4e03ac6d15006"
-  head "http://dl.osdn.jp/mix-mplus-ipa/63545/migu-1m-#{version}.zip"
+  url "http://dl.osdn.jp/mix-mplus-ipa/63545/migu-1m-#{version}.zip"
   desc "Migu 1M"
   homepage "http://mix-mplus-ipa.osdn.jp/migu/#migu1m"
   def install
