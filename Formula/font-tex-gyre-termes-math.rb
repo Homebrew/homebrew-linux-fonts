4cb5f6ee9b053923126beb7bd07ea1e4aa985731
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-termes-math.rb from ae069a537e694d476b567118c3c6c390bf25b74c

---
+++ Formula/font-tex-gyre-termes-math.rb
@@ -0,0 +1,11 @@
+class FontTexGyreTermesMath < Formula
+  version "1.543"
+  sha256 "5875b39d3987cbe4258e5933a2292fbece870177053c5a8f495492769e2d4bb2"
+  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyretermes-math-1543.zip"
+  desc "Termes Math"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tg-math"
+  def install
+    
+  test do
+  end
+end
