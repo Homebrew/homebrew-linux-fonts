b2db4a1d673dfdf0ae87d47fd3ffc2947029a997
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:28 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cousine-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-cousine-nerd-font.rb
@@ -0,0 +1,11 @@
+class FontCousineNerdFont < Formula
+  version "2.0.0"
+  sha256 "dae2a0a363b03ef2adaf9cd341e9e5210b23daef7dea617bdf79c375ced50c4e"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Cousine.zip"
+  desc "Cousine Nerd Font (Cousine)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
