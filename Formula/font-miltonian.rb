44f674a68bace446411a13b7591577e9255d3d3d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-miltonian.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-miltonian.rb
@@ -0,0 +1,9 @@
+class FontMiltonian < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/miltonian/Miltonian-Regular.ttf"
+  desc "Miltonian"
+  homepage "https://www.google.com/fonts/specimen/Miltonian"
+  def install
+    
+  test do
+  end
+end
