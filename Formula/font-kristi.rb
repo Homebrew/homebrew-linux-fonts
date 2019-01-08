9a1450627aaab97707b9bbb5110a4b9cb99a4738
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:28 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kristi.rb from 35859bb265d2af7f670d1fd1d44f13790939f9f1

---
+++ Formula/font-kristi.rb
@@ -0,0 +1,9 @@
+class FontKristi < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kristi/Kristi-Regular.ttf"
+  desc "Kristi"
+  homepage "https://www.google.com/fonts/specimen/Kristi"
+  def install
+    
+  test do
+  end
+end
