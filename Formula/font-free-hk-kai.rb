de14c11028c5019cc2b14ed23cce3fb57a676d24
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:53 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-free-hk-kai.rb from 57a73bba14f185a982b5d10aaf31ee2b35b0afa9

--- Formula/font-free-hk-kai.rb
+++ Formula/font-free-hk-kai.rb
@@ -1,7 +1,7 @@
 class FontFreeHkKai < Formula
   version "1.01"
   sha256 "f949362df4c3e8d8f2f5e9c6e5932ce3aa4ab3176b3c0bfa61afbb8c7b750ada"
-  head "https://github.com/freehkfonts/freehkkai/raw/download/Free-HK-Kai_4700-v#{version}.ttf"
+  url "https://github.com/freehkfonts/freehkkai/raw/download/Free-HK-Kai_4700-v#{version}.ttf"
   desc "Free HK Kai"
   homepage "https://freehkfonts.opensource.hk/"
   def install
