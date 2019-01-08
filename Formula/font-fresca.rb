df226e769c07d087f4fc5250b52435255a6329ad
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fresca.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-fresca.rb
@@ -0,0 +1,9 @@
+class FontFresca < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/fresca/Fresca-Regular.ttf"
+  desc "Fresca"
+  homepage "https://www.google.com/fonts/specimen/Fresca"
+  def install
+    
+  test do
+  end
+end
