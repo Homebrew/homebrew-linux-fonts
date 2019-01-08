77fb13ccc1b26512cf454cc868a07755c18d0149
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dana-yad.rb from e9c808d983358e606d15047bceca26d6d5ba8245

---
+++ Formula/font-dana-yad.rb
@@ -0,0 +1,9 @@
+class FontDanaYad < Formula
+  head "https://alefalefalef.co.il/wp-content/fonts/free/dana-yad-alefalefalef.zip"
+  desc "Dana Yad"
+  homepage "https://alefalefalef.co.il/resources/פונטים-בחינם/"
+  def install
+    
+  test do
+  end
+end
