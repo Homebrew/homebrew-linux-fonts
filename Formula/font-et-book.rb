f44bf14fba90e62e1211100abe6f13dd0fee94af
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-et-book.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-et-book.rb
@@ -0,0 +1,10 @@
+class FontEtBook < Formula
+  head "https://github.com/edwardtufte/et-book/archive/gh-pages.zip"
+  desc "ET Book"
+  desc "Edward Tufte Book"
+  homepage "https://edwardtufte.github.io/et-book/"
+  def install
+    
+  test do
+  end
+end
