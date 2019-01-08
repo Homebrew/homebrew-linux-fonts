37e8e76bd7207ee9701b762c8de92d733859b413
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mountains-of-christmas.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-mountains-of-christmas.rb
@@ -0,0 +1,10 @@
+class FontMountainsOfChristmas < Formula
+  head "https://github.com/google/fonts/trunk/apache/mountainsofchristmas", using: :svn, revision: "50", trust_cert: true
+  desc "Mountains of Christmas"
+  homepage "https://www.google.com/fonts/specimen/Mountains%20of%20Christmas"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
