ae6e3001ff9ed6c0126fa7d9464b73be6af72a97
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-disclaimer.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-disclaimer.rb
@@ -0,0 +1,9 @@
+class FontDisclaimer < Formula
+  head "https://fontfabric.com/downfont/disclaimer.zip"
+  desc "Disclaimer"
+  homepage "https://fontfabric.com/disclaimer-free-font/"
+  def install
+    
+  test do
+  end
+end
