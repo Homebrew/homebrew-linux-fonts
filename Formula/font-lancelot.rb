9682b0d4c8208eb5654513e475481def9e876768
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lancelot.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-lancelot.rb
@@ -0,0 +1,9 @@
+class FontLancelot < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/lancelot/Lancelot-Regular.ttf"
+  desc "Lancelot"
+  homepage "https://www.google.com/fonts/specimen/Lancelot"
+  def install
+    
+  test do
+  end
+end
