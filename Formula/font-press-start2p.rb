8b9141bbcf3a7de761337702192780581679587a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-press-start2p.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-press-start2p.rb
@@ -0,0 +1,9 @@
+class FontPressStart2p < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/pressstart2p/PressStart2P-Regular.ttf"
+  desc "Press Start 2P"
+  homepage "https://www.google.com/fonts/specimen/Press+Start+2P"
+  def install
+    
+  test do
+  end
+end
