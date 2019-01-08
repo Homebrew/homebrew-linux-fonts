6d2a08a948c3b20637870ac91c39e4de1a84b41d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-poppins.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-poppins.rb
@@ -0,0 +1,11 @@
+class FontPoppins < Formula
+  version "3.200"
+  sha256 "a05ecec9599a698fd7a52fa492c868af1fa3462be33bb038d14dbbac7a5456dc"
+  url "https://github.com/itfoundry/Poppins/releases/download/v#{version}/Poppins-#{version.to_s.gsub(".", "")}-OTF.zip"
+  desc "Poppins"
+  homepage "https://github.com/itfoundry/poppins"
+  def install
+    
+  test do
+  end
+end
