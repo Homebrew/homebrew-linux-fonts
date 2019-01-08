cd3f1e013c2744d52d908fb11985010ce2bb13a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ribeye.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-ribeye.rb
@@ -0,0 +1,9 @@
+class FontRibeye < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/ribeye/Ribeye-Regular.ttf"
+  desc "Ribeye"
+  homepage "https://www.google.com/fonts/specimen/Ribeye"
+  def install
+    
+  test do
+  end
+end
