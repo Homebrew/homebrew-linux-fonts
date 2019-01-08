a5ced7620ce6f3cf214ff3fc54a745325994f3ae
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marcellus.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-marcellus.rb
@@ -0,0 +1,9 @@
+class FontMarcellus < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/marcellus/Marcellus-Regular.ttf"
+  desc "Marcellus"
+  homepage "https://www.google.com/fonts/specimen/Marcellus"
+  def install
+    
+  test do
+  end
+end
