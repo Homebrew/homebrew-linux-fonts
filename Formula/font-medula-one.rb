ffce92aedf6c548d5f03dc95986058289178ae2f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-medula-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-medula-one.rb
@@ -0,0 +1,9 @@
+class FontMedulaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/medulaone/MedulaOne-Regular.ttf"
+  desc "Medula One"
+  homepage "https://www.google.com/fonts/specimen/Medula+One"
+  def install
+    
+  test do
+  end
+end
