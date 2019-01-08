09dcaa5078e1d6be3cf911fb3a5405546b509b98
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-laila.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-laila.rb
+++ Formula/font-laila.rb
@@ -1,7 +1,7 @@
 class FontLaila < Formula
   version "2.000"
   sha256 "caa87cae373702288a7510c74881077aa15e3b5c94a4746b61f3f977d8499f73"
-  url "https://github.com/itfoundry/laila/releases/download/v#{version}/laila-#{version.to_s.gsub(".", "_")}.zip"
+  head "https://github.com/itfoundry/laila/releases/download/v#{version}/laila-#{version.dots_to_underscores}.zip"
   desc "Laila"
   homepage "https://github.com/itfoundry/laila"
   def install
