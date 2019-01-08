9f88266c6e1ed6a812f909780b48a78c1adf74f3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pt-mono.rb from da8771428617a431053cb51d1c9715ebac170e81

---
+++ Formula/font-pt-mono.rb
@@ -0,0 +1,9 @@
+class FontPtMono < Formula
+  head "https://company.paratype.com/system/attachments/631/original/ptmono.zip"
+  desc "PT Mono"
+  homepage "https://www.paratype.com/public/"
+  def install
+    
+  test do
+  end
+end
