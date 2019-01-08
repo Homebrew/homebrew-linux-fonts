28bcb6561b59b7bc039384c85621539616c14013
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-simonetta.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-simonetta.rb
@@ -0,0 +1,10 @@
+class FontSimonetta < Formula
+  head "https://github.com/google/fonts/trunk/ofl/simonetta", using: :svn, revision: "50", trust_cert: true
+  desc "Simonetta"
+  homepage "https://www.google.com/fonts/specimen/Simonetta"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
