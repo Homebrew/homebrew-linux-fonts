b6d5836794d8feda8fba58fbd20ba27809b4fb8f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-encodesans-expanded.rb from 4b855fd673d71fdc6cb6a25766a751f68319e2ec

---
+++ Formula/font-encodesans-expanded.rb
@@ -0,0 +1,10 @@
+class FontEncodesansExpanded < Formula
+  head "https://github.com/google/fonts/trunk/ofl/encodesansexpanded", using: :svn, trust_cert: true
+  desc "Encode Sans Expanded"
+  homepage "https://fonts.google.com/specimen/Encode+Sans+Expanded"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
