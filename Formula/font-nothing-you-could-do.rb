f20e3ada579e5b9f29e6cdbea0d6c2eb65332d37
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nothing-you-could-do.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-nothing-you-could-do.rb
@@ -0,0 +1,9 @@
+class FontNothingYouCouldDo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/nothingyoucoulddo/NothingYouCouldDo.ttf"
+  desc "Nothing You Could Do"
+  homepage "https://www.google.com/fonts/specimen/Nothing+You+Could+Do"
+  def install
+    
+  test do
+  end
+end
