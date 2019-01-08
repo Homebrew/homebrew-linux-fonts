0250f72000f3aea0b35f99550852cb04f9c9be0f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-lao.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-lao.rb
@@ -0,0 +1,9 @@
+class FontNotoSansLao < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLao-unhinted.zip"
+  desc "Noto Sans Lao"
+  homepage "https://www.google.com/get/noto/#sans-laoo"
+  def install
+    
+  test do
+  end
+end
