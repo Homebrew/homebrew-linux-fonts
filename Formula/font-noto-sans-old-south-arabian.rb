487cb22cdf3289096b1274a7e7d5ab4d5806705c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-old-south-arabian.rb from 7eff405e323aa4047b9815ba7e3a316e52a51c46

---
+++ Formula/font-noto-sans-old-south-arabian.rb
@@ -0,0 +1,9 @@
+class FontNotoSansOldSouthArabian < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldSouthArabian-unhinted.zip"
+  desc "Noto Sans Old South Arabian"
+  homepage "https://www.google.com/get/noto/#sans-sarb"
+  def install
+    
+  test do
+  end
+end
