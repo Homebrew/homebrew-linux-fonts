3a03a3557233b2e5be470c4f9e88b0da0ec05d34
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-code2001.rb from 9d060732cf82b4e45a1f27e2695e05a5726ef970

---
+++ Formula/font-code2001.rb
@@ -0,0 +1,11 @@
+class FontCode2001 < Formula
+  version "0.919"
+  sha256 "6e8173132072d099db5bdc3f8035a2519f6d92d65beab75efd0c3b697c8fe048"
+  url "http://distcache.freebsd.org/ports-distfiles/code2001-#{version}/CODE2001.ZIP"
+  desc "Code2001"
+  homepage "http://ftp-archive.freebsd.org/pub/FreeBSD/"
+  def install
+    
+  test do
+  end
+end
