d4fc3861a7569a42d82821830a6af7cd64d3df8f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jaapokki.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-jaapokki.rb
@@ -0,0 +1,11 @@
+class FontJaapokki < Formula
+  version "1.0"
+  sha256 "aa5f38cbb682df8732d406996e37349b83e47d3c14bae4af2125f2a7784430b7"
+  url "https://mikkonuuttila.com/jaapokki/Jaapokki-font-package-#{version}.zip"
+  desc "Jaapokki"
+  homepage "https://mikkonuuttila.com/jaapokki/"
+  def install
+    
+  test do
+  end
+end
