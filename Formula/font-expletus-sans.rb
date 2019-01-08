93faface11bd2fdbba6c9e74d9cc2a3adb90f37a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-expletus-sans.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-expletus-sans.rb
@@ -0,0 +1,10 @@
+class FontExpletusSans < Formula
+  head "https://github.com/google/fonts/trunk/ofl/expletussans", using: :svn, revision: "50", trust_cert: true
+  desc "Expletus Sans"
+  homepage "https://www.google.com/fonts/specimen/Expletus%20Sans"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
