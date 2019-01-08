319a8b8b5089163681b4941ed6ddfb905f11dac7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-french-canon-sc.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-im-fell-french-canon-sc.rb
@@ -0,0 +1,9 @@
+class FontImFellFrenchCanonSc < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/imfellfrenchcanonsc/IMFeFCsc28P.ttf"
+  desc "IM Fell French Canon SC"
+  homepage "https://www.google.com/fonts/specimen/IM+Fell+French+Canon+SC"
+  def install
+    
+  test do
+  end
+end
