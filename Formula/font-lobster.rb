94b65c7aa6e296ddb5e69a2f75e78ef717ba5e0c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lobster.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-lobster.rb
@@ -0,0 +1,9 @@
+class FontLobster < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/lobster/Lobster-Regular.ttf"
+  desc "Lobster"
+  homepage "https://www.google.com/fonts/specimen/Lobster"
+  def install
+    
+  test do
+  end
+end
