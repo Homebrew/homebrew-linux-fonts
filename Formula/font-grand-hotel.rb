1e63477000f24315b73a2fa64c459349fc9f4853
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-grand-hotel.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-grand-hotel.rb
@@ -0,0 +1,9 @@
+class FontGrandHotel < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/grandhotel/GrandHotel-Regular.ttf"
+  desc "Grand Hotel"
+  homepage "https://www.google.com/fonts/specimen/Grand+Hotel"
+  def install
+    
+  test do
+  end
+end
