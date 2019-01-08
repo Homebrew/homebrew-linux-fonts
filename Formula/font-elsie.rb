c4ad5d4aca46069eafda528adc78fd211597adcf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-elsie.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-elsie.rb
@@ -0,0 +1,10 @@
+class FontElsie < Formula
+  head "https://github.com/google/fonts/trunk/ofl/elsie", using: :svn, revision: "50", trust_cert: true
+  desc "Elsie"
+  homepage "https://www.google.com/fonts/specimen/Elsie"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
