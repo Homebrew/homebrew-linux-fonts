d8ab0d3ca335af8d3782f8d89e7842b1c6e1cc3b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-goudy-bookletter1911.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-goudy-bookletter1911.rb
@@ -0,0 +1,9 @@
+class FontGoudyBookletter1911 < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/goudybookletter1911/GoudyBookletter1911.ttf"
+  desc "Goudy Bookletter 1911"
+  homepage "https://www.google.com/fonts/specimen/Goudy+Bookletter+1911"
+  def install
+    
+  test do
+  end
+end
