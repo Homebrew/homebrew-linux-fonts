e9a30b2af1fea4ac0323d04d6cbdeba1b228ae12
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-everson-mono.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-everson-mono.rb
@@ -0,0 +1,11 @@
+class FontEversonMono < Formula
+  version "7.0.0"
+  sha256 "a7196feb79faa1933ecbfc34579586d87addc208b7f6ac9dd93b92d64b0d81f5"
+  url "https://www.evertype.com/emono/evermono.zip"
+  desc "Everson Mono"
+  homepage "https://www.evertype.com/emono/"
+  def install
+    
+  test do
+  end
+end
