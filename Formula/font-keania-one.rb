d8ea0539409e4febb779aa49568983173bc6c2c4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:19 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-keania-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-keania-one.rb
@@ -0,0 +1,9 @@
+class FontKeaniaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/keaniaone/KeaniaOne-Regular.ttf"
+  desc "Keania One"
+  homepage "https://www.google.com/fonts/specimen/Keania+One"
+  def install
+    
+  test do
+  end
+end
