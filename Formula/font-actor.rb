e36e315af66241864eb595f1e803baed33b8b434
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:41:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-actor.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-actor.rb
@@ -0,0 +1,9 @@
+class FontActor < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/actor/Actor-Regular.ttf"
+  desc "Actor"
+  homepage "https://www.google.com/fonts/specimen/Actor"
+  def install
+    
+  test do
+  end
+end
