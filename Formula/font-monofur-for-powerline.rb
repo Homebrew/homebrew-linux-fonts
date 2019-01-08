1c8bfe47fb414f31c25b60f077c9de6e347a3de5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monofur-for-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-monofur-for-powerline.rb
@@ -0,0 +1,10 @@
+class FontMonofurForPowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/Monofur", using: :svn, trust_cert: true
+  desc "monofur for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/Monofur"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
