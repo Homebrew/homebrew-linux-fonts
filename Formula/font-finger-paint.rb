1ddbcf314c69db34a997cb3fa58319c067f26aca
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-finger-paint.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-finger-paint.rb
@@ -0,0 +1,9 @@
+class FontFingerPaint < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/fingerpaint/FingerPaint-Regular.ttf"
+  desc "Finger Paint"
+  homepage "https://www.google.com/fonts/specimen/Finger+Paint"
+  def install
+    
+  test do
+  end
+end
