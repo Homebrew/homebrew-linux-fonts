a564efd1d7b4caefd03b2a73ea3db909d7253b64
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ultra.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-ultra.rb
@@ -0,0 +1,9 @@
+class FontUltra < Formula
+  head "https://github.com/google/fonts/raw/master/apache/ultra/Ultra-Regular.ttf"
+  desc "Ultra"
+  homepage "https://www.google.com/fonts/specimen/Ultra"
+  def install
+    
+  test do
+  end
+end
