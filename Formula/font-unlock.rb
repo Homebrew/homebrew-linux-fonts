1f6f661abca6554780ed279943e1ab6a4e46ae50
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-unlock.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-unlock.rb
@@ -0,0 +1,9 @@
+class FontUnlock < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/unlock/Unlock-Regular.ttf"
+  desc "Unlock"
+  homepage "https://www.google.com/fonts/specimen/Unlock"
+  def install
+    
+  test do
+  end
+end
