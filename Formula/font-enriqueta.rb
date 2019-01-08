ab60c14292fb8498a9dc307e65e7e1a2a4e37526
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-enriqueta.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-enriqueta.rb
@@ -0,0 +1,10 @@
+class FontEnriqueta < Formula
+  head "https://github.com/google/fonts/trunk/ofl/enriqueta", using: :svn, revision: "50", trust_cert: true
+  desc "Enriqueta"
+  homepage "https://www.google.com/fonts/specimen/Enriqueta"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
