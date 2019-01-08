db9740784bd9d59c87aa93357efe39bc595ccbff
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-paprika.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-paprika.rb
@@ -0,0 +1,9 @@
+class FontPaprika < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/paprika/Paprika-Regular.ttf"
+  desc "Paprika"
+  homepage "https://www.google.com/fonts/specimen/Paprika"
+  def install
+    
+  test do
+  end
+end
