e37bdd7533a47bfde4a6851c528c09ab252edd8e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-for-powerline.rb from d06c17537689e97ce404b2d83e04c7bf08efa054

---
+++ Formula/font-inconsolata-for-powerline.rb
@@ -0,0 +1,9 @@
+class FontInconsolataForPowerline < Formula
+  head "https://github.com/powerline/fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf"
+  desc "Inconsolata for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/Inconsolata"
+  def install
+    
+  test do
+  end
+end
