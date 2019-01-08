4a7e439ffddd3c8309aac2aa2f34fdb5944c4bd7
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-liberation-sans.rb from 33b62e469afe5af66f9e8771d5bbf798bbafb31b

--- Formula/font-liberation-sans.rb
+++ Formula/font-liberation-sans.rb
@@ -1,7 +1,7 @@
 class FontLiberationSans < Formula
   version "2.00.1"
   sha256 "7890278a6cd17873c57d9cd785c2d230d9abdea837e96516019c5885dd271504"
-  head "https://releases.pagure.org/liberation-fonts/liberation-fonts-ttf-#{version}.tar.gz"
+  url "https://releases.pagure.org/liberation-fonts/liberation-fonts-ttf-#{version}.tar.gz"
   desc "Liberation Sans"
   homepage "https://pagure.io/liberation-fonts/"
   def install
