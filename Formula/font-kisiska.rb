83050ec076d4ec09de578f433256922069cb92c5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kisiska.rb from 19051fdf2775161975d51fa88147d3a8f7b20ec8

---
+++ Formula/font-kisiska.rb
@@ -0,0 +1,9 @@
+class FontKisiska < Formula
+  head "http://www.languagegeek.com/font/kisiska.zip"
+  desc "Kisiska"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
