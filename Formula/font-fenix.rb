14f0aa8616603042409169d6b7e1cb6d4ade70e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fenix.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-fenix.rb
@@ -0,0 +1,9 @@
+class FontFenix < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/fenix/Fenix-Regular.ttf"
+  desc "Fenix"
+  homepage "https://www.google.com/fonts/specimen/Fenix"
+  def install
+    
+  test do
+  end
+end
