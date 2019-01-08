6959837ec201e3cb1e223d2ad3ab8d53457ddbb5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-istok-web.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-istok-web.rb
@@ -0,0 +1,10 @@
+class FontIstokWeb < Formula
+  head "https://github.com/google/fonts/trunk/ofl/istokweb", using: :svn, revision: "50", trust_cert: true
+  desc "Istok Web"
+  homepage "https://www.google.com/fonts/specimen/Istok%20Web"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
