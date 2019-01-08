2626f292b423285ad482bae2b7498c539889fa1f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fruktur.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-fruktur.rb
@@ -0,0 +1,9 @@
+class FontFruktur < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/fruktur/Fruktur-Regular.ttf"
+  desc "Fruktur"
+  homepage "https://www.google.com/fonts/specimen/Fruktur"
+  def install
+    
+  test do
+  end
+end
