76198b75ba5db92ed31edc5530dff9271446f8fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-molle.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-molle.rb
@@ -0,0 +1,9 @@
+class FontMolle < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/molle/Molle-Regular.ttf"
+  desc "Molle"
+  homepage "https://www.google.com/fonts/specimen/Molle"
+  def install
+    
+  test do
+  end
+end
