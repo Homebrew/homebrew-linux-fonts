d36d26da8af6fd7e5b2fa0ca81aea9c94249b6bb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-atomic-age.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-atomic-age.rb
@@ -0,0 +1,9 @@
+class FontAtomicAge < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/atomicage/AtomicAge-Regular.ttf"
+  desc "Atomic Age"
+  homepage "https://www.google.com/fonts/specimen/Atomic+Age"
+  def install
+    
+  test do
+  end
+end
