d20f1ada0cfd3d715cc1aad8cd91e60a44cddd3d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-andale-mono.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-andale-mono.rb
@@ -0,0 +1,9 @@
+class FontAndaleMono < Formula
+  head "https://downloads.sourceforge.net/corefonts/andale32.exe"
+  desc "Andale Mono"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  def install
+    
+  test do
+  end
+end
