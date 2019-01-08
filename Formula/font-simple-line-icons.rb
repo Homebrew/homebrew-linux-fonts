a56cac06d78131a842ca528d06d8127667c3a8d6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-simple-line-icons.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-simple-line-icons.rb
@@ -0,0 +1,11 @@
+class FontSimpleLineIcons < Formula
+  version "2.4.1"
+  sha256 "fd0e9edd1550f8f8ded705160f0e433aae4170fe9f21a595eb583ac54c7e2d12"
+  url "https://github.com/thesabbir/simple-line-icons/archive/#{version}.zip"
+  desc "Simple Line Icons"
+  homepage "http://simplelineicons.com/"
+  def install
+    
+  test do
+  end
+end
