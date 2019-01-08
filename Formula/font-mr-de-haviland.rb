62fcfdd2702203d5678b7f2dc42d28507bc7cd37
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mr-de-haviland.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-mr-de-haviland.rb
@@ -0,0 +1,9 @@
+class FontMrDeHaviland < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/mrdehaviland/MrDeHaviland-Regular.ttf"
+  desc "Mr De Haviland"
+  homepage "https://www.google.com/fonts/specimen/Mr+De+Haviland"
+  def install
+    
+  test do
+  end
+end
