f63f5e16fc339b6354e015823803a1219fc9d2b3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tamil.rb from 429fe80b45ad0fcc4027e2cc38e6908c61e6d571

---
+++ Formula/font-noto-sans-tamil.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTamil < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTamil-unhinted.zip"
+  desc "Noto Sans Tamil"
+  homepage "https://www.google.com/get/noto/#sans-taml"
+  def install
+    
+  test do
+  end
+end
