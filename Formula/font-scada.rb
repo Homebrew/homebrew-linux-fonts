31bc9c52f209b4bf7ea1ba8241c8ba181e5dc781
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-scada.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-scada.rb
@@ -0,0 +1,10 @@
+class FontScada < Formula
+  head "https://github.com/google/fonts/trunk/ofl/scada", using: :svn, revision: "50", trust_cert: true
+  desc "Scada"
+  homepage "https://www.google.com/fonts/specimen/Scada"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
