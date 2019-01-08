dac5af9109dd79f29070e5f045a0521eb5fca1c4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-canter.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-canter.rb
@@ -0,0 +1,9 @@
+class FontCanter < Formula
+  head "https://fontfabric.com/downfont/canter.zip"
+  desc "Canter"
+  homepage "https://fontfabric.com/canter-free-font/"
+  def install
+    
+  test do
+  end
+end
