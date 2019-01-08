4244da9d561255f29a1b6c397fba1dbd9b6ca0b8
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-camingocode.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-camingocode.rb
+++ Formula/font-camingocode.rb
@@ -1,7 +1,7 @@
 class FontCamingocode < Formula
   version "1.000"
   sha256 "13b188ca8d1b5fe2c3ba880771f20a6faac550405ae98d12edd46240f8896714"
-  head "https://janfromm.de/_data/downloads/CamingoCode-v1.0.zip"
+  url "https://janfromm.de/_data/downloads/CamingoCode-v1.0.zip"
   desc "CamingoCode"
   homepage "https://www.janfromm.de/typefaces/camingomono/camingocode/"
   def install
