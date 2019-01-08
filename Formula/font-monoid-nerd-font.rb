104f506b5f4f7cb578ef07ef50b9073e17d60354
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:56 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monoid-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-monoid-nerd-font.rb
+++ Formula/font-monoid-nerd-font.rb
@@ -1,7 +1,7 @@
 class FontMonoidNerdFont < Formula
   version "2.0.0"
   sha256 "ad63efadd67364f2e20eb1d0c387927dd0d638899036a70b4a06ae7842178d48"
-  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Monoid.zip"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Monoid.zip"
   desc "Monoid Nerd Font (Monoid)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
