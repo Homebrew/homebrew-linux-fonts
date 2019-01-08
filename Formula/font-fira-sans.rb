95bd5b39985ad281fb7dd7f20dcc660c023405bf
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:53 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-sans.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-fira-sans.rb
+++ Formula/font-fira-sans.rb
@@ -1,7 +1,7 @@
 class FontFiraSans < Formula
   version "4.202"
   sha256 "d86269657387f144d77ba12011124f30f423f70672e1576dc16f918bb16ddfe4"
-  head "https://github.com/mozilla/Fira/archive/#{version}.tar.gz"
+  url "https://github.com/mozilla/Fira/archive/#{version}.tar.gz"
   desc "Fira Sans"
   homepage "https://mozilla.github.io/Fira/"
   def install
