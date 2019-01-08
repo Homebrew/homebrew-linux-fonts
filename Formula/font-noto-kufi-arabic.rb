9ced0ac5881d2b49e4fe9a07da8d6d4d16fa93e1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-kufi-arabic.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-kufi-arabic.rb
@@ -0,0 +1,9 @@
+class FontNotoKufiArabic < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoKufiArabic-unhinted.zip"
+  desc "Noto Kufi Arabic"
+  homepage "https://www.google.com/get/noto/#kufi-arab"
+  def install
+    
+  test do
+  end
+end
