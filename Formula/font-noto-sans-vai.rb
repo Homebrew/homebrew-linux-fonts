a0ff360ce0565343159c171a5dbbda45131aabd5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-vai.rb from 45cd631147cee78c9cf83f81cc0be2270a59d7eb

---
+++ Formula/font-noto-sans-vai.rb
@@ -0,0 +1,9 @@
+class FontNotoSansVai < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansVai-unhinted.zip"
+  desc "Noto Sans Vai"
+  homepage "https://www.google.com/get/noto/#sans-vaii"
+  def install
+    
+  test do
+  end
+end
