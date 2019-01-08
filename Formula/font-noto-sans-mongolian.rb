f7498282a0ce8d5142a5524db55c3d9f10e532a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-mongolian.rb from 8167a1389c5c625d034671ce92e82afabbbdb995

---
+++ Formula/font-noto-sans-mongolian.rb
@@ -0,0 +1,9 @@
+class FontNotoSansMongolian < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMongolian-unhinted.zip"
+  desc "Noto Sans Mongolian"
+  homepage "https://www.google.com/get/noto/#sans-mong"
+  def install
+    
+  test do
+  end
+end
