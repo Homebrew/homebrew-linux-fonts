791cecfe232d28f168f46ef5d83a204207c62c60
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-great-primer-sc.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-im-fell-great-primer-sc.rb
@@ -0,0 +1,9 @@
+class FontImFellGreatPrimerSc < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/imfellgreatprimersc/IMFeGPsc28P.ttf"
+  desc "IM Fell Great Primer SC"
+  homepage "https://www.google.com/fonts/specimen/IM+Fell+Great+Primer+SC"
+  def install
+    
+  test do
+  end
+end
