443dd59129af120f9d5f445140e3c40212b698ca
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-forum.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-forum.rb
@@ -0,0 +1,9 @@
+class FontForum < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/forum/Forum-Regular.ttf"
+  desc "Forum"
+  homepage "https://www.google.com/fonts/specimen/Forum"
+  def install
+    
+  test do
+  end
+end
