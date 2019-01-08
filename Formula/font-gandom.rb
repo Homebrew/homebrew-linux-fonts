560750122fe517e98db6e476b04829ebb4b975f6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gandom.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-gandom.rb
@@ -0,0 +1,11 @@
+class FontGandom < Formula
+  version "0.5.1"
+  sha256 "87fea02baa89524b8cf2873a08c28fddd35dcf3558110c13522328b0cbefaa62"
+  url "https://github.com/rastikerdar/gandom-font/releases/download/v#{version}/gandom-font-v#{version}.zip"
+  desc "Gandom"
+  homepage "https://rastikerdar.github.io/gandom-font"
+  def install
+    
+  test do
+  end
+end
