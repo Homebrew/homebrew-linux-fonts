a4a88f85c8038f78f0e86c2cc7ae05482249e7d5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marta.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-marta.rb
@@ -0,0 +1,9 @@
+class FontMarta < Formula
+  head "https://fontfabric.com/downfont/marta.zip"
+  desc "Marta"
+  homepage "https://fontfabric.com/marta-free-font/"
+  def install
+    
+  test do
+  end
+end
