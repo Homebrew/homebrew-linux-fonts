8d05602a241606d8cfd1af657800060bf311cda2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cantora-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-cantora-one.rb
@@ -0,0 +1,9 @@
+class FontCantoraOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/cantoraone/CantoraOne-Regular.ttf"
+  desc "Cantora One"
+  homepage "https://www.google.com/fonts/specimen/Cantora+One"
+  def install
+    
+  test do
+  end
+end
