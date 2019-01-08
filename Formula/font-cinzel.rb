b3d993b21cb7c8ec513d25aa4f276fff3e747fe6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cinzel.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-cinzel.rb
@@ -0,0 +1,10 @@
+class FontCinzel < Formula
+  head "https://github.com/google/fonts/trunk/ofl/cinzel", using: :svn, revision: "50", trust_cert: true
+  desc "Cinzel"
+  homepage "https://www.google.com/fonts/specimen/Cinzel"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
