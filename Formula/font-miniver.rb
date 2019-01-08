b3416d348c5e91137a4f0bf4a5a1a9b57d10f6cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-miniver.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-miniver.rb
@@ -0,0 +1,9 @@
+class FontMiniver < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/miniver/Miniver-Regular.ttf"
+  desc "Miniver"
+  homepage "https://www.google.com/fonts/specimen/Miniver"
+  def install
+    
+  test do
+  end
+end
