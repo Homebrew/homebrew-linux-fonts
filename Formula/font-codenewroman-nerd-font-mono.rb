cb9e6936cd1ee8c512de324f10fd89c913ffe7f8
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-codenewroman-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-codenewroman-nerd-font-mono.rb
+++ Formula/font-codenewroman-nerd-font-mono.rb
@@ -1,7 +1,7 @@
 class FontCodenewromanNerdFontMono < Formula
   version "1.2.0"
   sha256 "b4302eafd46bb7ffa4a5e349d298e2cfd68a82c02c68e9d2235f4d4d383668a3"
-  head "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/CodeNewRoman.zip"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/CodeNewRoman.zip"
   desc "CodeNewRoman Nerd Font (CodeNewRoman)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
