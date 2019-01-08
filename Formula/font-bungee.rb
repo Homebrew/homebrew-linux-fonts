fefd7716ac77cd1452c54a25ae88b44ab91cb12b
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bungee.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-bungee.rb
+++ Formula/font-bungee.rb
@@ -1,7 +1,7 @@
 class FontBungee < Formula
   version "1.1.0"
   sha256 "d012a9e6293201c3165feba64342d29c42bb4e67b1cc66e07509c12bab760a6f"
-  url "https://github.com/djrrb/bungee/releases/download/#{version}/Bungee-fonts.zip"
+  head "https://github.com/djrrb/bungee/releases/download/#{version}/Bungee-fonts.zip"
   desc "Bungee"
   homepage "https://djr.com/bungee/"
   def install
