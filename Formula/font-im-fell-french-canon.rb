63e14e0a7c702d0b66fbf273295fe2570855891e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:37 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-french-canon.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-im-fell-french-canon.rb
@@ -0,0 +1,10 @@
+class FontImFellFrenchCanon < Formula
+  head "https://github.com/google/fonts/trunk/ofl/imfellfrenchcanon", using: :svn, revision: "50", trust_cert: true
+  desc "IM Fell French Canon"
+  homepage "https://www.google.com/fonts/specimen/IM%20Fell%20French%20Canon"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
