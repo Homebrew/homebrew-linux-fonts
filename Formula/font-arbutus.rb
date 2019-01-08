5790b7a970d35b95296e07ea79d2cc61a8b60861
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:56 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arbutus.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-arbutus.rb
@@ -0,0 +1,9 @@
+class FontArbutus < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/arbutus/Arbutus-Regular.ttf"
+  desc "Arbutus"
+  homepage "https://www.google.com/fonts/specimen/Arbutus"
+  def install
+    
+  test do
+  end
+end
