52cb6d6fc487f4c9416924a15f64a5c7a4542ff5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-voces.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-voces.rb
@@ -0,0 +1,9 @@
+class FontVoces < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/voces/Voces-Regular.ttf"
+  desc "Voces"
+  homepage "https://www.google.com/fonts/specimen/Voces"
+  def install
+    
+  test do
+  end
+end
