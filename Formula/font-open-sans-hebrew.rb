15b00b7e195f1b406611fc79d270aba3ad139b17
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-open-sans-hebrew.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-open-sans-hebrew.rb
@@ -0,0 +1,10 @@
+class FontOpenSansHebrew < Formula
+  head "https://github.com/google/fonts/trunk/apache/opensanshebrew", using: :svn, revision: "50", trust_cert: true
+  desc "Open Sans Hebrew"
+  homepage "https://www.google.com/fonts/earlyaccess"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
