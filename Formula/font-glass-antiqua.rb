dccd7c3890c8e2160fc5a39605b1bb14fbd14527
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-glass-antiqua.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-glass-antiqua.rb
@@ -0,0 +1,9 @@
+class FontGlassAntiqua < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/glassantiqua/GlassAntiqua-Regular.ttf"
+  desc "Glass Antiqua"
+  homepage "https://www.google.com/fonts/specimen/Glass+Antiqua"
+  def install
+    
+  test do
+  end
+end
