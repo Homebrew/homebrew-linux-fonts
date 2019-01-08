afb4311d1b0a9f17c227bf29cae3dd4af2f12edc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-cherokee.rb from 91ec7fe0323c8467fb529c8559b961db2070ea2c

---
+++ Formula/font-noto-sans-cherokee.rb
@@ -0,0 +1,9 @@
+class FontNotoSansCherokee < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCherokee-unhinted.zip"
+  desc "Noto Sans Cherokee"
+  homepage "https://www.google.com/get/noto/#sans-cher"
+  def install
+    
+  test do
+  end
+end
