5c2a5b22547a67642953574bfa8b038a32dc6107
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-andada.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-andada.rb
@@ -0,0 +1,10 @@
+class FontAndada < Formula
+  head "https://github.com/google/fonts/trunk/ofl/andada", using: :svn, revision: "50", trust_cert: true
+  desc "Andada"
+  homepage "https://www.google.com/fonts/specimen/Andada"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
