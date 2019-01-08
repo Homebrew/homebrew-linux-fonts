fa15b762516a0b95e9e4289b2d13ebb0fd73239b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-lao.rb from 7ba99eb0d220c5dcbd47bbd6186bede514f0d71a

---
+++ Formula/font-noto-serif-lao.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifLao < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifLao-unhinted.zip"
+  desc "Noto Serif Lao"
+  homepage "https://www.google.com/get/noto/#serif-laoo"
+  def install
+    
+  test do
+  end
+end
