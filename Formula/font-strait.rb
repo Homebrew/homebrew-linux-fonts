1e658f142c17caa737bfc2e52c5c7f296af02612
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-strait.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-strait.rb
@@ -0,0 +1,9 @@
+class FontStrait < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/strait/Strait-Regular.ttf"
+  desc "Strait"
+  homepage "https://www.google.com/fonts/specimen/Strait"
+  def install
+    
+  test do
+  end
+end
