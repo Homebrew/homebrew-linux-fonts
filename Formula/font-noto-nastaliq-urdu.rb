59f217c6184e84e83816c0e9a604c9304a3c9585
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-nastaliq-urdu.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-nastaliq-urdu.rb
@@ -0,0 +1,9 @@
+class FontNotoNastaliqUrdu < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoNastaliqUrdu-unhinted.zip"
+  desc "Noto Nastaliq Urdu"
+  homepage "https://www.google.com/get/noto/#nastaliq-aran"
+  def install
+    
+  test do
+  end
+end
