aff2c1b78f8a9f08ade989cbad5c9aa9a999325f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-average-mono.rb from 5a773efe8bd59a705fae99157c0ddac14f20c8ea

---
+++ Formula/font-average-mono.rb
@@ -0,0 +1,9 @@
+class FontAverageMono < Formula
+  head "https://fontlibrary.org/assets/downloads/average-mono/7e3ab639a72edd95bd08537df255440d/average-mono.zip"
+  desc "Average Mono"
+  homepage "https://fontlibrary.org/en/font/average-mono"
+  def install
+    
+  test do
+  end
+end
