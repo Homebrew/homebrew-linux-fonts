774c3b5deb3bce9da3d9cbc6c02d1a494333f2c4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chewy.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-chewy.rb
@@ -0,0 +1,9 @@
+class FontChewy < Formula
+  head "https://github.com/google/fonts/raw/master/apache/chewy/Chewy-Regular.ttf"
+  desc "Chewy"
+  homepage "https://www.google.com/fonts/specimen/Chewy"
+  def install
+    
+  test do
+  end
+end
