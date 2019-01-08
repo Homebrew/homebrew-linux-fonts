e85e683077894151730e61b0d5d03d10649302bf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-port-lligat-slab.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-port-lligat-slab.rb
@@ -0,0 +1,9 @@
+class FontPortLligatSlab < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/portlligatslab/PortLligatSlab-Regular.ttf"
+  desc "Port Lligat Slab"
+  homepage "https://www.google.com/fonts/specimen/Port+Lligat+Slab"
+  def install
+    
+  test do
+  end
+end
