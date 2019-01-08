07b116e0f154dbd311935b826e1564a6113ba21b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-symbols2.rb from a877d71f000c4cd26ee9ddccfec7d06085e2aa0a

---
+++ Formula/font-noto-sans-symbols2.rb
@@ -0,0 +1,9 @@
+class FontNotoSansSymbols2 < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSymbols2-unhinted.zip"
+  desc "Noto Sans Symbols2"
+  homepage "https://www.google.com/get/noto/#sans-sym2"
+  def install
+    
+  test do
+  end
+end
