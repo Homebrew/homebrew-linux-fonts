9af8a852620d64d66399bf0455cb3a2144986425
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-space-mono.rb from cb406b6bd3096ad5c16e4e1c1edcc3badf75a4a5

---
+++ Formula/font-space-mono.rb
@@ -0,0 +1,9 @@
+class FontSpaceMono < Formula
+  head "https://github.com/googlefonts/spacemono/archive/master.zip"
+  desc "Space Mono"
+  homepage "https://github.com/googlefonts/spacemono"
+  def install
+    
+  test do
+  end
+end
