a2ed4abb8457c749237f3d133fc50e17b98b8f9a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-osage.rb from a877d71f000c4cd26ee9ddccfec7d06085e2aa0a

---
+++ Formula/font-noto-sans-osage.rb
@@ -0,0 +1,9 @@
+class FontNotoSansOsage < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOsage-unhinted.zip"
+  desc "Noto Sans Osage"
+  homepage "https://www.google.com/get/noto/#sans-osge"
+  def install
+    
+  test do
+  end
+end
