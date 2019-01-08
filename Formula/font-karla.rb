bac5d8498474d06cf1876f302641627e57e14a4f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-karla.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-karla.rb
@@ -0,0 +1,10 @@
+class FontKarla < Formula
+  head "https://github.com/google/fonts/trunk/ofl/karla", using: :svn, revision: "50", trust_cert: true
+  desc "Karla"
+  homepage "https://www.google.com/fonts/specimen/Karla"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
