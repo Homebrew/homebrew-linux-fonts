5306f042a775e367810a5372f7fa57f602fca9d5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-megrim.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-megrim.rb
@@ -0,0 +1,9 @@
+class FontMegrim < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/megrim/Megrim.ttf"
+  desc "Megrim"
+  homepage "https://www.google.com/fonts/specimen/Megrim"
+  def install
+    
+  test do
+  end
+end
