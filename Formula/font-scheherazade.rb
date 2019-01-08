6053174acfee0284508676d6fc9279978bb7299a
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:22 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-scheherazade.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-scheherazade.rb
+++ Formula/font-scheherazade.rb
@@ -1,7 +1,7 @@
 class FontScheherazade < Formula
   version "2.020"
   sha256 "d8bb12997507937347efba4e59550a9655350563d1df662f0651e04ba916aff1"
-  url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Scheherazade-#{version}.zip&filename=Scheherazade-#{version}.zip"
+  head "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Scheherazade-#{version}.zip&filename=Scheherazade-#{version}.zip"
   desc "Scheherazade "
   homepage "https://scripts.sil.org/Scheherazade"
   def install
