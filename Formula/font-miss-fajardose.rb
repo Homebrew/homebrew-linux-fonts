d83c6f81fadb22205e28abc720f3c6b5479a50b1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-miss-fajardose.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-miss-fajardose.rb
@@ -0,0 +1,9 @@
+class FontMissFajardose < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/missfajardose/MissFajardose-Regular.ttf"
+  desc "Miss Fajardose"
+  homepage "https://www.google.com/fonts/specimen/Miss+Fajardose"
+  def install
+    
+  test do
+  end
+end
