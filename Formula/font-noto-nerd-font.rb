0d6acbe828215e57d25a31c2f54725ed3821dd8c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-nerd-font.rb from 83b8b465f69e9dd3727fdbce277eabe5e16c0213

---
+++ Formula/font-noto-nerd-font.rb
@@ -0,0 +1,11 @@
+class FontNotoNerdFont < Formula
+  version "2.0.0"
+  sha256 "47598f6b3f7a21c27cb46510f14fe6b3490f1d636799a75dae23a2250db0cadd"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Noto.zip"
+  desc "Noto Nerd Font"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
