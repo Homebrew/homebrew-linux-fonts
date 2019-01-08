a97eed7162a40ffe67690033b58ae371e86a2f61
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-domine.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-domine.rb
@@ -0,0 +1,10 @@
+class FontDomine < Formula
+  head "https://github.com/google/fonts/trunk/ofl/domine", using: :svn, revision: "50", trust_cert: true
+  desc "Domine"
+  homepage "https://www.google.com/fonts/specimen/Domine"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
