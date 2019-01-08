6fcfd18837ed0bad51577b1cbd2738ef1eda44bc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lobster-two.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-lobster-two.rb
@@ -0,0 +1,10 @@
+class FontLobsterTwo < Formula
+  head "https://github.com/google/fonts/trunk/ofl/lobstertwo", using: :svn, revision: "50", trust_cert: true
+  desc "Lobster Two"
+  homepage "https://www.google.com/fonts/specimen/Lobster%20Two"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
