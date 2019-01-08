8f810385099e4d316d477619ce63157058ed198d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-doulos-sil.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-doulos-sil.rb
@@ -0,0 +1,11 @@
+class FontDoulosSil < Formula
+  version "5.000"
+  sha256 "0b309c3db813a98ce884c0bd25c7f5c0bd96bbffd076459e39298812ca22472e"
+  url "https://software.sil.org/downloads/r/doulos/DoulosSIL-#{version}.zip"
+  desc "Doulos SIL"
+  homepage "https://software.sil.org/doulos/"
+  def install
+    
+  test do
+  end
+end
