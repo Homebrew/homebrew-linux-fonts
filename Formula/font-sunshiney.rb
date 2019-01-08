55dd44934a83b2274d6479b115cc880118ead847
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sunshiney.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sunshiney.rb
@@ -0,0 +1,9 @@
+class FontSunshiney < Formula
+  head "https://github.com/google/fonts/raw/master/apache/sunshiney/Sunshiney-Regular.ttf"
+  desc "Sunshiney"
+  homepage "https://www.google.com/fonts/specimen/Sunshiney"
+  def install
+    
+  test do
+  end
+end
