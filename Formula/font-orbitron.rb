c0a67caf4cec194038a6cac0c9e6294bc86252e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:47 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-orbitron.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-orbitron.rb
@@ -0,0 +1,10 @@
+class FontOrbitron < Formula
+  head "https://github.com/google/fonts/trunk/ofl/orbitron", using: :svn, revision: "50", trust_cert: true
+  desc "Orbitron"
+  homepage "https://www.google.com/fonts/specimen/Orbitron"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
