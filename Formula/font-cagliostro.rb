dad873cb578fc205120434302f038b32b1dae30a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cagliostro.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-cagliostro.rb
@@ -0,0 +1,9 @@
+class FontCagliostro < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/cagliostro/Cagliostro-Regular.ttf"
+  desc "Cagliostro"
+  homepage "https://www.google.com/fonts/specimen/Cagliostro"
+  def install
+    
+  test do
+  end
+end
