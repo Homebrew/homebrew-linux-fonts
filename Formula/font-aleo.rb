520c56e462389f45329fe6b7840850fec3651b6d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aleo.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-aleo.rb
@@ -0,0 +1,11 @@
+class FontAleo < Formula
+  version "1.2.2"
+  sha256 "64e98e1063da945086b4685535f0b3d463efbc1409e18bddf925624bf27b7e57"
+  url "https://fontfabric.com/downfont/aleo.zip"
+  desc "Aleo"
+  homepage "https://fontfabric.com/aleo-free-font/"
+  def install
+    
+  test do
+  end
+end
