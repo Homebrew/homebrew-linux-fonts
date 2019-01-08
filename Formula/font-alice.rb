aa344d779a399bdec74328395375b6daae38d6a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alice.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-alice.rb
@@ -0,0 +1,9 @@
+class FontAlice < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/alice/Alice-Regular.ttf"
+  desc "Alice"
+  homepage "https://www.google.com/fonts/specimen/Alice"
+  def install
+    
+  test do
+  end
+end
