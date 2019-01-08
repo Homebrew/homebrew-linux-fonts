aac56e39ba51eec7c4730007cd1d908fddcdf85e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:29 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-coustard.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-coustard.rb
@@ -0,0 +1,10 @@
+class FontCoustard < Formula
+  head "https://github.com/google/fonts/trunk/ofl/coustard", using: :svn, revision: "50", trust_cert: true
+  desc "Coustard"
+  homepage "https://www.google.com/fonts/specimen/Coustard"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
