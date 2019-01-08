9cac2e59387cf7afef47838a914ffd6865148498
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rochester.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-rochester.rb
@@ -0,0 +1,9 @@
+class FontRochester < Formula
+  head "https://github.com/google/fonts/raw/master/apache/rochester/Rochester-Regular.ttf"
+  desc "Rochester"
+  homepage "https://www.google.com/fonts/specimen/Rochester"
+  def install
+    
+  test do
+  end
+end
