7767a6b5295a2bebfd64223cbfca9d0fd7afc7e6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-russo-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-russo-one.rb
@@ -0,0 +1,9 @@
+class FontRussoOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/russoone/RussoOne-Regular.ttf"
+  desc "Russo One"
+  homepage "https://www.google.com/fonts/specimen/Russo+One"
+  def install
+    
+  test do
+  end
+end
