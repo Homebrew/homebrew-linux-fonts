49c79cf4c222ab60e8cb78d6f96c0d5bbadfb0d8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-phetsarath.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-phetsarath.rb
@@ -0,0 +1,10 @@
+class FontPhetsarath < Formula
+  head "https://github.com/google/fonts/trunk/ofl/phetsarath", using: :svn, revision: "50", trust_cert: true
+  desc "Phetsarath"
+  homepage "https://www.google.com/fonts/earlyaccess"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
