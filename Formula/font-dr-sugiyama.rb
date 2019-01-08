f1b945c84f6f5bbfe9d671e55fe2ad79bd18ea19
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dr-sugiyama.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-dr-sugiyama.rb
@@ -0,0 +1,9 @@
+class FontDrSugiyama < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/drsugiyama/DrSugiyama-Regular.ttf"
+  desc "Dr Sugiyama"
+  homepage "https://www.google.com/fonts/specimen/Dr+Sugiyama"
+  def install
+    
+  test do
+  end
+end
