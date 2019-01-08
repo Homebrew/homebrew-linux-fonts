93a937daed74900665cf8dfc7f2db883ef1b2695
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-carrois-gothic-sc.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-carrois-gothic-sc.rb
@@ -0,0 +1,9 @@
+class FontCarroisGothicSc < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/carroisgothicsc/CarroisGothicSC-Regular.ttf"
+  desc "Carrois Gothic SC"
+  homepage "https://www.google.com/fonts/specimen/Carrois+Gothic+SC"
+  def install
+    
+  test do
+  end
+end
