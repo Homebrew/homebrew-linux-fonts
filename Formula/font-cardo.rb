158686bf5443cbd77bbcf808568386b2f38bf8b7
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cardo.rb from df38f9cc902adca4f7db9eff17ad1814f426e4a1

--- Formula/font-cardo.rb
+++ Formula/font-cardo.rb
@@ -1,7 +1,7 @@
 class FontCardo < Formula
   version "1.04"
   sha256 "9401db6357cb71fa1f8791323679f81d6b0473d6280a7ec8abdf11b5e52f455f"
-  url "http://scholarsfonts.net/cardo#{version.to_s.gsub(".", "")}.zip"
+  head "http://scholarsfonts.net/cardo#{version.no_dots}.zip"
   desc "Cardo"
   homepage "http://scholarsfonts.net/cardofnt.html"
   def install
