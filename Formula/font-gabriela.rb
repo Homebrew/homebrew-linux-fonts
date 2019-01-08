c1e177b5c92ab493f34d5ed41bbf446c89c54acb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gabriela.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-gabriela.rb
@@ -0,0 +1,9 @@
+class FontGabriela < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/gabriela/Gabriela-Regular.ttf"
+  desc "Gabriela"
+  homepage "https://www.google.com/fonts/specimen/Gabriela"
+  def install
+    
+  test do
+  end
+end
