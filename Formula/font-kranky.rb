ecf9067a98ff846b9e8ad063616a9511a7542436
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kranky.rb from 769660f3ccc192d97cddd0943f066b79ee88a6be

---
+++ Formula/font-kranky.rb
@@ -0,0 +1,9 @@
+class FontKranky < Formula
+  head "https://github.com/google/fonts/raw/master/apache/kranky/Kranky-Regular.ttf"
+  desc "Kranky"
+  homepage "https://www.google.com/fonts/specimen/Kranky"
+  def install
+    
+  test do
+  end
+end
