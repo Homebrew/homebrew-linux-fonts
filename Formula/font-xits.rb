81696c88fa29d32e5f4d1ab36a2f85c8e2dc99cd
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-xits.rb from c96007b21362dcc8b77f86001c5644f950e8f321

---
+++ Formula/font-xits.rb
@@ -0,0 +1,11 @@
+class FontXits < Formula
+  version "1.200"
+  sha256 "039493b95b86d15e451e818e065e6f6719977a31e43504eb6946513ba09c8a72"
+  url "https://github.com/khaledhosny/xits/archive/v#{version}.zip"
+  desc "XITS"
+  homepage "https://github.com/khaledhosny/xits"
+  def install
+    
+  test do
+  end
+end
