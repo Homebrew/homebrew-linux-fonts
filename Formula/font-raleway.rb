f8baab1807b346f1bcbfcf23c285cd602dc86f32
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-raleway.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-raleway.rb
@@ -0,0 +1,10 @@
+class FontRaleway < Formula
+  head "https://github.com/google/fonts/trunk/ofl/raleway", using: :svn, revision: "50", trust_cert: true
+  desc "Raleway"
+  homepage "https://www.google.com/fonts/specimen/Raleway"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
