d8e639b5ed91be1ee374c34d67d457ac1f7710a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aurulentsansmono-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-aurulentsansmono-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontAurulentsansmonoNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "916a4a74f895587578cb5b2d55c37e513a87a15ce65c394e99f9c95a04b60fba"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
+  desc "AurulentSansMono Nerd Font (AurulentSansMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
