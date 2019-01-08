a7e962ee456c47a64a8346596523e6cc05150150
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-bonum.rb from b00d472f0de1f6b6e0eb023b94b377fbaafb51aa

---
+++ Formula/font-tex-gyre-bonum.rb
@@ -0,0 +1,11 @@
+class FontTexGyreBonum < Formula
+  version "2.004"
+  sha256 "30e7e9bf24b73709edff9916c95214a66b7fac38f78a11a19e4bc18ab019d398"
+  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/bonum/qbk#{version}otf.zip"
+  desc "TeX Gyre Bonum"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/bonum"
+  def install
+    
+  test do
+  end
+end
