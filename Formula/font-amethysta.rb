a024acb185769b24158bda6f50800e7a95b76a9f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-amethysta.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-amethysta.rb
@@ -0,0 +1,9 @@
+class FontAmethysta < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/amethysta/Amethysta-Regular.ttf"
+  desc "Amethysta"
+  homepage "https://www.google.com/fonts/specimen/Amethysta"
+  def install
+    
+  test do
+  end
+end
