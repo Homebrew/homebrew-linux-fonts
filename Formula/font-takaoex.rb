a0f0ea4f331093bcb99df89ed37a36c6d8b6cfc3
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:59 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-takaoex.rb from ad5b894f7c4be5d2faaefeaa50632bc2213c1a52

--- Formula/font-takaoex.rb
+++ Formula/font-takaoex.rb
@@ -1,7 +1,7 @@
 class FontTakaoex < Formula
   version "00201.01"
   sha256 "6af43928cb4d9943cbd5565079d1dfed60aef20badf4e99c3a1edc0e6a07d42b"
-  head "https://launchpad.net/takao-fonts/trunk/15.03/+download/TakaoExFonts_#{version}.zip"
+  url "https://launchpad.net/takao-fonts/trunk/15.03/+download/TakaoExFonts_#{version}.zip"
   desc "TakaoEx"
   homepage "https://launchpad.net/takao-fonts"
   def install
