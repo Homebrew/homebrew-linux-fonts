0479d0b1215bb745f3f786ff46e990cadf7f96a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rupakara.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-rupakara.rb
@@ -0,0 +1,11 @@
+class FontRupakara < Formula
+  version "1.0.4"
+  sha256 "d6ad49842444846ad0ef2ea40d471533993a3137a7aceacbb28b5a8d89aaddc0"
+  url "https://www.evertype.com/fonts/rupakara/rupakara.zip"
+  desc "Rupakara"
+  homepage "https://www.evertype.com/fonts/rupakara/"
+  def install
+    
+  test do
+  end
+end
