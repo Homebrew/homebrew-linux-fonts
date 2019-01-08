157277c00005e70e60a69e0934be82592bc3ce46
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-telex.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-telex.rb
@@ -0,0 +1,9 @@
+class FontTelex < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/telex/Telex-Regular.ttf"
+  desc "Telex"
+  homepage "https://www.google.com/fonts/specimen/Telex"
+  def install
+    
+  test do
+  end
+end
