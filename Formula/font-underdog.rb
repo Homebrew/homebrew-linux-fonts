cda2314e0da66a64399e1ca74035902d1bc7412e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-underdog.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-underdog.rb
@@ -0,0 +1,9 @@
+class FontUnderdog < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/underdog/Underdog-Regular.ttf"
+  desc "Underdog"
+  homepage "https://www.google.com/fonts/specimen/Underdog"
+  def install
+    
+  test do
+  end
+end
