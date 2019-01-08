185ea7a1dba3868e116d4ed05de70a18df1da2d9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jockey-one.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-jockey-one.rb
@@ -0,0 +1,9 @@
+class FontJockeyOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/jockeyone/JockeyOne-Regular.ttf"
+  desc "Jockey One"
+  homepage "https://www.google.com/fonts/specimen/Jockey+One"
+  def install
+    
+  test do
+  end
+end
