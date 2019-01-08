5257692636d2fedae44e0afd895478dc39bc242a
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:58:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-poly.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-poly.rb
@@ -0,0 +1,12 @@
+class FontPoly < Formula
+  head "https://github.com/google/fonts/trunk/ofl/poly", using: :svn, revision: "50", trust_cert: true
+  desc "Poly"
+  homepage "https://www.google.com/fonts/specimen/Poly"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "Poly-Italic.ttf"
+    (share/"fonts").install "Poly-Regular.ttf"
+  end
+  test do
+  end
+end
