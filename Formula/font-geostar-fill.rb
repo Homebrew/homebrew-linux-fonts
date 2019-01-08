f133ced6a8e32de9c476a84f071749da175578cd
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-geostar-fill.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-geostar-fill.rb
@@ -0,0 +1,9 @@
+class FontGeostarFill < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/geostarfill/GeostarFill-Regular.ttf"
+  desc "Geostar Fill"
+  homepage "https://www.google.com/fonts/specimen/Geostar+Fill"
+  def install
+    
+  test do
+  end
+end
