a5de6d919699b3bb4210c7111357ab4aa52cfba5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ricty-diminished.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-ricty-diminished.rb
@@ -0,0 +1,11 @@
+class FontRictyDiminished < Formula
+  version "4.0.1"
+  sha256 "b32ba2877651df31914797cc4938b98d726d4777ecb2064c9dbbb620efcff24f"
+  url "https://www.rs.tus.ac.jp/yyusa/ricty_diminished/ricty_diminished-#{version}.tar.gz"
+  desc "Ricty Diminished"
+  homepage "https://www.rs.tus.ac.jp/yyusa/ricty_diminished.html"
+  def install
+    
+  test do
+  end
+end
