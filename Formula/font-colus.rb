8f265ae7dacea62bb4785a6c7df1bfb8abfa477b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-colus.rb from 79b13982d69d7b8590ac3e616ca85569ed61b86b

---
+++ Formula/font-colus.rb
@@ -0,0 +1,9 @@
+class FontColus < Formula
+  head "https://www.fontfabric.com/downfont/colus.zip"
+  desc "Colus"
+  homepage "https://www.fontfabric.com/colus/"
+  def install
+    
+  test do
+  end
+end
