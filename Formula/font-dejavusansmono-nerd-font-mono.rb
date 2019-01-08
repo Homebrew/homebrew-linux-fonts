cf3dbce36dcd836b0dbb8317f96212694b26f090
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dejavusansmono-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-dejavusansmono-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontDejavusansmonoNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "0a3a0f67e94bb6fdeb08215eef67afe4b9e01cee3df509b05eb9a3c31f4eb31b"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DejaVuSansMono.zip"
+  desc "DejaVuSansMono Nerd Font (DejaVuSansMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
