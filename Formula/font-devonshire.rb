ed235f4544bbbdc7abcbb975c2e788b126485dc5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:47 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-devonshire.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-devonshire.rb
@@ -0,0 +1,9 @@
+class FontDevonshire < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/devonshire/Devonshire-Regular.ttf"
+  desc "Devonshire"
+  homepage "https://www.google.com/fonts/specimen/Devonshire"
+  def install
+    
+  test do
+  end
+end
