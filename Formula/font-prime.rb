54b27eecf995a74a1a6de48c98a603923ae402a0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-prime.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-prime.rb
@@ -0,0 +1,9 @@
+class FontPrime < Formula
+  head "https://fontfabric.com/downfont/prime.zip"
+  desc "Prime"
+  homepage "https://fontfabric.com/prime-free-font/"
+  def install
+    
+  test do
+  end
+end
