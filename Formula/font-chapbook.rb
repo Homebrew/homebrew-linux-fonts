8117a190f07881008d860560f4ccff29a3df7ec7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chapbook.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-chapbook.rb
@@ -0,0 +1,9 @@
+class FontChapbook < Formula
+  head "https://www.feorag.com/freestuff/archives/Chapbook-OTF.zip"
+  desc "Chapbook"
+  homepage "https://www.feorag.com/freestuff/chapbook.html"
+  def install
+    
+  test do
+  end
+end
