e1632ac0853c6f7d2f12b90dd8a752fbe46c7081
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-anonymous-pro.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-anonymous-pro.rb
@@ -0,0 +1,11 @@
+class FontAnonymousPro < Formula
+  version "1.002"
+  sha256 "86665847a51cdfb58a1e1dfd8b1ba33f183485affe50b53e3304f63d3d3552ab"
+  url "https://www.marksimonson.com/assets/content/fonts/AnonymousPro-#{version}.zip"
+  desc "Anonymous Pro"
+  homepage "https://www.marksimonson.com/fonts/view/anonymous-pro"
+  def install
+    
+  test do
+  end
+end
