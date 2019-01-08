f2f48d21ff6352791206976eacd45e97e732ee73
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-courier-prime-medium-and-semi-bold.rb from 859e1182db5e3b5bf0bc5e1c894557d2b3e08eb2

---
+++ Formula/font-courier-prime-medium-and-semi-bold.rb
@@ -0,0 +1,9 @@
+class FontCourierPrimeMediumAndSemiBold < Formula
+  head "https://quoteunquoteapps.com/courierprime/Courier-Prime-Medium-Semi-Bold.zip"
+  desc "Courier Prime Medium & Semi-Bold"
+  homepage "https://quoteunquoteapps.com/courierprime/#cyrillic-semi-bold"
+  def install
+    
+  test do
+  end
+end
