6a04cdffed5e02884774ebe9aca3f3d4f3055869
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mr-dafoe.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-mr-dafoe.rb
@@ -0,0 +1,9 @@
+class FontMrDafoe < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/mrdafoe/MrDafoe-Regular.ttf"
+  desc "Mr Dafoe"
+  homepage "https://www.google.com/fonts/specimen/Mr+Dafoe"
+  def install
+    
+  test do
+  end
+end
