df1d0df247f0836ae6d63f33951a89fe69b924e6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-balinese.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-sans-balinese.rb
@@ -0,0 +1,9 @@
+class FontNotoSansBalinese < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBalinese-unhinted.zip"
+  desc "Noto Sans Balinese"
+  homepage "https://www.google.com/get/noto/#sans-bali"
+  def install
+    
+  test do
+  end
+end
