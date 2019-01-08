8318d686a8b2ec30d2268c49d3387baa6cee0b93
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aileron.rb from 58fd3add03b475e20701bb727d653095f1b9b4f3

---
+++ Formula/font-aileron.rb
@@ -0,0 +1,9 @@
+class FontAileron < Formula
+  head "http://dotcolon.net/DL/font/aileron.zip"
+  desc "Aileron"
+  homepage "http://dotcolon.net/font/aileron/"
+  def install
+    
+  test do
+  end
+end
