e1cad3d0a63330b72537532471d7f33daf8ede1f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-freesans.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-freesans.rb
@@ -0,0 +1,11 @@
+class FontFreesans < Formula
+  version "20120503"
+  sha256 "3a6c51868c71b006c33c4bcde63d90927e6fcca8f51c965b8ad62d021614a860"
+  url "https://ftp.gnu.org/gnu/freefont/freefont-otf-#{version}.tar.gz"
+  desc "FreeSans"
+  homepage "https://ftp.gnu.org/gnu/freefont/"
+  def install
+    
+  test do
+  end
+end
