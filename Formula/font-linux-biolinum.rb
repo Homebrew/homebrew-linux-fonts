dae145a3e60a19dc7aa0333239fc2f909e0f80c7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-linux-biolinum.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-linux-biolinum.rb
@@ -0,0 +1,11 @@
+class FontLinuxBiolinum < Formula
+  version "5.3.0_2012_07_02"
+  sha256 "24a593a949808d976850131a953c0c0d7a72299531dfbb348191964cc038d75d"
+  url "https://downloads.sourceforge.net/linuxlibertine/LinLibertineTTF_#{version}.tgz"
+  desc "Linux Biolinum"
+  homepage "http://linuxlibertine.org/"
+  def install
+    
+  test do
+  end
+end
