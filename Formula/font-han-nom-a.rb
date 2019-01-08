8dbd46d197a44299a7c7ed407dbc94c507bdea8f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-han-nom-a.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-han-nom-a.rb
@@ -0,0 +1,11 @@
+class FontHanNomA < Formula
+  version "2005"
+  sha256 "428a31ff62e26dc33080789f8d49c39ac529dbfdbcfd50825f78cbd0903a3936"
+  url "https://downloads.sourceforge.net/vietunicode/hannomH.zip"
+  desc "HAN NOM A"
+  homepage "https://sourceforge.net/projects/vietunicode/files/hannom/hannom%20v2005/"
+  def install
+    
+  test do
+  end
+end
