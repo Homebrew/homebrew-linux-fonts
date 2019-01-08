ea012946f5c9e9264090a5bb7ff594815c8a1183
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-radley.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-radley.rb
@@ -0,0 +1,10 @@
+class FontRadley < Formula
+  head "https://github.com/google/fonts/trunk/ofl/radley", using: :svn, revision: "50", trust_cert: true
+  desc "Radley"
+  homepage "https://www.google.com/fonts/specimen/Radley"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
