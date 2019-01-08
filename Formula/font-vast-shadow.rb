28590cb274212d65203bac05099f75a69d2f7bbe
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:46 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-vast-shadow.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-vast-shadow.rb
@@ -0,0 +1,9 @@
+class FontVastShadow < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/vastshadow/VastShadow-Regular.ttf"
+  desc "Vast Shadow"
+  homepage "https://www.google.com/fonts/specimen/Vast+Shadow"
+  def install
+    
+  test do
+  end
+end
