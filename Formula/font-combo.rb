b2002cf14930529e12254a1045949ea28e6ef64a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-combo.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-combo.rb
@@ -0,0 +1,9 @@
+class FontCombo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/combo/Combo-Regular.ttf"
+  desc "Combo"
+  homepage "https://www.google.com/fonts/specimen/Combo"
+  def install
+    
+  test do
+  end
+end
