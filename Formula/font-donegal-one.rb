f0b9466147c2a8d259f4c280fff77432f4e09536
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-donegal-one.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-donegal-one.rb
@@ -0,0 +1,9 @@
+class FontDonegalOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/donegalone/DonegalOne-Regular.ttf"
+  desc "Donegal One"
+  homepage "https://www.google.com/fonts/specimen/Donegal+One"
+  def install
+    
+  test do
+  end
+end
