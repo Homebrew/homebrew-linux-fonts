58121e94d04bb08bfbda45efc4503be19ec5ed81
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:25 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tillana.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-tillana.rb
+++ Formula/font-tillana.rb
@@ -1,7 +1,7 @@
 class FontTillana < Formula
   version "2.002"
   sha256 "0a7965cffee3765ac59dd3beae43f30ace6f1e5cbd9806d86aa26fd90a5ac7c4"
-  url "https://github.com/itfoundry/tillana/releases/download/v#{version}/tillana-#{version.to_s.gsub(".", "_")}.zip"
+  head "https://github.com/itfoundry/tillana/releases/download/v#{version}/tillana-#{version.dots_to_underscores}.zip"
   desc "Tillana"
   homepage "https://github.com/itfoundry/tillana"
   def install
