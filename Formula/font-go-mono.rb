46a77a825bb7e86be0c4b30cc16277e184df8a73
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:02 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-go-mono.rb from 70e7c259a51190fddeb4265e1fe5ce4b6c12b118

---
+++ Formula/font-go-mono.rb
@@ -0,0 +1,9 @@
+class FontGoMono < Formula
+  head "https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz"
+  desc "Go Mono"
+  homepage "https://go.googlesource.com/image/+/master/font/gofont/ttfs/README"
+  def install
+    
+  test do
+  end
+end
