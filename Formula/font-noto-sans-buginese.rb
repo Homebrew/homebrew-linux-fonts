8ae32e0b19bca84bec4a1a5f96d1599d52a6b9fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-buginese.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-sans-buginese.rb
@@ -0,0 +1,9 @@
+class FontNotoSansBuginese < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBuginese-unhinted.zip"
+  desc "Noto Sans Buginese"
+  homepage "https://www.google.com/get/noto/#sans-bugi"
+  def install
+    
+  test do
+  end
+end
