098ce0c9f465597e5ad13853a3f5b97c9aed504f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:41 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-javanese.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-javanese.rb
@@ -0,0 +1,9 @@
+class FontNotoSansJavanese < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansJavanese-unhinted.zip"
+  desc "Noto Sans Javanese"
+  homepage "https://www.google.com/get/noto/#sans-java"
+  def install
+    
+  test do
+  end
+end
