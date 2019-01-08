ffd1795aeff27beebd6031a170016b9519d1273f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cwtex-q.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-cwtex-q.rb
@@ -0,0 +1,11 @@
+class FontCwtexQ < Formula
+  version "0.4"
+  sha256 "c336e028cccf86121694e5c534566741994569701ddea194697932e6e992b8a2"
+  url "https://github.com/l10n-tw/cwtex-q-fonts-TTFs/archive/v#{version}.tar.gz"
+  desc "cwtex-q-fonts"
+  homepage "https://github.com/l10n-tw/cwtex-q-fonts"
+  def install
+    
+  test do
+  end
+end
