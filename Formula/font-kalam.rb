fcb47c00cf240ba77c6874780ef80d93b9ab43a6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kalam.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-kalam.rb
@@ -0,0 +1,11 @@
+class FontKalam < Formula
+  version "2.001"
+  sha256 "221df2b265d12789af5ef52afd25479ed84eabf7802e59de3de01291b237c605"
+  url "https://github.com/itfoundry/kalam/releases/download/v#{version}/kalam-#{version.to_s.gsub(".", "_")}.zip"
+  desc "Kalam"
+  homepage "https://github.com/itfoundry/kalam"
+  def install
+    
+  test do
+  end
+end
