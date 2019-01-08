1d0615c2f79df86381ef4c9bd97a48138086b7b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ovo.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-ovo.rb
@@ -0,0 +1,9 @@
+class FontOvo < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/ovo/Ovo-Regular.ttf"
+  desc "Ovo"
+  homepage "https://www.google.com/fonts/specimen/Ovo"
+  def install
+    
+  test do
+  end
+end
