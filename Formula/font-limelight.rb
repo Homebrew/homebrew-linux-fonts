361aa0e0e0eb484de3aa5cb06593ef1c7bd8ad55
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-limelight.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-limelight.rb
@@ -0,0 +1,9 @@
+class FontLimelight < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/limelight/Limelight-Regular.ttf"
+  desc "Limelight"
+  homepage "https://www.google.com/fonts/specimen/Limelight"
+  def install
+    
+  test do
+  end
+end
