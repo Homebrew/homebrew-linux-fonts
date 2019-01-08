f44806b5ea8e2671bf2cd273de8745d821baab2f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-italianno.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-italianno.rb
@@ -0,0 +1,9 @@
+class FontItalianno < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/italianno/Italianno-Regular.ttf"
+  desc "Italianno"
+  homepage "https://www.google.com/fonts/specimen/Italianno"
+  def install
+    
+  test do
+  end
+end
