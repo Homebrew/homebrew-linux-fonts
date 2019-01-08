439625bc1484727808bf8ac695533836e7511810
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:29 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hyppolit.rb from 39df46afee88e73a56a60146b2cd3049973b1444

---
+++ Formula/font-hyppolit.rb
@@ -0,0 +1,9 @@
+class FontHyppolit < Formula
+  head "https://dl.1001fonts.com/hyppolit.zip"
+  desc "Hyppolit"
+  homepage "https://www.1001fonts.com/hyppolit-font.html"
+  def install
+    
+  test do
+  end
+end
