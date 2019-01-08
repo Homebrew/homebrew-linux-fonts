a42ef2a3edc87429c8fe7228b983930d0e0818ce
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-podkova.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-podkova.rb
@@ -0,0 +1,10 @@
+class FontPodkova < Formula
+  head "https://github.com/google/fonts/trunk/ofl/podkova", using: :svn, revision: "50", trust_cert: true
+  desc "Podkova"
+  homepage "https://www.google.com/fonts/specimen/Podkova"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
