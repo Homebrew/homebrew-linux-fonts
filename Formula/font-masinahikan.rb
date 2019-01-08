4698650c2545e643e9af0dbb8d3c6f61f4edb35b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-masinahikan.rb from dc9afabc655aae96070da8489f3524d2bfc757db

---
+++ Formula/font-masinahikan.rb
@@ -0,0 +1,9 @@
+class FontMasinahikan < Formula
+  head "http://www.languagegeek.com/font/Masinahikan.zip"
+  desc "Masinahikan"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
