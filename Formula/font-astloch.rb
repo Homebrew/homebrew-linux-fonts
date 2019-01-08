0e67294d58b726a7292ea872e9330939a90ce291
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-astloch.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-astloch.rb
@@ -0,0 +1,10 @@
+class FontAstloch < Formula
+  head "https://github.com/google/fonts/trunk/ofl/astloch", using: :svn, revision: "50", trust_cert: true
+  desc "Astloch"
+  homepage "https://www.google.com/fonts/specimen/Astloch"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
