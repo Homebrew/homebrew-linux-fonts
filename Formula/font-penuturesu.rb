b461cd96e83af15fc089b290b4ee1ea3fbdc524f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-penuturesu.rb from fc0d0ffd7bc9778e1574d9789974c4ddf22cfb4c

---
+++ Formula/font-penuturesu.rb
@@ -0,0 +1,9 @@
+class FontPenuturesu < Formula
+  head "http://www.i18nguy.com/unicode/penuture.zip", user_agent: :fake
+  desc "Penuturesu"
+  homepage "http://www.i18nguy.com/unicode/unicode-font.html"
+  def install
+    
+  test do
+  end
+end
