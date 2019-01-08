560a0f8ae6ec583ebad812685a7b72b48b5bfeea
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:59 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stalinist-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-stalinist-one.rb
@@ -0,0 +1,9 @@
+class FontStalinistOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/stalinistone/StalinistOne-Regular.ttf"
+  desc "Stalinist One"
+  homepage "https://www.google.com/fonts/specimen/Stalinist+One"
+  def install
+    
+  test do
+  end
+end
