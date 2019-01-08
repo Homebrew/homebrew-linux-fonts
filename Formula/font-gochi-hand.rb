95fd338c5af6cdc7468b34ce2770748f55af3a9c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gochi-hand.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-gochi-hand.rb
@@ -0,0 +1,9 @@
+class FontGochiHand < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/gochihand/GochiHand-Regular.ttf"
+  desc "Gochi Hand"
+  homepage "https://www.google.com/fonts/specimen/Gochi+Hand"
+  def install
+    
+  test do
+  end
+end
