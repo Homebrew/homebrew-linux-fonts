ce3571dcd95392f82a12731304dda679a5b0df95
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:26 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-khmer.rb from 7ba99eb0d220c5dcbd47bbd6186bede514f0d71a

---
+++ Formula/font-noto-serif-khmer.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifKhmer < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifKhmer-unhinted.zip"
+  desc "Noto Serif Khmer"
+  homepage "https://www.google.com/get/noto/#serif-khmr"
+  def install
+    
+  test do
+  end
+end
