ab1a044fccc62a97c41f026df90d94aa881df7b6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:30:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lovers-quarrel.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-lovers-quarrel.rb
@@ -0,0 +1,9 @@
+class FontLoversQuarrel < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/loversquarrel/LoversQuarrel-Regular.ttf"
+  desc "Lovers Quarrel"
+  homepage "https://www.google.com/fonts/specimen/Lovers+Quarrel"
+  def install
+    
+  test do
+  end
+end
