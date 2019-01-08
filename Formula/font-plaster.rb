4c3a083c4e9744ac38f74be73575f3bebc83b453
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-plaster.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-plaster.rb
@@ -0,0 +1,9 @@
+class FontPlaster < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/plaster/Plaster-Regular.ttf"
+  desc "Plaster"
+  homepage "https://www.google.com/fonts/specimen/Plaster"
+  def install
+    
+  test do
+  end
+end
