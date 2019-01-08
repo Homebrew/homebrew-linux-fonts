70c7cf3a5eff3b8ebcede483390f4dc77ef5e981
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-twitter-color-emoji.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-twitter-color-emoji.rb
@@ -0,0 +1,11 @@
+class FontTwitterColorEmoji < Formula
+  version "1.4"
+  sha256 "ce5c8c6901a1743a087b5b3c0522acecf0bb132feda30f58cbbe49f0d668fec7"
+  url "https://github.com/eosrei/twemoji-color-font/releases/download/v#{version}/TwitterColorEmoji-SVGinOT-#{version}.zip"
+  desc "Twitter Color Emoji"
+  homepage "https://github.com/eosrei/twemoji-color-font/"
+  def install
+    
+  test do
+  end
+end
