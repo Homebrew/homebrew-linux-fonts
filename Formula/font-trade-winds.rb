27c5d619bd93221bb476d0790f545b19bb343429
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:29 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-trade-winds.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-trade-winds.rb
@@ -0,0 +1,9 @@
+class FontTradeWinds < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/tradewinds/TradeWinds-Regular.ttf"
+  desc "Trade Winds"
+  homepage "https://www.google.com/fonts/specimen/Trade+Winds"
+  def install
+    
+  test do
+  end
+end
