6455bbe571d4d32f78d746e8cc714ff82558a41c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jura.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-jura.rb
@@ -0,0 +1,10 @@
+class FontJura < Formula
+  head "https://github.com/google/fonts/trunk/ofl/jura", using: :svn, revision: "50", trust_cert: true
+  desc "Jura"
+  homepage "https://www.google.com/fonts/specimen/Jura"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
