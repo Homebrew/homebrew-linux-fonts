8fe806b0f1da56c3a12bf19e9dad37c13fcedfc9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-exo.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-exo.rb
@@ -0,0 +1,10 @@
+class FontExo < Formula
+  head "https://github.com/google/fonts/trunk/ofl/exo", using: :svn, revision: "50", trust_cert: true
+  desc "Exo"
+  homepage "https://www.google.com/fonts/specimen/Exo"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
