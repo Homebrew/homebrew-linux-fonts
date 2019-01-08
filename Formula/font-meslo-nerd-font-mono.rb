410a71c52bb824db18dff2be8f572894a7dcdc2e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-meslo-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-meslo-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontMesloNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "350ff0b1061ca0d1e933c59861d6421ebb2667d494875fcb1821d3df44f08476"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Meslo.zip"
+  desc "Meslo Nerd Font (Meslo)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
