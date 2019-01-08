6dab8bf93a7d17d442c7a75eaa81491b853d76cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-padauk.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-padauk.rb
@@ -0,0 +1,10 @@
+class FontPadauk < Formula
+  head "https://github.com/google/fonts/trunk/ofl/padauk", using: :svn, revision: "50", trust_cert: true
+  desc "Padauk"
+  homepage "https://www.google.com/fonts/earlyaccess"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
