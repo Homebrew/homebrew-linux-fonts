e926b3bf66b3a184ed415f124b17120b20ac7420
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-roboto-mono-for-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-roboto-mono-for-powerline.rb
@@ -0,0 +1,10 @@
+class FontRobotoMonoForPowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/RobotoMono", using: :svn, trust_cert: true
+  desc "Roboto Mono for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/RobotoMono"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
