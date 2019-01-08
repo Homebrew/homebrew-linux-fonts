fa08779e79a6d7d1124dccb45b537628421179a2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-ethiopic.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-ethiopic.rb
@@ -0,0 +1,9 @@
+class FontNotoSansEthiopic < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansEthiopic-unhinted.zip"
+  desc "Noto Sans Ethiopic"
+  homepage "https://www.google.com/get/noto/#sans-ethi"
+  def install
+    
+  test do
+  end
+end
