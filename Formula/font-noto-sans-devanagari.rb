004e969507117f0eef5fa2c9d38e53a6500887dd
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-devanagari.rb from 91ec7fe0323c8467fb529c8559b961db2070ea2c

---
+++ Formula/font-noto-sans-devanagari.rb
@@ -0,0 +1,9 @@
+class FontNotoSansDevanagari < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansDevanagari-unhinted.zip"
+  desc "Noto Sans Devanagari"
+  homepage "https://www.google.com/get/noto/#sans-deva"
+  def install
+    
+  test do
+  end
+end
