d323808db1b7d5d567db63135beb1f2c39fd2e6a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rotinonhsonni-serif.rb from 61be54ed29e5e054f6263a257cd56e372ef5af65

---
+++ Formula/font-rotinonhsonni-serif.rb
@@ -0,0 +1,11 @@
+class FontRotinonhsonniSerif < Formula
+  version "4.2"
+  sha256 "fd7f76b1043ec817b3fd109130eafd1db85a3249ca7d98014a16bd8c207488d2"
+  url "http://www.languagegeek.com/font/RotinonhSerif.zip"
+  desc "Rotinohnsonni Serif"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
