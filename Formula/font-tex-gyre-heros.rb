b0eb67f5da88d9751e2e25eaedd809785232210d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-heros.rb from 18e964dbf2af050afb02a3fb6014a426fc6d4566

---
+++ Formula/font-tex-gyre-heros.rb
@@ -0,0 +1,11 @@
+class FontTexGyreHeros < Formula
+  version "2.004"
+  sha256 "755954b7349b803fc1c3d82fe9d9c4f7cf66467af718eaaf4f78ae1a09bf265d"
+  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/heros/qhv#{version}otf.zip"
+  desc "TeX Gyre Heros"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/heros"
+  def install
+    
+  test do
+  end
+end
