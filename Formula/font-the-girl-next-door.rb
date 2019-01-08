0f9c49138a85cf428a7496ab41375a09c71b7f54
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-the-girl-next-door.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-the-girl-next-door.rb
@@ -0,0 +1,9 @@
+class FontTheGirlNextDoor < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/thegirlnextdoor/TheGirlNextDoor.ttf"
+  desc "The Girl Next Door"
+  homepage "https://www.google.com/fonts/specimen/The+Girl+Next+Door"
+  def install
+    
+  test do
+  end
+end
