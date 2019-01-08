580d5141d338e8ec20b691b2181a7eae04065c28
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-han-sans.rb from 715219cd7ca4328d25541a73091484c31fde8201

--- Formula/font-source-han-sans.rb
+++ Formula/font-source-han-sans.rb
@@ -1,7 +1,7 @@
 class FontSourceHanSans < Formula
   version "2.000"
   sha256 "500a516d67ec25328f9bd9e357f7ee14bfa560372b6002b76b18749207469da9"
-  head "https://github.com/adobe-fonts/source-han-sans/raw/2.000R/SuperOTC/SourceHanSans.ttc.zip"
+  url "https://github.com/adobe-fonts/source-han-sans/raw/2.000R/SuperOTC/SourceHanSans.ttc.zip"
   desc "Source Han Sans"
   homepage "https://github.com/adobe-fonts/source-han-sans"
   def install
