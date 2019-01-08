f086a5dd2f8cfe0384c9974d9d2e81842eef72e8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rock-salt.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-rock-salt.rb
@@ -0,0 +1,9 @@
+class FontRockSalt < Formula
+  head "https://github.com/google/fonts/raw/master/apache/rocksalt/RockSalt-Regular.ttf"
+  desc "Rock Salt"
+  homepage "https://www.google.com/fonts/specimen/Rock+Salt"
+  def install
+    
+  test do
+  end
+end
