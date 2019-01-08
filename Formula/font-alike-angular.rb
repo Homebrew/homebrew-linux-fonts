26f1a3e6e70b95b692c80cc6eecce1c8aa940b4c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alike-angular.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-alike-angular.rb
@@ -0,0 +1,9 @@
+class FontAlikeAngular < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/alikeangular/AlikeAngular-Regular.ttf"
+  desc "Alike Angular"
+  homepage "https://www.google.com/fonts/specimen/Alike+Angular"
+  def install
+    
+  test do
+  end
+end
