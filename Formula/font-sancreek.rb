3b49c70abaf34133509b80a0bb700288a62af9e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sancreek.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-sancreek.rb
@@ -0,0 +1,9 @@
+class FontSancreek < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/sancreek/Sancreek-Regular.ttf"
+  desc "Sancreek"
+  homepage "https://www.google.com/fonts/specimen/Sancreek"
+  def install
+    
+  test do
+  end
+end
