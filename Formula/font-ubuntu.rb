431a93619aa9457d32cd4fd936d4edda8cdc7503
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:35:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu.rb from be157e0ebde60ffc07eee7df119c6d2109cae82f

--- Formula/font-ubuntu.rb
+++ Formula/font-ubuntu.rb
@@ -1,7 +1,7 @@
 class FontUbuntu < Formula
   version "0.83,fad7939b"
   sha256 "456d7d42797febd0d7d4cf1b782a2e03680bb4a5ee43cc9d06bda172bac05b42"
-  head "https://assets.ubuntu.com/v1/#{version.after_comma}-ubuntu-font-family-#{version.before_comma}.zip"
+  url "https://assets.ubuntu.com/v1/#{version.to_s.sub(/.*,/, "")}-ubuntu-font-family-#{version.to_s.sub(/,.*/, "")}.zip"
   desc "Ubuntu"
   homepage "http://font.ubuntu.com/"
   def install
