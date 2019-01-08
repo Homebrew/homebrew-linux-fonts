6f597288bb2db701aa405128288145d9845fd5e7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spinnaker.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-spinnaker.rb
@@ -0,0 +1,9 @@
+class FontSpinnaker < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/spinnaker/Spinnaker-Regular.ttf"
+  desc "Spinnaker"
+  homepage "https://www.google.com/fonts/specimen/Spinnaker"
+  def install
+    
+  test do
+  end
+end
