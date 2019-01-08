7fb2486ea8f0e97b09dac38965ee6c10f997ff3d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cooper-hewitt.rb from 4120f41e480e50e0ba35ca769ba1f58864da50ad

---
+++ Formula/font-cooper-hewitt.rb
@@ -0,0 +1,9 @@
+class FontCooperHewitt < Formula
+  head "https://uh8yh30l48rpize52xh0q1o6i-wpengine.netdna-ssl.com/wp-content/uploads/fonts/CooperHewitt-OTF-public.zip"
+  desc "Cooper Hewitt"
+  homepage "https://www.cooperhewitt.org/open-source-at-cooper-hewitt/cooper-hewitt-the-typeface-by-chester-jenkins/"
+  def install
+    
+  test do
+  end
+end
