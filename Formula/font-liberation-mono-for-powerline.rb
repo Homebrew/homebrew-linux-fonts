2122c4180273389bdb067d7b4cce6b0b416cc3b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-liberation-mono-for-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-liberation-mono-for-powerline.rb
@@ -0,0 +1,10 @@
+class FontLiberationMonoForPowerline < Formula
+  head "https://github.com/powerline/fonts/trunk/LiberationMono", using: :svn, trust_cert: true
+  desc "Literation Mono for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/LiberationMono"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
