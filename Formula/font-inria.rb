d2519a0a30fb6428f840ca0aaac33a90e00727ee
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inria.rb from c031f48281afc554d457d54f3c5867ede1f2a855

---
+++ Formula/font-inria.rb
@@ -0,0 +1,9 @@
+class FontInria < Formula
+  head "https://github.com/BlackFoundry/InriaFonts/archive/master.zip"
+  desc "Inria"
+  homepage "https://black-foundry.com/blog/inria-serif-and-inria/"
+  def install
+    
+  test do
+  end
+end
