1259a245a88c7fbe679c6232505f6089ce7263dc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:40 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bukyvede-italic.rb from d19cc96c7ef279cc9362bf9f917fc38083f70c2e

---
+++ Formula/font-bukyvede-italic.rb
@@ -0,0 +1,9 @@
+class FontBukyvedeItalic < Formula
+  head "http://kodeks.uni-bamberg.de/aksl/media/BukyVede-Italic.ttf"
+  desc "BukyVede Italic"
+  homepage "http://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm"
+  def install
+    
+  test do
+  end
+end
