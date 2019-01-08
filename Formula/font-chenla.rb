385564e16150507060f61ae33e8b9053cb107423
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:01 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chenla.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-chenla.rb
@@ -0,0 +1,9 @@
+class FontChenla < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/chenla/Chenla.ttf"
+  desc "Chenla"
+  homepage "https://www.google.com/fonts/specimen/Chenla"
+  def install
+    
+  test do
+  end
+end
