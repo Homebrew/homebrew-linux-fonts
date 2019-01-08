a451ca0baaffbb8a5a9797b16c9588a290ac1714
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oskideneb.rb from 9cf43ad1f61c2b4cc07deea8b9bcafb29a6251e2

---
+++ Formula/font-oskideneb.rb
@@ -0,0 +1,9 @@
+class FontOskideneb < Formula
+  head "http://www.languagegeek.com/font/OskiDeneB.zip"
+  desc "OskiDeneB"
+  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  def install
+    
+  test do
+  end
+end
