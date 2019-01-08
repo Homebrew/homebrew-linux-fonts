bf8106ccbe4af980287ff0ba09982086e249bf6e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-trykker.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-trykker.rb
@@ -0,0 +1,9 @@
+class FontTrykker < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/trykker/Trykker-Regular.ttf"
+  desc "Trykker"
+  homepage "https://www.google.com/fonts/specimen/Trykker"
+  def install
+    
+  test do
+  end
+end
