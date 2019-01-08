53fc9b0dfef97545ae250582a34f42513827cefe
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:39 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monofur-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-monofur-nerd-font.rb
@@ -0,0 +1,11 @@
+class FontMonofurNerdFont < Formula
+  version "2.0.0"
+  sha256 "16b5cc275bd5689d3f3b348119c7356d9c1de67c46dd2d24f752972ad086b765"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Monofur.zip"
+  desc "Monofurbold Nerd Font (Monofur)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
