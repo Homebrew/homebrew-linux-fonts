a42449b2041d7876cdff5f3d70202d3fd13a3711
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:53 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cutive.rb from 33a294768ad259203de32e33398d8d7ba1304e07

--- Formula/font-cutive.rb
+++ Formula/font-cutive.rb
@@ -1,7 +1,7 @@
 class FontCutive < Formula
   version "1.002"
   sha256 "3995832e966644c404d0e9e815c258aab80961342c80731884bff27319b84e4b"
-  head "https://github.com/google/fonts/raw/master/ofl/cutive/Cutive-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/cutive/Cutive-Regular.ttf"
   desc "Cutive"
   homepage "https://www.google.com/fonts/specimen/Cutive"
   def install
