82af8fd5b00bf411e2a4bd3718cfaff39ef0bf3a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stalemate.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-stalemate.rb
@@ -0,0 +1,9 @@
+class FontStalemate < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/stalemate/Stalemate-Regular.ttf"
+  desc "Stalemate"
+  homepage "https://www.google.com/fonts/specimen/Stalemate"
+  def install
+    
+  test do
+  end
+end
