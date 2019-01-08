1190c97634cbe5740e26c1cc3a19b76d2a9df1cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-medievalsharp.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-medievalsharp.rb
@@ -0,0 +1,9 @@
+class FontMedievalsharp < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/medievalsharp/MedievalSharp.ttf"
+  desc "MedievalSharp"
+  homepage "https://www.google.com/fonts/specimen/MedievalSharp"
+  def install
+    
+  test do
+  end
+end
