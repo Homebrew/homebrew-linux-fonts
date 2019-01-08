63b13ece152c556297ebd75b66288a036ce08fb5
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-sans-pro.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-source-sans-pro.rb
+++ Formula/font-source-sans-pro.rb
@@ -1,7 +1,7 @@
 class FontSourceSansPro < Formula
   version "2.020R-ro-1.075R-it"
   sha256 "cb2da9c80acef9234e0b95ed2f80694e9af49c2d353a85d71c1ca485a85a5ca9"
-  head "https://github.com/adobe-fonts/source-sans-pro/archive/#{version.sub('ro-', 'ro/')}.zip"
+  url "https://github.com/adobe-fonts/source-sans-pro/archive/#{version.to_s.sub('ro-', 'ro/')}.zip"
   desc "Source Sans Pro"
   homepage "https://adobe-fonts.github.io/source-sans-pro/"
   def install
