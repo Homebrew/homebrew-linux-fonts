be773166e59c0836a23a2a1e6cbc3e0ff8dea99d
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tai-le-valentinium.rb from 5852e82ae7e322cf06cb66d02ed1d5bf33062d5c

---
+++ Formula/font-tai-le-valentinium.rb
@@ -0,0 +1,9 @@
+class FontTaiLeValentinium < Formula
+  head "http://www.alanwood.net/downloads/tai-le-valentinium.zip"
+  desc "Tai Le Valentinium"
+  homepage "http://www.alanwood.net/downloads/index.html"
+  def install
+    
+  test do
+  end
+end
