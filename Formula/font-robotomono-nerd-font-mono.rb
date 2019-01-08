02c78616dd73f0accc7347fbdb746855a1ac6457
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-robotomono-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-robotomono-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontRobotomonoNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "0714a974b22bfedbed8855ef589a12f9c60167788278965bd61d3d5d06bcf8e8"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/RobotoMono.zip"
+  desc "RobotoMono Nerd Font (RobotoMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
