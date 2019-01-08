ac354681e4bdb37364069973afc11a4b0fbcf786
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-poly.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-poly.rb
@@ -0,0 +1,10 @@
+class FontPoly < Formula
+  head "https://github.com/google/fonts/trunk/ofl/poly", using: :svn, revision: "50", trust_cert: true
+  desc "Poly"
+  homepage "https://www.google.com/fonts/specimen/Poly"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
