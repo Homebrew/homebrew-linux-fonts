0c2033f55e14d7e20be142d2243b2193013d7c49
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:12 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-habibi.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-habibi.rb
@@ -0,0 +1,9 @@
+class FontHabibi < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/habibi/Habibi-Regular.ttf"
+  desc "Habibi"
+  homepage "https://www.google.com/fonts/specimen/Habibi"
+  def install
+    
+  test do
+  end
+end
