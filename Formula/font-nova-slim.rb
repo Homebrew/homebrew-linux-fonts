d1088f3ce1d8673365efbc733de05104f281dbca
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nova-slim.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-nova-slim.rb
@@ -0,0 +1,9 @@
+class FontNovaSlim < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/novaslim/NovaSlim.ttf"
+  desc "Nova Slim"
+  homepage "https://www.google.com/fonts/specimen/Nova%20Slim"
+  def install
+    
+  test do
+  end
+end
