846649eeefaee76b4c69273ab930d9d16602b085
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:03 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-shavian.rb from 429fe80b45ad0fcc4027e2cc38e6908c61e6d571

---
+++ Formula/font-noto-sans-shavian.rb
@@ -0,0 +1,9 @@
+class FontNotoSansShavian < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansShavian-unhinted.zip"
+  desc "Noto Sans Shavian"
+  homepage "https://www.google.com/get/noto/#sans-shaw"
+  def install
+    
+  test do
+  end
+end
