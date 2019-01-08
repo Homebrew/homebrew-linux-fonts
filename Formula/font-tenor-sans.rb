72184dc9e095feddff8678671a6742e7ca547626
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:11 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tenor-sans.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-tenor-sans.rb
@@ -0,0 +1,9 @@
+class FontTenorSans < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/tenorsans/TenorSans-Regular.ttf"
+  desc "Tenor Sans"
+  homepage "https://www.google.com/fonts/specimen/Tenor+Sans"
+  def install
+    
+  test do
+  end
+end
