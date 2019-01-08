dc9b1eefb81a4e876a9c3811d144a6639dea86ce
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mrs-sheppards.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-mrs-sheppards.rb
@@ -0,0 +1,9 @@
+class FontMrsSheppards < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/mrssheppards/MrsSheppards-Regular.ttf"
+  desc "Mrs Sheppards"
+  homepage "https://www.google.com/fonts/specimen/Mrs+Sheppards"
+  def install
+    
+  test do
+  end
+end
