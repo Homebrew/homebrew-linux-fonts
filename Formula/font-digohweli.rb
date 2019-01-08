2e7e5e47ce7f0e12211839125b79bcef859554d6
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:53 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-digohweli.rb from 33cc38a998cdbed21915e7b059ce5d52d7795ef5

--- Formula/font-digohweli.rb
+++ Formula/font-digohweli.rb
@@ -1,7 +1,7 @@
 class FontDigohweli < Formula
   version "1.7"
   sha256 "a17b6af34e0d96daf933b42f07fc382ae49f8abf66ccd2f397088c2d6672b77f"
-  head "http://www.languagegeek.com/font/Digohweli.zip"
+  url "http://www.languagegeek.com/font/Digohweli.zip"
   desc "Digohweli"
   homepage "http://www.languagegeek.com/font/fontdownload.html"
   def install
