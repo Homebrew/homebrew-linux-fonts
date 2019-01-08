eac16f71302ed1eb10ba8146128f3393b4931878
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:58:48 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-play.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-play.rb
@@ -0,0 +1,12 @@
+class FontPlay < Formula
+  head "https://github.com/google/fonts/trunk/ofl/play", using: :svn, revision: "50", trust_cert: true
+  desc "Play"
+  homepage "https://www.google.com/fonts/specimen/Play"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "Play-Bold.ttf"
+    (share/"fonts").install "Play-Regular.ttf"
+  end
+  test do
+  end
+end
