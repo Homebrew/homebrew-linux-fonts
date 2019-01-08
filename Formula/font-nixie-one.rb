477fda28ea6b24f54ef768f54859ff2605a67ead
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:06 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nixie-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-nixie-one.rb
@@ -0,0 +1,9 @@
+class FontNixieOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/nixieone/NixieOne-Regular.ttf"
+  desc "Nixie One"
+  homepage "https://www.google.com/fonts/specimen/Nixie+One"
+  def install
+    
+  test do
+  end
+end
