916a6fb3911516f0e91654cd24551fef15ab072c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:52 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-space-grotesk.rb from 40ef53ef1203c224a26ad1ed0a347d00ebadfb9f

---
+++ Formula/font-space-grotesk.rb
@@ -0,0 +1,11 @@
+class FontSpaceGrotesk < Formula
+  version "1.1.0"
+  sha256 "a4aaf23fe43de4096ce85c3b8dec7076c15ec5d0b8dbfbc65e91da5b173ea8e6"
+  url "https://github.com/floriankarsten/space-grotesk/releases/download/#{version}/SpaceGrotesk-v#{version}.zip"
+  desc "Space Grotesk"
+  homepage "https://github.com/floriankarsten/space-grotesk"
+  def install
+    
+  test do
+  end
+end
