37f3f1459ab026627a08e9736843319cf9cd7f8c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rye.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-rye.rb
@@ -0,0 +1,9 @@
+class FontRye < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/rye/Rye-Regular.ttf"
+  desc "Rye"
+  homepage "https://www.google.com/fonts/specimen/Rye"
+  def install
+    
+  test do
+  end
+end
