f1669c4a2956d6c2cb70770a2c4303d98bfa3b44
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:00 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nanummyeongjo.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-nanummyeongjo.rb
@@ -0,0 +1,10 @@
+class FontNanummyeongjo < Formula
+  head "https://github.com/google/fonts/trunk/ofl/nanummyeongjo", using: :svn, revision: "50", trust_cert: true
+  desc "Nanum Myeongjo"
+  homepage "https://www.google.com/fonts/earlyaccess"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
