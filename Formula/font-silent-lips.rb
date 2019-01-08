118341feba0c6bc3336eff9d6030a02078845230
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-silent-lips.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-silent-lips.rb
+++ Formula/font-silent-lips.rb
@@ -1,7 +1,7 @@
 class FontSilentLips < Formula
   version "2014.07"
   sha256 "b19e19937111b5e2642e8f1b3da9e0f211f454dc897e3657b839067e59be653e"
-  head "https://fontm.com/wp-content/uploads/#{version.dots_to_slashes}/Silent-Lips.zip"
+  url "https://fontm.com/wp-content/uploads/#{version.to_s.gsub(".", "/")}/Silent-Lips.zip"
   desc "Silent Lips"
   homepage "https://www.behance.net/gallery/18281129/Silent-Lips-Experimental-Free-Font"
   def install
