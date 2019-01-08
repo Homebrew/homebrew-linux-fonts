fd9050df16150eac2160e1ff499c945911c75ec6
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-code2002.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-code2002.rb
@@ -0,0 +1,9 @@
+class FontCode2002 < Formula
+  head "https://www.ffonts.net/Code2002.font.zip"
+  desc "Code2002"
+  homepage "https://www.ffonts.net/Code2002.font.download"
+  def install
+    
+  test do
+  end
+end
