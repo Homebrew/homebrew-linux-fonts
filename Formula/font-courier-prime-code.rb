9bc55c75dd21913ce14b03622f210e18b4c9ab11
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-courier-prime-code.rb from 4811ee84320d3a8b9bc71ac96b3bd471de77cc3b

---
+++ Formula/font-courier-prime-code.rb
@@ -0,0 +1,9 @@
+class FontCourierPrimeCode < Formula
+  head "https://quoteunquoteapps.com/downloads/courier-code.zip"
+  desc "Courier Prime Code"
+  homepage "https://quoteunquoteapps.com/courierprime/#code-sans"
+  def install
+    
+  test do
+  end
+end
