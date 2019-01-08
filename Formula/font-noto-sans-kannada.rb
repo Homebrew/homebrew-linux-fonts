384c228425fad8e5820ce81034ae5ee9bae9e259
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-kannada.rb from c1a4503a6612bbcc5f01f5244b2c3de07b4e8648

---
+++ Formula/font-noto-sans-kannada.rb
@@ -0,0 +1,9 @@
+class FontNotoSansKannada < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKannada-unhinted.zip"
+  desc "Noto Sans Kannada"
+  homepage "https://www.google.com/get/noto/#sans-knda"
+  def install
+    
+  test do
+  end
+end
