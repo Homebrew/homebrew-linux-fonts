f0eed59379695e66087bffa96f1a215cf645e4e0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:13 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hack.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-hack.rb
@@ -0,0 +1,11 @@
+class FontHack < Formula
+  version "3.003"
+  sha256 "0c2604631b1f055041c68a0e09ae4801acab6c5072ba2db6a822f53c3f8290ac"
+  url "https://github.com/source-foundry/Hack/releases/download/v#{version}/Hack-v#{version}-ttf.zip"
+  desc "Hack"
+  homepage "https://sourcefoundry.org/hack/"
+  def install
+    
+  test do
+  end
+end
