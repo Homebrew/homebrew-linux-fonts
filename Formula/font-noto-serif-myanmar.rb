b37770dac5555355bb514bd7090ac831fd885fed
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:28 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-myanmar.rb from 6612748208449843385714429f09395a19960b12

---
+++ Formula/font-noto-serif-myanmar.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifMyanmar < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifMyanmar-unhinted.zip"
+  desc "Noto Serif Myanmar"
+  homepage "https://www.google.com/get/noto/#serif-mymr"
+  def install
+    
+  test do
+  end
+end
