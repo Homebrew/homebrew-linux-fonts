223bcff4737d7d028191cccfc0a1933002352ce2
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:15 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-comic-sans-ms.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-comic-sans-ms.rb
@@ -0,0 +1,11 @@
+class FontComicSansMs < Formula
+  version "2.10"
+  sha256 "9c6df3feefde26d4e41d4a4fe5db2a89f9123a772594d7f59afd062625cd204e"
+  url "https://downloads.sourceforge.net/corefonts/comic32.exe"
+  desc "Comic Sans"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  def install
+    
+  test do
+  end
+end
