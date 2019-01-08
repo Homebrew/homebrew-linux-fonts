180ae047dd148f59f87028cdb3a5dbc09440b7d5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:32 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-crimson-text.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-crimson-text.rb
@@ -0,0 +1,10 @@
+class FontCrimsonText < Formula
+  head "https://github.com/google/fonts/trunk/ofl/crimsontext", using: :svn, revision: "50", trust_cert: true
+  desc "Crimson Text"
+  homepage "https://www.google.com/fonts/specimen/Crimson%20Text"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
