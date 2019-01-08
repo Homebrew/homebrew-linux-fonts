e91cd4dc75bec0f7aa0f249645c66a8140e0530c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marck-script.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-marck-script.rb
@@ -0,0 +1,9 @@
+class FontMarckScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/marckscript/MarckScript-Regular.ttf"
+  desc "Marck Script"
+  homepage "https://www.google.com/fonts/specimen/Marck+Script"
+  def install
+    
+  test do
+  end
+end
