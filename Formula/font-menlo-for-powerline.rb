4e983f2017d9d2bf103d71a5b6ab659ae04ef055
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-menlo-for-powerline.rb from 5bd556433b8baadf51b3196e8f71433e728f89d4

---
+++ Formula/font-menlo-for-powerline.rb
@@ -0,0 +1,9 @@
+class FontMenloForPowerline < Formula
+  head "https://github.com/abertsch/Menlo-for-Powerline/archive/master.zip"
+  desc "Menlo for Powerline"
+  homepage "https://github.com/abertsch/Menlo-for-Powerline"
+  def install
+    
+  test do
+  end
+end
