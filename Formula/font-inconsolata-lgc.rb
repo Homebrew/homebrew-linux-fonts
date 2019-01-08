3e640757786b84b2a034ad3fb101528df8d6647e
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:55:54 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-lgc.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-inconsolata-lgc.rb
@@ -0,0 +1,14 @@
+class FontInconsolataLgc < Formula
+  head "https://github.com/DeLaGuardo/Inconsolata-LGC/trunk", using: :svn, trust_cert: true
+  desc "Inconsolata LGC"
+  homepage "https://github.com/DeLaGuardo/Inconsolata-LGC"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "inconsolatalgc.ttf"
+    (share/"fonts").install "inconsolatalgcbold.ttf"
+    (share/"fonts").install "inconsolatalgcbolditalic.ttf"
+    (share/"fonts").install "inconsolatalgcitalic.ttf"
+  end
+  test do
+  end
+end
