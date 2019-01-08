ac05df52e9fd4ef24fadafc0ff281ceab3514a81
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lato.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-lato.rb
@@ -0,0 +1,9 @@
+class FontLato < Formula
+  head "https://www.latofonts.com/download/Lato2OFL.zip"
+  desc "Lato"
+  homepage "https://www.latofonts.com/"
+  def install
+    
+  test do
+  end
+end
