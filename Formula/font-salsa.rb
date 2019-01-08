fd0ee6c424a2e67f6f00017aa6ed4912b779ce93
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-salsa.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-salsa.rb
@@ -0,0 +1,9 @@
+class FontSalsa < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/salsa/Salsa-Regular.ttf"
+  desc "Salsa"
+  homepage "https://www.google.com/fonts/specimen/Salsa"
+  def install
+    
+  test do
+  end
+end
