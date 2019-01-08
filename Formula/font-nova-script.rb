324037ffddaaf939b2a88f3282bda1c8c0cf19a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nova-script.rb from d9671613fa2bf6efc3f490653b8b7f9bfe859400

---
+++ Formula/font-nova-script.rb
@@ -0,0 +1,9 @@
+class FontNovaScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/novascript/NovaScript-Regular.ttf"
+  desc "Nova Script"
+  homepage "https://www.google.com/fonts/specimen/Nova%20Script"
+  def install
+    
+  test do
+  end
+end
