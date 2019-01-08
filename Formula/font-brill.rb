5961150a9e8363651154a6a71d39338f835338a6
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:53:48 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-brill.rb from 82b13c722b57af1ab3714567372a6e1f81d1b1ab

---
+++ Formula/font-brill.rb
@@ -0,0 +1,15 @@
+class FontBrill < Formula
+  version "2.06"
+  sha256 "b58eeb26ea43d9a020ac9891a395ff7364bac7215d0714910561b675e4947166"
+  url "https://brill.com/fileasset/downloads_static/static_typefacedownload_typefacepackage.zip"
+  desc "Brill"
+  homepage "https://brill.com/page/BrillFont/"
+  def install
+    (share/"fonts").install "Brill_Typeface_#{version}/Brill Bold #{version} 051.ttf"
+    (share/"fonts").install "Brill_Typeface_#{version}/Brill Bold Italic #{version} 051.ttf"
+    (share/"fonts").install "Brill_Typeface_#{version}/Brill Italic #{version} 051.ttf"
+    (share/"fonts").install "Brill_Typeface_#{version}/Brill Roman #{version} 051.ttf"
+  end
+  test do
+  end
+end
