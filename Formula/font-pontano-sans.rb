b1c5d924b8d1c4ddeaf44b851f57c408d13842db
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:21 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pontano-sans.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-pontano-sans.rb
@@ -0,0 +1,9 @@
+class FontPontanoSans < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/pontanosans/PontanoSans-Regular.ttf"
+  desc "Pontano Sans"
+  homepage "https://www.google.com/fonts/specimen/Pontano+Sans"
+  def install
+    
+  test do
+  end
+end
