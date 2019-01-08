22314e0a6f84a06795e4421347b7562854bce50f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nova-oval.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-nova-oval.rb
@@ -0,0 +1,9 @@
+class FontNovaOval < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/novaoval/NovaOval.ttf"
+  desc "Nova Oval"
+  homepage "https://www.google.com/fonts/specimen/Nova%20Oval"
+  def install
+    
+  test do
+  end
+end
