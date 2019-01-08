bb145cbd0f29ddedc449274757535d462ff178a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rouge-script.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-rouge-script.rb
@@ -0,0 +1,9 @@
+class FontRougeScript < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/rougescript/RougeScript-Regular.ttf"
+  desc "Rouge Script"
+  homepage "https://www.google.com/fonts/specimen/Rouge+Script"
+  def install
+    
+  test do
+  end
+end
