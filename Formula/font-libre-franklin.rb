0e4e9e843ef0114021d0de66559f3a600a8dbdcf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-libre-franklin.rb from 40e426a8da7c45ce42a20e1422fd55be258b1022

---
+++ Formula/font-libre-franklin.rb
@@ -0,0 +1,9 @@
+class FontLibreFranklin < Formula
+  head "https://github.com/impallari/Libre-Franklin/archive/master.zip"
+  desc "Libre Franklin"
+  homepage "https://github.com/impallari/Libre-Franklin"
+  def install
+    
+  test do
+  end
+end
