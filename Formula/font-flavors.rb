98011b948c2201a10d163e30e77e6baf68254e85
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:33 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-flavors.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-flavors.rb
@@ -0,0 +1,9 @@
+class FontFlavors < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/flavors/Flavors-Regular.ttf"
+  desc "Flavors"
+  homepage "https://www.google.com/fonts/specimen/Flavors"
+  def install
+    
+  test do
+  end
+end
