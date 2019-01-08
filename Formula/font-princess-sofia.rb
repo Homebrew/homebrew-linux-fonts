11a828fcf0565de2daa8d3e0fa0535a616f2d748
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:27 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-princess-sofia.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-princess-sofia.rb
@@ -0,0 +1,9 @@
+class FontPrincessSofia < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/princesssofia/PrincessSofia-Regular.ttf"
+  desc "Princess Sofia"
+  homepage "https://www.google.com/fonts/specimen/Princess+Sofia"
+  def install
+    
+  test do
+  end
+end
