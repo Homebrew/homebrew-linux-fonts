04b0f93d0cb9b3cc11bdc8989bdaccbedcbf4553
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:31 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-cuneiform.rb from 91ec7fe0323c8467fb529c8559b961db2070ea2c

---
+++ Formula/font-noto-sans-cuneiform.rb
@@ -0,0 +1,9 @@
+class FontNotoSansCuneiform < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCuneiform-unhinted.zip"
+  desc "Noto Sans Cuneiform"
+  homepage "https://www.google.com/get/noto/#sans-xsux"
+  def install
+    
+  test do
+  end
+end
