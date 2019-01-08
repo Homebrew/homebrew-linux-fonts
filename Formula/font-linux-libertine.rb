4f2682af9feb9d25ad8a1150a63d71d882c6dc26
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-linux-libertine.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-linux-libertine.rb
@@ -0,0 +1,11 @@
+class FontLinuxLibertine < Formula
+  version "5.3.0_2012_07_02"
+  sha256 "24a593a949808d976850131a953c0c0d7a72299531dfbb348191964cc038d75d"
+  url "https://downloads.sourceforge.net/linuxlibertine/LinLibertineTTF_#{version}.tgz"
+  desc "Linux Libertine"
+  homepage "http://linuxlibertine.org/"
+  def install
+    
+  test do
+  end
+end
