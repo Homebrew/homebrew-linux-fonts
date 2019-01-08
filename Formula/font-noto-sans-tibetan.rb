b711e5acb2bfc0d4acc25bba6251907014a26204
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tibetan.rb from 45cd631147cee78c9cf83f81cc0be2270a59d7eb

---
+++ Formula/font-noto-sans-tibetan.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTibetan < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTibetan-unhinted.zip"
+  desc "Noto Sans Tibetan"
+  homepage "https://www.google.com/get/noto/#sans-tibt"
+  def install
+    
+  test do
+  end
+end
