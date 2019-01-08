c2ba57dd1bd24b8174cd6863e00426e2d09e27e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:34 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fontdiner-swanky.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-fontdiner-swanky.rb
@@ -0,0 +1,9 @@
+class FontFontdinerSwanky < Formula
+  head "https://github.com/google/fonts/raw/master/apache/fontdinerswanky/FontdinerSwanky-Regular.ttf"
+  desc "Fontdiner Swanky"
+  homepage "https://www.google.com/fonts/specimen/Fontdiner+Swanky"
+  def install
+    
+  test do
+  end
+end
