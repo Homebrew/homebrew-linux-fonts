2595d74ca04dbb70afbf7d90cd9e65a41e050788
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-verdana.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-verdana.rb
@@ -0,0 +1,9 @@
+class FontVerdana < Formula
+  head "https://downloads.sourceforge.net/corefonts/verdan32.exe"
+  desc "Verdana"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  def install
+    
+  test do
+  end
+end
