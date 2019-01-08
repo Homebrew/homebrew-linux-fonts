4faec28cc36d0e6c35a9fad1c2d0d13dabf0c173
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:42 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-butler.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-butler.rb
@@ -0,0 +1,9 @@
+class FontButler < Formula
+  head "https://fabiandesmet.com/wp-content/uploads/Butler_and_ButlerStencil.zip"
+  desc "Butler"
+  homepage "https://fabiandesmet.com/portfolio/butler-font/"
+  def install
+    
+  test do
+  end
+end
