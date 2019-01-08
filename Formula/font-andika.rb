480aa6378860e119316b42ee3dbf3b122a24cff0
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-andika.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-andika.rb
+++ Formula/font-andika.rb
@@ -1,7 +1,7 @@
 class FontAndika < Formula
   version "5.000"
   sha256 "604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507"
-  url "https://software.sil.org/downloads/r/andika/Andika-#{version}.zip"
+  head "https://software.sil.org/downloads/r/andika/Andika-#{version}.zip"
   desc "Andika"
   homepage "https://software.sil.org/andika/"
   def install
