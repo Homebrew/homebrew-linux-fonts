a55efafbaaee0e7143ce2d31804147dabeb2466c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:30 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ibm-plex.rb from f69ce9d9c4f80b66846b83bef85a669a10861620

---
+++ Formula/font-ibm-plex.rb
@@ -0,0 +1,11 @@
+class FontIbmPlex < Formula
+  version "1.1.6"
+  sha256 "f9514ba1cecb0a862296ce7f7c9d754e7b40fb64b55af66cf2e80b0c1bb3c596"
+  url "https://github.com/IBM/plex/releases/download/v#{version}/OpenType.zip"
+  desc "IBM Plex"
+  homepage "https://github.com/IBM/plex"
+  def install
+    
+  test do
+  end
+end
