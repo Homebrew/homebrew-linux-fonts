e9fb47becaf3a4ff17f3e2ecb9af2ce1ca97b1a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-volkhov.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-volkhov.rb
@@ -0,0 +1,10 @@
+class FontVolkhov < Formula
+  head "https://github.com/google/fonts/trunk/ofl/volkhov", using: :svn, revision: "50", trust_cert: true
+  desc "Volkhov"
+  homepage "https://www.google.com/fonts/specimen/Volkhov"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
