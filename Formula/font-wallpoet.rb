9a34e6495792dcd21f703f4f6fac3ebc7597330e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:55 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wallpoet.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-wallpoet.rb
@@ -0,0 +1,9 @@
+class FontWallpoet < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/wallpoet/Wallpoet-Regular.ttf"
+  desc "Wallpoet"
+  homepage "https://www.google.com/fonts/specimen/Wallpoet"
+  def install
+    
+  test do
+  end
+end
