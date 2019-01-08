9715c2a25f593b0678ee1c3e81d9619991c159fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bebas-neue.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-bebas-neue.rb
@@ -0,0 +1,9 @@
+class FontBebasNeue < Formula
+  head "https://fontfabric.com/downfont/bebas.zip"
+  desc "Bebas Neue"
+  homepage "https://fontfabric.com/bebas-neue/"
+  def install
+    
+  test do
+  end
+end
