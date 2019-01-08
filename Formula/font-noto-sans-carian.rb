0b025696e99d2e01dc7d51948900c5f2d997e758
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:25 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-carian.rb from 91ec7fe0323c8467fb529c8559b961db2070ea2c

---
+++ Formula/font-noto-sans-carian.rb
@@ -0,0 +1,9 @@
+class FontNotoSansCarian < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCarian-unhinted.zip"
+  desc "Noto Sans Carian"
+  homepage "https://www.google.com/get/noto/#sans-cari"
+  def install
+    
+  test do
+  end
+end
