6fa883570de0cb16aa6630c81783109cb25b3acd
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hasklig-nerd-font.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-hasklig-nerd-font.rb
@@ -0,0 +1,11 @@
+class FontHaskligNerdFont < Formula
+  version "2.0.0"
+  sha256 "1fd1d88f2ec48424654888e4c7afad7a423e4229f40b09be323dbf4a04600dbd"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hasklig.zip"
+  desc "Hasklug Nerd Font (Hasklig)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
