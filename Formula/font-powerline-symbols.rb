d884a1e1b878ed49f44ba6b43bd31524da07b9a5
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-powerline-symbols.rb from 69fca2d215c693bbd6a45418ec4d30c753c6ad57

---
+++ Formula/font-powerline-symbols.rb
@@ -0,0 +1,9 @@
+class FontPowerlineSymbols < Formula
+  head "https://github.com/powerline/powerline/raw/master/font/PowerlineSymbols.otf"
+  desc "Powerline Symbols"
+  homepage "https://github.com/powerline/powerline"
+  def install
+    
+  test do
+  end
+end
