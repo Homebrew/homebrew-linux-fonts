b310a3336d5564a82d9306e3cc1059b2974f48b1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:17:24 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-felipa.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-felipa.rb
@@ -0,0 +1,9 @@
+class FontFelipa < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/felipa/Felipa-Regular.ttf"
+  desc "Felipa"
+  homepage "https://www.google.com/fonts/specimen/Felipa"
+  def install
+    
+  test do
+  end
+end
