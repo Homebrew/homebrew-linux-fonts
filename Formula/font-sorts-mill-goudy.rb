b37f50a0cad6ca61f1a2ad2eb822e994f9864048
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:45 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sorts-mill-goudy.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-sorts-mill-goudy.rb
@@ -0,0 +1,10 @@
+class FontSortsMillGoudy < Formula
+  head "https://github.com/google/fonts/trunk/ofl/sortsmillgoudy", using: :svn, revision: "50", trust_cert: true
+  desc "Sorts Mill Goudy"
+  homepage "https://www.google.com/fonts/specimen/Sorts%20Mill%20Goudy"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
