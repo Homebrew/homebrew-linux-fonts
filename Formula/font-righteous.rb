bc968498acc0fabd9072979b9dc3c9cf3d3e20e1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:54 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-righteous.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-righteous.rb
@@ -0,0 +1,9 @@
+class FontRighteous < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/righteous/Righteous-Regular.ttf"
+  desc "Righteous"
+  homepage "https://www.google.com/fonts/specimen/Righteous"
+  def install
+    
+  test do
+  end
+end
