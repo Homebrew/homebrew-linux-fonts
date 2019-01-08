cd8706aea87644ae758038f02a34c10e9769cfe7
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:22 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-text-me-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-text-me-one.rb
@@ -0,0 +1,9 @@
+class FontTextMeOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/textmeone/TextMeOne-Regular.ttf"
+  desc "Text Me One"
+  homepage "https://www.google.com/fonts/specimen/Text+Me+One"
+  def install
+    
+  test do
+  end
+end
