e5bf30577c94cca021db00ba869ccf48588ff2a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:57 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chango.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-chango.rb
@@ -0,0 +1,9 @@
+class FontChango < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/chango/Chango-Regular.ttf"
+  desc "Chango"
+  homepage "https://www.google.com/fonts/specimen/Chango"
+  def install
+    
+  test do
+  end
+end
