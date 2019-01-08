bf49c1f3417679ae50d2c4811446db1bce669207
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sans-forgetica.rb from 76bd93ff8502f9356d7772a5abfa1b17ca0cf082

---
+++ Formula/font-sans-forgetica.rb
@@ -0,0 +1,9 @@
+class FontSansForgetica < Formula
+  head "http://sansforgetica.rmit/Common/Zips/Sans%20Forgetica.zip"
+  desc "Sans Forgetica"
+  homepage "http://sansforgetica.rmit/"
+  def install
+    
+  test do
+  end
+end
