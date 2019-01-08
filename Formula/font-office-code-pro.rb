c0c69af79c123b1b431acc15989c07dd41775af0
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:03:21 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-office-code-pro.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-office-code-pro.rb
+++ Formula/font-office-code-pro.rb
@@ -1,7 +1,7 @@
 class FontOfficeCodePro < Formula
   version "1.004"
   sha256 "9e24d15309ead8c523ec3f0444ed9c171bba535e109c43b1dde8abfa9d359150"
-  url "https://github.com/nathco/Office-Code-Pro/archive/#{version}.zip"
+  head "https://github.com/nathco/Office-Code-Pro/archive/#{version}.zip"
   desc "Office Code Pro"
   homepage "https://github.com/nathco/Office-Code-Pro"
   def install
