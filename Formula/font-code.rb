3ebfd5e73d333f3991eb2d5d54c029126966d853
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-code.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-code.rb
@@ -0,0 +1,9 @@
+class FontCode < Formula
+  head "https://fontfabric.com/downfont/code.zip"
+  desc "Code"
+  homepage "https://fontfabric.com/code-free-font-3/"
+  def install
+    
+  test do
+  end
+end
