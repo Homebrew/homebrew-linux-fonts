ce0bbef02f7b3e8676970cca275b67c3beb37aaf
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:14:44 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-andagii.rb from 3e7ca1d3fa78f7c746f0f2cb4ac86f54e5cca378

---
+++ Formula/font-andagii.rb
@@ -0,0 +1,9 @@
+class FontAndagii < Formula
+  head "http://www.i18nguy.com/unicode/andagii.zip", user_agent: :fake
+  desc "Andagii"
+  homepage "http://www.i18nguy.com/unicode/unicode-font.html"
+  def install
+    
+  test do
+  end
+end
