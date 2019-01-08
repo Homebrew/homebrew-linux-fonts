8dd06321f39a7ec80b5a184b92045abfe44740e0
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:36 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-english-sc.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-im-fell-english-sc.rb
@@ -0,0 +1,9 @@
+class FontImFellEnglishSc < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/imfellenglishsc/IMFeENsc28P.ttf"
+  desc "IM Fell English SC"
+  homepage "https://www.google.com/fonts/specimen/IM+Fell+English+SC"
+  def install
+    
+  test do
+  end
+end
