68a38c7f9a1b918b142082cd6e71d15eada09389
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:37:07 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-zeyada.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-zeyada.rb
@@ -0,0 +1,9 @@
+class FontZeyada < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/zeyada/Zeyada.ttf"
+  desc "Zeyada"
+  homepage "https://www.google.com/fonts/specimen/Zeyada"
+  def install
+    
+  test do
+  end
+end
