668d262ca072bca808cf2c855a2897ecb3c1b69c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:16 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hanalei.rb from bc9bf520bfdfcb73bc2752aa39f0bf6cc996abee

---
+++ Formula/font-hanalei.rb
@@ -0,0 +1,9 @@
+class FontHanalei < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/hanalei/Hanalei-Regular.ttf"
+  desc "Hanalei"
+  homepage "https://www.google.com/fonts/specimen/Hanalei"
+  def install
+    
+  test do
+  end
+end
