9d2ac41fb1cfce29d71f38c15658e2d2197e8924
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:05 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jsmath-cmbx10.rb from 2a087886606b136dba2e0d6e96ea2b626fcda7ef

---
+++ Formula/font-jsmath-cmbx10.rb
@@ -0,0 +1,9 @@
+class FontJsmathCmbx10 < Formula
+  head "http://www.math.union.edu/~dpvc/jsmath/download/TeX-fonts-10.zip"
+  desc "jsMath-cmbx10"
+  homepage "http://www.math.union.edu/~dpvc/jsmath/download/jsmath-fonts.html"
+  def install
+    
+  test do
+  end
+end
