00271c57c5cd81b392beb842117d02cc6bc432f8
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:53 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ionicons.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-ionicons.rb
@@ -0,0 +1,11 @@
+class FontIonicons < Formula
+  version "2.0.1"
+  sha256 "b222fcaede908b71d5b206db9fb7b625a07d313be00ee908eabd267604868661"
+  url "https://github.com/ionic-team/ionicons/archive/v#{version}.zip"
+  desc "Ionicons"
+  homepage "https://github.com/ionic-team/ionicons"
+  def install
+    
+  test do
+  end
+end
