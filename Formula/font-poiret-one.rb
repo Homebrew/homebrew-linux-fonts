3cd4e0ef3d13d5e71b7cbc975742d523d261cb18
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:34:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-poiret-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-poiret-one.rb
@@ -0,0 +1,9 @@
+class FontPoiretOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/poiretone/PoiretOne-Regular.ttf"
+  desc "Poiret One"
+  homepage "https://www.google.com/fonts/specimen/Poiret+One"
+  def install
+    
+  test do
+  end
+end
