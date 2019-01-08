fdb9c2db65fecd82c32383b4746fb86db06db12f
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wendy-one.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-wendy-one.rb
@@ -0,0 +1,9 @@
+class FontWendyOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/wendyone/WendyOne-Regular.ttf"
+  desc "Wendy One"
+  homepage "https://www.google.com/fonts/specimen/Wendy+One"
+  def install
+    
+  test do
+  end
+end
