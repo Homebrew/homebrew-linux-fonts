4c714cc991d9351fd9671a3d98b327da7dd2ccd9
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:09 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-taprom.rb from 2ceb0461c9095767288ca30214dc45b02e5555b5

---
+++ Formula/font-taprom.rb
@@ -0,0 +1,9 @@
+class FontTaprom < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/taprom/Taprom.ttf"
+  desc "Taprom"
+  homepage "https://www.google.com/fonts/specimen/Taprom"
+  def install
+    
+  test do
+  end
+end
