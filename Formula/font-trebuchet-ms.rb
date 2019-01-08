3d1092d1b4ffe74d6553f4f4b5baeb428d2806c1
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:36:29 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-trebuchet-ms.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-trebuchet-ms.rb
@@ -0,0 +1,9 @@
+class FontTrebuchetMs < Formula
+  head "https://downloads.sourceforge.net/corefonts/trebuc32.exe"
+  desc "Trebuchet MS"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  def install
+    
+  test do
+  end
+end
