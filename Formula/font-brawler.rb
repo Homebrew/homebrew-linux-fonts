988be28b6703a239b8044c8da1db76abb5892c1e
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:35 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-brawler.rb from 33a294768ad259203de32e33398d8d7ba1304e07

---
+++ Formula/font-brawler.rb
@@ -0,0 +1,9 @@
+class FontBrawler < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/brawler/Brawler-Regular.ttf"
+  desc "Brawler"
+  homepage "https://www.google.com/fonts/specimen/Brawler"
+  def install
+    
+  test do
+  end
+end
