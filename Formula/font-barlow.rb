6b96aa2b09d2a7ec366970b7f9f4271d0f772d8c
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 22:34:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-barlow.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-barlow.rb
+++ Formula/font-barlow.rb
@@ -1,7 +1,7 @@
 class FontBarlow < Formula
   version "1.403"
   sha256 "a75384658b2c49048bc82a0e1423938bc6e170a7ca297fe43b3b0f8a389bacdf"
-  head "https://github.com/jpt/barlow/archive/v#{version}.zip"
+  url "https://github.com/jpt/barlow/archive/v#{version}.zip"
   desc "Barlow"
   homepage "https://tribby.com/fonts/barlow"
   def install
