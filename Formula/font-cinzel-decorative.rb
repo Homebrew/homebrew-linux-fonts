cd0e782fdbd94a7ee80406b2488cf0656fd7c40a
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:16:04 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cinzel-decorative.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-cinzel-decorative.rb
@@ -0,0 +1,10 @@
+class FontCinzelDecorative < Formula
+  head "https://github.com/google/fonts/trunk/ofl/cinzeldecorative", using: :svn, revision: "50", trust_cert: true
+  desc "Cinzel Decorative"
+  homepage "https://www.google.com/fonts/specimen/Cinzel%20Decorative"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
