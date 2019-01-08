780d6c4252d8d8c4198a917150a2d0b42ce8cd7c
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:17 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-adlam.rb from a877d71f000c4cd26ee9ddccfec7d06085e2aa0a

---
+++ Formula/font-noto-sans-adlam.rb
@@ -0,0 +1,9 @@
+class FontNotoSansAdlam < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAdlam-unhinted.zip"
+  desc "Noto Sans Adlam"
+  homepage "https://www.google.com/get/noto/#sans-adlm"
+  def install
+    
+  test do
+  end
+end
