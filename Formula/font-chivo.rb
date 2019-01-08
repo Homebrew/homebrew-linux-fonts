534a080652e470df39f4f9d6de9ba6588b638dac
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:54:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chivo.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-chivo.rb
@@ -0,0 +1,14 @@
+class FontChivo < Formula
+  head "https://github.com/google/fonts/trunk/ofl/chivo", using: :svn, revision: "50", trust_cert: true
+  desc "Chivo"
+  homepage "https://www.google.com/fonts/specimen/Chivo"
+  depends_on macos: ">= :sierra"
+  def install
+    (share/"fonts").install "Chivo-Black.ttf"
+    (share/"fonts").install "Chivo-BlackItalic.ttf"
+    (share/"fonts").install "Chivo-Italic.ttf"
+    (share/"fonts").install "Chivo-Regular.ttf"
+  end
+  test do
+  end
+end
