90750226e26873b515fde96228097fd8f1f28b54
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:14 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-halant.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-halant.rb
@@ -0,0 +1,11 @@
+class FontHalant < Formula
+  version "2.000"
+  sha256 "d65e1077b7c3d3ca42cf41f8ee2db4f6da5cc8aa6f615d18a1bb497f8e37ca63"
+  url "https://github.com/itfoundry/halant/releases/download/v#{version}/halant-#{version.to_s.gsub(".", "_")}.zip"
+  desc "Halant"
+  homepage "https://github.com/itfoundry/halant"
+  def install
+    
+  test do
+  end
+end
