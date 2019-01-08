d3ef261f062e12e9804b41062ae6611ddab2f203
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:18:10 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gudea.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-gudea.rb
@@ -0,0 +1,10 @@
+class FontGudea < Formula
+  head "https://github.com/google/fonts/trunk/ofl/gudea", using: :svn, revision: "50", trust_cert: true
+  desc "Gudea"
+  homepage "https://www.google.com/fonts/specimen/Gudea"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
