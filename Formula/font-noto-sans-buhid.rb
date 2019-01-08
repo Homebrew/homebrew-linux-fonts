d857fd9d47b467eb590b1c9f394a1fbf3aad0952
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:32:23 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-buhid.rb from 7723813d0eff0185d8450b369ce8d1fab047bf79

---
+++ Formula/font-noto-sans-buhid.rb
@@ -0,0 +1,9 @@
+class FontNotoSansBuhid < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBuhid-unhinted.zip"
+  desc "Noto Sans Buhid"
+  homepage "https://www.google.com/get/noto/#sans-buhd"
+  def install
+    
+  test do
+  end
+end
