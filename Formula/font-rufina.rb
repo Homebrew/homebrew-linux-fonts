fedffe57b3b874ed79a2d7e58720a35640766207
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:35:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rufina.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-rufina.rb
@@ -0,0 +1,10 @@
+class FontRufina < Formula
+  head "https://github.com/google/fonts/trunk/ofl/rufina", using: :svn, revision: "50", trust_cert: true
+  desc "Rufina"
+  homepage "https://www.google.com/fonts/specimen/Rufina"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
