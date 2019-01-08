a6ff43c2a0969b2a13aa431af9c61b431194cf41
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:15:43 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cabin-condensed.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-cabin-condensed.rb
@@ -0,0 +1,10 @@
+class FontCabinCondensed < Formula
+  head "https://github.com/google/fonts/trunk/ofl/cabincondensed", using: :svn, revision: "50", trust_cert: true
+  desc "Cabin Condensed"
+  homepage "https://www.google.com/fonts/specimen/Cabin%20Condensed"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
