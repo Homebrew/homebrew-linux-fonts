212530900d7a9141c1978cf4f68fad225b251c63
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:31:18 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-merienda.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

---
+++ Formula/font-merienda.rb
@@ -0,0 +1,10 @@
+class FontMerienda < Formula
+  head "https://github.com/google/fonts/trunk/ofl/merienda", using: :svn, revision: "50", trust_cert: true
+  desc "Merienda"
+  homepage "https://www.google.com/fonts/specimen/Merienda"
+  depends_on macos: ">= :sierra"
+  def install
+    
+  test do
+  end
+end
