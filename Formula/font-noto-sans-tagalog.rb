d7e3fdd392a34258ef04068a898e0c87bc411038
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:08 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tagalog.rb from 429fe80b45ad0fcc4027e2cc38e6908c61e6d571

---
+++ Formula/font-noto-sans-tagalog.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTagalog < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagalog-unhinted.zip"
+  desc "Noto Sans Tagalog"
+  homepage "https://www.google.com/get/noto/#sans-tglg"
+  def install
+    
+  test do
+  end
+end
