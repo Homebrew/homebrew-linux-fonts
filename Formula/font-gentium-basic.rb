59e639a212aa7b65d57cc1e6d886c27c1c0f5b40
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-08 11:55:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gentium-basic.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

---
+++ Formula/font-gentium-basic.rb
@@ -0,0 +1,15 @@
+class FontGentiumBasic < Formula
+  version "1.102"
+  sha256 "2f1a2c5491d7305dffd3520c6375d2f3e14931ee35c6d8ae1e8f098bf1a7b3cc"
+  url "https://software.sil.org/downloads/r/gentium/GentiumBasic_#{version.to_s.gsub(".", "")}.zip"
+  desc "Gentium Basic"
+  homepage "https://software.sil.org/gentium/"
+  def install
+    (share/"fonts").install "GentiumBasic_#{version.no_dots}/GenBasB.ttf"
+    (share/"fonts").install "GentiumBasic_#{version.no_dots}/GenBasBI.ttf"
+    (share/"fonts").install "GentiumBasic_#{version.no_dots}/GenBasI.ttf"
+    (share/"fonts").install "GentiumBasic_#{version.no_dots}/GenBasR.ttf"
+  end
+  test do
+  end
+end
