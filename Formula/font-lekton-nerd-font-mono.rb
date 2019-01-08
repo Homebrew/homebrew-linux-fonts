3d8d1603e7adb3e1518ae0694b7eb67f8ea6f412
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:19:38 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lekton-nerd-font-mono.rb from 69560483c2ff7db7741094527558445da3049a52

---
+++ Formula/font-lekton-nerd-font-mono.rb
@@ -0,0 +1,11 @@
+class FontLektonNerdFontMono < Formula
+  version "2.0.0"
+  sha256 "07ef874e46462524c53c1923ffc98aa53f7c2544303e326f218edd101f775ea0"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Lekton.zip"
+  desc "Lekton Nerd Font (Lekton)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    
+  test do
+  end
+end
