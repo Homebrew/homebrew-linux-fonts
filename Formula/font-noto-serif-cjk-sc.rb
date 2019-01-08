0f9845a9e6d1a7f5b1543b2fd5cfca33ecc4396b
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-01-08 17:33:20 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-cjk-sc.rb from 45cd631147cee78c9cf83f81cc0be2270a59d7eb

---
+++ Formula/font-noto-serif-cjk-sc.rb
@@ -0,0 +1,9 @@
+class FontNotoSerifCjkSc < Formula
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKsc-hinted.zip"
+  desc "Noto Serif CJK SC"
+  homepage "https://www.google.com/get/noto/#serif-hans"
+  def install
+    
+  test do
+  end
+end
