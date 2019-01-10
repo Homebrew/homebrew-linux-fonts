a7d2602a0a83ac0c8a114be49e4ed0990dc42d15
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:43 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-almendra-sc.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-almendra-sc.rb
+++ Formula/font-almendra-sc.rb
@@ -2,7 +2,6 @@ class FontAlmendraSc < Formula
   head "https://github.com/google/fonts/trunk/ofl/almendrasc", using: :svn, revision: "50", trust_cert: true
   desc "Almendra SC"
   homepage "https://www.google.com/fonts/specimen/Almendra%20SC"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "AlmendraSC-Bold.ttf"
     (share/"fonts").install "AlmendraSC-BoldItalic.ttf"
