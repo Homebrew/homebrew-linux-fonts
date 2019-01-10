fa8cce258a3b7456fa8e01af1577241caa3993be
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-titillium-web.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-titillium-web.rb
+++ Formula/font-titillium-web.rb
@@ -2,7 +2,6 @@ class FontTitilliumWeb < Formula
   head "https://github.com/google/fonts/trunk/ofl/titilliumweb", using: :svn, revision: "50", trust_cert: true
   desc "Titillium Web"
   homepage "https://www.google.com/fonts/specimen/Titillium%20Web"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "TitilliumWeb-Black.ttf"
     (share/"fonts").install "TitilliumWeb-Bold.ttf"
