a9f0017bbffe9a190c872bae541d1fbb6793cef0
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tinos.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-tinos.rb
+++ Formula/font-tinos.rb
@@ -2,7 +2,6 @@ class FontTinos < Formula
   head "https://github.com/google/fonts/trunk/apache/tinos", using: :svn, revision: "50", trust_cert: true
   desc "Tinos"
   homepage "https://www.google.com/fonts/specimen/Tinos"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Tinos-Bold.ttf"
     (share/"fonts").install "Tinos-BoldItalic.ttf"
