192588501b7684acde8bacb9ac087f429023daa1
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-roboto-condensed.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-roboto-condensed.rb
+++ Formula/font-roboto-condensed.rb
@@ -2,7 +2,6 @@ class FontRobotoCondensed < Formula
   head "https://github.com/google/fonts/trunk/apache/robotocondensed", using: :svn, trust_cert: true
   desc "Roboto Condensed"
   homepage "https://www.google.com/fonts/specimen/Roboto%20Condensed"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "RobotoCondensed-Bold.ttf"
     (share/"fonts").install "RobotoCondensed-BoldItalic.ttf"
