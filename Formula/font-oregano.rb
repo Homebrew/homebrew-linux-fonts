884f739617ac59de2661848a65ad2833b2daec16
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oregano.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-oregano.rb
+++ Formula/font-oregano.rb
@@ -2,7 +2,6 @@ class FontOregano < Formula
   head "https://github.com/google/fonts/trunk/ofl/oregano", using: :svn, revision: "50", trust_cert: true
   desc "Oregano"
   homepage "https://www.google.com/fonts/specimen/Oregano"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Oregano-Italic.ttf"
     (share/"fonts").install "Oregano-Regular.ttf"
