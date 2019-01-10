aee4de8bbc32b9080e637e9ec0af2fa3a52ec236
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-judson.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-judson.rb
+++ Formula/font-judson.rb
@@ -2,7 +2,6 @@ class FontJudson < Formula
   head "https://github.com/google/fonts/trunk/ofl/judson", using: :svn, revision: "50", trust_cert: true
   desc "Judson"
   homepage "https://www.google.com/fonts/specimen/Judson"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Judson-Bold.ttf"
     (share/"fonts").install "Judson-Italic.ttf"
