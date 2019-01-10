57434977e3fdde58e75a23436d84f722df33ebeb
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-expletus-sans.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-expletus-sans.rb
+++ Formula/font-expletus-sans.rb
@@ -2,7 +2,6 @@ class FontExpletusSans < Formula
   head "https://github.com/google/fonts/trunk/ofl/expletussans", using: :svn, revision: "50", trust_cert: true
   desc "Expletus Sans"
   homepage "https://www.google.com/fonts/specimen/Expletus%20Sans"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "ExpletusSans-Bold.ttf"
     (share/"fonts").install "ExpletusSans-BoldItalic.ttf"
