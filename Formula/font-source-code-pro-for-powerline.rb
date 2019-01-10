b72cd0afb1293ac35397fa01008d005c9a83eeaa
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-code-pro-for-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-source-code-pro-for-powerline.rb
+++ Formula/font-source-code-pro-for-powerline.rb
@@ -2,7 +2,6 @@ class FontSourceCodeProForPowerline < Formula
   head "https://github.com/powerline/fonts/trunk/SourceCodePro", using: :svn, revision: "50", trust_cert: true
   desc "Source Code Pro for Powerline"
   homepage "https://github.com/powerline/fonts/tree/master/SourceCodePro"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Sauce Code Powerline Black.otf"
     (share/"fonts").install "Sauce Code Powerline Bold.otf"
