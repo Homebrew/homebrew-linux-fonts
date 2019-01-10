00ac4e30ae5c4027d2e5e8b9c6c958a9eb796df5
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-saira.rb from 577374024b27a8115cd45f677d37b660bc665722

--- Formula/font-saira.rb
+++ Formula/font-saira.rb
@@ -2,7 +2,6 @@ class FontSaira < Formula
   head "https://github.com/google/fonts/trunk/ofl/saira", using: :svn, trust_cert: true
   desc "Saira"
   homepage "https://fonts.google.com/specimen/Saira"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Saira-Black.ttf"
     (share/"fonts").install "Saira-Bold.ttf"
