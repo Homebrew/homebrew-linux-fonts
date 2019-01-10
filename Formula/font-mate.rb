9e8be9dbfae6b950002682f5bd71d4f70717cccc
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mate.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-mate.rb
+++ Formula/font-mate.rb
@@ -2,7 +2,6 @@ class FontMate < Formula
   head "https://github.com/google/fonts/trunk/ofl/mate", using: :svn, revision: "50", trust_cert: true
   desc "Mate"
   homepage "https://www.google.com/fonts/specimen/Mate"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Mate-Italic.ttf"
     (share/"fonts").install "Mate-Regular.ttf"
