d7a8fffece16b3fe856022cbc8e653321ca5dba8
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noticia-text.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-noticia-text.rb
+++ Formula/font-noticia-text.rb
@@ -2,7 +2,6 @@ class FontNoticiaText < Formula
   head "https://github.com/google/fonts/trunk/ofl/noticiatext", using: :svn, revision: "50", trust_cert: true
   desc "Noticia Text"
   homepage "https://www.google.com/fonts/specimen/Noticia%20Text"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "NoticiaText-Bold.ttf"
     (share/"fonts").install "NoticiaText-BoldItalic.ttf"
