cbf9ed0881a9f7a324be7715bc1aefa82bed1593
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-karla-tamil-upright.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-karla-tamil-upright.rb
+++ Formula/font-karla-tamil-upright.rb
@@ -2,7 +2,6 @@ class FontKarlaTamilUpright < Formula
   head "https://github.com/google/fonts/trunk/ofl/karlatamilupright", using: :svn, revision: "50", trust_cert: true
   desc "Karla Tamil Upright"
   homepage "https://www.google.com/fonts/earlyaccess"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "KarlaTamilUpright-Bold.ttf"
     (share/"fonts").install "KarlaTamilUpright-Regular.ttf"
