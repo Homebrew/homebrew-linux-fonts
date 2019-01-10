c7ecb47a14afcde82ab035254572760bb4e5cc81
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oxygen.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-oxygen.rb
+++ Formula/font-oxygen.rb
@@ -2,7 +2,6 @@ class FontOxygen < Formula
   head "https://github.com/google/fonts/trunk/ofl/oxygen", using: :svn, revision: "50", trust_cert: true
   desc "Oxygen"
   homepage "https://www.google.com/fonts/specimen/Oxygen"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Oxygen-Bold.ttf"
     (share/"fonts").install "Oxygen-Light.ttf"
