c9a409bef0c0f106386a42ecd90a14c47be49d11
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nobile.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-nobile.rb
+++ Formula/font-nobile.rb
@@ -2,7 +2,6 @@ class FontNobile < Formula
   head "https://github.com/google/fonts/trunk/ofl/nobile", using: :svn, revision: "50", trust_cert: true
   desc "Nobile"
   homepage "https://www.google.com/fonts/specimen/Nobile"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Nobile-Bold.ttf"
     (share/"fonts").install "Nobile-BoldItalic.ttf"
