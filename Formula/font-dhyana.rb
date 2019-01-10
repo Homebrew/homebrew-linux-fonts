36d328635bdf48db4a9ae44f8b72bfac5591a413
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dhyana.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-dhyana.rb
+++ Formula/font-dhyana.rb
@@ -2,7 +2,6 @@ class FontDhyana < Formula
   head "https://github.com/google/fonts/trunk/ofl/dhyana", using: :svn, revision: "50", trust_cert: true
   desc "Dhyana"
   homepage "https://www.google.com/fonts/earlyaccess"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Dhyana-Bold.ttf"
     (share/"fonts").install "Dhyana-Regular.ttf"
