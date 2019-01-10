f114bf1a7115f4221d221ca02f2246570795438c
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cabin.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-cabin.rb
+++ Formula/font-cabin.rb
@@ -2,7 +2,6 @@ class FontCabin < Formula
   head "https://github.com/google/fonts/trunk/ofl/cabin", using: :svn, revision: "50", trust_cert: true
   desc "Cabin"
   homepage "https://www.google.com/fonts/specimen/Cabin"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Cabin-Bold.ttf"
     (share/"fonts").install "Cabin-BoldItalic.ttf"
