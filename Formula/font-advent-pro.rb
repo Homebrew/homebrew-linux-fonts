a41bdb5293b88052b78b5b5502652b50e656f00b
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:43 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-advent-pro.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-advent-pro.rb
+++ Formula/font-advent-pro.rb
@@ -2,7 +2,6 @@ class FontAdventPro < Formula
   head "https://github.com/google/fonts/trunk/ofl/adventpro", using: :svn, revision: "50", trust_cert: true
   desc "Advent Pro"
   homepage "https://www.google.com/fonts/specimen/Advent%20Pro"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "AdventPro-Bold.ttf"
     (share/"fonts").install "AdventPro-ExtraLight.ttf"
