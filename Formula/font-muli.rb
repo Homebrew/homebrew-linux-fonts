9f2a69dbb1052fc5b1a2201577ca3553ce352950
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-muli.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-muli.rb
+++ Formula/font-muli.rb
@@ -2,7 +2,6 @@ class FontMuli < Formula
   head "https://github.com/google/fonts/trunk/ofl/muli", using: :svn, trust_cert: true
   desc "Muli"
   homepage "https://www.google.com/fonts/specimen/Muli"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Muli-Black.ttf"
     (share/"fonts").install "Muli-BlackItalic.ttf"
