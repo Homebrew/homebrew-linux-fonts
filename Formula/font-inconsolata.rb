964260bcf57fc23985a06e1811ec56e924b87237
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-inconsolata.rb
+++ Formula/font-inconsolata.rb
@@ -2,7 +2,6 @@ class FontInconsolata < Formula
   head "https://github.com/google/fonts/trunk/ofl/inconsolata", using: :svn, trust_cert: true
   desc "Inconsolata"
   homepage "https://levien.com/type/myfonts/inconsolata.html"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Inconsolata-Regular.ttf"
     (share/"fonts").install "Inconsolata-Bold.ttf"
