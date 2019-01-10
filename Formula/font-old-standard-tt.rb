bcfca2cc7dbfb0400960ccd4f61d9bf096dd6fbc
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-old-standard-tt.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-old-standard-tt.rb
+++ Formula/font-old-standard-tt.rb
@@ -2,7 +2,6 @@ class FontOldStandardTt < Formula
   head "https://github.com/google/fonts/trunk/ofl/oldstandardtt", using: :svn, revision: "50", trust_cert: true
   desc "Old Standard TT"
   homepage "https://www.google.com/fonts/specimen/Old%20Standard%20TT"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "OldStandard-Bold.ttf"
     (share/"fonts").install "OldStandard-Italic.ttf"
