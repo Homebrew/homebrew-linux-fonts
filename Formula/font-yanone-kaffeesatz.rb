924af40b75c0d76a9941a8208d20ef17d1ed44cc
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-yanone-kaffeesatz.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-yanone-kaffeesatz.rb
+++ Formula/font-yanone-kaffeesatz.rb
@@ -2,7 +2,6 @@ class FontYanoneKaffeesatz < Formula
   head "https://github.com/google/fonts/trunk/ofl/yanonekaffeesatz", using: :svn, revision: "50", trust_cert: true
   desc "Yanone Kaffeesatz"
   homepage "https://www.google.com/fonts/specimen/Yanone%20Kaffeesatz"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "YanoneKaffeesatz-Bold.ttf"
     (share/"fonts").install "YanoneKaffeesatz-ExtraLight.ttf"
