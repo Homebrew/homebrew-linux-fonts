c2e7337eaaaafb92646a201b6c150d6947ddf65a
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:43 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-andada.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-andada.rb
+++ Formula/font-andada.rb
@@ -2,7 +2,6 @@ class FontAndada < Formula
   head "https://github.com/google/fonts/trunk/ofl/andada", using: :svn, revision: "50", trust_cert: true
   desc "Andada"
   homepage "https://www.google.com/fonts/specimen/Andada"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Andada-Bold.ttf"
     (share/"fonts").install "Andada-BoldItalic.ttf"
