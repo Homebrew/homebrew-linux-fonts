467f8f7902748e366b31430061cc2c7a25b42434
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jura.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-jura.rb
+++ Formula/font-jura.rb
@@ -2,7 +2,6 @@ class FontJura < Formula
   head "https://github.com/google/fonts/trunk/ofl/jura", using: :svn, revision: "50", trust_cert: true
   desc "Jura"
   homepage "https://www.google.com/fonts/specimen/Jura"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Jura-DemiBold.ttf"
     (share/"fonts").install "Jura-Light.ttf"
