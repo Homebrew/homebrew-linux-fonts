ca81fd2b628410c7c4a5b0dbda190068443e04ee
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-puritan.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-puritan.rb
+++ Formula/font-puritan.rb
@@ -2,7 +2,6 @@ class FontPuritan < Formula
   head "https://github.com/google/fonts/trunk/ofl/puritan", using: :svn, revision: "50", trust_cert: true
   desc "Puritan"
   homepage "https://www.google.com/fonts/specimen/Puritan"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Puritan-Bold.ttf"
     (share/"fonts").install "Puritan-BoldItalic.ttf"
