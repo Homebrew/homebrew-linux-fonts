cfe4937aa87f36a9efc32d3c2d31139c16c83beb
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-scada.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-scada.rb
+++ Formula/font-scada.rb
@@ -2,7 +2,6 @@ class FontScada < Formula
   head "https://github.com/google/fonts/trunk/ofl/scada", using: :svn, revision: "50", trust_cert: true
   desc "Scada"
   homepage "https://www.google.com/fonts/specimen/Scada"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Scada-Bold.ttf"
     (share/"fonts").install "Scada-BoldItalic.ttf"
