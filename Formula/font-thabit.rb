0d7846ee657db33870a36bc1a3d175f49840f37d
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-thabit.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-thabit.rb
+++ Formula/font-thabit.rb
@@ -2,7 +2,6 @@ class FontThabit < Formula
   head "https://github.com/google/fonts/trunk/ofl/thabit", using: :svn, revision: "50", trust_cert: true
   desc "Thabit"
   homepage "https://www.arabeyes.org/"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Thabit-Bold.ttf"
     (share/"fonts").install "Thabit-BoldOblique.ttf"
