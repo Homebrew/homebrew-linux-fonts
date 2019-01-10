1c7acc18392e58c740a4d7c98b4d33663dfcaffc
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cousine.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-cousine.rb
+++ Formula/font-cousine.rb
@@ -2,7 +2,6 @@ class FontCousine < Formula
   head "https://github.com/google/fonts/trunk/apache/cousine", using: :svn, revision: "47", trust_cert: true
   desc "Cousine"
   homepage "https://www.google.com/fonts/specimen/Cousine"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Cousine-Regular.ttf"
     (share/"fonts").install "Cousine-Bold.ttf"
