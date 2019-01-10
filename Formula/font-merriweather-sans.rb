ddd62d1bea67ebbaa3c00f689530236dc7c156a9
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-merriweather-sans.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-merriweather-sans.rb
+++ Formula/font-merriweather-sans.rb
@@ -2,7 +2,6 @@ class FontMerriweatherSans < Formula
   head "https://github.com/google/fonts/trunk/ofl/merriweathersans", using: :svn, revision: "50", trust_cert: true
   desc "Merriweather Sans"
   homepage "https://www.google.com/fonts/specimen/Merriweather%20Sans"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "MerriweatherSans-Bold.ttf"
     (share/"fonts").install "MerriweatherSans-BoldItalic.ttf"
