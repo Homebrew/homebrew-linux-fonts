8ea245a033e4d760cb59a381c673acfff8951ac4
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lobster-two.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-lobster-two.rb
+++ Formula/font-lobster-two.rb
@@ -2,7 +2,6 @@ class FontLobsterTwo < Formula
   head "https://github.com/google/fonts/trunk/ofl/lobstertwo", using: :svn, revision: "50", trust_cert: true
   desc "Lobster Two"
   homepage "https://www.google.com/fonts/specimen/Lobster%20Two"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "LobsterTwo-Bold.ttf"
     (share/"fonts").install "LobsterTwo-BoldItalic.ttf"
