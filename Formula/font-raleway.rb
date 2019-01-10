8a959500b01e11f65a7053d866c41b93ddcb540a
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-raleway.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-raleway.rb
+++ Formula/font-raleway.rb
@@ -2,7 +2,6 @@ class FontRaleway < Formula
   head "https://github.com/google/fonts/trunk/ofl/raleway", using: :svn, revision: "50", trust_cert: true
   desc "Raleway"
   homepage "https://www.google.com/fonts/specimen/Raleway"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Raleway-Black.ttf"
     (share/"fonts").install "Raleway-BlackItalic.ttf"
