e3bccb500b6f6edec888608dd21bdce2deb13968
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sanchez.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-sanchez.rb
+++ Formula/font-sanchez.rb
@@ -2,7 +2,6 @@ class FontSanchez < Formula
   head "https://github.com/google/fonts/trunk/ofl/sanchez", using: :svn, revision: "50", trust_cert: true
   desc "Sanchez"
   homepage "https://www.google.com/fonts/specimen/Sanchez"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Sanchez-Italic.ttf"
     (share/"fonts").install "Sanchez-Regular.ttf"
