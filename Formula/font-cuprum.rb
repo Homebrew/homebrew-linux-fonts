bb86f22c765b1285d444e576d5e22611a1838a03
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cuprum.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-cuprum.rb
+++ Formula/font-cuprum.rb
@@ -2,7 +2,6 @@ class FontCuprum < Formula
   head "https://github.com/google/fonts/trunk/ofl/cuprum", using: :svn, revision: "50", trust_cert: true
   desc "Cuprum"
   homepage "https://www.google.com/fonts/specimen/Cuprum"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Cuprum-Bold.ttf"
     (share/"fonts").install "Cuprum-BoldItalic.ttf"
