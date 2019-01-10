725c25d52a326d4df6f8f1dc154c062339da3a54
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-trochut.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-trochut.rb
+++ Formula/font-trochut.rb
@@ -2,7 +2,6 @@ class FontTrochut < Formula
   head "https://github.com/google/fonts/trunk/ofl/trochut", using: :svn, revision: "50", trust_cert: true
   desc "Trochut"
   homepage "https://www.google.com/fonts/specimen/Trochut"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Trochut-Bold.ttf"
     (share/"fonts").install "Trochut-Italic.ttf"
