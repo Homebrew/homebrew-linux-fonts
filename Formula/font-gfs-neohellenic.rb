18be2f2b422563f2e7a62067de75d1c39f2ef9aa
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gfs-neohellenic.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-gfs-neohellenic.rb
+++ Formula/font-gfs-neohellenic.rb
@@ -2,7 +2,6 @@ class FontGfsNeohellenic < Formula
   head "https://github.com/google/fonts/trunk/ofl/gfsneohellenic", using: :svn, revision: "50", trust_cert: true
   desc "GFS Neohellenic"
   homepage "https://www.google.com/fonts/specimen/GFS%20Neohellenic"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "GFSNeohellenic.ttf"
     (share/"fonts").install "GFSNeohellenicBold.ttf"
