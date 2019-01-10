cf8baf718c8448dc3ff8b9ed214e2b90758f1a46
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-caudex.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-caudex.rb
+++ Formula/font-caudex.rb
@@ -2,7 +2,6 @@ class FontCaudex < Formula
   head "https://github.com/google/fonts/trunk/ofl/caudex", using: :svn, revision: "50", trust_cert: true
   desc "Caudex"
   homepage "https://www.google.com/fonts/specimen/Caudex"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Caudex-Bold.ttf"
     (share/"fonts").install "Caudex-BoldItalic.ttf"
