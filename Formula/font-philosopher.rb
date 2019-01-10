da52fcd9355d88d518457f5aa0cbff5d446ca30a
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-philosopher.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-philosopher.rb
+++ Formula/font-philosopher.rb
@@ -2,7 +2,6 @@ class FontPhilosopher < Formula
   head "https://github.com/google/fonts/trunk/ofl/philosopher", using: :svn, revision: "50", trust_cert: true
   desc "Philosopher"
   homepage "https://www.google.com/fonts/specimen/Philosopher"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Philosopher-Bold.ttf"
     (share/"fonts").install "Philosopher-BoldItalic.ttf"
