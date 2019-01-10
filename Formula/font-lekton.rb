c0ab37bf44e1210b49861fafe2354374ffee5f38
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lekton.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-lekton.rb
+++ Formula/font-lekton.rb
@@ -2,7 +2,6 @@ class FontLekton < Formula
   head "https://github.com/google/fonts/trunk/ofl/lekton", using: :svn, revision: "47", trust_cert: true
   desc "Lekton"
   homepage "https://www.google.com/fonts/specimen/Lekton"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Lekton-Bold.ttf"
     (share/"fonts").install "Lekton-Italic.ttf"
