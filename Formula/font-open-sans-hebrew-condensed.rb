f71cc77f06a51f1a3cb7c22140214289f0824fdd
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-open-sans-hebrew-condensed.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-open-sans-hebrew-condensed.rb
+++ Formula/font-open-sans-hebrew-condensed.rb
@@ -2,7 +2,6 @@ class FontOpenSansHebrewCondensed < Formula
   head "https://github.com/google/fonts/trunk/apache/opensanshebrewcondensed", using: :svn, revision: "50", trust_cert: true
   desc "Open Sans Hebrew Condensed"
   homepage "https://www.google.com/fonts/earlyaccess"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "OpenSansHebrewCondensed-Bold.ttf"
     (share/"fonts").install "OpenSansHebrewCondensed-BoldItalic.ttf"
