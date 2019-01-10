1255761ed6f661369d5bfe44d2a222f4b51b578a
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-economica.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-economica.rb
+++ Formula/font-economica.rb
@@ -2,7 +2,6 @@ class FontEconomica < Formula
   head "https://github.com/google/fonts/trunk/ofl/economica", using: :svn, revision: "50", trust_cert: true
   desc "Economica"
   homepage "https://www.google.com/fonts/specimen/Economica"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Economica-Bold.ttf"
     (share/"fonts").install "Economica-BoldItalic.ttf"
