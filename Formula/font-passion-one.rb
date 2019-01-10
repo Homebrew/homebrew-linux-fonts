fe3ed4db7a978052042ad343184c993d49bde369
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-passion-one.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-passion-one.rb
+++ Formula/font-passion-one.rb
@@ -2,7 +2,6 @@ class FontPassionOne < Formula
   head "https://github.com/google/fonts/trunk/ofl/passionone", using: :svn, revision: "50", trust_cert: true
   desc "Passion One"
   homepage "https://www.google.com/fonts/specimen/Passion%20One"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "PassionOne-Black.ttf"
     (share/"fonts").install "PassionOne-Bold.ttf"
