94a8714e9d0dbb7979bdf87134785c88d5ea1b8f
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quicksand.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-quicksand.rb
+++ Formula/font-quicksand.rb
@@ -2,7 +2,6 @@ class FontQuicksand < Formula
   head "https://github.com/google/fonts/trunk/ofl/quicksand", using: :svn, revision: "50", trust_cert: true
   desc "Quicksand"
   homepage "https://www.google.com/fonts/specimen/Quicksand"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Quicksand-Bold.ttf"
     (share/"fonts").install "Quicksand-BoldItalic.ttf"
