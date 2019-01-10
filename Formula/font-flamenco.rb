a5460a22b62e8ad7b97616ae48fb1d9f677d09e8
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-flamenco.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-flamenco.rb
+++ Formula/font-flamenco.rb
@@ -2,7 +2,6 @@ class FontFlamenco < Formula
   head "https://github.com/google/fonts/trunk/ofl/flamenco", using: :svn, revision: "50", trust_cert: true
   desc "Flamenco"
   homepage "https://www.google.com/fonts/specimen/Flamenco"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Flamenco-Light.ttf"
     (share/"fonts").install "Flamenco-Regular.ttf"
