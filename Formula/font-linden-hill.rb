df01c783086dd0c9c757551dccad367ef5de6f8e
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-linden-hill.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-linden-hill.rb
+++ Formula/font-linden-hill.rb
@@ -2,7 +2,6 @@ class FontLindenHill < Formula
   head "https://github.com/google/fonts/trunk/ofl/lindenhill", using: :svn, revision: "50", trust_cert: true
   desc "Linden Hill"
   homepage "https://www.google.com/fonts/specimen/Linden%20Hill"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "LindenHill-Italic.ttf"
     (share/"fonts").install "LindenHill-Regular.ttf"
