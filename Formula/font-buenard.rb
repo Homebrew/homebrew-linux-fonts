bfbe7cfc69236eaa04506e93fcb480b52dd69fe4
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-buenard.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-buenard.rb
+++ Formula/font-buenard.rb
@@ -2,7 +2,6 @@ class FontBuenard < Formula
   head "https://github.com/google/fonts/trunk/ofl/buenard", using: :svn, revision: "50", trust_cert: true
   desc "Buenard"
   homepage "https://www.google.com/fonts/specimen/Buenard"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Buenard-Bold.ttf"
     (share/"fonts").install "Buenard-Regular.ttf"
