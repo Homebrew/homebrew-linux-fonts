dd2c3f285a0ebb1ea6f72b959e1bfb8b8668f412
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quantico.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-quantico.rb
+++ Formula/font-quantico.rb
@@ -2,7 +2,6 @@ class FontQuantico < Formula
   head "https://github.com/google/fonts/trunk/ofl/quantico", using: :svn, revision: "50", trust_cert: true
   desc "Quantico"
   homepage "https://www.google.com/fonts/specimen/Quantico"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Quantico-Bold.ttf"
     (share/"fonts").install "Quantico-BoldItalic.ttf"
