53e0085599cf910c673ba91851cec5fe3a84d9ce
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lora.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-lora.rb
+++ Formula/font-lora.rb
@@ -2,7 +2,6 @@ class FontLora < Formula
   head "https://github.com/google/fonts/trunk/ofl/lora", using: :svn, revision: "50", trust_cert: true
   desc "Lora"
   homepage "https://www.google.com/fonts/specimen/Lora"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Lora-Bold.ttf"
     (share/"fonts").install "Lora-BoldItalic.ttf"
