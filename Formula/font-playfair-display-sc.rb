782ad9d9efd173e51d140538645acfe0e954647e
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-playfair-display-sc.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-playfair-display-sc.rb
+++ Formula/font-playfair-display-sc.rb
@@ -2,7 +2,6 @@ class FontPlayfairDisplaySc < Formula
   head "https://github.com/google/fonts/trunk/ofl/playfairdisplaysc", using: :svn, revision: "50", trust_cert: true
   desc "Playfair Display SC"
   homepage "https://www.google.com/fonts/specimen/Playfair%20Display%20SC"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "PlayfairDisplaySC-Black.ttf"
     (share/"fonts").install "PlayfairDisplaySC-BlackItalic.ttf"
