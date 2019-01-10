49ef07eecf40cc5c7d19ce50d538c4f3e93bb7d1
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:43 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-anonymice-powerline.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-anonymice-powerline.rb
+++ Formula/font-anonymice-powerline.rb
@@ -2,7 +2,6 @@ class FontAnonymicePowerline < Formula
   head "https://github.com/powerline/fonts/trunk/AnonymousPro", using: :svn, trust_cert: true
   desc "Anonymice Powerline"
   homepage "https://github.com/powerline/fonts/tree/master/AnonymousPro"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Anonymice Powerline Bold Italic.ttf"
     (share/"fonts").install "Anonymice Powerline Bold.ttf"
