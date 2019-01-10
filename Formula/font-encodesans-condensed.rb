aa64116488c85b4e163583af05a8e126d3f4c895
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-encodesans-condensed.rb from 4b855fd673d71fdc6cb6a25766a751f68319e2ec

--- Formula/font-encodesans-condensed.rb
+++ Formula/font-encodesans-condensed.rb
@@ -2,7 +2,6 @@ class FontEncodesansCondensed < Formula
   head "https://github.com/google/fonts/trunk/ofl/encodesanscondensed", using: :svn, trust_cert: true
   desc "Encode Sans Condensed"
   homepage "https://fonts.google.com/specimen/Encode+Sans+Condensed"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "EncodeSansCondensed-Black.ttf"
     (share/"fonts").install "EncodeSansCondensed-Bold.ttf"
