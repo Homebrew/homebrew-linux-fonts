19713531ccdebe1cecc94983d83844f4d8c6ccf4
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marvel.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-marvel.rb
+++ Formula/font-marvel.rb
@@ -2,7 +2,6 @@ class FontMarvel < Formula
   head "https://github.com/google/fonts/trunk/ofl/marvel", using: :svn, revision: "50", trust_cert: true
   desc "Marvel"
   homepage "https://www.google.com/fonts/specimen/Marvel"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Marvel-Bold.ttf"
     (share/"fonts").install "Marvel-BoldItalic.ttf"
