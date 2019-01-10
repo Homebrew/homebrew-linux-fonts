9a7fe29c06e87797ecc50a145cfc253c3e631c08
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-exo.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-exo.rb
+++ Formula/font-exo.rb
@@ -2,7 +2,6 @@ class FontExo < Formula
   head "https://github.com/google/fonts/trunk/ofl/exo", using: :svn, revision: "50", trust_cert: true
   desc "Exo"
   homepage "https://www.google.com/fonts/specimen/Exo"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Exo-Black.ttf"
     (share/"fonts").install "Exo-BlackItalic.ttf"
