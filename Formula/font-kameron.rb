c0a84705ad44e718714fba4f99c38337b2278a82
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kameron.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-kameron.rb
+++ Formula/font-kameron.rb
@@ -2,7 +2,6 @@ class FontKameron < Formula
   head "https://github.com/google/fonts/trunk/ofl/kameron", using: :svn, revision: "50", trust_cert: true
   desc "Kameron"
   homepage "https://www.google.com/fonts/specimen/Kameron"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Kameron-Bold.ttf"
     (share/"fonts").install "Kameron-Regular.ttf"
