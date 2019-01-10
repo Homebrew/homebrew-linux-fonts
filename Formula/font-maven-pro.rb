ed0d1fdecf7578f407a722273018cd6d513da7d8
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-maven-pro.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-maven-pro.rb
+++ Formula/font-maven-pro.rb
@@ -2,7 +2,6 @@ class FontMavenPro < Formula
   head "https://github.com/google/fonts/trunk/ofl/mavenpro", using: :svn, revision: "50", trust_cert: true
   desc "Maven Pro"
   homepage "https://www.google.com/fonts/specimen/Maven%20Pro"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "MavenPro-Black.ttf"
     (share/"fonts").install "MavenPro-Bold.ttf"
