0d31efe8b9f11a68a5c4a472ea761d05392355fc
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-10 03:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kantumruy.rb from eac605c28c2606d4be97db2b0492ac57cb41616f

--- Formula/font-kantumruy.rb
+++ Formula/font-kantumruy.rb
@@ -2,7 +2,6 @@ class FontKantumruy < Formula
   head "https://github.com/google/fonts/trunk/ofl/kantumruy", using: :svn, revision: "50", trust_cert: true
   desc "Kantumruy"
   homepage "https://www.google.com/fonts/specimen/Kantumruy"
-  depends_on macos: ">= :sierra"
   def install
     (share/"fonts").install "Kantumruy-Bold.ttf"
     (share/"fonts").install "Kantumruy-Light.ttf"
