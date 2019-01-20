53a7fa82205dcfc2138102f142e45f22967559d2
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-20 02:43:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rubik.rb from 81137b934002c44cf153ed763223b130d85e5cc3

--- Formula/font-rubik.rb
+++ Formula/font-rubik.rb
@@ -3,16 +3,16 @@ class FontRubik < Formula
   desc "Rubik"
   homepage "https://www.google.com/fonts/specimen/Rubik"
   def install
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-Black.ttf"
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-BlackItalic.ttf"
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-Bold.ttf"
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-BoldItalic.ttf"
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-Italic.ttf"
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-Light.ttf"
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-LightItalic.ttf"
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-Medium.ttf"
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-MediumItalic.ttf"
-    (share/"fonts").install "rubik-master/fonts/ttf/Rubik-Regular.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Black.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-BlackItalic.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Bold.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-BoldItalic.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Italic.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Light.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-LightItalic.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Medium.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-MediumItalic.ttf"
+    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Regular.ttf"
   end
   test do
   end
