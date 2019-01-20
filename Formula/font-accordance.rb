e2216f6b28f55d3c2dc7bbed7c1edb97dc6fb1a6
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-20 02:43:03 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-accordance.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-accordance.rb
+++ Formula/font-accordance.rb
@@ -3,10 +3,10 @@ class FontAccordance < Formula
   desc "Accordance"
   homepage "https://www.accordancebible.com/Accordance-Font"
   def install
-    (share/"fonts").install "Accordance_Font/Accordance.ttf"
-    (share/"fonts").install "Accordance_Font/AccordanceBold.ttf"
-    (share/"fonts").install "Accordance_Font/AccordanceBoldItalic.ttf"
-    (share/"fonts").install "Accordance_Font/AccordanceItalic.ttf"
+    (share/"fonts").install "../Accordance_Font/Accordance.ttf"
+    (share/"fonts").install "../Accordance_Font/AccordanceBold.ttf"
+    (share/"fonts").install "../Accordance_Font/AccordanceBoldItalic.ttf"
+    (share/"fonts").install "../Accordance_Font/AccordanceItalic.ttf"
   end
   test do
   end
