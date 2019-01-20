c9cb6cefa963ec3133388a070d94a3621346aa0c
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-20 02:43:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-signika.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-signika.rb
+++ Formula/font-signika.rb
@@ -3,14 +3,14 @@ class FontSignika < Formula
   desc "Signika"
   homepage "https://fontfabric.com/signika-font/"
   def install
-    (share/"fonts").install "Signika/Signika-Bold.ttf"
-    (share/"fonts").install "Signika/Signika-Light.ttf"
-    (share/"fonts").install "Signika/Signika-Regular.ttf"
-    (share/"fonts").install "Signika/Signika-Semibold.ttf"
-    (share/"fonts").install "Signika_Negative/SignikaNegative-Bold.ttf"
-    (share/"fonts").install "Signika_Negative/SignikaNegative-Light.ttf"
-    (share/"fonts").install "Signika_Negative/SignikaNegative-Regular.ttf"
-    (share/"fonts").install "Signika_Negative/SignikaNegative-Semibold.ttf"
+    (share/"fonts").install "../Signika/Signika-Bold.ttf"
+    (share/"fonts").install "../Signika/Signika-Light.ttf"
+    (share/"fonts").install "../Signika/Signika-Regular.ttf"
+    (share/"fonts").install "../Signika/Signika-Semibold.ttf"
+    (share/"fonts").install "../Signika_Negative/SignikaNegative-Bold.ttf"
+    (share/"fonts").install "../Signika_Negative/SignikaNegative-Light.ttf"
+    (share/"fonts").install "../Signika_Negative/SignikaNegative-Regular.ttf"
+    (share/"fonts").install "../Signika_Negative/SignikaNegative-Semibold.ttf"
   end
   test do
   end
