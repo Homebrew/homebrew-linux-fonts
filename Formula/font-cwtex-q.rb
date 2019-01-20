430f55d39a05bf64cdb2ea773b7077a2d1173da2
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-20 02:43:04 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cwtex-q.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-cwtex-q.rb
+++ Formula/font-cwtex-q.rb
@@ -5,16 +5,16 @@ class FontCwtexQ < Formula
   desc "cwtex-q-fonts"
   homepage "https://github.com/l10n-tw/cwtex-q-fonts"
   def install
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQFangsong-Medium.ttf"
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQFangsongZH-Medium.ttf"
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQHei-Bold.ttf"
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQHeiZH-Bold.ttf"
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQKai-Medium.ttf"
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQKaiZH-Medium.ttf"
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQMing-Medium.ttf"
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQMingZH-Medium.ttf"
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQYuan-Medium.ttf"
-    (share/"fonts").install "cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQYuanZH-Medium.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQFangsong-Medium.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQFangsongZH-Medium.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQHei-Bold.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQHeiZH-Bold.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQKai-Medium.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQKaiZH-Medium.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQMing-Medium.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQMingZH-Medium.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQYuan-Medium.ttf"
+    (share/"fonts").install "../cwtex-q-fonts-TTFs-#{version}/ttf/cwTeXQYuanZH-Medium.ttf"
   end
   test do
   end
