72628a035e52034199423a861602ff24d7c70561
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-20 02:43:04 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-et-book.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-et-book.rb
+++ Formula/font-et-book.rb
@@ -4,11 +4,11 @@ class FontEtBook < Formula
   desc "Edward Tufte Book"
   homepage "https://edwardtufte.github.io/et-book/"
   def install
-    (share/"fonts").install "et-book-gh-pages/et-book/et-book-bold-line-figures/et-book-bold-line-figures.ttf"
-    (share/"fonts").install "et-book-gh-pages/et-book/et-book-display-italic-old-style-figures/et-book-display-italic-old-style-figures.ttf"
-    (share/"fonts").install "et-book-gh-pages/et-book/et-book-roman-line-figures/et-book-roman-line-figures.ttf"
-    (share/"fonts").install "et-book-gh-pages/et-book/et-book-roman-old-style-figures/et-book-roman-old-style-figures.ttf"
-    (share/"fonts").install "et-book-gh-pages/et-book/et-book-semi-bold-old-style-figures/et-book-semi-bold-old-style-figures.ttf"
+    (share/"fonts").install "../et-book-gh-pages/et-book/et-book-bold-line-figures/et-book-bold-line-figures.ttf"
+    (share/"fonts").install "../et-book-gh-pages/et-book/et-book-display-italic-old-style-figures/et-book-display-italic-old-style-figures.ttf"
+    (share/"fonts").install "../et-book-gh-pages/et-book/et-book-roman-line-figures/et-book-roman-line-figures.ttf"
+    (share/"fonts").install "../et-book-gh-pages/et-book/et-book-roman-old-style-figures/et-book-roman-old-style-figures.ttf"
+    (share/"fonts").install "../et-book-gh-pages/et-book/et-book-semi-bold-old-style-figures/et-book-semi-bold-old-style-figures.ttf"
   end
   test do
   end
