fa7a13612904af375e38db30a2db80cfce1c38b7
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-20 02:43:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-space-mono.rb from cb406b6bd3096ad5c16e4e1c1edcc3badf75a4a5

--- Formula/font-space-mono.rb
+++ Formula/font-space-mono.rb
@@ -3,10 +3,10 @@ class FontSpaceMono < Formula
   desc "Space Mono"
   homepage "https://github.com/googlefonts/spacemono"
   def install
-    (share/"fonts").install "spacemono-master/fonts/SpaceMono-Bold.ttf"
-    (share/"fonts").install "spacemono-master/fonts/SpaceMono-BoldItalic.ttf"
-    (share/"fonts").install "spacemono-master/fonts/SpaceMono-Italic.ttf"
-    (share/"fonts").install "spacemono-master/fonts/SpaceMono-Regular.ttf"
+    (share/"fonts").install "../spacemono-master/fonts/SpaceMono-Bold.ttf"
+    (share/"fonts").install "../spacemono-master/fonts/SpaceMono-BoldItalic.ttf"
+    (share/"fonts").install "../spacemono-master/fonts/SpaceMono-Italic.ttf"
+    (share/"fonts").install "../spacemono-master/fonts/SpaceMono-Regular.ttf"
   end
   test do
   end
