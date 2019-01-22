f539a0cd452b542d47c2868337889a932911576e
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-22 15:00:42 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ia-writer-duo.rb from fec582ef5e5fb57931e44a776a363af1855a5e52

---
+++ Formula/font-ia-writer-duo.rb
@@ -0,0 +1,13 @@
+class FontIaWriterDuo < Formula
+  head "https://codeload.github.com/iaolo/iA-Fonts/zip/master"
+  desc "iA Writer Duospace"
+  homepage "https://github.com/iaolo/iA-Fonts"
+  def install
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Bold.ttf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-BoldItalic.ttf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Italic.ttf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Regular.ttf"
+  end
+  test do
+  end
+end
