611a20db31ee606670e252d3e6f9a1f5911ac644
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-22 15:00:42 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ia-writer-quattro.rb from 34a5645419d089a42e38bd23c808e03056174343

---
+++ Formula/font-ia-writer-quattro.rb
@@ -0,0 +1,13 @@
+class FontIaWriterQuattro < Formula
+  head "https://codeload.github.com/iaolo/iA-Fonts/zip/master"
+  desc "iA Writer Duospace"
+  homepage "https://github.com/iaolo/iA-Fonts"
+  def install
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Quattro/Static/iAWriterQuattroS-Bold.ttf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Quattro/Static/iAWriterQuattroS-BoldItalic.ttf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Quattro/Static/iAWriterQuattroS-Italic.ttf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Quattro/Static/iAWriterQuattroS-Regular.ttf"
+  end
+  test do
+  end
+end
