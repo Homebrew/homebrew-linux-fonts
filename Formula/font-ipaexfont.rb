17e13ec9d9bf05653bc7a2e840449bb280daefad
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-01-29 15:00:32 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ipaexfont.rb from 08e89d6698d24ba1c66091254adaf5aab130f34d

---
+++ Formula/font-ipaexfont.rb
@@ -0,0 +1,13 @@
+class FontIpaexfont < Formula
+  version "003.01"
+  sha256 "c7de095cfded3a549b439b7874cc21b8d73aa16a40d15c31b87bfe0c02f4ae5a"
+  url "https://oscdl.ipa.go.jp/IPAexfont/IPAexfont#{version.to_s.gsub(".", "")}.zip"
+  desc "IPAex Fonts"
+  homepage "https://ipafont.ipa.go.jp/"
+  def install
+    (share/"fonts").install "../IPAexfont#{version.no_dots}/ipaexg.ttf"
+    (share/"fonts").install "../IPAexfont#{version.no_dots}/ipaexm.ttf"
+  end
+  test do
+  end
+end
