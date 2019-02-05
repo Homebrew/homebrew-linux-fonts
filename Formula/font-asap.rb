d7d250365c030ccdd872f899df18ef692b4007e2
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-02-05 15:00:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-asap.rb from 6050d9f9dccdf4a883326a79e6088c513aefefde

--- Formula/font-asap.rb
+++ Formula/font-asap.rb
@@ -1,12 +1,16 @@
 class FontAsap < Formula
-  head "https://github.com/google/fonts/trunk/ofl/asap", using: :svn, revision: "50", trust_cert: true
+  head "https://www.omnibus-type.com/wp-content/uploads/Asap.zip"
   desc "Asap"
-  homepage "https://www.google.com/fonts/specimen/Asap"
+  homepage "https://www.omnibus-type.com/fonts/asap/"
   def install
-    (share/"fonts").install "Asap-Bold.ttf"
-    (share/"fonts").install "Asap-BoldItalic.ttf"
-    (share/"fonts").install "Asap-Italic.ttf"
-    (share/"fonts").install "Asap-Regular.ttf"
+    (share/"fonts").install "../Asap/Asap-Bold.ttf"
+    (share/"fonts").install "../Asap/Asap-BoldItalic.ttf"
+    (share/"fonts").install "../Asap/Asap-Italic.ttf"
+    (share/"fonts").install "../Asap/Asap-Medium.ttf"
+    (share/"fonts").install "../Asap/Asap-MediumItalic.ttf"
+    (share/"fonts").install "../Asap/Asap-Regular.ttf"
+    (share/"fonts").install "../Asap/Asap-SemiBold.ttf"
+    (share/"fonts").install "../Asap/Asap-SemiBoldItalic.ttf"
   end
   test do
   end
