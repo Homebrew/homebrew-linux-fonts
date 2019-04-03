923ff300f13db44fabe249488753a84a222aa39a
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-04-03 15:00:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-academicons.rb from 192ee27870b52857f7c1c62728823917812d9d0a

---
+++ Formula/font-academicons.rb
@@ -0,0 +1,12 @@
+class FontAcademicons < Formula
+  version "1.8.6"
+  sha256 "777e4c42031f0c8ecb0f228285955bd7516b6f1b82497ffa77434e616529b7df"
+  url "https://github.com/jpswalsh/academicons/archive/v#{version}.zip"
+  desc "Academicons"
+  homepage "https://github.com/jpswalsh/academicons"
+  def install
+    (share/"fonts").install "../academicons-#{version}/fonts/academicons.ttf"
+  end
+  test do
+  end
+end
