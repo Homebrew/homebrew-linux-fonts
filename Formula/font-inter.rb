e6fec10df9c1a505c6b431cc868e283ecf7d0269
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-04-12 15:00:24 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inter.rb from 8f47f54042b7f2903661bf666ef6a069484fdb87

--- Formula/font-inter.rb
+++ Formula/font-inter.rb
@@ -1,6 +1,6 @@
 class FontInter < Formula
-  version "3.3"
-  sha256 "b8733799800cedbc5459944ce48fbb8bb0b8fa674a865cd7359bfbaa0038fb0c"
+  version "3.5"
+  sha256 "81f6ac333068803eb1524ea7e0d8b33c9cdad140f97666e63fc7083ecb9e325f"
   url "https://github.com/rsms/inter/releases/download/v#{version}/Inter-#{version}.zip"
   desc "Inter"
   homepage "https://rsms.me/inter/"
@@ -11,12 +11,18 @@ class FontInter < Formula
     (share/"fonts").install "../Inter (OTF)/Inter-BoldItalic.otf"
     (share/"fonts").install "../Inter (OTF)/Inter-ExtraBold.otf"
     (share/"fonts").install "../Inter (OTF)/Inter-ExtraBoldItalic.otf"
+    (share/"fonts").install "../Inter (OTF)/Inter-ExtraLight-BETA.otf"
+    (share/"fonts").install "../Inter (OTF)/Inter-ExtraLightItalic-BETA.otf"
     (share/"fonts").install "../Inter (OTF)/Inter-Italic.otf"
+    (share/"fonts").install "../Inter (OTF)/Inter-Light-BETA.otf"
+    (share/"fonts").install "../Inter (OTF)/Inter-LightItalic-BETA.otf"
     (share/"fonts").install "../Inter (OTF)/Inter-Medium.otf"
     (share/"fonts").install "../Inter (OTF)/Inter-MediumItalic.otf"
     (share/"fonts").install "../Inter (OTF)/Inter-Regular.otf"
     (share/"fonts").install "../Inter (OTF)/Inter-SemiBold.otf"
     (share/"fonts").install "../Inter (OTF)/Inter-SemiBoldItalic.otf"
+    (share/"fonts").install "../Inter (OTF)/Inter-Thin-BETA.otf"
+    (share/"fonts").install "../Inter (OTF)/Inter-ThinItalic-BETA.otf"
   end
   test do
   end
