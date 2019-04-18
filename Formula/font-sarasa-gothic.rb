16ec12f9268aee2cb1351b6d624b84a403e1fc11
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-04-18 15:00:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sarasa-gothic.rb from f4ce87409a499a9eeea1d951e615ffb4e0ba9542

--- Formula/font-sarasa-gothic.rb
+++ Formula/font-sarasa-gothic.rb
@@ -1,13 +1,19 @@
 class FontSarasaGothic < Formula
-  version "0.7.0"
-  sha256 "9cd9fbdc741c99590a8b76feb2da4cda49695f148107df338a0eb57f6edc3dd3"
+  version "0.8.0"
+  sha256 "6a78ce58fc5c572e2f6e17d6af84589b26dc97cccaf253851496c6773ae40f65"
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   desc "Sarasa Gothic"
   homepage "https://github.com/be5invis/Sarasa-Gothic"
   def install
     (share/"fonts").install "sarasa-bold.ttc"
     (share/"fonts").install "sarasa-bolditalic.ttc"
+    (share/"fonts").install "sarasa-extralight.ttc"
+    (share/"fonts").install "sarasa-extralightitalic.ttc"
     (share/"fonts").install "sarasa-italic.ttc"
+    (share/"fonts").install "sarasa-light.ttc"
+    (share/"fonts").install "sarasa-lightitalic.ttc"
+    (share/"fonts").install "sarasa-medium.ttc"
+    (share/"fonts").install "sarasa-mediumitalic.ttc"
     (share/"fonts").install "sarasa-regular.ttc"
   end
   test do
