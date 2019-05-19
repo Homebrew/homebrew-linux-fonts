bdc9cac61e894dc81e0694da9682287c35ac248d
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-05-19 15:00:31 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bukyvede-regular.rb from 561c7cf8e1628ec9da62ccbef72d08500cc780a6

--- Formula/font-bukyvede-regular.rb
+++ Formula/font-bukyvede-regular.rb
@@ -1,7 +1,7 @@
 class FontBukyvedeRegular < Formula
-  head "http://kodeks.uni-bamberg.de/aksl/media/BukyVede-Regular.ttf"
+  head "https://kodeks.uni-bamberg.de/aksl/media/BukyVede-Regular.ttf"
   desc "BukyVede Regular"
-  homepage "http://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm"
+  homepage "https://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm"
   def install
     (share/"fonts").install "BukyVede-Regular.ttf"
   end
