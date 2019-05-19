350e3d7ecd4473989c12361958677398d80e8305
TANIGUCHI Masaya <ta2gch+circleci@users.noreply.github.com>
2019-05-19 15:00:31 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hanamina.rb from 561c7cf8e1628ec9da62ccbef72d08500cc780a6

--- Formula/font-hanamina.rb
+++ Formula/font-hanamina.rb
@@ -3,7 +3,7 @@ class FontHanamina < Formula
   sha256 "571cd4a09ae7da0c642d640fc2442c050aa450ebb0587a95cdd097d41a9c9572"
   url "http://dl.osdn.jp/hanazono-font/#{version.to_s.sub(/.*,/, "")}/hanazono-#{version.to_s.sub(/,.*/, "").gsub(".", "")}.zip"
   desc "HanaMinA"
-  homepage "http://fonts.jp/hanazono/"
+  homepage "https://fonts.jp/hanazono/"
   def install
     (share/"fonts").install "HanaMinA.ttf"
     (share/"fonts").install "HanaMinB.ttf"
