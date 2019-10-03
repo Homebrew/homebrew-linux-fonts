c787bb82589f3099ddc1be413e836fde61f4a23d
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-03 04:06:10 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sudo.rb from 0e4684712a5148a89a35f7cc0476d01ad139b4a6

---
+++ Formula/font-sudo.rb
@@ -0,0 +1,12 @@
+class FontSudo < Formula
+  version "0.40"
+  sha256 "bd85aab2e8e9f0a903bd891fd4294e836cda6180ad926506f4907d1517d714b9"
+  url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip"
+  desc "Sudo"
+  homepage "https://github.com/jenskutilek/sudo-font/"
+  def install
+    (share/"fonts").install "../sudo/SudoVariable.ttf"
+  end
+  test do
+  end
+end
