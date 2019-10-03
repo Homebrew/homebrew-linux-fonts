bb0f19a1c77b6add8ddad1396d95fcb32bf29df7
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-03 04:06:10 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-input.rb from ea68307665385c9986239ad8f8df0bf18219d439

--- Formula/font-input.rb
+++ Formula/font-input.rb
@@ -1,7 +1,7 @@
 class FontInput < Formula
-  head "http://input.fontbureau.com/build/?basic=1&fontSelection=whole&a=0&g=0&i=0&l=0&zero=0&asterisk=0&lineHeight=1.2&accept=I+do"
+  head "https://input.fontbureau.com/build/?basic=1&fontSelection=whole&a=0&g=0&i=0&l=0&zero=0&asterisk=0&lineHeight=1.2&accept=I+do"
   desc "Input"
-  homepage "http://input.fontbureau.com/"
+  homepage "https://input.fontbureau.com/"
   def install
     (share/"fonts").install "../Input_Fonts/InputMono/InputMono/InputMono-Black.ttf"
     (share/"fonts").install "../Input_Fonts/InputMono/InputMono/InputMono-BlackItalic.ttf"
@@ -175,7 +175,7 @@ class FontInput < Formula
   def creavat; <<~EOS
     To use the Input fonts, you must agree to the terms of the license.
 
-      http://input.fontbureau.com/license/
+      https://input.fontbureau.com/license/
   EOS
   end
   test do
