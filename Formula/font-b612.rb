703d52942a7abeec89425bce3433626a4cf937bc
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:21:51 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-b612.rb from 561c7cf8e1628ec9da62ccbef72d08500cc780a6

--- Formula/font-b612.rb
+++ Formula/font-b612.rb
@@ -3,14 +3,14 @@ class FontB612 < Formula
   desc "B612"
   homepage "https://b612-font.com/"
   def install
-    (share/"fonts").install "../b612-master/fonts/ttf/B612-Bold.ttf"
-    (share/"fonts").install "../b612-master/fonts/ttf/B612-BoldItalic.ttf"
-    (share/"fonts").install "../b612-master/fonts/ttf/B612-Italic.ttf"
-    (share/"fonts").install "../b612-master/fonts/ttf/B612-Regular.ttf"
-    (share/"fonts").install "../b612-master/fonts/ttf/B612Mono-Bold.ttf"
-    (share/"fonts").install "../b612-master/fonts/ttf/B612Mono-BoldItalic.ttf"
-    (share/"fonts").install "../b612-master/fonts/ttf/B612Mono-Italic.ttf"
-    (share/"fonts").install "../b612-master/fonts/ttf/B612Mono-Regular.ttf"
+    (share/"fonts").install "../b612-master/fonts/ttfB612-Bold.ttf"
+    (share/"fonts").install "../b612-master/fonts/ttfB612-BoldItalic.ttf"
+    (share/"fonts").install "../b612-master/fonts/ttfB612-Italic.ttf"
+    (share/"fonts").install "../b612-master/fonts/ttfB612-Regular.ttf"
+    (share/"fonts").install "../b612-master/fonts/ttfB612Mono-Bold.ttf"
+    (share/"fonts").install "../b612-master/fonts/ttfB612Mono-BoldItalic.ttf"
+    (share/"fonts").install "../b612-master/fonts/ttfB612Mono-Italic.ttf"
+    (share/"fonts").install "../b612-master/fonts/ttfB612Mono-Regular.ttf"
   end
   test do
   end
