f19abe63f1a8299cd2584ff81ec7d2c9dd06b1a1
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:26:50 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-libre-caslon-text.rb from f33456dd2e07fbab7cfeb9d86e59cc0f0f9a4b9c

--- Formula/font-libre-caslon-text.rb
+++ Formula/font-libre-caslon-text.rb
@@ -3,9 +3,9 @@ class FontLibreCaslonText < Formula
   desc "Libre Caslon Text"
   homepage "http://www.impallari.com/projects/overview/libre-caslon-display-and-text"
   def install
-    (share/"fonts").install "../Libre-Caslon-Text-master/fonts/OTFLibreCaslonText-Bold.otf"
-    (share/"fonts").install "../Libre-Caslon-Text-master/fonts/OTFLibreCaslonText-Italic.otf"
-    (share/"fonts").install "../Libre-Caslon-Text-master/fonts/OTFLibreCaslonText-Regular.otf"
+    (share/"fonts").install "../Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Bold.otf"
+    (share/"fonts").install "../Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Italic.otf"
+    (share/"fonts").install "../Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Regular.otf"
   end
   test do
   end
