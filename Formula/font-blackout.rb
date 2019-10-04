c1a3b5ff643b0ed3bf11ef4bf185089e277e57cf
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:26:46 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-blackout.rb from a10f3ebc91c5bff2c319e9473c8e2a30e565549f

--- Formula/font-blackout.rb
+++ Formula/font-blackout.rb
@@ -3,9 +3,9 @@ class FontBlackout < Formula
   desc "Blackout"
   homepage "https://www.theleagueofmoveabletype.com/blackout"
   def install
-    (share/"fonts").install "../blackout-masterBlackout 2 AM.ttf"
-    (share/"fonts").install "../blackout-masterBlackout Midnight.ttf"
-    (share/"fonts").install "../blackout-masterBlackout Sunrise.ttf"
+    (share/"fonts").install "../blackout-master/Blackout 2 AM.ttf"
+    (share/"fonts").install "../blackout-master/Blackout Midnight.ttf"
+    (share/"fonts").install "../blackout-master/Blackout Sunrise.ttf"
   end
   test do
   end
