5eed9206388447fd5a5b82813dbea48b8ec8bb62
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:26:49 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-junction.rb from 03f29544d6de653b32e5f2752fd0cb6a973ae576

--- Formula/font-junction.rb
+++ Formula/font-junction.rb
@@ -3,9 +3,9 @@ class FontJunction < Formula
   desc "Junction"
   homepage "https://www.theleagueofmoveabletype.com/junction"
   def install
-    (share/"fonts").install "../junction-masterJunction-bold.otf"
-    (share/"fonts").install "../junction-masterJunction-light.otf"
-    (share/"fonts").install "../junction-masterJunction-regular.otf"
+    (share/"fonts").install "../junction-master/Junction-bold.otf"
+    (share/"fonts").install "../junction-master/Junction-light.otf"
+    (share/"fonts").install "../junction-master/Junction-regular.otf"
   end
   test do
   end
