9b2597c3729e07ac7d2c94965cd703cb238ff69f
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:26:46 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-conakry.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-conakry.rb
+++ Formula/font-conakry.rb
@@ -3,7 +3,7 @@ class FontConakry < Formula
   desc "Conakry"
   homepage "https://www.evertype.com/fonts/nko/"
   def install
-    (share/"fonts").install "../ConakryFontConakry.ttf"
+    (share/"fonts").install "../ConakryFont/Conakry.ttf"
   end
   test do
   end
