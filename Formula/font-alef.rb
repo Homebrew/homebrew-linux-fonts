d18ebfca232f1509078f2a08b58a453c81d7651b
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:21:50 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alef.rb from 7a452470f64852577965a7eaa180a82783368f98

--- Formula/font-alef.rb
+++ Formula/font-alef.rb
@@ -3,8 +3,8 @@ class FontAlef < Formula
   desc "Alef"
   homepage "http://alef.hagilda.com/"
   def install
-    (share/"fonts").install "../TTF/Alef-Bold.ttf"
-    (share/"fonts").install "../TTF/Alef-Regular.ttf"
+    (share/"fonts").install "TTF/Alef-Bold.ttf"
+    (share/"fonts").install "TTF/Alef-Regular.ttf"
   end
   test do
   end
