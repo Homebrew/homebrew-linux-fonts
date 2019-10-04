2ad0f165105f1b936b08ec6e5278d77e4eb71d8b
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:26:48 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ia-writer-duospace.rb from da262c5b7b603a3d1bb81095fa04381951605e18

--- Formula/font-ia-writer-duospace.rb
+++ Formula/font-ia-writer-duospace.rb
@@ -3,10 +3,10 @@ class FontIaWriterDuospace < Formula
   desc "iA Writer Duospace"
   homepage "https://github.com/iaolo/iA-Fonts"
   def install
-    (share/"fonts").install "../iA-Fonts-master/iA Writer Duospace/OTF (Mac)iAWriterDuospace-Bold.otf"
-    (share/"fonts").install "../iA-Fonts-master/iA Writer Duospace/OTF (Mac)iAWriterDuospace-BoldItalic.otf"
-    (share/"fonts").install "../iA-Fonts-master/iA Writer Duospace/OTF (Mac)iAWriterDuospace-Regular.otf"
-    (share/"fonts").install "../iA-Fonts-master/iA Writer Duospace/OTF (Mac)iAWriterDuospace-Italic.otf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Bold.otf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-BoldItalic.otf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Regular.otf"
+    (share/"fonts").install "../iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Italic.otf"
   end
   test do
   end
