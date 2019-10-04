430e774391f9718ee82c5278b41804c2a0d40aed
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:26:52 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-redacted.rb from ad5b894f7c4be5d2faaefeaa50632bc2213c1a52

--- Formula/font-redacted.rb
+++ Formula/font-redacted.rb
@@ -3,10 +3,10 @@ class FontRedacted < Formula
   desc "Redacted"
   homepage "https://github.com/christiannaths/Redacted-Font"
   def install
-    (share/"fonts").install "../Redacted-Font-old-sources/fontsredacted-regular.ttf"
-    (share/"fonts").install "../Redacted-Font-old-sources/fontsredacted-script-bold.ttf"
-    (share/"fonts").install "../Redacted-Font-old-sources/fontsredacted-script-light.ttf"
-    (share/"fonts").install "../Redacted-Font-old-sources/fontsredacted-script-regular.ttf"
+    (share/"fonts").install "../Redacted-Font-old-sources/fonts/redacted-regular.ttf"
+    (share/"fonts").install "../Redacted-Font-old-sources/fonts/redacted-script-bold.ttf"
+    (share/"fonts").install "../Redacted-Font-old-sources/fonts/redacted-script-light.ttf"
+    (share/"fonts").install "../Redacted-Font-old-sources/fonts/redacted-script-regular.ttf"
   end
   test do
   end
