fb6d0fe18cad460089c15bf919dc361e27762640
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:26:51 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ostrich-sans.rb from 062e596974855a7d4232a99cded5c827b69af959

--- Formula/font-ostrich-sans.rb
+++ Formula/font-ostrich-sans.rb
@@ -3,15 +3,15 @@ class FontOstrichSans < Formula
   desc "Ostrich Sans"
   homepage "https://www.theleagueofmoveabletype.com/ostrich-sans"
   def install
-    (share/"fonts").install "../ostrich-sans-masterOstrichSans-Black.otf"
-    (share/"fonts").install "../ostrich-sans-masterOstrichSans-Bold.otf"
-    (share/"fonts").install "../ostrich-sans-masterOstrichSans-Heavy.otf"
-    (share/"fonts").install "../ostrich-sans-masterOstrichSans-Light.otf"
-    (share/"fonts").install "../ostrich-sans-masterOstrichSans-Medium.otf"
-    (share/"fonts").install "../ostrich-sans-masterOstrichSansDashed-Medium.otf"
-    (share/"fonts").install "../ostrich-sans-masterOstrichSansInline-Italic.otf"
-    (share/"fonts").install "../ostrich-sans-masterOstrichSansInline-Regular.otf"
-    (share/"fonts").install "../ostrich-sans-masterOstrichSansRounded-Medium.otf"
+    (share/"fonts").install "../ostrich-sans-master/OstrichSans-Black.otf"
+    (share/"fonts").install "../ostrich-sans-master/OstrichSans-Bold.otf"
+    (share/"fonts").install "../ostrich-sans-master/OstrichSans-Heavy.otf"
+    (share/"fonts").install "../ostrich-sans-master/OstrichSans-Light.otf"
+    (share/"fonts").install "../ostrich-sans-master/OstrichSans-Medium.otf"
+    (share/"fonts").install "../ostrich-sans-master/OstrichSansDashed-Medium.otf"
+    (share/"fonts").install "../ostrich-sans-master/OstrichSansInline-Italic.otf"
+    (share/"fonts").install "../ostrich-sans-master/OstrichSansInline-Regular.otf"
+    (share/"fonts").install "../ostrich-sans-master/OstrichSansRounded-Medium.otf"
   end
   test do
   end
