dfa5324bb8e1921eb4b78238dca08b1d162e40d2
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-04 04:26:45 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-awesome-terminal-fonts.rb from cb406b6bd3096ad5c16e4e1c1edcc3badf75a4a5

--- Formula/font-awesome-terminal-fonts.rb
+++ Formula/font-awesome-terminal-fonts.rb
@@ -3,9 +3,9 @@ class FontAwesomeTerminalFonts < Formula
   desc "Awesome Terminal Fonts"
   homepage "https://github.com/gabrielelana/awesome-terminal-fonts"
   def install
-    (share/"fonts").install "../awesome-terminal-fonts-patching-strategy/patchedDroid+Sans+Mono+Awesome.ttf"
-    (share/"fonts").install "../awesome-terminal-fonts-patching-strategy/patchedInconsolata+Awesome.ttf"
-    (share/"fonts").install "../awesome-terminal-fonts-patching-strategy/patchedSourceCodePro+Powerline+Awesome+Regular.ttf"
+    (share/"fonts").install "../awesome-terminal-fonts-patching-strategy/patched/Droid+Sans+Mono+Awesome.ttf"
+    (share/"fonts").install "../awesome-terminal-fonts-patching-strategy/patched/Inconsolata+Awesome.ttf"
+    (share/"fonts").install "../awesome-terminal-fonts-patching-strategy/patched/SourceCodePro+Powerline+Awesome+Regular.ttf"
   end
   test do
   end
