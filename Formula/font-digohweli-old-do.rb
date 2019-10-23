93c390757c2f3290f68dd0d913a65ef97b7b78d3
Fernando Garcia Borges <fgborges@pm.me>
2019-10-23 00:11:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-digohweli-old-do.rb from d9bc266dc76d25a09153073421f1d8e03f258c49

--- Formula/font-digohweli-old-do.rb
+++ Formula/font-digohweli-old-do.rb
@@ -1,7 +1,7 @@
 class FontDigohweliOldDo < Formula
-  head "http://www.languagegeek.com/font/OldDODigohweli.zip"
+  head "https://www.languagegeek.com/font/OldDODigohweli.zip"
   desc "Digohweli Old DO"
-  homepage "http://www.languagegeek.com/font/fontdownload.html"
+  homepage "https://www.languagegeek.com/font/fontdownload.html"
   def install
     (share/"fonts").install "OldDODigohweli.ttf"
   end
