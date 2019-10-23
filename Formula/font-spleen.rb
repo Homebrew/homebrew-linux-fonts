2cfb3d4972e015f7af9f294c755884392a217e48
Fernando Garcia Borges <fgborges@pm.me>
2019-10-23 00:11:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spleen.rb from c35a055bd44521fca97a667f7bd9e006ba77bec3

---
+++ Formula/font-spleen.rb
@@ -0,0 +1,12 @@
+class FontSpleen < Formula
+  version "1.5.0"
+  sha256 "a346844625416ede531bcb720bc786924792a0538fe90c8f1215d619dcd0f6c2"
+  url "https://github.com/fcambus/spleen/releases/download/#{version}/spleen-#{version}.tar.gz"
+  desc "Spleen"
+  homepage "https://github.com/fcambus/spleen"
+  def install
+    (share/"fonts").install "../spleen-#{version}/spleen-32x64.dfont"
+  end
+  test do
+  end
+end
