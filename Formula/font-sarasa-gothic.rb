ea603f4d4afa65975486f520e70cb8350407d999
Fernando Garcia Borges <fgborges@pm.me>
2019-10-23 00:11:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sarasa-gothic.rb from fd5ce117ee7eaf7c70015a0156d2579af6dfd7e6

--- Formula/font-sarasa-gothic.rb
+++ Formula/font-sarasa-gothic.rb
@@ -1,6 +1,6 @@
 class FontSarasaGothic < Formula
-  version "0.8.1"
-  sha256 "31e8c2a4ae26ce20379caf4fded0c74b5ad4125f24381d89ffbbd41e746ba30a"
+  version "0.10.0"
+  sha256 "2a8cad733d706b4f1677f91a1d9e97434c00583b662823692358edc90762eabc"
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   desc "Sarasa Gothic"
   homepage "https://github.com/be5invis/Sarasa-Gothic"
@@ -12,9 +12,9 @@ class FontSarasaGothic < Formula
     (share/"fonts").install "sarasa-italic.ttc"
     (share/"fonts").install "sarasa-light.ttc"
     (share/"fonts").install "sarasa-lightitalic.ttc"
-    (share/"fonts").install "sarasa-medium.ttc"
-    (share/"fonts").install "sarasa-mediumitalic.ttc"
     (share/"fonts").install "sarasa-regular.ttc"
+    (share/"fonts").install "sarasa-semibold.ttc"
+    (share/"fonts").install "sarasa-semibolditalic.ttc"
   end
   test do
   end
