348b9463cd1b68cf64677e8322f590d70539c0d4
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:43 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ia-writer-duo.rb from ca00392c50ef1fe480d3fd43565ee5cf87d70374

--- Formula/font-ia-writer-duo.rb
+++ Formula/font-ia-writer-duo.rb
@@ -3,10 +3,11 @@ class FontIaWriterDuo < Formula
   desc "iA Writer Duo"
   homepage "https://github.com/iaolo/iA-Fonts"
   def install
-    (share/"fonts").install "../iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Bold.ttf"
-    (share/"fonts").install "../iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-BoldItalic.ttf"
-    (share/"fonts").install "../iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Italic.ttf"
-    (share/"fonts").install "../iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Regular.ttf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Bold.ttf"
+    (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-BoldItalic.ttf"
+    (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Italic.ttf"
+    (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Regular.ttf"
   end
   test do
   end
