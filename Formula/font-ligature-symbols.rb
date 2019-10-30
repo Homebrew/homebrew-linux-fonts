c1cf4b348474ce02c0afd1dedc861f04e205f2b5
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:45 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ligature-symbols.rb from ea68307665385c9986239ad8f8df0bf18219d439

--- Formula/font-ligature-symbols.rb
+++ Formula/font-ligature-symbols.rb
@@ -3,7 +3,8 @@ class FontLigatureSymbols < Formula
   desc "Ligature Symbols"
   homepage "https://kudakurage.com/ligature_symbols/"
   def install
-    (share/"fonts").install "../LigatureSymbols/LigatureSymbols-2.11.otf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}LigatureSymbols/LigatureSymbols-2.11.otf"
   end
   test do
   end
