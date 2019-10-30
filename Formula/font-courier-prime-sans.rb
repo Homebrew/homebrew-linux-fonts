3d3989a214bbcf7bec5265ae6077dd5b34b1e460
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:41 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-courier-prime-sans.rb from 17a2ea5b10d25563e261095dce00a48abfcc9493

--- Formula/font-courier-prime-sans.rb
+++ Formula/font-courier-prime-sans.rb
@@ -3,10 +3,11 @@ class FontCourierPrimeSans < Formula
   desc "Courier Prime Sans"
   homepage "https://quoteunquoteapps.com/courierprime/#code-sans"
   def install
-    (share/"fonts").install "../CourierPrimeSans-master/ttf/Courier Prime Sans Bold Italic.ttf"
-    (share/"fonts").install "../CourierPrimeSans-master/ttf/Courier Prime Sans Bold.ttf"
-    (share/"fonts").install "../CourierPrimeSans-master/ttf/Courier Prime Sans Italic.ttf"
-    (share/"fonts").install "../CourierPrimeSans-master/ttf/Courier Prime Sans.ttf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}CourierPrimeSans-master/ttf/Courier Prime Sans Bold Italic.ttf"
+    (share/"fonts").install "#{parent}CourierPrimeSans-master/ttf/Courier Prime Sans Bold.ttf"
+    (share/"fonts").install "#{parent}CourierPrimeSans-master/ttf/Courier Prime Sans Italic.ttf"
+    (share/"fonts").install "#{parent}CourierPrimeSans-master/ttf/Courier Prime Sans.ttf"
   end
   test do
   end
