b95c5e003fe9f20a96c7c58aa4f2fddad157766d
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:40 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chapbook.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-chapbook.rb
+++ Formula/font-chapbook.rb
@@ -3,9 +3,10 @@ class FontChapbook < Formula
   desc "Chapbook"
   homepage "https://www.feorag.com/freestuff/chapbook.html"
   def install
-    (share/"fonts").install "../Chapbook OTF/Chapbook-Dingbats.otf"
-    (share/"fonts").install "../Chapbook OTF/Chapbook-Italic.otf"
-    (share/"fonts").install "../Chapbook OTF/Chapbook-Regular.otf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}Chapbook OTF/Chapbook-Dingbats.otf"
+    (share/"fonts").install "#{parent}Chapbook OTF/Chapbook-Italic.otf"
+    (share/"fonts").install "#{parent}Chapbook OTF/Chapbook-Regular.otf"
   end
   test do
   end
