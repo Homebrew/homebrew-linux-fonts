b487ab378387c01048b22d65f9aa8f98b1dfa1fb
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:45 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-migu-1m.rb from 561c7cf8e1628ec9da62ccbef72d08500cc780a6

--- Formula/font-migu-1m.rb
+++ Formula/font-migu-1m.rb
@@ -5,8 +5,9 @@ class FontMigu1m < Formula
   desc "Migu 1M"
   homepage "https://mix-mplus-ipa.osdn.jp/migu/#migu1m"
   def install
-    (share/"fonts").install "../migu-1m-#{version}/migu-1m-bold.ttf"
-    (share/"fonts").install "../migu-1m-#{version}/migu-1m-regular.ttf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}migu-1m-#{version}/migu-1m-bold.ttf"
+    (share/"fonts").install "#{parent}migu-1m-#{version}/migu-1m-regular.ttf"
   end
   test do
   end
