ee84f6f0aac7d083ddf3a0e0f29354268681dcb6
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:39 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alef.rb from 7a452470f64852577965a7eaa180a82783368f98

--- Formula/font-alef.rb
+++ Formula/font-alef.rb
@@ -3,8 +3,9 @@ class FontAlef < Formula
   desc "Alef"
   homepage "http://alef.hagilda.com/"
   def install
-    (share/"fonts").install "../TTF/Alef-Bold.ttf"
-    (share/"fonts").install "../TTF/Alef-Regular.ttf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}TTF/Alef-Bold.ttf"
+    (share/"fonts").install "#{parent}TTF/Alef-Regular.ttf"
   end
   test do
   end
