bd0b222e0433a271e44478e8ed69d9cf918a4158
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:43 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jsmath-cmbx10.rb from 2a087886606b136dba2e0d6e96ea2b626fcda7ef

--- Formula/font-jsmath-cmbx10.rb
+++ Formula/font-jsmath-cmbx10.rb
@@ -3,12 +3,13 @@ class FontJsmathCmbx10 < Formula
   desc "jsMath-cmbx10"
   homepage "http://www.math.union.edu/~dpvc/jsmath/download/jsmath-fonts.html"
   def install
-    (share/"fonts").install "../TeX-fonts-10/jsMath-cmbx10.ttf"
-    (share/"fonts").install "../TeX-fonts-10/jsMath-cmex10.ttf"
-    (share/"fonts").install "../TeX-fonts-10/jsMath-cmmi10.ttf"
-    (share/"fonts").install "../TeX-fonts-10/jsMath-cmr10.ttf"
-    (share/"fonts").install "../TeX-fonts-10/jsMath-cmsy10.ttf"
-    (share/"fonts").install "../TeX-fonts-10/jsMath-cmti10.ttf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}TeX-fonts-10/jsMath-cmbx10.ttf"
+    (share/"fonts").install "#{parent}TeX-fonts-10/jsMath-cmex10.ttf"
+    (share/"fonts").install "#{parent}TeX-fonts-10/jsMath-cmmi10.ttf"
+    (share/"fonts").install "#{parent}TeX-fonts-10/jsMath-cmr10.ttf"
+    (share/"fonts").install "#{parent}TeX-fonts-10/jsMath-cmsy10.ttf"
+    (share/"fonts").install "#{parent}TeX-fonts-10/jsMath-cmti10.ttf"
   end
   test do
   end
