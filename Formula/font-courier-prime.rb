621deb73d2463a0022ce527c9412928471e620b1
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:41 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-courier-prime.rb from 17a2ea5b10d25563e261095dce00a48abfcc9493

--- Formula/font-courier-prime.rb
+++ Formula/font-courier-prime.rb
@@ -3,10 +3,11 @@ class FontCourierPrime < Formula
   desc "Courier Prime"
   homepage "https://quoteunquoteapps.com/courierprime/"
   def install
-    (share/"fonts").install "../Courier Prime/Courier Prime.ttf"
-    (share/"fonts").install "../Courier Prime/Courier Prime Bold.ttf"
-    (share/"fonts").install "../Courier Prime/Courier Prime Bold Italic.ttf"
-    (share/"fonts").install "../Courier Prime/Courier Prime Italic.ttf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}Courier Prime/Courier Prime.ttf"
+    (share/"fonts").install "#{parent}Courier Prime/Courier Prime Bold.ttf"
+    (share/"fonts").install "#{parent}Courier Prime/Courier Prime Bold Italic.ttf"
+    (share/"fonts").install "#{parent}Courier Prime/Courier Prime Italic.ttf"
   end
   test do
   end
