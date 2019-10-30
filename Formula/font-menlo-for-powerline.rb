5161e62531756c3b2b2b92f19b56b11be97c18e7
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:45 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-menlo-for-powerline.rb from 5bd556433b8baadf51b3196e8f71433e728f89d4

--- Formula/font-menlo-for-powerline.rb
+++ Formula/font-menlo-for-powerline.rb
@@ -3,10 +3,11 @@ class FontMenloForPowerline < Formula
   desc "Menlo for Powerline"
   homepage "https://github.com/abertsch/Menlo-for-Powerline"
   def install
-    (share/"fonts").install "../Menlo-for-Powerline-master/Menlo for Powerline.ttf"
-    (share/"fonts").install "../Menlo-for-Powerline-master/Menlo Bold for Powerline.ttf"
-    (share/"fonts").install "../Menlo-for-Powerline-master/Menlo Italic for Powerline.ttf"
-    (share/"fonts").install "../Menlo-for-Powerline-master/Menlo Bold Italic for Powerline.ttf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}Menlo-for-Powerline-master/Menlo for Powerline.ttf"
+    (share/"fonts").install "#{parent}Menlo-for-Powerline-master/Menlo Bold for Powerline.ttf"
+    (share/"fonts").install "#{parent}Menlo-for-Powerline-master/Menlo Italic for Powerline.ttf"
+    (share/"fonts").install "#{parent}Menlo-for-Powerline-master/Menlo Bold Italic for Powerline.ttf"
   end
   test do
   end
