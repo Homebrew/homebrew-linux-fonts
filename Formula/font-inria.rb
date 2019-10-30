09fdc19eb1214a7d60cf71cefa6ce48768748590
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:43 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inria.rb from c031f48281afc554d457d54f3c5867ede1f2a855

--- Formula/font-inria.rb
+++ Formula/font-inria.rb
@@ -3,18 +3,19 @@ class FontInria < Formula
   desc "Inria"
   homepage "https://black-foundry.com/blog/inria-serif-and-inria/"
   def install
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Bold.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSans/OTF/InriaSans-BoldItalic.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Italic.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Light.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSans/OTF/InriaSans-LightItalic.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Regular.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Bold.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-BoldItalic.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Italic.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Light.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-LightItalic.otf"
-    (share/"fonts").install "../InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Regular.otf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Bold.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSans/OTF/InriaSans-BoldItalic.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Italic.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Light.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSans/OTF/InriaSans-LightItalic.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Regular.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Bold.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-BoldItalic.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Italic.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Light.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-LightItalic.otf"
+    (share/"fonts").install "#{parent}InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Regular.otf"
   end
   test do
   end
