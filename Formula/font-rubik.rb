0a04c9eb180ca0f0a9d6e53941eee607d3b2a433
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:47 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rubik.rb from 81137b934002c44cf153ed763223b130d85e5cc3

--- Formula/font-rubik.rb
+++ Formula/font-rubik.rb
@@ -3,16 +3,17 @@ class FontRubik < Formula
   desc "Rubik"
   homepage "https://www.google.com/fonts/specimen/Rubik"
   def install
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Black.ttf"
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-BlackItalic.ttf"
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Bold.ttf"
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-BoldItalic.ttf"
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Italic.ttf"
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Light.ttf"
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-LightItalic.ttf"
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Medium.ttf"
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-MediumItalic.ttf"
-    (share/"fonts").install "../rubik-master/fonts/ttf/Rubik-Regular.ttf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-Black.ttf"
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-BlackItalic.ttf"
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-Bold.ttf"
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-BoldItalic.ttf"
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-Italic.ttf"
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-Light.ttf"
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-LightItalic.ttf"
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-Medium.ttf"
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-MediumItalic.ttf"
+    (share/"fonts").install "#{parent}rubik-master/fonts/ttf/Rubik-Regular.ttf"
   end
   test do
   end
