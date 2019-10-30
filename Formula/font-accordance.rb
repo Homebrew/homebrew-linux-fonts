aff0360d395fa907d6a698e2715bb4e44742b3a5
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-30 22:50:39 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-accordance.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-accordance.rb
+++ Formula/font-accordance.rb
@@ -3,10 +3,11 @@ class FontAccordance < Formula
   desc "Accordance"
   homepage "https://www.accordancebible.com/Accordance-Font"
   def install
-    (share/"fonts").install "../Accordance_Font/Accordance.ttf"
-    (share/"fonts").install "../Accordance_Font/AccordanceBold.ttf"
-    (share/"fonts").install "../Accordance_Font/AccordanceBoldItalic.ttf"
-    (share/"fonts").install "../Accordance_Font/AccordanceItalic.ttf"
+    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    (share/"fonts").install "#{parent}Accordance_Font/Accordance.ttf"
+    (share/"fonts").install "#{parent}Accordance_Font/AccordanceBold.ttf"
+    (share/"fonts").install "#{parent}Accordance_Font/AccordanceBoldItalic.ttf"
+    (share/"fonts").install "#{parent}Accordance_Font/AccordanceItalic.ttf"
   end
   test do
   end
