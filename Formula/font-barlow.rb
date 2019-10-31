9fb4526abaadae1f4fad91a1f1faced672399301
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:00 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-barlow.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-barlow.rb
+++ Formula/font-barlow.rb
@@ -5,7 +5,7 @@ class FontBarlow < Formula
   desc "Barlow"
   homepage "https://tribby.com/fonts/barlow"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}barlow-#{version}/fonts/otf/Barlow-Black.otf"
     (share/"fonts").install "#{parent}barlow-#{version}/fonts/otf/Barlow-BlackItalic.otf"
     (share/"fonts").install "#{parent}barlow-#{version}/fonts/otf/Barlow-Bold.otf"
