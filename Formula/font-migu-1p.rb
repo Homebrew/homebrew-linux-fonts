b6f25130be54101741ae139b30fa874b9019a58d
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:05 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-migu-1p.rb from 561c7cf8e1628ec9da62ccbef72d08500cc780a6

--- Formula/font-migu-1p.rb
+++ Formula/font-migu-1p.rb
@@ -5,7 +5,7 @@ class FontMigu1p < Formula
   desc "Migu 1P"
   homepage "https://mix-mplus-ipa.osdn.jp/migu/#migu1p"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}migu-1p-#{version}/migu-1p-bold.ttf"
     (share/"fonts").install "#{parent}migu-1p-#{version}/migu-1p-regular.ttf"
   end
