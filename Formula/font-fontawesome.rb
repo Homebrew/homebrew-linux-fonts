21476e99e4fbad220dfb5c0754fb5be15de3cecf
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:02 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fontawesome.rb from 8774cbcbe400ebdecc09cedd93bcc9f263e8f5d4

--- Formula/font-fontawesome.rb
+++ Formula/font-fontawesome.rb
@@ -5,7 +5,7 @@ class FontFontawesome < Formula
   desc "Font Awesome"
   homepage "https://fontawesome.com/"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}fontawesome-free-#{version}-desktop/otfs/Font Awesome #{version.to_s.sub(/\..*/, "")} Brands-Regular-400.otf"
     (share/"fonts").install "#{parent}fontawesome-free-#{version}-desktop/otfs/Font Awesome #{version.to_s.sub(/\..*/, "")} Free-Regular-400.otf"
     (share/"fonts").install "#{parent}fontawesome-free-#{version}-desktop/otfs/Font Awesome #{version.to_s.sub(/\..*/, "")} Free-Solid-900.otf"
