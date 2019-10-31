72448e8ac1176ff18264b91aa393120001b00b93
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:07 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-scheherazade.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-scheherazade.rb
+++ Formula/font-scheherazade.rb
@@ -5,7 +5,7 @@ class FontScheherazade < Formula
   desc "Scheherazade "
   homepage "https://scripts.sil.org/Scheherazade"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}Scheherazade-#{version}/Scheherazade-R.ttf"
   end
   test do
