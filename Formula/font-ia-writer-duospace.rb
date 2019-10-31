1287160d6d8920b183d8f45aa3462098087fea29
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:02 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ia-writer-duospace.rb from da262c5b7b603a3d1bb81095fa04381951605e18

--- Formula/font-ia-writer-duospace.rb
+++ Formula/font-ia-writer-duospace.rb
@@ -3,7 +3,7 @@ class FontIaWriterDuospace < Formula
   desc "iA Writer Duospace"
   homepage "https://github.com/iaolo/iA-Fonts"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Bold.otf"
     (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-BoldItalic.otf"
     (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Regular.otf"
