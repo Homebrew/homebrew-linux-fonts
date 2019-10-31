f8fadc1f6534d0bb949ef33cb8d737d893b4b4bc
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:06 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-profontx.rb from e9524946e2e56c46bc191bf1a8f445489222ecaf

--- Formula/font-profontx.rb
+++ Formula/font-profontx.rb
@@ -3,7 +3,7 @@ class FontProfontx < Formula
   desc "ProFontX"
   homepage "http://faisal.com/software/profontx/"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}ProFontX/ProFontX"
   end
   test do
