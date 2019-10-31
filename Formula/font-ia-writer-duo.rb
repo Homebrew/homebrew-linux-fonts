33b4bdd21be6218226d06464f365cdbf29a908ed
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:02 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ia-writer-duo.rb from ca00392c50ef1fe480d3fd43565ee5cf87d70374

--- Formula/font-ia-writer-duo.rb
+++ Formula/font-ia-writer-duo.rb
@@ -3,7 +3,7 @@ class FontIaWriterDuo < Formula
   desc "iA Writer Duo"
   homepage "https://github.com/iaolo/iA-Fonts"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Bold.ttf"
     (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-BoldItalic.ttf"
     (share/"fonts").install "#{parent}iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Italic.ttf"
