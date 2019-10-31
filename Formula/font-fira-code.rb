7d135e9e74f413bed9d878ac3e6059edb7d8be82
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:02 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-code.rb from 3ce1905f2fe93aa5d6b071e4edc34e2fb0fdb7cb

--- Formula/font-fira-code.rb
+++ Formula/font-fira-code.rb
@@ -5,7 +5,7 @@ class FontFiraCode < Formula
   desc "Fira Code"
   homepage "https://github.com/tonsky/FiraCode"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}otf/FiraCode-Bold.otf"
     (share/"fonts").install "#{parent}otf/FiraCode-Light.otf"
     (share/"fonts").install "#{parent}otf/FiraCode-Medium.otf"
