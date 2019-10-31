352b8715ec621f738b593f1145c9a134f771a1a9
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:05 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-luculent.rb from 45816c0b352beecb6270abd109a863436931bd12

--- Formula/font-luculent.rb
+++ Formula/font-luculent.rb
@@ -3,7 +3,7 @@ class FontLuculent < Formula
   desc "Luculent"
   homepage "http://eastfarthing.com/luculent/"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}luculent/luculent.ttf"
     (share/"fonts").install "#{parent}luculent/luculentb.ttf"
     (share/"fonts").install "#{parent}luculent/luculentbi.ttf"
