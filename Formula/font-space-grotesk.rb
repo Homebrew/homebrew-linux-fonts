e570fc52fa2f38d7e7f76f6dc2fb108dfd15933f
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:07 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-space-grotesk.rb from 40ef53ef1203c224a26ad1ed0a347d00ebadfb9f

--- Formula/font-space-grotesk.rb
+++ Formula/font-space-grotesk.rb
@@ -5,7 +5,7 @@ class FontSpaceGrotesk < Formula
   desc "Space Grotesk"
   homepage "https://github.com/floriankarsten/space-grotesk"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}SpaceGrotesk-v#{version}/otf/SpaceGrotesk-Bold.otf"
     (share/"fonts").install "#{parent}SpaceGrotesk-v#{version}/otf/SpaceGrotesk-Light.otf"
     (share/"fonts").install "#{parent}SpaceGrotesk-v#{version}/otf/SpaceGrotesk-Medium.otf"
