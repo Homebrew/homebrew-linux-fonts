d1eb7a193cf0ec30501de2dc772153ca8b722e8d
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:03 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kawkab-mono.rb from 847e3d9266879c8cf441621fd7536a4c4b5e5bd3

--- Formula/font-kawkab-mono.rb
+++ Formula/font-kawkab-mono.rb
@@ -5,7 +5,7 @@ class FontKawkabMono < Formula
   desc "Kawkab Mono"
   homepage "https://makkuk.com/kawkab-mono"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}kawkab-mono-#{version}/KawkabMono-Bold.otf"
     (share/"fonts").install "#{parent}kawkab-mono-#{version}/KawkabMono-Light.otf"
     (share/"fonts").install "#{parent}kawkab-mono-#{version}/KawkabMono-Regular.otf"
