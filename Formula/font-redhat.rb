d0d44958ddb2dfede3ec02f5d1d926802ed5dfad
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:07 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-redhat.rb from 81375aa9af87fbce7e977fa12b5e4d9ffd779621

--- Formula/font-redhat.rb
+++ Formula/font-redhat.rb
@@ -5,7 +5,7 @@ class FontRedhat < Formula
   desc "Red Hat"
   homepage "https://github.com/RedHatOfficial/RedHatFont/"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}RedHatFont-#{version}/OTF/RedHatDisplay-Black.otf"
     (share/"fonts").install "#{parent}RedHatFont-#{version}/OTF/RedHatDisplay-BlackItalic.otf"
     (share/"fonts").install "#{parent}RedHatFont-#{version}/OTF/RedHatDisplay-Bold.otf"
