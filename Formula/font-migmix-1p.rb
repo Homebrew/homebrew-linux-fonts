30fa13325c08326ae5a7b1edde0039ce656ab476
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:05 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-migmix-1p.rb from 561c7cf8e1628ec9da62ccbef72d08500cc780a6

--- Formula/font-migmix-1p.rb
+++ Formula/font-migmix-1p.rb
@@ -5,7 +5,7 @@ class FontMigmix1p < Formula
   desc "MigMix 1P"
   homepage "https://mix-mplus-ipa.osdn.jp/migmix/#migmix1p"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}migmix-1p-#{version}/migmix-1p-bold.ttf"
     (share/"fonts").install "#{parent}migmix-1p-#{version}/migmix-1p-regular.ttf"
   end
