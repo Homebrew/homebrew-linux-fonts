ab563f57ff81aadd5c4d3837fc7478ae0949355e
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:06 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-office-code-pro.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-office-code-pro.rb
+++ Formula/font-office-code-pro.rb
@@ -5,7 +5,7 @@ class FontOfficeCodePro < Formula
   desc "Office Code Pro"
   homepage "https://github.com/nathco/Office-Code-Pro"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Bold.otf"
     (share/"fonts").install "#{parent}Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-BoldItalic.otf"
     (share/"fonts").install "#{parent}Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Light.otf"
