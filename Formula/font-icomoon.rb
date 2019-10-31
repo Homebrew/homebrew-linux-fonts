9ed12d73107c5dacbb89227b096b77e62e9d89bf
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:03 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-icomoon.rb from 52d0325913fb31930bfe0a6ed6ba10b2640eed9a

--- Formula/font-icomoon.rb
+++ Formula/font-icomoon.rb
@@ -3,7 +3,7 @@ class FontIcomoon < Formula
   desc "IcoMoon"
   homepage "https://icomoon.io/"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}IcoMoon-Free-master/Font/IcoMoon-Free.ttf"
   end
   test do
