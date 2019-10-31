952d771932f5497f7c0d60b82858dce394a82b1f
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2019-10-31 15:43:05 +0300

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-montserrat.rb from 69560483c2ff7db7741094527558445da3049a52

--- Formula/font-montserrat.rb
+++ Formula/font-montserrat.rb
@@ -5,7 +5,7 @@ class FontMontserrat < Formula
   desc "Montserrat"
   homepage "https://github.com/JulietaUla/Montserrat"
   def install
-    parent = %x( [ ${PWD%/*} != ${HOMEBREW_TEMP=-/tmp} ] && echo -n '../' )
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}Montserrat-#{version}/fonts/otf/Montserrat-Black.otf"
     (share/"fonts").install "#{parent}Montserrat-#{version}/fonts/otf/Montserrat-BlackItalic.otf"
     (share/"fonts").install "#{parent}Montserrat-#{version}/fonts/otf/Montserrat-Bold.otf"
