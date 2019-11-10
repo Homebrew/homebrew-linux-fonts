f65d23c638027a9126c6325e158a45ee9363d8a3
Fernando Garcia Borges <fgborges@pm.me>
2019-11-10 00:16:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ipaexfont.rb from 069b06c53f33c0c06f55c3923b3217f3a9f4fd61

--- Formula/font-ipaexfont.rb
+++ Formula/font-ipaexfont.rb
@@ -1,7 +1,7 @@
 class FontIpaexfont < Formula
   version "004.01"
   sha256 "bcf8374ab3f9672c421120430dd19a51c99f5265cf06fc340d9a661ddfd7974b"
-  url "https://oscdl.ipa.go.jp/IPAexfont/IPAexfont#{version.to_s.gsub(".", "")}.zip"
+  url "https://ipafont.ipa.go.jp/IPAexfont/IPAexfont#{version.to_s.gsub(".", "")}.zip"
   desc "IPAex Fonts"
   homepage "https://ipafont.ipa.go.jp/"
   def install
