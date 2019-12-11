eadf209b46f215833ef9e1bab1ee8dec9a0fc7a8
Fernando Garcia Borges <fgborges@pm.me>
2019-12-11 00:07:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-libre-caslon-text.rb from 31f5c5fe1deb7b96db124c8e98c43deeff3b9311

--- Formula/font-libre-caslon-text.rb
+++ Formula/font-libre-caslon-text.rb
@@ -1,7 +1,7 @@
 class FontLibreCaslonText < Formula
   head "https://github.com/impallari/Libre-Caslon-Text/archive/master.zip"
   desc "Libre Caslon Text"
-  homepage "http://www.impallari.com/projects/overview/libre-caslon-display-and-text"
+  homepage "https://github.com/impallari/Libre-Caslon-Text/"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Bold.otf"
