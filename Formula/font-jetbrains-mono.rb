f9607d81adbca496a15d844871ec8d94c0b29d71
Fernando Garcia Borges <fgborges@pm.me>
2020-01-22 00:08:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jetbrains-mono.rb from dffcec566e99539f44a0eef812fee18b38c5fb0d

--- Formula/font-jetbrains-mono.rb
+++ Formula/font-jetbrains-mono.rb
@@ -1,18 +1,19 @@
 class FontJetbrainsMono < Formula
-  version "1.0.0"
-  sha256 "82bf0dec956e4ca9afa4da4978fa5de80a75a576b3353d570ccbaabe4e858389"
-  url "https://download.jetbrains.com/fonts/JetBrainsMono-#{version}.zip"
+  version "1.0.1"
+  sha256 "5c280da4dd1d8adcaddf14cd0d3a60b461d65cface30a0a0d7cb72a7ea1c0cd9"
+  url "https://github.com/JetBrains/JetBrainsMono/releases/download/#{version}/JetBrainsMono-#{version}.zip"
   desc "JetBrains Mono"
   homepage "https://www.jetbrains.com/lp/mono"
   def install
-    (share/"fonts").install "JetBrainsMono-Bold-Italic.ttf"
-    (share/"fonts").install "JetBrainsMono-Bold.ttf"
-    (share/"fonts").install "JetBrainsMono-ExtraBold-Italic.ttf"
-    (share/"fonts").install "JetBrainsMono-ExtraBold.ttf"
-    (share/"fonts").install "JetBrainsMono-Italic.ttf"
-    (share/"fonts").install "JetBrainsMono-Medium-Italic.ttf"
-    (share/"fonts").install "JetBrainsMono-Medium.ttf"
-    (share/"fonts").install "JetBrainsMono-Regular.ttf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}ttf/JetBrainsMono-Bold-Italic.ttf"
+    (share/"fonts").install "#{parent}ttf/JetBrainsMono-Bold.ttf"
+    (share/"fonts").install "#{parent}ttf/JetBrainsMono-ExtraBold-Italic.ttf"
+    (share/"fonts").install "#{parent}ttf/JetBrainsMono-ExtraBold.ttf"
+    (share/"fonts").install "#{parent}ttf/JetBrainsMono-Italic.ttf"
+    (share/"fonts").install "#{parent}ttf/JetBrainsMono-Medium-Italic.ttf"
+    (share/"fonts").install "#{parent}ttf/JetBrainsMono-Medium.ttf"
+    (share/"fonts").install "#{parent}ttf/JetBrainsMono-Regular.ttf"
   end
   test do
   end
