c9483ada6e139657741c7262dbb7f00e5207d066
Fernando Garcia Borges <fgborges@pm.me>
2020-02-04 00:07:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jetbrainsmono-nerd-font.rb from 0750b0d95787f4af5987fbce807b420e774667d7

---
+++ Formula/font-jetbrainsmono-nerd-font.rb
@@ -0,0 +1,19 @@
+class FontJetbrainsmonoNerdFont < Formula
+  version "2.1.0"
+  sha256 "842013fa44b6896d4eb91635a81ef75244d78d7f61ff866c9dfd3315a67788cd"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/JetBrainsMono.zip"
+  desc "JetBrains Mono Nerd Font (JetBrains Mono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/JetBrainsMono"
+  def install
+    (share/"fonts").install "JetBrains Mono Bold Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "JetBrains Mono Bold Nerd Font Complete.ttf"
+    (share/"fonts").install "JetBrains Mono ExtraBold ExBd I Nerd Font Complete.ttf"
+    (share/"fonts").install "JetBrains Mono ExtraBold ExtBd Nerd Font Complete.ttf"
+    (share/"fonts").install "JetBrains Mono Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "JetBrains Mono Medium Med Ita Nerd Font Complete.ttf"
+    (share/"fonts").install "JetBrains Mono Medium Medium Nerd Font Complete.ttf"
+    (share/"fonts").install "JetBrains Mono Regular Nerd Font Complete.ttf"
+  end
+  test do
+  end
+end
