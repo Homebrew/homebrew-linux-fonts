8cefe83d95f31b1b3ca81442822d829993621e7c
Fernando Garcia Borges <fgborges@pm.me>
2020-02-10 00:13:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jetbrains-mono.rb from d4616fd8d264803ad14fed2c7aa3809f1d81cdd2

--- Formula/font-jetbrains-mono.rb
+++ Formula/font-jetbrains-mono.rb
@@ -1,19 +1,19 @@
 class FontJetbrainsMono < Formula
-  version "1.0.2"
-  sha256 "59f9b9762d5625eb438eedf034dbbcdcf09ed18ded994540b466872840229762"
+  version "1.0.3"
+  sha256 "e357094992c4aff1e933354db198c6ffe2ee741bc4f3d9c071956c9e05b871ed"
   url "https://github.com/JetBrains/JetBrainsMono/releases/download/v#{version}/JetBrainsMono-#{version}.zip"
   desc "JetBrains Mono"
   homepage "https://www.jetbrains.com/lp/mono"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}JetBrainsMono-#{version}/ttf/JetBrainsMono-Bold-Italic.ttf"
-    (share/"fonts").install "#{parent}JetBrainsMono-#{version}/ttf/JetBrainsMono-Bold.ttf"
-    (share/"fonts").install "#{parent}JetBrainsMono-#{version}/ttf/JetBrainsMono-ExtraBold-Italic.ttf"
-    (share/"fonts").install "#{parent}JetBrainsMono-#{version}/ttf/JetBrainsMono-ExtraBold.ttf"
-    (share/"fonts").install "#{parent}JetBrainsMono-#{version}/ttf/JetBrainsMono-Italic.ttf"
-    (share/"fonts").install "#{parent}JetBrainsMono-#{version}/ttf/JetBrainsMono-Medium-Italic.ttf"
-    (share/"fonts").install "#{parent}JetBrainsMono-#{version}/ttf/JetBrainsMono-Medium.ttf"
-    (share/"fonts").install "#{parent}JetBrainsMono-#{version}/ttf/JetBrainsMono-Regular.ttf"
+    (share/"fonts").install "#{parent}JetBrainsMono-#{version}-Source/ttf/JetBrainsMono-Bold-Italic.ttf"
+    (share/"fonts").install "#{parent}JetBrainsMono-#{version}-Source/ttf/JetBrainsMono-Bold.ttf"
+    (share/"fonts").install "#{parent}JetBrainsMono-#{version}-Source/ttf/JetBrainsMono-ExtraBold-Italic.ttf"
+    (share/"fonts").install "#{parent}JetBrainsMono-#{version}-Source/ttf/JetBrainsMono-ExtraBold.ttf"
+    (share/"fonts").install "#{parent}JetBrainsMono-#{version}-Source/ttf/JetBrainsMono-Italic.ttf"
+    (share/"fonts").install "#{parent}JetBrainsMono-#{version}-Source/ttf/JetBrainsMono-Medium-Italic.ttf"
+    (share/"fonts").install "#{parent}JetBrainsMono-#{version}-Source/ttf/JetBrainsMono-Medium.ttf"
+    (share/"fonts").install "#{parent}JetBrainsMono-#{version}-Source/ttf/JetBrainsMono-Regular.ttf"
   end
   test do
   end
