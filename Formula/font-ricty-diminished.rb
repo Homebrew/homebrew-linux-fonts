0dee1abfb4517cdbe6cdc6e2c18af2b97d5c3e4e
Fernando Garcia Borges <fgborges@pm.me>
2020-03-11 00:15:31 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ricty-diminished.rb from f0776bb08ddae1720861e2586aeaf7cbee1bbf16

--- Formula/font-ricty-diminished.rb
+++ Formula/font-ricty-diminished.rb
@@ -1,9 +1,9 @@
 class FontRictyDiminished < Formula
   version "4.1.1"
   sha256 "09d6cf8eaf3402205dd8a0fa35469d41833b4e85bedddfac235255d78747766b"
-  url "http://www.yusa.lab.uec.ac.jp/~yusa/ricty_diminished/ricty_diminished-#{version}.tar.gz"
+  url "https://rictyfonts.github.io/files/ricty_diminished-#{version}.tar.gz"
   desc "Ricty Diminished"
-  homepage "http://www.yusa.lab.uec.ac.jp/~yusa/ricty_diminished.html"
+  homepage "https://rictyfonts.github.io/diminished"
   def install
     (share/"fonts").install "RictyDiminished-Bold.ttf"
     (share/"fonts").install "RictyDiminished-BoldOblique.ttf"
