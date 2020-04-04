9050d888e3019d30cab09c9e18108f9806a5b543
Fernando Garcia Borges <fgborges@pm.me>
2020-04-04 00:12:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gnu-unifont.rb from d67992875ef158ba31c59dc9a89aaf1e4d348fdb

--- Formula/font-gnu-unifont.rb
+++ Formula/font-gnu-unifont.rb
@@ -1,11 +1,16 @@
 class FontGnuUnifont < Formula
-  version "10.0.06"
-  sha256 "669a9764fb640c832a8ebfbd2c351be744b55c6f65b40f1fb203144309659ac0"
-  url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.ttf"
+  version "13.0.01"
+  sha256 "8b486450b2bd1dc9357ceca096faf493d13f7e582f29984e46552ca8faa87feb"
+  url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.tar.gz"
   desc "GNU Unifont"
   homepage "http://unifoundry.com/unifont.html"
   def install
-    (share/"fonts").install "unifont-#{version}.ttf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont-#{version}.ttf"
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_upper-#{version}.ttf"
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_sample-#{version}.ttf"
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_jp-#{version}.ttf"
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_csur-#{version}.ttf"
   end
   test do
   end
