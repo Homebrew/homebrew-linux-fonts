8d6113657d865dfdcc3529b5d6a8dade3013cf1b
Fernando Garcia Borges <fgborges@pm.me>
2020-04-04 00:12:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-go.rb from 5f7a4918aaf62aacc4e912c35803d49315fd1e1d

--- Formula/font-go.rb
+++ Formula/font-go.rb
@@ -1,12 +1,20 @@
 class FontGo < Formula
   head "https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz"
-  desc "Go Mono"
+  desc "Go"
   homepage "https://go.googlesource.com/image/+/master/font/gofont/ttfs/README"
   def install
     (share/"fonts").install "Go-Bold-Italic.ttf"
     (share/"fonts").install "Go-Bold.ttf"
     (share/"fonts").install "Go-Italic.ttf"
+    (share/"fonts").install "Go-Medium-Italic.ttf"
+    (share/"fonts").install "Go-Medium.ttf"
+    (share/"fonts").install "Go-Mono-Bold-Italic.ttf"
+    (share/"fonts").install "Go-Mono-Bold.ttf"
+    (share/"fonts").install "Go-Mono-Italic.ttf"
+    (share/"fonts").install "Go-Mono.ttf"
     (share/"fonts").install "Go-Regular.ttf"
+    (share/"fonts").install "Go-Smallcaps-Italic.ttf"
+    (share/"fonts").install "Go-Smallcaps.ttf"
   end
   test do
   end
