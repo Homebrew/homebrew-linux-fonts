12d569ecb0629e7da699399e2993cbb29cebd2cb
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aurulentsansmono-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-aurulentsansmono-nerd-font.rb
+++ Formula/font-aurulentsansmono-nerd-font.rb
@@ -1,11 +1,12 @@
 class FontAurulentsansmonoNerdFont < Formula
-  version "2.0.0"
-  sha256 "916a4a74f895587578cb5b2d55c37e513a87a15ce65c394e99f9c95a04b60fba"
+  version "2.1.0"
+  sha256 "5f318ab83d3b53ddcdd73739e2c522661d5dacd348abedde611845d510496560"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
-  desc "AurulentSansMono Nerd Font (AurulentSansMono)"
+  desc "AurulentSansMono Nerd Font (Aurulent Sans Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
     (share/"fonts").install "AurulentSansMono-Regular Nerd Font Complete.otf"
+    (share/"fonts").install "AurulentSansMono-Regular Nerd Font Complete Mono.otf"
   end
   test do
   end
