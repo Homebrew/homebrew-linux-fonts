21ef07ba4b8e80590befc7722fc603fce666a0e1
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-robotomono-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-robotomono-nerd-font.rb
+++ Formula/font-robotomono-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontRobotomonoNerdFont < Formula
-  version "2.0.0"
-  sha256 "0714a974b22bfedbed8855ef589a12f9c60167788278965bd61d3d5d06bcf8e8"
+  version "2.1.0"
+  sha256 "1dcd5d319bb70c098cb3499059f1aa7536be4c59399724db0de833c07eca0bda"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/RobotoMono.zip"
   desc "RobotoMono Nerd Font (RobotoMono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
@@ -15,6 +15,16 @@ class FontRobotomonoNerdFont < Formula
     (share/"fonts").install "Roboto Mono Thin Nerd Font Complete.ttf"
     (share/"fonts").install "Roboto Mono Thin Italic Nerd Font Complete.ttf"
     (share/"fonts").install "Roboto Mono Light Nerd Font Complete.ttf"
+    (share/"fonts").install "Roboto Mono Medium Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Roboto Mono Medium Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Roboto Mono Light Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Roboto Mono Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Roboto Mono Bold Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Roboto Mono Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Roboto Mono Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Roboto Mono Thin Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Roboto Mono Thin Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Roboto Mono Light Nerd Font Complete Mono.ttf"
   end
   test do
   end
