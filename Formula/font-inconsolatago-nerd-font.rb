7b48cfecdfa133778c3ef91a89602335c436bd3b
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolatago-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-inconsolatago-nerd-font.rb
+++ Formula/font-inconsolatago-nerd-font.rb
@@ -1,12 +1,14 @@
 class FontInconsolatagoNerdFont < Formula
-  version "2.0.0"
-  sha256 "4a3860cb82de6e5e00dc13871269073a0367ccb75bc8d0f5d33d303e7d1e4986"
+  version "2.1.0"
+  sha256 "b5483ade3759fdcab372dab143347e4a021f2d16442be9a96a0235414487de30"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataGo.zip"
-  desc "InconsolataGo Nerd Font (InconsolataGo)"
+  desc "InconsolataGo Nerd Font (Inconsolata Go)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
     (share/"fonts").install "InconsolataGo Bold Nerd Font Complete.ttf"
     (share/"fonts").install "InconsolataGo Nerd Font Complete.ttf"
+    (share/"fonts").install "InconsolataGo Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "InconsolataGo Nerd Font Complete Mono.ttf"
   end
   test do
   end
