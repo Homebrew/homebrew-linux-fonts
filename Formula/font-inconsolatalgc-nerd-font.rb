25ea7a6fa86bd4f47e6e7d0963e9fda46bd896d0
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolatalgc-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-inconsolatalgc-nerd-font.rb
+++ Formula/font-inconsolatalgc-nerd-font.rb
@@ -1,14 +1,18 @@
 class FontInconsolatalgcNerdFont < Formula
-  version "2.0.0"
-  sha256 "e764c3fc484088dd12d93688f4be8efa7e89b8e87ff5be2e9473ab2f37a60c40"
+  version "2.1.0"
+  sha256 "ed0217588f8169e9fdf0a02ce9f68f3cfc81b92006b5ba9e631a929f03b5869c"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataLGC.zip"
-  desc "InconsolataLGC Nerd Font (InconsolataLGC)"
+  desc "InconsolataLGC Nerd Font (Inconsolata LGC)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
     (share/"fonts").install "Inconsolata LGC Bold Nerd Font Complete.ttf"
     (share/"fonts").install "Inconsolata LGC Nerd Font Complete.ttf"
     (share/"fonts").install "Inconsolata LGC Bold Italic Nerd Font Complete.ttf"
     (share/"fonts").install "Inconsolata LGC Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Inconsolata LGC Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Inconsolata LGC Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Inconsolata LGC Bold Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Inconsolata LGC Italic Nerd Font Complete Mono.ttf"
   end
   test do
   end
