b3e73fb95b597e12cffcaf717634d05fa8e18dd5
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-codenewroman-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-codenewroman-nerd-font.rb
+++ Formula/font-codenewroman-nerd-font.rb
@@ -1,13 +1,16 @@
 class FontCodenewromanNerdFont < Formula
-  version "1.2.0"
-  sha256 "b4302eafd46bb7ffa4a5e349d298e2cfd68a82c02c68e9d2235f4d4d383668a3"
+  version "2.1.0"
+  sha256 "ea0a58e9559e07f805aa48805b589d3adef81cfd10f0e481ef8aae3b2457fdd5"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/CodeNewRoman.zip"
-  desc "CodeNewRoman Nerd Font (CodeNewRoman)"
+  desc "CodeNewRoman Nerd Font (Code New Roman)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
     (share/"fonts").install "Code New Roman Italic Nerd Font Complete.otf"
     (share/"fonts").install "Code New Roman Nerd Font Complete.otf"
     (share/"fonts").install "Code New Roman Bold Nerd Font Complete.otf"
+    (share/"fonts").install "Code New Roman Italic Nerd Font Complete Mono.otf"
+    (share/"fonts").install "Code New Roman Nerd Font Complete Mono.otf"
+    (share/"fonts").install "Code New Roman Bold Nerd Font Complete Mono.otf"
   end
   test do
   end
