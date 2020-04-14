ab6c88761ea6102d80a8aaa5b97059a248912efb
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monoid-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-monoid-nerd-font.rb
+++ Formula/font-monoid-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontMonoidNerdFont < Formula
-  version "2.0.0"
-  sha256 "ad63efadd67364f2e20eb1d0c387927dd0d638899036a70b4a06ae7842178d48"
+  version "2.1.0"
+  sha256 "ad111056f16d6174482aa5c4ae23b0300e71acc095a20d698a15f3e8a440f154"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Monoid.zip"
   desc "Monoid Nerd Font (Monoid)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
@@ -9,6 +9,10 @@ class FontMonoidNerdFont < Formula
     (share/"fonts").install "Monoid Bold Nerd Font Complete.ttf"
     (share/"fonts").install "Monoid Retina Nerd Font Complete.ttf"
     (share/"fonts").install "Monoid Regular Nerd Font Complete.ttf"
+    (share/"fonts").install "Monoid Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Monoid Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Monoid Retina Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Monoid Regular Nerd Font Complete Mono.ttf"
   end
   test do
   end
