cfaf24d54efc5ec8fca9fb67aad528abedc6ae1c
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spacemono-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-spacemono-nerd-font.rb
+++ Formula/font-spacemono-nerd-font.rb
@@ -1,14 +1,18 @@
 class FontSpacemonoNerdFont < Formula
-  version "2.0.0"
-  sha256 "c41c8b4d9a634a8662aaa70d4541a534e82a13fbcc40c14149a01cda32925648"
+  version "2.1.0"
+  sha256 "b3bee63e157aefd83424b3b02db90007e21452fe23df7707657e28dd9e70b5f6"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/SpaceMono.zip"
-  desc "SpaceMono Nerd Font (SpaceMono)"
+  desc "SpaceMono Nerd Font (Space Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
     (share/"fonts").install "Space Mono Italic Nerd Font Complete.ttf"
     (share/"fonts").install "Space Mono Bold Italic Nerd Font Complete.ttf"
     (share/"fonts").install "Space Mono Bold Nerd Font Complete.ttf"
     (share/"fonts").install "Space Mono Nerd Font Complete.ttf"
+    (share/"fonts").install "Space Mono Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Space Mono Bold Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Space Mono Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Space Mono Nerd Font Complete Mono.ttf"
   end
   test do
   end
