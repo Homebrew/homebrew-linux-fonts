669021fc1ab01a59c7837e84b1229cfab711c54a
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-profont-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-profont-nerd-font.rb
+++ Formula/font-profont-nerd-font.rb
@@ -1,11 +1,14 @@
 class FontProfontNerdFont < Formula
-  version "2.0.0"
-  sha256 "a82de4b5d5322bbe21ba3aed4520d7f2f3da72280a4caf005fb3d2787b36d325"
+  version "2.1.0"
+  sha256 "27ff63d48396611c3051133ca5e6a8e6e0c6ccfbc1928ac7be497020f3cd6432"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ProFont.zip"
-  desc "ProFontIIx Nerd Font (ProFont)"
+  desc "ProFont Nerd Font families (ProFont)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
+    (share/"fonts").install "ProFontWindows Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "ProFontWindows Nerd Font Complete.ttf"
     (share/"fonts").install "ProFont IIx Nerd Font Complete.ttf"
+    (share/"fonts").install "ProFont IIx Nerd Font Complete Mono.ttf"
   end
   test do
   end
