00a0644ea174742b8ac6620e4e77b10cfa5b7220
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-inconsolata-nerd-font.rb
+++ Formula/font-inconsolata-nerd-font.rb
@@ -1,11 +1,20 @@
 class FontInconsolataNerdFont < Formula
-  version "2.0.0"
-  sha256 "7ef196ce9fa7b4bc3f9e0290a0de0fbefee123a705ba84a1993d6336a92a5164"
+  version "2.1.0"
+  sha256 "21b2e09afb0fd7f0c06aba6605dae79abe1b7c9e695fc34d2232b43101e3eb91"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Inconsolata.zip"
   desc "Inconsolata Nerd Font (Inconsolata)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
+    (share/"fonts").install "Inconsolata Regular Nerd Font Complete.otf"
+    (share/"fonts").install "Inconsolata Bold Nerd Font Complete.ttf"
+    (share/"fonts").install "Inconsolata Bold Nerd Font Complete Mono.otf"
+    (share/"fonts").install "Inconsolata Regular Nerd Font Complete Mono.ttf"
     (share/"fonts").install "Inconsolata Nerd Font Complete.otf"
+    (share/"fonts").install "Inconsolata Nerd Font Complete Mono.otf"
+    (share/"fonts").install "Inconsolata Regular Nerd Font Complete Mono.otf"
+    (share/"fonts").install "Inconsolata Bold Nerd Font Complete.otf"
+    (share/"fonts").install "Inconsolata Regular Nerd Font Complete.ttf"
+    (share/"fonts").install "Inconsolata Bold Nerd Font Complete Mono.ttf"
   end
   test do
   end
