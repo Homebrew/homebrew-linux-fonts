82093f8749e5eda536623028920957edb8aa6df3
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lekton-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-lekton-nerd-font.rb
+++ Formula/font-lekton-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontLektonNerdFont < Formula
-  version "2.0.0"
-  sha256 "07ef874e46462524c53c1923ffc98aa53f7c2544303e326f218edd101f775ea0"
+  version "2.1.0"
+  sha256 "71b424525f5e22d86b91e4fe36e26100510d785baa72f138012169bb922cde56"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Lekton.zip"
   desc "Lekton Nerd Font (Lekton)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
@@ -8,6 +8,9 @@ class FontLektonNerdFont < Formula
     (share/"fonts").install "Lekton-Bold Nerd Font Complete.ttf"
     (share/"fonts").install "Lekton Nerd Font Complete.ttf"
     (share/"fonts").install "Lekton-Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Lekton-Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Lekton Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Lekton-Italic Nerd Font Complete Mono.ttf"
   end
   test do
   end
