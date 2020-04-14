73fc7aa0c4f3d1d3824c9e586f8477bdd6ff13db
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arimo-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-arimo-nerd-font.rb
+++ Formula/font-arimo-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontArimoNerdFont < Formula
-  version "2.0.0"
-  sha256 "5b23a4607604cfecc7a4551fab86df86acd49c5ce8dfb93d51d195e6b7f0a09e"
+  version "2.1.0"
+  sha256 "683a1b4f33dcf20d6ceee89161786b1684a22bc7296efd5fd4c9d766ba6bf4cc"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Arimo.zip"
   desc "Arimo Nerd Font (Arimo)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
@@ -9,6 +9,10 @@ class FontArimoNerdFont < Formula
     (share/"fonts").install "Arimo Bold Italic Nerd Font Complete.ttf"
     (share/"fonts").install "Arimo Regular Nerd Font Complete.ttf"
     (share/"fonts").install "Arimo Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Arimo Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Arimo Bold Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Arimo Regular Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Arimo Italic Nerd Font Complete Mono.ttf"
   end
   test do
   end
