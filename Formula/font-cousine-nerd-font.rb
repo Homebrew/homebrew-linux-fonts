9d8ed053be7f15671c02542badf7270b1b9d339b
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cousine-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-cousine-nerd-font.rb
+++ Formula/font-cousine-nerd-font.rb
@@ -1,14 +1,18 @@
 class FontCousineNerdFont < Formula
-  version "2.0.0"
-  sha256 "dae2a0a363b03ef2adaf9cd341e9e5210b23daef7dea617bdf79c375ced50c4e"
+  version "2.1.0"
+  sha256 "c16fb6d228178013684a7b71b6ebb6ee5d81f1316d8f7221836da8a5737f204b"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Cousine.zip"
   desc "Cousine Nerd Font (Cousine)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
     (share/"fonts").install "Cousine Bold Nerd Font Complete.ttf"
+    (share/"fonts").install "Cousine Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Cousine Bold Italic Nerd Font Complete Mono.ttf"
     (share/"fonts").install "Cousine Bold Italic Nerd Font Complete.ttf"
     (share/"fonts").install "Cousine Regular Nerd Font Complete.ttf"
+    (share/"fonts").install "Cousine Regular Nerd Font Complete Mono.ttf"
     (share/"fonts").install "Cousine Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Cousine Italic Nerd Font Complete Mono.ttf"
   end
   test do
   end
