38e3856110976a67807896f3106b59bc4427c7f1
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-3270-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-3270-nerd-font.rb
+++ Formula/font-3270-nerd-font.rb
@@ -1,14 +1,16 @@
 class Font3270NerdFont < Formula
-  version "2.0.0"
-  sha256 "d4b34b2d6c7cb758d8d6c7213a08a732a3759da123904e316df08e38d421641b"
+  version "2.1.0"
+  sha256 "b39bb03a46bf51cacf094a53d9f07c090ede4571efa2c24600c66a25a3fee1cc"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/3270.zip"
-  desc "3270Medium Nerd Font (3270)"
+  desc "3270 Nerd Font (3270)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
     (share/"fonts").install "3270-Medium Nerd Font Complete.otf"
-    (share/"fonts").install "3270-Medium Nerd Font Complete.ttf"
-    (share/"fonts").install "3270 Narrow Nerd Font Complete.ttf"
     (share/"fonts").install "3270 Narrow Nerd Font Complete.otf"
+    (share/"fonts").install "3270 Semi-Narrow Nerd Font Complete.otf"
+    (share/"fonts").install "3270-Medium Nerd Font Complete Mono.otf"
+    (share/"fonts").install "3270 Narrow Nerd Font Complete Mono.otf"
+    (share/"fonts").install "3270 Semi-Narrow Nerd Font Complete Mono.otf"
   end
   test do
   end
