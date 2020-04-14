ebe4aa5b071de79f8e149efc0f0f614c2400a3a4
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-droidsansmono-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-droidsansmono-nerd-font.rb
+++ Formula/font-droidsansmono-nerd-font.rb
@@ -1,11 +1,12 @@
 class FontDroidsansmonoNerdFont < Formula
-  version "2.0.0"
-  sha256 "b48ace2486e2503aa829adcb4d5814567d23e46a99af4a79b21c2c00bb13bfda"
+  version "2.1.0"
+  sha256 "ffdcbbed0f95d2f4030fdc35297ed94f8e1da4e9313cd510cd900b452c5beca0"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DroidSansMono.zip"
-  desc "DroidSansMono Nerd Font (DroidSansMono)"
+  desc "DroidSansMono Nerd Font (Droid Sans Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
     (share/"fonts").install "Droid Sans Mono Nerd Font Complete.otf"
+    (share/"fonts").install "Droid Sans Mono Nerd Font Complete Mono.otf"
   end
   test do
   end
