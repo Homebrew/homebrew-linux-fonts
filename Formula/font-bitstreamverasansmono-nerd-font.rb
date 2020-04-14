2f9b8cbdb7b8a7527a7743f5e8504c4126179174
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bitstreamverasansmono-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-bitstreamverasansmono-nerd-font.rb
+++ Formula/font-bitstreamverasansmono-nerd-font.rb
@@ -1,14 +1,18 @@
 class FontBitstreamverasansmonoNerdFont < Formula
-  version "2.0.0"
-  sha256 "285c7c08ee2d651cf1f0bdb8a83b787adcab2449ac09b04c23aa4ae17a50d84b"
+  version "2.1.0"
+  sha256 "b31595d1e717156a1f42b0ff635b93d5da631312e4f0bd5c581259171e4a42d8"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BitstreamVeraSansMono.zip"
-  desc "BitstreamVeraSansMono Nerd Font (BitstreamVeraSansMono)"
+  desc "BitstreamVeraSansMono Nerd Font (Bitstream Vera Sans Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
     (share/"fonts").install "Bitstream Vera Sans Mono Bold Nerd Font Complete.ttf"
+    (share/"fonts").install "Bitstream Vera Sans Mono Bold Nerd Font Complete Mono.ttf"
     (share/"fonts").install "Bitstream Vera Sans Mono Bold Oblique Nerd Font Complete.ttf"
+    (share/"fonts").install "Bitstream Vera Sans Mono Bold Oblique Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Bitstream Vera Sans Mono Nerd Font Complete Mono.ttf"
     (share/"fonts").install "Bitstream Vera Sans Mono Nerd Font Complete.ttf"
     (share/"fonts").install "Bitstream Vera Sans Mono Oblique Nerd Font Complete.ttf"
+    (share/"fonts").install "Bitstream Vera Sans Mono Oblique Nerd Font Complete Mono.ttf"
   end
   test do
   end
