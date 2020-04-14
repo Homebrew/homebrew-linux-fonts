26be2e8be7776158c93c4df21c4a7b4235e0f9f4
Fernando Garcia Borges <fgborges@pm.me>
2020-04-14 00:13:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu-nerd-font.rb from 99949ee6979de9a99fdf6bb8a1a9c457c05fd747

--- Formula/font-ubuntu-nerd-font.rb
+++ Formula/font-ubuntu-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontUbuntuNerdFont < Formula
-  version "2.0.0"
-  sha256 "1c63e03728d89bbc6df6d357d156b5adb1c90bde247933c7cb429e6482ad22bb"
+  version "2.1.0"
+  sha256 "30e241751705401885f265bf3f1e420ede62a35a6f6ed859e4adcc8dd6cbedd3"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Ubuntu.zip"
   desc "Ubuntu Nerd Font (Ubuntu)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
@@ -14,6 +14,15 @@ class FontUbuntuNerdFont < Formula
     (share/"fonts").install "Ubuntu Condensed Nerd Font Complete.ttf"
     (share/"fonts").install "Ubuntu Light Nerd Font Complete.ttf"
     (share/"fonts").install "Ubuntu Light Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Ubuntu Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Ubuntu Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Ubuntu Bold Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Ubuntu Medium Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Ubuntu Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Ubuntu Medium Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Ubuntu Condensed Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Ubuntu Light Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Ubuntu Light Italic Nerd Font Complete Mono.ttf"
   end
   test do
   end
