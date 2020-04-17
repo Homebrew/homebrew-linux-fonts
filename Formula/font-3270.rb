1247d9ebe077666598c2f923de6e3020c74d954b
Fernando Garcia Borges <fgborges@pm.me>
2020-04-17 00:11:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-3270.rb from 776540228d9144de7156fc1fccbfdc8c2f9e47b5

--- Formula/font-3270.rb
+++ Formula/font-3270.rb
@@ -1,13 +1,16 @@
 class Font3270 < Formula
-  version "1.2.23,d250fd9"
-  sha256 "9cf7235088b3afe7827b24fec534a569b5cab15c26e0b5b6956cc2af0db837b6"
+  version "2.0.4,ece94f6"
+  sha256 "d5755c4774eb5ab81b8284d53021930ec55b191ab977f14d27b25c6f33358963"
   url "https://github.com/rbanffy/3270font/releases/download/v#{version.to_s.sub(/,.*/, "")}/3270_fonts_#{version.to_s.sub(/.*,/, "")}.zip"
   desc "IBM 3270"
   homepage "https://github.com/rbanffy/3270font"
   def install
-    (share/"fonts").install "3270Medium.otf"
-    (share/"fonts").install "3270Narrow.otf"
-    (share/"fonts").install "3270SemiNarrow.otf"
+    (share/"fonts").install "3270-Regular.otf"
+    (share/"fonts").install "3270-Regular.ttf"
+    (share/"fonts").install "3270Condensed-Regular.otf"
+    (share/"fonts").install "3270Condensed-Regular.ttf"
+    (share/"fonts").install "3270SemiCondensed-Regular.otf"
+    (share/"fonts").install "3270SemiCondensed-Regular.ttf"
   end
   test do
   end
