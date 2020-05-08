365bec2bee191f0e3f33b8d1c8e431f005ebb798
TANIGUCHI Masaya <mew@cat.ovh>
2020-05-08 00:12:42 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-code-pro-for-powerline.rb from 2997a377d214b56d9f5ee864bea6b5c7f2581ca6

--- Formula/font-source-code-pro-for-powerline.rb
+++ Formula/font-source-code-pro-for-powerline.rb
@@ -1,15 +1,22 @@
 class FontSourceCodeProForPowerline < Formula
-  head "https://github.com/powerline/fonts/trunk/SourceCodePro", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/powerline/fonts/trunk/SourceCodePro", using: :svn, trust_cert: true
   desc "Source Code Pro for Powerline"
   homepage "https://github.com/powerline/fonts/tree/master/SourceCodePro"
   def install
-    (share/"fonts").install "Sauce Code Powerline Black.otf"
-    (share/"fonts").install "Sauce Code Powerline Bold.otf"
-    (share/"fonts").install "Sauce Code Powerline ExtraLight.otf"
-    (share/"fonts").install "Sauce Code Powerline Light.otf"
-    (share/"fonts").install "Sauce Code Powerline Medium.otf"
-    (share/"fonts").install "Sauce Code Powerline Regular.otf"
-    (share/"fonts").install "Sauce Code Powerline Semibold.otf"
+    (share/"fonts").install "Source Code Pro Black for Powerline.otf"
+    (share/"fonts").install "Source Code Pro Bold for Powerline.otf"
+    (share/"fonts").install "Source Code Pro ExtraLight for Powerline.otf"
+    (share/"fonts").install "Source Code Pro Light for Powerline.otf"
+    (share/"fonts").install "Source Code Pro Medium for Powerline.otf"
+    (share/"fonts").install "Source Code Pro Powerline BlackItalic.otf"
+    (share/"fonts").install "Source Code Pro Powerline BoldItalic.otf"
+    (share/"fonts").install "Source Code Pro Powerline ExtraLightItalic.otf"
+    (share/"fonts").install "Source Code Pro Powerline Italic.otf"
+    (share/"fonts").install "Source Code Pro Powerline LightItalic.otf"
+    (share/"fonts").install "Source Code Pro Powerline MediumItalic.otf"
+    (share/"fonts").install "Source Code Pro Powerline SemiboldItalic.otf"
+    (share/"fonts").install "Source Code Pro Semibold for Powerline.otf"
+    (share/"fonts").install "Source Code Pro for Powerline.otf"
   end
   test do
   end
