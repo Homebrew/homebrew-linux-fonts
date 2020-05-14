98622968cf3956535908a982305558d9c85a587c
TANIGUCHI Masaya <mew@cat.ovh>
2020-05-14 00:14:32 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cascadia.rb from c880bad3235107326fab62bc8dd4e764054182ec

--- Formula/font-cascadia.rb
+++ Formula/font-cascadia.rb
@@ -1,11 +1,12 @@
 class FontCascadia < Formula
-  version "1911.21"
-  sha256 "cf5b69933c568eac4231303a952ce57c1581dac10c6e73c70b763cf9ecaabed4"
-  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/Cascadia.ttf"
+  version "2004.30"
+  sha256 "e240fcf61260bd3d6bc95f75336717fa5312a7bff312edbcb4dfc377825e7faf"
+  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode_#{version}.zip"
   desc "Cascadia"
   homepage "https://github.com/microsoft/cascadia-code"
   def install
-    (share/"fonts").install "Cascadia.ttf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}otf/CascadiaCode.otf"
   end
   test do
   end
