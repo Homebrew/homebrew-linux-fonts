02845c170e94b19e37a1957f4ccf16807d6e281b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-aile.rb from 7eceeed56ebed04ee0d1edf5133f4d60dfd7f351

---
+++ Formula/font-iosevka-aile.rb
@@ -0,0 +1,20 @@
+class FontIosevkaAile < Formula
+  version "3.1.1"
+  sha256 "5b23389ff2aafa8c0791fe9434e4a08451aa5c50c78c26743acb5a4fa1427f96"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-aile-#{version}.zip"
+  desc "Iosevka Aile"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install "iosevka-aile-bold.ttc"
+    (share/"fonts").install "iosevka-aile-extrabold.ttc"
+    (share/"fonts").install "iosevka-aile-extralight.ttc"
+    (share/"fonts").install "iosevka-aile-heavy.ttc"
+    (share/"fonts").install "iosevka-aile-light.ttc"
+    (share/"fonts").install "iosevka-aile-medium.ttc"
+    (share/"fonts").install "iosevka-aile-regular.ttc"
+    (share/"fonts").install "iosevka-aile-semibold.ttc"
+    (share/"fonts").install "iosevka-aile-thin.ttc"
+  end
+  test do
+  end
+end
