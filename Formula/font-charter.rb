09d04617c14346f621b5752de2ef9b9c1727e20c
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-charter.rb from 898fcbba7e60af5f66e37e9f58d05ab7913f04ce

--- Formula/font-charter.rb
+++ Formula/font-charter.rb
@@ -1,13 +1,15 @@
 class FontCharter < Formula
-  head "https://practicaltypography.com/fonts/charter.zip"
+  version "200512"
+  sha256 "39d1615e8b9c71f6e339c4d66f9cd1c6c5e85a63e201cb880f7691fa2d26d3a3"
+  url "https://practicaltypography.com/fonts/Charter%20#{version}.zip"
   desc "Charter"
   homepage "https://practicaltypography.com/charter.html"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}charter/otf/Charter Bold Italic.otf"
-    (share/"fonts").install "#{parent}charter/otf/Charter Bold.otf"
-    (share/"fonts").install "#{parent}charter/otf/Charter Italic.otf"
-    (share/"fonts").install "#{parent}charter/otf/Charter Regular.otf"
+    (share/"fonts").install "#{parent}Charter/Charter/OpenType/Charter Bold Italic.otf"
+    (share/"fonts").install "#{parent}Charter/Charter/OpenType/Charter Bold.otf"
+    (share/"fonts").install "#{parent}Charter/Charter/OpenType/Charter Italic.otf"
+    (share/"fonts").install "#{parent}Charter/Charter/OpenType/Charter Regular.otf"
   end
   test do
   end
