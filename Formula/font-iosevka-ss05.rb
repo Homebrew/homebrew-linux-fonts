8f65688deb335761942d277c8a353a71fd653110
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss05.rb from 1b184ef44315483b35a6baddc581aa2060df4fa8

---
+++ Formula/font-iosevka-ss05.rb
@@ -0,0 +1,20 @@
+class FontIosevkaSs05 < Formula
+  version "3.1.1"
+  sha256 "0abbffcbb90da2cf24acdc1014055608188307ccc2e953c4ceb542bb67700bd0"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss05-#{version}.zip"
+  desc "Iosevka SS05"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install "iosevka-ss05-bold.ttc"
+    (share/"fonts").install "iosevka-ss05-extrabold.ttc"
+    (share/"fonts").install "iosevka-ss05-extralight.ttc"
+    (share/"fonts").install "iosevka-ss05-heavy.ttc"
+    (share/"fonts").install "iosevka-ss05-light.ttc"
+    (share/"fonts").install "iosevka-ss05-medium.ttc"
+    (share/"fonts").install "iosevka-ss05-regular.ttc"
+    (share/"fonts").install "iosevka-ss05-semibold.ttc"
+    (share/"fonts").install "iosevka-ss05-thin.ttc"
+  end
+  test do
+  end
+end
