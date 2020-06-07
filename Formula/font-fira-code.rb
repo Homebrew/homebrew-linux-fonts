e10fab668e53c60b4fc23c13d5b535207edb7725
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-code.rb from 2b58af90773d1b2b3447dc8092838e35daf93655

--- Formula/font-fira-code.rb
+++ Formula/font-fira-code.rb
@@ -1,7 +1,7 @@
 class FontFiraCode < Formula
-  version "3.1"
-  sha256 "cbcabd2c412ee4c3fec4688be8387de18a33bb77bc5c5988e9fd9293a0b9dbb7"
-  url "https://github.com/tonsky/FiraCode/releases/download/#{version}/FiraCode_#{version}.zip"
+  version "4"
+  sha256 "46ed45d1a793a56e13d31ed10fb7e09f5277731953a0d9522915644fc59086d8"
+  url "https://github.com/tonsky/FiraCode/releases/download/#{version}/Fira_Code_v#{version}.zip"
   desc "Fira Code"
   homepage "https://github.com/tonsky/FiraCode"
   def install
@@ -11,6 +11,7 @@ class FontFiraCode < Formula
     (share/"fonts").install "#{parent}otf/FiraCode-Medium.otf"
     (share/"fonts").install "#{parent}otf/FiraCode-Regular.otf"
     (share/"fonts").install "#{parent}otf/FiraCode-Retina.otf"
+    (share/"fonts").install "#{parent}otf/FiraCode-SemiBold.otf"
   end
   test do
   end
