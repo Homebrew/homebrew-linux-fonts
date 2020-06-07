1eedcc4b9025187fb4ea66d6eef39eb8f167da9d
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-curly-slab.rb from 7eceeed56ebed04ee0d1edf5133f4d60dfd7f351

---
+++ Formula/font-iosevka-curly-slab.rb
@@ -0,0 +1,20 @@
+class FontIosevkaCurlySlab < Formula
+  version "3.1.1"
+  sha256 "b364653bba8085115b428c9002c6847baa285066401cae5a6bd457564d3cc797"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-slab-#{version}.zip"
+  desc "Iosevka Curly Slab"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install "iosevka-curly-slab-bold.ttc"
+    (share/"fonts").install "iosevka-curly-slab-extrabold.ttc"
+    (share/"fonts").install "iosevka-curly-slab-extralight.ttc"
+    (share/"fonts").install "iosevka-curly-slab-heavy.ttc"
+    (share/"fonts").install "iosevka-curly-slab-light.ttc"
+    (share/"fonts").install "iosevka-curly-slab-medium.ttc"
+    (share/"fonts").install "iosevka-curly-slab-regular.ttc"
+    (share/"fonts").install "iosevka-curly-slab-semibold.ttc"
+    (share/"fonts").install "iosevka-curly-slab-thin.ttc"
+  end
+  test do
+  end
+end
