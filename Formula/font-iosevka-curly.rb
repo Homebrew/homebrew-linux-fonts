1fe9245ca185b50cf42403bf3d354fe82f8351fd
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-curly.rb from 7eceeed56ebed04ee0d1edf5133f4d60dfd7f351

---
+++ Formula/font-iosevka-curly.rb
@@ -0,0 +1,20 @@
+class FontIosevkaCurly < Formula
+  version "3.1.1"
+  sha256 "1f8b5400ccc033b6956fc2b321b667d73f3b35c08a527ed6881b61c618b44545"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
+  desc "Iosevka Curly"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install "iosevka-curly-bold.ttc"
+    (share/"fonts").install "iosevka-curly-extrabold.ttc"
+    (share/"fonts").install "iosevka-curly-extralight.ttc"
+    (share/"fonts").install "iosevka-curly-heavy.ttc"
+    (share/"fonts").install "iosevka-curly-light.ttc"
+    (share/"fonts").install "iosevka-curly-medium.ttc"
+    (share/"fonts").install "iosevka-curly-regular.ttc"
+    (share/"fonts").install "iosevka-curly-semibold.ttc"
+    (share/"fonts").install "iosevka-curly-thin.ttc"
+  end
+  test do
+  end
+end
