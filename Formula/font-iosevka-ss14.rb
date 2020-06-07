52d414e9f739c36d6b6ab1dd5ccbbd4f0468e4cb
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:51 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss14.rb from fe87b1d9f5f82a532492b22da48a4ff1bd328114

---
+++ Formula/font-iosevka-ss14.rb
@@ -0,0 +1,20 @@
+class FontIosevkaSs14 < Formula
+  version "3.1.1"
+  sha256 "4c58d03615b8d767776980572c1dfec3bd2c439d701603cba063d56250c443e7"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss14-#{version}.zip"
+  desc "Iosevka SS14"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install "iosevka-ss14-bold.ttc"
+    (share/"fonts").install "iosevka-ss14-extrabold.ttc"
+    (share/"fonts").install "iosevka-ss14-extralight.ttc"
+    (share/"fonts").install "iosevka-ss14-heavy.ttc"
+    (share/"fonts").install "iosevka-ss14-light.ttc"
+    (share/"fonts").install "iosevka-ss14-medium.ttc"
+    (share/"fonts").install "iosevka-ss14-regular.ttc"
+    (share/"fonts").install "iosevka-ss14-semibold.ttc"
+    (share/"fonts").install "iosevka-ss14-thin.ttc"
+  end
+  test do
+  end
+end
