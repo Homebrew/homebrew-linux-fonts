07febacdac708ff964ce07d03d8c18bcf831289a
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:50 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-etoile.rb from 7eceeed56ebed04ee0d1edf5133f4d60dfd7f351

---
+++ Formula/font-iosevka-etoile.rb
@@ -0,0 +1,20 @@
+class FontIosevkaEtoile < Formula
+  version "3.1.1"
+  sha256 "18e59ebe12f9ba2901050974de10c68266753f5849feb44311b5a4c26a4a2261"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-etoile-#{version}.zip"
+  desc "Iosevka Etoile"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install "iosevka-etoile-bold.ttc"
+    (share/"fonts").install "iosevka-etoile-extrabold.ttc"
+    (share/"fonts").install "iosevka-etoile-extralight.ttc"
+    (share/"fonts").install "iosevka-etoile-heavy.ttc"
+    (share/"fonts").install "iosevka-etoile-light.ttc"
+    (share/"fonts").install "iosevka-etoile-medium.ttc"
+    (share/"fonts").install "iosevka-etoile-regular.ttc"
+    (share/"fonts").install "iosevka-etoile-semibold.ttc"
+    (share/"fonts").install "iosevka-etoile-thin.ttc"
+  end
+  test do
+  end
+end
