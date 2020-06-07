33276f57151634bb17d530a0af2aedc4fb4081d8
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:49 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-roboto-slab.rb from e0a54cbac476ec3a18c0925443d35191bd25b460

--- Formula/font-roboto-slab.rb
+++ Formula/font-roboto-slab.rb
@@ -1,13 +1,12 @@
 class FontRobotoSlab < Formula
-  head "https://github.com/google/fonts/trunk/apache/robotoslab", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/apache/robotoslab/static", using: :svn, trust_cert: true
   desc "Roboto Slab"
   homepage "https://www.google.com/fonts/specimen/Roboto%20Slab"
   def install
-    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}static/RobotoSlab-Bold.ttf"
-    (share/"fonts").install "#{parent}static/RobotoSlab-Light.ttf"
-    (share/"fonts").install "#{parent}static/RobotoSlab-Regular.ttf"
-    (share/"fonts").install "#{parent}static/RobotoSlab-Thin.ttf"
+    (share/"fonts").install "RobotoSlab-Bold.ttf"
+    (share/"fonts").install "RobotoSlab-Light.ttf"
+    (share/"fonts").install "RobotoSlab-Regular.ttf"
+    (share/"fonts").install "RobotoSlab-Thin.ttf"
   end
   test do
   end
