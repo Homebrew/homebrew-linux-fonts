e898b0c9c4a9fdde419a92158eb09a15b3d59eea
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-07 20:37:48 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-league-spartan.rb from 1837305bf78ba5e542595db9c31a382bb44fa394

--- Formula/font-league-spartan.rb
+++ Formula/font-league-spartan.rb
@@ -1,12 +1,20 @@
 class FontLeagueSpartan < Formula
-  version "c350408b07"
-  sha256 "a166294a7e156a0eb14df5714acfeeb3ad4db01eab2bc88f961695da917337fd"
-  url "http://files.theleagueofmoveabletype.com/downloads/league-spartan-#{version}.zip"
+  version "2.201"
+  sha256 "6c027fb675c3479cc50da28bdd43bdae0debcb40eabdd42a659d4df8bb315d0a"
+  url "https://github.com/theleagueof/league-spartan/archive/#{version}.zip"
   desc "League Spartan"
   homepage "https://www.theleagueofmoveabletype.com/league-spartan"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}league-spartan-master/LeagueSpartan-Bold.otf"
+    (share/"fonts").install "#{parent}league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Black.otf"
+    (share/"fonts").install "#{parent}league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Bold.otf"
+    (share/"fonts").install "#{parent}league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Extrabold.otf"
+    (share/"fonts").install "#{parent}league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Extralight.otf"
+    (share/"fonts").install "#{parent}league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Light.otf"
+    (share/"fonts").install "#{parent}league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Medium.otf"
+    (share/"fonts").install "#{parent}league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Regular.otf"
+    (share/"fonts").install "#{parent}league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Semibold.otf"
+    (share/"fonts").install "#{parent}league-spartan-#{version}/fonts/variable/LeagueSpartanVariable.ttf"
   end
   test do
   end
