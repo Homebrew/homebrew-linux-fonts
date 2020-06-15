5470be3a7821d55c478fce5886f2e11947c361b2
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-doppio-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-doppio-one.rb
+++ Formula/font-doppio-one.rb
@@ -1,7 +1,7 @@
 class FontDoppioOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/doppioone/DoppioOne-Regular.ttf"
   desc "Doppio One"
-  homepage "https://www.google.com/fonts/specimen/Doppio+One"
+  homepage "https://fonts.google.com/specimen/Doppio+One"
   def install
     (share/"fonts").install "DoppioOne-Regular.ttf"
   end
