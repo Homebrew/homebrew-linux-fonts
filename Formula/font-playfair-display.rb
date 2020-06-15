b72f0170e31ec6bb7c258f68e42393ef0c6fe9de
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-playfair-display.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-playfair-display.rb
+++ Formula/font-playfair-display.rb
@@ -1,14 +1,10 @@
 class FontPlayfairDisplay < Formula
-  head "https://github.com/google/fonts/trunk/ofl/playfairdisplay", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/playfairdisplay", using: :svn, trust_cert: true
   desc "Playfair Display"
-  homepage "https://www.google.com/fonts/specimen/Playfair%20Display"
+  homepage "https://fonts.google.com/specimen/Playfair+Display"
   def install
-    (share/"fonts").install "PlayfairDisplay-Black.ttf"
-    (share/"fonts").install "PlayfairDisplay-BlackItalic.ttf"
-    (share/"fonts").install "PlayfairDisplay-Bold.ttf"
-    (share/"fonts").install "PlayfairDisplay-BoldItalic.ttf"
-    (share/"fonts").install "PlayfairDisplay-Italic.ttf"
-    (share/"fonts").install "PlayfairDisplay-Regular.ttf"
+    (share/"fonts").install "PlayfairDisplay-Italic[wght].ttf"
+    (share/"fonts").install "PlayfairDisplay[wght].ttf"
   end
   test do
   end
