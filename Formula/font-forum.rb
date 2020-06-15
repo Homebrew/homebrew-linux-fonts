91da5bfd48807261809bbcc4018b18b1d4a1d24f
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-forum.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-forum.rb
+++ Formula/font-forum.rb
@@ -1,7 +1,7 @@
 class FontForum < Formula
   head "https://github.com/google/fonts/raw/master/ofl/forum/Forum-Regular.ttf"
   desc "Forum"
-  homepage "https://www.google.com/fonts/specimen/Forum"
+  homepage "https://fonts.google.com/specimen/Forum"
   def install
     (share/"fonts").install "Forum-Regular.ttf"
   end
