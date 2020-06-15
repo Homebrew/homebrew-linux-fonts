1ecabfbfa4b8412d81ad6f5a978e46d6dc01d286
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-carter-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-carter-one.rb
+++ Formula/font-carter-one.rb
@@ -1,7 +1,7 @@
 class FontCarterOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/carterone/CarterOne.ttf"
   desc "Carter One"
-  homepage "https://www.google.com/fonts/specimen/Carter+One"
+  homepage "https://fonts.google.com/specimen/Carter+One"
   def install
     (share/"fonts").install "CarterOne.ttf"
   end
