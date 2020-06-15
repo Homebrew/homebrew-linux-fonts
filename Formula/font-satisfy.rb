d9a2a07b19e3449c8429f5336ac5fa58e6213c58
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-satisfy.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-satisfy.rb
+++ Formula/font-satisfy.rb
@@ -1,7 +1,7 @@
 class FontSatisfy < Formula
   head "https://github.com/google/fonts/raw/master/apache/satisfy/Satisfy-Regular.ttf"
   desc "Satisfy"
-  homepage "https://www.google.com/fonts/specimen/Satisfy"
+  homepage "https://fonts.google.com/specimen/Satisfy"
   def install
     (share/"fonts").install "Satisfy-Regular.ttf"
   end
