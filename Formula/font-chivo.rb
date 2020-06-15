c6bd40ba93457d2db22274a9dd77e666e7a32918
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chivo.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-chivo.rb
+++ Formula/font-chivo.rb
@@ -1,11 +1,15 @@
 class FontChivo < Formula
-  head "https://github.com/google/fonts/trunk/ofl/chivo", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/chivo", using: :svn, trust_cert: true
   desc "Chivo"
-  homepage "https://www.google.com/fonts/specimen/Chivo"
+  homepage "https://fonts.google.com/specimen/Chivo"
   def install
     (share/"fonts").install "Chivo-Black.ttf"
     (share/"fonts").install "Chivo-BlackItalic.ttf"
+    (share/"fonts").install "Chivo-Bold.ttf"
+    (share/"fonts").install "Chivo-BoldItalic.ttf"
     (share/"fonts").install "Chivo-Italic.ttf"
+    (share/"fonts").install "Chivo-Light.ttf"
+    (share/"fonts").install "Chivo-LightItalic.ttf"
     (share/"fonts").install "Chivo-Regular.ttf"
   end
   test do
