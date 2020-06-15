ae5d2e715a0a3a533cf6b9224840c292b0ecbf09
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-moul.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-moul.rb
+++ Formula/font-moul.rb
@@ -1,7 +1,7 @@
 class FontMoul < Formula
   head "https://github.com/google/fonts/raw/master/ofl/moul/Moul.ttf"
   desc "Moul"
-  homepage "https://www.google.com/fonts/specimen/Moul"
+  homepage "https://fonts.google.com/specimen/Moul"
   def install
     (share/"fonts").install "Moul.ttf"
   end
