58d4f6e9a34eec12e274f8543beb89535a950d4b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-federo.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-federo.rb
+++ Formula/font-federo.rb
@@ -1,7 +1,7 @@
 class FontFedero < Formula
   head "https://github.com/google/fonts/raw/master/ofl/federo/Federo-Regular.ttf"
   desc "Federo"
-  homepage "https://www.google.com/fonts/specimen/Federo"
+  homepage "https://fonts.google.com/specimen/Federo"
   def install
     (share/"fonts").install "Federo-Regular.ttf"
   end
