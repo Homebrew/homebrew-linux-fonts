a480c8b320257f7ed8fdeb31661309d054639732
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-griffy.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-griffy.rb
+++ Formula/font-griffy.rb
@@ -1,7 +1,7 @@
 class FontGriffy < Formula
   head "https://github.com/google/fonts/raw/master/ofl/griffy/Griffy-Regular.ttf"
   desc "Griffy"
-  homepage "https://www.google.com/fonts/specimen/Griffy"
+  homepage "https://fonts.google.com/specimen/Griffy"
   def install
     (share/"fonts").install "Griffy-Regular.ttf"
   end
