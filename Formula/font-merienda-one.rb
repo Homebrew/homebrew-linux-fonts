a6ef80e5d28a34f152cfd33642103085965dcc26
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-merienda-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-merienda-one.rb
+++ Formula/font-merienda-one.rb
@@ -1,7 +1,7 @@
 class FontMeriendaOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/meriendaone/MeriendaOne-Regular.ttf"
   desc "Merienda One"
-  homepage "https://www.google.com/fonts/specimen/Merienda+One"
+  homepage "https://fonts.google.com/specimen/Merienda+One"
   def install
     (share/"fonts").install "MeriendaOne-Regular.ttf"
   end
