cdcd557807c422a1245e8893f8f4e97597865caa
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-asset.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-asset.rb
+++ Formula/font-asset.rb
@@ -1,7 +1,7 @@
 class FontAsset < Formula
   head "https://github.com/google/fonts/raw/master/ofl/asset/Asset-Regular.ttf"
   desc "Asset"
-  homepage "https://www.google.com/fonts/specimen/Asset"
+  homepage "https://fonts.google.com/specimen/Asset"
   def install
     (share/"fonts").install "Asset-Regular.ttf"
   end
