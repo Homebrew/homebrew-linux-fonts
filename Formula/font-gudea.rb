1c2595a04edaaffe707dca0c46bc3530e7948e50
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gudea.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-gudea.rb
+++ Formula/font-gudea.rb
@@ -1,7 +1,7 @@
 class FontGudea < Formula
-  head "https://github.com/google/fonts/trunk/ofl/gudea", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/gudea", using: :svn, trust_cert: true
   desc "Gudea"
-  homepage "https://www.google.com/fonts/specimen/Gudea"
+  homepage "https://fonts.google.com/specimen/Gudea"
   def install
     (share/"fonts").install "Gudea-Bold.ttf"
     (share/"fonts").install "Gudea-Italic.ttf"
