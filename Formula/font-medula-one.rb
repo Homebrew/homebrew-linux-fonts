69a1afce33a5e155159d9cc3f17e4e12ff0153ab
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-medula-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-medula-one.rb
+++ Formula/font-medula-one.rb
@@ -1,7 +1,7 @@
 class FontMedulaOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/medulaone/MedulaOne-Regular.ttf"
   desc "Medula One"
-  homepage "https://www.google.com/fonts/specimen/Medula+One"
+  homepage "https://fonts.google.com/specimen/Medula+One"
   def install
     (share/"fonts").install "MedulaOne-Regular.ttf"
   end
