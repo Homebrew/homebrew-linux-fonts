316e476d745826104d56f3ec4c2972c07a980510
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-crimson-text.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-crimson-text.rb
+++ Formula/font-crimson-text.rb
@@ -1,14 +1,14 @@
 class FontCrimsonText < Formula
-  head "https://github.com/google/fonts/trunk/ofl/crimsontext", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/crimsontext", using: :svn, trust_cert: true
   desc "Crimson Text"
-  homepage "https://www.google.com/fonts/specimen/Crimson%20Text"
+  homepage "https://fonts.google.com/specimen/Crimson+Text"
   def install
     (share/"fonts").install "CrimsonText-Bold.ttf"
     (share/"fonts").install "CrimsonText-BoldItalic.ttf"
     (share/"fonts").install "CrimsonText-Italic.ttf"
     (share/"fonts").install "CrimsonText-Regular.ttf"
-    (share/"fonts").install "CrimsonText-Semibold.ttf"
-    (share/"fonts").install "CrimsonText-SemiboldItalic.ttf"
+    (share/"fonts").install "CrimsonText-SemiBold.ttf"
+    (share/"fonts").install "CrimsonText-SemiBoldItalic.ttf"
   end
   test do
   end
