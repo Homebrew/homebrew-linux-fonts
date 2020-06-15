8e43e6109b3f129f9fdecd8582507d268ca7b6b0
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gilda-display.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-gilda-display.rb
+++ Formula/font-gilda-display.rb
@@ -1,7 +1,7 @@
 class FontGildaDisplay < Formula
   head "https://github.com/google/fonts/raw/master/ofl/gildadisplay/GildaDisplay-Regular.ttf"
   desc "Gilda Display"
-  homepage "https://www.google.com/fonts/specimen/Gilda+Display"
+  homepage "https://fonts.google.com/specimen/Gilda+Display"
   def install
     (share/"fonts").install "GildaDisplay-Regular.ttf"
   end
