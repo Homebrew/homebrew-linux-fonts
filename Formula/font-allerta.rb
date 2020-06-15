1931eae4e09c78cc1847c0f6f07d1a3300a8b02b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-allerta.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-allerta.rb
+++ Formula/font-allerta.rb
@@ -1,7 +1,7 @@
 class FontAllerta < Formula
   head "https://github.com/google/fonts/raw/master/ofl/allerta/Allerta-Regular.ttf"
   desc "Allerta"
-  homepage "https://www.google.com/fonts/specimen/Allerta"
+  homepage "https://fonts.google.com/specimen/Allerta"
   def install
     (share/"fonts").install "Allerta-Regular.ttf"
   end
