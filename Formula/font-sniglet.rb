c9ffac8646072f56e1e051a7703d1a547b2975a9
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sniglet.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-sniglet.rb
+++ Formula/font-sniglet.rb
@@ -1,7 +1,7 @@
 class FontSniglet < Formula
-  head "https://github.com/google/fonts/trunk/ofl/sniglet", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/sniglet", using: :svn, trust_cert: true
   desc "Sniglet"
-  homepage "https://www.google.com/fonts/specimen/Sniglet"
+  homepage "https://fonts.google.com/specimen/Sniglet"
   def install
     (share/"fonts").install "Sniglet-ExtraBold.ttf"
     (share/"fonts").install "Sniglet-Regular.ttf"
