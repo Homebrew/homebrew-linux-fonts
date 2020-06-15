7ef5917fa10709ce102157eb863a4f016a98a1db
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alice.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-alice.rb
+++ Formula/font-alice.rb
@@ -1,7 +1,7 @@
 class FontAlice < Formula
   head "https://github.com/google/fonts/raw/master/ofl/alice/Alice-Regular.ttf"
   desc "Alice"
-  homepage "https://www.google.com/fonts/specimen/Alice"
+  homepage "https://fonts.google.com/specimen/Alice"
   def install
     (share/"fonts").install "Alice-Regular.ttf"
   end
