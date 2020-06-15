ddad1564ec4d7234487c7e2412198fdcf60c7d8e
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-life-savers.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-life-savers.rb
+++ Formula/font-life-savers.rb
@@ -1,9 +1,10 @@
 class FontLifeSavers < Formula
-  head "https://github.com/google/fonts/trunk/ofl/lifesavers", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/lifesavers", using: :svn, trust_cert: true
   desc "Life Savers"
-  homepage "https://www.google.com/fonts/specimen/Life%20Savers"
+  homepage "https://fonts.google.com/specimen/Life+Savers"
   def install
     (share/"fonts").install "LifeSavers-Bold.ttf"
+    (share/"fonts").install "LifeSavers-ExtraBold.ttf"
     (share/"fonts").install "LifeSavers-Regular.ttf"
   end
   test do
