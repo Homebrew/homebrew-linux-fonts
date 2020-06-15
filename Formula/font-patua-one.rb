919fe88dc20f9439369a3787291e846112a6ae91
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-patua-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-patua-one.rb
+++ Formula/font-patua-one.rb
@@ -1,7 +1,7 @@
 class FontPatuaOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/patuaone/PatuaOne-Regular.ttf"
   desc "Patua One"
-  homepage "https://www.google.com/fonts/specimen/Patua+One"
+  homepage "https://fonts.google.com/specimen/Patua+One"
   def install
     (share/"fonts").install "PatuaOne-Regular.ttf"
   end
