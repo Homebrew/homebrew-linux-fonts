13bbe0b1aa4b0f6552e3a63e0a55438c87380522
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bowlby-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-bowlby-one.rb
+++ Formula/font-bowlby-one.rb
@@ -1,7 +1,7 @@
 class FontBowlbyOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/bowlbyone/BowlbyOne-Regular.ttf"
   desc "Bowlby One"
-  homepage "https://www.google.com/fonts/specimen/Bowlby+One"
+  homepage "https://fonts.google.com/specimen/Bowlby+One"
   def install
     (share/"fonts").install "BowlbyOne-Regular.ttf"
   end
