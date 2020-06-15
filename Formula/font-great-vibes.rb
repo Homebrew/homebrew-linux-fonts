a12fce2cb48be55c162a8260a1bdce86c9acee80
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-great-vibes.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-great-vibes.rb
+++ Formula/font-great-vibes.rb
@@ -1,7 +1,7 @@
 class FontGreatVibes < Formula
   head "https://github.com/google/fonts/raw/master/ofl/greatvibes/GreatVibes-Regular.ttf"
   desc "Great Vibes"
-  homepage "https://www.google.com/fonts/specimen/Great+Vibes"
+  homepage "https://fonts.google.com/specimen/Great+Vibes"
   def install
     (share/"fonts").install "GreatVibes-Regular.ttf"
   end
