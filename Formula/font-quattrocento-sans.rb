a216ccacf2f94371812d8f43f10d2a60ab208dbc
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quattrocento-sans.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-quattrocento-sans.rb
+++ Formula/font-quattrocento-sans.rb
@@ -1,7 +1,7 @@
 class FontQuattrocentoSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/quattrocentosans", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/quattrocentosans", using: :svn, trust_cert: true
   desc "Quattrocento Sans"
-  homepage "https://www.google.com/fonts/specimen/Quattrocento%20Sans"
+  homepage "https://fonts.google.com/specimen/Quattrocento+Sans"
   def install
     (share/"fonts").install "QuattrocentoSans-Bold.ttf"
     (share/"fonts").install "QuattrocentoSans-BoldItalic.ttf"
