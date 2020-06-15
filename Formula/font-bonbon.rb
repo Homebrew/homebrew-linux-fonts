d192637c9345afa9dbf39ca7205e4d56547dba56
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bonbon.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-bonbon.rb
+++ Formula/font-bonbon.rb
@@ -1,7 +1,7 @@
 class FontBonbon < Formula
   head "https://github.com/google/fonts/raw/master/ofl/bonbon/Bonbon-Regular.ttf"
   desc "Bonbon"
-  homepage "https://www.google.com/fonts/specimen/Bonbon"
+  homepage "https://fonts.google.com/specimen/Bonbon"
   def install
     (share/"fonts").install "Bonbon-Regular.ttf"
   end
