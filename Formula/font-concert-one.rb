812cc0fd687a36dcaab542f9db6a98709819a66d
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-concert-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-concert-one.rb
+++ Formula/font-concert-one.rb
@@ -1,7 +1,7 @@
 class FontConcertOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/concertone/ConcertOne-Regular.ttf"
   desc "Concert One"
-  homepage "https://www.google.com/fonts/specimen/Concert+One"
+  homepage "https://fonts.google.com/specimen/Concert+One"
   def install
     (share/"fonts").install "ConcertOne-Regular.ttf"
   end
