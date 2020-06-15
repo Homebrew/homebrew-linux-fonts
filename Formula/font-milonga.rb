7f318464f968bcf948bbd565cb240293c483d328
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-milonga.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-milonga.rb
+++ Formula/font-milonga.rb
@@ -1,7 +1,7 @@
 class FontMilonga < Formula
   head "https://github.com/google/fonts/raw/master/ofl/milonga/Milonga-Regular.ttf"
   desc "Milonga"
-  homepage "https://www.google.com/fonts/specimen/Milonga"
+  homepage "https://fonts.google.com/specimen/Milonga"
   def install
     (share/"fonts").install "Milonga-Regular.ttf"
   end
