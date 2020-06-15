e6f9a57d5a190f77e6d9be574cea1439a269202b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-montez.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-montez.rb
+++ Formula/font-montez.rb
@@ -1,7 +1,7 @@
 class FontMontez < Formula
   head "https://github.com/google/fonts/raw/master/apache/montez/Montez-Regular.ttf"
   desc "Montez"
-  homepage "https://www.google.com/fonts/specimen/Montez"
+  homepage "https://fonts.google.com/specimen/Montez"
   def install
     (share/"fonts").install "Montez-Regular.ttf"
   end
