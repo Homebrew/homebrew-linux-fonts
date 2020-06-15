bbf1f77eaac0247fd7a0bb8dd8e4d240afa4800b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-montaga.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-montaga.rb
+++ Formula/font-montaga.rb
@@ -1,7 +1,7 @@
 class FontMontaga < Formula
   head "https://github.com/google/fonts/raw/master/ofl/montaga/Montaga-Regular.ttf"
   desc "Montaga"
-  homepage "https://www.google.com/fonts/specimen/Montaga"
+  homepage "https://fonts.google.com/specimen/Montaga"
   def install
     (share/"fonts").install "Montaga-Regular.ttf"
   end
