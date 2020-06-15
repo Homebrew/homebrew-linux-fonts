5f7a7a1a99c78797262918319d15fc73e4141ba0
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-finger-paint.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-finger-paint.rb
+++ Formula/font-finger-paint.rb
@@ -1,7 +1,7 @@
 class FontFingerPaint < Formula
   head "https://github.com/google/fonts/raw/master/ofl/fingerpaint/FingerPaint-Regular.ttf"
   desc "Finger Paint"
-  homepage "https://www.google.com/fonts/specimen/Finger+Paint"
+  homepage "https://fonts.google.com/specimen/Finger+Paint"
   def install
     (share/"fonts").install "FingerPaint-Regular.ttf"
   end
