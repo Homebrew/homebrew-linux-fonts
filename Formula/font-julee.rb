3d85b6ebfe8125e167785cf9d9a757816ba70b26
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-julee.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-julee.rb
+++ Formula/font-julee.rb
@@ -1,7 +1,7 @@
 class FontJulee < Formula
   head "https://github.com/google/fonts/raw/master/ofl/julee/Julee-Regular.ttf"
   desc "Julee"
-  homepage "https://www.google.com/fonts/specimen/Julee"
+  homepage "https://fonts.google.com/specimen/Julee"
   def install
     (share/"fonts").install "Julee-Regular.ttf"
   end
