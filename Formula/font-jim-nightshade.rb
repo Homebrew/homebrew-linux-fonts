18432b5c993619e6652678698e875aa8788d8c3e
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jim-nightshade.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-jim-nightshade.rb
+++ Formula/font-jim-nightshade.rb
@@ -1,7 +1,7 @@
 class FontJimNightshade < Formula
   head "https://github.com/google/fonts/raw/master/ofl/jimnightshade/JimNightshade-Regular.ttf"
   desc "Jim Nightshade"
-  homepage "https://www.google.com/fonts/specimen/Jim+Nightshade"
+  homepage "https://fonts.google.com/specimen/Jim+Nightshade"
   def install
     (share/"fonts").install "JimNightshade-Regular.ttf"
   end
