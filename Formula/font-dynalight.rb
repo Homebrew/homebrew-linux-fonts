d44ab275c2a300703ab2a52f8d0af50b052fdc96
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dynalight.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-dynalight.rb
+++ Formula/font-dynalight.rb
@@ -1,7 +1,7 @@
 class FontDynalight < Formula
   head "https://github.com/google/fonts/raw/master/ofl/dynalight/Dynalight-Regular.ttf"
   desc "Dynalight"
-  homepage "https://www.google.com/fonts/specimen/Dynalight"
+  homepage "https://fonts.google.com/specimen/Dynalight"
   def install
     (share/"fonts").install "Dynalight-Regular.ttf"
   end
