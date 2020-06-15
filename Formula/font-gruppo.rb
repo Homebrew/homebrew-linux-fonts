3340717137032b7441985e4cd8f4a6b06dee9a84
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gruppo.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-gruppo.rb
+++ Formula/font-gruppo.rb
@@ -1,7 +1,7 @@
 class FontGruppo < Formula
   head "https://github.com/google/fonts/raw/master/ofl/gruppo/Gruppo-Regular.ttf"
   desc "Gruppo"
-  homepage "https://www.google.com/fonts/specimen/Gruppo"
+  homepage "https://fonts.google.com/specimen/Gruppo"
   def install
     (share/"fonts").install "Gruppo-Regular.ttf"
   end
