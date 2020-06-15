b2a6eb349327683132eb62c5b1a1c67aa67debc6
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hanalei-fill.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-hanalei-fill.rb
+++ Formula/font-hanalei-fill.rb
@@ -1,7 +1,7 @@
 class FontHanaleiFill < Formula
   head "https://github.com/google/fonts/raw/master/ofl/hanaleifill/HanaleiFill-Regular.ttf"
   desc "Hanalei Fill"
-  homepage "https://www.google.com/fonts/specimen/Hanalei+Fill"
+  homepage "https://fonts.google.com/specimen/Hanalei+Fill"
   def install
     (share/"fonts").install "HanaleiFill-Regular.ttf"
   end
