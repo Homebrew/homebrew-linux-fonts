9557ba126c9a60f04e54eb5e06b01dc758cb8089
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-condiment.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-condiment.rb
+++ Formula/font-condiment.rb
@@ -1,7 +1,7 @@
 class FontCondiment < Formula
   head "https://github.com/google/fonts/raw/master/ofl/condiment/Condiment-Regular.ttf"
   desc "Condiment"
-  homepage "https://www.google.com/fonts/specimen/Condiment"
+  homepage "https://fonts.google.com/specimen/Condiment"
   def install
     (share/"fonts").install "Condiment-Regular.ttf"
   end
