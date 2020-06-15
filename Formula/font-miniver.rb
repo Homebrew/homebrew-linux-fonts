c91d375f4add6b3262b9444c6b7a5da6837c2aee
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-miniver.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-miniver.rb
+++ Formula/font-miniver.rb
@@ -1,7 +1,7 @@
 class FontMiniver < Formula
   head "https://github.com/google/fonts/raw/master/ofl/miniver/Miniver-Regular.ttf"
   desc "Miniver"
-  homepage "https://www.google.com/fonts/specimen/Miniver"
+  homepage "https://fonts.google.com/specimen/Miniver"
   def install
     (share/"fonts").install "Miniver-Regular.ttf"
   end
