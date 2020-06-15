af2fb61109608b46828064124f7d481880b4de5e
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-abeezee.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-abeezee.rb
+++ Formula/font-abeezee.rb
@@ -1,7 +1,7 @@
 class FontAbeezee < Formula
-  head "https://github.com/google/fonts/trunk/ofl/abeezee", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/abeezee", using: :svn, trust_cert: true
   desc "ABeeZee"
-  homepage "https://www.google.com/fonts/specimen/ABeeZee"
+  homepage "https://fonts.google.com/specimen/ABeeZee"
   def install
     (share/"fonts").install "ABeeZee-Italic.ttf"
     (share/"fonts").install "ABeeZee-Regular.ttf"
