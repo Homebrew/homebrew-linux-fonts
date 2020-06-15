4fce95f85c820b3c9526ea9961c97f6b96a7ef72
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-engagement.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-engagement.rb
+++ Formula/font-engagement.rb
@@ -1,7 +1,7 @@
 class FontEngagement < Formula
   head "https://github.com/google/fonts/raw/master/ofl/engagement/Engagement-Regular.ttf"
   desc "Engagement"
-  homepage "https://www.google.com/fonts/specimen/Engagement"
+  homepage "https://fonts.google.com/specimen/Engagement"
   def install
     (share/"fonts").install "Engagement-Regular.ttf"
   end
