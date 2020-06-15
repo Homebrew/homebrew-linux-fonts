d7a077973bdbe87f45e0ea5ed62b5248a07ac021
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mountains-of-christmas.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-mountains-of-christmas.rb
+++ Formula/font-mountains-of-christmas.rb
@@ -1,7 +1,7 @@
 class FontMountainsOfChristmas < Formula
-  head "https://github.com/google/fonts/trunk/apache/mountainsofchristmas", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/apache/mountainsofchristmas", using: :svn, trust_cert: true
   desc "Mountains of Christmas"
-  homepage "https://www.google.com/fonts/specimen/Mountains%20of%20Christmas"
+  homepage "https://fonts.google.com/specimen/Mountains+of+Christmas"
   def install
     (share/"fonts").install "MountainsofChristmas-Bold.ttf"
     (share/"fonts").install "MountainsofChristmas-Regular.ttf"
