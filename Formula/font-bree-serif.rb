ee7f5b2f2054565240f09d00c9c16b643340551c
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bree-serif.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-bree-serif.rb
+++ Formula/font-bree-serif.rb
@@ -1,7 +1,7 @@
 class FontBreeSerif < Formula
   head "https://github.com/google/fonts/raw/master/ofl/breeserif/BreeSerif-Regular.ttf"
   desc "Bree Serif"
-  homepage "https://www.google.com/fonts/specimen/Bree+Serif"
+  homepage "https://fonts.google.com/specimen/Bree+Serif"
   def install
     (share/"fonts").install "BreeSerif-Regular.ttf"
   end
