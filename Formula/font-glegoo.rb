3b6a816dba0d590e863e2b0b77233e1d0bbaa8df
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-glegoo.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-glegoo.rb
+++ Formula/font-glegoo.rb
@@ -1,7 +1,7 @@
 class FontGlegoo < Formula
   head "https://github.com/google/fonts/trunk/ofl/glegoo", using: :svn, trust_cert: true
   desc "Glegoo"
-  homepage "https://www.google.com/fonts/specimen/Glegoo"
+  homepage "https://fonts.google.com/specimen/Glegoo"
   def install
     (share/"fonts").install "Glegoo-Bold.ttf"
     (share/"fonts").install "Glegoo-Regular.ttf"
