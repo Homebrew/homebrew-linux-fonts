59f05e19dd8e1b37aede96f752b627cced2c8c47
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lilita-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-lilita-one.rb
+++ Formula/font-lilita-one.rb
@@ -1,7 +1,7 @@
 class FontLilitaOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/lilitaone/LilitaOne-Regular.ttf"
   desc "Lilita One"
-  homepage "https://www.google.com/fonts/specimen/Lilita+One"
+  homepage "https://fonts.google.com/specimen/Lilita+One"
   def install
     (share/"fonts").install "LilitaOne-Regular.ttf"
   end
