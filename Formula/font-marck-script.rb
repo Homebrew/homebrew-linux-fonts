d3691f793cfe4c811dbc5772d1df34095947cdbe
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marck-script.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-marck-script.rb
+++ Formula/font-marck-script.rb
@@ -1,7 +1,7 @@
 class FontMarckScript < Formula
   head "https://github.com/google/fonts/raw/master/ofl/marckscript/MarckScript-Regular.ttf"
   desc "Marck Script"
-  homepage "https://www.google.com/fonts/specimen/Marck+Script"
+  homepage "https://fonts.google.com/specimen/Marck+Script"
   def install
     (share/"fonts").install "MarckScript-Regular.ttf"
   end
