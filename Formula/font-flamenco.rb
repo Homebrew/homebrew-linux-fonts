f48a978bb9c830b4846feea4ecebac967e516b3a
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-flamenco.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-flamenco.rb
+++ Formula/font-flamenco.rb
@@ -1,7 +1,7 @@
 class FontFlamenco < Formula
-  head "https://github.com/google/fonts/trunk/ofl/flamenco", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/flamenco", using: :svn, trust_cert: true
   desc "Flamenco"
-  homepage "https://www.google.com/fonts/specimen/Flamenco"
+  homepage "https://fonts.google.com/specimen/Flamenco"
   def install
     (share/"fonts").install "Flamenco-Light.ttf"
     (share/"fonts").install "Flamenco-Regular.ttf"
