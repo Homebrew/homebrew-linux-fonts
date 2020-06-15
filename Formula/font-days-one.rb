a9795a9279cb7cb9f71ced61f406580d89172e4a
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-days-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-days-one.rb
+++ Formula/font-days-one.rb
@@ -1,7 +1,7 @@
 class FontDaysOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/daysone/DaysOne-Regular.ttf"
   desc "Days One"
-  homepage "https://www.google.com/fonts/specimen/Days+One"
+  homepage "https://fonts.google.com/specimen/Days+One"
   def install
     (share/"fonts").install "DaysOne-Regular.ttf"
   end
