13d93b4926381680244fcfe13606c3aab44900b3
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-acme.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-acme.rb
+++ Formula/font-acme.rb
@@ -1,7 +1,7 @@
 class FontAcme < Formula
   head "https://github.com/google/fonts/raw/master/ofl/acme/Acme-Regular.ttf"
   desc "Acme"
-  homepage "https://www.google.com/fonts/specimen/Acme"
+  homepage "https://fonts.google.com/specimen/Acme"
   def install
     (share/"fonts").install "Acme-Regular.ttf"
   end
