41d65e4196abbc77e384a356d20f2845c6fd966c
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gorditas.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-gorditas.rb
+++ Formula/font-gorditas.rb
@@ -1,7 +1,7 @@
 class FontGorditas < Formula
-  head "https://github.com/google/fonts/trunk/ofl/gorditas", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/gorditas", using: :svn, trust_cert: true
   desc "Gorditas"
-  homepage "https://www.google.com/fonts/specimen/Gorditas"
+  homepage "https://fonts.google.com/specimen/Gorditas"
   def install
     (share/"fonts").install "Gorditas-Bold.ttf"
     (share/"fonts").install "Gorditas-Regular.ttf"
