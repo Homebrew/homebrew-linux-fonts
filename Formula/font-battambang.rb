fda0b0df9f66de50df2befc0035705b0423c4495
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-battambang.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-battambang.rb
+++ Formula/font-battambang.rb
@@ -1,7 +1,7 @@
 class FontBattambang < Formula
-  head "https://github.com/google/fonts/trunk/ofl/battambang", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/battambang", using: :svn, trust_cert: true
   desc "Battambang"
-  homepage "https://www.google.com/fonts/specimen/Battambang"
+  homepage "https://fonts.google.com/specimen/Battambang"
   def install
     (share/"fonts").install "Battambang-Bold.ttf"
     (share/"fonts").install "Battambang-Regular.ttf"
