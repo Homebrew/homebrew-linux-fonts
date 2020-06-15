988800219532297dc17a0aa3526424713a86aa9c
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-varela-round.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-varela-round.rb
+++ Formula/font-varela-round.rb
@@ -1,7 +1,7 @@
 class FontVarelaRound < Formula
   head "https://github.com/google/fonts/raw/master/ofl/varelaround/VarelaRound-Regular.ttf"
   desc "Varela Round"
-  homepage "https://www.google.com/fonts/specimen/Varela+Round"
+  homepage "https://fonts.google.com/specimen/Varela+Round"
   def install
     (share/"fonts").install "VarelaRound-Regular.ttf"
   end
