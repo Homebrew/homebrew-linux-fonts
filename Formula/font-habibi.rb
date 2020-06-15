5eb7520b3a0be5311e673ee465e824a0e2525450
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-habibi.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-habibi.rb
+++ Formula/font-habibi.rb
@@ -1,7 +1,7 @@
 class FontHabibi < Formula
   head "https://github.com/google/fonts/raw/master/ofl/habibi/Habibi-Regular.ttf"
   desc "Habibi"
-  homepage "https://www.google.com/fonts/specimen/Habibi"
+  homepage "https://fonts.google.com/specimen/Habibi"
   def install
     (share/"fonts").install "Habibi-Regular.ttf"
   end
