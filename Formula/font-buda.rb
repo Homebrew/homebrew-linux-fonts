531fb6d7817918b32a447d2d278f75bd7c486692
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-buda.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-buda.rb
+++ Formula/font-buda.rb
@@ -1,7 +1,7 @@
 class FontBuda < Formula
   head "https://github.com/google/fonts/raw/master/ofl/buda/Buda-Light.ttf"
   desc "Buda"
-  homepage "https://www.google.com/fonts/specimen/Buda"
+  homepage "https://fonts.google.com/specimen/Buda"
   def install
     (share/"fonts").install "Buda-Light.ttf"
   end
