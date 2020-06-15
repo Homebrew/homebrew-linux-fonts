49d0fe76fa04dee0d066f6037a48bab3b3e34166
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-englebert.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-englebert.rb
+++ Formula/font-englebert.rb
@@ -1,7 +1,7 @@
 class FontEnglebert < Formula
   head "https://github.com/google/fonts/raw/master/ofl/englebert/Englebert-Regular.ttf"
   desc "Englebert"
-  homepage "https://www.google.com/fonts/specimen/Englebert"
+  homepage "https://fonts.google.com/specimen/Englebert"
   def install
     (share/"fonts").install "Englebert-Regular.ttf"
   end
