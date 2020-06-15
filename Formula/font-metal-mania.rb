52b8ed7d06f540b547d4aacad83a0da498c8d139
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-metal-mania.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-metal-mania.rb
+++ Formula/font-metal-mania.rb
@@ -1,7 +1,7 @@
 class FontMetalMania < Formula
   head "https://github.com/google/fonts/raw/master/ofl/metalmania/MetalMania-Regular.ttf"
   desc "Metal Mania"
-  homepage "https://www.google.com/fonts/specimen/Metal+Mania"
+  homepage "https://fonts.google.com/specimen/Metal+Mania"
   def install
     (share/"fonts").install "MetalMania-Regular.ttf"
   end
