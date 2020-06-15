fd73f7dbb5bd4d8316b7d53849783ad7c3eeda87
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-londrina-sketch.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-londrina-sketch.rb
+++ Formula/font-londrina-sketch.rb
@@ -1,7 +1,7 @@
 class FontLondrinaSketch < Formula
   head "https://github.com/google/fonts/raw/master/ofl/londrinasketch/LondrinaSketch-Regular.ttf"
   desc "Londrina Sketch"
-  homepage "https://www.google.com/fonts/specimen/Londrina+Sketch"
+  homepage "https://fonts.google.com/specimen/Londrina+Sketch"
   def install
     (share/"fonts").install "LondrinaSketch-Regular.ttf"
   end
