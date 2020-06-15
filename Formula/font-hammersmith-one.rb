b1fee3e98154198da1bfd9b55b4d9e5a1e2021ea
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hammersmith-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-hammersmith-one.rb
+++ Formula/font-hammersmith-one.rb
@@ -1,7 +1,7 @@
 class FontHammersmithOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/hammersmithone/HammersmithOne-Regular.ttf"
   desc "Hammersmith One"
-  homepage "https://www.google.com/fonts/specimen/Hammersmith+One"
+  homepage "https://fonts.google.com/specimen/Hammersmith+One"
   def install
     (share/"fonts").install "HammersmithOne-Regular.ttf"
   end
