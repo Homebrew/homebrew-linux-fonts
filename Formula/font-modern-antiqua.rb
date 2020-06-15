4745778177609b4772b119e598c0113c2fffa532
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-modern-antiqua.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-modern-antiqua.rb
+++ Formula/font-modern-antiqua.rb
@@ -1,7 +1,7 @@
 class FontModernAntiqua < Formula
   head "https://github.com/google/fonts/raw/master/ofl/modernantiqua/ModernAntiqua-Regular.ttf"
   desc "Modern Antiqua"
-  homepage "https://www.google.com/fonts/specimen/Modern+Antiqua"
+  homepage "https://fonts.google.com/specimen/Modern+Antiqua"
   def install
     (share/"fonts").install "ModernAntiqua-Regular.ttf"
   end
