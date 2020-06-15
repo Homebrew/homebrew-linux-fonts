1f2212b73a25f8fd9a0992ea035e005fca8d1c17
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-exo.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-exo.rb
+++ Formula/font-exo.rb
@@ -1,26 +1,10 @@
 class FontExo < Formula
-  head "https://github.com/google/fonts/trunk/ofl/exo", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/exo", using: :svn, trust_cert: true
   desc "Exo"
-  homepage "https://www.google.com/fonts/specimen/Exo"
+  homepage "https://fonts.google.com/specimen/Exo"
   def install
-    (share/"fonts").install "Exo-Black.ttf"
-    (share/"fonts").install "Exo-BlackItalic.ttf"
-    (share/"fonts").install "Exo-Bold.ttf"
-    (share/"fonts").install "Exo-BoldItalic.ttf"
-    (share/"fonts").install "Exo-ExtraBold.ttf"
-    (share/"fonts").install "Exo-ExtraBoldItalic.ttf"
-    (share/"fonts").install "Exo-ExtraLight.ttf"
-    (share/"fonts").install "Exo-ExtraLightItalic.ttf"
-    (share/"fonts").install "Exo-Italic.ttf"
-    (share/"fonts").install "Exo-Light.ttf"
-    (share/"fonts").install "Exo-LightItalic.ttf"
-    (share/"fonts").install "Exo-Medium.ttf"
-    (share/"fonts").install "Exo-MediumItalic.ttf"
-    (share/"fonts").install "Exo-Regular.ttf"
-    (share/"fonts").install "Exo-SemiBold.ttf"
-    (share/"fonts").install "Exo-SemiBoldItalic.ttf"
-    (share/"fonts").install "Exo-Thin.ttf"
-    (share/"fonts").install "Exo-ThinItalic.ttf"
+    (share/"fonts").install "Exo-Italic[wght].ttf"
+    (share/"fonts").install "Exo[wght].ttf"
   end
   test do
   end
