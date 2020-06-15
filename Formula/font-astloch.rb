62ae63e5a162e7234196627139237775f1344a17
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-astloch.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-astloch.rb
+++ Formula/font-astloch.rb
@@ -1,7 +1,7 @@
 class FontAstloch < Formula
-  head "https://github.com/google/fonts/trunk/ofl/astloch", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/astloch", using: :svn, trust_cert: true
   desc "Astloch"
-  homepage "https://www.google.com/fonts/specimen/Astloch"
+  homepage "https://fonts.google.com/specimen/Astloch"
   def install
     (share/"fonts").install "Astloch-Bold.ttf"
     (share/"fonts").install "Astloch-Regular.ttf"
