aab4db6582b9e2864d80688943d406aff2863509
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-give-you-glory.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-give-you-glory.rb
+++ Formula/font-give-you-glory.rb
@@ -1,7 +1,7 @@
 class FontGiveYouGlory < Formula
   head "https://github.com/google/fonts/raw/master/ofl/giveyouglory/GiveYouGlory.ttf"
   desc "Give You Glory"
-  homepage "https://www.google.com/fonts/specimen/Give+You+Glory"
+  homepage "https://fonts.google.com/specimen/Give+You+Glory"
   def install
     (share/"fonts").install "GiveYouGlory.ttf"
   end
