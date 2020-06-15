a6a1d9041a839da1057868eb8c621996b9962d68
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monoton.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-monoton.rb
+++ Formula/font-monoton.rb
@@ -1,7 +1,7 @@
 class FontMonoton < Formula
   head "https://github.com/google/fonts/raw/master/ofl/monoton/Monoton-Regular.ttf"
   desc "Monoton"
-  homepage "https://www.google.com/fonts/specimen/Monoton"
+  homepage "https://fonts.google.com/specimen/Monoton"
   def install
     (share/"fonts").install "Monoton-Regular.ttf"
   end
