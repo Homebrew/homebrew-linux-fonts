ac76a6624c5ff0026704d8506d435e988f549000
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fresca.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-fresca.rb
+++ Formula/font-fresca.rb
@@ -1,7 +1,7 @@
 class FontFresca < Formula
   head "https://github.com/google/fonts/raw/master/ofl/fresca/Fresca-Regular.ttf"
   desc "Fresca"
-  homepage "https://www.google.com/fonts/specimen/Fresca"
+  homepage "https://fonts.google.com/specimen/Fresca"
   def install
     (share/"fonts").install "Fresca-Regular.ttf"
   end
