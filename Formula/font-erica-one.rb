3e325cfbf43500330614bbb4377fc972fb8c8f9d
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-erica-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-erica-one.rb
+++ Formula/font-erica-one.rb
@@ -1,7 +1,7 @@
 class FontEricaOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/ericaone/EricaOne-Regular.ttf"
   desc "Erica One"
-  homepage "https://www.google.com/fonts/specimen/Erica+One"
+  homepage "https://fonts.google.com/specimen/Erica+One"
   def install
     (share/"fonts").install "EricaOne-Regular.ttf"
   end
