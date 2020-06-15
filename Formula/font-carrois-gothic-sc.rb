33bf7631fcb818d194fa4ffc10d98702f39e71a6
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-carrois-gothic-sc.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-carrois-gothic-sc.rb
+++ Formula/font-carrois-gothic-sc.rb
@@ -1,7 +1,7 @@
 class FontCarroisGothicSc < Formula
   head "https://github.com/google/fonts/raw/master/ofl/carroisgothicsc/CarroisGothicSC-Regular.ttf"
   desc "Carrois Gothic SC"
-  homepage "https://www.google.com/fonts/specimen/Carrois+Gothic+SC"
+  homepage "https://fonts.google.com/specimen/Carrois+Gothic+SC"
   def install
     (share/"fonts").install "CarroisGothicSC-Regular.ttf"
   end
