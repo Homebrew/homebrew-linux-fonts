7c70ddf6cf97d33a72f79887f540cf6444fbe8eb
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cinzel.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-cinzel.rb
+++ Formula/font-cinzel.rb
@@ -1,7 +1,7 @@
 class FontCinzel < Formula
-  head "https://github.com/google/fonts/trunk/ofl/cinzel", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/cinzel", using: :svn, trust_cert: true
   desc "Cinzel"
-  homepage "https://www.google.com/fonts/specimen/Cinzel"
+  homepage "https://fonts.google.com/specimen/Cinzel"
   def install
     (share/"fonts").install "Cinzel-Black.ttf"
     (share/"fonts").install "Cinzel-Bold.ttf"
