8005aba941fa00aca074e9e0c982851fffe0cd74
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ribeye-marrow.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-ribeye-marrow.rb
+++ Formula/font-ribeye-marrow.rb
@@ -1,7 +1,7 @@
 class FontRibeyeMarrow < Formula
   head "https://github.com/google/fonts/raw/master/ofl/ribeyemarrow/RibeyeMarrow-Regular.ttf"
   desc "Ribeye Marrow"
-  homepage "https://www.google.com/fonts/specimen/Ribeye+Marrow"
+  homepage "https://fonts.google.com/specimen/Ribeye+Marrow"
   def install
     (share/"fonts").install "RibeyeMarrow-Regular.ttf"
   end
