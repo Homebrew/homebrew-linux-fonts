e3c86b4f169865060abc2438810cc7af2222c23b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-delius-swash-caps.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-delius-swash-caps.rb
+++ Formula/font-delius-swash-caps.rb
@@ -1,7 +1,7 @@
 class FontDeliusSwashCaps < Formula
   head "https://github.com/google/fonts/raw/master/ofl/deliusswashcaps/DeliusSwashCaps-Regular.ttf"
   desc "Delius Swash Caps"
-  homepage "https://www.google.com/fonts/specimen/Delius+Swash+Caps"
+  homepage "https://fonts.google.com/specimen/Delius+Swash+Caps"
   def install
     (share/"fonts").install "DeliusSwashCaps-Regular.ttf"
   end
