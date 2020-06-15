507893bec413fe7cd491bae093affdea69554c25
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-geo.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-geo.rb
+++ Formula/font-geo.rb
@@ -1,7 +1,7 @@
 class FontGeo < Formula
-  head "https://github.com/google/fonts/trunk/ofl/geo", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/geo", using: :svn, trust_cert: true
   desc "Geo"
-  homepage "https://www.google.com/fonts/specimen/Geo"
+  homepage "https://fonts.google.com/specimen/Geo"
   def install
     (share/"fonts").install "Geo-Oblique.ttf"
     (share/"fonts").install "Geo-Regular.ttf"
