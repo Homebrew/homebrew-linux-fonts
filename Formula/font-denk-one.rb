7b1b28b23abb3a0aef948f24f3b013aea26cb11a
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-denk-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-denk-one.rb
+++ Formula/font-denk-one.rb
@@ -1,7 +1,7 @@
 class FontDenkOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/denkone/DenkOne-Regular.ttf"
   desc "Denk One"
-  homepage "https://www.google.com/fonts/specimen/Denk+One"
+  homepage "https://fonts.google.com/specimen/Denk+One"
   def install
     (share/"fonts").install "DenkOne-Regular.ttf"
   end
