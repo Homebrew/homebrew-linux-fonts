c2b0a50ae7ddd0e690e15b35de1894a779e43078
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-overlock-sc.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-overlock-sc.rb
+++ Formula/font-overlock-sc.rb
@@ -1,7 +1,7 @@
 class FontOverlockSc < Formula
   head "https://github.com/google/fonts/raw/master/ofl/overlocksc/OverlockSC-Regular.ttf"
   desc "Overlock SC"
-  homepage "https://www.google.com/fonts/specimen/Overlock+SC"
+  homepage "https://fonts.google.com/specimen/Overlock+SC"
   def install
     (share/"fonts").install "OverlockSC-Regular.ttf"
   end
