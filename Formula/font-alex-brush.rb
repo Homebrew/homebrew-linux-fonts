64de64a7fba3602f5b7d7ad8a99c80a32bff8d1d
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alex-brush.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-alex-brush.rb
+++ Formula/font-alex-brush.rb
@@ -1,7 +1,7 @@
 class FontAlexBrush < Formula
   head "https://github.com/google/fonts/raw/master/ofl/alexbrush/AlexBrush-Regular.ttf"
   desc "Alex Brush"
-  homepage "https://www.google.com/fonts/specimen/Alex+Brush"
+  homepage "https://fonts.google.com/specimen/Alex+Brush"
   def install
     (share/"fonts").install "AlexBrush-Regular.ttf"
   end
