472eca5096ac6ff7e598d24581a47b9d0e1f512b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-share.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-share.rb
+++ Formula/font-share.rb
@@ -1,7 +1,7 @@
 class FontShare < Formula
-  head "https://github.com/google/fonts/trunk/ofl/share", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/share", using: :svn, trust_cert: true
   desc "Share"
-  homepage "https://www.google.com/fonts/specimen/Share"
+  homepage "https://fonts.google.com/specimen/Share"
   def install
     (share/"fonts").install "Share-Bold.ttf"
     (share/"fonts").install "Share-BoldItalic.ttf"
