567a91829a36e5b190d12d15a8f5c652283c2025
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-krona-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-krona-one.rb
+++ Formula/font-krona-one.rb
@@ -1,7 +1,7 @@
 class FontKronaOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/kronaone/KronaOne-Regular.ttf"
   desc "Krona One"
-  homepage "https://www.google.com/fonts/specimen/Krona+One"
+  homepage "https://fonts.google.com/specimen/Krona+One"
   def install
     (share/"fonts").install "KronaOne-Regular.ttf"
   end
