f6e232c24c9692d1895bc5c7bbc2562629298702
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-abel.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-abel.rb
+++ Formula/font-abel.rb
@@ -1,7 +1,7 @@
 class FontAbel < Formula
   head "https://github.com/google/fonts/raw/master/ofl/abel/Abel-Regular.ttf"
   desc "Abel"
-  homepage "https://www.google.com/fonts/specimen/Abel"
+  homepage "https://fonts.google.com/specimen/Abel"
   def install
     (share/"fonts").install "Abel-Regular.ttf"
   end
