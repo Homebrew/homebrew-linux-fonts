7faef5b8aa9882e985d7edda330bf8e3a7e8d89e
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mrs-saint-delafield.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-mrs-saint-delafield.rb
+++ Formula/font-mrs-saint-delafield.rb
@@ -1,7 +1,7 @@
 class FontMrsSaintDelafield < Formula
   head "https://github.com/google/fonts/raw/master/ofl/mrssaintdelafield/MrsSaintDelafield-Regular.ttf"
   desc "Mrs Saint Delafield"
-  homepage "https://www.google.com/fonts/specimen/Mrs+Saint+Delafield"
+  homepage "https://fonts.google.com/specimen/Mrs+Saint+Delafield"
   def install
     (share/"fonts").install "MrsSaintDelafield-Regular.ttf"
   end
