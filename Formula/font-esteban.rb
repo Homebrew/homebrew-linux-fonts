9dd322cd99e66094ebacf4e8115877f32d945e6f
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-esteban.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-esteban.rb
+++ Formula/font-esteban.rb
@@ -1,7 +1,7 @@
 class FontEsteban < Formula
   head "https://github.com/google/fonts/raw/master/ofl/esteban/Esteban-Regular.ttf"
   desc "Esteban"
-  homepage "https://www.google.com/fonts/specimen/Esteban"
+  homepage "https://fonts.google.com/specimen/Esteban"
   def install
     (share/"fonts").install "Esteban-Regular.ttf"
   end
