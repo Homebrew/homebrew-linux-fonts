a7d56cdbfe76c636dd0da7948df549410a13cfe2
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nova-round.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-nova-round.rb
+++ Formula/font-nova-round.rb
@@ -1,7 +1,7 @@
 class FontNovaRound < Formula
   head "https://github.com/google/fonts/raw/master/ofl/novaround/NovaRound.ttf"
   desc "Nova Round"
-  homepage "https://www.google.com/fonts/specimen/Nova%20Round"
+  homepage "https://fonts.google.com/specimen/Nova+Round"
   def install
     (share/"fonts").install "NovaRound.ttf"
   end
