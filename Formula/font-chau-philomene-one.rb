28d33d2bc8d0d35284e228243ff9a0b459e838bb
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chau-philomene-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-chau-philomene-one.rb
+++ Formula/font-chau-philomene-one.rb
@@ -1,7 +1,7 @@
 class FontChauPhilomeneOne < Formula
-  head "https://github.com/google/fonts/trunk/ofl/chauphilomeneone", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/chauphilomeneone", using: :svn, trust_cert: true
   desc "Chau Philomene One"
-  homepage "https://www.google.com/fonts/specimen/Chau%20Philomene%20One"
+  homepage "https://fonts.google.com/specimen/Chau+Philomene+One"
   def install
     (share/"fonts").install "ChauPhilomeneOne-Italic.ttf"
     (share/"fonts").install "ChauPhilomeneOne-Regular.ttf"
