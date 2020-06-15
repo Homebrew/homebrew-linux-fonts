9a26ffa28953f09a9deee8a5ae1e8ee96211b056
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-archivo-narrow.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-archivo-narrow.rb
+++ Formula/font-archivo-narrow.rb
@@ -1,12 +1,10 @@
 class FontArchivoNarrow < Formula
-  head "https://github.com/google/fonts/trunk/ofl/archivonarrow", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/archivonarrow", using: :svn, trust_cert: true
   desc "Archivo Narrow"
-  homepage "https://www.google.com/fonts/specimen/Archivo%20Narrow"
+  homepage "https://fonts.google.com/specimen/Archivo+Narrow"
   def install
-    (share/"fonts").install "ArchivoNarrow-Bold.ttf"
-    (share/"fonts").install "ArchivoNarrow-BoldItalic.ttf"
-    (share/"fonts").install "ArchivoNarrow-Italic.ttf"
-    (share/"fonts").install "ArchivoNarrow-Regular.ttf"
+    (share/"fonts").install "ArchivoNarrow-Italic[wght].ttf"
+    (share/"fonts").install "ArchivoNarrow[wght].ttf"
   end
   test do
   end
