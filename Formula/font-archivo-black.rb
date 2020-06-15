ade7b5eb39f9c586dd73c8863a998530176f5005
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-archivo-black.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-archivo-black.rb
+++ Formula/font-archivo-black.rb
@@ -1,7 +1,7 @@
 class FontArchivoBlack < Formula
   head "https://github.com/google/fonts/raw/master/ofl/archivoblack/ArchivoBlack-Regular.ttf"
   desc "Archivo Black"
-  homepage "https://www.google.com/fonts/specimen/Archivo+Black"
+  homepage "https://fonts.google.com/specimen/Archivo+Black"
   def install
     (share/"fonts").install "ArchivoBlack-Regular.ttf"
   end
