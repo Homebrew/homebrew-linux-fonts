d094a36bed49e1fd685aef7a18399c5b43e55c85
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chicle.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-chicle.rb
+++ Formula/font-chicle.rb
@@ -1,7 +1,7 @@
 class FontChicle < Formula
   head "https://github.com/google/fonts/raw/master/ofl/chicle/Chicle-Regular.ttf"
   desc "Chicle"
-  homepage "https://www.google.com/fonts/specimen/Chicle"
+  homepage "https://fonts.google.com/specimen/Chicle"
   def install
     (share/"fonts").install "Chicle-Regular.ttf"
   end
