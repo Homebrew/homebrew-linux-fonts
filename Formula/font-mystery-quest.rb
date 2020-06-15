bacb5d85e8fe5d118aea0a12f6fab96d68185650
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mystery-quest.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-mystery-quest.rb
+++ Formula/font-mystery-quest.rb
@@ -1,7 +1,7 @@
 class FontMysteryQuest < Formula
   head "https://github.com/google/fonts/raw/master/ofl/mysteryquest/MysteryQuest-Regular.ttf"
   desc "Mystery Quest"
-  homepage "https://www.google.com/fonts/specimen/Mystery+Quest"
+  homepage "https://fonts.google.com/specimen/Mystery+Quest"
   def install
     (share/"fonts").install "MysteryQuest-Regular.ttf"
   end
