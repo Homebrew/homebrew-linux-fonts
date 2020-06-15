b64e1d8eda41480217911542873cd5e3c33ef708
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alfa-slab-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-alfa-slab-one.rb
+++ Formula/font-alfa-slab-one.rb
@@ -1,7 +1,7 @@
 class FontAlfaSlabOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/alfaslabone/AlfaSlabOne-Regular.ttf"
   desc "Alfa Slab One"
-  homepage "https://www.google.com/fonts/specimen/Alfa+Slab+One"
+  homepage "https://fonts.google.com/specimen/Alfa+Slab+One"
   def install
     (share/"fonts").install "AlfaSlabOne-Regular.ttf"
   end
