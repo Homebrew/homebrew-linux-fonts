c4586b00caf97b7d6def24f8a88e304723e45ba2
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quattrocento.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-quattrocento.rb
+++ Formula/font-quattrocento.rb
@@ -1,7 +1,7 @@
 class FontQuattrocento < Formula
-  head "https://github.com/google/fonts/trunk/ofl/quattrocento", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/quattrocento", using: :svn, trust_cert: true
   desc "Quattrocento"
-  homepage "https://www.google.com/fonts/specimen/Quattrocento"
+  homepage "https://fonts.google.com/specimen/Quattrocento"
   def install
     (share/"fonts").install "Quattrocento-Bold.ttf"
     (share/"fonts").install "Quattrocento-Regular.ttf"
