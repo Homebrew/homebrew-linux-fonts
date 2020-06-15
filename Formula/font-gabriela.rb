50f916a2a12a8aae752d694da717e17841c6e19a
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gabriela.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-gabriela.rb
+++ Formula/font-gabriela.rb
@@ -1,7 +1,7 @@
 class FontGabriela < Formula
   head "https://github.com/google/fonts/raw/master/ofl/gabriela/Gabriela-Regular.ttf"
   desc "Gabriela"
-  homepage "https://www.google.com/fonts/specimen/Gabriela"
+  homepage "https://fonts.google.com/specimen/Gabriela"
   def install
     (share/"fonts").install "Gabriela-Regular.ttf"
   end
