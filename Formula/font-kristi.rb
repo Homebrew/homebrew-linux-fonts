0dab1f73adacafbbaefe263a25ca426c806e40be
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kristi.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-kristi.rb
+++ Formula/font-kristi.rb
@@ -1,7 +1,7 @@
 class FontKristi < Formula
   head "https://github.com/google/fonts/raw/master/ofl/kristi/Kristi-Regular.ttf"
   desc "Kristi"
-  homepage "https://www.google.com/fonts/specimen/Kristi"
+  homepage "https://fonts.google.com/specimen/Kristi"
   def install
     (share/"fonts").install "Kristi-Regular.ttf"
   end
