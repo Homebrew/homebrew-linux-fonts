6d0105caaae04a9c557f0e33d3e066cfa9a0df8f
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-carme.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-carme.rb
+++ Formula/font-carme.rb
@@ -1,7 +1,7 @@
 class FontCarme < Formula
   head "https://github.com/google/fonts/raw/master/ofl/carme/Carme-Regular.ttf"
   desc "Carme"
-  homepage "https://www.google.com/fonts/specimen/Carme"
+  homepage "https://fonts.google.com/specimen/Carme"
   def install
     (share/"fonts").install "Carme-Regular.ttf"
   end
