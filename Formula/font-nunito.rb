cf5ba3b5887cac2b11135eb93fa14ce66d4b5967
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nunito.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-nunito.rb
+++ Formula/font-nunito.rb
@@ -1,11 +1,22 @@
 class FontNunito < Formula
-  head "https://github.com/google/fonts/trunk/ofl/nunito", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/nunito", using: :svn, trust_cert: true
   desc "Nunito"
-  homepage "https://www.google.com/fonts/specimen/Nunito"
+  homepage "https://fonts.google.com/specimen/Nunito"
   def install
+    (share/"fonts").install "Nunito-Black.ttf"
+    (share/"fonts").install "Nunito-BlackItalic.ttf"
     (share/"fonts").install "Nunito-Bold.ttf"
+    (share/"fonts").install "Nunito-BoldItalic.ttf"
+    (share/"fonts").install "Nunito-ExtraBold.ttf"
+    (share/"fonts").install "Nunito-ExtraBoldItalic.ttf"
+    (share/"fonts").install "Nunito-ExtraLight.ttf"
+    (share/"fonts").install "Nunito-ExtraLightItalic.ttf"
+    (share/"fonts").install "Nunito-Italic.ttf"
     (share/"fonts").install "Nunito-Light.ttf"
+    (share/"fonts").install "Nunito-LightItalic.ttf"
     (share/"fonts").install "Nunito-Regular.ttf"
+    (share/"fonts").install "Nunito-SemiBold.ttf"
+    (share/"fonts").install "Nunito-SemiBoldItalic.ttf"
   end
   test do
   end
