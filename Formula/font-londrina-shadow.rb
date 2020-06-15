5d664cab9230d27106f6817c995be5543d5b0ab8
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-londrina-shadow.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-londrina-shadow.rb
+++ Formula/font-londrina-shadow.rb
@@ -1,7 +1,7 @@
 class FontLondrinaShadow < Formula
   head "https://github.com/google/fonts/raw/master/ofl/londrinashadow/LondrinaShadow-Regular.ttf"
   desc "Londrina Shadow"
-  homepage "https://www.google.com/fonts/specimen/Londrina+Shadow"
+  homepage "https://fonts.google.com/specimen/Londrina+Shadow"
   def install
     (share/"fonts").install "LondrinaShadow-Regular.ttf"
   end
