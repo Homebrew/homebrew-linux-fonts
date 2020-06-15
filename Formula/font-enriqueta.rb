c33289fa0a56a86a739441a2444fea3451862ccf
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-enriqueta.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-enriqueta.rb
+++ Formula/font-enriqueta.rb
@@ -1,10 +1,12 @@
 class FontEnriqueta < Formula
-  head "https://github.com/google/fonts/trunk/ofl/enriqueta", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/enriqueta", using: :svn, trust_cert: true
   desc "Enriqueta"
-  homepage "https://www.google.com/fonts/specimen/Enriqueta"
+  homepage "https://fonts.google.com/specimen/Enriqueta"
   def install
     (share/"fonts").install "Enriqueta-Bold.ttf"
+    (share/"fonts").install "Enriqueta-Medium.ttf"
     (share/"fonts").install "Enriqueta-Regular.ttf"
+    (share/"fonts").install "Enriqueta-SemiBold.ttf"
   end
   test do
   end
