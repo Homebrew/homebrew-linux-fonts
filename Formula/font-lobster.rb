3c6636244cf50d2efde0234dbe04d9e9117cc1c5
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lobster.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-lobster.rb
+++ Formula/font-lobster.rb
@@ -1,7 +1,7 @@
 class FontLobster < Formula
   head "https://github.com/google/fonts/raw/master/ofl/lobster/Lobster-Regular.ttf"
   desc "Lobster"
-  homepage "https://www.google.com/fonts/specimen/Lobster"
+  homepage "https://fonts.google.com/specimen/Lobster"
   def install
     (share/"fonts").install "Lobster-Regular.ttf"
   end
