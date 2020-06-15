87209c1341e92cc39d1213d016be155e9e233503
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aladin.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-aladin.rb
+++ Formula/font-aladin.rb
@@ -1,7 +1,7 @@
 class FontAladin < Formula
   head "https://github.com/google/fonts/raw/master/ofl/aladin/Aladin-Regular.ttf"
   desc "Aladin"
-  homepage "https://www.google.com/fonts/specimen/Aladin"
+  homepage "https://fonts.google.com/specimen/Aladin"
   def install
     (share/"fonts").install "Aladin-Regular.ttf"
   end
