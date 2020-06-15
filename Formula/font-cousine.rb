60dc8e685d0dd19c8b6c0ba16460ff6a7007ca98
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cousine.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-cousine.rb
+++ Formula/font-cousine.rb
@@ -1,12 +1,12 @@
 class FontCousine < Formula
-  head "https://github.com/google/fonts/trunk/apache/cousine", using: :svn, revision: "47", trust_cert: true
+  head "https://github.com/google/fonts/trunk/apache/cousine", using: :svn, trust_cert: true
   desc "Cousine"
-  homepage "https://www.google.com/fonts/specimen/Cousine"
+  homepage "https://fonts.google.com/specimen/Cousine"
   def install
-    (share/"fonts").install "Cousine-Regular.ttf"
     (share/"fonts").install "Cousine-Bold.ttf"
     (share/"fonts").install "Cousine-BoldItalic.ttf"
     (share/"fonts").install "Cousine-Italic.ttf"
+    (share/"fonts").install "Cousine-Regular.ttf"
   end
   test do
   end
