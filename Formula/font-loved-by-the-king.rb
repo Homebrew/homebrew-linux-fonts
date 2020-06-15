58935078f6e44b1372f2892180455d5172059d5f
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-loved-by-the-king.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-loved-by-the-king.rb
+++ Formula/font-loved-by-the-king.rb
@@ -1,7 +1,7 @@
 class FontLovedByTheKing < Formula
   head "https://github.com/google/fonts/raw/master/ofl/lovedbytheking/LovedbytheKing.ttf"
   desc "Loved by the King"
-  homepage "https://www.google.com/fonts/specimen/Loved+by+the+King"
+  homepage "https://fonts.google.com/specimen/Loved+by+the+King"
   def install
     (share/"fonts").install "LovedbytheKing.ttf"
   end
