c5dc3283c44fe01f32c88ee317a8bb2b0a50f42e
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-buenard.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-buenard.rb
+++ Formula/font-buenard.rb
@@ -1,7 +1,7 @@
 class FontBuenard < Formula
-  head "https://github.com/google/fonts/trunk/ofl/buenard", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/buenard", using: :svn, trust_cert: true
   desc "Buenard"
-  homepage "https://www.google.com/fonts/specimen/Buenard"
+  homepage "https://fonts.google.com/specimen/Buenard"
   def install
     (share/"fonts").install "Buenard-Bold.ttf"
     (share/"fonts").install "Buenard-Regular.ttf"
