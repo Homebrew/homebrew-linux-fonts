a1fa882b2da4cd0f868aeb8ba329e71aedaeff3d
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mouse-memoirs.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-mouse-memoirs.rb
+++ Formula/font-mouse-memoirs.rb
@@ -1,7 +1,7 @@
 class FontMouseMemoirs < Formula
   head "https://github.com/google/fonts/raw/master/ofl/mousememoirs/MouseMemoirs-Regular.ttf"
   desc "Mouse Memoirs"
-  homepage "https://www.google.com/fonts/specimen/Mouse+Memoirs"
+  homepage "https://fonts.google.com/specimen/Mouse+Memoirs"
   def install
     (share/"fonts").install "MouseMemoirs-Regular.ttf"
   end
