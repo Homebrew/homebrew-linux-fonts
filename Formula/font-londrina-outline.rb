419132c19f00e0049813fe71e7b916a0bf195a20
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-londrina-outline.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-londrina-outline.rb
+++ Formula/font-londrina-outline.rb
@@ -1,7 +1,7 @@
 class FontLondrinaOutline < Formula
   head "https://github.com/google/fonts/raw/master/ofl/londrinaoutline/LondrinaOutline-Regular.ttf"
   desc "Londrina Outline"
-  homepage "https://www.google.com/fonts/specimen/Londrina+Outline"
+  homepage "https://fonts.google.com/specimen/Londrina+Outline"
   def install
     (share/"fonts").install "LondrinaOutline-Regular.ttf"
   end
