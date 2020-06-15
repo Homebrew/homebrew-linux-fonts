1886ab4c804a6b943039a98eff8fcd9ebfae8d1f
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marcellus-sc.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-marcellus-sc.rb
+++ Formula/font-marcellus-sc.rb
@@ -1,7 +1,7 @@
 class FontMarcellusSc < Formula
   head "https://github.com/google/fonts/raw/master/ofl/marcellussc/MarcellusSC-Regular.ttf"
   desc "Marcellus SC"
-  homepage "https://www.google.com/fonts/specimen/Marcellus+SC"
+  homepage "https://fonts.google.com/specimen/Marcellus+SC"
   def install
     (share/"fonts").install "MarcellusSC-Regular.ttf"
   end
