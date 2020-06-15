6cbf8c1237748e9f92c098da25a37bc974727285
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jacques-francois.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-jacques-francois.rb
+++ Formula/font-jacques-francois.rb
@@ -1,7 +1,7 @@
 class FontJacquesFrancois < Formula
   head "https://github.com/google/fonts/raw/master/ofl/jacquesfrancois/JacquesFrancois-Regular.ttf"
   desc "Jacques Francois"
-  homepage "https://www.google.com/fonts/specimen/Jacques+Francois"
+  homepage "https://fonts.google.com/specimen/Jacques+Francois"
   def install
     (share/"fonts").install "JacquesFrancois-Regular.ttf"
   end
