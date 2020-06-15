667b1de926b8d867414d9ac8c17fd0119f727405
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-khmer.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-khmer.rb
+++ Formula/font-khmer.rb
@@ -1,7 +1,7 @@
 class FontKhmer < Formula
   head "https://github.com/google/fonts/raw/master/ofl/khmer/Khmer.ttf"
   desc "Khmer"
-  homepage "https://www.google.com/fonts/specimen/Khmer"
+  homepage "https://fonts.google.com/specimen/Khmer"
   def install
     (share/"fonts").install "Khmer.ttf"
   end
