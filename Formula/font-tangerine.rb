ae1535b5b7d4eae2e3ca75ff8211b8ec6bb08310
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tangerine.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-tangerine.rb
+++ Formula/font-tangerine.rb
@@ -1,10 +1,10 @@
 class FontTangerine < Formula
-  head "https://github.com/google/fonts/trunk/ofl/tangerine", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/tangerine", using: :svn, trust_cert: true
   desc "Tangerine"
-  homepage "https://www.google.com/fonts/specimen/Tangerine"
+  homepage "https://fonts.google.com/specimen/Tangerine"
   def install
-    (share/"fonts").install "Tangerine_Bold.ttf"
-    (share/"fonts").install "Tangerine_Regular.ttf"
+    (share/"fonts").install "Tangerine-Bold.ttf"
+    (share/"fonts").install "Tangerine-Regular.ttf"
   end
   test do
   end
