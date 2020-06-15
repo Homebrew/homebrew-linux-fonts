c78dac98f208f959eb7212ad20b60f7ea3b83605
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iceberg.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-iceberg.rb
+++ Formula/font-iceberg.rb
@@ -1,7 +1,7 @@
 class FontIceberg < Formula
   head "https://github.com/google/fonts/raw/master/ofl/iceberg/Iceberg-Regular.ttf"
   desc "Iceberg"
-  homepage "https://www.google.com/fonts/specimen/Iceberg"
+  homepage "https://fonts.google.com/specimen/Iceberg"
   def install
     (share/"fonts").install "Iceberg-Regular.ttf"
   end
