ff494a8d1b9aad1dadf53606c42274109c63f0b6
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-germania-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-germania-one.rb
+++ Formula/font-germania-one.rb
@@ -1,7 +1,7 @@
 class FontGermaniaOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/germaniaone/GermaniaOne-Regular.ttf"
   desc "Germania One"
-  homepage "https://www.google.com/fonts/specimen/Germania+One"
+  homepage "https://fonts.google.com/specimen/Germania+One"
   def install
     (share/"fonts").install "GermaniaOne-Regular.ttf"
   end
