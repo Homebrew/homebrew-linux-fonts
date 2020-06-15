0e4c555b4267fe1520039625b437676a0b0dd4aa
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bentham.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-bentham.rb
+++ Formula/font-bentham.rb
@@ -1,7 +1,7 @@
 class FontBentham < Formula
   head "https://github.com/google/fonts/raw/master/ofl/bentham/Bentham-Regular.ttf"
   desc "Bentham"
-  homepage "https://www.google.com/fonts/specimen/Bentham"
+  homepage "https://fonts.google.com/specimen/Bentham"
   def install
     (share/"fonts").install "Bentham-Regular.ttf"
   end
