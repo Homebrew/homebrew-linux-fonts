41594ad12f6e00050eba82ec821747384c10c0c6
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-londrina-solid.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-londrina-solid.rb
+++ Formula/font-londrina-solid.rb
@@ -1,9 +1,12 @@
 class FontLondrinaSolid < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/londrinasolid/LondrinaSolid-Regular.ttf"
+  head "https://github.com/google/fonts/trunk/ofl/londrinasolid", using: :svn, trust_cert: true
   desc "Londrina Solid"
-  homepage "https://www.google.com/fonts/specimen/Londrina+Solid"
+  homepage "https://fonts.google.com/specimen/Londrina+Solid"
   def install
+    (share/"fonts").install "LondrinaSolid-Black.ttf"
+    (share/"fonts").install "LondrinaSolid-Light.ttf"
     (share/"fonts").install "LondrinaSolid-Regular.ttf"
+    (share/"fonts").install "LondrinaSolid-Thin.ttf"
   end
   test do
   end
