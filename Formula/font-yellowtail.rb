424c1bb735a8b20fb1db106d7c82835f0b7e17aa
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-yellowtail.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-yellowtail.rb
+++ Formula/font-yellowtail.rb
@@ -1,7 +1,7 @@
 class FontYellowtail < Formula
   head "https://github.com/google/fonts/raw/master/apache/yellowtail/Yellowtail-Regular.ttf"
   desc "Yellowtail"
-  homepage "https://www.google.com/fonts/specimen/Yellowtail"
+  homepage "https://fonts.google.com/specimen/Yellowtail"
   def install
     (share/"fonts").install "Yellowtail-Regular.ttf"
   end
