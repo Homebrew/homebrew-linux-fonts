88d3974a72284765633b2a309562bbe2fa34f017
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bevan.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-bevan.rb
+++ Formula/font-bevan.rb
@@ -1,7 +1,7 @@
 class FontBevan < Formula
   head "https://github.com/google/fonts/raw/master/ofl/bevan/Bevan-Regular.ttf"
   desc "Bevan"
-  homepage "https://www.google.com/fonts/specimen/Bevan"
+  homepage "https://fonts.google.com/specimen/Bevan"
   def install
     (share/"fonts").install "Bevan-Regular.ttf"
   end
