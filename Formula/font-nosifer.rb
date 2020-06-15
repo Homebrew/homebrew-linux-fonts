b2461911f47ba96a62811981329a38fec00e1776
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nosifer.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-nosifer.rb
+++ Formula/font-nosifer.rb
@@ -1,7 +1,7 @@
 class FontNosifer < Formula
   head "https://github.com/google/fonts/raw/master/ofl/nosifer/Nosifer-Regular.ttf"
   desc "Nosifer"
-  homepage "https://www.google.com/fonts/specimen/Nosifer"
+  homepage "https://fonts.google.com/specimen/Nosifer"
   def install
     (share/"fonts").install "Nosifer-Regular.ttf"
   end
