b974efe1448d5e0ba1753d9e9adbbc8f5e0b88b1
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-petit-formal-script.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-petit-formal-script.rb
+++ Formula/font-petit-formal-script.rb
@@ -1,7 +1,7 @@
 class FontPetitFormalScript < Formula
   head "https://github.com/google/fonts/raw/master/ofl/petitformalscript/PetitFormalScript-Regular.ttf"
   desc "Petit Formal Script"
-  homepage "https://www.google.com/fonts/specimen/Petit+Formal+Script"
+  homepage "https://fonts.google.com/specimen/Petit+Formal+Script"
   def install
     (share/"fonts").install "PetitFormalScript-Regular.ttf"
   end
