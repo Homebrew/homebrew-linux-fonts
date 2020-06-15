762778ba6b8c5f2d34315093510d5b5fde7810df
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marmelad.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-marmelad.rb
+++ Formula/font-marmelad.rb
@@ -1,7 +1,7 @@
 class FontMarmelad < Formula
   head "https://github.com/google/fonts/raw/master/ofl/marmelad/Marmelad-Regular.ttf"
   desc "Marmelad"
-  homepage "https://www.google.com/fonts/specimen/Marmelad"
+  homepage "https://fonts.google.com/specimen/Marmelad"
   def install
     (share/"fonts").install "Marmelad-Regular.ttf"
   end
