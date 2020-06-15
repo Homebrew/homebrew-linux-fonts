88e4daca91ebf2bc172e4e3d7bc1e62fb18f4a63
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gloria-hallelujah.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-gloria-hallelujah.rb
+++ Formula/font-gloria-hallelujah.rb
@@ -1,7 +1,7 @@
 class FontGloriaHallelujah < Formula
   head "https://github.com/google/fonts/raw/master/ofl/gloriahallelujah/GloriaHallelujah.ttf"
   desc "Gloria Hallelujah"
-  homepage "https://www.google.com/fonts/specimen/Gloria+Hallelujah"
+  homepage "https://fonts.google.com/specimen/Gloria+Hallelujah"
   def install
     (share/"fonts").install "GloriaHallelujah.ttf"
   end
