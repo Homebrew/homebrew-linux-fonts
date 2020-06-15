79e684e90581401414571e8012f8c1ff5ec2b425
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-metal.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-metal.rb
+++ Formula/font-metal.rb
@@ -1,7 +1,7 @@
 class FontMetal < Formula
   head "https://github.com/google/fonts/raw/master/ofl/metal/Metal.ttf"
   desc "Metal"
-  homepage "https://www.google.com/fonts/specimen/Metal"
+  homepage "https://fonts.google.com/specimen/Metal"
   def install
     (share/"fonts").install "Metal.ttf"
   end
