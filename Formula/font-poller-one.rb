a91e47f14144dd44d1c0b0afcd874a125d5fadb5
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-poller-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-poller-one.rb
+++ Formula/font-poller-one.rb
@@ -1,7 +1,7 @@
 class FontPollerOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/pollerone/PollerOne.ttf"
   desc "Poller One"
-  homepage "https://www.google.com/fonts/specimen/Poller+One"
+  homepage "https://fonts.google.com/specimen/Poller+One"
   def install
     (share/"fonts").install "PollerOne.ttf"
   end
