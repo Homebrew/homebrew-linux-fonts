55f70dda2680a08fb6c0103a69a5a35fc3031ddc
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-josefin-sans.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-josefin-sans.rb
+++ Formula/font-josefin-sans.rb
@@ -1,18 +1,10 @@
 class FontJosefinSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/josefinsans", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/josefinsans", using: :svn, trust_cert: true
   desc "Josefin Sans"
-  homepage "https://www.google.com/fonts/specimen/Josefin%20Sans"
+  homepage "https://fonts.google.com/specimen/Josefin+Sans"
   def install
-    (share/"fonts").install "JosefinSans-Bold.ttf"
-    (share/"fonts").install "JosefinSans-BoldItalic.ttf"
-    (share/"fonts").install "JosefinSans-Italic.ttf"
-    (share/"fonts").install "JosefinSans-Light.ttf"
-    (share/"fonts").install "JosefinSans-LightItalic.ttf"
-    (share/"fonts").install "JosefinSans-Regular.ttf"
-    (share/"fonts").install "JosefinSans-SemiBold.ttf"
-    (share/"fonts").install "JosefinSans-SemiBoldItalic.ttf"
-    (share/"fonts").install "JosefinSans-Thin.ttf"
-    (share/"fonts").install "JosefinSans-ThinItalic.ttf"
+    (share/"fonts").install "JosefinSans-Italic[wght].ttf"
+    (share/"fonts").install "JosefinSans[wght].ttf"
   end
   test do
   end
