aecb5a2878ac4deb31671d0320b12e48041da499
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-herr-von-muellerhoff.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-herr-von-muellerhoff.rb
+++ Formula/font-herr-von-muellerhoff.rb
@@ -1,7 +1,7 @@
 class FontHerrVonMuellerhoff < Formula
   head "https://github.com/google/fonts/raw/master/ofl/herrvonmuellerhoff/HerrVonMuellerhoff-Regular.ttf"
   desc "Herr Von Muellerhoff"
-  homepage "https://www.google.com/fonts/specimen/Herr+Von+Muellerhoff"
+  homepage "https://fonts.google.com/specimen/Herr+Von+Muellerhoff"
   def install
     (share/"fonts").install "HerrVonMuellerhoff-Regular.ttf"
   end
