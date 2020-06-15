fac2076ceb7d6f3ecd2574af0f2d5c24579e50da
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fanwood-text.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-fanwood-text.rb
+++ Formula/font-fanwood-text.rb
@@ -1,7 +1,7 @@
 class FontFanwoodText < Formula
-  head "https://github.com/google/fonts/trunk/ofl/fanwoodtext", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/fanwoodtext", using: :svn, trust_cert: true
   desc "Fanwood Text"
-  homepage "https://www.google.com/fonts/specimen/Fanwood%20Text"
+  homepage "https://fonts.google.com/specimen/Fanwood+Text"
   def install
     (share/"fonts").install "FanwoodText-Italic.ttf"
     (share/"fonts").install "FanwoodText-Regular.ttf"
