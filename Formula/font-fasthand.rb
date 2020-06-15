59dadc6f4169131ad481093e60fbe2911c84bed8
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fasthand.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-fasthand.rb
+++ Formula/font-fasthand.rb
@@ -1,7 +1,7 @@
 class FontFasthand < Formula
   head "https://github.com/google/fonts/raw/master/ofl/fasthand/Fasthand-Regular.ttf"
   desc "Fasthand"
-  homepage "https://www.google.com/fonts/specimen/Fasthand"
+  homepage "https://fonts.google.com/specimen/Fasthand"
   def install
     (share/"fonts").install "Fasthand-Regular.ttf"
   end
