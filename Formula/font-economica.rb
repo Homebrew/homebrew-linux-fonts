318bda77e860ddeffc6062809832bd085dd78e94
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-economica.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-economica.rb
+++ Formula/font-economica.rb
@@ -1,7 +1,7 @@
 class FontEconomica < Formula
-  head "https://github.com/google/fonts/trunk/ofl/economica", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/economica", using: :svn, trust_cert: true
   desc "Economica"
-  homepage "https://www.google.com/fonts/specimen/Economica"
+  homepage "https://fonts.google.com/specimen/Economica"
   def install
     (share/"fonts").install "Economica-Bold.ttf"
     (share/"fonts").install "Economica-BoldItalic.ttf"
