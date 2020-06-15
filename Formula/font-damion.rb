4775fa1e49cc2ef5a937191277642ef3578cf024
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-damion.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-damion.rb
+++ Formula/font-damion.rb
@@ -1,7 +1,7 @@
 class FontDamion < Formula
   head "https://github.com/google/fonts/raw/master/ofl/damion/Damion-Regular.ttf"
   desc "Damion"
-  homepage "https://www.google.com/fonts/specimen/Damion"
+  homepage "https://fonts.google.com/specimen/Damion"
   def install
     (share/"fonts").install "Damion-Regular.ttf"
   end
