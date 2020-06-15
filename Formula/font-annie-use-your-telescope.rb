d16c3cd089d5feb21fb3a09832a8fb718739b280
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-annie-use-your-telescope.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-annie-use-your-telescope.rb
+++ Formula/font-annie-use-your-telescope.rb
@@ -1,7 +1,7 @@
 class FontAnnieUseYourTelescope < Formula
   head "https://github.com/google/fonts/raw/master/ofl/annieuseyourtelescope/AnnieUseYourTelescope-Regular.ttf"
   desc "Annie Use Your Telescope"
-  homepage "https://www.google.com/fonts/specimen/Annie+Use+Your+Telescope"
+  homepage "https://fonts.google.com/specimen/Annie+Use+Your+Telescope"
   def install
     (share/"fonts").install "AnnieUseYourTelescope-Regular.ttf"
   end
