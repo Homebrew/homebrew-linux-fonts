012f4e5d5e496e86f0378840298fe254d40cd64e
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-antic.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-antic.rb
+++ Formula/font-antic.rb
@@ -1,7 +1,7 @@
 class FontAntic < Formula
   head "https://github.com/google/fonts/raw/master/ofl/antic/Antic-Regular.ttf"
   desc "Antic"
-  homepage "https://www.google.com/fonts/specimen/Antic"
+  homepage "https://fonts.google.com/specimen/Antic"
   def install
     (share/"fonts").install "Antic-Regular.ttf"
   end
