4cfde69fbde282673f2ccf58423253eca0e23d98
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jolly-lodger.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-jolly-lodger.rb
+++ Formula/font-jolly-lodger.rb
@@ -1,7 +1,7 @@
 class FontJollyLodger < Formula
   head "https://github.com/google/fonts/raw/master/ofl/jollylodger/JollyLodger-Regular.ttf"
   desc "Jolly Lodger"
-  homepage "https://www.google.com/fonts/specimen/Jolly+Lodger"
+  homepage "https://fonts.google.com/specimen/Jolly+Lodger"
   def install
     (share/"fonts").install "JollyLodger-Regular.ttf"
   end
