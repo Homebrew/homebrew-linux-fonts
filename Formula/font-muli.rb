e1c1142d973c61839cec99f9e06c778d11b35f15
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-muli.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-muli.rb
+++ Formula/font-muli.rb
@@ -1,23 +1,10 @@
 class FontMuli < Formula
   head "https://github.com/google/fonts/trunk/ofl/muli", using: :svn, trust_cert: true
   desc "Muli"
-  homepage "https://www.google.com/fonts/specimen/Muli"
+  homepage "https://fonts.google.com/specimen/Muli"
   def install
-    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}static/Muli-Black.ttf"
-    (share/"fonts").install "#{parent}static/Muli-BlackItalic.ttf"
-    (share/"fonts").install "#{parent}static/Muli-Bold.ttf"
-    (share/"fonts").install "#{parent}static/Muli-BoldItalic.ttf"
-    (share/"fonts").install "#{parent}static/Muli-ExtraBold.ttf"
-    (share/"fonts").install "#{parent}static/Muli-ExtraBoldItalic.ttf"
-    (share/"fonts").install "#{parent}static/Muli-ExtraLight.ttf"
-    (share/"fonts").install "#{parent}static/Muli-ExtraLightItalic.ttf"
-    (share/"fonts").install "#{parent}static/Muli-Italic.ttf"
-    (share/"fonts").install "#{parent}static/Muli-Light.ttf"
-    (share/"fonts").install "#{parent}static/Muli-LightItalic.ttf"
-    (share/"fonts").install "#{parent}static/Muli-Regular.ttf"
-    (share/"fonts").install "#{parent}static/Muli-SemiBold.ttf"
-    (share/"fonts").install "#{parent}static/Muli-SemiBoldItalic.ttf"
+    (share/"fonts").install "Muli-Italic[wght].ttf"
+    (share/"fonts").install "Muli[wght].ttf"
   end
   test do
   end
