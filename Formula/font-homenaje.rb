fde793ecc96ce78becdaf4d82c4c02764d655325
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-homenaje.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-homenaje.rb
+++ Formula/font-homenaje.rb
@@ -1,7 +1,7 @@
 class FontHomenaje < Formula
   head "https://github.com/google/fonts/raw/master/ofl/homenaje/Homenaje-Regular.ttf"
   desc "Homenaje"
-  homepage "https://www.google.com/fonts/specimen/Homenaje"
+  homepage "https://fonts.google.com/specimen/Homenaje"
   def install
     (share/"fonts").install "Homenaje-Regular.ttf"
   end
