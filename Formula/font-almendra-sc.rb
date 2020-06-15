996a6ab94416480ab8f3a36d56cfe6b2460b4c9f
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-almendra-sc.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-almendra-sc.rb
+++ Formula/font-almendra-sc.rb
@@ -1,11 +1,8 @@
 class FontAlmendraSc < Formula
-  head "https://github.com/google/fonts/trunk/ofl/almendrasc", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/raw/master/ofl/almendrasc/AlmendraSC-Regular.ttf"
   desc "Almendra SC"
-  homepage "https://www.google.com/fonts/specimen/Almendra%20SC"
+  homepage "https://fonts.google.com/specimen/Almendra+SC"
   def install
-    (share/"fonts").install "AlmendraSC-Bold.ttf"
-    (share/"fonts").install "AlmendraSC-BoldItalic.ttf"
-    (share/"fonts").install "AlmendraSC-Italic.ttf"
     (share/"fonts").install "AlmendraSC-Regular.ttf"
   end
   test do
