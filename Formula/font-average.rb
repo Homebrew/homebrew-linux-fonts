048c5db50c529e9fd2f2a39d0398fd364d3da524
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-average.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-average.rb
+++ Formula/font-average.rb
@@ -1,7 +1,7 @@
 class FontAverage < Formula
   head "https://github.com/google/fonts/raw/master/ofl/average/Average-Regular.ttf"
   desc "Average"
-  homepage "https://www.google.com/fonts/specimen/Average"
+  homepage "https://fonts.google.com/specimen/Average"
   def install
     (share/"fonts").install "Average-Regular.ttf"
   end
