39d175e52b40b8c9eec0054d4bef7136b3b7b81a
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marko-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-marko-one.rb
+++ Formula/font-marko-one.rb
@@ -1,7 +1,7 @@
 class FontMarkoOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/markoone/MarkoOne-Regular.ttf"
   desc "Marko One"
-  homepage "https://www.google.com/fonts/specimen/Marko+One"
+  homepage "https://fonts.google.com/specimen/Marko+One"
   def install
     (share/"fonts").install "MarkoOne-Regular.ttf"
   end
