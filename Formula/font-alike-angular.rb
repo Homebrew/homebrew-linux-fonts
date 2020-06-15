e5010dc7b1c0b93b9ecf6381e32c80acb8e73c6c
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alike-angular.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-alike-angular.rb
+++ Formula/font-alike-angular.rb
@@ -1,7 +1,7 @@
 class FontAlikeAngular < Formula
   head "https://github.com/google/fonts/raw/master/ofl/alikeangular/AlikeAngular-Regular.ttf"
   desc "Alike Angular"
-  homepage "https://www.google.com/fonts/specimen/Alike+Angular"
+  homepage "https://fonts.google.com/specimen/Alike+Angular"
   def install
     (share/"fonts").install "AlikeAngular-Regular.ttf"
   end
