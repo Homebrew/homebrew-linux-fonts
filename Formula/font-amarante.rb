65e0e26ee47d124ae0625af7c51f4795850e20c6
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-amarante.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-amarante.rb
+++ Formula/font-amarante.rb
@@ -1,7 +1,7 @@
 class FontAmarante < Formula
   head "https://github.com/google/fonts/raw/master/ofl/amarante/Amarante-Regular.ttf"
   desc "Amarante"
-  homepage "https://www.google.com/fonts/specimen/Amarante"
+  homepage "https://fonts.google.com/specimen/Amarante"
   def install
     (share/"fonts").install "Amarante-Regular.ttf"
   end
