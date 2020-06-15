661280b7606b6446298c6c49f2a6af50ebaaa4fb
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-baumans.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-baumans.rb
+++ Formula/font-baumans.rb
@@ -1,7 +1,7 @@
 class FontBaumans < Formula
   head "https://github.com/google/fonts/raw/master/ofl/baumans/Baumans-Regular.ttf"
   desc "Baumans"
-  homepage "https://www.google.com/fonts/specimen/Baumans"
+  homepage "https://fonts.google.com/specimen/Baumans"
   def install
     (share/"fonts").install "Baumans-Regular.ttf"
   end
