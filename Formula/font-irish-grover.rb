fbea1f9a998fb25116761493628409601b058f80
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-irish-grover.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-irish-grover.rb
+++ Formula/font-irish-grover.rb
@@ -1,7 +1,7 @@
 class FontIrishGrover < Formula
   head "https://github.com/google/fonts/raw/master/apache/irishgrover/IrishGrover-Regular.ttf"
   desc "Irish Grover"
-  homepage "https://www.google.com/fonts/specimen/Irish+Grover"
+  homepage "https://fonts.google.com/specimen/Irish+Grover"
   def install
     (share/"fonts").install "IrishGrover-Regular.ttf"
   end
