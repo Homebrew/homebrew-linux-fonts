f2fe5c965bde371894871beb0c015d7378b51f42
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-convergence.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-convergence.rb
+++ Formula/font-convergence.rb
@@ -1,7 +1,7 @@
 class FontConvergence < Formula
   head "https://github.com/google/fonts/raw/master/ofl/convergence/Convergence-Regular.ttf"
   desc "Convergence"
-  homepage "https://www.google.com/fonts/specimen/Convergence"
+  homepage "https://fonts.google.com/specimen/Convergence"
   def install
     (share/"fonts").install "Convergence-Regular.ttf"
   end
