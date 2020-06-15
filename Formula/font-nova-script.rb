872f9ff889e9e00e4e448d0bbc8315384f38114b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nova-script.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-nova-script.rb
+++ Formula/font-nova-script.rb
@@ -1,7 +1,7 @@
 class FontNovaScript < Formula
   head "https://github.com/google/fonts/raw/master/ofl/novascript/NovaScript-Regular.ttf"
   desc "Nova Script"
-  homepage "https://www.google.com/fonts/specimen/Nova%20Script"
+  homepage "https://fonts.google.com/specimen/Nova+Script"
   def install
     (share/"fonts").install "NovaScript-Regular.ttf"
   end
