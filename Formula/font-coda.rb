099cd0253f59e57330083ab09de61b3ed7b0b0c0
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-coda.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-coda.rb
+++ Formula/font-coda.rb
@@ -1,9 +1,9 @@
 class FontCoda < Formula
-  head "https://github.com/google/fonts/trunk/ofl/coda", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/coda", using: :svn, trust_cert: true
   desc "Coda"
-  homepage "https://www.google.com/fonts/specimen/Coda"
+  homepage "https://fonts.google.com/specimen/Coda"
   def install
-    (share/"fonts").install "Coda-Heavy.ttf"
+    (share/"fonts").install "Coda-ExtraBold.ttf"
     (share/"fonts").install "Coda-Regular.ttf"
   end
   test do
