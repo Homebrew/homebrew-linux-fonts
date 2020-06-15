c23f419c5e1cde84f253f2a561ffde3535b6cbff
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-niconne.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-niconne.rb
+++ Formula/font-niconne.rb
@@ -1,7 +1,7 @@
 class FontNiconne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/niconne/Niconne-Regular.ttf"
   desc "Niconne"
-  homepage "https://www.google.com/fonts/specimen/Niconne"
+  homepage "https://fonts.google.com/specimen/Niconne"
   def install
     (share/"fonts").install "Niconne-Regular.ttf"
   end
