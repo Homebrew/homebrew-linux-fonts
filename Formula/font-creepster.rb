e7c072ef99376395a5f07e3565ad196a742e30c0
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-creepster.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-creepster.rb
+++ Formula/font-creepster.rb
@@ -1,7 +1,7 @@
 class FontCreepster < Formula
   head "https://github.com/google/fonts/raw/master/ofl/creepster/Creepster-Regular.ttf"
   desc "Creepster"
-  homepage "https://www.google.com/fonts/specimen/Creepster"
+  homepage "https://fonts.google.com/specimen/Creepster"
   def install
     (share/"fonts").install "Creepster-Regular.ttf"
   end
