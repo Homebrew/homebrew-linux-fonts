d8efc6dc8bf3a1af56e0ddfeeb975be33cf6247f
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-capriola.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-capriola.rb
+++ Formula/font-capriola.rb
@@ -1,7 +1,7 @@
 class FontCapriola < Formula
   head "https://github.com/google/fonts/raw/master/ofl/capriola/Capriola-Regular.ttf"
   desc "Capriola"
-  homepage "https://www.google.com/fonts/specimen/Capriola"
+  homepage "https://fonts.google.com/specimen/Capriola"
   def install
     (share/"fonts").install "Capriola-Regular.ttf"
   end
