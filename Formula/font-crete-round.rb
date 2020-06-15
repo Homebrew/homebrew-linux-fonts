d4a93e6600af874ba8c93f670470d0b30d48ee3b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-crete-round.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-crete-round.rb
+++ Formula/font-crete-round.rb
@@ -1,7 +1,7 @@
 class FontCreteRound < Formula
-  head "https://github.com/google/fonts/trunk/ofl/creteround", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/creteround", using: :svn, trust_cert: true
   desc "Crete Round"
-  homepage "https://www.google.com/fonts/specimen/Crete%20Round"
+  homepage "https://fonts.google.com/specimen/Crete+Round"
   def install
     (share/"fonts").install "CreteRound-Italic.ttf"
     (share/"fonts").install "CreteRound-Regular.ttf"
