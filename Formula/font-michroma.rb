329fafa4070b9d9ca248b9ab99056c0181812677
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-michroma.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-michroma.rb
+++ Formula/font-michroma.rb
@@ -1,7 +1,7 @@
 class FontMichroma < Formula
   head "https://github.com/google/fonts/raw/master/ofl/michroma/Michroma.ttf"
   desc "Michroma"
-  homepage "https://www.google.com/fonts/specimen/Michroma"
+  homepage "https://fonts.google.com/specimen/Michroma"
   def install
     (share/"fonts").install "Michroma.ttf"
   end
