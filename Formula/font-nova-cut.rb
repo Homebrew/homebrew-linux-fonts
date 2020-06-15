1a1c8a4422e6b06e54911deedc8d398a0d59e6e3
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nova-cut.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-nova-cut.rb
+++ Formula/font-nova-cut.rb
@@ -1,7 +1,7 @@
 class FontNovaCut < Formula
   head "https://github.com/google/fonts/raw/master/ofl/novacut/NovaCut.ttf"
   desc "Nova Cut"
-  homepage "https://www.google.com/fonts/specimen/Nova%20Cut"
+  homepage "https://fonts.google.com/specimen/Nova+Cut"
   def install
     (share/"fonts").install "NovaCut.ttf"
   end
