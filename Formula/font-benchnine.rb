0bb7227fa511279aa8f60565bb81cdb1a70fce43
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-benchnine.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-benchnine.rb
+++ Formula/font-benchnine.rb
@@ -1,7 +1,7 @@
 class FontBenchnine < Formula
-  head "https://github.com/google/fonts/trunk/ofl/benchnine", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/benchnine", using: :svn, trust_cert: true
   desc "BenchNine"
-  homepage "https://www.google.com/fonts/specimen/BenchNine"
+  homepage "https://fonts.google.com/specimen/BenchNine"
   def install
     (share/"fonts").install "BenchNine-Bold.ttf"
     (share/"fonts").install "BenchNine-Light.ttf"
