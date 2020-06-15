231c4b0f473d0b7173d9dbc2ccb43e52f895f2df
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monsieur-la-doulaise.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-monsieur-la-doulaise.rb
+++ Formula/font-monsieur-la-doulaise.rb
@@ -1,7 +1,7 @@
 class FontMonsieurLaDoulaise < Formula
   head "https://github.com/google/fonts/raw/master/ofl/monsieurladoulaise/MonsieurLaDoulaise-Regular.ttf"
   desc "Monsieur La Doulaise"
-  homepage "https://www.google.com/fonts/specimen/Monsieur+La+Doulaise"
+  homepage "https://fonts.google.com/specimen/Monsieur+La+Doulaise"
   def install
     (share/"fonts").install "MonsieurLaDoulaise-Regular.ttf"
   end
