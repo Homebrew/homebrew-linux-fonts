99ac54b4dd8b41af9196a2a114ee0723a3dce9ad
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-croissant-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-croissant-one.rb
+++ Formula/font-croissant-one.rb
@@ -1,7 +1,7 @@
 class FontCroissantOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/croissantone/CroissantOne-Regular.ttf"
   desc "Croissant One"
-  homepage "https://www.google.com/fonts/specimen/Croissant+One"
+  homepage "https://fonts.google.com/specimen/Croissant+One"
   def install
     (share/"fonts").install "CroissantOne-Regular.ttf"
   end
