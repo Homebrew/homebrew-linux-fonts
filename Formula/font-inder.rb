3100431bd1f78244c51394dd74ba545efe5e5bdf
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inder.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-inder.rb
+++ Formula/font-inder.rb
@@ -1,7 +1,7 @@
 class FontInder < Formula
   head "https://github.com/google/fonts/raw/master/ofl/inder/Inder-Regular.ttf"
   desc "Inder"
-  homepage "https://www.google.com/fonts/specimen/Inder"
+  homepage "https://fonts.google.com/specimen/Inder"
   def install
     (share/"fonts").install "Inder-Regular.ttf"
   end
