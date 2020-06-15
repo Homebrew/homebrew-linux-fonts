9d01f400387a9f830637a6b31b5e5f0946264293
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nova-mono.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-nova-mono.rb
+++ Formula/font-nova-mono.rb
@@ -1,7 +1,7 @@
 class FontNovaMono < Formula
   head "https://github.com/google/fonts/raw/master/ofl/novamono/NovaMono.ttf"
   desc "Nova Mono"
-  homepage "https://www.google.com/fonts/specimen/Nova%20Mono"
+  homepage "https://fonts.google.com/specimen/Nova+Mono"
   def install
     (share/"fonts").install "NovaMono.ttf"
   end
