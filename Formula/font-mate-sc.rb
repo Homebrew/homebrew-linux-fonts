57fdaa4143e17baad1a1a845d07312d555519558
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mate-sc.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-mate-sc.rb
+++ Formula/font-mate-sc.rb
@@ -1,7 +1,7 @@
 class FontMateSc < Formula
   head "https://github.com/google/fonts/raw/master/ofl/matesc/MateSC-Regular.ttf"
   desc "Mate SC"
-  homepage "https://www.google.com/fonts/specimen/Mate+SC"
+  homepage "https://fonts.google.com/specimen/Mate+SC"
   def install
     (share/"fonts").install "MateSC-Regular.ttf"
   end
