807791d5919992c392818f01ac9e129c54b54af3
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-autour-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-autour-one.rb
+++ Formula/font-autour-one.rb
@@ -1,7 +1,7 @@
 class FontAutourOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/autourone/AutourOne-Regular.ttf"
   desc "Autour One"
-  homepage "https://www.google.com/fonts/specimen/Autour+One"
+  homepage "https://fonts.google.com/specimen/Autour+One"
   def install
     (share/"fonts").install "AutourOne-Regular.ttf"
   end
