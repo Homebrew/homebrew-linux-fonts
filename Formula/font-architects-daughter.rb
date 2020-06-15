f59f1a504e2da6203baf45db2dfd786888db35dc
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-architects-daughter.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-architects-daughter.rb
+++ Formula/font-architects-daughter.rb
@@ -1,7 +1,7 @@
 class FontArchitectsDaughter < Formula
   head "https://github.com/google/fonts/raw/master/ofl/architectsdaughter/ArchitectsDaughter-Regular.ttf"
   desc "Architects Daughter"
-  homepage "https://www.google.com/fonts/specimen/Architects+Daughter"
+  homepage "https://fonts.google.com/specimen/Architects+Daughter"
   def install
     (share/"fonts").install "ArchitectsDaughter-Regular.ttf"
   end
