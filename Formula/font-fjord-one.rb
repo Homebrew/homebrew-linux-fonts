313be6842a8f4cb515b5c92b18b311732afbe6f7
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fjord-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-fjord-one.rb
+++ Formula/font-fjord-one.rb
@@ -1,7 +1,7 @@
 class FontFjordOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/fjordone/FjordOne-Regular.ttf"
   desc "Fjord One"
-  homepage "https://www.google.com/fonts/specimen/Fjord+One"
+  homepage "https://fonts.google.com/specimen/Fjord+One"
   def install
     (share/"fonts").install "FjordOne-Regular.ttf"
   end
