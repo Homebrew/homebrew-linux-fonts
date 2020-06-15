758729793ff2c7cdd5c3c7007e4be41df6333339
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-crimson-pro.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-crimson-pro.rb
+++ Formula/font-crimson-pro.rb
@@ -1,10 +1,10 @@
 class FontCrimsonPro < Formula
   head "https://github.com/google/fonts/trunk/ofl/crimsonpro", using: :svn, trust_cert: true
   desc "Crimson Pro"
-  homepage "https://www.google.com/fonts/specimen/Crimson%20Pro"
+  homepage "https://fonts.google.com/specimen/Crimson+Pro"
   def install
-    (share/"fonts").install "CrimsonPro[wght].ttf"
     (share/"fonts").install "CrimsonPro-Italic[wght].ttf"
+    (share/"fonts").install "CrimsonPro[wght].ttf"
   end
   test do
   end
