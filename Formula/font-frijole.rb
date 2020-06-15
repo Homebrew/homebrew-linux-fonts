bb9f40a2ee783d541493ecb7a83e9fefdeca2bb7
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-frijole.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-frijole.rb
+++ Formula/font-frijole.rb
@@ -1,7 +1,7 @@
 class FontFrijole < Formula
   head "https://github.com/google/fonts/raw/master/ofl/frijole/Frijole-Regular.ttf"
   desc "Frijole"
-  homepage "https://www.google.com/fonts/specimen/Frijole"
+  homepage "https://fonts.google.com/specimen/Frijole"
   def install
     (share/"fonts").install "Frijole-Regular.ttf"
   end
