7a057a08497bfb59c4736c444b745eba53a66d54
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-averia-sans-libre.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-averia-sans-libre.rb
+++ Formula/font-averia-sans-libre.rb
@@ -1,7 +1,7 @@
 class FontAveriaSansLibre < Formula
-  head "https://github.com/google/fonts/trunk/ofl/averiasanslibre", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/averiasanslibre", using: :svn, trust_cert: true
   desc "Averia Sans Libre"
-  homepage "https://www.google.com/fonts/specimen/Averia%20Sans%20Libre"
+  homepage "https://fonts.google.com/specimen/Averia+Sans+Libre"
   def install
     (share/"fonts").install "AveriaSansLibre-Bold.ttf"
     (share/"fonts").install "AveriaSansLibre-BoldItalic.ttf"
