f8a1fc82d7ce13adc981485acdf94e781e4c378c
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-artifika.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-artifika.rb
+++ Formula/font-artifika.rb
@@ -1,7 +1,7 @@
 class FontArtifika < Formula
   head "https://github.com/google/fonts/raw/master/ofl/artifika/Artifika-Regular.ttf"
   desc "Artifika"
-  homepage "https://www.google.com/fonts/specimen/Artifika"
+  homepage "https://fonts.google.com/specimen/Artifika"
   def install
     (share/"fonts").install "Artifika-Regular.ttf"
   end
