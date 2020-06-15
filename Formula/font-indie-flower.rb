ff6cb1c87f23a1e5830a8255ce5d070390a35a5b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-indie-flower.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-indie-flower.rb
+++ Formula/font-indie-flower.rb
@@ -1,7 +1,7 @@
 class FontIndieFlower < Formula
   head "https://github.com/google/fonts/raw/master/ofl/indieflower/IndieFlower-Regular.ttf"
   desc "Indie Flower"
-  homepage "https://www.google.com/fonts/specimen/Indie+Flower"
+  homepage "https://fonts.google.com/specimen/Indie+Flower"
   def install
     (share/"fonts").install "IndieFlower-Regular.ttf"
   end
