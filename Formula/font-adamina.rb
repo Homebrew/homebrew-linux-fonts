cd70ab00c9ac02bf06551e7a1a3f660cf2558536
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-adamina.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-adamina.rb
+++ Formula/font-adamina.rb
@@ -1,7 +1,7 @@
 class FontAdamina < Formula
   head "https://github.com/google/fonts/raw/master/ofl/adamina/Adamina-Regular.ttf"
   desc "Adamina"
-  homepage "https://www.google.com/fonts/specimen/Adamina"
+  homepage "https://fonts.google.com/specimen/Adamina"
   def install
     (share/"fonts").install "Adamina-Regular.ttf"
   end
