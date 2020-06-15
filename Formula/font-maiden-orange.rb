6cb13f6f80bd40b6accb2f0a6ef8b1bc10552969
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-maiden-orange.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-maiden-orange.rb
+++ Formula/font-maiden-orange.rb
@@ -1,7 +1,7 @@
 class FontMaidenOrange < Formula
   head "https://github.com/google/fonts/raw/master/apache/maidenorange/MaidenOrange-Regular.ttf"
   desc "Maiden Orange"
-  homepage "https://www.google.com/fonts/specimen/Maiden+Orange"
+  homepage "https://fonts.google.com/specimen/Maiden+Orange"
   def install
     (share/"fonts").install "MaidenOrange-Regular.ttf"
   end
