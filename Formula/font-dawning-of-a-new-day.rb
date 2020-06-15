27918a8be1d2bd1e5bf339f53ef8e8e79298a547
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dawning-of-a-new-day.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-dawning-of-a-new-day.rb
+++ Formula/font-dawning-of-a-new-day.rb
@@ -1,7 +1,7 @@
 class FontDawningOfANewDay < Formula
   head "https://github.com/google/fonts/raw/master/ofl/dawningofanewday/DawningofaNewDay.ttf"
   desc "Dawning of a New Day"
-  homepage "https://www.google.com/fonts/specimen/Dawning+of+a+New+Day"
+  homepage "https://fonts.google.com/specimen/Dawning+of+a+New+Day"
   def install
     (share/"fonts").install "DawningofaNewDay.ttf"
   end
