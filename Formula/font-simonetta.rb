cdcebbf36847d9eaa9ca569a052718d3e5e8388a
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-simonetta.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-simonetta.rb
+++ Formula/font-simonetta.rb
@@ -1,7 +1,7 @@
 class FontSimonetta < Formula
-  head "https://github.com/google/fonts/trunk/ofl/simonetta", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/simonetta", using: :svn, trust_cert: true
   desc "Simonetta"
-  homepage "https://www.google.com/fonts/specimen/Simonetta"
+  homepage "https://fonts.google.com/specimen/Simonetta"
   def install
     (share/"fonts").install "Simonetta-Black.ttf"
     (share/"fonts").install "Simonetta-BlackItalic.ttf"
