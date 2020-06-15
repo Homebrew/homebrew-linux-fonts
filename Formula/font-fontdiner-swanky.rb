67236d72c0cca8bf3d168c316dd7c3f3edad5b8e
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fontdiner-swanky.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-fontdiner-swanky.rb
+++ Formula/font-fontdiner-swanky.rb
@@ -1,7 +1,7 @@
 class FontFontdinerSwanky < Formula
   head "https://github.com/google/fonts/raw/master/apache/fontdinerswanky/FontdinerSwanky-Regular.ttf"
   desc "Fontdiner Swanky"
-  homepage "https://www.google.com/fonts/specimen/Fontdiner+Swanky"
+  homepage "https://fonts.google.com/specimen/Fontdiner+Swanky"
   def install
     (share/"fonts").install "FontdinerSwanky-Regular.ttf"
   end
