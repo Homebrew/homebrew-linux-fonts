2f9cd2e490d4c16bd45b4a308c3b76140055e4c5
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-merriweather-sans.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-merriweather-sans.rb
+++ Formula/font-merriweather-sans.rb
@@ -1,7 +1,7 @@
 class FontMerriweatherSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/merriweathersans", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/merriweathersans", using: :svn, trust_cert: true
   desc "Merriweather Sans"
-  homepage "https://www.google.com/fonts/specimen/Merriweather%20Sans"
+  homepage "https://fonts.google.com/specimen/Merriweather+Sans"
   def install
     (share/"fonts").install "MerriweatherSans-Bold.ttf"
     (share/"fonts").install "MerriweatherSans-BoldItalic.ttf"
