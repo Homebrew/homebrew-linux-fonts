9a2b2c48fe1cc5e62781e63491277c0a1cb8e704
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-leckerli-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-leckerli-one.rb
+++ Formula/font-leckerli-one.rb
@@ -1,7 +1,7 @@
 class FontLeckerliOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/leckerlione/LeckerliOne-Regular.ttf"
   desc "Leckerli One"
-  homepage "https://www.google.com/fonts/specimen/Leckerli+One"
+  homepage "https://fonts.google.com/specimen/Leckerli+One"
   def install
     (share/"fonts").install "LeckerliOne-Regular.ttf"
   end
