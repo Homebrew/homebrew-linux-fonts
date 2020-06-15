430622ba12cea26b95d731e2edf347ab7b98a48d
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-actor.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-actor.rb
+++ Formula/font-actor.rb
@@ -1,7 +1,7 @@
 class FontActor < Formula
   head "https://github.com/google/fonts/raw/master/ofl/actor/Actor-Regular.ttf"
   desc "Actor"
-  homepage "https://www.google.com/fonts/specimen/Actor"
+  homepage "https://fonts.google.com/specimen/Actor"
   def install
     (share/"fonts").install "Actor-Regular.ttf"
   end
