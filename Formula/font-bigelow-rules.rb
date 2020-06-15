d46f9e7d967f8406242222f14ef2a4ce2d13a98b
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bigelow-rules.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-bigelow-rules.rb
+++ Formula/font-bigelow-rules.rb
@@ -1,7 +1,7 @@
 class FontBigelowRules < Formula
   head "https://github.com/google/fonts/raw/master/ofl/bigelowrules/BigelowRules-Regular.ttf"
   desc "Bigelow Rules"
-  homepage "https://www.google.com/fonts/specimen/Bigelow+Rules"
+  homepage "https://fonts.google.com/specimen/Bigelow+Rules"
   def install
     (share/"fonts").install "BigelowRules-Regular.ttf"
   end
