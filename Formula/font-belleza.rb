9b7c226577e6a848bbc67ef7f82f44877d134616
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-belleza.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-belleza.rb
+++ Formula/font-belleza.rb
@@ -1,7 +1,7 @@
 class FontBelleza < Formula
   head "https://github.com/google/fonts/raw/master/ofl/belleza/Belleza-Regular.ttf"
   desc "Belleza"
-  homepage "https://www.google.com/fonts/specimen/Belleza"
+  homepage "https://fonts.google.com/specimen/Belleza"
   def install
     (share/"fonts").install "Belleza-Regular.ttf"
   end
