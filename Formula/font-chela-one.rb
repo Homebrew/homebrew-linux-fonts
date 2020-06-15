7e054dd7f361ca0d0d7a806e9ae2db1973364c43
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chela-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-chela-one.rb
+++ Formula/font-chela-one.rb
@@ -1,7 +1,7 @@
 class FontChelaOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/chelaone/ChelaOne-Regular.ttf"
   desc "Chela One"
-  homepage "https://www.google.com/fonts/specimen/Chela+One"
+  homepage "https://fonts.google.com/specimen/Chela+One"
   def install
     (share/"fonts").install "ChelaOne-Regular.ttf"
   end
