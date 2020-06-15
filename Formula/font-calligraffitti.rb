1d467c6f6d43d12d783a838d2d2a8c4b8765e1af
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-calligraffitti.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-calligraffitti.rb
+++ Formula/font-calligraffitti.rb
@@ -1,7 +1,7 @@
 class FontCalligraffitti < Formula
   head "https://github.com/google/fonts/raw/master/apache/calligraffitti/Calligraffitti-Regular.ttf"
   desc "Calligraffitti"
-  homepage "https://www.google.com/fonts/specimen/Calligraffitti"
+  homepage "https://fonts.google.com/specimen/Calligraffitti"
   def install
     (share/"fonts").install "Calligraffitti-Regular.ttf"
   end
