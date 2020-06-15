6d314cf2aa4ec513eb1c5372a8d0df6ed5495d44
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-macondo.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-macondo.rb
+++ Formula/font-macondo.rb
@@ -1,7 +1,7 @@
 class FontMacondo < Formula
   head "https://github.com/google/fonts/raw/master/ofl/macondo/Macondo-Regular.ttf"
   desc "Macondo"
-  homepage "https://www.google.com/fonts/specimen/Macondo"
+  homepage "https://fonts.google.com/specimen/Macondo"
   def install
     (share/"fonts").install "Macondo-Regular.ttf"
   end
