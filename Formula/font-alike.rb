aec1ad0687c27c4d20f48f7298e55fee539895c5
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alike.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-alike.rb
+++ Formula/font-alike.rb
@@ -1,7 +1,7 @@
 class FontAlike < Formula
   head "https://github.com/google/fonts/raw/master/ofl/alike/Alike-Regular.ttf"
   desc "Alike"
-  homepage "https://www.google.com/fonts/specimen/Alike"
+  homepage "https://fonts.google.com/specimen/Alike"
   def install
     (share/"fonts").install "Alike-Regular.ttf"
   end
