18cbc8f6a27a299196e756db1a0a14f7960855d6
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fascinate-inline.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-fascinate-inline.rb
+++ Formula/font-fascinate-inline.rb
@@ -1,7 +1,7 @@
 class FontFascinateInline < Formula
   head "https://github.com/google/fonts/raw/master/ofl/fascinateinline/FascinateInline-Regular.ttf"
   desc "Fascinate Inline"
-  homepage "https://www.google.com/fonts/specimen/Fascinate+Inline"
+  homepage "https://fonts.google.com/specimen/Fascinate+Inline"
   def install
     (share/"fonts").install "FascinateInline-Regular.ttf"
   end
