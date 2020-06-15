f94f228eaaebd8bf88957d9f9822b8734b213e88
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cedarville-cursive.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-cedarville-cursive.rb
+++ Formula/font-cedarville-cursive.rb
@@ -1,7 +1,7 @@
 class FontCedarvilleCursive < Formula
   head "https://github.com/google/fonts/raw/master/ofl/cedarvillecursive/Cedarville-Cursive.ttf"
   desc "Cedarville Cursive"
-  homepage "https://www.google.com/fonts/specimen/Cedarville+Cursive"
+  homepage "https://fonts.google.com/specimen/Cedarville+Cursive"
   def install
     (share/"fonts").install "Cedarville-Cursive.ttf"
   end
