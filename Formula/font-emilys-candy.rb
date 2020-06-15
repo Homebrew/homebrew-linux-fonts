7e6d60d232aaa7d5d9eaf42cd7d2474477761b2f
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-emilys-candy.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-emilys-candy.rb
+++ Formula/font-emilys-candy.rb
@@ -1,7 +1,7 @@
 class FontEmilysCandy < Formula
   head "https://github.com/google/fonts/raw/master/ofl/emilyscandy/EmilysCandy-Regular.ttf"
   desc "Emilys Candy"
-  homepage "https://www.google.com/fonts/specimen/Emilys+Candy"
+  homepage "https://fonts.google.com/specimen/Emilys+Candy"
   def install
     (share/"fonts").install "EmilysCandy-Regular.ttf"
   end
