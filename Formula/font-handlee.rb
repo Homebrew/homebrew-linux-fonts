f61590ca96685e2ffbb36ced060a10500af34a91
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-handlee.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-handlee.rb
+++ Formula/font-handlee.rb
@@ -1,7 +1,7 @@
 class FontHandlee < Formula
   head "https://github.com/google/fonts/raw/master/ofl/handlee/Handlee-Regular.ttf"
   desc "Handlee"
-  homepage "https://www.google.com/fonts/specimen/Handlee"
+  homepage "https://fonts.google.com/specimen/Handlee"
   def install
     (share/"fonts").install "Handlee-Regular.ttf"
   end
