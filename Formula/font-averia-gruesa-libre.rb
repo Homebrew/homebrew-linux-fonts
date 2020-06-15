061c81939f14b868731bf8296089c0867e666c94
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-averia-gruesa-libre.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-averia-gruesa-libre.rb
+++ Formula/font-averia-gruesa-libre.rb
@@ -1,7 +1,7 @@
 class FontAveriaGruesaLibre < Formula
   head "https://github.com/google/fonts/raw/master/ofl/averiagruesalibre/AveriaGruesaLibre-Regular.ttf"
   desc "Averia Gruesa Libre"
-  homepage "https://www.google.com/fonts/specimen/Averia+Gruesa+Libre"
+  homepage "https://fonts.google.com/specimen/Averia+Gruesa+Libre"
   def install
     (share/"fonts").install "AveriaGruesaLibre-Regular.ttf"
   end
