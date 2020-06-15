cce7b7fa9d1b59294f9c031276227aefe1bf4ec8
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-julius-sans-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-julius-sans-one.rb
+++ Formula/font-julius-sans-one.rb
@@ -1,7 +1,7 @@
 class FontJuliusSansOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/juliussansone/JuliusSansOne-Regular.ttf"
   desc "Julius Sans One"
-  homepage "https://www.google.com/fonts/specimen/Julius+Sans+One"
+  homepage "https://fonts.google.com/specimen/Julius+Sans+One"
   def install
     (share/"fonts").install "JuliusSansOne-Regular.ttf"
   end
