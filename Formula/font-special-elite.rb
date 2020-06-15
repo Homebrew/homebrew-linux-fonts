bff6026534a67936379972be2f550347a7fceaa5
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-special-elite.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-special-elite.rb
+++ Formula/font-special-elite.rb
@@ -1,7 +1,7 @@
 class FontSpecialElite < Formula
   head "https://github.com/google/fonts/raw/master/apache/specialelite/SpecialElite-Regular.ttf"
   desc "Special Elite"
-  homepage "https://www.google.com/fonts/specimen/Special+Elite"
+  homepage "https://fonts.google.com/specimen/Special+Elite"
   def install
     (share/"fonts").install "SpecialElite-Regular.ttf"
   end
