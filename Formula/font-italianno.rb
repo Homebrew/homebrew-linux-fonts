2c088a9377e34344457654a6d4767be9d1cd0399
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-italianno.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-italianno.rb
+++ Formula/font-italianno.rb
@@ -1,7 +1,7 @@
 class FontItalianno < Formula
   head "https://github.com/google/fonts/raw/master/ofl/italianno/Italianno-Regular.ttf"
   desc "Italianno"
-  homepage "https://www.google.com/fonts/specimen/Italianno"
+  homepage "https://fonts.google.com/specimen/Italianno"
   def install
     (share/"fonts").install "Italianno-Regular.ttf"
   end
