b25ed3b229c085ae894d841fde95cf9b180e1bc7
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-linden-hill.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-linden-hill.rb
+++ Formula/font-linden-hill.rb
@@ -1,7 +1,7 @@
 class FontLindenHill < Formula
-  head "https://github.com/google/fonts/trunk/ofl/lindenhill", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/lindenhill", using: :svn, trust_cert: true
   desc "Linden Hill"
-  homepage "https://www.google.com/fonts/specimen/Linden%20Hill"
+  homepage "https://fonts.google.com/specimen/Linden+Hill"
   def install
     (share/"fonts").install "LindenHill-Italic.ttf"
     (share/"fonts").install "LindenHill-Regular.ttf"
