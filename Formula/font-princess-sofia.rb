8faa9f1182c215741d77359c1082d49f8b9581c9
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-princess-sofia.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-princess-sofia.rb
+++ Formula/font-princess-sofia.rb
@@ -1,7 +1,7 @@
 class FontPrincessSofia < Formula
   head "https://github.com/google/fonts/raw/master/ofl/princesssofia/PrincessSofia-Regular.ttf"
   desc "Princess Sofia"
-  homepage "https://www.google.com/fonts/specimen/Princess+Sofia"
+  homepage "https://fonts.google.com/specimen/Princess+Sofia"
   def install
     (share/"fonts").install "PrincessSofia-Regular.ttf"
   end
