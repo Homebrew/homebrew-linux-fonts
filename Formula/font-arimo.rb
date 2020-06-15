49e6cf0eb2f879d2d5883605472e9d810731acef
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arimo.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-arimo.rb
+++ Formula/font-arimo.rb
@@ -1,7 +1,7 @@
 class FontArimo < Formula
-  head "https://github.com/google/fonts/trunk/apache/arimo", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/apache/arimo", using: :svn, trust_cert: true
   desc "Arimo"
-  homepage "https://www.google.com/fonts/specimen/Arimo"
+  homepage "https://fonts.google.com/specimen/Arimo"
   def install
     (share/"fonts").install "Arimo-Bold.ttf"
     (share/"fonts").install "Arimo-BoldItalic.ttf"
