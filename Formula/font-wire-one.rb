b81503bf6e9a97e502ad29f925fbc1ee655ebecd
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wire-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-wire-one.rb
+++ Formula/font-wire-one.rb
@@ -1,7 +1,7 @@
 class FontWireOne < Formula
   head "https://github.com/google/fonts/raw/master/ofl/wireone/WireOne.ttf"
   desc "Wire One"
-  homepage "https://www.google.com/fonts/specimen/Wire+One"
+  homepage "https://fonts.google.com/specimen/Wire+One"
   def install
     (share/"fonts").install "WireOne.ttf"
   end
