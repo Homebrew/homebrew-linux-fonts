eff8946243ce708d3ea8ebf858760b62aaedaeea
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fondamento.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-fondamento.rb
+++ Formula/font-fondamento.rb
@@ -1,7 +1,7 @@
 class FontFondamento < Formula
-  head "https://github.com/google/fonts/trunk/ofl/fondamento", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/fondamento", using: :svn, trust_cert: true
   desc "Fondamento"
-  homepage "https://www.google.com/fonts/specimen/Fondamento"
+  homepage "https://fonts.google.com/specimen/Fondamento"
   def install
     (share/"fonts").install "Fondamento-Italic.ttf"
     (share/"fonts").install "Fondamento-Regular.ttf"
