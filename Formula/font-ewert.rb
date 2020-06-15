55534f09d10e60907079f1510524665f4ee4163e
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ewert.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-ewert.rb
+++ Formula/font-ewert.rb
@@ -1,7 +1,7 @@
 class FontEwert < Formula
   head "https://github.com/google/fonts/raw/master/ofl/ewert/Ewert-Regular.ttf"
   desc "Ewert"
-  homepage "https://www.google.com/fonts/specimen/Ewert"
+  homepage "https://fonts.google.com/specimen/Ewert"
   def install
     (share/"fonts").install "Ewert-Regular.ttf"
   end
