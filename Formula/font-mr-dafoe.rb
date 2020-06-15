e48d77b5af7e23ca7170591a646de92a48bec2eb
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mr-dafoe.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-mr-dafoe.rb
+++ Formula/font-mr-dafoe.rb
@@ -1,7 +1,7 @@
 class FontMrDafoe < Formula
   head "https://github.com/google/fonts/raw/master/ofl/mrdafoe/MrDafoe-Regular.ttf"
   desc "Mr Dafoe"
-  homepage "https://www.google.com/fonts/specimen/Mr+Dafoe"
+  homepage "https://fonts.google.com/specimen/Mr+Dafoe"
   def install
     (share/"fonts").install "MrDafoe-Regular.ttf"
   end
