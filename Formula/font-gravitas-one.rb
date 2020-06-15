523f4cde695cdfe0dd4226526276c6a8e5ff90e2
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-15 00:25:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gravitas-one.rb from 43bb18005c1698e89ce6498351b012f7a1d803b0

--- Formula/font-gravitas-one.rb
+++ Formula/font-gravitas-one.rb
@@ -1,7 +1,7 @@
 class FontGravitasOne < Formula
-  head "https://github.com/google/fonts/trunk/ofl/gravitasone", using: :svn, revision: "50", trust_cert: true
+  head "https://github.com/google/fonts/raw/master/ofl/gravitasone/GravitasOne.ttf"
   desc "Gravitas One"
-  homepage "https://www.google.com/fonts/specimen/Gravitas%20One"
+  homepage "https://fonts.google.com/specimen/Gravitas+One"
   def install
     (share/"fonts").install "GravitasOne.ttf"
   end
