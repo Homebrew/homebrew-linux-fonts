f2f0960a3cf3cc12d8fd2c8f068e91a884c15442
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-17 00:25:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-liberation.rb from abdc577326a5a9bf30049137fe1173279f495f59

--- Formula/font-liberation.rb
+++ Formula/font-liberation.rb
@@ -2,7 +2,7 @@ class FontLiberation < Formula
   version "2.1.1,4743886"
   sha256 "8ee2c16fe0f055e60dd8375449aff72d25dd623b9cc6f24790ce9d2e91446fca"
   url "https://github.com/liberationfonts/liberation-fonts/files/#{version.to_s.sub(/.*,/, "")}/liberation-fonts-ttf-#{version.to_s.sub(/,.*/, "")}.tar.gz"
-  desc "Liberation Sans"
+  desc "Liberation"
   homepage "https://github.com/liberationfonts/liberation-fonts"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
