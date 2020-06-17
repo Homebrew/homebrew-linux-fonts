67e82258fa96ac4699c606dfaf717b1cc030a9e9
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-17 00:25:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-typographer-woodcut-initials-one.rb from 61f82c1fc3f52956ecf259abf718d8613a42ff11

---
+++ Formula/font-typographer-woodcut-initials-one.rb
@@ -0,0 +1,10 @@
+class FontTypographerWoodcutInitialsOne < Formula
+  head "https://dl.1001fonts.com/typographerwoodcutinitialsone.zip"
+  desc "Typographer Woodcut Initials One"
+  homepage "https://www.1001fonts.com/typographerwoodcutinitialsone-font.html"
+  def install
+    (share/"fonts").install "TypographerWoodcut01.ttf"
+  end
+  test do
+  end
+end
