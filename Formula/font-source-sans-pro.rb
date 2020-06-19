f983bdacd36d53578ceac9793887a19e3c92cd89
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-19 00:25:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-sans-pro.rb from 4002bfa454011d0bb83f8897637055bbe5f9550a

--- Formula/font-source-sans-pro.rb
+++ Formula/font-source-sans-pro.rb
@@ -1,9 +1,9 @@
 class FontSourceSansPro < Formula
-  version "2.045R-ro-1.095R-it"
-  sha256 "5f090b821e117f452ef399c3c1b8b244c1647728a1e901c4d167cbf7fc298ab6"
-  url "https://github.com/adobe-fonts/source-sans-pro/archive/#{version.to_s.sub('ro-', 'ro/')}.zip"
+  version "3.006R"
+  sha256 "b22c7a4c45050ed8ae1b5293a9fd3b886d6f5150eb1e2ded429b9a11ac1602c5"
+  url "https://github.com/adobe-fonts/source-sans-pro/releases/download/#{version}/source-sans-pro-#{version}.zip"
   desc "Source Sans Pro"
-  homepage "https://adobe-fonts.github.io/source-sans-pro/"
+  homepage "https://github.com/adobe-fonts/source-sans-pro"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-Black.otf"
@@ -18,6 +18,8 @@ class FontSourceSansPro < Formula
     (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-Regular.otf"
     (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-Semibold.otf"
     (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-SemiboldIt.otf"
+    (share/"fonts").install "#{parent}source-sans-pro-#{version}/VAR/SourceSansVariable-Italic.otf"
+    (share/"fonts").install "#{parent}source-sans-pro-#{version}/VAR/SourceSansVariable-Roman.otf"
   end
   test do
   end
