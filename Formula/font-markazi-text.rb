420fa286304c87031e8514fa8f23097efde157fd
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-24 00:25:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-markazi-text.rb from 81801c54001d1b2d1125038f916afcc5a1c6793e

--- Formula/font-markazi-text.rb
+++ Formula/font-markazi-text.rb
@@ -1,9 +1,9 @@
 class FontMarkaziText < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/markazitext/MarkaziText%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/markazitextvfbeta/MarkaziText-VF.ttf"
   desc "Markazi Text"
   homepage "https://fonts.google.com/specimen/Markazi+Text"
   def install
-    (share/"fonts").install "MarkaziText[wght].ttf"
+    (share/"fonts").install "MarkaziText-VF.ttf"
   end
   test do
   end
