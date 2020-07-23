5b4f32a9de3fbc1bd3c0a503325d7ad9be67d302
TANIGUCHI Masaya <mew@cat.ovh>
2020-07-23 00:34:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spleen.rb from 25257f5ba6a4e3de92b4be319478ea56516953f9

--- Formula/font-spleen.rb
+++ Formula/font-spleen.rb
@@ -1,11 +1,15 @@
 class FontSpleen < Formula
-  version "1.8.0"
-  sha256 "e761249e859ec3cebacb79b6ea2f0e51701316866305940b2431e61157207747"
+  version "1.8.1"
+  sha256 "59119044399b2262fdf4c504c6f1a8b1230d349dfd07c68a2fb07a394a880e1e"
   url "https://github.com/fcambus/spleen/releases/download/#{version}/spleen-#{version}.tar.gz"
   desc "Spleen"
   homepage "https://github.com/fcambus/spleen"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}spleen-#{version}/spleen-6x12.otf"
+    (share/"fonts").install "#{parent}spleen-#{version}/spleen-8x16.otf"
+    (share/"fonts").install "#{parent}spleen-#{version}/spleen-12x24.otf"
+    (share/"fonts").install "#{parent}spleen-#{version}/spleen-16x32.otf"
     (share/"fonts").install "#{parent}spleen-#{version}/spleen-32x64.otf"
   end
   test do
