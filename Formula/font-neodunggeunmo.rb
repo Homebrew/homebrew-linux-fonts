c3e0101d53cd407d3a669c610828e253aca58ba0
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-01 00:38:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-neodunggeunmo.rb from 0e3237b983c56606b0ad6e01d64db6a99022225d

--- Formula/font-neodunggeunmo.rb
+++ Formula/font-neodunggeunmo.rb
@@ -3,7 +3,7 @@ class FontNeodunggeunmo < Formula
   sha256 "67765532d17afe0c5b81bd0f6109e0d2619ba2f15c860f3e8cc9b561287a5b28"
   url "https://github.com/Dalgona/neodgm/releases/download/v#{version}/neodgm.ttf"
   desc "NeoDunggeunmo"
-  homepage "https://dalgona.dev/neodgm"
+  homepage "https://neodgm.dalgona.dev/"
   def install
     (share/"fonts").install "neodgm.ttf"
   end
