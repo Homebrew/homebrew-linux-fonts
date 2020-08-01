e17b8df6646df6535fdc5371e48365d2f3939025
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-01 00:38:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hackgen.rb from d0e82dc8400c4508008c84bc3eaabb3ae38c1171

--- Formula/font-hackgen.rb
+++ Formula/font-hackgen.rb
@@ -1,6 +1,6 @@
 class FontHackgen < Formula
-  version "1.4.0"
-  sha256 "c21de5388fa94c7d2dbd16111524fa871dd0f2a3908941fb2200154cab48972e"
+  version "2.1.0"
+  sha256 "a1ccb44e72abb7bf0bc3dd6bc208cc9514d5b22e3096010cf82ebccecc1d0384"
   url "https://github.com/yuru7/HackGen/releases/download/v#{version}/HackGen_v#{version}.zip"
   desc "HackGen"
   homepage "https://github.com/yuru7/HackGen"
@@ -10,13 +10,9 @@ class FontHackgen < Formula
     (share/"fonts").install "#{parent}HackGen_v#{version}/HackGen-Regular.ttf"
     (share/"fonts").install "#{parent}HackGen_v#{version}/HackGen35-Bold.ttf"
     (share/"fonts").install "#{parent}HackGen_v#{version}/HackGen35-Regular.ttf"
-    (share/"fonts").install "#{parent}HackGen_v#{version}/HackGen35Console-Bold-forPowerline.ttf"
     (share/"fonts").install "#{parent}HackGen_v#{version}/HackGen35Console-Bold.ttf"
-    (share/"fonts").install "#{parent}HackGen_v#{version}/HackGen35Console-Regular-forPowerline.ttf"
     (share/"fonts").install "#{parent}HackGen_v#{version}/HackGen35Console-Regular.ttf"
-    (share/"fonts").install "#{parent}HackGen_v#{version}/HackGenConsole-Bold-forPowerline.ttf"
     (share/"fonts").install "#{parent}HackGen_v#{version}/HackGenConsole-Bold.ttf"
-    (share/"fonts").install "#{parent}HackGen_v#{version}/HackGenConsole-Regular-forPowerline.ttf"
     (share/"fonts").install "#{parent}HackGen_v#{version}/HackGenConsole-Regular.ttf"
   end
   test do
