d6549bb6462a20b88d535788eed897b656e198c2
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-14 00:39:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu.rb from 7b8e417102f22f2ec8adab20ad8c92d9c1e66ecf

--- Formula/font-ubuntu.rb
+++ Formula/font-ubuntu.rb
@@ -1,6 +1,6 @@
 class FontUbuntu < Formula
-  version "0.83,fad7939b"
-  sha256 "456d7d42797febd0d7d4cf1b782a2e03680bb4a5ee43cc9d06bda172bac05b42"
+  version "0.83,0cef8205"
+  sha256 "61a2b342526fd552f19fef438bb9211a8212de19ad96e32a1209c039f1d68ecf"
   url "https://assets.ubuntu.com/v1/#{version.to_s.sub(/.*,/, "")}-ubuntu-font-family-#{version.to_s.sub(/,.*/, "")}.zip"
   desc "Ubuntu"
   homepage "https://design.ubuntu.com/font/"
@@ -15,6 +15,7 @@ class FontUbuntu < Formula
     (share/"fonts").install "#{parent}ubuntu-font-family-#{version.to_s.sub(/,.*/, "")}/Ubuntu-MI.ttf"
     (share/"fonts").install "#{parent}ubuntu-font-family-#{version.to_s.sub(/,.*/, "")}/Ubuntu-R.ttf"
     (share/"fonts").install "#{parent}ubuntu-font-family-#{version.to_s.sub(/,.*/, "")}/Ubuntu-RI.ttf"
+    (share/"fonts").install "#{parent}ubuntu-font-family-#{version.to_s.sub(/,.*/, "")}/Ubuntu-Th.ttf"
     (share/"fonts").install "#{parent}ubuntu-font-family-#{version.to_s.sub(/,.*/, "")}/UbuntuMono-B.ttf"
     (share/"fonts").install "#{parent}ubuntu-font-family-#{version.to_s.sub(/,.*/, "")}/UbuntuMono-BI.ttf"
     (share/"fonts").install "#{parent}ubuntu-font-family-#{version.to_s.sub(/,.*/, "")}/UbuntuMono-R.ttf"
