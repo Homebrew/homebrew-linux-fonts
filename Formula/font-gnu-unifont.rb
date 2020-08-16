24a6eb70879937ff6189471bd4ac1d8d22259857
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-16 00:41:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gnu-unifont.rb from daf4209f69eaa42ed6c127b7947a396adb345d50

--- Formula/font-gnu-unifont.rb
+++ Formula/font-gnu-unifont.rb
@@ -1,6 +1,6 @@
 class FontGnuUnifont < Formula
-  version "13.0.02"
-  sha256 "b1aa56482e38abf3f3151472da3432fd75fadf1ed2e17f979b24c0a63002e9b9"
+  version "13.0.03"
+  sha256 "071e2ca005d97d72e79b8cb68df886f1f730cb5f0e7739a4c6e8d068ded28212"
   url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.tar.gz"
   desc "GNU Unifont"
   homepage "http://unifoundry.com/unifont.html"
