b85c3bba4c1fbc40d8f1abc8de6c1d41307760ab
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-17 00:41:24 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-harmattan.rb from be2313365ccccfa366b2a6f73ac9a71d50ee2c61

--- Formula/font-harmattan.rb
+++ Formula/font-harmattan.rb
@@ -1,8 +1,9 @@
 class FontHarmattan < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/harmattan/Harmattan-Regular.ttf"
+  head "https://github.com/google/fonts/trunk/ofl/harmattan", using: :svn, trust_cert: true
   desc "Harmattan"
   homepage "https://fonts.google.com/specimen/Harmattan"
   def install
+    (share/"fonts").install "Harmattan-Bold.ttf"
     (share/"fonts").install "Harmattan-Regular.ttf"
   end
   test do
