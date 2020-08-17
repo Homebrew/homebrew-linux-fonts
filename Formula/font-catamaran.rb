61a3bd3c99600b47fa5a14a8ce627ca8f57b0bcb
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-17 00:41:24 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-catamaran.rb from be2313365ccccfa366b2a6f73ac9a71d50ee2c61

--- Formula/font-catamaran.rb
+++ Formula/font-catamaran.rb
@@ -1,17 +1,9 @@
 class FontCatamaran < Formula
-  head "https://github.com/google/fonts/trunk/ofl/catamaran", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/raw/master/ofl/catamaran/Catamaran%5Bwght%5D.ttf"
   desc "Catamaran"
   homepage "https://fonts.google.com/specimen/Catamaran"
   def install
-    (share/"fonts").install "Catamaran-Black.ttf"
-    (share/"fonts").install "Catamaran-Bold.ttf"
-    (share/"fonts").install "Catamaran-ExtraBold.ttf"
-    (share/"fonts").install "Catamaran-ExtraLight.ttf"
-    (share/"fonts").install "Catamaran-Light.ttf"
-    (share/"fonts").install "Catamaran-Medium.ttf"
-    (share/"fonts").install "Catamaran-Regular.ttf"
-    (share/"fonts").install "Catamaran-SemiBold.ttf"
-    (share/"fonts").install "Catamaran-Thin.ttf"
+    (share/"fonts").install "Catamaran[wght].ttf"
   end
   test do
   end
