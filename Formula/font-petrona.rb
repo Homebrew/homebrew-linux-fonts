2209337e7d12072e320d497554c6417df216d497
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-17 00:41:24 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-petrona.rb from be2313365ccccfa366b2a6f73ac9a71d50ee2c61

--- Formula/font-petrona.rb
+++ Formula/font-petrona.rb
@@ -1,9 +1,10 @@
 class FontPetrona < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/petrona/Petrona-Regular.ttf"
+  head "https://github.com/google/fonts/trunk/ofl/petrona", using: :svn, trust_cert: true
   desc "Petrona"
   homepage "https://fonts.google.com/specimen/Petrona"
   def install
-    (share/"fonts").install "Petrona-Regular.ttf"
+    (share/"fonts").install "Petrona-Italic[wght].ttf"
+    (share/"fonts").install "Petrona[wght].ttf"
   end
   test do
   end
