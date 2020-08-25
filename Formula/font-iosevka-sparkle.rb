b41e2916d712f34eced91e682d4e2c71b97d1e27
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-25 00:42:23 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-sparkle.rb from a829889904fe8945c9d7c2f66042d8ae03f508b9

--- Formula/font-iosevka-sparkle.rb
+++ Formula/font-iosevka-sparkle.rb
@@ -1,6 +1,6 @@
 class FontIosevkaSparkle < Formula
-  version "3.3.1"
-  sha256 "35224c3a95f9a96d2afabe548a09ecfaa50cafb63308cd90fe227d51beaab554"
+  version "3.4.6"
+  sha256 "7200ea7e8d1e4b4f5768a0d5941de37298e16a23d0479cd1189cf2aa886c3457"
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-sparkle-#{version}.zip"
   desc "Iosevka Sparkle"
   homepage "https://github.com/be5invis/Iosevka/"
