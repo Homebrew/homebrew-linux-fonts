8e23398c69872cff1e05b5c97091cec58c3af556
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-26 00:42:22 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-simple-line-icons.rb from f01cfe6f4906442fd787891e62ca30a7ee5f76f4

--- Formula/font-simple-line-icons.rb
+++ Formula/font-simple-line-icons.rb
@@ -3,7 +3,7 @@ class FontSimpleLineIcons < Formula
   sha256 "fd0e9edd1550f8f8ded705160f0e433aae4170fe9f21a595eb583ac54c7e2d12"
   url "https://github.com/thesabbir/simple-line-icons/archive/#{version}.zip"
   desc "Simple Line Icons"
-  homepage "http://simplelineicons.com/"
+  homepage "https://simplelineicons.github.io/"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}simple-line-icons-#{version}/fonts/Simple-Line-Icons.ttf"
