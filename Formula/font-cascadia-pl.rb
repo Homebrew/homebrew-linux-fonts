950c9ee41d331195644dfdc72309887d57b84f4f
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-27 00:42:34 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cascadia-pl.rb from 0d1655a57a5ac482ffa658133efbc0dae535c31f

--- Formula/font-cascadia-pl.rb
+++ Formula/font-cascadia-pl.rb
@@ -1,12 +1,17 @@
 class FontCascadiaPl < Formula
-  version "2007.01"
-  sha256 "9f066d0d9cb2669bea2e130d7add43d496bf24ef995f42dc603fc2014574a3a4"
+  version "2008.25"
+  sha256 "4602a0b8b986b18a1c3f460fd959ec1ce522aedcf11ab4c0d41a5fc5ed839be0"
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
   desc "Cascadia PL"
   homepage "https://github.com/microsoft/cascadia-code"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}ttf/CascadiaCodePL.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodePL-Bold.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodePL-ExtraLight.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodePL-Light.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodePL-Regular.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodePL-SemiBold.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodePL-SemiLight.ttf"
   end
   test do
   end
