0aa75192d49b583f92e67fe467edd7f6f8d04f30
TANIGUCHI Masaya <mew@cat.ovh>
2020-08-27 00:42:34 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-juliamono.rb from 7900f58da1f74289cb8f0ef44fb059175a49870b

---
+++ Formula/font-juliamono.rb
@@ -0,0 +1,19 @@
+class FontJuliamono < Formula
+  version "0.009"
+  sha256 "fcb2392dec813d94c2c19621f2171493b8fd29d7a17ee1c2dd0978a64c875c55"
+  url "https://github.com/cormullion/juliamono/releases/download/v#{version}/JuliaMono.tar.gz"
+  desc "JuliaMono"
+  desc "Monospaced typeface designed for programming in Julia"
+  homepage "https://github.com/cormullion/juliamono"
+  def install
+    (share/"fonts").install "JuliaMono-Black.ttf"
+    (share/"fonts").install "JuliaMono-Bold.ttf"
+    (share/"fonts").install "JuliaMono-ExtraBold.ttf"
+    (share/"fonts").install "JuliaMono-Light.ttf"
+    (share/"fonts").install "JuliaMono-Medium.ttf"
+    (share/"fonts").install "JuliaMono-Regular.ttf"
+    (share/"fonts").install "JuliaMono-RegularLatin.ttf"
+  end
+  test do
+  end
+end
