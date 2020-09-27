5f493dffaeab99115f2590e5c9cf0c19ea7e176a
TANIGUCHI Masaya <mew@cat.ovh>
2020-09-27 00:53:22 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-red-rose.rb from e9a17550ce404bb47611689c66c75dbe86be5dac

--- Formula/font-red-rose.rb
+++ Formula/font-red-rose.rb
@@ -1,12 +1,10 @@
 class FontRedRose < Formula
-  head "https://github.com/google/fonts/trunk/ofl/redrose", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/raw/master/ofl/redrose/RedRose%5Bwght%5D.ttf"
   desc "Red Rose"
   desc "Latin display typeface designed for posters"
   homepage "https://fonts.google.com/specimen/Red+Rose"
   def install
-    (share/"fonts").install "RedRose-Bold.ttf"
-    (share/"fonts").install "RedRose-Light.ttf"
-    (share/"fonts").install "RedRose-Regular.ttf"
+    (share/"fonts").install "RedRose[wght].ttf"
   end
   test do
   end
