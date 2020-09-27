d21c9eedbd9fd380065ccffb9065a510ed7dd64c
TANIGUCHI Masaya <mew@cat.ovh>
2020-09-27 00:53:21 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cardo.rb from e1f9f9ed7ca864f223f903bc8b4abacebe8f2923

--- Formula/font-cardo.rb
+++ Formula/font-cardo.rb
@@ -1,9 +1,9 @@
 class FontCardo < Formula
   version "1.04"
   sha256 "9401db6357cb71fa1f8791323679f81d6b0473d6280a7ec8abdf11b5e52f455f"
-  url "http://scholarsfonts.net/cardo#{version.to_s.gsub(".", "")}.zip"
+  url "https://scholarsfonts.net/cardo#{version.to_s.gsub(".", "")}.zip"
   desc "Cardo"
-  homepage "http://scholarsfonts.net/cardofnt.html"
+  homepage "https://scholarsfonts.net/cardofnt.html"
   def install
     (share/"fonts").install "Cardo104s.ttf"
     (share/"fonts").install "Cardoi99.ttf"
