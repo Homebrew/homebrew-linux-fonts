ef2f549ede24b34c10114f794ca8facdd74bc3c4
TANIGUCHI Masaya <mew@cat.ovh>
2020-09-30 00:51:43 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-curly.rb from 1842c51bce68796aab0f67333fb88c86e2599c1d

--- Formula/font-iosevka-curly.rb
+++ Formula/font-iosevka-curly.rb
@@ -1,6 +1,6 @@
 class FontIosevkaCurly < Formula
-  version "3.6.1"
-  sha256 "ff7aaa44e6c4042eebf3425def0879fff4ad89f2ea05e2ccbdc531d83e3065ac"
+  version "3.6.2"
+  sha256 "5792f961e6029caed669c05d324f8ecc2fe9bf6210414d01d71f4ae12d042985"
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
   desc "Iosevka Curly"
   homepage "https://github.com/be5invis/Iosevka/"
