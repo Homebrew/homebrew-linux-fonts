7dae5c41aae63f49e4d6d60136a2d09adbaadf15
TANIGUCHI Masaya <mew@cat.ovh>
2020-10-09 00:54:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-neodunggeunmo-code.rb from cf2cb626a322b3c383df7be304a2cc56113ae4b5

--- Formula/font-neodunggeunmo-code.rb
+++ Formula/font-neodunggeunmo-code.rb
@@ -1,8 +1,9 @@
 class FontNeodunggeunmoCode < Formula
-  version "1.50"
-  sha256 "08982324e81355aca5cc0b40edad9520ccacab4457dd40b45847d6d62cdefcb0"
+  version "1.510"
+  sha256 "9a7465b01f006e3b4720a594e114a2985e7aba8694a54ce752baa76dbb677fc6"
   url "https://github.com/Dalgona/neodgm/releases/download/v#{version}/neodgm_code.ttf"
   desc "NeoDunggeunmo Code"
+  desc "Modern TrueType font based on an old-but-good Korean bitmap font"
   homepage "https://neodgm.dalgona.dev/"
   def install
     (share/"fonts").install "neodgm_code.ttf"
