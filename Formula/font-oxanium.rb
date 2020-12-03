6d19e26dfa830d4bc87ebc82a7620fdfa20b02cb
TANIGUCHI Masaya <m@docs.casa>
2020-12-03 00:14:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oxanium.rb from 8ca0c4faa9692044262c2f5c3ec186b32f4bb0f7

--- Formula/font-oxanium.rb
+++ Formula/font-oxanium.rb
@@ -1,15 +1,9 @@
 class FontOxanium < Formula
-  head "https://github.com/google/fonts/trunk/ofl/oxanium", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/raw/master/ofl/oxanium/Oxanium%5Bwght%5D.ttf"
   desc "Oxanium"
   homepage "https://fonts.google.com/specimen/Oxanium"
   def install
-    (share/"fonts").install "Oxanium-Bold.ttf"
-    (share/"fonts").install "Oxanium-ExtraBold.ttf"
-    (share/"fonts").install "Oxanium-ExtraLight.ttf"
-    (share/"fonts").install "Oxanium-Light.ttf"
-    (share/"fonts").install "Oxanium-Medium.ttf"
-    (share/"fonts").install "Oxanium-Regular.ttf"
-    (share/"fonts").install "Oxanium-SemiBold.ttf"
+    (share/"fonts").install "Oxanium[wght].ttf"
   end
   test do
   end
