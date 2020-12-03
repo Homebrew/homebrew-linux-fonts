0445c5f3e171246e35be2df6bc40a9a476905ed0
TANIGUCHI Masaya <m@docs.casa>
2020-12-03 00:14:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-poprumcute.rb from e98cc35d1a25e407556d2c16768ae72b66da68ac

---
+++ Formula/font-poprumcute.rb
@@ -0,0 +1,12 @@
+class FontPoprumcute < Formula
+  head "https://moji-waku.com/download/poprumcute.zip"
+  desc "PopRumCute"
+  desc "Modern Showa styled Japanese font"
+  homepage "https://moji-waku.com/poprumcute/index.html"
+  def install
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}poprumcute/PopRumCute.otf"
+  end
+  test do
+  end
+end
