f053f538a360f4338c3e18744c1c86f8a4f5c7e9
TANIGUCHI Masaya <m@docs.casa>
2020-12-03 00:14:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-assistant.rb from 8ca0c4faa9692044262c2f5c3ec186b32f4bb0f7

--- Formula/font-assistant.rb
+++ Formula/font-assistant.rb
@@ -1,14 +1,9 @@
 class FontAssistant < Formula
-  head "https://github.com/google/fonts/trunk/ofl/assistant", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/raw/master/ofl/assistant/Assistant%5Bwght%5D.ttf"
   desc "Assistant"
   homepage "https://fonts.google.com/specimen/Assistant"
   def install
-    (share/"fonts").install "Assistant-Bold.ttf"
-    (share/"fonts").install "Assistant-ExtraBold.ttf"
-    (share/"fonts").install "Assistant-ExtraLight.ttf"
-    (share/"fonts").install "Assistant-Light.ttf"
-    (share/"fonts").install "Assistant-Regular.ttf"
-    (share/"fonts").install "Assistant-SemiBold.ttf"
+    (share/"fonts").install "Assistant[wght].ttf"
   end
   test do
   end
