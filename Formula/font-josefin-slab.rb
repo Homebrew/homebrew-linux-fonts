84669d6574608e16c452e4feb27be76845f3a1ff
TANIGUCHI Masaya <m@docs.casa>
2020-12-03 00:14:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-josefin-slab.rb from 8ca0c4faa9692044262c2f5c3ec186b32f4bb0f7

--- Formula/font-josefin-slab.rb
+++ Formula/font-josefin-slab.rb
@@ -3,16 +3,8 @@ class FontJosefinSlab < Formula
   desc "Josefin Slab"
   homepage "https://fonts.google.com/specimen/Josefin+Slab"
   def install
-    (share/"fonts").install "JosefinSlab-Bold.ttf"
-    (share/"fonts").install "JosefinSlab-BoldItalic.ttf"
-    (share/"fonts").install "JosefinSlab-Italic.ttf"
-    (share/"fonts").install "JosefinSlab-Light.ttf"
-    (share/"fonts").install "JosefinSlab-LightItalic.ttf"
-    (share/"fonts").install "JosefinSlab-Regular.ttf"
-    (share/"fonts").install "JosefinSlab-SemiBold.ttf"
-    (share/"fonts").install "JosefinSlab-SemiBoldItalic.ttf"
-    (share/"fonts").install "JosefinSlab-Thin.ttf"
-    (share/"fonts").install "JosefinSlab-ThinItalic.ttf"
+    (share/"fonts").install "JosefinSlab-Italic[wght].ttf"
+    (share/"fonts").install "JosefinSlab[wght].ttf"
   end
   test do
   end
