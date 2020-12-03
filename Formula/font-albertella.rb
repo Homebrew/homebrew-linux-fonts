3b13c41a4ad192dfb6a5682b34c6dbb7061b6427
TANIGUCHI Masaya <m@docs.casa>
2020-12-03 00:14:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-albertella.rb from 46d588e36d8d5b0f2f9ca6a278c1fedf87bfe0b0

---
+++ Formula/font-albertella.rb
@@ -0,0 +1,12 @@
+class FontAlbertella < Formula
+  head "https://moji-waku.com/download/albertella.zip"
+  desc "Albertella"
+  desc "Ancient biology styled font inspired by trilobite"
+  homepage "https://moji-waku.com/albertella/index.html"
+  def install
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}albertella/Albertella-Light.otf"
+  end
+  test do
+  end
+end
