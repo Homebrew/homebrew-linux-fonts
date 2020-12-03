b8c572a46345898d6ca3c1849dfaa48e50c3788f
TANIGUCHI Masaya <m@docs.casa>
2020-12-03 00:14:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pigmo01.rb from 80cb4841a4aa6844d66091f23b85ad950c73f006

---
+++ Formula/font-pigmo01.rb
@@ -0,0 +1,12 @@
+class FontPigmo01 < Formula
+  head "https://moji-waku.com/download/pigmo01.zip"
+  desc "Pigmo 01"
+  desc "Cute drawing styled Japanese font"
+  homepage "https://moji-waku.com/pigmo01/index.html"
+  def install
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}pigmo01/Pigmo-01.otf"
+  end
+  test do
+  end
+end
