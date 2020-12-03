ad7a2adad1704b7b81d932b79edd8011e6266fb0
TANIGUCHI Masaya <m@docs.casa>
2020-12-03 00:14:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-sans-pro.rb from 2160d38b22fe6cd40c2b070c1b5faeda0a1259d6

--- Formula/font-source-sans-pro.rb
+++ Formula/font-source-sans-pro.rb
@@ -1,25 +1,15 @@
 class FontSourceSansPro < Formula
-  version "3.006R"
-  sha256 "b22c7a4c45050ed8ae1b5293a9fd3b886d6f5150eb1e2ded429b9a11ac1602c5"
-  url "https://github.com/adobe-fonts/source-sans-pro/releases/download/#{version}/source-sans-pro-#{version}.zip"
+  version "3.028R"
+  sha256 "1186d594ecb1336679a1d102bb6ddbe6041f0fd029482e53f02e46aa3ab011cf"
+  url "https://github.com/adobe-fonts/source-sans-pro/releases/download/#{version}/source-sans-#{version.to_s.sub(/\./, "
+  v ")}.zip"
   desc "Source Sans Pro"
+  desc "Fonts designed for user interfaces"
   homepage "https://github.com/adobe-fonts/source-sans-pro"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-Black.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-BlackIt.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-Bold.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-BoldIt.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-ExtraLight.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-ExtraLightIt.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-It.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-Light.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-LightIt.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-Regular.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-Semibold.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/OTF/SourceSansPro-SemiboldIt.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/VAR/SourceSansVariable-Italic.otf"
-    (share/"fonts").install "#{parent}source-sans-pro-#{version}/VAR/SourceSansVariable-Roman.otf"
+    (share/"fonts").install "#{parent}VAR/SourceSans3VF-Italic.otf"
+    (share/"fonts").install "#{parent}VAR/SourceSans3VF-Roman.otf"
   end
   test do
   end
