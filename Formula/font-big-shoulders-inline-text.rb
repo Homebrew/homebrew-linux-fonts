571c7f440b4d5d4b1bf591eb91af4a0806fa1171
TANIGUCHI Masaya <m@docs.casa>
2020-12-06 00:15:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-big-shoulders-inline-text.rb from e241d7d238eb50fb97004d9d179f36b1bb5ef9ae

---
+++ Formula/font-big-shoulders-inline-text.rb
@@ -0,0 +1,18 @@
+class FontBigShouldersInlineText < Formula
+  head "https://github.com/google/fonts/trunk/ofl/bigshouldersinlinetext", using: :svn, trust_cert: true, verified: "github.com/google/fonts/"
+  desc "Big Shoulders Inline Text"
+  desc "Superfamily of condensed American Gothic typefaces"
+  homepage "https://fonts.google.com/specimen/Big+Shoulders+Inline+Text"
+  def install
+    (share/"fonts").install "BigShouldersInlineText-Black.ttf"
+    (share/"fonts").install "BigShouldersInlineText-Bold.ttf"
+    (share/"fonts").install "BigShouldersInlineText-ExtraBold.ttf"
+    (share/"fonts").install "BigShouldersInlineText-Light.ttf"
+    (share/"fonts").install "BigShouldersInlineText-Medium.ttf"
+    (share/"fonts").install "BigShouldersInlineText-Regular.ttf"
+    (share/"fonts").install "BigShouldersInlineText-SemiBold.ttf"
+    (share/"fonts").install "BigShouldersInlineText-Thin.ttf"
+  end
+  test do
+  end
+end
