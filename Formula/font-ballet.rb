cb5eb68fcd18a4782c3b4312df0f30a4cf88e8f7
TANIGUCHI Masaya <m@docs.casa>
2020-12-06 00:15:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ballet.rb from e241d7d238eb50fb97004d9d179f36b1bb5ef9ae

---
+++ Formula/font-ballet.rb
@@ -0,0 +1,11 @@
+class FontBallet < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/ballet/Ballet%5Bopsz%5D.ttf", verified: "github.com/google/fonts/"
+  desc "Ballet"
+  desc "Designed by maximiliano sproviero and developed by eduardo tunni"
+  homepage "https://fonts.google.com/specimen/Ballet"
+  def install
+    (share/"fonts").install "Ballet[opsz].ttf"
+  end
+  test do
+  end
+end
