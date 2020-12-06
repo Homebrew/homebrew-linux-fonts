76d45bfe3603aef65b3263c00bdb263ec74afe7a
TANIGUCHI Masaya <m@docs.casa>
2020-12-06 00:15:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-big-shoulders-stencil-text.rb from e241d7d238eb50fb97004d9d179f36b1bb5ef9ae

---
+++ Formula/font-big-shoulders-stencil-text.rb
@@ -0,0 +1,18 @@
+class FontBigShouldersStencilText < Formula
+  head "https://github.com/google/fonts/trunk/ofl/bigshouldersstenciltext", using: :svn, trust_cert: true, verified: "github.com/google/fonts/"
+  desc "Big Shoulders Stencil Text"
+  desc "Superfamily of condensed American Gothic typefaces"
+  homepage "https://fonts.google.com/specimen/Big+Shoulders+Stencil+Text"
+  def install
+    (share/"fonts").install "BigShouldersStencilText-Black.ttf"
+    (share/"fonts").install "BigShouldersStencilText-Bold.ttf"
+    (share/"fonts").install "BigShouldersStencilText-ExtraBold.ttf"
+    (share/"fonts").install "BigShouldersStencilText-Light.ttf"
+    (share/"fonts").install "BigShouldersStencilText-Medium.ttf"
+    (share/"fonts").install "BigShouldersStencilText-Regular.ttf"
+    (share/"fonts").install "BigShouldersStencilText-SemiBold.ttf"
+    (share/"fonts").install "BigShouldersStencilText-Thin.ttf"
+  end
+  test do
+  end
+end
