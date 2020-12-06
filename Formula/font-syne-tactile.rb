95ed5dcf87f67feaeba1e7cacea3dc6a8160cbb5
TANIGUCHI Masaya <m@docs.casa>
2020-12-06 00:15:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-syne-tactile.rb from e241d7d238eb50fb97004d9d179f36b1bb5ef9ae

---
+++ Formula/font-syne-tactile.rb
@@ -0,0 +1,11 @@
+class FontSyneTactile < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/synetactile/SyneTactile-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Syne Tactile"
+  desc "Originally designed for the Art Center 'Synesthésie'"
+  homepage "https://fonts.google.com/specimen/Syne+Tactile"
+  def install
+    (share/"fonts").install "SyneTactile-Regular.ttf"
+  end
+  test do
+  end
+end
