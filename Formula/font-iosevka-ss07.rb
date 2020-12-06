fd4844b366edd4d648d500d02deaf31de65d98d7
TANIGUCHI Masaya <m@docs.casa>
2020-12-06 00:15:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss07.rb from ec4f69e45fa41caa2fd78f64a4e1b524c18c2f9a

--- Formula/font-iosevka-ss07.rb
+++ Formula/font-iosevka-ss07.rb
@@ -1,6 +1,6 @@
 class FontIosevkaSs07 < Formula
-  version "4.0.0"
-  sha256 "86238a41fbf049953def57a7cb733a8879386135d36a76e06d959456ba1ea307"
+  version "4.0.1"
+  sha256 "9b5a2ad3463bcccb55117573b0e6cac4197321335d6c74b199a5727c878c369b"
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss07-#{version}.zip"
   desc "Iosevka SS07"
   homepage "https://github.com/be5invis/Iosevka/"
