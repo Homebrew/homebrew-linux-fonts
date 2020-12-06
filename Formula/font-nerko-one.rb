3494bcf5b9289d03c27a7598bd5e0c75cb1d762b
TANIGUCHI Masaya <m@docs.casa>
2020-12-06 00:15:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nerko-one.rb from e241d7d238eb50fb97004d9d179f36b1bb5ef9ae

---
+++ Formula/font-nerko-one.rb
@@ -0,0 +1,11 @@
+class FontNerkoOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/nerkoone/NerkoOne-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Nerko One"
+  desc "Chunky ‘marker’ effect font"
+  homepage "https://fonts.google.com/specimen/Nerko+One"
+  def install
+    (share/"fonts").install "NerkoOne-Regular.ttf"
+  end
+  test do
+  end
+end
