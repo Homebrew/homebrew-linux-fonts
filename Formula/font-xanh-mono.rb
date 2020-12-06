335ba571384ae39577e4659b90d0d8216a336283
TANIGUCHI Masaya <m@docs.casa>
2020-12-06 00:15:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-xanh-mono.rb from e241d7d238eb50fb97004d9d179f36b1bb5ef9ae

---
+++ Formula/font-xanh-mono.rb
@@ -0,0 +1,12 @@
+class FontXanhMono < Formula
+  head "https://github.com/google/fonts/trunk/ofl/xanhmono", using: :svn, trust_cert: true, verified: "github.com/google/fonts/"
+  desc "Xanh Mono"
+  desc "Mono-serif typeface, designed by lam bao and duy dao"
+  homepage "https://fonts.google.com/specimen/Xanh+Mono"
+  def install
+    (share/"fonts").install "XanhMono-Italic.ttf"
+    (share/"fonts").install "XanhMono-Regular.ttf"
+  end
+  test do
+  end
+end
