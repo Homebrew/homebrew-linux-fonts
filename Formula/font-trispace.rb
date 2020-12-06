b74584dfc586ccfe3994fc36c2c4a0e5b0ef366b
TANIGUCHI Masaya <m@docs.casa>
2020-12-06 00:15:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-trispace.rb from e241d7d238eb50fb97004d9d179f36b1bb5ef9ae

---
+++ Formula/font-trispace.rb
@@ -0,0 +1,11 @@
+class FontTrispace < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/trispace/Trispace%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
+  desc "Trispace"
+  desc "Designed by tyler finck (etc)"
+  homepage "https://fonts.google.com/specimen/Trispace"
+  def install
+    (share/"fonts").install "Trispace[wdth,wght].ttf"
+  end
+  test do
+  end
+end
