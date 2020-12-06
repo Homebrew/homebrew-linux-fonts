9101e92cd8175324467dc58b1d887ac405e9b393
TANIGUCHI Masaya <m@docs.casa>
2020-12-06 00:15:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-big-shoulders-inline-display.rb from e241d7d238eb50fb97004d9d179f36b1bb5ef9ae

---
+++ Formula/font-big-shoulders-inline-display.rb
@@ -0,0 +1,18 @@
+class FontBigShouldersInlineDisplay < Formula
+  head "https://github.com/google/fonts/trunk/ofl/bigshouldersinlinedisplay", using: :svn, trust_cert: true, verified: "github.com/google/fonts/"
+  desc "Big Shoulders Inline Display"
+  desc "Superfamily of condensed American Gothic typefaces"
+  homepage "https://fonts.google.com/specimen/Big+Shoulders+Inline+Display"
+  def install
+    (share/"fonts").install "BigShouldersInlineDisplay-Black.ttf"
+    (share/"fonts").install "BigShouldersInlineDisplay-Bold.ttf"
+    (share/"fonts").install "BigShouldersInlineDisplay-ExtraBold.ttf"
+    (share/"fonts").install "BigShouldersInlineDisplay-Light.ttf"
+    (share/"fonts").install "BigShouldersInlineDisplay-Medium.ttf"
+    (share/"fonts").install "BigShouldersInlineDisplay-Regular.ttf"
+    (share/"fonts").install "BigShouldersInlineDisplay-SemiBold.ttf"
+    (share/"fonts").install "BigShouldersInlineDisplay-Thin.ttf"
+  end
+  test do
+  end
+end
