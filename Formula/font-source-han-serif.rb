5b94608c5c8acace832eb914d31cbd4b0e1e0ee3
TANIGUCHI Masaya <m@docs.casa>
2020-12-08 00:14:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-han-serif.rb from 528eff38de4db47638c8dac014d9ba2ac13b0d17

--- Formula/font-source-han-serif.rb
+++ Formula/font-source-han-serif.rb
@@ -3,6 +3,10 @@ class FontSourceHanSerif < Formula
   sha256 "85cc634fa228286ca307803bbb4ca61f61fa821b3ed573f4f07c6f0c064426b5"
   url "https://github.com/adobe-fonts/source-han-serif/releases/download/1.001R/SourceHanSerif.ttc"
   desc "Source Han Serif"
+  desc "思源宋體"
+  desc "源ノ明朝"
+  desc "본명조"
+  desc "OpenType/CFF Pan-CJK fonts"
   homepage "https://github.com/adobe-fonts/source-han-serif"
   def install
     (share/"fonts").install "SourceHanSerif.ttc"
