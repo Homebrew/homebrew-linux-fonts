2ba5cf0e0805cd6e8e838d516517133476266600
TANIGUCHI Masaya <m@docs.casa>
2020-12-08 00:14:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-glow-sans-sc-normal.rb from 035d023cff220e1b98f33b09f3517af48fa27f65

--- Formula/font-glow-sans-sc-normal.rb
+++ Formula/font-glow-sans-sc-normal.rb
@@ -1,6 +1,6 @@
 class FontGlowSansScNormal < Formula
-  version "0.9"
-  sha256 "0652f5e9bfaf7e4fe19b9c4b605c546e79828cf4779f63467411e907f482213b"
+  version "0.91"
+  sha256 "222be92818f7d8edb5432ef709b3790bf515996822975aead5040aa70ca93eda"
   url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansSC-Normal-v#{version}.zip"
   desc "Glow Sans SC Normal"
   homepage "https://github.com/welai/glow-sans"
