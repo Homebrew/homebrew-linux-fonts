9aa98092197207ab46e269e0d065fe68f1b21be1
TANIGUCHI Masaya <m@docs.casa>
2020-12-08 12:21:59 +0900

    Support string interpolation, which closes #23, closes #24

--- Formula/font-source-sans-pro.rb
+++ Formula/font-source-sans-pro.rb
@@ -1,8 +1,7 @@
 class FontSourceSansPro < Formula
   version "3.028R"
   sha256 "1186d594ecb1336679a1d102bb6ddbe6041f0fd029482e53f02e46aa3ab011cf"
-  url "https://github.com/adobe-fonts/source-sans-pro/releases/download/#{version}/source-sans-#{version.to_s.sub(/\./, "
-  v ")}.zip"
+  url "https://github.com/adobe-fonts/source-sans-pro/releases/download/#{version}/source-sans-#{version.to_s.sub(/\./, "v")}.zip"
   desc "Source Sans Pro"
   desc "Fonts designed for user interfaces"
   homepage "https://github.com/adobe-fonts/source-sans-pro"
