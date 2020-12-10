ea24247872bea97a37c298596da243ed698faca9
TANIGUCHI Masaya <m@docs.casa>
2020-12-10 00:15:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-baloo-thambi-2.rb from 484ffd9d9d612d67228af86d8abc13e8c7611b27

--- Formula/font-baloo-thambi-2.rb
+++ Formula/font-baloo-thambi-2.rb
@@ -1,5 +1,5 @@
 class FontBalooThambi2 < Formula
-  head "https://github.com/google/fonts/trunk/ofl/baloothambi2", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/baloothambi2", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
   desc "Baloo Thambi 2"
   homepage "https://fonts.google.com/specimen/Baloo+Thambi+2"
   def install
