61babea1dedd2ea79e5b5f7d836351dea25fd0ea
TANIGUCHI Masaya <m@docs.casa>
2020-12-10 00:15:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bhavuka.rb from 77ef05d6de49f00f22aff1909de928ce9243fa31

--- Formula/font-bhavuka.rb
+++ Formula/font-bhavuka.rb
@@ -1,5 +1,5 @@
 class FontBhavuka < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/bhavuka/Bhavuka-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/bhavuka/Bhavuka-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Bhavuka"
   homepage "https://fonts.google.com/specimen/Bhavuka"
   def install
