a44dd90bbf38972bdc96fd8d055becef0c200a13
TANIGUCHI Masaya <m@docs.casa>
2020-12-10 00:15:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bowlby-one-sc.rb from ad52e107b5b9012622ced5b39f5716f323cee04c

--- Formula/font-bowlby-one-sc.rb
+++ Formula/font-bowlby-one-sc.rb
@@ -1,5 +1,5 @@
 class FontBowlbyOneSc < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/bowlbyonesc/BowlbyOneSC-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/bowlbyonesc/BowlbyOneSC-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Bowlby One SC"
   homepage "https://fonts.google.com/specimen/Bowlby+One+SC"
   def install
