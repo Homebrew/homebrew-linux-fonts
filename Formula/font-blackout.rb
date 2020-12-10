2c28e24c94afe176cc76e53119a98fddd7cdcb6d
TANIGUCHI Masaya <m@docs.casa>
2020-12-10 00:15:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-blackout.rb from c7911e16a3659ab227f65b31b7744513b84be6dc

--- Formula/font-blackout.rb
+++ Formula/font-blackout.rb
@@ -1,5 +1,5 @@
 class FontBlackout < Formula
-  head "https://github.com/theleagueof/blackout/archive/master.zip"
+  head "https://github.com/theleagueof/blackout/archive/master.zip", verified: "github.com/theleagueof/blackout/"
   desc "Blackout"
   homepage "https://www.theleagueofmoveabletype.com/blackout"
   def install
