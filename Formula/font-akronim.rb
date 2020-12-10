03583268c69c31aea5c55b4fba3d1376c5760f7c
TANIGUCHI Masaya <m@docs.casa>
2020-12-10 00:15:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-akronim.rb from 427a29dc0e97f4b7863ea780f93a4b7324e6f189

--- Formula/font-akronim.rb
+++ Formula/font-akronim.rb
@@ -1,5 +1,5 @@
 class FontAkronim < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/akronim/Akronim-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/akronim/Akronim-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Akronim"
   homepage "https://fonts.google.com/specimen/Akronim"
   def install
