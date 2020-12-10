fbf2780e9c5c30f8023ede146582d3d96d107cf1
TANIGUCHI Masaya <m@docs.casa>
2020-12-10 00:15:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aldrich.rb from 643d54637651a6fa315e8e445f8944602ca2ebd6

--- Formula/font-aldrich.rb
+++ Formula/font-aldrich.rb
@@ -1,5 +1,5 @@
 class FontAldrich < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/aldrich/Aldrich-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/aldrich/Aldrich-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Aldrich"
   homepage "https://fonts.google.com/specimen/Aldrich"
   def install
