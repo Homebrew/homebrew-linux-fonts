2850e9fe7b0eb2f0b7df22377201dc43b9f075c6
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-foundry-sterling.rb from 1d05a6227e3d51fa4fae073348c3f5c603d9f51f

--- Formula/font-foundry-sterling.rb
+++ Formula/font-foundry-sterling.rb
@@ -1,6 +1,5 @@
 class FontFoundrySterling < Formula
   version "001.005"
-  sha256 "53ad432d4ce9c4a0f880351a91ee68fb37ee51c8c21ca4f2f172b62fbc862b6f"
   url "https://fontlot.com/wp-content/uploads/2017/09/foundrysterling.zip"
   desc "Foundry Sterling"
   homepage "https://fontlot.com/7134/foundry-sterling/"
