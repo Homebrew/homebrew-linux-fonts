63bff4340b3d1e0b3b4e5b278b9a870f84f46af2
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-african-sans.rb from 1d05a6227e3d51fa4fae073348c3f5c603d9f51f

--- Formula/font-african-sans.rb
+++ Formula/font-african-sans.rb
@@ -1,6 +1,5 @@
 class FontAfricanSans < Formula
   version "9.380"
-  sha256 "66176e5517ace047affc23ca7b7137018c907eee00b113666a671f31254ac7a4"
   url "https://www.languagegeek.com/font/AfricanSans.zip"
   desc "African Sans"
   homepage "https://www.languagegeek.com/font/fontdownload.html"
