1ebe1acb287f29d32a5019a9fa77f2b41de00db3
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-national-park.rb from 1d05a6227e3d51fa4fae073348c3f5c603d9f51f

--- Formula/font-national-park.rb
+++ Formula/font-national-park.rb
@@ -1,6 +1,5 @@
 class FontNationalPark < Formula
   version "1.000"
-  sha256 "b03567a235e0a81dca30f961704f8f0a352e2dd40a5f91bbce111b31bee17322"
   url "https://files.cargocollective.com/c206464/NationalPark.zip"
   desc "National Park"
   homepage "https://nationalparktypeface.com/"
