5837eba06cb511458c148e1408079f36d6d16402
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-times-newer-roman.rb from 1d05a6227e3d51fa4fae073348c3f5c603d9f51f

--- Formula/font-times-newer-roman.rb
+++ Formula/font-times-newer-roman.rb
@@ -1,6 +1,5 @@
 class FontTimesNewerRoman < Formula
   version "1.000"
-  sha256 "2f4d66839d5b72105269d3a9d019ec19f0946280b33b2437460364c0d98dadba"
   url "https://timesnewerroman.com/assets/TimesNewerRoman.zip"
   desc "Times Newer Roman"
   desc "Looks just like Times New Roman, but characters are 5-10% wider"
