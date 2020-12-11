8dab0aca273479fd615ab49f163080ed1559e9a9
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-charm.rb from e1b14924ab59764d28695efa45b6d5059d525204

--- Formula/font-charm.rb
+++ Formula/font-charm.rb
@@ -1,5 +1,5 @@
 class FontCharm < Formula
-  head "https://github.com/google/fonts/trunk/ofl/charm", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/charm", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
   desc "Charm"
   homepage "https://fonts.google.com/specimen/Charm"
   def install
