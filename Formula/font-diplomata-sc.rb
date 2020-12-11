678e0a106fdff63543d970dfd54007775abdeb95
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-diplomata-sc.rb from 3bef2d3346d4e581f23aba652247f4a939725290

--- Formula/font-diplomata-sc.rb
+++ Formula/font-diplomata-sc.rb
@@ -1,5 +1,5 @@
 class FontDiplomataSc < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/diplomatasc/DiplomataSC-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/diplomatasc/DiplomataSC-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Diplomata SC"
   homepage "https://fonts.google.com/specimen/Diplomata+SC"
   def install
