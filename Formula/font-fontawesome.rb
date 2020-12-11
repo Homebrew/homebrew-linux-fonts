e6503808a2702f8aee07a85b5b8c3e3b86548a9c
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fontawesome.rb from 9ecbb66b462fc02783a172438f060cfb32f99280

--- Formula/font-fontawesome.rb
+++ Formula/font-fontawesome.rb
@@ -1,8 +1,9 @@
 class FontFontawesome < Formula
-  version "5.14.0"
-  sha256 "3295a2578a9fec3bf12f485a3ddba23eea8eab5802580c3de81ebc292405eeeb"
-  url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip"
+  version "5.15.1"
+  sha256 "d269f26f3bc10925f6b33632e9dd877e6cdf0326f78dae6a47a60479ff1eb743"
+  url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip", verified: "github.com/FortAwesome/Font-Awesome/"
   desc "Font Awesome"
+  desc "Icon set and toolkit"
   homepage "https://fontawesome.com/"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
