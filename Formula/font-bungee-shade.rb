e751b08bcf2b57eb15d365b4504af283cf8da6e7
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bungee-shade.rb from bf98787536ad711fbe421e34fac0d56b1439312f

--- Formula/font-bungee-shade.rb
+++ Formula/font-bungee-shade.rb
@@ -1,5 +1,5 @@
 class FontBungeeShade < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/bungeeshade/BungeeShade-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/bungeeshade/BungeeShade-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Bungee Shade"
   homepage "https://fonts.google.com/specimen/Bungee+Shade"
   def install
