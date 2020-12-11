113d34b15a4e1224a4110cab13c1ca7ef83a940c
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fanwood-text.rb from 3ed90a6055d346575ec817e0d960444a6e434250

--- Formula/font-fanwood-text.rb
+++ Formula/font-fanwood-text.rb
@@ -1,5 +1,5 @@
 class FontFanwoodText < Formula
-  head "https://github.com/google/fonts/trunk/ofl/fanwoodtext", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/fanwoodtext", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
   desc "Fanwood Text"
   homepage "https://fonts.google.com/specimen/Fanwood+Text"
   def install
