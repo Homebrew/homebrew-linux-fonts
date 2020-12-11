dc7d4b275f335cd4602ca8adcb171a6cd152ea7a
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-infini.rb from 1d05a6227e3d51fa4fae073348c3f5c603d9f51f

--- Formula/font-infini.rb
+++ Formula/font-infini.rb
@@ -1,6 +1,5 @@
 class FontInfini < Formula
   version "1.003"
-  sha256 "410d742658b3da3f5e7446a8aa18cedb3be12b26b0e7eebe5b7c830058be56b5"
   url "http://www.cnap.graphismeenfrance.fr/infini/en/download/Infini-cnap-engl_fonts-specimen-v2.zip"
   desc "Infini"
   homepage "http://www.cnap.graphismeenfrance.fr/infini/en/"
