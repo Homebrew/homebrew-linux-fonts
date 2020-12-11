2e6540db3cc4b9a80369d39f1d860cb339194871
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chelsea-market.rb from 2b080f187f3cfd84a24912ef09ac127723aa726e

--- Formula/font-chelsea-market.rb
+++ Formula/font-chelsea-market.rb
@@ -1,5 +1,5 @@
 class FontChelseaMarket < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/chelseamarket/ChelseaMarket-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/chelseamarket/ChelseaMarket-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Chelsea Market"
   homepage "https://fonts.google.com/specimen/Chelsea+Market"
   def install
