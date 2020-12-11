d76cda9b647b470c9714b0c8441b1270ddc0de30
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-galada.rb from 8a3c148db5618571caa0d86d6af2ccdd30fe3b21

--- Formula/font-galada.rb
+++ Formula/font-galada.rb
@@ -1,5 +1,5 @@
 class FontGalada < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/galada/Galada-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/galada/Galada-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Galada"
   homepage "https://fonts.google.com/specimen/Galada"
   def install
