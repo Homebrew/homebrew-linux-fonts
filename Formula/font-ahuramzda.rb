27d7cdf519a47f16960f27a6be1dd8fd472f60a0
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ahuramzda.rb from 1d05a6227e3d51fa4fae073348c3f5c603d9f51f

--- Formula/font-ahuramzda.rb
+++ Formula/font-ahuramzda.rb
@@ -1,6 +1,5 @@
 class FontAhuramzda < Formula
   version "1.000"
-  sha256 "5a8afb0b24ceeb98f1ef121406ceecb124f2a300c5ee7877a7ff6abdd697b160"
   url "https://fontlibrary.org/assets/downloads/ahuramazda/b2c0eeb9186f389749746f075b5a1abf/ahuramazda.zip"
   desc "Ahuramzda"
   homepage "https://fontlibrary.org/font/ahuramazda/"
