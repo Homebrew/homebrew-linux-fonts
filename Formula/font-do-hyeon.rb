c493cd11cb2387f6b2fe14bacfbefa702193a653
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-do-hyeon.rb from 00e89616d23abca1cf3ce96f80ee51f579cf6b0c

--- Formula/font-do-hyeon.rb
+++ Formula/font-do-hyeon.rb
@@ -1,5 +1,5 @@
 class FontDoHyeon < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/dohyeon/DoHyeon-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/dohyeon/DoHyeon-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Do Hyeon"
   homepage "https://fonts.google.com/specimen/Do+Hyeon"
   def install
