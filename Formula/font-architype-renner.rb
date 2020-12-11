d6735311c8a23225145de0ff8a65345a1d9909ea
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:11 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-architype-renner.rb from 1d05a6227e3d51fa4fae073348c3f5c603d9f51f

--- Formula/font-architype-renner.rb
+++ Formula/font-architype-renner.rb
@@ -1,6 +1,5 @@
 class FontArchitypeRenner < Formula
   version "001.000"
-  sha256 "8a8de50838960d7e597b5eda4214c22418b32121505a61f3d0bbb120ff7fd24a"
   url "https://fontlot.com/wp-content/uploads/2017/08/archityperenner.zip"
   desc "Architype Renner"
   homepage "https://fontlot.com/4598/architype-renner/"
