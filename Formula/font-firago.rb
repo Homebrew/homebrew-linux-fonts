ef25fcc6dc9d1ae50038e4a68d8e471c1749c2b6
TANIGUCHI Masaya <m@docs.casa>
2020-12-11 00:15:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-firago.rb from 1d91a68439b1cfb95a28b7a67c7cf5c330a3287d

--- Formula/font-firago.rb
+++ Formula/font-firago.rb
@@ -1,7 +1,7 @@
 class FontFirago < Formula
   version "1.000"
   sha256 "b2f97f1ee02921ca1776903fa0e6f1358b017bf854c0e8776b6b8512e3c9d4a1"
-  url "https://github.com/bBoxType/FiraGO/archive/#{version}.tar.gz"
+  url "https://github.com/bBoxType/FiraGO/archive/#{version}.tar.gz", verified: "github.com/bBoxType/FiraGO/"
   desc "FiraGO"
   homepage "https://bboxtype.com/typefaces/FiraGO/"
   def install
