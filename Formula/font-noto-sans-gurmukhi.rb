9237be78e358a4b612dd4fda87e6e4eee9be5754
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-gurmukhi.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-gurmukhi.rb
+++ Formula/font-noto-sans-gurmukhi.rb
@@ -1,5 +1,5 @@
 class FontNotoSansGurmukhi < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGurmukhi-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGurmukhi-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Gurmukhi"
   homepage "https://www.google.com/get/noto/#sans-guru"
   def install
