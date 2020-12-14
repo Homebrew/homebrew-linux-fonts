0cd76ea995c7b455f65bb2390b5828edd103fa61
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-glagolitic.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-glagolitic.rb
+++ Formula/font-noto-sans-glagolitic.rb
@@ -1,5 +1,5 @@
 class FontNotoSansGlagolitic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGlagolitic-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGlagolitic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Glagolitic"
   homepage "https://www.google.com/get/noto/#sans-glag"
   def install
