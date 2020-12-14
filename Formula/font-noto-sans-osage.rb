bd76aa52eea619a2aee53f3471b7454328d75ac8
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-osage.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-osage.rb
+++ Formula/font-noto-sans-osage.rb
@@ -1,5 +1,5 @@
 class FontNotoSansOsage < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOsage-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOsage-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Osage"
   homepage "https://www.google.com/get/noto/#sans-osge"
   def install
