c8bb312062624ecb5ad09a4cace8c73e3e5bead4
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-batak.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-batak.rb
+++ Formula/font-noto-sans-batak.rb
@@ -1,5 +1,5 @@
 class FontNotoSansBatak < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBatak-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBatak-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Batak"
   homepage "https://www.google.com/get/noto/#sans-batk"
   def install
