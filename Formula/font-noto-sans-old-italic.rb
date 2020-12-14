9922d607b54b559497f1767a52d232dd0a49fae6
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-old-italic.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-old-italic.rb
+++ Formula/font-noto-sans-old-italic.rb
@@ -1,5 +1,5 @@
 class FontNotoSansOldItalic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldItalic-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldItalic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Old Italic"
   homepage "https://www.google.com/get/noto/#sans-ital"
   def install
