49ab419fde469930726feecdf0f48535243c80c8
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-seaweed-script.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-seaweed-script.rb
+++ Formula/font-seaweed-script.rb
@@ -1,5 +1,5 @@
 class FontSeaweedScript < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/seaweedscript/SeaweedScript-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/seaweedscript/SeaweedScript-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Seaweed Script"
   homepage "https://fonts.google.com/specimen/Seaweed+Script"
   def install
