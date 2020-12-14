04a75efcd77955124af75f8fd7169952536c79c1
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-ol-chiki.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-ol-chiki.rb
+++ Formula/font-noto-sans-ol-chiki.rb
@@ -1,5 +1,5 @@
 class FontNotoSansOlChiki < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOlChiki-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOlChiki-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Ol Chiki"
   homepage "https://www.google.com/get/noto/#sans-olck"
   def install
