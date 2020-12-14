fbbf4c57fb4d3b03db10b3daf5e1883ddcb7778e
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-metamorphous.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-metamorphous.rb
+++ Formula/font-metamorphous.rb
@@ -1,5 +1,5 @@
 class FontMetamorphous < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/metamorphous/Metamorphous-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/metamorphous/Metamorphous-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Metamorphous"
   homepage "https://fonts.google.com/specimen/Metamorphous"
   def install
