16aa3b1c05ae2e7590bf218ccd28b89677de9a59
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-brahmi.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-brahmi.rb
+++ Formula/font-noto-sans-brahmi.rb
@@ -1,5 +1,5 @@
 class FontNotoSansBrahmi < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBrahmi-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBrahmi-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Brahmi"
   homepage "https://www.google.com/get/noto/#sans-brah"
   def install
