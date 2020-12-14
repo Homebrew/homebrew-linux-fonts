d3c9d82584eede40b80190ba3d0b9e66568faa42
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:36 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mansalva.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-mansalva.rb
+++ Formula/font-mansalva.rb
@@ -1,5 +1,5 @@
 class FontMansalva < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/mansalva/Mansalva-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/mansalva/Mansalva-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Mansalva"
   homepage "https://fonts.google.com/specimen/Mansalva"
   def install
