8f3af0ce36181f38e650d06d4f8d0a73be4005b1
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-balinese.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-balinese.rb
+++ Formula/font-noto-sans-balinese.rb
@@ -1,5 +1,5 @@
 class FontNotoSansBalinese < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBalinese-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBalinese-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Balinese"
   homepage "https://www.google.com/get/noto/#sans-bali"
   def install
