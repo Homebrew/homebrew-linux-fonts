a9b1f9250b37e31832cf9f056536d9596534eb42
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-cjk-tc.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-serif-cjk-tc.rb
+++ Formula/font-noto-serif-cjk-tc.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifCjkTc < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKtc-hinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKtc-hinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Serif CJK TC"
   homepage "https://www.google.com/get/noto/#serif-hant"
   def install
