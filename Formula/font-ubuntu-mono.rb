95f4a71a5fa2c14540a2fa69ff893ccd63f7ad73
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu-mono.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-ubuntu-mono.rb
+++ Formula/font-ubuntu-mono.rb
@@ -1,5 +1,5 @@
 class FontUbuntuMono < Formula
-  head "https://github.com/google/fonts/trunk/ufl/ubuntumono", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/ufl/ubuntumono", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
   desc "Ubuntu Mono"
   homepage "https://fonts.google.com/specimen/Ubuntu+Mono"
   def install
