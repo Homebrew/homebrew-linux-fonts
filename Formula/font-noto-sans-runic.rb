3a61dba9b2f4e74326483bf092aba07a37637307
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-runic.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-runic.rb
+++ Formula/font-noto-sans-runic.rb
@@ -1,5 +1,5 @@
 class FontNotoSansRunic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRunic-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRunic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Runic"
   homepage "https://www.google.com/get/noto/#sans-runr"
   def install
