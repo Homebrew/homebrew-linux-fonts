d8651024c9db5f975bc2394125eaa6c645a12858
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mogra.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-mogra.rb
+++ Formula/font-mogra.rb
@@ -1,5 +1,5 @@
 class FontMogra < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/mogra/Mogra-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/mogra/Mogra-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Mogra"
   homepage "https://fonts.google.com/specimen/Mogra"
   def install
