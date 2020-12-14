ee072d15d9ee7da5512400cec6e4bbed46ba77a7
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-malayalam.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-serif-malayalam.rb
+++ Formula/font-noto-serif-malayalam.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifMalayalam < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifMalayalam-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifMalayalam-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Serif Malayalam"
   homepage "https://www.google.com/get/noto/#serif-mlym"
   def install
