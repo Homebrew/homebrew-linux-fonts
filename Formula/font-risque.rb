688c3378cd92f228f2ae1d07a7fbdf800f313c25
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-risque.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-risque.rb
+++ Formula/font-risque.rb
@@ -1,5 +1,5 @@
 class FontRisque < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/risque/Risque-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/risque/Risque-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Risque"
   homepage "https://fonts.google.com/specimen/Risque"
   def install
