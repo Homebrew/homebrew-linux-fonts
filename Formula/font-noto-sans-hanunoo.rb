9c0b07f16ace5ec713de7fe6a756a822dca24ea1
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-hanunoo.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-hanunoo.rb
+++ Formula/font-noto-sans-hanunoo.rb
@@ -1,5 +1,5 @@
 class FontNotoSansHanunoo < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansHanunoo-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansHanunoo-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Hanunoo"
   homepage "https://www.google.com/get/noto/#sans-hano"
   def install
