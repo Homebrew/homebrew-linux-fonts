65eda13a0a5fe6d2b94745446bd721707302fc84
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-old-turkic.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-old-turkic.rb
+++ Formula/font-noto-sans-old-turkic.rb
@@ -1,5 +1,5 @@
 class FontNotoSansOldTurkic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldTurkic-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldTurkic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Old Turkic"
   homepage "https://www.google.com/get/noto/#sans-orkh"
   def install
