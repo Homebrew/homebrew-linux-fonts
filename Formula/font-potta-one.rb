e10e9e1e8e6a7b7c373635cf87ea5ff15137bba9
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-potta-one.rb from 77eba979427ac3501d635d657e35837c95aa2438

---
+++ Formula/font-potta-one.rb
@@ -0,0 +1,11 @@
+class FontPottaOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/pottaone/PottaOne-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Potta One"
+  desc "Single style font family featuring letterforms inspired by brush lettering"
+  homepage "https://fonts.google.com/specimen/Potta+One"
+  def install
+    (share/"fonts").install "PottaOne-Regular.ttf"
+  end
+  test do
+  end
+end
