67ed7e5100c264194376e4a5cd17032a63e644a9
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-deseret.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-deseret.rb
+++ Formula/font-noto-sans-deseret.rb
@@ -1,5 +1,5 @@
 class FontNotoSansDeseret < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansDeseret-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansDeseret-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Deseret"
   homepage "https://www.google.com/get/noto/#sans-dsrt"
   def install
