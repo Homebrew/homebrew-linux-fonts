df176f66a66eee51a3f6afc8aa8a70de4789364b
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bitter.rb from 77eba979427ac3501d635d657e35837c95aa2438

---
+++ Formula/font-bitter.rb
@@ -0,0 +1,12 @@
+class FontBitter < Formula
+  head "https://github.com/google/fonts/trunk/ofl/bitter", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
+  desc "Bitter"
+  desc "Slab-serif typeface optimized for e-ink screens"
+  homepage "https://fonts.google.com/specimen/Bitter"
+  def install
+    (share/"fonts").install "Bitter-Italic[wght].ttf"
+    (share/"fonts").install "Bitter[wght].ttf"
+  end
+  test do
+  end
+end
