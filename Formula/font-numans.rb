55fc1404b4e27b9fa9581d6a9f8904ecb149f52d
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-numans.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-numans.rb
+++ Formula/font-numans.rb
@@ -1,5 +1,5 @@
 class FontNumans < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/numans/Numans-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/numans/Numans-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Numans"
   homepage "https://fonts.google.com/specimen/Numans"
   def install
