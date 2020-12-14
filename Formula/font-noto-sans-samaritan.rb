3fa9632702add8eb1c0dbe9be6282d42d21eebfd
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-samaritan.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-samaritan.rb
+++ Formula/font-noto-sans-samaritan.rb
@@ -1,5 +1,5 @@
 class FontNotoSansSamaritan < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSamaritan-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSamaritan-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Samaritan"
   homepage "https://www.google.com/get/noto/#sans-samr"
   def install
