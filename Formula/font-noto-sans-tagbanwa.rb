635a5f40031a01192f8a8db6c16087068248afe2
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tagbanwa.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-tagbanwa.rb
+++ Formula/font-noto-sans-tagbanwa.rb
@@ -1,5 +1,5 @@
 class FontNotoSansTagbanwa < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagbanwa-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagbanwa-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Tagbanwa"
   homepage "https://www.google.com/get/noto/#sans-tagb"
   def install
