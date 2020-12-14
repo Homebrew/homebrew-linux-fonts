dc708fb7c877fdd50fc22a26f35ea6f696410bff
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-emoji.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-emoji.rb
+++ Formula/font-noto-emoji.rb
@@ -1,5 +1,5 @@
 class FontNotoEmoji < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoEmoji-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoEmoji-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Emoji"
   homepage "https://www.google.com/get/noto/#emoji-zsye"
   def install
