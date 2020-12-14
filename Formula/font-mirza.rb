cd58e31bf9cc6dceed93d1ed04dbd5bfb85b6fa2
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mirza.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-mirza.rb
+++ Formula/font-mirza.rb
@@ -1,5 +1,5 @@
 class FontMirza < Formula
-  head "https://github.com/google/fonts/trunk/ofl/mirza", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/mirza", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
   desc "Mirza"
   homepage "https://fonts.google.com/specimen/Mirza"
   def install
