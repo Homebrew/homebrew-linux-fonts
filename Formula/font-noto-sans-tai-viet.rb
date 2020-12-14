46143d800ba2b1228bb913f0db384069696b1aef
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tai-viet.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-tai-viet.rb
+++ Formula/font-noto-sans-tai-viet.rb
@@ -1,5 +1,5 @@
 class FontNotoSansTaiViet < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiViet-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiViet-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Tai Viet"
   homepage "https://www.google.com/get/noto/#sans-tavt"
   def install
