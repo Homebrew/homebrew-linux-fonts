97ef28bc4272b0648daf2cc7dcf2e3da3a92c063
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rokkitt.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-rokkitt.rb
+++ Formula/font-rokkitt.rb
@@ -1,5 +1,5 @@
 class FontRokkitt < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/rokkitt/Rokkitt%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/rokkitt/Rokkitt%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Rokkitt"
   homepage "https://fonts.google.com/specimen/Rokkitt"
   def install
