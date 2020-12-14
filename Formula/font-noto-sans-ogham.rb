3736ffe3607b940ae74cd4010dfc712b8656dbae
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-ogham.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-ogham.rb
+++ Formula/font-noto-sans-ogham.rb
@@ -1,5 +1,5 @@
 class FontNotoSansOgham < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOgham-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOgham-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Ogham"
   homepage "https://www.google.com/get/noto/#sans-ogam"
   def install
