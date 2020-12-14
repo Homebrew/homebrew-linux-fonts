7a2c7bea7d049b504ac3ddb7faa72d63948550d7
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-mono.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-mono.rb
+++ Formula/font-noto-sans-mono.rb
@@ -1,5 +1,5 @@
 class FontNotoSansMono < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMono-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMono-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Mono"
   homepage "https://www.google.com/get/noto/#sans-mono"
   def install
