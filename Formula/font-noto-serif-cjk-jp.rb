fdb1a528eaa3ba30e48d4e60cb4fa7e00930dd9e
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-cjk-jp.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-serif-cjk-jp.rb
+++ Formula/font-noto-serif-cjk-jp.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifCjkJp < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKjp-hinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKjp-hinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Serif CJK JP"
   homepage "https://www.google.com/get/noto/#serif-jpan"
   def install
