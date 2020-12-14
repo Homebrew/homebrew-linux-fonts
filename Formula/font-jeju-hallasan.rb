4f8cb5accd77b10b755375d8468eb27609e1dbdf
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jeju-hallasan.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-jeju-hallasan.rb
+++ Formula/font-jeju-hallasan.rb
@@ -1,5 +1,5 @@
 class FontJejuHallasan < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/jejuhallasan/JejuHallasan-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/jejuhallasan/JejuHallasan-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Jeju Hallasan"
   homepage "https://fonts.google.com/earlyaccess"
   def install
