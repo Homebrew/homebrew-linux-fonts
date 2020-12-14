8503ef857f54a923179b9d937d67f08292fe8199
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-cham.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-cham.rb
+++ Formula/font-noto-sans-cham.rb
@@ -1,5 +1,5 @@
 class FontNotoSansCham < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCham-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCham-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Cham"
   homepage "https://www.google.com/get/noto/#sans-cham"
   def install
