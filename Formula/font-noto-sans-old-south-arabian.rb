add3c6fa5ee0e2ca32d2a69b4a7d7ec4e141982c
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-old-south-arabian.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-old-south-arabian.rb
+++ Formula/font-noto-sans-old-south-arabian.rb
@@ -1,5 +1,5 @@
 class FontNotoSansOldSouthArabian < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldSouthArabian-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldSouthArabian-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Old South Arabian"
   homepage "https://www.google.com/get/noto/#sans-sarb"
   def install
