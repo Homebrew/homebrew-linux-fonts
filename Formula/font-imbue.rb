2782f35f78f8106d45efa4053343de2ba300a8dd
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-imbue.rb from 77eba979427ac3501d635d657e35837c95aa2438

---
+++ Formula/font-imbue.rb
@@ -0,0 +1,11 @@
+class FontImbue < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/imbue/Imbue%5Bopsz%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
+  desc "Imbue"
+  desc "Variable condensed Didone font"
+  homepage "https://fonts.google.com/specimen/Imbue"
+  def install
+    (share/"fonts").install "Imbue[opsz,wght].ttf"
+  end
+  test do
+  end
+end
