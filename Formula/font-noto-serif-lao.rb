671cfd75aabb5af563578302c24d37648f530288
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-lao.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-serif-lao.rb
+++ Formula/font-noto-serif-lao.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifLao < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifLao-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifLao-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Serif Lao"
   homepage "https://www.google.com/get/noto/#serif-laoo"
   def install
