0c6961dccd71328c4debee4cbe136f6ed2d00484
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-canadian-aboriginal.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-canadian-aboriginal.rb
+++ Formula/font-noto-sans-canadian-aboriginal.rb
@@ -1,5 +1,5 @@
 class FontNotoSansCanadianAboriginal < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCanadianAboriginal-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCanadianAboriginal-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Canadian Aboriginal"
   homepage "https://www.google.com/get/noto/#sans-cans"
   def install
