9ae5dfd6ced04f7ca2391d663cb1ba752bafee76
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-myanmar.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-myanmar.rb
+++ Formula/font-noto-sans-myanmar.rb
@@ -1,5 +1,5 @@
 class FontNotoSansMyanmar < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMyanmar-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMyanmar-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Myanmar"
   homepage "https://www.google.com/get/noto/#sans-mymr"
   def install
