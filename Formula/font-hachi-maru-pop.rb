a5be3a65db81d18a8b896165453d6bc1bbf3f118
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hachi-maru-pop.rb from 77eba979427ac3501d635d657e35837c95aa2438

---
+++ Formula/font-hachi-maru-pop.rb
@@ -0,0 +1,11 @@
+class FontHachiMaruPop < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/hachimarupop/HachiMaruPop-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Hachi Maru Pop"
+  desc "Handwritten font"
+  homepage "https://fonts.google.com/specimen/Hachi+Maru+Pop"
+  def install
+    (share/"fonts").install "HachiMaruPop-Regular.ttf"
+  end
+  test do
+  end
+end
