ab721f96e51af72665a8ce72ff63580458aae9d8
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-new-tai-lue.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-new-tai-lue.rb
+++ Formula/font-noto-sans-new-tai-lue.rb
@@ -1,5 +1,5 @@
 class FontNotoSansNewTaiLue < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNewTaiLue-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNewTaiLue-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans New Tai Lue"
   homepage "https://www.google.com/get/noto/#sans-talu"
   def install
