d6b8ea0fb87c346bc0e567acc75e7bf1db0a2f18
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:34 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-khand.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-khand.rb
+++ Formula/font-khand.rb
@@ -1,5 +1,5 @@
 class FontKhand < Formula
-  head "https://github.com/google/fonts/trunk/ofl/khand", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/khand", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
   desc "Khand"
   homepage "https://fonts.google.com/specimen/Khand"
   def install
