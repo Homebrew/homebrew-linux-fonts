f91ceacefd82ddcad01eaeb39ec32cecf82619ed
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-old-persian.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-old-persian.rb
+++ Formula/font-noto-sans-old-persian.rb
@@ -1,5 +1,5 @@
 class FontNotoSansOldPersian < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldPersian-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldPersian-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Old Persian"
   homepage "https://www.google.com/get/noto/#sans-xpeo"
   def install
