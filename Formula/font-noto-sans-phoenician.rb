5c8ecdc264ff24a8c5318a9077180eb821fbdce9
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-phoenician.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-phoenician.rb
+++ Formula/font-noto-sans-phoenician.rb
@@ -1,5 +1,5 @@
 class FontNotoSansPhoenician < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansPhoenician-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansPhoenician-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Phoenician"
   homepage "https://www.google.com/get/noto/#sans-phnx"
   def install
