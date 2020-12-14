48aeaefa1a6d5dc332cd5afd6fd8690d36856e5a
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-lepcha.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-lepcha.rb
+++ Formula/font-noto-sans-lepcha.rb
@@ -1,5 +1,5 @@
 class FontNotoSansLepcha < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLepcha-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLepcha-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Lepcha"
   homepage "https://www.google.com/get/noto/#sans-lepc"
   def install
