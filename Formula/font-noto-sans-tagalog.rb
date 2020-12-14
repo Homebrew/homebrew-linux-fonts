fc47def79299a6c2c5a9debcd367552a846d1029
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tagalog.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-tagalog.rb
+++ Formula/font-noto-sans-tagalog.rb
@@ -1,5 +1,5 @@
 class FontNotoSansTagalog < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagalog-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagalog-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Tagalog"
   homepage "https://www.google.com/get/noto/#sans-tglg"
   def install
