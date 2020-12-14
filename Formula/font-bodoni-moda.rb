cf0b1d888018453f30517d13b60ddbf31b66aa24
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bodoni-moda.rb from 77eba979427ac3501d635d657e35837c95aa2438

---
+++ Formula/font-bodoni-moda.rb
@@ -0,0 +1,12 @@
+class FontBodoniModa < Formula
+  head "https://github.com/google/fonts/trunk/ofl/bodonimoda", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
+  desc "Bodoni Moda"
+  desc "Serif typeface with high contrast"
+  homepage "https://fonts.google.com/specimen/Bodoni+Moda"
+  def install
+    (share/"fonts").install "BodoniModa-Italic[opsz,wght].ttf"
+    (share/"fonts").install "BodoniModa[opsz,wght].ttf"
+  end
+  test do
+  end
+end
