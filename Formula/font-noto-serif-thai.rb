4983615034064c290a5902acfc37ccd9de10e45b
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-thai.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-serif-thai.rb
+++ Formula/font-noto-serif-thai.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifThai < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifThai-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifThai-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Serif Thai"
   homepage "https://www.google.com/get/noto/#serif-thai"
   def install
