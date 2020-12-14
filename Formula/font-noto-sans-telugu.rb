f411617444a8a959cd4cb3a1c4dc514bab3eea50
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-telugu.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-telugu.rb
+++ Formula/font-noto-sans-telugu.rb
@@ -1,5 +1,5 @@
 class FontNotoSansTelugu < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTelugu-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTelugu-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Telugu"
   homepage "https://www.google.com/get/noto/#sans-telu"
   def install
