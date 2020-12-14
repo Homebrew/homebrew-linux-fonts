05eeae636305ed1e84ec89791a2937f8a2fc9310
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-yi.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-yi.rb
+++ Formula/font-noto-sans-yi.rb
@@ -1,5 +1,5 @@
 class FontNotoSansYi < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansYi-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansYi-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Yi"
   homepage "https://www.google.com/get/noto/#sans-yiii"
   def install
