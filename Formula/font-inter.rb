9d2e2e3fd5a34828ee8003f44a092a0496bab30d
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inter.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-inter.rb
+++ Formula/font-inter.rb
@@ -1,7 +1,7 @@
 class FontInter < Formula
   version "3.15"
   sha256 "1534288eb599f57acbf2c5ac0010da9495c2ee53116e08d799555c6f8ee22156"
-  url "https://github.com/rsms/inter/releases/download/v#{version}/Inter-#{version}.zip"
+  url "https://github.com/rsms/inter/releases/download/v#{version}/Inter-#{version}.zip", verified: "github.com/rsms/inter/"
   desc "Inter"
   homepage "https://rsms.me/inter/"
   def install
