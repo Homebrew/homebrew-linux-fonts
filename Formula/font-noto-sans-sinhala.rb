f5633550d50845e99bbab4d30b5f8413bd5e80f5
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-sinhala.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-sinhala.rb
+++ Formula/font-noto-sans-sinhala.rb
@@ -1,5 +1,5 @@
 class FontNotoSansSinhala < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSinhala-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSinhala-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Sinhala"
   homepage "https://www.google.com/get/noto/#sans-sinh"
   def install
