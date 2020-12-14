e8b1a14bb6ed8ed6bc48a0db3e4877a3a27ba4e3
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-optician-sans.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-optician-sans.rb
+++ Formula/font-optician-sans.rb
@@ -1,5 +1,5 @@
 class FontOpticianSans < Formula
-  head "https://github.com/anewtypeofinterference/Optician-Sans/raw/master/OpenType-PS/Optician-Sans.otf"
+  head "https://github.com/anewtypeofinterference/Optician-Sans/raw/master/OpenType-PS/Optician-Sans.otf", verified: "github.com/anewtypeofinterference/Optician-Sans/"
   desc "Optician Sans"
   homepage "https://optician-sans.com/"
   def install
