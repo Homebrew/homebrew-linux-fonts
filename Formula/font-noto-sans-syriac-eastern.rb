722b702db2e4c833059b3fcd8ababd83422dd505
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-syriac-eastern.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-syriac-eastern.rb
+++ Formula/font-noto-sans-syriac-eastern.rb
@@ -1,5 +1,5 @@
 class FontNotoSansSyriacEastern < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEastern-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEastern-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Syriac Eastern"
   homepage "https://www.google.com/get/noto/#sans-syrc-eastern"
   def install
