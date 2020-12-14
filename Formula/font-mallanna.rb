dcd9e59d7906af9a0648c671475c547cbc7e5cfe
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:36 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mallanna.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-mallanna.rb
+++ Formula/font-mallanna.rb
@@ -1,5 +1,5 @@
 class FontMallanna < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/mallanna/Mallanna-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/mallanna/Mallanna-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Mallanna"
   homepage "https://fonts.google.com/specimen/Mallanna"
   def install
