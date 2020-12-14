c3ddef92cd923d199fac03c362dbca778cf3fb8d
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:35 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-league-script.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-league-script.rb
+++ Formula/font-league-script.rb
@@ -1,5 +1,5 @@
 class FontLeagueScript < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/leaguescript/LeagueScript-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/leaguescript/LeagueScript-Regular.ttf", verified: "github.com/google/fonts/"
   desc "League Script"
   homepage "https://fonts.google.com/specimen/League+Script"
   def install
