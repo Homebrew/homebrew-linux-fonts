41f8cda01fe792414649efc881b320ffd5a8bf07
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-devanagari.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-serif-devanagari.rb
+++ Formula/font-noto-serif-devanagari.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifDevanagari < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifDevanagari-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifDevanagari-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Serif Devanagari"
   homepage "https://www.google.com/get/noto/#serif-deva"
   def install
