4f6c71c8733eda08b26a8ff8fa08fc0d99c63d77
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-color-emoji.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-color-emoji.rb
+++ Formula/font-noto-color-emoji.rb
@@ -1,5 +1,5 @@
 class FontNotoColorEmoji < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Color Emoji"
   homepage "https://www.google.com/get/noto/#emoji-zsye-color"
   def install
