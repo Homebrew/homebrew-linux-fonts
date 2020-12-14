e8600b15aca1ebb2ded188b23b23d9f6389498d3
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-syriac-estrangela.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-syriac-estrangela.rb
+++ Formula/font-noto-sans-syriac-estrangela.rb
@@ -1,5 +1,5 @@
 class FontNotoSansSyriacEstrangela < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEstrangela-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEstrangela-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Syriac Estrangela"
   homepage "https://www.google.com/get/noto/#sans-syrc-estrangela"
   def install
