48c67ec37b9fe590ef143f518057f127dc83edca
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monoisome.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-monoisome.rb
+++ Formula/font-monoisome.rb
@@ -1,6 +1,6 @@
 class FontMonoisome < Formula
   version "0.61"
-  url "https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true"
+  url "https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true", verified: "github.com/larsenwork/monoid/"
   desc "Monoisome"
   homepage "https://larsenwork.com/monoid/"
   def install
