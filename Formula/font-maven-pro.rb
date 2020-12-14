d6b4ae580501ef7ac72b8855229a3905b5eac428
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:36 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-maven-pro.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-maven-pro.rb
+++ Formula/font-maven-pro.rb
@@ -1,5 +1,5 @@
 class FontMavenPro < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/mavenpro/MavenPro%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/mavenpro/MavenPro%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Maven Pro"
   homepage "https://fonts.google.com/specimen/Maven+Pro"
   def install
