217d8dded6aff249876a783064316e0f98208a0c
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-khmer.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-serif-khmer.rb
+++ Formula/font-noto-serif-khmer.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifKhmer < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifKhmer-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifKhmer-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Serif Khmer"
   homepage "https://www.google.com/get/noto/#serif-khmr"
   def install
