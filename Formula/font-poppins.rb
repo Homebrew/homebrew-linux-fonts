9002a0e098f5d49a22b12604058c3b41d7cba0e6
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:42 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-poppins.rb from a668185eef71e0800a616af76cef8b15f6378cfb

--- Formula/font-poppins.rb
+++ Formula/font-poppins.rb
@@ -1,15 +1,28 @@
 class FontPoppins < Formula
-  version "3.200"
-  sha256 "a05ecec9599a698fd7a52fa492c868af1fa3462be33bb038d14dbbac7a5456dc"
-  url "https://github.com/itfoundry/Poppins/releases/download/v#{version}/Poppins-#{version.to_s.gsub(".", "")}-OTF.zip"
+  version "4.003"
+  sha256 "3a1a58cfc497b3a57a8a6ec51d62800ad42a50b9827b1e3297135bf5e860b1a0"
+  url "https://github.com/itfoundry/Poppins/raw/master/products/Poppins-#{version}-GoogleFonts-OTF.zip"
   desc "Poppins"
   homepage "https://github.com/itfoundry/poppins"
   def install
+    (share/"fonts").install "Poppins-Black.otf"
+    (share/"fonts").install "Poppins-BlackItalic.otf"
     (share/"fonts").install "Poppins-Bold.otf"
+    (share/"fonts").install "Poppins-BoldItalic.otf"
+    (share/"fonts").install "Poppins-ExtraBold.otf"
+    (share/"fonts").install "Poppins-ExtraBoldItalic.otf"
+    (share/"fonts").install "Poppins-ExtraLight.otf"
+    (share/"fonts").install "Poppins-ExtraLightItalic.otf"
+    (share/"fonts").install "Poppins-Italic.otf"
     (share/"fonts").install "Poppins-Light.otf"
+    (share/"fonts").install "Poppins-LightItalic.otf"
     (share/"fonts").install "Poppins-Medium.otf"
+    (share/"fonts").install "Poppins-MediumItalic.otf"
     (share/"fonts").install "Poppins-Regular.otf"
     (share/"fonts").install "Poppins-SemiBold.otf"
+    (share/"fonts").install "Poppins-SemiBoldItalic.otf"
+    (share/"fonts").install "Poppins-Thin.otf"
+    (share/"fonts").install "Poppins-ThinItalic.otf"
   end
   test do
   end
