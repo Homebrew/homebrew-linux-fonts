7ee40fd525ad5e009a19ace2d4ae16676641239e
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-coptic.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-coptic.rb
+++ Formula/font-noto-sans-coptic.rb
@@ -1,5 +1,5 @@
 class FontNotoSansCoptic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCoptic-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCoptic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Coptic"
   homepage "https://www.google.com/get/noto/#sans-copt"
   def install
