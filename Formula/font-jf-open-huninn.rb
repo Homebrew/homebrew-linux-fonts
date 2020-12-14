d0fb4b8a7c8b29e8c5a907198b0826f6631f0d7e
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jf-open-huninn.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-jf-open-huninn.rb
+++ Formula/font-jf-open-huninn.rb
@@ -1,7 +1,7 @@
 class FontJfOpenHuninn < Formula
   version "1.1"
   sha256 "1b5a8d0204f5763408609d5e8182455d23a2ebc7450730aed19cc13c619bca37"
-  url "https://github.com/justfont/open-huninn-font/releases/download/v#{version}/jf-openhuninn-#{version}.zip"
+  url "https://github.com/justfont/open-huninn-font/releases/download/v#{version}/jf-openhuninn-#{version}.zip", verified: "github.com/justfont/open-huninn-font/"
   desc "jf open huninn"
   desc "jf open 粉圓"
   homepage "https://justfont.com/huninn/"
