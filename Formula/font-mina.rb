6ab3a097f99d59877ddd9417106be1b7ef3178ee
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mina.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-mina.rb
+++ Formula/font-mina.rb
@@ -1,5 +1,5 @@
 class FontMina < Formula
-  head "https://github.com/google/fonts/trunk/ofl/mina", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/mina", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
   desc "Mina"
   homepage "https://fonts.google.com/specimen/Mina"
   def install
