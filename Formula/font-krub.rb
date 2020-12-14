2bc0da7523e8f318d76529f3e624d77f3768506d
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:34 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-krub.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-krub.rb
+++ Formula/font-krub.rb
@@ -1,5 +1,5 @@
 class FontKrub < Formula
-  head "https://github.com/google/fonts/trunk/ofl/krub", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/krub", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
   desc "Krub"
   homepage "https://fonts.google.com/specimen/Krub"
   def install
