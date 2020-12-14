d207a48e4abc39d67a6b3b9ffa3f1767f2c1d988
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-sundanese.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-sundanese.rb
+++ Formula/font-noto-sans-sundanese.rb
@@ -1,5 +1,5 @@
 class FontNotoSansSundanese < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSundanese-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSundanese-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Sundanese"
   homepage "https://www.google.com/get/noto/#sans-sund"
   def install
