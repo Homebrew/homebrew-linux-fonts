0fcbee24d7c38a5448b4abfb82bcfe7e36887572
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chokokutai.rb from 77eba979427ac3501d635d657e35837c95aa2438

---
+++ Formula/font-chokokutai.rb
@@ -0,0 +1,11 @@
+class FontChokokutai < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/chokokutai/Chokokutai-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Chokokutai"
+  desc "Japanese display font whose characters have a funky appearance"
+  homepage "https://fonts.google.com/specimen/Chokokutai"
+  def install
+    (share/"fonts").install "Chokokutai-Regular.ttf"
+  end
+  test do
+  end
+end
