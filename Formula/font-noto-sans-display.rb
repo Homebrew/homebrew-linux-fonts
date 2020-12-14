893f77787c6f59433c86d37acee478fcaa73c239
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-display.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-display.rb
+++ Formula/font-noto-sans-display.rb
@@ -1,5 +1,5 @@
 class FontNotoSansDisplay < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansDisplay-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansDisplay-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Display"
   homepage "https://www.google.com/get/noto/#sans-lgc-display"
   def install
