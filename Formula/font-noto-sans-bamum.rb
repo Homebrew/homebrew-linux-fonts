7e8f88752ce2b6630d9c66130e9c961c69a0a374
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-bamum.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-bamum.rb
+++ Formula/font-noto-sans-bamum.rb
@@ -1,5 +1,5 @@
 class FontNotoSansBamum < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBamum-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBamum-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Bamum"
   homepage "https://www.google.com/get/noto/#sans-bamu"
   def install
