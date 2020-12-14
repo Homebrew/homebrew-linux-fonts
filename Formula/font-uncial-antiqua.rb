b559440aea1dcb76ef5e378062ea5b18f971d16e
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-uncial-antiqua.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-uncial-antiqua.rb
+++ Formula/font-uncial-antiqua.rb
@@ -1,5 +1,5 @@
 class FontUncialAntiqua < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/uncialantiqua/UncialAntiqua-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/uncialantiqua/UncialAntiqua-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Uncial Antiqua"
   homepage "https://fonts.google.com/specimen/Uncial+Antiqua"
   def install
