4b07906fb037e5bcb2d2826e03180c9f2cf9c2a9
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-syloti-nagri.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-syloti-nagri.rb
+++ Formula/font-noto-sans-syloti-nagri.rb
@@ -1,5 +1,5 @@
 class FontNotoSansSylotiNagri < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSylotiNagri-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSylotiNagri-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Syloti Nagri"
   homepage "https://www.google.com/get/noto/#sans-sylo"
   def install
