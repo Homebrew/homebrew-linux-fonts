558f78bdf832f883cfc52b7952a8a55796c107e2
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:42 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-palanquin.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-palanquin.rb
+++ Formula/font-palanquin.rb
@@ -1,5 +1,5 @@
 class FontPalanquin < Formula
-  head "https://github.com/google/fonts/trunk/ofl/palanquin", using: :svn, trust_cert: true
+  head "https://github.com/google/fonts/trunk/ofl/palanquin", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
   desc "Palanquin"
   homepage "https://fonts.google.com/specimen/Palanquin"
   def install
