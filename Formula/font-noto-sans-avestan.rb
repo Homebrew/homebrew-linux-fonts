1b813c705cc85ec24d5374814c01b34d5b5864ab
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-avestan.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-avestan.rb
+++ Formula/font-noto-sans-avestan.rb
@@ -1,5 +1,5 @@
 class FontNotoSansAvestan < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAvestan-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAvestan-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Avestan"
   homepage "https://www.google.com/get/noto/#sans-avst"
   def install
