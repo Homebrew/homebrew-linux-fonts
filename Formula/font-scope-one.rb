e42dcdbd8693740ef292ff93925cc596991af4c0
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-scope-one.rb from 37821a5f5eed725f63065c52ca47e3bea428f9c7

--- Formula/font-scope-one.rb
+++ Formula/font-scope-one.rb
@@ -1,5 +1,5 @@
 class FontScopeOne < Formula
-  head "https://github.com/google/fonts/raw/master/ofl/scopeone/ScopeOne-Regular.ttf"
+  head "https://github.com/google/fonts/raw/master/ofl/scopeone/ScopeOne-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Scope One"
   homepage "https://fonts.google.com/specimen/Scope+One"
   def install
