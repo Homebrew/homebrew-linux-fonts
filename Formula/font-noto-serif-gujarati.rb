99437c48218b66cb7245a0ce48579d89f5a1891b
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-gujarati.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-serif-gujarati.rb
+++ Formula/font-noto-serif-gujarati.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifGujarati < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifGujarati-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifGujarati-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Serif Gujarati"
   homepage "https://www.google.com/get/noto/#serif-gujr"
   def install
