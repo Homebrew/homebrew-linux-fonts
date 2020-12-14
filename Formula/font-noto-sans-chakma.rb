b8f6f7aead454e2c163bb7ef62ee3c5042dc898b
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-chakma.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-chakma.rb
+++ Formula/font-noto-sans-chakma.rb
@@ -1,5 +1,5 @@
 class FontNotoSansChakma < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansChakma-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansChakma-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Chakma"
   homepage "https://www.google.com/get/noto/#sans-cakm"
   def install
