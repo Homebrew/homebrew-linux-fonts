7d46f082b9420121d88d5bbc6e013829e253d569
TANIGUCHI Masaya <m@docs.casa>
2020-12-14 00:15:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tai-tham.rb from b1f804a0a622c13cc09b2dbba650636ff90ed71a

--- Formula/font-noto-sans-tai-tham.rb
+++ Formula/font-noto-sans-tai-tham.rb
@@ -1,5 +1,5 @@
 class FontNotoSansTaiTham < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiTham-unhinted.zip"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiTham-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
   desc "Noto Sans Tai Tham"
   homepage "https://www.google.com/get/noto/#sans-lana"
   def install
