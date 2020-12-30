7d460cd521af3d643f89bb5eef69063e7182079d
TANIGUCHI Masaya <m@docs.casa>
2020-12-30 00:25:24 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-benne.rb from e0204a30a23f332589648f54b687ebaba692c624

---
+++ Formula/font-benne.rb
@@ -0,0 +1,11 @@
+class FontBenne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/benne/Benne-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Benne"
+  desc "Kannada text font developed by john harrington"
+  homepage "https://fonts.google.com/specimen/Benne"
+  def install
+    (share/"fonts").install "Benne-Regular.ttf"
+  end
+  test do
+  end
+end
