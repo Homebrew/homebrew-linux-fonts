202044edfbcbbbc9fa0c7685442b88c7c64d594a
TANIGUCHI Masaya <m@docs.casa>
2021-01-11 00:31:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-shippori-mincho-b1.rb from f9cabb8f2de5547959293ea019e6563e4dfc9afe

---
+++ Formula/font-shippori-mincho-b1.rb
@@ -0,0 +1,15 @@
+class FontShipporiMinchoB1 < Formula
+  head "https://github.com/google/fonts/trunk/ofl/shipporiminchob1", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
+  desc "Shippori Mincho B1"
+  desc "Based on the Tsukiji Typeface making facility of Tokyo"
+  homepage "https://fonts.google.com/specimen/Shippori+Mincho+B1"
+  def install
+    (share/"fonts").install "ShipporiMinchoB1-Bold.ttf"
+    (share/"fonts").install "ShipporiMinchoB1-ExtraBold.ttf"
+    (share/"fonts").install "ShipporiMinchoB1-Medium.ttf"
+    (share/"fonts").install "ShipporiMinchoB1-Regular.ttf"
+    (share/"fonts").install "ShipporiMinchoB1-SemiBold.ttf"
+  end
+  test do
+  end
+end
