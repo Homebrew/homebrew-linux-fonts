3e55c13c1549048135dc691aa088d0e7cee2fcba
TANIGUCHI Masaya <m@docs.casa>
2021-01-26 00:19:04 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss15.rb from 0f1cbfb377bbc82285a1f261feddd7b9ca686c4c

---
+++ Formula/font-iosevka-ss15.rb
@@ -0,0 +1,21 @@
+class FontIosevkaSs15 < Formula
+  version "5.0.0-beta.2"
+  sha256 "6016b57c0c47d84a97c01d51b2b1116faaaa0773cc2b73aa2c82c5b4b512d0e3"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss15-#{version}.zip"
+  desc "Iosevka SS15"
+  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install "iosevka-ss15-bold.ttc"
+    (share/"fonts").install "iosevka-ss15-extrabold.ttc"
+    (share/"fonts").install "iosevka-ss15-extralight.ttc"
+    (share/"fonts").install "iosevka-ss15-heavy.ttc"
+    (share/"fonts").install "iosevka-ss15-light.ttc"
+    (share/"fonts").install "iosevka-ss15-medium.ttc"
+    (share/"fonts").install "iosevka-ss15-regular.ttc"
+    (share/"fonts").install "iosevka-ss15-semibold.ttc"
+    (share/"fonts").install "iosevka-ss15-thin.ttc"
+  end
+  test do
+  end
+end
