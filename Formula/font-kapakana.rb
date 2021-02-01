2a2ed17322e2cb218e000ebd2b0ace206b7501ac
TANIGUCHI Masaya <m@docs.casa>
2021-02-01 00:18:50 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kapakana.rb from 0a916fda8a2c27cda8836a0587511c657c7f43ef

---
+++ Formula/font-kapakana.rb
@@ -0,0 +1,11 @@
+class FontKapakana < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/kapakana/Kapakana%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
+  desc "Kapakana"
+  desc "Two weight font and available both as static instances and as a variable font"
+  homepage "https://fonts.google.com/specimen/Kapakana"
+  def install
+    (share/"fonts").install "Kapakana[wght].ttf"
+  end
+  test do
+  end
+end
