aa90811ff5bc425a68ef44567c80c0f575c1fb47
TANIGUCHI Masaya <m@docs.casa>
2021-02-01 00:18:53 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-reggae-one.rb from 0a916fda8a2c27cda8836a0587511c657c7f43ef

---
+++ Formula/font-reggae-one.rb
@@ -0,0 +1,10 @@
+class FontReggaeOne < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/reggaeone/ReggaeOne-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Reggae One"
+  homepage "https://fonts.google.com/specimen/Reggae+One"
+  def install
+    (share/"fonts").install "ReggaeOne-Regular.ttf"
+  end
+  test do
+  end
+end
