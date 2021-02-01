b0108c6c250ee93c6b51c567182782efc48c9a84
TANIGUCHI Masaya <m@docs.casa>
2021-02-01 00:18:53 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stick.rb from 0a916fda8a2c27cda8836a0587511c657c7f43ef

---
+++ Formula/font-stick.rb
@@ -0,0 +1,11 @@
+class FontStick < Formula
+  head "https://github.com/google/fonts/raw/master/ofl/stick/Stick-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Stick"
+  desc "Designed with straight lines that create a cute and playful feel"
+  homepage "https://fonts.google.com/specimen/Stick"
+  def install
+    (share/"fonts").install "Stick-Regular.ttf"
+  end
+  test do
+  end
+end
