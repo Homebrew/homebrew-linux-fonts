f56722cc91d3109b04068ed3d7a7bdc4406b0454
TANIGUCHI Masaya <m@docs.casa>
2021-02-08 00:14:21 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-archivo.rb from 6da6da8ebe4da6eb43f5c32855aad25ae32a05a4

--- Formula/font-archivo.rb
+++ Formula/font-archivo.rb
@@ -3,14 +3,8 @@ class FontArchivo < Formula
   desc "Archivo"
   homepage "https://fonts.google.com/specimen/Archivo"
   def install
-    (share/"fonts").install "Archivo-Bold.ttf"
-    (share/"fonts").install "Archivo-BoldItalic.ttf"
-    (share/"fonts").install "Archivo-Italic.ttf"
-    (share/"fonts").install "Archivo-Medium.ttf"
-    (share/"fonts").install "Archivo-MediumItalic.ttf"
-    (share/"fonts").install "Archivo-Regular.ttf"
-    (share/"fonts").install "Archivo-SemiBold.ttf"
-    (share/"fonts").install "Archivo-SemiBoldItalic.ttf"
+    (share/"fonts").install "Archivo-Italic[wdth,wght].ttf"
+    (share/"fonts").install "Archivo[wdth,wght].ttf"
   end
   test do
   end
