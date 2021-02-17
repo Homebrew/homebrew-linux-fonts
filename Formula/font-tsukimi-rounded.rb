1b3656450cc262e887c7cca6236b9fd768e1371d
TANIGUCHI Masaya <m@docs.casa>
2021-02-17 00:13:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tsukimi-rounded.rb from 5ecde72e1d08d0aae7ae1f60e34aa6648928f56e

---
+++ Formula/font-tsukimi-rounded.rb
@@ -0,0 +1,15 @@
+class FontTsukimiRounded < Formula
+  head "https://github.com/google/fonts/trunk/ofl/tsukimirounded", verified: "github.com/google/fonts/", using: :svn, trust_cert: true
+  desc "Tsukimi Rounded"
+  desc "Sans-serif typeface with rounded terminals"
+  homepage "https://fonts.google.com/specimen/Tsukimi+Rounded"
+  def install
+    (share/"fonts").install "TsukimiRounded-Bold.ttf"
+    (share/"fonts").install "TsukimiRounded-Light.ttf"
+    (share/"fonts").install "TsukimiRounded-Medium.ttf"
+    (share/"fonts").install "TsukimiRounded-Regular.ttf"
+    (share/"fonts").install "TsukimiRounded-SemiBold.ttf"
+  end
+  test do
+  end
+end
