578b7156c7de1f3a1d0d10486c3794d8fbcaa13b
TANIGUCHI Masaya <m@docs.casa>
2021-02-17 00:13:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss16.rb from ac1f9d94ca4a09d34fa765647061f95fc985592a

---
+++ Formula/font-iosevka-ss16.rb
@@ -0,0 +1,21 @@
+class FontIosevkaSs16 < Formula
+  version "5.0.1"
+  sha256 "8b6febd02e42f2002abff002b11a7f916116dbe4f8b33f37150146fe56ce198d"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss16-#{version}.zip"
+  desc "Iosevka SS16"
+  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install "iosevka-ss16-bold.ttc"
+    (share/"fonts").install "iosevka-ss16-extrabold.ttc"
+    (share/"fonts").install "iosevka-ss16-extralight.ttc"
+    (share/"fonts").install "iosevka-ss16-heavy.ttc"
+    (share/"fonts").install "iosevka-ss16-light.ttc"
+    (share/"fonts").install "iosevka-ss16-medium.ttc"
+    (share/"fonts").install "iosevka-ss16-regular.ttc"
+    (share/"fonts").install "iosevka-ss16-semibold.ttc"
+    (share/"fonts").install "iosevka-ss16-thin.ttc"
+  end
+  test do
+  end
+end
