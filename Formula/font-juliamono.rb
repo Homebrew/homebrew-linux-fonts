a735d7452d53f09b451f836f4e9a60a40f8be235
TANIGUCHI Masaya <m@docs.casa>
2021-02-19 00:13:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-juliamono.rb from 7dfbc2e0cf5b9cb303a73ca166b8e394f8f1840f

--- Formula/font-juliamono.rb
+++ Formula/font-juliamono.rb
@@ -1,6 +1,6 @@
 class FontJuliamono < Formula
-  version "0.032"
-  sha256 "fddcc9528431f502f6db6257122bca79db5f8d1f492c0c5a23ecc3ba37568f1f"
+  version "0.033"
+  sha256 "e0874049c8816511367271405dadbacbb15033171441085e9ee72c2326955531"
   url "https://github.com/cormullion/juliamono/releases/download/v#{version}/JuliaMono.tar.gz"
   desc "JuliaMono"
   desc "Monospaced typeface designed for programming in Julia"
@@ -8,11 +8,13 @@ class FontJuliamono < Formula
   def install
     (share/"fonts").install "JuliaMono-Black.ttf"
     (share/"fonts").install "JuliaMono-Bold.ttf"
+    (share/"fonts").install "JuliaMono-BoldLatin.ttf"
     (share/"fonts").install "JuliaMono-ExtraBold.ttf"
     (share/"fonts").install "JuliaMono-Light.ttf"
     (share/"fonts").install "JuliaMono-Medium.ttf"
     (share/"fonts").install "JuliaMono-Regular.ttf"
     (share/"fonts").install "JuliaMono-RegularLatin.ttf"
+    (share/"fonts").install "JuliaMono-SemiBold.ttf"
   end
   test do
   end
