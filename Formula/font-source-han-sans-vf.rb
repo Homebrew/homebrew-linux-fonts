a1d2f038294d120b13648f3d799420311c14aecb
TANIGUCHI Masaya <m@docs.casa>
2021-04-18 00:06:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-han-sans-vf.rb from 9f9d47169fedf662fe043337b02ae01df1c8b7e3

--- Formula/font-source-han-sans-vf.rb
+++ Formula/font-source-han-sans-vf.rb
@@ -1,9 +1,12 @@
 class FontSourceHanSansVf < Formula
-  head "https://github.com/adobe-fonts/source-han-sans/raw/release/Variable/OTC/SourceHanSans-VF.ttc"
+  version "2.003"
+  sha256 "f06c326fb2ca0d211b03bf752777e7006049867825188da9aecab42945550a07"
+  url "https://github.com/adobe-fonts/source-han-sans/raw/#{version}R/Variable/OTC/SourceHanSans-VF.ttc"
   desc "Source Han Sans VF"
   desc "思源黑體 VF"
   desc "源ノ角ゴシック VF"
   desc "본고딕 VF"
+  desc "Variable font edition of source-han-sans"
   homepage "https://github.com/adobe-fonts/source-han-sans"
   def install
     (share/"fonts").install "SourceHanSans-VF.ttc"
