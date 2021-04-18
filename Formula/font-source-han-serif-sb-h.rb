dae23261d617bab767a875f651f527cc0cae450b
TANIGUCHI Masaya <m@docs.casa>
2021-04-18 00:06:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-han-serif-sb-h.rb from 87aabbf49e5198ae76b809ce9f02be9f02ab9d0a

--- Formula/font-source-han-serif-sb-h.rb
+++ Formula/font-source-han-serif-sb-h.rb
@@ -1,7 +1,9 @@
 class FontSourceHanSerifSbH < Formula
-  version "1.001R"
-  url "https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_SB-H.zip"
+  version "1.001"
+  sha256 "e7c7c0c02e43f7cc6b1dd54e5e3a313b434123a796c8b64459f95174a6021645"
+  url "https://github.com/adobe-fonts/source-han-serif/raw/#{version}R/OTC/SourceHanSerifOTC_SB-H.zip"
   desc "Source Han Serif SB-H"
+  desc "SemiBold, Bold, and Heavy weights of source-han-serif"
   homepage "https://github.com/adobe-fonts/source-han-serif"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
@@ -13,7 +15,11 @@ class FontSourceHanSerifSbH < Formula
     #{token} only installs the SemiBold, Bold, and Heavy weights.
     To install ExtraLight, Light, Regular, and Medium:
 
-      brew install font-source-han-serif-el-m
+      brew install --cask font-source-han-serif-el-m
+
+    To install all weights in one cask:
+
+      brew install --cask font-source-han-serif
   EOS
   end
   test do
