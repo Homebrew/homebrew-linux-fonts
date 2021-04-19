90d3b2e334205580b058e053ff99da4b7c0b0b50
TANIGUCHI Masaya <m@docs.casa>
2021-04-19 00:06:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-reem-kufi.rb from c5d397cf0a70aed2185890b8c01ef355bc02aeca

--- Formula/font-reem-kufi.rb
+++ Formula/font-reem-kufi.rb
@@ -1,9 +1,9 @@
 class FontReemKufi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/reemkufi/ReemKufi-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/reemkufi/ReemKufi%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Reem Kufi"
   homepage "https://fonts.google.com/specimen/Reem+Kufi"
   def install
-    (share/"fonts").install "ReemKufi-Regular.ttf"
+    (share/"fonts").install "ReemKufi[wght].ttf"
   end
   test do
   end
