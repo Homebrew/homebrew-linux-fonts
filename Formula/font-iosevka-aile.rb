f3939dddbf24b2ecd929e5b77cca8fcbbe607eea
TANIGUCHI Masaya <m@docs.casa>
2021-04-25 00:06:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-aile.rb from 7da6c2342f1855dea5eba6548d1c9d0370b04646

--- Formula/font-iosevka-aile.rb
+++ Formula/font-iosevka-aile.rb
@@ -1,6 +1,6 @@
 class FontIosevkaAile < Formula
-  version "5.2.1"
-  sha256 "7f9fa46adc9ced1501207d8accef5c87c6ac17485c4c54da19440b864bf9ee9e"
+  version "6.0.0"
+  sha256 "40ed540a75cc388b0a45421e3f0d07c05d69f30a459625c392c9beccfd8ab7a5"
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-aile-#{version}.zip"
   desc "Iosevka Aile"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
