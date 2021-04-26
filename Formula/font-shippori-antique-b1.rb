eaecac761c23703a30119b9d2568d890a6ba49b4
TANIGUCHI Masaya <m@docs.casa>
2021-04-26 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-shippori-antique-b1.rb from 5d66fbd79267b8d0631e4cdb3a561f537c1a60a3

---
+++ Formula/font-shippori-antique-b1.rb
@@ -0,0 +1,10 @@
+class FontShipporiAntiqueB1 < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/shipporiantiqueb1/ShipporiAntiqueB1-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Shippori Antique B1"
+  homepage "https://fonts.google.com/specimen/Shippori+Antique+B1"
+  def install
+    (share/"fonts").install "ShipporiAntiqueB1-Regular.ttf"
+  end
+  test do
+  end
+end
