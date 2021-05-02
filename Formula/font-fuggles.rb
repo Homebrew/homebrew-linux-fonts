a6f7abb100ef7ad1ea968f67348c044479055272
TANIGUCHI Masaya <m@docs.casa>
2021-05-02 00:31:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fuggles.rb from e702687c0502830aa097cf046bec73fd0a512850

---
+++ Formula/font-fuggles.rb
@@ -0,0 +1,11 @@
+class FontFuggles < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/fuggles/Fuggles-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Fuggles"
+  desc "Powerful script that can be used for sophisticated commercial design"
+  homepage "https://fonts.google.com/specimen/Fuggles"
+  def install
+    (share/"fonts").install "Fuggles-Regular.ttf"
+  end
+  test do
+  end
+end
