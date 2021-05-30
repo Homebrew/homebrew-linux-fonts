711689e9352e3245e92deb7bb58958d2e67ac338
TANIGUCHI Masaya <m@docs.casa>
2021-05-30 01:00:35 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cascadia-mono.rb from 08c676ab2b9112194919e5e556ea8b5940e8f43e

--- Formula/font-cascadia-mono.rb
+++ Formula/font-cascadia-mono.rb
@@ -1,6 +1,6 @@
 class FontCascadiaMono < Formula
-  version "2102.25"
-  sha256 "a64e8834f06ec9acca4a5307cb6cf35922021d4fd0ca19096f8bf6a3e2515ad3"
+  version "2105.24"
+  sha256 "4edd89afd1009e1da121792c44bec6192001f3ffe2186a9e6f554c3bb8f5fb0b"
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
   desc "Cascadia Mono"
   desc "Version of Cascadia Code without ligatures"
@@ -13,6 +13,12 @@ class FontCascadiaMono < Formula
     (share/"fonts").install "#{parent}ttf/static/CascadiaMono-Regular.ttf"
     (share/"fonts").install "#{parent}ttf/static/CascadiaMono-SemiBold.ttf"
     (share/"fonts").install "#{parent}ttf/static/CascadiaMono-SemiLight.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaMonoItalic-BoldItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaMonoItalic-ExtraLightItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaMonoItalic-Italic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaMonoItalic-LightItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaMonoItalic-SemiBoldItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaMonoItalic-SemiLightItalic.ttf"
   end
   test do
   end
