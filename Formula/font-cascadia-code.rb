2733de49595426560a562f99036956d7321de2f9
TANIGUCHI Masaya <m@docs.casa>
2021-05-30 01:00:35 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cascadia-code.rb from 4568a80886565a7369eba5f656fd738fddd6e159

--- Formula/font-cascadia-code.rb
+++ Formula/font-cascadia-code.rb
@@ -1,6 +1,6 @@
 class FontCascadiaCode < Formula
-  version "2102.25"
-  sha256 "a64e8834f06ec9acca4a5307cb6cf35922021d4fd0ca19096f8bf6a3e2515ad3"
+  version "2105.24"
+  sha256 "4edd89afd1009e1da121792c44bec6192001f3ffe2186a9e6f554c3bb8f5fb0b"
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
   desc "Cascadia Code"
   desc "Monospaced font that includes programming ligatures"
@@ -13,6 +13,12 @@ class FontCascadiaCode < Formula
     (share/"fonts").install "#{parent}ttf/static/CascadiaCode-Regular.ttf"
     (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiBold.ttf"
     (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiLight.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-BoldItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-ExtraLightItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-Italic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-LightItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-SemiBoldItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-SemiLightItalic.ttf"
   end
   test do
   end
