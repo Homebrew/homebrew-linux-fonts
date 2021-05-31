7153f8777918a295e7b5540232b71a1a3de275e2
TANIGUCHI Masaya <m@docs.casa>
2021-05-31 01:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss18.rb from ae5b89e5c1441d7fe539d584ea69f505d30fae7b

---
+++ Formula/font-iosevka-ss18.rb
@@ -0,0 +1,21 @@
+class FontIosevkaSs18 < Formula
+  version "7.0.1"
+  sha256 "ac288eafadb0686c3620067d711a57855befee2b90b642fbbd79f664a805511c"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss18-#{version}.zip"
+  desc "Iosevka SS18"
+  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install "iosevka-ss18-bold.ttc"
+    (share/"fonts").install "iosevka-ss18-extrabold.ttc"
+    (share/"fonts").install "iosevka-ss18-extralight.ttc"
+    (share/"fonts").install "iosevka-ss18-heavy.ttc"
+    (share/"fonts").install "iosevka-ss18-light.ttc"
+    (share/"fonts").install "iosevka-ss18-medium.ttc"
+    (share/"fonts").install "iosevka-ss18-regular.ttc"
+    (share/"fonts").install "iosevka-ss18-semibold.ttc"
+    (share/"fonts").install "iosevka-ss18-thin.ttc"
+  end
+  test do
+  end
+end
