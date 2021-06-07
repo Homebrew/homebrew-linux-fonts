695a3857d9170411abe3877dfdc167efa5025f74
TANIGUCHI Masaya <m@docs.casa>
2021-06-07 00:43:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kumbh-sans.rb from be2f304b00d60a5eb8d287114c6722c9c301e951

--- Formula/font-kumbh-sans.rb
+++ Formula/font-kumbh-sans.rb
@@ -1,12 +1,10 @@
 class FontKumbhSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/kumbhsans", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/kumbhsans/KumbhSans%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Kumbh Sans"
   desc "Geometric sans-serif multi-purpose font"
   homepage "https://fonts.google.com/specimen/Kumbh+Sans"
   def install
-    (share/"fonts").install "KumbhSans-Bold.ttf"
-    (share/"fonts").install "KumbhSans-Light.ttf"
-    (share/"fonts").install "KumbhSans-Regular.ttf"
+    (share/"fonts").install "KumbhSans[wght].ttf"
   end
   test do
   end
