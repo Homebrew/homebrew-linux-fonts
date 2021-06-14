107699a46e5f35957242f34948d2e0f5dcf6eb28
TANIGUCHI Masaya <m@docs.casa>
2021-06-14 00:22:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-azeret-mono.rb from 56b5f04cc3904df158fa632c3776943d3e8177c2

---
+++ Formula/font-azeret-mono.rb
@@ -0,0 +1,12 @@
+class FontAzeretMono < Formula
+  head "https://github.com/google/fonts/trunk/ofl/azeret", verified: "github.com/google/fonts/", using: :svn
+  desc "Azeret Mono"
+  desc "Designed by martin vácha and daniel quisek"
+  homepage "https://fonts.google.com/specimen/Azeret+Mono"
+  def install
+    (share/"fonts").install "AzeretMono-Italic[wght].ttf"
+    (share/"fonts").install "AzeretMono[wght].ttf"
+  end
+  test do
+  end
+end
