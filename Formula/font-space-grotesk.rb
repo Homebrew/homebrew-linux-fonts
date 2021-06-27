e184630909920114db9619440c653baf021d7353
TANIGUCHI Masaya <m@docs.casa>
2021-06-27 00:26:32 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-space-grotesk.rb from 29797c1b7b6d26cb3d1dc775e64a1f43753716ab

--- Formula/font-space-grotesk.rb
+++ Formula/font-space-grotesk.rb
@@ -1,16 +1,15 @@
 class FontSpaceGrotesk < Formula
-  version "1.1.0"
-  sha256 "a4aaf23fe43de4096ce85c3b8dec7076c15ec5d0b8dbfbc65e91da5b173ea8e6"
-  url "https://github.com/floriankarsten/space-grotesk/releases/download/#{version}/SpaceGrotesk-v#{version}.zip"
+  version "2.0.0"
+  sha256 "53b415577d4139248555300710bea0d268c7a5be67b93de53b716a9736cabffd"
+  url "https://github.com/floriankarsten/space-grotesk/releases/download/#{version}/SpaceGrotesk-#{version}.zip"
   desc "Space Grotesk"
   homepage "https://github.com/floriankarsten/space-grotesk"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}SpaceGrotesk-v#{version}/otf/SpaceGrotesk-Bold.otf"
-    (share/"fonts").install "#{parent}SpaceGrotesk-v#{version}/otf/SpaceGrotesk-Light.otf"
-    (share/"fonts").install "#{parent}SpaceGrotesk-v#{version}/otf/SpaceGrotesk-Medium.otf"
-    (share/"fonts").install "#{parent}SpaceGrotesk-v#{version}/otf/SpaceGrotesk-Regular.otf"
-    (share/"fonts").install "#{parent}SpaceGrotesk-v#{version}/otf/SpaceGrotesk-SemiBold.otf"
+    (share/"fonts").install "#{parent}SpaceGrotesk-#{version}/otf/SpaceGrotesk-Bold.otf"
+    (share/"fonts").install "#{parent}SpaceGrotesk-#{version}/otf/SpaceGrotesk-Light.otf"
+    (share/"fonts").install "#{parent}SpaceGrotesk-#{version}/otf/SpaceGrotesk-Medium.otf"
+    (share/"fonts").install "#{parent}SpaceGrotesk-#{version}/otf/SpaceGrotesk-Regular.otf"
   end
   test do
   end
