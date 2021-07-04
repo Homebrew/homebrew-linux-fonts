34f5e7c3eae7edb9ae3165cfcb8002c646ca76c8
TANIGUCHI Masaya <m@docs.casa>
2021-07-04 00:22:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gemunu-libre.rb from 5826027184c951e4f1c07263d1ff4335089f551c

--- Formula/font-gemunu-libre.rb
+++ Formula/font-gemunu-libre.rb
@@ -1,15 +1,9 @@
 class FontGemunuLibre < Formula
-  head "https://github.com/google/fonts/trunk/ofl/gemunulibre", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/gemunulibre/GemunuLibre%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Gemunu Libre"
   homepage "https://fonts.google.com/specimen/Gemunu+Libre"
   def install
-    (share/"fonts").install "GemunuLibre-Bold.ttf"
-    (share/"fonts").install "GemunuLibre-ExtraBold.ttf"
-    (share/"fonts").install "GemunuLibre-ExtraLight.ttf"
-    (share/"fonts").install "GemunuLibre-Light.ttf"
-    (share/"fonts").install "GemunuLibre-Medium.ttf"
-    (share/"fonts").install "GemunuLibre-Regular.ttf"
-    (share/"fonts").install "GemunuLibre-SemiBold.ttf"
+    (share/"fonts").install "GemunuLibre[wght].ttf"
   end
   test do
   end
