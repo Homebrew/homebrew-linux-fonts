979eb5a61fd5cbeb3a1389eb550248d08d6bcd1b
TANIGUCHI Masaya <m@docs.casa>
2021-07-05 00:21:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ibm-plex-sans-devanagari.rb from f2bdd0f1f5f18548bb6b36d8deff0440924c34b3

---
+++ Formula/font-ibm-plex-sans-devanagari.rb
@@ -0,0 +1,16 @@
+class FontIbmPlexSansDevanagari < Formula
+  head "https://github.com/google/fonts/trunk/ofl/ibmplexsansdevanagari", verified: "github.com/google/fonts/", using: :svn
+  desc "IBM Plex Sans Devanagari"
+  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+Devanagari"
+  def install
+    (share/"fonts").install "IBMPlexSansDevanagari-Bold.ttf"
+    (share/"fonts").install "IBMPlexSansDevanagari-ExtraLight.ttf"
+    (share/"fonts").install "IBMPlexSansDevanagari-Light.ttf"
+    (share/"fonts").install "IBMPlexSansDevanagari-Medium.ttf"
+    (share/"fonts").install "IBMPlexSansDevanagari-Regular.ttf"
+    (share/"fonts").install "IBMPlexSansDevanagari-SemiBold.ttf"
+    (share/"fonts").install "IBMPlexSansDevanagari-Thin.ttf"
+  end
+  test do
+  end
+end
