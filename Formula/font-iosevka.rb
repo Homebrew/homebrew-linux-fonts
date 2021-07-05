c17bb5955b7965f39d05d832f8191ed36589a0bc
TANIGUCHI Masaya <m@docs.casa>
2021-07-05 00:21:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka.rb from 7fc1aee57151636421da88f6f2e8329d698a56dd

--- Formula/font-iosevka.rb
+++ Formula/font-iosevka.rb
@@ -1,6 +1,6 @@
 class FontIosevka < Formula
-  version "7.2.4"
-  sha256 "3285cd68bd20f2ad046963063229ce66b3fa3608dfcec2a8dc915138dc57408e"
+  version "7.2.5"
+  sha256 "c6e068c37eede7ebf656593514976b7e6ab0f0dc55ead5d401f2ae6c5d77be23"
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   desc "Iosevka"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
