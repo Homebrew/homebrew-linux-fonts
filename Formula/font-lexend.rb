11bbe361c41e968a8c44cbc0e7cadc66a88657b9
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lexend.rb from df2c6947d18a11eed430eb6b531e0eec65db48c7

--- Formula/font-lexend.rb
+++ Formula/font-lexend.rb
@@ -1,16 +1,10 @@
 class FontLexend < Formula
-  head "https://github.com/google/fonts/trunk/ofl/lexend", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/lexend/Lexend%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Lexend"
   desc "Expanded by font bureau with a range of 8 weights"
   homepage "https://fonts.google.com/specimen/Lexend"
   def install
-    (share/"fonts").install "Lexend-Bold.ttf"
-    (share/"fonts").install "Lexend-ExtraBold.ttf"
-    (share/"fonts").install "Lexend-Light.ttf"
-    (share/"fonts").install "Lexend-Medium.ttf"
-    (share/"fonts").install "Lexend-Regular.ttf"
-    (share/"fonts").install "Lexend-SemiBold.ttf"
-    (share/"fonts").install "Lexend-Thin.ttf"
+    (share/"fonts").install "Lexend[wght].ttf"
   end
   test do
   end
