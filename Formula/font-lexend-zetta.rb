c0c5fb04c79d72f554f15529fc68ccdcb7ce7f1e
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lexend-zetta.rb from df2c6947d18a11eed430eb6b531e0eec65db48c7

--- Formula/font-lexend-zetta.rb
+++ Formula/font-lexend-zetta.rb
@@ -1,9 +1,9 @@
 class FontLexendZetta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lexendzetta/LexendZetta-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/lexendzetta/LexendZetta%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Lexend Zetta"
   homepage "https://fonts.google.com/specimen/Lexend+Zetta"
   def install
-    (share/"fonts").install "LexendZetta-Regular.ttf"
+    (share/"fonts").install "LexendZetta[wght].ttf"
   end
   test do
   end
