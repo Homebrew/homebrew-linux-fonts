e3450ca511b54cd7127cab0181e46eba9e6f84c5
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lexend-exa.rb from df2c6947d18a11eed430eb6b531e0eec65db48c7

--- Formula/font-lexend-exa.rb
+++ Formula/font-lexend-exa.rb
@@ -1,9 +1,9 @@
 class FontLexendExa < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lexendexa/LexendExa-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/lexendexa/LexendExa%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Lexend Exa"
   homepage "https://fonts.google.com/specimen/Lexend+Exa"
   def install
-    (share/"fonts").install "LexendExa-Regular.ttf"
+    (share/"fonts").install "LexendExa[wght].ttf"
   end
   test do
   end
