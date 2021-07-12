0052724f714a43ef8a83265a051c5316674ff014
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lexend-tera.rb from df2c6947d18a11eed430eb6b531e0eec65db48c7

--- Formula/font-lexend-tera.rb
+++ Formula/font-lexend-tera.rb
@@ -1,9 +1,9 @@
 class FontLexendTera < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lexendtera/LexendTera-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/lexendtera/LexendTera%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Lexend Tera"
   homepage "https://fonts.google.com/specimen/Lexend+Tera"
   def install
-    (share/"fonts").install "LexendTera-Regular.ttf"
+    (share/"fonts").install "LexendTera[wght].ttf"
   end
   test do
   end
