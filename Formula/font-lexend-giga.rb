9a6effcbd2667ecffc55fb2b606ad896ece1b681
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lexend-giga.rb from df2c6947d18a11eed430eb6b531e0eec65db48c7

--- Formula/font-lexend-giga.rb
+++ Formula/font-lexend-giga.rb
@@ -1,9 +1,9 @@
 class FontLexendGiga < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lexendgiga/LexendGiga-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/lexendgiga/LexendGiga%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Lexend Giga"
   homepage "https://fonts.google.com/specimen/Lexend+Giga"
   def install
-    (share/"fonts").install "LexendGiga-Regular.ttf"
+    (share/"fonts").install "LexendGiga[wght].ttf"
   end
   test do
   end
