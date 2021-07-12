2bd70243a0199cf57a00986250c66b8e952d143c
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-urbanist.rb from ffcbd1025e766766ca9c50607e881f042647958a

--- Formula/font-urbanist.rb
+++ Formula/font-urbanist.rb
@@ -1,12 +1,30 @@
 class FontUrbanist < Formula
-  version "1.220"
-  sha256 "ed4f5d9fb76ce6e147cce266a81268e9aa270ee2c2633f198499e51be3b1ae8e"
-  url "https://github.com/coreywho/Urbanist/releases/download/#{version}/Urbanist.Font.Family.zip"
+  version "1.251"
+  sha256 "653b0298e14c0ee6a530b466e2af88b88a1b829edbdaf0e6500efcd4561fd1d0"
+  url "https://github.com/coreywho/Urbanist/releases/download/#{version}/Urbanist_Font_Family_#{version}.zip"
   desc "Urbanist"
   homepage "https://github.com/coreywho/Urbanist"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}fonts/variable/UrbanistGX.ttf"
+    (share/"fonts").install "#{parent}fonts/variable/Urbanist[ital,wght].ttf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-Black.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-BlackItalic.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-Bold.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-BoldItalic.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-ExtraBold.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-ExtraBoldItalic.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-ExtraLight.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-ExtraLightItalic.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-Italic.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-Light.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-LightItalic.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-Medium.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-MediumItalic.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-Regular.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-SemiBold.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-SemiBoldItalic.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-Thin.otf"
+    (share/"fonts").install "#{parent}fonts/otf/Urbanist-ThinItalic.otf"
   end
   test do
   end
