34ba6463efb533987d6315897575e13238261ca3
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lexend-peta.rb from df2c6947d18a11eed430eb6b531e0eec65db48c7

--- Formula/font-lexend-peta.rb
+++ Formula/font-lexend-peta.rb
@@ -1,9 +1,9 @@
 class FontLexendPeta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lexendpeta/LexendPeta-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/lexendpeta/LexendPeta%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Lexend Peta"
   homepage "https://fonts.google.com/specimen/Lexend+Peta"
   def install
-    (share/"fonts").install "LexendPeta-Regular.ttf"
+    (share/"fonts").install "LexendPeta[wght].ttf"
   end
   test do
   end
