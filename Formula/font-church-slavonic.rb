c1e0cd0209ed1b034c0f316162e8e231d5fc816f
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:32 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-church-slavonic.rb from 886001a57001947543c5c2069e8f1f85d36f91a4

--- Formula/font-church-slavonic.rb
+++ Formula/font-church-slavonic.rb
@@ -1,19 +1,26 @@
 class FontChurchSlavonic < Formula
-  version "2.1"
-  sha256 "76ac825d4c7c853cad374b6cba9ca18be7040a892e2ebf3954ed978623f43cab"
+  version "2.2.1"
+  sha256 "6fd44c6fb4fecc01ecea8dda6efc18bf46646f2e5d997c7d60e0cbae3aa8ff2e"
   url "https://github.com/typiconman/fonts-cu/releases/download/v#{version}/fonts-churchslavonic.zip"
   desc "Church Slavonic Fonts"
   homepage "https://github.com/typiconman/fonts-cu"
   def install
-    (share/"fonts").install "CathismaUnicode.otf"
-    (share/"fonts").install "FedorovskUnicode.otf"
-    (share/"fonts").install "IndictionUnicode.otf"
-    (share/"fonts").install "MenaionUnicode.otf"
-    (share/"fonts").install "MonomakhUnicode.otf"
-    (share/"fonts").install "OglavieUnicode.otf"
-    (share/"fonts").install "PomorskyUnicode.otf"
-    (share/"fonts").install "PonomarUnicode.otf"
-    (share/"fonts").install "VertogradUnicode.otf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}fonts-churchslavonic/Acathist-Regular.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/CathismaUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/FedorovskUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/IndictionUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/MenaionUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/MezenetsUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/MonomakhUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/OglavieUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/PochaevskUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/PomorskyUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/PonomarUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/Shafarik-Regular.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/TriodionUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/VertogradUnicode.otf"
+    (share/"fonts").install "#{parent}fonts-churchslavonic/Voskresensky-Regular.otf"
   end
   test do
   end
