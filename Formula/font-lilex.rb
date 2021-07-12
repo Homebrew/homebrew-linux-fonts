5582d2b532e428ead29a7227a198482fea3596e3
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lilex.rb from 85b50c10a1d86ee1d911de9d196087910525820c

--- Formula/font-lilex.rb
+++ Formula/font-lilex.rb
@@ -1,15 +1,15 @@
 class FontLilex < Formula
-  version "1.000"
-  sha256 "c73b0241fedac898014cbfe9b8701593e8d6472f43adb6b8bc31a5c45d44c904"
+  version "1.100"
+  sha256 "1570385aa6fd596d11971a3ab800a220f10a6e8fd1f3b164177561244495bb7d"
   url "https://github.com/mishamyrt/Lilex/releases/download/#{version}/Lilex-#{version}.zip"
   desc "Lilex"
   homepage "https://github.com/mishamyrt/Lilex"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}otf/Lilex-Bold.otf"
-    (share/"fonts").install "#{parent}otf/Lilex-Light.otf"
     (share/"fonts").install "#{parent}otf/Lilex-Medium.otf"
     (share/"fonts").install "#{parent}otf/Lilex-Regular.otf"
+    (share/"fonts").install "#{parent}variable_ttf/Lilex-VF.ttf"
   end
   test do
   end
