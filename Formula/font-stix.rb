65f0de1a6704a2bf1c7b80adb5af2d9e698edd3f
TANIGUCHI Masaya <m@docs.casa>
2021-07-12 00:24:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stix.rb from 50e7b8e0755f932a3771a6e61830075b257d4381

--- Formula/font-stix.rb
+++ Formula/font-stix.rb
@@ -1,17 +1,21 @@
 class FontStix < Formula
-  version "2.0.2"
-  sha256 "b8eb0e63739e839ad620c82db1f6f38d8927f7fd30abcc8f147700ba3bc71918"
+  version "2.13"
+  sha256 "c4671ec4a116d887c8ebf91b2706301bebc43e776ac00f549c36ad1f34964c98"
   url "https://github.com/stipub/stixfonts/archive/v#{version}.tar.gz", verified: "github.com/stipub/stixfonts/"
   desc "STIX"
   desc "Unicode fonts for scientific, technical, and mathematical texts"
   homepage "https://stixfonts.org/"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}stixfonts-#{version}/OTF/STIX2Math.otf"
-    (share/"fonts").install "#{parent}stixfonts-#{version}/OTF/STIX2Text-Bold.otf"
-    (share/"fonts").install "#{parent}stixfonts-#{version}/OTF/STIX2Text-BoldItalic.otf"
-    (share/"fonts").install "#{parent}stixfonts-#{version}/OTF/STIX2Text-Italic.otf"
-    (share/"fonts").install "#{parent}stixfonts-#{version}/OTF/STIX2Text-Regular.otf"
+    (share/"fonts").install "#{parent}stixfonts-#{version}/fonts/static_otf/STIXTwoMath-Regular.otf"
+    (share/"fonts").install "#{parent}stixfonts-#{version}/fonts/static_otf/STIXTwoText-Bold.otf"
+    (share/"fonts").install "#{parent}stixfonts-#{version}/fonts/static_otf/STIXTwoText-BoldItalic.otf"
+    (share/"fonts").install "#{parent}stixfonts-#{version}/fonts/static_otf/STIXTwoText-Italic.otf"
+    (share/"fonts").install "#{parent}stixfonts-#{version}/fonts/static_otf/STIXTwoText-Medium.otf"
+    (share/"fonts").install "#{parent}stixfonts-#{version}/fonts/static_otf/STIXTwoText-MediumItalic.otf"
+    (share/"fonts").install "#{parent}stixfonts-#{version}/fonts/static_otf/STIXTwoText-Regular.otf"
+    (share/"fonts").install "#{parent}stixfonts-#{version}/fonts/static_otf/STIXTwoText-SemiBold.otf"
+    (share/"fonts").install "#{parent}stixfonts-#{version}/fonts/static_otf/STIXTwoText-SemiBoldItalic.otf"
   end
   test do
   end
