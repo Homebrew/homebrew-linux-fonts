9e34d157dc473076536c403e0898faf4d0b34bfb
TANIGUCHI Masaya <m@docs.casa>
2021-07-15 00:22:24 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cascadia-code.rb from 4ee2f2f725d4d146549416aedf81a4a24b2895ec

--- Formula/font-cascadia-code.rb
+++ Formula/font-cascadia-code.rb
@@ -1,6 +1,6 @@
 class FontCascadiaCode < Formula
-  version "2105.24"
-  sha256 "4edd89afd1009e1da121792c44bec6192001f3ffe2186a9e6f554c3bb8f5fb0b"
+  version "2106.17"
+  sha256 "22c030fec5e5c516580fd6e24b385c935baae2f6c110024aeba20240d533a68a"
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
   desc "Cascadia Code"
   desc "Monospaced font that includes programming ligatures"
@@ -8,17 +8,17 @@ class FontCascadiaCode < Formula
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}ttf/static/CascadiaCode-Bold.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-BoldItalic.ttf"
     (share/"fonts").install "#{parent}ttf/static/CascadiaCode-ExtraLight.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-ExtraLightItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-Italic.ttf"
     (share/"fonts").install "#{parent}ttf/static/CascadiaCode-Light.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-LightItalic.ttf"
     (share/"fonts").install "#{parent}ttf/static/CascadiaCode-Regular.ttf"
     (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiBold.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiBoldItalic.ttf"
     (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiLight.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-BoldItalic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-ExtraLightItalic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-Italic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-LightItalic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-SemiBoldItalic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCodeItalic-SemiLightItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiLightItalic.ttf"
   end
   test do
   end
