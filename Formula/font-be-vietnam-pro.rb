bf5fa29785b71ce9efde626e7b421b5bb3e32d76
TANIGUCHI Masaya <m@docs.casa>
2021-07-19 00:24:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-be-vietnam-pro.rb from 08395c17dfe880bf2cbe1e1ebeb95fd970ba8886

---
+++ Formula/font-be-vietnam-pro.rb
@@ -0,0 +1,28 @@
+class FontBeVietnamPro < Formula
+  head "https://github.com/google/fonts/trunk/ofl/bevietnampro", verified: "github.com/google/fonts/", using: :svn
+  desc "Be Vietnam Pro"
+  desc "Well suited to tech companies and startups"
+  homepage "https://fonts.google.com/specimen/Be+Vietnam+Pro"
+  def install
+    (share/"fonts").install "BeVietnamPro-Black.ttf"
+    (share/"fonts").install "BeVietnamPro-BlackItalic.ttf"
+    (share/"fonts").install "BeVietnamPro-Bold.ttf"
+    (share/"fonts").install "BeVietnamPro-BoldItalic.ttf"
+    (share/"fonts").install "BeVietnamPro-ExtraBold.ttf"
+    (share/"fonts").install "BeVietnamPro-ExtraBoldItalic.ttf"
+    (share/"fonts").install "BeVietnamPro-ExtraLight.ttf"
+    (share/"fonts").install "BeVietnamPro-ExtraLightItalic.ttf"
+    (share/"fonts").install "BeVietnamPro-Italic.ttf"
+    (share/"fonts").install "BeVietnamPro-Light.ttf"
+    (share/"fonts").install "BeVietnamPro-LightItalic.ttf"
+    (share/"fonts").install "BeVietnamPro-Medium.ttf"
+    (share/"fonts").install "BeVietnamPro-MediumItalic.ttf"
+    (share/"fonts").install "BeVietnamPro-Regular.ttf"
+    (share/"fonts").install "BeVietnamPro-SemiBold.ttf"
+    (share/"fonts").install "BeVietnamPro-SemiBoldItalic.ttf"
+    (share/"fonts").install "BeVietnamPro-Thin.ttf"
+    (share/"fonts").install "BeVietnamPro-ThinItalic.ttf"
+  end
+  test do
+  end
+end
