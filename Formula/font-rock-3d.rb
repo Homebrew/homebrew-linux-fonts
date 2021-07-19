a2597f3c4c5e6784b74c6ee816859ea1882d96ed
TANIGUCHI Masaya <m@docs.casa>
2021-07-19 00:24:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rock-3d.rb from 08395c17dfe880bf2cbe1e1ebeb95fd970ba8886

---
+++ Formula/font-rock-3d.rb
@@ -0,0 +1,10 @@
+class FontRock3d < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/rock3d/Rock3D-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Rock 3D"
+  homepage "https://fonts.google.com/specimen/Rock+3D"
+  def install
+    (share/"fonts").install "Rock3D-Regular.ttf"
+  end
+  test do
+  end
+end
