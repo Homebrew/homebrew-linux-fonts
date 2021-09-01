7e1696de465a519d7238405c5b6e513fbc428f99
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-09-01 10:49:58 +0900

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-curly.rb from f5527fdcc179c444a5d82a2b3dc22dcb2026ca53

--- Formula/font-iosevka-curly.rb
+++ Formula/font-iosevka-curly.rb
@@ -1,6 +1,6 @@
 class FontIosevkaCurly < Formula
-  version "7.3.3"
-  sha256 "688e291aa73b2f7d7d70805f1518b177dab9ec837b9a2792c4ba032cca3ef2f4"
+  version "10.1.1"
+  sha256 "52295123fb8cea97ee32f7e69077acddc526376d4c1ab2ae6d4b0141c7cf2ae2"
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
   desc "Iosevka Curly"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
