48b54069d1867d50ef495e83770dcd90d58bbc41
TANIGUCHI Masaya <m@docs.casa>
2021-09-10 00:28:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-urbanist.rb from 3b85fbab1fe3f11a02548130f8f6fe70897a94ca

--- Formula/font-urbanist.rb
+++ Formula/font-urbanist.rb
@@ -1,7 +1,7 @@
 class FontUrbanist < Formula
-  version "1.300"
-  sha256 "cd9e045dd2bfab1aded56abf58efbeda87868829c3df89f1baac48211fe0443e"
-  url "https://github.com/coreywho/Urbanist/releases/download/#{version}/Urbanist_Font_Family_#{version}.zip"
+  version "1.302"
+  sha256 "7d139d674d20831e6b2257f734b1cb700c57cdd8961e26cfb74d8304bdf63aea"
+  url "https://github.com/coreywho/Urbanist/releases/download/v#{version}/Urbanist_Font_Family_#{version}.zip"
   desc "Urbanist"
   homepage "https://github.com/coreywho/Urbanist"
   def install
