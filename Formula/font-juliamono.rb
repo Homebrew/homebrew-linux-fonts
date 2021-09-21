a5f760743c1eb8baa7b2eccb47af951bc62dbc8f
TANIGUCHI Masaya <m@docs.casa>
2021-09-21 00:24:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-juliamono.rb from 0cee3bfeb9e50323984c65b32e76c578a9cf3a60

--- Formula/font-juliamono.rb
+++ Formula/font-juliamono.rb
@@ -1,20 +1,27 @@
 class FontJuliamono < Formula
-  version "0.042"
-  sha256 "bf7894f2c682f00a5ec733cd1ac02883a8d3f7b03e093a5bcd8848d08e9c1ffc"
+  version "0.043"
+  sha256 "4e15bc5886bdb1767179912e6079ae4c2844c42f29efce0e16e2b5944c930f71"
   url "https://github.com/cormullion/juliamono/releases/download/v#{version}/JuliaMono.tar.gz"
   desc "JuliaMono"
   desc "Monospaced typeface designed for programming in Julia"
   homepage "https://github.com/cormullion/juliamono"
   def install
     (share/"fonts").install "JuliaMono-Black.ttf"
+    (share/"fonts").install "JuliaMono-BlackItalic.ttf"
     (share/"fonts").install "JuliaMono-Bold.ttf"
+    (share/"fonts").install "JuliaMono-BoldItalic.ttf"
     (share/"fonts").install "JuliaMono-BoldLatin.ttf"
     (share/"fonts").install "JuliaMono-ExtraBold.ttf"
+    (share/"fonts").install "JuliaMono-ExtraBoldItalic.ttf"
     (share/"fonts").install "JuliaMono-Light.ttf"
+    (share/"fonts").install "JuliaMono-LightItalic.ttf"
     (share/"fonts").install "JuliaMono-Medium.ttf"
+    (share/"fonts").install "JuliaMono-MediumItalic.ttf"
     (share/"fonts").install "JuliaMono-Regular.ttf"
+    (share/"fonts").install "JuliaMono-RegularItalic.ttf"
     (share/"fonts").install "JuliaMono-RegularLatin.ttf"
     (share/"fonts").install "JuliaMono-SemiBold.ttf"
+    (share/"fonts").install "JuliaMono-SemiBoldItalic.ttf"
   end
   test do
   end
