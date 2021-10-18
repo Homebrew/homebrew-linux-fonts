ccc233e4003dfc322115c65e832364b6a2e2f385
TANIGUCHI Masaya <m@docs.casa>
2021-10-18 00:27:34 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-etoile.rb from bc6cbbbec8bb61af2b621df138c29cb0973d8f43

--- Formula/font-iosevka-etoile.rb
+++ Formula/font-iosevka-etoile.rb
@@ -1,6 +1,6 @@
 class FontIosevkaEtoile < Formula
-  version "10.3.2"
-  sha256 "5cc46c7b664a20b0c06a47de5145be07fc10d4dc6afeee8ec9ba70d1dfbeb169"
+  version "10.3.3"
+  sha256 "633befcf90f1fa27096ead7f1511d5270dd2bee9870fc5ff6a0cc95c770dc439"
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-etoile-#{version}.zip"
   desc "Iosevka Etoile"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
