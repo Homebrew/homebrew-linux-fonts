5fdc89b0cff6346d2d6b1c1434bbda65d044b53e
TANIGUCHI Masaya <m@docs.casa>
2021-11-05 00:27:14 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-code-pro.rb from af813d6bb0bf96e39759bc44216073390306d3cb

--- Formula/font-source-code-pro.rb
+++ Formula/font-source-code-pro.rb
@@ -3,20 +3,8 @@ class FontSourceCodePro < Formula
   desc "Source Code Pro"
   homepage "https://fonts.google.com/specimen/Source+Code+Pro"
   def install
-    (share/"fonts").install "SourceCodePro-Black.ttf"
-    (share/"fonts").install "SourceCodePro-BlackItalic.ttf"
-    (share/"fonts").install "SourceCodePro-Bold.ttf"
-    (share/"fonts").install "SourceCodePro-BoldItalic.ttf"
-    (share/"fonts").install "SourceCodePro-ExtraLight.ttf"
-    (share/"fonts").install "SourceCodePro-ExtraLightItalic.ttf"
-    (share/"fonts").install "SourceCodePro-Italic.ttf"
-    (share/"fonts").install "SourceCodePro-Light.ttf"
-    (share/"fonts").install "SourceCodePro-LightItalic.ttf"
-    (share/"fonts").install "SourceCodePro-Medium.ttf"
-    (share/"fonts").install "SourceCodePro-MediumItalic.ttf"
-    (share/"fonts").install "SourceCodePro-Regular.ttf"
-    (share/"fonts").install "SourceCodePro-SemiBold.ttf"
-    (share/"fonts").install "SourceCodePro-SemiBoldItalic.ttf"
+    (share/"fonts").install "SourceCodePro[wght].ttf"
+    (share/"fonts").install "SourceCodePro-Italic[wght].ttf"
   end
   test do
   end
