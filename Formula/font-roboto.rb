242e9d2b38c5e06b346b152cd17d1057ba86a962
TANIGUCHI Masaya <m@docs.casa>
2022-01-05 00:32:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-roboto.rb from 132c473969e14b7aa41c2f072673163b75728c24

--- Formula/font-roboto.rb
+++ Formula/font-roboto.rb
@@ -1,6 +1,7 @@
 class FontRoboto < Formula
   head "https://github.com/google/fonts/trunk/apache/roboto", verified: "github.com/google/fonts/", using: :svn
   desc "Roboto"
+  desc "Font with a mechanical skeleton and the forms are largely geometric"
   homepage "https://fonts.google.com/specimen/Roboto"
   def install
     (share/"fonts").install "Roboto-Italic[wdth,wght].ttf"
