ae221c08222ffece9e8f4c0047fcb4bf09025d2b
TANIGUCHI Masaya <m@docs.casa>
2022-01-09 00:32:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss11.rb from 52204469048063a68a2e8e2720672503446be870

--- Formula/font-iosevka-ss11.rb
+++ Formula/font-iosevka-ss11.rb
@@ -1,6 +1,6 @@
 class FontIosevkaSs11 < Formula
-  version "11.2.4"
-  sha256 "d50a0066084c88f033bbefb2ea24dacdb2f3f41f7a8014e709992de8bf3522f3"
+  version "11.2.5"
+  sha256 "45903a84b591fa54ecea235013177aa5162811025652e667152b65465f6880c1"
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss11-#{version}.zip"
   desc "Iosevka SS11"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
