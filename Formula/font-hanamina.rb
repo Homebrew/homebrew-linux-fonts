86bb6825424206350c41e8ae73b5c05339a6ef95
TANIGUCHI Masaya <m@docs.casa>
2022-01-13 00:33:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hanamina.rb from 7408724109e62357b0ece047f59127a7467b29a7

--- Formula/font-hanamina.rb
+++ Formula/font-hanamina.rb
@@ -1,7 +1,7 @@
 class FontHanamina < Formula
   version "2017.09.04,68253"
   sha256 "571cd4a09ae7da0c642d640fc2442c050aa450ebb0587a95cdd097d41a9c9572"
-  url "https://osdn.dl.osdn.jp/hanazono-font/#{version.to_s.sub(/.*,/, "")}/hanazono-#{version.to_s.sub(/,.*/, "").gsub(".", "")}.zip", verified: "osdn.dl.osdn.jp/hanazono-font/"
+  url "https://osdn.dl.osdn.jp/hanazono-font/#{version.to_s.csv.second}/hanazono-#{version.to_s.csv.first.gsub(".", "")}.zip", verified: "osdn.dl.osdn.jp/hanazono-font/"
   desc "HanaMinA"
   homepage "https://fonts.jp/hanazono/"
   def install
