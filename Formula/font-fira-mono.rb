caaeb14bfbe679698351463633147724429563a2
TANIGUCHI Masaya <m@docs.casa>
2022-01-13 00:33:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-mono.rb from 7408724109e62357b0ece047f59127a7467b29a7

--- Formula/font-fira-mono.rb
+++ Formula/font-fira-mono.rb
@@ -1,7 +1,7 @@
 class FontFiraMono < Formula
   version "3.206,4.202"
   sha256 "d86269657387f144d77ba12011124f30f423f70672e1576dc16f918bb16ddfe4"
-  url "https://github.com/mozilla/Fira/archive/#{version.to_s.sub(/.*,/, "")}.tar.gz", verified: "github.com/mozilla/Fira/"
+  url "https://github.com/mozilla/Fira/archive/#{version.to_s.csv.second}.tar.gz", verified: "github.com/mozilla/Fira/"
   desc "Fira Mono"
   homepage "https://mozilla.github.io/Fira/"
   def install
