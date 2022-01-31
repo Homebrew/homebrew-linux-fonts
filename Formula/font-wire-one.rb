8a8d4ac644224363580dc63bd633ae15a486be57
TANIGUCHI Masaya <m@docs.casa>
2022-01-31 00:29:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wire-one.rb from 79db65c83e5291253faec46751c05fcb4c614265

--- Formula/font-wire-one.rb
+++ Formula/font-wire-one.rb
@@ -1,9 +1,9 @@
 class FontWireOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/wireone/WireOne.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/wireone/WireOne-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Wire One"
   homepage "https://fonts.google.com/specimen/Wire+One"
   def install
-    (share/"fonts").install "WireOne.ttf"
+    (share/"fonts").install "WireOne-Regular.ttf"
   end
   test do
   end
