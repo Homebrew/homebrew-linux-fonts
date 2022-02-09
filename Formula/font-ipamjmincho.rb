90330d4ecdf91fcd63d036e41325cc85151be8e2
TANIGUCHI Masaya <m@docs.casa>
2022-02-09 00:32:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ipamjmincho.rb from 0204b07e378b1c979666eaf9895262276ce7d009

--- Formula/font-ipamjmincho.rb
+++ Formula/font-ipamjmincho.rb
@@ -1,10 +1,10 @@
 class FontIpamjmincho < Formula
   version "006.01"
   sha256 "35494e0f2896f38b3f7369a8421a895cea6440a42c0a66ac95eab47d6ed25b68"
-  url "https://mojikiban.ipa.go.jp/OSCDL/IPAmjMincho/ipamjm#{version.to_s.gsub(".", "")}.zip"
+  url "https://dforest.watch.impress.co.jp/library/i/ipamjfont/10750/ipamjm#{version.to_s.gsub(".", "")}.zip", verified: "dforest.watch.impress.co.jp/library/i/ipamjfont/10750/"
   desc "IPAmjMincho"
   desc "IPAmj明朝"
-  homepage "https://mojikiban.ipa.go.jp/1300.html"
+  homepage "https://moji.or.jp/mojikiban/font/"
   def install
     (share/"fonts").install "ipamjm.ttf"
   end
