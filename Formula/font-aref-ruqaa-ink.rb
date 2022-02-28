01efcf6a9e63d4fe3162f320b35dc65e0c7b813a
TANIGUCHI Masaya <m@docs.casa>
2022-02-28 00:36:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aref-ruqaa-ink.rb from 5819b7f4ff627c8eb722527a4289aee255bd210e

---
+++ Formula/font-aref-ruqaa-ink.rb
@@ -0,0 +1,12 @@
+class FontArefRuqaaInk < Formula
+  head "https://github.com/google/fonts/trunk/ofl/arefruqaaink", verified: "github.com/google/fonts/", using: :svn
+  desc "Aref Ruqaa Ink"
+  desc "Led by khaled hosny, a type designer based in cairo, egypt"
+  homepage "https://fonts.google.com/specimen/Aref+Ruqaa+Ink"
+  def install
+    (share/"fonts").install "ArefRuqaaInk-Bold.otf"
+    (share/"fonts").install "ArefRuqaaInk-Regular.otf"
+  end
+  test do
+  end
+end
