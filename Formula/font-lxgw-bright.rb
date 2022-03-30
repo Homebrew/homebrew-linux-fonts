5ec40a6c417c303d65fbaac5371979c94453ba40
TANIGUCHI Masaya <m@docs.casa>
2022-03-30 00:40:29 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lxgw-bright.rb from 2df5e8ada8608a60ab51f912007d5f50b74bf9b6

---
+++ Formula/font-lxgw-bright.rb
@@ -0,0 +1,19 @@
+class FontLxgwBright < Formula
+  version "1.230"
+  sha256 "9530b2c273407cae8146d74bffd7f1993fb4059b88c2fdff4d02f51a41e41b9b"
+  url "https://github.com/lxgw/LxgwBright/archive/refs/tags/v#{version}.zip"
+  desc "LXGW Bright"
+  desc "Merged font of Ysabeau Office and LXGW WenKai"
+  homepage "https://github.com/lxgw/LxgwBright"
+  def install
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}LxgwBright-#{version}/OTF/LXGWBright-Medium.otf"
+    (share/"fonts").install "#{parent}LxgwBright-#{version}/OTF/LXGWBright-MediumItalic.otf"
+    (share/"fonts").install "#{parent}LxgwBright-#{version}/OTF/LXGWBright-Regular.otf"
+    (share/"fonts").install "#{parent}LxgwBright-#{version}/OTF/LXGWBright-Italic.otf"
+    (share/"fonts").install "#{parent}LxgwBright-#{version}/OTF/LXGWBright-SemiLight.otf"
+    (share/"fonts").install "#{parent}LxgwBright-#{version}/OTF/LXGWBright-SemiLightItalic.otf"
+  end
+  test do
+  end
+end
