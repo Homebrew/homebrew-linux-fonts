a9ec96375d85903ddb01beb8063e0911cbb7aaf2
TANIGUCHI Masaya <m@docs.casa>
2022-04-19 00:46:32 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-amiri-quran-colored.rb from fb1c70d20f29b94f79c5f8d95ee00f9cfea2116d

--- Formula/font-amiri-quran-colored.rb
+++ Formula/font-amiri-quran-colored.rb
@@ -1,9 +1,9 @@
 class FontAmiriQuranColored < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/amiriqurancolored/AmiriQuranColored-Regular.otf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/amiriqurancolored/AmiriQuranColored-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Amiri Quran Colored"
   homepage "https://fonts.google.com/specimen/Amiri+Quran+Colored"
   def install
-    (share/"fonts").install "AmiriQuranColored-Regular.otf"
+    (share/"fonts").install "AmiriQuranColored-Regular.ttf"
   end
   test do
   end
