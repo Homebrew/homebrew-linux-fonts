1e813d8ca07d8a8656b947d7a0d6b2e20d81aa69
TANIGUCHI Masaya <m@docs.casa>
2022-05-02 00:46:54 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-radio-canada.rb from a9d28816a9001eded30c4caa6c6900a9eb400ec4

--- Formula/font-radio-canada.rb
+++ Formula/font-radio-canada.rb
@@ -1,8 +1,9 @@
 class FontRadioCanada < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/radiocanada/RadioCanada%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/trunk/ofl/radiocanada", verified: "github.com/google/fonts/", using: :svn
   desc "Radio Canada"
   homepage "https://fonts.google.com/specimen/Radio+Canada"
   def install
+    (share/"fonts").install "RadioCanada-Italic[wdth,wght].ttf"
     (share/"fonts").install "RadioCanada[wdth,wght].ttf"
   end
   test do
