4f8289c4982b31a5c9062185113517b8473cf7f7
TANIGUCHI Masaya <m@docs.casa>
2022-05-08 00:44:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-kr.rb from b0bc9ebb4d84ff0e2399090db03724e5aaf5b9ad

--- Formula/font-noto-sans-kr.rb
+++ Formula/font-noto-sans-kr.rb
@@ -1,15 +1,10 @@
 class FontNotoSansKr < Formula
-  head "https://github.com/google/fonts/trunk/ofl/notosanskr", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/notosanskr/NotoSansKR%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans KR"
   desc "Unmodulated (“sans serif”) design for the korean language"
   homepage "https://fonts.google.com/specimen/Noto+Sans+KR"
   def install
-    (share/"fonts").install "NotoSansKR-Black.otf"
-    (share/"fonts").install "NotoSansKR-Bold.otf"
-    (share/"fonts").install "NotoSansKR-Light.otf"
-    (share/"fonts").install "NotoSansKR-Medium.otf"
-    (share/"fonts").install "NotoSansKR-Regular.otf"
-    (share/"fonts").install "NotoSansKR-Thin.otf"
+    (share/"fonts").install "NotoSansKR[wght].ttf"
   end
   test do
   end
