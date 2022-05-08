8fde265abbf673065ca72bceb66cb6c0ae58bcc2
TANIGUCHI Masaya <m@docs.casa>
2022-05-08 00:44:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-hk.rb from b0bc9ebb4d84ff0e2399090db03724e5aaf5b9ad

--- Formula/font-noto-sans-hk.rb
+++ Formula/font-noto-sans-hk.rb
@@ -1,15 +1,10 @@
 class FontNotoSansHk < Formula
-  head "https://github.com/google/fonts/trunk/ofl/notosanshk", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/notosanshk/NotoSansHK%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans HK"
   desc "Sans-serif design using the traditional chinese variant of the han ideograms"
   homepage "https://fonts.google.com/specimen/Noto+Sans+HK"
   def install
-    (share/"fonts").install "NotoSansHK-Black.otf"
-    (share/"fonts").install "NotoSansHK-Bold.otf"
-    (share/"fonts").install "NotoSansHK-Light.otf"
-    (share/"fonts").install "NotoSansHK-Medium.otf"
-    (share/"fonts").install "NotoSansHK-Regular.otf"
-    (share/"fonts").install "NotoSansHK-Thin.otf"
+    (share/"fonts").install "NotoSansHK[wght].ttf"
   end
   test do
   end
