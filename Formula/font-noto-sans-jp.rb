9567f3dd23055be0ad54fabdfe3b0b8267a0d609
TANIGUCHI Masaya <m@docs.casa>
2022-05-08 00:44:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-jp.rb from b0bc9ebb4d84ff0e2399090db03724e5aaf5b9ad

--- Formula/font-noto-sans-jp.rb
+++ Formula/font-noto-sans-jp.rb
@@ -1,15 +1,10 @@
 class FontNotoSansJp < Formula
-  head "https://github.com/google/fonts/trunk/ofl/notosansjp", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/notosansjp/NotoSansJP%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans JP"
   desc "Unmodulated (“sans serif”) design for the japanese language"
   homepage "https://fonts.google.com/specimen/Noto+Sans+JP"
   def install
-    (share/"fonts").install "NotoSansJP-Black.otf"
-    (share/"fonts").install "NotoSansJP-Bold.otf"
-    (share/"fonts").install "NotoSansJP-Light.otf"
-    (share/"fonts").install "NotoSansJP-Medium.otf"
-    (share/"fonts").install "NotoSansJP-Regular.otf"
-    (share/"fonts").install "NotoSansJP-Thin.otf"
+    (share/"fonts").install "NotoSansJP[wght].ttf"
   end
   test do
   end
