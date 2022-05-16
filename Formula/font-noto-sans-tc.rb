551f4279a199184daa782725a4523bd5b6ade8d6
TANIGUCHI Masaya <m@docs.casa>
2022-05-16 00:50:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tc.rb from 8204016e1cb88629635de105a5a11925c99ab023

--- Formula/font-noto-sans-tc.rb
+++ Formula/font-noto-sans-tc.rb
@@ -1,15 +1,10 @@
 class FontNotoSansTc < Formula
-  head "https://github.com/google/fonts/trunk/ofl/notosanstc", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/notosanstc/NotoSansTC%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans TC"
   desc "Unmodulated (“sans serif”) design for languages in taiwan and macau"
   homepage "https://fonts.google.com/specimen/Noto+Sans+TC"
   def install
-    (share/"fonts").install "NotoSansTC-Black.otf"
-    (share/"fonts").install "NotoSansTC-Bold.otf"
-    (share/"fonts").install "NotoSansTC-Light.otf"
-    (share/"fonts").install "NotoSansTC-Medium.otf"
-    (share/"fonts").install "NotoSansTC-Regular.otf"
-    (share/"fonts").install "NotoSansTC-Thin.otf"
+    (share/"fonts").install "NotoSansTC[wght].ttf"
   end
   test do
   end
