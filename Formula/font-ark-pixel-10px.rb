11c35e12536e29b16c62a03aeeb1032dd4d9d0d4
Alan Ferguson <alan@ferguson.info>
2022-05-21 15:32:05 +0100

    Fixed 'no_hyphen' version property in Formulae

--- Formula/font-ark-pixel-10px.rb
+++ Formula/font-ark-pixel-10px.rb
@@ -1,7 +1,7 @@
 class FontArkPixel10px < Formula
   version "2022-05-04"
   sha256 "860363726a131d0790278d994779f8ed47ae1668bf6c740ee5661145189e27a6"
-  url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-10px-otf-v0.0.0-dev-#{version.to_s.no_hyphens}.zip"
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-10px-otf-v0.0.0-dev-#{version.to_s.gsub("-", "")}.zip"
   desc "Ark Pixel 10px"
   homepage "https://github.com/TakWolf/ark-pixel-font"
   def install
--- Formula/font-ark-pixel-12px.rb
+++ Formula/font-ark-pixel-12px.rb
@@ -1,7 +1,7 @@
 class FontArkPixel12px < Formula
   version "2022-05-04"
   sha256 "8ce91e766f478dc75967d80f8e7bdeb060ff1f1fad96d7e0421dbd74bd5ba7c7"
-  url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-12px-otf-v0.0.0-dev-#{version.to_s.no_hyphens}.zip"
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-12px-otf-v0.0.0-dev-#{version.to_s.gsub("-", "")}.zip"
   desc "Ark Pixel 12px"
   homepage "https://github.com/TakWolf/ark-pixel-font"
   def install
