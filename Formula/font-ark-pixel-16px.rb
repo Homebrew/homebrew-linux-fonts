979674bfa6e2d83e15e8d6510c5cb9297cf84512
TANIGUCHI Masaya <m@docs.casa>
2022-05-23 00:46:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ark-pixel-16px.rb from 2ac61f367655f29e5bc0f27f231b95481e5eee0d

--- Formula/font-ark-pixel-16px.rb
+++ Formula/font-ark-pixel-16px.rb
@@ -1,7 +1,7 @@
 class FontArkPixel16px < Formula
   version "2022-05-20"
   sha256 "e59d6a6a53fafdd67b1b34032d16c6a0c31883f4ea4cbac0bbe134941c537db9"
-  url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-16px-otf-v0.0.0-dev-#{version.to_s.no_hyphens}.zip"
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-16px-otf-v0.0.0-dev-#{version.to_s.gsub("-", "")}.zip"
   desc "Ark Pixel 16px"
   homepage "https://github.com/TakWolf/ark-pixel-font"
   def install
