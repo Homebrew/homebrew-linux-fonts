30bbef1252a7f126207581a6bd3238e2417dcab3
TANIGUCHI Masaya <m@docs.casa>
2022-05-25 00:52:56 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-camingocode.rb from 9f4ad8f24ecba25e3134a7ef32a877efab3a69c5

--- Formula/font-camingocode.rb
+++ Formula/font-camingocode.rb
@@ -1,14 +1,14 @@
 class FontCamingocode < Formula
   version "1.0"
-  sha256 "13b188ca8d1b5fe2c3ba880771f20a6faac550405ae98d12edd46240f8896714"
-  url "https://janfromm.de/_data/downloads/CamingoCode-v#{version}.zip"
+  url "https://janfromm.de/typefaces/camingocode/camingocode.zip"
   desc "CamingoCode"
   homepage "https://www.janfromm.de/typefaces/camingomono/camingocode/"
   def install
-    (share/"fonts").install "CamingoCode-Bold.ttf"
-    (share/"fonts").install "CamingoCode-BoldItalic.ttf"
-    (share/"fonts").install "CamingoCode-Regular.ttf"
-    (share/"fonts").install "CamingoCode-Italic.ttf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}TTF/CamingoCode-Bold.ttf"
+    (share/"fonts").install "#{parent}TTF/CamingoCode-BoldItalic.ttf"
+    (share/"fonts").install "#{parent}TTF/CamingoCode-Italic.ttf"
+    (share/"fonts").install "#{parent}TTF/CamingoCode-Regular.ttf"
   end
   test do
   end
