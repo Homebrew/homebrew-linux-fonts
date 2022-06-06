9737f6cccdfda7e18c7298df9fcdb1b23276fc01
TANIGUCHI Masaya <m@docs.casa>
2022-06-06 00:51:03 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spline-sans-mono.rb from dbcf6fe06c7920346d70faca55df2a28996b9c03

--- Formula/font-spline-sans-mono.rb
+++ Formula/font-spline-sans-mono.rb
@@ -1,9 +1,10 @@
 class FontSplineSansMono < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/splinesansmono/SplineSansMono%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/trunk/ofl/splinesansmono", verified: "github.com/google/fonts/", using: :svn
   desc "Spline Sans Mono"
   desc "Original typeface initiated by the spline team"
   homepage "https://fonts.google.com/specimen/Spline+Sans+Mono"
   def install
+    (share/"fonts").install "SplineSansMono-Italic[wght].ttf"
     (share/"fonts").install "SplineSansMono[wght].ttf"
   end
   test do
