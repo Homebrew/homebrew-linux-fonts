578f4eede0f1e18d7fb88e1940784bb5052fcab2
TANIGUCHI Masaya <m@docs.casa>
2022-06-06 00:51:03 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gluten.rb from dbcf6fe06c7920346d70faca55df2a28996b9c03

--- Formula/font-gluten.rb
+++ Formula/font-gluten.rb
@@ -1,10 +1,10 @@
 class FontGluten < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gluten/Gluten%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/gluten/Gluten%5Bslnt%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Gluten"
   desc "Filling, we'll put it that way"
   homepage "https://fonts.google.com/specimen/Gluten"
   def install
-    (share/"fonts").install "Gluten[wght].ttf"
+    (share/"fonts").install "Gluten[slnt,wght].ttf"
   end
   test do
   end
