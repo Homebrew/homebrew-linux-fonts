d2f48c0ac0bc5befd3f2b5d9a260648eb9140bdb
TANIGUCHI Masaya <m@docs.casa>
2022-06-13 00:51:36 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-kr.rb from 267d5c2f8b30ab38411423823c06d9c79faaa667

--- Formula/font-noto-serif-kr.rb
+++ Formula/font-noto-serif-kr.rb
@@ -1,16 +1,10 @@
 class FontNotoSerifKr < Formula
-  head "https://github.com/google/fonts/trunk/ofl/notoserifkr", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifkr/NotoSerifKR%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Serif KR"
   desc "Modulated (“serif”) design for the korean language"
   homepage "https://fonts.google.com/specimen/Noto+Serif+KR"
   def install
-    (share/"fonts").install "NotoSerifKR-Black.otf"
-    (share/"fonts").install "NotoSerifKR-Bold.otf"
-    (share/"fonts").install "NotoSerifKR-ExtraLight.otf"
-    (share/"fonts").install "NotoSerifKR-Light.otf"
-    (share/"fonts").install "NotoSerifKR-Medium.otf"
-    (share/"fonts").install "NotoSerifKR-Regular.otf"
-    (share/"fonts").install "NotoSerifKR-SemiBold.otf"
+    (share/"fonts").install "NotoSerifKR[wght].ttf"
   end
   test do
   end
