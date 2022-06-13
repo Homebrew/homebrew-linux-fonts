4f0856e76d72066b8e8626119f302d3d6bd26a87
TANIGUCHI Masaya <m@docs.casa>
2022-06-13 00:51:36 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-sc.rb from 267d5c2f8b30ab38411423823c06d9c79faaa667

--- Formula/font-noto-serif-sc.rb
+++ Formula/font-noto-serif-sc.rb
@@ -1,16 +1,10 @@
 class FontNotoSerifSc < Formula
-  head "https://github.com/google/fonts/trunk/ofl/notoserifsc", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifsc/NotoSerifSC%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Serif SC"
   desc "Modulated (“serif”) design for simplified chinese"
   homepage "https://fonts.google.com/specimen/Noto+Serif+SC"
   def install
-    (share/"fonts").install "NotoSerifSC-Black.otf"
-    (share/"fonts").install "NotoSerifSC-Bold.otf"
-    (share/"fonts").install "NotoSerifSC-ExtraLight.otf"
-    (share/"fonts").install "NotoSerifSC-Light.otf"
-    (share/"fonts").install "NotoSerifSC-Medium.otf"
-    (share/"fonts").install "NotoSerifSC-Regular.otf"
-    (share/"fonts").install "NotoSerifSC-SemiBold.otf"
+    (share/"fonts").install "NotoSerifSC[wght].ttf"
   end
   test do
   end
