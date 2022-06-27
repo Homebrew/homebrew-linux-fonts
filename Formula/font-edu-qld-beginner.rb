6434f7c7ccd853a29387e07fd4ef8a18d4a272ca
TANIGUCHI Masaya <m@docs.casa>
2022-06-27 00:55:22 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-edu-qld-beginner.rb from a8ded148fc85bdd1555df80a8de968157a284897

--- Formula/font-edu-qld-beginner.rb
+++ Formula/font-edu-qld-beginner.rb
@@ -1,7 +1,7 @@
 class FontEduQldBeginner < Formula
   head "https://github.com/google/fonts/raw/main/ofl/eduqldbeginner/EduQLDBeginner%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Edu QLD Beginners"
-  homepage "https://fonts.google.com/specimen/Edu+QLD+Beginners"
+  desc "Edu QLD Beginner"
+  homepage "https://fonts.google.com/specimen/Edu+QLD+Beginner"
   def install
     (share/"fonts").install "EduQLDBeginner[wght].ttf"
   end
