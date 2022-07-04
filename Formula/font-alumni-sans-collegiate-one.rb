5816cb56e5375097514cdbfe10a468517b3d1c89
TANIGUCHI Masaya <m@docs.casa>
2022-07-04 00:07:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alumni-sans-collegiate-one.rb from 8dc293c9d21ff47dad4a0cc37ae747e9595240df

--- Formula/font-alumni-sans-collegiate-one.rb
+++ Formula/font-alumni-sans-collegiate-one.rb
@@ -1,9 +1,10 @@
 class FontAlumniSansCollegiateOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alumnisanscollegiateone/AlumniSansCollegiateOne-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/trunk/ofl/alumnisanscollegiateone", verified: "github.com/google/fonts/", using: :svn
   desc "Alumni Sans Collegiate One"
   desc "Font inspired by Impact Black"
   homepage "https://fonts.google.com/specimen/Alumni+Sans+Collegiate+One"
   def install
+    (share/"fonts").install "AlumniSansCollegiateOne-Italic.ttf"
     (share/"fonts").install "AlumniSansCollegiateOne-Regular.ttf"
   end
   test do
