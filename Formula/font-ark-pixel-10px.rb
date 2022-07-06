254d2d26190d785aab9d6f59fdce816b63b34854
TANIGUCHI Masaya <m@docs.casa>
2022-07-06 00:06:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ark-pixel-10px.rb from bdccb68663bfa45bef98f27d109f38bdde76b8ce

--- Formula/font-ark-pixel-10px.rb
+++ Formula/font-ark-pixel-10px.rb
@@ -1,7 +1,7 @@
 class FontArkPixel10px < Formula
-  version "2022-06-08"
-  sha256 "0573c7d43054bafd3a379fe79a2516643e48421fd81575530a337c5a6693badb"
-  url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-10px-otf-v0.0.0-dev-#{version.to_s.gsub("-", "")}.zip"
+  version "2022.07.05"
+  sha256 "872c95e5316c418fcbeb69618cfaa12695193d33a8fb0a6d002896ad8640e441"
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-otf-v#{version.to_s.gsub("-", "")}.zip"
   desc "Ark Pixel 10px"
   homepage "https://github.com/TakWolf/ark-pixel-font"
   def install
