dfb9b3e7b2b233d97ffff11dee5d10dcf9db835d
TANIGUCHI Masaya <m@docs.casa>
2022-07-12 00:06:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-charis-sil.rb from 02d8190834fc0cf9e1a3d681815e8f30c01af086

--- Formula/font-charis-sil.rb
+++ Formula/font-charis-sil.rb
@@ -1,15 +1,16 @@
 class FontCharisSil < Formula
-  version "5.000"
-  sha256 "5e3e5473b30363008c289cc87e2aa584a0916087a63a3f689defa8e0cee09bfd"
+  version "6.101"
+  sha256 "2de585f4517636d18039f1dc082258dfb89262d7a7feea2526fb21c0cc69131f"
   url "https://software.sil.org/downloads/r/charis/CharisSIL-#{version}.zip"
   desc "Charis SIL"
-  homepage "https://software.sil.org/charis/download/"
+  desc "Unicode-based font family supporting languages using Latin and Cyrillic scripts"
+  homepage "https://software.sil.org/charis/"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}CharisSIL-#{version}/CharisSIL-R.ttf"
-    (share/"fonts").install "#{parent}CharisSIL-#{version}/CharisSIL-B.ttf"
-    (share/"fonts").install "#{parent}CharisSIL-#{version}/CharisSIL-BI.ttf"
-    (share/"fonts").install "#{parent}CharisSIL-#{version}/CharisSIL-I.ttf"
+    (share/"fonts").install "#{parent}CharisSIL-#{version}/CharisSIL-Bold.ttf"
+    (share/"fonts").install "#{parent}CharisSIL-#{version}/CharisSIL-BoldItalic.ttf"
+    (share/"fonts").install "#{parent}CharisSIL-#{version}/CharisSIL-Italic.ttf"
+    (share/"fonts").install "#{parent}CharisSIL-#{version}/CharisSIL-Regular.ttf"
   end
   test do
   end
