718e01db8ec16219bb9c97a4e93e55b9e0bc1155
TANIGUCHI Masaya <m@docs.casa>
2022-07-12 00:06:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-andika.rb from 5f74776bae04d7e094cbf686b14c02014aedb01c

--- Formula/font-andika.rb
+++ Formula/font-andika.rb
@@ -1,12 +1,16 @@
 class FontAndika < Formula
-  version "5.000"
-  sha256 "604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507"
+  version "6.101"
+  sha256 "8595a879054a540b24fa942a2621efcd194a9f6468d9cf2541ae693e496b8dd4"
   url "https://software.sil.org/downloads/r/andika/Andika-#{version}.zip"
   desc "Andika"
+  desc "Sans-serif font family designed and optimized for literacy use"
   homepage "https://software.sil.org/andika/"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}Andika-#{version}/Andika-R.ttf"
+    (share/"fonts").install "#{parent}Andika-#{version}/Andika-Bold.ttf"
+    (share/"fonts").install "#{parent}Andika-#{version}/Andika-BoldItalic.ttf"
+    (share/"fonts").install "#{parent}Andika-#{version}/Andika-Italic.ttf"
+    (share/"fonts").install "#{parent}Andika-#{version}/Andika-Regular.ttf"
   end
   test do
   end
