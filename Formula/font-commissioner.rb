e6afc8158f64880b1d1b8042d829518f13b16053
TANIGUCHI Masaya <m@docs.casa>
2022-07-25 00:06:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-commissioner.rb from cc1f50ee418658a364bd54c5d6dce3cecfa05f1e

--- Formula/font-commissioner.rb
+++ Formula/font-commissioner.rb
@@ -1,10 +1,10 @@
 class FontCommissioner < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/commissioner/Commissioner%5BFLAR%2CVOLM%2Cslnt%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/commissioner/Commissioner%5Bslnt%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Commissioner"
   desc "Low-contrast humanist sans-serif font with almost classical proportions"
   homepage "https://fonts.google.com/specimen/Commissioner"
   def install
-    (share/"fonts").install "Commissioner[FLAR,VOLM,slnt,wght].ttf"
+    (share/"fonts").install "Commissioner[slnt,wght].ttf"
   end
   test do
   end
