4b3238782479ea84b8d7393bcc835eea75a7b293
TANIGUCHI Masaya <m@docs.casa>
2022-08-09 00:07:23 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fruktur.rb from 385e32db668873c89960b2754187dca881c00c42

--- Formula/font-fruktur.rb
+++ Formula/font-fruktur.rb
@@ -1,8 +1,9 @@
 class FontFruktur < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/fruktur/Fruktur-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/trunk/ofl/fruktur", verified: "github.com/google/fonts/", using: :svn
   desc "Fruktur"
   homepage "https://fonts.google.com/specimen/Fruktur"
   def install
+    (share/"fonts").install "Fruktur-Italic.ttf"
     (share/"fonts").install "Fruktur-Regular.ttf"
   end
   test do
