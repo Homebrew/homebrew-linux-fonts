2022b18add6c3d73a959211a2afc18ad80fe8fdb
TANIGUCHI Masaya <m@docs.casa>
2022-08-16 00:07:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hackgen-nerd.rb from eaadf3f0882d4440306607d123f393a73e4ab5cd

--- Formula/font-hackgen-nerd.rb
+++ Formula/font-hackgen-nerd.rb
@@ -1,20 +1,20 @@
 class FontHackgenNerd < Formula
-  version "2.6.3"
-  sha256 "3120f82ac2a7b742acd863425c080031d7cd8f5716403ea582adc4b5193bf691"
-  url "https://github.com/yuru7/HackGen/releases/download/v#{version}/HackGenNerd_v#{version}.zip"
+  version "2.7.0"
+  sha256 "58f9b5b652ebbb0e07a2bae1d8ad1177848ec8b961519e4fecdb464524b73f7c"
+  url "https://github.com/yuru7/HackGen/releases/download/v#{version}/HackGen_NF_v#{version}.zip"
   desc "HackGenNerd"
   desc "Japanese programming font which is a composed of Hack and GenJyuu-Gothic"
   homepage "https://github.com/yuru7/HackGen"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}HackGenNerd_v#{version}/HackGen35Nerd-Bold.ttf"
-    (share/"fonts").install "#{parent}HackGenNerd_v#{version}/HackGen35Nerd-Regular.ttf"
-    (share/"fonts").install "#{parent}HackGenNerd_v#{version}/HackGen35NerdConsole-Bold.ttf"
-    (share/"fonts").install "#{parent}HackGenNerd_v#{version}/HackGen35NerdConsole-Regular.ttf"
-    (share/"fonts").install "#{parent}HackGenNerd_v#{version}/HackGenNerd-Bold.ttf"
-    (share/"fonts").install "#{parent}HackGenNerd_v#{version}/HackGenNerd-Regular.ttf"
-    (share/"fonts").install "#{parent}HackGenNerd_v#{version}/HackGenNerdConsole-Bold.ttf"
-    (share/"fonts").install "#{parent}HackGenNerd_v#{version}/HackGenNerdConsole-Regular.ttf"
+    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGen35ConsoleNF-Bold.ttf"
+    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGen35ConsoleNF-Regular.ttf"
+    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGen35ConsoleNFJ-Bold.ttf"
+    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGen35ConsoleNFJ-Regular.ttf"
+    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGenConsoleNF-Bold.ttf"
+    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGenConsoleNF-Regular.ttf"
+    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGenConsoleNFJ-Bold.ttf"
+    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGenConsoleNFJ-Regular.ttf"
   end
   test do
   end
