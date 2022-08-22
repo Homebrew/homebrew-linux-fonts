37e8e11af91fe34160cdfe35216437212caaf13d
TANIGUCHI Masaya <m@docs.casa>
2022-08-22 00:07:22 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-reem-kufi-ink.rb from 65a8bb22de8dbb283ad102ae9446c0788eb3c7e4

--- Formula/font-reem-kufi-ink.rb
+++ Formula/font-reem-kufi-ink.rb
@@ -1,10 +1,9 @@
 class FontReemKufiInk < Formula
-  head "https://github.com/google/fonts/trunk/ofl/reemkufiink", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/reemkufiink/ReemKufiInk-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Reem Kufi Ink"
   desc "Also the name of khaled's daughter"
   homepage "https://fonts.google.com/specimen/Reem+Kufi+Ink"
   def install
-    (share/"fonts").install "ReemKufiInk-Bold.ttf"
     (share/"fonts").install "ReemKufiInk-Regular.ttf"
   end
   test do
