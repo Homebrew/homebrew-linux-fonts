d22a2311b1a194320c4fb3388ebdc612e2bdd0a3
TANIGUCHI Masaya <m@docs.casa>
2022-08-24 00:06:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cascadia-code.rb from 476c76090b8bf93c36cff0792d9faa74a56f96f1

--- Formula/font-cascadia-code.rb
+++ Formula/font-cascadia-code.rb
@@ -7,18 +7,8 @@ class FontCascadiaCode < Formula
   homepage "https://github.com/microsoft/cascadia-code"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-Bold.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-BoldItalic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-ExtraLight.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-ExtraLightItalic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-Italic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-Light.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-LightItalic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-Regular.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiBold.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiBoldItalic.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiLight.ttf"
-    (share/"fonts").install "#{parent}ttf/static/CascadiaCode-SemiLightItalic.ttf"
+    (share/"fonts").install "#{parent}ttf/CascadiaCode.ttf"
+    (share/"fonts").install "#{parent}ttf/CascadiaCodeItalic.ttf"
   end
   test do
   end
