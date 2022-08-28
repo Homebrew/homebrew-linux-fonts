10ff70df79f8f707660117c2cc62fea7002b94be
TANIGUCHI Masaya <m@docs.casa>
2022-08-28 00:07:59 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lxgw-bright.rb from e2a8ee854b35f5ba765e755352a03d23c66e42ce

--- Formula/font-lxgw-bright.rb
+++ Formula/font-lxgw-bright.rb
@@ -1,6 +1,6 @@
 class FontLxgwBright < Formula
-  version "2.149"
-  sha256 "6decf98cd70eca672c74b4a6f79db030708d404f49144d1bcd1f1d4a3b2d5df6"
+  version "2.155"
+  sha256 "c0529c169638a86afb860dfb8c160f55a59e8c7cfa740b88a01667541aea262f"
   url "https://github.com/lxgw/LxgwBright/archive/refs/tags/v#{version}.zip"
   desc "LXGW Bright"
   desc "Merged font of Ysabeau Office and LXGW WenKai"
