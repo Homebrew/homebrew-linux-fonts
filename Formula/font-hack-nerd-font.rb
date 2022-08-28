e59b8267f939f946b45ea62af6b8ba1e47ce6758
TANIGUCHI Masaya <m@docs.casa>
2022-08-28 00:07:59 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hack-nerd-font.rb from 79c0eeaf58d1afbc8381dd778ea1172e4052323e

--- Formula/font-hack-nerd-font.rb
+++ Formula/font-hack-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontHackNerdFont < Formula
-  version "2.1.0"
-  sha256 "70852e59fcffbe31d401f615625bcb9ebb6af72732c2f1fe9b9d5370c2565514"
+  version "2.2.0"
+  sha256 "09f9b0ad4ceec25f7bb81bed0ae10b4e3ceae08301a973104e07add17bc9df98"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hack.zip"
   desc "Hack Nerd Font (Hack)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
