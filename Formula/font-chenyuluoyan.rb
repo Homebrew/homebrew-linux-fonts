8cdf414f925b9fe29f643e249ea0a83aa36fac9d
TANIGUCHI Masaya <m@docs.casa>
2022-09-10 00:07:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chenyuluoyan.rb from 68d9db56bee93115a2e8856ded554dbf2356c138

---
+++ Formula/font-chenyuluoyan.rb
@@ -0,0 +1,14 @@
+class FontChenyuluoyan < Formula
+  version "1.0-motospaced"
+  sha256 "d8ecd0598634c92f0b29f90aabef8f7916f17aef19b0350883ca7b46979a5373"
+  url "https://github.com/Chenyu-otf/chenyuluoyan_thin/releases/download/v#{version}/ChenYuluoyan-Thin-Monospaced.ttf"
+  desc "font-chenyuluoyan"
+  desc "辰宇落雁體"
+  desc "Open-source traditional Chinese handwritten font"
+  homepage "https://github.com/Chenyu-otf/chenyuluoyan_thin"
+  def install
+    (share/"fonts").install "ChenYuluoyan-Thin-Monospaced.ttf"
+  end
+  test do
+  end
+end
