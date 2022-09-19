beab7eae3f2373d470c4541183eb6e785364a73e
TANIGUCHI Masaya <m@docs.casa>
2022-09-19 00:07:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nabla.rb from 683bcf8e13dc7d275a056a6733a45a7e20cc8202

--- Formula/font-nabla.rb
+++ Formula/font-nabla.rb
@@ -1,7 +1,8 @@
 class FontNabla < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf", verified: "github.com/google/fonts/"
   desc "Nabla"
-  homepage "https://github.com/justvanrossum/nabla"
+  desc "Color font inspired by isometric computer games, built using the colrv1 format"
+  homepage "https://fonts.google.com/specimen/Nabla"
   def install
     (share/"fonts").install "Nabla[EDPT,EHLT].ttf"
   end
