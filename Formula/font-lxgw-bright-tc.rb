91e759a83108af009a275c9c85adfccd79355da6
TANIGUCHI Masaya <m@docs.casa>
2022-09-29 00:12:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lxgw-bright-tc.rb from acadf2ba9a0e5508e7e945076eb26b7d549236ca

--- Formula/font-lxgw-bright-tc.rb
+++ Formula/font-lxgw-bright-tc.rb
@@ -1,18 +1,18 @@
 class FontLxgwBrightTc < Formula
-  version "4.124"
-  sha256 "31ba535644f3ccc76ea4bb3e4fb8e03288aee4e9a617f95c51d8d66f1de21b0f"
-  url "https://github.com/lxgw/LxgwBright/archive/refs/tags/v#{version}.zip"
+  version "4.132.4"
+  sha256 "a7b1eabd0f50705257adebbb995ec6d99710aef95a69f49100ab9f52b9b617d6"
+  url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightTC.7z"
   desc "LXGW Bright TC"
   desc "Merged font of Ysabeau Office and LXGW WenKai TC"
   homepage "https://github.com/lxgw/LxgwBright"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-Medium.otf"
-    (share/"fonts").install "#{parent}LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-MediumItalic.otf"
-    (share/"fonts").install "#{parent}LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-Regular.otf"
-    (share/"fonts").install "#{parent}LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-Italic.otf"
-    (share/"fonts").install "#{parent}LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-SemiLight.otf"
-    (share/"fonts").install "#{parent}LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-SemiLightItalic.otf"
+    (share/"fonts").install "#{parent}LXGWBrightTC/LXGWBrightTC-Medium.otf"
+    (share/"fonts").install "#{parent}LXGWBrightTC/LXGWBrightTC-MediumItalic.otf"
+    (share/"fonts").install "#{parent}LXGWBrightTC/LXGWBrightTC-Regular.otf"
+    (share/"fonts").install "#{parent}LXGWBrightTC/LXGWBrightTC-Italic.otf"
+    (share/"fonts").install "#{parent}LXGWBrightTC/LXGWBrightTC-SemiLight.otf"
+    (share/"fonts").install "#{parent}LXGWBrightTC/LXGWBrightTC-SemiLightItalic.otf"
   end
   test do
   end
