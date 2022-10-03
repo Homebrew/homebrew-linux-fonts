71a47e78335727b3e477f91c71f384b3bfaddec6
TANIGUCHI Masaya <m@docs.casa>
2022-10-03 00:09:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-bassa-vah.rb from 406e3bdf4b4542b46a7673cca419655a9558dd58

--- Formula/font-noto-sans-bassa-vah.rb
+++ Formula/font-noto-sans-bassa-vah.rb
@@ -1,9 +1,9 @@
 class FontNotoSansBassaVah < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansbassavah/NotoSansBassaVah-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansbassavah/NotoSansBassaVah%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans Bassa Vah"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Bassa+Vah"
   def install
-    (share/"fonts").install "NotoSansBassaVah-Regular.ttf"
+    (share/"fonts").install "NotoSansBassaVah[wght].ttf"
   end
   test do
   end
