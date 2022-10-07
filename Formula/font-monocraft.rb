6c341e8f1fd0d1ba0d442884c1a766d1db4a7bed
TANIGUCHI Masaya <m@docs.casa>
2022-10-07 00:12:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monocraft.rb from ac551e50f9d3c27204e4686407a05d206cb8e925

--- Formula/font-monocraft.rb
+++ Formula/font-monocraft.rb
@@ -1,6 +1,6 @@
 class FontMonocraft < Formula
-  version "1.3"
-  sha256 "4d7d54ce08cdacf66991a683ff325f519668623404dd111c03ae4c9d54996e36"
+  version "1.4"
+  sha256 "fbdf76af2b4b72def4e6ea035fbc92aac12f093900db1b9e2c47430e3cc21030"
   url "https://github.com/IdreesInc/Monocraft/releases/download/v#{version}/Monocraft.otf"
   desc "Monocraft"
   desc "Programming font based on the typeface used in Minecraft"
