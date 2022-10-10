f4a8019ebcab035ab15a69385ab5ac6036c1b3da
TANIGUCHI Masaya <m@docs.casa>
2022-10-10 00:09:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cairo.rb from 2923c0c21508f3f1dbdcde7098891f2c87e2c52c

--- Formula/font-cairo.rb
+++ Formula/font-cairo.rb
@@ -1,9 +1,9 @@
 class FontCairo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/cairo/Cairo%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/cairo/Cairo%5Bslnt%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Cairo"
   homepage "https://fonts.google.com/specimen/Cairo"
   def install
-    (share/"fonts").install "Cairo[wght].ttf"
+    (share/"fonts").install "Cairo[slnt,wght].ttf"
   end
   test do
   end
