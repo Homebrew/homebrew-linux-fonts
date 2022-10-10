9d014a9ee5552d5d47557401f10f7ddc0438b507
TANIGUCHI Masaya <m@docs.casa>
2022-10-10 00:09:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-readex-pro.rb from 2923c0c21508f3f1dbdcde7098891f2c87e2c52c

--- Formula/font-readex-pro.rb
+++ Formula/font-readex-pro.rb
@@ -1,10 +1,10 @@
 class FontReadexPro < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/readexpro/ReadexPro%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/readexpro/ReadexPro%5BHEXP%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Readex Pro"
   desc "Family of variable fonts"
   homepage "https://fonts.google.com/specimen/Readex+Pro"
   def install
-    (share/"fonts").install "ReadexPro[wght].ttf"
+    (share/"fonts").install "ReadexPro[HEXP,wght].ttf"
   end
   test do
   end
