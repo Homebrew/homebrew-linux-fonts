1e1f0ac17b76db5803b9f210f16d12f5713456c1
TANIGUCHI Masaya <m@docs.casa>
2022-10-18 00:10:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-merienda.rb from 385e3937d2edce719f4171d6d591bee94a191509

--- Formula/font-merienda.rb
+++ Formula/font-merienda.rb
@@ -1,10 +1,9 @@
 class FontMerienda < Formula
-  head "https://github.com/google/fonts/trunk/ofl/merienda", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts/raw/main/ofl/merienda/Merienda%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Merienda"
   homepage "https://fonts.google.com/specimen/Merienda"
   def install
-    (share/"fonts").install "Merienda-Bold.ttf"
-    (share/"fonts").install "Merienda-Regular.ttf"
+    (share/"fonts").install "Merienda[wght].ttf"
   end
   test do
   end
