ed0faf20da4f4dc82cdc7dbef250e160e531e5d4
TANIGUCHI Masaya <m@docs.casa>
2022-10-18 00:10:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-handjet.rb from 385e3937d2edce719f4171d6d591bee94a191509

--- Formula/font-handjet.rb
+++ Formula/font-handjet.rb
@@ -1,10 +1,10 @@
 class FontHandjet < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/handjet/Handjet%5BEGRD%2CESHP%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/handjet/Handjet%5BELGR%2CELSH%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Handjet"
   desc "Font tailored to work well with handjet printers"
   homepage "https://fonts.google.com/specimen/Handjet"
   def install
-    (share/"fonts").install "Handjet[EGRD,ESHP,wght].ttf"
+    (share/"fonts").install "Handjet[ELGR,ELSH,wght].ttf"
   end
   test do
   end
