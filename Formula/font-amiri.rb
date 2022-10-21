90c00ddc44512795142bebab049fe4d5433fbbea
TANIGUCHI Masaya <m@docs.casa>
2022-10-21 00:11:59 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-amiri.rb from 2f79042714531784ba73b8d52aea122eaa786a40

--- Formula/font-amiri.rb
+++ Formula/font-amiri.rb
@@ -1,8 +1,9 @@
 class FontAmiri < Formula
-  version "0.117"
-  sha256 "9c4e768893e0023a0ad6f488d5c84bd5add6565d3dcadb838ba5b20e75fcc9a7"
+  version "0.900"
+  sha256 "ab5dbcf2464ef929e8bd0a25533686a305c921731456173d2e19023281bfe361"
   url "https://github.com/alif-type/amiri/releases/download/#{version}/Amiri-#{version}.zip", verified: "github.com/alif-type/amiri/"
   desc "Amiri"
+  desc "Classical Arabic typeface in Naskh style"
   homepage "https://www.amirifont.org/"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
