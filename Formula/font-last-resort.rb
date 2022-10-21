5dc1a9edc32c848adcc80731232c8375c60f969e
TANIGUCHI Masaya <m@docs.casa>
2022-10-21 00:11:59 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-last-resort.rb from e443e6956bed9e78746a7e3f0ea9be650ef7f7d9

--- Formula/font-last-resort.rb
+++ Formula/font-last-resort.rb
@@ -1,6 +1,6 @@
 class FontLastResort < Formula
-  version "14.000"
-  sha256 "da83a62294e74d963a10de4c3750ccf089273e3b7fc6744daef9844163ade078"
+  version "15.000"
+  sha256 "b5a6cbd70646342aca928c619d544e2c844d73ba346db256b421ebaac02c5fd1"
   url "https://github.com/unicode-org/last-resort-font/releases/download/#{version}/LastResort-Regular.ttf"
   desc "Last Resort"
   desc "Special-purpose font, providing glyphs to represent types of Unicode characters"
