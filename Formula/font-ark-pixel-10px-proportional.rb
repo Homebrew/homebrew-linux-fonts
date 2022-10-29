0ca6c7720998c57614be00bf221913a143e4458c
TANIGUCHI Masaya <m@docs.casa>
2022-10-29 00:06:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ark-pixel-10px-proportional.rb from 4b269b21da4d3bc2eaa83d07135cdc43d9f76dd4

---
+++ Formula/font-ark-pixel-10px-proportional.rb
@@ -0,0 +1,19 @@
+class FontArkPixel10pxProportional < Formula
+  version "2022.10.27"
+  sha256 "073a12e79b7cfc62035c459d1762c3f1b5e7abac95d7c777ef5552c4d7f9a632"
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-proportional-otf-v#{version}.zip"
+  desc "Ark Pixel 10px proportional"
+  desc "Open source Pan-CJK pixel font"
+  homepage "https://github.com/TakWolf/ark-pixel-font"
+  def install
+    (share/"fonts").install "ark-pixel-10px-proportional-ja.otf"
+    (share/"fonts").install "ark-pixel-10px-proportional-ko.otf"
+    (share/"fonts").install "ark-pixel-10px-proportional-latin.otf"
+    (share/"fonts").install "ark-pixel-10px-proportional-zh_cn.otf"
+    (share/"fonts").install "ark-pixel-10px-proportional-zh_hk.otf"
+    (share/"fonts").install "ark-pixel-10px-proportional-zh_tr.otf"
+    (share/"fonts").install "ark-pixel-10px-proportional-zh_tw.otf"
+  end
+  test do
+  end
+end
