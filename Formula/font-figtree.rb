736f72589e9f7a08fc5c3890c162ae26c2c80eb3
TANIGUCHI Masaya <m@docs.casa>
2022-11-07 00:08:03 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-figtree.rb from f3cc9512724757a3e28bfc8b39728320045fbd10

--- Formula/font-figtree.rb
+++ Formula/font-figtree.rb
@@ -1,9 +1,10 @@
 class FontFigtree < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/figtree/Figtree%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/trunk/ofl/figtree", verified: "github.com/google/fonts/", using: :svn
   desc "Figtree"
   desc "Clean yet friendly geometric sans serif font for usage in web and mobile apps"
   homepage "https://fonts.google.com/specimen/Figtree"
   def install
+    (share/"fonts").install "Figtree-Italic[wght].ttf"
     (share/"fonts").install "Figtree[wght].ttf"
   end
   test do
