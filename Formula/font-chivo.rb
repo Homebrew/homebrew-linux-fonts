fc42596482244ecd373d0675e125948c5d665c99
TANIGUCHI Masaya <m@docs.casa>
2022-11-07 00:08:03 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chivo.rb from f3cc9512724757a3e28bfc8b39728320045fbd10

--- Formula/font-chivo.rb
+++ Formula/font-chivo.rb
@@ -3,14 +3,8 @@ class FontChivo < Formula
   desc "Chivo"
   homepage "https://fonts.google.com/specimen/Chivo"
   def install
-    (share/"fonts").install "Chivo-Black.ttf"
-    (share/"fonts").install "Chivo-BlackItalic.ttf"
-    (share/"fonts").install "Chivo-Bold.ttf"
-    (share/"fonts").install "Chivo-BoldItalic.ttf"
-    (share/"fonts").install "Chivo-Italic.ttf"
-    (share/"fonts").install "Chivo-Light.ttf"
-    (share/"fonts").install "Chivo-LightItalic.ttf"
-    (share/"fonts").install "Chivo-Regular.ttf"
+    (share/"fonts").install "Chivo-Italic[wght].ttf"
+    (share/"fonts").install "Chivo[wght].ttf"
   end
   test do
   end
