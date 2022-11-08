b83d8f28ccec5787d26e0f08491ab639d283bfc8
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sorts-mill-goudy.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-sorts-mill-goudy.rb
+++ Formula/font-sorts-mill-goudy.rb
@@ -1,5 +1,5 @@
 class FontSortsMillGoudy < Formula
-  head "https://github.com/google/fonts/trunk/ofl/sortsmillgoudy", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sortsmillgoudy"
   desc "Sorts Mill Goudy"
   homepage "https://fonts.google.com/specimen/Sorts+Mill+Goudy"
   def install
