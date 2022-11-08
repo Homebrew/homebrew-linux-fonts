6fdb2cfcfbda70caed5c296d711a3dff18874b82
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cabin.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-cabin.rb
+++ Formula/font-cabin.rb
@@ -1,5 +1,5 @@
 class FontCabin < Formula
-  head "https://github.com/google/fonts/trunk/ofl/cabin", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cabin"
   desc "Cabin"
   homepage "https://fonts.google.com/specimen/Cabin"
   def install
