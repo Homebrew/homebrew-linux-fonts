78661357de69e7eebf0ed783d5416445d8913856
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kaisei-decol.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-kaisei-decol.rb
+++ Formula/font-kaisei-decol.rb
@@ -1,5 +1,5 @@
 class FontKaiseiDecol < Formula
-  head "https://github.com/google/fonts/trunk/ofl/kaiseidecol", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kaiseidecol"
   desc "Kaisei Decol"
   desc "Designed with the same element in kanji, the little dot at the end of the stroke"
   homepage "https://fonts.google.com/specimen/Kaisei+Decol"
