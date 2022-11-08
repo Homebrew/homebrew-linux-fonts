182c0371b5c14648ef9782a4a4419aa6b84358ba
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-epilogue.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-epilogue.rb
+++ Formula/font-epilogue.rb
@@ -1,5 +1,5 @@
 class FontEpilogue < Formula
-  head "https://github.com/google/fonts/trunk/ofl/epilogue", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/epilogue"
   desc "Epilogue"
   desc "Sans serif variable font with a weight axis"
   homepage "https://fonts.google.com/specimen/Epilogue"
