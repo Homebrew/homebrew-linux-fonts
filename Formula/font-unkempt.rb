c740f346265933dd899c7adc29a56f306bf1da1c
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-unkempt.rb from eff0ccefebe685e31013b42b8bb4fb452bd17482

--- Formula/font-unkempt.rb
+++ Formula/font-unkempt.rb
@@ -1,5 +1,5 @@
 class FontUnkempt < Formula
-  head "https://github.com/google/fonts/trunk/apache/unkempt", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "apache/unkempt"
   desc "Unkempt"
   homepage "https://fonts.google.com/specimen/Unkempt"
   def install
