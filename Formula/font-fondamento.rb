702c676216d4a491c90192dfa3e4d04bb2b6ccf7
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fondamento.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-fondamento.rb
+++ Formula/font-fondamento.rb
@@ -1,5 +1,5 @@
 class FontFondamento < Formula
-  head "https://github.com/google/fonts/trunk/ofl/fondamento", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fondamento"
   desc "Fondamento"
   homepage "https://fonts.google.com/specimen/Fondamento"
   def install
