4c57397c4a78a90b0a7f455d53b074a717b4b703
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-open-sans.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-open-sans.rb
+++ Formula/font-open-sans.rb
@@ -1,5 +1,5 @@
 class FontOpenSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/opensans", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/opensans"
   desc "Open Sans"
   homepage "https://fonts.google.com/specimen/Open+Sans"
   def install
