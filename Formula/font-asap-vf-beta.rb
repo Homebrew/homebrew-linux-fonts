4db846858bdfc109c776f8087e5f88eb4777de20
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-asap-vf-beta.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-asap-vf-beta.rb
+++ Formula/font-asap-vf-beta.rb
@@ -1,5 +1,5 @@
 class FontAsapVfBeta < Formula
-  head "https://github.com/google/fonts/trunk/ofl/asapvfbeta", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/asapvfbeta"
   desc "Asap VF Beta"
   homepage "https://fonts.google.com/earlyaccess"
   def install
