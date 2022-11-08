25e7f8697758a067e26550aa8f5cf1d9a16274f7
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hanuman.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-hanuman.rb
+++ Formula/font-hanuman.rb
@@ -1,5 +1,5 @@
 class FontHanuman < Formula
-  head "https://github.com/google/fonts/trunk/ofl/hanuman", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/hanuman"
   desc "Hanuman"
   homepage "https://fonts.google.com/specimen/Hanuman"
   def install
