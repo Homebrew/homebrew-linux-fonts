a8b461aa31dd8dbed38a7a963e5ec05f4b3157a4
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kantumruy.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-kantumruy.rb
+++ Formula/font-kantumruy.rb
@@ -1,5 +1,5 @@
 class FontKantumruy < Formula
-  head "https://github.com/google/fonts/trunk/ofl/kantumruy", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kantumruy"
   desc "Kantumruy"
   homepage "https://fonts.google.com/specimen/Kantumruy"
   def install
