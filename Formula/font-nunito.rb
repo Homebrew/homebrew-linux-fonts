59eea756767408a811b121d27b2f6416cde4e6e2
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nunito.rb from eff0ccefebe685e31013b42b8bb4fb452bd17482

--- Formula/font-nunito.rb
+++ Formula/font-nunito.rb
@@ -1,5 +1,5 @@
 class FontNunito < Formula
-  head "https://github.com/google/fonts/trunk/ofl/nunito", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/nunito"
   desc "Nunito"
   homepage "https://fonts.google.com/specimen/Nunito"
   def install
