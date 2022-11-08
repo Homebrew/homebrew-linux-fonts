58d1913dabeef51815d848a2c65c1c0d7c5dded4
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cantarell.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-cantarell.rb
+++ Formula/font-cantarell.rb
@@ -1,5 +1,5 @@
 class FontCantarell < Formula
-  head "https://github.com/google/fonts/trunk/ofl/cantarell", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cantarell"
   desc "Cantarell"
   homepage "https://fonts.google.com/specimen/Cantarell"
   def install
