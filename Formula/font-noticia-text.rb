034230d34e49ca82db29ca1a637d7a1f5c817242
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noticia-text.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-noticia-text.rb
+++ Formula/font-noticia-text.rb
@@ -1,5 +1,5 @@
 class FontNoticiaText < Formula
-  head "https://github.com/google/fonts/trunk/ofl/noticiatext", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/noticiatext"
   desc "Noticia Text"
   homepage "https://fonts.google.com/specimen/Noticia+Text"
   def install
