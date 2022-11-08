6e7157dc9725190efe70917aa1b54f23f0926a7d
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fanwood-text.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-fanwood-text.rb
+++ Formula/font-fanwood-text.rb
@@ -1,5 +1,5 @@
 class FontFanwoodText < Formula
-  head "https://github.com/google/fonts/trunk/ofl/fanwoodtext", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fanwoodtext"
   desc "Fanwood Text"
   homepage "https://fonts.google.com/specimen/Fanwood+Text"
   def install
