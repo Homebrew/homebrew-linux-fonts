372fb8fb328c56ac11c2c70086ae95338414818e
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-radley.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-radley.rb
+++ Formula/font-radley.rb
@@ -1,5 +1,5 @@
 class FontRadley < Formula
-  head "https://github.com/google/fonts/trunk/ofl/radley", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/radley"
   desc "Radley"
   homepage "https://fonts.google.com/specimen/Radley"
   def install
