2f8ef8fe8bb52a1dfc15c9bac8cc4dedcedfeb8c
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-delius-unicase.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-delius-unicase.rb
+++ Formula/font-delius-unicase.rb
@@ -1,5 +1,5 @@
 class FontDeliusUnicase < Formula
-  head "https://github.com/google/fonts/trunk/ofl/deliusunicase", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/deliusunicase"
   desc "Delius Unicase"
   homepage "https://fonts.google.com/specimen/Delius+Unicase"
   def install
