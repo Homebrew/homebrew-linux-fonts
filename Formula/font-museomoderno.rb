8b026d44f2eabd906bd3696d8a4f5fac6475cc8b
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-museomoderno.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-museomoderno.rb
+++ Formula/font-museomoderno.rb
@@ -1,5 +1,5 @@
 class FontMuseomoderno < Formula
-  head "https://github.com/google/fonts/trunk/ofl/museomoderno", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/museomoderno"
   desc "MuseoModerno"
   homepage "https://fonts.google.com/specimen/MuseoModerno"
   def install
