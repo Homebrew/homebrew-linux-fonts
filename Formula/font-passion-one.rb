33f13b7cd4b322eada16777c14d11ac8bc5359ad
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-passion-one.rb from eff0ccefebe685e31013b42b8bb4fb452bd17482

--- Formula/font-passion-one.rb
+++ Formula/font-passion-one.rb
@@ -1,5 +1,5 @@
 class FontPassionOne < Formula
-  head "https://github.com/google/fonts/trunk/ofl/passionone", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/passionone"
   desc "Passion One"
   homepage "https://fonts.google.com/specimen/Passion+One"
   def install
