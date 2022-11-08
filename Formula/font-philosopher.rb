5bad992a0764f4a8b0ed33e1beee94bca50bab6f
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-philosopher.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-philosopher.rb
+++ Formula/font-philosopher.rb
@@ -1,5 +1,5 @@
 class FontPhilosopher < Formula
-  head "https://github.com/google/fonts/trunk/ofl/philosopher", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/philosopher"
   desc "Philosopher"
   homepage "https://fonts.google.com/specimen/Philosopher"
   def install
