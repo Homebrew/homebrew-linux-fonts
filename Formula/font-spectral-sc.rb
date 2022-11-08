f5d8a923e5ce6dc3c7e152f4e75bcc81798dd665
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-spectral-sc.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-spectral-sc.rb
+++ Formula/font-spectral-sc.rb
@@ -1,5 +1,5 @@
 class FontSpectralSc < Formula
-  head "https://github.com/google/fonts/trunk/ofl/spectralsc", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/spectralsc"
   desc "Spectral SC"
   homepage "https://fonts.google.com/specimen/Spectral+SC"
   def install
