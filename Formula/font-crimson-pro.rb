f27f66bf4d1ee1ff1c41562c824665412d0fddd8
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-crimson-pro.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-crimson-pro.rb
+++ Formula/font-crimson-pro.rb
@@ -1,5 +1,5 @@
 class FontCrimsonPro < Formula
-  head "https://github.com/google/fonts/trunk/ofl/crimsonpro", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/crimsonpro"
   desc "Crimson Pro"
   homepage "https://fonts.google.com/specimen/Crimson+Pro"
   def install
