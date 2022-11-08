4b41e0cf88fcd7ab05169125c2e763015011036d
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-exo-2.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-exo-2.rb
+++ Formula/font-exo-2.rb
@@ -1,5 +1,5 @@
 class FontExo2 < Formula
-  head "https://github.com/google/fonts/trunk/ofl/exo2", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/exo2"
   desc "Exo 2"
   homepage "https://fonts.google.com/specimen/Exo+2"
   def install
