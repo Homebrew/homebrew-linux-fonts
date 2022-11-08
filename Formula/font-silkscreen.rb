bebec673449fbac4d9b9a24e1d7823349417b213
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-silkscreen.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-silkscreen.rb
+++ Formula/font-silkscreen.rb
@@ -1,5 +1,5 @@
 class FontSilkscreen < Formula
-  head "https://github.com/google/fonts/trunk/ofl/silkscreen", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/silkscreen"
   desc "Silkscreen"
   desc "Typeface for your web graphics"
   homepage "https://fonts.google.com/specimen/Silkscreen"
