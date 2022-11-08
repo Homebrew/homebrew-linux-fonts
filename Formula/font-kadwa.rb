a7712835b3ebff65d668f94790bdaeed2b32c318
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kadwa.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-kadwa.rb
+++ Formula/font-kadwa.rb
@@ -1,5 +1,5 @@
 class FontKadwa < Formula
-  head "https://github.com/google/fonts/trunk/ofl/kadwa", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kadwa"
   desc "Kadwa"
   homepage "https://fonts.google.com/specimen/Kadwa"
   def install
