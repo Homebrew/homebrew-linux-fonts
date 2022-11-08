b45e28147b33616fad74f66bc25f26a6279f64f3
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-libre-baskerville.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-libre-baskerville.rb
+++ Formula/font-libre-baskerville.rb
@@ -1,5 +1,5 @@
 class FontLibreBaskerville < Formula
-  head "https://github.com/google/fonts/trunk/ofl/librebaskerville", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/librebaskerville"
   desc "Libre Baskerville"
   homepage "https://fonts.google.com/specimen/Libre+Baskerville"
   def install
