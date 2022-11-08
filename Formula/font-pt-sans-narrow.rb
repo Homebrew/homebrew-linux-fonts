9e2200bff79d2ae1ccadaafbdd87d470d985c3a0
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pt-sans-narrow.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-pt-sans-narrow.rb
+++ Formula/font-pt-sans-narrow.rb
@@ -1,5 +1,5 @@
 class FontPtSansNarrow < Formula
-  head "https://github.com/google/fonts/trunk/ofl/ptsansnarrow", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ptsansnarrow"
   desc "PT Sans Narrow"
   homepage "https://fonts.google.com/specimen/PT+Sans+Narrow"
   def install
