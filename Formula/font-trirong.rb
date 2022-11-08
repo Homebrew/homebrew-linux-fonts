403901ce3f96abdcda9c8e8a6a4e79c72c674714
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-trirong.rb from d01f5ef7a5f2f40bc400836c57d86725a31d0480

--- Formula/font-trirong.rb
+++ Formula/font-trirong.rb
@@ -1,5 +1,5 @@
 class FontTrirong < Formula
-  head "https://github.com/google/fonts/trunk/ofl/trirong", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/trirong"
   desc "Trirong"
   homepage "https://fonts.google.com/specimen/Trirong"
   def install
