667a4ef45eabdafdeea6ae42ccad0d4d41178e0b
TANIGUCHI Masaya <m@docs.casa>
2022-11-08 00:08:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arimo.rb from eff0ccefebe685e31013b42b8bb4fb452bd17482

--- Formula/font-arimo.rb
+++ Formula/font-arimo.rb
@@ -1,5 +1,5 @@
 class FontArimo < Formula
-  head "https://github.com/google/fonts/trunk/apache/arimo", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "apache/arimo"
   desc "Arimo"
   homepage "https://fonts.google.com/specimen/Arimo"
   def install
