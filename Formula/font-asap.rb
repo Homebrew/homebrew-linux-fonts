b385842385a4c7b421ba8ea2e684b36edf1d27c1
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-asap.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-asap.rb
+++ Formula/font-asap.rb
@@ -1,5 +1,5 @@
 class FontAsap < Formula
-  head "https://github.com/google/fonts/trunk/ofl/asap", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/asap"
   desc "Asap"
   homepage "https://fonts.google.com/specimen/Asap"
   def install
