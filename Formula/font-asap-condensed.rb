3fe811e5607aa0d6c86cc91354a6ee000190f8a3
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-asap-condensed.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-asap-condensed.rb
+++ Formula/font-asap-condensed.rb
@@ -1,5 +1,5 @@
 class FontAsapCondensed < Formula
-  head "https://github.com/google/fonts/trunk/ofl/asapcondensed", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/asapcondensed"
   desc "Asap Condensed"
   homepage "https://fonts.google.com/specimen/Asap+Condensed"
   def install
