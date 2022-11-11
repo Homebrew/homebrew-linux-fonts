84d23bd0be0f5b44c41d0338f6291736208b9244
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gantari.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-gantari.rb
+++ Formula/font-gantari.rb
@@ -1,5 +1,5 @@
 class FontGantari < Formula
-  head "https://github.com/google/fonts/trunk/ofl/gantari", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gantari"
   desc "Gantari"
   desc "Designed so that all characters can look harmonious"
   homepage "https://fonts.google.com/specimen/Gantari"
