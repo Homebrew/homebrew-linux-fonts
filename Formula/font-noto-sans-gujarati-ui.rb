09b76e5d0f90997879e531f5b1dd4d668b4a4c01
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-gujarati-ui.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-noto-sans-gujarati-ui.rb
+++ Formula/font-noto-sans-gujarati-ui.rb
@@ -1,7 +1,7 @@
 class FontNotoSansGujaratiUi < Formula
-  head "https://github.com/google/fonts/trunk/ofl/notosansgujaratiui", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosansgujaratiui"
   desc "Noto Sans Gujarati UI"
-  homepage "https://fonts.google.com/specimen/Noto+Sans+Gujarati+UI"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Gujarati"
   def install
     (share/"fonts").install "NotoSansGujaratiUI-Black.ttf"
     (share/"fonts").install "NotoSansGujaratiUI-Bold.ttf"
