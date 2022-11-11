dd425ccd685a50501b1d4ffdf8cbdaad82948591
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kaisei-harunoumi.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-kaisei-harunoumi.rb
+++ Formula/font-kaisei-harunoumi.rb
@@ -1,5 +1,5 @@
 class FontKaiseiHarunoumi < Formula
-  head "https://github.com/google/fonts/trunk/ofl/kaiseiharunoumi", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kaiseiharunoumi"
   desc "Kaisei HarunoUmi"
   homepage "https://fonts.google.com/specimen/Kaisei+HarunoUmi"
   def install
