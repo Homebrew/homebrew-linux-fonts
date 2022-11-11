dd3f9a0747f75319dd441da06b7121825cb2ed9d
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rasa.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-rasa.rb
+++ Formula/font-rasa.rb
@@ -1,5 +1,5 @@
 class FontRasa < Formula
-  head "https://github.com/google/fonts/trunk/ofl/rasa", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rasa"
   desc "Rasa"
   homepage "https://fonts.google.com/specimen/Rasa"
   def install
