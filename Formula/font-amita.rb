f964595e003d9bf071c1177aabc489e5fbc90358
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-amita.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-amita.rb
+++ Formula/font-amita.rb
@@ -1,5 +1,5 @@
 class FontAmita < Formula
-  head "https://github.com/google/fonts/trunk/ofl/amita", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/amita"
   desc "Amita"
   homepage "https://fonts.google.com/specimen/Amita"
   def install
