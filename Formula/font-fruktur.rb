324ff3600de9a63ef889a2a1968f436c4122d857
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fruktur.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-fruktur.rb
+++ Formula/font-fruktur.rb
@@ -1,5 +1,5 @@
 class FontFruktur < Formula
-  head "https://github.com/google/fonts/trunk/ofl/fruktur", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fruktur"
   desc "Fruktur"
   homepage "https://fonts.google.com/specimen/Fruktur"
   def install
