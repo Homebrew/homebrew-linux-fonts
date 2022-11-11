936b66ef4e052da02dbbc39719849a216b060ce0
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-volkhov.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-volkhov.rb
+++ Formula/font-volkhov.rb
@@ -1,5 +1,5 @@
 class FontVolkhov < Formula
-  head "https://github.com/google/fonts/trunk/ofl/volkhov", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/volkhov"
   desc "Volkhov"
   homepage "https://fonts.google.com/specimen/Volkhov"
   def install
