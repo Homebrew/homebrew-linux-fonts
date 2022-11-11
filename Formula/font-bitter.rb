9b209e114369a2848d1d9a547735ab2867235894
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bitter.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-bitter.rb
+++ Formula/font-bitter.rb
@@ -1,5 +1,5 @@
 class FontBitter < Formula
-  head "https://github.com/google/fonts/trunk/ofl/bitter", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/bitter"
   desc "Bitter"
   desc "Slab-serif typeface optimized for e-ink screens"
   homepage "https://fonts.google.com/specimen/Bitter"
