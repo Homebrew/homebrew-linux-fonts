3d208feb36697b0e21a83bb3008b2a6e9b5bc2a0
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-harmattan.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-harmattan.rb
+++ Formula/font-harmattan.rb
@@ -1,5 +1,5 @@
 class FontHarmattan < Formula
-  head "https://github.com/google/fonts/trunk/ofl/harmattan", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/harmattan"
   desc "Harmattan"
   homepage "https://fonts.google.com/specimen/Harmattan"
   def install
