b372ed20380525ab8d5d5474eedeb9b2d8d56697
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-averia-libre.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-averia-libre.rb
+++ Formula/font-averia-libre.rb
@@ -1,5 +1,5 @@
 class FontAveriaLibre < Formula
-  head "https://github.com/google/fonts/trunk/ofl/averialibre", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/averialibre"
   desc "Averia Libre"
   homepage "https://fonts.google.com/specimen/Averia+Libre"
   def install
