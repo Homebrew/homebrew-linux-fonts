130620f51b43e3829c104103689531e065d3c0ea
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-source-code-pro.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-source-code-pro.rb
+++ Formula/font-source-code-pro.rb
@@ -1,5 +1,5 @@
 class FontSourceCodePro < Formula
-  head "https://github.com/google/fonts/trunk/ofl/sourcecodepro", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sourcecodepro"
   desc "Source Code Pro"
   homepage "https://fonts.google.com/specimen/Source+Code+Pro"
   def install
