7514e3b9678bd519facb10f38243516c63f2d03a
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-grenze.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-grenze.rb
+++ Formula/font-grenze.rb
@@ -1,5 +1,5 @@
 class FontGrenze < Formula
-  head "https://github.com/google/fonts/trunk/ofl/grenze", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/grenze"
   desc "Grenze"
   homepage "https://fonts.google.com/specimen/Grenze"
   def install
