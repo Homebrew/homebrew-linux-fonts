b26973bb980744a7b54fd8bbcf0929f0c8947d7b
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mukta.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-mukta.rb
+++ Formula/font-mukta.rb
@@ -1,5 +1,5 @@
 class FontMukta < Formula
-  head "https://github.com/google/fonts/trunk/ofl/mukta", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/mukta"
   desc "Mukta"
   homepage "https://fonts.google.com/specimen/Mukta"
   def install
