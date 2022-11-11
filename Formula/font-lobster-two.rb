be48ffbc1ceaeb30c94fa5abe77bd4e0f4da5964
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lobster-two.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-lobster-two.rb
+++ Formula/font-lobster-two.rb
@@ -1,5 +1,5 @@
 class FontLobsterTwo < Formula
-  head "https://github.com/google/fonts/trunk/ofl/lobstertwo", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lobstertwo"
   desc "Lobster Two"
   homepage "https://fonts.google.com/specimen/Lobster+Two"
   def install
