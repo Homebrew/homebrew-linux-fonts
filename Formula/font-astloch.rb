3b19721a3af2c16a1bee50ebca94bbfd1cb29548
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-astloch.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-astloch.rb
+++ Formula/font-astloch.rb
@@ -1,5 +1,5 @@
 class FontAstloch < Formula
-  head "https://github.com/google/fonts/trunk/ofl/astloch", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/astloch"
   desc "Astloch"
   homepage "https://fonts.google.com/specimen/Astloch"
   def install
