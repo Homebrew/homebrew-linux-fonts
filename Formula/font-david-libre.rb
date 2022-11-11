fbfccc69953c886dd65d9fc65b7d7060d77eef16
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-david-libre.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-david-libre.rb
+++ Formula/font-david-libre.rb
@@ -1,5 +1,5 @@
 class FontDavidLibre < Formula
-  head "https://github.com/google/fonts/trunk/ofl/davidlibre", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/davidlibre"
   desc "David Libre"
   desc "Led by meir sadan, a type designer based in tel aviv, israel"
   homepage "https://fonts.google.com/specimen/David+Libre"
