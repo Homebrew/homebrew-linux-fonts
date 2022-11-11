87a9cf1e371201d9cac9f101f79eaf0e8a4cde77
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nanum-gothic.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-nanum-gothic.rb
+++ Formula/font-nanum-gothic.rb
@@ -1,5 +1,5 @@
 class FontNanumGothic < Formula
-  head "https://github.com/google/fonts/trunk/ofl/nanumgothic", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/nanumgothic"
   desc "Nanum Gothic"
   homepage "https://fonts.google.com/specimen/Nanum+Gothic"
   def install
