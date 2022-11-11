ba85db5951fc25e2fa263e2af0e561b62874f4fa
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arsenal.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-arsenal.rb
+++ Formula/font-arsenal.rb
@@ -1,5 +1,5 @@
 class FontArsenal < Formula
-  head "https://github.com/google/fonts/trunk/ofl/arsenal", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/arsenal"
   desc "Arsenal"
   homepage "https://fonts.google.com/specimen/Arsenal"
   def install
