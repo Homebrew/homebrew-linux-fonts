6ec3e4aee27e20522190bc99669bcceb569adf05
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stoke.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-stoke.rb
+++ Formula/font-stoke.rb
@@ -1,5 +1,5 @@
 class FontStoke < Formula
-  head "https://github.com/google/fonts/trunk/ofl/stoke", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/stoke"
   desc "Stoke"
   homepage "https://fonts.google.com/specimen/Stoke"
   def install
