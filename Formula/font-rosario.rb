4fd413f6bf3c233a286a62673a335eb6c710191c
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rosario.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-rosario.rb
+++ Formula/font-rosario.rb
@@ -1,5 +1,5 @@
 class FontRosario < Formula
-  head "https://github.com/google/fonts/trunk/ofl/rosario", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rosario"
   desc "Rosario"
   homepage "https://fonts.google.com/specimen/Rosario"
   def install
