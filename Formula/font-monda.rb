41428b13fac8c2d70378b87e396d18e4b92fb821
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monda.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-monda.rb
+++ Formula/font-monda.rb
@@ -1,5 +1,5 @@
 class FontMonda < Formula
-  head "https://github.com/google/fonts/trunk/ofl/monda", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/monda"
   desc "Monda"
   homepage "https://fonts.google.com/specimen/Monda"
   def install
