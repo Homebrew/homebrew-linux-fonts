79e0a82f129ec7758262d6c10bc9d1437adbc5d0
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:40 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sunflower.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-sunflower.rb
+++ Formula/font-sunflower.rb
@@ -1,5 +1,5 @@
 class FontSunflower < Formula
-  head "https://github.com/google/fonts/trunk/ofl/sunflower", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sunflower"
   desc "Sunflower"
   homepage "https://fonts.google.com/specimen/Sunflower"
   def install
