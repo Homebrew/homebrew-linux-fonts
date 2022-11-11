05a8e2a9c7b88d7baa1387636abbe80a983a0b04
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inika.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-inika.rb
+++ Formula/font-inika.rb
@@ -1,5 +1,5 @@
 class FontInika < Formula
-  head "https://github.com/google/fonts/trunk/ofl/inika", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/inika"
   desc "Inika"
   homepage "https://fonts.google.com/specimen/Inika"
   def install
