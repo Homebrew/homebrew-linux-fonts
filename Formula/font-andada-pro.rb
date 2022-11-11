729f2bebf32d6e5a549ee0ee8a489ef4697ab5a6
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-andada-pro.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-andada-pro.rb
+++ Formula/font-andada-pro.rb
@@ -1,5 +1,5 @@
 class FontAndadaPro < Formula
-  head "https://github.com/google/fonts/trunk/ofl/andadapro", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/andadapro"
   desc "Andada Pro"
   desc "Organic-slab serif, hybrid style and medium contrast type for text"
   homepage "https://fonts.google.com/specimen/Andada+Pro"
