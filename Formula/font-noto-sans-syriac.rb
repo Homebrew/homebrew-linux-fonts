9a0d3e615811bcfa5b61d4d94ba9b9f779685b58
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-syriac.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-noto-sans-syriac.rb
+++ Formula/font-noto-sans-syriac.rb
@@ -1,5 +1,5 @@
 class FontNotoSansSyriac < Formula
-  head "https://github.com/google/fonts/trunk/ofl/notosanssyriac", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosanssyriac"
   desc "Noto Sans Syriac"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Syriac"
   def install
