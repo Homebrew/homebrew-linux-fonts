683e1752837d82ce1a5ff84b95f8893309355f73
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-old-standard-tt.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-old-standard-tt.rb
+++ Formula/font-old-standard-tt.rb
@@ -1,5 +1,5 @@
 class FontOldStandardTt < Formula
-  head "https://github.com/google/fonts/trunk/ofl/oldstandardtt", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/oldstandardtt"
   desc "Old Standard TT"
   homepage "https://fonts.google.com/specimen/Old+Standard+TT"
   def install
