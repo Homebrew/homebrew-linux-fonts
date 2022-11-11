6c971b2ea47b105d68be868894a4b985461ae55b
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-b612-mono.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-b612-mono.rb
+++ Formula/font-b612-mono.rb
@@ -1,5 +1,5 @@
 class FontB612Mono < Formula
-  head "https://github.com/google/fonts/trunk/ofl/b612mono", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/b612mono"
   desc "B612 Mono"
   homepage "https://fonts.google.com/specimen/B612+Mono"
   def install
