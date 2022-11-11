d79b873665e001a83206ca9fe621ed67e434307a
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-expletus-sans.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-expletus-sans.rb
+++ Formula/font-expletus-sans.rb
@@ -1,5 +1,5 @@
 class FontExpletusSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/expletussans", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/expletussans"
   desc "Expletus Sans"
   homepage "https://fonts.google.com/specimen/Expletus+Sans"
   def install
