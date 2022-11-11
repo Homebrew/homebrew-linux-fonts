cd71b42920d1471f552dae5d16a8b4b38e5c34da
TANIGUCHI Masaya <m@docs.casa>
2022-11-11 00:07:39 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-seoul-hangang-condensed.rb from 59272d1cec92e35a24491a86e4313f5e6ae313c9

--- Formula/font-seoul-hangang-condensed.rb
+++ Formula/font-seoul-hangang-condensed.rb
@@ -1,7 +1,7 @@
 class FontSeoulHangangCondensed < Formula
-  head "https://github.com/google/fonts/trunk/ofl/seoulhangangcondensed", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/seoulhangangcondensed"
   desc "Seoul Hangang Condensed"
-  homepage "https://fonts.google.com/specimen/Seoul+Hangang+Condensed"
+  homepage "http://english.seoul.go.kr/seoul-views/seoul-symbols/5-fonts/"
   def install
     (share/"fonts").install "SeoulHangangCondensed-Bold.ttf"
     (share/"fonts").install "SeoulHangangCondensed-BoldL.ttf"
