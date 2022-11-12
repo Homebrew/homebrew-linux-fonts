9cbe6bdac1ce6d3cc3a83682e477302bec8b0b3d
TANIGUCHI Masaya <m@docs.casa>
2022-11-12 00:07:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ark-pixel-12px-monospaced.rb from f6e01a52c21c965505d4d5ac79197f4227cbcada

--- Formula/font-ark-pixel-12px-monospaced.rb
+++ Formula/font-ark-pixel-12px-monospaced.rb
@@ -1,6 +1,6 @@
 class FontArkPixel12pxMonospaced < Formula
-  version "2022.10.27"
-  sha256 "40271fce303e80aa04ace2eca6682a855fbe710cdb66f94c2f03c91e2d9963fd"
+  version "2022.11.11"
+  sha256 "d276f4fb51bf58bfc23a9d44e156b5c0909d32fdfd0f6d820fcebf8b83662e8d"
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-12px-monospaced-otf-v#{version}.zip"
   desc "Ark Pixel 12px monospaced"
   desc "Open source Pan-CJK pixel font"
