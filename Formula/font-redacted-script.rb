6da8f5f8b83f6014f872614fba28ab5e56ab881f
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-redacted-script.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-redacted-script.rb
+++ Formula/font-redacted-script.rb
@@ -1,5 +1,5 @@
 class FontRedactedScript < Formula
-  head "https://github.com/google/fonts/trunk/ofl/redactedscript", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/redactedscript"
   desc "Redacted Script"
   homepage "https://fonts.google.com/specimen/Redacted+Script"
   def install
