979a6b4ff8edff645ec0d764d982a661b6d165be
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-xanh-mono.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-xanh-mono.rb
+++ Formula/font-xanh-mono.rb
@@ -1,5 +1,5 @@
 class FontXanhMono < Formula
-  head "https://github.com/google/fonts/trunk/ofl/xanhmono", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/xanhmono"
   desc "Xanh Mono"
   desc "Mono-serif typeface, designed by lam bao and duy dao"
   homepage "https://fonts.google.com/specimen/Xanh+Mono"
