e28d6f71faa268b31f358c70a54065b28f88fdd5
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-sans-condensed.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-fira-sans-condensed.rb
+++ Formula/font-fira-sans-condensed.rb
@@ -1,5 +1,5 @@
 class FontFiraSansCondensed < Formula
-  head "https://github.com/google/fonts/trunk/ofl/firasanscondensed", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/firasanscondensed"
   desc "Fira Sans Condensed"
   homepage "https://fonts.google.com/specimen/Fira+Sans+Condensed"
   def install
