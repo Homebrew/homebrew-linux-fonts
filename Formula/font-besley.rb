25158eda9c8f61b62b643f94a9e84300ccb6acc5
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-besley.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-besley.rb
+++ Formula/font-besley.rb
@@ -1,5 +1,5 @@
 class FontBesley < Formula
-  head "https://github.com/google/fonts/trunk/ofl/besley", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/besley"
   desc "Besley"
   desc "Designed by owen earl (indestructible type*)"
   homepage "https://fonts.google.com/specimen/Besley"
