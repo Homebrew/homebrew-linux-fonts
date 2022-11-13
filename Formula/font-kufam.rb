440f075cbd9a5e1772237af74f7d233fcdd62355
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kufam.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-kufam.rb
+++ Formula/font-kufam.rb
@@ -1,5 +1,5 @@
 class FontKufam < Formula
-  head "https://github.com/google/fonts/trunk/ofl/kufam", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kufam"
   desc "Kufam"
   desc "Arabic-Latin bilingual typeface intended for contemporary information design"
   homepage "https://fonts.google.com/specimen/Kufam"
