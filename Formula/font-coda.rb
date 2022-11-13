3084d3e091d13516ddfaeefd1712ad6148b29536
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-coda.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-coda.rb
+++ Formula/font-coda.rb
@@ -1,5 +1,5 @@
 class FontCoda < Formula
-  head "https://github.com/google/fonts/trunk/ofl/coda", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/coda"
   desc "Coda"
   homepage "https://fonts.google.com/specimen/Coda"
   def install
