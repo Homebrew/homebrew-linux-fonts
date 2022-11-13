0393a7d57ff232ca751e5487efc37f2cab09463b
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-content.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-content.rb
+++ Formula/font-content.rb
@@ -1,5 +1,5 @@
 class FontContent < Formula
-  head "https://github.com/google/fonts/trunk/ofl/content", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/content"
   desc "Content"
   homepage "https://fonts.google.com/specimen/Content"
   def install
