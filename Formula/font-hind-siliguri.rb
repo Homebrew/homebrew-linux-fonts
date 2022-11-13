a8d8eb1a3c9f835bed5da54dc1aa7335ecb7ede8
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hind-siliguri.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-hind-siliguri.rb
+++ Formula/font-hind-siliguri.rb
@@ -1,5 +1,5 @@
 class FontHindSiliguri < Formula
-  head "https://github.com/google/fonts/trunk/ofl/hindsiliguri", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/hindsiliguri"
   desc "Hind Siliguri"
   homepage "https://fonts.google.com/specimen/Hind+Siliguri"
   def install
