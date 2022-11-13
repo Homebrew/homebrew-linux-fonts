d33ce656d6f0d6cc95bc3675108f6f213036fa2b
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rowdies.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-rowdies.rb
+++ Formula/font-rowdies.rb
@@ -1,5 +1,5 @@
 class FontRowdies < Formula
-  head "https://github.com/google/fonts/trunk/ofl/rowdies", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rowdies"
   desc "Rowdies"
   homepage "https://fonts.google.com/specimen/Rowdies"
   def install
