48b9b2be54ab8800f7dac2386141edef62df268a
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sumana.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-sumana.rb
+++ Formula/font-sumana.rb
@@ -1,5 +1,5 @@
 class FontSumana < Formula
-  head "https://github.com/google/fonts/trunk/ofl/sumana", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sumana"
   desc "Sumana"
   homepage "https://fonts.google.com/specimen/Sumana"
   def install
