91cf0e7dd5766e463d27902f58791b44b932dcd6
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-brawler.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-brawler.rb
+++ Formula/font-brawler.rb
@@ -1,5 +1,5 @@
 class FontBrawler < Formula
-  head "https://github.com/google/fonts/trunk/ofl/brawler", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/brawler"
   desc "Brawler"
   homepage "https://fonts.google.com/specimen/Brawler"
   def install
