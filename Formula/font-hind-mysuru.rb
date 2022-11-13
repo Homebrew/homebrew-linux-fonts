c0aa9be3728b5221af957547735d2b104b7e9493
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hind-mysuru.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-hind-mysuru.rb
+++ Formula/font-hind-mysuru.rb
@@ -1,7 +1,7 @@
 class FontHindMysuru < Formula
-  head "https://github.com/google/fonts/trunk/ofl/hindmysuru", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/hindmysuru"
   desc "Hind Mysuru"
-  homepage "https://fonts.google.com/specimen/Hind+Mysuru"
+  homepage "https://github.com/itfoundry/hind-mysuru"
   def install
     (share/"fonts").install "HindMysuru-Bold.ttf"
     (share/"fonts").install "HindMysuru-Light.ttf"
