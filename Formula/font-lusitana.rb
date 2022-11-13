fb63bd1d4b29e72756c2799cb73ea120d8d563bf
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lusitana.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-lusitana.rb
+++ Formula/font-lusitana.rb
@@ -1,5 +1,5 @@
 class FontLusitana < Formula
-  head "https://github.com/google/fonts/trunk/ofl/lusitana", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lusitana"
   desc "Lusitana"
   homepage "https://fonts.google.com/specimen/Lusitana"
   def install
