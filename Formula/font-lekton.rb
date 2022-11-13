3ac8a0711e02d648357bb884412ea51a0a093377
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lekton.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-lekton.rb
+++ Formula/font-lekton.rb
@@ -1,5 +1,5 @@
 class FontLekton < Formula
-  head "https://github.com/google/fonts/trunk/ofl/lekton", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lekton"
   desc "Lekton"
   homepage "https://fonts.google.com/specimen/Lekton"
   def install
