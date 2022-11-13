d7864354426e6ecbc0254447d013a19e138fcebe
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-flamenco.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-flamenco.rb
+++ Formula/font-flamenco.rb
@@ -1,5 +1,5 @@
 class FontFlamenco < Formula
-  head "https://github.com/google/fonts/trunk/ofl/flamenco", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/flamenco"
   desc "Flamenco"
   homepage "https://fonts.google.com/specimen/Flamenco"
   def install
