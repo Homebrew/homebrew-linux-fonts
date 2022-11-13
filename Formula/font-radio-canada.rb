78835dae894f4750d5c41a96ed180b1e30cc621d
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-radio-canada.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-radio-canada.rb
+++ Formula/font-radio-canada.rb
@@ -1,5 +1,5 @@
 class FontRadioCanada < Formula
-  head "https://github.com/google/fonts/trunk/ofl/radiocanada", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/radiocanada"
   desc "Radio Canada"
   homepage "https://fonts.google.com/specimen/Radio+Canada"
   def install
