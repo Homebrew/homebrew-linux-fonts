4d74b2ab398ff0d856c57d4b129f5e7fe886470b
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-stix-two-text.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-stix-two-text.rb
+++ Formula/font-stix-two-text.rb
@@ -1,5 +1,5 @@
 class FontStixTwoText < Formula
-  head "https://github.com/google/fonts/trunk/ofl/stixtwotext", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/stixtwotext"
   desc "STIX Two Text"
   homepage "https://fonts.google.com/specimen/STIX+Two+Text"
   def install
