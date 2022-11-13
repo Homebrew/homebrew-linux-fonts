183eb723e1f96ead2dcb8b3ba8bb1392d5844297
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cherry-swash.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-cherry-swash.rb
+++ Formula/font-cherry-swash.rb
@@ -1,5 +1,5 @@
 class FontCherrySwash < Formula
-  head "https://github.com/google/fonts/trunk/ofl/cherryswash", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cherryswash"
   desc "Cherry Swash"
   homepage "https://fonts.google.com/specimen/Cherry+Swash"
   def install
