7986d34453c35db388e493f8f7e604ee3bf0b8d2
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tillana.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-tillana.rb
+++ Formula/font-tillana.rb
@@ -1,5 +1,5 @@
 class FontTillana < Formula
-  head "https://github.com/google/fonts/trunk/ofl/tillana", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tillana"
   desc "Tillana"
   homepage "https://fonts.google.com/specimen/Tillana"
   def install
