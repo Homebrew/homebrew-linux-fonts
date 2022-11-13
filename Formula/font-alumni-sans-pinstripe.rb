265c3282fe4cb1d1e46d1a04521dce48ce893192
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alumni-sans-pinstripe.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-alumni-sans-pinstripe.rb
+++ Formula/font-alumni-sans-pinstripe.rb
@@ -1,5 +1,5 @@
 class FontAlumniSansPinstripe < Formula
-  head "https://github.com/google/fonts/trunk/ofl/alumnisanspinstripe", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/alumnisanspinstripe"
   desc "Alumni Sans Pinstripe"
   homepage "https://fonts.google.com/specimen/Alumni+Sans+Pinstripe"
   def install
