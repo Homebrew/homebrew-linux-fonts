b52b60246f5ca4be6283abe19ae9d429ecb9e403
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cabin-sketch.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-cabin-sketch.rb
+++ Formula/font-cabin-sketch.rb
@@ -1,5 +1,5 @@
 class FontCabinSketch < Formula
-  head "https://github.com/google/fonts/trunk/ofl/cabinsketch", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cabinsketch"
   desc "Cabin Sketch"
   homepage "https://fonts.google.com/specimen/Cabin+Sketch"
   def install
