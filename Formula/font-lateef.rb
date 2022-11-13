59d6d09caf2b8d715d8f1a554a72976f90615a74
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lateef.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-lateef.rb
+++ Formula/font-lateef.rb
@@ -1,5 +1,5 @@
 class FontLateef < Formula
-  head "https://github.com/google/fonts/trunk/ofl/lateef", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lateef"
   desc "Lateef"
   homepage "https://fonts.google.com/specimen/Lateef"
   def install
