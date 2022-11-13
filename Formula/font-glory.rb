f8cf61104827a6889afa6bc493fdcc7ed1f7cec5
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-glory.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-glory.rb
+++ Formula/font-glory.rb
@@ -1,5 +1,5 @@
 class FontGlory < Formula
-  head "https://github.com/google/fonts/trunk/ofl/glory", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/glory"
   desc "Glory"
   desc "Suitable for logos, headlines and body text with the available six weights"
   homepage "https://fonts.google.com/specimen/Glory"
