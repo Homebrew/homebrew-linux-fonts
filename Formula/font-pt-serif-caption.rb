54492191a75657b504ef9c2c57fb657d52896180
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pt-serif-caption.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-pt-serif-caption.rb
+++ Formula/font-pt-serif-caption.rb
@@ -1,5 +1,5 @@
 class FontPtSerifCaption < Formula
-  head "https://github.com/google/fonts/trunk/ofl/ptserifcaption", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ptserifcaption"
   desc "PT Serif Caption"
   homepage "https://fonts.google.com/specimen/PT+Serif+Caption"
   def install
