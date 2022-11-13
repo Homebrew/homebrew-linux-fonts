16696aa2e991dafb5c63269f66028513cdf0e04d
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tiro-telugu.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-tiro-telugu.rb
+++ Formula/font-tiro-telugu.rb
@@ -1,5 +1,5 @@
 class FontTiroTelugu < Formula
-  head "https://github.com/google/fonts/trunk/ofl/tirotelugu", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tirotelugu"
   desc "Tiro Telugu"
   desc "Especially suited to traditional literary publishing"
   homepage "https://fonts.google.com/specimen/Tiro+Telugu"
