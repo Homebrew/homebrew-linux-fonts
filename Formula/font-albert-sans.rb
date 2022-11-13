8cf7d6462350c7991a8bb78162d508b3450b2291
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-albert-sans.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-albert-sans.rb
+++ Formula/font-albert-sans.rb
@@ -1,5 +1,5 @@
 class FontAlbertSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/albertsans", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/albertsans"
   desc "Albert Sans"
   desc "Modern geometric sans serif family"
   homepage "https://fonts.google.com/specimen/Albert+Sans"
