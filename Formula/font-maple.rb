6ab35808fea3d2de6c1cbc2311965f583aee52eb
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-maple.rb from 248224f07442e4cd74fa12cb88c9324168aa4069

--- Formula/font-maple.rb
+++ Formula/font-maple.rb
@@ -2,7 +2,7 @@ class FontMaple < Formula
   version "5.5"
   sha256 "c6467d35e7e35873e358d5bc2c633c0a04f666b8388f57afbf1d4ae7ab14cd1f"
   url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMono.zip"
-  desc "font Maple"
+  desc "Maple Mono"
   desc "Nerd Font font with round corners"
   homepage "https://github.com/subframe7536/Maple-font"
   def install
