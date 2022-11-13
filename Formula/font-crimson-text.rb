eba310a65ad7814afac19123cb4c6faa0caadde3
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-crimson-text.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-crimson-text.rb
+++ Formula/font-crimson-text.rb
@@ -1,5 +1,5 @@
 class FontCrimsonText < Formula
-  head "https://github.com/google/fonts/trunk/ofl/crimsontext", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/crimsontext"
   desc "Crimson Text"
   homepage "https://fonts.google.com/specimen/Crimson+Text"
   def install
