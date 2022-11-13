ff4c84d87d653ff0fc96bf3de584fe44037f4482
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-double-pica.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-im-fell-double-pica.rb
+++ Formula/font-im-fell-double-pica.rb
@@ -1,5 +1,5 @@
 class FontImFellDoublePica < Formula
-  head "https://github.com/google/fonts/trunk/ofl/imfelldoublepica", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/imfelldoublepica"
   desc "IM Fell Double Pica"
   homepage "https://fonts.google.com/specimen/IM+Fell+Double+Pica"
   def install
