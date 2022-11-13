d12f3ea13af7d9fb42dc03bcd7a02af702352255
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-qwitcher-grypen.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-qwitcher-grypen.rb
+++ Formula/font-qwitcher-grypen.rb
@@ -1,5 +1,5 @@
 class FontQwitcherGrypen < Formula
-  head "https://github.com/google/fonts/trunk/ofl/qwitchergrypen", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/qwitchergrypen"
   desc "Qwitcher Grypen"
   desc "Casual brush script with a bit of an edge"
   homepage "https://fonts.google.com/specimen/Qwitcher+Grypen"
