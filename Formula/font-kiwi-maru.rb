36dbfd77ce9e65e359d1ddf6ebe09d5e82ef8ad4
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kiwi-maru.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-kiwi-maru.rb
+++ Formula/font-kiwi-maru.rb
@@ -1,5 +1,5 @@
 class FontKiwiMaru < Formula
-  head "https://github.com/google/fonts/trunk/ofl/kiwimaru", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kiwimaru"
   desc "Kiwi Maru"
   desc "Typeface for visualization of everyday and slang expressions"
   homepage "https://fonts.google.com/specimen/Kiwi+Maru"
