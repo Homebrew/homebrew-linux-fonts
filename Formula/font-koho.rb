f06964cb3d73298b949dccdf48c6ca51fc59a253
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-koho.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-koho.rb
+++ Formula/font-koho.rb
@@ -1,5 +1,5 @@
 class FontKoho < Formula
-  head "https://github.com/google/fonts/trunk/ofl/koho", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/koho"
   desc "KoHo"
   homepage "https://fonts.google.com/specimen/KoHo"
   def install
