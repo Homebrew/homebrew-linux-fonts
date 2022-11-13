7a5dbb35d059bb99fe085cc8d273ba110db7438f
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-brygada-1918.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-brygada-1918.rb
+++ Formula/font-brygada-1918.rb
@@ -1,5 +1,5 @@
 class FontBrygada1918 < Formula
-  head "https://github.com/google/fonts/trunk/ofl/brygada1918", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/brygada1918"
   desc "Brygada 1918"
   desc "Available on the project's website"
   homepage "https://fonts.google.com/specimen/Brygada+1918"
