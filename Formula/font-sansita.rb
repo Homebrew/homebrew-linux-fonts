df43b012765cdc9bbc976b5e89c412ed697f84b4
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sansita.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-sansita.rb
+++ Formula/font-sansita.rb
@@ -1,5 +1,5 @@
 class FontSansita < Formula
-  head "https://github.com/google/fonts/trunk/ofl/sansita", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sansita"
   desc "Sansita"
   homepage "https://fonts.google.com/specimen/Sansita"
   def install
