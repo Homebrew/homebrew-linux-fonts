6a49ef675627d0e15b9f11627a095b383b2835a7
TANIGUCHI Masaya <m@docs.casa>
2022-11-13 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fuzzy-bubbles.rb from 9d132bd26dbbabf161aa29511c2f2358815d4f98

--- Formula/font-fuzzy-bubbles.rb
+++ Formula/font-fuzzy-bubbles.rb
@@ -1,5 +1,5 @@
 class FontFuzzyBubbles < Formula
-  head "https://github.com/google/fonts/trunk/ofl/fuzzybubbles", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fuzzybubbles"
   desc "Fuzzy Bubbles"
   desc "Perfect for children's parties"
   homepage "https://fonts.google.com/specimen/Fuzzy+Bubbles"
