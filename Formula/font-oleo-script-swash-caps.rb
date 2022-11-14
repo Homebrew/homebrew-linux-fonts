6cb3a8bdafc53661d830538891e4411470df5d96
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oleo-script-swash-caps.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-oleo-script-swash-caps.rb
+++ Formula/font-oleo-script-swash-caps.rb
@@ -1,5 +1,5 @@
 class FontOleoScriptSwashCaps < Formula
-  head "https://github.com/google/fonts/trunk/ofl/oleoscriptswashcaps", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/oleoscriptswashcaps"
   desc "Oleo Script Swash Caps"
   homepage "https://fonts.google.com/specimen/Oleo+Script+Swash+Caps"
   def install
