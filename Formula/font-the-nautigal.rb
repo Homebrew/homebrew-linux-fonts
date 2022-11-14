ad6c39e833df25458dec5fa18ce30b3395048ef6
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-the-nautigal.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-the-nautigal.rb
+++ Formula/font-the-nautigal.rb
@@ -1,5 +1,5 @@
 class FontTheNautigal < Formula
-  head "https://github.com/google/fonts/trunk/ofl/thenautigal", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/thenautigal"
   desc "The Nautigal"
   desc "Fluid yet formal with beautiful connectors"
   homepage "https://fonts.google.com/specimen/The+Nautigal"
