4d6ffe0813b1c3092fcfad4176a9241ac3621bd5
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chau-philomene-one.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-chau-philomene-one.rb
+++ Formula/font-chau-philomene-one.rb
@@ -1,5 +1,5 @@
 class FontChauPhilomeneOne < Formula
-  head "https://github.com/google/fonts/trunk/ofl/chauphilomeneone", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/chauphilomeneone"
   desc "Chau Philomene One"
   homepage "https://fonts.google.com/specimen/Chau+Philomene+One"
   def install
