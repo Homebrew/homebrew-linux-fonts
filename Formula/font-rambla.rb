27b1117bbeb5183854ee27cd615dbb539f0b6cc6
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rambla.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-rambla.rb
+++ Formula/font-rambla.rb
@@ -1,5 +1,5 @@
 class FontRambla < Formula
-  head "https://github.com/google/fonts/trunk/ofl/rambla", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rambla"
   desc "Rambla"
   homepage "https://fonts.google.com/specimen/Rambla"
   def install
