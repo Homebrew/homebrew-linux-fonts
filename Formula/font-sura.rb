8a0f6a83f90f33569152ea71b271be5fbc613893
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sura.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-sura.rb
+++ Formula/font-sura.rb
@@ -1,5 +1,5 @@
 class FontSura < Formula
-  head "https://github.com/google/fonts/trunk/ofl/sura", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sura"
   desc "Sura"
   homepage "https://fonts.google.com/specimen/Sura"
   def install
