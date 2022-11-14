4dd1ba2b7ca67e6ad6e7ad77ff4fa9653c2b0b15
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-srisakdi.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-srisakdi.rb
+++ Formula/font-srisakdi.rb
@@ -1,5 +1,5 @@
 class FontSrisakdi < Formula
-  head "https://github.com/google/fonts/trunk/ofl/srisakdi", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/srisakdi"
   desc "Srisakdi"
   homepage "https://fonts.google.com/specimen/Srisakdi"
   def install
