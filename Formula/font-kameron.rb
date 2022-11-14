77b8e7dba2ef915da35038f1134100858969b3b7
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kameron.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-kameron.rb
+++ Formula/font-kameron.rb
@@ -1,5 +1,5 @@
 class FontKameron < Formula
-  head "https://github.com/google/fonts/trunk/ofl/kameron", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kameron"
   desc "Kameron"
   homepage "https://fonts.google.com/specimen/Kameron"
   def install
