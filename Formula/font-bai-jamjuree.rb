e90c87201ec730e6f1a6df20a4ffbd803620db21
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bai-jamjuree.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-bai-jamjuree.rb
+++ Formula/font-bai-jamjuree.rb
@@ -1,5 +1,5 @@
 class FontBaiJamjuree < Formula
-  head "https://github.com/google/fonts/trunk/ofl/baijamjuree", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/baijamjuree"
   desc "Bai Jamjuree"
   homepage "https://fonts.google.com/specimen/Bai+Jamjuree"
   def install
