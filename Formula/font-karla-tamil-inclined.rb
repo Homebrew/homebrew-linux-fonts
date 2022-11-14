4eb270b8ee405353200f87da615439bdd5aff49d
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-karla-tamil-inclined.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-karla-tamil-inclined.rb
+++ Formula/font-karla-tamil-inclined.rb
@@ -1,7 +1,7 @@
 class FontKarlaTamilInclined < Formula
-  head "https://github.com/google/fonts/trunk/ofl/karlatamilinclined", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/karlatamilinclined"
   desc "Karla Tamil Inclined"
-  homepage "https://fonts.google.com/specimen/Karla+Tamil+Inclined"
+  homepage "https://fonts.google.com/specimen/Karla"
   def install
     (share/"fonts").install "KarlaTamilInclined-Bold.ttf"
     (share/"fonts").install "KarlaTamilInclined-Regular.ttf"
