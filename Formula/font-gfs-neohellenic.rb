09a191c4300a4f9a99dfc2d97b35b58b10f450bc
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gfs-neohellenic.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-gfs-neohellenic.rb
+++ Formula/font-gfs-neohellenic.rb
@@ -1,5 +1,5 @@
 class FontGfsNeohellenic < Formula
-  head "https://github.com/google/fonts/trunk/ofl/gfsneohellenic", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gfsneohellenic"
   desc "GFS Neohellenic"
   homepage "https://fonts.google.com/specimen/GFS+Neohellenic"
   def install
