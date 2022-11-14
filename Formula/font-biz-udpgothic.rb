bc84fcd9b9acb445b669859534f73d73fc649012
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-biz-udpgothic.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-biz-udpgothic.rb
+++ Formula/font-biz-udpgothic.rb
@@ -1,5 +1,5 @@
 class FontBizUdpgothic < Formula
-  head "https://github.com/google/fonts/trunk/ofl/bizudpgothic", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/bizudpgothic"
   desc "BIZ UDPGothic"
   homepage "https://fonts.google.com/specimen/BIZ+UDPGothic"
   def install
