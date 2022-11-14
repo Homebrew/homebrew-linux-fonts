4fa09b1e062bc8445f8c159aaf9f329faf899b5b
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mali.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-mali.rb
+++ Formula/font-mali.rb
@@ -1,5 +1,5 @@
 class FontMali < Formula
-  head "https://github.com/google/fonts/trunk/ofl/mali", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/mali"
   desc "Mali"
   homepage "https://fonts.google.com/specimen/Mali"
   def install
