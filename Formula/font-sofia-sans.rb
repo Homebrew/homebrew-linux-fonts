69c52da78a50db8abc23281b0c25f2cf65ff1cd2
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sofia-sans.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-sofia-sans.rb
+++ Formula/font-sofia-sans.rb
@@ -1,8 +1,8 @@
 class FontSofiaSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/sofiasans", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/sofiasans"
   desc "Sofia Sans"
   desc "Opentype family with large character set"
-  homepage "https://fonts.google.com/specimen/Sofia+Sans"
+  homepage "https://github.com/lettersoup/Sofia-Sans"
   def install
     (share/"fonts").install "SofiaSans-Italic[wdth,wght].ttf"
     (share/"fonts").install "SofiaSans[wdth,wght].ttf"
