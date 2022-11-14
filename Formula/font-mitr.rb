a9748e7bce05ef76d166b76e4f3729679eb51152
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mitr.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-mitr.rb
+++ Formula/font-mitr.rb
@@ -1,5 +1,5 @@
 class FontMitr < Formula
-  head "https://github.com/google/fonts/trunk/ofl/mitr", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/mitr"
   desc "Mitr"
   homepage "https://fonts.google.com/specimen/Mitr"
   def install
