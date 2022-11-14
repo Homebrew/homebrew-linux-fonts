f67ee42da44f7a1a3fcaafb539542d48ad59fe11
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-genos.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-genos.rb
+++ Formula/font-genos.rb
@@ -1,5 +1,5 @@
 class FontGenos < Formula
-  head "https://github.com/google/fonts/trunk/ofl/genos", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/genos"
   desc "Genos"
   desc "Modern display font with a futuristic feel"
   homepage "https://fonts.google.com/specimen/Genos"
