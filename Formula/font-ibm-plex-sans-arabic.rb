5f23100b5ed17d5eaa7e9ec594c260e03fb01827
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ibm-plex-sans-arabic.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-ibm-plex-sans-arabic.rb
+++ Formula/font-ibm-plex-sans-arabic.rb
@@ -1,5 +1,5 @@
 class FontIbmPlexSansArabic < Formula
-  head "https://github.com/google/fonts/trunk/ofl/ibmplexsansarabic", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ibmplexsansarabic"
   desc "IBM Plex Sans Arabic"
   homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+Arabic"
   def install
