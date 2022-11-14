324770f461fce2a91b4c84472f3e9f36b46688c2
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-birthstone-bounce.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-birthstone-bounce.rb
+++ Formula/font-birthstone-bounce.rb
@@ -1,5 +1,5 @@
 class FontBirthstoneBounce < Formula
-  head "https://github.com/google/fonts/trunk/ofl/birthstonebounce", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/birthstonebounce"
   desc "Birthstone Bounce"
   desc "Sibling family of birthstone that adds more luster and playfulness to it"
   homepage "https://fonts.google.com/specimen/Birthstone+Bounce"
