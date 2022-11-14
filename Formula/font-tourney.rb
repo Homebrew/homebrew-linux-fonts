e22e2bbc7d568a286228e220176d6f1451429518
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tourney.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-tourney.rb
+++ Formula/font-tourney.rb
@@ -1,5 +1,5 @@
 class FontTourney < Formula
-  head "https://github.com/google/fonts/trunk/ofl/tourney", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tourney"
   desc "Tourney"
   desc "Completely solid"
   homepage "https://fonts.google.com/specimen/Tourney"
