7ed75e95cb3fdbd3cb165bc8e3f7df70a9c9f3d1
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-quattrocento-sans.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-quattrocento-sans.rb
+++ Formula/font-quattrocento-sans.rb
@@ -1,5 +1,5 @@
 class FontQuattrocentoSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/quattrocentosans", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/quattrocentosans"
   desc "Quattrocento Sans"
   homepage "https://fonts.google.com/specimen/Quattrocento+Sans"
   def install
