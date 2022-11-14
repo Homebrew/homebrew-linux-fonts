ed4921625b0cc518fb58229d28d6fe23bceb1646
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fraunces.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-fraunces.rb
+++ Formula/font-fraunces.rb
@@ -1,5 +1,5 @@
 class FontFraunces < Formula
-  head "https://github.com/google/fonts/trunk/ofl/fraunces", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fraunces"
   desc "Fraunces"
   desc "Display, old style soft-serif typeface"
   homepage "https://fonts.google.com/specimen/Fraunces"
