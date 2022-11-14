b9b6f3469ed31aa11e967a6efcd2d33deaad8733
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-geo.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-geo.rb
+++ Formula/font-geo.rb
@@ -1,5 +1,5 @@
 class FontGeo < Formula
-  head "https://github.com/google/fonts/trunk/ofl/geo", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/geo"
   desc "Geo"
   homepage "https://fonts.google.com/specimen/Geo"
   def install
