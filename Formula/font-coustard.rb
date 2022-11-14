699a945feb05424cb4d8b39ff3e6cca9c46caa09
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-coustard.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-coustard.rb
+++ Formula/font-coustard.rb
@@ -1,5 +1,5 @@
 class FontCoustard < Formula
-  head "https://github.com/google/fonts/trunk/ofl/coustard", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/coustard"
   desc "Coustard"
   homepage "https://fonts.google.com/specimen/Coustard"
   def install
