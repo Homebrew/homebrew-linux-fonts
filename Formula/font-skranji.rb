53dd0de87f967fecf0eeabe19e97fa88413ad7ba
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-skranji.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-skranji.rb
+++ Formula/font-skranji.rb
@@ -1,5 +1,5 @@
 class FontSkranji < Formula
-  head "https://github.com/google/fonts/trunk/ofl/skranji", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/skranji"
   desc "Skranji"
   homepage "https://fonts.google.com/specimen/Skranji"
   def install
