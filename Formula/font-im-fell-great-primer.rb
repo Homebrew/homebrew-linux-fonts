ed84b16b3d566e544a3d444ca835dabd541bf524
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-great-primer.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-im-fell-great-primer.rb
+++ Formula/font-im-fell-great-primer.rb
@@ -1,5 +1,5 @@
 class FontImFellGreatPrimer < Formula
-  head "https://github.com/google/fonts/trunk/ofl/imfellgreatprimer", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/imfellgreatprimer"
   desc "IM Fell Great Primer"
   homepage "https://fonts.google.com/specimen/IM+Fell+Great+Primer"
   def install
