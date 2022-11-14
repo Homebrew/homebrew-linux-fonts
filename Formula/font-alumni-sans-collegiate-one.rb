76518dc5198d1df68887cf4a32c83686cd888bb7
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alumni-sans-collegiate-one.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-alumni-sans-collegiate-one.rb
+++ Formula/font-alumni-sans-collegiate-one.rb
@@ -1,5 +1,5 @@
 class FontAlumniSansCollegiateOne < Formula
-  head "https://github.com/google/fonts/trunk/ofl/alumnisanscollegiateone", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/alumnisanscollegiateone"
   desc "Alumni Sans Collegiate One"
   desc "Font inspired by Impact Black"
   homepage "https://fonts.google.com/specimen/Alumni+Sans+Collegiate+One"
