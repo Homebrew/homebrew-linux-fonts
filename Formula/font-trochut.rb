87a5c9c5142a8a46d2bc269787e8601362803e69
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-trochut.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-trochut.rb
+++ Formula/font-trochut.rb
@@ -1,5 +1,5 @@
 class FontTrochut < Formula
-  head "https://github.com/google/fonts/trunk/ofl/trochut", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/trochut"
   desc "Trochut"
   homepage "https://fonts.google.com/specimen/Trochut"
   def install
