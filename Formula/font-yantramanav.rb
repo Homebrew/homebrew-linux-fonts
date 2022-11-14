ed77d163f2362ee26d8a0e019460dfc92c3b1e14
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-yantramanav.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-yantramanav.rb
+++ Formula/font-yantramanav.rb
@@ -1,5 +1,5 @@
 class FontYantramanav < Formula
-  head "https://github.com/google/fonts/trunk/ofl/yantramanav", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/yantramanav"
   desc "Yantramanav"
   homepage "https://fonts.google.com/specimen/Yantramanav"
   def install
