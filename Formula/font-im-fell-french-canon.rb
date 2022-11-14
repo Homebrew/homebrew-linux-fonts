4f5259493ce25c3726e00091607fef6a74603700
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-im-fell-french-canon.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-im-fell-french-canon.rb
+++ Formula/font-im-fell-french-canon.rb
@@ -1,5 +1,5 @@
 class FontImFellFrenchCanon < Formula
-  head "https://github.com/google/fonts/trunk/ofl/imfellfrenchcanon", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/imfellfrenchcanon"
   desc "IM Fell French Canon"
   homepage "https://fonts.google.com/specimen/IM+Fell+French+Canon"
   def install
