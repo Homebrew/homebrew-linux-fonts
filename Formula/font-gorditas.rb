9f5f5706217ee131070b307b87f62215dcb5ccce
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gorditas.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-gorditas.rb
+++ Formula/font-gorditas.rb
@@ -1,5 +1,5 @@
 class FontGorditas < Formula
-  head "https://github.com/google/fonts/trunk/ofl/gorditas", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gorditas"
   desc "Gorditas"
   homepage "https://fonts.google.com/specimen/Gorditas"
   def install
