31dfcbd90d592ddbb1068c3f03a275968292b17c
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-marhey.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-marhey.rb
+++ Formula/font-marhey.rb
@@ -1,8 +1,8 @@
 class FontMarhey < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/marhey/Marhey%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/marhey/Marhey%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Marhey"
   desc "Playful display typeface, custom hand lettering with contrast strokes"
-  homepage "https://github.com/namelatype/Marhey"
+  homepage "https://fonts.google.com/specimen/Marhey"
   def install
     (share/"fonts").install "Marhey[wght].ttf"
   end
