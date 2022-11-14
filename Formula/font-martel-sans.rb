a7b68776fa10565846dbd5177a51e53e55f27f21
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-martel-sans.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-martel-sans.rb
+++ Formula/font-martel-sans.rb
@@ -1,5 +1,5 @@
 class FontMartelSans < Formula
-  head "https://github.com/google/fonts/trunk/ofl/martelsans", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/martelsans"
   desc "Martel Sans"
   homepage "https://fonts.google.com/specimen/Martel+Sans"
   def install
