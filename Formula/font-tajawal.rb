9676e826e8f06084fcb6d55336752129c97e96e5
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tajawal.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-tajawal.rb
+++ Formula/font-tajawal.rb
@@ -1,5 +1,5 @@
 class FontTajawal < Formula
-  head "https://github.com/google/fonts/trunk/ofl/tajawal", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tajawal"
   desc "Tajawal"
   homepage "https://fonts.google.com/specimen/Tajawal"
   def install
