4b7daa028279426c78dc4e2b72e90894cf571923
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ibarra-real-nova.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-ibarra-real-nova.rb
+++ Formula/font-ibarra-real-nova.rb
@@ -1,5 +1,5 @@
 class FontIbarraRealNova < Formula
-  head "https://github.com/google/fonts/trunk/ofl/ibarrarealnova", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ibarrarealnova"
   desc "Ibarra Real Nova"
   homepage "https://fonts.google.com/specimen/Ibarra+Real+Nova"
   def install
