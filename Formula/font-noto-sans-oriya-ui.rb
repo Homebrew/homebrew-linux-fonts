aa9967b0f84bb509c5dffe6033525c0ea3908ee2
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-oriya-ui.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-noto-sans-oriya-ui.rb
+++ Formula/font-noto-sans-oriya-ui.rb
@@ -1,7 +1,7 @@
 class FontNotoSansOriyaUi < Formula
-  head "https://github.com/google/fonts/trunk/ofl/notosansoriyaui", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosansoriyaui"
   desc "Noto Sans Oriya UI"
-  homepage "https://fonts.google.com/specimen/Noto+Sans+Oriya+UI"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Oriya"
   def install
     (share/"fonts").install "NotoSansOriyaUI-Black.ttf"
     (share/"fonts").install "NotoSansOriyaUI-Bold.ttf"
