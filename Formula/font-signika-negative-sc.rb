3de6059ccef70db381b0c86e2abe37f0944dd039
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-signika-negative-sc.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-signika-negative-sc.rb
+++ Formula/font-signika-negative-sc.rb
@@ -1,8 +1,8 @@
 class FontSignikaNegativeSc < Formula
-  head "https://github.com/google/fonts/trunk/ofl/signikanegativesc", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/signikanegativesc"
   desc "Signika Negative SC"
   desc "Alternative version of the Signika SC font"
-  homepage "https://fonts.google.com/specimen/Signika+Negative+SC"
+  homepage "https://fonts.google.com/specimen/Signika+Negative"
   def install
     (share/"fonts").install "SignikaNegativeSC-Bold.ttf"
     (share/"fonts").install "SignikaNegativeSC-Light.ttf"
