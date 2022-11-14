d7e9a578642595a3464f93c7fd669afa21ed043d
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nokora.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-nokora.rb
+++ Formula/font-nokora.rb
@@ -1,5 +1,5 @@
 class FontNokora < Formula
-  head "https://github.com/google/fonts/trunk/ofl/nokora", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/nokora"
   desc "Nokora"
   homepage "https://fonts.google.com/specimen/Nokora"
   def install
