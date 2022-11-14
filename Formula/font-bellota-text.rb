11a6f9fcd645b44a9ca808c497727b12621e630c
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bellota-text.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-bellota-text.rb
+++ Formula/font-bellota-text.rb
@@ -1,5 +1,5 @@
 class FontBellotaText < Formula
-  head "https://github.com/google/fonts/trunk/ofl/bellotatext", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/bellotatext"
   desc "Bellota Text"
   homepage "https://fonts.google.com/specimen/Bellota+Text"
   def install
