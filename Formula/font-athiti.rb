2589aadf07b0d58a0b779f62f9a8c06e1c578376
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-athiti.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-athiti.rb
+++ Formula/font-athiti.rb
@@ -1,5 +1,5 @@
 class FontAthiti < Formula
-  head "https://github.com/google/fonts/trunk/ofl/athiti", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/athiti"
   desc "Athiti"
   homepage "https://fonts.google.com/specimen/Athiti"
   def install
