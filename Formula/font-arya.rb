75ea324567933015f017634a4ef649f967c4b7a7
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arya.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-arya.rb
+++ Formula/font-arya.rb
@@ -1,5 +1,5 @@
 class FontArya < Formula
-  head "https://github.com/google/fonts/trunk/ofl/arya", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/arya"
   desc "Arya"
   homepage "https://fonts.google.com/specimen/Arya"
   def install
