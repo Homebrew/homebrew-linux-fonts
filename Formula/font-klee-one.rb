86c70b8b2ab9ec2c755a9746f75ef9cf1ac3a1ba
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-klee-one.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-klee-one.rb
+++ Formula/font-klee-one.rb
@@ -1,5 +1,5 @@
 class FontKleeOne < Formula
-  head "https://github.com/google/fonts/trunk/ofl/kleeone", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kleeone"
   desc "Klee One"
   homepage "https://fonts.google.com/specimen/Klee+One"
   def install
