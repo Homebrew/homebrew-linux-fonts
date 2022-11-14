3bdf7c4c3caf4e097e4c0ba8527e83fcb18393b0
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-seoul-namsan.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-seoul-namsan.rb
+++ Formula/font-seoul-namsan.rb
@@ -1,7 +1,7 @@
 class FontSeoulNamsan < Formula
-  head "https://github.com/google/fonts/trunk/ofl/seoulnamsan", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/seoulnamsan"
   desc "Seoul Namsan"
-  homepage "https://fonts.google.com/specimen/Seoul+Namsan"
+  homepage "https://english.seoul.go.kr/seoul-views/seoul-symbols/5-fonts/"
   def install
     (share/"fonts").install "SeoulNamsan-Bold.ttf"
     (share/"fonts").install "SeoulNamsan-ExtraBold.ttf"
