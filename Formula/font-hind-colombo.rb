54a57ce95c76b781c6eda82c3ffa62cac6938a6b
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hind-colombo.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-hind-colombo.rb
+++ Formula/font-hind-colombo.rb
@@ -1,7 +1,7 @@
 class FontHindColombo < Formula
-  head "https://github.com/google/fonts/trunk/ofl/hindcolombo", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/hindcolombo"
   desc "Hind Colombo"
-  homepage "https://fonts.google.com/specimen/Hind+Colombo"
+  homepage "https://fonts.google.com/specimen/Hind"
   def install
     (share/"fonts").install "HindColombo-Bold.ttf"
     (share/"fonts").install "HindColombo-Light.ttf"
