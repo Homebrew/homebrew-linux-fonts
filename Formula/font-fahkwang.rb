d1d1c19d17434a1e944e41d909ccd036d84161c6
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fahkwang.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-fahkwang.rb
+++ Formula/font-fahkwang.rb
@@ -1,5 +1,5 @@
 class FontFahkwang < Formula
-  head "https://github.com/google/fonts/trunk/ofl/fahkwang", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fahkwang"
   desc "Fahkwang"
   homepage "https://fonts.google.com/specimen/Fahkwang"
   def install
