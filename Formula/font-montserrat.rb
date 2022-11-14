a7cc610b302a9e6c75de86b3a6de25915dce243b
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-montserrat.rb from dc75c413e9665508db7954cb29c617a6b64306db

--- Formula/font-montserrat.rb
+++ Formula/font-montserrat.rb
@@ -1,5 +1,5 @@
 class FontMontserrat < Formula
-  head "https://github.com/google/fonts/trunk/ofl/montserrat", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/montserrat"
   desc "Montserrat"
   homepage "https://fonts.google.com/specimen/Montserrat"
   def install
