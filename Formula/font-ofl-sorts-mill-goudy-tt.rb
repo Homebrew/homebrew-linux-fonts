7375ed1f5c275d2293ddb65f17d679defa0941ba
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ofl-sorts-mill-goudy-tt.rb from 8f3f7388be706e5d48109498744ae85e93161b18

--- Formula/font-ofl-sorts-mill-goudy-tt.rb
+++ Formula/font-ofl-sorts-mill-goudy-tt.rb
@@ -1,7 +1,7 @@
 class FontOflSortsMillGoudyTt < Formula
-  head "https://github.com/google/fonts/trunk/ofl/oflsortsmillgoudytt", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/oflsortsmillgoudytt"
   desc "OFL Sorts Mill Goudy TT"
-  homepage "https://fonts.google.com/specimen/OFL+Sorts+Mill+Goudy+TT"
+  homepage "https://fonts.google.com/specimen/Sorts+Mill+Goudy"
   def install
     (share/"fonts").install "OFLGoudyStMTT-Italic.ttf"
     (share/"fonts").install "OFLGoudyStMTT.ttf"
