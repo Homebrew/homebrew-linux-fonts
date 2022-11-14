0f4006b58536d03a56df9d80d4b39df47f56c74d
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tiro-devanagari-hindi.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-tiro-devanagari-hindi.rb
+++ Formula/font-tiro-devanagari-hindi.rb
@@ -1,5 +1,5 @@
 class FontTiroDevanagariHindi < Formula
-  head "https://github.com/google/fonts/trunk/ofl/tirodevanagarihindi", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tirodevanagarihindi"
   desc "Tiro Devanagari Hindi"
   desc "Broader proportions, generous counters, and strong diagonal strokes"
   homepage "https://fonts.google.com/specimen/Tiro+Devanagari+Hindi"
