b8af63009d0504202511d22f135923dcca3ac482
TANIGUCHI Masaya <m@docs.casa>
2022-11-14 00:07:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-chakra-petch.rb from 4f665d61014a941ae5e293909fb7ed7b7c5cdac9

--- Formula/font-chakra-petch.rb
+++ Formula/font-chakra-petch.rb
@@ -1,5 +1,5 @@
 class FontChakraPetch < Formula
-  head "https://github.com/google/fonts/trunk/ofl/chakrapetch", verified: "github.com/google/fonts/", using: :svn
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/chakrapetch"
   desc "Chakra Petch"
   homepage "https://fonts.google.com/specimen/Chakra+Petch"
   def install
