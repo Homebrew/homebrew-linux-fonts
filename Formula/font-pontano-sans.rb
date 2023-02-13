74706d4968c29c111bc579cdd14f53baeb8cb94e
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-02-13 00:06:34 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pontano-sans.rb from 675a1f8c2f995dd3da3220eca54228f3780f2367

--- Formula/font-pontano-sans.rb
+++ Formula/font-pontano-sans.rb
@@ -1,9 +1,10 @@
 class FontPontanoSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/pontanosans/PontanoSans-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/pontanosans/PontanoSans%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Pontano Sans"
+  desc "Minimalist and light-weighted sans serif"
   homepage "https://fonts.google.com/specimen/Pontano+Sans"
   def install
-    (share/"fonts").install "PontanoSans-Regular.ttf"
+    (share/"fonts").install "PontanoSans[wght].ttf"
   end
   test do
   end
