63edd8e19da893467d2c15ec7b8bf7935c84def5
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-02-20 00:07:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gajraj-one.rb from 6887bdf937ab5e84292a0dee4622a8370f499b4a

--- Formula/font-gajraj-one.rb
+++ Formula/font-gajraj-one.rb
@@ -1,7 +1,7 @@
 class FontGajrajOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gajrajone/GajrajOne-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/gajrajone/GajrajOne-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Gajraj One"
-  homepage "https://github.com/xconsau/GajrajOne"
+  homepage "https://fonts.google.com/specimen/Gajraj+One"
   def install
     (share/"fonts").install "GajrajOne-Regular.ttf"
   end
