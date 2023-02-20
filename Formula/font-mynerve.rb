d31994055683373cc2941a14f6eeab3566ce373e
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-02-20 00:07:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mynerve.rb from 6887bdf937ab5e84292a0dee4622a8370f499b4a

--- Formula/font-mynerve.rb
+++ Formula/font-mynerve.rb
@@ -1,7 +1,7 @@
 class FontMynerve < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mynerve/Mynerve-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/mynerve/Mynerve-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Mynerve"
-  homepage "https://github.com/carolinashort/MyNerve"
+  homepage "https://fonts.google.com/specimen/Mynerve"
   def install
     (share/"fonts").install "Mynerve-Regular.ttf"
   end
