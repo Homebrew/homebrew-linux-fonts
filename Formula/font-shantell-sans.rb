10f1ac78f90c1dab300853df68097e6a8ec4010b
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-02-20 00:07:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-shantell-sans.rb from 6887bdf937ab5e84292a0dee4622a8370f499b4a

--- Formula/font-shantell-sans.rb
+++ Formula/font-shantell-sans.rb
@@ -1,7 +1,7 @@
 class FontShantellSans < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/shantellsans"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/shantellsans"
   desc "Shantell Sans"
-  homepage "https://github.com/arrowtype/shantell-sans"
+  homepage "https://fonts.google.com/specimen/Shantell+Sans"
   def install
     (share/"fonts").install "ShantellSans-Italic[BNCE,INFM,SPAC,wght].ttf"
     (share/"fonts").install "ShantellSans[BNCE,INFM,SPAC,wght].ttf"
