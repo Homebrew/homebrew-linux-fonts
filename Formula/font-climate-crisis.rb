553724c08c296acb57139d015c2684eae4dcb218
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-02-27 00:06:34 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-climate-crisis.rb from f4c3ee29aae827c5fff7ccbada7bf62fea99bfb5

--- Formula/font-climate-crisis.rb
+++ Formula/font-climate-crisis.rb
@@ -1,7 +1,7 @@
 class FontClimateCrisis < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/climatecrisis/ClimateCrisis%5BYEAR%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/climatecrisis/ClimateCrisis%5BYEAR%5D.ttf", verified: "github.com/google/fonts/"
   desc "Climate Crisis"
-  homepage "https://github.com/dancoull/ClimateCrisis"
+  homepage "https://fonts.google.com/specimen/Climate+Crisis"
   def install
     (share/"fonts").install "ClimateCrisis[YEAR].ttf"
   end
