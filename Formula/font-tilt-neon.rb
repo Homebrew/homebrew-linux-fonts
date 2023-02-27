3dda1db0a1cca9b79a6c1109245bd0bd6efb9546
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-02-27 00:06:34 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tilt-neon.rb from f4c3ee29aae827c5fff7ccbada7bf62fea99bfb5

--- Formula/font-tilt-neon.rb
+++ Formula/font-tilt-neon.rb
@@ -1,7 +1,7 @@
 class FontTiltNeon < Formula
   head "https://github.com/google/fonts/raw/main/ofl/tiltneon/TiltNeon%5BXROT%2CYROT%5D.ttf", verified: "github.com/google/fonts/"
   desc "Tilt Neon"
-  homepage "https://math-practice.github.io/tilt-specimen/"
+  homepage "https://fonts.google.com/specimen/Tilt+Neon"
   def install
     (share/"fonts").install "TiltNeon[XROT,YROT].ttf"
   end
