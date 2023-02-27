b040f3388df4edac78b9992f7d328106629c1ae9
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-02-27 00:06:34 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tilt-prism.rb from f4c3ee29aae827c5fff7ccbada7bf62fea99bfb5

--- Formula/font-tilt-prism.rb
+++ Formula/font-tilt-prism.rb
@@ -1,7 +1,7 @@
 class FontTiltPrism < Formula
   head "https://github.com/google/fonts/raw/main/ofl/tiltprism/TiltPrism%5BXROT%2CYROT%5D.ttf", verified: "github.com/google/fonts/"
   desc "Tilt Prism"
-  homepage "https://math-practice.github.io/tilt-specimen/"
+  homepage "https://fonts.google.com/specimen/Tilt+Prism"
   def install
     (share/"fonts").install "TiltPrism[XROT,YROT].ttf"
   end
