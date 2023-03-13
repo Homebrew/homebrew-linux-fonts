1028d506d890db699664c00a867ba78a90d539e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-03-13 00:06:28 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nunito-sans.rb from 527bc25a970b2b8053e57145755d929bc559a6f1

--- Formula/font-nunito-sans.rb
+++ Formula/font-nunito-sans.rb
@@ -3,20 +3,8 @@ class FontNunitoSans < Formula
   desc "Nunito Sans"
   homepage "https://fonts.google.com/specimen/Nunito+Sans"
   def install
-    (share/"fonts").install "NunitoSans-Black.ttf"
-    (share/"fonts").install "NunitoSans-BlackItalic.ttf"
-    (share/"fonts").install "NunitoSans-Bold.ttf"
-    (share/"fonts").install "NunitoSans-BoldItalic.ttf"
-    (share/"fonts").install "NunitoSans-ExtraBold.ttf"
-    (share/"fonts").install "NunitoSans-ExtraBoldItalic.ttf"
-    (share/"fonts").install "NunitoSans-ExtraLight.ttf"
-    (share/"fonts").install "NunitoSans-ExtraLightItalic.ttf"
-    (share/"fonts").install "NunitoSans-Italic.ttf"
-    (share/"fonts").install "NunitoSans-Light.ttf"
-    (share/"fonts").install "NunitoSans-LightItalic.ttf"
-    (share/"fonts").install "NunitoSans-Regular.ttf"
-    (share/"fonts").install "NunitoSans-SemiBold.ttf"
-    (share/"fonts").install "NunitoSans-SemiBoldItalic.ttf"
+    (share/"fonts").install "NunitoSans-Italic[YTLC,opsz,wdth,wght].ttf"
+    (share/"fonts").install "NunitoSans[YTLC,opsz,wdth,wght].ttf"
   end
   test do
   end
