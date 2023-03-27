6f53a83f4c42ad2c4141cad037c9185fa309758f
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-03-27 00:06:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-alkatra.rb from 2082c0119050139d3b2b1386e1e17a7a6da8c848

--- Formula/font-alkatra.rb
+++ Formula/font-alkatra.rb
@@ -1,8 +1,8 @@
 class FontAlkatra < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alkatra/Alkatra%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/alkatra/Alkatra%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Alkatra"
   desc "Display typeface family"
-  homepage "https://github.com/suman51284/Alkatra"
+  homepage "https://fonts.google.com/specimen/Alkatra"
   def install
     (share/"fonts").install "Alkatra[wght].ttf"
   end
