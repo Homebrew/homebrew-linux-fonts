acfbd44d6d9c47936a36a38a0e9f5ee818ab4d0d
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-10 00:06:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-anuphan.rb from 1afea3daa7e30372f2c4179fbd5e962e8127aca6

--- Formula/font-anuphan.rb
+++ Formula/font-anuphan.rb
@@ -1,8 +1,8 @@
 class FontAnuphan < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anuphan/Anuphan%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/anuphan/Anuphan%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Anuphan"
   desc "Not a modification of ibm plex sans thai"
-  homepage "https://github.com/cadsondemak/Anuphan"
+  homepage "https://fonts.google.com/specimen/Anuphan"
   def install
     (share/"fonts").install "Anuphan[wght].ttf"
   end
