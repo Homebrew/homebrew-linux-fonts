319315d6999d3a203f7653273ff2bead06b35ad9
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-10 00:06:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rubik-pixels.rb from 1afea3daa7e30372f2c4179fbd5e962e8127aca6

--- Formula/font-rubik-pixels.rb
+++ Formula/font-rubik-pixels.rb
@@ -1,7 +1,7 @@
 class FontRubikPixels < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikpixels/RubikPixels-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikpixels/RubikPixels-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Rubik Pixels"
-  homepage "https://github.com/NaN-xyz/Rubik-Filtered"
+  homepage "https://fonts.google.com/specimen/Rubik+Pixels"
   def install
     (share/"fonts").install "RubikPixels-Regular.ttf"
   end
