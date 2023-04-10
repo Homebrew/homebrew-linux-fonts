db045b97e00bd85c535815bb4c644f99578ce6bd
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-10 00:06:10 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-zhudou-sans.rb from 3bf25814f84c4a09825d4d064f705d13043921f5

--- Formula/font-zhudou-sans.rb
+++ Formula/font-zhudou-sans.rb
@@ -1,19 +1,20 @@
 class FontZhudouSans < Formula
-  version "1.000"
-  sha256 "dae5ff79f8f1db78661a6ec0ae7c1732965c034a3c04bd95cd43baabd14996ca"
+  version "2.000"
+  sha256 "1a2718aa52c98d1ac7e18d60e0f1d61057b18e558e8196a3a770104855a6fc69"
   url "https://github.com/Buernia/Zhudou-Sans/releases/download/v#{version}/Zhudou.Sans.zip"
   desc "Zhudou Sans"
   desc "煮豆黑体"
   desc "CJK symbols and punctuation derived from Noto Sans"
   homepage "https://github.com/Buernia/Zhudou-Sans"
   def install
-    (share/"fonts").install "Zhudou Sans Bold.ttf"
-    (share/"fonts").install "Zhudou Sans ExtraLight.ttf"
-    (share/"fonts").install "Zhudou Sans Heavy.ttf"
-    (share/"fonts").install "Zhudou Sans Light.ttf"
-    (share/"fonts").install "Zhudou Sans Medium.ttf"
-    (share/"fonts").install "Zhudou Sans Normal.ttf"
-    (share/"fonts").install "Zhudou Sans Regular.ttf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}otf/ZhudouSans-Bold.otf"
+    (share/"fonts").install "#{parent}otf/ZhudouSans-ExtraLight.otf"
+    (share/"fonts").install "#{parent}otf/ZhudouSans-Heavy.otf"
+    (share/"fonts").install "#{parent}otf/ZhudouSans-Light.otf"
+    (share/"fonts").install "#{parent}otf/ZhudouSans-Medium.otf"
+    (share/"fonts").install "#{parent}otf/ZhudouSans-Normal.otf"
+    (share/"fonts").install "#{parent}otf/ZhudouSans-Regular.otf"
   end
   test do
   end
