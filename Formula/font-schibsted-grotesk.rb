6d18f12a7cf556b4c5da5bdc3a386c425ecd48de
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-10 00:06:09 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-schibsted-grotesk.rb from 1afea3daa7e30372f2c4179fbd5e962e8127aca6

--- Formula/font-schibsted-grotesk.rb
+++ Formula/font-schibsted-grotesk.rb
@@ -1,8 +1,8 @@
 class FontSchibstedGrotesk < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/schibstedgrotesk"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/schibstedgrotesk"
   desc "Schibsted Grotesk"
   desc "Digital-first font family crafted for user interfaces"
-  homepage "https://github.com/schibsted/schibsted-grotesk"
+  homepage "https://fonts.google.com/specimen/Schibsted+Grotesk"
   def install
     (share/"fonts").install "SchibstedGrotesk-Italic[wght].ttf"
     (share/"fonts").install "SchibstedGrotesk[wght].ttf"
