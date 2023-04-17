a21cbe9983dc5eccc6fd8d1a30ad2d7333895197
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-17 00:06:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-syriac.rb from b96ebde83cb6227cc9d388f852d011bdb1e4e2a8

--- Formula/font-noto-sans-syriac.rb
+++ Formula/font-noto-sans-syriac.rb
@@ -1,11 +1,9 @@
 class FontNotoSansSyriac < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosanssyriac"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanssyriac/NotoSansSyriac%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans Syriac"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Syriac"
   def install
-    (share/"fonts").install "NotoSansSyriac-Black.ttf"
-    (share/"fonts").install "NotoSansSyriac-Regular.ttf"
-    (share/"fonts").install "NotoSansSyriac-Thin.ttf"
+    (share/"fonts").install "NotoSansSyriac[wght].ttf"
   end
   test do
   end
