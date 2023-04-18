012ac6bccb2804473b663d0da0ae509fad2588a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-18 00:06:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lilex.rb from 67fdc3cbbbccf6e7ae7a791a1dfcd20276bcc608

--- Formula/font-lilex.rb
+++ Formula/font-lilex.rb
@@ -1,15 +1,19 @@
 class FontLilex < Formula
-  version "1.100"
-  sha256 "1570385aa6fd596d11971a3ab800a220f10a6e8fd1f3b164177561244495bb7d"
-  url "https://github.com/mishamyrt/Lilex/releases/download/#{version}/Lilex-#{version}.zip"
+  version "2.200"
+  sha256 "c4029f22577077f0e82c39af6f3513df5d621e4668f49e7cea9c514aba93a59a"
+  url "https://github.com/mishamyrt/Lilex/releases/download/#{version}/Lilex.zip"
   desc "Lilex"
+  desc "Programming font"
   homepage "https://github.com/mishamyrt/Lilex"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}otf/Lilex-Bold.otf"
-    (share/"fonts").install "#{parent}otf/Lilex-Medium.otf"
-    (share/"fonts").install "#{parent}otf/Lilex-Regular.otf"
-    (share/"fonts").install "#{parent}variable_ttf/Lilex-VF.ttf"
+    (share/"fonts").install "#{parent}ttf/Lilex-Bold.ttf"
+    (share/"fonts").install "#{parent}ttf/Lilex-ExtraLight.ttf"
+    (share/"fonts").install "#{parent}ttf/Lilex-ExtraThick.ttf"
+    (share/"fonts").install "#{parent}ttf/Lilex-Meduim.ttf"
+    (share/"fonts").install "#{parent}ttf/Lilex-Regular.ttf"
+    (share/"fonts").install "#{parent}ttf/Lilex-Thin.ttf"
+    (share/"fonts").install "#{parent}variable/Lilex-VF.ttf"
   end
   test do
   end
