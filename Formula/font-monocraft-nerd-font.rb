4b3684e873a8bcc432643e92cb7092a768bda045
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-23 00:06:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monocraft-nerd-font.rb from b537a017c7bfd4321a5e0e949b12b58d39a7c82d

---
+++ Formula/font-monocraft-nerd-font.rb
@@ -0,0 +1,13 @@
+class FontMonocraftNerdFont < Formula
+  version "2.5"
+  sha256 "431329f14c1c4635b248d1d6a0d797dac5fdb5e678fad0858fe0c6e356b3b17c"
+  url "https://github.com/IdreesInc/Monocraft/releases/download/v#{version}/Monocraft-nerd-fonts-patched.ttf"
+  desc "Monocraft with Nerd glyphs"
+  desc "Monospaced font inspired by the Minecraft typeface patched to add Nerd glyphs"
+  homepage "https://github.com/IdreesInc/Monocraft"
+  def install
+    (share/"fonts").install "Monocraft-nerd-fonts-patched.ttf"
+  end
+  test do
+  end
+end
