aa4030695c4ad0d2ba2f523ddf6972b17658b670
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-24 00:06:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-poltawski-nowy.rb from 5a03ed6052bb67277940a4e577b01fce72e36384

--- Formula/font-poltawski-nowy.rb
+++ Formula/font-poltawski-nowy.rb
@@ -1,7 +1,7 @@
 class FontPoltawskiNowy < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/poltawskinowy"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/poltawskinowy"
   desc "Poltawski Nowy"
-  homepage "https://github.com/kosmynkab/Poltawski-Nowy"
+  homepage "https://fonts.google.com/specimen/Poltawski+Nowy"
   def install
     (share/"fonts").install "PoltawskiNowy-Italic[wght].ttf"
     (share/"fonts").install "PoltawskiNowy[wght].ttf"
