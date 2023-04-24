f556f5c18d35550d736337ff153e4b62df66f678
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-24 00:06:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sigmar.rb from 5a03ed6052bb67277940a4e577b01fce72e36384

--- Formula/font-sigmar.rb
+++ Formula/font-sigmar.rb
@@ -1,7 +1,7 @@
 class FontSigmar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sigmar/Sigmar-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/sigmar/Sigmar-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Sigmar"
-  homepage "https://github.com/googlefonts/sigmarone"
+  homepage "https://fonts.google.com/specimen/Sigmar"
   def install
     (share/"fonts").install "Sigmar-Regular.ttf"
   end
