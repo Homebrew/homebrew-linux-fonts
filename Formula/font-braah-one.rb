5bb721c1c9a01785063c9e88599156f5c12c00bc
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-24 00:06:19 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-braah-one.rb from 5a03ed6052bb67277940a4e577b01fce72e36384

--- Formula/font-braah-one.rb
+++ Formula/font-braah-one.rb
@@ -1,8 +1,8 @@
 class FontBraahOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/braahone/BraahOne-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/braahone/BraahOne-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Braah One"
   desc "Bold and playful font"
-  homepage "https://github.com/artandtype/Braah"
+  homepage "https://fonts.google.com/specimen/Braah+One"
   def install
     (share/"fonts").install "BraahOne-Regular.ttf"
   end
