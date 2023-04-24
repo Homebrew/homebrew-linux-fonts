a9411f3384b248c4ecb08c4c44e6c18c2e3bec60
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-24 00:06:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pathway-extreme.rb from 5a03ed6052bb67277940a4e577b01fce72e36384

--- Formula/font-pathway-extreme.rb
+++ Formula/font-pathway-extreme.rb
@@ -1,8 +1,8 @@
 class FontPathwayExtreme < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/pathwayextreme"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/pathwayextreme"
   desc "Pathway Extreme"
   desc "Very popular historic typographic style"
-  homepage "https://github.com/etunni/Pathway-Variable-Font"
+  homepage "https://fonts.google.com/specimen/Pathway+Extreme"
   def install
     (share/"fonts").install "PathwayExtreme-Italic[opsz,wdth,wght].ttf"
     (share/"fonts").install "PathwayExtreme[opsz,wdth,wght].ttf"
