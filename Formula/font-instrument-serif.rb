bf2bad9b51c782e556aeea5a4715bf38f541a451
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-04-24 00:06:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-instrument-serif.rb from 5a03ed6052bb67277940a4e577b01fce72e36384

--- Formula/font-instrument-serif.rb
+++ Formula/font-instrument-serif.rb
@@ -1,7 +1,7 @@
 class FontInstrumentSerif < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/instrumentserif"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/instrumentserif"
   desc "Instrument Serif"
-  homepage "https://github.com/Instrument/instrument-serif"
+  homepage "https://fonts.google.com/specimen/Instrument+Serif"
   def install
     (share/"fonts").install "InstrumentSerif-Italic.ttf"
     (share/"fonts").install "InstrumentSerif-Regular.ttf"
