795f6b55d1db3527d170258e49444f40492b8a16
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-01 00:06:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-markazi-text.rb from dd5744b54b4728c9577201d962b42df157c7273d

--- Formula/font-markazi-text.rb
+++ Formula/font-markazi-text.rb
@@ -1,10 +1,10 @@
 class FontMarkaziText < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/markazitext/MarkaziText%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/markazitextvfbeta/MarkaziText-VF.ttf", verified: "github.com/google/fonts/"
   desc "Markazi Text"
   desc "Contemporary and highly readable typeface"
   homepage "https://fonts.google.com/specimen/Markazi+Text"
   def install
-    (share/"fonts").install "MarkaziText[wght].ttf"
+    (share/"fonts").install "MarkaziText-VF.ttf"
   end
   test do
   end
