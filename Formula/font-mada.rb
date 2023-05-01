2f047f4b2139ebc732288fe276f923e894b7f6b8
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-01 00:06:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-mada.rb from dd5744b54b4728c9577201d962b42df157c7273d

--- Formula/font-mada.rb
+++ Formula/font-mada.rb
@@ -1,15 +1,9 @@
 class FontMada < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/mada"
+  head "https://github.com/google/fonts/raw/main/ofl/mada/Mada%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Mada"
   homepage "https://fonts.google.com/specimen/Mada"
   def install
-    (share/"fonts").install "Mada-Black.ttf"
-    (share/"fonts").install "Mada-Bold.ttf"
-    (share/"fonts").install "Mada-ExtraLight.ttf"
-    (share/"fonts").install "Mada-Light.ttf"
-    (share/"fonts").install "Mada-Medium.ttf"
-    (share/"fonts").install "Mada-Regular.ttf"
-    (share/"fonts").install "Mada-SemiBold.ttf"
+    (share/"fonts").install "Mada[wght].ttf"
   end
   test do
   end
