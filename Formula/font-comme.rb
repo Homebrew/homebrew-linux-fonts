9a5f8362fc814c23b15d05ef14611c399834d647
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-01 00:06:36 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-comme.rb from dd5744b54b4728c9577201d962b42df157c7273d

--- Formula/font-comme.rb
+++ Formula/font-comme.rb
@@ -1,8 +1,8 @@
 class FontComme < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/comme/Comme%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/comme/Comme%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Comme"
   desc "Variable, with a weight axis ranging from thin to black"
-  homepage "https://github.com/googlefonts/comme"
+  homepage "https://fonts.google.com/specimen/Comme"
   def install
     (share/"fonts").install "Comme[wght].ttf"
   end
