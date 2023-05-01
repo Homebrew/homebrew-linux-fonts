0e6d0408f1ba48c1a524e08dded8a809d0e15d68
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-01 00:06:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-foldit.rb from dd5744b54b4728c9577201d962b42df157c7273d

--- Formula/font-foldit.rb
+++ Formula/font-foldit.rb
@@ -1,8 +1,8 @@
 class FontFoldit < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/foldit/Foldit%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/foldit/Foldit%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Foldit"
   desc "Font which uses gradients to play with dimension and sense of space"
-  homepage "https://github.com/SophiaDesign/Foldit"
+  homepage "https://fonts.google.com/specimen/Foldit"
   def install
     (share/"fonts").install "Foldit[wght].ttf"
   end
