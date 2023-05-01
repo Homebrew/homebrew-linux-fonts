451d34ed228896ac71d188ff9e92809821ed34db
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-01 00:06:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-konkhmer-sleokchher.rb from dd5744b54b4728c9577201d962b42df157c7273d

--- Formula/font-konkhmer-sleokchher.rb
+++ Formula/font-konkhmer-sleokchher.rb
@@ -1,8 +1,8 @@
 class FontKonkhmerSleokchher < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/konkhmersleokchher/KonkhmerSleokchher-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/konkhmersleokchher/KonkhmerSleokchher-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Konkhmer Sleokchher"
   desc "Ideal for use as headings or in large typography"
-  homepage "https://github.com/suonmaysophanith7/KonKhmer_SleokChher"
+  homepage "https://fonts.google.com/specimen/Konkhmer+Sleokchher"
   def install
     (share/"fonts").install "KonkhmerSleokchher-Regular.ttf"
   end
