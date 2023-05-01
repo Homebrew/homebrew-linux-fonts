020069c5eaf03c9eafbb7bcf95b13d31cb918366
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-01 00:06:37 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-monofett.rb from dd5744b54b4728c9577201d962b42df157c7273d

--- Formula/font-monofett.rb
+++ Formula/font-monofett.rb
@@ -1,9 +1,9 @@
 class FontMonofett < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/monofett/Monofett.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/monofett/Monofett-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Monofett"
   homepage "https://fonts.google.com/specimen/Monofett"
   def install
-    (share/"fonts").install "Monofett.ttf"
+    (share/"fonts").install "Monofett-Regular.ttf"
   end
   test do
   end
