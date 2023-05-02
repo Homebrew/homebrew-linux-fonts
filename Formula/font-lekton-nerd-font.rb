e8ee4bfedc6534d8933a2ae9f1df25956fa865a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lekton-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-lekton-nerd-font.rb
+++ Formula/font-lekton-nerd-font.rb
@@ -1,17 +1,20 @@
 class FontLektonNerdFont < Formula
-  version "2.3.3"
-  sha256 "56e437509e5e56a18909f1a167858fba5f8e43c98055590ec645eb55ed47670c"
+  version "3.0.0"
+  sha256 "6ccb4417729fffcec59a925dc581afca2443b3b27a55863b7377dcfe3aee2930"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Lekton.zip"
   desc "Lekton Nerd Font (Lekton)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Lekton Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Lekton Nerd Font Complete.ttf"
-    (share/"fonts").install "Lekton-Bold Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Lekton-Bold Nerd Font Complete.ttf"
-    (share/"fonts").install "Lekton-Italic Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Lekton-Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "LektonNerdFont-Bold.ttf"
+    (share/"fonts").install "LektonNerdFont-Italic.ttf"
+    (share/"fonts").install "LektonNerdFont-Regular.ttf"
+    (share/"fonts").install "LektonNerdFontMono-Bold.ttf"
+    (share/"fonts").install "LektonNerdFontMono-Italic.ttf"
+    (share/"fonts").install "LektonNerdFontMono-Regular.ttf"
+    (share/"fonts").install "LektonNerdFontPropo-Bold.ttf"
+    (share/"fonts").install "LektonNerdFontPropo-Italic.ttf"
+    (share/"fonts").install "LektonNerdFontPropo-Regular.ttf"
   end
   test do
   end
