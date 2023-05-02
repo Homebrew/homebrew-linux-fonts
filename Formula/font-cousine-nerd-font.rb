cb575aaf01c7c223a253b31accca333c77291a26
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-cousine-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-cousine-nerd-font.rb
+++ Formula/font-cousine-nerd-font.rb
@@ -1,19 +1,23 @@
 class FontCousineNerdFont < Formula
-  version "2.3.3"
-  sha256 "329b95fe1fcedbc6527e6b5b5ab2c58b7469fdc1c4cfcc3b88d537f3186da2a2"
+  version "3.0.0"
+  sha256 "b3fa57d3ccbce0bfe412c81bb4907f6dd08d91cc82a05509d27bc32d00704f32"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Cousine.zip"
   desc "Cousine Nerd Font (Cousine)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Cousine Bold Italic Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Cousine Bold Italic Nerd Font Complete.ttf"
-    (share/"fonts").install "Cousine Bold Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Cousine Bold Nerd Font Complete.ttf"
-    (share/"fonts").install "Cousine Italic Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Cousine Italic Nerd Font Complete.ttf"
-    (share/"fonts").install "Cousine Regular Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Cousine Regular Nerd Font Complete.ttf"
+    (share/"fonts").install "CousineNerdFont-Bold.ttf"
+    (share/"fonts").install "CousineNerdFont-BoldItalic.ttf"
+    (share/"fonts").install "CousineNerdFont-Italic.ttf"
+    (share/"fonts").install "CousineNerdFont-Regular.ttf"
+    (share/"fonts").install "CousineNerdFontMono-Bold.ttf"
+    (share/"fonts").install "CousineNerdFontMono-BoldItalic.ttf"
+    (share/"fonts").install "CousineNerdFontMono-Italic.ttf"
+    (share/"fonts").install "CousineNerdFontMono-Regular.ttf"
+    (share/"fonts").install "CousineNerdFontPropo-Bold.ttf"
+    (share/"fonts").install "CousineNerdFontPropo-BoldItalic.ttf"
+    (share/"fonts").install "CousineNerdFontPropo-Italic.ttf"
+    (share/"fonts").install "CousineNerdFontPropo-Regular.ttf"
   end
   test do
   end
