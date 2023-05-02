b95f5906834b55ea54f51073028f912f99eb402a
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-agave-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-agave-nerd-font.rb
+++ Formula/font-agave-nerd-font.rb
@@ -1,13 +1,17 @@
 class FontAgaveNerdFont < Formula
-  version "2.3.3"
-  sha256 "1513d49b4d345b53beeda968f090332824f9e5e6d9a32bab5edd112ea917eb9c"
+  version "3.0.0"
+  sha256 "f31ad97d42848f5b293e2ba4a75f04e1bf140b4a718ba1d8a95c77e71a1323bd"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Agave.zip"
   desc "Agave Nerd Font (Agave)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "agave regular Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "agave regular Nerd Font Complete.ttf"
+    (share/"fonts").install "AgaveNerdFont-Bold.ttf"
+    (share/"fonts").install "AgaveNerdFont-Regular.ttf"
+    (share/"fonts").install "AgaveNerdFontMono-Bold.ttf"
+    (share/"fonts").install "AgaveNerdFontMono-Regular.ttf"
+    (share/"fonts").install "AgaveNerdFontPropo-Bold.ttf"
+    (share/"fonts").install "AgaveNerdFontPropo-Regular.ttf"
   end
   test do
   end
