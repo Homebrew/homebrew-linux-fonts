68f7d8022eadeef9afab92bcfcf20ea7e70f0150
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-go-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-inconsolata-go-nerd-font.rb
+++ Formula/font-inconsolata-go-nerd-font.rb
@@ -1,15 +1,17 @@
 class FontInconsolataGoNerdFont < Formula
-  version "2.3.3"
-  sha256 "e54a328970bb060cf94b5ad5edd7bf7e844512bb12341adf467864a10d33e732"
+  version "3.0.0"
+  sha256 "e39771fe2e9fe279511e0369d3b64b1c700d9391c8cc084db17878b512d69c8e"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataGo.zip"
   desc "InconsolataGo Nerd Font (InconsolataGo)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "InconsolataGo Bold Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "InconsolataGo Bold Nerd Font Complete.ttf"
-    (share/"fonts").install "InconsolataGo Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "InconsolataGo Nerd Font Complete.ttf"
+    (share/"fonts").install "InconsolataGoNerdFont-Bold.ttf"
+    (share/"fonts").install "InconsolataGoNerdFont-Regular.ttf"
+    (share/"fonts").install "InconsolataGoNerdFontMono-Bold.ttf"
+    (share/"fonts").install "InconsolataGoNerdFontMono-Regular.ttf"
+    (share/"fonts").install "InconsolataGoNerdFontPropo-Bold.ttf"
+    (share/"fonts").install "InconsolataGoNerdFontPropo-Regular.ttf"
   end
   test do
   end
