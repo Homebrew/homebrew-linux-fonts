64e2dfd4bdb76d3e8c9fed72f4930ea6f9bad619
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-mono-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-fira-mono-nerd-font.rb
+++ Formula/font-fira-mono-nerd-font.rb
@@ -1,17 +1,20 @@
 class FontFiraMonoNerdFont < Formula
-  version "2.3.3"
-  sha256 "490e72a6db6a686ca93079248c761f23bb9b5dda9b3fa7eb51cbc40e12ddc5ce"
+  version "3.0.0"
+  sha256 "9e5ce824731cd365b5ec2b3ad0355797326b3d421eeb39358cb27f78f485e4db"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraMono.zip"
-  desc "FuraMono Nerd Font (Fira)"
+  desc "FiraMono Nerd Font (Fira)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Fura Mono Bold Nerd Font Complete Mono.otf"
-    (share/"fonts").install "Fura Mono Bold Nerd Font Complete.otf"
-    (share/"fonts").install "Fura Mono Medium Nerd Font Complete Mono.otf"
-    (share/"fonts").install "Fura Mono Medium Nerd Font Complete.otf"
-    (share/"fonts").install "Fura Mono Regular Nerd Font Complete Mono.otf"
-    (share/"fonts").install "Fura Mono Regular Nerd Font Complete.otf"
+    (share/"fonts").install "FiraMonoNerdFont-Bold.otf"
+    (share/"fonts").install "FiraMonoNerdFont-Medium.otf"
+    (share/"fonts").install "FiraMonoNerdFont-Regular.otf"
+    (share/"fonts").install "FiraMonoNerdFontMono-Bold.otf"
+    (share/"fonts").install "FiraMonoNerdFontMono-Medium.otf"
+    (share/"fonts").install "FiraMonoNerdFontMono-Regular.otf"
+    (share/"fonts").install "FiraMonoNerdFontPropo-Bold.otf"
+    (share/"fonts").install "FiraMonoNerdFontPropo-Medium.otf"
+    (share/"fonts").install "FiraMonoNerdFontPropo-Regular.otf"
   end
   test do
   end
