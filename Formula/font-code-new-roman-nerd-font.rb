4838e5dec8cd052d1e6c23659317a178733ba4d0
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-code-new-roman-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-code-new-roman-nerd-font.rb
+++ Formula/font-code-new-roman-nerd-font.rb
@@ -1,17 +1,20 @@
 class FontCodeNewRomanNerdFont < Formula
-  version "2.3.3"
-  sha256 "d1a6ddfa06c1c78221318751ab878b9d70fc5ceda1650c6a10a097302e2298e4"
+  version "3.0.0"
+  sha256 "01529d02564511fb8a3c9e10011c9a0d1c4912fb979bd661cf606087ea2a5df4"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/CodeNewRoman.zip"
   desc "CodeNewRoman Nerd Font (Code New Roman)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Code New Roman Bold Nerd Font Complete Mono.otf"
-    (share/"fonts").install "Code New Roman Bold Nerd Font Complete.otf"
-    (share/"fonts").install "Code New Roman Italic Nerd Font Complete Mono.otf"
-    (share/"fonts").install "Code New Roman Italic Nerd Font Complete.otf"
-    (share/"fonts").install "Code New Roman Nerd Font Complete Mono.otf"
-    (share/"fonts").install "Code New Roman Nerd Font Complete.otf"
+    (share/"fonts").install "CodeNewRomanNerdFont-Bold.otf"
+    (share/"fonts").install "CodeNewRomanNerdFont-Italic.otf"
+    (share/"fonts").install "CodeNewRomanNerdFont-Regular.otf"
+    (share/"fonts").install "CodeNewRomanNerdFontMono-Bold.otf"
+    (share/"fonts").install "CodeNewRomanNerdFontMono-Italic.otf"
+    (share/"fonts").install "CodeNewRomanNerdFontMono-Regular.otf"
+    (share/"fonts").install "CodeNewRomanNerdFontPropo-Bold.otf"
+    (share/"fonts").install "CodeNewRomanNerdFontPropo-Italic.otf"
+    (share/"fonts").install "CodeNewRomanNerdFontPropo-Regular.otf"
   end
   test do
   end
