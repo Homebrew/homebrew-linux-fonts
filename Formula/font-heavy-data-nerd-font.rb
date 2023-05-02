a49294d76a6c9cab1bf3a0326bb382b40815f93b
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-heavy-data-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-heavy-data-nerd-font.rb
+++ Formula/font-heavy-data-nerd-font.rb
@@ -1,12 +1,13 @@
 class FontHeavyDataNerdFont < Formula
-  version "2.3.3"
-  sha256 "57d24ae8793a91a983022b547c7cb9bee635f1e427e9c48ed9b32449c57cf7e6"
+  version "3.0.0"
+  sha256 "00164c3331126eaf32f170322c845ef81c5f78f5a9b9bd03165e1f82df190bb4"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/HeavyData.zip"
   desc "HeavyData Nerd Font (Heavy Data)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Heavy Data Nerd Font Complete.ttf"
+    (share/"fonts").install "HeavyDataNerdFont-Regular.ttf"
+    (share/"fonts").install "HeavyDataNerdFontPropo-Regular.ttf"
   end
   test do
   end
