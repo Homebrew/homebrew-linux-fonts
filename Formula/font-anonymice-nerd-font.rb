30e53eeea7f553e642c39a42e023d8c5d7d51a0b
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-anonymice-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-anonymice-nerd-font.rb
+++ Formula/font-anonymice-nerd-font.rb
@@ -1,13 +1,23 @@
 class FontAnonymiceNerdFont < Formula
-  version "2.3.3"
-  sha256 "06cf35ef407b8e71788cff5a9551a1d72db761df82c112a73ec4a77786eac562"
+  version "3.0.0"
+  sha256 "c5fc928f8bcf5c44e0d24c2f16d46c9a185eb1ddc7547274c45cae850f5693a1"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AnonymousPro.zip"
-  desc "Anonymice Nerd Font (Anonymous Pro)"
+  desc "AnonymicePro Nerd Font (Anonymous Pro)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Anonymice Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Anonymice Nerd Font Complete.ttf"
+    (share/"fonts").install "AnonymiceProNerdFont-Bold.ttf"
+    (share/"fonts").install "AnonymiceProNerdFont-BoldItalic.ttf"
+    (share/"fonts").install "AnonymiceProNerdFont-Italic.ttf"
+    (share/"fonts").install "AnonymiceProNerdFont-Regular.ttf"
+    (share/"fonts").install "AnonymiceProNerdFontMono-Bold.ttf"
+    (share/"fonts").install "AnonymiceProNerdFontMono-BoldItalic.ttf"
+    (share/"fonts").install "AnonymiceProNerdFontMono-Italic.ttf"
+    (share/"fonts").install "AnonymiceProNerdFontMono-Regular.ttf"
+    (share/"fonts").install "AnonymiceProNerdFontPropo-Bold.ttf"
+    (share/"fonts").install "AnonymiceProNerdFontPropo-BoldItalic.ttf"
+    (share/"fonts").install "AnonymiceProNerdFontPropo-Italic.ttf"
+    (share/"fonts").install "AnonymiceProNerdFontPropo-Regular.ttf"
   end
   test do
   end
