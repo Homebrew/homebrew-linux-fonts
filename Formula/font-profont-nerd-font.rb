04fe7fd58ea52b8799bc0dfe807651d9d0c5cd16
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-profont-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-profont-nerd-font.rb
+++ Formula/font-profont-nerd-font.rb
@@ -1,15 +1,17 @@
 class FontProfontNerdFont < Formula
-  version "2.3.3"
-  sha256 "da9a286a4cd149ab8e42b54965821dddbf0689f5c3fa5a2cad2985cb2852783c"
+  version "3.0.0"
+  sha256 "6c407fede4ebfb29b0913ab1a1b8e7e6ca5e57bea2222c9c2f65fc7c61df9445"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ProFont.zip"
   desc "ProFont Nerd Font families (ProFont)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "ProFont IIx Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "ProFont IIx Nerd Font Complete.ttf"
-    (share/"fonts").install "ProFontWindows Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "ProFontWindows Nerd Font Complete.ttf"
+    (share/"fonts").install "ProFontIIxNerdFont-Regular.ttf"
+    (share/"fonts").install "ProFontIIxNerdFontMono-Regular.ttf"
+    (share/"fonts").install "ProFontIIxNerdFontPropo-Regular.ttf"
+    (share/"fonts").install "ProFontWindowsNerdFont-Regular.ttf"
+    (share/"fonts").install "ProFontWindowsNerdFontMono-Regular.ttf"
+    (share/"fonts").install "ProFontWindowsNerdFontPropo-Regular.ttf"
   end
   test do
   end
