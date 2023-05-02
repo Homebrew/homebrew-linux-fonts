d3366a35ccba899ac44f7617fbde8332705a1d57
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-symbols-only-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-symbols-only-nerd-font.rb
+++ Formula/font-symbols-only-nerd-font.rb
@@ -1,15 +1,13 @@
 class FontSymbolsOnlyNerdFont < Formula
-  version "2.3.3"
-  sha256 "9cf0c800de86bc31f1115b0f4762ef8d80a749f6cfa4cf7413b01fc563cc3f6a"
+  version "3.0.0"
+  sha256 "91c0bbb8690d978cabbd693ab1e4df18d2f6d021e7f05fd7fd853fed4d56234b"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/NerdFontsSymbolsOnly.zip"
   desc "Symbols Nerd Font (Symbols Only)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Symbols-1000-em Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Symbols-1000-em Nerd Font Complete.ttf"
-    (share/"fonts").install "Symbols-2048-em Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Symbols-2048-em Nerd Font Complete.ttf"
+    (share/"fonts").install "SymbolsNerdFont-Regular.ttf"
+    (share/"fonts").install "SymbolsNerdFontMono-Regular.ttf"
   end
   test do
   end
