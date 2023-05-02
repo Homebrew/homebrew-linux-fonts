054e3a9636ae92f2fdf66894c7e382dcf7d6e071
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bigblue-terminal-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-bigblue-terminal-nerd-font.rb
+++ Formula/font-bigblue-terminal-nerd-font.rb
@@ -1,15 +1,17 @@
 class FontBigblueTerminalNerdFont < Formula
-  version "2.3.3"
-  sha256 "405c9df47121a29b9befd1f4eca77e2abfb8c984bac25b87f2306e4a56b3b1a9"
+  version "3.0.0"
+  sha256 "d017dbab1c1a79c2e9c7a79e494c66ae6fd8a18532caf3217de6e6e5ab45e344"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BigBlueTerminal.zip"
-  desc "BigBlue_Terminal Nerd Font families (BigBlue Terminal)"
+  desc "BigBlueTerm Nerd Font families (BigBlue Terminal)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "BigBlue Terminal 437TT Nerd Font Complete Mono.TTF"
-    (share/"fonts").install "BigBlue Terminal 437TT Nerd Font Complete.TTF"
-    (share/"fonts").install "BigBlue TerminalPlus Nerd Font Complete Mono.TTF"
-    (share/"fonts").install "BigBlue TerminalPlus Nerd Font Complete.TTF"
+    (share/"fonts").install "BigBlueTerm437NerdFont-Regular.ttf"
+    (share/"fonts").install "BigBlueTerm437NerdFontMono-Regular.ttf"
+    (share/"fonts").install "BigBlueTerm437NerdFontPropo-Regular.ttf"
+    (share/"fonts").install "BigBlueTermPlusNerdFont-Regular.ttf"
+    (share/"fonts").install "BigBlueTermPlusNerdFontMono-Regular.ttf"
+    (share/"fonts").install "BigBlueTermPlusNerdFontPropo-Regular.ttf"
   end
   test do
   end
