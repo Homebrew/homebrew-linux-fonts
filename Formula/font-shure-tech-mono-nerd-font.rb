b6e7b88fa8112645417e493fe4f8599d130627e0
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-shure-tech-mono-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-shure-tech-mono-nerd-font.rb
+++ Formula/font-shure-tech-mono-nerd-font.rb
@@ -1,13 +1,14 @@
 class FontShureTechMonoNerdFont < Formula
-  version "2.3.3"
-  sha256 "52a51d0a852b7baa33cac37af92d369d7034d9954e0fdc39522c34f84a78b014"
+  version "3.0.0"
+  sha256 "906002e40b70933de50e8ccac48e2f8fff816c6060499f4a81e2bc16af0cdfff"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ShareTechMono.zip"
   desc "ShureTechMono Nerd Font (Share Tech Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Shure Tech Mono Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Shure Tech Mono Nerd Font Complete.ttf"
+    (share/"fonts").install "ShureTechMonoNerdFont-Regular.ttf"
+    (share/"fonts").install "ShureTechMonoNerdFontMono-Regular.ttf"
+    (share/"fonts").install "ShureTechMonoNerdFontPropo-Regular.ttf"
   end
   test do
   end
