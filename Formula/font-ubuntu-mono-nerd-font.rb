5eb5060a94f1607c445b2c126c848fc712e27108
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu-mono-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-ubuntu-mono-nerd-font.rb
+++ Formula/font-ubuntu-mono-nerd-font.rb
@@ -1,19 +1,23 @@
 class FontUbuntuMonoNerdFont < Formula
-  version "2.3.3"
-  sha256 "15bccf8a1a239915ae5854956d382e873a82caf145594a618bbaf41e30b0cbf7"
+  version "3.0.0"
+  sha256 "7af191908ae8e37ab1146eebd2c67f5f5151aa65e954ec1be59c65dc998ef587"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/UbuntuMono.zip"
   desc "UbuntuMono Nerd Font (Ubuntu Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Ubuntu Mono Bold Italic Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Ubuntu Mono Bold Italic Nerd Font Complete.ttf"
-    (share/"fonts").install "Ubuntu Mono Bold Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Ubuntu Mono Bold Nerd Font Complete.ttf"
-    (share/"fonts").install "Ubuntu Mono Italic Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Ubuntu Mono Italic Nerd Font Complete.ttf"
-    (share/"fonts").install "Ubuntu Mono Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "Ubuntu Mono Nerd Font Complete.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFont-Bold.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFont-BoldItalic.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFont-Italic.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFont-Regular.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFontMono-Bold.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFontMono-BoldItalic.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFontMono-Italic.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFontMono-Regular.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFontPropo-Bold.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFontPropo-BoldItalic.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFontPropo-Italic.ttf"
+    (share/"fonts").install "UbuntuMonoNerdFontPropo-Regular.ttf"
   end
   test do
   end
