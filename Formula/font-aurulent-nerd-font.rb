e1b95bd15895934a2392545b3cf3974fa48cf804
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aurulent-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-aurulent-nerd-font.rb
+++ Formula/font-aurulent-nerd-font.rb
@@ -1,13 +1,14 @@
 class FontAurulentNerdFont < Formula
-  version "2.3.3"
-  sha256 "963ce11f65171cc8d368c69af5ed2d1684141b35863a24f4c23f1870cc73b098"
+  version "3.0.0"
+  sha256 "5df45377ef472f0d721be28cf8c424dd38a4e1d444f868f9a471a234de6773ea"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
-  desc "AurulentSansMono Nerd Font (Aurulent Sans Mono)"
+  desc "AurulentSansM Nerd Font (Aurulent Sans Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "AurulentSansMono-Regular Nerd Font Complete Mono.otf"
-    (share/"fonts").install "AurulentSansMono-Regular Nerd Font Complete.otf"
+    (share/"fonts").install "AurulentSansMNerdFont-Regular.otf"
+    (share/"fonts").install "AurulentSansMNerdFontMono-Regular.otf"
+    (share/"fonts").install "AurulentSansMNerdFontPropo-Regular.otf"
   end
   test do
   end
