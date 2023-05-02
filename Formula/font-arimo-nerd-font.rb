46a92d932d153aa2130eedee3dfd46b11bc92593
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arimo-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-arimo-nerd-font.rb
+++ Formula/font-arimo-nerd-font.rb
@@ -1,15 +1,19 @@
 class FontArimoNerdFont < Formula
-  version "2.3.3"
-  sha256 "2e1a2ce9a02a5367b2bbaf80c1476942762f063daae6c8c2321bd0870b7cc92c"
+  version "3.0.0"
+  sha256 "14a04ecb56f9b520e97b8974a475dee1ed9fcd75d80b18b3f79ae087a5cf73aa"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Arimo.zip"
   desc "Arimo Nerd Font (Arimo)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "Arimo Bold Italic Nerd Font Complete.ttf"
-    (share/"fonts").install "Arimo Bold Nerd Font Complete.ttf"
-    (share/"fonts").install "Arimo Italic Nerd Font Complete.ttf"
-    (share/"fonts").install "Arimo Regular Nerd Font Complete.ttf"
+    (share/"fonts").install "ArimoNerdFont-Bold.ttf"
+    (share/"fonts").install "ArimoNerdFont-BoldItalic.ttf"
+    (share/"fonts").install "ArimoNerdFont-Italic.ttf"
+    (share/"fonts").install "ArimoNerdFont-Regular.ttf"
+    (share/"fonts").install "ArimoNerdFontPropo-Bold.ttf"
+    (share/"fonts").install "ArimoNerdFontPropo-BoldItalic.ttf"
+    (share/"fonts").install "ArimoNerdFontPropo-Italic.ttf"
+    (share/"fonts").install "ArimoNerdFontPropo-Regular.ttf"
   end
   test do
   end
