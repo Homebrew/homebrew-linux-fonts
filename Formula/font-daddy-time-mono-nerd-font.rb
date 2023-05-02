d572455179de781c48811d86bebf6eb47ec26c2d
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-02 00:06:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-daddy-time-mono-nerd-font.rb from 516018032c03e9ae1f8c3e57604c5592fe2a6104

--- Formula/font-daddy-time-mono-nerd-font.rb
+++ Formula/font-daddy-time-mono-nerd-font.rb
@@ -1,13 +1,14 @@
 class FontDaddyTimeMonoNerdFont < Formula
-  version "2.3.3"
-  sha256 "8d050b0f96e218fd706fdea6ee773754f147b9120cd1bac057efe1dbc9a20563"
+  version "3.0.0"
+  sha256 "2d5df5e3e05454b65c81f40322fb7392b6f18e1f8e6f2f65380a5575e592da20"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DaddyTimeMono.zip"
   desc "DaddyTimeMono Nerd Font (DaddyTimeMono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "DaddyTimeMono Nerd Font Complete Mono.ttf"
-    (share/"fonts").install "DaddyTimeMono Nerd Font Complete.ttf"
+    (share/"fonts").install "DaddyTimeMonoNerdFont-Regular.ttf"
+    (share/"fonts").install "DaddyTimeMonoNerdFontMono-Regular.ttf"
+    (share/"fonts").install "DaddyTimeMonoNerdFontPropo-Regular.ttf"
   end
   test do
   end
