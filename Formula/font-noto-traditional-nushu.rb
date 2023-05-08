1d73e885ead38233f8102a43270ae8192999f3b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-08 00:05:50 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-traditional-nushu.rb from 4daf96a88baacf5048d6309481570226745b5351

--- Formula/font-noto-traditional-nushu.rb
+++ Formula/font-noto-traditional-nushu.rb
@@ -1,9 +1,9 @@
 class FontNotoTraditionalNushu < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nototraditionalnushu/NotoTraditionalNushu-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/nototraditionalnushu/NotoTraditionalNushu%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Traditional Nushu"
   homepage "https://fonts.google.com/specimen/Noto+Traditional+Nushu"
   def install
-    (share/"fonts").install "NotoTraditionalNushu-Regular.ttf"
+    (share/"fonts").install "NotoTraditionalNushu[wght].ttf"
   end
   test do
   end
