7986c691c295b92cfded99e9f313a6358be93690
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-14 00:06:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-go-nerd-font.rb from 1003d5f1b78b68ae8d192ac50cfef5848a33ad51

--- Formula/font-inconsolata-go-nerd-font.rb
+++ Formula/font-inconsolata-go-nerd-font.rb
@@ -1,18 +1,1 @@
-class FontInconsolataGoNerdFont < Formula
-  version "3.0.0"
-  sha256 "e39771fe2e9fe279511e0369d3b64b1c700d9391c8cc084db17878b512d69c8e"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataGo.zip"
-  desc "InconsolataGo Nerd Font (InconsolataGo)"
-  desc "Developer targeted fonts with a high number of glyphs"
-  homepage "https://github.com/ryanoasis/nerd-fonts"
-  def install
-    (share/"fonts").install "InconsolataGoNerdFont-Bold.ttf"
-    (share/"fonts").install "InconsolataGoNerdFont-Regular.ttf"
-    (share/"fonts").install "InconsolataGoNerdFontMono-Bold.ttf"
-    (share/"fonts").install "InconsolataGoNerdFontMono-Regular.ttf"
-    (share/"fonts").install "InconsolataGoNerdFontPropo-Bold.ttf"
-    (share/"fonts").install "InconsolataGoNerdFontPropo-Regular.ttf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-inconsolata-go-nerd-font\"", :before=>["version \"3.0.1\""@42, "sha256 \"e85ca68ddd37031760ec8f9b46d0cf3d803d967d99d9b51e31df30d84ee74ea2\""@60, "url \"https://github.com/ryanoasis/nerd-fonts/releases/download/v\#{version}/InconsolataGo.zip\""@137, "desc \"InconsolataGo Nerd Font (InconsolataGo)\"", "desc \"Developer targeted fonts with a high number of glyphs\""@282, "homepage \"https://github.com/ryanoasis/nerd-fonts\""@345, ""], :fonts=>["(share/\"fonts\").install \"InconsolataGoNerdFont-Bold.ttf\"", "(share/\"fonts\").install \"InconsolataGoNerdFont-Regular.ttf\"", "(share/\"fonts\").install \"InconsolataGoNerdFontMono-Bold.ttf\"", "(share/\"fonts\").install \"InconsolataGoNerdFontMono-Regular.ttf\"", "(share/\"fonts\").install \"InconsolataGoNerdFontPropo-Bold.ttf\"", "(share/\"fonts\").install \"InconsolataGoNerdFontPropo-Regular.ttf\""], :after=>"# No zap stanza required\n"@730}
