a409e98f8b17726053a358be21302aa9b65976fb
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-14 00:06:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-hackgen-nerd.rb from d8ff85379a4a9ea59cc9d7cf6dc604b41f2381e7

--- Formula/font-hackgen-nerd.rb
+++ Formula/font-hackgen-nerd.rb
@@ -1,6 +1,6 @@
 class FontHackgenNerd < Formula
-  version "2.8.0"
-  sha256 "25cdd1487cc46df384d32c041303b4a04541fd61d0b973e8bb38fd1a3de65eb1"
+  version "2.9.0"
+  sha256 "277cb874bbcf8a884e512bb2a01d62341d57286d8947057d652fab6488fc941c"
   url "https://github.com/yuru7/HackGen/releases/download/v#{version}/HackGen_NF_v#{version}.zip"
   desc "HackGenNerd"
   desc "Japanese programming font which is a composed of Hack and GenJyuu-Gothic"
@@ -9,12 +9,8 @@ class FontHackgenNerd < Formula
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGen35ConsoleNF-Bold.ttf"
     (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGen35ConsoleNF-Regular.ttf"
-    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGen35ConsoleNFJ-Bold.ttf"
-    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGen35ConsoleNFJ-Regular.ttf"
     (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGenConsoleNF-Bold.ttf"
     (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGenConsoleNF-Regular.ttf"
-    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGenConsoleNFJ-Bold.ttf"
-    (share/"fonts").install "#{parent}HackGen_NF_v#{version}/HackGenConsoleNFJ-Regular.ttf"
   end
   test do
   end
