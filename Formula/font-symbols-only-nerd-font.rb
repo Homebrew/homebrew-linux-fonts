6c0852276ffe27ed56edf77395860ce8c3588d10
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-14 00:06:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-symbols-only-nerd-font.rb from 1003d5f1b78b68ae8d192ac50cfef5848a33ad51

--- Formula/font-symbols-only-nerd-font.rb
+++ Formula/font-symbols-only-nerd-font.rb
@@ -1,14 +1,1 @@
-class FontSymbolsOnlyNerdFont < Formula
-  version "3.0.0"
-  sha256 "91c0bbb8690d978cabbd693ab1e4df18d2f6d021e7f05fd7fd853fed4d56234b"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/NerdFontsSymbolsOnly.zip"
-  desc "Symbols Nerd Font (Symbols Only)"
-  desc "Developer targeted fonts with a high number of glyphs"
-  homepage "https://github.com/ryanoasis/nerd-fonts"
-  def install
-    (share/"fonts").install "SymbolsNerdFont-Regular.ttf"
-    (share/"fonts").install "SymbolsNerdFontMono-Regular.ttf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-symbols-only-nerd-font\"", :before=>["version \"3.0.1\""@40, "sha256 \"6dcde1ec4a321e1142a75075b527c046dc288b35d4e5f991285902c9a4c04ecb\""@58, "url \"https://github.com/ryanoasis/nerd-fonts/releases/download/v\#{version}/NerdFontsSymbolsOnly.zip\""@135, "desc \"Symbols Nerd Font (Symbols Only)\"", "desc \"Developer targeted fonts with a high number of glyphs\""@280, "homepage \"https://github.com/ryanoasis/nerd-fonts\""@343, ""], :fonts=>["(share/\"fonts\").install \"SymbolsNerdFont-Regular.ttf\"", "(share/\"fonts\").install \"SymbolsNerdFontMono-Regular.ttf\""], :after=>"# No zap stanza required\n"@539}
