ef1eed314714b672eb3ae7bb741fed2f2a6279c2
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-14 00:06:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-agave-nerd-font.rb from 1003d5f1b78b68ae8d192ac50cfef5848a33ad51

--- Formula/font-agave-nerd-font.rb
+++ Formula/font-agave-nerd-font.rb
@@ -1,18 +1,1 @@
-class FontAgaveNerdFont < Formula
-  version "3.0.0"
-  sha256 "f31ad97d42848f5b293e2ba4a75f04e1bf140b4a718ba1d8a95c77e71a1323bd"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Agave.zip"
-  desc "Agave Nerd Font (Agave)"
-  desc "Developer targeted fonts with a high number of glyphs"
-  homepage "https://github.com/ryanoasis/nerd-fonts"
-  def install
-    (share/"fonts").install "AgaveNerdFont-Bold.ttf"
-    (share/"fonts").install "AgaveNerdFont-Regular.ttf"
-    (share/"fonts").install "AgaveNerdFontMono-Bold.ttf"
-    (share/"fonts").install "AgaveNerdFontMono-Regular.ttf"
-    (share/"fonts").install "AgaveNerdFontPropo-Bold.ttf"
-    (share/"fonts").install "AgaveNerdFontPropo-Regular.ttf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-agave-nerd-font\"", :before=>["version \"3.0.1\""@33, "sha256 \"3dcab8cedf34e02441b86be44551197e31bb3c2a073892c12c973d5f5ae06b69\""@51, "url \"https://github.com/ryanoasis/nerd-fonts/releases/download/v\#{version}/Agave.zip\""@128, "desc \"Agave Nerd Font (Agave)\"", "desc \"Developer targeted fonts with a high number of glyphs\""@249, "homepage \"https://github.com/ryanoasis/nerd-fonts\""@312, ""], :fonts=>["(share/\"fonts\").install \"AgaveNerdFont-Bold.ttf\"", "(share/\"fonts\").install \"AgaveNerdFont-Regular.ttf\"", "(share/\"fonts\").install \"AgaveNerdFontMono-Bold.ttf\"", "(share/\"fonts\").install \"AgaveNerdFontMono-Regular.ttf\"", "(share/\"fonts\").install \"AgaveNerdFontPropo-Bold.ttf\"", "(share/\"fonts\").install \"AgaveNerdFontPropo-Regular.ttf\""], :after=>"# No zap stanza required\n"@649}
