9193f1f88080b12ad8b3374b9960b4efee43b934
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-14 00:06:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bigblue-terminal-nerd-font.rb from 1003d5f1b78b68ae8d192ac50cfef5848a33ad51

--- Formula/font-bigblue-terminal-nerd-font.rb
+++ Formula/font-bigblue-terminal-nerd-font.rb
@@ -1,18 +1,1 @@
-class FontBigblueTerminalNerdFont < Formula
-  version "3.0.0"
-  sha256 "d017dbab1c1a79c2e9c7a79e494c66ae6fd8a18532caf3217de6e6e5ab45e344"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BigBlueTerminal.zip"
-  desc "BigBlueTerm Nerd Font families (BigBlue Terminal)"
-  desc "Developer targeted fonts with a high number of glyphs"
-  homepage "https://github.com/ryanoasis/nerd-fonts"
-  def install
-    (share/"fonts").install "BigBlueTerm437NerdFont-Regular.ttf"
-    (share/"fonts").install "BigBlueTerm437NerdFontMono-Regular.ttf"
-    (share/"fonts").install "BigBlueTerm437NerdFontPropo-Regular.ttf"
-    (share/"fonts").install "BigBlueTermPlusNerdFont-Regular.ttf"
-    (share/"fonts").install "BigBlueTermPlusNerdFontMono-Regular.ttf"
-    (share/"fonts").install "BigBlueTermPlusNerdFontPropo-Regular.ttf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-bigblue-terminal-nerd-font\"", :before=>["version \"3.0.1\""@44, "sha256 \"fba95367888c4acaeaf6ba705bde59bb535d3cc897f9f86f9e961f08723d289e\""@62, "url \"https://github.com/ryanoasis/nerd-fonts/releases/download/v\#{version}/BigBlueTerminal.zip\""@139, "desc \"BigBlueTerm Nerd Font families (BigBlue Terminal)\"", "desc \"Developer targeted fonts with a high number of glyphs\""@296, "homepage \"https://github.com/ryanoasis/nerd-fonts\""@359, ""], :fonts=>["(share/\"fonts\").install \"BigBlueTerm437NerdFont-Regular.ttf\"", "(share/\"fonts\").install \"BigBlueTerm437NerdFontMono-Regular.ttf\"", "(share/\"fonts\").install \"BigBlueTerm437NerdFontPropo-Regular.ttf\"", "(share/\"fonts\").install \"BigBlueTermPlusNerdFont-Regular.ttf\"", "(share/\"fonts\").install \"BigBlueTermPlusNerdFontMono-Regular.ttf\"", "(share/\"fonts\").install \"BigBlueTermPlusNerdFontPropo-Regular.ttf\""], :after=>"# No zap stanza required\n"@762}
