5d81b1996cc29d72e8cd0b02c1362ed7cc1ab1cf
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-14 00:06:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-aurulent-nerd-font.rb from 1003d5f1b78b68ae8d192ac50cfef5848a33ad51

--- Formula/font-aurulent-nerd-font.rb
+++ Formula/font-aurulent-nerd-font.rb
@@ -1,15 +1,1 @@
-class FontAurulentNerdFont < Formula
-  version "3.0.0"
-  sha256 "5df45377ef472f0d721be28cf8c424dd38a4e1d444f868f9a471a234de6773ea"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
-  desc "AurulentSansM Nerd Font (Aurulent Sans Mono)"
-  desc "Developer targeted fonts with a high number of glyphs"
-  homepage "https://github.com/ryanoasis/nerd-fonts"
-  def install
-    (share/"fonts").install "AurulentSansMNerdFont-Regular.otf"
-    (share/"fonts").install "AurulentSansMNerdFontMono-Regular.otf"
-    (share/"fonts").install "AurulentSansMNerdFontPropo-Regular.otf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-aurulent-nerd-font\"", :before=>["version \"3.0.1\""@36, "sha256 \"3594eb6f674bd5d980454b46510de9efdf9ad6ff587fb10b0b8ca13f82a6df79\""@54, "url \"https://github.com/ryanoasis/nerd-fonts/releases/download/v\#{version}/AurulentSansMono.zip\""@131, "desc \"AurulentSansM Nerd Font (Aurulent Sans Mono)\"", "desc \"Developer targeted fonts with a high number of glyphs\""@284, "homepage \"https://github.com/ryanoasis/nerd-fonts\""@347, ""], :fonts=>["(share/\"fonts\").install \"AurulentSansMNerdFont-Regular.otf\"", "(share/\"fonts\").install \"AurulentSansMNerdFontMono-Regular.otf\"", "(share/\"fonts\").install \"AurulentSansMNerdFontPropo-Regular.otf\""], :after=>"# No zap stanza required\n"@603}
