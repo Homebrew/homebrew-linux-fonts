3bcb37015fef2554a3098ff04191531c6331498d
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-14 00:06:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-droid-sans-mono-nerd-font.rb from 1003d5f1b78b68ae8d192ac50cfef5848a33ad51

--- Formula/font-droid-sans-mono-nerd-font.rb
+++ Formula/font-droid-sans-mono-nerd-font.rb
@@ -1,15 +1,1 @@
-class FontDroidSansMonoNerdFont < Formula
-  version "3.0.0"
-  sha256 "d186336f5a6ce7c67b9e2485d5b143e4c655ef59dcad9ee47d3fe3ff9c65b197"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DroidSansMono.zip"
-  desc "DroidSansM Nerd Font (Droid Sans Mono)"
-  desc "Developer targeted fonts with a high number of glyphs"
-  homepage "https://github.com/ryanoasis/nerd-fonts"
-  def install
-    (share/"fonts").install "DroidSansMNerdFont-Regular.otf"
-    (share/"fonts").install "DroidSansMNerdFontMono-Regular.otf"
-    (share/"fonts").install "DroidSansMNerdFontPropo-Regular.otf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-droid-sans-mono-nerd-font\"", :before=>["version \"3.0.1\""@43, "sha256 \"9c2f674b4946414c202df40544dee936ba3edfefcf086790eab3c278873a2815\""@61, "url \"https://github.com/ryanoasis/nerd-fonts/releases/download/v\#{version}/DroidSansMono.zip\""@138, "desc \"DroidSansM Nerd Font (Droid Sans Mono)\"", "desc \"Developer targeted fonts with a high number of glyphs\""@282, "homepage \"https://github.com/ryanoasis/nerd-fonts\""@345, ""], :fonts=>["(share/\"fonts\").install \"DroidSansMNerdFont-Regular.otf\"", "(share/\"fonts\").install \"DroidSansMNerdFontMono-Regular.otf\"", "(share/\"fonts\").install \"DroidSansMNerdFontPropo-Regular.otf\""], :after=>"# No zap stanza required\n"@592}
