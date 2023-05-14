ec086a417eb29c41b3db57e90450285e4c87436a
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-14 00:06:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-nerd-font.rb from 1003d5f1b78b68ae8d192ac50cfef5848a33ad51

--- Formula/font-inconsolata-nerd-font.rb
+++ Formula/font-inconsolata-nerd-font.rb
@@ -1,18 +1,1 @@
-class FontInconsolataNerdFont < Formula
-  version "3.0.0"
-  sha256 "42ba957b998fccc972d196b31f372ef5b67eab39583f9a14c8c1aec16b4ee13e"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Inconsolata.zip"
-  desc "Inconsolata Nerd Font (Inconsolata)"
-  desc "Developer targeted fonts with a high number of glyphs"
-  homepage "https://github.com/ryanoasis/nerd-fonts"
-  def install
-    (share/"fonts").install "InconsolataNerdFont-Bold.ttf"
-    (share/"fonts").install "InconsolataNerdFont-Regular.ttf"
-    (share/"fonts").install "InconsolataNerdFontMono-Bold.ttf"
-    (share/"fonts").install "InconsolataNerdFontMono-Regular.ttf"
-    (share/"fonts").install "InconsolataNerdFontPropo-Bold.ttf"
-    (share/"fonts").install "InconsolataNerdFontPropo-Regular.ttf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-inconsolata-nerd-font\"", :before=>["version \"3.0.1\""@39, "sha256 \"691498a9cfb6113d6380f66ecca9f0517e330fdc6941f39efc6ea512ee2588ba\""@57, "url \"https://github.com/ryanoasis/nerd-fonts/releases/download/v\#{version}/Inconsolata.zip\""@134, "desc \"Inconsolata Nerd Font (Inconsolata)\"", "desc \"Developer targeted fonts with a high number of glyphs\""@273, "homepage \"https://github.com/ryanoasis/nerd-fonts\""@336, ""], :fonts=>["(share/\"fonts\").install \"InconsolataNerdFont-Bold.ttf\"", "(share/\"fonts\").install \"InconsolataNerdFont-Regular.ttf\"", "(share/\"fonts\").install \"InconsolataNerdFontMono-Bold.ttf\"", "(share/\"fonts\").install \"InconsolataNerdFontMono-Regular.ttf\"", "(share/\"fonts\").install \"InconsolataNerdFontPropo-Bold.ttf\"", "(share/\"fonts\").install \"InconsolataNerdFontPropo-Regular.ttf\""], :after=>"# No zap stanza required\n"@709}
