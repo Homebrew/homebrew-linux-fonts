2810e3bbab298e130218a7e0da49fc805c6bcf3b
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-14 00:06:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-comic-shanns-mono-nerd-font.rb from 1003d5f1b78b68ae8d192ac50cfef5848a33ad51

--- Formula/font-comic-shanns-mono-nerd-font.rb
+++ Formula/font-comic-shanns-mono-nerd-font.rb
@@ -1,18 +1,1 @@
-class FontComicShannsMonoNerdFont < Formula
-  version "3.0.0"
-  sha256 "35db8f7d96b2094a211b169de020b29f1f337b5e29c649d1993b12860c48c024"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ComicShannsMono.zip"
-  desc "ComicShannsMono Nerd Font (Comic Shanns Mono)"
-  desc "Developer targeted fonts with a high number of glyphs"
-  homepage "https://github.com/ryanoasis/nerd-fonts"
-  def install
-    (share/"fonts").install "ComicShannsMonoNerdFont-Bold.otf"
-    (share/"fonts").install "ComicShannsMonoNerdFont-Regular.otf"
-    (share/"fonts").install "ComicShannsMonoNerdFontMono-Bold.otf"
-    (share/"fonts").install "ComicShannsMonoNerdFontMono-Regular.otf"
-    (share/"fonts").install "ComicShannsMonoNerdFontPropo-Bold.otf"
-    (share/"fonts").install "ComicShannsMonoNerdFontPropo-Regular.otf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-comic-shanns-mono-nerd-font\"", :before=>["version \"3.0.1\""@45, "sha256 \"df10ec6d3d3b440d7d509ecaffb953c5b36178c5b61ba972e29604e017bf5ca9\""@63, "url \"https://github.com/ryanoasis/nerd-fonts/releases/download/v\#{version}/ComicShannsMono.zip\""@140, "desc \"ComicShannsMono Nerd Font (Comic Shanns Mono)\"", "desc \"Developer targeted fonts with a high number of glyphs\""@293, "homepage \"https://github.com/ryanoasis/nerd-fonts\""@356, ""], :fonts=>["(share/\"fonts\").install \"ComicShannsMonoNerdFont-Bold.otf\"", "(share/\"fonts\").install \"ComicShannsMonoNerdFont-Regular.otf\"", "(share/\"fonts\").install \"ComicShannsMonoNerdFontMono-Bold.otf\"", "(share/\"fonts\").install \"ComicShannsMonoNerdFontMono-Regular.otf\"", "(share/\"fonts\").install \"ComicShannsMonoNerdFontPropo-Bold.otf\"", "(share/\"fonts\").install \"ComicShannsMonoNerdFontPropo-Regular.otf\""], :after=>"# No zap stanza required\n"@753}
