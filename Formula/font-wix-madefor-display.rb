cf347ab34207c22de27cd96a1bd4f2c520bf9bc8
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-15 00:06:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wix-madefor-display.rb from 05f1237ea49f7bea85cce74fe3a66c97755585f8

--- Formula/font-wix-madefor-display.rb
+++ Formula/font-wix-madefor-display.rb
@@ -1,7 +1,7 @@
 class FontWixMadeforDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/wixmadefordisplay/WixMadeforDisplay%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/wixmadefordisplay/WixMadeforDisplay%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Wix Madefor Display"
-  homepage "https://github.com/wix/wixmadefor"
+  homepage "https://fonts.google.com/specimen/Wix+Madefor+Display"
   def install
     (share/"fonts").install "WixMadeforDisplay[wght].ttf"
   end
