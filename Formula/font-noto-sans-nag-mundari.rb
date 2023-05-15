7169da53af3a6dcc5c25b4471b39421479312739
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-15 00:06:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-nag-mundari.rb from 05f1237ea49f7bea85cce74fe3a66c97755585f8

--- Formula/font-noto-sans-nag-mundari.rb
+++ Formula/font-noto-sans-nag-mundari.rb
@@ -1,8 +1,8 @@
 class FontNotoSansNagMundari < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansnagmundari/NotoSansNagMundari%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansnagmundari/NotoSansNagMundari%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans Nag Mundari"
   desc "Design for the indic nag mundari script"
-  homepage "https://github.com/notofonts/nag-mundari"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Nag+Mundari"
   def install
     (share/"fonts").install "NotoSansNagMundari[wght].ttf"
   end
