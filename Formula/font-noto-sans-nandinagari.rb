71db81430703529ec569f8b28addcc976ca11eaa
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-15 00:06:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-nandinagari.rb from 05f1237ea49f7bea85cce74fe3a66c97755585f8

--- Formula/font-noto-sans-nandinagari.rb
+++ Formula/font-noto-sans-nandinagari.rb
@@ -1,8 +1,8 @@
 class FontNotoSansNandinagari < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansnandinagari/NotoSansNandinagari-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansnandinagari/NotoSansNandinagari-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans Nandinagari"
   desc "Historical indic nandinagari script"
-  homepage "https://github.com/notofonts/nandinagari"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Nandinagari"
   def install
     (share/"fonts").install "NotoSansNandinagari-Regular.ttf"
   end
