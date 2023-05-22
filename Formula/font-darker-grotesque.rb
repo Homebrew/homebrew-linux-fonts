e23361cf1cd65c9e2b2b480cf6ae7d463dfded60
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-22 00:06:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-darker-grotesque.rb from c722bbddd22c59c372e182145f00a17019a0b3e3

--- Formula/font-darker-grotesque.rb
+++ Formula/font-darker-grotesque.rb
@@ -1,16 +1,1 @@
-class FontDarkerGrotesque < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/darkergrotesque"
-  desc "Darker Grotesque"
-  homepage "https://fonts.google.com/specimen/Darker+Grotesque"
-  def install
-    (share/"fonts").install "DarkerGrotesque-Black.ttf"
-    (share/"fonts").install "DarkerGrotesque-Bold.ttf"
-    (share/"fonts").install "DarkerGrotesque-ExtraBold.ttf"
-    (share/"fonts").install "DarkerGrotesque-Light.ttf"
-    (share/"fonts").install "DarkerGrotesque-Medium.ttf"
-    (share/"fonts").install "DarkerGrotesque-Regular.ttf"
-    (share/"fonts").install "DarkerGrotesque-SemiBold.ttf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-darker-grotesque\"", :before=>["", "", "head \"https://github.com/google/fonts/raw/main/ofl/darkergrotesque/DarkerGrotesque%5Bwght%5D.ttf\", verified: \"github.com/google/fonts/\"", "desc \"Darker Grotesque\"", "homepage \"https://fonts.google.com/specimen/Darker+Grotesque\""@241], :fonts=>["(share/\"fonts\").install \"DarkerGrotesque[wght].ttf\""], :after=>"# No zap stanza required\n"@342}
