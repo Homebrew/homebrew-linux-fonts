e79f0134f280ca0f1e8ef37ee77d19cf8c74c7a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-25 00:06:25 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gnu-unifont.rb from 32600bb67a8c78b82be0f95d5de3f8ec4b801ae0

--- Formula/font-gnu-unifont.rb
+++ Formula/font-gnu-unifont.rb
@@ -1,18 +1,1 @@
-class FontGnuUnifont < Formula
-  version "15.0.01"
-  sha256 "7d11a924bf3c63ea7fdf2da2b96d6d4986435bedfd1e6816c8ac2e6db47634d5"
-  url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.tar.gz", verified: "ftpmirror.gnu.org/unifont/"
-  desc "GNU Unifont"
-  desc "Unicode bitmap font"
-  homepage "https://unifoundry.com/unifont.html"
-  def install
-    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont-#{version}.ttf"
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_upper-#{version}.ttf"
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_sample-#{version}.ttf"
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_jp-#{version}.ttf"
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_csur-#{version}.ttf"
-  end
-  test do
-  end
-end
+{:name=>"\"font-gnu-unifont\"", :before=>["version \"15.0.03\""@29, "sha256 \"734a4f9f583b5184eb240a49e6bf768785168d1ebb443dc39b42d15c0bd2c767\""@49, "url \"https://ftpmirror.gnu.org/unifont/unifont-\#{version}/unifont-\#{version}.tar.gz\", verified: \"ftpmirror.gnu.org/unifont/\""@126, "desc \"GNU Unifont\"", "desc \"Unicode bitmap font\""@280, "homepage \"https://unifoundry.com/unifont.html\""@309, ""], :fonts=>["(share/\"fonts\").install \"\#{parent}unifont-\#{version}/font/precompiled/unifont-\#{version}.ttf\"", "(share/\"fonts\").install \"\#{parent}unifont-\#{version}/font/precompiled/unifont_upper-\#{version}.ttf\"", "(share/\"fonts\").install \"\#{parent}unifont-\#{version}/font/precompiled/unifont_sample-\#{version}.ttf\"", "(share/\"fonts\").install \"\#{parent}unifont-\#{version}/font/precompiled/unifont_jp-\#{version}.ttf\"", "(share/\"fonts\").install \"\#{parent}unifont-\#{version}/font/precompiled/unifont_csur-\#{version}.ttf\""], :after=>"# No zap stanza required\n"@853}
