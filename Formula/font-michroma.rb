3a955d8e0e7551617972c723e1ee97a07a6cdc01
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-29 00:06:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-michroma.rb from 709ffe99c5e60ddc79caeddc631fbf231eafbf09

--- Formula/font-michroma.rb
+++ Formula/font-michroma.rb
@@ -1,9 +1,9 @@
 class FontMichroma < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/michroma/Michroma.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/michroma/Michroma-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Michroma"
   homepage "https://fonts.google.com/specimen/Michroma"
   def install
-    (share/"fonts").install "Michroma.ttf"
+    (share/"fonts").install "Michroma-Regular.ttf"
   end
   test do
   end
