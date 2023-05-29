f26d7c2a1967431ea0a46fdbeb4cae1e7f40b117
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-05-29 00:06:18 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-chorasmian.rb from 709ffe99c5e60ddc79caeddc631fbf231eafbf09

--- Formula/font-noto-sans-chorasmian.rb
+++ Formula/font-noto-sans-chorasmian.rb
@@ -1,8 +1,8 @@
 class FontNotoSansChorasmian < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanschorasmian/NotoSansChorasmian-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanschorasmian/NotoSansChorasmian-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans Chorasmian"
   desc "Design for the historical middle eastern chorasmian script"
-  homepage "https://github.com/notofonts/chorasmian"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Chorasmian"
   def install
     (share/"fonts").install "NotoSansChorasmian-Regular.ttf"
   end
