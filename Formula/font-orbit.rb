e41acb41eb154d5f3faac876a1b7c02e2183e471
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-05 00:06:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-orbit.rb from d5e678eb87029587304b2cd12518df995bad2db4

--- Formula/font-orbit.rb
+++ Formula/font-orbit.rb
@@ -1,1 +1,11 @@
-{:name=>"\"font-orbit\"", :before=>["", "", "head \"https://github.com/google/fonts/raw/main/ofl/orbit/Orbit-Regular.ttf\"", "desc \"Orbit\"", "desc \"Recommended to use bright writing on a dark background, below 10pt\""@153, "homepage \"https://github.com/JAMO-TYPEFACE/Orbit\""@229], :fonts=>["(share/\"fonts\").install \"Orbit-Regular.ttf\""], :after=>"# No zap stanza required\n"@310}
+class FontOrbit < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/orbit/Orbit-Regular.ttf"
+  desc "Orbit"
+  desc "Recommended to use bright writing on a dark background, below 10pt"
+  homepage "https://github.com/JAMO-TYPEFACE/Orbit"
+  def install
+    (share/"fonts").install "Orbit-Regular.ttf"
+  end
+  test do
+  end
+end
