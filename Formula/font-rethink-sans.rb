0a8ca26a2a2a5337bc6e4567fbd9e3eb8cc44c1d
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-07 00:06:24 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-rethink-sans.rb from bad6c4c34e4435d58fb9e68d5fbf277a56ae095b

--- Formula/font-rethink-sans.rb
+++ Formula/font-rethink-sans.rb
@@ -1,1 +1,13 @@
-{:name=>"\"font-rethink-sans\"", :before=>["", "", "head \"https://github.com/google/fonts.git\", branch: \"main\", only_path: \"ofl/rethinksans\"", "desc \"Rethink Sans\"", "homepage \"https://github.com/hans-thiessen/Rethink-Sans\""@195], :fonts=>["(share/\"fonts\").install \"RethinkSans-Italic[wght].ttf\"", "(share/\"fonts\").install \"RethinkSans[wght].ttf\""], :after=>"# No zap stanza required\n"@325}
+class FontRethinkSans < Formula
+  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/rethinksans"
+  desc "Rethink Sans"
+  homepage "https://github.com/hans-thiessen/Rethink-Sans"
+  def install
+    (share/"fonts").install "RethinkSans-Italic[wght].ttf"
+    (share/"fonts").install "RethinkSans[wght].ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
