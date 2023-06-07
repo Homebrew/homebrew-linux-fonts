001c99b5d6f26d05391f0150eb15b7b7a784449d
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-07 00:06:23 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gohufont-nerd-font.rb from 89073929da854096ec214d1b080bc7c375725a8b

--- Formula/font-gohufont-nerd-font.rb
+++ Formula/font-gohufont-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontGohufontNerdFont < Formula
-  version "3.0.1"
-  sha256 "d2a9e19555a043cd0cbd9f1e9e3274824a3442417b7434a4ad3a1c964957f655"
+  version "3.0.2"
+  sha256 "9d2107306a78055eedd7633dfd66589dff95f74142380bf8c0d41dc7c6550245"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Gohu.zip"
   desc "GohuFont  Nerd Font families (Gohu)"
   desc "Developer targeted fonts with a high number of glyphs"
@@ -12,12 +12,12 @@ class FontGohufontNerdFont < Formula
     (share/"fonts").install "GohuFont14NerdFont-Regular.ttf"
     (share/"fonts").install "GohuFont14NerdFontMono-Regular.ttf"
     (share/"fonts").install "GohuFont14NerdFontPropo-Regular.ttf"
-    (share/"fonts").install "GohuFontuni-11NerdFont-Regular.ttf"
-    (share/"fonts").install "GohuFontuni-11NerdFontMono-Regular.ttf"
-    (share/"fonts").install "GohuFontuni-11NerdFontPropo-Regular.ttf"
-    (share/"fonts").install "GohuFontuni-14NerdFont-Regular.ttf"
-    (share/"fonts").install "GohuFontuni-14NerdFontMono-Regular.ttf"
-    (share/"fonts").install "GohuFontuni-14NerdFontPropo-Regular.ttf"
+    (share/"fonts").install "GohuFontuni11NerdFont-Regular.ttf"
+    (share/"fonts").install "GohuFontuni11NerdFontMono-Regular.ttf"
+    (share/"fonts").install "GohuFontuni11NerdFontPropo-Regular.ttf"
+    (share/"fonts").install "GohuFontuni14NerdFont-Regular.ttf"
+    (share/"fonts").install "GohuFontuni14NerdFontMono-Regular.ttf"
+    (share/"fonts").install "GohuFontuni14NerdFontPropo-Regular.ttf"
   end
   # No zap stanza required
 
