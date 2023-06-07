21edcc29f37a1877a2e7e9fcc552be3bcbe9a424
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-07 00:06:23 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-daddy-time-mono-nerd-font.rb from 89073929da854096ec214d1b080bc7c375725a8b

--- Formula/font-daddy-time-mono-nerd-font.rb
+++ Formula/font-daddy-time-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontDaddyTimeMonoNerdFont < Formula
-  version "3.0.1"
-  sha256 "d384b31785f635b3cdb744039223e4a0753850b02b849c7861d7953016a35718"
+  version "3.0.2"
+  sha256 "51c85f0fbe2089e844570bd0c7c9eefed5f0dfd2b4183bfc6da87cfb655085f0"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DaddyTimeMono.zip"
   desc "DaddyTimeMono Nerd Font (DaddyTimeMono)"
   desc "Developer targeted fonts with a high number of glyphs"
