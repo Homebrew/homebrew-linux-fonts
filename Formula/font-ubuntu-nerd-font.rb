694db3c55995f0228252f8731eaf7896b5aa9495
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-07 00:06:24 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu-nerd-font.rb from 89073929da854096ec214d1b080bc7c375725a8b

--- Formula/font-ubuntu-nerd-font.rb
+++ Formula/font-ubuntu-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontUbuntuNerdFont < Formula
-  version "3.0.1"
-  sha256 "3f0b17b0a156dd7d271f9477cbdc9cf9eb12c3fa2648b9690637d8fc0b6e4361"
+  version "3.0.2"
+  sha256 "cb1b134ffd67c484a12d963aaa87dd0c3d8234ce62877c49716767abef0d925c"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Ubuntu.zip"
   desc "Ubuntu Nerd Font (Ubuntu)"
   desc "Developer targeted fonts with a high number of glyphs"
