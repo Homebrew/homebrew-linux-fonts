6cbb9c516b1c76ba39964ad891a62974c56a5e58
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-12 00:06:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-orbit.rb from 8f8b238109f9d694c0c593b52e1d0b327c9939d7

--- Formula/font-orbit.rb
+++ Formula/font-orbit.rb
@@ -1,8 +1,8 @@
 class FontOrbit < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/orbit/Orbit-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/orbit/Orbit-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Orbit"
   desc "Recommended to use bright writing on a dark background, below 10pt"
-  homepage "https://github.com/JAMO-TYPEFACE/Orbit"
+  homepage "https://fonts.google.com/specimen/Orbit"
   def install
     (share/"fonts").install "Orbit-Regular.ttf"
   end
