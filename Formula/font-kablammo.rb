7114a6a82b3b2c6ffbf695ba6375550b8c9d1fa6
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-12 00:06:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-kablammo.rb from 8f8b238109f9d694c0c593b52e1d0b327c9939d7

--- Formula/font-kablammo.rb
+++ Formula/font-kablammo.rb
@@ -1,7 +1,7 @@
 class FontKablammo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kablammo/Kablammo%5BMORF%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/kablammo/Kablammo%5BMORF%5D.ttf", verified: "github.com/google/fonts/"
   desc "Kablammo"
-  homepage "https://github.com/Vectro-Type-Foundry/kablammo"
+  homepage "https://fonts.google.com/specimen/Kablammo"
   def install
     (share/"fonts").install "Kablammo[MORF].ttf"
   end
