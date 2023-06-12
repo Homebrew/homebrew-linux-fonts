313cd486668a980c44b4de22281d1581cf53aa29
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-12 00:06:33 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-ottoman-siyaq.rb from 225ff8b644adf07f056da3f92ae5283a8dd8d8eb

---
+++ Formula/font-noto-serif-ottoman-siyaq.rb
@@ -0,0 +1,13 @@
+class FontNotoSerifOttomanSiyaq < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifottomansiyaq/NotoSerifOttomanSiyaq-Regular.ttf"
+  desc "Noto Serif Ottoman Siyaq"
+  desc "Modulated (“serif”) design for the arabic form of the siyaq numeral system"
+  homepage "https://github.com/notofonts/noto-project-template"
+  def install
+    (share/"fonts").install "NotoSerifOttomanSiyaq-Regular.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
