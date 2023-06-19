05f14d7b83e7b16e2fc87ea5a831ee4f525bdb95
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-19 00:06:21 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-duployan.rb from c1d811b1fa88dffdac36ceeee0e68a371d2db4b4

--- Formula/font-noto-sans-duployan.rb
+++ Formula/font-noto-sans-duployan.rb
@@ -1,8 +1,9 @@
 class FontNotoSansDuployan < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansduployan/NotoSansDuployan-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosansduployan"
   desc "Noto Sans Duployan"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Duployan"
   def install
+    (share/"fonts").install "NotoSansDuployan-Bold.ttf"
     (share/"fonts").install "NotoSansDuployan-Regular.ttf"
   end
   test do
