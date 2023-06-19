c6990fd6c9174854a337e60b4bbc8ab0c29042af
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-19 00:06:21 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-gunjala-gondi.rb from c1d811b1fa88dffdac36ceeee0e68a371d2db4b4

--- Formula/font-noto-sans-gunjala-gondi.rb
+++ Formula/font-noto-sans-gunjala-gondi.rb
@@ -1,9 +1,9 @@
 class FontNotoSansGunjalaGondi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansgunjalagondi/NotoSansGunjalaGondi-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansgunjalagondi/NotoSansGunjalaGondi%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans Gunjala Gondi"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Gunjala+Gondi"
   def install
-    (share/"fonts").install "NotoSansGunjalaGondi-Regular.ttf"
+    (share/"fonts").install "NotoSansGunjalaGondi[wght].ttf"
   end
   test do
   end
