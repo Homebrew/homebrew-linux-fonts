11f184c9530cd874e397ba9fc6d60c82f815446c
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-22 00:06:22 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-petemoss.rb from 7cd09a58fbf735c434b1466be6434f357ba16269

--- Formula/font-petemoss.rb
+++ Formula/font-petemoss.rb
@@ -1,7 +1,7 @@
 class FontPetemoss < Formula
   head "https://github.com/google/fonts/raw/main/ofl/petemoss/Petemoss-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Petemoss"
-  desc "Inspired by the forms created using a pentel™ color brush"
+  desc "Inspired by the forms created using a pentel color brush"
   homepage "https://fonts.google.com/specimen/Petemoss"
   def install
     (share/"fonts").install "Petemoss-Regular.ttf"
