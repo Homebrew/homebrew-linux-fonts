5c7f1f937135502a08fb1232e44f5c84378d9eaa
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-23 00:07:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iansui.rb from 2864ccf3f65ed752b0cadc530b8a8f9c889741d1

--- Formula/font-iansui.rb
+++ Formula/font-iansui.rb
@@ -1,12 +1,12 @@
 class FontIansui < Formula
-  version "0.943"
-  sha256 "d8157b39d10b3f4dbc8b09d240824f81ebb61d41e4bcbc283f249d9fdac5e4b3"
+  version "1.000"
+  sha256 "f58add84ad60d5bbeec4b1e7b988f0fe19ec980d953cf7e67c8f2ea783e2fe38"
   url "https://github.com/ButTaiwan/iansui/releases/download/v#{version}/iansui.zip"
   desc "iansui"
   desc "Chinese font derived from Klee One"
   homepage "https://github.com/ButTaiwan/iansui"
   def install
-    (share/"fonts").install "Iansui#{version.to_s.gsub(".", "")[0, 3]}-Regular.ttf"
+    (share/"fonts").install "Iansui-Regular.ttf"
   end
   test do
   end
