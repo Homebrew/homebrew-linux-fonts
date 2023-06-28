0bd8734396bae8f35ff3fe9917e947cfb4929e89
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-28 00:07:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lxgw-fasmart-gothic.rb from f3b19725111ea6a64010c326cef4015fee4e85ad

---
+++ Formula/font-lxgw-fasmart-gothic.rb
@@ -0,0 +1,16 @@
+class FontLxgwFasmartGothic < Formula
+  version "1.102"
+  sha256 "f715cfc97808cca55d21c75a23170e1c8b1f6d79fef2f1613495b5b1433a78cf"
+  url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWFasmartGothic.ttf"
+  desc "LXGW Fasmart Gothic"
+  desc "霞鹜尚智黑"
+  desc "Chinese sans-serif font derived from IPAex Gothic"
+  homepage "https://github.com/lxgw/LxgwNeoXiHei"
+  def install
+    (share/"fonts").install "LXGWFasmartGothic.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
