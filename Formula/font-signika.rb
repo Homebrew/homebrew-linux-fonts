394fbe7f8b81c05d1a45dbc59d692d7f45e0bcd9
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-03 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-signika.rb from aadece9db2a204dfb96217a12da841ef2eba3eeb

--- Formula/font-signika.rb
+++ Formula/font-signika.rb
@@ -1,11 +1,13 @@
 class FontSignika < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/signika/Signika%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/signika/Signika%5BGRAD%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Signika"
   desc "Sans-serif signage typeface with a gentle character"
   homepage "https://fonts.google.com/specimen/Signika"
   def install
-    (share/"fonts").install "Signika[wght].ttf"
+    (share/"fonts").install "Signika[GRAD,wght].ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
