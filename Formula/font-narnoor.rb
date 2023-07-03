3d6981fceed1863fbdbfc039301686b4c57ba55e
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-03 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-narnoor.rb from f3788069470d51259f83716c50e5f9a4e1889777

--- Formula/font-narnoor.rb
+++ Formula/font-narnoor.rb
@@ -1,11 +1,13 @@
 class FontNarnoor < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/narnoor/Narnoor-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/narnoor/Narnoor-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Narnoor"
   desc "Actively being revived"
-  homepage "https://github.com/silnrsi/font-narnoor"
+  homepage "https://fonts.google.com/specimen/Narnoor"
   def install
     (share/"fonts").install "Narnoor-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
