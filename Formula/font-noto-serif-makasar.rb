4fc506f07fe091c370c239d085e44d0c50994dd0
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-03 00:07:17 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-makasar.rb from 256ab9e09675591b560cce10f63f1f55ada6405a

--- Formula/font-noto-serif-makasar.rb
+++ Formula/font-noto-serif-makasar.rb
@@ -1,11 +1,13 @@
 class FontNotoSerifMakasar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifmakasar/NotoSerifMakasar-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifmakasar/NotoSerifMakasar-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Noto Serif Makasar"
   desc "Design for the historical southeast asian makasar script"
-  homepage "https://github.com/notofonts/makasar"
+  homepage "https://fonts.google.com/specimen/Noto+Serif+Makasar"
   def install
     (share/"fonts").install "NotoSerifMakasar-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
