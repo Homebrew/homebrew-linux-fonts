3837b11cdf19079223e4316000f6bd6eb5c57b42
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-courier-prime-medium-and-semi-bold.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-courier-prime-medium-and-semi-bold.rb
+++ Formula/font-courier-prime-medium-and-semi-bold.rb
@@ -6,6 +6,8 @@ class FontCourierPrimeMediumAndSemiBold < Formula
     (share/"fonts").install "CourierPrime-Medium.ttf"
     (share/"fonts").install "CourierPrime-SemiBold.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
