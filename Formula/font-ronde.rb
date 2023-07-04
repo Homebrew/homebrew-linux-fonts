27f9543520378c441627100225890370b180b3c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:08:03 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ronde.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-ronde.rb
+++ Formula/font-ronde.rb
@@ -7,6 +7,8 @@ class FontRonde < Formula
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}ronde_square/Ronde-B_square.otf"
   end
+  # No zap stanza required
+
   test do
   end
 end
