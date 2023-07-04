e5bbb3619eced490d823d7fde095e5971d021201
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-big-shoulders-stencil-text.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-big-shoulders-stencil-text.rb
+++ Formula/font-big-shoulders-stencil-text.rb
@@ -6,6 +6,8 @@ class FontBigShouldersStencilText < Formula
   def install
     (share/"fonts").install "BigShouldersStencilText[wght].ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
