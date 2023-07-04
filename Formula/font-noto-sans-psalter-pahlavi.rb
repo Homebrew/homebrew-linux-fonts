577e29555a7f6ed27bc44957e56f27b0be3369ef
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:54 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-psalter-pahlavi.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-noto-sans-psalter-pahlavi.rb
+++ Formula/font-noto-sans-psalter-pahlavi.rb
@@ -5,6 +5,8 @@ class FontNotoSansPsalterPahlavi < Formula
   def install
     (share/"fonts").install "NotoSansPsalterPahlavi-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
