6558af645033a094f008471db2a36d659e94e5a6
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:56 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-gujarati.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-noto-serif-gujarati.rb
+++ Formula/font-noto-serif-gujarati.rb
@@ -6,6 +6,8 @@ class FontNotoSerifGujarati < Formula
     (share/"fonts").install "NotoSerifGujarati-Bold.ttf"
     (share/"fonts").install "NotoSerifGujarati-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
