0986a64223cd683f361e2070263d3e60b8c8a53c
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:08:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-silkscreen.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-silkscreen.rb
+++ Formula/font-silkscreen.rb
@@ -7,6 +7,8 @@ class FontSilkscreen < Formula
     (share/"fonts").install "Silkscreen-Bold.ttf"
     (share/"fonts").install "Silkscreen-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
