dbd345170a9abb113d1b8640191ee11c75892f67
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-caucasian-albanian.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-noto-sans-caucasian-albanian.rb
+++ Formula/font-noto-sans-caucasian-albanian.rb
@@ -5,6 +5,8 @@ class FontNotoSansCaucasianAlbanian < Formula
   def install
     (share/"fonts").install "NotoSansCaucasianAlbanian-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
