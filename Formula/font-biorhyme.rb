da95328111c822d9d247a0807036e7a9ef9eef4a
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:21 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-biorhyme.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-biorhyme.rb
+++ Formula/font-biorhyme.rb
@@ -10,6 +10,8 @@ class FontBiorhyme < Formula
     (share/"fonts").install "BioRhyme-Light.ttf"
     (share/"fonts").install "BioRhyme-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
