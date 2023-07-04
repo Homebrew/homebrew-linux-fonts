0643a1433170e8ff7c4282d00510428997244c73
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-nobile.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-nobile.rb
+++ Formula/font-nobile.rb
@@ -10,6 +10,8 @@ class FontNobile < Formula
     (share/"fonts").install "Nobile-MediumItalic.ttf"
     (share/"fonts").install "Nobile-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
