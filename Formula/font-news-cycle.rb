4f9bd6a61f005e775b69bbb18469786057107a3a
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:49 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-news-cycle.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-news-cycle.rb
+++ Formula/font-news-cycle.rb
@@ -6,6 +6,8 @@ class FontNewsCycle < Formula
     (share/"fonts").install "NewsCycle-Bold.ttf"
     (share/"fonts").install "NewsCycle-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
