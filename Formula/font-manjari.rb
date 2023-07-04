6dc503ba0db0211ec1bfe16e35eb129b61bd0b3c
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-manjari.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-manjari.rb
+++ Formula/font-manjari.rb
@@ -7,6 +7,8 @@ class FontManjari < Formula
     (share/"fonts").install "Manjari-Regular.ttf"
     (share/"fonts").install "Manjari-Thin.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
