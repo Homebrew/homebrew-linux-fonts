d95bd3b5038595f93a65ac45155332fcf9ec0b5f
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-egyptian-hieroglyphs.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-noto-sans-egyptian-hieroglyphs.rb
+++ Formula/font-noto-sans-egyptian-hieroglyphs.rb
@@ -5,6 +5,8 @@ class FontNotoSansEgyptianHieroglyphs < Formula
   def install
     (share/"fonts").install "NotoSansEgyptianHieroglyphs-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
