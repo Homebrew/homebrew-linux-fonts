bfac3ec9e819d1fd5ab5b74059e340c4b6ccbe21
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:08:07 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sofia-sans-semi-condensed.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-sofia-sans-semi-condensed.rb
+++ Formula/font-sofia-sans-semi-condensed.rb
@@ -6,6 +6,8 @@ class FontSofiaSansSemiCondensed < Formula
     (share/"fonts").install "SofiaSansSemiCondensed-Italic[wght].ttf"
     (share/"fonts").install "SofiaSansSemiCondensed[wght].ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
