acbf3d3eeb75f48b0f4f8f6ccfd309319933bce3
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-krub.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-krub.rb
+++ Formula/font-krub.rb
@@ -16,6 +16,8 @@ class FontKrub < Formula
     (share/"fonts").install "Krub-SemiBold.ttf"
     (share/"fonts").install "Krub-SemiBoldItalic.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
