cf0164dae36ce7fc662f90fa2fd565e83c78119b
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:38 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss06.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-iosevka-ss06.rb
+++ Formula/font-iosevka-ss06.rb
@@ -16,6 +16,8 @@ class FontIosevkaSs06 < Formula
     (share/"fonts").install "iosevka-ss06-semibold.ttc"
     (share/"fonts").install "iosevka-ss06-thin.ttc"
   end
+  # No zap stanza required
+
   test do
   end
 end
