de928f584b9bc44691296b70aee1f5814898c7db
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-indic-siyaq-numbers.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-noto-sans-indic-siyaq-numbers.rb
+++ Formula/font-noto-sans-indic-siyaq-numbers.rb
@@ -5,6 +5,8 @@ class FontNotoSansIndicSiyaqNumbers < Formula
   def install
     (share/"fonts").install "NotoSansIndicSiyaqNumbers-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
