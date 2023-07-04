f87bdeb980aeac624c3a7af3cf0b1a1142eb9f95
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tamil-supplement.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-noto-sans-tamil-supplement.rb
+++ Formula/font-noto-sans-tamil-supplement.rb
@@ -5,6 +5,8 @@ class FontNotoSansTamilSupplement < Formula
   def install
     (share/"fonts").install "NotoSansTamilSupplement-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
