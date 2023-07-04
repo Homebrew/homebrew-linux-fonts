5a45da2c3aac08fd7655b5eb19985e272572d2d4
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:55 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-tibetan.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-noto-sans-tibetan.rb
+++ Formula/font-noto-sans-tibetan.rb
@@ -6,6 +6,8 @@ class FontNotoSansTibetan < Formula
     (share/"fonts").install "NotoSansTibetan-Bold.ttf"
     (share/"fonts").install "NotoSansTibetan-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
