0dfabe8904c7a433e262c633cd3d78224f68d29d
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:15 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-albertella.rb from f363b2a4e8c72d04e26d55f0fc2e02bebf53c48b

--- Formula/font-albertella.rb
+++ Formula/font-albertella.rb
@@ -7,6 +7,8 @@ class FontAlbertella < Formula
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}albertella/Albertella-Light.otf"
   end
+  # No zap stanza required
+
   test do
   end
 end
