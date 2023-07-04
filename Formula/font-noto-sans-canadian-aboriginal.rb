41a7c40ce16a7eaac08e19ce3243a797531db561
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:50 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-canadian-aboriginal.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-noto-sans-canadian-aboriginal.rb
+++ Formula/font-noto-sans-canadian-aboriginal.rb
@@ -5,6 +5,8 @@ class FontNotoSansCanadianAboriginal < Formula
   def install
     (share/"fonts").install "NotoSansCanadianAboriginal-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
