40ad6cacd3b077eb19d2a806fb938a1728613ec6
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:50 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-color-emoji-compat-test.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-noto-color-emoji-compat-test.rb
+++ Formula/font-noto-color-emoji-compat-test.rb
@@ -5,6 +5,8 @@ class FontNotoColorEmojiCompatTest < Formula
   def install
     (share/"fonts").install "NotoColorEmojiCompatTest-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
