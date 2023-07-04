4f53b851460886cf44c0a7bd5a7d569f3454fc68
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-04 00:07:20 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-benchnine.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-benchnine.rb
+++ Formula/font-benchnine.rb
@@ -7,6 +7,8 @@ class FontBenchnine < Formula
     (share/"fonts").install "BenchNine-Light.ttf"
     (share/"fonts").install "BenchNine-Regular.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
