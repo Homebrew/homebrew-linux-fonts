c1f283e52506c1714ed8b1e7ecf512db5a486825
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-11 00:07:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-latin-modern-math.rb from e47dab2bd3d13f1073706fb119bb6ffe684b975a

--- Formula/font-latin-modern-math.rb
+++ Formula/font-latin-modern-math.rb
@@ -1,7 +1,8 @@
 class FontLatinModernMath < Formula
-  head "http://www.gust.org.pl/projects/e-foundry/lm-math/download/latinmodern-math-1959.zip"
+  head "https://www.gust.org.pl/projects/e-foundry/lm-math/download/latinmodern-math-1959.zip"
   desc "Latin Modern Math"
-  homepage "http://www.gust.org.pl/projects/e-foundry/lm-math"
+  desc "Latin Modern variation focused on mathematics"
+  homepage "https://www.gust.org.pl/projects/e-foundry/lm-math"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}latinmodern-math-1959/otf/latinmodern-math.otf"
