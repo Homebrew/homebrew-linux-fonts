e1a98be7d40ba6635ed7ebeb455b7240491e6573
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-17 00:08:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-envy-code-r-nerd-font.rb from 2855f3502b532cb848e016fab7f589c4ad0efece

---
+++ Formula/font-envy-code-r-nerd-font.rb
@@ -0,0 +1,23 @@
+class FontEnvyCodeRNerdFont < Formula
+  version "3.0.2"
+  sha256 "427304ca50f742ace3b0f2c1733ebbe24eb176af3b6a429ae01e9966f3cb94c7"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/EnvyCodeR.zip"
+  desc "EnvyCodeR Nerd Font (Envy Code R)"
+  desc "Developer targeted fonts with a high number of glyphs"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  def install
+    (share/"fonts").install "EnvyCodeRNerdFont-Bold.ttf"
+    (share/"fonts").install "EnvyCodeRNerdFont-Italic.ttf"
+    (share/"fonts").install "EnvyCodeRNerdFont-Regular.ttf"
+    (share/"fonts").install "EnvyCodeRNerdFontMono-Bold.ttf"
+    (share/"fonts").install "EnvyCodeRNerdFontMono-Italic.ttf"
+    (share/"fonts").install "EnvyCodeRNerdFontMono-Regular.ttf"
+    (share/"fonts").install "EnvyCodeRNerdFontPropo-Bold.ttf"
+    (share/"fonts").install "EnvyCodeRNerdFontPropo-Italic.ttf"
+    (share/"fonts").install "EnvyCodeRNerdFontPropo-Regular.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
