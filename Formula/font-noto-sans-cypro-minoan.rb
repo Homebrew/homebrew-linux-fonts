83d84bf7b60ab663c428d07f5378b8ded90cc50c
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-17 00:08:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-cypro-minoan.rb from fbcbf78619d1c2e540b37b59b77fb32b0f1c16e2

--- Formula/font-noto-sans-cypro-minoan.rb
+++ Formula/font-noto-sans-cypro-minoan.rb
@@ -1,8 +1,8 @@
 class FontNotoSansCyproMinoan < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanscyprominoan/NotoSansCyproMinoan-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanscyprominoan/NotoSansCyproMinoan-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Noto Sans Cypro Minoan"
   desc "Design for the historical european cypro-minoan script"
-  homepage "https://github.com/notofonts/cypro-minoan.git"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Cypro+Minoan"
   def install
     (share/"fonts").install "NotoSansCyproMinoan-Regular.ttf"
   end
