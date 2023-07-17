bf08e1bfee9b566043a9c9dd8f8aa80dbf19d077
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-17 00:08:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-khitan-small-script.rb from fbcbf78619d1c2e540b37b59b77fb32b0f1c16e2

--- Formula/font-noto-serif-khitan-small-script.rb
+++ Formula/font-noto-serif-khitan-small-script.rb
@@ -1,8 +1,8 @@
 class FontNotoSerifKhitanSmallScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifkhitansmallscript/NotoSerifKhitanSmallScript-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifkhitansmallscript/NotoSerifKhitanSmallScript-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Noto Serif Khitan Small Script"
   desc "Design for the historical east asian khitan small script script"
-  homepage "https://github.com/notofonts/khitan-small-script.git"
+  homepage "https://fonts.google.com/specimen/Noto+Serif+Khitan+Small+Script"
   def install
     (share/"fonts").install "NotoSerifKhitanSmallScript-Regular.ttf"
   end
