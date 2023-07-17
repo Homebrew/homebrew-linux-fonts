d6cff9e89433e9a950e0e399c4610f5862e0a5b7
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-17 00:08:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lumanosimo.rb from fbcbf78619d1c2e540b37b59b77fb32b0f1c16e2

--- Formula/font-lumanosimo.rb
+++ Formula/font-lumanosimo.rb
@@ -1,8 +1,8 @@
 class FontLumanosimo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lumanosimo/Lumanosimo-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/lumanosimo/Lumanosimo-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Lumanosimo"
   desc "Expressive typeface"
-  homepage "https://github.com/docrepair-fonts/lumanosimo-fonts"
+  homepage "https://fonts.google.com/specimen/Lumanosimo"
   def install
     (share/"fonts").install "Lumanosimo-Regular.ttf"
   end
