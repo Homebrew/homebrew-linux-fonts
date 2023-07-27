6cad58b66e0a1c1fe61af74639d7d0a5d213febe
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-27 00:06:23 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pt-serif.rb from b37a4e392fde4a4e342da6cae75fa06082dc8e42

--- Formula/font-pt-serif.rb
+++ Formula/font-pt-serif.rb
@@ -1,14 +1,16 @@
 class FontPtSerif < Formula
-  head "https://company.paratype.com/system/attachments/634/original/ptserif.zip"
+  head "https://api.paratype.com/api/editions/ofl/download/52194", referer: "https://www.paratype.com/"
   desc "PT Serif"
-  homepage "https://www.paratype.com/public/"
+  desc "Universal type family designed for use together with PT Sans"
+  homepage "https://www.paratype.com/fonts/pt/pt-serif"
   def install
-    (share/"fonts").install "PTF55F.ttf"
-    (share/"fonts").install "PTF56F.ttf"
-    (share/"fonts").install "PTF75F.ttf"
-    (share/"fonts").install "PTF76F.ttf"
-    (share/"fonts").install "PTZ55F.ttf"
-    (share/"fonts").install "PTZ56F.ttf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}PT/PT/PT-Serif/pt-serif_bold-italic.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Serif/pt-serif_bold.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Serif/pt-serif_caption-italic.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Serif/pt-serif_caption-regular.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Serif/pt-serif_italic.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Serif/pt-serif_regular.ttf"
   end
   # No zap stanza required
 
