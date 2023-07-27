6ed2d6cc2f40ce18732fbc3990146aadeed640c5
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-27 00:06:23 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-pt-sans.rb from d0db3dbb57c024c504db44b585d390cb091ae100

--- Formula/font-pt-sans.rb
+++ Formula/font-pt-sans.rb
@@ -1,16 +1,18 @@
 class FontPtSans < Formula
-  head "https://company.paratype.com/system/attachments/629/original/ptsans.zip"
+  head "https://api.paratype.com/api/editions/ofl/download/52212", referer: "https://www.paratype.com/"
   desc "PT Sans"
-  homepage "https://www.paratype.com/public/"
+  desc "Type family of universal use"
+  homepage "https://www.paratype.com/fonts/pt/pt-sans"
   def install
-    (share/"fonts").install "PTC55F.ttf"
-    (share/"fonts").install "PTC75F.ttf"
-    (share/"fonts").install "PTN57F.ttf"
-    (share/"fonts").install "PTN77F.ttf"
-    (share/"fonts").install "PTS55F.ttf"
-    (share/"fonts").install "PTS56F.ttf"
-    (share/"fonts").install "PTS75F.ttf"
-    (share/"fonts").install "PTS76F.ttf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}PT/PT/PT-Sans/pt-sans_bold-italic.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Sans/pt-sans_bold.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Sans/pt-sans_caption-bold.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Sans/pt-sans_caption-regular.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Sans/pt-sans_italic.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Sans/pt-sans_narrow-bold.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Sans/pt-sans_narrow-regular.ttf"
+    (share/"fonts").install "#{parent}PT/PT/PT-Sans/pt-sans_regular.ttf"
   end
   # No zap stanza required
 
