fca6e23eef5371e0f681521d989bf26770d1e028
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-07-31 00:06:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-teko.rb from cfd45510a25c82acbb1ea2a95ac7291eab18d5d7

--- Formula/font-teko.rb
+++ Formula/font-teko.rb
@@ -1,13 +1,9 @@
 class FontTeko < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/teko"
+  head "https://github.com/google/fonts/raw/main/ofl/teko/Teko%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Teko"
   homepage "https://fonts.google.com/specimen/Teko"
   def install
-    (share/"fonts").install "Teko-Bold.ttf"
-    (share/"fonts").install "Teko-Light.ttf"
-    (share/"fonts").install "Teko-Medium.ttf"
-    (share/"fonts").install "Teko-Regular.ttf"
-    (share/"fonts").install "Teko-SemiBold.ttf"
+    (share/"fonts").install "Teko[wght].ttf"
   end
   # No zap stanza required
 
