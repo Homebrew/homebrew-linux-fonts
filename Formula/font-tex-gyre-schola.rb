c24c3ade4eda4b2cfcd948b70f7b00446d3a7c83
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-03 00:06:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-schola.rb from 6cf3ed6855bafb6be583db13d9040feb9a86e4b3

--- Formula/font-tex-gyre-schola.rb
+++ Formula/font-tex-gyre-schola.rb
@@ -1,9 +1,9 @@
 class FontTexGyreSchola < Formula
   version "2.005"
   sha256 "24063368bfdc1046439e290299157621a437294ecbb39a938d2ddb2afa3daaf8"
-  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/schola/qcs#{version}otf.zip"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/schola/qcs#{version}otf.zip"
   desc "TeX Gyre Schola"
-  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/schola"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/schola"
   def install
     (share/"fonts").install "texgyreschola-bold.otf"
     (share/"fonts").install "texgyreschola-bolditalic.otf"
