d7d9953b80b6090917f5bd11e17ee7ac18e5a1a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-03 00:06:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-bonum-math.rb from 7898c753b305c5ce5301e0a97b93978315436878

--- Formula/font-tex-gyre-bonum-math.rb
+++ Formula/font-tex-gyre-bonum-math.rb
@@ -1,9 +1,9 @@
 class FontTexGyreBonumMath < Formula
   version "1.005"
   sha256 "8f8dc6f52ff838201f581f20b4ab634508e6d4b1e2745fe5d6b7732e1df73290"
-  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrebonum-math-#{version.to_s.gsub(".", "")}.zip"
+  url "https://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrebonum-math-#{version.to_s.gsub(".", "")}.zip"
   desc "Bonum Math"
-  homepage "http://www.gust.org.pl/projects/e-foundry/tg-math"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tg-math"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}texgyrebonum-math-#{version.to_s.gsub(".", "")}/opentype/texgyrebonum-math.otf"
