ee7c82cb36ba11bd094a1f3f3eaf6237f4a676a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-03 00:06:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-termes.rb from 1d9b7fad5d71cd0099c54aac98a329afe215b662

--- Formula/font-tex-gyre-termes.rb
+++ Formula/font-tex-gyre-termes.rb
@@ -1,9 +1,9 @@
 class FontTexGyreTermes < Formula
   version "2.004"
   sha256 "5d467d8db17c96037b04409d682f071d7cc33cf94eda35a0a0465776a2c862b2"
-  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/termes/qtm#{version}otf.zip"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/termes/qtm#{version}otf.zip"
   desc "TeX Gyre Termes"
-  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/termes"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/termes"
   def install
     (share/"fonts").install "texgyretermes-bold.otf"
     (share/"fonts").install "texgyretermes-bolditalic.otf"
