209ccfcdaebc860156107f710451afba374cbf2e
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-03 00:06:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-adventor.rb from 6bad81281b89a2afba3fad0110da94cbbc12e5dc

--- Formula/font-tex-gyre-adventor.rb
+++ Formula/font-tex-gyre-adventor.rb
@@ -1,9 +1,9 @@
 class FontTexGyreAdventor < Formula
   version "2.501"
   sha256 "9e619eb1c8af5cb55240f8bb865453562a2efd9059dee39d085fb71f7a00f7a2"
-  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor/qag#{version.to_s.gsub(".", "_")}otf.zip"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor/qag#{version.to_s.gsub(".", "_")}otf.zip"
   desc "TeX Gyre Adventor"
-  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}qag#{version.to_s.gsub(".", "_")}otf/texgyreadventor-bold.otf"
