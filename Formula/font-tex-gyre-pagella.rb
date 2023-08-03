c44ae9ca68848629305dbad048b2df2e90ea4591
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-03 00:06:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tex-gyre-pagella.rb from 7c394ca0e39a43c0ab88750366fd07b1c0ff2411

--- Formula/font-tex-gyre-pagella.rb
+++ Formula/font-tex-gyre-pagella.rb
@@ -1,9 +1,9 @@
 class FontTexGyrePagella < Formula
   version "2.501"
   sha256 "e16078d19860d68a54bcaedc00e35c2a81b396cdc36842700f1d556f89cf8ef2"
-  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl#{version.to_s.gsub(".", "_")}otf.zip"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl#{version.to_s.gsub(".", "_")}otf.zip"
   desc "TeX Gyre Pagella"
-  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}qpl#{version.to_s.gsub(".", "_")}otf/texgyrepagella-bold.otf"
