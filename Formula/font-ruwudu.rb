15ee2f72807e210de160ba7f14edd3328f78a72f
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-14 00:05:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ruwudu.rb from 8a461ceeb3e4a6c9f49b771af9d1794147aaecb8

--- Formula/font-ruwudu.rb
+++ Formula/font-ruwudu.rb
@@ -1,8 +1,8 @@
 class FontRuwudu < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/ruwudu"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ruwudu"
   desc "Ruwudu"
   desc "Open font family for arabic script languages that use the rubutun kano style"
-  homepage "https://github.com/silnrsi/font-ruwudu"
+  homepage "https://fonts.google.com/specimen/Ruwudu"
   def install
     (share/"fonts").install "Ruwudu-Bold.ttf"
     (share/"fonts").install "Ruwudu-Medium.ttf"
