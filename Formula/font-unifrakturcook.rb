1c11d11091057b5b62e90a524a97ec4643ecf748
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:06:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-unifrakturcook.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-unifrakturcook.rb
@@ -0,0 +1,12 @@
+class FontUnifrakturcook < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/unifrakturcook/UnifrakturCook-Bold.ttf", verified: "github.com/google/fonts/"
+  desc "UnifrakturCook"
+  homepage "https://fonts.google.com/specimen/UnifrakturCook"
+  def install
+    (share/"fonts").install "UnifrakturCook-Bold.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
