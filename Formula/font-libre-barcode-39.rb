6b42c9279e408047e4ed9a37e9cc7164c9fcdc77
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-libre-barcode-39.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-libre-barcode-39.rb
@@ -0,0 +1,12 @@
+class FontLibreBarcode39 < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/librebarcode39/LibreBarcode39-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Libre Barcode 39"
+  homepage "https://fonts.google.com/specimen/Libre+Barcode+39"
+  def install
+    (share/"fonts").install "LibreBarcode39-Regular.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
