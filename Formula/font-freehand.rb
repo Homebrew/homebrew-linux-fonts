97db891c4980afefbd625d33ed53be3f09718fe8
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-freehand.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-freehand.rb
@@ -0,0 +1,12 @@
+class FontFreehand < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/freehand/Freehand-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Freehand"
+  homepage "https://fonts.google.com/specimen/Freehand"
+  def install
+    (share/"fonts").install "Freehand-Regular.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
