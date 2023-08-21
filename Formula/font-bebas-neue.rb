37e627f96bbe2b59d23b8e5a6bfdbc5881f77fd1
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:56 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bebas-neue.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-bebas-neue.rb
@@ -0,0 +1,12 @@
+class FontBebasNeue < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/bebasneue/BebasNeue-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Bebas Neue"
+  homepage "https://fonts.google.com/specimen/Bebas+Neue"
+  def install
+    (share/"fonts").install "BebasNeue-Regular.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
