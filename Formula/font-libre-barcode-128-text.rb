7abb63d7e9c8d61c68c6d12c28cc0ccd23be055a
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-libre-barcode-128-text.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-libre-barcode-128-text.rb
@@ -0,0 +1,12 @@
+class FontLibreBarcode128Text < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/librebarcode128text/LibreBarcode128Text-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Libre Barcode 128 Text"
+  homepage "https://fonts.google.com/specimen/Libre+Barcode+128+Text"
+  def install
+    (share/"fonts").install "LibreBarcode128Text-Regular.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
