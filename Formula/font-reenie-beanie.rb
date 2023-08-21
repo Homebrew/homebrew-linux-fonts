cdeefa940f7ee21b0375bb51ab0fae0a0b2a92e7
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:59 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-reenie-beanie.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-reenie-beanie.rb
@@ -0,0 +1,12 @@
+class FontReenieBeanie < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/reeniebeanie/ReenieBeanie.ttf", verified: "github.com/google/fonts/"
+  desc "Reenie Beanie"
+  homepage "https://fonts.google.com/specimen/Reenie+Beanie"
+  def install
+    (share/"fonts").install "ReenieBeanie.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
