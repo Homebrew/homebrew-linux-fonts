eb5e744bf54c1ab36b77f32f08d63e72217847e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:56 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-amarante.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-amarante.rb
@@ -0,0 +1,12 @@
+class FontAmarante < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/amarante/Amarante-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Amarante"
+  homepage "https://fonts.google.com/specimen/Amarante"
+  def install
+    (share/"fonts").install "Amarante-Regular.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
