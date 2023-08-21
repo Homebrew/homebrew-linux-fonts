3ca10c96584a3a7f3f3c32f76dc2e8a02caabbf5
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:56 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lxgw-fasmartgothic.rb from a76323506970266eb014ceb2b2db03656153b8a4

--- Formula/font-lxgw-fasmartgothic.rb
+++ Formula/font-lxgw-fasmartgothic.rb
@@ -1,6 +1,6 @@
 class FontLxgwFasmartgothic < Formula
-  version "1.103.1"
-  sha256 "041607bf2a0669edcd5c1aadcb3ccc272b1020778c3c35e4a1c885e60883b973"
+  version "1.104"
+  sha256 "f13b35bb743b28291e7231adf8874ebc8aca190a16202a5d9cd9fbffd7d59270"
   url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWFasmartGothic.ttf"
   desc "LXGW FasmartGothic"
   desc "霞鹜尚智黑"
