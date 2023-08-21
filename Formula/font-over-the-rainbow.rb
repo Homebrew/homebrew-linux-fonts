98a6079658c0bba6781079a797931134fca5357f
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:59 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-over-the-rainbow.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-over-the-rainbow.rb
@@ -0,0 +1,12 @@
+class FontOverTheRainbow < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/overtherainbow/OvertheRainbow.ttf", verified: "github.com/google/fonts/"
+  desc "Over the Rainbow"
+  homepage "https://fonts.google.com/specimen/Over+the+Rainbow"
+  def install
+    (share/"fonts").install "OvertheRainbow.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
