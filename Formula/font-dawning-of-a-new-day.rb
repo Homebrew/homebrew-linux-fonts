4270469ee09bb6bd1cd18597a3f6442d809a2a61
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:57 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-dawning-of-a-new-day.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-dawning-of-a-new-day.rb
@@ -0,0 +1,12 @@
+class FontDawningOfANewDay < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/dawningofanewday/DawningofaNewDay.ttf", verified: "github.com/google/fonts/"
+  desc "Dawning of a New Day"
+  homepage "https://fonts.google.com/specimen/Dawning+of+a+New+Day"
+  def install
+    (share/"fonts").install "DawningofaNewDay.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
