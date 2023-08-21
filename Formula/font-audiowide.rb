b0e6ee8d99d0b42c63f7a734699f4f3bf688c7fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-08-21 00:05:56 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-audiowide.rb from 6c8693e404d56ebed5eb3b20b1c5ef3f9aa749d1

---
+++ Formula/font-audiowide.rb
@@ -0,0 +1,12 @@
+class FontAudiowide < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/audiowide/Audiowide-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Audiowide"
+  homepage "https://fonts.google.com/specimen/Audiowide"
+  def install
+    (share/"fonts").install "Audiowide-Regular.ttf"
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
