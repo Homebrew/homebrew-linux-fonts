c7781e90f071ea232c9d967a8bcca6050d0b279d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ruslan-display.rb from 9dd221f633438056c743830be6b29bc687d001dc

--- Formula/font-ruslan-display.rb
+++ Formula/font-ruslan-display.rb
@@ -1,9 +1,9 @@
 class FontRuslanDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ruslandisplay/RuslanDisplay.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/ruslandisplay/RuslanDisplay-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Ruslan Display"
   homepage "https://fonts.google.com/specimen/Ruslan+Display"
   def install
-    (share/"fonts").install "RuslanDisplay.ttf"
+    (share/"fonts").install "RuslanDisplay-Regular.ttf"
   end
   # No zap stanza required
 
