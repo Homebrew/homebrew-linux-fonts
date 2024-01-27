7bca8b7ab6767b66024870f2f70078a93d9f10e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:03 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-aile.rb from ecaa95dfc7c8f686f0b7c994b2b44409176900f3

--- Formula/font-iosevka-aile.rb
+++ Formula/font-iosevka-aile.rb
@@ -1,20 +1,20 @@
 class FontIosevkaAile < Formula
-  version "26.2.0"
-  sha256 "0ed8078d4042e86c395bc29a9b16f6500755ffd1916f56b7e23d31375b848722"
-  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-aile-#{version}.zip"
+  version "28.0.6"
+  sha256 "933c795a9da25abc4a58ad6737b92989d3540306b0c57c4a66c6c2492bd9d743"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaAile-#{version}.zip"
   desc "Iosevka Aile"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
   def install
-    (share/"fonts").install "ofl/intertight/" + "iosevka-aile-bold.ttc"
-    (share/"fonts").install "ofl/intertight/" + "iosevka-aile-extrabold.ttc"
-    (share/"fonts").install "ofl/intertight/" + "iosevka-aile-extralight.ttc"
-    (share/"fonts").install "ofl/intertight/" + "iosevka-aile-heavy.ttc"
-    (share/"fonts").install "ofl/intertight/" + "iosevka-aile-light.ttc"
-    (share/"fonts").install "ofl/intertight/" + "iosevka-aile-medium.ttc"
-    (share/"fonts").install "ofl/intertight/" + "iosevka-aile-regular.ttc"
-    (share/"fonts").install "ofl/intertight/" + "iosevka-aile-semibold.ttc"
-    (share/"fonts").install "ofl/intertight/" + "iosevka-aile-thin.ttc"
+    (share/"fonts").install "ofl/intertight/" + "IosevkaAile-Bold.ttc"
+    (share/"fonts").install "ofl/intertight/" + "IosevkaAile-ExtraBold.ttc"
+    (share/"fonts").install "ofl/intertight/" + "IosevkaAile-ExtraLight.ttc"
+    (share/"fonts").install "ofl/intertight/" + "IosevkaAile-Heavy.ttc"
+    (share/"fonts").install "ofl/intertight/" + "IosevkaAile-Light.ttc"
+    (share/"fonts").install "ofl/intertight/" + "IosevkaAile-Medium.ttc"
+    (share/"fonts").install "ofl/intertight/" + "IosevkaAile-Regular.ttc"
+    (share/"fonts").install "ofl/intertight/" + "IosevkaAile-SemiBold.ttc"
+    (share/"fonts").install "ofl/intertight/" + "IosevkaAile-Thin.ttc"
   end
   # No zap stanza required
 
