477b83195b3bdc3fbd897c3419898c2c589dd7b4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-roboto.rb from 64aa5aae070b8975e2f8955346051af6c101b591

--- Formula/font-roboto.rb
+++ Formula/font-roboto.rb
@@ -1,11 +1,11 @@
 class FontRoboto < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "apache/roboto"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/roboto"
   desc "Roboto"
   desc "Font with a mechanical skeleton and the forms are largely geometric"
   homepage "https://fonts.google.com/specimen/Roboto"
   def install
-    (share/"fonts").install "apache/roboto/" + "Roboto-Italic[wdth,wght].ttf"
-    (share/"fonts").install "apache/roboto/" + "Roboto[wdth,wght].ttf"
+    (share/"fonts").install "ofl/roboto/" + "Roboto-Italic[wdth,wght].ttf"
+    (share/"fonts").install "ofl/roboto/" + "Roboto[wdth,wght].ttf"
   end
   # No zap stanza required
 
