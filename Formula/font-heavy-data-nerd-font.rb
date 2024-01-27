5baa238abfbd4f1b31b65f378f18a042325ddc0a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-heavy-data-nerd-font.rb from 2aa56a8e96d8e2524bfce0d46fdd9401091bbace

--- Formula/font-heavy-data-nerd-font.rb
+++ Formula/font-heavy-data-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontHeavyDataNerdFont < Formula
-  version "3.0.2"
-  sha256 "af2b01d57c823fe0f144df8268db0d537ef45798e8913f80675635ed518b333a"
+  version "3.1.1"
+  sha256 "c0c98e8c91c53381842ac3135ad3a1f09134a3279a059eb04dd8d5a83fa9765e"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/HeavyData.zip"
   desc "HeavyData Nerd Font (Heavy Data)"
   desc "Developer targeted fonts with a high number of glyphs"
