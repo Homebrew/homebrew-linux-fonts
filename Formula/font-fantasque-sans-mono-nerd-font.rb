680be3bd54701c336b05efe81e5d00af2bf13ef4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fantasque-sans-mono-nerd-font.rb from 2aa56a8e96d8e2524bfce0d46fdd9401091bbace

--- Formula/font-fantasque-sans-mono-nerd-font.rb
+++ Formula/font-fantasque-sans-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontFantasqueSansMonoNerdFont < Formula
-  version "3.0.2"
-  sha256 "aaa2a76df805beea647e9905e4464e72d112193e61fdc7e52c7da02d280e36c1"
+  version "3.1.1"
+  sha256 "c00049c0c58d2d0e38f46b2566ef60fe58958b6fd540d5ae44dd79f8c52bc0e4"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FantasqueSansMono.zip"
   desc "FantasqueSansM Nerd Font (Fantasque Sans Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
