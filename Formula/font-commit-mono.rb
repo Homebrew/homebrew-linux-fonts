9a04d11e5ff5015734b70b501ab9a0714d1724f4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:01 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-commit-mono.rb from 76ec8c8905f963de9031df252f358ae4696f2508

--- Formula/font-commit-mono.rb
+++ Formula/font-commit-mono.rb
@@ -1,15 +1,16 @@
 class FontCommitMono < Formula
-  version "1.134"
-  sha256 "022f80aa671503a6d0802807dfa0c56f19485a57518ff9865861ff6af314abab"
-  url "https://github.com/eigilnikolajsen/commit-mono/releases/download/#{version}/CommitMono-#{version}.zip", verified: "github.com/eigilnikolajsen/commit-mono/"
+  version "1.143"
+  sha256 "f7d1f26a7c7554800a996f76f5d706bf0648b936ca2a66b5bc4d46e3a2c49ed0"
+  url "https://github.com/eigilnikolajsen/commit-mono/releases/download/v#{version}/CommitMono-#{version}.zip", verified: "github.com/eigilnikolajsen/commit-mono/"
   desc "Commit Mono"
   desc "Neutral programming typeface"
   homepage "https://commitmono.com/"
   def install
-    (share/"fonts").install "CommitMono-450-Italic.otf"
-    (share/"fonts").install "CommitMono-450-Regular.otf"
-    (share/"fonts").install "CommitMono-700-Italic.otf"
-    (share/"fonts").install "CommitMono-700-Regular.otf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}CommitMono-#{version}/CommitMono-400-Italic.otf"
+    (share/"fonts").install "#{parent}CommitMono-#{version}/CommitMono-400-Regular.otf"
+    (share/"fonts").install "#{parent}CommitMono-#{version}/CommitMono-700-Italic.otf"
+    (share/"fonts").install "#{parent}CommitMono-#{version}/CommitMono-700-Regular.otf"
   end
   # No zap stanza required
 
