b04fd824c0205d5528c9535417e6174e9a880c4a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-b612.rb from 34bcca079570a4d7f9b5aaad1802ed23a8fe7f9c

--- Formula/font-b612.rb
+++ Formula/font-b612.rb
@@ -1,17 +1,16 @@
 class FontB612 < Formula
-  head "https://github.com/polarsys/b612/archive/master.zip", verified: "github.com/polarsys/b612/"
+  version "1.008"
+  sha256 "727cb91e47d65ac49c2d97d7b1c36d9891b885d9ddf06e15ad3d23d22bdad9cf"
+  url "https://github.com/polarsys/b612/archive/refs/tags/#{version}.zip", verified: "github.com/polarsys/b612/"
   desc "B612"
+  desc "Font designed and tested to be used on aircraft cockpit screens"
   homepage "https://b612-font.com/"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}b612-master/fonts/ttf/B612-Bold.ttf"
-    (share/"fonts").install "#{parent}b612-master/fonts/ttf/B612-BoldItalic.ttf"
-    (share/"fonts").install "#{parent}b612-master/fonts/ttf/B612-Italic.ttf"
-    (share/"fonts").install "#{parent}b612-master/fonts/ttf/B612-Regular.ttf"
-    (share/"fonts").install "#{parent}b612-master/fonts/ttf/B612Mono-Bold.ttf"
-    (share/"fonts").install "#{parent}b612-master/fonts/ttf/B612Mono-BoldItalic.ttf"
-    (share/"fonts").install "#{parent}b612-master/fonts/ttf/B612Mono-Italic.ttf"
-    (share/"fonts").install "#{parent}b612-master/fonts/ttf/B612Mono-Regular.ttf"
+    (share/"fonts").install "#{parent}b612-#{version}/fonts/ttf/B612-Bold.ttf"
+    (share/"fonts").install "#{parent}b612-#{version}/fonts/ttf/B612-BoldItalic.ttf"
+    (share/"fonts").install "#{parent}b612-#{version}/fonts/ttf/B612-Italic.ttf"
+    (share/"fonts").install "#{parent}b612-#{version}/fonts/ttf/B612-Regular.ttf"
   end
   # No zap stanza required
 
