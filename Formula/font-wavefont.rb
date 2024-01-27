9ee25fa26decd0cc09eacbd9024f18633b7178d7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:12 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-wavefont.rb from fef7dea6a1cf5d9090b073773b0f6c63117b640c

--- Formula/font-wavefont.rb
+++ Formula/font-wavefont.rb
@@ -1,8 +1,8 @@
 class FontWavefont < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/wavefont/Wavefont%5BROND%2CYELA%2Cwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/wavefont/Wavefont%5BROND%2CYELA%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Wavefont"
   desc "Useful to adjust distance between bars, 1ch === 1 bar width"
-  homepage "https://github.com/dy/wavefont"
+  homepage "https://fonts.google.com/specimen/Wavefont"
   def install
     (share/"fonts").install "Wavefont[ROND,YELA,wght].ttf"
   end
