bd166a775d6e1eb6589accec83209927eda97c9a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:03 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss03.rb from c3a04ec027edd9cc40e2e21b1fd64258c349d258

--- Formula/font-iosevka-ss03.rb
+++ Formula/font-iosevka-ss03.rb
@@ -1,20 +1,20 @@
 class FontIosevkaSs03 < Formula
-  version "26.2.0"
-  sha256 "ede0fb35d39baf4ad42ef3c93afcb4fc70b5163a748d21300c8ac9d5652bff6e"
-  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss03-#{version}.zip"
+  version "28.0.6"
+  sha256 "0360116b120b86c91ee62ed6d8d0c861b823eb9ec8b6c16122f0971e32911820"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaSS03-#{version}.zip"
   desc "Iosevka SS03"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
   def install
-    (share/"fonts").install "iosevka-ss03-bold.ttc"
-    (share/"fonts").install "iosevka-ss03-extrabold.ttc"
-    (share/"fonts").install "iosevka-ss03-extralight.ttc"
-    (share/"fonts").install "iosevka-ss03-heavy.ttc"
-    (share/"fonts").install "iosevka-ss03-light.ttc"
-    (share/"fonts").install "iosevka-ss03-medium.ttc"
-    (share/"fonts").install "iosevka-ss03-regular.ttc"
-    (share/"fonts").install "iosevka-ss03-semibold.ttc"
-    (share/"fonts").install "iosevka-ss03-thin.ttc"
+    (share/"fonts").install "IosevkaSS03-Bold.ttc"
+    (share/"fonts").install "IosevkaSS03-ExtraBold.ttc"
+    (share/"fonts").install "IosevkaSS03-ExtraLight.ttc"
+    (share/"fonts").install "IosevkaSS03-Heavy.ttc"
+    (share/"fonts").install "IosevkaSS03-Light.ttc"
+    (share/"fonts").install "IosevkaSS03-Medium.ttc"
+    (share/"fonts").install "IosevkaSS03-Regular.ttc"
+    (share/"fonts").install "IosevkaSS03-SemiBold.ttc"
+    (share/"fonts").install "IosevkaSS03-Thin.ttc"
   end
   # No zap stanza required
 
