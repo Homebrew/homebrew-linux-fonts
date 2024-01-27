218ccbf6694d3f42f81f0baa8fb89c681236d749
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:07 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-cjk.rb from 041927892e52025f3b1442d9b900b225564f7160

--- Formula/font-noto-sans-cjk.rb
+++ Formula/font-noto-sans-cjk.rb
@@ -1,7 +1,10 @@
 class FontNotoSansCjk < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJK.ttc.zip", verified: "noto-website-2.storage.googleapis.com/"
+  version "2.004"
+  sha256 "a56077736038875f3a5b1e6715b2713a2bd049cc0bf0dfdf645927b070d38bfc"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Sans#{version}/00_NotoSansCJK.ttc.zip"
   desc "Noto Sans CJK"
-  homepage "https://www.google.com/get/noto/help/cjk/"
+  desc "Static Super OTC"
+  homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
   def install
     (share/"fonts").install "NotoSansCJK.ttc"
   end
