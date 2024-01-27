8c2244cd02d697785c690251ec995d9e2d1a5225
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:05 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lilex.rb from d73614b0e6bc16812b2e5913dfda313752de2faf

--- Formula/font-lilex.rb
+++ Formula/font-lilex.rb
@@ -1,6 +1,6 @@
 class FontLilex < Formula
-  version "2.200"
-  sha256 "c4029f22577077f0e82c39af6f3513df5d621e4668f49e7cea9c514aba93a59a"
+  version "2.400"
+  sha256 "6e50639476221f8d11c859d4e8d36d164c236e049f6625414d4cf82b02f1f10f"
   url "https://github.com/mishamyrt/Lilex/releases/download/#{version}/Lilex.zip"
   desc "Lilex"
   desc "Programming font"
@@ -10,7 +10,7 @@ class FontLilex < Formula
     (share/"fonts").install "#{parent}ttf/Lilex-Bold.ttf"
     (share/"fonts").install "#{parent}ttf/Lilex-ExtraLight.ttf"
     (share/"fonts").install "#{parent}ttf/Lilex-ExtraThick.ttf"
-    (share/"fonts").install "#{parent}ttf/Lilex-Meduim.ttf"
+    (share/"fonts").install "#{parent}ttf/Lilex-Medium.ttf"
     (share/"fonts").install "#{parent}ttf/Lilex-Regular.ttf"
     (share/"fonts").install "#{parent}ttf/Lilex-Thin.ttf"
     (share/"fonts").install "#{parent}variable/Lilex-VF.ttf"
