6b6cc44d045a6868547a018d01189794ba07656d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-serif-cjk-tc.rb from a74ff6eeba40ff4eb1486c25e9753b2876c44688

--- Formula/font-noto-serif-cjk-tc.rb
+++ Formula/font-noto-serif-cjk-tc.rb
@@ -1,15 +1,19 @@
 class FontNotoSerifCjkTc < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKtc-hinted.zip", verified: "noto-website-2.storage.googleapis.com/"
+  version "2.002"
+  sha256 "bd14b808431c5058c69f999c3c6d898b9f37d96962ec26f7212fb34c2d49ea5c"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Serif#{version}/10_NotoSerifCJKtc.zip"
   desc "Noto Serif CJK TC"
-  homepage "https://www.google.com/get/noto/#serif-hant"
+  desc "Language Specific OTFs Traditional Chinese — Taiwan (繁體中文—臺灣)"
+  homepage "https://github.com/notofonts/noto-cjk/tree/main/Serif"
   def install
-    (share/"fonts").install "NotoSerifCJKtc-Bold.otf"
-    (share/"fonts").install "NotoSerifCJKtc-Light.otf"
-    (share/"fonts").install "NotoSerifCJKtc-Regular.otf"
-    (share/"fonts").install "NotoSerifCJKtc-Black.otf"
-    (share/"fonts").install "NotoSerifCJKtc-ExtraLight.otf"
-    (share/"fonts").install "NotoSerifCJKtc-Medium.otf"
-    (share/"fonts").install "NotoSerifCJKtc-SemiBold.otf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}OTF/TraditionalChinese/NotoSerifCJKtc-Black.otf"
+    (share/"fonts").install "#{parent}OTF/TraditionalChinese/NotoSerifCJKtc-Bold.otf"
+    (share/"fonts").install "#{parent}OTF/TraditionalChinese/NotoSerifCJKtc-ExtraLight.otf"
+    (share/"fonts").install "#{parent}OTF/TraditionalChinese/NotoSerifCJKtc-Light.otf"
+    (share/"fonts").install "#{parent}OTF/TraditionalChinese/NotoSerifCJKtc-Medium.otf"
+    (share/"fonts").install "#{parent}OTF/TraditionalChinese/NotoSerifCJKtc-Regular.otf"
+    (share/"fonts").install "#{parent}OTF/TraditionalChinese/NotoSerifCJKtc-SemiBold.otf"
   end
   # No zap stanza required
 
