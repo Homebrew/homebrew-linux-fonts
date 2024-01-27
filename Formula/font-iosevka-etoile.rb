a6fbafa2459f9249dcae1862f900043777385784
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:03 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-etoile.rb from 0471c88e66f0a36ce9aa932639d001d07b197e18

--- Formula/font-iosevka-etoile.rb
+++ Formula/font-iosevka-etoile.rb
@@ -1,20 +1,20 @@
 class FontIosevkaEtoile < Formula
-  version "26.2.0"
-  sha256 "486c38ffb5d0a52937a124a7e4b7ad5d13146b87d0ff49c4855dfbf6a21cee6d"
-  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-etoile-#{version}.zip"
+  version "28.0.6"
+  sha256 "458180342c91466d551024992e3703d424bf88234c585ae4515bd581a5605d46"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaEtoile-#{version}.zip"
   desc "Iosevka Etoile"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
   def install
-    (share/"fonts").install "iosevka-etoile-bold.ttc"
-    (share/"fonts").install "iosevka-etoile-extrabold.ttc"
-    (share/"fonts").install "iosevka-etoile-extralight.ttc"
-    (share/"fonts").install "iosevka-etoile-heavy.ttc"
-    (share/"fonts").install "iosevka-etoile-light.ttc"
-    (share/"fonts").install "iosevka-etoile-medium.ttc"
-    (share/"fonts").install "iosevka-etoile-regular.ttc"
-    (share/"fonts").install "iosevka-etoile-semibold.ttc"
-    (share/"fonts").install "iosevka-etoile-thin.ttc"
+    (share/"fonts").install "IosevkaEtoile-Bold.ttc"
+    (share/"fonts").install "IosevkaEtoile-ExtraBold.ttc"
+    (share/"fonts").install "IosevkaEtoile-ExtraLight.ttc"
+    (share/"fonts").install "IosevkaEtoile-Heavy.ttc"
+    (share/"fonts").install "IosevkaEtoile-Light.ttc"
+    (share/"fonts").install "IosevkaEtoile-Medium.ttc"
+    (share/"fonts").install "IosevkaEtoile-Regular.ttc"
+    (share/"fonts").install "IosevkaEtoile-SemiBold.ttc"
+    (share/"fonts").install "IosevkaEtoile-Thin.ttc"
   end
   # No zap stanza required
 
