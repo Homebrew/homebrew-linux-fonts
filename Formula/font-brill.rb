1f1ad27383431030af10cb1dbab837f8f9b6f417
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-brill.rb from bac691e1d7b5bd7372e7e0befae989a3ff7ad449

--- Formula/font-brill.rb
+++ Formula/font-brill.rb
@@ -1,15 +1,17 @@
 class FontBrill < Formula
-  version "2.06"
-  url "https://brill.com/fileasset/downloads_static/static_typefacedownload_typefacepackage.zip"
+  version "4.0"
+  sha256 "5575fbca12d5f8f50b8613bf01ae373f4237fcb2fdf5a379e7378f477e54352e"
+  url "https://brill.com/fileasset/The_Brill_Typeface_Package_v_#{version.to_s.gsub(".", "_")}.zip"
   desc "Brill"
   homepage "https://brill.com/page/BrillFont/"
   def install
-    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}Brill_Typeface_#{version}/Brill Bold #{version} 051.ttf"
-    (share/"fonts").install "#{parent}Brill_Typeface_#{version}/Brill Bold Italic #{version} 051.ttf"
-    (share/"fonts").install "#{parent}Brill_Typeface_#{version}/Brill Italic #{version} 051.ttf"
-    (share/"fonts").install "#{parent}Brill_Typeface_#{version}/Brill Roman #{version} 051.ttf"
+    (share/"fonts").install "Brill-Bold.ttf"
+    (share/"fonts").install "Brill-BoldItalic.ttf"
+    (share/"fonts").install "Brill-Italic.ttf"
+    (share/"fonts").install "Brill-Roman.ttf"
   end
+  # No zap stanza required
+
   test do
   end
 end
