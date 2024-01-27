7bfe7936f960cab734d3c89ee5a0227dec6a4868
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:13 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-xits.rb from 29e7272677974d015137056f5a3f67bfa498f182

--- Formula/font-xits.rb
+++ Formula/font-xits.rb
@@ -1,9 +1,11 @@
 class FontXits < Formula
-  version "1.200"
-  sha256 "039493b95b86d15e451e818e065e6f6719977a31e43504eb6946513ba09c8a72"
+  version "1.302"
+  sha256 "afb3e0804985bc8ab822e9cfc2a94706383491e7f5f56ccdca04909f30688fb0"
   url "https://github.com/khaledhosny/xits/archive/v#{version}.zip"
   desc "XITS"
+  desc "Times-like typeface for mathematical and scientific publishing"
   homepage "https://github.com/khaledhosny/xits"
+  deprecate! date: "2023-12-17", because: :unsupported
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}xits-#{version}/xits-bold.otf"
@@ -13,6 +15,8 @@ class FontXits < Formula
     (share/"fonts").install "#{parent}xits-#{version}/xitsmath-bold.otf"
     (share/"fonts").install "#{parent}xits-#{version}/xitsmath-regular.otf"
   end
+  # No zap stanza required
+
   test do
   end
 end
