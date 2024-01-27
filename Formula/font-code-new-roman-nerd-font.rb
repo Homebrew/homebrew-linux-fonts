885b8ab441d4cec64fa4cbe0655451eb7978738f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-code-new-roman-nerd-font.rb from 2aa56a8e96d8e2524bfce0d46fdd9401091bbace

--- Formula/font-code-new-roman-nerd-font.rb
+++ Formula/font-code-new-roman-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontCodeNewRomanNerdFont < Formula
-  version "3.0.2"
-  sha256 "a5dc918087c776720860ec8fbf87ba6a43975c91013ac0971366f00324888651"
+  version "3.1.1"
+  sha256 "afdf5d952836dd9bad27a41d9d8ab89b3b70a33c76a8ff9e8987a50fe43bbd7f"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/CodeNewRoman.zip"
   desc "CodeNewRoman Nerd Font (Code New Roman)"
   desc "Developer targeted fonts with a high number of glyphs"
