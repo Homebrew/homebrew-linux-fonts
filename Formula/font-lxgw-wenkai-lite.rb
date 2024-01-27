6807c9f86f862d1b33c325e93182f623ef7c6f25
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lxgw-wenkai-lite.rb from 06d385a8d48bf6ebe3f5253e45549867780c41d4

--- Formula/font-lxgw-wenkai-lite.rb
+++ Formula/font-lxgw-wenkai-lite.rb
@@ -1,11 +1,11 @@
 class FontLxgwWenkaiLite < Formula
-  version "1.300"
-  sha256 "ff88a3e3d1465423ae6d6d4e1fc574ab69643010de0615a72a686da0b34ecd0a"
-  url "https://github.com/lxgw/LxgwWenKai-Core/releases/download/v#{version}/lxgw-wenkai-lite-v#{version}.zip"
+  version "1.311"
+  sha256 "f185ce0beb03dc80e2e3104698ab80cc0e27fc2c54a0f4131b147660be5d505e"
+  url "https://github.com/lxgw/LxgwWenKai-Lite/releases/download/v#{version}/lxgw-wenkai-lite-v#{version}.zip"
   desc "LXGW WenKai Lite"
   desc "霞鹜文楷 轻便版"
   desc "Open-source Chinese font derived from Fontworks' Klee One"
-  homepage "https://github.com/lxgw/LxgwWenKai-Core"
+  homepage "https://github.com/lxgw/LxgwWenKai-Lite"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
     (share/"fonts").install "#{parent}lxgw-wenkai-lite-v#{version}/LXGWWenKaiLite-Bold.ttf"
