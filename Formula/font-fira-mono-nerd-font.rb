e473d7c565308b20a5f749a250940d3c5797d4a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-mono-nerd-font.rb from 2aa56a8e96d8e2524bfce0d46fdd9401091bbace

--- Formula/font-fira-mono-nerd-font.rb
+++ Formula/font-fira-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontFiraMonoNerdFont < Formula
-  version "3.0.2"
-  sha256 "8af0d6cc7c51af347fe9d4c8ef1cb822689b4353ac50ecb62f69e8503757695c"
+  version "3.1.1"
+  sha256 "16b2e467bda33c1b6692778ccc3cc3cd860137a5d9f9f90233f10fc6b9d17b58"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraMono.zip"
   desc "FiraMono Nerd Font (Fira)"
   desc "Developer targeted fonts with a high number of glyphs"
