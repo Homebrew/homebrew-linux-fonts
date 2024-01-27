0f6f21b9811ca65eeb52cb4b2fbb52cda5ddad2a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:07 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-sans-cjk-jp.rb from 408627e4b76c9e12376df12c4b500a9472d601f6

--- Formula/font-noto-sans-cjk-jp.rb
+++ Formula/font-noto-sans-cjk-jp.rb
@@ -1,7 +1,10 @@
 class FontNotoSansCjkJp < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip", verified: "noto-website-2.storage.googleapis.com/"
+  version "2.004"
+  sha256 "bf5b26feb83ee02533d4eb4a127bd90ed9d42bdd7ca7cb6e4030b2f21ef55cc5"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Sans#{version}/06_NotoSansCJKjp.zip"
   desc "Noto Sans CJK JP"
-  homepage "https://www.google.com/get/noto/#sans-jpan"
+  desc "Language Specific OTFs Japanese (日本語)"
+  homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
   def install
     (share/"fonts").install "NotoSansCJKjp-Black.otf"
     (share/"fonts").install "NotoSansCJKjp-Bold.otf"
@@ -10,8 +13,6 @@ class FontNotoSansCjkJp < Formula
     (share/"fonts").install "NotoSansCJKjp-Medium.otf"
     (share/"fonts").install "NotoSansCJKjp-Regular.otf"
     (share/"fonts").install "NotoSansCJKjp-Thin.otf"
-    (share/"fonts").install "NotoSansMonoCJKjp-Bold.otf"
-    (share/"fonts").install "NotoSansMonoCJKjp-Regular.otf"
   end
   # No zap stanza required
 
