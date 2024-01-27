18e7adc32f7196d094158fbf28c51cb7c9622510
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:00 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-clear-sans.rb from 5876bbfc5753a8785fcba7af7d11c6adb4fb7ee9

--- Formula/font-clear-sans.rb
+++ Formula/font-clear-sans.rb
@@ -1,19 +1,18 @@
 class FontClearSans < Formula
-  version "1.00"
-  sha256 "41809a296870dd7b4753d6532b4093821d61f9806281e6c053ccb11083ad1190"
-  url "https://01.org/sites/default/files/downloads/clear-sans/clearsans-#{version}.zip"
+  head "https://github.com/intel/clear-sans.git", branch: "main", only_path: "TTF"
   desc "Clear Sans"
-  homepage "https://01.org/clear-sans"
+  desc "Sans-serif typeface"
+  homepage "https://github.com/intel/clear-sans"
+  deprecate! date: "2023-12-17", because: :unsupported
   def install
-    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}TTF/ClearSans-Bold.ttf"
-    (share/"fonts").install "#{parent}TTF/ClearSans-BoldItalic.ttf"
-    (share/"fonts").install "#{parent}TTF/ClearSans-Italic.ttf"
-    (share/"fonts").install "#{parent}TTF/ClearSans-Light.ttf"
-    (share/"fonts").install "#{parent}TTF/ClearSans-Medium.ttf"
-    (share/"fonts").install "#{parent}TTF/ClearSans-MediumItalic.ttf"
-    (share/"fonts").install "#{parent}TTF/ClearSans-Regular.ttf"
-    (share/"fonts").install "#{parent}TTF/ClearSans-Thin.ttf"
+    (share/"fonts").install "TTF/" + "ClearSans-Bold.ttf"
+    (share/"fonts").install "TTF/" + "ClearSans-BoldItalic.ttf"
+    (share/"fonts").install "TTF/" + "ClearSans-Italic.ttf"
+    (share/"fonts").install "TTF/" + "ClearSans-Light.ttf"
+    (share/"fonts").install "TTF/" + "ClearSans-Medium.ttf"
+    (share/"fonts").install "TTF/" + "ClearSans-MediumItalic.ttf"
+    (share/"fonts").install "TTF/" + "ClearSans-Regular.ttf"
+    (share/"fonts").install "TTF/" + "ClearSans-Thin.ttf"
   end
   # No zap stanza required
 
