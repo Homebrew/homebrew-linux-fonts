0c7f7bb83b761503c80a8667e678becebab80f67
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-proggy-clean-tt-nerd-font.rb from 2aa56a8e96d8e2524bfce0d46fdd9401091bbace

--- Formula/font-proggy-clean-tt-nerd-font.rb
+++ Formula/font-proggy-clean-tt-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontProggyCleanTtNerdFont < Formula
-  version "3.0.2"
-  sha256 "17d34f28b786fab5a8bfd8b3711573a69c976f83f1112e8d61d2d247b14f317d"
+  version "3.1.1"
+  sha256 "d2cc116f1f34aa9649458a35dc340245a2c93eec272e47e1d4d21ec66df60cc6"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ProggyClean.zip"
   desc "ProggyClean Nerd Font families (ProggyCleanTT)"
   desc "Developer targeted fonts with a high number of glyphs"
