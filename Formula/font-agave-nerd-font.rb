e9cd43dafdf964dd275af4640564d00c74d84236
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:09:59 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-agave-nerd-font.rb from 2aa56a8e96d8e2524bfce0d46fdd9401091bbace

--- Formula/font-agave-nerd-font.rb
+++ Formula/font-agave-nerd-font.rb
@@ -1,17 +1,17 @@
 class FontAgaveNerdFont < Formula
-  version "3.0.2"
-  sha256 "f2e3f9b3345933c1ca6fbc9881f686dc7d818b06e6a88618dc689aafb9663548"
+  version "3.1.1"
+  sha256 "4ee6e90e9dcba475afd01e3b1dcd1ef0298482948328d2da09b7e6ea82abf036"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Agave.zip"
   desc "Agave Nerd Font (Agave)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "ofl/adventpro/" + "AgaveNerdFont-Bold.ttf"
-    (share/"fonts").install "ofl/adventpro/" + "AgaveNerdFont-Regular.ttf"
-    (share/"fonts").install "ofl/adventpro/" + "AgaveNerdFontMono-Bold.ttf"
-    (share/"fonts").install "ofl/adventpro/" + "AgaveNerdFontMono-Regular.ttf"
-    (share/"fonts").install "ofl/adventpro/" + "AgaveNerdFontPropo-Bold.ttf"
-    (share/"fonts").install "ofl/adventpro/" + "AgaveNerdFontPropo-Regular.ttf"
+    (share/"fonts").install "ofl/afacad/" + "AgaveNerdFont-Bold.ttf"
+    (share/"fonts").install "ofl/afacad/" + "AgaveNerdFont-Regular.ttf"
+    (share/"fonts").install "ofl/afacad/" + "AgaveNerdFontMono-Bold.ttf"
+    (share/"fonts").install "ofl/afacad/" + "AgaveNerdFontMono-Regular.ttf"
+    (share/"fonts").install "ofl/afacad/" + "AgaveNerdFontPropo-Bold.ttf"
+    (share/"fonts").install "ofl/afacad/" + "AgaveNerdFontPropo-Regular.ttf"
   end
   # No zap stanza required
 
