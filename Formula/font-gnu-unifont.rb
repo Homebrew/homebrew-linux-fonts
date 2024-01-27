d7c69fd4999dff548b0f849ec0661d2b91f8517f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-01-27 05:10:02 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gnu-unifont.rb from 37d2f475a69a94f314e8050bc66784523c5b9c9f

--- Formula/font-gnu-unifont.rb
+++ Formula/font-gnu-unifont.rb
@@ -1,17 +1,17 @@
 class FontGnuUnifont < Formula
-  version "15.0.06"
-  sha256 "36668eb1326d22e1466b94b3929beeafd10b9838bf3d41f4e5e3b52406ae69f1"
+  version "15.1.04"
+  sha256 "4a0a481c06338543399f5130cb50cbec6b73eeff6b3466f72891c38cbe1c5cf7"
   url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.tar.gz", verified: "ftpmirror.gnu.org/unifont/"
   desc "GNU Unifont"
   desc "Unicode bitmap font"
   homepage "https://unifoundry.com/unifont.html"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont-#{version}.ttf"
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_upper-#{version}.ttf"
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_sample-#{version}.ttf"
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_jp-#{version}.ttf"
-    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_csur-#{version}.ttf"
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont-#{version}.otf"
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_upper-#{version}.otf"
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_sample-#{version}.otf"
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_jp-#{version}.otf"
+    (share/"fonts").install "#{parent}unifont-#{version}/font/precompiled/unifont_csur-#{version}.otf"
   end
   # No zap stanza required
 
