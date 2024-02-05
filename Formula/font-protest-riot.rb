e64bc5070baef9de6ef1a7d9b95274d188f2e5fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-02-05 00:19:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-protest-riot.rb from beea909bdbe69cd597ffd5c6ae06f50c2eb1d16a

--- Formula/font-protest-riot.rb
+++ Formula/font-protest-riot.rb
@@ -1,7 +1,7 @@
 class FontProtestRiot < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/protestriot/ProtestRiot-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/protestriot/ProtestRiot-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Protest Riot"
-  homepage "https://github.com/octaviopardo/Protest"
+  homepage "https://fonts.google.com/specimen/Protest+Riot"
   def install
     (share/"fonts").install "ProtestRiot-Regular.ttf"
   end
