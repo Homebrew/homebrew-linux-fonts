d7e3de619dcc01e737a1d9a8c722e7aa6593309a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-02-08 00:18:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-smiley-sans.rb from 0c226bb2e5812a4a38ac535aae852256376aed78

--- Formula/font-smiley-sans.rb
+++ Formula/font-smiley-sans.rb
@@ -1,6 +1,6 @@
 class FontSmileySans < Formula
-  version "1.1.1"
-  sha256 "17c30d067b90ef6fb259caaf66e4339e30d70d5f197485da8f305c040d41413f"
+  version "2.0.1"
+  sha256 "299c0be6c960ae37361762eca76f7d0cd516615435bb96c0d4b98a1e70178a07"
   url "https://github.com/atelier-anchor/smiley-sans/releases/download/v#{version}/smiley-sans-v#{version}.zip", verified: "github.com/atelier-anchor/smiley-sans/"
   desc "Smiley Sans"
   desc "Chinese typeface seeking a visual balance between the humanist and the geometric"
