b42d9bff362e1edd22b700de82ab52036741bf90
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-02-19 00:19:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-annapurna-sil.rb from c986681b9974e43150ac5ff00df5da7da172452d

--- Formula/font-annapurna-sil.rb
+++ Formula/font-annapurna-sil.rb
@@ -1,8 +1,8 @@
 class FontAnnapurnaSil < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/annapurnasil"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/annapurnasil"
   desc "Annapurna SIL"
   desc "Unicode-based font with support for systems that use the devanagari script"
-  homepage "https://github.com/silnrsi/font-annapurna"
+  homepage "https://fonts.google.com/specimen/Annapurna+SIL"
   def install
     (share/"fonts").install "ofl/annapurnasil/" + "AnnapurnaSIL-Bold.ttf"
     (share/"fonts").install "ofl/annapurnasil/" + "AnnapurnaSIL-Regular.ttf"
