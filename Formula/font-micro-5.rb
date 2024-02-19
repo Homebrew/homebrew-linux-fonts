757af242b0da785fc267bb6442e84e62e1faa4a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-02-19 00:19:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-micro-5.rb from c986681b9974e43150ac5ff00df5da7da172452d

--- Formula/font-micro-5.rb
+++ Formula/font-micro-5.rb
@@ -1,8 +1,8 @@
 class FontMicro5 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/micro5/Micro5-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/micro5/Micro5-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Micro 5"
   desc "Teeny-tiny typeface that can fit anywhere on your project"
-  homepage "https://github.com/scfried/soft-type-micro"
+  homepage "https://fonts.google.com/specimen/Micro+5"
   def install
     (share/"fonts").install "Micro5-Regular.ttf"
   end
