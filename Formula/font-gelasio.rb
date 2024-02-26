90e7355b759fdc1b0dbb3c93cd72b7495cc74cf9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-02-26 00:18:51 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-gelasio.rb from 55df12b5d765f68011b82384367a24f474f79ba2

--- Formula/font-gelasio.rb
+++ Formula/font-gelasio.rb
@@ -3,14 +3,8 @@ class FontGelasio < Formula
   desc "Gelasio"
   homepage "https://fonts.google.com/specimen/Gelasio"
   def install
-    (share/"fonts").install "ofl/gelasio/" + "Gelasio-Bold.ttf"
-    (share/"fonts").install "ofl/gelasio/" + "Gelasio-BoldItalic.ttf"
-    (share/"fonts").install "ofl/gelasio/" + "Gelasio-Italic.ttf"
-    (share/"fonts").install "ofl/gelasio/" + "Gelasio-Medium.ttf"
-    (share/"fonts").install "ofl/gelasio/" + "Gelasio-MediumItalic.ttf"
-    (share/"fonts").install "ofl/gelasio/" + "Gelasio-Regular.ttf"
-    (share/"fonts").install "ofl/gelasio/" + "Gelasio-SemiBold.ttf"
-    (share/"fonts").install "ofl/gelasio/" + "Gelasio-SemiBoldItalic.ttf"
+    (share/"fonts").install "ofl/gelasio/" + "Gelasio-Italic[wght].ttf"
+    (share/"fonts").install "ofl/gelasio/" + "Gelasio[wght].ttf"
   end
   # No zap stanza required
 
