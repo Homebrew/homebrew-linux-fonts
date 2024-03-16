0ae5e5ab9cdc5d902c0888f9fb63129a40a51b31
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-03-16 00:17:41 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-oppo-sans.rb from cb7bb62012619b8dde1fed45d3a741591c8ed84c

--- Formula/font-oppo-sans.rb
+++ Formula/font-oppo-sans.rb
@@ -1,14 +1,14 @@
 class FontOppoSans < Formula
   head "https://static01.coloros.com/www/public/img/topic7/font-opposans.zip"
   desc "OPPO Sans"
-  homepage "https://www.coloros.com/topic/coloros7.html"
+  homepage "https://www.coloros.com/index/newsDetail?id=72"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}Font-OPPOSans/OPPOSans-B.ttf"
-    (share/"fonts").install "#{parent}Font-OPPOSans/OPPOSans-H.ttf"
-    (share/"fonts").install "#{parent}Font-OPPOSans/OPPOSans-L.ttf"
-    (share/"fonts").install "#{parent}Font-OPPOSans/OPPOSans-M.ttf"
-    (share/"fonts").install "#{parent}Font-OPPOSans/OPPOSans-R.ttf"
+    (share/"fonts").install "#{parent}3.0 Designer_分级/OPlusSans3-ExtraLight.ttf"
+    (share/"fonts").install "#{parent}3.0 Designer_分级/OPlusSans3-Light.ttf"
+    (share/"fonts").install "#{parent}3.0 Designer_分级/OPlusSans3-Regular.ttf"
+    (share/"fonts").install "#{parent}3.0 Designer_分级/OPlusSans3-Medium.ttf"
+    (share/"fonts").install "#{parent}3.0 Designer_分级/OPlusSans3-Bold.ttf"
   end
   # No zap stanza required
 
