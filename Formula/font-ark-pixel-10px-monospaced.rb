fb07caa332d14627c8ff01e844b9b2c6b81a4b4e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-03-23 00:18:16 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ark-pixel-10px-monospaced.rb from d2ea45dd3c72254bf7396b863126f7a429fc4add

--- Formula/font-ark-pixel-10px-monospaced.rb
+++ Formula/font-ark-pixel-10px-monospaced.rb
@@ -1,6 +1,6 @@
 class FontArkPixel10pxMonospaced < Formula
-  version "2023.11.26"
-  sha256 "0ee398b6cc191d6f7d0d0c79ea23588b1e775dc810752979b0e89b8088e2323a"
+  version "2024.03.22"
+  sha256 "0e4c0a20e61d1c1247eaa52fba60c28be75c503c7bd8833140f26aef5d4e38b4"
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-monospaced-otf-v#{version}.zip"
   desc "Ark Pixel 10px Monospaced"
   desc "Open source Pan-CJK pixel font"
