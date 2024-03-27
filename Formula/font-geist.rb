2e29f4a40b732d30fde1554c609e7614fea8a564
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-03-27 00:18:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-geist.rb from 4c862a0fb05d9b705c9393b53cc949e4f52c0173

--- Formula/font-geist.rb
+++ Formula/font-geist.rb
@@ -1,22 +1,22 @@
 class FontGeist < Formula
-  version "1.1.0"
-  sha256 "8f96b6e69f416706cec3293ceaa4cbb41a981cba7a32ed7c92dff14a5a44497e"
-  url "https://github.com/vercel/geist-font/releases/download/#{version}/Geist.zip", verified: "github.com/vercel/geist-font/"
+  version "1.3.0"
+  sha256 "6a656e5efc991a0b465bc288b5455eebd7219e0668a936f8705a2e9d3a2a62c9"
+  url "https://github.com/vercel/geist-font/releases/download/#{version}/Geist-#{version}.zip", verified: "github.com/vercel/geist-font/"
   desc "Geist"
   desc "Sans-serif typeface"
   homepage "https://vercel.com/font/sans"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}Geist/Geist-Black.otf"
-    (share/"fonts").install "#{parent}Geist/Geist-Bold.otf"
-    (share/"fonts").install "#{parent}Geist/Geist-Light.otf"
-    (share/"fonts").install "#{parent}Geist/Geist-Medium.otf"
-    (share/"fonts").install "#{parent}Geist/Geist-Regular.otf"
-    (share/"fonts").install "#{parent}Geist/Geist-SemiBold.otf"
-    (share/"fonts").install "#{parent}Geist/Geist-Thin.otf"
-    (share/"fonts").install "#{parent}Geist/Geist-UltraBlack.otf"
-    (share/"fonts").install "#{parent}Geist/Geist-UltraLight.otf"
-    (share/"fonts").install "#{parent}Geist/GeistVariableVF.ttf"
+    (share/"fonts").install "#{parent}Geist-#{version}/statics-otf/Geist-Black.otf"
+    (share/"fonts").install "#{parent}Geist-#{version}/statics-otf/Geist-Bold.otf"
+    (share/"fonts").install "#{parent}Geist-#{version}/statics-otf/Geist-Light.otf"
+    (share/"fonts").install "#{parent}Geist-#{version}/statics-otf/Geist-Medium.otf"
+    (share/"fonts").install "#{parent}Geist-#{version}/statics-otf/Geist-Regular.otf"
+    (share/"fonts").install "#{parent}Geist-#{version}/statics-otf/Geist-SemiBold.otf"
+    (share/"fonts").install "#{parent}Geist-#{version}/statics-otf/Geist-Thin.otf"
+    (share/"fonts").install "#{parent}Geist-#{version}/statics-otf/Geist-UltraBlack.otf"
+    (share/"fonts").install "#{parent}Geist-#{version}/statics-otf/Geist-UltraLight.otf"
+    (share/"fonts").install "#{parent}Geist-#{version}/variable-ttf/GeistVF.ttf"
   end
   # No zap stanza required
 
