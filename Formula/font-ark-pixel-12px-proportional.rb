32164176ea2d11962a270f86c252734c8a6ca5a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-03-28 00:18:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ark-pixel-12px-proportional.rb from e53d5e3f1a2b1f74a31c73d3d41f395afc6fc514

--- Formula/font-ark-pixel-12px-proportional.rb
+++ Formula/font-ark-pixel-12px-proportional.rb
@@ -1,6 +1,6 @@
 class FontArkPixel12pxProportional < Formula
-  version "2024.03.22"
-  sha256 "2cfd6acacc5ca3c0e3330e71956f2709a3d5a0f8e0649f97b2b904fe552a97a1"
+  version "2024.03.27"
+  sha256 "4a9587a15ecf14c15546d99d00c2d9c41264fd9f27eed98138b0e56e3e74121b"
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-12px-proportional-otf-v#{version}.zip"
   desc "Ark Pixel 12px Proportional"
   desc "Open source Pan-CJK pixel font"
