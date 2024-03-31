9fa5086e7dc3916385c79ebf14349a382d754dd5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-03-31 00:21:27 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-ss15.rb from f95be5fbab7059f2d3fe239cbbea016ec5830efc

--- Formula/font-iosevka-ss15.rb
+++ Formula/font-iosevka-ss15.rb
@@ -1,6 +1,6 @@
 class FontIosevkaSs15 < Formula
-  version "29.0.4"
-  sha256 "99533ad83ee547f70b5874eb36438be6c5fe0b60842578f48e7b134b3f621b43"
+  version "29.0.5"
+  sha256 "86424bf1de6c5cb7e81946cdbb299c3839a6574979b668cfceac21af32c032a4"
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaSS15-#{version}.zip"
   desc "Iosevka SS15"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
