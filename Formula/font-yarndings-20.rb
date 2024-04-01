67e000fb9030ec75707cd26a6e3f9ed1194bf51b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-01 00:21:23 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-yarndings-20.rb from 89f6b3ec29ab700e8e5dd2c5ae821a5f35bca7af

--- Formula/font-yarndings-20.rb
+++ Formula/font-yarndings-20.rb
@@ -1,7 +1,7 @@
 class FontYarndings20 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yarndings20/Yarndings20-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/yarndings20/Yarndings20-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Yarndings 20"
-  homepage "https://github.com/scfried/soft-type-yarndings"
+  homepage "https://fonts.google.com/specimen/Yarndings+20"
   def install
     (share/"fonts").install "Yarndings20-Regular.ttf"
   end
