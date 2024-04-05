ff00b8e10ce8ece15e04de7490e1660a28f4a947
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-05 00:18:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-iosevka-term-slab-nerd-font.rb from 45cb72e3886da05c5f0979aa0d734b3810811168

--- Formula/font-iosevka-term-slab-nerd-font.rb
+++ Formula/font-iosevka-term-slab-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontIosevkaTermSlabNerdFont < Formula
-  version "3.1.1"
-  sha256 "32655d4b3c38be3732abfede2e6bcb5d71079be0b3d424e9474b65a79188e75b"
+  version "3.2.0"
+  sha256 "4163dd3aef352aff93749eb09dbea2a8f6ed5293fb2bebfeb6123495f83cd944"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/IosevkaTermSlab.zip"
   desc "IosevkaTermSlab Nerd Font (Iosevka Term Slab)"
   desc "Developer targeted fonts with a high number of glyphs"
