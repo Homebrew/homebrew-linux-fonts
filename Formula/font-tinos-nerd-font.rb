dc37c04403eddff295d04861b934ed3872180592
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-05 00:18:58 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-tinos-nerd-font.rb from 45cb72e3886da05c5f0979aa0d734b3810811168

--- Formula/font-tinos-nerd-font.rb
+++ Formula/font-tinos-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontTinosNerdFont < Formula
-  version "3.1.1"
-  sha256 "be3072b0af54b699a99385a74572a2f4b6769d6960cbcdfc691db2b22b257bfd"
+  version "3.2.0"
+  sha256 "f747cc45c12360ce01f2400ecbbda73944631d7a92ecb635928047703317fa90"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Tinos.zip"
   desc "Tinos Nerd Font (Tinos)"
   desc "Developer targeted fonts with a high number of glyphs"
