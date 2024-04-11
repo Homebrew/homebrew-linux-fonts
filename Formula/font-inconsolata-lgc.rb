7b654eb13ce283022196b9bffd76e943f2be0e17
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-11 00:19:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-lgc.rb from 7d3af93d84aeb49e231f5fad7a2fde22b895cbfc

--- Formula/font-inconsolata-lgc.rb
+++ Formula/font-inconsolata-lgc.rb
@@ -1,12 +1,16 @@
 class FontInconsolataLgc < Formula
-  head "https://github.com/DeLaGuardo/Inconsolata-LGC.git"
+  version "1.5.2"
+  sha256 "8711ada8ff5b34a5c3930a0c502b4fbc90b4b433d3ddfcb13313cea33bffc549"
+  url "https://github.com/MihailJP/Inconsolata-LGC/releases/download/LGC-#{version}/InconsolataLGC-#{version}.tar.xz"
   desc "Inconsolata LGC"
-  homepage "https://github.com/DeLaGuardo/Inconsolata-LGC"
+  desc "Inconsolata LGC is a modified version of Inconsolata with Cyrillic alphabet"
+  homepage "https://github.com/MihailJP/Inconsolata-LGC"
   def install
-    (share/"fonts").install "inconsolatalgc.ttf"
-    (share/"fonts").install "inconsolatalgcbold.ttf"
-    (share/"fonts").install "inconsolatalgcbolditalic.ttf"
-    (share/"fonts").install "inconsolatalgcitalic.ttf"
+    parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
+    (share/"fonts").install "#{parent}InconsolataLGC/Inconsolata-LGC.ttf"
+    (share/"fonts").install "#{parent}InconsolataLGC/Inconsolata-LGC-Bold.ttf"
+    (share/"fonts").install "#{parent}InconsolataLGC/Inconsolata-LGC-Italic.ttf"
+    (share/"fonts").install "#{parent}InconsolataLGC/Inconsolata-LGC-BoldItalic.ttf"
   end
   # No zap stanza required
 
