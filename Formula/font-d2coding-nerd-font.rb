eeb70ea3f56ae94fb89e84935d421386d68ea95d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-13 00:16:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-d2coding-nerd-font.rb from 70c6ad3ca45f16b36a065e88898d28bcd695a609

--- Formula/font-d2coding-nerd-font.rb
+++ Formula/font-d2coding-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontD2codingNerdFont < Formula
-  version "3.2.0"
-  sha256 "36ddd0942c3178cd0aafa0c5b69da4e65257a198464019cae956ae6380586d99"
+  version "3.2.1"
+  sha256 "d7c2ec38d316583c8af0319068ac05488f0c053cccea842aae6089f3d64d758f"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/D2Coding.zip"
   desc "D2CodingLigature Nerd Font (D2Coding)"
   desc "Developer targeted fonts with a high number of glyphs"
