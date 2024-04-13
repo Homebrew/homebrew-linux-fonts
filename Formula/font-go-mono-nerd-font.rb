316b20b18f235da58ba2e25740f8c289b281fbcd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-13 00:16:45 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-go-mono-nerd-font.rb from 70c6ad3ca45f16b36a065e88898d28bcd695a609

--- Formula/font-go-mono-nerd-font.rb
+++ Formula/font-go-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontGoMonoNerdFont < Formula
-  version "3.2.0"
-  sha256 "85862c3a7816bb33ec7d4a5066e7e90f59283301ce306a42159dd8c64ea8fda7"
+  version "3.2.1"
+  sha256 "1440419676af8287b1e707ec1d2846880b6745524099f566b34ba7facd28d84a"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Go-Mono.zip"
   desc "GoMono Nerd Font (Go Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
