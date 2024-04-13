474b7bbdc3f18468991357e8ddac76614b5f2318
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-13 00:16:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-arimo-nerd-font.rb from 70c6ad3ca45f16b36a065e88898d28bcd695a609

--- Formula/font-arimo-nerd-font.rb
+++ Formula/font-arimo-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontArimoNerdFont < Formula
-  version "3.2.0"
-  sha256 "553ee5ef343aaae8b47e24f0b3cbd2340f12d5432ceacf74266c3fe4d51eeba7"
+  version "3.2.1"
+  sha256 "1a7a66238590e8edf86aa749527859604639bca467e4ae9bcd290ae5365360e4"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Arimo.zip"
   desc "Arimo Nerd Font (Arimo)"
   desc "Developer targeted fonts with a high number of glyphs"
