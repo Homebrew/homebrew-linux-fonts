4ccd08bde9ee49c4be6ea052637b148d02b93048
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-13 00:16:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-symbols-only-nerd-font.rb from 70c6ad3ca45f16b36a065e88898d28bcd695a609

--- Formula/font-symbols-only-nerd-font.rb
+++ Formula/font-symbols-only-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontSymbolsOnlyNerdFont < Formula
-  version "3.2.0"
-  sha256 "025e64ad8afacb11cbb2bd14b1aea97966ccf0b67dd8ffa1cf4f179cdbd2863c"
+  version "3.2.1"
+  sha256 "bc59c2ea74d022a6262ff9e372fde5c36cd5ae3f82a567941489ecfab4f03d66"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/NerdFontsSymbolsOnly.zip"
   desc "Symbols Nerd Font (Symbols Only)"
   desc "Developer targeted fonts with a high number of glyphs"
