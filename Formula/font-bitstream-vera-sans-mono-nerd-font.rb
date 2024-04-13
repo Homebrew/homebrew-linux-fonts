53df7eaf1fa538fa95eb19bbc75cf05dad2407c4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-13 00:16:44 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-bitstream-vera-sans-mono-nerd-font.rb from 70c6ad3ca45f16b36a065e88898d28bcd695a609

--- Formula/font-bitstream-vera-sans-mono-nerd-font.rb
+++ Formula/font-bitstream-vera-sans-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 class FontBitstreamVeraSansMonoNerdFont < Formula
-  version "3.2.0"
-  sha256 "283bfa5a0209176ce4c8de3b5d3b0e769447b6948c9ec3d38b2ad6ec96ea103f"
+  version "3.2.1"
+  sha256 "6a5d20c72f068a65ebf6e1cf1e168ced5d8c78d703d90b6041a5232db8111205"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BitstreamVeraSansMono.zip"
   desc "BitstromWera Nerd Font (Bitstream Vera Sans Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
