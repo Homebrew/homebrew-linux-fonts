f53de76fefc9d4fa460ebb49755b73395b29a838
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-15 02:35:30 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-micro-5-charted.rb from 60ae74cbc787a715ea334da4f37dd368607f720f

--- Formula/font-micro-5-charted.rb
+++ Formula/font-micro-5-charted.rb
@@ -1,7 +1,7 @@
 class FontMicro5Charted < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/micro5charted/Micro5Charted-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/micro5charted/Micro5Charted-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Micro 5 Charted"
-  homepage "https://github.com/scfried/soft-type-micro"
+  homepage "https://fonts.google.com/specimen/Micro+5+Charted"
   def install
     (share/"fonts").install "Micro5Charted-Regular.ttf"
   end
