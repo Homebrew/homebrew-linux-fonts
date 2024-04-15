656caf534e8bd55b10a0164fa141a632ba48d085
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-15 02:35:30 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jersey-20-charted.rb from 60ae74cbc787a715ea334da4f37dd368607f720f

--- Formula/font-jersey-20-charted.rb
+++ Formula/font-jersey-20-charted.rb
@@ -1,7 +1,7 @@
 class FontJersey20Charted < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jersey20charted/Jersey20Charted-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/jersey20charted/Jersey20Charted-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Jersey 20 Charted"
-  homepage "https://github.com/scfried/soft-type-jersey"
+  homepage "https://fonts.google.com/specimen/Jersey+20+Charted"
   def install
     (share/"fonts").install "Jersey20Charted-Regular.ttf"
   end
