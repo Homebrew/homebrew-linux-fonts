4f0beef3e67e7ae88afdd207473c37c50835983d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-15 02:35:30 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-sedan.rb from 60ae74cbc787a715ea334da4f37dd368607f720f

--- Formula/font-sedan.rb
+++ Formula/font-sedan.rb
@@ -1,7 +1,7 @@
 class FontSedan < Formula
   head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sedan"
   desc "Sedan"
-  homepage "https://tipotype.com/underground/sedan/"
+  homepage "https://fonts.google.com/specimen/Sedan"
   def install
     (share/"fonts").install "ofl/sedan/" + "Sedan-Italic.ttf"
     (share/"fonts").install "ofl/sedan/" + "Sedan-Regular.ttf"
