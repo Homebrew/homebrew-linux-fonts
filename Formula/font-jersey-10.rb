ecb85f360f232e72d3fd43347ebaf8f87ef50851
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-15 02:35:29 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jersey-10.rb from 60ae74cbc787a715ea334da4f37dd368607f720f

--- Formula/font-jersey-10.rb
+++ Formula/font-jersey-10.rb
@@ -1,7 +1,7 @@
 class FontJersey10 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jersey10/Jersey10-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/jersey10/Jersey10-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Jersey 10"
-  homepage "https://github.com/scfried/soft-type-jersey"
+  homepage "https://fonts.google.com/specimen/Jersey+10"
   def install
     (share/"fonts").install "Jersey10-Regular.ttf"
   end
