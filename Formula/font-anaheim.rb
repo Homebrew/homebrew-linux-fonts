925509d2f7dd0cf4766e4959311ebd33079e63e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-29 00:19:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-anaheim.rb from b988e86ec361197a70814910f95ff7b6276a9b9b

--- Formula/font-anaheim.rb
+++ Formula/font-anaheim.rb
@@ -1,9 +1,9 @@
 class FontAnaheim < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anaheim/Anaheim-Regular.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/anaheim/Anaheim%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Anaheim"
   homepage "https://fonts.google.com/specimen/Anaheim"
   def install
-    (share/"fonts").install "Anaheim-Regular.ttf"
+    (share/"fonts").install "Anaheim[wght].ttf"
   end
   # No zap stanza required
 
