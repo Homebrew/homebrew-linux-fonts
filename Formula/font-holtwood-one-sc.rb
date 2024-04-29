1c3fd7e499af021451d01d1ac94c20e83e594d36
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-04-29 00:19:26 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-holtwood-one-sc.rb from b988e86ec361197a70814910f95ff7b6276a9b9b

--- Formula/font-holtwood-one-sc.rb
+++ Formula/font-holtwood-one-sc.rb
@@ -1,9 +1,9 @@
 class FontHoltwoodOneSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/holtwoodonesc/HoltwoodOneSC.ttf", verified: "github.com/google/fonts/"
+  head "https://github.com/google/fonts/raw/main/ofl/holtwoodonesc/HoltwoodOneSC-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Holtwood One SC"
   homepage "https://fonts.google.com/specimen/Holtwood+One+SC"
   def install
-    (share/"fonts").install "HoltwoodOneSC.ttf"
+    (share/"fonts").install "HoltwoodOneSC-Regular.ttf"
   end
   # No zap stanza required
 
