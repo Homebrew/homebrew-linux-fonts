75a74501af17b87a8d74f8835e93c77efa20f254
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-01 00:20:47 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-lxgw-bright-gb.rb from 77a6fb7d2e4a87b3dc3c722547962c97cd32ef3f

--- Formula/font-lxgw-bright-gb.rb
+++ Formula/font-lxgw-bright-gb.rb
@@ -1,18 +1,18 @@
 class FontLxgwBrightGb < Formula
-  version "5.321"
-  sha256 "bf7d298c1dc17fe05ea99bf1459b753a32641aec7faabeb92b4f68b171ea21a9"
+  version "5.330"
+  sha256 "f9378f55d7c77216be7b430ec5b9a834d89e92a16fd640eb5a32d73b43adc3a0"
   url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightGB.7z"
   desc "LXGW Bright GB"
   desc "Merged font of Ysabeau Office and LXGW WenKai GB"
   homepage "https://github.com/lxgw/LxgwBright"
   def install
     parent = File.dirname(Dir.pwd) != (ENV['HOMEBREW_TEMP'] || '/tmp') ?  '../' : ''
-    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-Medium.otf"
-    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-MediumItalic.otf"
-    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-Regular.otf"
-    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-Italic.otf"
-    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-SemiLight.otf"
-    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-SemiLightItalic.otf"
+    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-Medium.ttf"
+    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-MediumItalic.ttf"
+    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-Regular.ttf"
+    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-Italic.ttf"
+    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-SemiLight.ttf"
+    (share/"fonts").install "#{parent}LXGWBrightGB/LXGWBrightGB-SemiLightItalic.ttf"
   end
   # No zap stanza required
 
