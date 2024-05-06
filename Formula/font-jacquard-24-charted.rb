b32c7f4b2a94a9b2d79a049aebd7e258afec26d8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-06 00:19:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jacquard-24-charted.rb from b311f2552da11748df7efa43231a6c0788843c02

--- Formula/font-jacquard-24-charted.rb
+++ Formula/font-jacquard-24-charted.rb
@@ -1,7 +1,7 @@
 class FontJacquard24Charted < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jacquard24charted/Jacquard24Charted-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/jacquard24charted/Jacquard24Charted-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Jacquard 24 Charted"
-  homepage "https://github.com/scfried/soft-type-jacquard"
+  homepage "https://fonts.google.com/specimen/Jacquard+24+Charted"
   def install
     (share/"fonts").install "Jacquard24Charted-Regular.ttf"
   end
