0acdd14609109d6dee63b21c1991797cc0cd8fd9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-06 00:19:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-radio-canada-big.rb from b311f2552da11748df7efa43231a6c0788843c02

--- Formula/font-radio-canada-big.rb
+++ Formula/font-radio-canada-big.rb
@@ -1,8 +1,8 @@
 class FontRadioCanadaBig < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/radiocanadabig"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/radiocanadabig"
   desc "Radio Canada Big"
   desc "Variable font with a weight axis that spans from regular (400) to bold (700)"
-  homepage "https://github.com/googlefonts/radiocanadadisplay"
+  homepage "https://fonts.google.com/specimen/Radio+Canada+Big"
   def install
     (share/"fonts").install "ofl/radiocanadabig/" + "RadioCanadaBig-Italic[wght].ttf"
     (share/"fonts").install "ofl/radiocanadabig/" + "RadioCanadaBig[wght].ttf"
