90d117051f3e37b654a493444ccd9145636331f5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-06 00:19:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jaro.rb from b311f2552da11748df7efa43231a6c0788843c02

--- Formula/font-jaro.rb
+++ Formula/font-jaro.rb
@@ -1,8 +1,8 @@
 class FontJaro < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jaro/Jaro%5Bopsz%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/jaro/Jaro%5Bopsz%5D.ttf", verified: "github.com/google/fonts/"
   desc "Jaro"
   desc "Global display typeface inspired by the work of jaroslav benda"
-  homepage "https://github.com/agyeiarcher/Jaro"
+  homepage "https://fonts.google.com/specimen/Jaro"
   def install
     (share/"fonts").install "Jaro[opsz].ttf"
   end
