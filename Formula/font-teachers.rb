bd8ba706acbae3f5554c47eb9594842b7b878e68
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-06 00:19:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-teachers.rb from b311f2552da11748df7efa43231a6c0788843c02

--- Formula/font-teachers.rb
+++ Formula/font-teachers.rb
@@ -1,7 +1,7 @@
 class FontTeachers < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/teachers"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/teachers"
   desc "Teachers"
-  homepage "https://github.com/chankfonts/Teachers-fonts"
+  homepage "https://fonts.google.com/specimen/Teachers"
   def install
     (share/"fonts").install "ofl/teachers/" + "Teachers-Italic[wght].ttf"
     (share/"fonts").install "ofl/teachers/" + "Teachers[wght].ttf"
