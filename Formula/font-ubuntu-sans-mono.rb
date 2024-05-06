b0adeffe0718d0e6245b007919f964b45b39f132
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-06 00:19:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu-sans-mono.rb from b311f2552da11748df7efa43231a6c0788843c02

--- Formula/font-ubuntu-sans-mono.rb
+++ Formula/font-ubuntu-sans-mono.rb
@@ -1,7 +1,7 @@
 class FontUbuntuSansMono < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ufl/ubuntusansmono"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ufl/ubuntusansmono"
   desc "Ubuntu Sans Mono"
-  homepage "https://github.com/canonical/Ubuntu-Sans-Mono-fonts"
+  homepage "https://fonts.google.com/specimen/Ubuntu+Sans+Mono"
   def install
     (share/"fonts").install "ufl/ubuntusansmono/" + "UbuntuSansMono-Italic[wght].ttf"
     (share/"fonts").install "ufl/ubuntusansmono/" + "UbuntuSansMono[wght].ttf"
