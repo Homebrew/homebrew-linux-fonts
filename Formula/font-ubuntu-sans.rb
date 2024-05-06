9f96612c3bb7aea5e33d36256ba078d7b3eaebb2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-06 00:19:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-ubuntu-sans.rb from b311f2552da11748df7efa43231a6c0788843c02

--- Formula/font-ubuntu-sans.rb
+++ Formula/font-ubuntu-sans.rb
@@ -1,7 +1,7 @@
 class FontUbuntuSans < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ufl/ubuntusans"
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ufl/ubuntusans"
   desc "Ubuntu Sans"
-  homepage "https://github.com/canonical/Ubuntu-Sans-fonts"
+  homepage "https://fonts.google.com/specimen/Ubuntu+Sans"
   def install
     (share/"fonts").install "ufl/ubuntusans/" + "UbuntuSans-Italic[wdth,wght].ttf"
     (share/"fonts").install "ufl/ubuntusans/" + "UbuntuSans[wdth,wght].ttf"
