5cd632a35cca55961a1e68d008ae8ecd655286c1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-06 00:19:52 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-reddit-sans-condensed.rb from b311f2552da11748df7efa43231a6c0788843c02

--- Formula/font-reddit-sans-condensed.rb
+++ Formula/font-reddit-sans-condensed.rb
@@ -1,8 +1,8 @@
 class FontRedditSansCondensed < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/redditsanscondensed/RedditSansCondensed%5Bwght%5D.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/redditsanscondensed/RedditSansCondensed%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
   desc "Reddit Sans Condensed"
   desc "Complemented by reddit sans and reddit mono"
-  homepage "https://github.com/reddit/redditsans"
+  homepage "https://fonts.google.com/specimen/Reddit+Sans+Condensed"
   def install
     (share/"fonts").install "RedditSansCondensed[wght].ttf"
   end
