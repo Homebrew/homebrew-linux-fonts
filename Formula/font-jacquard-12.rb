63690f2b63366df0a75bbcf7c4257cdc41476d21
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-15 00:19:46 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-jacquard-12.rb from 3cbab22b8139d3fc0c16a2c1cb6c5f4147749fc9

--- Formula/font-jacquard-12.rb
+++ Formula/font-jacquard-12.rb
@@ -1,7 +1,7 @@
 class FontJacquard12 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jacquard12/Jacquard12-Regular.ttf"
+  head "https://github.com/google/fonts/raw/main/ofl/jacquard12/Jacquard12-Regular.ttf", verified: "github.com/google/fonts/"
   desc "Jacquard 12"
-  homepage "https://github.com/scfried/soft-type-jacquard"
+  homepage "https://fonts.google.com/specimen/Jacquard+12"
   def install
     (share/"fonts").install "Jacquard12-Regular.ttf"
   end
