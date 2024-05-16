0c2d36f16b2972911289518dfed30885605b4685
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-16 00:19:08 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-fira-mono-nerd-font.rb from 70c6ad3ca45f16b36a065e88898d28bcd695a609

--- Formula/font-fira-mono-nerd-font.rb
+++ Formula/font-fira-mono-nerd-font.rb
@@ -6,15 +6,15 @@ class FontFiraMonoNerdFont < Formula
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   def install
-    (share/"fonts").install "FiraMono/" + "FiraMonoNerdFont-Bold.otf"
-    (share/"fonts").install "FiraMono/" + "FiraMonoNerdFont-Medium.otf"
-    (share/"fonts").install "FiraMono/" + "FiraMonoNerdFont-Regular.otf"
-    (share/"fonts").install "FiraMono/" + "FiraMonoNerdFontMono-Bold.otf"
-    (share/"fonts").install "FiraMono/" + "FiraMonoNerdFontMono-Medium.otf"
-    (share/"fonts").install "FiraMono/" + "FiraMonoNerdFontMono-Regular.otf"
-    (share/"fonts").install "FiraMono/" + "FiraMonoNerdFontPropo-Bold.otf"
-    (share/"fonts").install "FiraMono/" + "FiraMonoNerdFontPropo-Medium.otf"
-    (share/"fonts").install "FiraMono/" + "FiraMonoNerdFontPropo-Regular.otf"
+    (share/"fonts").install "FiraMonoNerdFont-Bold.otf"
+    (share/"fonts").install "FiraMonoNerdFont-Medium.otf"
+    (share/"fonts").install "FiraMonoNerdFont-Regular.otf"
+    (share/"fonts").install "FiraMonoNerdFontMono-Bold.otf"
+    (share/"fonts").install "FiraMonoNerdFontMono-Medium.otf"
+    (share/"fonts").install "FiraMonoNerdFontMono-Regular.otf"
+    (share/"fonts").install "FiraMonoNerdFontPropo-Bold.otf"
+    (share/"fonts").install "FiraMonoNerdFontPropo-Medium.otf"
+    (share/"fonts").install "FiraMonoNerdFontPropo-Regular.otf"
   end
   # No zap stanza required
 
