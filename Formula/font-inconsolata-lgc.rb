6c2283251ed3924c6858c7aa4594b1c990af38cd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-20 00:20:06 +0000

    import https://github.com/homebrew/homebrew-cask-fonts/blob/master/Casks/font-inconsolata-lgc.rb from

--- Formula/font-inconsolata-lgc.rb
+++ Formula/font-inconsolata-lgc.rb
@@ -1,6 +1,6 @@
 class FontInconsolataLgc < Formula
-  version "1.7.2"
-  sha256 "e771a9375994909434cf1d1d1adb8956ca994bc4ea0964f60c64cf7a0b9ecf1d"
+  version "1.8.0"
+  sha256 "d9135f9c40abc8d2673f0415575ded31d0fd89a7f36f778570faf5fe2952e4f1"
   url "https://github.com/MihailJP/Inconsolata-LGC/releases/download/LGC-#{version}/InconsolataLGC-#{version}.tar.xz"
   desc "Inconsolata LGC"
   desc "Inconsolata LGC is a modified version of Inconsolata with Cyrillic alphabet"
