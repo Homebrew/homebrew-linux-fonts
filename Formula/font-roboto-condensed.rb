3496a5868d00d29647834c4d2c8b3561e37dfcce
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:43:02 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-r/font-roboto-condensed.rb from

---
+++ Formula/font-roboto-condensed.rb
@@ -0,0 +1,13 @@
+class FontRobotoCondensed < Formula
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/robotocondensed"
+  desc "Roboto Condensed"
+  homepage "https://fonts.google.com/specimen/Roboto+Condensed"
+  def install
+    (share/"fonts").install Dir.glob("ofl/robotocondensed/./**/RobotoCondensed-Italic\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/robotocondensed/./**/RobotoCondensed\\[wght\\].ttf")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
