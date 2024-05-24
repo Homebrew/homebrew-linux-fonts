87df689fc538f0aa61833bef8fe57e926b5dc224
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:42:46 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-i/font-iosevka-ss04.rb from

---
+++ Formula/font-iosevka-ss04.rb
@@ -0,0 +1,23 @@
+class FontIosevkaSs04 < Formula
+  version "30.0.1"
+  sha256 "b8a360cdf997a6cdf1e9e282e229ce54cbec0ecfae8e8a1074fbc10a15b31725"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaSS04-#{version}.zip"
+  desc "Iosevka SS04"
+  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  homepage "https://github.com/be5invis/Iosevka/"
+  def install
+    (share/"fonts").install Dir.glob("./**/IosevkaSS04-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS04-ExtraBold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS04-ExtraLight.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS04-Heavy.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS04-Light.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS04-Medium.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS04-Regular.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS04-SemiBold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS04-Thin.ttc")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
