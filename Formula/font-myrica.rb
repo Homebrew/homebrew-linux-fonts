bc7ec1cee5caa4e9832873248c09e959f43c8e4d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:42:52 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-m/font-myrica.rb from

---
+++ Formula/font-myrica.rb
@@ -0,0 +1,15 @@
+class FontMyrica < Formula
+  version "2.006.20150301"
+  sha256 "a90eb9b79885f02ad9e0e752a0b979b699847be7de13dc3b6113658f006d12bd"
+  url "https://github.com/tomokuni/Myrica/archive/refs/tags/#{version}.tar.gz", verified: "github.com/tomokuni/Myrica/"
+  desc "Myrica"
+  desc "Programming font"
+  homepage "https://myrica.estable.jp/"
+  def install
+    (share/"fonts").install Dir.glob("Myrica-#{version}/**/Myrica.TTC")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
