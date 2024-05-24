1426983b89fc79198c31ed5811b251c203761abf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:43:06 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-s/font-sura.rb from

---
+++ Formula/font-sura.rb
@@ -0,0 +1,13 @@
+class FontSura < Formula
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sura"
+  desc "Sura"
+  homepage "https://fonts.google.com/specimen/Sura"
+  def install
+    (share/"fonts").install Dir.glob("ofl/sura/./**/Sura-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sura/./**/Sura-Regular.ttf")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
