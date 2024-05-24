8598050e9072322b49e32a23bfe4ee00c513fb0d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:42:57 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-n/font-noto-serif-cjk.rb from

---
+++ Formula/font-noto-serif-cjk.rb
@@ -0,0 +1,15 @@
+class FontNotoSerifCjk < Formula
+  version "2.002"
+  sha256 "e78b23ea9f803d8376eee66d330a556e1133b713ff97014a75991d5723324f85"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Serif#{version}/01_NotoSerifCJK.ttc.zip"
+  desc "Noto Serif CJK"
+  desc "Static Super OTC"
+  homepage "https://github.com/notofonts/noto-cjk/tree/main/Serif"
+  def install
+    (share/"fonts").install Dir.glob("./**/NotoSerifCJK.ttc")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
