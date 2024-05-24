cb3f0c1c02ae04d50490223e5de5bd9f5d2b0594
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:42:58 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-o/font-open-iconic.rb from

---
+++ Formula/font-open-iconic.rb
@@ -0,0 +1,14 @@
+class FontOpenIconic < Formula
+  version "1.1.1"
+  sha256 "8acf49f08ae5a069935b48e6be20349c4e9f43fcfc773ea0aba5b972b5b3743c"
+  url "https://codeload.github.com/iconic/open-iconic/zip/#{version}", verified: "codeload.github.com/iconic/open-iconic/"
+  desc "Open Iconic"
+  homepage "https://useiconic.com/open/"
+  def install
+    (share/"fonts").install Dir.glob("open-iconic-#{version}/font/fonts/**/open-iconic.ttf")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
