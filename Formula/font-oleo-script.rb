4c62936b1b16be15915a0c76dabbd73b45c18cfd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:42:58 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-o/font-oleo-script.rb from

---
+++ Formula/font-oleo-script.rb
@@ -0,0 +1,13 @@
+class FontOleoScript < Formula
+  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/oleoscript"
+  desc "Oleo Script"
+  homepage "https://fonts.google.com/specimen/Oleo+Script"
+  def install
+    (share/"fonts").install Dir.glob("ofl/oleoscript/./**/OleoScript-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/oleoscript/./**/OleoScript-Regular.ttf")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
