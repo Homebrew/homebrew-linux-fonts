1707a71a02d02079d42105f407458e7ee53f0d6a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:42:36 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-a/font-anek-kannada.rb from

---
+++ Formula/font-anek-kannada.rb
@@ -0,0 +1,12 @@
+class FontAnekKannada < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/anekkannada/AnekKannada%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
+  desc "Anek Kannada"
+  homepage "https://fonts.google.com/specimen/Anek+Kannada"
+  def install
+    (share/"fonts").install Dir.glob("./**/AnekKannada\\[wdth,wght\\].ttf")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
