541f4375f00ac7404357919a1558114dc467f5a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:42:42 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-g/font-galindo.rb from

---
+++ Formula/font-galindo.rb
@@ -0,0 +1,12 @@
+class FontGalindo < Formula
+  head "https://github.com/google/fonts/raw/main/ofl/galindo/Galindo-Regular.ttf", verified: "github.com/google/fonts/"
+  desc "Galindo"
+  homepage "https://fonts.google.com/specimen/Galindo"
+  def install
+    (share/"fonts").install Dir.glob("./**/Galindo-Regular.ttf")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
