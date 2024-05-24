1b48e25b796ffe0a80070e87c7149894a8fd542b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-24 04:42:43 +0000

    import https://github.com/Homebrew/homebrew-cask/blob/master/Casks/font/font-g/font-gentium-book-basic.rb from

---
+++ Formula/font-gentium-book-basic.rb
@@ -0,0 +1,17 @@
+class FontGentiumBookBasic < Formula
+  version "1.102"
+  sha256 "2f1a2c5491d7305dffd3520c6375d2f3e14931ee35c6d8ae1e8f098bf1a7b3cc"
+  url "https://software.sil.org/downloads/r/gentium/GentiumBasic_#{version.to_s.gsub(".", "")}.zip"
+  desc "Gentium Book Basic"
+  homepage "https://software.sil.org/gentium/"
+  def install
+    (share/"fonts").install Dir.glob("GentiumBasic_#{version.to_s.gsub(".", "")}/**/GenBkBasB.ttf")[0]
+    (share/"fonts").install Dir.glob("GentiumBasic_#{version.to_s.gsub(".", "")}/**/GenBkBasBI.ttf")[0]
+    (share/"fonts").install Dir.glob("GentiumBasic_#{version.to_s.gsub(".", "")}/**/GenBkBasI.ttf")[0]
+    (share/"fonts").install Dir.glob("GentiumBasic_#{version.to_s.gsub(".", "")}/**/GenBkBasR.ttf")[0]
+  end
+  # No zap stanza required
+
+  test do
+  end
+end
