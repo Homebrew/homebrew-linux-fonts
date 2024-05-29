531f9a36f3e6a404e1f421d160d47dfb653c3d31
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-awesome-terminal-fonts updated

--- Formula/font-awesome-terminal-fonts.rb
+++ Formula/font-awesome-terminal-fonts.rb
@@ -1,14 +1,15 @@
 class FontAwesomeTerminalFonts < Formula
-  head "https://github.com/gabrielelana/awesome-terminal-fonts/archive/patching-strategy.tar.gz"
-  desc "Awesome Terminal Fonts"
+  desc "Awesome terminal fonts font"
   homepage "https://github.com/gabrielelana/awesome-terminal-fonts"
+  head "https://github.com/gabrielelana/awesome-terminal-fonts/archive/patching-strategy.tar.gz"
+
   def install
-    (share/"fonts").install Dir.glob("awesome-terminal-fonts-patching-strategy/patched/**/Droid+Sans+Mono+Awesome.ttf")[0]
-    (share/"fonts").install Dir.glob("awesome-terminal-fonts-patching-strategy/patched/**/Inconsolata+Awesome.ttf")[0]
-    (share/"fonts").install Dir.glob("awesome-terminal-fonts-patching-strategy/patched/**/SourceCodePro+Powerline+Awesome+Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/awesome-terminal-fonts-patching-strategy/patched/Droid+Sans+Mono+Awesome.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/awesome-terminal-fonts-patching-strategy/patched/Inconsolata+Awesome.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/awesome-terminal-fonts-patching-strategy/patched/SourceCodePro+Powerline+Awesome+Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
