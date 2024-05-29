2180003a42cba8ed2fb3f0983e2b42fb8397c9c4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-decovar-alpha updated

--- Formula/font-decovar-alpha.rb
+++ Formula/font-decovar-alpha.rb
@@ -1,12 +1,14 @@
 class FontDecovarAlpha < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/decovaralpha/DecovarAlpha-VF.ttf", verified: "github.com/google/fonts/"
-  desc "Decovar Alpha"
+  desc "Decovar alpha font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts/raw/main/ofl/decovaralpha/DecovarAlpha-VF.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DecovarAlpha-VF.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
