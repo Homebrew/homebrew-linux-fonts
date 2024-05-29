e185f8a1c0d5cea4f5f7ca8da573f9a532a0daff
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:32 +0000

    font-libre-baskerville updated

--- Formula/font-libre-baskerville.rb
+++ Formula/font-libre-baskerville.rb
@@ -1,14 +1,18 @@
 class FontLibreBaskerville < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/librebaskerville"
-  desc "Libre Baskerville"
+  desc "Libre baskerville font"
   homepage "https://fonts.google.com/specimen/Libre+Baskerville"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/librebaskerville"
+
   def install
     (share/"fonts").install Dir.glob("ofl/librebaskerville/./**/LibreBaskerville-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/librebaskerville/./**/LibreBaskerville-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/librebaskerville/./**/LibreBaskerville-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
