ed80bd74ee5cec71c023b261b1fd684ee391916d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-love-ya-like-a-sister updated

--- Formula/font-love-ya-like-a-sister.rb
+++ Formula/font-love-ya-like-a-sister.rb
@@ -1,12 +1,14 @@
 class FontLoveYaLikeASister < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/loveyalikeasister/LoveYaLikeASister.ttf", verified: "github.com/google/fonts/"
-  desc "Love Ya Like A Sister"
+  desc "Love ya like a sister font"
   homepage "https://fonts.google.com/specimen/Love+Ya+Like+A+Sister"
+  head "https://github.com/google/fonts/raw/main/ofl/loveyalikeasister/LoveYaLikeASister.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LoveYaLikeASister.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
