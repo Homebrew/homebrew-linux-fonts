1c4a006686e52d16b6551efa54e27d683c65f280
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:51 +0000

    font-noto-sans-kannada updated

--- Formula/font-noto-sans-kannada.rb
+++ Formula/font-noto-sans-kannada.rb
@@ -1,13 +1,15 @@
 class FontNotoSansKannada < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKannada-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Kannada"
+  desc "Noto sans kannada font"
   homepage "https://www.google.com/get/noto/#sans-knda"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKannada-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansKannada-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansKannada-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
