b630deebd783a09e93de053401dd771b4272f3ba
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-comic-shanns-mono-nerd-font updated

--- Formula/font-comic-shanns-mono-nerd-font.rb
+++ Formula/font-comic-shanns-mono-nerd-font.rb
@@ -1,10 +1,10 @@
 class FontComicShannsMonoNerdFont < Formula
-  version "3.2.1"
-  sha256 "46fff4ae86377c7d4d28343ec24566eeb9e58c700beedb272160f6ea3567519b"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ComicShannsMono.zip"
-  desc "ComicShannsMono Nerd Font (Comic Shanns Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/ComicShannsMono.zip"
+  version "3.2.1"
+  sha256 "46fff4ae86377c7d4d28343ec24566eeb9e58c700beedb272160f6ea3567519b"
+
   def install
     (share/"fonts").install Dir.glob("./**/ComicShannsMonoNerdFont-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/ComicShannsMonoNerdFont-Regular.otf")[0]
@@ -13,8 +13,8 @@ class FontComicShannsMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/ComicShannsMonoNerdFontPropo-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/ComicShannsMonoNerdFontPropo-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
