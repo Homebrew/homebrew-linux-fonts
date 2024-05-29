dacf78a1ae7ef961f78a1a4972a292c0ed54a66c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-didact-gothic updated

--- Formula/font-didact-gothic.rb
+++ Formula/font-didact-gothic.rb
@@ -1,12 +1,14 @@
 class FontDidactGothic < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/didactgothic/DidactGothic-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Didact Gothic"
+  desc "Didact gothic font"
   homepage "https://fonts.google.com/specimen/Didact+Gothic"
+  head "https://github.com/google/fonts/raw/main/ofl/didactgothic/DidactGothic-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DidactGothic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
