96652cc32111444045435e4503d4ceccd3c1adba
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-texturina updated

--- Formula/font-texturina.rb
+++ Formula/font-texturina.rb
@@ -1,14 +1,17 @@
 class FontTexturina < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/texturina"
-  desc "Texturina"
   desc "Designed by guillermo torres"
   homepage "https://fonts.google.com/specimen/Texturina"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/texturina"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/texturina/./**/Texturina-Italic\\[opsz,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/texturina/./**/Texturina\\[opsz,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/texturina/./**/Texturina-Italic[opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/texturina/./**/Texturina[opsz,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
