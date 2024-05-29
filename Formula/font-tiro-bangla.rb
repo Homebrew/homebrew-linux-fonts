a3cd1c54d5d2c444c8fd74a69a13193310b15235
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-tiro-bangla updated

--- Formula/font-tiro-bangla.rb
+++ Formula/font-tiro-bangla.rb
@@ -1,14 +1,17 @@
 class FontTiroBangla < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tirobangla"
-  desc "Tiro Bangla"
   desc "Suited to traditional literary publishing"
   homepage "https://fonts.google.com/specimen/Tiro+Bangla"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tirobangla"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tirobangla/./**/TiroBangla-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tirobangla/./**/TiroBangla-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
