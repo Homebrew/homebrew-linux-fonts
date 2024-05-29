7cf14cf6f69365452a2a11a514821f6312824d17
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-gantari updated

--- Formula/font-gantari.rb
+++ Formula/font-gantari.rb
@@ -1,14 +1,17 @@
 class FontGantari < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gantari"
-  desc "Gantari"
   desc "Designed so that all characters can look harmonious"
   homepage "https://fonts.google.com/specimen/Gantari"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gantari"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/gantari/./**/Gantari-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/gantari/./**/Gantari\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/gantari/./**/Gantari-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/gantari/./**/Gantari[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
