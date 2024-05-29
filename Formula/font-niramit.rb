ef45ad105cf9ec23414fcb01f517200080494fee
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-niramit updated

--- Formula/font-niramit.rb
+++ Formula/font-niramit.rb
@@ -1,9 +1,13 @@
 class FontNiramit < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/niramit"
-  desc "Niramit"
+  desc "Niramit font"
   homepage "https://fonts.google.com/specimen/Niramit"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/niramit"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-ExtraLightItalic.ttf")[0]
@@ -14,10 +18,10 @@ class FontNiramit < Formula
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
