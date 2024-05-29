4dc17f379c405b3fa29315cd3c3d3b4e045af4c2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-black-ops-one updated

--- Formula/font-black-ops-one.rb
+++ Formula/font-black-ops-one.rb
@@ -1,12 +1,14 @@
 class FontBlackOpsOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/blackopsone/BlackOpsOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Black Ops One"
+  desc "Black ops one font"
   homepage "https://fonts.google.com/specimen/Black+Ops+One"
+  head "https://github.com/google/fonts/raw/main/ofl/blackopsone/BlackOpsOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BlackOpsOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
