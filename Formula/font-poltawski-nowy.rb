e65a8cd37500dc02dd30453bef59e04ab1ad4d73
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:09 +0000

    font-poltawski-nowy updated

--- Formula/font-poltawski-nowy.rb
+++ Formula/font-poltawski-nowy.rb
@@ -1,13 +1,17 @@
 class FontPoltawskiNowy < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/poltawskinowy"
-  desc "Poltawski Nowy"
+  desc "Poltawski nowy font"
   homepage "https://fonts.google.com/specimen/Poltawski+Nowy"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/poltawskinowy"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/poltawskinowy/./**/PoltawskiNowy-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/poltawskinowy/./**/PoltawskiNowy\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/poltawskinowy/./**/PoltawskiNowy-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/poltawskinowy/./**/PoltawskiNowy[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
