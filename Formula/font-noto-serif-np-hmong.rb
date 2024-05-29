c091538bb0b90834df70e63286447f0712a0a73d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:00 +0000

    font-noto-serif-np-hmong updated

--- Formula/font-noto-serif-np-hmong.rb
+++ Formula/font-noto-serif-np-hmong.rb
@@ -1,12 +1,14 @@
 class FontNotoSerifNpHmong < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifnphmong/NotoSerifNPHmong%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif NP Hmong"
+  desc "Noto serif np hmong font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+NP+Hmong"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifnphmong/NotoSerifNPHmong%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifNPHmong\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifNPHmong[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
