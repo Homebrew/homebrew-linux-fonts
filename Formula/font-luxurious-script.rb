ef10f0cf8392cbe190e74a18d4535a0f353307ba
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-luxurious-script updated

--- Formula/font-luxurious-script.rb
+++ Formula/font-luxurious-script.rb
@@ -1,12 +1,14 @@
 class FontLuxuriousScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/luxuriousscript/LuxuriousScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Luxurious Script"
+  desc "Luxurious script font"
   homepage "https://fonts.google.com/specimen/Luxurious+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/luxuriousscript/LuxuriousScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LuxuriousScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
