1774c610c86456035e0ed73451f1a30c61dff65c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:42 +0000

    font-monomaniac-one updated

--- Formula/font-monomaniac-one.rb
+++ Formula/font-monomaniac-one.rb
@@ -1,13 +1,14 @@
 class FontMonomaniacOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/monomaniacone/MonomaniacOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Monomaniac One"
   desc "Display font with the mood of monospaced typefaces"
   homepage "https://fonts.google.com/specimen/Monomaniac+One"
+  head "https://github.com/google/fonts/raw/main/ofl/monomaniacone/MonomaniacOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MonomaniacOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
