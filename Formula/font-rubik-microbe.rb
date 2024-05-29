b074a23af3064f0f2c345c15eaf05b650b606d20
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-microbe updated

--- Formula/font-rubik-microbe.rb
+++ Formula/font-rubik-microbe.rb
@@ -1,12 +1,14 @@
 class FontRubikMicrobe < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikmicrobe/RubikMicrobe-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Microbe"
+  desc "Rubik microbe font"
   homepage "https://fonts.google.com/specimen/Rubik+Microbe"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikmicrobe/RubikMicrobe-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikMicrobe-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
