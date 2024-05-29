3136143bad783650491b57524fc1317e2a675245
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-cinzel-decorative updated

--- Formula/font-cinzel-decorative.rb
+++ Formula/font-cinzel-decorative.rb
@@ -1,14 +1,18 @@
 class FontCinzelDecorative < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cinzeldecorative"
-  desc "Cinzel Decorative"
+  desc "Cinzel decorative font"
   homepage "https://fonts.google.com/specimen/Cinzel+Decorative"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cinzeldecorative"
+
   def install
     (share/"fonts").install Dir.glob("ofl/cinzeldecorative/./**/CinzelDecorative-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cinzeldecorative/./**/CinzelDecorative-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cinzeldecorative/./**/CinzelDecorative-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
