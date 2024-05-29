d0f447a643b107e8f90e922f3d1572c5d3d714ca
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-nanum-myeongjo updated

--- Formula/font-nanum-myeongjo.rb
+++ Formula/font-nanum-myeongjo.rb
@@ -1,14 +1,18 @@
 class FontNanumMyeongjo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/nanummyeongjo"
-  desc "Nanum Myeongjo"
+  desc "Nanum myeongjo font"
   homepage "https://fonts.google.com/specimen/Nanum+Myeongjo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/nanummyeongjo"
+
   def install
     (share/"fonts").install Dir.glob("ofl/nanummyeongjo/./**/NanumMyeongjo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/nanummyeongjo/./**/NanumMyeongjo-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/nanummyeongjo/./**/NanumMyeongjo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
