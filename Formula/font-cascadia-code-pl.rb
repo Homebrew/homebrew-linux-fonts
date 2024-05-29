231af4f82e6419691044c2db8f0f30c8de05f8b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:52 +0000

    font-cascadia-code-pl updated

--- Formula/font-cascadia-code-pl.rb
+++ Formula/font-cascadia-code-pl.rb
@@ -1,16 +1,16 @@
 class FontCascadiaCodePl < Formula
-  version "2404.23"
-  sha256 "a911410626c0e09d03fa3fdda827188fda96607df50fecc3c5fee5906e33251b"
-  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
-  desc "Cascadia Code PL"
   desc "Version of Cascadia Code with embedded Powerline symbols"
   homepage "https://github.com/microsoft/cascadia-code"
+  url "https://github.com/microsoft/cascadia-code/releases/download/v2404.23/CascadiaCode-2404.23.zip"
+  version "2404.23"
+  sha256 "a911410626c0e09d03fa3fdda827188fda96607df50fecc3c5fee5906e33251b"
+
   def install
-    (share/"fonts").install Dir.glob("ttf/**/CascadiaCodePL.ttf")[0]
-    (share/"fonts").install Dir.glob("ttf/**/CascadiaCodePLItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ttf/CascadiaCodePL.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ttf/CascadiaCodePLItalic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
