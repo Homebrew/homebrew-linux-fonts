49e92a01ad94dde4392a6e82668ccf943628485f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-sofia updated

--- Formula/font-sofia.rb
+++ Formula/font-sofia.rb
@@ -1,12 +1,14 @@
 class FontSofia < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sofia/Sofia-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sofia"
+  desc "Sofia font"
   homepage "https://fonts.google.com/specimen/Sofia"
+  head "https://github.com/google/fonts/raw/main/ofl/sofia/Sofia-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Sofia-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
