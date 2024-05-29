9def194433ad27b7bc0566556d287fd9faf54028
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:33 +0000

    font-licorice updated

--- Formula/font-licorice.rb
+++ Formula/font-licorice.rb
@@ -1,13 +1,14 @@
 class FontLicorice < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/licorice/Licorice-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Licorice"
   desc "Perfect scrapbooking, cards, invitations and fun events"
   homepage "https://fonts.google.com/specimen/Licorice"
+  head "https://github.com/google/fonts/raw/main/ofl/licorice/Licorice-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Licorice-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
