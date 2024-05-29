016a9eee470cb99771b016ce79af28f818750ffb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-tex-gyre-adventor updated

--- Formula/font-tex-gyre-adventor.rb
+++ Formula/font-tex-gyre-adventor.rb
@@ -1,17 +1,18 @@
 class FontTexGyreAdventor < Formula
+  desc "Tex gyre adventor font"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor/qag2_501otf.zip"
   version "2.501"
   sha256 "9e619eb1c8af5cb55240f8bb865453562a2efd9059dee39d085fb71f7a00f7a2"
-  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor/qag#{version.to_s.gsub(".", "_")}otf.zip"
-  desc "TeX Gyre Adventor"
-  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor"
+
   def install
-    (share/"fonts").install Dir.glob("qag#{version.to_s.gsub(".", "_")}otf/**/texgyreadventor-bold.otf")[0]
-    (share/"fonts").install Dir.glob("qag#{version.to_s.gsub(".", "_")}otf/**/texgyreadventor-bolditalic.otf")[0]
-    (share/"fonts").install Dir.glob("qag#{version.to_s.gsub(".", "_")}otf/**/texgyreadventor-italic.otf")[0]
-    (share/"fonts").install Dir.glob("qag#{version.to_s.gsub(".", "_")}otf/**/texgyreadventor-regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/qag2_501otf/texgyreadventor-bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/qag2_501otf/texgyreadventor-bolditalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/qag2_501otf/texgyreadventor-italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/qag2_501otf/texgyreadventor-regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
