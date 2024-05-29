e7f77133d68809b4716f3ae206544bdf76f9766b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-gentium-basic updated

--- Formula/font-gentium-basic.rb
+++ Formula/font-gentium-basic.rb
@@ -1,17 +1,18 @@
 class FontGentiumBasic < Formula
+  desc "Gentium basic font"
+  homepage "https://software.sil.org/gentium/"
+  url "https://software.sil.org/downloads/r/gentium/GentiumBasic_1102.zip"
   version "1.102"
   sha256 "2f1a2c5491d7305dffd3520c6375d2f3e14931ee35c6d8ae1e8f098bf1a7b3cc"
-  url "https://software.sil.org/downloads/r/gentium/GentiumBasic_#{version.to_s.gsub(".", "")}.zip"
-  desc "Gentium Basic"
-  homepage "https://software.sil.org/gentium/"
+
   def install
-    (share/"fonts").install Dir.glob("GentiumBasic_#{version.to_s.gsub(".", "")}/**/GenBasB.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumBasic_#{version.to_s.gsub(".", "")}/**/GenBasBI.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumBasic_#{version.to_s.gsub(".", "")}/**/GenBasI.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumBasic_#{version.to_s.gsub(".", "")}/**/GenBasR.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumBasic_1102/GenBasB.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumBasic_1102/GenBasBI.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumBasic_1102/GenBasI.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumBasic_1102/GenBasR.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
