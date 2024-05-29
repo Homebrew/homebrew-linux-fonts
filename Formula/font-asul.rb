688ae84cdf850125b6148e31b58bfd2e3a3a6456
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-asul updated

--- Formula/font-asul.rb
+++ Formula/font-asul.rb
@@ -1,13 +1,17 @@
 class FontAsul < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/asul"
-  desc "Asul"
+  desc "Asul font"
   homepage "https://fonts.google.com/specimen/Asul"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/asul"
+
   def install
     (share/"fonts").install Dir.glob("ofl/asul/./**/Asul-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/asul/./**/Asul-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
