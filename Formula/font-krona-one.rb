af7290d18db690bf412d7daf5390b3937f7c6ecb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-krona-one updated

--- Formula/font-krona-one.rb
+++ Formula/font-krona-one.rb
@@ -1,12 +1,14 @@
 class FontKronaOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kronaone/KronaOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Krona One"
+  desc "Krona one font"
   homepage "https://fonts.google.com/specimen/Krona+One"
+  head "https://github.com/google/fonts/raw/main/ofl/kronaone/KronaOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/KronaOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
