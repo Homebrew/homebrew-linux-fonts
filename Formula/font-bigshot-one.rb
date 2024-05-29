1aae37ed171bc0912bdcf63ff7fdaf1e1af735f9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-bigshot-one updated

--- Formula/font-bigshot-one.rb
+++ Formula/font-bigshot-one.rb
@@ -1,12 +1,14 @@
 class FontBigshotOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bigshotone/BigshotOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bigshot One"
+  desc "Bigshot one font"
   homepage "https://fonts.google.com/specimen/Bigshot+One"
+  head "https://github.com/google/fonts/raw/main/ofl/bigshotone/BigshotOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BigshotOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
