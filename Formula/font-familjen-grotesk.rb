ec487141e4ea8e5b1d75a18847da27a60f982a43
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:05 +0000

    font-familjen-grotesk updated

--- Formula/font-familjen-grotesk.rb
+++ Formula/font-familjen-grotesk.rb
@@ -1,14 +1,17 @@
 class FontFamiljenGrotesk < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/familjengrotesk"
-  desc "Familjen Grotesk"
   desc "Sans serif typeface with a contemporary appearance"
   homepage "https://fonts.google.com/specimen/Familjen+Grotesk"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/familjengrotesk"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/familjengrotesk/./**/FamiljenGrotesk-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/familjengrotesk/./**/FamiljenGrotesk\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/familjengrotesk/./**/FamiljenGrotesk-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/familjengrotesk/./**/FamiljenGrotesk[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
