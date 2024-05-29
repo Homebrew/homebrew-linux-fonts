cb65c6177b49c34e1e2758423ad24055ebbdc7de
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:36 +0000

    font-lxgw-wenkai-mono-tc updated

--- Formula/font-lxgw-wenkai-mono-tc.rb
+++ Formula/font-lxgw-wenkai-mono-tc.rb
@@ -1,14 +1,17 @@
 class FontLxgwWenkaiMonoTc < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/lxgwwenkaimonotc"
-  desc "LXGW WenKai Mono TC"
+  desc "Lxgw wenkai mono tc font"
   homepage "https://github.com/aaronbell/LxgwWenkaiTC"
+  head "https://github.com/google/fonts.git",
+       branch:    "main",
+       only_path: "ofl/lxgwwenkaimonotc"
+
   def install
     (share/"fonts").install Dir.glob("ofl/lxgwwenkaimonotc/./**/LXGWWenKaiMonoTC-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lxgwwenkaimonotc/./**/LXGWWenKaiMonoTC-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lxgwwenkaimonotc/./**/LXGWWenKaiMonoTC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
