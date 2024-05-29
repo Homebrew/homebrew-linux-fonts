b13ee114ee9b0a9337c9624b919e1072e6e50df6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-noto-znamenny-musical-notation updated

--- Formula/font-noto-znamenny-musical-notation.rb
+++ Formula/font-noto-znamenny-musical-notation.rb
@@ -1,12 +1,14 @@
 class FontNotoZnamennyMusicalNotation < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoznamennymusicalnotation/NotoZnamennyMusicalNotation-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Znamenny Musical Notation"
+  desc "Noto znamenny musical notation font"
   homepage "https://fonts.google.com/specimen/Noto+Znamenny+Musical+Notation"
+  head "https://github.com/google/fonts/raw/main/ofl/notoznamennymusicalnotation/NotoZnamennyMusicalNotation-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoZnamennyMusicalNotation-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
