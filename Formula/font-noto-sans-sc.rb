29f1cacc1bedad120ce50a3f144de39737ef2e27
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-sc updated

--- Formula/font-noto-sans-sc.rb
+++ Formula/font-noto-sans-sc.rb
@@ -1,13 +1,14 @@
 class FontNotoSansSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanssc/NotoSansSC%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans SC"
   desc "Sans-serif design for simplified chinese variant of the han ideograms"
   homepage "https://fonts.google.com/specimen/Noto+Sans+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanssc/NotoSansSC%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansSC\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansSC[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
