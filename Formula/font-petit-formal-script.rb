765d4a185f3c34f4e8db1200913100d70c4890d3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-petit-formal-script updated

--- Formula/font-petit-formal-script.rb
+++ Formula/font-petit-formal-script.rb
@@ -1,12 +1,14 @@
 class FontPetitFormalScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/petitformalscript/PetitFormalScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Petit Formal Script"
+  desc "Petit formal script font"
   homepage "https://fonts.google.com/specimen/Petit+Formal+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/petitformalscript/PetitFormalScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PetitFormalScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
