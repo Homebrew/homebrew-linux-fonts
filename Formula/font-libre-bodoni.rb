c619400c04bdc7c3ad7f4cd3853cd0fc059c6937
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:33 +0000

    font-libre-bodoni updated

--- Formula/font-libre-bodoni.rb
+++ Formula/font-libre-bodoni.rb
@@ -1,13 +1,17 @@
 class FontLibreBodoni < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/librebodoni"
-  desc "Libre Bodoni"
+  desc "Libre bodoni font"
   homepage "https://fonts.google.com/specimen/Libre+Bodoni"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/librebodoni"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/librebodoni/./**/LibreBodoni-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/librebodoni/./**/LibreBodoni\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/librebodoni/./**/LibreBodoni-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/librebodoni/./**/LibreBodoni[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
