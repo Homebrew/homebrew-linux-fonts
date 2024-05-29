62f75a6a5d303f3fd55ec06046369db7aad53a5f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-nats updated

--- Formula/font-nats.rb
+++ Formula/font-nats.rb
@@ -1,12 +1,14 @@
 class FontNats < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nats/NATS-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "NATS"
+  desc "Nats font"
   homepage "https://fonts.google.com/specimen/NATS"
+  head "https://github.com/google/fonts/raw/main/ofl/nats/NATS-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NATS-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
