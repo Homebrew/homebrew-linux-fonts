4492598a91e36f5c4ca5687f6e18a7eefaaff0fd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-almarai updated

--- Formula/font-almarai.rb
+++ Formula/font-almarai.rb
@@ -1,15 +1,19 @@
 class FontAlmarai < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/almarai"
-  desc "Almarai"
+  desc "Almarai font"
   homepage "https://fonts.google.com/specimen/Almarai"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/almarai"
+
   def install
     (share/"fonts").install Dir.glob("ofl/almarai/./**/Almarai-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/almarai/./**/Almarai-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/almarai/./**/Almarai-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/almarai/./**/Almarai-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
