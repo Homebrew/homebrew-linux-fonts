210662dd0f1a5f6de53d9247e40673dc930ce4d9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:16 +0000

    font-harano-aji updated

--- Formula/font-harano-aji.rb
+++ Formula/font-harano-aji.rb
@@ -1,29 +1,28 @@
 class FontHaranoAji < Formula
-  version "20231009"
-  sha256 "27f3e553ca324fce689cc86634fcda190b4c5106cc1eaecc5a3c18ff5d16785e"
-  url "https://github.com/trueroad/HaranoAjiFonts/archive/refs/tags/#{version}.zip"
-  desc "Harano Aji"
-  desc "原ノ味"
   desc "Japanese fonts (Source Han fonts with Adobe-Japan1 (AJ1) CIDs)"
   homepage "https://github.com/trueroad/HaranoAjiFonts"
+  url "https://github.com/trueroad/HaranoAjiFonts/archive/refs/tags/20231009.zip"
+  version "20231009"
+  sha256 "27f3e553ca324fce689cc86634fcda190b4c5106cc1eaecc5a3c18ff5d16785e"
+
   def install
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiGothic-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiGothic-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiGothic-Heavy.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiGothic-Light.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiGothic-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiGothic-Normal.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiGothic-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiMincho-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiMincho-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiMincho-Heavy.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiMincho-Light.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiMincho-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiMincho-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("HaranoAjiFonts-#{version}/**/HaranoAjiMincho-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiMincho-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiGothic-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiGothic-Heavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiGothic-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiGothic-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiGothic-Normal.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiGothic-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiMincho-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiMincho-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiMincho-Heavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiMincho-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiMincho-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiMincho-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HaranoAjiFonts-20231009/HaranoAjiGothic-Bold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
