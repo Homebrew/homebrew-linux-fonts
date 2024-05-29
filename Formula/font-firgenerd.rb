8257e68a946a6899e17110b3ea5ca739788aa626
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-firgenerd updated

--- Formula/font-firgenerd.rb
+++ Formula/font-firgenerd.rb
@@ -1,18 +1,18 @@
 class FontFirgenerd < Formula
-  version "0.3.0"
-  sha256 "54cd76378fbc5025f42d441d95ca6ec1d3ecc4270e6107558840fed7c04cfe4f"
-  url "https://github.com/yuru7/Firge/releases/download/v#{version}/FirgeNerd_v#{version}.zip"
-  desc "FirgeNerd"
   desc "Programming font based on Fira Mono and Genshin Gothic"
   homepage "https://github.com/yuru7/Firge"
+  url "https://github.com/yuru7/Firge/releases/download/v0.3.0/FirgeNerd_v0.3.0.zip"
+  version "0.3.0"
+  sha256 "54cd76378fbc5025f42d441d95ca6ec1d3ecc4270e6107558840fed7c04cfe4f"
+
   def install
-    (share/"fonts").install Dir.glob("FirgeNerd_v#{version}/**/Firge35NerdConsole-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("FirgeNerd_v#{version}/**/Firge35NerdConsole-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("FirgeNerd_v#{version}/**/FirgeNerdConsole-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("FirgeNerd_v#{version}/**/FirgeNerdConsole-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FirgeNerd_v0.3.0/Firge35NerdConsole-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FirgeNerd_v0.3.0/Firge35NerdConsole-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FirgeNerd_v0.3.0/FirgeNerdConsole-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FirgeNerd_v0.3.0/FirgeNerdConsole-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
