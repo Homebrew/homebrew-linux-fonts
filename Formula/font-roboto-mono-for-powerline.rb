f87594b65a5822eb4e5a374110df7c16a3691fb6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:15 +0000

    font-roboto-mono-for-powerline updated

--- Formula/font-roboto-mono-for-powerline.rb
+++ Formula/font-roboto-mono-for-powerline.rb
@@ -1,9 +1,12 @@
 class FontRobotoMonoForPowerline < Formula
-  head "https://github.com/powerline/fonts.git", branch: "master", only_path: "RobotoMono"
-  desc "Roboto Mono for Powerline"
+  desc "Roboto mono for powerline font"
   homepage "https://github.com/powerline/fonts/tree/master/RobotoMono"
+  head "https://github.com/powerline/fonts.git",
+       branch:    "master",
+       only_path: "RobotoMono"
+
   def install
-    (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Bold Italic for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Bold for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Italic for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Light Italic for Powerline.ttf")[0]
@@ -12,10 +15,10 @@ class FontRobotoMonoForPowerline < Formula
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Medium for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Thin Italic for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Thin for Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Bold Italic for Powerline.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
