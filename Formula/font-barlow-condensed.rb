ab84aa8b05c10bf7c99654cb2d173a5bb86253bb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:45 +0000

    font-barlow-condensed updated

--- Formula/font-barlow-condensed.rb
+++ Formula/font-barlow-condensed.rb
@@ -1,9 +1,13 @@
 class FontBarlowCondensed < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/barlowcondensed"
-  desc "Barlow Condensed"
+  desc "Barlow condensed font"
   homepage "https://fonts.google.com/specimen/Barlow+Condensed"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/barlowcondensed"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-ThinItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-BoldItalic.ttf")[0]
@@ -20,10 +24,10 @@ class FontBarlowCondensed < Formula
     (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/barlowcondensed/./**/BarlowCondensed-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
