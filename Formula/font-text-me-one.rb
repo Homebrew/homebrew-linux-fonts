a049047260e0439c01aae40e2d915b3b40b1a235
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-text-me-one updated

--- Formula/font-text-me-one.rb
+++ Formula/font-text-me-one.rb
@@ -1,12 +1,14 @@
 class FontTextMeOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/textmeone/TextMeOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Text Me One"
+  desc "Text me one font"
   homepage "https://fonts.google.com/specimen/Text+Me+One"
+  head "https://github.com/google/fonts/raw/main/ofl/textmeone/TextMeOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/TextMeOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
