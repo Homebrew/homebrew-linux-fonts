2ecf764cee5fa114d4a1974c372149e9e09933cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-vina-sans updated

--- Formula/font-vina-sans.rb
+++ Formula/font-vina-sans.rb
@@ -1,12 +1,14 @@
 class FontVinaSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/vinasans/VinaSans-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Vina Sans"
+  desc "Vina sans font"
   homepage "https://fonts.google.com/specimen/Vina+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/vinasans/VinaSans-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/VinaSans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
