c75a4f4e749f485aae6260798c9ba98db39294ac
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:33 +0000

    font-unna updated

--- Formula/font-unna.rb
+++ Formula/font-unna.rb
@@ -1,15 +1,19 @@
 class FontUnna < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/unna"
-  desc "Unna"
+  desc "Unna font"
   homepage "https://fonts.google.com/specimen/Unna"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/unna"
+
   def install
     (share/"fonts").install Dir.glob("ofl/unna/./**/Unna-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/unna/./**/Unna-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/unna/./**/Unna-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/unna/./**/Unna-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
