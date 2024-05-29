2a16b1c6a082df5c89ed9852348615db7af0a6bb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-arial updated

--- Formula/font-arial.rb
+++ Formula/font-arial.rb
@@ -1,15 +1,16 @@
 class FontArial < Formula
-  head "https://downloads.sourceforge.net/corefonts/arial32.exe"
-  desc "Arial"
+  desc "Arial font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  head "https://downloads.sourceforge.net/corefonts/arial32.exe"
+
   def install
     (share/"fonts").install Dir.glob("./**/Ariali.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Arialbd.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Arialbi.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Arial.TTF")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
