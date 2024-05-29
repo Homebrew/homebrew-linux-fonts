f42dbc45c3f502042742a264028a07ffae78a60a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-edu-qld-beginner updated

--- Formula/font-edu-qld-beginner.rb
+++ Formula/font-edu-qld-beginner.rb
@@ -1,12 +1,14 @@
 class FontEduQldBeginner < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/eduqldbeginner/EduQLDBeginner%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Edu QLD Beginner"
+  desc "Edu qld beginner font"
   homepage "https://fonts.google.com/specimen/Edu+QLD+Beginner"
+  head "https://github.com/google/fonts/raw/main/ofl/eduqldbeginner/EduQLDBeginner%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/EduQLDBeginner\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/EduQLDBeginner[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
