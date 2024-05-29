651d45569c2453246406c40a9340540e62291d97
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-lepcha updated

--- Formula/font-noto-sans-lepcha.rb
+++ Formula/font-noto-sans-lepcha.rb
@@ -1,12 +1,14 @@
 class FontNotoSansLepcha < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLepcha-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Lepcha"
+  desc "Noto sans lepcha font"
   homepage "https://www.google.com/get/noto/#sans-lepc"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLepcha-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansLepcha-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
