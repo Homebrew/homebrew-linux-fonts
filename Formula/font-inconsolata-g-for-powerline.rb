c3e06c4da2a27ba0cb85af254217c462cf7994cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:20 +0000

    font-inconsolata-g-for-powerline updated

--- Formula/font-inconsolata-g-for-powerline.rb
+++ Formula/font-inconsolata-g-for-powerline.rb
@@ -1,12 +1,15 @@
 class FontInconsolataGForPowerline < Formula
-  head "https://github.com/powerline/fonts.git", branch: "master", only_path: "Inconsolata-g"
-  desc "Inconsolata-g for Powerline"
+  desc "Inconsolata-g for powerline font"
   homepage "https://github.com/powerline/fonts/tree/master/Inconsolata-g"
+  head "https://github.com/powerline/fonts.git",
+       branch:    "master",
+       only_path: "Inconsolata-g"
+
   def install
     (share/"fonts").install Dir.glob("Inconsolata-g/./**/Inconsolata-g for Powerline.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
