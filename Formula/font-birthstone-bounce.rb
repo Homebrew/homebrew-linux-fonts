6534ceffdd5cf178eb257939085ed5a5d5e01400
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-birthstone-bounce updated

--- Formula/font-birthstone-bounce.rb
+++ Formula/font-birthstone-bounce.rb
@@ -1,14 +1,17 @@
 class FontBirthstoneBounce < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/birthstonebounce"
-  desc "Birthstone Bounce"
   desc "Sibling family of birthstone that adds more luster and playfulness to it"
   homepage "https://fonts.google.com/specimen/Birthstone+Bounce"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/birthstonebounce"
+
   def install
     (share/"fonts").install Dir.glob("ofl/birthstonebounce/./**/BirthstoneBounce-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/birthstonebounce/./**/BirthstoneBounce-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
