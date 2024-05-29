25d864d0be2d694e9e9bc4c2d55491d41215d8ac
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-passion-one updated

--- Formula/font-passion-one.rb
+++ Formula/font-passion-one.rb
@@ -1,14 +1,18 @@
 class FontPassionOne < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/passionone"
-  desc "Passion One"
+  desc "Passion one font"
   homepage "https://fonts.google.com/specimen/Passion+One"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/passionone"
+
   def install
     (share/"fonts").install Dir.glob("ofl/passionone/./**/PassionOne-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/passionone/./**/PassionOne-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/passionone/./**/PassionOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
