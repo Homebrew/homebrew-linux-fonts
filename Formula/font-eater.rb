5b8137e57e4a6b77e8bb4027942d82f919d439b0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-eater updated

--- Formula/font-eater.rb
+++ Formula/font-eater.rb
@@ -1,12 +1,14 @@
 class FontEater < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/eater/Eater-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Eater"
+  desc "Eater font"
   homepage "https://fonts.google.com/specimen/Eater"
+  head "https://github.com/google/fonts/raw/main/ofl/eater/Eater-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Eater-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
