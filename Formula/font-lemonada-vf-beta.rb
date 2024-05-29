176d4b06acaafcfe91229cafd094c19d52a5068c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:31 +0000

    font-lemonada-vf-beta updated

--- Formula/font-lemonada-vf-beta.rb
+++ Formula/font-lemonada-vf-beta.rb
@@ -1,12 +1,14 @@
 class FontLemonadaVfBeta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lemonadavfbeta/LemonadaVFBeta.ttf", verified: "github.com/google/fonts/"
-  desc "Lemonada VF Beta"
+  desc "Lemonada vf beta font"
   homepage "https://fonts.google.com/specimen/Lemonada+VF+Beta"
+  head "https://github.com/google/fonts/raw/main/ofl/lemonadavfbeta/LemonadaVFBeta.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LemonadaVFBeta.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
