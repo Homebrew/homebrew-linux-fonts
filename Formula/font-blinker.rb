b47d7e17a7618d94262f9cb0cfc9e15b85ab6f70
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-blinker updated

--- Formula/font-blinker.rb
+++ Formula/font-blinker.rb
@@ -1,19 +1,23 @@
 class FontBlinker < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/blinker"
-  desc "Blinker"
+  desc "Blinker font"
   homepage "https://fonts.google.com/specimen/Blinker"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/blinker"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
