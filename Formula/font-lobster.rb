be3c51806f51e5223f2410d6f1d7501f513220b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-lobster updated

--- Formula/font-lobster.rb
+++ Formula/font-lobster.rb
@@ -1,12 +1,14 @@
 class FontLobster < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lobster/Lobster-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Lobster"
+  desc "Lobster font"
   homepage "https://fonts.google.com/specimen/Lobster"
+  head "https://github.com/google/fonts/raw/main/ofl/lobster/Lobster-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Lobster-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
