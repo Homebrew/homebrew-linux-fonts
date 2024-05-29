297ea9cc3c8bd79d1c24b0a36f145cac3b878311
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:08 +0000

    font-playwrite-in updated

--- Formula/font-playwrite-in.rb
+++ Formula/font-playwrite-in.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteIn < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwritein/PlaywriteIN%5Bwght%5D.ttf"
-  desc "Playwrite IN"
+  desc "Playwrite in font"
   homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritein/PlaywriteIN%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteIN\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteIN[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
