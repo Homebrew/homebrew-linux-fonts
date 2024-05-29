9a88f8215feecf534000cd693072afd4daae2d55
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-comforter-brush updated

--- Formula/font-comforter-brush.rb
+++ Formula/font-comforter-brush.rb
@@ -1,13 +1,14 @@
 class FontComforterBrush < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/comforterbrush/ComforterBrush-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Comforter Brush"
   desc "Brushy companion of comforter, a bouncy, upright brush style script"
   homepage "https://fonts.google.com/specimen/Comforter+Brush"
+  head "https://github.com/google/fonts/raw/main/ofl/comforterbrush/ComforterBrush-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ComforterBrush-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
