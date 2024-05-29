03b726e8976f5c4d6251a229382e0f25219c345e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-flamenco updated

--- Formula/font-flamenco.rb
+++ Formula/font-flamenco.rb
@@ -1,13 +1,17 @@
 class FontFlamenco < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/flamenco"
-  desc "Flamenco"
+  desc "Flamenco font"
   homepage "https://fonts.google.com/specimen/Flamenco"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/flamenco"
+
   def install
     (share/"fonts").install Dir.glob("ofl/flamenco/./**/Flamenco-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/flamenco/./**/Flamenco-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
