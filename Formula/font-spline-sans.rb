ab33a832190017aaf53445f7b37cc0657fbf95ad
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-spline-sans updated

--- Formula/font-spline-sans.rb
+++ Formula/font-spline-sans.rb
@@ -1,13 +1,14 @@
 class FontSplineSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/splinesans/SplineSans%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Spline Sans"
   desc "Original typeface initiated by the spline team"
   homepage "https://fonts.google.com/specimen/Spline+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/splinesans/SplineSans%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/SplineSans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/SplineSans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
