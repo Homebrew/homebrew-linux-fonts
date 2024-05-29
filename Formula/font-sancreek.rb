f992838a13db2d7f5601f56928a4ee1c36ceb851
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-sancreek updated

--- Formula/font-sancreek.rb
+++ Formula/font-sancreek.rb
@@ -1,12 +1,14 @@
 class FontSancreek < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sancreek/Sancreek-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sancreek"
+  desc "Sancreek font"
   homepage "https://fonts.google.com/specimen/Sancreek"
+  head "https://github.com/google/fonts/raw/main/ofl/sancreek/Sancreek-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Sancreek-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
