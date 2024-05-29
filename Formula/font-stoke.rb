928dc3f1146ff092580c831e77bfe0796352c4eb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-stoke updated

--- Formula/font-stoke.rb
+++ Formula/font-stoke.rb
@@ -1,13 +1,17 @@
 class FontStoke < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/stoke"
-  desc "Stoke"
+  desc "Stoke font"
   homepage "https://fonts.google.com/specimen/Stoke"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/stoke"
+
   def install
     (share/"fonts").install Dir.glob("ofl/stoke/./**/Stoke-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/stoke/./**/Stoke-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
