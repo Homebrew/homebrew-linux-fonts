2e28fa3d79a19fb0d623e9d7d63ff94f319a9023
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-average-mono updated

--- Formula/font-average-mono.rb
+++ Formula/font-average-mono.rb
@@ -1,15 +1,16 @@
 class FontAverageMono < Formula
-  head "https://fontlibrary.org/assets/downloads/average-mono/7e3ab639a72edd95bd08537df255440d/average-mono.zip"
-  desc "Average Mono"
+  desc "Average mono font"
   homepage "https://fontlibrary.org/en/font/average-mono"
+  head "https://fontlibrary.org/assets/downloads/average-mono/7e3ab639a72edd95bd08537df255440d/average-mono.zip"
+
   def install
-    (share/"fonts").install Dir.glob("AverageMono/**/AverageMono.otf")[0]
-    (share/"fonts").install Dir.glob("AverageMono/**/AverageMonoBold.otf")[0]
-    (share/"fonts").install Dir.glob("AverageMono/**/AverageMonoItalic.otf")[0]
-    (share/"fonts").install Dir.glob("AverageMono/**/AverageMonoBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/AverageMono/AverageMono.otf")[0]
+    (share/"fonts").install Dir.glob("./**/AverageMono/AverageMonoBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/AverageMono/AverageMonoItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/AverageMono/AverageMonoBoldItalic.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
