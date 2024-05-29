a263d9dc43666bbe451ca0adb465cf895cfc7359
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-digital-numbers updated

--- Formula/font-digital-numbers.rb
+++ Formula/font-digital-numbers.rb
@@ -1,12 +1,14 @@
 class FontDigitalNumbers < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/digitalnumbers/DigitalNumbers-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Digital Numbers"
+  desc "Digital numbers font"
   homepage "https://fonts.google.com/specimen/Digital+Numbers"
+  head "https://github.com/google/fonts/raw/main/ofl/digitalnumbers/DigitalNumbers-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DigitalNumbers-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
