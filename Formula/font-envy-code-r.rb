fc3cd701f0592630bec57c53a2037296fbc862bf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-envy-code-r updated

--- Formula/font-envy-code-r.rb
+++ Formula/font-envy-code-r.rb
@@ -1,16 +1,17 @@
 class FontEnvyCodeR < Formula
+  desc "Envy code r font"
+  homepage "https://damieng.com/blog/tag/envy-code/"
+  url "https://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip"
   version "PR7"
   sha256 "9f7e9703aaf21110b4e1a54d954d57d4092727546348598a5a8e8101e4597aff"
-  url "https://download.damieng.com/fonts/original/EnvyCodeR-#{version}.zip"
-  desc "Envy Code R"
-  homepage "https://damieng.com/blog/tag/envy-code/"
+
   def install
-    (share/"fonts").install Dir.glob("Envy Code R PR7/**/Envy Code R Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("Envy Code R PR7/**/Envy Code R Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("Envy Code R PR7/**/Envy Code R.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Envy Code R PR7/Envy Code R Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Envy Code R PR7/Envy Code R Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Envy Code R PR7/Envy Code R.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
