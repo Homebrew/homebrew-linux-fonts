30e899d5c93bd3f39aebac08251274de5b7e275b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-fresca updated

--- Formula/font-fresca.rb
+++ Formula/font-fresca.rb
@@ -1,12 +1,14 @@
 class FontFresca < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/fresca/Fresca-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Fresca"
+  desc "Fresca font"
   homepage "https://fonts.google.com/specimen/Fresca"
+  head "https://github.com/google/fonts/raw/main/ofl/fresca/Fresca-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Fresca-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
