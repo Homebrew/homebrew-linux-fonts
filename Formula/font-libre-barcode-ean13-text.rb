841a63b25e0cd34bdfc6bd56bc0ff17764aad278
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:32 +0000

    font-libre-barcode-ean13-text updated

--- Formula/font-libre-barcode-ean13-text.rb
+++ Formula/font-libre-barcode-ean13-text.rb
@@ -1,13 +1,14 @@
 class FontLibreBarcodeEan13Text < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/librebarcodeean13text/LibreBarcodeEAN13Text-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Libre Barcode EAN13 Text"
   desc "Led by lasse fister, a font and web developer based in nuremberg, germany"
   homepage "https://fonts.google.com/specimen/Libre+Barcode+EAN13+Text"
+  head "https://github.com/google/fonts/raw/main/ofl/librebarcodeean13text/LibreBarcodeEAN13Text-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LibreBarcodeEAN13Text-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
