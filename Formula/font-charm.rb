fa1301527026d42409a8becee7f50b4258ad3ee2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-charm updated

--- Formula/font-charm.rb
+++ Formula/font-charm.rb
@@ -1,13 +1,17 @@
 class FontCharm < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/charm"
-  desc "Charm"
+  desc "Charm font"
   homepage "https://fonts.google.com/specimen/Charm"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/charm"
+
   def install
     (share/"fonts").install Dir.glob("ofl/charm/./**/Charm-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/charm/./**/Charm-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
