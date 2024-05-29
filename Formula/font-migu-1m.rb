952979ee7c5b76df400a45aafcfd70248be47d7a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:40 +0000

    font-migu-1m updated

--- Formula/font-migu-1m.rb
+++ Formula/font-migu-1m.rb
@@ -1,15 +1,16 @@
 class FontMigu1m < Formula
+  desc "Migu 1m font"
+  homepage "https://mix-mplus-ipa.osdn.jp/migu/#migu1m"
+  url "https://osdn.dl.osdn.jp/mix-mplus-ipa/63545/migu-1m-20150712.zip"
   version "20150712"
   sha256 "d4c38664dd57bc5927abe8f4fbea8f06a8ece3fea49ea02354d4e03ac6d15006"
-  url "https://osdn.dl.osdn.jp/mix-mplus-ipa/63545/migu-1m-#{version}.zip"
-  desc "Migu 1M"
-  homepage "https://mix-mplus-ipa.osdn.jp/migu/#migu1m"
+
   def install
-    (share/"fonts").install Dir.glob("migu-1m-#{version}/**/migu-1m-bold.ttf")[0]
-    (share/"fonts").install Dir.glob("migu-1m-#{version}/**/migu-1m-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-1m-20150712/migu-1m-bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-1m-20150712/migu-1m-regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
