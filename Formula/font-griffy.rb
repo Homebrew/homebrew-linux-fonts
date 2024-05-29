108aba27b42fdfc01e644683fcce38e46256a480
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-griffy updated

--- Formula/font-griffy.rb
+++ Formula/font-griffy.rb
@@ -1,12 +1,14 @@
 class FontGriffy < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/griffy/Griffy-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Griffy"
+  desc "Griffy font"
   homepage "https://fonts.google.com/specimen/Griffy"
+  head "https://github.com/google/fonts/raw/main/ofl/griffy/Griffy-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Griffy-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
