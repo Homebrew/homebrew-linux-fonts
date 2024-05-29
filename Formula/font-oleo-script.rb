ebe75671acbb2a557e54994522c8e277b2460b2d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-oleo-script updated

--- Formula/font-oleo-script.rb
+++ Formula/font-oleo-script.rb
@@ -1,13 +1,17 @@
 class FontOleoScript < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/oleoscript"
-  desc "Oleo Script"
+  desc "Oleo script font"
   homepage "https://fonts.google.com/specimen/Oleo+Script"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/oleoscript"
+
   def install
     (share/"fonts").install Dir.glob("ofl/oleoscript/./**/OleoScript-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/oleoscript/./**/OleoScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
