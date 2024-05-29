ce1704dae9f17e132d5b19e7607b2a1bfd78ba2a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-last-resort updated

--- Formula/font-last-resort.rb
+++ Formula/font-last-resort.rb
@@ -1,15 +1,15 @@
 class FontLastResort < Formula
-  version "15.100"
-  sha256 "2c25999fae0a09f4cf5d1c084729b43e81296f497c59e5891a710a7744c724ff"
-  url "https://github.com/unicode-org/last-resort-font/releases/download/#{version}/LastResort-Regular.ttf"
-  desc "Last Resort"
   desc "Special-purpose font, providing glyphs to represent types of Unicode characters"
   homepage "https://github.com/unicode-org/last-resort-font"
+  url "https://github.com/unicode-org/last-resort-font/releases/download/15.100/LastResort-Regular.ttf"
+  version "15.100"
+  sha256 "2c25999fae0a09f4cf5d1c084729b43e81296f497c59e5891a710a7744c724ff"
+
   def install
     (share/"fonts").install Dir.glob("./**/LastResort-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
