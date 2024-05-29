2ac3e83a94f1d31dd4fa283c50659ac2597ad2f1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-dhyana updated

--- Formula/font-dhyana.rb
+++ Formula/font-dhyana.rb
@@ -1,13 +1,17 @@
 class FontDhyana < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/dhyana"
-  desc "Dhyana"
+  desc "Dhyana font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/dhyana"
+
   def install
     (share/"fonts").install Dir.glob("ofl/dhyana/./**/Dhyana-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/dhyana/./**/Dhyana-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
