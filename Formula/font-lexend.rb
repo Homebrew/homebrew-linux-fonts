8de0ad60ae56f8ff5d4ae62cfdc1da9fb1bac6a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:32 +0000

    font-lexend updated

--- Formula/font-lexend.rb
+++ Formula/font-lexend.rb
@@ -1,13 +1,14 @@
 class FontLexend < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lexend/Lexend%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Lexend"
   desc "Expanded by font bureau with a range of 8 weights"
   homepage "https://fonts.google.com/specimen/Lexend"
+  head "https://github.com/google/fonts/raw/main/ofl/lexend/Lexend%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Lexend\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lexend[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
