83b44ff72ac694a8abeaf289c3c10223c05719f8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-reem-kufi-fun updated

--- Formula/font-reem-kufi-fun.rb
+++ Formula/font-reem-kufi-fun.rb
@@ -1,13 +1,14 @@
 class FontReemKufiFun < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/reemkufifun/ReemKufiFun%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Reem Kufi Fun"
   desc "Also the name of khaled's daughter"
   homepage "https://fonts.google.com/specimen/Reem+Kufi+Fun"
+  head "https://github.com/google/fonts/raw/main/ofl/reemkufifun/ReemKufiFun%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/ReemKufiFun\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ReemKufiFun[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
