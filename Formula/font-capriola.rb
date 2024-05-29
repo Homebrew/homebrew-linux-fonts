ae16054c1a538161dc4eed5731ac097c164c21b7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-capriola updated

--- Formula/font-capriola.rb
+++ Formula/font-capriola.rb
@@ -1,12 +1,14 @@
 class FontCapriola < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/capriola/Capriola-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Capriola"
+  desc "Capriola font"
   homepage "https://fonts.google.com/specimen/Capriola"
+  head "https://github.com/google/fonts/raw/main/ofl/capriola/Capriola-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Capriola-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
