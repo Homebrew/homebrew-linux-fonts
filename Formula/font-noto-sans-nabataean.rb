aad5e11084e1552632354ab24b713554c478c9c2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:54 +0000

    font-noto-sans-nabataean updated

--- Formula/font-noto-sans-nabataean.rb
+++ Formula/font-noto-sans-nabataean.rb
@@ -1,12 +1,14 @@
 class FontNotoSansNabataean < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansnabataean/NotoSansNabataean-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Nabataean"
+  desc "Noto sans nabataean font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Nabataean"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansnabataean/NotoSansNabataean-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansNabataean-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
