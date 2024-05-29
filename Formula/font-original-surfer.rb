34a37afe771e6e699cc44853075fd0c96127f09f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-original-surfer updated

--- Formula/font-original-surfer.rb
+++ Formula/font-original-surfer.rb
@@ -1,12 +1,14 @@
 class FontOriginalSurfer < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/originalsurfer/OriginalSurfer-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Original Surfer"
+  desc "Original surfer font"
   homepage "https://fonts.google.com/specimen/Original+Surfer"
+  head "https://github.com/google/fonts/raw/main/ofl/originalsurfer/OriginalSurfer-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/OriginalSurfer-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
