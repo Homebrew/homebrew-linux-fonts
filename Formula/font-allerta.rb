32f6f2762451747ad765afac53ec5020741050d8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-allerta updated

--- Formula/font-allerta.rb
+++ Formula/font-allerta.rb
@@ -1,12 +1,14 @@
 class FontAllerta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/allerta/Allerta-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Allerta"
+  desc "Allerta font"
   homepage "https://fonts.google.com/specimen/Allerta"
+  head "https://github.com/google/fonts/raw/main/ofl/allerta/Allerta-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Allerta-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
