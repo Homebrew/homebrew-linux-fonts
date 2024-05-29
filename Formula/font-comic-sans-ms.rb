504d37ed5ac348dd92f026860bcfe16c9e181568
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-comic-sans-ms updated

--- Formula/font-comic-sans-ms.rb
+++ Formula/font-comic-sans-ms.rb
@@ -1,13 +1,14 @@
 class FontComicSansMs < Formula
-  head "https://downloads.sourceforge.net/corefonts/comic32.exe"
-  desc "Comic Sans"
+  desc "Comic sans font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  head "https://downloads.sourceforge.net/corefonts/comic32.exe"
+
   def install
     (share/"fonts").install Dir.glob("./**/Comicbd.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Comic.TTF")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
