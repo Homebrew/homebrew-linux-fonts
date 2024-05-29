73317b67e6626f7294011ac959e50ae33cf54fec
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-eagle-lake updated

--- Formula/font-eagle-lake.rb
+++ Formula/font-eagle-lake.rb
@@ -1,12 +1,14 @@
 class FontEagleLake < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/eaglelake/EagleLake-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Eagle Lake"
+  desc "Eagle lake font"
   homepage "https://fonts.google.com/specimen/Eagle+Lake"
+  head "https://github.com/google/fonts/raw/main/ofl/eaglelake/EagleLake-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/EagleLake-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
