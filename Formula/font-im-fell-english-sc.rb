5bbc5c775df7822c4ad9bba3617504795806f7f8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-im-fell-english-sc updated

--- Formula/font-im-fell-english-sc.rb
+++ Formula/font-im-fell-english-sc.rb
@@ -1,12 +1,14 @@
 class FontImFellEnglishSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/imfellenglishsc/IMFeENsc28P.ttf", verified: "github.com/google/fonts/"
-  desc "IM Fell English SC"
+  desc "Im fell english sc font"
   homepage "https://fonts.google.com/specimen/IM+Fell+English+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/imfellenglishsc/IMFeENsc28P.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/IMFeENsc28P.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
