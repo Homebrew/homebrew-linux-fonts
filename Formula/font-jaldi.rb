868783708fd741712e6d1dadf0f7175cbd156b3e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jaldi updated

--- Formula/font-jaldi.rb
+++ Formula/font-jaldi.rb
@@ -1,13 +1,17 @@
 class FontJaldi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/jaldi"
-  desc "Jaldi"
+  desc "Jaldi font"
   homepage "https://fonts.google.com/specimen/Jaldi"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/jaldi"
+
   def install
     (share/"fonts").install Dir.glob("ofl/jaldi/./**/Jaldi-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/jaldi/./**/Jaldi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
