01b6b74f9a713e39b55a3ec99632f215b434df7a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-malayalam-ui updated

--- Formula/font-noto-sans-malayalam-ui.rb
+++ Formula/font-noto-sans-malayalam-ui.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMalayalamUi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmalayalamui/NotoSansMalayalamUI%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Malayalam UI"
+  desc "Noto sans malayalam ui font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Malayalam+UI"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmalayalamui/NotoSansMalayalamUI%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansMalayalamUI\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansMalayalamUI[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
