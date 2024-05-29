da6e443705fcb4a6fd3ced3090252c33a31ced19
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:34 +0000

    font-3270-nerd-font updated

--- Formula/font-3270-nerd-font.rb
+++ Formula/font-3270-nerd-font.rb
@@ -1,12 +1,12 @@
 class Font3270NerdFont < Formula
-  version "3.2.1"
-  sha256 "0e7cffe10575de3d80df920065ea6f320e86cd515ddd16beed817d284ba44afc"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/3270.zip"
-  desc "3270 Nerd Font (IBM 3270)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/3270.zip"
+  version "3.2.1"
+  sha256 "0e7cffe10575de3d80df920065ea6f320e86cd515ddd16beed817d284ba44afc"
+
   def install
-    (share/"fonts").install Dir.glob("./**/3270NerdFont-Condensed.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/3270NerdFontPropo-SemiCondensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFont-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFont-SemiCondensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFontMono-Condensed.ttf")[0]
@@ -14,10 +14,10 @@ class Font3270NerdFont < Formula
     (share/"fonts").install Dir.glob("./**/3270NerdFontMono-SemiCondensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFontPropo-Condensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFontPropo-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/3270NerdFontPropo-SemiCondensed.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/3270NerdFont-Condensed.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
