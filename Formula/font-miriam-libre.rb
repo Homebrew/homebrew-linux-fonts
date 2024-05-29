8c30cd4d435267a8b3e343544da701bcb2c745c6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-miriam-libre updated

--- Formula/font-miriam-libre.rb
+++ Formula/font-miriam-libre.rb
@@ -1,13 +1,17 @@
 class FontMiriamLibre < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/miriamlibre"
-  desc "Miriam Libre"
+  desc "Miriam libre font"
   homepage "https://fonts.google.com/specimen/Miriam+Libre"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/miriamlibre"
+
   def install
     (share/"fonts").install Dir.glob("ofl/miriamlibre/./**/MiriamLibre-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/miriamlibre/./**/MiriamLibre-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
