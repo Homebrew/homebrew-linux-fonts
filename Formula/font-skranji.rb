3d6ea2f4d77295bb962c26f09b5302cefeddda8d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-skranji updated

--- Formula/font-skranji.rb
+++ Formula/font-skranji.rb
@@ -1,13 +1,17 @@
 class FontSkranji < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/skranji"
-  desc "Skranji"
+  desc "Skranji font"
   homepage "https://fonts.google.com/specimen/Skranji"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/skranji"
+
   def install
     (share/"fonts").install Dir.glob("ofl/skranji/./**/Skranji-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/skranji/./**/Skranji-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
