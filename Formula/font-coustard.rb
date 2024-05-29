71207787804589be85bf1bf79844f8b071c0a0df
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-coustard updated

--- Formula/font-coustard.rb
+++ Formula/font-coustard.rb
@@ -1,13 +1,17 @@
 class FontCoustard < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/coustard"
-  desc "Coustard"
+  desc "Coustard font"
   homepage "https://fonts.google.com/specimen/Coustard"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/coustard"
+
   def install
     (share/"fonts").install Dir.glob("ofl/coustard/./**/Coustard-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/coustard/./**/Coustard-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
