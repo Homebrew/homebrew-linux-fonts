8b467f5ebf56ce63a4ac19f2e13b3ba990df50d0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-shantell-sans updated

--- Formula/font-shantell-sans.rb
+++ Formula/font-shantell-sans.rb
@@ -1,13 +1,17 @@
 class FontShantellSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/shantellsans"
-  desc "Shantell Sans"
+  desc "Shantell sans font"
   homepage "https://fonts.google.com/specimen/Shantell+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/shantellsans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/shantellsans/./**/ShantellSans-Italic\\[BNCE,INFM,SPAC,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/shantellsans/./**/ShantellSans\\[BNCE,INFM,SPAC,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/shantellsans/./**/ShantellSans-Italic[BNCE,INFM,SPAC,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/shantellsans/./**/ShantellSans[BNCE,INFM,SPAC,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
