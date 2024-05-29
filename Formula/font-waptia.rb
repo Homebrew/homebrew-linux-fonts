3daa0c15f78abbc94d446f57f184c106047f42e7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-waptia updated

--- Formula/font-waptia.rb
+++ Formula/font-waptia.rb
@@ -1,14 +1,14 @@
 class FontWaptia < Formula
-  head "https://moji-waku.com/download/waptia.zip"
-  desc "WAPTIA"
   desc "Ancient-biology styled font"
   homepage "https://moji-waku.com/waptia/index.html"
+  head "https://moji-waku.com/download/waptia.zip"
+
   def install
-    (share/"fonts").install Dir.glob("waptia/**/Waptia-Light.otf")[0]
-    (share/"fonts").install Dir.glob("waptia/**/Waptia-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/waptia/Waptia-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/waptia/Waptia-Thin.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
