fe2252ad7584088be04ecccdcc01b4b7d20effae
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-kr updated

--- Formula/font-noto-serif-kr.rb
+++ Formula/font-noto-serif-kr.rb
@@ -1,13 +1,14 @@
 class FontNotoSerifKr < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifkr/NotoSerifKR%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif KR"
   desc "Modulated (“serif”) design for the korean language"
   homepage "https://fonts.google.com/specimen/Noto+Serif+KR"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifkr/NotoSerifKR%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifKR\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifKR[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
