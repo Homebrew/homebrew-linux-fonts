6be5e1b24272624929045e00238a203c298651e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:36 +0000

    font-lxgw-wenkai updated

--- Formula/font-lxgw-wenkai.rb
+++ Formula/font-lxgw-wenkai.rb
@@ -1,21 +1,20 @@
 class FontLxgwWenkai < Formula
-  version "1.330"
-  sha256 "2f317e15480608cabf471cbabdbc5f524066d0bbb177a3f439c7b5ee7b61780c"
-  url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/lxgw-wenkai-v#{version}.zip"
-  desc "LXGW WenKai"
-  desc "霞鹜文楷"
   desc "Open-source Chinese font derived from Fontworks' Klee One"
   homepage "https://github.com/lxgw/LxgwWenKai"
+  url "https://github.com/lxgw/LxgwWenKai/releases/download/v1.330/lxgw-wenkai-v1.330.zip"
+  version "1.330"
+  sha256 "2f317e15480608cabf471cbabdbc5f524066d0bbb177a3f439c7b5ee7b61780c"
+
   def install
-    (share/"fonts").install Dir.glob("lxgw-wenkai-v#{version}/**/LXGWWenKai-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-v#{version}/**/LXGWWenKai-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-v#{version}/**/LXGWWenKai-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-v#{version}/**/LXGWWenKaiMono-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-v#{version}/**/LXGWWenKaiMono-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-v#{version}/**/LXGWWenKaiMono-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-v1.330/LXGWWenKai-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-v1.330/LXGWWenKai-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-v1.330/LXGWWenKai-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-v1.330/LXGWWenKaiMono-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-v1.330/LXGWWenKaiMono-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-v1.330/LXGWWenKaiMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
