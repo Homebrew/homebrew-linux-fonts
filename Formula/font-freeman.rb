a8b582133629667886aeddaef8d5ab7d2fcdffdc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-freeman updated

--- Formula/font-freeman.rb
+++ Formula/font-freeman.rb
@@ -1,13 +1,14 @@
 class FontFreeman < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/freeman/Freeman-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Freeman"
   desc "Re-interpretation of the traditional display sans serif gothic typeface"
   homepage "https://fonts.google.com/specimen/Freeman"
+  head "https://github.com/google/fonts/raw/main/ofl/freeman/Freeman-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Freeman-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
