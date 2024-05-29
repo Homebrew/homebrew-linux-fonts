77197f44823e6038c761dce29eddb337c57a12b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-arimo updated

--- Formula/font-arimo.rb
+++ Formula/font-arimo.rb
@@ -1,13 +1,17 @@
 class FontArimo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "apache/arimo"
-  desc "Arimo"
+  desc "Arimo font"
   homepage "https://fonts.google.com/specimen/Arimo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "apache/arimo"
+
   def install
-    (share/"fonts").install Dir.glob("apache/arimo/./**/Arimo-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("apache/arimo/./**/Arimo\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("apache/arimo/./**/Arimo-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("apache/arimo/./**/Arimo[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
