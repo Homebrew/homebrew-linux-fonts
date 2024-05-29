ce4299ea147871f23c23c2d0f474cd44cec71be3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-manichaean updated

--- Formula/font-noto-sans-manichaean.rb
+++ Formula/font-noto-sans-manichaean.rb
@@ -1,12 +1,14 @@
 class FontNotoSansManichaean < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmanichaean/NotoSansManichaean-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Manichaean"
+  desc "Noto sans manichaean font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Manichaean"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmanichaean/NotoSansManichaean-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansManichaean-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
