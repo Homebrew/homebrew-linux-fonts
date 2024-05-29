e0577496b72e3fad5a60306364655521cb912373
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-hanuman updated

--- Formula/font-hanuman.rb
+++ Formula/font-hanuman.rb
@@ -1,7 +1,11 @@
 class FontHanuman < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/hanuman"
-  desc "Hanuman"
+  desc "Hanuman font"
   homepage "https://fonts.google.com/specimen/Hanuman"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/hanuman"
+
   def install
     (share/"fonts").install Dir.glob("ofl/hanuman/./**/Hanuman-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hanuman/./**/Hanuman-Bold.ttf")[0]
@@ -9,8 +13,8 @@ class FontHanuman < Formula
     (share/"fonts").install Dir.glob("ofl/hanuman/./**/Hanuman-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hanuman/./**/Hanuman-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
