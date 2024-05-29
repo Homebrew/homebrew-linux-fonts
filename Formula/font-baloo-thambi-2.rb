7faae16e2a44861e2934ac1ef301df8b597fcb4a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:45 +0000

    font-baloo-thambi-2 updated

--- Formula/font-baloo-thambi-2.rb
+++ Formula/font-baloo-thambi-2.rb
@@ -1,12 +1,14 @@
 class FontBalooThambi2 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/baloothambi2/BalooThambi2%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Baloo Thambi 2"
+  desc "Baloo thambi 2 font"
   homepage "https://fonts.google.com/specimen/Baloo+Thambi+2"
+  head "https://github.com/google/fonts/raw/main/ofl/baloothambi2/BalooThambi2%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BalooThambi2\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooThambi2[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
