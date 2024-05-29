6ae6125cfad0ff2be1a05f4377a7c63d8d20ea77
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:08 +0000

    font-playwrite-vn updated

--- Formula/font-playwrite-vn.rb
+++ Formula/font-playwrite-vn.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteVn < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwritevn/PlaywriteVN%5Bwght%5D.ttf"
-  desc "Playwrite VN"
+  desc "Playwrite vn font"
   homepage "https://github.com/vv-monsalve/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritevn/PlaywriteVN%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteVN\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteVN[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
