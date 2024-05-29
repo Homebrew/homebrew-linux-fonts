fbc4b64080bd4691511bfc7b51cf1df0b822d9f1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-ark-pixel-12px-monospaced updated

--- Formula/font-ark-pixel-12px-monospaced.rb
+++ Formula/font-ark-pixel-12px-monospaced.rb
@@ -1,10 +1,10 @@
 class FontArkPixel12pxMonospaced < Formula
-  version "2024.05.12"
-  sha256 "2780a0c1ebf0c9bad784d4452b61e00aa262d7429d6eed21c0f944e040409c93"
-  url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-12px-monospaced-otf-v#{version}.zip"
-  desc "Ark Pixel 12px Monospaced"
   desc "Open source Pan-CJK pixel font"
   homepage "https://github.com/TakWolf/ark-pixel-font"
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/2024.05.12/ark-pixel-font-12px-monospaced-otf-v2024.05.12.zip"
+  version "2024.05.12"
+  sha256 "2780a0c1ebf0c9bad784d4452b61e00aa262d7429d6eed21c0f944e040409c93"
+
   def install
     (share/"fonts").install Dir.glob("./**/ark-pixel-12px-monospaced-ja.otf")[0]
     (share/"fonts").install Dir.glob("./**/ark-pixel-12px-monospaced-ko.otf")[0]
@@ -14,8 +14,8 @@ class FontArkPixel12pxMonospaced < Formula
     (share/"fonts").install Dir.glob("./**/ark-pixel-12px-monospaced-zh_tr.otf")[0]
     (share/"fonts").install Dir.glob("./**/ark-pixel-12px-monospaced-zh_tw.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
