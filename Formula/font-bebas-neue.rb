ac2a130830f80c8dff2e8acd50eb5b42efc7ce24
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-bebas-neue updated

--- Formula/font-bebas-neue.rb
+++ Formula/font-bebas-neue.rb
@@ -1,12 +1,14 @@
 class FontBebasNeue < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bebasneue/BebasNeue-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bebas Neue"
+  desc "Bebas neue font"
   homepage "https://fonts.google.com/specimen/Bebas+Neue"
+  head "https://github.com/google/fonts/raw/main/ofl/bebasneue/BebasNeue-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BebasNeue-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
