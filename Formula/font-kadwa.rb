4e582970ffa9fb3eea277e3d3c3d76aa2b067c16
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-kadwa updated

--- Formula/font-kadwa.rb
+++ Formula/font-kadwa.rb
@@ -1,13 +1,17 @@
 class FontKadwa < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kadwa"
-  desc "Kadwa"
+  desc "Kadwa font"
   homepage "https://fonts.google.com/specimen/Kadwa"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kadwa"
+
   def install
     (share/"fonts").install Dir.glob("ofl/kadwa/./**/Kadwa-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kadwa/./**/Kadwa-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
