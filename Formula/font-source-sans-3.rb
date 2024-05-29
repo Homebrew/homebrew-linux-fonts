b68071d7fc0d2ec89d0aff50598f5ffda599e569
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-source-sans-3 updated

--- Formula/font-source-sans-3.rb
+++ Formula/font-source-sans-3.rb
@@ -1,13 +1,17 @@
 class FontSourceSans3 < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sourcesans3"
-  desc "Source Sans 3"
+  desc "Source sans 3 font"
   homepage "https://fonts.google.com/specimen/Source+Sans+3"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sourcesans3"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/sourcesans3/./**/SourceSans3-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/sourcesans3/./**/SourceSans3\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sourcesans3/./**/SourceSans3-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sourcesans3/./**/SourceSans3[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
