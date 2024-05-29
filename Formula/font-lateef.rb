8e73b281201c38dedc975b90e3152c549a549aa0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-lateef updated

--- Formula/font-lateef.rb
+++ Formula/font-lateef.rb
@@ -1,7 +1,11 @@
 class FontLateef < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lateef"
-  desc "Lateef"
+  desc "Lateef font"
   homepage "https://fonts.google.com/specimen/Lateef"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/lateef"
+
   def install
     (share/"fonts").install Dir.glob("ofl/lateef/./**/Lateef-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lateef/./**/Lateef-ExtraBold.ttf")[0]
@@ -11,8 +15,8 @@ class FontLateef < Formula
     (share/"fonts").install Dir.glob("ofl/lateef/./**/Lateef-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lateef/./**/Lateef-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
