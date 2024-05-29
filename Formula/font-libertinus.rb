4fd1a4fb66ab2c0004f7e17d37f45121e5b84dc7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:32 +0000

    font-libertinus updated

--- Formula/font-libertinus.rb
+++ Formula/font-libertinus.rb
@@ -1,28 +1,28 @@
 class FontLibertinus < Formula
-  version "7.040"
-  sha256 "7fe9f022722d1c1cc67dc2c28a110b3bb55bae3575196160d2422c89333b3850"
-  url "https://github.com/alerque/libertinus/releases/download/v#{version}/Libertinus-#{version}.tar.xz"
-  desc "Libertinus"
   desc "Proportional serif typeface inspired by 19th century book type"
   homepage "https://github.com/alerque/libertinus"
+  url "https://github.com/alerque/libertinus/releases/download/v7.040/Libertinus-7.040.tar.xz"
+  version "7.040"
+  sha256 "7fe9f022722d1c1cc67dc2c28a110b3bb55bae3575196160d2422c89333b3850"
+
   def install
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusKeyboard-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusMath-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusMono-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSans-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSans-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSans-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSerif-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSerif-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSerif-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSerif-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSerif-Semibold.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSerif-SemiboldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSerifDisplay-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Libertinus-#{version}/static/OTF/**/LibertinusSerifInitials-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSerifInitials-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusMath-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusMono-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSans-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSans-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSans-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSerif-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSerif-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSerif-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSerif-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSerif-Semibold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSerif-SemiboldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusSerifDisplay-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libertinus-7.040/static/OTF/LibertinusKeyboard-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
