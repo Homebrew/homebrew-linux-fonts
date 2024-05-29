2e2ea0b13b6002a63aadc2daf00bb5c7702f11cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-new-rocker updated

--- Formula/font-new-rocker.rb
+++ Formula/font-new-rocker.rb
@@ -1,12 +1,14 @@
 class FontNewRocker < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/newrocker/NewRocker-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "New Rocker"
+  desc "New rocker font"
   homepage "https://fonts.google.com/specimen/New+Rocker"
+  head "https://github.com/google/fonts/raw/main/ofl/newrocker/NewRocker-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NewRocker-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
