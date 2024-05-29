e18e38b427c670b8f56c673adcaba56b1b839f45
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:00 +0000

    font-noto-serif-ottoman-siyaq updated

--- Formula/font-noto-serif-ottoman-siyaq.rb
+++ Formula/font-noto-serif-ottoman-siyaq.rb
@@ -1,13 +1,14 @@
 class FontNotoSerifOttomanSiyaq < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifottomansiyaq/NotoSerifOttomanSiyaq-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Ottoman Siyaq"
   desc "Modulated (“serif”) design for the arabic form of the siyaq numeral system"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Ottoman+Siyaq"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifottomansiyaq/NotoSerifOttomanSiyaq-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifOttomanSiyaq-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
