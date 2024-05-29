e37fdb33f7d609327b53977f9382ee4c712b2f15
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-color-emoji updated

--- Formula/font-noto-color-emoji.rb
+++ Formula/font-noto-color-emoji.rb
@@ -1,12 +1,14 @@
 class FontNotoColorEmoji < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Color Emoji"
+  desc "Noto color emoji font"
   homepage "https://www.google.com/get/noto/#emoji-zsye-color"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoColorEmoji.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
