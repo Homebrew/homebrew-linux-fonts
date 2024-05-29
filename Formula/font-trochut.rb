8ef25ff0afea55120736c06e97138d0321eb3315
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:31 +0000

    font-trochut updated

--- Formula/font-trochut.rb
+++ Formula/font-trochut.rb
@@ -1,14 +1,18 @@
 class FontTrochut < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/trochut"
-  desc "Trochut"
+  desc "Trochut font"
   homepage "https://fonts.google.com/specimen/Trochut"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/trochut"
+
   def install
     (share/"fonts").install Dir.glob("ofl/trochut/./**/Trochut-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/trochut/./**/Trochut-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/trochut/./**/Trochut-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
