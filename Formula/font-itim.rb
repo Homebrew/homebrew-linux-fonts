9eded3ef21fe752a95bb607444723174fba36737
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-itim updated

--- Formula/font-itim.rb
+++ Formula/font-itim.rb
@@ -1,12 +1,14 @@
 class FontItim < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/itim/Itim-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Itim"
+  desc "Itim font"
   homepage "https://fonts.google.com/specimen/Itim"
+  head "https://github.com/google/fonts/raw/main/ofl/itim/Itim-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Itim-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
