dff1b66ed3d4b84008fbf9c5fdfa52487f7a4b91
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-dorsa updated

--- Formula/font-dorsa.rb
+++ Formula/font-dorsa.rb
@@ -1,12 +1,14 @@
 class FontDorsa < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/dorsa/Dorsa-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Dorsa"
+  desc "Dorsa font"
   homepage "https://fonts.google.com/specimen/Dorsa"
+  head "https://github.com/google/fonts/raw/main/ofl/dorsa/Dorsa-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Dorsa-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
