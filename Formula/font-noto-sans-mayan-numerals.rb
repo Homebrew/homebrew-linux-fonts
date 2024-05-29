38b25ff4e39455423fdc64d0ce2e45e29a974a78
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-mayan-numerals updated

--- Formula/font-noto-sans-mayan-numerals.rb
+++ Formula/font-noto-sans-mayan-numerals.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMayanNumerals < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmayannumerals/NotoSansMayanNumerals-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Mayan Numerals"
+  desc "Noto sans mayan numerals font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Mayan+Numerals"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmayannumerals/NotoSansMayanNumerals-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMayanNumerals-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
