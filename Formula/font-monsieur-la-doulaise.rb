f02a3da41d3f4b70e0dcb9deb05cac099cc74359
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-monsieur-la-doulaise updated

--- Formula/font-monsieur-la-doulaise.rb
+++ Formula/font-monsieur-la-doulaise.rb
@@ -1,12 +1,14 @@
 class FontMonsieurLaDoulaise < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/monsieurladoulaise/MonsieurLaDoulaise-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Monsieur La Doulaise"
+  desc "Monsieur la doulaise font"
   homepage "https://fonts.google.com/specimen/Monsieur+La+Doulaise"
+  head "https://github.com/google/fonts/raw/main/ofl/monsieurladoulaise/MonsieurLaDoulaise-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MonsieurLaDoulaise-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
