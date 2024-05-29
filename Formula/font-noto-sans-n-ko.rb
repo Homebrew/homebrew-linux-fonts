9743653cd6ad4cff2019be7f62c65c9c12584dfe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:54 +0000

    font-noto-sans-n-ko updated

--- Formula/font-noto-sans-n-ko.rb
+++ Formula/font-noto-sans-n-ko.rb
@@ -1,12 +1,14 @@
 class FontNotoSansNKo < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNKo-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans NKo"
+  desc "Noto sans nko font"
   homepage "https://www.google.com/get/noto/#sans-nkoo"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNKo-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansNKo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
