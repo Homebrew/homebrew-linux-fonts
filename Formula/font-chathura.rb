c0e7fd5186cf1dd520ac155d51553b3d423be875
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-chathura updated

--- Formula/font-chathura.rb
+++ Formula/font-chathura.rb
@@ -1,7 +1,11 @@
 class FontChathura < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/chathura"
-  desc "Chathura"
+  desc "Chathura font"
   homepage "https://fonts.google.com/specimen/Chathura"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/chathura"
+
   def install
     (share/"fonts").install Dir.glob("ofl/chathura/./**/Chathura-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/chathura/./**/Chathura-ExtraBold.ttf")[0]
@@ -9,8 +13,8 @@ class FontChathura < Formula
     (share/"fonts").install Dir.glob("ofl/chathura/./**/Chathura-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/chathura/./**/Chathura-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
