99621bf09638a29462a104ca9ab6b37e45790a74
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-emoji updated

--- Formula/font-noto-emoji.rb
+++ Formula/font-noto-emoji.rb
@@ -1,12 +1,14 @@
 class FontNotoEmoji < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoemoji/NotoEmoji%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Emoji"
+  desc "Noto emoji font"
   homepage "https://fonts.google.com/specimen/Noto+Emoji"
+  head "https://github.com/google/fonts/raw/main/ofl/notoemoji/NotoEmoji%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoEmoji\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoEmoji[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
