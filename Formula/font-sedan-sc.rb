88da1db7c843d2630d19c5ab17f1dbbfb7291d98
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-sedan-sc updated

--- Formula/font-sedan-sc.rb
+++ Formula/font-sedan-sc.rb
@@ -1,12 +1,14 @@
 class FontSedanSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sedansc/SedanSC-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sedan SC"
+  desc "Sedan sc font"
   homepage "https://fonts.google.com/specimen/Sedan+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/sedansc/SedanSC-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SedanSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
