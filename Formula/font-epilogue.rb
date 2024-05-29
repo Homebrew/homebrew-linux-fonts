955d940732d2126eab27e2d303ffab17ad7503df
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-epilogue updated

--- Formula/font-epilogue.rb
+++ Formula/font-epilogue.rb
@@ -1,14 +1,17 @@
 class FontEpilogue < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/epilogue"
-  desc "Epilogue"
   desc "Sans serif variable font with a weight axis"
   homepage "https://fonts.google.com/specimen/Epilogue"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/epilogue"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/epilogue/./**/Epilogue-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/epilogue/./**/Epilogue\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/epilogue/./**/Epilogue-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/epilogue/./**/Epilogue[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
