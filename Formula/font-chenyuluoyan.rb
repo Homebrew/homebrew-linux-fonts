9bc8e619f91af814470f6cb54daf7a41bed92ad7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:54 +0000

    font-chenyuluoyan updated

--- Formula/font-chenyuluoyan.rb
+++ Formula/font-chenyuluoyan.rb
@@ -1,16 +1,15 @@
 class FontChenyuluoyan < Formula
-  version "1.0-motospaced"
-  sha256 "d8ecd0598634c92f0b29f90aabef8f7916f17aef19b0350883ca7b46979a5373"
-  url "https://github.com/Chenyu-otf/chenyuluoyan_thin/releases/download/v#{version}/ChenYuluoyan-Thin-Monospaced.ttf"
-  desc "Chenyuluoyan"
-  desc "辰宇落雁體"
   desc "Open-source traditional Chinese handwritten font"
   homepage "https://github.com/Chenyu-otf/chenyuluoyan_thin"
+  url "https://github.com/Chenyu-otf/chenyuluoyan_thin/releases/download/v1.0-motospaced/ChenYuluoyan-Thin-Monospaced.ttf"
+  version "1.0-motospaced"
+  sha256 "d8ecd0598634c92f0b29f90aabef8f7916f17aef19b0350883ca7b46979a5373"
+
   def install
     (share/"fonts").install Dir.glob("./**/ChenYuluoyan-Thin-Monospaced.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
