1312b595fca4093c4cd266d33e45f48f655f75a0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:38 +0000

    font-zcool-kuaile updated

--- Formula/font-zcool-kuaile.rb
+++ Formula/font-zcool-kuaile.rb
@@ -1,12 +1,14 @@
 class FontZcoolKuaile < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/zcoolkuaile/ZCOOLKuaiLe-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "ZCOOL KuaiLe"
+  desc "Zcool kuaile font"
   homepage "https://fonts.google.com/specimen/ZCOOL+KuaiLe"
+  head "https://github.com/google/fonts/raw/main/ofl/zcoolkuaile/ZCOOLKuaiLe-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ZCOOLKuaiLe-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
