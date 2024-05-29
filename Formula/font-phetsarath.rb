b5a48e2fd8604617add2f944770e4d0d89d77b97
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-phetsarath updated

--- Formula/font-phetsarath.rb
+++ Formula/font-phetsarath.rb
@@ -1,13 +1,17 @@
 class FontPhetsarath < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/phetsarath"
-  desc "Phetsarath"
+  desc "Phetsarath font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/phetsarath"
+
   def install
     (share/"fonts").install Dir.glob("ofl/phetsarath/./**/Phetsarath-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/phetsarath/./**/Phetsarath-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
