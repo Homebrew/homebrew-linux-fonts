9c07c04275bfb0663e80a8587e48e13e9a4bf1ee
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-piazzolla updated

--- Formula/font-piazzolla.rb
+++ Formula/font-piazzolla.rb
@@ -1,14 +1,17 @@
 class FontPiazzolla < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/piazzolla"
-  desc "Piazzolla"
   desc "Serif font family for media"
   homepage "https://fonts.google.com/specimen/Piazzolla"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/piazzolla"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/piazzolla/./**/Piazzolla-Italic\\[opsz,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/piazzolla/./**/Piazzolla\\[opsz,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/piazzolla/./**/Piazzolla-Italic[opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/piazzolla/./**/Piazzolla[opsz,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
