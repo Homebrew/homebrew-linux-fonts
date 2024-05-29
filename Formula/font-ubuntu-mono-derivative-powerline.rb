f148bcb69bec09864a238de77aa1c15c95f81892
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:32 +0000

    font-ubuntu-mono-derivative-powerline updated

--- Formula/font-ubuntu-mono-derivative-powerline.rb
+++ Formula/font-ubuntu-mono-derivative-powerline.rb
@@ -1,15 +1,18 @@
 class FontUbuntuMonoDerivativePowerline < Formula
-  head "https://github.com/powerline/fonts.git", branch: "master", only_path: "UbuntuMono"
-  desc "Ubuntu Mono derivative Powerline"
+  desc "Ubuntu mono derivative powerline font"
   homepage "https://github.com/powerline/fonts/tree/master/UbuntuMono"
+  head "https://github.com/powerline/fonts.git",
+       branch:    "master",
+       only_path: "UbuntuMono"
+
   def install
     (share/"fonts").install Dir.glob("UbuntuMono/./**/Ubuntu Mono derivative Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("UbuntuMono/./**/Ubuntu Mono derivative Powerline Bold.ttf")[0]
     (share/"fonts").install Dir.glob("UbuntuMono/./**/Ubuntu Mono derivative Powerline Bold Italic.ttf")[0]
     (share/"fonts").install Dir.glob("UbuntuMono/./**/Ubuntu Mono derivative Powerline Italic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
