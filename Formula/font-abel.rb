e96f75fb455700d3f259cefbdf14c389d25ca53d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-abel updated

--- Formula/font-abel.rb
+++ Formula/font-abel.rb
@@ -1,12 +1,14 @@
 class FontAbel < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/abel/Abel-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Abel"
+  desc "Abel font"
   homepage "https://fonts.google.com/specimen/Abel"
+  head "https://github.com/google/fonts/raw/main/ofl/abel/Abel-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Abel-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
