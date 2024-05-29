c93816ad791348081edfca30451a2a200eca5fbf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:54 +0000

    font-chewy updated

--- Formula/font-chewy.rb
+++ Formula/font-chewy.rb
@@ -1,12 +1,14 @@
 class FontChewy < Formula
-  head "https://github.com/google/fonts/raw/main/apache/chewy/Chewy-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Chewy"
+  desc "Chewy font"
   homepage "https://fonts.google.com/specimen/Chewy"
+  head "https://github.com/google/fonts/raw/main/apache/chewy/Chewy-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Chewy-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
