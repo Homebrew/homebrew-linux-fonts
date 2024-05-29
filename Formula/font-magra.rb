1b95b0d6ae9070a5faa510703a66e1f5dc68cf59
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:36 +0000

    font-magra updated

--- Formula/font-magra.rb
+++ Formula/font-magra.rb
@@ -1,13 +1,17 @@
 class FontMagra < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/magra"
-  desc "Magra"
+  desc "Magra font"
   homepage "https://fonts.google.com/specimen/Magra"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/magra"
+
   def install
     (share/"fonts").install Dir.glob("ofl/magra/./**/Magra-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/magra/./**/Magra-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
