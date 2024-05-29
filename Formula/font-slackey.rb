76b014b5e52b33249148f244265bcd9f0f19dc1b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-slackey updated

--- Formula/font-slackey.rb
+++ Formula/font-slackey.rb
@@ -1,12 +1,14 @@
 class FontSlackey < Formula
-  head "https://github.com/google/fonts/raw/main/apache/slackey/Slackey-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Slackey"
+  desc "Slackey font"
   homepage "https://fonts.google.com/specimen/Slackey"
+  head "https://github.com/google/fonts/raw/main/apache/slackey/Slackey-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Slackey-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
