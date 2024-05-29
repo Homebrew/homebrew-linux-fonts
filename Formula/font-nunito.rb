16636f0ae39a9e26c409f478f6c2c9f373ca8514
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-nunito updated

--- Formula/font-nunito.rb
+++ Formula/font-nunito.rb
@@ -1,13 +1,17 @@
 class FontNunito < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/nunito"
-  desc "Nunito"
+  desc "Nunito font"
   homepage "https://fonts.google.com/specimen/Nunito"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/nunito"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/nunito/./**/Nunito-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/nunito/./**/Nunito\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/nunito/./**/Nunito-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/nunito/./**/Nunito[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
