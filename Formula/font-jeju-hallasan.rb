e59af1a51784aab428cfea6329b3eb27ffb4c11a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jeju-hallasan updated

--- Formula/font-jeju-hallasan.rb
+++ Formula/font-jeju-hallasan.rb
@@ -1,12 +1,14 @@
 class FontJejuHallasan < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jejuhallasan/JejuHallasan-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jeju Hallasan"
+  desc "Jeju hallasan font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts/raw/main/ofl/jejuhallasan/JejuHallasan-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JejuHallasan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
