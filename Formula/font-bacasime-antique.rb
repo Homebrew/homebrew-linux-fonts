61679cfdc52d1cb0ca5c006c20584323b6f65d2e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-bacasime-antique updated

--- Formula/font-bacasime-antique.rb
+++ Formula/font-bacasime-antique.rb
@@ -1,13 +1,14 @@
 class FontBacasimeAntique < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bacasimeantique/BacasimeAntique-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bacasime Antique"
   desc "Stylistically a transitional design"
   homepage "https://fonts.google.com/specimen/Bacasime+Antique"
+  head "https://github.com/google/fonts/raw/main/ofl/bacasimeantique/BacasimeAntique-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BacasimeAntique-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
