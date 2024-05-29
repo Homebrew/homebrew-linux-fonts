7eb88ae3a6ab2437ee3aea9017cbd4b70a01fcd2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:05 +0000

    font-fairfax updated

--- Formula/font-fairfax.rb
+++ Formula/font-fairfax.rb
@@ -1,15 +1,17 @@
 class FontFairfax < Formula
-  head "https://www.kreativekorp.com/swdownload/fonts/relaybm/fairfax.zip", user_agent: :fake
-  desc "Fairfax"
+  desc "Fairfax font"
   homepage "https://www.kreativekorp.com/software/fonts/constructium.shtml"
+  head "https://www.kreativekorp.com/swdownload/fonts/relaybm/fairfax.zip",
+       user_agent: ":fake"
+
   def install
     (share/"fonts").install Dir.glob("./**/Fairfax.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FairfaxBold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FairfaxItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FairfaxSerif.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
