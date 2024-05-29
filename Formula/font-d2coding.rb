cf9ec212ca60925552a9682c6af51d4112a5c09f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-d2coding updated

--- Formula/font-d2coding.rb
+++ Formula/font-d2coding.rb
@@ -1,14 +1,15 @@
 class FontD2coding < Formula
+  desc "D2 coding font"
+  homepage "https://github.com/naver/d2codingfont"
+  url "https://github.com/naver/d2codingfont/releases/download/VER1.3.2/D2Coding-Ver1.3.2-20180524.zip"
   version "1.3.2,20180524"
   sha256 "0f1c9192eac7d56329dddc620f9f1666b707e9c8ed38fe1f988d0ae3e30b24e6"
-  url "https://github.com/naver/d2codingfont/releases/download/VER#{version.to_s.sub(/,.*/, "")}/D2Coding-Ver#{version.to_s.sub(/,.*/, "")}-#{version.to_s.sub(/.*,/, "")}.zip"
-  desc "D2 Coding"
-  homepage "https://github.com/naver/d2codingfont"
+
   def install
-    (share/"fonts").install Dir.glob("D2Coding/**/D2Coding-Ver#{version.to_s.sub(/,.*/, "")}-#{version.to_s.sub(/.*,/, "")}.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/D2Coding/D2Coding-Ver1.3.2-20180524.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
