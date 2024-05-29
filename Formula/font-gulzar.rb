5dc393db75222dd98ed5b0c399f173207c96259a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-gulzar updated

--- Formula/font-gulzar.rb
+++ Formula/font-gulzar.rb
@@ -1,13 +1,14 @@
 class FontGulzar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gulzar/Gulzar-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gulzar"
   desc "Nasta’liq type for which an original latin counterpart was designed"
   homepage "https://fonts.google.com/specimen/Gulzar"
+  head "https://github.com/google/fonts/raw/main/ofl/gulzar/Gulzar-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Gulzar-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
