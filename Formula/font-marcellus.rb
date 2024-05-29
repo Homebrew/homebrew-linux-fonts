7747157d6a4aca10ab82ea54d0af675132d4feba
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:37 +0000

    font-marcellus updated

--- Formula/font-marcellus.rb
+++ Formula/font-marcellus.rb
@@ -1,12 +1,14 @@
 class FontMarcellus < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/marcellus/Marcellus-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Marcellus"
+  desc "Marcellus font"
   homepage "https://fonts.google.com/specimen/Marcellus"
+  head "https://github.com/google/fonts/raw/main/ofl/marcellus/Marcellus-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Marcellus-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
