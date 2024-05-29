0705fccce281339f673d518782b2e0779cb26713
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:05 +0000

    font-explora updated

--- Formula/font-explora.rb
+++ Formula/font-explora.rb
@@ -1,13 +1,14 @@
 class FontExplora < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/explora/Explora-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Explora"
   desc "Beautiful calligraphic typeface with swash forms"
   homepage "https://fonts.google.com/specimen/Explora"
+  head "https://github.com/google/fonts/raw/main/ofl/explora/Explora-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Explora-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
