5932ac18f0280765b6a0bfa5a9aba2b0e5a68d21
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-petrona updated

--- Formula/font-petrona.rb
+++ Formula/font-petrona.rb
@@ -1,13 +1,17 @@
 class FontPetrona < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/petrona"
-  desc "Petrona"
+  desc "Petrona font"
   homepage "https://fonts.google.com/specimen/Petrona"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/petrona"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/petrona/./**/Petrona-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/petrona/./**/Petrona\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/petrona/./**/Petrona-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/petrona/./**/Petrona[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
