4f1be15fa1b7dd2c80992369c29057511e5f48cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-gochi-hand updated

--- Formula/font-gochi-hand.rb
+++ Formula/font-gochi-hand.rb
@@ -1,12 +1,14 @@
 class FontGochiHand < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gochihand/GochiHand-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gochi Hand"
+  desc "Gochi hand font"
   homepage "https://fonts.google.com/specimen/Gochi+Hand"
+  head "https://github.com/google/fonts/raw/main/ofl/gochihand/GochiHand-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GochiHand-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
