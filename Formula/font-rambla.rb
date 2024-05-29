10130a4e47131ceb946184cd949fc9974480abdb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-rambla updated

--- Formula/font-rambla.rb
+++ Formula/font-rambla.rb
@@ -1,15 +1,19 @@
 class FontRambla < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rambla"
-  desc "Rambla"
+  desc "Rambla font"
   homepage "https://fonts.google.com/specimen/Rambla"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/rambla"
+
   def install
     (share/"fonts").install Dir.glob("ofl/rambla/./**/Rambla-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/rambla/./**/Rambla-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/rambla/./**/Rambla-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/rambla/./**/Rambla-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
