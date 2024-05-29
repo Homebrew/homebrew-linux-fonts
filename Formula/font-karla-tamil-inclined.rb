1f718356c0c8933a7273a3691c0c9fd3b1858383
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:27 +0000

    font-karla-tamil-inclined updated

--- Formula/font-karla-tamil-inclined.rb
+++ Formula/font-karla-tamil-inclined.rb
@@ -1,13 +1,17 @@
 class FontKarlaTamilInclined < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/karlatamilinclined"
-  desc "Karla Tamil Inclined"
+  desc "Karla tamil inclined font"
   homepage "https://fonts.google.com/specimen/Karla"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/karlatamilinclined"
+
   def install
     (share/"fonts").install Dir.glob("ofl/karlatamilinclined/./**/KarlaTamilInclined-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/karlatamilinclined/./**/KarlaTamilInclined-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
