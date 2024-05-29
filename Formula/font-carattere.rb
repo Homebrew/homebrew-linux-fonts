29397ad8ffb0cb9684bfadebf844e76e4fcbefa0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-carattere updated

--- Formula/font-carattere.rb
+++ Formula/font-carattere.rb
@@ -1,13 +1,14 @@
 class FontCarattere < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/carattere/Carattere-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Carattere"
   desc "Perfect where formal elegance and beauty are essential"
   homepage "https://fonts.google.com/specimen/Carattere"
+  head "https://github.com/google/fonts/raw/main/ofl/carattere/Carattere-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Carattere-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
