15c069264da0d908b428bdd8000bc44930c35076
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:40 +0000

    font-metal-mania updated

--- Formula/font-metal-mania.rb
+++ Formula/font-metal-mania.rb
@@ -1,12 +1,14 @@
 class FontMetalMania < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/metalmania/MetalMania-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Metal Mania"
+  desc "Metal mania font"
   homepage "https://fonts.google.com/specimen/Metal+Mania"
+  head "https://github.com/google/fonts/raw/main/ofl/metalmania/MetalMania-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MetalMania-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
