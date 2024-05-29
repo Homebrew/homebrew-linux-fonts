30762e44aa6f54f77b1c77517312b3ccae3a8410
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-sans-adlam updated

--- Formula/font-noto-sans-adlam.rb
+++ Formula/font-noto-sans-adlam.rb
@@ -1,12 +1,14 @@
 class FontNotoSansAdlam < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAdlam-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Adlam"
+  desc "Noto sans adlam font"
   homepage "https://www.google.com/get/noto/#sans-adlm"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAdlam-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansAdlam-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
