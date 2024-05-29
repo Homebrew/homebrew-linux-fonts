08843318063118165ff99ab612f1c1f003739c01
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-signika updated

--- Formula/font-signika.rb
+++ Formula/font-signika.rb
@@ -1,13 +1,14 @@
 class FontSignika < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/signika/Signika%5BGRAD%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Signika"
   desc "Sans-serif signage typeface with a gentle character"
   homepage "https://fonts.google.com/specimen/Signika"
+  head "https://github.com/google/fonts/raw/main/ofl/signika/Signika%5BGRAD%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Signika\\[GRAD,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Signika[GRAD,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
