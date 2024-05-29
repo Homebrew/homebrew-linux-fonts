bccebfea2f42b6c59ce25680d739ef37f217b2c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-sarala updated

--- Formula/font-sarala.rb
+++ Formula/font-sarala.rb
@@ -1,13 +1,17 @@
 class FontSarala < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sarala"
-  desc "Sarala"
+  desc "Sarala font"
   homepage "https://fonts.google.com/specimen/Sarala"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sarala"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sarala/./**/Sarala-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sarala/./**/Sarala-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
