26d067821c32b92a547c78942c84e2c9bd9d7f98
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:11 +0000

    font-puritan updated

--- Formula/font-puritan.rb
+++ Formula/font-puritan.rb
@@ -1,15 +1,19 @@
 class FontPuritan < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/puritan"
-  desc "Puritan"
+  desc "Puritan font"
   homepage "https://fonts.google.com/specimen/Puritan"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/puritan"
+
   def install
     (share/"fonts").install Dir.glob("ofl/puritan/./**/Puritan-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/puritan/./**/Puritan-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/puritan/./**/Puritan-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/puritan/./**/Puritan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
