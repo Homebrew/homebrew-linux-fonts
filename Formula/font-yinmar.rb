b2df60e3cf43b824121691f04b856558518b0d45
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-yinmar updated

--- Formula/font-yinmar.rb
+++ Formula/font-yinmar.rb
@@ -1,12 +1,14 @@
 class FontYinmar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yinmar/Yinmar-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yinmar"
+  desc "Yinmar font"
   homepage "https://fonts.google.com/specimen/Yinmar"
+  head "https://github.com/google/fonts/raw/main/ofl/yinmar/Yinmar-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Yinmar-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
