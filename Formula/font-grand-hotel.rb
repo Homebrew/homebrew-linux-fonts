b937c61ae2c2e8957813e4c3299554a8b9d9eb84
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-grand-hotel updated

--- Formula/font-grand-hotel.rb
+++ Formula/font-grand-hotel.rb
@@ -1,12 +1,14 @@
 class FontGrandHotel < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/grandhotel/GrandHotel-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Grand Hotel"
+  desc "Grand hotel font"
   homepage "https://fonts.google.com/specimen/Grand+Hotel"
+  head "https://github.com/google/fonts/raw/main/ofl/grandhotel/GrandHotel-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GrandHotel-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
