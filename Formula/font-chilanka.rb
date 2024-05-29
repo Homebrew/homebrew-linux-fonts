b644f4aebbf686c031b2a3cb62f4e3d063e88a0c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:54 +0000

    font-chilanka updated

--- Formula/font-chilanka.rb
+++ Formula/font-chilanka.rb
@@ -1,12 +1,14 @@
 class FontChilanka < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/chilanka/Chilanka-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Chilanka"
+  desc "Chilanka font"
   homepage "https://fonts.google.com/specimen/Chilanka"
+  head "https://github.com/google/fonts/raw/main/ofl/chilanka/Chilanka-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Chilanka-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
