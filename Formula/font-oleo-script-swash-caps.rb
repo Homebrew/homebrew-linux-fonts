def3792caa354b5b335a33068783addd2802c94e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-oleo-script-swash-caps updated

--- Formula/font-oleo-script-swash-caps.rb
+++ Formula/font-oleo-script-swash-caps.rb
@@ -1,13 +1,17 @@
 class FontOleoScriptSwashCaps < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/oleoscriptswashcaps"
-  desc "Oleo Script Swash Caps"
+  desc "Oleo script swash caps font"
   homepage "https://fonts.google.com/specimen/Oleo+Script+Swash+Caps"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/oleoscriptswashcaps"
+
   def install
     (share/"fonts").install Dir.glob("ofl/oleoscriptswashcaps/./**/OleoScriptSwashCaps-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/oleoscriptswashcaps/./**/OleoScriptSwashCaps-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
