63337835c598061fb29a75e18f26f2a46812184d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-telex updated

--- Formula/font-telex.rb
+++ Formula/font-telex.rb
@@ -1,12 +1,14 @@
 class FontTelex < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/telex/Telex-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Telex"
+  desc "Telex font"
   homepage "https://fonts.google.com/specimen/Telex"
+  head "https://github.com/google/fonts/raw/main/ofl/telex/Telex-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Telex-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
