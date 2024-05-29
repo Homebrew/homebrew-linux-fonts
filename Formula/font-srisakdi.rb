15edcf3ff39c2882337b94059f241386ca921c88
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-srisakdi updated

--- Formula/font-srisakdi.rb
+++ Formula/font-srisakdi.rb
@@ -1,13 +1,17 @@
 class FontSrisakdi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/srisakdi"
-  desc "Srisakdi"
+  desc "Srisakdi font"
   homepage "https://fonts.google.com/specimen/Srisakdi"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/srisakdi"
+
   def install
     (share/"fonts").install Dir.glob("ofl/srisakdi/./**/Srisakdi-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/srisakdi/./**/Srisakdi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
