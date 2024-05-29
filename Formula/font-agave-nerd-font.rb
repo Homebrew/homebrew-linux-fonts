0bd7e3994ce25d59330f85010a18c6b8daf6a0b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-agave-nerd-font updated

--- Formula/font-agave-nerd-font.rb
+++ Formula/font-agave-nerd-font.rb
@@ -1,10 +1,10 @@
 class FontAgaveNerdFont < Formula
-  version "3.2.1"
-  sha256 "248fdd6944f635ab1e8094cfd161d2408e64b7ff444401709734c73bdf6eb9cc"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Agave.zip"
-  desc "Agave Nerd Font (Agave)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Agave.zip"
+  version "3.2.1"
+  sha256 "248fdd6944f635ab1e8094cfd161d2408e64b7ff444401709734c73bdf6eb9cc"
+
   def install
     (share/"fonts").install Dir.glob("./**/AgaveNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AgaveNerdFont-Regular.ttf")[0]
@@ -13,8 +13,8 @@ class FontAgaveNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/AgaveNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AgaveNerdFontPropo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
