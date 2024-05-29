43cfe332531b857c9c973ef3811ed2ed22e3d1ad
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-fontdiner-swanky updated

--- Formula/font-fontdiner-swanky.rb
+++ Formula/font-fontdiner-swanky.rb
@@ -1,12 +1,14 @@
 class FontFontdinerSwanky < Formula
-  head "https://github.com/google/fonts/raw/main/apache/fontdinerswanky/FontdinerSwanky-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Fontdiner Swanky"
+  desc "Fontdiner swanky font"
   homepage "https://fonts.google.com/specimen/Fontdiner+Swanky"
+  head "https://github.com/google/fonts/raw/main/apache/fontdinerswanky/FontdinerSwanky-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/FontdinerSwanky-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
