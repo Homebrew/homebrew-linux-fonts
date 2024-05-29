68445c71afef213fbc8f1bc4aa3512c6801693ab
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:31 +0000

    font-tiro-kannada updated

--- Formula/font-tiro-kannada.rb
+++ Formula/font-tiro-kannada.rb
@@ -1,14 +1,17 @@
 class FontTiroKannada < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tirokannada"
-  desc "Tiro Kannada"
   desc "Especially suited to traditional literary publishing"
   homepage "https://fonts.google.com/specimen/Tiro+Kannada"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tirokannada"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tirokannada/./**/TiroKannada-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tirokannada/./**/TiroKannada-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
