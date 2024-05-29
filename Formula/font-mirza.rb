ab0fc92aac601ce4f63c435d9bdc12b634adb808
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-mirza updated

--- Formula/font-mirza.rb
+++ Formula/font-mirza.rb
@@ -1,15 +1,19 @@
 class FontMirza < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/mirza"
-  desc "Mirza"
+  desc "Mirza font"
   homepage "https://fonts.google.com/specimen/Mirza"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/mirza"
+
   def install
     (share/"fonts").install Dir.glob("ofl/mirza/./**/Mirza-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/mirza/./**/Mirza-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/mirza/./**/Mirza-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/mirza/./**/Mirza-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
