00ef78543842ef5c301f5c5c00344fcce092624a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:12 +0000

    font-radley updated

--- Formula/font-radley.rb
+++ Formula/font-radley.rb
@@ -1,13 +1,17 @@
 class FontRadley < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/radley"
-  desc "Radley"
+  desc "Radley font"
   homepage "https://fonts.google.com/specimen/Radley"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/radley"
+
   def install
     (share/"fonts").install Dir.glob("ofl/radley/./**/Radley-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/radley/./**/Radley-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
