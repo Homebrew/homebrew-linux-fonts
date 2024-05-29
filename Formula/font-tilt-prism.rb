326805880b3c20c19fcbd9f657c7eff0ef4d99bd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-tilt-prism updated

--- Formula/font-tilt-prism.rb
+++ Formula/font-tilt-prism.rb
@@ -1,12 +1,14 @@
 class FontTiltPrism < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tiltprism/TiltPrism%5BXROT%2CYROT%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Tilt Prism"
+  desc "Tilt prism font"
   homepage "https://fonts.google.com/specimen/Tilt+Prism"
+  head "https://github.com/google/fonts/raw/main/ofl/tiltprism/TiltPrism%5BXROT%2CYROT%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/TiltPrism\\[XROT,YROT\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TiltPrism[XROT,YROT].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
