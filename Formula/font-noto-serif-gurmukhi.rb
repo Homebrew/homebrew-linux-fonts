154153622dba5f3b3b1138c78dcacd1132f5eb43
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-gurmukhi updated

--- Formula/font-noto-serif-gurmukhi.rb
+++ Formula/font-noto-serif-gurmukhi.rb
@@ -1,12 +1,14 @@
 class FontNotoSerifGurmukhi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifgurmukhi/NotoSerifGurmukhi%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Gurmukhi"
+  desc "Noto serif gurmukhi font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Gurmukhi"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifgurmukhi/NotoSerifGurmukhi%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifGurmukhi\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifGurmukhi[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
