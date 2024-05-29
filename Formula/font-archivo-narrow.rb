c7c17e3198d9c1efd7438a127e866d43a23f2aec
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-archivo-narrow updated

--- Formula/font-archivo-narrow.rb
+++ Formula/font-archivo-narrow.rb
@@ -1,13 +1,17 @@
 class FontArchivoNarrow < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/archivonarrow"
-  desc "Archivo Narrow"
+  desc "Archivo narrow font"
   homepage "https://fonts.google.com/specimen/Archivo+Narrow"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/archivonarrow"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/archivonarrow/./**/ArchivoNarrow-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/archivonarrow/./**/ArchivoNarrow\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/archivonarrow/./**/ArchivoNarrow-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/archivonarrow/./**/ArchivoNarrow[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
