b404df071e595624c76fe08139358b582cddf16d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-sometype-mono updated

--- Formula/font-sometype-mono.rb
+++ Formula/font-sometype-mono.rb
@@ -1,17 +1,19 @@
 class FontSometypeMono < Formula
-  head "https://github.com/dharmatype/Sometype-Mono/archive/master.zip", verified: "github.com/dharmatype/Sometype-Mono/"
-  desc "Sometype Mono"
+  desc "Sometype mono font"
   homepage "https://monospacedfont.com/"
+  head "https://github.com/dharmatype/Sometype-Mono/archive/master.zip",
+       verified: "github.com/dharmatype/Sometype-Mono/"
+
   def install
-    (share/"fonts").install Dir.glob("Sometype-Mono-master/fonts/otf/**/SometypeMono-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("Sometype-Mono-master/fonts/otf/**/SometypeMono-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Sometype-Mono-master/fonts/otf/**/SometypeMono-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("Sometype-Mono-master/fonts/otf/**/SometypeMono-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Sometype-Mono-master/fonts/otf/**/SometypeMono-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Sometype-Mono-master/fonts/otf/**/SometypeMono-RegularItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Sometype-Mono-master/fonts/otf/SometypeMono-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Sometype-Mono-master/fonts/otf/SometypeMono-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Sometype-Mono-master/fonts/otf/SometypeMono-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Sometype-Mono-master/fonts/otf/SometypeMono-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Sometype-Mono-master/fonts/otf/SometypeMono-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Sometype-Mono-master/fonts/otf/SometypeMono-RegularItalic.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
