147b0b73a5751588c056ffecff49f9ed6b6c91dd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:51 +0000

    font-noto-sans-javanese updated

--- Formula/font-noto-sans-javanese.rb
+++ Formula/font-noto-sans-javanese.rb
@@ -1,12 +1,14 @@
 class FontNotoSansJavanese < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansJavanese-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Javanese"
+  desc "Noto sans javanese font"
   homepage "https://www.google.com/get/noto/#sans-java"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansJavanese-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansJavanese-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
