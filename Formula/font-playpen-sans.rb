023f4caf1d22cfe532798123481a3149a0a790da
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:07 +0000

    font-playpen-sans updated

--- Formula/font-playpen-sans.rb
+++ Formula/font-playpen-sans.rb
@@ -1,13 +1,14 @@
 class FontPlaypenSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playpensans/PlaypenSans%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Playpen Sans"
   desc "Variable font with a weight range from thin to extrabold"
   homepage "https://fonts.google.com/specimen/Playpen+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/playpensans/PlaypenSans%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaypenSans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaypenSans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
