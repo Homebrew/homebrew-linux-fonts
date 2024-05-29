1bfe462d70014c97de0db60a192df1e72963fe99
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-miss-fajardose updated

--- Formula/font-miss-fajardose.rb
+++ Formula/font-miss-fajardose.rb
@@ -1,12 +1,14 @@
 class FontMissFajardose < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/missfajardose/MissFajardose-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Miss Fajardose"
+  desc "Miss fajardose font"
   homepage "https://fonts.google.com/specimen/Miss+Fajardose"
+  head "https://github.com/google/fonts/raw/main/ofl/missfajardose/MissFajardose-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MissFajardose-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
