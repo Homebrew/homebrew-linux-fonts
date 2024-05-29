c9021ec9aeaa6251f519143bace5346764c3baf5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-gidugu updated

--- Formula/font-gidugu.rb
+++ Formula/font-gidugu.rb
@@ -1,12 +1,14 @@
 class FontGidugu < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gidugu/Gidugu-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gidugu"
+  desc "Gidugu font"
   homepage "https://fonts.google.com/specimen/Gidugu"
+  head "https://github.com/google/fonts/raw/main/ofl/gidugu/Gidugu-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Gidugu-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
