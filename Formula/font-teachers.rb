7765c0a7f943e9172bbdfd67abe19e2c0cc673e6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-teachers updated

--- Formula/font-teachers.rb
+++ Formula/font-teachers.rb
@@ -1,13 +1,17 @@
 class FontTeachers < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/teachers"
-  desc "Teachers"
+  desc "Teachers font"
   homepage "https://fonts.google.com/specimen/Teachers"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/teachers"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/teachers/./**/Teachers-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/teachers/./**/Teachers\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/teachers/./**/Teachers-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/teachers/./**/Teachers[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
