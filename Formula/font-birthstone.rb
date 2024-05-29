085448ad092c129810b88647bb4a97a1e3a8d0c3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-birthstone updated

--- Formula/font-birthstone.rb
+++ Formula/font-birthstone.rb
@@ -1,13 +1,14 @@
 class FontBirthstone < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/birthstone/Birthstone-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Birthstone"
   desc "Sibling birthstone bounce that includes caps and ending swashed forms"
   homepage "https://fonts.google.com/specimen/Birthstone"
+  head "https://github.com/google/fonts/raw/main/ofl/birthstone/Birthstone-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Birthstone-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
