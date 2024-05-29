33c7044e1fa564a2898a7e04e4c1e70cf60cec61
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-nosifer updated

--- Formula/font-nosifer.rb
+++ Formula/font-nosifer.rb
@@ -1,12 +1,14 @@
 class FontNosifer < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nosifer/Nosifer-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Nosifer"
+  desc "Nosifer font"
   homepage "https://fonts.google.com/specimen/Nosifer"
+  head "https://github.com/google/fonts/raw/main/ofl/nosifer/Nosifer-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Nosifer-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
