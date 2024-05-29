907919feadf9b8da97323142e91c4b24078aac96
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-cantora-one updated

--- Formula/font-cantora-one.rb
+++ Formula/font-cantora-one.rb
@@ -1,12 +1,14 @@
 class FontCantoraOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/cantoraone/CantoraOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Cantora One"
+  desc "Cantora one font"
   homepage "https://fonts.google.com/specimen/Cantora+One"
+  head "https://github.com/google/fonts/raw/main/ofl/cantoraone/CantoraOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/CantoraOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
