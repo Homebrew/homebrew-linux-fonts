70bf34ccec6f7093ec6107c8865837e1ff8f2f75
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-lacquer updated

--- Formula/font-lacquer.rb
+++ Formula/font-lacquer.rb
@@ -1,12 +1,14 @@
 class FontLacquer < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lacquer/Lacquer-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Lacquer"
+  desc "Lacquer font"
   homepage "https://fonts.google.com/specimen/Lacquer"
+  head "https://github.com/google/fonts/raw/main/ofl/lacquer/Lacquer-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Lacquer-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
