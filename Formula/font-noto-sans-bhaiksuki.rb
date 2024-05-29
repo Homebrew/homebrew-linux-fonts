cf02291ce415fb08725a72972dab082851258a52
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-bhaiksuki updated

--- Formula/font-noto-sans-bhaiksuki.rb
+++ Formula/font-noto-sans-bhaiksuki.rb
@@ -1,12 +1,14 @@
 class FontNotoSansBhaiksuki < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansbhaiksuki/NotoSansBhaiksuki-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Bhaiksuki"
+  desc "Noto sans bhaiksuki font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Bhaiksuki"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansbhaiksuki/NotoSansBhaiksuki-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansBhaiksuki-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
