f4a1bb41cc0bf6951436e6c528aac4723a799bbd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:33 +0000

    font-unbounded updated

--- Formula/font-unbounded.rb
+++ Formula/font-unbounded.rb
@@ -1,13 +1,14 @@
 class FontUnbounded < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/unbounded/Unbounded%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Unbounded"
   desc "Possibly the first on-chain funded font in the world"
   homepage "https://fonts.google.com/specimen/Unbounded"
+  head "https://github.com/google/fonts/raw/main/ofl/unbounded/Unbounded%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Unbounded\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Unbounded[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
