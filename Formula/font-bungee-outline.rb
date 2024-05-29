781ce03e0f8c21b5e9bd6d1c7ec2fab1b0ce1b28
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:50 +0000

    font-bungee-outline updated

--- Formula/font-bungee-outline.rb
+++ Formula/font-bungee-outline.rb
@@ -1,12 +1,14 @@
 class FontBungeeOutline < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bungeeoutline/BungeeOutline-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bungee Outline"
+  desc "Bungee outline font"
   homepage "https://fonts.google.com/specimen/Bungee+Outline"
+  head "https://github.com/google/fonts/raw/main/ofl/bungeeoutline/BungeeOutline-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BungeeOutline-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
