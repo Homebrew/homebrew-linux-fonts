6cbb345cdf2f5a998dbe43c615857b16b8064fda
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-sawarabi-mincho updated

--- Formula/font-sawarabi-mincho.rb
+++ Formula/font-sawarabi-mincho.rb
@@ -1,12 +1,14 @@
 class FontSawarabiMincho < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sawarabimincho/SawarabiMincho-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sawarabi Mincho"
+  desc "Sawarabi mincho font"
   homepage "https://fonts.google.com/specimen/Sawarabi+Mincho"
+  head "https://github.com/google/fonts/raw/main/ofl/sawarabimincho/SawarabiMincho-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SawarabiMincho-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
