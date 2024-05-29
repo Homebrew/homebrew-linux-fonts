5312f39703b093be4955226b4baf5af1c29ca831
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-mountains-of-christmas updated

--- Formula/font-mountains-of-christmas.rb
+++ Formula/font-mountains-of-christmas.rb
@@ -1,13 +1,17 @@
 class FontMountainsOfChristmas < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "apache/mountainsofchristmas"
-  desc "Mountains of Christmas"
+  desc "Mountains of christmas font"
   homepage "https://fonts.google.com/specimen/Mountains+of+Christmas"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "apache/mountainsofchristmas"
+
   def install
     (share/"fonts").install Dir.glob("apache/mountainsofchristmas/./**/MountainsofChristmas-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("apache/mountainsofchristmas/./**/MountainsofChristmas-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
