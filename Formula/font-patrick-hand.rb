b017f28490cb3798d2845cba5726718576850a65
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-patrick-hand updated

--- Formula/font-patrick-hand.rb
+++ Formula/font-patrick-hand.rb
@@ -1,12 +1,14 @@
 class FontPatrickHand < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/patrickhand/PatrickHand-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Patrick Hand"
+  desc "Patrick hand font"
   homepage "https://fonts.google.com/specimen/Patrick+Hand"
+  head "https://github.com/google/fonts/raw/main/ofl/patrickhand/PatrickHand-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PatrickHand-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
