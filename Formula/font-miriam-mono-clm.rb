4eea83b66830d60f7f7ce57bcfa3a2791deba6a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-miriam-mono-clm updated

--- Formula/font-miriam-mono-clm.rb
+++ Formula/font-miriam-mono-clm.rb
@@ -1,15 +1,16 @@
 class FontMiriamMonoClm < Formula
-  head "https://opensiddur.org/wp-content/uploads/fonts/MiriamMonoCLM/MiriamMonoCLM.zip"
-  desc "Miriam Mono CLM"
+  desc "Miriam mono clm font"
   homepage "https://opensiddur.org/wp-content/uploads/fonts/display-font-charmap.php?fnt=MiriamMonoCLM"
+  head "https://opensiddur.org/wp-content/uploads/fonts/MiriamMonoCLM/MiriamMonoCLM.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/MiriamMonoCLM-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MiriamMonoCLM-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MiriamMonoCLM-Book.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MiriamMonoCLM-BookOblique.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
