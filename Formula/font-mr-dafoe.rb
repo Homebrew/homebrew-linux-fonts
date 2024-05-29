d2ece73b865bd197d5a608ed3e2ae17139c9064c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-mr-dafoe updated

--- Formula/font-mr-dafoe.rb
+++ Formula/font-mr-dafoe.rb
@@ -1,12 +1,14 @@
 class FontMrDafoe < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mrdafoe/MrDafoe-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mr Dafoe"
+  desc "Mr dafoe font"
   homepage "https://fonts.google.com/specimen/Mr+Dafoe"
+  head "https://github.com/google/fonts/raw/main/ofl/mrdafoe/MrDafoe-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MrDafoe-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
