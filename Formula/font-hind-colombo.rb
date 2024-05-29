c0ac47cacb5c19b3608893ed24143b3904895b1c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:16 +0000

    font-hind-colombo updated

--- Formula/font-hind-colombo.rb
+++ Formula/font-hind-colombo.rb
@@ -1,7 +1,11 @@
 class FontHindColombo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/hindcolombo"
-  desc "Hind Colombo"
+  desc "Hind colombo font"
   homepage "https://fonts.google.com/specimen/Hind"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/hindcolombo"
+
   def install
     (share/"fonts").install Dir.glob("ofl/hindcolombo/./**/HindColombo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hindcolombo/./**/HindColombo-Light.ttf")[0]
@@ -9,8 +13,8 @@ class FontHindColombo < Formula
     (share/"fonts").install Dir.glob("ofl/hindcolombo/./**/HindColombo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hindcolombo/./**/HindColombo-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
