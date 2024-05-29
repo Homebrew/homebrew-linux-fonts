053b113095e5be2b7c6038b1a2574f2ad4eddea3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-jomolhari updated

--- Formula/font-jomolhari.rb
+++ Formula/font-jomolhari.rb
@@ -1,12 +1,14 @@
 class FontJomolhari < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jomolhari/Jomolhari-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jomolhari"
+  desc "Jomolhari font"
   homepage "https://fonts.google.com/specimen/Jomolhari"
+  head "https://github.com/google/fonts/raw/main/ofl/jomolhari/Jomolhari-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Jomolhari-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
