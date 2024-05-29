69212942195c645c2241e7273b3ba70ab926ad00
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:58 +0000

    font-noto-serif-ahom updated

--- Formula/font-noto-serif-ahom.rb
+++ Formula/font-noto-serif-ahom.rb
@@ -1,12 +1,14 @@
 class FontNotoSerifAhom < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifahom/NotoSerifAhom-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Ahom"
+  desc "Noto serif ahom font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Ahom"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifahom/NotoSerifAhom-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifAhom-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
