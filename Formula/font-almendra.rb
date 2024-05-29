a05ac8dc4af083001f4b57596cb7c3966a3ae666
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-almendra updated

--- Formula/font-almendra.rb
+++ Formula/font-almendra.rb
@@ -1,15 +1,19 @@
 class FontAlmendra < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/almendra"
-  desc "Almendra"
+  desc "Almendra font"
   homepage "https://fonts.google.com/specimen/Almendra"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/almendra"
+
   def install
     (share/"fonts").install Dir.glob("ofl/almendra/./**/Almendra-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/almendra/./**/Almendra-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/almendra/./**/Almendra-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/almendra/./**/Almendra-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
