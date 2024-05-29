211d667703e7a6b660da6a263dfaf0f00c7fa54b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-elice-digital-baeum updated

--- Formula/font-elice-digital-baeum.rb
+++ Formula/font-elice-digital-baeum.rb
@@ -1,13 +1,14 @@
 class FontEliceDigitalBaeum < Formula
-  head "https://font.elice.io/static/downloads/EliceDigitalBaeum_OTF.zip"
-  desc "Elice Digital Baeum"
+  desc "Elice digital baeum font"
   homepage "https://font.elice.io/"
+  head "https://font.elice.io/static/downloads/EliceDigitalBaeum_OTF.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/EliceDigitalBaeumOTF_Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/EliceDigitalBaeumOTF_Bold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
