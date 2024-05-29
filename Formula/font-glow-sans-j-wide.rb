11a459016920e43726aa94f7818fd433e7869f0f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:12 +0000

    font-glow-sans-j-wide updated

--- Formula/font-glow-sans-j-wide.rb
+++ Formula/font-glow-sans-j-wide.rb
@@ -1,12 +1,14 @@
 class FontGlowSansJWide < Formula
+  desc "Glow sans j wide font"
+  homepage "https://github.com/welai/glow-sans"
+  url "https://github.com/welai/glow-sans/releases/download/v0.93/GlowSansJ-Wide-v0.93.zip"
   version "0.93"
   sha256 "eb20892d347dce7b53084bf420b3947dad79993cbe54fdebca425273c635e521"
-  url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansJ-Wide-v#{version}.zip"
-  desc "Glow Sans J Wide"
-  homepage "https://github.com/welai/glow-sans"
-  deprecate! date: "2024-02-17", because: :unsupported
+
+  deprecate! "2024-02-17", because: :discontinued
+
   def install
-    (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-Thin.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-Book.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-ExtraBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-ExtraLight.otf")[0]
@@ -14,10 +16,10 @@ class FontGlowSansJWide < Formula
     (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-Light.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansJ-Wide-Bold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
