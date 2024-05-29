5cba9fb816d400df0568216ba082dc66eced9d1a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:05 +0000

    font-faster-one updated

--- Formula/font-faster-one.rb
+++ Formula/font-faster-one.rb
@@ -1,12 +1,14 @@
 class FontFasterOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/fasterone/FasterOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Faster One"
+  desc "Faster one font"
   homepage "https://fonts.google.com/specimen/Faster+One"
+  head "https://github.com/google/fonts/raw/main/ofl/fasterone/FasterOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/FasterOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
