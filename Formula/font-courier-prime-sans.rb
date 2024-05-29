835b7dd09519588db628f8301f42b3f855cacdc6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-courier-prime-sans updated

--- Formula/font-courier-prime-sans.rb
+++ Formula/font-courier-prime-sans.rb
@@ -1,15 +1,16 @@
 class FontCourierPrimeSans < Formula
-  head "https://quoteunquoteapps.com/courierprime/downloads/courier-prime-sans.zip"
-  desc "Courier Prime Sans"
+  desc "Courier prime sans font"
   homepage "https://quoteunquoteapps.com/courierprime/#code-sans"
+  head "https://quoteunquoteapps.com/courierprime/downloads/courier-prime-sans.zip"
+
   def install
-    (share/"fonts").install Dir.glob("CourierPrimeSans-master/ttf/**/Courier Prime Sans Bold Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("CourierPrimeSans-master/ttf/**/Courier Prime Sans Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("CourierPrimeSans-master/ttf/**/Courier Prime Sans Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("CourierPrimeSans-master/ttf/**/Courier Prime Sans.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CourierPrimeSans-master/ttf/Courier Prime Sans Bold Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CourierPrimeSans-master/ttf/Courier Prime Sans Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CourierPrimeSans-master/ttf/Courier Prime Sans Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CourierPrimeSans-master/ttf/Courier Prime Sans.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
