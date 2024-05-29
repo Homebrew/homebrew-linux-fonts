c7511bd01351b9ce5a7f178089e7daefc71c4e52
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-merriweather updated

--- Formula/font-merriweather.rb
+++ Formula/font-merriweather.rb
@@ -1,19 +1,23 @@
 class FontMerriweather < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/merriweather"
-  desc "Merriweather"
+  desc "Merriweather font"
   homepage "https://fonts.google.com/specimen/Merriweather"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/merriweather"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-LightItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
