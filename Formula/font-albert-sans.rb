9e50c4949f3560aed623fe9b0954c41f148fa21d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-albert-sans updated

--- Formula/font-albert-sans.rb
+++ Formula/font-albert-sans.rb
@@ -1,14 +1,17 @@
 class FontAlbertSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/albertsans"
-  desc "Albert Sans"
   desc "Modern geometric sans serif family"
   homepage "https://fonts.google.com/specimen/Albert+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/albertsans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/albertsans/./**/AlbertSans-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/albertsans/./**/AlbertSans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/albertsans/./**/AlbertSans-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/albertsans/./**/AlbertSans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
