df75379cc1c8a55c44c1899d1986424792d89821
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-geist-mono-nerd-font updated

--- Formula/font-geist-mono-nerd-font.rb
+++ Formula/font-geist-mono-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontGeistMonoNerdFont < Formula
-  version "3.2.1"
-  sha256 "87f9a0a7a585ffcf29a881002b1b5fc5ea091d5bc175b130154dc10a59fb3c76"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/GeistMono.zip"
-  desc "GeistMono Nerd Font (Geist Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/GeistMono.zip"
+  version "3.2.1"
+  sha256 "87f9a0a7a585ffcf29a881002b1b5fc5ea091d5bc175b130154dc10a59fb3c76"
+
   def install
-    (share/"fonts").install Dir.glob("./**/GeistMonoNerdFont-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GeistMonoNerdFontPropo-UltraLight.otf")[0]
     (share/"fonts").install Dir.glob("./**/GeistMonoNerdFont-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/GeistMonoNerdFont-Light.otf")[0]
     (share/"fonts").install Dir.glob("./**/GeistMonoNerdFont-Medium.otf")[0]
@@ -32,10 +32,10 @@ class FontGeistMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/GeistMonoNerdFontPropo-SemiBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/GeistMonoNerdFontPropo-Thin.otf")[0]
     (share/"fonts").install Dir.glob("./**/GeistMonoNerdFontPropo-UltraBlack.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMonoNerdFontPropo-UltraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GeistMonoNerdFont-Black.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
