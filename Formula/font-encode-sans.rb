39bdcc7cb894e38104f14571b44d74b57d682859
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-encode-sans updated

--- Formula/font-encode-sans.rb
+++ Formula/font-encode-sans.rb
@@ -1,12 +1,14 @@
 class FontEncodeSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/encodesans/EncodeSans%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Encode Sans"
+  desc "Encode sans font"
   homepage "https://fonts.google.com/specimen/Encode+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/encodesans/EncodeSans%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/EncodeSans\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/EncodeSans[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
