d81ad62c69f1ff49110dc6c4ff7087c7a68a4534
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:31 +0000

    font-lekton updated

--- Formula/font-lekton.rb
+++ Formula/font-lekton.rb
@@ -1,14 +1,18 @@
 class FontLekton < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lekton"
-  desc "Lekton"
+  desc "Lekton font"
   homepage "https://fonts.google.com/specimen/Lekton"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/lekton"
+
   def install
     (share/"fonts").install Dir.glob("ofl/lekton/./**/Lekton-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lekton/./**/Lekton-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lekton/./**/Lekton-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
