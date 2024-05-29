05ef71a0a3a28d692cfce028e1e15a42d25214a7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:10 +0000

    font-praise updated

--- Formula/font-praise.rb
+++ Formula/font-praise.rb
@@ -1,13 +1,14 @@
 class FontPraise < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/praise/Praise-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Praise"
   desc "Versatile script with variations from casual (non-connecting) to formal appeal"
   homepage "https://fonts.google.com/specimen/Praise"
+  head "https://github.com/google/fonts/raw/main/ofl/praise/Praise-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Praise-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
