b170b122c8cceffd79a3488f8292f488315053f1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:15 +0000

    font-roboto-condensed updated

--- Formula/font-roboto-condensed.rb
+++ Formula/font-roboto-condensed.rb
@@ -1,13 +1,17 @@
 class FontRobotoCondensed < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/robotocondensed"
-  desc "Roboto Condensed"
+  desc "Roboto condensed font"
   homepage "https://fonts.google.com/specimen/Roboto+Condensed"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/robotocondensed"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/robotocondensed/./**/RobotoCondensed-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/robotocondensed/./**/RobotoCondensed\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/robotocondensed/./**/RobotoCondensed-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/robotocondensed/./**/RobotoCondensed[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
