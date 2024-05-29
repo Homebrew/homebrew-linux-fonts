c458fddd5ddaaea22c7a5dc175b547aa02ccd0ab
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-imperial-script updated

--- Formula/font-imperial-script.rb
+++ Formula/font-imperial-script.rb
@@ -1,13 +1,14 @@
 class FontImperialScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/imperialscript/ImperialScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Imperial Script"
   desc "Formal script font with clean connections and an elegant look"
   homepage "https://fonts.google.com/specimen/Imperial+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/imperialscript/ImperialScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ImperialScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
