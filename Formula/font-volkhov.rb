3ddc4fce21d5d410d4e756f9d65928366d6561b2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-volkhov updated

--- Formula/font-volkhov.rb
+++ Formula/font-volkhov.rb
@@ -1,15 +1,19 @@
 class FontVolkhov < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/volkhov"
-  desc "Volkhov"
+  desc "Volkhov font"
   homepage "https://fonts.google.com/specimen/Volkhov"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/volkhov"
+
   def install
     (share/"fonts").install Dir.glob("ofl/volkhov/./**/Volkhov-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/volkhov/./**/Volkhov-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/volkhov/./**/Volkhov-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/volkhov/./**/Volkhov-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
