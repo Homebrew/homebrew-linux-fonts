ce8805ea112d969495ab63c0264e19a83b43debb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-tenor-sans updated

--- Formula/font-tenor-sans.rb
+++ Formula/font-tenor-sans.rb
@@ -1,12 +1,14 @@
 class FontTenorSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tenorsans/TenorSans-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Tenor Sans"
+  desc "Tenor sans font"
   homepage "https://fonts.google.com/specimen/Tenor+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/tenorsans/TenorSans-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/TenorSans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
