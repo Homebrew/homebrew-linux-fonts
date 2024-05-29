d9511dad822153b802b4f1836e8c49df1ec581da
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:50 +0000

    font-noto-sans-gunjala-gondi updated

--- Formula/font-noto-sans-gunjala-gondi.rb
+++ Formula/font-noto-sans-gunjala-gondi.rb
@@ -1,12 +1,14 @@
 class FontNotoSansGunjalaGondi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansgunjalagondi/NotoSansGunjalaGondi%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Gunjala Gondi"
+  desc "Noto sans gunjala gondi font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Gunjala+Gondi"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansgunjalagondi/NotoSansGunjalaGondi%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansGunjalaGondi\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansGunjalaGondi[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
