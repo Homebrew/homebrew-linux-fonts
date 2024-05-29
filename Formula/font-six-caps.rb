52d7a840a732e00df8d6ac8cb71e007265cb6343
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-six-caps updated

--- Formula/font-six-caps.rb
+++ Formula/font-six-caps.rb
@@ -1,12 +1,14 @@
 class FontSixCaps < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sixcaps/SixCaps.ttf", verified: "github.com/google/fonts/"
-  desc "Six Caps"
+  desc "Six caps font"
   homepage "https://fonts.google.com/specimen/Six+Caps"
+  head "https://github.com/google/fonts/raw/main/ofl/sixcaps/SixCaps.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SixCaps.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
