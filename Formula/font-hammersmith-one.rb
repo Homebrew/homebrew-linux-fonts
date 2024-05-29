8e368f3786c403402fe426177509195ac1da0f09
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-hammersmith-one updated

--- Formula/font-hammersmith-one.rb
+++ Formula/font-hammersmith-one.rb
@@ -1,12 +1,14 @@
 class FontHammersmithOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/hammersmithone/HammersmithOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Hammersmith One"
+  desc "Hammersmith one font"
   homepage "https://fonts.google.com/specimen/Hammersmith+One"
+  head "https://github.com/google/fonts/raw/main/ofl/hammersmithone/HammersmithOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/HammersmithOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
