3a28553b88c67058cecf16a6472a4901dc020168
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:12 +0000

    font-gloria-hallelujah updated

--- Formula/font-gloria-hallelujah.rb
+++ Formula/font-gloria-hallelujah.rb
@@ -1,12 +1,14 @@
 class FontGloriaHallelujah < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gloriahallelujah/GloriaHallelujah.ttf", verified: "github.com/google/fonts/"
-  desc "Gloria Hallelujah"
+  desc "Gloria hallelujah font"
   homepage "https://fonts.google.com/specimen/Gloria+Hallelujah"
+  head "https://github.com/google/fonts/raw/main/ofl/gloriahallelujah/GloriaHallelujah.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GloriaHallelujah.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
