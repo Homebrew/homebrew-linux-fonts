e7c3f7b54d544c853f1aec9aa0e2724a0719deec
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-flow-block updated

--- Formula/font-flow-block.rb
+++ Formula/font-flow-block.rb
@@ -1,12 +1,14 @@
 class FontFlowBlock < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/flowblock/FlowBlock-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Flow Block"
+  desc "Flow block font"
   homepage "https://fonts.google.com/specimen/Flow+Block"
+  head "https://github.com/google/fonts/raw/main/ofl/flowblock/FlowBlock-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/FlowBlock-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
