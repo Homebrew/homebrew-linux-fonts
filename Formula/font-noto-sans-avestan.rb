5b6c792bc3746c13f397db66889d8ab0e2c7281b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-avestan updated

--- Formula/font-noto-sans-avestan.rb
+++ Formula/font-noto-sans-avestan.rb
@@ -1,12 +1,14 @@
 class FontNotoSansAvestan < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAvestan-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Avestan"
+  desc "Noto sans avestan font"
   homepage "https://www.google.com/get/noto/#sans-avst"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAvestan-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansAvestan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
