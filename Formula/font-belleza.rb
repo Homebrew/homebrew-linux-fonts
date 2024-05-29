90d7141a5b5f6e7f7d366c68ed5323eeba0c02b6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-belleza updated

--- Formula/font-belleza.rb
+++ Formula/font-belleza.rb
@@ -1,12 +1,14 @@
 class FontBelleza < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/belleza/Belleza-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Belleza"
+  desc "Belleza font"
   homepage "https://fonts.google.com/specimen/Belleza"
+  head "https://github.com/google/fonts/raw/main/ofl/belleza/Belleza-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Belleza-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
