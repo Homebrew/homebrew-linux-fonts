8f3155b398eeeefc777d2feeb9f95088b63b153f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:36 +0000

    font-m-plus-2 updated

--- Formula/font-m-plus-2.rb
+++ Formula/font-m-plus-2.rb
@@ -1,12 +1,14 @@
 class FontMPlus2 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mplus2/MPLUS2%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "M PLUS 2"
+  desc "M plus 2 font"
   homepage "https://fonts.google.com/specimen/M+PLUS+2"
+  head "https://github.com/google/fonts/raw/main/ofl/mplus2/MPLUS2%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/MPLUS2\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MPLUS2[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
