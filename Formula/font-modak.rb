e2bdbbfcc4fcc8000e81718e6125cdbbdd99fce9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-modak updated

--- Formula/font-modak.rb
+++ Formula/font-modak.rb
@@ -1,12 +1,14 @@
 class FontModak < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/modak/Modak-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Modak"
+  desc "Modak font"
   homepage "https://fonts.google.com/specimen/Modak"
+  head "https://github.com/google/fonts/raw/main/ofl/modak/Modak-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Modak-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
