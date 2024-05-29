5cfec0ed5aa67c0e6b64a19c65917fd77f58ef1f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-crafty-girls updated

--- Formula/font-crafty-girls.rb
+++ Formula/font-crafty-girls.rb
@@ -1,12 +1,14 @@
 class FontCraftyGirls < Formula
-  head "https://github.com/google/fonts/raw/main/apache/craftygirls/CraftyGirls-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Crafty Girls"
+  desc "Crafty girls font"
   homepage "https://fonts.google.com/specimen/Crafty+Girls"
+  head "https://github.com/google/fonts/raw/main/apache/craftygirls/CraftyGirls-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/CraftyGirls-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
