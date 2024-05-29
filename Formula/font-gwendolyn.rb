14e88311d4d80d6b2e9bfbe1cf89e102a936dba4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-gwendolyn updated

--- Formula/font-gwendolyn.rb
+++ Formula/font-gwendolyn.rb
@@ -1,13 +1,17 @@
 class FontGwendolyn < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gwendolyn"
-  desc "Gwendolyn"
+  desc "Gwendolyn font"
   homepage "https://fonts.google.com/specimen/Gwendolyn"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gwendolyn"
+
   def install
     (share/"fonts").install Dir.glob("ofl/gwendolyn/./**/Gwendolyn-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gwendolyn/./**/Gwendolyn-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
