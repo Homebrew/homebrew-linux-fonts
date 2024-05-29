31de877fe8f96b7a88054c7a98cd8b9676daa207
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-piedra updated

--- Formula/font-piedra.rb
+++ Formula/font-piedra.rb
@@ -1,12 +1,14 @@
 class FontPiedra < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/piedra/Piedra-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Piedra"
+  desc "Piedra font"
   homepage "https://fonts.google.com/specimen/Piedra"
+  head "https://github.com/google/fonts/raw/main/ofl/piedra/Piedra-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Piedra-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
