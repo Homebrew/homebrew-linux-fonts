caab29c276589a0a88c78388ddb09c2ffa5daad5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-grantha updated

--- Formula/font-noto-serif-grantha.rb
+++ Formula/font-noto-serif-grantha.rb
@@ -1,12 +1,14 @@
 class FontNotoSerifGrantha < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifgrantha/NotoSerifGrantha-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Grantha"
+  desc "Noto serif grantha font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Grantha"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifgrantha/NotoSerifGrantha-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifGrantha-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
