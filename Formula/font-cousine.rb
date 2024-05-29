06fd60c2488bf11190534dd20fc0ea9187e23204
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-cousine updated

--- Formula/font-cousine.rb
+++ Formula/font-cousine.rb
@@ -1,15 +1,19 @@
 class FontCousine < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "apache/cousine"
-  desc "Cousine"
+  desc "Cousine font"
   homepage "https://fonts.google.com/specimen/Cousine"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "apache/cousine"
+
   def install
     (share/"fonts").install Dir.glob("apache/cousine/./**/Cousine-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("apache/cousine/./**/Cousine-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("apache/cousine/./**/Cousine-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("apache/cousine/./**/Cousine-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
