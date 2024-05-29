77fd157502d417653f9518ba5730f3140582fa4f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-duru-sans updated

--- Formula/font-duru-sans.rb
+++ Formula/font-duru-sans.rb
@@ -1,12 +1,14 @@
 class FontDuruSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/durusans/DuruSans-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Duru Sans"
+  desc "Duru sans font"
   homepage "https://fonts.google.com/specimen/Duru+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/durusans/DuruSans-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DuruSans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
