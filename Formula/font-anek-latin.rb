4868d110f321903bd39184abbc0ef7fd2ef06d95
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-anek-latin updated

--- Formula/font-anek-latin.rb
+++ Formula/font-anek-latin.rb
@@ -1,12 +1,14 @@
 class FontAnekLatin < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/aneklatin/AnekLatin%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Anek Latin"
+  desc "Anek latin font"
   homepage "https://fonts.google.com/specimen/Anek+Latin"
+  head "https://github.com/google/fonts/raw/main/ofl/aneklatin/AnekLatin%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/AnekLatin\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekLatin[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
