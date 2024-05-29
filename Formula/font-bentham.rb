5ba0161d9fbbfc9dd869a9567ea4e69dc7e66d89
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-bentham updated

--- Formula/font-bentham.rb
+++ Formula/font-bentham.rb
@@ -1,12 +1,14 @@
 class FontBentham < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bentham/Bentham-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bentham"
+  desc "Bentham font"
   homepage "https://fonts.google.com/specimen/Bentham"
+  head "https://github.com/google/fonts/raw/main/ofl/bentham/Bentham-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Bentham-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
