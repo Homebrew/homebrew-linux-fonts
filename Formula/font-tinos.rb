fc437dcd8a0e5d34ccdfca43cbe3652cf9c9dc3a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-tinos updated

--- Formula/font-tinos.rb
+++ Formula/font-tinos.rb
@@ -1,15 +1,19 @@
 class FontTinos < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "apache/tinos"
-  desc "Tinos"
+  desc "Tinos font"
   homepage "https://fonts.google.com/specimen/Tinos"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "apache/tinos"
+
   def install
     (share/"fonts").install Dir.glob("apache/tinos/./**/Tinos-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("apache/tinos/./**/Tinos-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("apache/tinos/./**/Tinos-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("apache/tinos/./**/Tinos-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
