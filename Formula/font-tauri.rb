3d0e26d8dfbd5f18b4d33358a706e9851d4930a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-tauri updated

--- Formula/font-tauri.rb
+++ Formula/font-tauri.rb
@@ -1,12 +1,14 @@
 class FontTauri < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tauri/Tauri-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Tauri"
+  desc "Tauri font"
   homepage "https://fonts.google.com/specimen/Tauri"
+  head "https://github.com/google/fonts/raw/main/ofl/tauri/Tauri-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Tauri-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
