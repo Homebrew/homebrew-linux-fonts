55fe9fa955dc245f74265d7c6d3f3ed9ee71e6d8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-keania-one updated

--- Formula/font-keania-one.rb
+++ Formula/font-keania-one.rb
@@ -1,12 +1,14 @@
 class FontKeaniaOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/keaniaone/KeaniaOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Keania One"
+  desc "Keania one font"
   homepage "https://fonts.google.com/specimen/Keania+One"
+  head "https://github.com/google/fonts/raw/main/ofl/keaniaone/KeaniaOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/KeaniaOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
