e4f16f4d785d345e6cc7c2229088efb298a3d323
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-hind-madurai updated

--- Formula/font-hind-madurai.rb
+++ Formula/font-hind-madurai.rb
@@ -1,7 +1,11 @@
 class FontHindMadurai < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/hindmadurai"
-  desc "Hind Madurai"
+  desc "Hind madurai font"
   homepage "https://fonts.google.com/specimen/Hind+Madurai"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/hindmadurai"
+
   def install
     (share/"fonts").install Dir.glob("ofl/hindmadurai/./**/HindMadurai-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hindmadurai/./**/HindMadurai-Light.ttf")[0]
@@ -9,8 +13,8 @@ class FontHindMadurai < Formula
     (share/"fonts").install Dir.glob("ofl/hindmadurai/./**/HindMadurai-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hindmadurai/./**/HindMadurai-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
