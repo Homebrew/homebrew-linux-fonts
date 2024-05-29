23abd6085ea3c05db00f2db60a06a4e3cf59e813
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-batak updated

--- Formula/font-noto-sans-batak.rb
+++ Formula/font-noto-sans-batak.rb
@@ -1,12 +1,14 @@
 class FontNotoSansBatak < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBatak-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Batak"
+  desc "Noto sans batak font"
   homepage "https://www.google.com/get/noto/#sans-batk"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBatak-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansBatak-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
