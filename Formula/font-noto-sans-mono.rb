13d6a7654ed335799ccdcbd7d6f084dc066321a7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-mono updated

--- Formula/font-noto-sans-mono.rb
+++ Formula/font-noto-sans-mono.rb
@@ -1,15 +1,16 @@
 class FontNotoSansMono < Formula
-  version "2.014"
-  sha256 "090cf6c5e03f337a755630ca888b1fef463e64ae7b33ee134e9309c05f978732"
-  url "https://github.com/notofonts/latin-greek-cyrillic/releases/download/NotoSansMono-v#{version}/NotoSansMono-v#{version}.zip", verified: "github.com/notofonts/"
-  desc "Noto Sans"
   desc "Monospaced variable font"
   homepage "https://notofonts.github.io/"
+  url "https://github.com/notofonts/latin-greek-cyrillic/releases/download/NotoSansMono-v2.014/NotoSansMono-v2.014.zip",
+       verified: "github.com/notofonts/"
+  version "2.014"
+  sha256 "090cf6c5e03f337a755630ca888b1fef463e64ae7b33ee134e9309c05f978732"
+
   def install
-    (share/"fonts").install Dir.glob("NotoSansMono/unhinted/variable/**/NotoSansMono\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansMono/unhinted/variable/NotoSansMono[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
