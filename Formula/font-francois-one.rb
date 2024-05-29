3179ba9484d89d25031571b13db8a1a580953d20
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-francois-one updated

--- Formula/font-francois-one.rb
+++ Formula/font-francois-one.rb
@@ -1,12 +1,14 @@
 class FontFrancoisOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/francoisone/FrancoisOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Francois One"
+  desc "Francois one font"
   homepage "https://fonts.google.com/specimen/Francois+One"
+  head "https://github.com/google/fonts/raw/main/ofl/francoisone/FrancoisOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/FrancoisOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
