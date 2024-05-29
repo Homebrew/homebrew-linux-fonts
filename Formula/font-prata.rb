375716126388b64f74a4fd93c037a1f71ba7ec56
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:10 +0000

    font-prata updated

--- Formula/font-prata.rb
+++ Formula/font-prata.rb
@@ -1,12 +1,14 @@
 class FontPrata < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/prata/Prata-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Prata"
+  desc "Prata font"
   homepage "https://fonts.google.com/specimen/Prata"
+  head "https://github.com/google/fonts/raw/main/ofl/prata/Prata-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Prata-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
