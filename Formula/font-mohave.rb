c48f1ffec5dcdd41cc08c4f3690818c921b99a5f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:42 +0000

    font-mohave updated

--- Formula/font-mohave.rb
+++ Formula/font-mohave.rb
@@ -1,13 +1,17 @@
 class FontMohave < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/mohave"
-  desc "Mohave"
+  desc "Mohave font"
   homepage "https://fonts.google.com/specimen/Mohave"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/mohave"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/mohave/./**/Mohave-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/mohave/./**/Mohave\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/mohave/./**/Mohave-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/mohave/./**/Mohave[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
