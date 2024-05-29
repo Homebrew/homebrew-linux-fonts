6a0d48f602c3b9a20d1c601caabaa14a34f83645
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:12 +0000

    font-rajdhani updated

--- Formula/font-rajdhani.rb
+++ Formula/font-rajdhani.rb
@@ -1,9 +1,10 @@
 class FontRajdhani < Formula
+  desc "Rajdhani font"
+  homepage "https://github.com/itfoundry/rajdhani"
+  url "https://github.com/itfoundry/rajdhani/releases/download/v2.000/rajdhani-2_000.zip"
   version "2.000"
   sha256 "2dd0b14ed9989319fde8518e4b596467ae635571ffe7733d7968201d8cdeb9cf"
-  url "https://github.com/itfoundry/rajdhani/releases/download/v#{version}/rajdhani-#{version.to_s.gsub(".", "_")}.zip"
-  desc "Rajdhani"
-  homepage "https://github.com/itfoundry/rajdhani"
+
   def install
     (share/"fonts").install Dir.glob("./**/Rajdhani-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/Rajdhani-Light.otf")[0]
@@ -11,8 +12,8 @@ class FontRajdhani < Formula
     (share/"fonts").install Dir.glob("./**/Rajdhani-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/Rajdhani-SemiBold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
