a5ec73ae122eb8c6276fb03999165ec5d99153fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:49 +0000

    font-brawler updated

--- Formula/font-brawler.rb
+++ Formula/font-brawler.rb
@@ -1,13 +1,17 @@
 class FontBrawler < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/brawler"
-  desc "Brawler"
+  desc "Brawler font"
   homepage "https://fonts.google.com/specimen/Brawler"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/brawler"
+
   def install
     (share/"fonts").install Dir.glob("ofl/brawler/./**/Brawler-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/brawler/./**/Brawler-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
