784a89eec81b070413ee7c8951847bc9e83af2fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-jsmath-cmbx10 updated

--- Formula/font-jsmath-cmbx10.rb
+++ Formula/font-jsmath-cmbx10.rb
@@ -1,17 +1,18 @@
 class FontJsmathCmbx10 < Formula
-  head "https://www.math.union.edu/~dpvc/jsMath/download/TeX-fonts-10.zip"
-  desc "jsMath-cmbx10"
+  desc "Jsmath-cmbx10 font"
   homepage "https://www.math.union.edu/~dpvc/jsMath/download/jsMath-fonts.html"
+  head "https://www.math.union.edu/~dpvc/jsMath/download/TeX-fonts-10.zip"
+
   def install
-    (share/"fonts").install Dir.glob("TeX-fonts-10/**/jsMath-cmbx10.ttf")[0]
-    (share/"fonts").install Dir.glob("TeX-fonts-10/**/jsMath-cmex10.ttf")[0]
-    (share/"fonts").install Dir.glob("TeX-fonts-10/**/jsMath-cmmi10.ttf")[0]
-    (share/"fonts").install Dir.glob("TeX-fonts-10/**/jsMath-cmr10.ttf")[0]
-    (share/"fonts").install Dir.glob("TeX-fonts-10/**/jsMath-cmsy10.ttf")[0]
-    (share/"fonts").install Dir.glob("TeX-fonts-10/**/jsMath-cmti10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TeX-fonts-10/jsMath-cmbx10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TeX-fonts-10/jsMath-cmex10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TeX-fonts-10/jsMath-cmmi10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TeX-fonts-10/jsMath-cmr10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TeX-fonts-10/jsMath-cmsy10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TeX-fonts-10/jsMath-cmti10.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
