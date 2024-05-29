c153729db9218cbbb6451752b81049b14e8d8355
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-cormorant-sc updated

--- Formula/font-cormorant-sc.rb
+++ Formula/font-cormorant-sc.rb
@@ -1,7 +1,11 @@
 class FontCormorantSc < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cormorantsc"
-  desc "Cormorant SC"
+  desc "Cormorant sc font"
   homepage "https://fonts.google.com/specimen/Cormorant+SC"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cormorantsc"
+
   def install
     (share/"fonts").install Dir.glob("ofl/cormorantsc/./**/CormorantSC-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantsc/./**/CormorantSC-Light.ttf")[0]
@@ -9,8 +13,8 @@ class FontCormorantSc < Formula
     (share/"fonts").install Dir.glob("ofl/cormorantsc/./**/CormorantSC-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantsc/./**/CormorantSC-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
