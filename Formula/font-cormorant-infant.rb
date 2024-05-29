04ac1d0c2edef08c2d5e93e1ff4f791b94324231
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-cormorant-infant updated

--- Formula/font-cormorant-infant.rb
+++ Formula/font-cormorant-infant.rb
@@ -1,9 +1,13 @@
 class FontCormorantInfant < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cormorantinfant"
-  desc "Cormorant Infant"
+  desc "Cormorant infant font"
   homepage "https://fonts.google.com/specimen/Cormorant+Infant"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cormorantinfant"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-Light.ttf")[0]
@@ -12,10 +16,10 @@ class FontCormorantInfant < Formula
     (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/cormorantinfant/./**/CormorantInfant-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
