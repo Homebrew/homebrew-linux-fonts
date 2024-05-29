e5e9e39fae5434353a60505347371533ccca03aa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-biz-udpmincho updated

--- Formula/font-biz-udpmincho.rb
+++ Formula/font-biz-udpmincho.rb
@@ -1,14 +1,17 @@
 class FontBizUdpmincho < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/bizudpmincho"
-  desc "BIZ UDPMincho"
   desc "Universal design typeface in the Japanese Mincho type tradition"
   homepage "https://fonts.google.com/specimen/BIZ+UDPMincho"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/bizudpmincho"
+
   def install
     (share/"fonts").install Dir.glob("ofl/bizudpmincho/./**/BIZUDPMincho-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/bizudpmincho/./**/BIZUDPMincho-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
