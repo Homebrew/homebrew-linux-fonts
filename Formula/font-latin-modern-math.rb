7de7187fcb00601022d2bc29e0a4cd2e927093ba
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:31 +0000

    font-latin-modern-math updated

--- Formula/font-latin-modern-math.rb
+++ Formula/font-latin-modern-math.rb
@@ -1,13 +1,13 @@
 class FontLatinModernMath < Formula
-  head "https://www.gust.org.pl/projects/e-foundry/lm-math/download/latinmodern-math-1959.zip"
-  desc "Latin Modern Math"
   desc "Latin Modern variation focused on mathematics"
   homepage "https://www.gust.org.pl/projects/e-foundry/lm-math"
+  head "https://www.gust.org.pl/projects/e-foundry/lm-math/download/latinmodern-math-1959.zip"
+
   def install
-    (share/"fonts").install Dir.glob("latinmodern-math-1959/otf/**/latinmodern-math.otf")[0]
+    (share/"fonts").install Dir.glob("./**/latinmodern-math-1959/otf/latinmodern-math.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
