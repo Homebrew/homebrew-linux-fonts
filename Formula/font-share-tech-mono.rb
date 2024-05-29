6c5b9afc9b6d0d729b43de8bdbcfe3b39b8f19fb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-share-tech-mono updated

--- Formula/font-share-tech-mono.rb
+++ Formula/font-share-tech-mono.rb
@@ -1,12 +1,14 @@
 class FontShareTechMono < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sharetechmono/ShareTechMono-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Share Tech Mono"
+  desc "Share tech mono font"
   homepage "https://fonts.google.com/specimen/Share+Tech+Mono"
+  head "https://github.com/google/fonts/raw/main/ofl/sharetechmono/ShareTechMono-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ShareTechMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
