0f8f2985532d7e26aac0d01202934fc33e026d70
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:36 +0000

    font-yanone-kaffeesatz updated

--- Formula/font-yanone-kaffeesatz.rb
+++ Formula/font-yanone-kaffeesatz.rb
@@ -1,12 +1,14 @@
 class FontYanoneKaffeesatz < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yanonekaffeesatz/YanoneKaffeesatz%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Yanone Kaffeesatz"
+  desc "Yanone kaffeesatz font"
   homepage "https://fonts.google.com/specimen/Yanone+Kaffeesatz"
+  head "https://github.com/google/fonts/raw/main/ofl/yanonekaffeesatz/YanoneKaffeesatz%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/YanoneKaffeesatz\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/YanoneKaffeesatz[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
