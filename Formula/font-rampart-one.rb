15787ea33ca0467fe44724c174de1e8a0ddd8cf1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-rampart-one updated

--- Formula/font-rampart-one.rb
+++ Formula/font-rampart-one.rb
@@ -1,12 +1,14 @@
 class FontRampartOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rampartone/RampartOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rampart One"
+  desc "Rampart one font"
   homepage "https://fonts.google.com/specimen/Rampart+One"
+  head "https://github.com/google/fonts/raw/main/ofl/rampartone/RampartOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RampartOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
