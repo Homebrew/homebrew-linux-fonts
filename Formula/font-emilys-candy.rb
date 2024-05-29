58dc7c9741fa4709a8158b9fe84fc3a1d71b90a5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-emilys-candy updated

--- Formula/font-emilys-candy.rb
+++ Formula/font-emilys-candy.rb
@@ -1,12 +1,14 @@
 class FontEmilysCandy < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/emilyscandy/EmilysCandy-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Emilys Candy"
+  desc "Emilys candy font"
   homepage "https://fonts.google.com/specimen/Emilys+Candy"
+  head "https://github.com/google/fonts/raw/main/ofl/emilyscandy/EmilysCandy-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/EmilysCandy-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
