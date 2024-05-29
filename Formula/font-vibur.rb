232702dc25c963655dca4a800b7f885844a914d0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-vibur updated

--- Formula/font-vibur.rb
+++ Formula/font-vibur.rb
@@ -1,12 +1,14 @@
 class FontVibur < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/vibur/Vibur-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Vibur"
+  desc "Vibur font"
   homepage "https://fonts.google.com/specimen/Vibur"
+  head "https://github.com/google/fonts/raw/main/ofl/vibur/Vibur-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Vibur-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
