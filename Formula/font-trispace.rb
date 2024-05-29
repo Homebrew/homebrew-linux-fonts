3f2d0b76546b47dbde469a581326a2dfa8f2b833
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:31 +0000

    font-trispace updated

--- Formula/font-trispace.rb
+++ Formula/font-trispace.rb
@@ -1,13 +1,14 @@
 class FontTrispace < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/trispace/Trispace%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Trispace"
   desc "Designed by tyler finck (etc)"
   homepage "https://fonts.google.com/specimen/Trispace"
+  head "https://github.com/google/fonts/raw/main/ofl/trispace/Trispace%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Trispace\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Trispace[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
