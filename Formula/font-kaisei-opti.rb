fd07a4b0c4b887161d9a92c2b021991f70f9b155
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:27 +0000

    font-kaisei-opti updated

--- Formula/font-kaisei-opti.rb
+++ Formula/font-kaisei-opti.rb
@@ -1,15 +1,18 @@
 class FontKaiseiOpti < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kaiseiopti"
-  desc "Kaisei Opti"
   desc "Modern style japanese typeface"
   homepage "https://fonts.google.com/specimen/Kaisei+Opti"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kaiseiopti"
+
   def install
     (share/"fonts").install Dir.glob("ofl/kaiseiopti/./**/KaiseiOpti-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kaiseiopti/./**/KaiseiOpti-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kaiseiopti/./**/KaiseiOpti-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
