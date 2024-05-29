3ada473d5f9cb02e6ba9ea12eecd04c773d76a2a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:27 +0000

    font-kaisei-tokumin updated

--- Formula/font-kaisei-tokumin.rb
+++ Formula/font-kaisei-tokumin.rb
@@ -1,16 +1,19 @@
 class FontKaiseiTokumin < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kaiseitokumin"
-  desc "Kaisei Tokumin"
   desc "Designed to keep the legibility and still have power as an extra bold typeface"
   homepage "https://fonts.google.com/specimen/Kaisei+Tokumin"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kaiseitokumin"
+
   def install
     (share/"fonts").install Dir.glob("ofl/kaiseitokumin/./**/KaiseiTokumin-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kaiseitokumin/./**/KaiseiTokumin-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kaiseitokumin/./**/KaiseiTokumin-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kaiseitokumin/./**/KaiseiTokumin-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
