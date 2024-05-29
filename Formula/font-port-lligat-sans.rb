0a83236664f20e018f94e7740e2b46fd9772a56b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:09 +0000

    font-port-lligat-sans updated

--- Formula/font-port-lligat-sans.rb
+++ Formula/font-port-lligat-sans.rb
@@ -1,12 +1,14 @@
 class FontPortLligatSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/portlligatsans/PortLligatSans-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Port Lligat Sans"
+  desc "Port lligat sans font"
   homepage "https://fonts.google.com/specimen/Port+Lligat+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/portlligatsans/PortLligatSans-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PortLligatSans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
