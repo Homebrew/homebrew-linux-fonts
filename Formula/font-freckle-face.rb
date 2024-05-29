4a969594e68c99904ebde5f8b2670a1de7112e69
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-freckle-face updated

--- Formula/font-freckle-face.rb
+++ Formula/font-freckle-face.rb
@@ -1,12 +1,14 @@
 class FontFreckleFace < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/freckleface/FreckleFace-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Freckle Face"
+  desc "Freckle face font"
   homepage "https://fonts.google.com/specimen/Freckle+Face"
+  head "https://github.com/google/fonts/raw/main/ofl/freckleface/FreckleFace-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/FreckleFace-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
