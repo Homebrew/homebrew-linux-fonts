5b7ea89d01ad70e9688649ec7c8c4fea06dbfda2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-song-myung updated

--- Formula/font-song-myung.rb
+++ Formula/font-song-myung.rb
@@ -1,12 +1,14 @@
 class FontSongMyung < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/songmyung/SongMyung-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Song Myung"
+  desc "Song myung font"
   homepage "https://fonts.google.com/specimen/Song+Myung"
+  head "https://github.com/google/fonts/raw/main/ofl/songmyung/SongMyung-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SongMyung-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
