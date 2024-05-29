a3b67f581c51affefcb2fb4e67793c4c26dcdb1e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-k2d updated

--- Formula/font-k2d.rb
+++ Formula/font-k2d.rb
@@ -1,9 +1,13 @@
 class FontK2d < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/k2d"
-  desc "K2D"
+  desc "K2d font"
   homepage "https://fonts.google.com/specimen/K2D"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/k2d"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-ThinItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-ExtraBoldItalic.ttf")[0]
@@ -18,10 +22,10 @@ class FontK2d < Formula
     (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/k2d/./**/K2D-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
