1a1ed02798e6867f7a442e6217efe488ac9baaa3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-fugaz-one updated

--- Formula/font-fugaz-one.rb
+++ Formula/font-fugaz-one.rb
@@ -1,12 +1,14 @@
 class FontFugazOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/fugazone/FugazOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Fugaz One"
+  desc "Fugaz one font"
   homepage "https://fonts.google.com/specimen/Fugaz+One"
+  head "https://github.com/google/fonts/raw/main/ofl/fugazone/FugazOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/FugazOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
