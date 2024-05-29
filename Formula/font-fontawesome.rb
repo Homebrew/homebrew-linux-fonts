68af103342c4d5e6b1ead6f9d01821d19c0839b8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-fontawesome updated

--- Formula/font-fontawesome.rb
+++ Formula/font-fontawesome.rb
@@ -1,17 +1,18 @@
 class FontFontawesome < Formula
-  version "6.5.2"
-  sha256 "6392bc956eb3d391c9d7a14e891ce8010226ffc0c75f1338db126f13cb9cb8f4"
-  url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip", verified: "github.com/FortAwesome/Font-Awesome/"
-  desc "Font Awesome"
   desc "Icon set and toolkit"
   homepage "https://fontawesome.com/"
+  url "https://github.com/FortAwesome/Font-Awesome/releases/download/6.5.2/fontawesome-free-6.5.2-desktop.zip",
+       verified: "github.com/FortAwesome/Font-Awesome/"
+  version "6.5.2"
+  sha256 "6392bc956eb3d391c9d7a14e891ce8010226ffc0c75f1338db126f13cb9cb8f4"
+
   def install
-    (share/"fonts").install Dir.glob("fontawesome-free-#{version}-desktop/otfs/**/Font Awesome #{version.to_s.sub(/\..*/, "")} Brands-Regular-400.otf")[0]
-    (share/"fonts").install Dir.glob("fontawesome-free-#{version}-desktop/otfs/**/Font Awesome #{version.to_s.sub(/\..*/, "")} Free-Regular-400.otf")[0]
-    (share/"fonts").install Dir.glob("fontawesome-free-#{version}-desktop/otfs/**/Font Awesome #{version.to_s.sub(/\..*/, "")} Free-Solid-900.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fontawesome-free-6.5.2-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fontawesome-free-6.5.2-desktop/otfs/Font Awesome 6 Free-Regular-400.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fontawesome-free-6.5.2-desktop/otfs/Font Awesome 6 Free-Solid-900.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
