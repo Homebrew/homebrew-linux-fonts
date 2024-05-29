65998f9b48da1ebab2b29a81ced9eee041b25792
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-hind updated

--- Formula/font-hind.rb
+++ Formula/font-hind.rb
@@ -1,9 +1,10 @@
 class FontHind < Formula
+  desc "Hind font"
+  homepage "https://github.com/itfoundry/hind"
+  url "https://github.com/itfoundry/hind/releases/download/v2.000/hind-2_000.zip"
   version "2.000"
   sha256 "8748ce1fa0db67d0c782d7824a9fdcf0b8544b9d063db477dff9733774571193"
-  url "https://github.com/itfoundry/hind/releases/download/v2.000/hind-#{version.to_s.gsub(".", "_")}.zip"
-  desc "Hind"
-  homepage "https://github.com/itfoundry/hind"
+
   def install
     (share/"fonts").install Dir.glob("./**/Hind-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/Hind-Light.otf")[0]
@@ -11,8 +12,8 @@ class FontHind < Formula
     (share/"fonts").install Dir.glob("./**/Hind-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/Hind-SemiBold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
