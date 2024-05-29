dea20ddc9ee2c7b97876589874bbc19bda968036
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:40 +0000

    font-metrophobic updated

--- Formula/font-metrophobic.rb
+++ Formula/font-metrophobic.rb
@@ -1,12 +1,14 @@
 class FontMetrophobic < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/metrophobic/Metrophobic-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Metrophobic"
+  desc "Metrophobic font"
   homepage "https://fonts.google.com/specimen/Metrophobic"
+  head "https://github.com/google/fonts/raw/main/ofl/metrophobic/Metrophobic-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Metrophobic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
