b4ec4a2d1a275fb75d1d122b77879ae248ed9586
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:33 +0000

    font-ubuntu-sans updated

--- Formula/font-ubuntu-sans.rb
+++ Formula/font-ubuntu-sans.rb
@@ -1,13 +1,17 @@
 class FontUbuntuSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ufl/ubuntusans"
-  desc "Ubuntu Sans"
+  desc "Ubuntu sans font"
   homepage "https://fonts.google.com/specimen/Ubuntu+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ufl/ubuntusans"
+
   def install
-    (share/"fonts").install Dir.glob("ufl/ubuntusans/./**/UbuntuSans-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ufl/ubuntusans/./**/UbuntuSans\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ufl/ubuntusans/./**/UbuntuSans-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ufl/ubuntusans/./**/UbuntuSans[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
