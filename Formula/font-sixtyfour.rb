a2e87b37d399cc1b06ad21c680f216cd324b8834
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-sixtyfour updated

--- Formula/font-sixtyfour.rb
+++ Formula/font-sixtyfour.rb
@@ -1,12 +1,14 @@
 class FontSixtyfour < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sixtyfour/Sixtyfour%5BBLED%2CSCAN%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Sixtyfour"
+  desc "Sixtyfour font"
   homepage "https://fonts.google.com/specimen/Sixtyfour"
+  head "https://github.com/google/fonts/raw/main/ofl/sixtyfour/Sixtyfour%5BBLED%2CSCAN%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Sixtyfour\\[BLED,SCAN\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Sixtyfour[BLED,SCAN].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
