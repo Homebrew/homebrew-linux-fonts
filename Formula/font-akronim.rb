2ed816473f66674ac2778e0e2a0a2e1e93cac1ec
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-akronim updated

--- Formula/font-akronim.rb
+++ Formula/font-akronim.rb
@@ -1,12 +1,14 @@
 class FontAkronim < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/akronim/Akronim-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Akronim"
+  desc "Akronim font"
   homepage "https://fonts.google.com/specimen/Akronim"
+  head "https://github.com/google/fonts/raw/main/ofl/akronim/Akronim-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Akronim-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
