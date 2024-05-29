4789adb700b092d32adb955f4f95a7aff45be944
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:12 +0000

    font-glory updated

--- Formula/font-glory.rb
+++ Formula/font-glory.rb
@@ -1,14 +1,17 @@
 class FontGlory < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/glory"
-  desc "Glory"
   desc "Suitable for logos, headlines and body text with the available six weights"
   homepage "https://fonts.google.com/specimen/Glory"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/glory"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/glory/./**/Glory-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/glory/./**/Glory\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/glory/./**/Glory-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/glory/./**/Glory[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
