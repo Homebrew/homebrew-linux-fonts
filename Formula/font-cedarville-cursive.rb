64c0627ef934c12f21c4f6e98aac2f8d2f9ce156
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-cedarville-cursive updated

--- Formula/font-cedarville-cursive.rb
+++ Formula/font-cedarville-cursive.rb
@@ -1,12 +1,14 @@
 class FontCedarvilleCursive < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/cedarvillecursive/Cedarville-Cursive.ttf", verified: "github.com/google/fonts/"
-  desc "Cedarville Cursive"
+  desc "Cedarville cursive font"
   homepage "https://fonts.google.com/specimen/Cedarville+Cursive"
+  head "https://github.com/google/fonts/raw/main/ofl/cedarvillecursive/Cedarville-Cursive.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Cedarville-Cursive.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
