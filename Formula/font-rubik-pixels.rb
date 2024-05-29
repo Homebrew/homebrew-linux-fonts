29ed09fdc175aeb0dc8682839ef6a970b0cd4764
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-pixels updated

--- Formula/font-rubik-pixels.rb
+++ Formula/font-rubik-pixels.rb
@@ -1,12 +1,14 @@
 class FontRubikPixels < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikpixels/RubikPixels-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Pixels"
+  desc "Rubik pixels font"
   homepage "https://fonts.google.com/specimen/Rubik+Pixels"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikpixels/RubikPixels-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikPixels-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
