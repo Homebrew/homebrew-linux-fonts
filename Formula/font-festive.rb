57dd4517089915aab6dba3e07f11ecde98e26bbe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:06 +0000

    font-festive updated

--- Formula/font-festive.rb
+++ Formula/font-festive.rb
@@ -1,12 +1,14 @@
 class FontFestive < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/festive/Festive-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Festive"
+  desc "Festive font"
   homepage "https://fonts.google.com/specimen/Festive"
+  head "https://github.com/google/fonts/raw/main/ofl/festive/Festive-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Festive-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
