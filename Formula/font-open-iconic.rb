2e51bc8132f3f4411fc467a79fbf8d750192fc4d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-open-iconic updated

--- Formula/font-open-iconic.rb
+++ Formula/font-open-iconic.rb
@@ -1,14 +1,16 @@
 class FontOpenIconic < Formula
+  desc "Open iconic font"
+  homepage "https://useiconic.com/open/"
+  url "https://codeload.github.com/iconic/open-iconic/zip/1.1.1",
+       verified: "codeload.github.com/iconic/open-iconic/"
   version "1.1.1"
   sha256 "8acf49f08ae5a069935b48e6be20349c4e9f43fcfc773ea0aba5b972b5b3743c"
-  url "https://codeload.github.com/iconic/open-iconic/zip/#{version}", verified: "codeload.github.com/iconic/open-iconic/"
-  desc "Open Iconic"
-  homepage "https://useiconic.com/open/"
+
   def install
-    (share/"fonts").install Dir.glob("open-iconic-#{version}/font/fonts/**/open-iconic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/open-iconic-1.1.1/font/fonts/open-iconic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
