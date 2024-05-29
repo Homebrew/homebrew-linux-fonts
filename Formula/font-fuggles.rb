d7f564b358ec2cf77141741216f93bbfcc0cdaf9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-fuggles updated

--- Formula/font-fuggles.rb
+++ Formula/font-fuggles.rb
@@ -1,13 +1,14 @@
 class FontFuggles < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/fuggles/Fuggles-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Fuggles"
   desc "Powerful script that can be used for sophisticated commercial design"
   homepage "https://fonts.google.com/specimen/Fuggles"
+  head "https://github.com/google/fonts/raw/main/ofl/fuggles/Fuggles-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Fuggles-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
