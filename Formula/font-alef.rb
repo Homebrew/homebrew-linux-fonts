debf399197d7a52bc330104d8ca23c9dfce0b685
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-alef updated

--- Formula/font-alef.rb
+++ Formula/font-alef.rb
@@ -1,13 +1,14 @@
 class FontAlef < Formula
-  head "http://alef.hagilda.com/Alef.zip"
-  desc "Alef"
+  desc "Alef font"
   homepage "http://alef.hagilda.com/"
+  head "http://alef.hagilda.com/Alef.zip"
+
   def install
-    (share/"fonts").install Dir.glob("TTF/**/Alef-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("TTF/**/Alef-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TTF/Alef-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TTF/Alef-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
