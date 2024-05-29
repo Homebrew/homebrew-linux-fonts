f021750024d3bcbb78d47e532435f02267ba2681
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:18 +0000

    font-ia-writer-quattro updated

--- Formula/font-ia-writer-quattro.rb
+++ Formula/font-ia-writer-quattro.rb
@@ -1,17 +1,18 @@
 class FontIaWriterQuattro < Formula
-  head "https://codeload.github.com/iaolo/iA-Fonts/zip/master"
-  desc "iA Writer Quattro"
+  desc "Ia writer quattro font"
   homepage "https://github.com/iaolo/iA-Fonts"
+  head "https://codeload.github.com/iaolo/iA-Fonts/zip/master"
+
   def install
-    (share/"fonts").install Dir.glob("iA-Fonts-master/iA Writer Quattro/Static/**/iAWriterQuattroS-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("iA-Fonts-master/iA Writer Quattro/Static/**/iAWriterQuattroS-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("iA-Fonts-master/iA Writer Quattro/Static/**/iAWriterQuattroS-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("iA-Fonts-master/iA Writer Quattro/Static/**/iAWriterQuattroS-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("iA-Fonts-master/iA Writer Quattro/Variable/**/iAWriterQuattroV-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("iA-Fonts-master/iA Writer Quattro/Variable/**/iAWriterQuattroV.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Quattro/Static/iAWriterQuattroS-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Quattro/Static/iAWriterQuattroS-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Quattro/Static/iAWriterQuattroS-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Quattro/Static/iAWriterQuattroS-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Quattro/Variable/iAWriterQuattroV-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Quattro/Variable/iAWriterQuattroV.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
