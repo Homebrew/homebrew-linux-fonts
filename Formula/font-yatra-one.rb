708153104eb8539ed094088b9cd83d8c5f08eed1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:36 +0000

    font-yatra-one updated

--- Formula/font-yatra-one.rb
+++ Formula/font-yatra-one.rb
@@ -1,12 +1,14 @@
 class FontYatraOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yatraone/YatraOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yatra One"
+  desc "Yatra one font"
   homepage "https://fonts.google.com/specimen/Yatra+One"
+  head "https://github.com/google/fonts/raw/main/ofl/yatraone/YatraOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/YatraOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
