798c1af0df3480b0809ae3833f0be2547b277c88
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-dangrek updated

--- Formula/font-dangrek.rb
+++ Formula/font-dangrek.rb
@@ -1,12 +1,14 @@
 class FontDangrek < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/dangrek/Dangrek-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Dangrek"
+  desc "Dangrek font"
   homepage "https://fonts.google.com/specimen/Dangrek"
+  head "https://github.com/google/fonts/raw/main/ofl/dangrek/Dangrek-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Dangrek-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
