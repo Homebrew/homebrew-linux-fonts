e735d502aa78f74b667a3f5741301c81b9d419c8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:00 +0000

    font-noto-serif-tibetan updated

--- Formula/font-noto-serif-tibetan.rb
+++ Formula/font-noto-serif-tibetan.rb
@@ -1,12 +1,14 @@
 class FontNotoSerifTibetan < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoseriftibetan/NotoSerifTibetan%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Tibetan"
+  desc "Noto serif tibetan font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Tibetan"
+  head "https://github.com/google/fonts/raw/main/ofl/notoseriftibetan/NotoSerifTibetan%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifTibetan\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifTibetan[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
