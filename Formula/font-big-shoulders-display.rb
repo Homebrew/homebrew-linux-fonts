7ed8801ce58a3e428429f8e73fa3f35492ef7ad6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-big-shoulders-display updated

--- Formula/font-big-shoulders-display.rb
+++ Formula/font-big-shoulders-display.rb
@@ -1,12 +1,14 @@
 class FontBigShouldersDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bigshouldersdisplay/BigShouldersDisplay%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Big Shoulders Display"
+  desc "Big shoulders display font"
   homepage "https://fonts.google.com/specimen/Big+Shoulders+Display"
+  head "https://github.com/google/fonts/raw/main/ofl/bigshouldersdisplay/BigShouldersDisplay%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BigShouldersDisplay\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BigShouldersDisplay[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
