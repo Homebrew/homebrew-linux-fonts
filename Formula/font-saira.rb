84e7eab19bd9600f5cd953c9f764e09d2f4c654d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-saira updated

--- Formula/font-saira.rb
+++ Formula/font-saira.rb
@@ -1,13 +1,17 @@
 class FontSaira < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/saira"
-  desc "Saira"
+  desc "Saira font"
   homepage "https://fonts.google.com/specimen/Saira"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/saira"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/saira/./**/Saira-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/saira/./**/Saira\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/saira/./**/Saira-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/saira/./**/Saira[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
