5a4b1b2ee2fe7a897b4cf0c24dec267ac87add6c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:32 +0000

    font-ubuntu-mono updated

--- Formula/font-ubuntu-mono.rb
+++ Formula/font-ubuntu-mono.rb
@@ -1,15 +1,19 @@
 class FontUbuntuMono < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ufl/ubuntumono"
-  desc "Ubuntu Mono"
+  desc "Ubuntu mono font"
   homepage "https://fonts.google.com/specimen/Ubuntu+Mono"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ufl/ubuntumono"
+
   def install
     (share/"fonts").install Dir.glob("ufl/ubuntumono/./**/UbuntuMono-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ufl/ubuntumono/./**/UbuntuMono-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ufl/ubuntumono/./**/UbuntuMono-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ufl/ubuntumono/./**/UbuntuMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
