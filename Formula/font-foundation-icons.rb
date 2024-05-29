feb7620ea718d5b18c98cead8d5f0dbecfd8281f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-foundation-icons updated

--- Formula/font-foundation-icons.rb
+++ Formula/font-foundation-icons.rb
@@ -1,12 +1,13 @@
 class FontFoundationIcons < Formula
-  head "https://zurb.com/playground/uploads/upload/upload/288/foundation-icons.zip"
-  desc "Foundation Icons"
+  desc "Foundation icons font"
   homepage "https://zurb.com/playground/foundation-icon-fonts-3"
+  head "https://zurb.com/playground/uploads/upload/upload/288/foundation-icons.zip"
+
   def install
-    (share/"fonts").install Dir.glob("foundation-icons/**/foundation-icons.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/foundation-icons/foundation-icons.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
