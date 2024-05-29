e07f20c45bc0226acb4cec4cbe83bdfbe74a120c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-scada updated

--- Formula/font-scada.rb
+++ Formula/font-scada.rb
@@ -1,15 +1,19 @@
 class FontScada < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/scada"
-  desc "Scada"
+  desc "Scada font"
   homepage "https://fonts.google.com/specimen/Scada"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/scada"
+
   def install
     (share/"fonts").install Dir.glob("ofl/scada/./**/Scada-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/scada/./**/Scada-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/scada/./**/Scada-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/scada/./**/Scada-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
