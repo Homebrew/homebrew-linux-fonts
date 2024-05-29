b47fa6ed85d15a43eb1afb272ea6c0629c5fb495
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-oregano updated

--- Formula/font-oregano.rb
+++ Formula/font-oregano.rb
@@ -1,13 +1,17 @@
 class FontOregano < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/oregano"
-  desc "Oregano"
+  desc "Oregano font"
   homepage "https://fonts.google.com/specimen/Oregano"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/oregano"
+
   def install
     (share/"fonts").install Dir.glob("ofl/oregano/./**/Oregano-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/oregano/./**/Oregano-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
