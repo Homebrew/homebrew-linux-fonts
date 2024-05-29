b5181c9ba303637f68ae071dfebac31408ad9cb5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-source-serif-4 updated

--- Formula/font-source-serif-4.rb
+++ Formula/font-source-serif-4.rb
@@ -1,13 +1,17 @@
 class FontSourceSerif4 < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sourceserif4"
-  desc "Source Serif 4"
+  desc "Source serif 4 font"
   homepage "https://fonts.google.com/specimen/Source+Serif+4"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sourceserif4"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/sourceserif4/./**/SourceSerif4-Italic\\[opsz,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/sourceserif4/./**/SourceSerif4\\[opsz,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sourceserif4/./**/SourceSerif4-Italic[opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sourceserif4/./**/SourceSerif4[opsz,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
