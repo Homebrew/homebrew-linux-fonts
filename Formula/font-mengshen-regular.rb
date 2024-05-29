8951d7c1388739259f1fbc744d8d253a2d0be754
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-mengshen-regular updated

--- Formula/font-mengshen-regular.rb
+++ Formula/font-mengshen-regular.rb
@@ -1,17 +1,16 @@
 class FontMengshenRegular < Formula
-  version "1.03"
-  sha256 "421350caee9dfc6b9a90c61a02dcb2743ee0b2f09d229cb3e15177aa0d092e9f"
-  url "https://github.com/MaruTama/Mengshen-pinyin-font/releases/download/#{version}/Mengshen.zip"
-  desc "Mengshen Pinyin"
-  desc "萌神拼音"
   desc "OpenSource Pinyin font that supports homograph"
   homepage "https://github.com/MaruTama/Mengshen-pinyin-font"
+  url "https://github.com/MaruTama/Mengshen-pinyin-font/releases/download/1.03/Mengshen.zip"
+  version "1.03"
+  sha256 "421350caee9dfc6b9a90c61a02dcb2743ee0b2f09d229cb3e15177aa0d092e9f"
+
   def install
     (share/"fonts").install Dir.glob("./**/Mengshen-HanSerif.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Mengshen-Handwritten.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
