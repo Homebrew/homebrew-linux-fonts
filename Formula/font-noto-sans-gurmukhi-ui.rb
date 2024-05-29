621fcda1f89e0159361ace785a0b0f36d4eb8df7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:50 +0000

    font-noto-sans-gurmukhi-ui updated

--- Formula/font-noto-sans-gurmukhi-ui.rb
+++ Formula/font-noto-sans-gurmukhi-ui.rb
@@ -1,12 +1,14 @@
 class FontNotoSansGurmukhiUi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansgurmukhiui/NotoSansGurmukhiUI%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Gurmukhi UI"
+  desc "Noto sans gurmukhi ui font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Gurmukhi+UI"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansgurmukhiui/NotoSansGurmukhiUI%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansGurmukhiUI\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansGurmukhiUI[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
