2cbde15329d3cd623068deb245ab694a8be99aac
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:06 +0000

    font-fira-code-nerd-font updated

--- Formula/font-fira-code-nerd-font.rb
+++ Formula/font-fira-code-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontFiraCodeNerdFont < Formula
-  version "3.2.1"
-  sha256 "4ee8fbafecfc90460399b9828270b8ece30ccbf60b3ab875d64ff77696c6e262"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraCode.zip"
-  desc "FiraCode Nerd Font (Fira Code)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip"
+  version "3.2.1"
+  sha256 "4ee8fbafecfc90460399b9828270b8ece30ccbf60b3ab875d64ff77696c6e262"
+
   def install
-    (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Light.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Regular.ttf")[0]
@@ -23,10 +23,10 @@ class FontFiraCodeNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-Retina.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-SemiBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
