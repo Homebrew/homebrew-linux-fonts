7fbe7317cbe501d2bb2b2f3b940edbfd650f3a8f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-arsenal updated

--- Formula/font-arsenal.rb
+++ Formula/font-arsenal.rb
@@ -1,15 +1,19 @@
 class FontArsenal < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/arsenal"
-  desc "Arsenal"
+  desc "Arsenal font"
   homepage "https://fonts.google.com/specimen/Arsenal"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/arsenal"
+
   def install
     (share/"fonts").install Dir.glob("ofl/arsenal/./**/Arsenal-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/arsenal/./**/Arsenal-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/arsenal/./**/Arsenal-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/arsenal/./**/Arsenal-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
