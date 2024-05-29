eadd5644dad7ac32c690b172d9cf20378eca2847
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:54 +0000

    font-noto-sans-ol-chiki updated

--- Formula/font-noto-sans-ol-chiki.rb
+++ Formula/font-noto-sans-ol-chiki.rb
@@ -1,12 +1,14 @@
 class FontNotoSansOlChiki < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOlChiki-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Ol Chiki"
+  desc "Noto sans ol chiki font"
   homepage "https://www.google.com/get/noto/#sans-olck"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOlChiki-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansOlChiki-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
