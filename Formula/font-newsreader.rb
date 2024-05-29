6b195ed1d00c9d8ed4efe92dabcd09af5d00302b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-newsreader updated

--- Formula/font-newsreader.rb
+++ Formula/font-newsreader.rb
@@ -1,14 +1,17 @@
 class FontNewsreader < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/newsreader"
-  desc "Newsreader"
   desc "Original typeface primarily intended for continuous on-screen reading"
   homepage "https://fonts.google.com/specimen/Newsreader"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/newsreader"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/newsreader/./**/Newsreader-Italic\\[opsz,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/newsreader/./**/Newsreader\\[opsz,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/newsreader/./**/Newsreader-Italic[opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/newsreader/./**/Newsreader[opsz,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
