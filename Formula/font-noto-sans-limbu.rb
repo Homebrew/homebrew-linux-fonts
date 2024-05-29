30283080b7adf7092df9600efeda162cd1ce5d93
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-limbu updated

--- Formula/font-noto-sans-limbu.rb
+++ Formula/font-noto-sans-limbu.rb
@@ -1,12 +1,14 @@
 class FontNotoSansLimbu < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLimbu-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Limbu"
+  desc "Noto sans limbu font"
   homepage "https://www.google.com/get/noto/#sans-limb"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLimbu-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansLimbu-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
