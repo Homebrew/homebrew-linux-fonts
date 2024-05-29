c01828e3a0dfd87975116fcbb8568d729a597a65
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-gothic-a1 updated

--- Formula/font-gothic-a1.rb
+++ Formula/font-gothic-a1.rb
@@ -1,9 +1,13 @@
 class FontGothicA1 < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gothica1"
-  desc "Gothic A1"
+  desc "Gothic a1 font"
   homepage "https://fonts.google.com/specimen/Gothic+A1"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gothica1"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-ExtraLight.ttf")[0]
@@ -11,10 +15,10 @@ class FontGothicA1 < Formula
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
