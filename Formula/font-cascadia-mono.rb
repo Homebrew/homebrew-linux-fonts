eeb807845aca52594514e798b1be4d8bb2ea07c8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:52 +0000

    font-cascadia-mono updated

--- Formula/font-cascadia-mono.rb
+++ Formula/font-cascadia-mono.rb
@@ -1,16 +1,16 @@
 class FontCascadiaMono < Formula
-  version "2404.23"
-  sha256 "a911410626c0e09d03fa3fdda827188fda96607df50fecc3c5fee5906e33251b"
-  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
-  desc "Cascadia Mono"
   desc "Version of Cascadia Code without ligatures"
   homepage "https://github.com/microsoft/cascadia-code"
+  url "https://github.com/microsoft/cascadia-code/releases/download/v2404.23/CascadiaCode-2404.23.zip"
+  version "2404.23"
+  sha256 "a911410626c0e09d03fa3fdda827188fda96607df50fecc3c5fee5906e33251b"
+
   def install
-    (share/"fonts").install Dir.glob("ttf/**/CascadiaMono.ttf")[0]
-    (share/"fonts").install Dir.glob("ttf/**/CascadiaMonoItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ttf/CascadiaMono.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ttf/CascadiaMonoItalic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
