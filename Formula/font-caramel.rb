c4bda552045fd4252090a32c97175a1ff610aee8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-caramel updated

--- Formula/font-caramel.rb
+++ Formula/font-caramel.rb
@@ -1,13 +1,14 @@
 class FontCaramel < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/caramel/Caramel-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Caramel"
   desc "Fun, hand lettered script with three variations"
   homepage "https://fonts.google.com/specimen/Caramel"
+  head "https://github.com/google/fonts/raw/main/ofl/caramel/Caramel-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Caramel-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
