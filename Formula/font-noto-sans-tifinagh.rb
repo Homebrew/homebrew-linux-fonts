c0460ad6a6d898de424a29dea77b87a04e1d86d4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-tifinagh updated

--- Formula/font-noto-sans-tifinagh.rb
+++ Formula/font-noto-sans-tifinagh.rb
@@ -1,12 +1,14 @@
 class FontNotoSansTifinagh < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTifinagh-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Tifinagh"
+  desc "Noto sans tifinagh font"
   homepage "https://www.google.com/get/noto/#sans-tfng"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTifinagh-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansTifinagh-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
