b9db7737d29b73eb51b47e7b547de1d4771e8405
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:32 +0000

    font-tulpen-one updated

--- Formula/font-tulpen-one.rb
+++ Formula/font-tulpen-one.rb
@@ -1,12 +1,14 @@
 class FontTulpenOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tulpenone/TulpenOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Tulpen One"
+  desc "Tulpen one font"
   homepage "https://fonts.google.com/specimen/Tulpen+One"
+  head "https://github.com/google/fonts/raw/main/ofl/tulpenone/TulpenOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/TulpenOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
