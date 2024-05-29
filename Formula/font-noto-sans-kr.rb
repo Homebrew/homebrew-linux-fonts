51584500076d5236829eeddd79610aaf3bd4a62a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:51 +0000

    font-noto-sans-kr updated

--- Formula/font-noto-sans-kr.rb
+++ Formula/font-noto-sans-kr.rb
@@ -1,13 +1,14 @@
 class FontNotoSansKr < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanskr/NotoSansKR%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans KR"
   desc "Unmodulated (“sans serif”) design for the korean language"
   homepage "https://fonts.google.com/specimen/Noto+Sans+KR"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanskr/NotoSansKR%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansKR\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansKR[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
