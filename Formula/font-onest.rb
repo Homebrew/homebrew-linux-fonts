5b0873b6b3c39b097b34a6fe6298814ff642dca0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-onest updated

--- Formula/font-onest.rb
+++ Formula/font-onest.rb
@@ -1,13 +1,14 @@
 class FontOnest < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/onest/Onest%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Onest"
   desc "Modern typeface, so it has a lot of useful conveniences"
   homepage "https://fonts.google.com/specimen/Onest"
+  head "https://github.com/google/fonts/raw/main/ofl/onest/Onest%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Onest\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Onest[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
