54926936bf012b866c6192bf87bf76394a8523ad
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-great-vibes updated

--- Formula/font-great-vibes.rb
+++ Formula/font-great-vibes.rb
@@ -1,12 +1,14 @@
 class FontGreatVibes < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/greatvibes/GreatVibes-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Great Vibes"
+  desc "Great vibes font"
   homepage "https://fonts.google.com/specimen/Great+Vibes"
+  head "https://github.com/google/fonts/raw/main/ofl/greatvibes/GreatVibes-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GreatVibes-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
