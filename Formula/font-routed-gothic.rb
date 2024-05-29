c88b514a3e4ac37b26d427524b245c87521edd93
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-routed-gothic updated

--- Formula/font-routed-gothic.rb
+++ Formula/font-routed-gothic.rb
@@ -1,22 +1,23 @@
 class FontRoutedGothic < Formula
+  desc "Routed gothic font"
+  homepage "https://webonastick.com/fonts/routed-gothic/"
+  url "https://webonastick.com/fonts/routed-gothic/download/routed-gothic-ttf-v1.0.0.zip"
   version "1.0.0"
   sha256 "3ecffba109bf2619057815411253e5c6f47f82fedba38ade6c951e13a8b2ad5e"
-  url "https://webonastick.com/fonts/routed-gothic/download/routed-gothic-ttf-v#{version}.zip"
-  desc "Routed Gothic"
-  homepage "https://webonastick.com/fonts/routed-gothic/"
+
   def install
-    (share/"fonts").install Dir.glob("routed-gothic-ttf-v#{version}/**/routed-gothic-half-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("routed-gothic-ttf-v#{version}/**/routed-gothic-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("routed-gothic-ttf-v#{version}/**/routed-gothic-narrow-half-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("routed-gothic-ttf-v#{version}/**/routed-gothic-narrow-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("routed-gothic-ttf-v#{version}/**/routed-gothic-narrow.ttf")[0]
-    (share/"fonts").install Dir.glob("routed-gothic-ttf-v#{version}/**/routed-gothic-wide-half-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("routed-gothic-ttf-v#{version}/**/routed-gothic-wide-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("routed-gothic-ttf-v#{version}/**/routed-gothic-wide.ttf")[0]
-    (share/"fonts").install Dir.glob("routed-gothic-ttf-v#{version}/**/routed-gothic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-narrow-half-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-narrow-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-narrow.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-wide-half-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-wide-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-wide.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-half-italic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
