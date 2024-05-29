cf3e13d93583a96f63560ee846ca7a8d3e7a8080
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:37 +0000

    font-marko-one updated

--- Formula/font-marko-one.rb
+++ Formula/font-marko-one.rb
@@ -1,12 +1,14 @@
 class FontMarkoOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/markoone/MarkoOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Marko One"
+  desc "Marko one font"
   homepage "https://fonts.google.com/specimen/Marko+One"
+  head "https://github.com/google/fonts/raw/main/ofl/markoone/MarkoOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MarkoOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
