1786941c6b83d17ebcd62cae8e55e98fb520dd98
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-baloo-bhaina-2 updated

--- Formula/font-baloo-bhaina-2.rb
+++ Formula/font-baloo-bhaina-2.rb
@@ -1,12 +1,14 @@
 class FontBalooBhaina2 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/baloobhaina2/BalooBhaina2%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Baloo Bhaina 2"
+  desc "Baloo bhaina 2 font"
   homepage "https://fonts.google.com/specimen/Baloo+Bhaina+2"
+  head "https://github.com/google/fonts/raw/main/ofl/baloobhaina2/BalooBhaina2%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BalooBhaina2\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooBhaina2[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
