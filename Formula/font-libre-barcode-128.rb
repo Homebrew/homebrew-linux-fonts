ee5ae734428310e5eb77ac4b23e468db4da41068
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:32 +0000

    font-libre-barcode-128 updated

--- Formula/font-libre-barcode-128.rb
+++ Formula/font-libre-barcode-128.rb
@@ -1,12 +1,14 @@
 class FontLibreBarcode128 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/librebarcode128/LibreBarcode128-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Libre Barcode 128"
+  desc "Libre barcode 128 font"
   homepage "https://fonts.google.com/specimen/Libre+Barcode+128"
+  head "https://github.com/google/fonts/raw/main/ofl/librebarcode128/LibreBarcode128-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LibreBarcode128-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
