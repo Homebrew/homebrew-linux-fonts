64cb6250ae21ec039795cf3d6a667f2df30b7d8b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-mea-culpa updated

--- Formula/font-mea-culpa.rb
+++ Formula/font-mea-culpa.rb
@@ -1,13 +1,14 @@
 class FontMeaCulpa < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/meaculpa/MeaCulpa-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mea Culpa"
   desc "Beautiful formal script with flourished capitals"
   homepage "https://fonts.google.com/specimen/Mea+Culpa"
+  head "https://github.com/google/fonts/raw/main/ofl/meaculpa/MeaCulpa-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MeaCulpa-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
