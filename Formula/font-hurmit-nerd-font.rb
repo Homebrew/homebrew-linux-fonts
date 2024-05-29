a335408537ce0c24b980d4e6aebc133ddaad8d5e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-hurmit-nerd-font updated

--- Formula/font-hurmit-nerd-font.rb
+++ Formula/font-hurmit-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontHurmitNerdFont < Formula
-  version "3.2.1"
-  sha256 "81a6b2db8f1897b2639653d3ce81388462dafc6312d5ca70006c0d50283d57a2"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hermit.zip"
-  desc "Hurmit Nerd Font (Hermit)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hermit.zip"
+  version "3.2.1"
+  sha256 "81a6b2db8f1897b2639653d3ce81388462dafc6312d5ca70006c0d50283d57a2"
+
   def install
-    (share/"fonts").install Dir.glob("./**/HurmitNerdFont-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HurmitNerdFontPropo-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/HurmitNerdFont-BoldItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/HurmitNerdFont-Italic.otf")[0]
     (share/"fonts").install Dir.glob("./**/HurmitNerdFont-Light.otf")[0]
@@ -23,10 +23,10 @@ class FontHurmitNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/HurmitNerdFontPropo-Italic.otf")[0]
     (share/"fonts").install Dir.glob("./**/HurmitNerdFontPropo-Light.otf")[0]
     (share/"fonts").install Dir.glob("./**/HurmitNerdFontPropo-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/HurmitNerdFontPropo-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/HurmitNerdFont-Bold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
