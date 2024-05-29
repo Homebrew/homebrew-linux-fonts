7a8b2d6b81c23728f0c76f159635c9a193fb1a59
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-puddles updated

--- Formula/font-rubik-puddles.rb
+++ Formula/font-rubik-puddles.rb
@@ -1,12 +1,14 @@
 class FontRubikPuddles < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikpuddles/RubikPuddles-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Puddles"
+  desc "Rubik puddles font"
   homepage "https://fonts.google.com/specimen/Rubik+Puddles"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikpuddles/RubikPuddles-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikPuddles-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
