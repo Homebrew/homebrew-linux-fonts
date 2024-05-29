9591eee4cdf7874d70b739e03a05bab7ef2c69ce
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:58 +0000

    font-noto-sans-vithkuqi updated

--- Formula/font-noto-sans-vithkuqi.rb
+++ Formula/font-noto-sans-vithkuqi.rb
@@ -1,13 +1,14 @@
 class FontNotoSansVithkuqi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansvithkuqi/NotoSansVithkuqi%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Vithkuqi"
   desc "Design for the historical european vithkuqi script"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Vithkuqi"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansvithkuqi/NotoSansVithkuqi%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansVithkuqi\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansVithkuqi[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
