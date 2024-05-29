1d3fcb0a4a4e60947ceb0901cbdc2a8f257cbebc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-coiny updated

--- Formula/font-coiny.rb
+++ Formula/font-coiny.rb
@@ -1,12 +1,14 @@
 class FontCoiny < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/coiny/Coiny-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Coiny"
+  desc "Coiny font"
   homepage "https://fonts.google.com/specimen/Coiny"
+  head "https://github.com/google/fonts/raw/main/ofl/coiny/Coiny-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Coiny-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
