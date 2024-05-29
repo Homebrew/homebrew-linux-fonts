368cf91fd070e90e0c2134830447dc7eb1f077e8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-josefin-slab updated

--- Formula/font-josefin-slab.rb
+++ Formula/font-josefin-slab.rb
@@ -1,13 +1,17 @@
 class FontJosefinSlab < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/josefinslab"
-  desc "Josefin Slab"
+  desc "Josefin slab font"
   homepage "https://fonts.google.com/specimen/Josefin+Slab"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/josefinslab"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/josefinslab/./**/JosefinSlab-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/josefinslab/./**/JosefinSlab\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/josefinslab/./**/JosefinSlab-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/josefinslab/./**/JosefinSlab[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
