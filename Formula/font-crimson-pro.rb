40e880df2c100cbe8c5703b4760ea0d9525a1b56
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-crimson-pro updated

--- Formula/font-crimson-pro.rb
+++ Formula/font-crimson-pro.rb
@@ -1,13 +1,17 @@
 class FontCrimsonPro < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/crimsonpro"
-  desc "Crimson Pro"
+  desc "Crimson pro font"
   homepage "https://fonts.google.com/specimen/Crimson+Pro"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/crimsonpro"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/crimsonpro/./**/CrimsonPro-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/crimsonpro/./**/CrimsonPro\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/crimsonpro/./**/CrimsonPro-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/crimsonpro/./**/CrimsonPro[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
