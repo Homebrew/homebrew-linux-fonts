3c1daf1728448837965bae20cd1ae52744a3dd2b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-tillana updated

--- Formula/font-tillana.rb
+++ Formula/font-tillana.rb
@@ -1,7 +1,11 @@
 class FontTillana < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tillana"
-  desc "Tillana"
+  desc "Tillana font"
   homepage "https://fonts.google.com/specimen/Tillana"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tillana"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tillana/./**/Tillana-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tillana/./**/Tillana-ExtraBold.ttf")[0]
@@ -9,8 +13,8 @@ class FontTillana < Formula
     (share/"fonts").install Dir.glob("ofl/tillana/./**/Tillana-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tillana/./**/Tillana-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
