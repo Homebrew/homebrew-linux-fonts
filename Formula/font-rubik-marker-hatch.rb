2a558fcc2e3d289721a939a29215e5fc5e2afe09
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-marker-hatch updated

--- Formula/font-rubik-marker-hatch.rb
+++ Formula/font-rubik-marker-hatch.rb
@@ -1,12 +1,14 @@
 class FontRubikMarkerHatch < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikmarkerhatch/RubikMarkerHatch-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Marker Hatch"
+  desc "Rubik marker hatch font"
   homepage "https://fonts.google.com/specimen/Rubik+Marker+Hatch"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikmarkerhatch/RubikMarkerHatch-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikMarkerHatch-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
