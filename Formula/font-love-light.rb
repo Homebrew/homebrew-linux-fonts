0146d0566b359058a2d4a6f7d34816fc1fea5509
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-love-light updated

--- Formula/font-love-light.rb
+++ Formula/font-love-light.rb
@@ -1,13 +1,14 @@
 class FontLoveLight < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lovelight/LoveLight-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Love Light"
   desc "Adaptation of another font"
   homepage "https://fonts.google.com/specimen/Love+Light"
+  head "https://github.com/google/fonts/raw/main/ofl/lovelight/LoveLight-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LoveLight-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
