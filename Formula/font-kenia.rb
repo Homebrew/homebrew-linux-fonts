6a7266a09d279b5c5847e8446c9c9332bbbb91a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-kenia updated

--- Formula/font-kenia.rb
+++ Formula/font-kenia.rb
@@ -1,12 +1,14 @@
 class FontKenia < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kenia/Kenia-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kenia"
+  desc "Kenia font"
   homepage "https://fonts.google.com/specimen/Kenia"
+  head "https://github.com/google/fonts/raw/main/ofl/kenia/Kenia-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Kenia-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
