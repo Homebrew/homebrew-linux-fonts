7343e54f76c03fe7b21ab4d4edf71b36bf46d89e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-bakbak-one updated

--- Formula/font-bakbak-one.rb
+++ Formula/font-bakbak-one.rb
@@ -1,13 +1,14 @@
 class FontBakbakOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bakbakone/BakbakOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bakbak One"
   desc "Render anything from a warning to a mockery"
   homepage "https://fonts.google.com/specimen/Bakbak+One"
+  head "https://github.com/google/fonts/raw/main/ofl/bakbakone/BakbakOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BakbakOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
