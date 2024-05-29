3d4811b5060ac71aba434950542efb34dd121092
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:11 +0000

    font-puppies-play updated

--- Formula/font-puppies-play.rb
+++ Formula/font-puppies-play.rb
@@ -1,13 +1,14 @@
 class FontPuppiesPlay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/puppiesplay/PuppiesPlay-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Puppies Play"
   desc "Fun, bouncy script with connectors that give a playful flow"
   homepage "https://fonts.google.com/specimen/Puppies+Play"
+  head "https://github.com/google/fonts/raw/main/ofl/puppiesplay/PuppiesPlay-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PuppiesPlay-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
