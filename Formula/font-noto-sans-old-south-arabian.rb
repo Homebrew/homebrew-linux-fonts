7fe37c4bcd1186908fccb390d3e1540d1450be38
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:54 +0000

    font-noto-sans-old-south-arabian updated

--- Formula/font-noto-sans-old-south-arabian.rb
+++ Formula/font-noto-sans-old-south-arabian.rb
@@ -1,12 +1,14 @@
 class FontNotoSansOldSouthArabian < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldSouthArabian-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Old South Arabian"
+  desc "Noto sans old south arabian font"
   homepage "https://www.google.com/get/noto/#sans-sarb"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldSouthArabian-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansOldSouthArabian-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
