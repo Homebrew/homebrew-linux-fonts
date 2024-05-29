9dea58ebec2dcb1a04e5fb3994768ca2e18a5680
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-wellfleet updated

--- Formula/font-wellfleet.rb
+++ Formula/font-wellfleet.rb
@@ -1,12 +1,14 @@
 class FontWellfleet < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/wellfleet/Wellfleet-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Wellfleet"
+  desc "Wellfleet font"
   homepage "https://fonts.google.com/specimen/Wellfleet"
+  head "https://github.com/google/fonts/raw/main/ofl/wellfleet/Wellfleet-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Wellfleet-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
