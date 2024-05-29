f424c9d8bae7c1a0616fb248ddd11d9e00ab155e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:54 +0000

    font-noto-sans-old-sogdian updated

--- Formula/font-noto-sans-old-sogdian.rb
+++ Formula/font-noto-sans-old-sogdian.rb
@@ -1,12 +1,14 @@
 class FontNotoSansOldSogdian < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansoldsogdian/NotoSansOldSogdian-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Old Sogdian"
+  desc "Noto sans old sogdian font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Old+Sogdian"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansoldsogdian/NotoSansOldSogdian-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansOldSogdian-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
