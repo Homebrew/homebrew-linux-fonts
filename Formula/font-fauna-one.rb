f8f6340195b87bdbf4aa14824920e1b7b37d7da6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:06 +0000

    font-fauna-one updated

--- Formula/font-fauna-one.rb
+++ Formula/font-fauna-one.rb
@@ -1,12 +1,14 @@
 class FontFaunaOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/faunaone/FaunaOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Fauna One"
+  desc "Fauna one font"
   homepage "https://fonts.google.com/specimen/Fauna+One"
+  head "https://github.com/google/fonts/raw/main/ofl/faunaone/FaunaOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/FaunaOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
