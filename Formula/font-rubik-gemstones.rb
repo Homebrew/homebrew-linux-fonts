6be29e3dae70afaca4768e8e984e80f04aed7b6b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-gemstones updated

--- Formula/font-rubik-gemstones.rb
+++ Formula/font-rubik-gemstones.rb
@@ -1,12 +1,14 @@
 class FontRubikGemstones < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikgemstones/RubikGemstones-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Gemstones"
+  desc "Rubik gemstones font"
   homepage "https://fonts.google.com/specimen/Rubik+Gemstones"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikgemstones/RubikGemstones-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikGemstones-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
