0014a9ec367c7fd0d578a7d80569e0fe414dd6ed
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:15 +0000

    font-roboto-slab updated

--- Formula/font-roboto-slab.rb
+++ Formula/font-roboto-slab.rb
@@ -1,12 +1,14 @@
 class FontRobotoSlab < Formula
-  head "https://github.com/google/fonts/raw/main/apache/robotoslab/RobotoSlab%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Roboto Slab"
+  desc "Roboto slab font"
   homepage "https://fonts.google.com/specimen/Roboto+Slab"
+  head "https://github.com/google/fonts/raw/main/apache/robotoslab/RobotoSlab%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/RobotoSlab\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/RobotoSlab[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
