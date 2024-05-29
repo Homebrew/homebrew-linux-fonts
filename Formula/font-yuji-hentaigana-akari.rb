9efad02feb434f9258e3ff2ecadbbd1e27d9d86b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-yuji-hentaigana-akari updated

--- Formula/font-yuji-hentaigana-akari.rb
+++ Formula/font-yuji-hentaigana-akari.rb
@@ -1,12 +1,14 @@
 class FontYujiHentaiganaAkari < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yujihentaiganaakari/YujiHentaiganaAkari-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yuji Hentaigana Akari"
+  desc "Yuji hentaigana akari font"
   homepage "https://fonts.google.com/specimen/Yuji+Hentaigana+Akari"
+  head "https://github.com/google/fonts/raw/main/ofl/yujihentaiganaakari/YujiHentaiganaAkari-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/YujiHentaiganaAkari-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
