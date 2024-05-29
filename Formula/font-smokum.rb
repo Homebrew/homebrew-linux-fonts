d1fd83685901633f09432614d930a26d8d2b0929
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-smokum updated

--- Formula/font-smokum.rb
+++ Formula/font-smokum.rb
@@ -1,12 +1,14 @@
 class FontSmokum < Formula
-  head "https://github.com/google/fonts/raw/main/apache/smokum/Smokum-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Smokum"
+  desc "Smokum font"
   homepage "https://fonts.google.com/specimen/Smokum"
+  head "https://github.com/google/fonts/raw/main/apache/smokum/Smokum-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Smokum-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
