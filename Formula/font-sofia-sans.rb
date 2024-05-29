67329924d52ceab84dab3dea5d4968cc3b916d47
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-sofia-sans updated

--- Formula/font-sofia-sans.rb
+++ Formula/font-sofia-sans.rb
@@ -1,13 +1,17 @@
 class FontSofiaSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sofiasans"
-  desc "Sofia Sans"
+  desc "Sofia sans font"
   homepage "https://fonts.google.com/specimen/Sofia+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sofiasans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/sofiasans/./**/SofiaSans-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/sofiasans/./**/SofiaSans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sofiasans/./**/SofiaSans-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sofiasans/./**/SofiaSans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
