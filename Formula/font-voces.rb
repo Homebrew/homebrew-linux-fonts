21f65a212906595beb5e879e4a7d585788111e15
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-voces updated

--- Formula/font-voces.rb
+++ Formula/font-voces.rb
@@ -1,12 +1,14 @@
 class FontVoces < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/voces/Voces-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Voces"
+  desc "Voces font"
   homepage "https://fonts.google.com/specimen/Voces"
+  head "https://github.com/google/fonts/raw/main/ofl/voces/Voces-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Voces-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
