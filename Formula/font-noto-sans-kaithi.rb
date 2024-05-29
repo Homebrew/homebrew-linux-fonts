16323f9393215ab4b8fe2a62a1c07805fb56f1b2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:51 +0000

    font-noto-sans-kaithi updated

--- Formula/font-noto-sans-kaithi.rb
+++ Formula/font-noto-sans-kaithi.rb
@@ -1,12 +1,14 @@
 class FontNotoSansKaithi < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKaithi-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Kaithi"
+  desc "Noto sans kaithi font"
   homepage "https://www.google.com/get/noto/#sans-kthi"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKaithi-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansKaithi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
