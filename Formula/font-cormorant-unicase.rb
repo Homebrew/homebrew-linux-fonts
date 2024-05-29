1db9024688a2ec47e1142e854790c75db64d083b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-cormorant-unicase updated

--- Formula/font-cormorant-unicase.rb
+++ Formula/font-cormorant-unicase.rb
@@ -1,7 +1,11 @@
 class FontCormorantUnicase < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cormorantunicase"
-  desc "Cormorant Unicase"
+  desc "Cormorant unicase font"
   homepage "https://fonts.google.com/specimen/Cormorant+Unicase"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cormorantunicase"
+
   def install
     (share/"fonts").install Dir.glob("ofl/cormorantunicase/./**/CormorantUnicase-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantunicase/./**/CormorantUnicase-Light.ttf")[0]
@@ -9,8 +13,8 @@ class FontCormorantUnicase < Formula
     (share/"fonts").install Dir.glob("ofl/cormorantunicase/./**/CormorantUnicase-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantunicase/./**/CormorantUnicase-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
