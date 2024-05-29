a78658e4680f2c1f4dc4e20fa3ca5bb777afbb8f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-comfortaa updated

--- Formula/font-comfortaa.rb
+++ Formula/font-comfortaa.rb
@@ -1,12 +1,14 @@
 class FontComfortaa < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/comfortaa/Comfortaa%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Comfortaa"
+  desc "Comfortaa font"
   homepage "https://fonts.google.com/specimen/Comfortaa"
+  head "https://github.com/google/fonts/raw/main/ofl/comfortaa/Comfortaa%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Comfortaa\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Comfortaa[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
