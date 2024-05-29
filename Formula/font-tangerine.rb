d5045061561f6590685bc4faee7e30eb10322ded
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-tangerine updated

--- Formula/font-tangerine.rb
+++ Formula/font-tangerine.rb
@@ -1,13 +1,17 @@
 class FontTangerine < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tangerine"
-  desc "Tangerine"
+  desc "Tangerine font"
   homepage "https://fonts.google.com/specimen/Tangerine"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tangerine"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tangerine/./**/Tangerine-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tangerine/./**/Tangerine-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
