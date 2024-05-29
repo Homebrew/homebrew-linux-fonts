7659e619515a652fe7870e06e7853deae149c6aa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-readex-pro updated

--- Formula/font-readex-pro.rb
+++ Formula/font-readex-pro.rb
@@ -1,13 +1,14 @@
 class FontReadexPro < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/readexpro/ReadexPro%5BHEXP%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Readex Pro"
   desc "Family of variable fonts"
   homepage "https://fonts.google.com/specimen/Readex+Pro"
+  head "https://github.com/google/fonts/raw/main/ofl/readexpro/ReadexPro%5BHEXP%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/ReadexPro\\[HEXP,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ReadexPro[HEXP,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
