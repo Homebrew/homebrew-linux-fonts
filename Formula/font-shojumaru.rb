1f4b9c2959d1ab4dc63b2892528e5d612e939ab7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-shojumaru updated

--- Formula/font-shojumaru.rb
+++ Formula/font-shojumaru.rb
@@ -1,12 +1,14 @@
 class FontShojumaru < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/shojumaru/Shojumaru-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Shojumaru"
+  desc "Shojumaru font"
   homepage "https://fonts.google.com/specimen/Shojumaru"
+  head "https://github.com/google/fonts/raw/main/ofl/shojumaru/Shojumaru-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Shojumaru-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
