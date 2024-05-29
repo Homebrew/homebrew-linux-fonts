68826e5816d83a0dc2a67ba35168c6291bd5dfbb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-signika-sc updated

--- Formula/font-signika-sc.rb
+++ Formula/font-signika-sc.rb
@@ -1,13 +1,14 @@
 class FontSignikaSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/signikasc/SignikaSC%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Signika SC"
   desc "Small-caps version of the Signika font"
   homepage "https://fonts.google.com/specimen/Signika+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/signikasc/SignikaSC%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/SignikaSC\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/SignikaSC[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
