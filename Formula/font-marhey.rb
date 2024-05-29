7ed4871bd43525dbae6c855f8f59149d45697075
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:37 +0000

    font-marhey updated

--- Formula/font-marhey.rb
+++ Formula/font-marhey.rb
@@ -1,13 +1,14 @@
 class FontMarhey < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/marhey/Marhey%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Marhey"
   desc "Playful display typeface, custom hand lettering with contrast strokes"
   homepage "https://fonts.google.com/specimen/Marhey"
+  head "https://github.com/google/fonts/raw/main/ofl/marhey/Marhey%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Marhey\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Marhey[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
