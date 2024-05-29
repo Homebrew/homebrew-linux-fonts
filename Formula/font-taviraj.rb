6ecd9bcf52209267f9b0341c6350cd12548abf9c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-taviraj updated

--- Formula/font-taviraj.rb
+++ Formula/font-taviraj.rb
@@ -1,9 +1,13 @@
 class FontTaviraj < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/taviraj"
-  desc "Taviraj"
+  desc "Taviraj font"
   homepage "https://fonts.google.com/specimen/Taviraj"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/taviraj"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-ThinItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-BoldItalic.ttf")[0]
@@ -20,10 +24,10 @@ class FontTaviraj < Formula
     (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/taviraj/./**/Taviraj-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
