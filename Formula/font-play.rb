17a92eedb6f67fd3d39635fe47049179d7010dbf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-play updated

--- Formula/font-play.rb
+++ Formula/font-play.rb
@@ -1,13 +1,17 @@
 class FontPlay < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/play"
-  desc "Play"
+  desc "Play font"
   homepage "https://fonts.google.com/specimen/Play"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/play"
+
   def install
     (share/"fonts").install Dir.glob("ofl/play/./**/Play-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/play/./**/Play-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
