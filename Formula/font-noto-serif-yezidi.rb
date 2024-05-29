f74bf8ccaacb12826496644a287d3ebeb1e68d28
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-noto-serif-yezidi updated

--- Formula/font-noto-serif-yezidi.rb
+++ Formula/font-noto-serif-yezidi.rb
@@ -1,12 +1,14 @@
 class FontNotoSerifYezidi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifyezidi/NotoSerifYezidi%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Yezidi"
+  desc "Noto serif yezidi font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Yezidi"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifyezidi/NotoSerifYezidi%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifYezidi\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifYezidi[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
