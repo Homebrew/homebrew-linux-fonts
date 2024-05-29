09f629662e2c2dcddcfcdc679bf41a1b4c72634c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-la-belle-aurore updated

--- Formula/font-la-belle-aurore.rb
+++ Formula/font-la-belle-aurore.rb
@@ -1,12 +1,14 @@
 class FontLaBelleAurore < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/labelleaurore/LaBelleAurore.ttf", verified: "github.com/google/fonts/"
-  desc "La Belle Aurore"
+  desc "La belle aurore font"
   homepage "https://fonts.google.com/specimen/La+Belle+Aurore"
+  head "https://github.com/google/fonts/raw/main/ofl/labelleaurore/LaBelleAurore.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LaBelleAurore.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
