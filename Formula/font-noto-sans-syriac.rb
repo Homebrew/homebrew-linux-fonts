8a32789d40cef0d731d22d0bec1f85546aefddb4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:56 +0000

    font-noto-sans-syriac updated

--- Formula/font-noto-sans-syriac.rb
+++ Formula/font-noto-sans-syriac.rb
@@ -1,12 +1,14 @@
 class FontNotoSansSyriac < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanssyriac/NotoSansSyriac%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Syriac"
+  desc "Noto sans syriac font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Syriac"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanssyriac/NotoSansSyriac%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansSyriac\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansSyriac[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
