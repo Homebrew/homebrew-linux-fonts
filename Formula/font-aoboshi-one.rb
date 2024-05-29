23fc3ac2f2941fa1fa23302efff3bd4abc5cb529
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-aoboshi-one updated

--- Formula/font-aoboshi-one.rb
+++ Formula/font-aoboshi-one.rb
@@ -1,12 +1,14 @@
 class FontAoboshiOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/aoboshione/AoboshiOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Aoboshi One"
+  desc "Aoboshi one font"
   homepage "https://fonts.google.com/specimen/Aoboshi+One"
+  head "https://github.com/google/fonts/raw/main/ofl/aoboshione/AoboshiOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AoboshiOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
