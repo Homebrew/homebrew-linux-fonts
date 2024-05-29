39d91a7123559cf6c72eafb5dd71453b75bfb042
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-slabo-13px updated

--- Formula/font-slabo-13px.rb
+++ Formula/font-slabo-13px.rb
@@ -1,12 +1,14 @@
 class FontSlabo13px < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/slabo13px/Slabo13px-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Slabo 13px"
+  desc "Slabo 13px font"
   homepage "https://fonts.google.com/specimen/Slabo+13px"
+  head "https://github.com/google/fonts/raw/main/ofl/slabo13px/Slabo13px-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Slabo13px-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
