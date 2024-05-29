c060565190b6ea50126def02c041122e534ab325
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-anek-devanagari updated

--- Formula/font-anek-devanagari.rb
+++ Formula/font-anek-devanagari.rb
@@ -1,12 +1,14 @@
 class FontAnekDevanagari < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anekdevanagari/AnekDevanagari%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Anek Devanagari"
+  desc "Anek devanagari font"
   homepage "https://fonts.google.com/specimen/Anek+Devanagari"
+  head "https://github.com/google/fonts/raw/main/ofl/anekdevanagari/AnekDevanagari%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/AnekDevanagari\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekDevanagari[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
