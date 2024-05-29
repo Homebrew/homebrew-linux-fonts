e91d9a1111abc5f91d4f848de79f99d9028f4ca4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-genjyuugothic-x updated

--- Formula/font-genjyuugothic-x.rb
+++ Formula/font-genjyuugothic-x.rb
@@ -1,11 +1,13 @@
 class FontGenjyuugothicX < Formula
+  desc "Gen jyuu gothicx font"
+  homepage "http://jikasei.me/font/genjyuu/"
+  url "https://osdn.dl.osdn.jp/users/8/8644/genjyuugothic-x-20150607.zip",
+       verified: "osdn.jp/"
   version "20150607,8.644"
   sha256 "e4a0ea11b8155056ad2b678c8501b2e76dd99b8c8eb5363d396fe7c3079201b3"
-  url "https://osdn.dl.osdn.jp/users/#{version.to_s.sub(/.*,/, "").sub(/\..*/, "")}/#{version.to_s.sub(/.*,/, "").gsub(".", "")}/genjyuugothic-x-#{version.to_s.sub(/,.*/, "")}.zip", verified: "osdn.jp/"
-  desc "Gen Jyuu GothicX"
-  homepage "http://jikasei.me/font/genjyuu/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-Heavy.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-Light.ttf")[0]
@@ -25,10 +27,10 @@ class FontGenjyuugothicX < Formula
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-P-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-P-Normal.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-P-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenJyuuGothicX-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
