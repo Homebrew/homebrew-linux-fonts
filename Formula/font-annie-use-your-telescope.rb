3543f227d5c34540386ac5816eed1f76bdf1b2fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-annie-use-your-telescope updated

--- Formula/font-annie-use-your-telescope.rb
+++ Formula/font-annie-use-your-telescope.rb
@@ -1,12 +1,14 @@
 class FontAnnieUseYourTelescope < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/annieuseyourtelescope/AnnieUseYourTelescope-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Annie Use Your Telescope"
+  desc "Annie use your telescope font"
   homepage "https://fonts.google.com/specimen/Annie+Use+Your+Telescope"
+  head "https://github.com/google/fonts/raw/main/ofl/annieuseyourtelescope/AnnieUseYourTelescope-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AnnieUseYourTelescope-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
