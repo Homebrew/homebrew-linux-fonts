1dae3ad29bf9695e3752b9677c08f33226ba96cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-ropa-sans updated

--- Formula/font-ropa-sans.rb
+++ Formula/font-ropa-sans.rb
@@ -1,13 +1,17 @@
 class FontRopaSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ropasans"
-  desc "Ropa Sans"
+  desc "Ropa sans font"
   homepage "https://fonts.google.com/specimen/Ropa+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ropasans"
+
   def install
     (share/"fonts").install Dir.glob("ofl/ropasans/./**/RopaSans-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ropasans/./**/RopaSans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
