0976185c220c0e19e22b97d19b722f25cc564664
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-astloch updated

--- Formula/font-astloch.rb
+++ Formula/font-astloch.rb
@@ -1,13 +1,17 @@
 class FontAstloch < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/astloch"
-  desc "Astloch"
+  desc "Astloch font"
   homepage "https://fonts.google.com/specimen/Astloch"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/astloch"
+
   def install
     (share/"fonts").install Dir.glob("ofl/astloch/./**/Astloch-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/astloch/./**/Astloch-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
