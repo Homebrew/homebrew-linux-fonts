5919dede09270efc798671755ce51afcb83e34cf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-koho updated

--- Formula/font-koho.rb
+++ Formula/font-koho.rb
@@ -1,9 +1,13 @@
 class FontKoho < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/koho"
-  desc "KoHo"
+  desc "Koho font"
   homepage "https://fonts.google.com/specimen/KoHo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/koho"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-ExtraLightItalic.ttf")[0]
@@ -14,10 +18,10 @@ class FontKoho < Formula
     (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/koho/./**/KoHo-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
