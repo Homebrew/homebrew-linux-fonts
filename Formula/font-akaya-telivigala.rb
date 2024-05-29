d3ecd84b6361c484a91c109b5b4990f950304876
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-akaya-telivigala updated

--- Formula/font-akaya-telivigala.rb
+++ Formula/font-akaya-telivigala.rb
@@ -1,12 +1,14 @@
 class FontAkayaTelivigala < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/akayatelivigala/AkayaTelivigala-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Akaya Telivigala"
+  desc "Akaya telivigala font"
   homepage "https://fonts.google.com/specimen/Akaya+Telivigala"
+  head "https://github.com/google/fonts/raw/main/ofl/akayatelivigala/AkayaTelivigala-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AkayaTelivigala-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
