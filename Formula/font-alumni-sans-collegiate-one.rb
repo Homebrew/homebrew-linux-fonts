015d756f8affc9c55c416ee6a21fb3bdd821738a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-alumni-sans-collegiate-one updated

--- Formula/font-alumni-sans-collegiate-one.rb
+++ Formula/font-alumni-sans-collegiate-one.rb
@@ -1,14 +1,17 @@
 class FontAlumniSansCollegiateOne < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/alumnisanscollegiateone"
-  desc "Alumni Sans Collegiate One"
   desc "Font inspired by Impact Black"
   homepage "https://fonts.google.com/specimen/Alumni+Sans+Collegiate+One"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/alumnisanscollegiateone"
+
   def install
     (share/"fonts").install Dir.glob("ofl/alumnisanscollegiateone/./**/AlumniSansCollegiateOne-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/alumnisanscollegiateone/./**/AlumniSansCollegiateOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
