ff2b87b5cb0349c2e5cc705641aae9b986ff259d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:16 +0000

    font-hedvig-letters-sans updated

--- Formula/font-hedvig-letters-sans.rb
+++ Formula/font-hedvig-letters-sans.rb
@@ -1,13 +1,14 @@
 class FontHedvigLettersSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/hedvigletterssans/HedvigLettersSans-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Hedvig Letters Sans"
   desc "Perfect when it’s not"
   homepage "https://fonts.google.com/specimen/Hedvig+Letters+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/hedvigletterssans/HedvigLettersSans-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/HedvigLettersSans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
