a37e39ed14644188be8fce5f22d80fe0cd7a8c79
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:37 +0000

    font-mandali updated

--- Formula/font-mandali.rb
+++ Formula/font-mandali.rb
@@ -1,12 +1,14 @@
 class FontMandali < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mandali/Mandali-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mandali"
+  desc "Mandali font"
   homepage "https://fonts.google.com/specimen/Mandali"
+  head "https://github.com/google/fonts/raw/main/ofl/mandali/Mandali-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Mandali-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
