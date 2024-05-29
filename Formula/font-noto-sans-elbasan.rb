7a766cb3fa76cae3966a5d88069bb0917f1d83e6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:50 +0000

    font-noto-sans-elbasan updated

--- Formula/font-noto-sans-elbasan.rb
+++ Formula/font-noto-sans-elbasan.rb
@@ -1,12 +1,14 @@
 class FontNotoSansElbasan < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanselbasan/NotoSansElbasan-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Elbasan"
+  desc "Noto sans elbasan font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Elbasan"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanselbasan/NotoSansElbasan-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansElbasan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
