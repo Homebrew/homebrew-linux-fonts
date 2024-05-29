cbc6f7389e75f162135f0cbb94a9bd3321f563ab
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:49 +0000

    font-bonheur-royale updated

--- Formula/font-bonheur-royale.rb
+++ Formula/font-bonheur-royale.rb
@@ -1,12 +1,14 @@
 class FontBonheurRoyale < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bonheurroyale/BonheurRoyale-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bonheur Royale"
+  desc "Bonheur royale font"
   homepage "https://fonts.google.com/specimen/Bonheur+Royale"
+  head "https://github.com/google/fonts/raw/main/ofl/bonheurroyale/BonheurRoyale-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BonheurRoyale-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
