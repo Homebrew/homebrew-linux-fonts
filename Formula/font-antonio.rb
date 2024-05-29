e76c9f2dcc81de2a30d9dcc926390e19a1dcf80b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-antonio updated

--- Formula/font-antonio.rb
+++ Formula/font-antonio.rb
@@ -1,12 +1,14 @@
 class FontAntonio < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/antonio/Antonio%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Antonio"
+  desc "Antonio font"
   homepage "https://fonts.google.com/specimen/Antonio"
+  head "https://github.com/google/fonts/raw/main/ofl/antonio/Antonio%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Antonio\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Antonio[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
