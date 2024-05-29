42bb2a4d541ecbde93c7a7d5d75cb081e49c5c16
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-pirata-one updated

--- Formula/font-pirata-one.rb
+++ Formula/font-pirata-one.rb
@@ -1,12 +1,14 @@
 class FontPirataOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/pirataone/PirataOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Pirata One"
+  desc "Pirata one font"
   homepage "https://fonts.google.com/specimen/Pirata+One"
+  head "https://github.com/google/fonts/raw/main/ofl/pirataone/PirataOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PirataOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
