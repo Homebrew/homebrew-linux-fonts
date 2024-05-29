27137fa9a607006e3d790b6f755216c8b32c0dcc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-tex-gyre-bonum-math updated

--- Formula/font-tex-gyre-bonum-math.rb
+++ Formula/font-tex-gyre-bonum-math.rb
@@ -1,14 +1,15 @@
 class FontTexGyreBonumMath < Formula
+  desc "Bonum math font"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tg-math"
+  url "https://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrebonum-math-1005.zip"
   version "1.005"
   sha256 "8f8dc6f52ff838201f581f20b4ab634508e6d4b1e2745fe5d6b7732e1df73290"
-  url "https://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrebonum-math-#{version.to_s.gsub(".", "")}.zip"
-  desc "Bonum Math"
-  homepage "https://www.gust.org.pl/projects/e-foundry/tg-math"
+
   def install
-    (share/"fonts").install Dir.glob("texgyrebonum-math-#{version.to_s.gsub(".", "")}/opentype/**/texgyrebonum-math.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyrebonum-math-1005/opentype/texgyrebonum-math.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
