171860419f54f0016d1ce3e937ec1af78a345e67
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-old-standard-tt updated

--- Formula/font-old-standard-tt.rb
+++ Formula/font-old-standard-tt.rb
@@ -1,14 +1,18 @@
 class FontOldStandardTt < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/oldstandardtt"
-  desc "Old Standard TT"
+  desc "Old standard tt font"
   homepage "https://fonts.google.com/specimen/Old+Standard+TT"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/oldstandardtt"
+
   def install
     (share/"fonts").install Dir.glob("ofl/oldstandardtt/./**/OldStandard-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/oldstandardtt/./**/OldStandard-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/oldstandardtt/./**/OldStandard-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
