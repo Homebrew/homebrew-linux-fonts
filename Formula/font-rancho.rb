50798b4be46e2ce400b6751e71509c0085cbe1a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-rancho updated

--- Formula/font-rancho.rb
+++ Formula/font-rancho.rb
@@ -1,12 +1,14 @@
 class FontRancho < Formula
-  head "https://github.com/google/fonts/raw/main/apache/rancho/Rancho-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rancho"
+  desc "Rancho font"
   homepage "https://fonts.google.com/specimen/Rancho"
+  head "https://github.com/google/fonts/raw/main/apache/rancho/Rancho-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Rancho-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
