d01d22864f5722a3395dd44162803d837dbe18e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:11 +0000

    font-pt-serif updated

--- Formula/font-pt-serif.rb
+++ Formula/font-pt-serif.rb
@@ -1,18 +1,19 @@
 class FontPtSerif < Formula
-  head "https://api.paratype.com/api/editions/ofl/download/52194", referer: "https://www.paratype.com/"
-  desc "PT Serif"
   desc "Universal type family designed for use together with PT Sans"
   homepage "https://www.paratype.com/fonts/pt/pt-serif"
+  head "https://api.paratype.com/api/editions/ofl/download/52194",
+       referer: "https://www.paratype.com/"
+
   def install
-    (share/"fonts").install Dir.glob("PT/PT/PT-Serif/**/pt-serif_bold-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("PT/PT/PT-Serif/**/pt-serif_bold.ttf")[0]
-    (share/"fonts").install Dir.glob("PT/PT/PT-Serif/**/pt-serif_caption-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("PT/PT/PT-Serif/**/pt-serif_caption-regular.ttf")[0]
-    (share/"fonts").install Dir.glob("PT/PT/PT-Serif/**/pt-serif_italic.ttf")[0]
-    (share/"fonts").install Dir.glob("PT/PT/PT-Serif/**/pt-serif_regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PT/PT/PT-Serif/pt-serif_bold-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PT/PT/PT-Serif/pt-serif_bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PT/PT/PT-Serif/pt-serif_caption-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PT/PT/PT-Serif/pt-serif_caption-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PT/PT/PT-Serif/pt-serif_italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PT/PT/PT-Serif/pt-serif_regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
