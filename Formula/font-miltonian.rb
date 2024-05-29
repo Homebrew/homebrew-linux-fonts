c2588e2a59e484fd42cece30f66b4b9c2a4557ad
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-miltonian updated

--- Formula/font-miltonian.rb
+++ Formula/font-miltonian.rb
@@ -1,12 +1,14 @@
 class FontMiltonian < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/miltonian/Miltonian-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Miltonian"
+  desc "Miltonian font"
   homepage "https://fonts.google.com/specimen/Miltonian"
+  head "https://github.com/google/fonts/raw/main/ofl/miltonian/Miltonian-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Miltonian-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
