62e8e34883bcea68ff8ed63b73fafbc89efe5deb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:33 +0000

    font-unlock updated

--- Formula/font-unlock.rb
+++ Formula/font-unlock.rb
@@ -1,12 +1,14 @@
 class FontUnlock < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/unlock/Unlock-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Unlock"
+  desc "Unlock font"
   homepage "https://fonts.google.com/specimen/Unlock"
+  head "https://github.com/google/fonts/raw/main/ofl/unlock/Unlock-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Unlock-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
