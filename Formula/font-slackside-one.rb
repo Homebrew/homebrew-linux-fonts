0fca81783f5c3a2f3119d78cc65e1949717dbb2b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-slackside-one updated

--- Formula/font-slackside-one.rb
+++ Formula/font-slackside-one.rb
@@ -1,13 +1,14 @@
 class FontSlacksideOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/slacksideone/SlacksideOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Slackside One"
   desc "Handwritten display font"
   homepage "https://fonts.google.com/specimen/Slackside+One"
+  head "https://github.com/google/fonts/raw/main/ofl/slacksideone/SlacksideOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SlacksideOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
