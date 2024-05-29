dea8d98262afd051b0382a3e250e2537ead32468
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:36 +0000

    font-yarndings-20 updated

--- Formula/font-yarndings-20.rb
+++ Formula/font-yarndings-20.rb
@@ -1,12 +1,14 @@
 class FontYarndings20 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yarndings20/Yarndings20-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yarndings 20"
+  desc "Yarndings 20 font"
   homepage "https://fonts.google.com/specimen/Yarndings+20"
+  head "https://github.com/google/fonts/raw/main/ofl/yarndings20/Yarndings20-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Yarndings20-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
