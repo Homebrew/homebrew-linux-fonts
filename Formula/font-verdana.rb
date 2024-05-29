ea9dcc24d5d4beaae4a2ca932f1529c161ff97cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-verdana updated

--- Formula/font-verdana.rb
+++ Formula/font-verdana.rb
@@ -1,15 +1,16 @@
 class FontVerdana < Formula
-  head "https://downloads.sourceforge.net/corefonts/verdan32.exe"
-  desc "Verdana"
+  desc "Verdana font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  head "https://downloads.sourceforge.net/corefonts/verdan32.exe"
+
   def install
     (share/"fonts").install Dir.glob("./**/Verdana.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Verdanab.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Verdanai.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Verdanaz.TTF")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
