3b2806c8a1b9ca19a719532c30230367b6142304
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-halant updated

--- Formula/font-halant.rb
+++ Formula/font-halant.rb
@@ -1,9 +1,10 @@
 class FontHalant < Formula
+  desc "Halant font"
+  homepage "https://github.com/itfoundry/halant"
+  url "https://github.com/itfoundry/halant/releases/download/v2.000/halant-2_000.zip"
   version "2.000"
   sha256 "d65e1077b7c3d3ca42cf41f8ee2db4f6da5cc8aa6f615d18a1bb497f8e37ca63"
-  url "https://github.com/itfoundry/halant/releases/download/v#{version}/halant-#{version.to_s.gsub(".", "_")}.zip"
-  desc "Halant"
-  homepage "https://github.com/itfoundry/halant"
+
   def install
     (share/"fonts").install Dir.glob("./**/Halant-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/Halant-Light.otf")[0]
@@ -11,8 +12,8 @@ class FontHalant < Formula
     (share/"fonts").install Dir.glob("./**/Halant-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/Halant-SemiBold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
