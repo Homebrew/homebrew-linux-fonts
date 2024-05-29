29d9417ff5e9c11ba4cc941d6e9d32979fdce562
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-pixelify-sans updated

--- Formula/font-pixelify-sans.rb
+++ Formula/font-pixelify-sans.rb
@@ -1,13 +1,14 @@
 class FontPixelifySans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/pixelifysans/PixelifySans%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Pixelify Sans"
   desc "Achieved by using a grid of small, square pixels to create each letterform"
   homepage "https://fonts.google.com/specimen/Pixelify+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/pixelifysans/PixelifySans%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PixelifySans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PixelifySans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
