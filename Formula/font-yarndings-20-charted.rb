ebd0211a160dd53f8951038ac31d5690aa2c917c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:36 +0000

    font-yarndings-20-charted updated

--- Formula/font-yarndings-20-charted.rb
+++ Formula/font-yarndings-20-charted.rb
@@ -1,12 +1,14 @@
 class FontYarndings20Charted < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yarndings20charted/Yarndings20Charted-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yarndings 20 Charted"
+  desc "Yarndings 20 charted font"
   homepage "https://fonts.google.com/specimen/Yarndings+20+Charted"
+  head "https://github.com/google/fonts/raw/main/ofl/yarndings20charted/Yarndings20Charted-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Yarndings20Charted-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
