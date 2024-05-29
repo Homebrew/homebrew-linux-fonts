31d93b916d304418aeaa0d12aac18d51eb9ce66a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-material-symbols updated

--- Formula/font-material-symbols.rb
+++ Formula/font-material-symbols.rb
@@ -1,15 +1,18 @@
 class FontMaterialSymbols < Formula
-  head "https://github.com/google/material-design-icons.git", verified: "github.com/google/material-design-icons", branch: "master", only_path: "variablefont"
-  desc "Material Symbols"
   desc "Icons based on Material Design principles as variable fonts"
   homepage "https://fonts.google.com/icons"
+  head "https://github.com/google/material-design-icons.git",
+       verified:  "github.com/google/material-design-icons",
+       branch:    "master",
+       only_path: "variablefont"
+
   def install
-    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsOutlined\\[FILL,GRAD,opsz,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsRounded\\[FILL,GRAD,opsz,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsSharp\\[FILL,GRAD,opsz,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsRounded[FILL,GRAD,opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsSharp[FILL,GRAD,opsz,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
