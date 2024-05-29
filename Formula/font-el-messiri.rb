74ba874a776334895ff1e8da5dd052d91701fbd0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-el-messiri updated

--- Formula/font-el-messiri.rb
+++ Formula/font-el-messiri.rb
@@ -1,12 +1,14 @@
 class FontElMessiri < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/elmessiri/ElMessiri%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "El Messiri"
+  desc "El messiri font"
   homepage "https://fonts.google.com/specimen/El+Messiri"
+  head "https://github.com/google/fonts/raw/main/ofl/elmessiri/ElMessiri%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/ElMessiri\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ElMessiri[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
