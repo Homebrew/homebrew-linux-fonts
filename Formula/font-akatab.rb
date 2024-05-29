5c03776ab769312f1eb276f83caa0f986095047e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-akatab updated

--- Formula/font-akatab.rb
+++ Formula/font-akatab.rb
@@ -1,7 +1,11 @@
 class FontAkatab < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/akatab"
-  desc "Akatab"
+  desc "Akatab font"
   homepage "https://fonts.google.com/specimen/Akatab"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/akatab"
+
   def install
     (share/"fonts").install Dir.glob("ofl/akatab/./**/Akatab-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/akatab/./**/Akatab-Bold.ttf")[0]
@@ -10,8 +14,8 @@ class FontAkatab < Formula
     (share/"fonts").install Dir.glob("ofl/akatab/./**/Akatab-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/akatab/./**/Akatab-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
