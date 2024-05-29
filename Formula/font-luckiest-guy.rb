688ef8b4845953a445ece0db18b61a4b6ec0eee8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-luckiest-guy updated

--- Formula/font-luckiest-guy.rb
+++ Formula/font-luckiest-guy.rb
@@ -1,12 +1,14 @@
 class FontLuckiestGuy < Formula
-  head "https://github.com/google/fonts/raw/main/apache/luckiestguy/LuckiestGuy-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Luckiest Guy"
+  desc "Luckiest guy font"
   homepage "https://fonts.google.com/specimen/Luckiest+Guy"
+  head "https://github.com/google/fonts/raw/main/apache/luckiestguy/LuckiestGuy-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LuckiestGuy-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
