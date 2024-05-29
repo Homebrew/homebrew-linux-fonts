b53a351b0d6b4124204b68a5750ccd8e1a0f74e8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:51 +0000

    font-noto-sans-kawi updated

--- Formula/font-noto-sans-kawi.rb
+++ Formula/font-noto-sans-kawi.rb
@@ -1,13 +1,14 @@
 class FontNotoSansKawi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanskawi/NotoSansKawi%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Kawi"
   desc "Design for the historical southeast asian kawi script"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Kawi"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanskawi/NotoSansKawi%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansKawi\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansKawi[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
