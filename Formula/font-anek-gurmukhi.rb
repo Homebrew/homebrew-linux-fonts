544010d2d302a703dc337bb7eccb60ba2dcecc35
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-anek-gurmukhi updated

--- Formula/font-anek-gurmukhi.rb
+++ Formula/font-anek-gurmukhi.rb
@@ -1,12 +1,14 @@
 class FontAnekGurmukhi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anekgurmukhi/AnekGurmukhi%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Anek Gurmukhi"
+  desc "Anek gurmukhi font"
   homepage "https://fonts.google.com/specimen/Anek+Gurmukhi"
+  head "https://github.com/google/fonts/raw/main/ofl/anekgurmukhi/AnekGurmukhi%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/AnekGurmukhi\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekGurmukhi[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
