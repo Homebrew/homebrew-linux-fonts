a128389887c03f18114f1998af14ed33dae2fcfc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jacques-francois-shadow updated

--- Formula/font-jacques-francois-shadow.rb
+++ Formula/font-jacques-francois-shadow.rb
@@ -1,12 +1,14 @@
 class FontJacquesFrancoisShadow < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jacquesfrancoisshadow/JacquesFrancoisShadow-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jacques Francois Shadow"
+  desc "Jacques francois shadow font"
   homepage "https://fonts.google.com/specimen/Jacques+Francois+Shadow"
+  head "https://github.com/google/fonts/raw/main/ofl/jacquesfrancoisshadow/JacquesFrancoisShadow-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JacquesFrancoisShadow-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
