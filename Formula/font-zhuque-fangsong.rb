97a215f79000fa396a490b6f28e36d9524d63046
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:39 +0000

    font-zhuque-fangsong updated

--- Formula/font-zhuque-fangsong.rb
+++ Formula/font-zhuque-fangsong.rb
@@ -1,16 +1,15 @@
 class FontZhuqueFangsong < Formula
-  version "0.108"
-  sha256 "ff7bab9764b88d58f0f4b18fe197fd1b3eac3a67c10030a240556da8dca0c60a"
-  url "https://github.com/TrionesType/zhuque/releases/download/v#{version}/ZhuqueFangsong-v#{version}.zip"
-  desc "Zhuque Fangsong"
-  desc "朱雀仿宋"
   desc "Open source Chinese Fangsong font created by TrionesType"
   homepage "https://github.com/TrionesType/zhuque"
+  url "https://github.com/TrionesType/zhuque/releases/download/v0.108/ZhuqueFangsong-v0.108.zip"
+  version "0.108"
+  sha256 "ff7bab9764b88d58f0f4b18fe197fd1b3eac3a67c10030a240556da8dca0c60a"
+
   def install
     (share/"fonts").install Dir.glob("./**/ZhuqueFangsong-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
