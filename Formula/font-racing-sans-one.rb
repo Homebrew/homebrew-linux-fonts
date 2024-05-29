a422c2ca7993b3ab6e43fb25d213108d54ced394
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:12 +0000

    font-racing-sans-one updated

--- Formula/font-racing-sans-one.rb
+++ Formula/font-racing-sans-one.rb
@@ -1,12 +1,14 @@
 class FontRacingSansOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/racingsansone/RacingSansOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Racing Sans One"
+  desc "Racing sans one font"
   homepage "https://fonts.google.com/specimen/Racing+Sans+One"
+  head "https://github.com/google/fonts/raw/main/ofl/racingsansone/RacingSansOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RacingSansOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
