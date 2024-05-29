de520d4b2f15838b7f6b0c43eca66c02bbf8364c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-syne updated

--- Formula/font-syne.rb
+++ Formula/font-syne.rb
@@ -1,13 +1,14 @@
 class FontSyne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/syne/Syne%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Syne"
   desc "Typeface originally designed for the art center Synesthésie"
   homepage "https://fonts.google.com/specimen/Syne"
+  head "https://github.com/google/fonts/raw/main/ofl/syne/Syne%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Syne\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Syne[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
