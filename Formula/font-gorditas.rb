08a181f0b222dcbc51f9be09048fbf2841de1c2d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-gorditas updated

--- Formula/font-gorditas.rb
+++ Formula/font-gorditas.rb
@@ -1,13 +1,17 @@
 class FontGorditas < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gorditas"
-  desc "Gorditas"
+  desc "Gorditas font"
   homepage "https://fonts.google.com/specimen/Gorditas"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gorditas"
+
   def install
     (share/"fonts").install Dir.glob("ofl/gorditas/./**/Gorditas-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gorditas/./**/Gorditas-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
