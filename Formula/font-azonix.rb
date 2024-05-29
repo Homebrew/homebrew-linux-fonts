9778dacc5eb7bffbe023247a18992b82522fdf46
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-azonix updated

--- Formula/font-azonix.rb
+++ Formula/font-azonix.rb
@@ -1,13 +1,13 @@
 class FontAzonix < Formula
-  head "https://dl.dafont.com/dl/?f=azonix"
-  desc "Azonix"
   desc "Modern sans-serif typeface"
   homepage "https://www.dafont.com/azonix.font"
+  head "https://dl.dafont.com/dl/?f=azonix"
+
   def install
     (share/"fonts").install Dir.glob("./**/Azonix.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
