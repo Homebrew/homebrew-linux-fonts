58c844a3f634618be3244333cc6b357d2a2946fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-elsie-swash-caps updated

--- Formula/font-elsie-swash-caps.rb
+++ Formula/font-elsie-swash-caps.rb
@@ -1,13 +1,17 @@
 class FontElsieSwashCaps < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/elsieswashcaps"
-  desc "Elsie Swash Caps"
+  desc "Elsie swash caps font"
   homepage "https://fonts.google.com/specimen/Elsie+Swash+Caps"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/elsieswashcaps"
+
   def install
     (share/"fonts").install Dir.glob("ofl/elsieswashcaps/./**/ElsieSwashCaps-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/elsieswashcaps/./**/ElsieSwashCaps-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
