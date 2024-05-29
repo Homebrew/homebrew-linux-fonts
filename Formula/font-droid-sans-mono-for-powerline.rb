1f21a96108addb353624adef3eef49799df2614a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-droid-sans-mono-for-powerline updated

--- Formula/font-droid-sans-mono-for-powerline.rb
+++ Formula/font-droid-sans-mono-for-powerline.rb
@@ -1,12 +1,15 @@
 class FontDroidSansMonoForPowerline < Formula
-  head "https://github.com/powerline/fonts.git", branch: "master", only_path: "DroidSansMono"
-  desc "Droid Sans Mono for Powerline"
+  desc "Droid sans mono for powerline font"
   homepage "https://github.com/powerline/fonts/tree/master/DroidSansMono"
+  head "https://github.com/powerline/fonts.git",
+       branch:    "master",
+       only_path: "DroidSansMono"
+
   def install
     (share/"fonts").install Dir.glob("DroidSansMono/./**/Droid Sans Mono for Powerline.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
