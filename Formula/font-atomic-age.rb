8b774b27ac704922a71283af544c6f8fb100258d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-atomic-age updated

--- Formula/font-atomic-age.rb
+++ Formula/font-atomic-age.rb
@@ -1,12 +1,14 @@
 class FontAtomicAge < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/atomicage/AtomicAge-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Atomic Age"
+  desc "Atomic age font"
   homepage "https://fonts.google.com/specimen/Atomic+Age"
+  head "https://github.com/google/fonts/raw/main/ofl/atomicage/AtomicAge-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AtomicAge-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
