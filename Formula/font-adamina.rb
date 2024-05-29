647db0b12377ade1c09812ab23998b3c84cf1f5a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-adamina updated

--- Formula/font-adamina.rb
+++ Formula/font-adamina.rb
@@ -1,12 +1,14 @@
 class FontAdamina < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/adamina/Adamina-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Adamina"
+  desc "Adamina font"
   homepage "https://fonts.google.com/specimen/Adamina"
+  head "https://github.com/google/fonts/raw/main/ofl/adamina/Adamina-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Adamina-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
