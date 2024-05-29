86899aa137b2a68ed89ac756bbbf031aeace5cc1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-sankofa-display updated

--- Formula/font-sankofa-display.rb
+++ Formula/font-sankofa-display.rb
@@ -1,13 +1,13 @@
 class FontSankofaDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sankofadisplay/SankofaDisplay-Regular.ttf"
-  desc "Sankofa Display"
   desc "African typeface with a particular focus on straight-line geometric designs"
   homepage "https://github.com/batsimadz/Sankofa-Display"
+  head "https://github.com/google/fonts/raw/main/ofl/sankofadisplay/SankofaDisplay-Regular.ttf"
+
   def install
     (share/"fonts").install Dir.glob("./**/SankofaDisplay-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
