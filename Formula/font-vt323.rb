caa9987fcf3831dc9c2ab7718d1b04c170959f7c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-vt323 updated

--- Formula/font-vt323.rb
+++ Formula/font-vt323.rb
@@ -1,12 +1,14 @@
 class FontVt323 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/vt323/VT323-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "VT323"
+  desc "Vt323 font"
   homepage "https://fonts.google.com/specimen/VT323"
+  head "https://github.com/google/fonts/raw/main/ofl/vt323/VT323-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/VT323-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
