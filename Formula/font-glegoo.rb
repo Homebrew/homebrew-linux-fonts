b59b540e3941733cfb844c560b801c20fb5d4087
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:12 +0000

    font-glegoo updated

--- Formula/font-glegoo.rb
+++ Formula/font-glegoo.rb
@@ -1,13 +1,17 @@
 class FontGlegoo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/glegoo"
-  desc "Glegoo"
+  desc "Glegoo font"
   homepage "https://fonts.google.com/specimen/Glegoo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/glegoo"
+
   def install
     (share/"fonts").install Dir.glob("ofl/glegoo/./**/Glegoo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/glegoo/./**/Glegoo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
