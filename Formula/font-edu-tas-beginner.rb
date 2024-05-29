526b38647ce0661f6a2044bad5086dc24fc5d4c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-edu-tas-beginner updated

--- Formula/font-edu-tas-beginner.rb
+++ Formula/font-edu-tas-beginner.rb
@@ -1,12 +1,14 @@
 class FontEduTasBeginner < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/edutasbeginner/EduTASBeginner%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Edu TAS Beginner"
+  desc "Edu tas beginner font"
   homepage "https://fonts.google.com/specimen/Edu+TAS+Beginner"
+  head "https://github.com/google/fonts/raw/main/ofl/edutasbeginner/EduTASBeginner%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/EduTASBeginner\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/EduTASBeginner[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
