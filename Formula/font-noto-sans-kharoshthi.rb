ba185bbec01e110d583da58fe7bab54bbaca6620
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:51 +0000

    font-noto-sans-kharoshthi updated

--- Formula/font-noto-sans-kharoshthi.rb
+++ Formula/font-noto-sans-kharoshthi.rb
@@ -1,12 +1,14 @@
 class FontNotoSansKharoshthi < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKharoshthi-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Kharoshthi"
+  desc "Noto sans kharoshthi font"
   homepage "https://www.google.com/get/noto/#sans-khar"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKharoshthi-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansKharoshthi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
