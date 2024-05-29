7ca241b6174ef6d725350b1d52b754f1bb117c54
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-kiwi-maru updated

--- Formula/font-kiwi-maru.rb
+++ Formula/font-kiwi-maru.rb
@@ -1,15 +1,18 @@
 class FontKiwiMaru < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kiwimaru"
-  desc "Kiwi Maru"
   desc "Typeface for visualization of everyday and slang expressions"
   homepage "https://fonts.google.com/specimen/Kiwi+Maru"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kiwimaru"
+
   def install
     (share/"fonts").install Dir.glob("ofl/kiwimaru/./**/KiwiMaru-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kiwimaru/./**/KiwiMaru-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kiwimaru/./**/KiwiMaru-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
