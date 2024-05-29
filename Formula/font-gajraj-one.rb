1c8eba99befaf880d0eb42270313076369795ba8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-gajraj-one updated

--- Formula/font-gajraj-one.rb
+++ Formula/font-gajraj-one.rb
@@ -1,12 +1,14 @@
 class FontGajrajOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gajrajone/GajrajOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gajraj One"
+  desc "Gajraj one font"
   homepage "https://fonts.google.com/specimen/Gajraj+One"
+  head "https://github.com/google/fonts/raw/main/ofl/gajrajone/GajrajOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GajrajOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
