db73fd4c38eda26c2234aab825a6e06fd45f5c3d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-cozette updated

--- Formula/font-cozette.rb
+++ Formula/font-cozette.rb
@@ -1,15 +1,15 @@
 class FontCozette < Formula
-  version "1.24.1"
-  sha256 "27176cc4f76259db3e65eecda8999b2167c305bca9e563c272216208d94044f7"
-  url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
-  desc "Cozette"
   desc "Bitmap programming font"
   homepage "https://github.com/slavfox/Cozette"
+  url "https://github.com/slavfox/Cozette/releases/download/v.1.24.1/CozetteVector.dfont"
+  version "1.24.1"
+  sha256 "27176cc4f76259db3e65eecda8999b2167c305bca9e563c272216208d94044f7"
+
   def install
     (share/"fonts").install Dir.glob("./**/CozetteVector.dfont")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
