4ee46e622397439ee664e8300eee152c66af4d1e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-darwin updated

--- Formula/font-darwin.rb
+++ Formula/font-darwin.rb
@@ -1,13 +1,13 @@
 class FontDarwin < Formula
-  head "https://github.com/topological-modular-forms/Darwin-Typeface/raw/main/output/DarwinSerif-Regular.otf"
-  desc "Darwin"
   desc "Typeface for books and articles with a focus on scientific writing"
   homepage "https://github.com/topological-modular-forms/Darwin-Typeface"
+  head "https://github.com/topological-modular-forms/Darwin-Typeface/raw/main/output/DarwinSerif-Regular.otf"
+
   def install
     (share/"fonts").install Dir.glob("./**/DarwinSerif-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
