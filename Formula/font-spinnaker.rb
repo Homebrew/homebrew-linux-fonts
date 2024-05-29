4a125b077608cef2c8840efbfb17bd9bd2e15b9e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-spinnaker updated

--- Formula/font-spinnaker.rb
+++ Formula/font-spinnaker.rb
@@ -1,12 +1,14 @@
 class FontSpinnaker < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/spinnaker/Spinnaker-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Spinnaker"
+  desc "Spinnaker font"
   homepage "https://fonts.google.com/specimen/Spinnaker"
+  head "https://github.com/google/fonts/raw/main/ofl/spinnaker/Spinnaker-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Spinnaker-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
