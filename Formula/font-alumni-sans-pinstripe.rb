d8d1dab48e7584a04cbbb2e47784e8bd660f0c64
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-alumni-sans-pinstripe updated

--- Formula/font-alumni-sans-pinstripe.rb
+++ Formula/font-alumni-sans-pinstripe.rb
@@ -1,13 +1,17 @@
 class FontAlumniSansPinstripe < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/alumnisanspinstripe"
-  desc "Alumni Sans Pinstripe"
+  desc "Alumni sans pinstripe font"
   homepage "https://fonts.google.com/specimen/Alumni+Sans+Pinstripe"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/alumnisanspinstripe"
+
   def install
     (share/"fonts").install Dir.glob("ofl/alumnisanspinstripe/./**/AlumniSansPinstripe-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/alumnisanspinstripe/./**/AlumniSansPinstripe-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
