5bb172e75e8bf710ab66b0d2e533bff7420188c0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:31 +0000

    font-trocchi updated

--- Formula/font-trocchi.rb
+++ Formula/font-trocchi.rb
@@ -1,12 +1,14 @@
 class FontTrocchi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/trocchi/Trocchi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Trocchi"
+  desc "Trocchi font"
   homepage "https://fonts.google.com/specimen/Trocchi"
+  head "https://github.com/google/fonts/raw/main/ofl/trocchi/Trocchi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Trocchi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
