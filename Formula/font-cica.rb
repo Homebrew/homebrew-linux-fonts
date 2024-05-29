29008317aa0807b3b2fab86e3e99d7b6b92546c7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-cica updated

--- Formula/font-cica.rb
+++ Formula/font-cica.rb
@@ -1,17 +1,18 @@
 class FontCica < Formula
+  desc "Cica font"
+  homepage "https://github.com/miiton/Cica"
+  url "https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3.zip"
   version "5.0.3"
   sha256 "cbd1bcf1f3fd1ddbffe444369c76e42529add8538b25aeb75ab682d398b0506f"
-  url "https://github.com/miiton/Cica/releases/download/v#{version}/Cica_v#{version}.zip"
-  desc "Cica"
-  homepage "https://github.com/miiton/Cica"
+
   def install
     (share/"fonts").install Dir.glob("./**/Cica-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Cica-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Cica-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Cica-RegularItalic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
