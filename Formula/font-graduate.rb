dc774e11aa9c18e5a9fb13d5db1a1b2d3c2218e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-graduate updated

--- Formula/font-graduate.rb
+++ Formula/font-graduate.rb
@@ -1,12 +1,14 @@
 class FontGraduate < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/graduate/Graduate-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Graduate"
+  desc "Graduate font"
   homepage "https://fonts.google.com/specimen/Graduate"
+  head "https://github.com/google/fonts/raw/main/ofl/graduate/Graduate-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Graduate-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
