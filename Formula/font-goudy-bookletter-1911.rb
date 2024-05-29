4c225a33f3636f15392b77abe2071ace03e06e8b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-goudy-bookletter-1911 updated

--- Formula/font-goudy-bookletter-1911.rb
+++ Formula/font-goudy-bookletter-1911.rb
@@ -1,12 +1,14 @@
 class FontGoudyBookletter1911 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/goudybookletter1911/GoudyBookletter1911.ttf", verified: "github.com/google/fonts/"
-  desc "Goudy Bookletter 1911"
+  desc "Goudy bookletter 1911 font"
   homepage "https://fonts.google.com/specimen/Goudy+Bookletter+1911"
+  head "https://github.com/google/fonts/raw/main/ofl/goudybookletter1911/GoudyBookletter1911.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GoudyBookletter1911.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
