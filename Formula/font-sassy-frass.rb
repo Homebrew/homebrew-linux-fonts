0bdc2fbd6df033f350df99958cd72f8a5d926f37
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-sassy-frass updated

--- Formula/font-sassy-frass.rb
+++ Formula/font-sassy-frass.rb
@@ -1,12 +1,14 @@
 class FontSassyFrass < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sassyfrass/SassyFrass-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sassy Frass"
+  desc "Sassy frass font"
   homepage "https://fonts.google.com/specimen/Sassy+Frass"
+  head "https://github.com/google/fonts/raw/main/ofl/sassyfrass/SassyFrass-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SassyFrass-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
