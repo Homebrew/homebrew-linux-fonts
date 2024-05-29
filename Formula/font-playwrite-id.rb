27427ec919f3b318c16452add8e45159bfc0d603
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:08 +0000

    font-playwrite-id updated

--- Formula/font-playwrite-id.rb
+++ Formula/font-playwrite-id.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteId < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteid/PlaywriteID%5Bwght%5D.ttf"
-  desc "Playwrite ID"
+  desc "Playwrite id font"
   homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteid/PlaywriteID%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteID\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteID[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
