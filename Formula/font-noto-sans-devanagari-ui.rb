c6ed440383dc9650b8bcfd89dfd57a19113583fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:49 +0000

    font-noto-sans-devanagari-ui updated

--- Formula/font-noto-sans-devanagari-ui.rb
+++ Formula/font-noto-sans-devanagari-ui.rb
@@ -1,9 +1,13 @@
 class FontNotoSansDevanagariUi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosansdevanagariui"
-  desc "Noto Sans Devanagari UI"
+  desc "Noto sans devanagari ui font"
   homepage "https://fonts.google.com/noto/specimen/Noto+Sans+Devanagari"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/notosansdevanagariui"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-ExtraLight.ttf")[0]
@@ -11,10 +15,10 @@ class FontNotoSansDevanagariUi < Formula
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
