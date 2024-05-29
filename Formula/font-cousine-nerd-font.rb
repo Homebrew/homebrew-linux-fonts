9d651592103ab6a4432628c5a0deace20f5713bc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-cousine-nerd-font updated

--- Formula/font-cousine-nerd-font.rb
+++ Formula/font-cousine-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontCousineNerdFont < Formula
-  version "3.2.1"
-  sha256 "4ebb4fe2dd2b70f9f6d8035226d15f4af61ec90f0b7f179272b3e9be12a4e3dc"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Cousine.zip"
-  desc "Cousine Nerd Font (Cousine)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Cousine.zip"
+  version "3.2.1"
+  sha256 "4ebb4fe2dd2b70f9f6d8035226d15f4af61ec90f0b7f179272b3e9be12a4e3dc"
+
   def install
-    (share/"fonts").install Dir.glob("./**/CousineNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CousineNerdFontPropo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/CousineNerdFont-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/CousineNerdFont-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/CousineNerdFont-Regular.ttf")[0]
@@ -17,10 +17,10 @@ class FontCousineNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/CousineNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/CousineNerdFontPropo-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/CousineNerdFontPropo-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/CousineNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CousineNerdFont-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
