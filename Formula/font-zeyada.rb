28c56207d20234cf2317b44f04e4a3d8719546ff
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:38 +0000

    font-zeyada updated

--- Formula/font-zeyada.rb
+++ Formula/font-zeyada.rb
@@ -1,12 +1,14 @@
 class FontZeyada < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/zeyada/Zeyada.ttf", verified: "github.com/google/fonts/"
-  desc "Zeyada"
+  desc "Zeyada font"
   homepage "https://fonts.google.com/specimen/Zeyada"
+  head "https://github.com/google/fonts/raw/main/ofl/zeyada/Zeyada.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Zeyada.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
