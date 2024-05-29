e9d62ab5adf637732cdd6f57b0c12e29144b86df
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-courgette updated

--- Formula/font-courgette.rb
+++ Formula/font-courgette.rb
@@ -1,12 +1,14 @@
 class FontCourgette < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/courgette/Courgette-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Courgette"
+  desc "Courgette font"
   homepage "https://fonts.google.com/specimen/Courgette"
+  head "https://github.com/google/fonts/raw/main/ofl/courgette/Courgette-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Courgette-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
