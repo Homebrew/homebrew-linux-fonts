3a7526ec36cd990614850171abf436106bcd6c37
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-iranian-sans updated

--- Formula/font-iranian-sans.rb
+++ Formula/font-iranian-sans.rb
@@ -1,13 +1,14 @@
 class FontIranianSans < Formula
-  head "https://fontlibrary.org/assets/downloads/iranian-sans/2619cbcea5e87e4bb3d77b2e2a94466f/iranian-sans.zip"
-  desc "Iranian Sans"
+  desc "Iranian sans font"
   homepage "https://fontlibrary.org/en/font/iranian-sans"
+  head "https://fontlibrary.org/assets/downloads/iranian-sans/2619cbcea5e87e4bb3d77b2e2a94466f/iranian-sans.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/irsans.ttf")[0]
     (share/"fonts").install Dir.glob("./**/irsansb.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
