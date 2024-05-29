a486ebfa35ef2a26c44e4b9997ddd44efaeb1d45
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:32 +0000

    font-tuffy updated

--- Formula/font-tuffy.rb
+++ Formula/font-tuffy.rb
@@ -1,15 +1,19 @@
 class FontTuffy < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tuffy"
-  desc "Tuffy"
+  desc "Tuffy font"
   homepage "http://tulrich.com/fonts/"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tuffy"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tuffy/./**/Tuffy-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tuffy/./**/Tuffy-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tuffy/./**/Tuffy-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tuffy/./**/Tuffy-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
