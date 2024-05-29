442272c39f8080d8c158c8e507b77c4dbf6238c6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-sudo updated

--- Formula/font-sudo.rb
+++ Formula/font-sudo.rb
@@ -1,15 +1,15 @@
 class FontSudo < Formula
-  version "1.2"
-  sha256 "161a8d81f882063fe54302c3fe0b2ce9bfc14065b6e1457278595826c41d6dc3"
-  url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip"
-  desc "Sudo"
   desc "Font for programmers and command-line users"
   homepage "https://github.com/jenskutilek/sudo-font/"
+  url "https://github.com/jenskutilek/sudo-font/releases/download/v1.2/sudo.zip"
+  version "1.2"
+  sha256 "161a8d81f882063fe54302c3fe0b2ce9bfc14065b6e1457278595826c41d6dc3"
+
   def install
-    (share/"fonts").install Dir.glob("sudo/**/SudoVariable.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/sudo/SudoVariable.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
