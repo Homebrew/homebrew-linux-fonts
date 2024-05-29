d3824a66c7c33649a236def9d2e5fb0a00a67a09
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-economica updated

--- Formula/font-economica.rb
+++ Formula/font-economica.rb
@@ -1,15 +1,19 @@
 class FontEconomica < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/economica"
-  desc "Economica"
+  desc "Economica font"
   homepage "https://fonts.google.com/specimen/Economica"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/economica"
+
   def install
     (share/"fonts").install Dir.glob("ofl/economica/./**/Economica-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/economica/./**/Economica-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/economica/./**/Economica-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/economica/./**/Economica-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
