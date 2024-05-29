3656ab6077dc9bdbe9ab4d288a7566bc2a638f7d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-mervale-script updated

--- Formula/font-mervale-script.rb
+++ Formula/font-mervale-script.rb
@@ -1,12 +1,14 @@
 class FontMervaleScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mervalescript/MervaleScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mervale Script"
+  desc "Mervale script font"
   homepage "https://fonts.google.com/specimen/Mervale+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/mervalescript/MervaleScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MervaleScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
