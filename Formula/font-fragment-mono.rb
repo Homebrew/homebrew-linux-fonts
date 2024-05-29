f48af9214206d2bc97c0107604f6aae3e96ab587
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-fragment-mono updated

--- Formula/font-fragment-mono.rb
+++ Formula/font-fragment-mono.rb
@@ -1,14 +1,17 @@
 class FontFragmentMono < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fragmentmono"
-  desc "Fragment Mono"
   desc "Monospaced coding font inspired on Helvetica"
   homepage "https://fonts.google.com/specimen/Fragment+Mono"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/fragmentmono"
+
   def install
     (share/"fonts").install Dir.glob("ofl/fragmentmono/./**/FragmentMono-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/fragmentmono/./**/FragmentMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
