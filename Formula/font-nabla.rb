102f9568099cb8f1932109c89b204cf77cffe41e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-nabla updated

--- Formula/font-nabla.rb
+++ Formula/font-nabla.rb
@@ -1,13 +1,14 @@
 class FontNabla < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Nabla"
   desc "Color font inspired by isometric computer games, built using the colrv1 format"
   homepage "https://fonts.google.com/specimen/Nabla"
+  head "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Nabla\\[EDPT,EHLT\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Nabla[EDPT,EHLT].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
