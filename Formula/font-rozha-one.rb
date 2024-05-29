4e0c07a3fb805cc3b91fcbc3ebd149029ec0207f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-rozha-one updated

--- Formula/font-rozha-one.rb
+++ Formula/font-rozha-one.rb
@@ -1,12 +1,14 @@
 class FontRozhaOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rozhaone/RozhaOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rozha One"
+  desc "Rozha one font"
   homepage "https://fonts.google.com/specimen/Rozha+One"
+  head "https://github.com/google/fonts/raw/main/ofl/rozhaone/RozhaOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RozhaOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
