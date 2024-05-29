8a66c4049da0d9db27018ae00b7cdfc02b023546
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:33 +0000

    font-linden-hill updated

--- Formula/font-linden-hill.rb
+++ Formula/font-linden-hill.rb
@@ -1,13 +1,17 @@
 class FontLindenHill < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lindenhill"
-  desc "Linden Hill"
+  desc "Linden hill font"
   homepage "https://fonts.google.com/specimen/Linden+Hill"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/lindenhill"
+
   def install
     (share/"fonts").install Dir.glob("ofl/lindenhill/./**/LindenHill-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lindenhill/./**/LindenHill-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
