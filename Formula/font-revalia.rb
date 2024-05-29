95b86abc93ca1a63d8a9c806ec014febf0ff985d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-revalia updated

--- Formula/font-revalia.rb
+++ Formula/font-revalia.rb
@@ -1,12 +1,14 @@
 class FontRevalia < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/revalia/Revalia-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Revalia"
+  desc "Revalia font"
   homepage "https://fonts.google.com/specimen/Revalia"
+  head "https://github.com/google/fonts/raw/main/ofl/revalia/Revalia-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Revalia-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
