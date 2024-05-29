c305e282e60fe05023695af28b0014788d910caf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-homenaje updated

--- Formula/font-homenaje.rb
+++ Formula/font-homenaje.rb
@@ -1,12 +1,14 @@
 class FontHomenaje < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/homenaje/Homenaje-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Homenaje"
+  desc "Homenaje font"
   homepage "https://fonts.google.com/specimen/Homenaje"
+  head "https://github.com/google/fonts/raw/main/ofl/homenaje/Homenaje-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Homenaje-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
