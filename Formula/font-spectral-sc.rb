bdf7328e078ab43bc35ad21362f3c76f1bab0f83
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-spectral-sc updated

--- Formula/font-spectral-sc.rb
+++ Formula/font-spectral-sc.rb
@@ -1,9 +1,13 @@
 class FontSpectralSc < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/spectralsc"
-  desc "Spectral SC"
+  desc "Spectral sc font"
   homepage "https://fonts.google.com/specimen/Spectral+SC"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/spectralsc"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-ExtraBoldItalic.ttf")[0]
@@ -16,10 +20,10 @@ class FontSpectralSc < Formula
     (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/spectralsc/./**/SpectralSC-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
