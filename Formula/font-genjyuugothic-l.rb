74600d362fea9718728f0b664d4302915284afcc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-genjyuugothic-l updated

--- Formula/font-genjyuugothic-l.rb
+++ Formula/font-genjyuugothic-l.rb
@@ -1,12 +1,13 @@
 class FontGenjyuugothicL < Formula
-  version "20150607,8.643"
-  sha256 "d2fccec290232df110d1116fef4411416875acf7647084c9ab7d7eb5e8a80c50"
-  url "https://osdn.dl.osdn.jp/users/#{version.to_s.sub(/.*,/, "").sub(/\..*/, "")}/#{version.to_s.sub(/.*,/, "").gsub(".", "")}/genjyuugothic-l-#{version.to_s.sub(/,.*/, "")}.zip", verified: "osdn.jp/"
-  desc "Gen Jyuu GothicL"
   desc "Weak-Rounded version of Gen Shin Gothic"
   homepage "http://jikasei.me/font/genjyuu/"
+  url "https://osdn.dl.osdn.jp/users/8/8643/genjyuugothic-l-20150607.zip",
+       verified: "osdn.jp/"
+  version "20150607,8.643"
+  sha256 "d2fccec290232df110d1116fef4411416875acf7647084c9ab7d7eb5e8a80c50"
+
   def install
-    (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-Heavy.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-Light.ttf")[0]
@@ -26,10 +27,10 @@ class FontGenjyuugothicL < Formula
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-P-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-P-Normal.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-P-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenJyuuGothicL-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
