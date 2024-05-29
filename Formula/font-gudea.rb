63315701b3e7842f1dcc318a998bede8c95ba704
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-gudea updated

--- Formula/font-gudea.rb
+++ Formula/font-gudea.rb
@@ -1,14 +1,18 @@
 class FontGudea < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gudea"
-  desc "Gudea"
+  desc "Gudea font"
   homepage "https://fonts.google.com/specimen/Gudea"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gudea"
+
   def install
     (share/"fonts").install Dir.glob("ofl/gudea/./**/Gudea-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gudea/./**/Gudea-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gudea/./**/Gudea-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
