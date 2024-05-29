b33065522bad212e11b04b18b0f964be23e877bb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:50 +0000

    font-noto-sans-elymaic updated

--- Formula/font-noto-sans-elymaic.rb
+++ Formula/font-noto-sans-elymaic.rb
@@ -1,12 +1,14 @@
 class FontNotoSansElymaic < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanselymaic/NotoSansElymaic-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Elymaic"
+  desc "Noto sans elymaic font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Elymaic"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanselymaic/NotoSansElymaic-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansElymaic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
