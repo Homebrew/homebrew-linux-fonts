11b36493bb54ee0eb61adefa574c44e2d9b205c5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-danfo updated

--- Formula/font-danfo.rb
+++ Formula/font-danfo.rb
@@ -1,13 +1,14 @@
 class FontDanfo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/danfo/Danfo%5BELSH%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Danfo"
   desc "Elsh axis variable font with three main styles at 0,50, and 100"
   homepage "https://fonts.google.com/specimen/Danfo"
+  head "https://github.com/google/fonts/raw/main/ofl/danfo/Danfo%5BELSH%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Danfo\\[ELSH\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Danfo[ELSH].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
