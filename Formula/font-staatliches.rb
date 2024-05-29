03d74b1d711e91c02e5c4db70c4779a645c71d7f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-staatliches updated

--- Formula/font-staatliches.rb
+++ Formula/font-staatliches.rb
@@ -1,12 +1,14 @@
 class FontStaatliches < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/staatliches/Staatliches-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Staatliches"
+  desc "Staatliches font"
   homepage "https://fonts.google.com/specimen/Staatliches"
+  head "https://github.com/google/fonts/raw/main/ofl/staatliches/Staatliches-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Staatliches-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
