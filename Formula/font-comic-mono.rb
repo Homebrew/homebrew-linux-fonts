6a2f32113dfd4a2075f5e6bf61b63252918ae63e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-comic-mono updated

--- Formula/font-comic-mono.rb
+++ Formula/font-comic-mono.rb
@@ -1,14 +1,15 @@
 class FontComicMono < Formula
-  head "https://codeload.github.com/dtinth/comic-mono-font/zip/master", verified: "codeload.github.com/dtinth/comic-mono-font/"
-  desc "Comic Mono"
   desc "Legible monospace font"
   homepage "https://dtinth.github.io/comic-mono-font/"
+  head "https://codeload.github.com/dtinth/comic-mono-font/zip/master",
+       verified: "codeload.github.com/dtinth/comic-mono-font/"
+
   def install
-    (share/"fonts").install Dir.glob("comic-mono-font-master/**/ComicMono.ttf")[0]
-    (share/"fonts").install Dir.glob("comic-mono-font-master/**/ComicMono-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/comic-mono-font-master/ComicMono.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/comic-mono-font-master/ComicMono-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
