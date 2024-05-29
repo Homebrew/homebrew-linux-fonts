6fac3a4fd77b777d34e168c03773d0d40ad3f5e4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-tex-gyre-chorus updated

--- Formula/font-tex-gyre-chorus.rb
+++ Formula/font-tex-gyre-chorus.rb
@@ -1,14 +1,15 @@
 class FontTexGyreChorus < Formula
+  desc "Tex gyre chorus font"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus/qzc2.003otf.zip"
   version "2.003"
   sha256 "fbd905a504e6f86df9c38b42c6ed58681a10e6debae6afa6308bc031695cdbd8"
-  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus/qzc#{version}otf.zip"
-  desc "TeX Gyre Chorus"
-  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus"
+
   def install
     (share/"fonts").install Dir.glob("./**/texgyrechorus-mediumitalic.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
