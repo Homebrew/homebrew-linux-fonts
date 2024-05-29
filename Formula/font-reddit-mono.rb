f99c9913bec41b39d776e462bfb95bb1a68e93cd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-reddit-mono updated

--- Formula/font-reddit-mono.rb
+++ Formula/font-reddit-mono.rb
@@ -1,13 +1,14 @@
 class FontRedditMono < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/redditmono/RedditMono%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Reddit Mono"
   desc "Complemented by reddit sans and reddit sans condensed"
   homepage "https://fonts.google.com/specimen/Reddit+Mono"
+  head "https://github.com/google/fonts/raw/main/ofl/redditmono/RedditMono%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/RedditMono\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/RedditMono[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
