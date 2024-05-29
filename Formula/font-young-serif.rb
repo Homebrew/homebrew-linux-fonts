c9bd47d9c7b7035acd23e3afdfb889abadd3c88f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-young-serif updated

--- Formula/font-young-serif.rb
+++ Formula/font-young-serif.rb
@@ -1,13 +1,14 @@
 class FontYoungSerif < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/youngserif/YoungSerif-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Young Serif"
   desc "Heavy weight old style serif typeface"
   homepage "https://fonts.google.com/specimen/Young+Serif"
+  head "https://github.com/google/fonts/raw/main/ofl/youngserif/YoungSerif-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/YoungSerif-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
