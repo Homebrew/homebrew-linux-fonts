ab650a61a8bfef6d770950fe30f434d84af73803
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-gayathri updated

--- Formula/font-gayathri.rb
+++ Formula/font-gayathri.rb
@@ -1,14 +1,18 @@
 class FontGayathri < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gayathri"
-  desc "Gayathri"
+  desc "Gayathri font"
   homepage "https://fonts.google.com/specimen/Gayathri"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gayathri"
+
   def install
     (share/"fonts").install Dir.glob("ofl/gayathri/./**/Gayathri-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gayathri/./**/Gayathri-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gayathri/./**/Gayathri-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
