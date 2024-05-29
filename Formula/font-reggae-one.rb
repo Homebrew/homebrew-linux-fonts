13268da087a6d80221ff9e7aeddf3a2629b6a616
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-reggae-one updated

--- Formula/font-reggae-one.rb
+++ Formula/font-reggae-one.rb
@@ -1,12 +1,14 @@
 class FontReggaeOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/reggaeone/ReggaeOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Reggae One"
+  desc "Reggae one font"
   homepage "https://fonts.google.com/specimen/Reggae+One"
+  head "https://github.com/google/fonts/raw/main/ofl/reggaeone/ReggaeOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ReggaeOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
