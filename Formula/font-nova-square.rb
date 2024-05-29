c6f48819e115412d3393540145ba9f8b2915f94b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-nova-square updated

--- Formula/font-nova-square.rb
+++ Formula/font-nova-square.rb
@@ -1,12 +1,14 @@
 class FontNovaSquare < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/novasquare/NovaSquare.ttf", verified: "github.com/google/fonts/"
-  desc "Nova Square"
+  desc "Nova square font"
   homepage "https://fonts.google.com/specimen/Nova+Square"
+  head "https://github.com/google/fonts/raw/main/ofl/novasquare/NovaSquare.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NovaSquare.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
