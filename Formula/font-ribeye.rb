f2ed6aa2821af3016a9fdb920265573fe9350459
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:15 +0000

    font-ribeye updated

--- Formula/font-ribeye.rb
+++ Formula/font-ribeye.rb
@@ -1,12 +1,14 @@
 class FontRibeye < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ribeye/Ribeye-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ribeye"
+  desc "Ribeye font"
   homepage "https://fonts.google.com/specimen/Ribeye"
+  head "https://github.com/google/fonts/raw/main/ofl/ribeye/Ribeye-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Ribeye-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
