0f47b16b647d5d34d4dcf5cb7fc3071d92969480
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-luculent updated

--- Formula/font-luculent.rb
+++ Formula/font-luculent.rb
@@ -1,15 +1,16 @@
 class FontLuculent < Formula
-  head "http://eastfarthing.com/luculent/luculent.tar.xz"
-  desc "Luculent"
+  desc "Luculent font"
   homepage "http://eastfarthing.com/luculent/"
+  head "http://eastfarthing.com/luculent/luculent.tar.xz"
+
   def install
-    (share/"fonts").install Dir.glob("luculent/**/luculent.ttf")[0]
-    (share/"fonts").install Dir.glob("luculent/**/luculentb.ttf")[0]
-    (share/"fonts").install Dir.glob("luculent/**/luculentbi.ttf")[0]
-    (share/"fonts").install Dir.glob("luculent/**/luculenti.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/luculent/luculent.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/luculent/luculentb.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/luculent/luculentbi.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/luculent/luculenti.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
