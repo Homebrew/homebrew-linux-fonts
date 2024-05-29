be4e017045133d3e0b1e171585a6018773cf546f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:52 +0000

    font-castoro-titling updated

--- Formula/font-castoro-titling.rb
+++ Formula/font-castoro-titling.rb
@@ -1,12 +1,14 @@
 class FontCastoroTitling < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/castorotitling/CastoroTitling-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Castoro Titling"
+  desc "Castoro titling font"
   homepage "https://fonts.google.com/specimen/Castoro+Titling"
+  head "https://github.com/google/fonts/raw/main/ofl/castorotitling/CastoroTitling-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/CastoroTitling-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
