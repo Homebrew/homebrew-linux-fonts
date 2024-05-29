e70db1cfcb5e7dec81b5a0b5981403450fa1b4a6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-dm-serif-text updated

--- Formula/font-dm-serif-text.rb
+++ Formula/font-dm-serif-text.rb
@@ -1,13 +1,17 @@
 class FontDmSerifText < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/dmseriftext"
-  desc "DM Serif Text"
+  desc "Dm serif text font"
   homepage "https://fonts.google.com/specimen/DM+Serif+Text"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/dmseriftext"
+
   def install
     (share/"fonts").install Dir.glob("ofl/dmseriftext/./**/DMSerifText-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/dmseriftext/./**/DMSerifText-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
