a8db38bee7b508ea98daae595702d8ae2780c4bd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:33 +0000

    font-updock updated

--- Formula/font-updock.rb
+++ Formula/font-updock.rb
@@ -1,13 +1,14 @@
 class FontUpdock < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/updock/Updock-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Updock"
   desc "Extremely legible formal script with clean connectors"
   homepage "https://fonts.google.com/specimen/Updock"
+  head "https://github.com/google/fonts/raw/main/ofl/updock/Updock-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Updock-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
