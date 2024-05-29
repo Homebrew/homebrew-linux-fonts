74e7cdb677e03b07bf6c2df7a5c78841d7138cc3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-kite-one updated

--- Formula/font-kite-one.rb
+++ Formula/font-kite-one.rb
@@ -1,12 +1,14 @@
 class FontKiteOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kiteone/KiteOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kite One"
+  desc "Kite one font"
   homepage "https://fonts.google.com/specimen/Kite+One"
+  head "https://github.com/google/fonts/raw/main/ofl/kiteone/KiteOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/KiteOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
