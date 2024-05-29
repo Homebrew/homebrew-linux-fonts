10fe10de3a59213b7b6115c6510836c951ea03a6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:49 +0000

    font-noto-sans-cjk-kr updated

--- Formula/font-noto-sans-cjk-kr.rb
+++ Formula/font-noto-sans-cjk-kr.rb
@@ -1,10 +1,10 @@
 class FontNotoSansCjkKr < Formula
-  version "2.004"
-  sha256 "e26fcf98e75176d24984875377ab921dbb46055b88ed4a39454d91d6146c5654"
-  url "https://github.com/notofonts/noto-cjk/releases/download/Sans#{version}/07_NotoSansCJKkr.zip"
-  desc "Noto Sans CJK KR"
   desc "Language Specific OTFs Korean (한국어)"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Sans2.004/07_NotoSansCJKkr.zip"
+  version "2.004"
+  sha256 "e26fcf98e75176d24984875377ab921dbb46055b88ed4a39454d91d6146c5654"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansCJKkr-Black.otf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCJKkr-Bold.otf")[0]
@@ -14,8 +14,8 @@ class FontNotoSansCjkKr < Formula
     (share/"fonts").install Dir.glob("./**/NotoSansCJKkr-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCJKkr-Thin.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
