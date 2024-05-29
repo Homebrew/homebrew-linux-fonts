ba8be356f155946c89c9c1ab5282c8d390da30a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-open-sans updated

--- Formula/font-open-sans.rb
+++ Formula/font-open-sans.rb
@@ -1,13 +1,17 @@
 class FontOpenSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/opensans"
-  desc "Open Sans"
+  desc "Open sans font"
   homepage "https://fonts.google.com/specimen/Open+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/opensans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/opensans/./**/OpenSans-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/opensans/./**/OpenSans\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/opensans/./**/OpenSans-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/opensans/./**/OpenSans[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
