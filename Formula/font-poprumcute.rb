70a94ac3e9a0b1aa1db89497cefe4601dc8cce3a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:09 +0000

    font-poprumcute updated

--- Formula/font-poprumcute.rb
+++ Formula/font-poprumcute.rb
@@ -1,13 +1,13 @@
 class FontPoprumcute < Formula
-  head "https://moji-waku.com/download/poprumcute.zip"
-  desc "PopRumCute"
   desc "Modern Showa styled Japanese font"
   homepage "https://moji-waku.com/poprumcute/index.html"
+  head "https://moji-waku.com/download/poprumcute.zip"
+
   def install
-    (share/"fonts").install Dir.glob("poprumcute/**/PopRumCute.otf")[0]
+    (share/"fonts").install Dir.glob("./**/poprumcute/PopRumCute.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
