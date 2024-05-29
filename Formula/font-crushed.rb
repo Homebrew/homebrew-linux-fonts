1e87a26b37850a864cd868aa2d281c64ac858891
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-crushed updated

--- Formula/font-crushed.rb
+++ Formula/font-crushed.rb
@@ -1,12 +1,14 @@
 class FontCrushed < Formula
-  head "https://github.com/google/fonts/raw/main/apache/crushed/Crushed-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Crushed"
+  desc "Crushed font"
   homepage "https://fonts.google.com/specimen/Crushed"
+  head "https://github.com/google/fonts/raw/main/apache/crushed/Crushed-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Crushed-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
