7b2fb9dcf687c571689579800b214e52b36194f8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-charmonman updated

--- Formula/font-charmonman.rb
+++ Formula/font-charmonman.rb
@@ -1,13 +1,17 @@
 class FontCharmonman < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/charmonman"
-  desc "Charmonman"
+  desc "Charmonman font"
   homepage "https://fonts.google.com/specimen/Charmonman"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/charmonman"
+
   def install
     (share/"fonts").install Dir.glob("ofl/charmonman/./**/Charmonman-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/charmonman/./**/Charmonman-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
