bf3131d13bf9bae752123ee00a21976588c311d2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:08 +0000

    font-playwrite-ie updated

--- Formula/font-playwrite-ie.rb
+++ Formula/font-playwrite-ie.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteIe < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteie/PlaywriteIE%5Bwght%5D.ttf"
-  desc "Playwrite IE"
+  desc "Playwrite ie font"
   homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteie/PlaywriteIE%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteIE\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteIE[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
