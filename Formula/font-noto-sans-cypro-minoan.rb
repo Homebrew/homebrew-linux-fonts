0d583f9ee3674e1556af6783b735314616cc7b83
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:49 +0000

    font-noto-sans-cypro-minoan updated

--- Formula/font-noto-sans-cypro-minoan.rb
+++ Formula/font-noto-sans-cypro-minoan.rb
@@ -1,13 +1,14 @@
 class FontNotoSansCyproMinoan < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanscyprominoan/NotoSansCyproMinoan-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Cypro Minoan"
   desc "Design for the historical european cypro-minoan script"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Cypro+Minoan"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanscyprominoan/NotoSansCyproMinoan-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansCyproMinoan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
