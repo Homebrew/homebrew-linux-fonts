47ce2d4959b61f56275b6ec56052a09d0badbd2a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-cute-font updated

--- Formula/font-cute-font.rb
+++ Formula/font-cute-font.rb
@@ -1,12 +1,14 @@
 class FontCuteFont < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/cutefont/CuteFont-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Cute Font"
+  desc "Cute font font"
   homepage "https://fonts.google.com/specimen/Cute+Font"
+  head "https://github.com/google/fonts/raw/main/ofl/cutefont/CuteFont-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/CuteFont-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
