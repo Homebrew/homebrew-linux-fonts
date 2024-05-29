d4adbb7e36e2ec4dc94f4518bf59516e189d51da
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-londrina-solid updated

--- Formula/font-londrina-solid.rb
+++ Formula/font-londrina-solid.rb
@@ -1,15 +1,19 @@
 class FontLondrinaSolid < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/londrinasolid"
-  desc "Londrina Solid"
+  desc "Londrina solid font"
   homepage "https://fonts.google.com/specimen/Londrina+Solid"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/londrinasolid"
+
   def install
     (share/"fonts").install Dir.glob("ofl/londrinasolid/./**/LondrinaSolid-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/londrinasolid/./**/LondrinaSolid-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/londrinasolid/./**/LondrinaSolid-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/londrinasolid/./**/LondrinaSolid-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
