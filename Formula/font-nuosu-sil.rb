d91cc21978ea812f2881e352c8c874463d80732b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-nuosu-sil updated

--- Formula/font-nuosu-sil.rb
+++ Formula/font-nuosu-sil.rb
@@ -1,12 +1,14 @@
 class FontNuosuSil < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nuosusil/NuosuSIL-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Nuosu SIL"
+  desc "Nuosu sil font"
   homepage "https://fonts.google.com/specimen/Nuosu+SIL"
+  head "https://github.com/google/fonts/raw/main/ofl/nuosusil/NuosuSIL-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NuosuSIL-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
