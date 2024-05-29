7ed7574c358208a08b41ca8d711ca04ae9be07c3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-aboriginal-serif updated

--- Formula/font-aboriginal-serif.rb
+++ Formula/font-aboriginal-serif.rb
@@ -1,15 +1,16 @@
 class FontAboriginalSerif < Formula
-  head "https://www.languagegeek.com/font/AboriginalSerif.zip"
-  desc "Aboriginal Serif"
+  desc "Aboriginal serif font"
   homepage "https://www.languagegeek.com/font/fontdownload.html"
+  head "https://www.languagegeek.com/font/AboriginalSerif.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/AboriginalSerifREGULAR.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AboriginalSerifBOLD.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AboriginalSerifBOLDITALIC.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AboriginalSerifITALIC.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
