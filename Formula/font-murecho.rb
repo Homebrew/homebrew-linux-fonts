c09c4e660c521c2efedf1f539b9d71bda42f6571
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-murecho updated

--- Formula/font-murecho.rb
+++ Formula/font-murecho.rb
@@ -1,13 +1,14 @@
 class FontMurecho < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/murecho/Murecho%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Murecho"
   desc "Available in 9 practical weights and as a variable font"
   homepage "https://fonts.google.com/specimen/Murecho"
+  head "https://github.com/google/fonts/raw/main/ofl/murecho/Murecho%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Murecho\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Murecho[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
