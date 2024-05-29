914b5b200b718c7065dd07ddb425813d805e714b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-alkatra updated

--- Formula/font-alkatra.rb
+++ Formula/font-alkatra.rb
@@ -1,13 +1,14 @@
 class FontAlkatra < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alkatra/Alkatra%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Alkatra"
   desc "Display typeface family"
   homepage "https://fonts.google.com/specimen/Alkatra"
+  head "https://github.com/google/fonts/raw/main/ofl/alkatra/Alkatra%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Alkatra\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Alkatra[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
