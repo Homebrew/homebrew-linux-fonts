c2d346e31092813824e50dd709abca9833342c9d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:40 +0000

    font-migmix-1m updated

--- Formula/font-migmix-1m.rb
+++ Formula/font-migmix-1m.rb
@@ -1,15 +1,16 @@
 class FontMigmix1m < Formula
+  desc "Migmix 1m font"
+  homepage "https://mix-mplus-ipa.osdn.jp/migmix/#migmix1m"
+  url "https://osdn.dl.osdn.jp/mix-mplus-ipa/63544/migmix-1m-20150712.zip"
   version "20150712"
   sha256 "ac91394f3687315fb2727f8ee2b8ef70c6801d0b674dfc991912400eb3e7a344"
-  url "https://osdn.dl.osdn.jp/mix-mplus-ipa/63544/migmix-1m-#{version}.zip"
-  desc "MigMix 1M"
-  homepage "https://mix-mplus-ipa.osdn.jp/migmix/#migmix1m"
+
   def install
-    (share/"fonts").install Dir.glob("migmix-1m-#{version}/**/migmix-1m-bold.ttf")[0]
-    (share/"fonts").install Dir.glob("migmix-1m-#{version}/**/migmix-1m-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migmix-1m-20150712/migmix-1m-bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migmix-1m-20150712/migmix-1m-regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
