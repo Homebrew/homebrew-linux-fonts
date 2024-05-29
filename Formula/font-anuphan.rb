305fbef7afdd7e8f26c86b4769c0e583da8d8d6e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-anuphan updated

--- Formula/font-anuphan.rb
+++ Formula/font-anuphan.rb
@@ -1,13 +1,14 @@
 class FontAnuphan < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anuphan/Anuphan%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Anuphan"
   desc "Not a modification of ibm plex sans thai"
   homepage "https://fonts.google.com/specimen/Anuphan"
+  head "https://github.com/google/fonts/raw/main/ofl/anuphan/Anuphan%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Anuphan\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Anuphan[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
