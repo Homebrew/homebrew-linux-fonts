61436476b852c466416ed01f36b29d201da0b9be
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-cochineal updated

--- Formula/font-cochineal.rb
+++ Formula/font-cochineal.rb
@@ -1,15 +1,16 @@
 class FontCochineal < Formula
-  head "http://mirrors.ctan.org/fonts/cochineal.zip"
-  desc "Cochineal"
+  desc "Cochineal font"
   homepage "https://ctan.org/pkg/cochineal"
+  head "http://mirrors.ctan.org/fonts/cochineal.zip"
+
   def install
-    (share/"fonts").install Dir.glob("cochineal/opentype/**/Cochineal-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("cochineal/opentype/**/Cochineal-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("cochineal/opentype/**/Cochineal-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("cochineal/opentype/**/Cochineal-Roman.otf")[0]
+    (share/"fonts").install Dir.glob("./**/cochineal/opentype/Cochineal-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/cochineal/opentype/Cochineal-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/cochineal/opentype/Cochineal-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/cochineal/opentype/Cochineal-Roman.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
