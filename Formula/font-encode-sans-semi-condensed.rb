dd8b459487e600f99d576f4c838d5cf235bd1f6d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-encode-sans-semi-condensed updated

--- Formula/font-encode-sans-semi-condensed.rb
+++ Formula/font-encode-sans-semi-condensed.rb
@@ -1,9 +1,13 @@
 class FontEncodeSansSemiCondensed < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/encodesanssemicondensed"
-  desc "Encode Sans Semi Condensed"
+  desc "Encode sans semi condensed font"
   homepage "https://fonts.google.com/specimen/Encode+Sans+Semi+Condensed"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/encodesanssemicondensed"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-ExtraLight.ttf")[0]
@@ -11,10 +15,10 @@ class FontEncodeSansSemiCondensed < Formula
     (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/encodesanssemicondensed/./**/EncodeSansSemiCondensed-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
