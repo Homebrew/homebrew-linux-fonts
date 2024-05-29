a6189c28e83c15dc49853aa4b622bd4bdd531177
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-junction updated

--- Formula/font-junction.rb
+++ Formula/font-junction.rb
@@ -1,14 +1,16 @@
 class FontJunction < Formula
-  head "https://github.com/theleagueof/junction/archive/master.zip", verified: "github.com/theleagueof/junction/"
-  desc "Junction"
+  desc "Junction font"
   homepage "https://www.theleagueofmoveabletype.com/junction"
+  head "https://github.com/theleagueof/junction/archive/master.zip",
+       verified: "github.com/theleagueof/junction/"
+
   def install
-    (share/"fonts").install Dir.glob("junction-master/**/Junction-bold.otf")[0]
-    (share/"fonts").install Dir.glob("junction-master/**/Junction-light.otf")[0]
-    (share/"fonts").install Dir.glob("junction-master/**/Junction-regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/junction-master/Junction-bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/junction-master/Junction-light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/junction-master/Junction-regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
