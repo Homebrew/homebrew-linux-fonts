4a8912334401fb055ea1027dff55825c11863b91
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-kufam updated

--- Formula/font-kufam.rb
+++ Formula/font-kufam.rb
@@ -1,14 +1,17 @@
 class FontKufam < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kufam"
-  desc "Kufam"
   desc "Arabic-Latin bilingual typeface intended for contemporary information design"
   homepage "https://fonts.google.com/specimen/Kufam"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kufam"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/kufam/./**/Kufam-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/kufam/./**/Kufam\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kufam/./**/Kufam-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kufam/./**/Kufam[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
