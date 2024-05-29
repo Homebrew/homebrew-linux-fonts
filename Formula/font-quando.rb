1cff8595ef7c8f809175fcb3254f8bd3cc6666aa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:12 +0000

    font-quando updated

--- Formula/font-quando.rb
+++ Formula/font-quando.rb
@@ -1,12 +1,14 @@
 class FontQuando < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/quando/Quando-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Quando"
+  desc "Quando font"
   homepage "https://fonts.google.com/specimen/Quando"
+  head "https://github.com/google/fonts/raw/main/ofl/quando/Quando-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Quando-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
