12c6c849e34b7a8e4e0ed9ab7fd3c6e1c2a5c509
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-maven-pro-vf-beta updated

--- Formula/font-maven-pro-vf-beta.rb
+++ Formula/font-maven-pro-vf-beta.rb
@@ -1,12 +1,14 @@
 class FontMavenProVfBeta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mavenprovfbeta/MavenProVFBeta.ttf", verified: "github.com/google/fonts/"
-  desc "Maven Pro VF Beta"
+  desc "Maven pro vf beta font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts/raw/main/ofl/mavenprovfbeta/MavenProVFBeta.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MavenProVFBeta.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
