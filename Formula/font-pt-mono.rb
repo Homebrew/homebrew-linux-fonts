a8f7b7692dcae1f3075183d66de46e59a001d52f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:11 +0000

    font-pt-mono updated

--- Formula/font-pt-mono.rb
+++ Formula/font-pt-mono.rb
@@ -1,14 +1,15 @@
 class FontPtMono < Formula
-  head "https://api.paratype.com/api/editions/ofl/download/52218", referer: "https://www.paratype.com/"
-  desc "PT Mono"
   desc "Monospaced font of the PT Project series"
   homepage "https://www.paratype.com/fonts/pt/pt-mono"
+  head "https://api.paratype.com/api/editions/ofl/download/52218",
+       referer: "https://www.paratype.com/"
+
   def install
-    (share/"fonts").install Dir.glob("PT/PT/PT-Mono/**/pt-mono_bold.ttf")[0]
-    (share/"fonts").install Dir.glob("PT/PT/PT-Mono/**/pt-mono_regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PT/PT/PT-Mono/pt-mono_bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PT/PT/PT-Mono/pt-mono_regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
