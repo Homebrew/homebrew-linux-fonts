1908588ec42f7fdaae1ecea45ee9db04bc6e6429
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-maps updated

--- Formula/font-rubik-maps.rb
+++ Formula/font-rubik-maps.rb
@@ -1,12 +1,14 @@
 class FontRubikMaps < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikmaps/RubikMaps-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Maps"
+  desc "Rubik maps font"
   homepage "https://fonts.google.com/specimen/Rubik+Maps"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikmaps/RubikMaps-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikMaps-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
