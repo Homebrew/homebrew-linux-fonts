fe447fdb773e197e2520f2afacd51c8b38a1ff08
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-warnes updated

--- Formula/font-warnes.rb
+++ Formula/font-warnes.rb
@@ -1,12 +1,14 @@
 class FontWarnes < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/warnes/Warnes-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Warnes"
+  desc "Warnes font"
   homepage "https://fonts.google.com/specimen/Warnes"
+  head "https://github.com/google/fonts/raw/main/ofl/warnes/Warnes-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Warnes-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
