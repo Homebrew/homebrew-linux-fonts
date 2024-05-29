7f9c56455cebd8f55650cd5a0df5a37451f3e057
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:34 +0000

    font-0xproto updated

--- Formula/font-0xproto.rb
+++ Formula/font-0xproto.rb
@@ -1,16 +1,16 @@
 class Font0xproto < Formula
-  version "2.001"
-  sha256 "fa35e66b4bc045717dcec67f6b6a83a8128b34b1bd024a7f6a689908f15d6cf2"
-  url "https://github.com/0xType/0xProto/releases/download/#{version}/0xProto_#{version.to_s.gsub(".", "_")}.zip"
-  desc "0xProto"
   desc "Programming font focused on source code legibility"
   homepage "https://github.com/0xType/0xProto"
+  url "https://github.com/0xType/0xProto/releases/download/2.001/0xProto_2_001.zip"
+  version "2.001"
+  sha256 "fa35e66b4bc045717dcec67f6b6a83a8128b34b1bd024a7f6a689908f15d6cf2"
+
   def install
     (share/"fonts").install Dir.glob("./**/0xProto-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/0xProto-Italic.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
