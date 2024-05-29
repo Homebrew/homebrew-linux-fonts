48d7756040ec0dad323388233d4cd9953f4a0463
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-nobile updated

--- Formula/font-nobile.rb
+++ Formula/font-nobile.rb
@@ -1,7 +1,11 @@
 class FontNobile < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/nobile"
-  desc "Nobile"
+  desc "Nobile font"
   homepage "https://fonts.google.com/specimen/Nobile"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/nobile"
+
   def install
     (share/"fonts").install Dir.glob("ofl/nobile/./**/Nobile-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/nobile/./**/Nobile-BoldItalic.ttf")[0]
@@ -10,8 +14,8 @@ class FontNobile < Formula
     (share/"fonts").install Dir.glob("ofl/nobile/./**/Nobile-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/nobile/./**/Nobile-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
