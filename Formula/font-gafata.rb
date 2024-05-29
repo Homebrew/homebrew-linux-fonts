1356a2a83794e0cf69dadcd87adbd3c2d17ff52b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-gafata updated

--- Formula/font-gafata.rb
+++ Formula/font-gafata.rb
@@ -1,12 +1,14 @@
 class FontGafata < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gafata/Gafata-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gafata"
+  desc "Gafata font"
   homepage "https://fonts.google.com/specimen/Gafata"
+  head "https://github.com/google/fonts/raw/main/ofl/gafata/Gafata-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Gafata-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
