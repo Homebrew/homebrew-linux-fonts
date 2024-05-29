b9068faecfa69fb0f4ff16a675839c00b4c84cb7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-tiny5 updated

--- Formula/font-tiny5.rb
+++ Formula/font-tiny5.rb
@@ -1,12 +1,13 @@
 class FontTiny5 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tiny5/Tiny5-Regular.ttf"
-  desc "Tiny5"
+  desc "Tiny5 font"
   homepage "https://github.com/Gissio/font_tiny5"
+  head "https://github.com/google/fonts/raw/main/ofl/tiny5/Tiny5-Regular.ttf"
+
   def install
     (share/"fonts").install Dir.glob("./**/Tiny5-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
