92ab1ba07a70592e7dc4218f022cde7f24882495
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-meera-inimai updated

--- Formula/font-meera-inimai.rb
+++ Formula/font-meera-inimai.rb
@@ -1,12 +1,14 @@
 class FontMeeraInimai < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/meerainimai/MeeraInimai-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Meera Inimai"
+  desc "Meera inimai font"
   homepage "https://fonts.google.com/specimen/Meera+Inimai"
+  head "https://github.com/google/fonts/raw/main/ofl/meerainimai/MeeraInimai-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MeeraInimai-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
