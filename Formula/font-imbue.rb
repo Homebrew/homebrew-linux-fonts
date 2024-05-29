9185aa7dd6e286b23c5a4252a9cb36635385315e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-imbue updated

--- Formula/font-imbue.rb
+++ Formula/font-imbue.rb
@@ -1,13 +1,14 @@
 class FontImbue < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/imbue/Imbue%5Bopsz%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Imbue"
   desc "Variable condensed Didone font"
   homepage "https://fonts.google.com/specimen/Imbue"
+  head "https://github.com/google/fonts/raw/main/ofl/imbue/Imbue%5Bopsz%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Imbue\\[opsz,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Imbue[opsz,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
