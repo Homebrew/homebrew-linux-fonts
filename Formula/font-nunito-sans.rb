b5f0ac7393195cb1c0bba23d395b424471e21c58
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-nunito-sans updated

--- Formula/font-nunito-sans.rb
+++ Formula/font-nunito-sans.rb
@@ -1,13 +1,17 @@
 class FontNunitoSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/nunitosans"
-  desc "Nunito Sans"
+  desc "Nunito sans font"
   homepage "https://fonts.google.com/specimen/Nunito+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/nunitosans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/nunitosans/./**/NunitoSans-Italic\\[YTLC,opsz,wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/nunitosans/./**/NunitoSans\\[YTLC,opsz,wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/nunitosans/./**/NunitoSans-Italic[YTLC,opsz,wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/nunitosans/./**/NunitoSans[YTLC,opsz,wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
