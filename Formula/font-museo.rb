02231d8bf4fdc70985395d0b402e67eba230075c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-museo updated

--- Formula/font-museo.rb
+++ Formula/font-museo.rb
@@ -1,14 +1,17 @@
 class FontMuseo < Formula
-  version "2.002"
-  url "https://abstractfonts-downloads.s3.amazonaws.com/zips/1/4/8/9/0/contents/museo-300.zip", verified: "abstractfonts-downloads.s3.amazonaws.com/"
-  desc "Museo"
+  desc "Museo font"
   homepage "http://www.abstractfonts.com/font/14890"
+  url "https://abstractfonts-downloads.s3.amazonaws.com/zips/1/4/8/9/0/contents/museo-300.zip",
+       verified: "abstractfonts-downloads.s3.amazonaws.com/"
+  version "2.002"
+  sha256 :no_check
+
   def install
     (share/"fonts").install Dir.glob("./**/Museo300-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/Museo700-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
