1ecf52f893e4097b621ac11633c6f0b3c596a932
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-afacad updated

--- Formula/font-afacad.rb
+++ Formula/font-afacad.rb
@@ -1,13 +1,17 @@
 class FontAfacad < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/afacad"
-  desc "Afacad"
+  desc "Afacad font"
   homepage "https://fonts.google.com/specimen/Afacad"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/afacad"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/afacad/./**/Afacad-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/afacad/./**/Afacad\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/afacad/./**/Afacad-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/afacad/./**/Afacad[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
