96bea866b7bb6ae2a77f0ace28dadb38e749d77c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-kings updated

--- Formula/font-kings.rb
+++ Formula/font-kings.rb
@@ -1,13 +1,14 @@
 class FontKings < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kings/Kings-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kings"
   desc "Based on the three set font family (kings honor, kings quest and kings dominion)"
   homepage "https://fonts.google.com/specimen/Kings"
+  head "https://github.com/google/fonts/raw/main/ofl/kings/Kings-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Kings-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
