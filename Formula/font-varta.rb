ced6efb6781759c186a39da4059f00770bf144ed
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-varta updated

--- Formula/font-varta.rb
+++ Formula/font-varta.rb
@@ -1,12 +1,14 @@
 class FontVarta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/varta/Varta%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Varta"
+  desc "Varta font"
   homepage "https://fonts.google.com/specimen/Varta"
+  head "https://github.com/google/fonts/raw/main/ofl/varta/Varta%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Varta\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Varta[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
