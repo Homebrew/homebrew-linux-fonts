be7f2e83ed56f20d76223be7df29cda4984f3ee4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-darker-grotesque updated

--- Formula/font-darker-grotesque.rb
+++ Formula/font-darker-grotesque.rb
@@ -1,12 +1,14 @@
 class FontDarkerGrotesque < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/darkergrotesque/DarkerGrotesque%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Darker Grotesque"
+  desc "Darker grotesque font"
   homepage "https://fonts.google.com/specimen/Darker+Grotesque"
+  head "https://github.com/google/fonts/raw/main/ofl/darkergrotesque/DarkerGrotesque%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/DarkerGrotesque\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DarkerGrotesque[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
