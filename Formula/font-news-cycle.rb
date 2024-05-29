7bceaee68e235a0c9ea79359871542a7740d482f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-news-cycle updated

--- Formula/font-news-cycle.rb
+++ Formula/font-news-cycle.rb
@@ -1,13 +1,17 @@
 class FontNewsCycle < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/newscycle"
-  desc "News Cycle"
+  desc "News cycle font"
   homepage "https://fonts.google.com/specimen/News+Cycle"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/newscycle"
+
   def install
     (share/"fonts").install Dir.glob("ofl/newscycle/./**/NewsCycle-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/newscycle/./**/NewsCycle-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
