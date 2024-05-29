41751758ceebf57f9d7c49cd90d4142ba2ff5c71
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-italianno updated

--- Formula/font-italianno.rb
+++ Formula/font-italianno.rb
@@ -1,12 +1,14 @@
 class FontItalianno < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/italianno/Italianno-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Italianno"
+  desc "Italianno font"
   homepage "https://fonts.google.com/specimen/Italianno"
+  head "https://github.com/google/fonts/raw/main/ofl/italianno/Italianno-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Italianno-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
