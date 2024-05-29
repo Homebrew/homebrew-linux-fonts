9b797ac4ed8f7eeb015488ea170eb8c2617d25c7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-rosarivo updated

--- Formula/font-rosarivo.rb
+++ Formula/font-rosarivo.rb
@@ -1,13 +1,17 @@
 class FontRosarivo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rosarivo"
-  desc "Rosarivo"
+  desc "Rosarivo font"
   homepage "https://fonts.google.com/specimen/Rosarivo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/rosarivo"
+
   def install
     (share/"fonts").install Dir.glob("ofl/rosarivo/./**/Rosarivo-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/rosarivo/./**/Rosarivo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
