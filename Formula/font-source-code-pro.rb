3416e348f089f77b0d4eafe5a4ac455dac8c6077
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-source-code-pro updated

--- Formula/font-source-code-pro.rb
+++ Formula/font-source-code-pro.rb
@@ -1,13 +1,17 @@
 class FontSourceCodePro < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sourcecodepro"
-  desc "Source Code Pro"
+  desc "Source code pro font"
   homepage "https://fonts.google.com/specimen/Source+Code+Pro"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sourcecodepro"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/sourcecodepro/./**/SourceCodePro-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/sourcecodepro/./**/SourceCodePro\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sourcecodepro/./**/SourceCodePro-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sourcecodepro/./**/SourceCodePro[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
