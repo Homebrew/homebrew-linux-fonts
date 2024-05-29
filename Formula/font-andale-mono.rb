a8666e1385ede376c5670036ef71141a62ceb17f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-andale-mono updated

--- Formula/font-andale-mono.rb
+++ Formula/font-andale-mono.rb
@@ -1,12 +1,13 @@
 class FontAndaleMono < Formula
-  head "https://downloads.sourceforge.net/corefonts/andale32.exe"
-  desc "Andale Mono"
+  desc "Andale mono font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  head "https://downloads.sourceforge.net/corefonts/andale32.exe"
+
   def install
     (share/"fonts").install Dir.glob("./**/AndaleMo.TTF")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
