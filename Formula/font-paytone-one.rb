dadeb844b2acd8c9f54125d5905e35eb5a07b407
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-paytone-one updated

--- Formula/font-paytone-one.rb
+++ Formula/font-paytone-one.rb
@@ -1,12 +1,14 @@
 class FontPaytoneOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/paytoneone/PaytoneOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Paytone One"
+  desc "Paytone one font"
   homepage "https://fonts.google.com/specimen/Paytone+One"
+  head "https://github.com/google/fonts/raw/main/ofl/paytoneone/PaytoneOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PaytoneOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
