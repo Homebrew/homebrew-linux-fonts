40f7f8b94a6db032b72ba60a6163004385e615fb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:27 +0000

    font-karla updated

--- Formula/font-karla.rb
+++ Formula/font-karla.rb
@@ -1,13 +1,17 @@
 class FontKarla < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/karla"
-  desc "Karla"
+  desc "Karla font"
   homepage "https://fonts.google.com/specimen/Karla"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/karla"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/karla/./**/Karla-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/karla/./**/Karla\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/karla/./**/Karla-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/karla/./**/Karla[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
