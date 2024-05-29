558428297fb43b1c8ecce3bafdd7ed7a7c287335
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:33 +0000

    font-limelight updated

--- Formula/font-limelight.rb
+++ Formula/font-limelight.rb
@@ -1,12 +1,14 @@
 class FontLimelight < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/limelight/Limelight-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Limelight"
+  desc "Limelight font"
   homepage "https://fonts.google.com/specimen/Limelight"
+  head "https://github.com/google/fonts/raw/main/ofl/limelight/Limelight-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Limelight-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
