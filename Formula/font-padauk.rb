8f597195340ce009e556b5cfe92a05821500cc38
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:04 +0000

    font-padauk updated

--- Formula/font-padauk.rb
+++ Formula/font-padauk.rb
@@ -1,13 +1,17 @@
 class FontPadauk < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/padauk"
-  desc "Padauk"
+  desc "Padauk font"
   homepage "https://fonts.google.com/specimen/Padauk"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/padauk"
+
   def install
     (share/"fonts").install Dir.glob("ofl/padauk/./**/Padauk-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/padauk/./**/Padauk-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
