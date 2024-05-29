5a69b04f797e9a64787500fc7101ebe0612b8b00
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-academicons updated

--- Formula/font-academicons.rb
+++ Formula/font-academicons.rb
@@ -1,15 +1,15 @@
 class FontAcademicons < Formula
-  version "1.9.4"
-  sha256 "6f27234f174e9a8872296b1778083393127773ddcb19f0b3cc5ab3bfa42c37e9"
-  url "https://github.com/jpswalsh/academicons/archive/v#{version}.zip"
-  desc "Academicons"
   desc "Specialist icon font for academics"
   homepage "https://github.com/jpswalsh/academicons"
+  url "https://github.com/jpswalsh/academicons/archive/v1.9.4.zip"
+  version "1.9.4"
+  sha256 "6f27234f174e9a8872296b1778083393127773ddcb19f0b3cc5ab3bfa42c37e9"
+
   def install
-    (share/"fonts").install Dir.glob("academicons-#{version}/fonts/**/academicons.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/academicons-1.9.4/fonts/academicons.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
