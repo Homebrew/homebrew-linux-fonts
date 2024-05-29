ebb895b114b76bde0e51157cf3f3eeca90852406
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-agbalumo updated

--- Formula/font-agbalumo.rb
+++ Formula/font-agbalumo.rb
@@ -1,13 +1,14 @@
 class FontAgbalumo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/agbalumo/Agbalumo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Agbalumo"
   desc "Single weight font"
   homepage "https://fonts.google.com/specimen/Agbalumo"
+  head "https://github.com/google/fonts/raw/main/ofl/agbalumo/Agbalumo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Agbalumo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
