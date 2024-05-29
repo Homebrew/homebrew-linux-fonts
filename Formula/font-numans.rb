00d48968e6c97e6ff86790d7368b0e30f3309fe7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-numans updated

--- Formula/font-numans.rb
+++ Formula/font-numans.rb
@@ -1,12 +1,14 @@
 class FontNumans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/numans/Numans-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Numans"
+  desc "Numans font"
   homepage "https://fonts.google.com/specimen/Numans"
+  head "https://github.com/google/fonts/raw/main/ofl/numans/Numans-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Numans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
