26c9685cde1b301b3fcac1106669f1c254299516
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:18 +0000

    font-ibm-plex-sans-jp updated

--- Formula/font-ibm-plex-sans-jp.rb
+++ Formula/font-ibm-plex-sans-jp.rb
@@ -1,7 +1,11 @@
 class FontIbmPlexSansJp < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ibmplexsansjp"
-  desc "IBM Plex Sans JP"
+  desc "Ibm plex sans jp font"
   homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+JP"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ibmplexsansjp"
+
   def install
     (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-ExtraLight.ttf")[0]
@@ -11,8 +15,8 @@ class FontIbmPlexSansJp < Formula
     (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
