f45914e6257ed563e80eae0257768239238b5f09
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:45 +0000

    font-basic updated

--- Formula/font-basic.rb
+++ Formula/font-basic.rb
@@ -1,12 +1,14 @@
 class FontBasic < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/basic/Basic-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Basic"
+  desc "Basic font"
   homepage "https://fonts.google.com/specimen/Basic"
+  head "https://github.com/google/fonts/raw/main/ofl/basic/Basic-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Basic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
