2347cc672659fb28ba3f3872d44d315588ee47e0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-sunflower updated

--- Formula/font-sunflower.rb
+++ Formula/font-sunflower.rb
@@ -1,14 +1,18 @@
 class FontSunflower < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sunflower"
-  desc "Sunflower"
+  desc "Sunflower font"
   homepage "https://fonts.google.com/specimen/Sunflower"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sunflower"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sunflower/./**/Sunflower-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sunflower/./**/Sunflower-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sunflower/./**/Sunflower-Medium.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
