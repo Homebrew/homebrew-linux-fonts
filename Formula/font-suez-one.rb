9bdb2fffca0adbe115377a00914e8a1729c81eb2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-suez-one updated

--- Formula/font-suez-one.rb
+++ Formula/font-suez-one.rb
@@ -1,12 +1,14 @@
 class FontSuezOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/suezone/SuezOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Suez One"
+  desc "Suez one font"
   homepage "https://fonts.google.com/specimen/Suez+One"
+  head "https://github.com/google/fonts/raw/main/ofl/suezone/SuezOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SuezOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
