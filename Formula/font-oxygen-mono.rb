336cd5dce020582814c809bcddbc9984d5277be6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:04 +0000

    font-oxygen-mono updated

--- Formula/font-oxygen-mono.rb
+++ Formula/font-oxygen-mono.rb
@@ -1,12 +1,14 @@
 class FontOxygenMono < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/oxygenmono/OxygenMono-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Oxygen Mono"
+  desc "Oxygen mono font"
   homepage "https://fonts.google.com/specimen/Oxygen+Mono"
+  head "https://github.com/google/fonts/raw/main/ofl/oxygenmono/OxygenMono-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/OxygenMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
