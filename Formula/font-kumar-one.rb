85577817dc41989d3d760761fcaaa5a299e5f1aa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-kumar-one updated

--- Formula/font-kumar-one.rb
+++ Formula/font-kumar-one.rb
@@ -1,12 +1,14 @@
 class FontKumarOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kumarone/KumarOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kumar One"
+  desc "Kumar one font"
   homepage "https://fonts.google.com/specimen/Kumar+One"
+  head "https://github.com/google/fonts/raw/main/ofl/kumarone/KumarOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/KumarOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
