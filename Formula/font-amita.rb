08d6b4e41ec114119a0acff408f51ba5c90280b4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-amita updated

--- Formula/font-amita.rb
+++ Formula/font-amita.rb
@@ -1,13 +1,17 @@
 class FontAmita < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/amita"
-  desc "Amita"
+  desc "Amita font"
   homepage "https://fonts.google.com/specimen/Amita"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/amita"
+
   def install
     (share/"fonts").install Dir.glob("ofl/amita/./**/Amita-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/amita/./**/Amita-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
