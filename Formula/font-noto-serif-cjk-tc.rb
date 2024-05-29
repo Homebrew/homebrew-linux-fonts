d436f494af91ebe31e122b93a991cbf050ce2d58
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:58 +0000

    font-noto-serif-cjk-tc updated

--- Formula/font-noto-serif-cjk-tc.rb
+++ Formula/font-noto-serif-cjk-tc.rb
@@ -1,21 +1,21 @@
 class FontNotoSerifCjkTc < Formula
-  version "2.002"
-  sha256 "bd14b808431c5058c69f999c3c6d898b9f37d96962ec26f7212fb34c2d49ea5c"
-  url "https://github.com/notofonts/noto-cjk/releases/download/Serif#{version}/10_NotoSerifCJKtc.zip"
-  desc "Noto Serif CJK TC"
   desc "Language Specific OTFs Traditional Chinese — Taiwan (繁體中文—臺灣)"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Serif"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Serif2.002/10_NotoSerifCJKtc.zip"
+  version "2.002"
+  sha256 "bd14b808431c5058c69f999c3c6d898b9f37d96962ec26f7212fb34c2d49ea5c"
+
   def install
-    (share/"fonts").install Dir.glob("OTF/TraditionalChinese/**/NotoSerifCJKtc-Black.otf")[0]
-    (share/"fonts").install Dir.glob("OTF/TraditionalChinese/**/NotoSerifCJKtc-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("OTF/TraditionalChinese/**/NotoSerifCJKtc-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("OTF/TraditionalChinese/**/NotoSerifCJKtc-Light.otf")[0]
-    (share/"fonts").install Dir.glob("OTF/TraditionalChinese/**/NotoSerifCJKtc-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("OTF/TraditionalChinese/**/NotoSerifCJKtc-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("OTF/TraditionalChinese/**/NotoSerifCJKtc-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TraditionalChinese/NotoSerifCJKtc-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TraditionalChinese/NotoSerifCJKtc-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TraditionalChinese/NotoSerifCJKtc-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TraditionalChinese/NotoSerifCJKtc-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TraditionalChinese/NotoSerifCJKtc-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TraditionalChinese/NotoSerifCJKtc-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TraditionalChinese/NotoSerifCJKtc-SemiBold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
