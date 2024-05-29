d569f696a1dd9131bb1683f6f555ed8f9b943e2c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-lustria updated

--- Formula/font-lustria.rb
+++ Formula/font-lustria.rb
@@ -1,12 +1,14 @@
 class FontLustria < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lustria/Lustria-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Lustria"
+  desc "Lustria font"
   homepage "https://fonts.google.com/specimen/Lustria"
+  head "https://github.com/google/fonts/raw/main/ofl/lustria/Lustria-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Lustria-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
