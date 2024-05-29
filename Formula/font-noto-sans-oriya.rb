ffc25ecf5dc16fe9fb288ccbffe6117276de67be
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-oriya updated

--- Formula/font-noto-sans-oriya.rb
+++ Formula/font-noto-sans-oriya.rb
@@ -1,13 +1,15 @@
 class FontNotoSansOriya < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOriya-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Oriya"
+  desc "Noto sans oriya font"
   homepage "https://www.google.com/get/noto/#sans-orya"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOriya-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansOriya-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansOriya-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
