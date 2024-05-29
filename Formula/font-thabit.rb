419036cd8ed82d2b67cd64238733c8e0a53bcc83
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-thabit updated

--- Formula/font-thabit.rb
+++ Formula/font-thabit.rb
@@ -1,15 +1,19 @@
 class FontThabit < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/thabit"
-  desc "Thabit"
+  desc "Thabit font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/thabit"
+
   def install
     (share/"fonts").install Dir.glob("ofl/thabit/./**/Thabit-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/thabit/./**/Thabit-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/thabit/./**/Thabit-Oblique.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/thabit/./**/Thabit.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
