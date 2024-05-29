2a0ddd2b756d798695e051cec4562353d9a40487
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-alatsi updated

--- Formula/font-alatsi.rb
+++ Formula/font-alatsi.rb
@@ -1,12 +1,14 @@
 class FontAlatsi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alatsi/Alatsi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Alatsi"
+  desc "Alatsi font"
   homepage "https://fonts.google.com/specimen/Alatsi"
+  head "https://github.com/google/fonts/raw/main/ofl/alatsi/Alatsi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Alatsi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
