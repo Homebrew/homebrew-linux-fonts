2071e679334d2bde81ce93723ef0833ef97b614a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-varela-round updated

--- Formula/font-varela-round.rb
+++ Formula/font-varela-round.rb
@@ -1,12 +1,14 @@
 class FontVarelaRound < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/varelaround/VarelaRound-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Varela Round"
+  desc "Varela round font"
   homepage "https://fonts.google.com/specimen/Varela+Round"
+  head "https://github.com/google/fonts/raw/main/ofl/varelaround/VarelaRound-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/VarelaRound-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
