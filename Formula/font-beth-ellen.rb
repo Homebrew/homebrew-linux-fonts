78353a9c6f1b90c3d9895c12ace4d9affaced9ce
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-beth-ellen updated

--- Formula/font-beth-ellen.rb
+++ Formula/font-beth-ellen.rb
@@ -1,12 +1,14 @@
 class FontBethEllen < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bethellen/BethEllen-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Beth Ellen"
+  desc "Beth ellen font"
   homepage "https://fonts.google.com/specimen/Beth+Ellen"
+  head "https://github.com/google/fonts/raw/main/ofl/bethellen/BethEllen-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BethEllen-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
