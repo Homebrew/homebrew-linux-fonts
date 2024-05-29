fae94a5c50bbaa2fdb52b4fcccf4337ec29bde57
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-jacquard-12-charted updated

--- Formula/font-jacquard-12-charted.rb
+++ Formula/font-jacquard-12-charted.rb
@@ -1,12 +1,14 @@
 class FontJacquard12Charted < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jacquard12charted/Jacquard12Charted-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jacquard 12 Charted"
+  desc "Jacquard 12 charted font"
   homepage "https://fonts.google.com/specimen/Jacquard+12+Charted"
+  head "https://github.com/google/fonts/raw/main/ofl/jacquard12charted/Jacquard12Charted-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Jacquard12Charted-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
