e5999225ecfae0704d302bc30c7e50864887399b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-dokdo updated

--- Formula/font-dokdo.rb
+++ Formula/font-dokdo.rb
@@ -1,12 +1,14 @@
 class FontDokdo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/dokdo/Dokdo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Dokdo"
+  desc "Dokdo font"
   homepage "https://fonts.google.com/specimen/Dokdo"
+  head "https://github.com/google/fonts/raw/main/ofl/dokdo/Dokdo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Dokdo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
