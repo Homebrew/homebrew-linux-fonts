e012de1efbd336f7ec736c14bb227276e92d5f83
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:07 +0000

    font-playwrite-de-grund updated

--- Formula/font-playwrite-de-grund.rb
+++ Formula/font-playwrite-de-grund.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteDeGrund < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwritedegrund/PlaywriteDEGrund%5Bwght%5D.ttf"
-  desc "Playwrite DE Grund"
+  desc "Playwrite de grund font"
   homepage "https://github.com/TypeTogether/Playwrite/"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritedegrund/PlaywriteDEGrund%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteDEGrund\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteDEGrund[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
