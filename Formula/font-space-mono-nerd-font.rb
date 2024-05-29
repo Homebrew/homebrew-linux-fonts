78afb9d4075fc47ec1f96d0aeda90f8694b3ce61
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-space-mono-nerd-font updated

--- Formula/font-space-mono-nerd-font.rb
+++ Formula/font-space-mono-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontSpaceMonoNerdFont < Formula
-  version "3.2.1"
-  sha256 "44f9138a42bc4c04007cf9230ebc55d790fbb2a40b9f88047aebc2b8ff4aa253"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/SpaceMono.zip"
-  desc "SpaceMono Nerd Font (Space Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/SpaceMono.zip"
+  version "3.2.1"
+  sha256 "44f9138a42bc4c04007cf9230ebc55d790fbb2a40b9f88047aebc2b8ff4aa253"
+
   def install
-    (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFontPropo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFont-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFont-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFont-Regular.ttf")[0]
@@ -17,10 +17,10 @@ class FontSpaceMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFontPropo-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFontPropo-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFont-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
