d2adb8d26ba8416db071cb64f68e323828161fca
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-single-day updated

--- Formula/font-single-day.rb
+++ Formula/font-single-day.rb
@@ -1,12 +1,14 @@
 class FontSingleDay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/singleday/SingleDay-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Single Day"
+  desc "Single day font"
   homepage "https://fonts.google.com/specimen/Single+Day"
+  head "https://github.com/google/fonts/raw/main/ofl/singleday/SingleDay-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SingleDay-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
