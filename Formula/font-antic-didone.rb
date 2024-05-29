295469318ae9251cff8c94a0ffab4472f42ad7d1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-antic-didone updated

--- Formula/font-antic-didone.rb
+++ Formula/font-antic-didone.rb
@@ -1,12 +1,14 @@
 class FontAnticDidone < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anticdidone/AnticDidone-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Antic Didone"
+  desc "Antic didone font"
   homepage "https://fonts.google.com/specimen/Antic+Didone"
+  head "https://github.com/google/fonts/raw/main/ofl/anticdidone/AnticDidone-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AnticDidone-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
