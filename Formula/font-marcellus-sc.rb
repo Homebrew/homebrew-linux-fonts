4801ac9692c34073cb216198e64482d644fa7072
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:37 +0000

    font-marcellus-sc updated

--- Formula/font-marcellus-sc.rb
+++ Formula/font-marcellus-sc.rb
@@ -1,12 +1,14 @@
 class FontMarcellusSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/marcellussc/MarcellusSC-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Marcellus SC"
+  desc "Marcellus sc font"
   homepage "https://fonts.google.com/specimen/Marcellus+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/marcellussc/MarcellusSC-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MarcellusSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
