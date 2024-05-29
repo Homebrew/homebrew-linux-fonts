6630fd2a79f770709301ded525fbfb51121fe944
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:21 +0000

    font-inspiration updated

--- Formula/font-inspiration.rb
+++ Formula/font-inspiration.rb
@@ -1,12 +1,14 @@
 class FontInspiration < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/inspiration/Inspiration-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Inspiration"
+  desc "Inspiration font"
   homepage "https://fonts.google.com/specimen/Inspiration"
+  head "https://github.com/google/fonts/raw/main/ofl/inspiration/Inspiration-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Inspiration-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
