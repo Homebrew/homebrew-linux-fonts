487cd81baac0bc21cc6fe0d2880a7a16b7725caa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-sans-anatolian-hieroglyphs updated

--- Formula/font-noto-sans-anatolian-hieroglyphs.rb
+++ Formula/font-noto-sans-anatolian-hieroglyphs.rb
@@ -1,12 +1,14 @@
 class FontNotoSansAnatolianHieroglyphs < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAnatolianHieroglyphs-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Anatolian Hieroglyphs"
+  desc "Noto sans anatolian hieroglyphs font"
   homepage "https://www.google.com/get/noto/#sans-hluw"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAnatolianHieroglyphs-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansAnatolianHieroglyphs-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
