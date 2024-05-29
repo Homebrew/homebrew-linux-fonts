c1a3519e2e2d1bfb1ca333657bdbda642972b563
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-sarina updated

--- Formula/font-sarina.rb
+++ Formula/font-sarina.rb
@@ -1,12 +1,14 @@
 class FontSarina < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sarina/Sarina-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sarina"
+  desc "Sarina font"
   homepage "https://fonts.google.com/specimen/Sarina"
+  head "https://github.com/google/fonts/raw/main/ofl/sarina/Sarina-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Sarina-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
