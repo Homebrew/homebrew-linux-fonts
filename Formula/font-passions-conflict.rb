9f0e3816c3b1e906ea4ef09fb995ed1d5fd23cde
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-passions-conflict updated

--- Formula/font-passions-conflict.rb
+++ Formula/font-passions-conflict.rb
@@ -1,12 +1,14 @@
 class FontPassionsConflict < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/passionsconflict/PassionsConflict-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Passions Conflict"
+  desc "Passions conflict font"
   homepage "https://fonts.google.com/specimen/Passions+Conflict"
+  head "https://github.com/google/fonts/raw/main/ofl/passionsconflict/PassionsConflict-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PassionsConflict-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
