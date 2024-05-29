84e8dbd29525967a6ed79f6bde04822a071a6627
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-sanchez updated

--- Formula/font-sanchez.rb
+++ Formula/font-sanchez.rb
@@ -1,13 +1,17 @@
 class FontSanchez < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sanchez"
-  desc "Sanchez"
+  desc "Sanchez font"
   homepage "https://fonts.google.com/specimen/Sanchez"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sanchez"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sanchez/./**/Sanchez-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sanchez/./**/Sanchez-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
