b617a153a5563dad407b41f87e24df5ff141340e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:50 +0000

    font-noto-sans-gujarati-ui updated

--- Formula/font-noto-sans-gujarati-ui.rb
+++ Formula/font-noto-sans-gujarati-ui.rb
@@ -1,9 +1,13 @@
 class FontNotoSansGujaratiUi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosansgujaratiui"
-  desc "Noto Sans Gujarati UI"
+  desc "Noto sans gujarati ui font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Gujarati"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/notosansgujaratiui"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-ExtraLight.ttf")[0]
@@ -11,10 +15,10 @@ class FontNotoSansGujaratiUi < Formula
     (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansgujaratiui/./**/NotoSansGujaratiUI-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
