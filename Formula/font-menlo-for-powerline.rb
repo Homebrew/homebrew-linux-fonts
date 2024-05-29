410521808782053dac7f20cc1a1a4545a96d0bfa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-menlo-for-powerline updated

--- Formula/font-menlo-for-powerline.rb
+++ Formula/font-menlo-for-powerline.rb
@@ -1,15 +1,16 @@
 class FontMenloForPowerline < Formula
-  head "https://github.com/abertsch/Menlo-for-Powerline/archive/master.zip"
-  desc "Menlo for Powerline"
+  desc "Menlo for powerline font"
   homepage "https://github.com/abertsch/Menlo-for-Powerline"
+  head "https://github.com/abertsch/Menlo-for-Powerline/archive/master.zip"
+
   def install
-    (share/"fonts").install Dir.glob("Menlo-for-Powerline-master/**/Menlo for Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("Menlo-for-Powerline-master/**/Menlo Bold for Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("Menlo-for-Powerline-master/**/Menlo Italic for Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("Menlo-for-Powerline-master/**/Menlo Bold Italic for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Menlo-for-Powerline-master/Menlo for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Menlo-for-Powerline-master/Menlo Bold for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Menlo-for-Powerline-master/Menlo Italic for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Menlo-for-Powerline-master/Menlo Bold Italic for Powerline.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
