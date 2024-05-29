962941915a2c106a848ff5c041f6785cef2901aa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:50 +0000

    font-noto-sans-gothic updated

--- Formula/font-noto-sans-gothic.rb
+++ Formula/font-noto-sans-gothic.rb
@@ -1,12 +1,14 @@
 class FontNotoSansGothic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGothic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Gothic"
+  desc "Noto sans gothic font"
   homepage "https://www.google.com/get/noto/#sans-goth"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGothic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansGothic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
