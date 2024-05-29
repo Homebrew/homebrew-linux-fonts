574b646190b9f6f8f6377f2a8fc0aa6f14ea019e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:27 +0000

    font-kaisei-harunoumi updated

--- Formula/font-kaisei-harunoumi.rb
+++ Formula/font-kaisei-harunoumi.rb
@@ -1,14 +1,18 @@
 class FontKaiseiHarunoumi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kaiseiharunoumi"
-  desc "Kaisei HarunoUmi"
+  desc "Kaisei harunoumi font"
   homepage "https://fonts.google.com/specimen/Kaisei+HarunoUmi"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kaiseiharunoumi"
+
   def install
     (share/"fonts").install Dir.glob("ofl/kaiseiharunoumi/./**/KaiseiHarunoUmi-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kaiseiharunoumi/./**/KaiseiHarunoUmi-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kaiseiharunoumi/./**/KaiseiHarunoUmi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
