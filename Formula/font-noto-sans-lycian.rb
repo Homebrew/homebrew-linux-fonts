655e2fde8d5c86f8d5d579c7cf06048f50d9d171
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-lycian updated

--- Formula/font-noto-sans-lycian.rb
+++ Formula/font-noto-sans-lycian.rb
@@ -1,12 +1,14 @@
 class FontNotoSansLycian < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLycian-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Lycian"
+  desc "Noto sans lycian font"
   homepage "https://www.google.com/get/noto/#sans-lyci"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLycian-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansLycian-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
