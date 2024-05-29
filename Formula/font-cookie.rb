7b5b5ef3602cdd57ff479a79bebcbe0373eb0d2e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-cookie updated

--- Formula/font-cookie.rb
+++ Formula/font-cookie.rb
@@ -1,12 +1,14 @@
 class FontCookie < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/cookie/Cookie-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Cookie"
+  desc "Cookie font"
   homepage "https://fonts.google.com/specimen/Cookie"
+  head "https://github.com/google/fonts/raw/main/ofl/cookie/Cookie-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Cookie-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
