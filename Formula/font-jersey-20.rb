62d73c0377aa8f8970d122305691493f0db0b3f9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jersey-20 updated

--- Formula/font-jersey-20.rb
+++ Formula/font-jersey-20.rb
@@ -1,12 +1,14 @@
 class FontJersey20 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jersey20/Jersey20-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jersey 20"
+  desc "Jersey 20 font"
   homepage "https://fonts.google.com/specimen/Jersey+20"
+  head "https://github.com/google/fonts/raw/main/ofl/jersey20/Jersey20-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Jersey20-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
