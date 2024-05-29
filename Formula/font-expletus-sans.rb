b8fbf158f85dcd5cb2926eb0adba4362435b16a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:05 +0000

    font-expletus-sans updated

--- Formula/font-expletus-sans.rb
+++ Formula/font-expletus-sans.rb
@@ -1,13 +1,17 @@
 class FontExpletusSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/expletussans"
-  desc "Expletus Sans"
+  desc "Expletus sans font"
   homepage "https://fonts.google.com/specimen/Expletus+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/expletussans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/expletussans/./**/ExpletusSans-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/expletussans/./**/ExpletusSans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/expletussans/./**/ExpletusSans-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/expletussans/./**/ExpletusSans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
