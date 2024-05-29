a3739d690fb3adaa7f1486e3ba4446c6260cf178
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-bigblue-terminal-nerd-font updated

--- Formula/font-bigblue-terminal-nerd-font.rb
+++ Formula/font-bigblue-terminal-nerd-font.rb
@@ -1,10 +1,10 @@
 class FontBigblueTerminalNerdFont < Formula
-  version "3.2.1"
-  sha256 "abc64063c7d13d87a3cfacc91912e3f647bb8b0fbae38a62b23d86548dc72b0a"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BigBlueTerminal.zip"
-  desc "BigBlueTerm Nerd Font families (BigBlue Terminal)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/BigBlueTerminal.zip"
+  version "3.2.1"
+  sha256 "abc64063c7d13d87a3cfacc91912e3f647bb8b0fbae38a62b23d86548dc72b0a"
+
   def install
     (share/"fonts").install Dir.glob("./**/BigBlueTerm437NerdFont-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BigBlueTerm437NerdFontMono-Regular.ttf")[0]
@@ -13,8 +13,8 @@ class FontBigblueTerminalNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/BigBlueTermPlusNerdFontMono-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BigBlueTermPlusNerdFontPropo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
