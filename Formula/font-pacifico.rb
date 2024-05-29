275f0046635110127fc954a30ef11488c777acd6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:04 +0000

    font-pacifico updated

--- Formula/font-pacifico.rb
+++ Formula/font-pacifico.rb
@@ -1,12 +1,14 @@
 class FontPacifico < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/pacifico/Pacifico-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Pacifico"
+  desc "Pacifico font"
   homepage "https://fonts.google.com/specimen/Pacifico"
+  head "https://github.com/google/fonts/raw/main/ofl/pacifico/Pacifico-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Pacifico-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
