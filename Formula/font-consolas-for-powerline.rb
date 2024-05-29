855f6e6c0a0c81abb7175153e817b8f235c1e1b2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-consolas-for-powerline updated

--- Formula/font-consolas-for-powerline.rb
+++ Formula/font-consolas-for-powerline.rb
@@ -1,15 +1,16 @@
 class FontConsolasForPowerline < Formula
-  head "https://github.com/eugeii/consolas-powerline-vim/archive/master.zip"
-  desc "Consolas for Powerline"
+  desc "Consolas for powerline font"
   homepage "https://github.com/eugeii/consolas-powerline-vim"
+  head "https://github.com/eugeii/consolas-powerline-vim/archive/master.zip"
+
   def install
-    (share/"fonts").install Dir.glob("consolas-powerline-vim-master/**/CONSOLA-Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("consolas-powerline-vim-master/**/CONSOLAB-Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("consolas-powerline-vim-master/**/CONSOLAI-Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("consolas-powerline-vim-master/**/CONSOLAZ-Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/consolas-powerline-vim-master/CONSOLA-Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/consolas-powerline-vim-master/CONSOLAB-Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/consolas-powerline-vim-master/CONSOLAI-Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/consolas-powerline-vim-master/CONSOLAZ-Powerline.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
