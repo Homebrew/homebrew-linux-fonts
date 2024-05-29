26a23f11ac217e99cddd257ee373dff2f7e6b686
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-aref-ruqaa-ink updated

--- Formula/font-aref-ruqaa-ink.rb
+++ Formula/font-aref-ruqaa-ink.rb
@@ -1,14 +1,17 @@
 class FontArefRuqaaInk < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/arefruqaaink"
-  desc "Aref Ruqaa Ink"
   desc "Led by khaled hosny, a type designer based in cairo, egypt"
   homepage "https://fonts.google.com/specimen/Aref+Ruqaa+Ink"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/arefruqaaink"
+
   def install
     (share/"fonts").install Dir.glob("ofl/arefruqaaink/./**/ArefRuqaaInk-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/arefruqaaink/./**/ArefRuqaaInk-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
