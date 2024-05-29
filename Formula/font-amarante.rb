980fc83b45b7622d53d26fdea7ba9e72d8e9756b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-amarante updated

--- Formula/font-amarante.rb
+++ Formula/font-amarante.rb
@@ -1,12 +1,14 @@
 class FontAmarante < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/amarante/Amarante-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Amarante"
+  desc "Amarante font"
   homepage "https://fonts.google.com/specimen/Amarante"
+  head "https://github.com/google/fonts/raw/main/ofl/amarante/Amarante-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Amarante-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
