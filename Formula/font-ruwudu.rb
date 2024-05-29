ef42724c68ff6ba83bd5de87ec49854781c23a30
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:18 +0000

    font-ruwudu updated

--- Formula/font-ruwudu.rb
+++ Formula/font-ruwudu.rb
@@ -1,16 +1,19 @@
 class FontRuwudu < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ruwudu"
-  desc "Ruwudu"
   desc "Open font family for arabic script languages that use the rubutun kano style"
   homepage "https://fonts.google.com/specimen/Ruwudu"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ruwudu"
+
   def install
     (share/"fonts").install Dir.glob("ofl/ruwudu/./**/Ruwudu-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ruwudu/./**/Ruwudu-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ruwudu/./**/Ruwudu-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ruwudu/./**/Ruwudu-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
