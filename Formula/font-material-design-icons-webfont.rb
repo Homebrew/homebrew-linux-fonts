e7c4d749c92b865b8e7997dadc9381d32e61de51
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-material-design-icons-webfont updated

--- Formula/font-material-design-icons-webfont.rb
+++ Formula/font-material-design-icons-webfont.rb
@@ -1,12 +1,16 @@
 class FontMaterialDesignIconsWebfont < Formula
-  head "https://github.com/templarian/materialdesign-webfont.git", verified: "github.com/templarian/materialdesign-webfont", branch: "master", only_path: "fonts"
-  desc "Material Design Icons Webfont"
+  desc "Material design icons webfont font"
   homepage "https://materialdesignicons.com/"
+  head "https://github.com/templarian/materialdesign-webfont.git",
+       verified:  "github.com/templarian/materialdesign-webfont",
+       branch:    "master",
+       only_path: "fonts"
+
   def install
     (share/"fonts").install Dir.glob("fonts/./**/materialdesignicons-webfont.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
