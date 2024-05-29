ea09fc338c5690c566b6a8a15db8880d51c27f1b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-neuton updated

--- Formula/font-neuton.rb
+++ Formula/font-neuton.rb
@@ -1,7 +1,11 @@
 class FontNeuton < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/neuton"
-  desc "Neuton"
+  desc "Neuton font"
   homepage "https://fonts.google.com/specimen/Neuton"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/neuton"
+
   def install
     (share/"fonts").install Dir.glob("ofl/neuton/./**/Neuton-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/neuton/./**/Neuton-ExtraBold.ttf")[0]
@@ -10,8 +14,8 @@ class FontNeuton < Formula
     (share/"fonts").install Dir.glob("ofl/neuton/./**/Neuton-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/neuton/./**/Neuton-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
