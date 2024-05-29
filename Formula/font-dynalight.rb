33c6b065a58a0b0aa059d74fbf61838ad7f7c759
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-dynalight updated

--- Formula/font-dynalight.rb
+++ Formula/font-dynalight.rb
@@ -1,12 +1,14 @@
 class FontDynalight < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/dynalight/Dynalight-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Dynalight"
+  desc "Dynalight font"
   homepage "https://fonts.google.com/specimen/Dynalight"
+  head "https://github.com/google/fonts/raw/main/ofl/dynalight/Dynalight-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Dynalight-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
