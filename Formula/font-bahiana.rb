347051bfd35425bc9144c3d2482a6e27d414588c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-bahiana updated

--- Formula/font-bahiana.rb
+++ Formula/font-bahiana.rb
@@ -1,12 +1,14 @@
 class FontBahiana < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bahiana/Bahiana-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bahiana"
+  desc "Bahiana font"
   homepage "https://fonts.google.com/specimen/Bahiana"
+  head "https://github.com/google/fonts/raw/main/ofl/bahiana/Bahiana-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Bahiana-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
