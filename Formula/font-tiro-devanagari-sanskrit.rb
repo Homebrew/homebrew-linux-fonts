88e28d810411ba8c7f4a76c50f5bbd7e54e372c8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-tiro-devanagari-sanskrit updated

--- Formula/font-tiro-devanagari-sanskrit.rb
+++ Formula/font-tiro-devanagari-sanskrit.rb
@@ -1,14 +1,17 @@
 class FontTiroDevanagariSanskrit < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tirodevanagarisanskrit"
-  desc "Tiro Devanagari Sanskrit"
   desc "Broader proportions, generous counters, and strong diagonal strokes"
   homepage "https://fonts.google.com/specimen/Tiro+Devanagari+Sanskrit"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tirodevanagarisanskrit"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tirodevanagarisanskrit/./**/TiroDevanagariSanskrit-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tirodevanagarisanskrit/./**/TiroDevanagariSanskrit-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
