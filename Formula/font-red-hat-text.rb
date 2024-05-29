59db290fb4c19ff44771a87f0e796c08beb173c8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-red-hat-text updated

--- Formula/font-red-hat-text.rb
+++ Formula/font-red-hat-text.rb
@@ -1,13 +1,17 @@
 class FontRedHatText < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/redhattext"
-  desc "Red Hat Text"
+  desc "Red hat text font"
   homepage "https://fonts.google.com/specimen/Red+Hat+Text"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/redhattext"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/redhattext/./**/RedHatText-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/redhattext/./**/RedHatText\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/redhattext/./**/RedHatText-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/redhattext/./**/RedHatText[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
