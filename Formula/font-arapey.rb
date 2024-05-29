f26f908c2550d145bc7a781704aabc52d5f362fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-arapey updated

--- Formula/font-arapey.rb
+++ Formula/font-arapey.rb
@@ -1,13 +1,17 @@
 class FontArapey < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/arapey"
-  desc "Arapey"
+  desc "Arapey font"
   homepage "https://fonts.google.com/specimen/Arapey"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/arapey"
+
   def install
     (share/"fonts").install Dir.glob("ofl/arapey/./**/Arapey-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/arapey/./**/Arapey-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
