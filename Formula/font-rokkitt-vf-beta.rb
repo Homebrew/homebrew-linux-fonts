980c2e16849600a0d4ecf0b32434cd4978e68f83
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-rokkitt-vf-beta updated

--- Formula/font-rokkitt-vf-beta.rb
+++ Formula/font-rokkitt-vf-beta.rb
@@ -1,12 +1,14 @@
 class FontRokkittVfBeta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rokkittvfbeta/RokkittVFBeta.ttf", verified: "github.com/google/fonts/"
-  desc "Rokkitt VF Beta"
+  desc "Rokkitt vf beta font"
   homepage "https://fonts.google.com/specimen/Rokkitt+VF+Beta"
+  head "https://github.com/google/fonts/raw/main/ofl/rokkittvfbeta/RokkittVFBeta.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RokkittVFBeta.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
