e7ee2d7253ba6de6564799940c451aabe2ec00b9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:45 +0000

    font-bangers updated

--- Formula/font-bangers.rb
+++ Formula/font-bangers.rb
@@ -1,12 +1,14 @@
 class FontBangers < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bangers/Bangers-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bangers"
+  desc "Bangers font"
   homepage "https://fonts.google.com/specimen/Bangers"
+  head "https://github.com/google/fonts/raw/main/ofl/bangers/Bangers-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Bangers-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
