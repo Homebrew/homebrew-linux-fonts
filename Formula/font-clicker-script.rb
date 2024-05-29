7381bf88d7ef21696b656379013d42ba31c2c509
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-clicker-script updated

--- Formula/font-clicker-script.rb
+++ Formula/font-clicker-script.rb
@@ -1,12 +1,14 @@
 class FontClickerScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/clickerscript/ClickerScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Clicker Script"
+  desc "Clicker script font"
   homepage "https://fonts.google.com/specimen/Clicker+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/clickerscript/ClickerScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ClickerScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
