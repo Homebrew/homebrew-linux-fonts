881d0bcf5e56595178db785b7fa9cba9bf85c120
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-kimberella updated

--- Formula/font-kimberella.rb
+++ Formula/font-kimberella.rb
@@ -1,13 +1,13 @@
 class FontKimberella < Formula
-  head "https://moji-waku.com/download/kimberella.zip"
-  desc "Kimberella"
   desc "Ancient biology styled font inspired by kimberella"
   homepage "https://moji-waku.com/kimberella/index.html"
+  head "https://moji-waku.com/download/kimberella.zip"
+
   def install
-    (share/"fonts").install Dir.glob("kimberella/**/Kimberella-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/kimberella/Kimberella-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
