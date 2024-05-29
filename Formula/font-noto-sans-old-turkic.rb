609fa76d20509d6c91af88f8b4dcd916f588bf78
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-old-turkic updated

--- Formula/font-noto-sans-old-turkic.rb
+++ Formula/font-noto-sans-old-turkic.rb
@@ -1,12 +1,14 @@
 class FontNotoSansOldTurkic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldTurkic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Old Turkic"
+  desc "Noto sans old turkic font"
   homepage "https://www.google.com/get/noto/#sans-orkh"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldTurkic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansOldTurkic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
