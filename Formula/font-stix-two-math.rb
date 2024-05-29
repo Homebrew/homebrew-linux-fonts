402e10c7159cbd4434ca9bc1848158af42a5cf08
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-stix-two-math updated

--- Formula/font-stix-two-math.rb
+++ Formula/font-stix-two-math.rb
@@ -1,12 +1,14 @@
 class FontStixTwoMath < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/stixtwomath/STIXTwoMath-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "STIX Two Math"
+  desc "Stix two math font"
   homepage "https://fonts.google.com/specimen/STIX+Two+Math"
+  head "https://github.com/google/fonts/raw/main/ofl/stixtwomath/STIXTwoMath-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/STIXTwoMath-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
