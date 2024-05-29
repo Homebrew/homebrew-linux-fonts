35879af404d034834d6eaa788d1da9ae75f6f490
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-adlam-display updated

--- Formula/font-adlam-display.rb
+++ Formula/font-adlam-display.rb
@@ -1,12 +1,14 @@
 class FontAdlamDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/adlamdisplay/ADLaMDisplay-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "ADLaM Display"
+  desc "Adlam display font"
   homepage "https://fonts.google.com/specimen/ADLaM+Display"
+  head "https://github.com/google/fonts/raw/main/ofl/adlamdisplay/ADLaMDisplay-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ADLaMDisplay-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
