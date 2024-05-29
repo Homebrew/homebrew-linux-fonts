d71a6cf7e38a9600e8fa43e87ad1784864aef004
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-black-and-white-picture updated

--- Formula/font-black-and-white-picture.rb
+++ Formula/font-black-and-white-picture.rb
@@ -1,12 +1,14 @@
 class FontBlackAndWhitePicture < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/blackandwhitepicture/BlackAndWhitePicture-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Black And White Picture"
+  desc "Black and white picture font"
   homepage "https://fonts.google.com/specimen/Black+And+White+Picture"
+  head "https://github.com/google/fonts/raw/main/ofl/blackandwhitepicture/BlackAndWhitePicture-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BlackAndWhitePicture-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
