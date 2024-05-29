8337982304e48f38dbd0a2ff41ca3fefbade4646
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:16 +0000

    font-harmattan updated

--- Formula/font-harmattan.rb
+++ Formula/font-harmattan.rb
@@ -1,15 +1,19 @@
 class FontHarmattan < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/harmattan"
-  desc "Harmattan"
+  desc "Harmattan font"
   homepage "https://fonts.google.com/specimen/Harmattan"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/harmattan"
+
   def install
     (share/"fonts").install Dir.glob("ofl/harmattan/./**/Harmattan-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/harmattan/./**/Harmattan-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/harmattan/./**/Harmattan-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/harmattan/./**/Harmattan-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
