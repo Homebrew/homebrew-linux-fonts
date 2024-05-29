c545a11dd683f69d22033d6cf48cbfee776f9203
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:54 +0000

    font-noto-sans-old-north-arabian updated

--- Formula/font-noto-sans-old-north-arabian.rb
+++ Formula/font-noto-sans-old-north-arabian.rb
@@ -1,12 +1,14 @@
 class FontNotoSansOldNorthArabian < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansoldnortharabian/NotoSansOldNorthArabian-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Old North Arabian"
+  desc "Noto sans old north arabian font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Old+North+Arabian"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansoldnortharabian/NotoSansOldNorthArabian-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansOldNorthArabian-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
