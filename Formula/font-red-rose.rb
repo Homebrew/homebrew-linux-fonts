4b65b3f4c89c69f4d3ecfd28eb843ae67780b849
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-red-rose updated

--- Formula/font-red-rose.rb
+++ Formula/font-red-rose.rb
@@ -1,13 +1,14 @@
 class FontRedRose < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/redrose/RedRose%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Red Rose"
   desc "Latin display typeface designed for posters"
   homepage "https://fonts.google.com/specimen/Red+Rose"
+  head "https://github.com/google/fonts/raw/main/ofl/redrose/RedRose%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/RedRose\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/RedRose[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
