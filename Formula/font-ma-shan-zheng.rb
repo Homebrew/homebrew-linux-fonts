2c7e147dbd7841b2a8cb0f35e0cf0f1acf475a5d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:36 +0000

    font-ma-shan-zheng updated

--- Formula/font-ma-shan-zheng.rb
+++ Formula/font-ma-shan-zheng.rb
@@ -1,12 +1,14 @@
 class FontMaShanZheng < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mashanzheng/MaShanZheng-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ma Shan Zheng"
+  desc "Ma shan zheng font"
   homepage "https://fonts.google.com/specimen/Ma+Shan+Zheng"
+  head "https://github.com/google/fonts/raw/main/ofl/mashanzheng/MaShanZheng-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MaShanZheng-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
