3fb8993093b309b748355fd35bbc35d81719510b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-glitch updated

--- Formula/font-rubik-glitch.rb
+++ Formula/font-rubik-glitch.rb
@@ -1,12 +1,14 @@
 class FontRubikGlitch < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikglitch/RubikGlitch-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Glitch"
+  desc "Rubik glitch font"
   homepage "https://fonts.google.com/specimen/Rubik+Glitch"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikglitch/RubikGlitch-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikGlitch-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
