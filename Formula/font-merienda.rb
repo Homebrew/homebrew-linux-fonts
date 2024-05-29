6fec67ec1c2ec041e7bd9d580a2cc95e7872a718
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-merienda updated

--- Formula/font-merienda.rb
+++ Formula/font-merienda.rb
@@ -1,12 +1,14 @@
 class FontMerienda < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/merienda/Merienda%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Merienda"
+  desc "Merienda font"
   homepage "https://fonts.google.com/specimen/Merienda"
+  head "https://github.com/google/fonts/raw/main/ofl/merienda/Merienda%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Merienda\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Merienda[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
