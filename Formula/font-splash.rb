cd8c2f22924e686121dec2bf70ccbb94d8788f22
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-splash updated

--- Formula/font-splash.rb
+++ Formula/font-splash.rb
@@ -1,12 +1,14 @@
 class FontSplash < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/splash/Splash-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Splash"
+  desc "Splash font"
   homepage "https://fonts.google.com/specimen/Splash"
+  head "https://github.com/google/fonts/raw/main/ofl/splash/Splash-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Splash-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
