030712412382791077bf72ca6bf4c850d5233260
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-commissioner updated

--- Formula/font-commissioner.rb
+++ Formula/font-commissioner.rb
@@ -1,13 +1,14 @@
 class FontCommissioner < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/commissioner/Commissioner%5BFLAR%2CVOLM%2Cslnt%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Commissioner"
   desc "Low-contrast humanist sans-serif font with almost classical proportions"
   homepage "https://fonts.google.com/specimen/Commissioner"
+  head "https://github.com/google/fonts/raw/main/ofl/commissioner/Commissioner%5BFLAR%2CVOLM%2Cslnt%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Commissioner\\[FLAR,VOLM,slnt,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Commissioner[FLAR,VOLM,slnt,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
