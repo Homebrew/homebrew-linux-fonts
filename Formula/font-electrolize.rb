2822d9df6edc3c03f3addfd012e58c98cb5312fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-electrolize updated

--- Formula/font-electrolize.rb
+++ Formula/font-electrolize.rb
@@ -1,12 +1,14 @@
 class FontElectrolize < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/electrolize/Electrolize-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Electrolize"
+  desc "Electrolize font"
   homepage "https://fonts.google.com/specimen/Electrolize"
+  head "https://github.com/google/fonts/raw/main/ofl/electrolize/Electrolize-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Electrolize-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
