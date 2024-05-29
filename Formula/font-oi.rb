7bacc9e7be492cc989db9c7161e200e9ce36f47f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-oi updated

--- Formula/font-oi.rb
+++ Formula/font-oi.rb
@@ -1,13 +1,14 @@
 class FontOi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/oi/Oi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Oi"
   desc "Interjection used in various languages"
   homepage "https://fonts.google.com/specimen/Oi"
+  head "https://github.com/google/fonts/raw/main/ofl/oi/Oi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Oi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
