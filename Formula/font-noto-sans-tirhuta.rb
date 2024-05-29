4bf451c18bb5a741f66e9a50798b633e5ac5d687
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:58 +0000

    font-noto-sans-tirhuta updated

--- Formula/font-noto-sans-tirhuta.rb
+++ Formula/font-noto-sans-tirhuta.rb
@@ -1,12 +1,14 @@
 class FontNotoSansTirhuta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanstirhuta/NotoSansTirhuta-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Tirhuta"
+  desc "Noto sans tirhuta font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Tirhuta"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanstirhuta/NotoSansTirhuta-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansTirhuta-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
