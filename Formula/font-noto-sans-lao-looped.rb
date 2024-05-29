84eb85e21a53a9db37a7dd4908ab818d2722f9e8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:51 +0000

    font-noto-sans-lao-looped updated

--- Formula/font-noto-sans-lao-looped.rb
+++ Formula/font-noto-sans-lao-looped.rb
@@ -1,13 +1,14 @@
 class FontNotoSansLaoLooped < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanslaolooped/NotoSansLaoLooped%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Lao Looped"
   desc "Looped variant of the southeast asian lao script"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Lao+Looped"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanslaolooped/NotoSansLaoLooped%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansLaoLooped\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansLaoLooped[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
