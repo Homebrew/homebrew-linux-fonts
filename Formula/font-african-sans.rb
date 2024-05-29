e042a6b73778ab2233b124bc51db56dd228c5686
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-african-sans updated

--- Formula/font-african-sans.rb
+++ Formula/font-african-sans.rb
@@ -1,16 +1,18 @@
 class FontAfricanSans < Formula
-  version "9.380"
-  url "https://www.languagegeek.com/font/AfricanSans.zip"
-  desc "African Sans"
+  desc "African sans font"
   homepage "https://www.languagegeek.com/font/fontdownload.html"
+  url "https://www.languagegeek.com/font/AfricanSans.zip"
+  version "9.380"
+  sha256 :no_check
+
   def install
     (share/"fonts").install Dir.glob("./**/African Sans REGULAR 938.ttf")[0]
     (share/"fonts").install Dir.glob("./**/African Sans BOLD 938.ttf")[0]
     (share/"fonts").install Dir.glob("./**/African Sans BOLD ITALIC 938.ttf")[0]
     (share/"fonts").install Dir.glob("./**/African Sans ITALIC 938.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
