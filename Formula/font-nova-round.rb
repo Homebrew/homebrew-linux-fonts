3ce9d0a37022368e554f0fe21eedec0e269bd496
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-nova-round updated

--- Formula/font-nova-round.rb
+++ Formula/font-nova-round.rb
@@ -1,12 +1,14 @@
 class FontNovaRound < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/novaround/NovaRound.ttf", verified: "github.com/google/fonts/"
-  desc "Nova Round"
+  desc "Nova round font"
   homepage "https://fonts.google.com/specimen/Nova+Round"
+  head "https://github.com/google/fonts/raw/main/ofl/novaround/NovaRound.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NovaRound.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
