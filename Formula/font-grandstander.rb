b00529314757227ddcec6682250bae6829c15e32
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-grandstander updated

--- Formula/font-grandstander.rb
+++ Formula/font-grandstander.rb
@@ -1,14 +1,17 @@
 class FontGrandstander < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/grandstander"
-  desc "Grandstander"
   desc "Display variable font with a weight axis"
   homepage "https://fonts.google.com/specimen/Grandstander"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/grandstander"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/grandstander/./**/Grandstander-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/grandstander/./**/Grandstander\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/grandstander/./**/Grandstander-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/grandstander/./**/Grandstander[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
