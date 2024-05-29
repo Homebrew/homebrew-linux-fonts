1e9a407b47977810b6864b0574cc81019a92fff9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-mono-for-powerline updated

--- Formula/font-noto-mono-for-powerline.rb
+++ Formula/font-noto-mono-for-powerline.rb
@@ -1,12 +1,15 @@
 class FontNotoMonoForPowerline < Formula
-  head "https://github.com/powerline/fonts.git", branch: "master", only_path: "NotoMono"
-  desc "Noto Mono for Powerline"
+  desc "Noto mono for powerline font"
   homepage "https://github.com/powerline/fonts/tree/master/NotoMono"
+  head "https://github.com/powerline/fonts.git",
+       branch:    "master",
+       only_path: "NotoMono"
+
   def install
     (share/"fonts").install Dir.glob("NotoMono/./**/Noto Mono for Powerline.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
