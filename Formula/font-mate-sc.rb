4f399df1b21d3e33a7c07e50ce9762ca37daa0b4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-mate-sc updated

--- Formula/font-mate-sc.rb
+++ Formula/font-mate-sc.rb
@@ -1,12 +1,14 @@
 class FontMateSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/matesc/MateSC-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mate SC"
+  desc "Mate sc font"
   homepage "https://fonts.google.com/specimen/Mate+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/matesc/MateSC-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MateSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
