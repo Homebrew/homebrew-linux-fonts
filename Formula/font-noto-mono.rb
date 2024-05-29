76c09bab83350f6d47b5e330bb1ac8589b5ec54f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-mono updated

--- Formula/font-noto-mono.rb
+++ Formula/font-noto-mono.rb
@@ -1,12 +1,14 @@
 class FontNotoMono < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoMono-hinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Mono"
+  desc "Noto mono font"
   homepage "https://www.google.com/get/noto/#mono-mono"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoMono-hinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
