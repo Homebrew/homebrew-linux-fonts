93e48d9807a29a94ca0fbdcb24c9f6f369f638f5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-gemunu-libre updated

--- Formula/font-gemunu-libre.rb
+++ Formula/font-gemunu-libre.rb
@@ -1,12 +1,14 @@
 class FontGemunuLibre < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gemunulibre/GemunuLibre%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Gemunu Libre"
+  desc "Gemunu libre font"
   homepage "https://fonts.google.com/specimen/Gemunu+Libre"
+  head "https://github.com/google/fonts/raw/main/ofl/gemunulibre/GemunuLibre%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/GemunuLibre\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GemunuLibre[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
