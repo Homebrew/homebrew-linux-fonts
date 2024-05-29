a19fef1c22a46d2f4041bb578e7d0ac9bb454653
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jersey-20-charted updated

--- Formula/font-jersey-20-charted.rb
+++ Formula/font-jersey-20-charted.rb
@@ -1,12 +1,14 @@
 class FontJersey20Charted < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jersey20charted/Jersey20Charted-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jersey 20 Charted"
+  desc "Jersey 20 charted font"
   homepage "https://fonts.google.com/specimen/Jersey+20+Charted"
+  head "https://github.com/google/fonts/raw/main/ofl/jersey20charted/Jersey20Charted-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Jersey20Charted-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
