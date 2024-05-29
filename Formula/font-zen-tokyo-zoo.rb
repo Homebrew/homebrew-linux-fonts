d8b20d1100393d84c35b51d63442ea924231120b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:38 +0000

    font-zen-tokyo-zoo updated

--- Formula/font-zen-tokyo-zoo.rb
+++ Formula/font-zen-tokyo-zoo.rb
@@ -1,12 +1,14 @@
 class FontZenTokyoZoo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/zentokyozoo/ZenTokyoZoo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Zen Tokyo Zoo"
+  desc "Zen tokyo zoo font"
   homepage "https://fonts.google.com/specimen/Zen+Tokyo+Zoo"
+  head "https://github.com/google/fonts/raw/main/ofl/zentokyozoo/ZenTokyoZoo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ZenTokyoZoo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
