7830f19c67fd6943ee43c94cdc1747ada7da306d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-tektur updated

--- Formula/font-tektur.rb
+++ Formula/font-tektur.rb
@@ -1,13 +1,14 @@
 class FontTektur < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tektur/Tektur%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Tektur"
   desc "Set high allowing for compact typesetting"
   homepage "https://fonts.google.com/specimen/Tektur"
+  head "https://github.com/google/fonts/raw/main/ofl/tektur/Tektur%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Tektur\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Tektur[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
