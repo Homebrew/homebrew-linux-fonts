e42adc55e8441dda2fc05b598282b314911fe853
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:52 +0000

    font-castoro updated

--- Formula/font-castoro.rb
+++ Formula/font-castoro.rb
@@ -1,14 +1,17 @@
 class FontCastoro < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/castoro"
-  desc "Castoro"
   desc "Named for the north american beaver, castor canadensis"
   homepage "https://fonts.google.com/specimen/Castoro"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/castoro"
+
   def install
     (share/"fonts").install Dir.glob("ofl/castoro/./**/Castoro-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/castoro/./**/Castoro-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
