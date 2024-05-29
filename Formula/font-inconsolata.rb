fbb976cfac6a7ff7d06202a5c446043bd8debba3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:20 +0000

    font-inconsolata updated

--- Formula/font-inconsolata.rb
+++ Formula/font-inconsolata.rb
@@ -1,12 +1,14 @@
 class FontInconsolata < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/inconsolata/Inconsolata%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Inconsolata"
+  desc "Inconsolata font"
   homepage "https://fonts.google.com/specimen/Inconsolata"
+  head "https://github.com/google/fonts/raw/main/ofl/inconsolata/Inconsolata%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Inconsolata\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Inconsolata[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
