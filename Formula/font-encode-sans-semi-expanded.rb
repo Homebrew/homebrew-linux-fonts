240f739e35fccff44039ec495f0846da746cbc87
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-encode-sans-semi-expanded updated

--- Formula/font-encode-sans-semi-expanded.rb
+++ Formula/font-encode-sans-semi-expanded.rb
@@ -1,9 +1,13 @@
 class FontEncodeSansSemiExpanded < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/encodesanssemiexpanded"
-  desc "Encode Sans Semi Expanded"
+  desc "Encode sans semi expanded font"
   homepage "https://fonts.google.com/specimen/Encode+Sans+Semi+Expanded"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/encodesanssemiexpanded"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-ExtraLight.ttf")[0]
@@ -11,10 +15,10 @@ class FontEncodeSansSemiExpanded < Formula
     (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/encodesanssemiexpanded/./**/EncodeSansSemiExpanded-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
