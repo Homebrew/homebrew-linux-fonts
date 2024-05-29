3e97c1d60902c2cd723fce690b93e41552cc43f6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-antic-slab updated

--- Formula/font-antic-slab.rb
+++ Formula/font-antic-slab.rb
@@ -1,12 +1,14 @@
 class FontAnticSlab < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anticslab/AnticSlab-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Antic Slab"
+  desc "Antic slab font"
   homepage "https://fonts.google.com/specimen/Antic+Slab"
+  head "https://github.com/google/fonts/raw/main/ofl/anticslab/AnticSlab-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AnticSlab-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
