3a7176dd95c65584f3f035c47e6995b84e1550fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:54 +0000

    font-noto-sans-nushu updated

--- Formula/font-noto-sans-nushu.rb
+++ Formula/font-noto-sans-nushu.rb
@@ -1,12 +1,14 @@
 class FontNotoSansNushu < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansnushu/NotoSansNushu-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Nushu"
+  desc "Noto sans nushu font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Nushu"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansnushu/NotoSansNushu-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansNushu-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
