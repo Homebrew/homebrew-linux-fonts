3240b3f8ed1a1e6bf72b72fa4b7ccd8a879f7716
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-mina updated

--- Formula/font-mina.rb
+++ Formula/font-mina.rb
@@ -1,13 +1,17 @@
 class FontMina < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/mina"
-  desc "Mina"
+  desc "Mina font"
   homepage "https://fonts.google.com/specimen/Mina"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/mina"
+
   def install
     (share/"fonts").install Dir.glob("ofl/mina/./**/Mina-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/mina/./**/Mina-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
