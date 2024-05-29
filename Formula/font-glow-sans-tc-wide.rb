8e86be7fb54492d2d2ffd9e321c3333ee9ac359f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-glow-sans-tc-wide updated

--- Formula/font-glow-sans-tc-wide.rb
+++ Formula/font-glow-sans-tc-wide.rb
@@ -1,12 +1,14 @@
 class FontGlowSansTcWide < Formula
+  desc "Glow sans tc wide font"
+  homepage "https://github.com/welai/glow-sans"
+  url "https://github.com/welai/glow-sans/releases/download/v0.93/GlowSansTC-Wide-v0.93.zip"
   version "0.93"
   sha256 "9287bd11c34c38ca78dde00d66e611fb12c08b1099a6b697667472855f712b3a"
-  url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansTC-Wide-v#{version}.zip"
-  desc "Glow Sans TC Wide"
-  homepage "https://github.com/welai/glow-sans"
-  deprecate! date: "2024-02-17", because: :unsupported
+
+  deprecate! "2024-02-17", because: :discontinued
+
   def install
-    (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-Thin.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-Book.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-ExtraBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-ExtraLight.otf")[0]
@@ -14,10 +16,10 @@ class FontGlowSansTcWide < Formula
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-Light.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansTC-Wide-Bold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
