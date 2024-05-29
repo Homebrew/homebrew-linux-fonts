f1b49e1001685de6618b981efa3575db69612200
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-tienne updated

--- Formula/font-tienne.rb
+++ Formula/font-tienne.rb
@@ -1,14 +1,18 @@
 class FontTienne < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tienne"
-  desc "Tienne"
+  desc "Tienne font"
   homepage "https://fonts.google.com/specimen/Tienne"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tienne"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tienne/./**/Tienne-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tienne/./**/Tienne-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tienne/./**/Tienne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
