c68733ee2d4f93b07def4d665ff8dee21b84dccf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-stix-two-text updated

--- Formula/font-stix-two-text.rb
+++ Formula/font-stix-two-text.rb
@@ -1,13 +1,17 @@
 class FontStixTwoText < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/stixtwotext"
-  desc "STIX Two Text"
+  desc "Stix two text font"
   homepage "https://fonts.google.com/specimen/STIX+Two+Text"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/stixtwotext"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/stixtwotext/./**/STIXTwoText-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/stixtwotext/./**/STIXTwoText\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/stixtwotext/./**/STIXTwoText-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/stixtwotext/./**/STIXTwoText[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
