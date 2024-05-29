3e270ecc4bec6be1d3438eeab5da0075dc37bc59
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-ark-pixel-10px-proportional updated

--- Formula/font-ark-pixel-10px-proportional.rb
+++ Formula/font-ark-pixel-10px-proportional.rb
@@ -1,10 +1,10 @@
 class FontArkPixel10pxProportional < Formula
-  version "2024.05.12"
-  sha256 "b193e7b49fe17ad20df86115dbaf169ab4f49c5b6b38ac2653ea643bd7afd67a"
-  url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-proportional-otf-v#{version}.zip"
-  desc "Ark Pixel 10px Proportional"
   desc "Open source Pan-CJK pixel font"
   homepage "https://github.com/TakWolf/ark-pixel-font"
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/2024.05.12/ark-pixel-font-10px-proportional-otf-v2024.05.12.zip"
+  version "2024.05.12"
+  sha256 "b193e7b49fe17ad20df86115dbaf169ab4f49c5b6b38ac2653ea643bd7afd67a"
+
   def install
     (share/"fonts").install Dir.glob("./**/ark-pixel-10px-proportional-ja.otf")[0]
     (share/"fonts").install Dir.glob("./**/ark-pixel-10px-proportional-ko.otf")[0]
@@ -14,8 +14,8 @@ class FontArkPixel10pxProportional < Formula
     (share/"fonts").install Dir.glob("./**/ark-pixel-10px-proportional-zh_tr.otf")[0]
     (share/"fonts").install Dir.glob("./**/ark-pixel-10px-proportional-zh_tw.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
