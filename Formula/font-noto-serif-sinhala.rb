8aa64f951e18581c2aa831f92d08348814df1681
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:00 +0000

    font-noto-serif-sinhala updated

--- Formula/font-noto-serif-sinhala.rb
+++ Formula/font-noto-serif-sinhala.rb
@@ -1,9 +1,11 @@
 class FontNotoSerifSinhala < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifSinhala-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Serif Sinhala"
+  desc "Noto serif sinhala font"
   homepage "https://www.google.com/get/noto/#serif-sinh"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifSinhala-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-ExtraLight.ttf")[0]
@@ -11,10 +13,10 @@ class FontNotoSerifSinhala < Formula
     (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifSinhala-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
