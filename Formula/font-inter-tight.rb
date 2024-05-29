1c8b47f533f26de549ac9014bb750111c1503451
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:21 +0000

    font-inter-tight updated

--- Formula/font-inter-tight.rb
+++ Formula/font-inter-tight.rb
@@ -1,13 +1,17 @@
 class FontInterTight < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/intertight"
-  desc "Inter Tight"
+  desc "Inter tight font"
   homepage "https://fonts.google.com/specimen/Inter+Tight"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/intertight"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/intertight/./**/InterTight-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/intertight/./**/InterTight\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/intertight/./**/InterTight-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/intertight/./**/InterTight[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
