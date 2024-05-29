6d0e80c674a35624be71833c8c16e3124dab45b8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-benchnine updated

--- Formula/font-benchnine.rb
+++ Formula/font-benchnine.rb
@@ -1,14 +1,18 @@
 class FontBenchnine < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/benchnine"
-  desc "BenchNine"
+  desc "Benchnine font"
   homepage "https://fonts.google.com/specimen/BenchNine"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/benchnine"
+
   def install
     (share/"fonts").install Dir.glob("ofl/benchnine/./**/BenchNine-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/benchnine/./**/BenchNine-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/benchnine/./**/BenchNine-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
