ad91340840f92fb2b231561c9f51e38b1e114369
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:18 +0000

    font-ruluko updated

--- Formula/font-ruluko.rb
+++ Formula/font-ruluko.rb
@@ -1,12 +1,14 @@
 class FontRuluko < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ruluko/Ruluko-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ruluko"
+  desc "Ruluko font"
   homepage "https://fonts.google.com/specimen/Ruluko"
+  head "https://github.com/google/fonts/raw/main/ofl/ruluko/Ruluko-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Ruluko-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
