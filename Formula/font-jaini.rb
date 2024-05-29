48d1086f6c0e78c7032bfff8614f8ddc876c6349
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jaini updated

--- Formula/font-jaini.rb
+++ Formula/font-jaini.rb
@@ -1,13 +1,14 @@
 class FontJaini < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jaini/Jaini-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jaini"
   desc "Typeface based on the calligraphic style of the Jain Kalpasutra manuscripts"
   homepage "https://fonts.google.com/specimen/Jaini"
+  head "https://github.com/google/fonts/raw/main/ofl/jaini/Jaini-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Jaini-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
