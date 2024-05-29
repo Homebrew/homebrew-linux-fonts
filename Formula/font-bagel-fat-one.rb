0e6a18b24c0614f04879ed13dda1b95f499523e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-bagel-fat-one updated

--- Formula/font-bagel-fat-one.rb
+++ Formula/font-bagel-fat-one.rb
@@ -1,12 +1,14 @@
 class FontBagelFatOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bagelfatone/BagelFatOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bagel Fat One"
+  desc "Bagel fat one font"
   homepage "https://fonts.google.com/specimen/Bagel+Fat+One"
+  head "https://github.com/google/fonts/raw/main/ofl/bagelfatone/BagelFatOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BagelFatOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
