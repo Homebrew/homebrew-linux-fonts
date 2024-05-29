430733c5d7ae83735c27667a0ef28193ce9730ff
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:21 +0000

    font-instrument-serif updated

--- Formula/font-instrument-serif.rb
+++ Formula/font-instrument-serif.rb
@@ -1,13 +1,17 @@
 class FontInstrumentSerif < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/instrumentserif"
-  desc "Instrument Serif"
+  desc "Instrument serif font"
   homepage "https://fonts.google.com/specimen/Instrument+Serif"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/instrumentserif"
+
   def install
     (share/"fonts").install Dir.glob("ofl/instrumentserif/./**/InstrumentSerif-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/instrumentserif/./**/InstrumentSerif-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
