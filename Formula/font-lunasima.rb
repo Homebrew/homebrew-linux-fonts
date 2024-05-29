6592d7d2bc06d23af90ce68483a26c7566e4568e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-lunasima updated

--- Formula/font-lunasima.rb
+++ Formula/font-lunasima.rb
@@ -1,14 +1,17 @@
 class FontLunasima < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lunasima"
-  desc "Lunasima"
   desc "Unmodulated (“sans serif”) design"
   homepage "https://fonts.google.com/specimen/Lunasima"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/lunasima"
+
   def install
     (share/"fonts").install Dir.glob("ofl/lunasima/./**/Lunasima-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lunasima/./**/Lunasima-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
