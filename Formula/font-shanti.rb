2723b0ea71fbb067e5ebb5eb15764dbdc2d1bb7f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-shanti updated

--- Formula/font-shanti.rb
+++ Formula/font-shanti.rb
@@ -1,12 +1,14 @@
 class FontShanti < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/shanti/Shanti-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Shanti"
+  desc "Shanti font"
   homepage "https://fonts.google.com/specimen/Shanti"
+  head "https://github.com/google/fonts/raw/main/ofl/shanti/Shanti-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Shanti-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
