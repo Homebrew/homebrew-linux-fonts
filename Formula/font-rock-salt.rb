247d76f8696feab8d22c3086dc1f3cc6e7466956
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:15 +0000

    font-rock-salt updated

--- Formula/font-rock-salt.rb
+++ Formula/font-rock-salt.rb
@@ -1,12 +1,14 @@
 class FontRockSalt < Formula
-  head "https://github.com/google/fonts/raw/main/apache/rocksalt/RockSalt-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rock Salt"
+  desc "Rock salt font"
   homepage "https://fonts.google.com/specimen/Rock+Salt"
+  head "https://github.com/google/fonts/raw/main/apache/rocksalt/RockSalt-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RockSalt-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
