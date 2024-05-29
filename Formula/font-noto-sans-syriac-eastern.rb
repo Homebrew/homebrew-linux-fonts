e89ca0a47bc3db41b3ff5aadb1a5fe11631e2ef1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:56 +0000

    font-noto-sans-syriac-eastern updated

--- Formula/font-noto-sans-syriac-eastern.rb
+++ Formula/font-noto-sans-syriac-eastern.rb
@@ -1,12 +1,14 @@
 class FontNotoSansSyriacEastern < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEastern-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Syriac Eastern"
+  desc "Noto sans syriac eastern font"
   homepage "https://www.google.com/get/noto/#sans-syrc-eastern"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEastern-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansSyriacEastern-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
