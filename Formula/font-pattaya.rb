3c5331b99b6db1d5d9fe21f3941fd18c8e175905
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-pattaya updated

--- Formula/font-pattaya.rb
+++ Formula/font-pattaya.rb
@@ -1,12 +1,14 @@
 class FontPattaya < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/pattaya/Pattaya-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Pattaya"
+  desc "Pattaya font"
   homepage "https://fonts.google.com/specimen/Pattaya"
+  head "https://github.com/google/fonts/raw/main/ofl/pattaya/Pattaya-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Pattaya-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
