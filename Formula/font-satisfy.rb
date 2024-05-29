54b17247eb63457d5c31a230f72815aff0be907f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-satisfy updated

--- Formula/font-satisfy.rb
+++ Formula/font-satisfy.rb
@@ -1,12 +1,14 @@
 class FontSatisfy < Formula
-  head "https://github.com/google/fonts/raw/main/apache/satisfy/Satisfy-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Satisfy"
+  desc "Satisfy font"
   homepage "https://fonts.google.com/specimen/Satisfy"
+  head "https://github.com/google/fonts/raw/main/apache/satisfy/Satisfy-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Satisfy-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
