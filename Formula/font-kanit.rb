4873487572f39d8cdcf576863f7a92e04bc398f3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:27 +0000

    font-kanit updated

--- Formula/font-kanit.rb
+++ Formula/font-kanit.rb
@@ -1,9 +1,13 @@
 class FontKanit < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kanit"
-  desc "Kanit"
+  desc "Kanit font"
   homepage "https://fonts.google.com/specimen/Kanit"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kanit"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-ThinItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-BoldItalic.ttf")[0]
@@ -20,10 +24,10 @@ class FontKanit < Formula
     (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kanit/./**/Kanit-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
