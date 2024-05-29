79d5bb904b476192ec7d8dd621970492c0db5668
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-yomogi updated

--- Formula/font-yomogi.rb
+++ Formula/font-yomogi.rb
@@ -1,12 +1,14 @@
 class FontYomogi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yomogi/Yomogi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yomogi"
+  desc "Yomogi font"
   homepage "https://fonts.google.com/specimen/Yomogi"
+  head "https://github.com/google/fonts/raw/main/ofl/yomogi/Yomogi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Yomogi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
