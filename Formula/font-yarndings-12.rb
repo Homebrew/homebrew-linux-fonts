3616e095756a42e1494c3a11d99d6a8967667bb9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:36 +0000

    font-yarndings-12 updated

--- Formula/font-yarndings-12.rb
+++ Formula/font-yarndings-12.rb
@@ -1,12 +1,14 @@
 class FontYarndings12 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yarndings12/Yarndings12-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yarndings 12"
+  desc "Yarndings 12 font"
   homepage "https://fonts.google.com/specimen/Yarndings+12"
+  head "https://github.com/google/fonts/raw/main/ofl/yarndings12/Yarndings12-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Yarndings12-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
