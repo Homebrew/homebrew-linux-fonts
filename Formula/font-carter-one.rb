fd8d10b1d4c344c5f40453fefc4a5c171f8e4f96
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:52 +0000

    font-carter-one updated

--- Formula/font-carter-one.rb
+++ Formula/font-carter-one.rb
@@ -1,12 +1,14 @@
 class FontCarterOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/carterone/CarterOne.ttf", verified: "github.com/google/fonts/"
-  desc "Carter One"
+  desc "Carter one font"
   homepage "https://fonts.google.com/specimen/Carter+One"
+  head "https://github.com/google/fonts/raw/main/ofl/carterone/CarterOne.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/CarterOne.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
