cea9921f310a4485c02a5c2d34629b3b21dca1cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:58 +0000

    font-noto-sans-yi updated

--- Formula/font-noto-sans-yi.rb
+++ Formula/font-noto-sans-yi.rb
@@ -1,12 +1,14 @@
 class FontNotoSansYi < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansYi-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Yi"
+  desc "Noto sans yi font"
   homepage "https://www.google.com/get/noto/#sans-yiii"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansYi-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansYi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
