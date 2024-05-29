b150891d46815ffd52e0af21f6b048696cf6a1ac
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-bhutuka-expanded-one updated

--- Formula/font-bhutuka-expanded-one.rb
+++ Formula/font-bhutuka-expanded-one.rb
@@ -1,13 +1,14 @@
 class FontBhutukaExpandedOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bhutukaexpandedone/BhuTukaExpandedOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "BhuTuka Expanded One"
   desc "Gurmukhi companion to aoife mooney’s biorhyme expanded light typeface"
   homepage "https://fonts.google.com/specimen/BhuTuka+Expanded+One"
+  head "https://github.com/google/fonts/raw/main/ofl/bhutukaexpandedone/BhuTukaExpandedOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BhuTukaExpandedOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
