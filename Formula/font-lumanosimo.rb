56008a8b47b5c9a2ca5b4cf01291cf7227e3bbd5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-lumanosimo updated

--- Formula/font-lumanosimo.rb
+++ Formula/font-lumanosimo.rb
@@ -1,13 +1,14 @@
 class FontLumanosimo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lumanosimo/Lumanosimo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Lumanosimo"
   desc "Expressive typeface"
   homepage "https://fonts.google.com/specimen/Lumanosimo"
+  head "https://github.com/google/fonts/raw/main/ofl/lumanosimo/Lumanosimo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Lumanosimo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
