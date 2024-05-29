2f61b34c568acb70fe2d0d4e7de7dfe4905d948f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:38 +0000

    font-zen-maru-gothic updated

--- Formula/font-zen-maru-gothic.rb
+++ Formula/font-zen-maru-gothic.rb
@@ -1,8 +1,11 @@
 class FontZenMaruGothic < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/zenmarugothic"
-  desc "Zen Maru Gothic"
   desc "Also easy to use in any scenes"
   homepage "https://fonts.google.com/specimen/Zen+Maru+Gothic"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/zenmarugothic"
+
   def install
     (share/"fonts").install Dir.glob("ofl/zenmarugothic/./**/ZenMaruGothic-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/zenmarugothic/./**/ZenMaruGothic-Bold.ttf")[0]
@@ -10,8 +13,8 @@ class FontZenMaruGothic < Formula
     (share/"fonts").install Dir.glob("ofl/zenmarugothic/./**/ZenMaruGothic-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/zenmarugothic/./**/ZenMaruGothic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
