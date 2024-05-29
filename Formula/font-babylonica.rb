6aee0fed39e06db73b7ec89eb1d54ef056c9b95c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-babylonica updated

--- Formula/font-babylonica.rb
+++ Formula/font-babylonica.rb
@@ -1,13 +1,14 @@
 class FontBabylonica < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/babylonica/Babylonica-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Babylonica"
   desc "Interrupted by upright or back-slanted forms giving it an interrupted stress"
   homepage "https://fonts.google.com/specimen/Babylonica"
+  head "https://github.com/google/fonts/raw/main/ofl/babylonica/Babylonica-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Babylonica-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
