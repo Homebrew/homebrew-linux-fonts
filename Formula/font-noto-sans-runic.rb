78723138aacdc6fc511de683dd2be2135b6e34ab
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-runic updated

--- Formula/font-noto-sans-runic.rb
+++ Formula/font-noto-sans-runic.rb
@@ -1,12 +1,14 @@
 class FontNotoSansRunic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRunic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Runic"
+  desc "Noto sans runic font"
   homepage "https://www.google.com/get/noto/#sans-runr"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRunic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansRunic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
