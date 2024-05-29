467f2acfeef641deb91b38cfdd7fd67266ac2c2c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-mrs-saint-delafield updated

--- Formula/font-mrs-saint-delafield.rb
+++ Formula/font-mrs-saint-delafield.rb
@@ -1,12 +1,14 @@
 class FontMrsSaintDelafield < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mrssaintdelafield/MrsSaintDelafield-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mrs Saint Delafield"
+  desc "Mrs saint delafield font"
   homepage "https://fonts.google.com/specimen/Mrs+Saint+Delafield"
+  head "https://github.com/google/fonts/raw/main/ofl/mrssaintdelafield/MrsSaintDelafield-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MrsSaintDelafield-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
