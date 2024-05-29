ce5752624654b64585f8fadea9c0d777da285e22
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-kufi-arabic updated

--- Formula/font-noto-kufi-arabic.rb
+++ Formula/font-noto-kufi-arabic.rb
@@ -1,13 +1,15 @@
 class FontNotoKufiArabic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoKufiArabic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Kufi Arabic"
+  desc "Noto kufi arabic font"
   homepage "https://www.google.com/get/noto/#kufi-arab"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoKufiArabic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoKufiArabic-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoKufiArabic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
