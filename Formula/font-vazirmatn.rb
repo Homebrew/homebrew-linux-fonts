4f3b5d0a3b2504de33d58d848597a54c5c21e4e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-vazirmatn updated

--- Formula/font-vazirmatn.rb
+++ Formula/font-vazirmatn.rb
@@ -1,13 +1,14 @@
 class FontVazirmatn < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/vazirmatn/Vazirmatn%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Vazirmatn"
   desc "Combined with roboto"
   homepage "https://fonts.google.com/specimen/Vazirmatn"
+  head "https://github.com/google/fonts/raw/main/ofl/vazirmatn/Vazirmatn%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Vazirmatn\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Vazirmatn[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
