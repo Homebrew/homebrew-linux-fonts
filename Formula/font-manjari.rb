4cf10705ba739e264264dd7c071d4964cf2baccf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:37 +0000

    font-manjari updated

--- Formula/font-manjari.rb
+++ Formula/font-manjari.rb
@@ -1,14 +1,18 @@
 class FontManjari < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/manjari"
-  desc "Manjari"
+  desc "Manjari font"
   homepage "https://fonts.google.com/specimen/Manjari"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/manjari"
+
   def install
     (share/"fonts").install Dir.glob("ofl/manjari/./**/Manjari-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/manjari/./**/Manjari-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/manjari/./**/Manjari-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
