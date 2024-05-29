0f0293afed3dcf7f6c28fb3bbac5b30f239109e1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-amiko updated

--- Formula/font-amiko.rb
+++ Formula/font-amiko.rb
@@ -1,14 +1,18 @@
 class FontAmiko < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/amiko"
-  desc "Amiko"
+  desc "Amiko font"
   homepage "https://fonts.google.com/specimen/Amiko"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/amiko"
+
   def install
     (share/"fonts").install Dir.glob("ofl/amiko/./**/Amiko-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/amiko/./**/Amiko-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/amiko/./**/Amiko-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
