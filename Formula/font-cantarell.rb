a4844d71959298b07b5f31a13a5d29517a160585
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-cantarell updated

--- Formula/font-cantarell.rb
+++ Formula/font-cantarell.rb
@@ -1,15 +1,19 @@
 class FontCantarell < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cantarell"
-  desc "Cantarell"
+  desc "Cantarell font"
   homepage "https://fonts.google.com/specimen/Cantarell"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cantarell"
+
   def install
     (share/"fonts").install Dir.glob("ofl/cantarell/./**/Cantarell-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cantarell/./**/Cantarell-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cantarell/./**/Cantarell-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cantarell/./**/Cantarell-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
