145ce8db87d5fb78fc189492870ce0e1f22d35b1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:37 +0000

    font-mansalva updated

--- Formula/font-mansalva.rb
+++ Formula/font-mansalva.rb
@@ -1,12 +1,14 @@
 class FontMansalva < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mansalva/Mansalva-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mansalva"
+  desc "Mansalva font"
   homepage "https://fonts.google.com/specimen/Mansalva"
+  head "https://github.com/google/fonts/raw/main/ofl/mansalva/Mansalva-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Mansalva-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
