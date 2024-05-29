893dd0fb540cc6f737429b167b68a30a22b6bdcb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-distressed updated

--- Formula/font-rubik-distressed.rb
+++ Formula/font-rubik-distressed.rb
@@ -1,12 +1,14 @@
 class FontRubikDistressed < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikdistressed/RubikDistressed-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Distressed"
+  desc "Rubik distressed font"
   homepage "https://fonts.google.com/specimen/Rubik+Distressed"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikdistressed/RubikDistressed-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikDistressed-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
