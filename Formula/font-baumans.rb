fc5689b732d47504637d7cd2d829ab5c4619f52d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:45 +0000

    font-baumans updated

--- Formula/font-baumans.rb
+++ Formula/font-baumans.rb
@@ -1,12 +1,14 @@
 class FontBaumans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/baumans/Baumans-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Baumans"
+  desc "Baumans font"
   homepage "https://fonts.google.com/specimen/Baumans"
+  head "https://github.com/google/fonts/raw/main/ofl/baumans/Baumans-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Baumans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
