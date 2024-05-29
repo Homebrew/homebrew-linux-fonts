c03cea5ba34f950f3e3b64c95443bd6161d282ad
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-clear-sans updated

--- Formula/font-clear-sans.rb
+++ Formula/font-clear-sans.rb
@@ -1,21 +1,24 @@
 class FontClearSans < Formula
-  head "https://github.com/intel/clear-sans.git", branch: "main", only_path: "TTF"
-  desc "Clear Sans"
   desc "Sans-serif typeface"
   homepage "https://github.com/intel/clear-sans"
-  deprecate! date: "2023-12-17", because: :unsupported
+  head "https://github.com/intel/clear-sans.git",
+       branch:    "main",
+       only_path: "TTF"
+
+  deprecate! "2023-12-17", because: :discontinued
+
   def install
-    (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Light.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
