ad6ccdff7a1e9a3a460a0a656a4e1b39a59fe739
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-sura updated

--- Formula/font-sura.rb
+++ Formula/font-sura.rb
@@ -1,13 +1,17 @@
 class FontSura < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sura"
-  desc "Sura"
+  desc "Sura font"
   homepage "https://fonts.google.com/specimen/Sura"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sura"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sura/./**/Sura-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sura/./**/Sura-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
