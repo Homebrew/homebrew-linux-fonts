d98b87b41d21b9292ae191a57c237b110f622625
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-im-fell-english updated

--- Formula/font-im-fell-english.rb
+++ Formula/font-im-fell-english.rb
@@ -1,13 +1,17 @@
 class FontImFellEnglish < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/imfellenglish"
-  desc "IM Fell English"
+  desc "Im fell english font"
   homepage "https://fonts.google.com/specimen/IM+Fell+English"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/imfellenglish"
+
   def install
     (share/"fonts").install Dir.glob("ofl/imfellenglish/./**/IMFeENit28P.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/imfellenglish/./**/IMFeENrm28P.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
