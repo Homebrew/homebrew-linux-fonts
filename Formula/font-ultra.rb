b5dc2ed2f3a5464e36bf00398ea46575e1f0be10
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:33 +0000

    font-ultra updated

--- Formula/font-ultra.rb
+++ Formula/font-ultra.rb
@@ -1,12 +1,14 @@
 class FontUltra < Formula
-  head "https://github.com/google/fonts/raw/main/apache/ultra/Ultra-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ultra"
+  desc "Ultra font"
   homepage "https://fonts.google.com/specimen/Ultra"
+  head "https://github.com/google/fonts/raw/main/apache/ultra/Ultra-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Ultra-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
