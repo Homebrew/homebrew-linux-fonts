5c99c786d63308ea1c224c4e20cec3f324de9070
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:49 +0000

    font-bowlby-one-sc updated

--- Formula/font-bowlby-one-sc.rb
+++ Formula/font-bowlby-one-sc.rb
@@ -1,12 +1,14 @@
 class FontBowlbyOneSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bowlbyonesc/BowlbyOneSC-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bowlby One SC"
+  desc "Bowlby one sc font"
   homepage "https://fonts.google.com/specimen/Bowlby+One+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/bowlbyonesc/BowlbyOneSC-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BowlbyOneSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
