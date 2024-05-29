1ddd3a59f287a8557c42a5422e37a6da4d43fe28
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-windsong updated

--- Formula/font-windsong.rb
+++ Formula/font-windsong.rb
@@ -1,14 +1,17 @@
 class FontWindsong < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/windsong"
-  desc "WindSong"
   desc "Elongated script with multiple stylistic sets"
   homepage "https://fonts.google.com/specimen/WindSong"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/windsong"
+
   def install
     (share/"fonts").install Dir.glob("ofl/windsong/./**/WindSong-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/windsong/./**/WindSong-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
