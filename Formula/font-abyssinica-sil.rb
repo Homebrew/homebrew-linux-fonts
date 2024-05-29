5c72c76be22d764087ecb64d75f12a72d6d11ab2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-abyssinica-sil updated

--- Formula/font-abyssinica-sil.rb
+++ Formula/font-abyssinica-sil.rb
@@ -1,12 +1,14 @@
 class FontAbyssinicaSil < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/abyssinicasil/AbyssinicaSIL-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Abyssinica SIL"
+  desc "Abyssinica sil font"
   homepage "https://fonts.google.com/specimen/Abyssinica+SIL"
+  head "https://github.com/google/fonts/raw/main/ofl/abyssinicasil/AbyssinicaSIL-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AbyssinicaSIL-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
