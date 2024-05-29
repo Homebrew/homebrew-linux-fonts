22fbc1d9447803f799310780099b1e091d0705d4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-hentaigana updated

--- Formula/font-noto-serif-hentaigana.rb
+++ Formula/font-noto-serif-hentaigana.rb
@@ -1,13 +1,13 @@
 class FontNotoSerifHentaigana < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifhentaigana/NotoSerifHentaigana%5Bwght%5D.ttf"
-  desc "Noto Serif Hentaigana"
   desc "Font that contains symbols for the kana supplement unicode block"
   homepage "https://github.com/notofonts/hentaigana.git"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifhentaigana/NotoSerifHentaigana%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifHentaigana\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifHentaigana[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
