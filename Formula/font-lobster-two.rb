da0d2e12cd6bdc191d7082d0d4d4e04221456caa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-lobster-two updated

--- Formula/font-lobster-two.rb
+++ Formula/font-lobster-two.rb
@@ -1,15 +1,19 @@
 class FontLobsterTwo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lobstertwo"
-  desc "Lobster Two"
+  desc "Lobster two font"
   homepage "https://fonts.google.com/specimen/Lobster+Two"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/lobstertwo"
+
   def install
     (share/"fonts").install Dir.glob("ofl/lobstertwo/./**/LobsterTwo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lobstertwo/./**/LobsterTwo-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lobstertwo/./**/LobsterTwo-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lobstertwo/./**/LobsterTwo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
