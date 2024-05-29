222888391496908d1e19d49f025a6228bd43d809
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-corben updated

--- Formula/font-corben.rb
+++ Formula/font-corben.rb
@@ -1,13 +1,17 @@
 class FontCorben < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/corben"
-  desc "Corben"
+  desc "Corben font"
   homepage "https://fonts.google.com/specimen/Corben"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/corben"
+
   def install
     (share/"fonts").install Dir.glob("ofl/corben/./**/Corben-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/corben/./**/Corben-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
