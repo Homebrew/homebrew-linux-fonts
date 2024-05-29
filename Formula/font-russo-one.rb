47da82f2db9497a12e1bb345d00b29aba2b6f32e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:18 +0000

    font-russo-one updated

--- Formula/font-russo-one.rb
+++ Formula/font-russo-one.rb
@@ -1,12 +1,14 @@
 class FontRussoOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/russoone/RussoOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Russo One"
+  desc "Russo one font"
   homepage "https://fonts.google.com/specimen/Russo+One"
+  head "https://github.com/google/fonts/raw/main/ofl/russoone/RussoOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RussoOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
