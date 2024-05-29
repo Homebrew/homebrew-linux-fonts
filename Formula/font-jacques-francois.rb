30fe4fda8c9f9eb6714fd4a77fa2019cbd8a7e60
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jacques-francois updated

--- Formula/font-jacques-francois.rb
+++ Formula/font-jacques-francois.rb
@@ -1,12 +1,14 @@
 class FontJacquesFrancois < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jacquesfrancois/JacquesFrancois-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jacques Francois"
+  desc "Jacques francois font"
   homepage "https://fonts.google.com/specimen/Jacques+Francois"
+  head "https://github.com/google/fonts/raw/main/ofl/jacquesfrancois/JacquesFrancois-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JacquesFrancois-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
