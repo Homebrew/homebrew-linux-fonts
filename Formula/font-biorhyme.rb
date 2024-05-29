3afc0f61437a705c049a091f5760b89b83668029
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-biorhyme updated

--- Formula/font-biorhyme.rb
+++ Formula/font-biorhyme.rb
@@ -1,13 +1,14 @@
 class FontBiorhyme < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/biorhyme/BioRhyme%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "BioRhyme"
   desc "Latin typeface family"
   homepage "https://fonts.google.com/specimen/BioRhyme"
+  head "https://github.com/google/fonts/raw/main/ofl/biorhyme/BioRhyme%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BioRhyme\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BioRhyme[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
