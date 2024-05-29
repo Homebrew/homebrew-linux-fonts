659bf8e38f7bf48da9b929645bb2a94c6a5f84db
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-kolker-brush updated

--- Formula/font-kolker-brush.rb
+++ Formula/font-kolker-brush.rb
@@ -1,13 +1,14 @@
 class FontKolkerBrush < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kolkerbrush/KolkerBrush-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kolker Brush"
   desc "Never recommended to use all caps when editing copy"
   homepage "https://fonts.google.com/specimen/Kolker+Brush"
+  head "https://github.com/google/fonts/raw/main/ofl/kolkerbrush/KolkerBrush-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/KolkerBrush-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
