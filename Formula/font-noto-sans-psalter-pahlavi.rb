c399ac6b23d8c8d4cb3b8430484fa128a314f0f5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-psalter-pahlavi updated

--- Formula/font-noto-sans-psalter-pahlavi.rb
+++ Formula/font-noto-sans-psalter-pahlavi.rb
@@ -1,12 +1,14 @@
 class FontNotoSansPsalterPahlavi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanspsalterpahlavi/NotoSansPsalterPahlavi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Psalter Pahlavi"
+  desc "Noto sans psalter pahlavi font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Psalter+Pahlavi"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanspsalterpahlavi/NotoSansPsalterPahlavi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansPsalterPahlavi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
