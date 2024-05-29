77e1c095b8f81bb3ca8ab5b98989e2c4b1a379f3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-creepster-caps updated

--- Formula/font-creepster-caps.rb
+++ Formula/font-creepster-caps.rb
@@ -1,12 +1,14 @@
 class FontCreepsterCaps < Formula
-  head "https://github.com/google/fonts/raw/main/apache/creepstercaps/CreepsterCaps-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Creepster Caps"
+  desc "Creepster caps font"
   homepage "https://fonts.google.com/specimen/Creepster+Caps"
+  head "https://github.com/google/fonts/raw/main/apache/creepstercaps/CreepsterCaps-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/CreepsterCaps-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
