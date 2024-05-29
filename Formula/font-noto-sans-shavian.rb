240500d74f41486480b243f6d0ff5a010f948077
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:56 +0000

    font-noto-sans-shavian updated

--- Formula/font-noto-sans-shavian.rb
+++ Formula/font-noto-sans-shavian.rb
@@ -1,12 +1,14 @@
 class FontNotoSansShavian < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansShavian-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Shavian"
+  desc "Noto sans shavian font"
   homepage "https://www.google.com/get/noto/#sans-shaw"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansShavian-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansShavian-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
