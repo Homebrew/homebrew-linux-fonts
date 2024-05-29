695e8f7e6328a4625395e45443b5aef8b6ba10a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-jacquard-24 updated

--- Formula/font-jacquard-24.rb
+++ Formula/font-jacquard-24.rb
@@ -1,12 +1,14 @@
 class FontJacquard24 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jacquard24/Jacquard24-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jacquard 24"
+  desc "Jacquard 24 font"
   homepage "https://fonts.google.com/specimen/Jacquard+24"
+  head "https://github.com/google/fonts/raw/main/ofl/jacquard24/Jacquard24-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Jacquard24-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
