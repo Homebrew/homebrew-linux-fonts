a53f59aef0ea2bd7327d74acd4f7c2c7e538698a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:32 +0000

    font-trykker updated

--- Formula/font-trykker.rb
+++ Formula/font-trykker.rb
@@ -1,12 +1,14 @@
 class FontTrykker < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/trykker/Trykker-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Trykker"
+  desc "Trykker font"
   homepage "https://fonts.google.com/specimen/Trykker"
+  head "https://github.com/google/fonts/raw/main/ofl/trykker/Trykker-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Trykker-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
