1cf1245164ef3f0473d8a94cba16ef147a101528
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-mandaic updated

--- Formula/font-noto-sans-mandaic.rb
+++ Formula/font-noto-sans-mandaic.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMandaic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMandaic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Mandaic"
+  desc "Noto sans mandaic font"
   homepage "https://www.google.com/get/noto/#sans-mand"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMandaic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMandaic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
