9f28ead0820bf35da1830102a6e674c515374fa3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:49 +0000

    font-briem-hand updated

--- Formula/font-briem-hand.rb
+++ Formula/font-briem-hand.rb
@@ -1,13 +1,14 @@
 class FontBriemHand < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/briemhand/BriemHand%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Briem Hand"
   desc "Modern adaptation of sixteenth-century chancery cursive"
   homepage "https://fonts.google.com/specimen/Briem+Hand"
+  head "https://github.com/google/fonts/raw/main/ofl/briemhand/BriemHand%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BriemHand\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BriemHand[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
