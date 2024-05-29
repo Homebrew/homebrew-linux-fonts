561078894c993f97b5a3752bd95c60741d6f98a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-alegreya updated

--- Formula/font-alegreya.rb
+++ Formula/font-alegreya.rb
@@ -1,33 +1,34 @@
 class FontAlegreya < Formula
+  desc "Alegreya font"
+  homepage "https://github.com/huertatipografica/Alegreya"
+  url "https://github.com/huertatipografica/Alegreya/archive/v2.008.zip"
   version "2.008"
   sha256 "8a10cae2b660b048c53e9be1612698b86ed123a284f9f9d17cc9a4a45388650a"
-  url "https://github.com/huertatipografica/Alegreya/archive/v#{version}.zip"
-  desc "Alegreya"
-  homepage "https://github.com/huertatipografica/Alegreya"
+
   def install
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-Black.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-BlackItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-ExtraBold.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-ExtraBoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/Alegreya-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-Black.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-BlackItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-ExtraBold.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-ExtraBoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Alegreya-#{version}/fonts/otf/**/AlegreyaSC-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-BlackItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-ExtraBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-ExtraBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-BlackItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-ExtraBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-ExtraBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/AlegreyaSC-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Alegreya-2.008/fonts/otf/Alegreya-Black.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
