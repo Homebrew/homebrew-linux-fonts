d48afa9270fd985b8ba508ee8fe7f2cdeb0eb6ee
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:20 +0000

    font-inika updated

--- Formula/font-inika.rb
+++ Formula/font-inika.rb
@@ -1,13 +1,17 @@
 class FontInika < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/inika"
-  desc "Inika"
+  desc "Inika font"
   homepage "https://fonts.google.com/specimen/Inika"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/inika"
+
   def install
     (share/"fonts").install Dir.glob("ofl/inika/./**/Inika-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/inika/./**/Inika-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
