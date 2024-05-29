9d0bbc74e54163cdba13021eae932a5a24d4d846
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-alata updated

--- Formula/font-alata.rb
+++ Formula/font-alata.rb
@@ -1,12 +1,14 @@
 class FontAlata < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alata/Alata-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Alata"
+  desc "Alata font"
   homepage "https://fonts.google.com/specimen/Alata"
+  head "https://github.com/google/fonts/raw/main/ofl/alata/Alata-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Alata-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
