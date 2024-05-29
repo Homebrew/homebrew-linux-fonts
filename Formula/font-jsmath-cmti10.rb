817382f4b40dea36a19a76695486c9120f8366ba
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-jsmath-cmti10 updated

--- Formula/font-jsmath-cmti10.rb
+++ Formula/font-jsmath-cmti10.rb
@@ -1,12 +1,14 @@
 class FontJsmathCmti10 < Formula
-  head "https://github.com/google/fonts/raw/main/apache/jsmathcmti10/jsMath-cmti10.ttf", verified: "github.com/google/fonts/"
-  desc "jsMath cmti10"
+  desc "Jsmath cmti10 font"
   homepage "https://fonts.google.com/specimen/jsMath+cmti10"
+  head "https://github.com/google/fonts/raw/main/apache/jsmathcmti10/jsMath-cmti10.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/jsMath-cmti10.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
