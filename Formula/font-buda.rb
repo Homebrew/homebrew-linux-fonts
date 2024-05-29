c913803ce0a48926322ebdc799cba47142de35d9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:49 +0000

    font-buda updated

--- Formula/font-buda.rb
+++ Formula/font-buda.rb
@@ -1,12 +1,14 @@
 class FontBuda < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/buda/Buda-Light.ttf", verified: "github.com/google/fonts/"
-  desc "Buda"
+  desc "Buda font"
   homepage "https://fonts.google.com/specimen/Buda"
+  head "https://github.com/google/fonts/raw/main/ofl/buda/Buda-Light.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Buda-Light.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
