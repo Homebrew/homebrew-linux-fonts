7936b9edeb8d4cf4d9f475a349c482db9656e7f3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-masinahikan-dene updated

--- Formula/font-masinahikan-dene.rb
+++ Formula/font-masinahikan-dene.rb
@@ -1,14 +1,15 @@
 class FontMasinahikanDene < Formula
-  head "https://www.languagegeek.com/font/MasinahikanDene.zip"
-  desc "Masinahikan Dene"
+  desc "Masinahikan dene font"
   homepage "https://www.languagegeek.com/font/fontdownload.html"
+  head "https://www.languagegeek.com/font/MasinahikanDene.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/MasinahikanDene_h.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MasinahikanDene_h_Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MasinahikanDene_h_Semi.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
