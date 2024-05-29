b3641ff43ea3aaca4adef671760d7bca9d7c5b85
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-besley updated

--- Formula/font-besley.rb
+++ Formula/font-besley.rb
@@ -1,14 +1,17 @@
 class FontBesley < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/besley"
-  desc "Besley"
   desc "Designed by owen earl (indestructible type*)"
   homepage "https://fonts.google.com/specimen/Besley"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/besley"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/besley/./**/Besley-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/besley/./**/Besley\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/besley/./**/Besley-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/besley/./**/Besley[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
