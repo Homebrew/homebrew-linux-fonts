6c73b88a72ff0d875178eb7411c4205f41f4f9a7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-slabo-27px updated

--- Formula/font-slabo-27px.rb
+++ Formula/font-slabo-27px.rb
@@ -1,12 +1,14 @@
 class FontSlabo27px < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/slabo27px/Slabo27px-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Slabo 27px"
+  desc "Slabo 27px font"
   homepage "https://fonts.google.com/specimen/Slabo+27px"
+  head "https://github.com/google/fonts/raw/main/ofl/slabo27px/Slabo27px-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Slabo27px-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
