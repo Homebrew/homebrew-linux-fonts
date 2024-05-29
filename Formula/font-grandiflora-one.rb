53dd297b788faacbe49cacf3678a55ccdd133de0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-grandiflora-one updated

--- Formula/font-grandiflora-one.rb
+++ Formula/font-grandiflora-one.rb
@@ -1,12 +1,14 @@
 class FontGrandifloraOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/grandifloraone/GrandifloraOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Grandiflora One"
+  desc "Grandiflora one font"
   homepage "https://fonts.google.com/specimen/Grandiflora+One"
+  head "https://github.com/google/fonts/raw/main/ofl/grandifloraone/GrandifloraOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GrandifloraOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
