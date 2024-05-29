f69ba7ccf092d97b0fe2ede54f7b310e1ba38719
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-the-nautigal updated

--- Formula/font-the-nautigal.rb
+++ Formula/font-the-nautigal.rb
@@ -1,14 +1,17 @@
 class FontTheNautigal < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/thenautigal"
-  desc "The Nautigal"
   desc "Fluid yet formal with beautiful connectors"
   homepage "https://fonts.google.com/specimen/The+Nautigal"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/thenautigal"
+
   def install
     (share/"fonts").install Dir.glob("ofl/thenautigal/./**/TheNautigal-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/thenautigal/./**/TheNautigal-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
