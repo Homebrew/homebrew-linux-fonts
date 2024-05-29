ff443b4c295288e50e4c1fdcb91f4b37b7f84f93
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:09 +0000

    font-poly updated

--- Formula/font-poly.rb
+++ Formula/font-poly.rb
@@ -1,13 +1,17 @@
 class FontPoly < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/poly"
-  desc "Poly"
+  desc "Poly font"
   homepage "https://fonts.google.com/specimen/Poly"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/poly"
+
   def install
     (share/"fonts").install Dir.glob("ofl/poly/./**/Poly-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/poly/./**/Poly-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
