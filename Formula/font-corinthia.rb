ae85312e0355bda493474e78bda9b6efd618873d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-corinthia updated

--- Formula/font-corinthia.rb
+++ Formula/font-corinthia.rb
@@ -1,13 +1,17 @@
 class FontCorinthia < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/corinthia"
-  desc "Corinthia"
+  desc "Corinthia font"
   homepage "https://fonts.google.com/specimen/Corinthia"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/corinthia"
+
   def install
     (share/"fonts").install Dir.glob("ofl/corinthia/./**/Corinthia-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/corinthia/./**/Corinthia-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
