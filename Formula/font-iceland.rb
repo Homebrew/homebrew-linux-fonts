41fb645693e1a91ee69f09e26e5e960f73a73de2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:18 +0000

    font-iceland updated

--- Formula/font-iceland.rb
+++ Formula/font-iceland.rb
@@ -1,12 +1,14 @@
 class FontIceland < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/iceland/Iceland-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Iceland"
+  desc "Iceland font"
   homepage "https://fonts.google.com/specimen/Iceland"
+  head "https://github.com/google/fonts/raw/main/ofl/iceland/Iceland-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Iceland-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
