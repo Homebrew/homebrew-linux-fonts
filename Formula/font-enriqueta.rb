0751b7bc4bc43bf7bb171ecaffae3a59fe63ce14
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-enriqueta updated

--- Formula/font-enriqueta.rb
+++ Formula/font-enriqueta.rb
@@ -1,15 +1,19 @@
 class FontEnriqueta < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/enriqueta"
-  desc "Enriqueta"
+  desc "Enriqueta font"
   homepage "https://fonts.google.com/specimen/Enriqueta"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/enriqueta"
+
   def install
     (share/"fonts").install Dir.glob("ofl/enriqueta/./**/Enriqueta-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/enriqueta/./**/Enriqueta-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/enriqueta/./**/Enriqueta-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/enriqueta/./**/Enriqueta-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
