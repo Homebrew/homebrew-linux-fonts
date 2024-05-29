3a436ba3f0a29315c298ddd2296f57771da4cbc2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-nimbus-sans-l updated

--- Formula/font-nimbus-sans-l.rb
+++ Formula/font-nimbus-sans-l.rb
@@ -1,19 +1,20 @@
 class FontNimbusSansL < Formula
-  head "https://fontlibrary.org/assets/downloads/nimbus-sans-l/2c1b22cb6708de61d2051f12c90a024e/nimbus-sans-l.zip"
-  desc "Nimbus Sans L"
+  desc "Nimbus sans l font"
   homepage "https://fontlibrary.org/en/font/nimbus-sans-l"
+  head "https://fontlibrary.org/assets/downloads/nimbus-sans-l/2c1b22cb6708de61d2051f12c90a024e/nimbus-sans-l.zip"
+
   def install
-    (share/"fonts").install Dir.glob("nimbus-sans-l/**/NimbusSanL-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("nimbus-sans-l/**/NimbusSanL-BoldCond.ttf")[0]
-    (share/"fonts").install Dir.glob("nimbus-sans-l/**/NimbusSanL-BoldCondItal.ttf")[0]
-    (share/"fonts").install Dir.glob("nimbus-sans-l/**/NimbusSanL-BoldItal.ttf")[0]
-    (share/"fonts").install Dir.glob("nimbus-sans-l/**/NimbusSanL-Regu.ttf")[0]
-    (share/"fonts").install Dir.glob("nimbus-sans-l/**/NimbusSanL-ReguCond.ttf")[0]
-    (share/"fonts").install Dir.glob("nimbus-sans-l/**/NimbusSanL-ReguCondItal.ttf")[0]
-    (share/"fonts").install Dir.glob("nimbus-sans-l/**/NimbusSanL-ReguItal.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-ReguItal.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-BoldCond.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-BoldCondItal.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-BoldItal.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-Regu.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-ReguCond.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-ReguCondItal.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
