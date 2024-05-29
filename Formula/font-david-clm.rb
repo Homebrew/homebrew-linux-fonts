dff2112413e5c40c4cfea2ca15219d5ed083c9eb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-david-clm updated

--- Formula/font-david-clm.rb
+++ Formula/font-david-clm.rb
@@ -1,15 +1,16 @@
 class FontDavidClm < Formula
-  head "https://opensiddur.org/wp-content/uploads/fonts/DavidCLM/DavidCLM.zip"
-  desc "David CLM"
+  desc "David clm font"
   homepage "https://opensiddur.org/wp-content/uploads/fonts/display-font-charmap.php?fnt=DavidCLM"
+  head "https://opensiddur.org/wp-content/uploads/fonts/DavidCLM/DavidCLM.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/DavidCLM-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/DavidCLM-BoldItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/DavidCLM-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/DavidCLM-MediumItalic.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
