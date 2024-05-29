9f06fdb6e6d06ce387d413caeba33583bed21746
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-kumbh-sans updated

--- Formula/font-kumbh-sans.rb
+++ Formula/font-kumbh-sans.rb
@@ -1,13 +1,14 @@
 class FontKumbhSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kumbhsans/KumbhSans%5BYOPQ%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Kumbh Sans"
   desc "Geometric sans-serif multi-purpose font"
   homepage "https://fonts.google.com/specimen/Kumbh+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/kumbhsans/KumbhSans%5BYOPQ%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/KumbhSans\\[YOPQ,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/KumbhSans[YOPQ,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
