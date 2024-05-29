a678bf51644f23c82fd055ab33d02019c5c5bb60
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-alfa-slab-one updated

--- Formula/font-alfa-slab-one.rb
+++ Formula/font-alfa-slab-one.rb
@@ -1,12 +1,14 @@
 class FontAlfaSlabOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alfaslabone/AlfaSlabOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Alfa Slab One"
+  desc "Alfa slab one font"
   homepage "https://fonts.google.com/specimen/Alfa+Slab+One"
+  head "https://github.com/google/fonts/raw/main/ofl/alfaslabone/AlfaSlabOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AlfaSlabOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
