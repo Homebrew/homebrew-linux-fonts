eb40b7634c02087f043481ae08ea7a54fee94804
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:38 +0000

    font-zen-kaku-gothic-antique updated

--- Formula/font-zen-kaku-gothic-antique.rb
+++ Formula/font-zen-kaku-gothic-antique.rb
@@ -1,8 +1,11 @@
 class FontZenKakuGothicAntique < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/zenkakugothicantique"
-  desc "Zen Kaku Gothic Antique"
   desc "Classical yet simple and stylish version"
   homepage "https://fonts.google.com/specimen/Zen+Kaku+Gothic+Antique"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/zenkakugothicantique"
+
   def install
     (share/"fonts").install Dir.glob("ofl/zenkakugothicantique/./**/ZenKakuGothicAntique-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/zenkakugothicantique/./**/ZenKakuGothicAntique-Bold.ttf")[0]
@@ -10,8 +13,8 @@ class FontZenKakuGothicAntique < Formula
     (share/"fonts").install Dir.glob("ofl/zenkakugothicantique/./**/ZenKakuGothicAntique-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/zenkakugothicantique/./**/ZenKakuGothicAntique-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
