8cddb208c33ef0240265b6374c00dbc6195e4d73
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-tac-one updated

--- Formula/font-tac-one.rb
+++ Formula/font-tac-one.rb
@@ -1,13 +1,14 @@
 class FontTacOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tacone/TacOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Tac One"
   desc "Project from afrotype"
   homepage "https://fonts.google.com/specimen/Tac+One"
+  head "https://github.com/google/fonts/raw/main/ofl/tacone/TacOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/TacOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
