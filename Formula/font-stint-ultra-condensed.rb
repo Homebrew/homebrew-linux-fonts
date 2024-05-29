c285ae283d5857bb4575925b7091dfa50715effc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-stint-ultra-condensed updated

--- Formula/font-stint-ultra-condensed.rb
+++ Formula/font-stint-ultra-condensed.rb
@@ -1,12 +1,14 @@
 class FontStintUltraCondensed < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/stintultracondensed/StintUltraCondensed-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Stint Ultra Condensed"
+  desc "Stint ultra condensed font"
   homepage "https://fonts.google.com/specimen/Stint+Ultra+Condensed"
+  head "https://github.com/google/fonts/raw/main/ofl/stintultracondensed/StintUltraCondensed-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/StintUltraCondensed-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
