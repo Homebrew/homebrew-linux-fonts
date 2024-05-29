749992466fc2165d32d9a3dee6bc77f195dddafe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-anonymice-powerline updated

--- Formula/font-anonymice-powerline.rb
+++ Formula/font-anonymice-powerline.rb
@@ -1,15 +1,18 @@
 class FontAnonymicePowerline < Formula
-  head "https://github.com/powerline/fonts.git", branch: "master", only_path: "AnonymousPro"
-  desc "Anonymice Powerline"
+  desc "Anonymice powerline font"
   homepage "https://github.com/powerline/fonts/tree/master/AnonymousPro"
+  head "https://github.com/powerline/fonts.git",
+       branch:    "master",
+       only_path: "AnonymousPro"
+
   def install
     (share/"fonts").install Dir.glob("AnonymousPro/./**/Anonymice Powerline Bold Italic.ttf")[0]
     (share/"fonts").install Dir.glob("AnonymousPro/./**/Anonymice Powerline Bold.ttf")[0]
     (share/"fonts").install Dir.glob("AnonymousPro/./**/Anonymice Powerline Italic.ttf")[0]
     (share/"fonts").install Dir.glob("AnonymousPro/./**/Anonymice Powerline.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
