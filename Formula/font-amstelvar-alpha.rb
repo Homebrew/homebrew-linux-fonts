15e7dadf3b3bbb312796c9e3ca0da83df714c8e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-amstelvar-alpha updated

--- Formula/font-amstelvar-alpha.rb
+++ Formula/font-amstelvar-alpha.rb
@@ -1,12 +1,14 @@
 class FontAmstelvarAlpha < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/amstelvaralpha/AmstelvarAlpha-VF.ttf", verified: "github.com/google/fonts/"
-  desc "Amstelvar Alpha"
+  desc "Amstelvar alpha font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts/raw/main/ofl/amstelvaralpha/AmstelvarAlpha-VF.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AmstelvarAlpha-VF.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
