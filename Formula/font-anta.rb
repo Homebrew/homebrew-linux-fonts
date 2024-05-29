c027863ae3965a8a035e6feb63ec00492a9e6047
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-anta updated

--- Formula/font-anta.rb
+++ Formula/font-anta.rb
@@ -1,13 +1,14 @@
 class FontAnta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anta/Anta-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Anta"
   desc "Particularly suitable for graphic design, but also for branding projects"
   homepage "https://fonts.google.com/specimen/Anta"
+  head "https://github.com/google/fonts/raw/main/ofl/anta/Anta-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Anta-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
