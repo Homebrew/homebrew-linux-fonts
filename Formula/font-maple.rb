349dc45be781fb877a5d32aac968603d676da832
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:37 +0000

    font-maple updated

--- Formula/font-maple.rb
+++ Formula/font-maple.rb
@@ -1,12 +1,10 @@
 class FontMaple < Formula
-  # Check on next version bump if the `container` stanza can be removed
-
-  version "6.4"
-  sha256 "7f07c594d6da5971428dc9e8ecd2b7759a80cec1cb87dadd1a44aadbac5cf6ac"
-  url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMono-otf.zip"
-  desc "Maple Mono"
   desc "Nerd Font font with round corners"
   homepage "https://github.com/subframe7536/Maple-font"
+  url "https://github.com/subframe7536/Maple-font/releases/download/v6.4/MapleMono-otf.zip"
+  version "6.4"
+  sha256 "7f07c594d6da5971428dc9e8ecd2b7759a80cec1cb87dadd1a44aadbac5cf6ac"
+
   def install
     (share/"fonts").install Dir.glob("./**/MapleMono-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/MapleMono-BoldItalic.otf")[0]
@@ -15,8 +13,8 @@ class FontMaple < Formula
     (share/"fonts").install Dir.glob("./**/MapleMono-LightItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/MapleMono-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
