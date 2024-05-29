af9b6b4be0fa7975692c70bd11a0bd44a316713b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-tharlon updated

--- Formula/font-tharlon.rb
+++ Formula/font-tharlon.rb
@@ -1,12 +1,14 @@
 class FontTharlon < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tharlon/Tharlon-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Tharlon"
+  desc "Tharlon font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts/raw/main/ofl/tharlon/Tharlon-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Tharlon-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
