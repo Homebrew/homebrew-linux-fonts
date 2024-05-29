c4c651dedadf5e72a976787eb238263d395416a0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-gelasio updated

--- Formula/font-gelasio.rb
+++ Formula/font-gelasio.rb
@@ -1,13 +1,17 @@
 class FontGelasio < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gelasio"
-  desc "Gelasio"
+  desc "Gelasio font"
   homepage "https://fonts.google.com/specimen/Gelasio"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gelasio"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/gelasio/./**/Gelasio-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/gelasio/./**/Gelasio\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/gelasio/./**/Gelasio-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/gelasio/./**/Gelasio[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
