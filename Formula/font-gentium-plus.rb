d81898a23331ad4c3f5f9b6900d815cafd779d48
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-gentium-plus updated

--- Formula/font-gentium-plus.rb
+++ Formula/font-gentium-plus.rb
@@ -1,22 +1,23 @@
 class FontGentiumPlus < Formula
-  version "6.200"
-  sha256 "9b21103b79961149b6508791572acb3b2fe7eb621474c57d5e4ee37e76d7b073"
-  url "https://github.com/silnrsi/font-gentium/releases/download/v#{version}/GentiumPlus-#{version}.zip", verified: "github.com/silnrsi/font-gentium/"
-  desc "Gentium"
   desc "Multilingual font"
   homepage "https://software.sil.org/gentium/"
+  url "https://github.com/silnrsi/font-gentium/releases/download/v6.200/GentiumPlus-6.200.zip",
+       verified: "github.com/silnrsi/font-gentium/"
+  version "6.200"
+  sha256 "9b21103b79961149b6508791572acb3b2fe7eb621474c57d5e4ee37e76d7b073"
+
   def install
-    (share/"fonts").install Dir.glob("GentiumPlus-#{version}/**/GentiumBookPlus-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumPlus-#{version}/**/GentiumBookPlus-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumPlus-#{version}/**/GentiumBookPlus-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumPlus-#{version}/**/GentiumBookPlus-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumPlus-#{version}/**/GentiumPlus-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumPlus-#{version}/**/GentiumPlus-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumPlus-#{version}/**/GentiumPlus-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("GentiumPlus-#{version}/**/GentiumPlus-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumPlus-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumBookPlus-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumBookPlus-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumBookPlus-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumPlus-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumPlus-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumPlus-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumBookPlus-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
