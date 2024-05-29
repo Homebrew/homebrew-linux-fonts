d973e5f15af2226e2b5f9870d8955a4fb6d0c2df
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-shadows-into-light-two updated

--- Formula/font-shadows-into-light-two.rb
+++ Formula/font-shadows-into-light-two.rb
@@ -1,12 +1,14 @@
 class FontShadowsIntoLightTwo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/shadowsintolighttwo/ShadowsIntoLightTwo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Shadows Into Light Two"
+  desc "Shadows into light two font"
   homepage "https://fonts.google.com/specimen/Shadows+Into+Light+Two"
+  head "https://github.com/google/fonts/raw/main/ofl/shadowsintolighttwo/ShadowsIntoLightTwo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ShadowsIntoLightTwo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
