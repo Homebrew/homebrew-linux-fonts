992d238e54bb3de86c4e29956aa2653bfe4aea90
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:49 +0000

    font-noto-sans-coptic updated

--- Formula/font-noto-sans-coptic.rb
+++ Formula/font-noto-sans-coptic.rb
@@ -1,12 +1,14 @@
 class FontNotoSansCoptic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCoptic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Coptic"
+  desc "Noto sans coptic font"
   homepage "https://www.google.com/get/noto/#sans-copt"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCoptic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansCoptic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
