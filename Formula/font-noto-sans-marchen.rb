208c4689777f13154009c45329bf610821de067e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-marchen updated

--- Formula/font-noto-sans-marchen.rb
+++ Formula/font-noto-sans-marchen.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMarchen < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmarchen/NotoSansMarchen-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Marchen"
+  desc "Noto sans marchen font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Marchen"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmarchen/NotoSansMarchen-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMarchen-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
