d4d63c3677f17babc887bc25af3f5711e03c23ca
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-moulpali updated

--- Formula/font-moulpali.rb
+++ Formula/font-moulpali.rb
@@ -1,12 +1,14 @@
 class FontMoulpali < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/moulpali/Moulpali-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Moulpali"
+  desc "Moulpali font"
   homepage "https://fonts.google.com/specimen/Moulpali"
+  head "https://github.com/google/fonts/raw/main/ofl/moulpali/Moulpali-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Moulpali-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
