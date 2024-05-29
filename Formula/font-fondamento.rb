82111b5649cade81be991b089a6c4a481d336220
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-fondamento updated

--- Formula/font-fondamento.rb
+++ Formula/font-fondamento.rb
@@ -1,13 +1,17 @@
 class FontFondamento < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fondamento"
-  desc "Fondamento"
+  desc "Fondamento font"
   homepage "https://fonts.google.com/specimen/Fondamento"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/fondamento"
+
   def install
     (share/"fonts").install Dir.glob("ofl/fondamento/./**/Fondamento-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/fondamento/./**/Fondamento-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
