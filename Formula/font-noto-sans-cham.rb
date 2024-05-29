fdfc8e1fdd6cf77c415d63302b253e207b2fabbc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-cham updated

--- Formula/font-noto-sans-cham.rb
+++ Formula/font-noto-sans-cham.rb
@@ -1,9 +1,11 @@
 class FontNotoSansCham < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCham-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Cham"
+  desc "Noto sans cham font"
   homepage "https://www.google.com/get/noto/#sans-cham"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCham-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansCham-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansCham-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCham-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCham-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCham-ExtraLight.ttf")[0]
@@ -11,10 +13,10 @@ class FontNotoSansCham < Formula
     (share/"fonts").install Dir.glob("./**/NotoSansCham-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCham-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCham-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/NotoSansCham-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansCham-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
