ef2f98c0d0e4722584408269f0d3133878ae1547
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-otomanopee-one updated

--- Formula/font-otomanopee-one.rb
+++ Formula/font-otomanopee-one.rb
@@ -1,12 +1,14 @@
 class FontOtomanopeeOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/otomanopeeone/OtomanopeeOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Otomanopee One"
+  desc "Otomanopee one font"
   homepage "https://fonts.google.com/specimen/Otomanopee+One"
+  head "https://github.com/google/fonts/raw/main/ofl/otomanopeeone/OtomanopeeOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/OtomanopeeOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
