ac886fa76522851fa63605ef97edfd3513cf2156
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-big-shoulders-stencil-display updated

--- Formula/font-big-shoulders-stencil-display.rb
+++ Formula/font-big-shoulders-stencil-display.rb
@@ -1,13 +1,14 @@
 class FontBigShouldersStencilDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bigshouldersstencildisplay/BigShouldersStencilDisplay%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Big Shoulders Stencil Display"
   desc "Superfamily of condensed American Gothic typefaces"
   homepage "https://fonts.google.com/specimen/Big+Shoulders+Stencil+Display"
+  head "https://github.com/google/fonts/raw/main/ofl/bigshouldersstencildisplay/BigShouldersStencilDisplay%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BigShouldersStencilDisplay\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BigShouldersStencilDisplay[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
