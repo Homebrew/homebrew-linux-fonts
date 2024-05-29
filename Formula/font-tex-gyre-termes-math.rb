760e8068643dbb0b0b8f77d6b2613716b9322efd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-tex-gyre-termes-math updated

--- Formula/font-tex-gyre-termes-math.rb
+++ Formula/font-tex-gyre-termes-math.rb
@@ -1,14 +1,15 @@
 class FontTexGyreTermesMath < Formula
+  desc "Termes math font"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tg-math"
+  url "https://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyretermes-math-1543.zip"
   version "1.543"
   sha256 "5875b39d3987cbe4258e5933a2292fbece870177053c5a8f495492769e2d4bb2"
-  url "https://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyretermes-math-#{version.to_s.gsub(".", "")}.zip"
-  desc "Termes Math"
-  homepage "https://www.gust.org.pl/projects/e-foundry/tg-math"
+
   def install
-    (share/"fonts").install Dir.glob("texgyretermes-math-1543/opentype/**/texgyretermes-math.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyretermes-math-1543/opentype/texgyretermes-math.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
