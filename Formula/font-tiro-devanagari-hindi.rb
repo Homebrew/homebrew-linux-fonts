3087dad77fe51f5fcc6b2b5246dc2d5d2cdb2d75
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-tiro-devanagari-hindi updated

--- Formula/font-tiro-devanagari-hindi.rb
+++ Formula/font-tiro-devanagari-hindi.rb
@@ -1,14 +1,17 @@
 class FontTiroDevanagariHindi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tirodevanagarihindi"
-  desc "Tiro Devanagari Hindi"
   desc "Broader proportions, generous counters, and strong diagonal strokes"
   homepage "https://fonts.google.com/specimen/Tiro+Devanagari+Hindi"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tirodevanagarihindi"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tirodevanagarihindi/./**/TiroDevanagariHindi-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tirodevanagarihindi/./**/TiroDevanagariHindi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
