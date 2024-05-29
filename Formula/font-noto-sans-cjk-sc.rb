31a02d6178c360bbe02c0afec74dd664ffb0b982
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:49 +0000

    font-noto-sans-cjk-sc updated

--- Formula/font-noto-sans-cjk-sc.rb
+++ Formula/font-noto-sans-cjk-sc.rb
@@ -1,10 +1,10 @@
 class FontNotoSansCjkSc < Formula
-  version "2.004"
-  sha256 "a927e56f53bd6c3b920bc139c0b94aa36c7d9ad0cf009b159437a1a003581140"
-  url "https://github.com/notofonts/noto-cjk/releases/download/Sans#{version}/08_NotoSansCJKsc.zip"
-  desc "Noto Sans CJK SC"
   desc "Language Specific OTFs Simplified Chinese (简体中文)"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Sans2.004/08_NotoSansCJKsc.zip"
+  version "2.004"
+  sha256 "a927e56f53bd6c3b920bc139c0b94aa36c7d9ad0cf009b159437a1a003581140"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansCJKsc-Black.otf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCJKsc-Bold.otf")[0]
@@ -14,8 +14,8 @@ class FontNotoSansCjkSc < Formula
     (share/"fonts").install Dir.glob("./**/NotoSansCJKsc-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCJKsc-Thin.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
