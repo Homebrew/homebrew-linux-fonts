9334b426d1fdc2ea9ba670ed530a00081ab3f752
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-alumni-sans-inline-one updated

--- Formula/font-alumni-sans-inline-one.rb
+++ Formula/font-alumni-sans-inline-one.rb
@@ -1,13 +1,17 @@
 class FontAlumniSansInlineOne < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/alumnisansinlineone"
-  desc "Alumni Sans Inline One"
+  desc "Alumni sans inline one font"
   homepage "https://fonts.google.com/specimen/Alumni+Sans+Inline+One"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/alumnisansinlineone"
+
   def install
     (share/"fonts").install Dir.glob("ofl/alumnisansinlineone/./**/AlumniSansInlineOne-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/alumnisansinlineone/./**/AlumniSansInlineOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
