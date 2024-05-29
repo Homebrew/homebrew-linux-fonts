478ab99acfe4263bcd8292699b498aaf68eb5d1b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-oooh-baby updated

--- Formula/font-oooh-baby.rb
+++ Formula/font-oooh-baby.rb
@@ -1,12 +1,14 @@
 class FontOoohBaby < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ooohbaby/OoohBaby-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Oooh Baby"
+  desc "Oooh baby font"
   homepage "https://fonts.google.com/specimen/Oooh+Baby"
+  head "https://github.com/google/fonts/raw/main/ofl/ooohbaby/OoohBaby-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/OoohBaby-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
