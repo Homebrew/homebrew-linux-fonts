d20b7b8bfab3917714a0255437819cae3339d28d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-merge-one updated

--- Formula/font-merge-one.rb
+++ Formula/font-merge-one.rb
@@ -1,12 +1,14 @@
 class FontMergeOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mergeone/MergeOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Merge One"
+  desc "Merge one font"
   homepage "https://fonts.google.com/specimen/Merge+One"
+  head "https://github.com/google/fonts/raw/main/ofl/mergeone/MergeOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MergeOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
