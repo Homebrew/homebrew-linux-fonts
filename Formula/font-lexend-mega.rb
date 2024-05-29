9bd1984d6e57463bab4c48a627495b0f8c1a797c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:32 +0000

    font-lexend-mega updated

--- Formula/font-lexend-mega.rb
+++ Formula/font-lexend-mega.rb
@@ -1,12 +1,14 @@
 class FontLexendMega < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lexendmega/LexendMega%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Lexend Mega"
+  desc "Lexend mega font"
   homepage "https://fonts.google.com/specimen/Lexend+Mega"
+  head "https://github.com/google/fonts/raw/main/ofl/lexendmega/LexendMega%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/LexendMega\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LexendMega[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
