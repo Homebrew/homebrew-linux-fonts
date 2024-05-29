5be236e38a632f7df9018aadc1d99cbde0896af7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-im-fell-double-pica-sc updated

--- Formula/font-im-fell-double-pica-sc.rb
+++ Formula/font-im-fell-double-pica-sc.rb
@@ -1,12 +1,14 @@
 class FontImFellDoublePicaSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/imfelldoublepicasc/IMFeDPsc28P.ttf", verified: "github.com/google/fonts/"
-  desc "IM Fell Double Pica SC"
+  desc "Im fell double pica sc font"
   homepage "https://fonts.google.com/specimen/IM+Fell+Double+Pica+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/imfelldoublepicasc/IMFeDPsc28P.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/IMFeDPsc28P.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
