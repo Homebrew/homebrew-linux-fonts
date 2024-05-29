629583945ea237723cfbd5baaea288763e3b77b4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-georama updated

--- Formula/font-georama.rb
+++ Formula/font-georama.rb
@@ -1,14 +1,17 @@
 class FontGeorama < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/georama"
-  desc "Georama"
   desc "Original typeface available in several widths and weights"
   homepage "https://fonts.google.com/specimen/Georama"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/georama"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/georama/./**/Georama-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/georama/./**/Georama\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/georama/./**/Georama-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/georama/./**/Georama[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
