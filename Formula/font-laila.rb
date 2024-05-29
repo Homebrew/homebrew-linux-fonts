e691093263130c386168d8391ab0625317708b38
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-laila updated

--- Formula/font-laila.rb
+++ Formula/font-laila.rb
@@ -1,9 +1,10 @@
 class FontLaila < Formula
+  desc "Laila font"
+  homepage "https://github.com/itfoundry/laila"
+  url "https://github.com/itfoundry/laila/releases/download/v2.000/laila-2_000.zip"
   version "2.000"
   sha256 "caa87cae373702288a7510c74881077aa15e3b5c94a4746b61f3f977d8499f73"
-  url "https://github.com/itfoundry/laila/releases/download/v#{version}/laila-#{version.to_s.gsub(".", "_")}.zip"
-  desc "Laila"
-  homepage "https://github.com/itfoundry/laila"
+
   def install
     (share/"fonts").install Dir.glob("./**/Laila-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/Laila-Light.otf")[0]
@@ -11,8 +12,8 @@ class FontLaila < Formula
     (share/"fonts").install Dir.glob("./**/Laila-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/Laila-SemiBold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
