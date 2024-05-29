44bf62beae78086ab78604060c90b6e5fdc29e6d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-bad-script updated

--- Formula/font-bad-script.rb
+++ Formula/font-bad-script.rb
@@ -1,12 +1,14 @@
 class FontBadScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/badscript/BadScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bad Script"
+  desc "Bad script font"
   homepage "https://fonts.google.com/specimen/Bad+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/badscript/BadScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BadScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
