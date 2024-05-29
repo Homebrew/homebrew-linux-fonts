d5dff331bb017a3b7a2dd0024fa207afba8b9cd2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-bm updated

--- Formula/font-bm.rb
+++ Formula/font-bm.rb
@@ -1,12 +1,14 @@
 class FontBm < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/hanna/BM-HANNA.ttf", verified: "github.com/google/fonts/"
-  desc "BM"
+  desc "Bm font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts/raw/main/ofl/hanna/BM-HANNA.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BM-HANNA.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
