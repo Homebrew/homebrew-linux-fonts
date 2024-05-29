085d52921b59be22448b335a7af476e331dea9de
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-gabriela updated

--- Formula/font-gabriela.rb
+++ Formula/font-gabriela.rb
@@ -1,12 +1,14 @@
 class FontGabriela < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gabriela/Gabriela-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gabriela"
+  desc "Gabriela font"
   homepage "https://fonts.google.com/specimen/Gabriela"
+  head "https://github.com/google/fonts/raw/main/ofl/gabriela/Gabriela-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Gabriela-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
