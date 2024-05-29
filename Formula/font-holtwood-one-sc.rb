396e1cb06d264552883f03c95c903237392a9364
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-holtwood-one-sc updated

--- Formula/font-holtwood-one-sc.rb
+++ Formula/font-holtwood-one-sc.rb
@@ -1,12 +1,14 @@
 class FontHoltwoodOneSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/holtwoodonesc/HoltwoodOneSC-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Holtwood One SC"
+  desc "Holtwood one sc font"
   homepage "https://fonts.google.com/specimen/Holtwood+One+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/holtwoodonesc/HoltwoodOneSC-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/HoltwoodOneSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
