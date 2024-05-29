dfe6ceb50f83d28a720343a97555738166dc7e87
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-bamum updated

--- Formula/font-noto-sans-bamum.rb
+++ Formula/font-noto-sans-bamum.rb
@@ -1,12 +1,14 @@
 class FontNotoSansBamum < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBamum-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Bamum"
+  desc "Noto sans bamum font"
   homepage "https://www.google.com/get/noto/#sans-bamu"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBamum-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansBamum-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
