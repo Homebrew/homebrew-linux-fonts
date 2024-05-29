0953b024762834d7e6cf72343eb1d1fc4c54bb1a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-jolly-lodger updated

--- Formula/font-jolly-lodger.rb
+++ Formula/font-jolly-lodger.rb
@@ -1,12 +1,14 @@
 class FontJollyLodger < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jollylodger/JollyLodger-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jolly Lodger"
+  desc "Jolly lodger font"
   homepage "https://fonts.google.com/specimen/Jolly+Lodger"
+  head "https://github.com/google/fonts/raw/main/ofl/jollylodger/JollyLodger-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JollyLodger-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
