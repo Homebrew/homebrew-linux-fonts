ae592a069eaf77737cd90ae4c02fbf9a9fe4ab7e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-anek-malayalam updated

--- Formula/font-anek-malayalam.rb
+++ Formula/font-anek-malayalam.rb
@@ -1,12 +1,14 @@
 class FontAnekMalayalam < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anekmalayalam/AnekMalayalam%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Anek Malayalam"
+  desc "Anek malayalam font"
   homepage "https://fonts.google.com/specimen/Anek+Malayalam"
+  head "https://github.com/google/fonts/raw/main/ofl/anekmalayalam/AnekMalayalam%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/AnekMalayalam\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekMalayalam[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
