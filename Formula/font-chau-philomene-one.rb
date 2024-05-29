39a4a05ac9bef6c6d55c5a6210a921bc8c7f42a7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-chau-philomene-one updated

--- Formula/font-chau-philomene-one.rb
+++ Formula/font-chau-philomene-one.rb
@@ -1,13 +1,17 @@
 class FontChauPhilomeneOne < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/chauphilomeneone"
-  desc "Chau Philomene One"
+  desc "Chau philomene one font"
   homepage "https://fonts.google.com/specimen/Chau+Philomene+One"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/chauphilomeneone"
+
   def install
     (share/"fonts").install Dir.glob("ofl/chauphilomeneone/./**/ChauPhilomeneOne-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/chauphilomeneone/./**/ChauPhilomeneOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
