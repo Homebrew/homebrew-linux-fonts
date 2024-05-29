d2ad0817d8b19f83f893c14f2a1a471ac9b9202b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:20 +0000

    font-ingrid-darling updated

--- Formula/font-ingrid-darling.rb
+++ Formula/font-ingrid-darling.rb
@@ -1,13 +1,14 @@
 class FontIngridDarling < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ingriddarling/IngridDarling-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ingrid Darling"
   desc "Based on a cursive hand writing style that has a playful, whimsical appeal"
   homepage "https://fonts.google.com/specimen/Ingrid+Darling"
+  head "https://github.com/google/fonts/raw/main/ofl/ingriddarling/IngridDarling-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/IngridDarling-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
