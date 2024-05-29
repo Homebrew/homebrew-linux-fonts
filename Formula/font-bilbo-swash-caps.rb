c8791a997f561ab950f3e42df4774bd40b1161e7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-bilbo-swash-caps updated

--- Formula/font-bilbo-swash-caps.rb
+++ Formula/font-bilbo-swash-caps.rb
@@ -1,12 +1,14 @@
 class FontBilboSwashCaps < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bilboswashcaps/BilboSwashCaps-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bilbo Swash Caps"
+  desc "Bilbo swash caps font"
   homepage "https://fonts.google.com/specimen/Bilbo+Swash+Caps"
+  head "https://github.com/google/fonts/raw/main/ofl/bilboswashcaps/BilboSwashCaps-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BilboSwashCaps-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
