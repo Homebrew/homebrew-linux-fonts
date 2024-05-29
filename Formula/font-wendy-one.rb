194eabf52eef966e4fe5bcfc182862a0785cf43c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-wendy-one updated

--- Formula/font-wendy-one.rb
+++ Formula/font-wendy-one.rb
@@ -1,12 +1,14 @@
 class FontWendyOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/wendyone/WendyOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Wendy One"
+  desc "Wendy one font"
   homepage "https://fonts.google.com/specimen/Wendy+One"
+  head "https://github.com/google/fonts/raw/main/ofl/wendyone/WendyOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/WendyOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
