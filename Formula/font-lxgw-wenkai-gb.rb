2a59aed219e22025c2ed1cbf3bc84359736ac1b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-lxgw-wenkai-gb updated

--- Formula/font-lxgw-wenkai-gb.rb
+++ Formula/font-lxgw-wenkai-gb.rb
@@ -1,21 +1,20 @@
 class FontLxgwWenkaiGb < Formula
-  version "1.330"
-  sha256 "4b842c2a12e6017f0e4495a12a364aefe1dd091edd8533c61a8518a8d1a2ffff"
-  url "https://github.com/lxgw/LxgwWenkaiGB/releases/download/v#{version}/lxgw-wenkai-gb-v#{version}.zip"
-  desc "LXGW WenKai GB"
-  desc "霞鹜文楷 GB"
   desc "Open-source Simplified Chinese font derived from Fontworks' Klee One"
   homepage "https://github.com/lxgw/LxgwWenkaiGB"
+  url "https://github.com/lxgw/LxgwWenkaiGB/releases/download/v1.330/lxgw-wenkai-gb-v1.330.zip"
+  version "1.330"
+  sha256 "4b842c2a12e6017f0e4495a12a364aefe1dd091edd8533c61a8518a8d1a2ffff"
+
   def install
-    (share/"fonts").install Dir.glob("lxgw-wenkai-gb-v#{version}/**/LXGWWenKaiMonoGB-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-gb-v#{version}/**/LXGWWenKaiMonoGB-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-gb-v#{version}/**/LXGWWenKaiMonoGB-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-gb-v#{version}/**/LXGWWenKaiGB-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-gb-v#{version}/**/LXGWWenKaiGB-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-gb-v#{version}/**/LXGWWenKaiGB-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-gb-v1.330/LXGWWenKaiMonoGB-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-gb-v1.330/LXGWWenKaiMonoGB-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-gb-v1.330/LXGWWenKaiMonoGB-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-gb-v1.330/LXGWWenKaiGB-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-gb-v1.330/LXGWWenKaiGB-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-gb-v1.330/LXGWWenKaiGB-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
