8dc28591a40a15be0ea75c621a04918b6640204c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-cinzel updated

--- Formula/font-cinzel.rb
+++ Formula/font-cinzel.rb
@@ -1,12 +1,14 @@
 class FontCinzel < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/cinzel/Cinzel%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Cinzel"
+  desc "Cinzel font"
   homepage "https://fonts.google.com/specimen/Cinzel"
+  head "https://github.com/google/fonts/raw/main/ofl/cinzel/Cinzel%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Cinzel\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Cinzel[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
