4344c1e28115bba93e1c8d65c76e3538345bcaa8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-sen updated

--- Formula/font-sen.rb
+++ Formula/font-sen.rb
@@ -1,12 +1,14 @@
 class FontSen < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sen/Sen%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Sen"
+  desc "Sen font"
   homepage "https://fonts.google.com/specimen/Sen"
+  head "https://github.com/google/fonts/raw/main/ofl/sen/Sen%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Sen\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Sen[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
