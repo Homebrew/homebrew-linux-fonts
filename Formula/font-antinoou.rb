489d023b7a6c89e84d14d165cf3603a20acb9a54
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-antinoou updated

--- Formula/font-antinoou.rb
+++ Formula/font-antinoou.rb
@@ -1,14 +1,16 @@
 class FontAntinoou < Formula
-  version "1.0.6"
-  url "https://www.evertype.com/fonts/coptic/AntinoouFont.zip"
-  desc "Antinoou"
+  desc "Antinoou font"
   homepage "https://www.evertype.com/fonts/coptic/"
+  url "https://www.evertype.com/fonts/coptic/AntinoouFont.zip"
+  version "1.0.6"
+  sha256 :no_check
+
   def install
-    (share/"fonts").install Dir.glob("AntinoouFont-#{version}/**/Antinoou.ttf")[0]
-    (share/"fonts").install Dir.glob("AntinoouFont-#{version}/**/AntinoouItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AntinoouFont-1.0.6/Antinoou.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AntinoouFont-1.0.6/AntinoouItalic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
