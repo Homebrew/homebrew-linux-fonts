9593c0a958a75172f52b917c2a70d2105170d2bb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-moul updated

--- Formula/font-moul.rb
+++ Formula/font-moul.rb
@@ -1,12 +1,14 @@
 class FontMoul < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/moul/Moul-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Moul"
+  desc "Moul font"
   homepage "https://fonts.google.com/specimen/Moul"
+  head "https://github.com/google/fonts/raw/main/ofl/moul/Moul-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Moul-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
