1a89a862a18b353e2b9cf41ffb1880b8dd287974
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:11 +0000

    font-protest-revolution updated

--- Formula/font-protest-revolution.rb
+++ Formula/font-protest-revolution.rb
@@ -1,12 +1,14 @@
 class FontProtestRevolution < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/protestrevolution/ProtestRevolution-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Protest Revolution"
+  desc "Protest revolution font"
   homepage "https://fonts.google.com/specimen/Protest+Revolution"
+  head "https://github.com/google/fonts/raw/main/ofl/protestrevolution/ProtestRevolution-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ProtestRevolution-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
