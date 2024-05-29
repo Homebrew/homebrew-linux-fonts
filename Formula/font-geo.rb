4f9cd8f53a94210a895233aff552a49e47232123
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-geo updated

--- Formula/font-geo.rb
+++ Formula/font-geo.rb
@@ -1,13 +1,17 @@
 class FontGeo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/geo"
-  desc "Geo"
+  desc "Geo font"
   homepage "https://fonts.google.com/specimen/Geo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/geo"
+
   def install
     (share/"fonts").install Dir.glob("ofl/geo/./**/Geo-Oblique.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/geo/./**/Geo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
