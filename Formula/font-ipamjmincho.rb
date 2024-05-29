e8f966f34b300b092d4271f4707470c0ea5d3f33
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-ipamjmincho updated

--- Formula/font-ipamjmincho.rb
+++ Formula/font-ipamjmincho.rb
@@ -1,15 +1,16 @@
 class FontIpamjmincho < Formula
+  desc "Ipamjmincho font"
+  homepage "https://moji.or.jp/mojikiban/font/"
+  url "https://dforest.watch.impress.co.jp/library/i/ipamjfont/10750/ipamjm00601.zip",
+       verified: "dforest.watch.impress.co.jp/library/i/ipamjfont/10750/"
   version "006.01"
   sha256 "35494e0f2896f38b3f7369a8421a895cea6440a42c0a66ac95eab47d6ed25b68"
-  url "https://dforest.watch.impress.co.jp/library/i/ipamjfont/10750/ipamjm#{version.to_s.gsub(".", "")}.zip", verified: "dforest.watch.impress.co.jp/library/i/ipamjfont/10750/"
-  desc "IPAmjMincho"
-  desc "IPAmj明朝"
-  homepage "https://moji.or.jp/mojikiban/font/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ipamjm.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
