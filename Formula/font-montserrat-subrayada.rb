b96feea8aa8bc10dc93de31f8a9a078d526967c3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-montserrat-subrayada updated

--- Formula/font-montserrat-subrayada.rb
+++ Formula/font-montserrat-subrayada.rb
@@ -1,13 +1,17 @@
 class FontMontserratSubrayada < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/montserratsubrayada"
-  desc "Montserrat Subrayada"
+  desc "Montserrat subrayada font"
   homepage "https://fonts.google.com/specimen/Montserrat+Subrayada"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/montserratsubrayada"
+
   def install
     (share/"fonts").install Dir.glob("ofl/montserratsubrayada/./**/MontserratSubrayada-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/montserratsubrayada/./**/MontserratSubrayada-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
