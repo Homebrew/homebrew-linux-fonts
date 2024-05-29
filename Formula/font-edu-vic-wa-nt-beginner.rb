1e6484b4cd9bae36d4e174737841fb4aebb0d5b4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-edu-vic-wa-nt-beginner updated

--- Formula/font-edu-vic-wa-nt-beginner.rb
+++ Formula/font-edu-vic-wa-nt-beginner.rb
@@ -1,12 +1,14 @@
 class FontEduVicWaNtBeginner < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/eduvicwantbeginner/EduVICWANTBeginner%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Edu VIC WA NT Beginner"
+  desc "Edu vic wa nt beginner font"
   homepage "https://fonts.google.com/specimen/Edu+VIC+WA+NT+Beginner"
+  head "https://github.com/google/fonts/raw/main/ofl/eduvicwantbeginner/EduVICWANTBeginner%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/EduVICWANTBeginner\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/EduVICWANTBeginner[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
