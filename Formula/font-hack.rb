1fb44ba32ec11dcc2ecd9bdb46c1876469ed81ce
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-hack updated

--- Formula/font-hack.rb
+++ Formula/font-hack.rb
@@ -1,17 +1,19 @@
 class FontHack < Formula
+  desc "Hack font"
+  homepage "https://sourcefoundry.org/hack/"
+  url "https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip",
+       verified: "github.com/source-foundry/Hack/"
   version "3.003"
   sha256 "0c2604631b1f055041c68a0e09ae4801acab6c5072ba2db6a822f53c3f8290ac"
-  url "https://github.com/source-foundry/Hack/releases/download/v#{version}/Hack-v#{version}-ttf.zip", verified: "github.com/source-foundry/Hack/"
-  desc "Hack"
-  homepage "https://sourcefoundry.org/hack/"
+
   def install
-    (share/"fonts").install Dir.glob("ttf/**/Hack-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("ttf/**/Hack-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("ttf/**/Hack-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ttf/**/Hack-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ttf/Hack-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ttf/Hack-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ttf/Hack-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ttf/Hack-BoldItalic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
