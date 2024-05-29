58581095c625c0d3bd2b17411f8fca6c2f7a3874
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:33 +0000

    font-lily-script-one updated

--- Formula/font-lily-script-one.rb
+++ Formula/font-lily-script-one.rb
@@ -1,12 +1,14 @@
 class FontLilyScriptOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lilyscriptone/LilyScriptOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Lily Script One"
+  desc "Lily script one font"
   homepage "https://fonts.google.com/specimen/Lily+Script+One"
+  head "https://github.com/google/fonts/raw/main/ofl/lilyscriptone/LilyScriptOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LilyScriptOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
