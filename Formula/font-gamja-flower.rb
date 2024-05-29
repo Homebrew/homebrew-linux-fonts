531f4223108a8f31b60742cb39b415fa24f91dea
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-gamja-flower updated

--- Formula/font-gamja-flower.rb
+++ Formula/font-gamja-flower.rb
@@ -1,12 +1,14 @@
 class FontGamjaFlower < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gamjaflower/GamjaFlower-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gamja Flower"
+  desc "Gamja flower font"
   homepage "https://fonts.google.com/specimen/Gamja+Flower"
+  head "https://github.com/google/fonts/raw/main/ofl/gamjaflower/GamjaFlower-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GamjaFlower-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
