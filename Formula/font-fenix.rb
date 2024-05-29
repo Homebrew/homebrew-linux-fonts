d1ec408f4092f365d62c4abf8a16135130bcf583
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:06 +0000

    font-fenix updated

--- Formula/font-fenix.rb
+++ Formula/font-fenix.rb
@@ -1,12 +1,14 @@
 class FontFenix < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/fenix/Fenix-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Fenix"
+  desc "Fenix font"
   homepage "https://fonts.google.com/specimen/Fenix"
+  head "https://github.com/google/fonts/raw/main/ofl/fenix/Fenix-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Fenix-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
