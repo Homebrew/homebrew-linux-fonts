f720f1faf91458b5177592a986bd003017528464
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:50 +0000

    font-cabin updated

--- Formula/font-cabin.rb
+++ Formula/font-cabin.rb
@@ -1,13 +1,17 @@
 class FontCabin < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cabin"
-  desc "Cabin"
+  desc "Cabin font"
   homepage "https://fonts.google.com/specimen/Cabin"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cabin"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/cabin/./**/Cabin-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/cabin/./**/Cabin\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/cabin/./**/Cabin-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/cabin/./**/Cabin[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
