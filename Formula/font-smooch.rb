876ba7f4052c00e4f891f07b2f9854fd4178918d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-smooch updated

--- Formula/font-smooch.rb
+++ Formula/font-smooch.rb
@@ -1,13 +1,14 @@
 class FontSmooch < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/smooch/Smooch-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Smooch"
   desc "Slightly bolder than other hand-lettered scripts"
   homepage "https://fonts.google.com/specimen/Smooch"
+  head "https://github.com/google/fonts/raw/main/ofl/smooch/Smooch-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Smooch-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
