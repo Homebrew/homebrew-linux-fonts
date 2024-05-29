8a607bc519d28cc34a3d00a0b63e705bc6df6b33
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-everson-mono updated

--- Formula/font-everson-mono.rb
+++ Formula/font-everson-mono.rb
@@ -1,16 +1,18 @@
 class FontEversonMono < Formula
-  version "7.0.0"
-  url "https://www.evertype.com/emono/evermono.zip"
-  desc "Everson Mono"
+  desc "Everson mono font"
   homepage "https://www.evertype.com/emono/"
+  url "https://www.evertype.com/emono/evermono.zip"
+  version "7.0.0"
+  sha256 :no_check
+
   def install
-    (share/"fonts").install Dir.glob("evermono-#{version}/**/Everson Mono.ttf")[0]
-    (share/"fonts").install Dir.glob("evermono-#{version}/**/Everson Mono Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("evermono-#{version}/**/Everson Mono Bold Oblique.ttf")[0]
-    (share/"fonts").install Dir.glob("evermono-#{version}/**/Everson Mono Oblique.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/evermono-7.0.0/Everson Mono.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/evermono-7.0.0/Everson Mono Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/evermono-7.0.0/Everson Mono Bold Oblique.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/evermono-7.0.0/Everson Mono Oblique.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
