273e05255c64895de7f345f9bb9e0d4b3cea4a8f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-varela updated

--- Formula/font-varela.rb
+++ Formula/font-varela.rb
@@ -1,12 +1,14 @@
 class FontVarela < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/varela/Varela-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Varela"
+  desc "Varela font"
   homepage "https://fonts.google.com/specimen/Varela"
+  head "https://github.com/google/fonts/raw/main/ofl/varela/Varela-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Varela-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
