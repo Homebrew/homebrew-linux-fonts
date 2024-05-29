c11ae06b25f3b7406135168870de8c034a8ac350
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-dream-han-sans updated

--- Formula/font-dream-han-sans.rb
+++ Formula/font-dream-han-sans.rb
@@ -1,12 +1,12 @@
 class FontDreamHanSans < Formula
-  version "3.01,2.004,2.002"
-  sha256 "7555b909965c4f32d9d757dfd7397365a4d7b2e8c149de6649c9d34a7bf8ed23"
-  url "https://github.com/Pal3love/dream-han-cjk/releases/download/dream-#{version.to_s.sub(/,.*/, "")}-sans-#{version.to_s.sub(/.*,/, "")}-serif-#{version.to_s.csv.third}/DreamHanSans.zip"
-  desc "Dream Han Sans"
   desc "Pan-CJK font families with range of weights"
   homepage "https://github.com/Pal3love/dream-han-cjk/"
+  url "https://github.com/Pal3love/dream-han-cjk/releases/download/dream-3.01-sans-2.004-serif-2.002/DreamHanSans.zip"
+  version "3.01,2.004,2.002"
+  sha256 "7555b909965c4f32d9d757dfd7397365a4d7b2e8c149de6649c9d34a7bf8ed23"
+
   def install
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W1.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W27.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W2.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W3.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W4.ttc")[0]
@@ -32,10 +32,10 @@ class FontDreamHanSans < Formula
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W24.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W25.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W26.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W27.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W1.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
