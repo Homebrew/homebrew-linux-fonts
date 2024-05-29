b1c7977cf8b839d3ce44270d87f0f9a6f06003ae
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:11 +0000

    font-pt-sans-caption updated

--- Formula/font-pt-sans-caption.rb
+++ Formula/font-pt-sans-caption.rb
@@ -1,13 +1,17 @@
 class FontPtSansCaption < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ptsanscaption"
-  desc "PT Sans Caption"
+  desc "Pt sans caption font"
   homepage "https://fonts.google.com/specimen/PT+Sans+Caption"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ptsanscaption"
+
   def install
     (share/"fonts").install Dir.glob("ofl/ptsanscaption/./**/PT_Sans-Caption-Web-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ptsanscaption/./**/PT_Sans-Caption-Web-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
