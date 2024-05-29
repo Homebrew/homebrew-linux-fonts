8edfd3d9c792f7137813075ffeb4edb6e3a8da4a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-judson updated

--- Formula/font-judson.rb
+++ Formula/font-judson.rb
@@ -1,14 +1,18 @@
 class FontJudson < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/judson"
-  desc "Judson"
+  desc "Judson font"
   homepage "https://fonts.google.com/specimen/Judson"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/judson"
+
   def install
     (share/"fonts").install Dir.glob("ofl/judson/./**/Judson-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/judson/./**/Judson-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/judson/./**/Judson-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
