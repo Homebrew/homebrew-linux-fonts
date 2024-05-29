3504fc3f434ccab9d9c7b8fa8d02de17f43a95eb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-arizonia updated

--- Formula/font-arizonia.rb
+++ Formula/font-arizonia.rb
@@ -1,12 +1,14 @@
 class FontArizonia < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/arizonia/Arizonia-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Arizonia"
+  desc "Arizonia font"
   homepage "https://fonts.google.com/specimen/Arizonia"
+  head "https://github.com/google/fonts/raw/main/ofl/arizonia/Arizonia-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Arizonia-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
