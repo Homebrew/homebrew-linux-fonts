c514a7a95cc450bc7f5ebb6315efb52d0fc76ef8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-aladin updated

--- Formula/font-aladin.rb
+++ Formula/font-aladin.rb
@@ -1,12 +1,14 @@
 class FontAladin < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/aladin/Aladin-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Aladin"
+  desc "Aladin font"
   homepage "https://fonts.google.com/specimen/Aladin"
+  head "https://github.com/google/fonts/raw/main/ofl/aladin/Aladin-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Aladin-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
