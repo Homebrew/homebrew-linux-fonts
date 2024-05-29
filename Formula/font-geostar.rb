1b0f4f942fa922b7adfd36364db799d43b2bc320
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-geostar updated

--- Formula/font-geostar.rb
+++ Formula/font-geostar.rb
@@ -1,12 +1,14 @@
 class FontGeostar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/geostar/Geostar-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Geostar"
+  desc "Geostar font"
   homepage "https://fonts.google.com/specimen/Geostar"
+  head "https://github.com/google/fonts/raw/main/ofl/geostar/Geostar-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Geostar-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
