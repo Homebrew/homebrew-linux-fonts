976aa2cffe184e1f8275663872b029ea6277b33f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-naskh-arabic updated

--- Formula/font-noto-naskh-arabic.rb
+++ Formula/font-noto-naskh-arabic.rb
@@ -1,13 +1,15 @@
 class FontNotoNaskhArabic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoNaskhArabic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Naskh Arabic"
+  desc "Noto naskh arabic font"
   homepage "https://www.google.com/get/noto/#naskh-arab"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoNaskhArabic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoNaskhArabic-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoNaskhArabic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
