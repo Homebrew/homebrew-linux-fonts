cc1bd0ba1f86ff2b35ee4def470e862158b288eb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-genos updated

--- Formula/font-genos.rb
+++ Formula/font-genos.rb
@@ -1,14 +1,17 @@
 class FontGenos < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/genos"
-  desc "Genos"
   desc "Modern display font with a futuristic feel"
   homepage "https://fonts.google.com/specimen/Genos"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/genos"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/genos/./**/Genos-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/genos/./**/Genos\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/genos/./**/Genos-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/genos/./**/Genos[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
