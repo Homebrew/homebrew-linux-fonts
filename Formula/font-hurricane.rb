b7054a1f3226b7f7b36af71b45ca4c8bdaaecc0c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-hurricane updated

--- Formula/font-hurricane.rb
+++ Formula/font-hurricane.rb
@@ -1,12 +1,14 @@
 class FontHurricane < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/hurricane/Hurricane-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Hurricane"
+  desc "Hurricane font"
   homepage "https://fonts.google.com/specimen/Hurricane"
+  head "https://github.com/google/fonts/raw/main/ofl/hurricane/Hurricane-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Hurricane-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
