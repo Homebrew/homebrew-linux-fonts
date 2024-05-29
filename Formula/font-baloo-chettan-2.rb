f874f91423f2a061b4f3ed03d1ba0d26edd740f5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-baloo-chettan-2 updated

--- Formula/font-baloo-chettan-2.rb
+++ Formula/font-baloo-chettan-2.rb
@@ -1,12 +1,14 @@
 class FontBalooChettan2 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/baloochettan2/BalooChettan2%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Baloo Chettan 2"
+  desc "Baloo chettan 2 font"
   homepage "https://fonts.google.com/specimen/Baloo+Chettan+2"
+  head "https://github.com/google/fonts/raw/main/ofl/baloochettan2/BalooChettan2%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BalooChettan2\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooChettan2[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
