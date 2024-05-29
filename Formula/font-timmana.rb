b35c0430ee4c2855484f328baf493397d435332c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-timmana updated

--- Formula/font-timmana.rb
+++ Formula/font-timmana.rb
@@ -1,12 +1,14 @@
 class FontTimmana < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/timmana/Timmana-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Timmana"
+  desc "Timmana font"
   homepage "https://fonts.google.com/specimen/Timmana"
+  head "https://github.com/google/fonts/raw/main/ofl/timmana/Timmana-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Timmana-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
