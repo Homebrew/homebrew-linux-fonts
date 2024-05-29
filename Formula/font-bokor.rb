7b3d9527f72a3bb6bf65ceaa3d45fe6a4456bac9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-bokor updated

--- Formula/font-bokor.rb
+++ Formula/font-bokor.rb
@@ -1,12 +1,14 @@
 class FontBokor < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bokor/Bokor-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bokor"
+  desc "Bokor font"
   homepage "https://fonts.google.com/specimen/Bokor"
+  head "https://github.com/google/fonts/raw/main/ofl/bokor/Bokor-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Bokor-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
