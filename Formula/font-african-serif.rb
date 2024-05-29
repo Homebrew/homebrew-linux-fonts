1f1b0f75b87e07e4e652b553fcf4ed8504befbe8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-african-serif updated

--- Formula/font-african-serif.rb
+++ Formula/font-african-serif.rb
@@ -1,16 +1,18 @@
 class FontAfricanSerif < Formula
-  version "9.380"
-  url "https://www.languagegeek.com/font/AfricanSerif.zip"
-  desc "African Serif"
+  desc "African serif font"
   homepage "https://www.languagegeek.com/font/fontdownload.html"
+  url "https://www.languagegeek.com/font/AfricanSerif.zip"
+  version "9.380"
+  sha256 :no_check
+
   def install
     (share/"fonts").install Dir.glob("./**/African Serif REGULAR 938.ttf")[0]
     (share/"fonts").install Dir.glob("./**/African Serif BOLD 938.ttf")[0]
     (share/"fonts").install Dir.glob("./**/African Serif BOLD ITALIC 938.ttf")[0]
     (share/"fonts").install Dir.glob("./**/African Serif ITALIC 938.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
