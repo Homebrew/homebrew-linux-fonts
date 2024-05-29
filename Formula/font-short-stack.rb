bb07120906dbd511b56998b0272679750c26a414
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-short-stack updated

--- Formula/font-short-stack.rb
+++ Formula/font-short-stack.rb
@@ -1,12 +1,14 @@
 class FontShortStack < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/shortstack/ShortStack-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Short Stack"
+  desc "Short stack font"
   homepage "https://fonts.google.com/specimen/Short+Stack"
+  head "https://github.com/google/fonts/raw/main/ofl/shortstack/ShortStack-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ShortStack-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
