965cea073c958486c040190178464284cb7c40db
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:10 +0000

    font-press-start-2p updated

--- Formula/font-press-start-2p.rb
+++ Formula/font-press-start-2p.rb
@@ -1,12 +1,14 @@
 class FontPressStart2p < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/pressstart2p/PressStart2P-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Press Start 2P"
+  desc "Press start 2p font"
   homepage "https://fonts.google.com/specimen/Press+Start+2P"
+  head "https://github.com/google/fonts/raw/main/ofl/pressstart2p/PressStart2P-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PressStart2P-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
