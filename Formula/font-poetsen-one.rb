39a9a6e0d016c61d0311c0f8e8cef7646d8b3ed6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:09 +0000

    font-poetsen-one updated

--- Formula/font-poetsen-one.rb
+++ Formula/font-poetsen-one.rb
@@ -1,12 +1,14 @@
 class FontPoetsenOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/poetsenone/PoetsenOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Poetsen One"
+  desc "Poetsen one font"
   homepage "https://fonts.google.com/specimen/Poetsen+One"
+  head "https://github.com/google/fonts/raw/main/ofl/poetsenone/PoetsenOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PoetsenOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
