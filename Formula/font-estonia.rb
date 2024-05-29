f2a2c4c583f91bc315f13ef9d1d321668ac5a031
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-estonia updated

--- Formula/font-estonia.rb
+++ Formula/font-estonia.rb
@@ -1,13 +1,14 @@
 class FontEstonia < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/estonia/Estonia-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Estonia"
   desc "Based on the calligraphic style found in the east european country of estonia"
   homepage "https://fonts.google.com/specimen/Estonia"
+  head "https://github.com/google/fonts/raw/main/ofl/estonia/Estonia-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Estonia-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
