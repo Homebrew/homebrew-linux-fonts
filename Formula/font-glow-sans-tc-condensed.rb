fb7cf7ab32bc8a8739b7b6a99d20318c8ea79b37
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:12 +0000

    font-glow-sans-tc-condensed updated

--- Formula/font-glow-sans-tc-condensed.rb
+++ Formula/font-glow-sans-tc-condensed.rb
@@ -1,12 +1,14 @@
 class FontGlowSansTcCondensed < Formula
+  desc "Glow sans tc condensed font"
+  homepage "https://github.com/welai/glow-sans"
+  url "https://github.com/welai/glow-sans/releases/download/v0.93/GlowSansTC-Condensed-v0.93.zip"
   version "0.93"
   sha256 "3ebe0b46abecd7a0f8d6232dd310205c38ab3d30ab624bdba18fae591bfd5d50"
-  url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansTC-Condensed-v#{version}.zip"
-  desc "Glow Sans TC Condensed"
-  homepage "https://github.com/welai/glow-sans"
-  deprecate! date: "2024-02-17", because: :unsupported
+
+  deprecate! "2024-02-17", because: :discontinued
+
   def install
-    (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-Thin.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-Book.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-ExtraBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-ExtraLight.otf")[0]
@@ -14,10 +16,10 @@ class FontGlowSansTcCondensed < Formula
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-Light.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansTC-Condensed-Bold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
