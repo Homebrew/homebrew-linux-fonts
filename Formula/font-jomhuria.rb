9813bcb9d59f4651214b947732c595f67698e9c0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-jomhuria updated

--- Formula/font-jomhuria.rb
+++ Formula/font-jomhuria.rb
@@ -1,12 +1,14 @@
 class FontJomhuria < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jomhuria/Jomhuria-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jomhuria"
+  desc "Jomhuria font"
   homepage "https://fonts.google.com/specimen/Jomhuria"
+  head "https://github.com/google/fonts/raw/main/ofl/jomhuria/Jomhuria-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Jomhuria-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
