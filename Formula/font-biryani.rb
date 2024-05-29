1d219f0530e470fe39660967dc435b65c82f32b0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-biryani updated

--- Formula/font-biryani.rb
+++ Formula/font-biryani.rb
@@ -1,7 +1,11 @@
 class FontBiryani < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/biryani"
-  desc "Biryani"
+  desc "Biryani font"
   homepage "https://fonts.google.com/specimen/Biryani"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/biryani"
+
   def install
     (share/"fonts").install Dir.glob("ofl/biryani/./**/Biryani-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/biryani/./**/Biryani-Bold.ttf")[0]
@@ -11,8 +15,8 @@ class FontBiryani < Formula
     (share/"fonts").install Dir.glob("ofl/biryani/./**/Biryani-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/biryani/./**/Biryani-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
