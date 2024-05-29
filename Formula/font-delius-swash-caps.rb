3ccbe87c006af7b3c5a3b2cd23c9a5a05ec83ec5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:00 +0000

    font-delius-swash-caps updated

--- Formula/font-delius-swash-caps.rb
+++ Formula/font-delius-swash-caps.rb
@@ -1,12 +1,14 @@
 class FontDeliusSwashCaps < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/deliusswashcaps/DeliusSwashCaps-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Delius Swash Caps"
+  desc "Delius swash caps font"
   homepage "https://fonts.google.com/specimen/Delius+Swash+Caps"
+  head "https://github.com/google/fonts/raw/main/ofl/deliusswashcaps/DeliusSwashCaps-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DeliusSwashCaps-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
