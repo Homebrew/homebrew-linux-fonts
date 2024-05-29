97446f073c6effdc82a16b5317f744fa03638b9f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-shadows-into-light updated

--- Formula/font-shadows-into-light.rb
+++ Formula/font-shadows-into-light.rb
@@ -1,12 +1,14 @@
 class FontShadowsIntoLight < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/shadowsintolight/ShadowsIntoLight.ttf", verified: "github.com/google/fonts/"
-  desc "Shadows Into Light"
+  desc "Shadows into light font"
   homepage "https://fonts.google.com/specimen/Shadows+Into+Light"
+  head "https://github.com/google/fonts/raw/main/ofl/shadowsintolight/ShadowsIntoLight.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ShadowsIntoLight.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
