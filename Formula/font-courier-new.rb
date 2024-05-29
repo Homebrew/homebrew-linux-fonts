1f6076373c5e9be0ebc68ec9851b515d09e39b08
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-courier-new updated

--- Formula/font-courier-new.rb
+++ Formula/font-courier-new.rb
@@ -1,15 +1,16 @@
 class FontCourierNew < Formula
-  head "https://downloads.sourceforge.net/corefonts/courie32.exe"
-  desc "Courier New"
+  desc "Courier new font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  head "https://downloads.sourceforge.net/corefonts/courie32.exe"
+
   def install
     (share/"fonts").install Dir.glob("./**/cour.ttf")[0]
     (share/"fonts").install Dir.glob("./**/courbd.ttf")[0]
     (share/"fonts").install Dir.glob("./**/courbi.ttf")[0]
     (share/"fonts").install Dir.glob("./**/couri.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
