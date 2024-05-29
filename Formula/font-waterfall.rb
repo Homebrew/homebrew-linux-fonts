c0c11defc4662b0df32a5fb35577017296b60379
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-waterfall updated

--- Formula/font-waterfall.rb
+++ Formula/font-waterfall.rb
@@ -1,13 +1,14 @@
 class FontWaterfall < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/waterfall/Waterfall-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Waterfall"
   desc "Calligraphic script"
   homepage "https://fonts.google.com/specimen/Waterfall"
+  head "https://github.com/google/fonts/raw/main/ofl/waterfall/Waterfall-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Waterfall-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
