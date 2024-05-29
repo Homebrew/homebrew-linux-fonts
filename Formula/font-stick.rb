cbe7b8f88beeefe936f4d8d48c24d42f7f40499a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-stick updated

--- Formula/font-stick.rb
+++ Formula/font-stick.rb
@@ -1,13 +1,14 @@
 class FontStick < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/stick/Stick-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Stick"
   desc "Designed with straight lines that create a cute and playful feel"
   homepage "https://fonts.google.com/specimen/Stick"
+  head "https://github.com/google/fonts/raw/main/ofl/stick/Stick-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Stick-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
