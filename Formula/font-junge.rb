40658c1b9e9afe1fdb883b749ea43c12089627a5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-junge updated

--- Formula/font-junge.rb
+++ Formula/font-junge.rb
@@ -1,12 +1,14 @@
 class FontJunge < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/junge/Junge-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Junge"
+  desc "Junge font"
   homepage "https://fonts.google.com/specimen/Junge"
+  head "https://github.com/google/fonts/raw/main/ofl/junge/Junge-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Junge-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
