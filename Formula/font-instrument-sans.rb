74e3be39c915da3ea9584a8da62c774a73c1cd6b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:21 +0000

    font-instrument-sans updated

--- Formula/font-instrument-sans.rb
+++ Formula/font-instrument-sans.rb
@@ -1,13 +1,17 @@
 class FontInstrumentSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/instrumentsans"
-  desc "Instrument Sans"
+  desc "Instrument sans font"
   homepage "https://fonts.google.com/specimen/Instrument+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/instrumentsans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/instrumentsans/./**/InstrumentSans-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/instrumentsans/./**/InstrumentSans\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/instrumentsans/./**/InstrumentSans-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/instrumentsans/./**/InstrumentSans[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
