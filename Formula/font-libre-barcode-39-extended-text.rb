51ef19f8da89f949ace9899e425d884504a760d3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:32 +0000

    font-libre-barcode-39-extended-text updated

--- Formula/font-libre-barcode-39-extended-text.rb
+++ Formula/font-libre-barcode-39-extended-text.rb
@@ -1,12 +1,14 @@
 class FontLibreBarcode39ExtendedText < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/librebarcode39extendedtext/LibreBarcode39ExtendedText-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Libre Barcode 39 Extended Text"
+  desc "Libre barcode 39 extended text font"
   homepage "https://fonts.google.com/specimen/Libre+Barcode+39+Extended+Text"
+  head "https://github.com/google/fonts/raw/main/ofl/librebarcode39extendedtext/LibreBarcode39ExtendedText-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LibreBarcode39ExtendedText-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
