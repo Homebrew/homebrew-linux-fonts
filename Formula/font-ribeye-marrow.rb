233b82d1511804afc248aec2afe0e10de295c9cd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:15 +0000

    font-ribeye-marrow updated

--- Formula/font-ribeye-marrow.rb
+++ Formula/font-ribeye-marrow.rb
@@ -1,12 +1,14 @@
 class FontRibeyeMarrow < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ribeyemarrow/RibeyeMarrow-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ribeye Marrow"
+  desc "Ribeye marrow font"
   homepage "https://fonts.google.com/specimen/Ribeye+Marrow"
+  head "https://github.com/google/fonts/raw/main/ofl/ribeyemarrow/RibeyeMarrow-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RibeyeMarrow-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
