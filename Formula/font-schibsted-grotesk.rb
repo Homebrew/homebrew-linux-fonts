cbbbfd79a07c675365d5b3e70cb2ae9958db8f09
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-schibsted-grotesk updated

--- Formula/font-schibsted-grotesk.rb
+++ Formula/font-schibsted-grotesk.rb
@@ -1,14 +1,17 @@
 class FontSchibstedGrotesk < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/schibstedgrotesk"
-  desc "Schibsted Grotesk"
   desc "Digital-first font family crafted for user interfaces"
   homepage "https://fonts.google.com/specimen/Schibsted+Grotesk"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/schibstedgrotesk"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/schibstedgrotesk/./**/SchibstedGrotesk-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/schibstedgrotesk/./**/SchibstedGrotesk\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/schibstedgrotesk/./**/SchibstedGrotesk-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/schibstedgrotesk/./**/SchibstedGrotesk[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
