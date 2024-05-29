ae887cfe8718459d9ee3417564872a6654e72622
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-marmelad updated

--- Formula/font-marmelad.rb
+++ Formula/font-marmelad.rb
@@ -1,12 +1,14 @@
 class FontMarmelad < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/marmelad/Marmelad-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Marmelad"
+  desc "Marmelad font"
   homepage "https://fonts.google.com/specimen/Marmelad"
+  head "https://github.com/google/fonts/raw/main/ofl/marmelad/Marmelad-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Marmelad-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
