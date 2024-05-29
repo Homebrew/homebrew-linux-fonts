61dda3ba08423aa318b4e39e9295a48e0525bb69
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-rubik-80s-fade updated

--- Formula/font-rubik-80s-fade.rb
+++ Formula/font-rubik-80s-fade.rb
@@ -1,12 +1,14 @@
 class FontRubik80sFade < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubik80sfade/Rubik80sFade-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik 80s Fade"
+  desc "Rubik 80s fade font"
   homepage "https://fonts.google.com/specimen/Rubik+80s+Fade"
+  head "https://github.com/google/fonts/raw/main/ofl/rubik80sfade/Rubik80sFade-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Rubik80sFade-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
