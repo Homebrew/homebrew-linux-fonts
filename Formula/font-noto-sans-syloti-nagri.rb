d2ce000462a7c3e19e7fb582b23db6fb6302d919
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:56 +0000

    font-noto-sans-syloti-nagri updated

--- Formula/font-noto-sans-syloti-nagri.rb
+++ Formula/font-noto-sans-syloti-nagri.rb
@@ -1,12 +1,14 @@
 class FontNotoSansSylotiNagri < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSylotiNagri-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Syloti Nagri"
+  desc "Noto sans syloti nagri font"
   homepage "https://www.google.com/get/noto/#sans-sylo"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSylotiNagri-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansSylotiNagri-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
