68c2a882eaeb349d5612aa026bbd5561aa21120f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:33 +0000

    font-linefont updated

--- Formula/font-linefont.rb
+++ Formula/font-linefont.rb
@@ -1,13 +1,14 @@
 class FontLinefont < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/linefont/Linefont%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Linefont"
   desc "Variable font for rendering small to medium-scale line charts"
   homepage "https://fonts.google.com/specimen/Linefont"
+  head "https://github.com/google/fonts/raw/main/ofl/linefont/Linefont%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Linefont\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Linefont[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
