ea2e40a73a81ff5a510f175ecaecfea7d50f3eeb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-asap updated

--- Formula/font-asap.rb
+++ Formula/font-asap.rb
@@ -1,13 +1,17 @@
 class FontAsap < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/asap"
-  desc "Asap"
+  desc "Asap font"
   homepage "https://fonts.google.com/specimen/Asap"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/asap"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/asap/./**/Asap-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/asap/./**/Asap\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/asap/./**/Asap-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/asap/./**/Asap[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
