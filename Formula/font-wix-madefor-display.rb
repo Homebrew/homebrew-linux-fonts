094b39e80ca23b8e3f8584f1a2b12d440046f2dc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:36 +0000

    font-wix-madefor-display updated

--- Formula/font-wix-madefor-display.rb
+++ Formula/font-wix-madefor-display.rb
@@ -1,12 +1,14 @@
 class FontWixMadeforDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/wixmadefordisplay/WixMadeforDisplay%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Wix Madefor Display"
+  desc "Wix madefor display font"
   homepage "https://fonts.google.com/specimen/Wix+Madefor+Display"
+  head "https://github.com/google/fonts/raw/main/ofl/wixmadefordisplay/WixMadeforDisplay%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/WixMadeforDisplay\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/WixMadeforDisplay[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
