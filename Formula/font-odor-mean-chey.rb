4e14dd7c0da226772743e770406e9032d9f4b3cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-odor-mean-chey updated

--- Formula/font-odor-mean-chey.rb
+++ Formula/font-odor-mean-chey.rb
@@ -1,12 +1,14 @@
 class FontOdorMeanChey < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/odormeanchey/OdorMeanChey-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Odor Mean Chey"
+  desc "Odor mean chey font"
   homepage "https://fonts.google.com/specimen/Odor+Mean+Chey"
+  head "https://github.com/google/fonts/raw/main/ofl/odormeanchey/OdorMeanChey-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/OdorMeanChey-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
