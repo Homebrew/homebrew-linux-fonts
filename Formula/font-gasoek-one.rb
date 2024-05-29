34891b53c0c0d93834fc136b395d3f203e37c4c8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-gasoek-one updated

--- Formula/font-gasoek-one.rb
+++ Formula/font-gasoek-one.rb
@@ -1,12 +1,14 @@
 class FontGasoekOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gasoekone/GasoekOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gasoek One"
+  desc "Gasoek one font"
   homepage "https://fonts.google.com/specimen/Gasoek+One"
+  head "https://github.com/google/fonts/raw/main/ofl/gasoekone/GasoekOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GasoekOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
