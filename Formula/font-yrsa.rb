1be67afc4392336ad8baef553cbdd28c5a18204d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-yrsa updated

--- Formula/font-yrsa.rb
+++ Formula/font-yrsa.rb
@@ -1,13 +1,17 @@
 class FontYrsa < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/yrsa"
-  desc "Yrsa"
+  desc "Yrsa font"
   homepage "https://fonts.google.com/specimen/Yrsa"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/yrsa"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/yrsa/./**/Yrsa-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/yrsa/./**/Yrsa\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/yrsa/./**/Yrsa-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/yrsa/./**/Yrsa[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
