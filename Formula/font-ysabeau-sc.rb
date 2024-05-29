262e08d9771dd8f0b809a2e01b6271a91ac00dc0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-ysabeau-sc updated

--- Formula/font-ysabeau-sc.rb
+++ Formula/font-ysabeau-sc.rb
@@ -1,13 +1,14 @@
 class FontYsabeauSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ysabeausc/YsabeauSC%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Ysabeau SC"
   desc "Exercise in restraint"
   homepage "https://fonts.google.com/specimen/Ysabeau+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/ysabeausc/YsabeauSC%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/YsabeauSC\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/YsabeauSC[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
