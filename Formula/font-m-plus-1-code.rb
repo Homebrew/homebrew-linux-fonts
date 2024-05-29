5625eb45914ffe0af33ff2b4d9ce498582708622
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:36 +0000

    font-m-plus-1-code updated

--- Formula/font-m-plus-1-code.rb
+++ Formula/font-m-plus-1-code.rb
@@ -1,12 +1,14 @@
 class FontMPlus1Code < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mplus1code/MPLUS1Code%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "M PLUS 1 Code"
+  desc "M plus 1 code font"
   homepage "https://fonts.google.com/specimen/M+PLUS+1+Code"
+  head "https://github.com/google/fonts/raw/main/ofl/mplus1code/MPLUS1Code%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/MPLUS1Code\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MPLUS1Code[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
