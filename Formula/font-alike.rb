f3d502a328865afc5f8251463f2c183ee99fa584
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-alike updated

--- Formula/font-alike.rb
+++ Formula/font-alike.rb
@@ -1,12 +1,14 @@
 class FontAlike < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alike/Alike-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Alike"
+  desc "Alike font"
   homepage "https://fonts.google.com/specimen/Alike"
+  head "https://github.com/google/fonts/raw/main/ofl/alike/Alike-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Alike-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
