e3b5ca0d378c4f195de0c3f3b4767da59a1ccc06
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:00 +0000

    font-noto-serif-telugu updated

--- Formula/font-noto-serif-telugu.rb
+++ Formula/font-noto-serif-telugu.rb
@@ -1,13 +1,15 @@
 class FontNotoSerifTelugu < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifTelugu-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Serif Telugu"
+  desc "Noto serif telugu font"
   homepage "https://www.google.com/get/noto/#serif-telu"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifTelugu-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifTelugu-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifTelugu-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
