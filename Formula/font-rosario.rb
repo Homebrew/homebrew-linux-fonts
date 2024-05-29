a2e42d9e1f5a9e957d1132b3f37d584856fd41dc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-rosario updated

--- Formula/font-rosario.rb
+++ Formula/font-rosario.rb
@@ -1,13 +1,17 @@
 class FontRosario < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rosario"
-  desc "Rosario"
+  desc "Rosario font"
   homepage "https://fonts.google.com/specimen/Rosario"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/rosario"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/rosario/./**/Rosario-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/rosario/./**/Rosario\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/rosario/./**/Rosario-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/rosario/./**/Rosario[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
