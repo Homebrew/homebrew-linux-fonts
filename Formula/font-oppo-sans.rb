96b9568d833a2924d8f97609eb3a64e85ade5a25
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-oppo-sans updated

--- Formula/font-oppo-sans.rb
+++ Formula/font-oppo-sans.rb
@@ -1,16 +1,17 @@
 class FontOppoSans < Formula
-  head "https://static01.coloros.com/www/public/img/topic7/font-opposans.zip"
-  desc "OPPO Sans"
+  desc "Oppo sans font"
   homepage "https://www.coloros.com/index/newsDetail?id=72"
+  head "https://static01.coloros.com/www/public/img/topic7/font-opposans.zip"
+
   def install
-    (share/"fonts").install Dir.glob("3.0 Designer_分级/**/OPlusSans3-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("3.0 Designer_分级/**/OPlusSans3-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("3.0 Designer_分级/**/OPlusSans3-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("3.0 Designer_分级/**/OPlusSans3-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("3.0 Designer_分级/**/OPlusSans3-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/3.0 Designer_分级/OPlusSans3-ExtraLight.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/3.0 Designer_分级/OPlusSans3-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/3.0 Designer_分级/OPlusSans3-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/3.0 Designer_分级/OPlusSans3-Medium.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/3.0 Designer_分级/OPlusSans3-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
