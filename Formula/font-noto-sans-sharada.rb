a7fc7b95024f89f2f49308535f8b922b391783bc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-sharada updated

--- Formula/font-noto-sans-sharada.rb
+++ Formula/font-noto-sans-sharada.rb
@@ -1,12 +1,14 @@
 class FontNotoSansSharada < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanssharada/NotoSansSharada-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Sharada"
+  desc "Noto sans sharada font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Sharada"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanssharada/NotoSansSharada-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansSharada-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
