73e9f9cdbac9254ddac7d19e744291d8e0567c59
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-bengali-ui updated

--- Formula/font-noto-sans-bengali-ui.rb
+++ Formula/font-noto-sans-bengali-ui.rb
@@ -1,12 +1,14 @@
 class FontNotoSansBengaliUi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansbengaliui/NotoSansBengaliUI%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Bengali UI"
+  desc "Noto sans bengali ui font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Bengali+UI"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansbengaliui/NotoSansBengaliUI%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansBengaliUI\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansBengaliUI[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
