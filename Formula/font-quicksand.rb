0d5f5fcf4dd3d1097eedfdef110115ac0af48b56
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:12 +0000

    font-quicksand updated

--- Formula/font-quicksand.rb
+++ Formula/font-quicksand.rb
@@ -1,12 +1,14 @@
 class FontQuicksand < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/quicksand/Quicksand%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Quicksand"
+  desc "Quicksand font"
   homepage "https://fonts.google.com/specimen/Quicksand"
+  head "https://github.com/google/fonts/raw/main/ofl/quicksand/Quicksand%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Quicksand\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Quicksand[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
