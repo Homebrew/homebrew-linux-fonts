414233e370f954e63f2b7ff5f3d2e90e13f5045a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-averia-serif-libre updated

--- Formula/font-averia-serif-libre.rb
+++ Formula/font-averia-serif-libre.rb
@@ -1,7 +1,11 @@
 class FontAveriaSerifLibre < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/averiaseriflibre"
-  desc "Averia Serif Libre"
+  desc "Averia serif libre font"
   homepage "https://fonts.google.com/specimen/Averia+Serif+Libre"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/averiaseriflibre"
+
   def install
     (share/"fonts").install Dir.glob("ofl/averiaseriflibre/./**/AveriaSerifLibre-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/averiaseriflibre/./**/AveriaSerifLibre-BoldItalic.ttf")[0]
@@ -10,8 +14,8 @@ class FontAveriaSerifLibre < Formula
     (share/"fonts").install Dir.glob("ofl/averiaseriflibre/./**/AveriaSerifLibre-LightItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/averiaseriflibre/./**/AveriaSerifLibre-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
