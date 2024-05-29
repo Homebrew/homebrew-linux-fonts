460df93f9906f9fa682f77eef5b3c4bcb8784892
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-solway updated

--- Formula/font-solway.rb
+++ Formula/font-solway.rb
@@ -1,7 +1,11 @@
 class FontSolway < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/solway"
-  desc "Solway"
+  desc "Solway font"
   homepage "https://fonts.google.com/specimen/Solway"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/solway"
+
   def install
     (share/"fonts").install Dir.glob("ofl/solway/./**/Solway-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/solway/./**/Solway-ExtraBold.ttf")[0]
@@ -9,8 +13,8 @@ class FontSolway < Formula
     (share/"fonts").install Dir.glob("ofl/solway/./**/Solway-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/solway/./**/Solway-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
