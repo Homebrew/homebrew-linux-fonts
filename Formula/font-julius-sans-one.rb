48fbcb2fb56fc783c57942538fa3f5621a4c047e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-julius-sans-one updated

--- Formula/font-julius-sans-one.rb
+++ Formula/font-julius-sans-one.rb
@@ -1,12 +1,14 @@
 class FontJuliusSansOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/juliussansone/JuliusSansOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Julius Sans One"
+  desc "Julius sans one font"
   homepage "https://fonts.google.com/specimen/Julius+Sans+One"
+  head "https://github.com/google/fonts/raw/main/ofl/juliussansone/JuliusSansOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JuliusSansOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
