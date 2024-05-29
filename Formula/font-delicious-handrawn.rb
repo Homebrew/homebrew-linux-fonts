70eac7b2d2b70aeaaf5a8b12a04d2f7ac8306b13
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:00 +0000

    font-delicious-handrawn updated

--- Formula/font-delicious-handrawn.rb
+++ Formula/font-delicious-handrawn.rb
@@ -1,13 +1,14 @@
 class FontDeliciousHandrawn < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/delicioushandrawn/DeliciousHandrawn-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Delicious Handrawn"
   desc "Font inspired by agung rohmat's handwriting"
   homepage "https://fonts.google.com/specimen/Delicious+Handrawn"
+  head "https://github.com/google/fonts/raw/main/ofl/delicioushandrawn/DeliciousHandrawn-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DeliciousHandrawn-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
