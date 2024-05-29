9645cdacbf1775a14b7a776b4733c39371c40ce4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-red-hat-display updated

--- Formula/font-red-hat-display.rb
+++ Formula/font-red-hat-display.rb
@@ -1,13 +1,17 @@
 class FontRedHatDisplay < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/redhatdisplay"
-  desc "Red Hat Display"
+  desc "Red hat display font"
   homepage "https://fonts.google.com/specimen/Red+Hat+Display"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/redhatdisplay"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/redhatdisplay/./**/RedHatDisplay-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/redhatdisplay/./**/RedHatDisplay\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/redhatdisplay/./**/RedHatDisplay-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/redhatdisplay/./**/RedHatDisplay[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
