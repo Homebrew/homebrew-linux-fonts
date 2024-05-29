5f08ac258026b50ffdcbac2c200516c3abb5fb11
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-big-shoulders-inline-display updated

--- Formula/font-big-shoulders-inline-display.rb
+++ Formula/font-big-shoulders-inline-display.rb
@@ -1,13 +1,14 @@
 class FontBigShouldersInlineDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bigshouldersinlinedisplay/BigShouldersInlineDisplay%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Big Shoulders Inline Display"
   desc "Superfamily of condensed American Gothic typefaces"
   homepage "https://fonts.google.com/specimen/Big+Shoulders+Inline+Display"
+  head "https://github.com/google/fonts/raw/main/ofl/bigshouldersinlinedisplay/BigShouldersInlineDisplay%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BigShouldersInlineDisplay\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BigShouldersInlineDisplay[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
