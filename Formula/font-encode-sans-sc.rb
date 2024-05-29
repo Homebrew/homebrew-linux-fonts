07499aafc045ef416d12a3b79e3a17e44fbc071e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-encode-sans-sc updated

--- Formula/font-encode-sans-sc.rb
+++ Formula/font-encode-sans-sc.rb
@@ -1,12 +1,14 @@
 class FontEncodeSansSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/encodesanssc/EncodeSansSC%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Encode Sans SC"
+  desc "Encode sans sc font"
   homepage "https://fonts.google.com/specimen/Encode+Sans+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/encodesanssc/EncodeSansSC%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/EncodeSansSC\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/EncodeSansSC[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
