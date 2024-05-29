562609e63fb41658d7b0062e5622833547829965
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-atma updated

--- Formula/font-atma.rb
+++ Formula/font-atma.rb
@@ -1,7 +1,11 @@
 class FontAtma < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/atma"
-  desc "Atma"
+  desc "Atma font"
   homepage "https://fonts.google.com/specimen/Atma"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/atma"
+
   def install
     (share/"fonts").install Dir.glob("ofl/atma/./**/Atma-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/atma/./**/Atma-Light.ttf")[0]
@@ -9,8 +13,8 @@ class FontAtma < Formula
     (share/"fonts").install Dir.glob("ofl/atma/./**/Atma-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/atma/./**/Atma-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
