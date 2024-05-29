cb5120b4973e97c6c04d294841c89c26c90e0529
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:08 +0000

    font-playwrite-gb-s updated

--- Formula/font-playwrite-gb-s.rb
+++ Formula/font-playwrite-gb-s.rb
@@ -1,13 +1,16 @@
 class FontPlaywriteGbS < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/playwritegbs"
-  desc "Playwrite GB S"
+  desc "Playwrite gb s font"
   homepage "https://github.com/TypeTogether/Playwrite/"
+  head "https://github.com/google/fonts.git",
+       branch:    "main",
+       only_path: "ofl/playwritegbs"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/playwritegbs/./**/PlaywriteGBS-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/playwritegbs/./**/PlaywriteGBS\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playwritegbs/./**/PlaywriteGBS-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playwritegbs/./**/PlaywriteGBS[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
