51e9d0d2e906b02efd4f3167b6ebb833b1b4c29e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-arial-black updated

--- Formula/font-arial-black.rb
+++ Formula/font-arial-black.rb
@@ -1,12 +1,13 @@
 class FontArialBlack < Formula
-  head "https://downloads.sourceforge.net/corefonts/arialb32.exe"
-  desc "Arial Black"
+  desc "Arial black font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  head "https://downloads.sourceforge.net/corefonts/arialb32.exe"
+
   def install
     (share/"fonts").install Dir.glob("./**/AriBlk.TTF")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
