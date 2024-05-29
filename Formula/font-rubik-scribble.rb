81c0efa8ae0291cf80592ea6715dd044f4a2843c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:18 +0000

    font-rubik-scribble updated

--- Formula/font-rubik-scribble.rb
+++ Formula/font-rubik-scribble.rb
@@ -1,12 +1,14 @@
 class FontRubikScribble < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikscribble/RubikScribble-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Scribble"
+  desc "Rubik scribble font"
   homepage "https://fonts.google.com/specimen/Rubik+Scribble"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikscribble/RubikScribble-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikScribble-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
