d2f01f677c3d978ba5ea829b54fcf20bd177ba2a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-national-park updated

--- Formula/font-national-park.rb
+++ Formula/font-national-park.rb
@@ -1,16 +1,19 @@
 class FontNationalPark < Formula
-  version "1.000"
-  url "https://files.cargocollective.com/c206464/NationalPark.zip", verified: "cargocollective.com/"
-  desc "National Park"
+  desc "National park font"
   homepage "https://nationalparktypeface.com/"
+  url "https://files.cargocollective.com/c206464/NationalPark.zip",
+       verified: "cargocollective.com/"
+  version "1.000"
+  sha256 :no_check
+
   def install
     (share/"fonts").install Dir.glob("./**/NationalPark-Thin.otf")[0]
     (share/"fonts").install Dir.glob("./**/NationalPark-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/NationalPark-Outline.otf")[0]
     (share/"fonts").install Dir.glob("./**/NationalPark-Heavy.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
