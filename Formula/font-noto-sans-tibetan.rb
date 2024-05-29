df1e16e81fc1d5caa41711492981986fecfcaa25
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-tibetan updated

--- Formula/font-noto-sans-tibetan.rb
+++ Formula/font-noto-sans-tibetan.rb
@@ -1,13 +1,15 @@
 class FontNotoSansTibetan < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTibetan-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Tibetan"
+  desc "Noto sans tibetan font"
   homepage "https://www.google.com/get/noto/#sans-tibt"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTibetan-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansTibetan-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansTibetan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
