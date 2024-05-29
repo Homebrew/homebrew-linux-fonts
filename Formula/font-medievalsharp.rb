ad95967ed8006c0856218a3aad3a7832649b8d25
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-medievalsharp updated

--- Formula/font-medievalsharp.rb
+++ Formula/font-medievalsharp.rb
@@ -1,12 +1,14 @@
 class FontMedievalsharp < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/medievalsharp/MedievalSharp.ttf", verified: "github.com/google/fonts/"
-  desc "MedievalSharp"
+  desc "Medievalsharp font"
   homepage "https://fonts.google.com/specimen/MedievalSharp"
+  head "https://github.com/google/fonts/raw/main/ofl/medievalsharp/MedievalSharp.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MedievalSharp.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
