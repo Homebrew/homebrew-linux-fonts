895151ce72daad08b0d208ad28a7ba6aaecd8973
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-redacted updated

--- Formula/font-redacted.rb
+++ Formula/font-redacted.rb
@@ -1,15 +1,16 @@
 class FontRedacted < Formula
-  head "https://github.com/christiannaths/Redacted-Font/archive/old-sources.zip"
-  desc "Redacted"
+  desc "Redacted font"
   homepage "https://github.com/christiannaths/Redacted-Font"
+  head "https://github.com/christiannaths/Redacted-Font/archive/old-sources.zip"
+
   def install
-    (share/"fonts").install Dir.glob("Redacted-Font-old-sources/fonts/**/redacted-regular.ttf")[0]
-    (share/"fonts").install Dir.glob("Redacted-Font-old-sources/fonts/**/redacted-script-bold.ttf")[0]
-    (share/"fonts").install Dir.glob("Redacted-Font-old-sources/fonts/**/redacted-script-light.ttf")[0]
-    (share/"fonts").install Dir.glob("Redacted-Font-old-sources/fonts/**/redacted-script-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Redacted-Font-old-sources/fonts/redacted-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Redacted-Font-old-sources/fonts/redacted-script-bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Redacted-Font-old-sources/fonts/redacted-script-light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Redacted-Font-old-sources/fonts/redacted-script-regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
