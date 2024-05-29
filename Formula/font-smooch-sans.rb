6a4e2a2b89352f65ca7b241769133e8c8b1afa4f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-smooch-sans updated

--- Formula/font-smooch-sans.rb
+++ Formula/font-smooch-sans.rb
@@ -1,12 +1,14 @@
 class FontSmoochSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/smoochsans/SmoochSans%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Smooch Sans"
+  desc "Smooch sans font"
   homepage "https://fonts.google.com/specimen/Smooch+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/smoochsans/SmoochSans%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/SmoochSans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/SmoochSans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
