590d0d4043729014e6f44129568ec9384e353b77
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-sarasa-gothic updated

--- Formula/font-sarasa-gothic.rb
+++ Formula/font-sarasa-gothic.rb
@@ -1,16 +1,12 @@
 class FontSarasaGothic < Formula
-  version "1.0.13"
-  sha256 "20c1036706de2d6deabbe68ff3d114e31b805a48669868ecd3b5bb788da86ac9"
-  url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/Sarasa-TTC-#{version}.7z"
-  desc "Sarasa Gothic"
-  desc "更纱黑体"
-  desc "更紗黑體"
-  desc "更紗ゴシック"
-  desc "사라사고딕"
   desc "CJK programming font based on Iosevka and Source Han Sans"
   homepage "https://github.com/be5invis/Sarasa-Gothic"
+  url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v1.0.13/Sarasa-TTC-1.0.13.7z"
+  version "1.0.13"
+  sha256 "20c1036706de2d6deabbe68ff3d114e31b805a48669868ecd3b5bb788da86ac9"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Sarasa-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/Sarasa-SemiBoldItalic.ttc")[0]
     (share/"fonts").install Dir.glob("./**/Sarasa-BoldItalic.ttc")[0]
     (share/"fonts").install Dir.glob("./**/Sarasa-ExtraLight.ttc")[0]
     (share/"fonts").install Dir.glob("./**/Sarasa-ExtraLightItalic.ttc")[0]
@@ -19,10 +15,10 @@ class FontSarasaGothic < Formula
     (share/"fonts").install Dir.glob("./**/Sarasa-LightItalic.ttc")[0]
     (share/"fonts").install Dir.glob("./**/Sarasa-Regular.ttc")[0]
     (share/"fonts").install Dir.glob("./**/Sarasa-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/Sarasa-SemiBoldItalic.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/Sarasa-Bold.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
