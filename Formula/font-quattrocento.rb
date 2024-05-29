7b4d61f454a0354958f3caeb242bf1613dffa9c7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:12 +0000

    font-quattrocento updated

--- Formula/font-quattrocento.rb
+++ Formula/font-quattrocento.rb
@@ -1,13 +1,17 @@
 class FontQuattrocento < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/quattrocento"
-  desc "Quattrocento"
+  desc "Quattrocento font"
   homepage "https://fonts.google.com/specimen/Quattrocento"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/quattrocento"
+
   def install
     (share/"fonts").install Dir.glob("ofl/quattrocento/./**/Quattrocento-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/quattrocento/./**/Quattrocento-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
