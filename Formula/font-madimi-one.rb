e65077e861c250da40940cbea32322f8e42864dc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:36 +0000

    font-madimi-one updated

--- Formula/font-madimi-one.rb
+++ Formula/font-madimi-one.rb
@@ -1,12 +1,14 @@
 class FontMadimiOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/madimione/MadimiOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Madimi One"
+  desc "Madimi one font"
   homepage "https://fonts.google.com/specimen/Madimi+One"
+  head "https://github.com/google/fonts/raw/main/ofl/madimione/MadimiOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MadimiOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
