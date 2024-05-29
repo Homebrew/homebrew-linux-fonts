777bd665d342664ed705156290f3c3b827065a43
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-square-peg updated

--- Formula/font-square-peg.rb
+++ Formula/font-square-peg.rb
@@ -1,12 +1,14 @@
 class FontSquarePeg < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/squarepeg/SquarePeg-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Square Peg"
+  desc "Square peg font"
   homepage "https://fonts.google.com/specimen/Square+Peg"
+  head "https://github.com/google/fonts/raw/main/ofl/squarepeg/SquarePeg-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SquarePeg-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
