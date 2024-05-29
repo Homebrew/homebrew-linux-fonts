8f3a2ca3c6a61acad6c7d4acd93cbc52f831c8aa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-fruktur updated

--- Formula/font-fruktur.rb
+++ Formula/font-fruktur.rb
@@ -1,13 +1,17 @@
 class FontFruktur < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fruktur"
-  desc "Fruktur"
+  desc "Fruktur font"
   homepage "https://fonts.google.com/specimen/Fruktur"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/fruktur"
+
   def install
     (share/"fonts").install Dir.glob("ofl/fruktur/./**/Fruktur-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/fruktur/./**/Fruktur-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
