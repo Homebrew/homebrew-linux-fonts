99ce353ca8397772716695d98f860447aa2c082b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-asar updated

--- Formula/font-asar.rb
+++ Formula/font-asar.rb
@@ -1,12 +1,14 @@
 class FontAsar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/asar/Asar-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Asar"
+  desc "Asar font"
   homepage "https://fonts.google.com/specimen/Asar"
+  head "https://github.com/google/fonts/raw/main/ofl/asar/Asar-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Asar-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
