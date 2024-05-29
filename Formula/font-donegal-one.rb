42010ecee131c0bd8a715e3ac093f8cb84d03cc9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-donegal-one updated

--- Formula/font-donegal-one.rb
+++ Formula/font-donegal-one.rb
@@ -1,12 +1,14 @@
 class FontDonegalOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/donegalone/DonegalOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Donegal One"
+  desc "Donegal one font"
   homepage "https://fonts.google.com/specimen/Donegal+One"
+  head "https://github.com/google/fonts/raw/main/ofl/donegalone/DonegalOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DonegalOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
