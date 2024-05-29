205bfefbbfc585b3df8fb9f4d29e5e617851f9be
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-hind-kochi updated

--- Formula/font-hind-kochi.rb
+++ Formula/font-hind-kochi.rb
@@ -1,7 +1,11 @@
 class FontHindKochi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/hindkochi"
-  desc "Hind Kochi"
+  desc "Hind kochi font"
   homepage "https://fonts.google.com/specimen/Hind"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/hindkochi"
+
   def install
     (share/"fonts").install Dir.glob("ofl/hindkochi/./**/HindKochi-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hindkochi/./**/HindKochi-Light.ttf")[0]
@@ -9,8 +13,8 @@ class FontHindKochi < Formula
     (share/"fonts").install Dir.glob("ofl/hindkochi/./**/HindKochi-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hindkochi/./**/HindKochi-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
