531bb230e82fc84e4d5e6a69be968890a545d105
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-sonsie-one updated

--- Formula/font-sonsie-one.rb
+++ Formula/font-sonsie-one.rb
@@ -1,12 +1,14 @@
 class FontSonsieOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sonsieone/SonsieOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sonsie One"
+  desc "Sonsie one font"
   homepage "https://fonts.google.com/specimen/Sonsie+One"
+  head "https://github.com/google/fonts/raw/main/ofl/sonsieone/SonsieOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SonsieOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
