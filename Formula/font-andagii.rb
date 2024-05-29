39b2bd4b9519d9c5a0af4763f9eed6a2f4ac6a5f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-andagii updated

--- Formula/font-andagii.rb
+++ Formula/font-andagii.rb
@@ -1,12 +1,14 @@
 class FontAndagii < Formula
-  head "http://www.i18nguy.com/unicode/andagii.zip", user_agent: :fake
-  desc "Andagii"
+  desc "Andagii font"
   homepage "http://www.i18nguy.com/unicode/unicode-font.html"
+  head "http://www.i18nguy.com/unicode/andagii.zip",
+       user_agent: ":fake"
+
   def install
     (share/"fonts").install Dir.glob("./**/ANDAGII_.TTF")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
