9cc16880ba0957650f7734cce2c6d1469bfd6652
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-contrail-one updated

--- Formula/font-contrail-one.rb
+++ Formula/font-contrail-one.rb
@@ -1,12 +1,14 @@
 class FontContrailOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/contrailone/ContrailOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Contrail One"
+  desc "Contrail one font"
   homepage "https://fonts.google.com/specimen/Contrail+One"
+  head "https://github.com/google/fonts/raw/main/ofl/contrailone/ContrailOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ContrailOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
