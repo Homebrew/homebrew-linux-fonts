07c7d3e9f1562185b3d132d011235dbf8cd99db3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-jua updated

--- Formula/font-jua.rb
+++ Formula/font-jua.rb
@@ -1,12 +1,14 @@
 class FontJua < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jua/Jua-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jua"
+  desc "Jua font"
   homepage "https://fonts.google.com/specimen/Jua"
+  head "https://github.com/google/fonts/raw/main/ofl/jua/Jua-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Jua-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
