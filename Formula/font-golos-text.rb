8365c7d571e7589b80dce5fff7fb145098361ce8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-golos-text updated

--- Formula/font-golos-text.rb
+++ Formula/font-golos-text.rb
@@ -1,12 +1,14 @@
 class FontGolosText < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/golostext/GolosText%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Golos Text"
+  desc "Golos text font"
   homepage "https://fonts.google.com/specimen/Golos+Text"
+  head "https://github.com/google/fonts/raw/main/ofl/golostext/GolosText%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/GolosText\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GolosText[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
