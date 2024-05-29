c3d4a9ad6bb53e7f068a8dadd9a6de3932498914
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:18 +0000

    font-sacramento updated

--- Formula/font-sacramento.rb
+++ Formula/font-sacramento.rb
@@ -1,12 +1,14 @@
 class FontSacramento < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sacramento/Sacramento-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sacramento"
+  desc "Sacramento font"
   homepage "https://fonts.google.com/specimen/Sacramento"
+  head "https://github.com/google/fonts/raw/main/ofl/sacramento/Sacramento-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Sacramento-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
