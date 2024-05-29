0e0f299feadab11013ed2374798b1e53848a660a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-siemreap updated

--- Formula/font-siemreap.rb
+++ Formula/font-siemreap.rb
@@ -1,12 +1,14 @@
 class FontSiemreap < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/siemreap/Siemreap.ttf", verified: "github.com/google/fonts/"
-  desc "Siemreap"
+  desc "Siemreap font"
   homepage "https://fonts.google.com/specimen/Siemreap"
+  head "https://github.com/google/fonts/raw/main/ofl/siemreap/Siemreap.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Siemreap.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
