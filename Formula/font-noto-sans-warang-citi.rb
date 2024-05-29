6e5694d844779e593365bb78c4d8bea48d3d7cf3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:58 +0000

    font-noto-sans-warang-citi updated

--- Formula/font-noto-sans-warang-citi.rb
+++ Formula/font-noto-sans-warang-citi.rb
@@ -1,12 +1,14 @@
 class FontNotoSansWarangCiti < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanswarangciti/NotoSansWarangCiti-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Warang Citi"
+  desc "Noto sans warang citi font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Warang+Citi"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanswarangciti/NotoSansWarangCiti-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansWarangCiti-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
