0eee5a5574e7641e4f79e75462c0be5223ab3995
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-khand updated

--- Formula/font-khand.rb
+++ Formula/font-khand.rb
@@ -1,7 +1,11 @@
 class FontKhand < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/khand"
-  desc "Khand"
+  desc "Khand font"
   homepage "https://fonts.google.com/specimen/Khand"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/khand"
+
   def install
     (share/"fonts").install Dir.glob("ofl/khand/./**/Khand-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/khand/./**/Khand-Light.ttf")[0]
@@ -9,8 +13,8 @@ class FontKhand < Formula
     (share/"fonts").install Dir.glob("ofl/khand/./**/Khand-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/khand/./**/Khand-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
