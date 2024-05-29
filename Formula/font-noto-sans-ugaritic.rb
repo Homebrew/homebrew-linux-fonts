dc595799dbd2be37bc5af6b351abb22e703b6db6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:58 +0000

    font-noto-sans-ugaritic updated

--- Formula/font-noto-sans-ugaritic.rb
+++ Formula/font-noto-sans-ugaritic.rb
@@ -1,12 +1,14 @@
 class FontNotoSansUgaritic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansUgaritic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Ugaritic"
+  desc "Noto sans ugaritic font"
   homepage "https://www.google.com/get/noto/#sans-ugar"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansUgaritic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansUgaritic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
