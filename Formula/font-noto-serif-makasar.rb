d0eb389a3c6a0826fb4172268023165977501632
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:00 +0000

    font-noto-serif-makasar updated

--- Formula/font-noto-serif-makasar.rb
+++ Formula/font-noto-serif-makasar.rb
@@ -1,13 +1,14 @@
 class FontNotoSerifMakasar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifmakasar/NotoSerifMakasar-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Makasar"
   desc "Design for the historical southeast asian makasar script"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Makasar"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifmakasar/NotoSerifMakasar-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifMakasar-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
