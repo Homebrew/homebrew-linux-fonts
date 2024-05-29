917a0dcb19dc4abe9f5aacb54c4d3a62924a09eb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-rubik-bubbles updated

--- Formula/font-rubik-bubbles.rb
+++ Formula/font-rubik-bubbles.rb
@@ -1,12 +1,14 @@
 class FontRubikBubbles < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikbubbles/RubikBubbles-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Bubbles"
+  desc "Rubik bubbles font"
   homepage "https://fonts.google.com/specimen/Rubik+Bubbles"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikbubbles/RubikBubbles-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikBubbles-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
