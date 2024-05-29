220727f82e624c86158cc6e98e051afe46e994f7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-arima updated

--- Formula/font-arima.rb
+++ Formula/font-arima.rb
@@ -1,13 +1,14 @@
 class FontArima < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/arima/Arima%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Arima"
   desc "Led by ndiscover, a type design foundry based in portugal"
   homepage "https://fonts.google.com/specimen/Arima"
+  head "https://github.com/google/fonts/raw/main/ofl/arima/Arima%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Arima\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Arima[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
