efa73c5ffd483f45272bcdb5cdd65a25ba28ed63
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-do-hyeon updated

--- Formula/font-do-hyeon.rb
+++ Formula/font-do-hyeon.rb
@@ -1,12 +1,14 @@
 class FontDoHyeon < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/dohyeon/DoHyeon-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Do Hyeon"
+  desc "Do hyeon font"
   homepage "https://fonts.google.com/specimen/Do+Hyeon"
+  head "https://github.com/google/fonts/raw/main/ofl/dohyeon/DoHyeon-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DoHyeon-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
