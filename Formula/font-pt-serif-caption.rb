b0f4beb1f9ecdacb0f26850232eaab681b7967e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:11 +0000

    font-pt-serif-caption updated

--- Formula/font-pt-serif-caption.rb
+++ Formula/font-pt-serif-caption.rb
@@ -1,13 +1,17 @@
 class FontPtSerifCaption < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ptserifcaption"
-  desc "PT Serif Caption"
+  desc "Pt serif caption font"
   homepage "https://fonts.google.com/specimen/PT+Serif+Caption"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ptserifcaption"
+
   def install
     (share/"fonts").install Dir.glob("ofl/ptserifcaption/./**/PT_Serif-Caption-Web-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ptserifcaption/./**/PT_Serif-Caption-Web-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
