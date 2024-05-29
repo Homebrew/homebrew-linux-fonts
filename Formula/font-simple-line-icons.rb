3d56b4b24b743f8a94c96003d99b755ecc6ccee0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-simple-line-icons updated

--- Formula/font-simple-line-icons.rb
+++ Formula/font-simple-line-icons.rb
@@ -1,14 +1,16 @@
 class FontSimpleLineIcons < Formula
+  desc "Simple line icons font"
+  homepage "https://simplelineicons.github.io/"
+  url "https://github.com/thesabbir/simple-line-icons/archive/2.5.4.zip",
+       verified: "github.com/thesabbir/simple-line-icons/"
   version "2.5.4"
   sha256 "4e21668ca8c22b082e63e016a4565af1a3875322a08cfcceaa2d9baf8fc21b3b"
-  url "https://github.com/thesabbir/simple-line-icons/archive/#{version}.zip", verified: "github.com/thesabbir/simple-line-icons/"
-  desc "Simple Line Icons"
-  homepage "https://simplelineicons.github.io/"
+
   def install
-    (share/"fonts").install Dir.glob("simple-line-icons-#{version}/fonts/**/Simple-Line-Icons.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/simple-line-icons-2.5.4/fonts/Simple-Line-Icons.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
