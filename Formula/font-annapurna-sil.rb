3f90a5c2a57c6389a6467797626ae065cedc3898
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-annapurna-sil updated

--- Formula/font-annapurna-sil.rb
+++ Formula/font-annapurna-sil.rb
@@ -1,14 +1,17 @@
 class FontAnnapurnaSil < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/annapurnasil"
-  desc "Annapurna SIL"
   desc "Unicode-based font with support for systems that use the devanagari script"
   homepage "https://fonts.google.com/specimen/Annapurna+SIL"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/annapurnasil"
+
   def install
     (share/"fonts").install Dir.glob("ofl/annapurnasil/./**/AnnapurnaSIL-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/annapurnasil/./**/AnnapurnaSIL-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
