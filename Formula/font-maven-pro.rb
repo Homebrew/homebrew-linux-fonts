b0d0f63b94099b4606a27f0f07f68856f0b3c672
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-maven-pro updated

--- Formula/font-maven-pro.rb
+++ Formula/font-maven-pro.rb
@@ -1,12 +1,14 @@
 class FontMavenPro < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mavenpro/MavenPro%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Maven Pro"
+  desc "Maven pro font"
   homepage "https://fonts.google.com/specimen/Maven+Pro"
+  head "https://github.com/google/fonts/raw/main/ofl/mavenpro/MavenPro%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/MavenPro\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MavenPro[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
