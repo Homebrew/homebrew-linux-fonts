36f9ac45b1dd750ac32f272534cb5132269f7635
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:04 +0000

    font-palanquin-dark updated

--- Formula/font-palanquin-dark.rb
+++ Formula/font-palanquin-dark.rb
@@ -1,15 +1,19 @@
 class FontPalanquinDark < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/palanquindark"
-  desc "Palanquin Dark"
+  desc "Palanquin dark font"
   homepage "https://fonts.google.com/specimen/Palanquin+Dark"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/palanquindark"
+
   def install
     (share/"fonts").install Dir.glob("ofl/palanquindark/./**/PalanquinDark-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/palanquindark/./**/PalanquinDark-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/palanquindark/./**/PalanquinDark-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/palanquindark/./**/PalanquinDark-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
