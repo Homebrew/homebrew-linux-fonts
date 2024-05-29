029360d00d01e8e2708aff2e3f0e107bc0d47c82
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-masinahikan updated

--- Formula/font-masinahikan.rb
+++ Formula/font-masinahikan.rb
@@ -1,14 +1,15 @@
 class FontMasinahikan < Formula
-  head "https://www.languagegeek.com/font/Masinahikan.zip"
-  desc "Masinahikan"
+  desc "Masinahikan font"
   homepage "https://www.languagegeek.com/font/fontdownload.html"
+  head "https://www.languagegeek.com/font/Masinahikan.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/Masinahikan_h.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Masinahikan_h_Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Masinahikan_h_Semi.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
