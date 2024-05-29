df3d4b000812b4b9e3a0f384d1d5863340355cbc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-baloo-bhaijaan-2 updated

--- Formula/font-baloo-bhaijaan-2.rb
+++ Formula/font-baloo-bhaijaan-2.rb
@@ -1,12 +1,14 @@
 class FontBalooBhaijaan2 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/baloobhaijaan2/BalooBhaijaan2%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Baloo Bhaijaan 2"
+  desc "Baloo bhaijaan 2 font"
   homepage "https://fonts.google.com/specimen/Baloo+Bhaijaan+2"
+  head "https://github.com/google/fonts/raw/main/ofl/baloobhaijaan2/BalooBhaijaan2%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BalooBhaijaan2\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooBhaijaan2[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
