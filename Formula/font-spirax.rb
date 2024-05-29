ffc0c7feb5fd4a467f8cd43828031b9fdd7efcb6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-spirax updated

--- Formula/font-spirax.rb
+++ Formula/font-spirax.rb
@@ -1,12 +1,14 @@
 class FontSpirax < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/spirax/Spirax-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Spirax"
+  desc "Spirax font"
   homepage "https://fonts.google.com/specimen/Spirax"
+  head "https://github.com/google/fonts/raw/main/ofl/spirax/Spirax-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Spirax-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
