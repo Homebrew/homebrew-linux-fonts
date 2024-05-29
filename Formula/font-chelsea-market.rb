de203504343d0d805b7cab28a0e44cbc4b5f6b45
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-chelsea-market updated

--- Formula/font-chelsea-market.rb
+++ Formula/font-chelsea-market.rb
@@ -1,12 +1,14 @@
 class FontChelseaMarket < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/chelseamarket/ChelseaMarket-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Chelsea Market"
+  desc "Chelsea market font"
   homepage "https://fonts.google.com/specimen/Chelsea+Market"
+  head "https://github.com/google/fonts/raw/main/ofl/chelseamarket/ChelseaMarket-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ChelseaMarket-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
