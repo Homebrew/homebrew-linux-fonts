d3426b9be092f50be4e4ec48628645b6529949c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:31 +0000

    font-lemonada updated

--- Formula/font-lemonada.rb
+++ Formula/font-lemonada.rb
@@ -1,12 +1,14 @@
 class FontLemonada < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lemonada/Lemonada%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Lemonada"
+  desc "Lemonada font"
   homepage "https://fonts.google.com/specimen/Lemonada"
+  head "https://github.com/google/fonts/raw/main/ofl/lemonada/Lemonada%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Lemonada\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lemonada[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
