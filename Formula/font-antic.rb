f902e5e821260c6c436ac84bb089bd66a6a84cdf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:40 +0000

    font-antic updated

--- Formula/font-antic.rb
+++ Formula/font-antic.rb
@@ -1,12 +1,14 @@
 class FontAntic < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/antic/Antic-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Antic"
+  desc "Antic font"
   homepage "https://fonts.google.com/specimen/Antic"
+  head "https://github.com/google/fonts/raw/main/ofl/antic/Antic-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Antic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
