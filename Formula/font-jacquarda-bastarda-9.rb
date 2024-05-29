643e19cb914eac2ba7e933d2ccb110543c579148
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jacquarda-bastarda-9 updated

--- Formula/font-jacquarda-bastarda-9.rb
+++ Formula/font-jacquarda-bastarda-9.rb
@@ -1,12 +1,14 @@
 class FontJacquardaBastarda9 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jacquardabastarda9/JacquardaBastarda9-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jacquarda Bastarda 9"
+  desc "Jacquarda bastarda 9 font"
   homepage "https://fonts.google.com/specimen/Jacquarda+Bastarda+9"
+  head "https://github.com/google/fonts/raw/main/ofl/jacquardabastarda9/JacquardaBastarda9-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JacquardaBastarda9-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
