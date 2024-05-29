9e4064b68d4d4211a09ce80365d39021385525cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-labrada updated

--- Formula/font-labrada.rb
+++ Formula/font-labrada.rb
@@ -1,13 +1,17 @@
 class FontLabrada < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/labrada"
-  desc "Labrada"
+  desc "Labrada font"
   homepage "https://fonts.google.com/specimen/Labrada"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/labrada"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/labrada/./**/Labrada-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/labrada/./**/Labrada\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/labrada/./**/Labrada-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/labrada/./**/Labrada[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
