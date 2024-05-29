3c35f6a22388d414fcbf6ee548c6afb7c4b4a571
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-are-you-serious updated

--- Formula/font-are-you-serious.rb
+++ Formula/font-are-you-serious.rb
@@ -1,13 +1,14 @@
 class FontAreYouSerious < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/areyouserious/AreYouSerious-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Are You Serious"
   desc "Fun playful font with a very joyful spirit"
   homepage "https://fonts.google.com/specimen/Are+You+Serious"
+  head "https://github.com/google/fonts/raw/main/ofl/areyouserious/AreYouSerious-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AreYouSerious-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
