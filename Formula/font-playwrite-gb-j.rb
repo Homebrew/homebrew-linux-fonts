84ea39b6ca3ee4a4934abbf7d9681b8aaffe49e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:08 +0000

    font-playwrite-gb-j updated

--- Formula/font-playwrite-gb-j.rb
+++ Formula/font-playwrite-gb-j.rb
@@ -1,13 +1,16 @@
 class FontPlaywriteGbJ < Formula
-  head "https://github.com/google/fonts.git", branch: "main", only_path: "ofl/playwritegbj"
-  desc "Playwrite GB J"
+  desc "Playwrite gb j font"
   homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts.git",
+       branch:    "main",
+       only_path: "ofl/playwritegbj"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/playwritegbj/./**/PlaywriteGBJ-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/playwritegbj/./**/PlaywriteGBJ\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playwritegbj/./**/PlaywriteGBJ-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playwritegbj/./**/PlaywriteGBJ[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
