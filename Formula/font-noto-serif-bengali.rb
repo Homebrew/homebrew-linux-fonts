af4bb6b78723d9cf66266eb7131bebef3ec9eafd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:58 +0000

    font-noto-serif-bengali updated

--- Formula/font-noto-serif-bengali.rb
+++ Formula/font-noto-serif-bengali.rb
@@ -1,13 +1,15 @@
 class FontNotoSerifBengali < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifBengali-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Serif Bengali"
+  desc "Noto serif bengali font"
   homepage "https://www.google.com/get/noto/#serif-beng"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifBengali-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifBengali-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifBengali-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
