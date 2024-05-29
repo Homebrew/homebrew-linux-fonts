3f08db7f177ef86a1739917795578a399db785bd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-nanum-gothic updated

--- Formula/font-nanum-gothic.rb
+++ Formula/font-nanum-gothic.rb
@@ -1,14 +1,18 @@
 class FontNanumGothic < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/nanumgothic"
-  desc "Nanum Gothic"
+  desc "Nanum gothic font"
   homepage "https://fonts.google.com/specimen/Nanum+Gothic"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/nanumgothic"
+
   def install
     (share/"fonts").install Dir.glob("ofl/nanumgothic/./**/NanumGothic-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/nanumgothic/./**/NanumGothic-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/nanumgothic/./**/NanumGothic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
