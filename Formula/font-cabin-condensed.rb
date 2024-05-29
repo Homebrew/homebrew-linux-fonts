9cb8e794458827f5776ac4d2236041fcdd7d993f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:50 +0000

    font-cabin-condensed updated

--- Formula/font-cabin-condensed.rb
+++ Formula/font-cabin-condensed.rb
@@ -1,15 +1,19 @@
 class FontCabinCondensed < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cabincondensed"
-  desc "Cabin Condensed"
+  desc "Cabin condensed font"
   homepage "https://fonts.google.com/specimen/Cabin+Condensed"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cabincondensed"
+
   def install
     (share/"fonts").install Dir.glob("ofl/cabincondensed/./**/CabinCondensed-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cabincondensed/./**/CabinCondensed-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cabincondensed/./**/CabinCondensed-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cabincondensed/./**/CabinCondensed-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
