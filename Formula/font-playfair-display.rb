7c42e4487a793d295251cf874e52bde134ab2881
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:07 +0000

    font-playfair-display updated

--- Formula/font-playfair-display.rb
+++ Formula/font-playfair-display.rb
@@ -1,13 +1,17 @@
 class FontPlayfairDisplay < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/playfairdisplay"
-  desc "Playfair Display"
+  desc "Playfair display font"
   homepage "https://fonts.google.com/specimen/Playfair+Display"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/playfairdisplay"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/playfairdisplay/./**/PlayfairDisplay-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/playfairdisplay/./**/PlayfairDisplay\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playfairdisplay/./**/PlayfairDisplay-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playfairdisplay/./**/PlayfairDisplay[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
