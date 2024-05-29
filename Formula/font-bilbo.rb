87d144d62ac962d6efb14756d8788fed7b97570b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-bilbo updated

--- Formula/font-bilbo.rb
+++ Formula/font-bilbo.rb
@@ -1,12 +1,14 @@
 class FontBilbo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bilbo/Bilbo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bilbo"
+  desc "Bilbo font"
   homepage "https://fonts.google.com/specimen/Bilbo"
+  head "https://github.com/google/fonts/raw/main/ofl/bilbo/Bilbo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Bilbo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
