033ede043f106342b1d6896f386564d5263f2a97
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:36 +0000

    font-xkcd-script updated

--- Formula/font-xkcd-script.rb
+++ Formula/font-xkcd-script.rb
@@ -1,12 +1,13 @@
 class FontXkcdScript < Formula
-  head "https://github.com/ipython/xkcd-font/raw/master/xkcd-script/font/xkcd-script.ttf"
-  desc "xkcd-script"
+  desc "Xkcd-script font"
   homepage "https://github.com/ipython/xkcd-font"
+  head "https://github.com/ipython/xkcd-font/raw/master/xkcd-script/font/xkcd-script.ttf"
+
   def install
     (share/"fonts").install Dir.glob("./**/xkcd-script.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
