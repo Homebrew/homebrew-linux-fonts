0c18ac792acadc3e422f84471acdd2810d20d3c4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:45 +0000

    font-barriecito updated

--- Formula/font-barriecito.rb
+++ Formula/font-barriecito.rb
@@ -1,12 +1,14 @@
 class FontBarriecito < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/barriecito/Barriecito-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Barriecito"
+  desc "Barriecito font"
   homepage "https://fonts.google.com/specimen/Barriecito"
+  head "https://github.com/google/fonts/raw/main/ofl/barriecito/Barriecito-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Barriecito-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
