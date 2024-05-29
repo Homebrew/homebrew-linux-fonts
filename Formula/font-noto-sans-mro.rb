bda92394d51c19a73705b71d6279f7802d671661
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-mro updated

--- Formula/font-noto-sans-mro.rb
+++ Formula/font-noto-sans-mro.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMro < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmro/NotoSansMro-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Mro"
+  desc "Noto sans mro font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Mro"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmro/NotoSansMro-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMro-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
