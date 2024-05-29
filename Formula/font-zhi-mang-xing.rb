58bf9fc3a880eddd7fae8ca52e73c1ad090f2274
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:38 +0000

    font-zhi-mang-xing updated

--- Formula/font-zhi-mang-xing.rb
+++ Formula/font-zhi-mang-xing.rb
@@ -1,12 +1,14 @@
 class FontZhiMangXing < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/zhimangxing/ZhiMangXing-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Zhi Mang Xing"
+  desc "Zhi mang xing font"
   homepage "https://fonts.google.com/specimen/Zhi+Mang+Xing"
+  head "https://github.com/google/fonts/raw/main/ofl/zhimangxing/ZhiMangXing-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ZhiMangXing-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
