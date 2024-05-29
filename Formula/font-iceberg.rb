c56e9d0c5ee47737f4228f836ffd65e4af370208
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:18 +0000

    font-iceberg updated

--- Formula/font-iceberg.rb
+++ Formula/font-iceberg.rb
@@ -1,12 +1,14 @@
 class FontIceberg < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/iceberg/Iceberg-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Iceberg"
+  desc "Iceberg font"
   homepage "https://fonts.google.com/specimen/Iceberg"
+  head "https://github.com/google/fonts/raw/main/ofl/iceberg/Iceberg-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Iceberg-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
