835e77bbe7f3222db099c7ae6510a73a7fa688bf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-pigmo00 updated

--- Formula/font-pigmo00.rb
+++ Formula/font-pigmo00.rb
@@ -1,13 +1,13 @@
 class FontPigmo00 < Formula
-  head "https://moji-waku.com/download/pigmo00.zip"
-  desc "Pigmo 00"
   desc "Cute drawing styled Japanese font"
   homepage "https://moji-waku.com/pigmo/index.html"
+  head "https://moji-waku.com/download/pigmo00.zip"
+
   def install
-    (share/"fonts").install Dir.glob("pigmo00/**/Pigmo-00.otf")[0]
+    (share/"fonts").install Dir.glob("./**/pigmo00/Pigmo-00.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
