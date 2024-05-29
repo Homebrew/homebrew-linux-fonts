95d717d9ae7e00f4d3f46b8d99b491a821e5d7df
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-fjord-one updated

--- Formula/font-fjord-one.rb
+++ Formula/font-fjord-one.rb
@@ -1,12 +1,14 @@
 class FontFjordOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/fjordone/FjordOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Fjord One"
+  desc "Fjord one font"
   homepage "https://fonts.google.com/specimen/Fjord+One"
+  head "https://github.com/google/fonts/raw/main/ofl/fjordone/FjordOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/FjordOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
