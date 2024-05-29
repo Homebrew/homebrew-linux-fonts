88e6f13bb50852a129f9567812e00e4a196b7bba
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-namdhinggo updated

--- Formula/font-namdhinggo.rb
+++ Formula/font-namdhinggo.rb
@@ -1,7 +1,11 @@
 class FontNamdhinggo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/namdhinggo"
-  desc "Namdhinggo"
+  desc "Namdhinggo font"
   homepage "https://fonts.google.com/specimen/Namdhinggo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/namdhinggo"
+
   def install
     (share/"fonts").install Dir.glob("ofl/namdhinggo/./**/Namdhinggo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/namdhinggo/./**/Namdhinggo-ExtraBold.ttf")[0]
@@ -9,8 +13,8 @@ class FontNamdhinggo < Formula
     (share/"fonts").install Dir.glob("ofl/namdhinggo/./**/Namdhinggo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/namdhinggo/./**/Namdhinggo-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
