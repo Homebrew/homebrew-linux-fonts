3fd2fc349aebe494cc222ad10391c3c115abe31f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-whisper updated

--- Formula/font-whisper.rb
+++ Formula/font-whisper.rb
@@ -1,12 +1,14 @@
 class FontWhisper < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/whisper/Whisper-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Whisper"
+  desc "Whisper font"
   homepage "https://fonts.google.com/specimen/Whisper"
+  head "https://github.com/google/fonts/raw/main/ofl/whisper/Whisper-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Whisper-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
