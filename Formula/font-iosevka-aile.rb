edc4d0bfe998475131659d09b4936f46508ca08a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:21 +0000

    font-iosevka-aile updated

--- Formula/font-iosevka-aile.rb
+++ Formula/font-iosevka-aile.rb
@@ -1,12 +1,12 @@
 class FontIosevkaAile < Formula
-  version "30.1.1"
-  sha256 "9355dd82cc4e77afa029d8883791baa9307fad46d3013dc8937ec4893abc65ec"
-  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaAile-#{version}.zip"
-  desc "Iosevka Aile"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
+  url "https://github.com/be5invis/Iosevka/releases/download/v30.1.1/PkgTTC-IosevkaAile-30.1.1.zip"
+  version "30.1.1"
+  sha256 "9355dd82cc4e77afa029d8883791baa9307fad46d3013dc8937ec4893abc65ec"
+
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaAile-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaAile-Thin.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-ExtraBold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-ExtraLight.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-Heavy.ttc")[0]
@@ -14,10 +14,10 @@ class FontIosevkaAile < Formula
     (share/"fonts").install Dir.glob("./**/IosevkaAile-Medium.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-Regular.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaAile-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaAile-Bold.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
