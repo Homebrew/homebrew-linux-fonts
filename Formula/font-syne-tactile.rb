1841f86cd3d12fec5a84a745235a8f28de4cfdb5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-syne-tactile updated

--- Formula/font-syne-tactile.rb
+++ Formula/font-syne-tactile.rb
@@ -1,13 +1,14 @@
 class FontSyneTactile < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/synetactile/SyneTactile-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Syne Tactile"
   desc "Originally designed for the Art Center 'Synesthésie'"
   homepage "https://fonts.google.com/specimen/Syne+Tactile"
+  head "https://github.com/google/fonts/raw/main/ofl/synetactile/SyneTactile-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SyneTactile-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
