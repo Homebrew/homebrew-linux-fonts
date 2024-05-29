927dad5217c7e573e20242f439c923f341774f76
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-blaka updated

--- Formula/font-blaka.rb
+++ Formula/font-blaka.rb
@@ -1,13 +1,14 @@
 class FontBlaka < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/blaka/Blaka-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Blaka"
   desc "Currently limited to few applications like google chrome (version 98 or later)"
   homepage "https://fonts.google.com/specimen/Blaka"
+  head "https://github.com/google/fonts/raw/main/ofl/blaka/Blaka-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Blaka-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
