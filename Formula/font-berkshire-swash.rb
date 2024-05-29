d96b21741d869f7e50e2fe8546123b1e4ac2ad4e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-berkshire-swash updated

--- Formula/font-berkshire-swash.rb
+++ Formula/font-berkshire-swash.rb
@@ -1,12 +1,14 @@
 class FontBerkshireSwash < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/berkshireswash/BerkshireSwash-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Berkshire Swash"
+  desc "Berkshire swash font"
   homepage "https://fonts.google.com/specimen/Berkshire+Swash"
+  head "https://github.com/google/fonts/raw/main/ofl/berkshireswash/BerkshireSwash-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BerkshireSwash-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
