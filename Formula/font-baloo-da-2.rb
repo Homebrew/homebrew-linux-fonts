d20d0126d1aa5c7acd1786aa02be03c1034132b1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-baloo-da-2 updated

--- Formula/font-baloo-da-2.rb
+++ Formula/font-baloo-da-2.rb
@@ -1,12 +1,14 @@
 class FontBalooDa2 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/balooda2/BalooDa2%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Baloo Da 2"
+  desc "Baloo da 2 font"
   homepage "https://fonts.google.com/specimen/Baloo+Da+2"
+  head "https://github.com/google/fonts/raw/main/ofl/balooda2/BalooDa2%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BalooDa2\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooDa2[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
