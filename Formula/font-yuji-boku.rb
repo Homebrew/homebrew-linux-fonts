ee24a775b5662d451889a9c2ae2e24206cd72e0d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-yuji-boku updated

--- Formula/font-yuji-boku.rb
+++ Formula/font-yuji-boku.rb
@@ -1,13 +1,14 @@
 class FontYujiBoku < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yujiboku/YujiBoku-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yuji Boku"
   desc "New and joyful design"
   homepage "https://fonts.google.com/specimen/Yuji+Boku"
+  head "https://github.com/google/fonts/raw/main/ofl/yujiboku/YujiBoku-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/YujiBoku-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
