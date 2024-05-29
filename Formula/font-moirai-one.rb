5f0dc9ebe1a90e2b06df3e50d737c0076c360f75
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:42 +0000

    font-moirai-one updated

--- Formula/font-moirai-one.rb
+++ Formula/font-moirai-one.rb
@@ -1,12 +1,14 @@
 class FontMoiraiOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/moiraione/MoiraiOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Moirai One"
+  desc "Moirai one font"
   homepage "https://fonts.google.com/specimen/Moirai+One"
+  head "https://github.com/google/fonts/raw/main/ofl/moiraione/MoiraiOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MoiraiOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
