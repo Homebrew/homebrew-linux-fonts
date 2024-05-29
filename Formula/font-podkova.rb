aab7ef5ce9b53407b011b5db0a23602c8ed236f8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:09 +0000

    font-podkova updated

--- Formula/font-podkova.rb
+++ Formula/font-podkova.rb
@@ -1,12 +1,14 @@
 class FontPodkova < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/podkova/Podkova%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Podkova"
+  desc "Podkova font"
   homepage "https://fonts.google.com/specimen/Podkova"
+  head "https://github.com/google/fonts/raw/main/ofl/podkova/Podkova%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Podkova\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Podkova[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
