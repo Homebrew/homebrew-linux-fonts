8961622b29747c1b9b7ef39d80db32555d31d0b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-khitan-small-script updated

--- Formula/font-noto-serif-khitan-small-script.rb
+++ Formula/font-noto-serif-khitan-small-script.rb
@@ -1,13 +1,14 @@
 class FontNotoSerifKhitanSmallScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifkhitansmallscript/NotoSerifKhitanSmallScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Khitan Small Script"
   desc "Design for the historical east asian khitan small script script"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Khitan+Small+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifkhitansmallscript/NotoSerifKhitanSmallScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifKhitanSmallScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
