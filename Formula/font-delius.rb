49d837eeab3481e55944459c05576333fbbf5e60
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:00 +0000

    font-delius updated

--- Formula/font-delius.rb
+++ Formula/font-delius.rb
@@ -1,12 +1,14 @@
 class FontDelius < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/delius/Delius-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Delius"
+  desc "Delius font"
   homepage "https://fonts.google.com/specimen/Delius"
+  head "https://github.com/google/fonts/raw/main/ofl/delius/Delius-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Delius-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
