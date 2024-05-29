b163e864e541eca6f5ad457ceab0cddd877f3d52
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:21 +0000

    font-iosevka-etoile updated

--- Formula/font-iosevka-etoile.rb
+++ Formula/font-iosevka-etoile.rb
@@ -1,12 +1,12 @@
 class FontIosevkaEtoile < Formula
-  version "30.1.1"
-  sha256 "0456a9da8e87d57a01ebdfaee23d0f17158ed2985a395d91a592563fde0bef29"
-  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaEtoile-#{version}.zip"
-  desc "Iosevka Etoile"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
+  url "https://github.com/be5invis/Iosevka/releases/download/v30.1.1/PkgTTC-IosevkaEtoile-30.1.1.zip"
+  version "30.1.1"
+  sha256 "0456a9da8e87d57a01ebdfaee23d0f17158ed2985a395d91a592563fde0bef29"
+
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Thin.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaEtoile-ExtraBold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaEtoile-ExtraLight.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Heavy.ttc")[0]
@@ -14,10 +14,10 @@ class FontIosevkaEtoile < Formula
     (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Medium.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Regular.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaEtoile-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Bold.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
