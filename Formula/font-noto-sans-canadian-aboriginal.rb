4e1e1d5a18020b99fdf5c926d8b2cfc80cebfec1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-canadian-aboriginal updated

--- Formula/font-noto-sans-canadian-aboriginal.rb
+++ Formula/font-noto-sans-canadian-aboriginal.rb
@@ -1,12 +1,14 @@
 class FontNotoSansCanadianAboriginal < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCanadianAboriginal-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Canadian Aboriginal"
+  desc "Noto sans canadian aboriginal font"
   homepage "https://www.google.com/get/noto/#sans-cans"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCanadianAboriginal-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansCanadianAboriginal-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
