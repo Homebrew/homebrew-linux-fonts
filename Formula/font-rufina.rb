9cf9ceaac800c11e0904550b9033635d797040cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:18 +0000

    font-rufina updated

--- Formula/font-rufina.rb
+++ Formula/font-rufina.rb
@@ -1,13 +1,17 @@
 class FontRufina < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rufina"
-  desc "Rufina"
+  desc "Rufina font"
   homepage "https://fonts.google.com/specimen/Rufina"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/rufina"
+
   def install
     (share/"fonts").install Dir.glob("ofl/rufina/./**/Rufina-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/rufina/./**/Rufina-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
