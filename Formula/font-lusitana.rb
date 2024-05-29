646b2e91505bb4077e9d646ecc2e6668bf81ea4c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-lusitana updated

--- Formula/font-lusitana.rb
+++ Formula/font-lusitana.rb
@@ -1,13 +1,17 @@
 class FontLusitana < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lusitana"
-  desc "Lusitana"
+  desc "Lusitana font"
   homepage "https://fonts.google.com/specimen/Lusitana"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/lusitana"
+
   def install
     (share/"fonts").install Dir.glob("ofl/lusitana/./**/Lusitana-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/lusitana/./**/Lusitana-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
