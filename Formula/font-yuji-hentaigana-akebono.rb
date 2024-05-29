e364c73fc55d3d084b6c007745132eb048164eb2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-yuji-hentaigana-akebono updated

--- Formula/font-yuji-hentaigana-akebono.rb
+++ Formula/font-yuji-hentaigana-akebono.rb
@@ -1,12 +1,14 @@
 class FontYujiHentaiganaAkebono < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yujihentaiganaakebono/YujiHentaiganaAkebono-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yuji Hentaigana Akebono"
+  desc "Yuji hentaigana akebono font"
   homepage "https://fonts.google.com/specimen/Yuji+Hentaigana+Akebono"
+  head "https://github.com/google/fonts/raw/main/ofl/yujihentaiganaakebono/YujiHentaiganaAkebono-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/YujiHentaiganaAkebono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
