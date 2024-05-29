91333602e0d9e08807c107f770efec9a55587078
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-arya updated

--- Formula/font-arya.rb
+++ Formula/font-arya.rb
@@ -1,13 +1,17 @@
 class FontArya < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/arya"
-  desc "Arya"
+  desc "Arya font"
   homepage "https://fonts.google.com/specimen/Arya"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/arya"
+
   def install
     (share/"fonts").install Dir.glob("ofl/arya/./**/Arya-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/arya/./**/Arya-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
