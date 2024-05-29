b68f3ac42e64d59f1bf788ad2f85435c35d7f911
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-tapestry updated

--- Formula/font-tapestry.rb
+++ Formula/font-tapestry.rb
@@ -1,13 +1,14 @@
 class FontTapestry < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tapestry/Tapestry-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Tapestry"
   desc "Roman calligraphic family with a slight rustic and country appearance"
   homepage "https://fonts.google.com/specimen/Tapestry"
+  head "https://github.com/google/fonts/raw/main/ofl/tapestry/Tapestry-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Tapestry-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
