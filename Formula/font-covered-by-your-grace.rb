b66a0563d72f00b2ca20da6e53a5bb13be03692f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-covered-by-your-grace updated

--- Formula/font-covered-by-your-grace.rb
+++ Formula/font-covered-by-your-grace.rb
@@ -1,12 +1,14 @@
 class FontCoveredByYourGrace < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/coveredbyyourgrace/CoveredByYourGrace.ttf", verified: "github.com/google/fonts/"
-  desc "Covered By Your Grace"
+  desc "Covered by your grace font"
   homepage "https://fonts.google.com/specimen/Covered+By+Your+Grace"
+  head "https://github.com/google/fonts/raw/main/ofl/coveredbyyourgrace/CoveredByYourGrace.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/CoveredByYourGrace.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
