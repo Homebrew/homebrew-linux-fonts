35fe03e6f665cbb5f9938c030a71e82c54fa652d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-sirin-stencil updated

--- Formula/font-sirin-stencil.rb
+++ Formula/font-sirin-stencil.rb
@@ -1,12 +1,14 @@
 class FontSirinStencil < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sirinstencil/SirinStencil-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sirin Stencil"
+  desc "Sirin stencil font"
   homepage "https://fonts.google.com/specimen/Sirin+Stencil"
+  head "https://github.com/google/fonts/raw/main/ofl/sirinstencil/SirinStencil-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SirinStencil-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
