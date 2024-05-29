e09a41b65387dd6e3d56a95c920ad45d4271a64f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-biz-udpgothic updated

--- Formula/font-biz-udpgothic.rb
+++ Formula/font-biz-udpgothic.rb
@@ -1,13 +1,17 @@
 class FontBizUdpgothic < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/bizudpgothic"
-  desc "BIZ UDPGothic"
+  desc "Biz udpgothic font"
   homepage "https://fonts.google.com/specimen/BIZ+UDPGothic"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/bizudpgothic"
+
   def install
     (share/"fonts").install Dir.glob("ofl/bizudpgothic/./**/BIZUDPGothic-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/bizudpgothic/./**/BIZUDPGothic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
