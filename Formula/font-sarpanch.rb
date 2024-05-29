4b2a4760835e46856de778f647eb1c04483dbeb1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-sarpanch updated

--- Formula/font-sarpanch.rb
+++ Formula/font-sarpanch.rb
@@ -1,7 +1,11 @@
 class FontSarpanch < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sarpanch"
-  desc "Sarpanch"
+  desc "Sarpanch font"
   homepage "https://fonts.google.com/specimen/Sarpanch"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sarpanch"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sarpanch/./**/Sarpanch-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sarpanch/./**/Sarpanch-Bold.ttf")[0]
@@ -10,8 +14,8 @@ class FontSarpanch < Formula
     (share/"fonts").install Dir.glob("ofl/sarpanch/./**/Sarpanch-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sarpanch/./**/Sarpanch-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
