cdb2b6707051051491350681f944a40c00e5019f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-dotgothic16 updated

--- Formula/font-dotgothic16.rb
+++ Formula/font-dotgothic16.rb
@@ -1,12 +1,14 @@
 class FontDotgothic16 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/dotgothic16/DotGothic16-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "DotGothic16"
+  desc "Dotgothic16 font"
   homepage "https://fonts.google.com/specimen/DotGothic16"
+  head "https://github.com/google/fonts/raw/main/ofl/dotgothic16/DotGothic16-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DotGothic16-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
