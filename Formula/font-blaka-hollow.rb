bd11bd5572c0049f7ed03ad246f15e63de82aaff
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-blaka-hollow updated

--- Formula/font-blaka-hollow.rb
+++ Formula/font-blaka-hollow.rb
@@ -1,12 +1,14 @@
 class FontBlakaHollow < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/blakahollow/BlakaHollow-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Blaka Hollow"
+  desc "Blaka hollow font"
   homepage "https://fonts.google.com/specimen/Blaka+Hollow"
+  head "https://github.com/google/fonts/raw/main/ofl/blakahollow/BlakaHollow-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BlakaHollow-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
