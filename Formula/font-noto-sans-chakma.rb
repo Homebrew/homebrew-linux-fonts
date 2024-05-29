d68b783f5e4b8d717e626e0e2b8960f498200afe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-chakma updated

--- Formula/font-noto-sans-chakma.rb
+++ Formula/font-noto-sans-chakma.rb
@@ -1,12 +1,14 @@
 class FontNotoSansChakma < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansChakma-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Chakma"
+  desc "Noto sans chakma font"
   homepage "https://www.google.com/get/noto/#sans-cakm"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansChakma-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansChakma-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
