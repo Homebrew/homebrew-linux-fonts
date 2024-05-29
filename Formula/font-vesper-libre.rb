cb2da33ebba529526a59c4cb8648b68de19fc944
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-vesper-libre updated

--- Formula/font-vesper-libre.rb
+++ Formula/font-vesper-libre.rb
@@ -1,15 +1,19 @@
 class FontVesperLibre < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/vesperlibre"
-  desc "Vesper Libre"
+  desc "Vesper libre font"
   homepage "https://fonts.google.com/specimen/Vesper+Libre"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/vesperlibre"
+
   def install
     (share/"fonts").install Dir.glob("ofl/vesperlibre/./**/VesperLibre-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/vesperlibre/./**/VesperLibre-Heavy.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/vesperlibre/./**/VesperLibre-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/vesperlibre/./**/VesperLibre-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
