2186e7db44b0545ed8bcb054a893698578966a24
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-space-grotesk updated

--- Formula/font-space-grotesk.rb
+++ Formula/font-space-grotesk.rb
@@ -1,17 +1,18 @@
 class FontSpaceGrotesk < Formula
+  desc "Space grotesk font"
+  homepage "https://github.com/floriankarsten/space-grotesk"
+  url "https://github.com/floriankarsten/space-grotesk/releases/download/2.0.0/SpaceGrotesk-2.0.0.zip"
   version "2.0.0"
   sha256 "53b415577d4139248555300710bea0d268c7a5be67b93de53b716a9736cabffd"
-  url "https://github.com/floriankarsten/space-grotesk/releases/download/#{version}/SpaceGrotesk-#{version}.zip"
-  desc "Space Grotesk"
-  homepage "https://github.com/floriankarsten/space-grotesk"
+
   def install
-    (share/"fonts").install Dir.glob("SpaceGrotesk-#{version}/otf/**/SpaceGrotesk-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("SpaceGrotesk-#{version}/otf/**/SpaceGrotesk-Light.otf")[0]
-    (share/"fonts").install Dir.glob("SpaceGrotesk-#{version}/otf/**/SpaceGrotesk-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("SpaceGrotesk-#{version}/otf/**/SpaceGrotesk-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SpaceGrotesk-2.0.0/otf/SpaceGrotesk-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SpaceGrotesk-2.0.0/otf/SpaceGrotesk-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SpaceGrotesk-2.0.0/otf/SpaceGrotesk-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SpaceGrotesk-2.0.0/otf/SpaceGrotesk-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
