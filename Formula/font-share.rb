faf9c70d057c3d82747d9c1029854d4e74dac7cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-share updated

--- Formula/font-share.rb
+++ Formula/font-share.rb
@@ -1,15 +1,19 @@
 class FontShare < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/share"
-  desc "Share"
+  desc "Share font"
   homepage "https://fonts.google.com/specimen/Share"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/share"
+
   def install
     (share/"fonts").install Dir.glob("ofl/share/./**/Share-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/share/./**/Share-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/share/./**/Share-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/share/./**/Share-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
