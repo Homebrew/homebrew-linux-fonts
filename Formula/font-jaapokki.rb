c5c08c59a85a0c020ed753b94d1a8de744e4a79b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-jaapokki updated

--- Formula/font-jaapokki.rb
+++ Formula/font-jaapokki.rb
@@ -1,16 +1,17 @@
 class FontJaapokki < Formula
+  desc "Jaapokki font"
+  homepage "https://mikkonuuttila.com/jaapokki/"
+  url "https://mikkonuuttila.com/jaapokki/Jaapokki-font-package-1.0.zip"
   version "1.0"
   sha256 "aa5f38cbb682df8732d406996e37349b83e47d3c14bae4af2125f2a7784430b7"
-  url "https://mikkonuuttila.com/jaapokki/Jaapokki-font-package-#{version}.zip"
-  desc "Jaapokki"
-  homepage "https://mikkonuuttila.com/jaapokki/"
+
   def install
-    (share/"fonts").install Dir.glob("Jaapokki-font-package-#{version}/Regular/**/Jaapokki-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Jaapokki-font-package-#{version}/enchance/**/Jaapokkienchance-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("Jaapokki-font-package-#{version}/subtract/**/Jaapokkisubtract-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Jaapokki-font-package-1.0/Regular/Jaapokki-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Jaapokki-font-package-1.0/enchance/Jaapokkienchance-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Jaapokki-font-package-1.0/subtract/Jaapokkisubtract-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
