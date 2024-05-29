008cc138eaee6f42785378eedfdf57b09ef11369
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-b612-mono updated

--- Formula/font-b612-mono.rb
+++ Formula/font-b612-mono.rb
@@ -1,15 +1,19 @@
 class FontB612Mono < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/b612mono"
-  desc "B612 Mono"
+  desc "B612 mono font"
   homepage "https://fonts.google.com/specimen/B612+Mono"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/b612mono"
+
   def install
     (share/"fonts").install Dir.glob("ofl/b612mono/./**/B612Mono-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/b612mono/./**/B612Mono-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/b612mono/./**/B612Mono-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/b612mono/./**/B612Mono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
