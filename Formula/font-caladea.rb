a50f784eabbf142b5156b44c0830421ae5d19e75
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-caladea updated

--- Formula/font-caladea.rb
+++ Formula/font-caladea.rb
@@ -1,15 +1,19 @@
 class FontCaladea < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/caladea"
-  desc "Caladea"
+  desc "Caladea font"
   homepage "https://fonts.google.com/specimen/Caladea"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/caladea"
+
   def install
     (share/"fonts").install Dir.glob("ofl/caladea/./**/Caladea-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/caladea/./**/Caladea-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/caladea/./**/Caladea-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/caladea/./**/Caladea-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
