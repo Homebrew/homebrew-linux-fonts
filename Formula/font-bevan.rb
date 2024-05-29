97c612cee3b8c1205b59a4b6692cead38614b0b2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-bevan updated

--- Formula/font-bevan.rb
+++ Formula/font-bevan.rb
@@ -1,13 +1,17 @@
 class FontBevan < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/bevan"
-  desc "Bevan"
+  desc "Bevan font"
   homepage "https://fonts.google.com/specimen/Bevan"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/bevan"
+
   def install
     (share/"fonts").install Dir.glob("ofl/bevan/./**/Bevan-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/bevan/./**/Bevan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
