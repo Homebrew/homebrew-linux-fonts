1cc33cdc22e001b3680a8e4714ca35d3b69044ad
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-iso updated

--- Formula/font-rubik-iso.rb
+++ Formula/font-rubik-iso.rb
@@ -1,12 +1,14 @@
 class FontRubikIso < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikiso/RubikIso-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Iso"
+  desc "Rubik iso font"
   homepage "https://fonts.google.com/specimen/Rubik+Iso"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikiso/RubikIso-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikIso-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
