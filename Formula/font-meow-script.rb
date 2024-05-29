c3706846be037329b57b232870fb6d0c0dd0942c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-meow-script updated

--- Formula/font-meow-script.rb
+++ Formula/font-meow-script.rb
@@ -1,13 +1,14 @@
 class FontMeowScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/meowscript/MeowScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Meow Script"
   desc "Monoline font with a number of alternate forms in six stylistic sets"
   homepage "https://fonts.google.com/specimen/Meow+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/meowscript/MeowScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MeowScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
