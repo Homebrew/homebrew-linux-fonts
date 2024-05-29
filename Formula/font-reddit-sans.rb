04439435d9fd55e16d51d6d76338c041cfe2d2d6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-reddit-sans updated

--- Formula/font-reddit-sans.rb
+++ Formula/font-reddit-sans.rb
@@ -1,14 +1,17 @@
 class FontRedditSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/redditsans"
-  desc "Reddit Sans"
   desc "Complemented by reddit sans condensed and reddit mono"
   homepage "https://fonts.google.com/specimen/Reddit+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/redditsans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/redditsans/./**/RedditSans-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/redditsans/./**/RedditSans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/redditsans/./**/RedditSans-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/redditsans/./**/RedditSans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
