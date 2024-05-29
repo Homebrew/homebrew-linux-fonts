4f82e7cf27c868d2cf4b4a809b6798496a590e2b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-damion updated

--- Formula/font-damion.rb
+++ Formula/font-damion.rb
@@ -1,12 +1,14 @@
 class FontDamion < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/damion/Damion-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Damion"
+  desc "Damion font"
   homepage "https://fonts.google.com/specimen/Damion"
+  head "https://github.com/google/fonts/raw/main/ofl/damion/Damion-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Damion-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
