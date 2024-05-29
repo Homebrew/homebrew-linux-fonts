0bcf9794ae9b18583b1e81998666f8195c77279e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:18 +0000

    font-ibm-plex-mono updated

--- Formula/font-ibm-plex-mono.rb
+++ Formula/font-ibm-plex-mono.rb
@@ -1,10 +1,13 @@
 class FontIbmPlexMono < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ibmplexmono"
-  desc "IBM Plex Mono"
   desc "Corporate typeface for IBM"
   homepage "https://fonts.google.com/specimen/IBM+Plex+Mono"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ibmplexmono"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-ThinItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-ExtraLightItalic.ttf")[0]
@@ -17,10 +20,10 @@ class FontIbmPlexMono < Formula
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
