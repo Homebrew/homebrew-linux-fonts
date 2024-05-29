8ee6001d3e85dc74701bac263443e21c92c63e67
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-peralta updated

--- Formula/font-peralta.rb
+++ Formula/font-peralta.rb
@@ -1,12 +1,14 @@
 class FontPeralta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/peralta/Peralta-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Peralta"
+  desc "Peralta font"
   homepage "https://fonts.google.com/specimen/Peralta"
+  head "https://github.com/google/fonts/raw/main/ofl/peralta/Peralta-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Peralta-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
