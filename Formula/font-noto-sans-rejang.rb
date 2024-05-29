4d04e314f049ebe48f196d826dd56999c04f988c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-rejang updated

--- Formula/font-noto-sans-rejang.rb
+++ Formula/font-noto-sans-rejang.rb
@@ -1,12 +1,14 @@
 class FontNotoSansRejang < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRejang-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Rejang"
+  desc "Noto sans rejang font"
   homepage "https://www.google.com/get/noto/#sans-rjng"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRejang-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansRejang-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
