4503b5bd3b4c7f6e2fd823c9aa76432125c88694
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:54 +0000

    font-chivo-mono updated

--- Formula/font-chivo-mono.rb
+++ Formula/font-chivo-mono.rb
@@ -1,14 +1,17 @@
 class FontChivoMono < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/chivomono"
-  desc "Chivo Mono"
   desc "Variable font ranging from thin to black with matching italics"
   homepage "https://fonts.google.com/specimen/Chivo+Mono"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/chivomono"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/chivomono/./**/ChivoMono-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/chivomono/./**/ChivoMono\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/chivomono/./**/ChivoMono-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/chivomono/./**/ChivoMono[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
