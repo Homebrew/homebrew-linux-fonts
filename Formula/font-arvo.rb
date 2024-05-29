59259e094f88310b30b7bb7f07a2bc28ab565ddd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-arvo updated

--- Formula/font-arvo.rb
+++ Formula/font-arvo.rb
@@ -1,15 +1,19 @@
 class FontArvo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/arvo"
-  desc "Arvo"
+  desc "Arvo font"
   homepage "https://fonts.google.com/specimen/Arvo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/arvo"
+
   def install
     (share/"fonts").install Dir.glob("ofl/arvo/./**/Arvo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/arvo/./**/Arvo-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/arvo/./**/Arvo-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/arvo/./**/Arvo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
