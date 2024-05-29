ae6c87f60512989d5a84626636822f106922f82e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-blaka-ink updated

--- Formula/font-blaka-ink.rb
+++ Formula/font-blaka-ink.rb
@@ -1,12 +1,14 @@
 class FontBlakaInk < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/blakaink/BlakaInk-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Blaka Ink"
+  desc "Blaka ink font"
   homepage "https://fonts.google.com/specimen/Blaka+Ink"
+  head "https://github.com/google/fonts/raw/main/ofl/blakaink/BlakaInk-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BlakaInk-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
