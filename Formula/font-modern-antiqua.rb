5b8455addec6f3344f79e0642a50d5e1272f2bc7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-modern-antiqua updated

--- Formula/font-modern-antiqua.rb
+++ Formula/font-modern-antiqua.rb
@@ -1,12 +1,14 @@
 class FontModernAntiqua < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/modernantiqua/ModernAntiqua-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Modern Antiqua"
+  desc "Modern antiqua font"
   homepage "https://fonts.google.com/specimen/Modern+Antiqua"
+  head "https://github.com/google/fonts/raw/main/ofl/modernantiqua/ModernAntiqua-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ModernAntiqua-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
