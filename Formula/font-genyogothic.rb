713b6236e1aa2d5c984ea976dde2999994408edc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-genyogothic updated

--- Formula/font-genyogothic.rb
+++ Formula/font-genyogothic.rb
@@ -1,9 +1,10 @@
 class FontGenyogothic < Formula
+  desc "Genyogothic font"
+  homepage "https://github.com/ButTaiwan/genyog-font"
+  url "https://github.com/ButTaiwan/genyog-font/releases/download/v1.501/GenYoGothic.zip"
   version "1.501"
   sha256 "d90705ecb3ab25a9b98972fcb244323b6a0c76c4320bb7e0ab1b82815f727086"
-  url "https://github.com/ButTaiwan/genyog-font/releases/download/v#{version}/GenYoGothic.zip"
-  desc "GenYoGothic"
-  homepage "https://github.com/ButTaiwan/genyog-font"
+
   def install
     (share/"fonts").install Dir.glob("./**/GenYoGothic-B.ttc")[0]
     (share/"fonts").install Dir.glob("./**/GenYoGothic-EL.ttc")[0]
@@ -13,8 +14,8 @@ class FontGenyogothic < Formula
     (share/"fonts").install Dir.glob("./**/GenYoGothic-N.ttc")[0]
     (share/"fonts").install Dir.glob("./**/GenYoGothic-R.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
