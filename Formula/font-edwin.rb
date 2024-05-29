c0ee77eb2956c663dfdb380b70ecd70e8d7465fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-edwin updated

--- Formula/font-edwin.rb
+++ Formula/font-edwin.rb
@@ -1,17 +1,18 @@
 class FontEdwin < Formula
+  desc "Text font for musical scores font"
+  homepage "https://github.com/MuseScoreFonts/Edwin"
+  url "https://github.com/MuseScoreFonts/Edwin/archive/refs/tags/v0.54.zip"
   version "0.54"
   sha256 "761a15932defe72b40eecb22e8255cba225bc9efedb208c83e2f5b0b77b7103e"
-  url "https://github.com/MuseScoreFonts/Edwin/archive/refs/tags/v#{version}.zip"
-  desc "Text font for musical scores"
-  homepage "https://github.com/MuseScoreFonts/Edwin"
+
   def install
-    (share/"fonts").install Dir.glob("Edwin-#{version}/**/Edwin-BdIta.otf")[0]
-    (share/"fonts").install Dir.glob("Edwin-#{version}/**/Edwin-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("Edwin-#{version}/**/Edwin-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("Edwin-#{version}/**/Edwin-Roman.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Edwin-0.54/Edwin-BdIta.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Edwin-0.54/Edwin-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Edwin-0.54/Edwin-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Edwin-0.54/Edwin-Roman.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
