64e443b294dba78e151274d5a0e0ee672f3518d0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-lohit-tamil updated

--- Formula/font-lohit-tamil.rb
+++ Formula/font-lohit-tamil.rb
@@ -1,12 +1,14 @@
 class FontLohitTamil < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lohittamil/Lohit-Tamil.ttf", verified: "github.com/google/fonts/"
-  desc "Lohit Tamil"
+  desc "Lohit tamil font"
   homepage "https://fonts.google.com/specimen/Lohit+Tamil"
+  head "https://github.com/google/fonts/raw/main/ofl/lohittamil/Lohit-Tamil.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Lohit-Tamil.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
