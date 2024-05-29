18aae6dd4bd29adb3a9085f8aca16475c69e95b8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:50 +0000

    font-bungee-shade updated

--- Formula/font-bungee-shade.rb
+++ Formula/font-bungee-shade.rb
@@ -1,12 +1,14 @@
 class FontBungeeShade < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bungeeshade/BungeeShade-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bungee Shade"
+  desc "Bungee shade font"
   homepage "https://fonts.google.com/specimen/Bungee+Shade"
+  head "https://github.com/google/fonts/raw/main/ofl/bungeeshade/BungeeShade-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BungeeShade-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
