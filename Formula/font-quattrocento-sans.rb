47ffd6e1a261f748b25d6d88dd77c47102b187a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:12 +0000

    font-quattrocento-sans updated

--- Formula/font-quattrocento-sans.rb
+++ Formula/font-quattrocento-sans.rb
@@ -1,15 +1,19 @@
 class FontQuattrocentoSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/quattrocentosans"
-  desc "Quattrocento Sans"
+  desc "Quattrocento sans font"
   homepage "https://fonts.google.com/specimen/Quattrocento+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/quattrocentosans"
+
   def install
     (share/"fonts").install Dir.glob("ofl/quattrocentosans/./**/QuattrocentoSans-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/quattrocentosans/./**/QuattrocentoSans-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/quattrocentosans/./**/QuattrocentoSans-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/quattrocentosans/./**/QuattrocentoSans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
