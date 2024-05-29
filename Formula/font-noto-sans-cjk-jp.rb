15944c43f18f423cabf035dff24aaae1afb55dd4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:49 +0000

    font-noto-sans-cjk-jp updated

--- Formula/font-noto-sans-cjk-jp.rb
+++ Formula/font-noto-sans-cjk-jp.rb
@@ -1,10 +1,10 @@
 class FontNotoSansCjkJp < Formula
-  version "2.004"
-  sha256 "bf5b26feb83ee02533d4eb4a127bd90ed9d42bdd7ca7cb6e4030b2f21ef55cc5"
-  url "https://github.com/notofonts/noto-cjk/releases/download/Sans#{version}/06_NotoSansCJKjp.zip"
-  desc "Noto Sans CJK JP"
   desc "Language Specific OTFs Japanese (日本語)"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Sans2.004/06_NotoSansCJKjp.zip"
+  version "2.004"
+  sha256 "bf5b26feb83ee02533d4eb4a127bd90ed9d42bdd7ca7cb6e4030b2f21ef55cc5"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansCJKjp-Black.otf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCJKjp-Bold.otf")[0]
@@ -14,8 +14,8 @@ class FontNotoSansCjkJp < Formula
     (share/"fonts").install Dir.glob("./**/NotoSansCJKjp-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansCJKjp-Thin.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
