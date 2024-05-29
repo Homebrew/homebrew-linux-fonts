e7420285a6e3cf8dd8eb9c65d5b965f41852045c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:16 +0000

    font-hedvig-letters-serif updated

--- Formula/font-hedvig-letters-serif.rb
+++ Formula/font-hedvig-letters-serif.rb
@@ -1,13 +1,14 @@
 class FontHedvigLettersSerif < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/hedviglettersserif/HedvigLettersSerif%5Bopsz%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Hedvig Letters Serif"
   desc "Perfect when it’s not"
   homepage "https://fonts.google.com/specimen/Hedvig+Letters+Serif"
+  head "https://github.com/google/fonts/raw/main/ofl/hedviglettersserif/HedvigLettersSerif%5Bopsz%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/HedvigLettersSerif\\[opsz\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/HedvigLettersSerif[opsz].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
