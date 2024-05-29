bede3cf11a2f6fe90590ac59c107fc2343406f90
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-telugu updated

--- Formula/font-noto-sans-telugu.rb
+++ Formula/font-noto-sans-telugu.rb
@@ -1,13 +1,15 @@
 class FontNotoSansTelugu < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTelugu-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Telugu"
+  desc "Noto sans telugu font"
   homepage "https://www.google.com/get/noto/#sans-telu"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTelugu-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansTelugu-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansTelugu-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
