d2977ee4403c31ad158fd0c43c7aeb9cf27d5dfc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:33 +0000

    font-unifrakturmaguntia updated

--- Formula/font-unifrakturmaguntia.rb
+++ Formula/font-unifrakturmaguntia.rb
@@ -1,12 +1,14 @@
 class FontUnifrakturmaguntia < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/unifrakturmaguntia/UnifrakturMaguntia-Book.ttf", verified: "github.com/google/fonts/"
-  desc "UnifrakturMaguntia"
+  desc "Unifrakturmaguntia font"
   homepage "https://fonts.google.com/specimen/UnifrakturMaguntia"
+  head "https://github.com/google/fonts/raw/main/ofl/unifrakturmaguntia/UnifrakturMaguntia-Book.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/UnifrakturMaguntia-Book.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
