aebf8a06f097b952136819f4c6e0eb651ea97365
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-aubrey updated

--- Formula/font-aubrey.rb
+++ Formula/font-aubrey.rb
@@ -1,12 +1,14 @@
 class FontAubrey < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/aubrey/Aubrey-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Aubrey"
+  desc "Aubrey font"
   homepage "https://fonts.google.com/specimen/Aubrey"
+  head "https://github.com/google/fonts/raw/main/ofl/aubrey/Aubrey-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Aubrey-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
