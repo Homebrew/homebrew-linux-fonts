706431adfaf75baffd4aeef0d62cb2acb8abb8cc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-linear-a updated

--- Formula/font-noto-sans-linear-a.rb
+++ Formula/font-noto-sans-linear-a.rb
@@ -1,12 +1,14 @@
 class FontNotoSansLinearA < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanslineara/NotoSansLinearA-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Linear A"
+  desc "Noto sans linear a font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Linear+A"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanslineara/NotoSansLinearA-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansLinearA-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
