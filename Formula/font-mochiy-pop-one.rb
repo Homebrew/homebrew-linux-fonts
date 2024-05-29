06c367816f41972763c17b409251d6b0e60b41c8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-mochiy-pop-one updated

--- Formula/font-mochiy-pop-one.rb
+++ Formula/font-mochiy-pop-one.rb
@@ -1,12 +1,14 @@
 class FontMochiyPopOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mochiypopone/MochiyPopOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mochiy Pop One"
+  desc "Mochiy pop one font"
   homepage "https://fonts.google.com/specimen/Mochiy+Pop+One"
+  head "https://github.com/google/fonts/raw/main/ofl/mochiypopone/MochiyPopOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MochiyPopOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
