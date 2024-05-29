c815068f1f956d4fedc2fb8ed386a9ed625f9034
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-im-fell-great-primer-sc updated

--- Formula/font-im-fell-great-primer-sc.rb
+++ Formula/font-im-fell-great-primer-sc.rb
@@ -1,12 +1,14 @@
 class FontImFellGreatPrimerSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/imfellgreatprimersc/IMFeGPsc28P.ttf", verified: "github.com/google/fonts/"
-  desc "IM Fell Great Primer SC"
+  desc "Im fell great primer sc font"
   homepage "https://fonts.google.com/specimen/IM+Fell+Great+Primer+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/imfellgreatprimersc/IMFeGPsc28P.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/IMFeGPsc28P.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
