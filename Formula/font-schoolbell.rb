eee7612e98830cd120172beb06f343920ff1fa2e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-schoolbell updated

--- Formula/font-schoolbell.rb
+++ Formula/font-schoolbell.rb
@@ -1,12 +1,14 @@
 class FontSchoolbell < Formula
-  head "https://github.com/google/fonts/raw/main/apache/schoolbell/Schoolbell-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Schoolbell"
+  desc "Schoolbell font"
   homepage "https://fonts.google.com/specimen/Schoolbell"
+  head "https://github.com/google/fonts/raw/main/apache/schoolbell/Schoolbell-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Schoolbell-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
