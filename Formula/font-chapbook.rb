f031f7946b16ec7580729ac50fd296e32dd01561
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-chapbook updated

--- Formula/font-chapbook.rb
+++ Formula/font-chapbook.rb
@@ -1,14 +1,15 @@
 class FontChapbook < Formula
-  head "https://www.feorag.com/freestuff/archives/Chapbook-OTF.zip"
-  desc "Chapbook"
+  desc "Chapbook font"
   homepage "https://www.feorag.com/freestuff/chapbook.html"
+  head "https://www.feorag.com/freestuff/archives/Chapbook-OTF.zip"
+
   def install
-    (share/"fonts").install Dir.glob("Chapbook OTF/**/Chapbook-Dingbats.otf")[0]
-    (share/"fonts").install Dir.glob("Chapbook OTF/**/Chapbook-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("Chapbook OTF/**/Chapbook-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Chapbook OTF/Chapbook-Dingbats.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Chapbook OTF/Chapbook-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Chapbook OTF/Chapbook-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
