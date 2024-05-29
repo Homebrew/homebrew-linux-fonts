d080b87e211ce975499983575ee05535e4929596
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-shippori-antique updated

--- Formula/font-shippori-antique.rb
+++ Formula/font-shippori-antique.rb
@@ -1,12 +1,14 @@
 class FontShipporiAntique < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/shipporiantique/ShipporiAntique-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Shippori Antique"
+  desc "Shippori antique font"
   homepage "https://fonts.google.com/specimen/Shippori+Antique"
+  head "https://github.com/google/fonts/raw/main/ofl/shipporiantique/ShipporiAntique-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ShipporiAntique-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
