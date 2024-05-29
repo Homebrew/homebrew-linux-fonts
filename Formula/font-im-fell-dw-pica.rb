342e65be52ec6d810939081b6cff8c8a2d1e39e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-im-fell-dw-pica updated

--- Formula/font-im-fell-dw-pica.rb
+++ Formula/font-im-fell-dw-pica.rb
@@ -1,13 +1,17 @@
 class FontImFellDwPica < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/imfelldwpica"
-  desc "IM Fell DW Pica"
+  desc "Im fell dw pica font"
   homepage "https://fonts.google.com/specimen/IM+Fell+DW+Pica"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/imfelldwpica"
+
   def install
     (share/"fonts").install Dir.glob("ofl/imfelldwpica/./**/IMFePIit28P.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/imfelldwpica/./**/IMFePIrm28P.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
