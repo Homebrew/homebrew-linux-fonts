ac7be1ec8aaaf5dbb7fb457097339414c97e4ba3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-museomoderno updated

--- Formula/font-museomoderno.rb
+++ Formula/font-museomoderno.rb
@@ -1,13 +1,17 @@
 class FontMuseomoderno < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/museomoderno"
-  desc "MuseoModerno"
+  desc "Museomoderno font"
   homepage "https://fonts.google.com/specimen/MuseoModerno"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/museomoderno"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/museomoderno/./**/MuseoModerno-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/museomoderno/./**/MuseoModerno\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/museomoderno/./**/MuseoModerno-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/museomoderno/./**/MuseoModerno[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
