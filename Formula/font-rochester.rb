37c7f29a469c3177686e1b122134fddffffe5bb3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:15 +0000

    font-rochester updated

--- Formula/font-rochester.rb
+++ Formula/font-rochester.rb
@@ -1,12 +1,14 @@
 class FontRochester < Formula
-  head "https://github.com/google/fonts/raw/main/apache/rochester/Rochester-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rochester"
+  desc "Rochester font"
   homepage "https://fonts.google.com/specimen/Rochester"
+  head "https://github.com/google/fonts/raw/main/apache/rochester/Rochester-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Rochester-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
