1e8e0ca32c981f8b28a8c56ad7b1a1079ae84a7c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:38 +0000

    font-zen-dots updated

--- Formula/font-zen-dots.rb
+++ Formula/font-zen-dots.rb
@@ -1,13 +1,14 @@
 class FontZenDots < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/zendots/ZenDots-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Zen Dots"
   desc "One of three latin fonts part of the zen fonts collection"
   homepage "https://fonts.google.com/specimen/Zen+Dots"
+  head "https://github.com/google/fonts/raw/main/ofl/zendots/ZenDots-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ZenDots-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
