7a2f70018e874ca26d6de0d76c4d370cce18f208
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-plangothic updated

--- Formula/font-plangothic.rb
+++ Formula/font-plangothic.rb
@@ -1,16 +1,16 @@
 class FontPlangothic < Formula
-  version "1.8.5760"
-  sha256 "036c72bb6de45d175039c86d64cf67fb88136b01989fa3880b0fdb79232e527e"
-  url "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic-Project/archive/refs/tags/V#{version}.zip"
-  desc "Plangothic"
   desc "Plangothic is a sans-serif font that covers CJK Unified Ideographs"
   homepage "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic-Project"
+  url "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic-Project/archive/refs/tags/V1.8.5760.zip"
+  version "1.8.5760"
+  sha256 "036c72bb6de45d175039c86d64cf67fb88136b01989fa3880b0fdb79232e527e"
+
   def install
-    (share/"fonts").install Dir.glob("Plangothic-Project-#{version}/**/PlangothicP1-Regular (allideo).ttf")[0]
-    (share/"fonts").install Dir.glob("Plangothic-Project-#{version}/**/PlangothicP2-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Plangothic-Project-1.8.5760/PlangothicP1-Regular (allideo).ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Plangothic-Project-1.8.5760/PlangothicP2-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
