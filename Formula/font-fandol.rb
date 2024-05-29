94d31d0afa2b077cbe3602a1907cca4d3c35a177
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:05 +0000

    font-fandol updated

--- Formula/font-fandol.rb
+++ Formula/font-fandol.rb
@@ -1,20 +1,22 @@
 class FontFandol < Formula
-  version "0.3"
-  url "http://mirrors.ctan.org/fonts/fandol.zip"
-  desc "Fandol"
+  desc "Fandol font"
   homepage "https://ctan.org/pkg/fandol"
+  url "http://mirrors.ctan.org/fonts/fandol.zip"
+  version "0.3"
+  sha256 :no_check
+
   def install
-    (share/"fonts").install Dir.glob("fandol/**/FandolBraille-Display.otf")[0]
-    (share/"fonts").install Dir.glob("fandol/**/FandolBraille-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("fandol/**/FandolFang-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("fandol/**/FandolHei-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("fandol/**/FandolHei-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("fandol/**/FandolKai-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("fandol/**/FandolSong-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("fandol/**/FandolSong-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fandol/FandolSong-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fandol/FandolBraille-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fandol/FandolFang-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fandol/FandolHei-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fandol/FandolHei-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fandol/FandolKai-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fandol/FandolSong-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fandol/FandolBraille-Display.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
