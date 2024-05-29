3177c2e5adb18b5ae9b7edc8068df617d942909c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-forum updated

--- Formula/font-forum.rb
+++ Formula/font-forum.rb
@@ -1,12 +1,14 @@
 class FontForum < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/forum/Forum-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Forum"
+  desc "Forum font"
   homepage "https://fonts.google.com/specimen/Forum"
+  head "https://github.com/google/fonts/raw/main/ofl/forum/Forum-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Forum-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
