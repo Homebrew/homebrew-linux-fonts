f14dfa78367e8f1be27644559ce115ca514f50ea
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-cuprum updated

--- Formula/font-cuprum.rb
+++ Formula/font-cuprum.rb
@@ -1,13 +1,17 @@
 class FontCuprum < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cuprum"
-  desc "Cuprum"
+  desc "Cuprum font"
   homepage "https://fonts.google.com/specimen/Cuprum"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cuprum"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/cuprum/./**/Cuprum-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/cuprum/./**/Cuprum\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/cuprum/./**/Cuprum-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/cuprum/./**/Cuprum[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
