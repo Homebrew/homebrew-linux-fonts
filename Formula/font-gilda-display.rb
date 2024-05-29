fc32f9029042f74c594d95397e2b8fd58fe2df54
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-gilda-display updated

--- Formula/font-gilda-display.rb
+++ Formula/font-gilda-display.rb
@@ -1,12 +1,14 @@
 class FontGildaDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gildadisplay/GildaDisplay-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gilda Display"
+  desc "Gilda display font"
   homepage "https://fonts.google.com/specimen/Gilda+Display"
+  head "https://github.com/google/fonts/raw/main/ofl/gildadisplay/GildaDisplay-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GildaDisplay-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
