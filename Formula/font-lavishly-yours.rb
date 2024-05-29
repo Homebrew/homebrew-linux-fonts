14451d31ee73b5f5bc102aa2f5f2c95f4eb96575
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:31 +0000

    font-lavishly-yours updated

--- Formula/font-lavishly-yours.rb
+++ Formula/font-lavishly-yours.rb
@@ -1,13 +1,14 @@
 class FontLavishlyYours < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lavishlyyours/LavishlyYours-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Lavishly Yours"
   desc "Charming calligraphic script"
   homepage "https://fonts.google.com/specimen/Lavishly+Yours"
+  head "https://github.com/google/fonts/raw/main/ofl/lavishlyyours/LavishlyYours-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LavishlyYours-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
