582ee53f8a3e0deca64944a6e6af990f169cd22e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-scheherazade updated

--- Formula/font-scheherazade.rb
+++ Formula/font-scheherazade.rb
@@ -1,14 +1,15 @@
 class FontScheherazade < Formula
+  desc "Scheherazade  font"
+  homepage "https://scripts.sil.org/Scheherazade"
+  url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Scheherazade-2.020.zip&filename=Scheherazade-2.020.zip"
   version "2.020"
   sha256 "d8bb12997507937347efba4e59550a9655350563d1df662f0651e04ba916aff1"
-  url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Scheherazade-#{version}.zip&filename=Scheherazade-#{version}.zip"
-  desc "Scheherazade "
-  homepage "https://scripts.sil.org/Scheherazade"
+
   def install
-    (share/"fonts").install Dir.glob("Scheherazade-#{version}/**/Scheherazade-R.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Scheherazade-2.020/Scheherazade-R.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
