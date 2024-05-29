451ac14210f046178720328c2fdeda64241f8e43
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:49 +0000

    font-noto-sans-cypriot updated

--- Formula/font-noto-sans-cypriot.rb
+++ Formula/font-noto-sans-cypriot.rb
@@ -1,12 +1,14 @@
 class FontNotoSansCypriot < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCypriot-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Cypriot"
+  desc "Noto sans cypriot font"
   homepage "https://www.google.com/get/noto/#sans-cprt"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCypriot-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansCypriot-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
