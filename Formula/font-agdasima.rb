6483a611fc27bd13e842d014a2c719e06cf01114
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-agdasima updated

--- Formula/font-agdasima.rb
+++ Formula/font-agdasima.rb
@@ -1,14 +1,17 @@
 class FontAgdasima < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/agdasima"
-  desc "Agdasima"
   desc "Based on big shoulders, a condensed american gothic sans-serif font family"
   homepage "https://fonts.google.com/specimen/Agdasima"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/agdasima"
+
   def install
     (share/"fonts").install Dir.glob("ofl/agdasima/./**/Agdasima-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/agdasima/./**/Agdasima-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
