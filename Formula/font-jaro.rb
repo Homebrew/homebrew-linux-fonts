94b577a7b9a0b1dd5e7b810f58e4244d91dc6908
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jaro updated

--- Formula/font-jaro.rb
+++ Formula/font-jaro.rb
@@ -1,13 +1,14 @@
 class FontJaro < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jaro/Jaro%5Bopsz%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Jaro"
   desc "Global display typeface inspired by the work of jaroslav benda"
   homepage "https://fonts.google.com/specimen/Jaro"
+  head "https://github.com/google/fonts/raw/main/ofl/jaro/Jaro%5Bopsz%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Jaro\\[opsz\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Jaro[opsz].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
