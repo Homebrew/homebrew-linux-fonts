8376defd8f89cc307aa2f2ad4b562c282cf5a949
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-palmyrene updated

--- Formula/font-noto-sans-palmyrene.rb
+++ Formula/font-noto-sans-palmyrene.rb
@@ -1,12 +1,14 @@
 class FontNotoSansPalmyrene < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanspalmyrene/NotoSansPalmyrene-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Palmyrene"
+  desc "Noto sans palmyrene font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Palmyrene"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanspalmyrene/NotoSansPalmyrene-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansPalmyrene-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
