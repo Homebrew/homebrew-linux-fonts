d84ef1e0f400fc928649ce11427d595a32507039
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-just-me-again-down-here updated

--- Formula/font-just-me-again-down-here.rb
+++ Formula/font-just-me-again-down-here.rb
@@ -1,12 +1,14 @@
 class FontJustMeAgainDownHere < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/justmeagaindownhere/JustMeAgainDownHere.ttf", verified: "github.com/google/fonts/"
-  desc "Just Me Again Down Here"
+  desc "Just me again down here font"
   homepage "https://fonts.google.com/specimen/Just+Me+Again+Down+Here"
+  head "https://github.com/google/fonts/raw/main/ofl/justmeagaindownhere/JustMeAgainDownHere.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JustMeAgainDownHere.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
