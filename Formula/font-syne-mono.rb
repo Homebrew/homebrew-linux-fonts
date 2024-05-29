5e377755b45c3227ab3d54b6aef79416013655e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-syne-mono updated

--- Formula/font-syne-mono.rb
+++ Formula/font-syne-mono.rb
@@ -1,13 +1,14 @@
 class FontSyneMono < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/synemono/SyneMono-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Syne Mono"
   desc "Another take on letting go of control"
   homepage "https://fonts.google.com/specimen/Syne+Mono"
+  head "https://github.com/google/fonts/raw/main/ofl/synemono/SyneMono-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SyneMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
