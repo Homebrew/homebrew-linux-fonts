88d2f1163efa63f332cf7f8179fa3f5af7b1e0ca
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-yeon-sung updated

--- Formula/font-yeon-sung.rb
+++ Formula/font-yeon-sung.rb
@@ -1,12 +1,14 @@
 class FontYeonSung < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yeonsung/YeonSung-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yeon Sung"
+  desc "Yeon sung font"
   homepage "https://fonts.google.com/specimen/Yeon+Sung"
+  head "https://github.com/google/fonts/raw/main/ofl/yeonsung/YeonSung-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/YeonSung-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
