4b9d1556af0533e5370bda323c7a6689c306ce24
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-hanamina updated

--- Formula/font-hanamina.rb
+++ Formula/font-hanamina.rb
@@ -1,15 +1,17 @@
 class FontHanamina < Formula
+  desc "Hanamina font"
+  homepage "https://fonts.jp/hanazono/"
+  url "https://osdn.dl.osdn.jp/hanazono-font/68253/hanazono-20170904.zip",
+       verified: "osdn.dl.osdn.jp/hanazono-font/"
   version "2017.09.04,68253"
   sha256 "571cd4a09ae7da0c642d640fc2442c050aa450ebb0587a95cdd097d41a9c9572"
-  url "https://osdn.dl.osdn.jp/hanazono-font/#{version.to_s.sub(/.*,/, "")}/hanazono-#{version.to_s.sub(/,.*/, "").gsub(".", "")}.zip", verified: "osdn.dl.osdn.jp/hanazono-font/"
-  desc "HanaMinA"
-  homepage "https://fonts.jp/hanazono/"
+
   def install
     (share/"fonts").install Dir.glob("./**/HanaMinA.ttf")[0]
     (share/"fonts").install Dir.glob("./**/HanaMinB.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
