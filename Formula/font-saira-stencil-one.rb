100879bf405f0d75f2037bee9a995540f6a0945f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-saira-stencil-one updated

--- Formula/font-saira-stencil-one.rb
+++ Formula/font-saira-stencil-one.rb
@@ -1,12 +1,14 @@
 class FontSairaStencilOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sairastencilone/SairaStencilOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Saira Stencil One"
+  desc "Saira stencil one font"
   homepage "https://fonts.google.com/specimen/Saira+Stencil+One"
+  head "https://github.com/google/fonts/raw/main/ofl/sairastencilone/SairaStencilOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SairaStencilOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
