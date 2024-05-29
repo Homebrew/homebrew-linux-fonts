36172f30f9866be82ba9c5ce38464d762f2af0c7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-telugu-ui updated

--- Formula/font-noto-sans-telugu-ui.rb
+++ Formula/font-noto-sans-telugu-ui.rb
@@ -1,12 +1,14 @@
 class FontNotoSansTeluguUi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansteluguui/NotoSansTeluguUI%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Telugu UI"
+  desc "Noto sans telugu ui font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Telugu+UI"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansteluguui/NotoSansTeluguUI%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansTeluguUI\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansTeluguUI[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
