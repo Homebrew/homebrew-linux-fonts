8fdc49fe924132f1e2b4c20e07c7b4d8fe08ce22
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:49 +0000

    font-bruno-ace-sc updated

--- Formula/font-bruno-ace-sc.rb
+++ Formula/font-bruno-ace-sc.rb
@@ -1,12 +1,14 @@
 class FontBrunoAceSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/brunoacesc/BrunoAceSC-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bruno Ace SC"
+  desc "Bruno ace sc font"
   homepage "https://fonts.google.com/specimen/Bruno+Ace+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/brunoacesc/BrunoAceSC-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BrunoAceSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
