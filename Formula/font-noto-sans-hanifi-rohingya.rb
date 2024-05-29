23c423c240c91d1c31904cc71a565866485779c2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:50 +0000

    font-noto-sans-hanifi-rohingya updated

--- Formula/font-noto-sans-hanifi-rohingya.rb
+++ Formula/font-noto-sans-hanifi-rohingya.rb
@@ -1,12 +1,14 @@
 class FontNotoSansHanifiRohingya < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanshanifirohingya/NotoSansHanifiRohingya%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Hanifi Rohingya"
+  desc "Noto sans hanifi rohingya font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Hanifi+Rohingya"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanshanifirohingya/NotoSansHanifiRohingya%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansHanifiRohingya\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansHanifiRohingya[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
