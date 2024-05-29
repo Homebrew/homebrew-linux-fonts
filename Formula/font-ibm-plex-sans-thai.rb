78389843969e1fae6cd64701b6275479fe57d2a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:18 +0000

    font-ibm-plex-sans-thai updated

--- Formula/font-ibm-plex-sans-thai.rb
+++ Formula/font-ibm-plex-sans-thai.rb
@@ -1,7 +1,11 @@
 class FontIbmPlexSansThai < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ibmplexsansthai"
-  desc "IBM Plex Sans Thai"
+  desc "Ibm plex sans thai font"
   homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+Thai"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ibmplexsansthai"
+
   def install
     (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-ExtraLight.ttf")[0]
@@ -11,8 +15,8 @@ class FontIbmPlexSansThai < Formula
     (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
