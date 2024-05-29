97456d19c4845d9bfb487430a78ababa69a8970a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-mr-de-haviland updated

--- Formula/font-mr-de-haviland.rb
+++ Formula/font-mr-de-haviland.rb
@@ -1,12 +1,14 @@
 class FontMrDeHaviland < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mrdehaviland/MrDeHaviland-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mr De Haviland"
+  desc "Mr de haviland font"
   homepage "https://fonts.google.com/specimen/Mr+De+Haviland"
+  head "https://github.com/google/fonts/raw/main/ofl/mrdehaviland/MrDeHaviland-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MrDeHaviland-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
