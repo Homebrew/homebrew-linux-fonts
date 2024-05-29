99af57360a3b42dae383c192cec3f02564c2ddd4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-taprom updated

--- Formula/font-taprom.rb
+++ Formula/font-taprom.rb
@@ -1,12 +1,14 @@
 class FontTaprom < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/taprom/Taprom-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Taprom"
+  desc "Taprom font"
   homepage "https://fonts.google.com/specimen/Taprom"
+  head "https://github.com/google/fonts/raw/main/ofl/taprom/Taprom-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Taprom-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
