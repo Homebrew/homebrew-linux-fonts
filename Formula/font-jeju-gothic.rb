6b5a28dd6349c4f62154e9cb56534525daabf272
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jeju-gothic updated

--- Formula/font-jeju-gothic.rb
+++ Formula/font-jeju-gothic.rb
@@ -1,12 +1,14 @@
 class FontJejuGothic < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jejugothic/JejuGothic-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jeju Gothic"
+  desc "Jeju gothic font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts/raw/main/ofl/jejugothic/JejuGothic-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JejuGothic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
