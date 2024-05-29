37e696bd8bdcbe60f3e9d6ce70813dc9229b6972
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-rem updated

--- Formula/font-rem.rb
+++ Formula/font-rem.rb
@@ -1,14 +1,17 @@
 class FontRem < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rem"
-  desc "REM"
   desc "Acronym for rapid eye movement"
   homepage "https://fonts.google.com/specimen/REM"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/rem"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/rem/./**/REM-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/rem/./**/REM\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/rem/./**/REM-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/rem/./**/REM[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
