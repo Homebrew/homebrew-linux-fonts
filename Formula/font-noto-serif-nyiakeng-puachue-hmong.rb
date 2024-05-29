24f34ad6b4e50ce850a91d9f39d8d0cdc8870f2f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:00 +0000

    font-noto-serif-nyiakeng-puachue-hmong updated

--- Formula/font-noto-serif-nyiakeng-puachue-hmong.rb
+++ Formula/font-noto-serif-nyiakeng-puachue-hmong.rb
@@ -1,12 +1,14 @@
 class FontNotoSerifNyiakengPuachueHmong < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifnyiakengpuachuehmong/NotoSerifNyiakengPuachueHmong%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Nyiakeng Puachue Hmong"
+  desc "Noto serif nyiakeng puachue hmong font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Nyiakeng+Puachue+Hmong"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifnyiakengpuachuehmong/NotoSerifNyiakengPuachueHmong%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifNyiakengPuachueHmong\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifNyiakengPuachueHmong[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
