2edbedd13531f0bf995e495847636f1e7b0c9419
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-goblin-one updated

--- Formula/font-goblin-one.rb
+++ Formula/font-goblin-one.rb
@@ -1,12 +1,14 @@
 class FontGoblinOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/goblinone/GoblinOne.ttf", verified: "github.com/google/fonts/"
-  desc "Goblin One"
+  desc "Goblin one font"
   homepage "https://fonts.google.com/specimen/Goblin+One"
+  head "https://github.com/google/fonts/raw/main/ofl/goblinone/GoblinOne.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GoblinOne.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
