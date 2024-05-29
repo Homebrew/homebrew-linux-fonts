34f77dedeae8748017051cab9253f8eb687a0759
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-audiowide updated

--- Formula/font-audiowide.rb
+++ Formula/font-audiowide.rb
@@ -1,12 +1,14 @@
 class FontAudiowide < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/audiowide/Audiowide-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Audiowide"
+  desc "Audiowide font"
   homepage "https://fonts.google.com/specimen/Audiowide"
+  head "https://github.com/google/fonts/raw/main/ofl/audiowide/Audiowide-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Audiowide-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
