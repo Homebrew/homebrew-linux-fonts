d8834d4c48cea38b1687673714961f1f532fb170
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:54 +0000

    font-chiron-sung-hk updated

--- Formula/font-chiron-sung-hk.rb
+++ Formula/font-chiron-sung-hk.rb
@@ -1,17 +1,16 @@
 class FontChironSungHk < Formula
-  version "1.011"
-  sha256 "67334720bcc01e043b9acebcfe6aa5ad023837b89650f448e13ec4274aae9c36"
-  url "https://github.com/chiron-fonts/chiron-sung-hk/archive/refs/tags/v#{version}.zip"
-  desc "Chiron Sung HK"
-  desc "昭源宋體"
   desc "Modern, region-agnostic traditional Chinese serif typeface"
   homepage "https://github.com/chiron-fonts/chiron-sung-hk"
+  url "https://github.com/chiron-fonts/chiron-sung-hk/archive/refs/tags/v1.011.zip"
+  version "1.011"
+  sha256 "67334720bcc01e043b9acebcfe6aa5ad023837b89650f448e13ec4274aae9c36"
+
   def install
-    (share/"fonts").install Dir.glob("chiron-sung-hk-#{version}/VAR/**/ChironSungHKItVF.otf")[0]
-    (share/"fonts").install Dir.glob("chiron-sung-hk-#{version}/VAR/**/ChironSungHKVF.otf")[0]
+    (share/"fonts").install Dir.glob("./**/chiron-sung-hk-1.011/VAR/ChironSungHKItVF.otf")[0]
+    (share/"fonts").install Dir.glob("./**/chiron-sung-hk-1.011/VAR/ChironSungHKVF.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
