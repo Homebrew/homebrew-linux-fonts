50e94f9c9bc99d61b80873cdbd6fa808f55fb8ac
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-im-fell-double-pica updated

--- Formula/font-im-fell-double-pica.rb
+++ Formula/font-im-fell-double-pica.rb
@@ -1,13 +1,17 @@
 class FontImFellDoublePica < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/imfelldoublepica"
-  desc "IM Fell Double Pica"
+  desc "Im fell double pica font"
   homepage "https://fonts.google.com/specimen/IM+Fell+Double+Pica"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/imfelldoublepica"
+
   def install
     (share/"fonts").install Dir.glob("ofl/imfelldoublepica/./**/IMFELLDoublePica-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/imfelldoublepica/./**/IMFELLDoublePica-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
