b7872c2b1a5cc2732887195baff030bb37512d64
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-han-nom-a updated

--- Formula/font-han-nom-a.rb
+++ Formula/font-han-nom-a.rb
@@ -1,14 +1,16 @@
 class FontHanNomA < Formula
-  version "2005"
-  url "https://downloads.sourceforge.net/vietunicode/hannomH.zip"
-  desc "HAN NOM A"
+  desc "Han nom a font"
   homepage "https://sourceforge.net/projects/vietunicode/files/hannom/hannom%20v2005/"
+  url "https://downloads.sourceforge.net/vietunicode/hannomH.zip"
+  version "2005"
+  sha256 :no_check
+
   def install
     (share/"fonts").install Dir.glob("./**/HAN NOM A.ttf")[0]
     (share/"fonts").install Dir.glob("./**/HAN NOM B.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
