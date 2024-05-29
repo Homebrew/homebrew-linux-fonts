50f8b28008018259254565ed88123e3d669cefe4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-advent-pro updated

--- Formula/font-advent-pro.rb
+++ Formula/font-advent-pro.rb
@@ -1,14 +1,17 @@
 class FontAdventPro < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/adventpro"
-  desc "Advent Pro"
   desc "Modern font designed for web and print"
   homepage "https://fonts.google.com/specimen/Advent+Pro"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/adventpro"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/adventpro/./**/AdventPro-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/adventpro/./**/AdventPro\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/adventpro/./**/AdventPro-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/adventpro/./**/AdventPro[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
