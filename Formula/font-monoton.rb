f103c225ef6618a91e2266a32ec0997b03ce6c33
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-monoton updated

--- Formula/font-monoton.rb
+++ Formula/font-monoton.rb
@@ -1,12 +1,14 @@
 class FontMonoton < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/monoton/Monoton-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Monoton"
+  desc "Monoton font"
   homepage "https://fonts.google.com/specimen/Monoton"
+  head "https://github.com/google/fonts/raw/main/ofl/monoton/Monoton-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Monoton-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
