06793ca23d4f90b9ccbd48becadd9c4da3c53778
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:12 +0000

    font-gloock updated

--- Formula/font-gloock.rb
+++ Formula/font-gloock.rb
@@ -1,13 +1,14 @@
 class FontGloock < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gloock/Gloock-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gloock"
   desc "Smooth relationship between the thin and thick strokes"
   homepage "https://fonts.google.com/specimen/Gloock"
+  head "https://github.com/google/fonts/raw/main/ofl/gloock/Gloock-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Gloock-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
