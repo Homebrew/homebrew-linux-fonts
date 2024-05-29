dacb38e6f37be1a530745a1f60f0d227b4fbfcc5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-dm-serif-display updated

--- Formula/font-dm-serif-display.rb
+++ Formula/font-dm-serif-display.rb
@@ -1,13 +1,17 @@
 class FontDmSerifDisplay < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/dmserifdisplay"
-  desc "DM Serif Display"
+  desc "Dm serif display font"
   homepage "https://fonts.google.com/specimen/DM+Serif+Display"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/dmserifdisplay"
+
   def install
     (share/"fonts").install Dir.glob("ofl/dmserifdisplay/./**/DMSerifDisplay-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/dmserifdisplay/./**/DMSerifDisplay-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
