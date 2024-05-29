b342f726581568ad996021b26a636be472f50f6a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-humor-sans updated

--- Formula/font-humor-sans.rb
+++ Formula/font-humor-sans.rb
@@ -1,14 +1,16 @@
 class FontHumorSans < Formula
+  desc "Humor sans font"
+  homepage "https://xkcdsucks.blogspot.com.au/2009/03/xkcdsucks-is-proud-to-present-humor.html"
+  url "https://antiyawn.com/uploads/Humor-Sans-1.0.ttf",
+       verified: "antiyawn.com/"
   version "1.0"
   sha256 "2ded6a27448c9ed30aaff177744e2bcf1e52e0aab018b2a8be64565df633318f"
-  url "https://antiyawn.com/uploads/Humor-Sans-#{version}.ttf", verified: "antiyawn.com/"
-  desc "Humor Sans"
-  homepage "https://xkcdsucks.blogspot.com.au/2009/03/xkcdsucks-is-proud-to-present-humor.html"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Humor-Sans-#{version}.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Humor-Sans-1.0.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
