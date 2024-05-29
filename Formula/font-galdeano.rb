a7678aa1b6871c81193ff87062458dab4262c6a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-galdeano updated

--- Formula/font-galdeano.rb
+++ Formula/font-galdeano.rb
@@ -1,12 +1,14 @@
 class FontGaldeano < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/galdeano/Galdeano-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Galdeano"
+  desc "Galdeano font"
   homepage "https://fonts.google.com/specimen/Galdeano"
+  head "https://github.com/google/fonts/raw/main/ofl/galdeano/Galdeano-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Galdeano-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
