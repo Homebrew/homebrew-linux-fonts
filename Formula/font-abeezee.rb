bbb71f44c6ea3a110cd4d905937fb9011a15a012
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-abeezee updated

--- Formula/font-abeezee.rb
+++ Formula/font-abeezee.rb
@@ -1,13 +1,17 @@
 class FontAbeezee < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/abeezee"
-  desc "ABeeZee"
+  desc "Abeezee font"
   homepage "https://fonts.google.com/specimen/ABeeZee"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/abeezee"
+
   def install
     (share/"fonts").install Dir.glob("ofl/abeezee/./**/ABeeZee-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/abeezee/./**/ABeeZee-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
