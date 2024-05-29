862b1f82bfdf41f3b9570dac84e55bdebbdec68e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-iranian-serif updated

--- Formula/font-iranian-serif.rb
+++ Formula/font-iranian-serif.rb
@@ -1,12 +1,13 @@
 class FontIranianSerif < Formula
-  head "https://fontlibrary.org/assets/downloads/iranian-serif/3bf122e0d984a0a116a95491c835ec49/iranian-serif.zip"
-  desc "Iranian Serif"
+  desc "Iranian serif font"
   homepage "https://fontlibrary.org/en/font/iranian-serif"
+  head "https://fontlibrary.org/assets/downloads/iranian-serif/3bf122e0d984a0a116a95491c835ec49/iranian-serif.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/irseri.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
