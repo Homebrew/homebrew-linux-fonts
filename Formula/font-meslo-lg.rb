7a9b6438d4fa41f16f86e43c9c720dd0087d20f1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:40 +0000

    font-meslo-lg updated

--- Formula/font-meslo-lg.rb
+++ Formula/font-meslo-lg.rb
@@ -1,25 +1,26 @@
 class FontMesloLg < Formula
+  desc "Meslo lg font"
+  homepage "https://github.com/andreberg/Meslo-Font"
+  url "https://github.com/andreberg/Meslo-Font/raw/master/dist/v1.2.1/Meslo%20LG%20v1.2.1.zip"
   version "1.2.1"
   sha256 "d0bcb7668dda8fa1a0f8162d626adb434c32854e243b5bd52a717cf569af08d0"
-  url "https://github.com/andreberg/Meslo-Font/raw/master/dist/v#{version}/Meslo%20LG%20v#{version}.zip"
-  desc "Meslo LG"
-  homepage "https://github.com/andreberg/Meslo-Font"
+
   def install
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGL-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGL-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGL-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGL-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGM-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGM-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGM-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGM-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGS-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGS-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGS-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("Meslo LG v#{version}/**/MesloLGS-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGS-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGL-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGL-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGL-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGM-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGM-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGM-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGM-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGS-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGS-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGS-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo LG v1.2.1/MesloLGL-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
