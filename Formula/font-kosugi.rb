10452cb15c923ecf304962d2b625a5438919b189
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-kosugi updated

--- Formula/font-kosugi.rb
+++ Formula/font-kosugi.rb
@@ -1,13 +1,14 @@
 class FontKosugi < Formula
-  head "https://github.com/google/fonts/raw/main/apache/kosugi/Kosugi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kosugi"
   desc "Available as kosugi maru"
   homepage "https://fonts.google.com/specimen/Kosugi"
+  head "https://github.com/google/fonts/raw/main/apache/kosugi/Kosugi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Kosugi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
