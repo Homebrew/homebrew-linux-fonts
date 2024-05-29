b5112e81ebdd7e9ce8748d27776f71bbd7941e8e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-ocr updated

--- Formula/font-ocr.rb
+++ Formula/font-ocr.rb
@@ -1,20 +1,22 @@
 class FontOcr < Formula
+  desc "Ocr font"
+  homepage "https://ansuz.sooke.bc.ca/page/fonts#ocra"
+  url "https://osdn.dl.osdn.jp/tsukurimashou/56948/ocr-0.2.zip",
+       verified: "osdn.dl.osdn.jp/tsukurimashou/"
   version "0.2"
   sha256 "39289c641520265ecedbade99f01600f316f8196ec57f71c8402d3ba09438666"
-  url "https://osdn.dl.osdn.jp/tsukurimashou/56948/ocr-#{version}.zip", verified: "osdn.dl.osdn.jp/tsukurimashou/"
-  desc "OCR"
-  homepage "https://ansuz.sooke.bc.ca/page/fonts#ocra"
+
   def install
-    (share/"fonts").install Dir.glob("ocr-#{version}/**/OCRA.otf")[0]
-    (share/"fonts").install Dir.glob("ocr-#{version}/**/OCRB.otf")[0]
-    (share/"fonts").install Dir.glob("ocr-#{version}/**/OCRBE.otf")[0]
-    (share/"fonts").install Dir.glob("ocr-#{version}/**/OCRBF.otf")[0]
-    (share/"fonts").install Dir.glob("ocr-#{version}/**/OCRBL.otf")[0]
-    (share/"fonts").install Dir.glob("ocr-#{version}/**/OCRBS.otf")[0]
-    (share/"fonts").install Dir.glob("ocr-#{version}/**/OCRBX.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ocr-0.2/OCRA.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ocr-0.2/OCRB.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ocr-0.2/OCRBE.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ocr-0.2/OCRBF.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ocr-0.2/OCRBL.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ocr-0.2/OCRBS.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ocr-0.2/OCRBX.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
