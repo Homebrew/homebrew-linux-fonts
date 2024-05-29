68ba14876ef7e8e590f559757b5bc469786e2ee3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-escumasia updated

--- Formula/font-escumasia.rb
+++ Formula/font-escumasia.rb
@@ -1,13 +1,13 @@
 class FontEscumasia < Formula
-  head "https://moji-waku.com/download/escumasia.zip"
-  desc "Escumasia"
   desc "Ancient biology styled font inspired by escumasia"
   homepage "https://moji-waku.com/escumasia/index.html"
+  head "https://moji-waku.com/download/escumasia.zip"
+
   def install
-    (share/"fonts").install Dir.glob("escumasia/**/Escumasia-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/escumasia/Escumasia-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
