6f3f8c44c07a67d735389b0885b1e19c8edd2463
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-caveat-brush updated

--- Formula/font-caveat-brush.rb
+++ Formula/font-caveat-brush.rb
@@ -1,12 +1,14 @@
 class FontCaveatBrush < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/caveatbrush/CaveatBrush-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Caveat Brush"
+  desc "Caveat brush font"
   homepage "https://fonts.google.com/specimen/Caveat+Brush"
+  head "https://github.com/google/fonts/raw/main/ofl/caveatbrush/CaveatBrush-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/CaveatBrush-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
