503462742f38e47f6115ab058b92e64cef393893
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-allura updated

--- Formula/font-allura.rb
+++ Formula/font-allura.rb
@@ -1,12 +1,14 @@
 class FontAllura < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/allura/Allura-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Allura"
+  desc "Allura font"
   homepage "https://fonts.google.com/specimen/Allura"
+  head "https://github.com/google/fonts/raw/main/ofl/allura/Allura-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Allura-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
