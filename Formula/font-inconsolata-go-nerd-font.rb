03499569731b219aa9f7817c22d170d9ff900e37
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:20 +0000

    font-inconsolata-go-nerd-font updated

--- Formula/font-inconsolata-go-nerd-font.rb
+++ Formula/font-inconsolata-go-nerd-font.rb
@@ -1,10 +1,10 @@
 class FontInconsolataGoNerdFont < Formula
-  version "3.2.1"
-  sha256 "ca4e96a27c1716a026ad8de51bbb6f066ed6e6582296e4d046b595a7ea3cb5b3"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataGo.zip"
-  desc "InconsolataGo Nerd Font (InconsolataGo)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/InconsolataGo.zip"
+  version "3.2.1"
+  sha256 "ca4e96a27c1716a026ad8de51bbb6f066ed6e6582296e4d046b595a7ea3cb5b3"
+
   def install
     (share/"fonts").install Dir.glob("./**/InconsolataGoNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/InconsolataGoNerdFont-Regular.ttf")[0]
@@ -13,8 +13,8 @@ class FontInconsolataGoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/InconsolataGoNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/InconsolataGoNerdFontPropo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
