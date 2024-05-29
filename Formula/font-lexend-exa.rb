1676c9665613dc640f6b12cd57b7704b2619547d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:31 +0000

    font-lexend-exa updated

--- Formula/font-lexend-exa.rb
+++ Formula/font-lexend-exa.rb
@@ -1,12 +1,14 @@
 class FontLexendExa < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lexendexa/LexendExa%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Lexend Exa"
+  desc "Lexend exa font"
   homepage "https://fonts.google.com/specimen/Lexend+Exa"
+  head "https://github.com/google/fonts/raw/main/ofl/lexendexa/LexendExa%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/LexendExa\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LexendExa[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
