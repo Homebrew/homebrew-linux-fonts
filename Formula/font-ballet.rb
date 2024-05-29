ec9dbb9acc9dd5f567cd94e0d7af43842d3b7f65
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-ballet updated

--- Formula/font-ballet.rb
+++ Formula/font-ballet.rb
@@ -1,13 +1,14 @@
 class FontBallet < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ballet/Ballet%5Bopsz%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Ballet"
   desc "Designed by maximiliano sproviero and developed by eduardo tunni"
   homepage "https://fonts.google.com/specimen/Ballet"
+  head "https://github.com/google/fonts/raw/main/ofl/ballet/Ballet%5Bopsz%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Ballet\\[opsz\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Ballet[opsz].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
