d55a0a433d5366633a1ecfb7c1dbefb38fa5a2a0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-medefaidrin updated

--- Formula/font-noto-sans-medefaidrin.rb
+++ Formula/font-noto-sans-medefaidrin.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMedefaidrin < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmedefaidrin/NotoSansMedefaidrin%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Medefaidrin"
+  desc "Noto sans medefaidrin font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Medefaidrin"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmedefaidrin/NotoSansMedefaidrin%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansMedefaidrin\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansMedefaidrin[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
