78090b701939baa7dcbdb3361fd12d2f5d7929e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-amaranth updated

--- Formula/font-amaranth.rb
+++ Formula/font-amaranth.rb
@@ -1,15 +1,19 @@
 class FontAmaranth < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/amaranth"
-  desc "Amaranth"
+  desc "Amaranth font"
   homepage "https://fonts.google.com/specimen/Amaranth"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/amaranth"
+
   def install
     (share/"fonts").install Dir.glob("ofl/amaranth/./**/Amaranth-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/amaranth/./**/Amaranth-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/amaranth/./**/Amaranth-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/amaranth/./**/Amaranth-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
