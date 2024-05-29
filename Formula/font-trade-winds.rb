190554cd6b55494ca5cae1642507363eeb3a9a9a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:31 +0000

    font-trade-winds updated

--- Formula/font-trade-winds.rb
+++ Formula/font-trade-winds.rb
@@ -1,12 +1,14 @@
 class FontTradeWinds < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tradewinds/TradeWinds-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Trade Winds"
+  desc "Trade winds font"
   homepage "https://fonts.google.com/specimen/Trade+Winds"
+  head "https://github.com/google/fonts/raw/main/ofl/tradewinds/TradeWinds-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/TradeWinds-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
