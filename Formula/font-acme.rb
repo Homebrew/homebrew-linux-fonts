026b085c75bb955b13bb4d341bb388a51b729b99
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-acme updated

--- Formula/font-acme.rb
+++ Formula/font-acme.rb
@@ -1,12 +1,14 @@
 class FontAcme < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/acme/Acme-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Acme"
+  desc "Acme font"
   homepage "https://fonts.google.com/specimen/Acme"
+  head "https://github.com/google/fonts/raw/main/ofl/acme/Acme-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Acme-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
