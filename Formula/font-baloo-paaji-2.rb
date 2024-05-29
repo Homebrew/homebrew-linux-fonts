772777359612e541b89243308cfe07e09b6593a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-baloo-paaji-2 updated

--- Formula/font-baloo-paaji-2.rb
+++ Formula/font-baloo-paaji-2.rb
@@ -1,12 +1,14 @@
 class FontBalooPaaji2 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/baloopaaji2/BalooPaaji2%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Baloo Paaji 2"
+  desc "Baloo paaji 2 font"
   homepage "https://fonts.google.com/specimen/Baloo+Paaji+2"
+  head "https://github.com/google/fonts/raw/main/ofl/baloopaaji2/BalooPaaji2%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BalooPaaji2\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooPaaji2[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
