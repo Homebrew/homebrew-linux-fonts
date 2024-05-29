4a13f60d8fc0b82e9e582979a3011162dc277c13
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-lxgw-neozhisong updated

--- Formula/font-lxgw-neozhisong.rb
+++ Formula/font-lxgw-neozhisong.rb
@@ -1,16 +1,15 @@
 class FontLxgwNeozhisong < Formula
-  version "0.923"
-  sha256 "329d163177c5388d68cd4354936d89c30b4bdc44afe7536a606ace9e203c4fba"
-  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v#{version}/LXGWNeoZhiSong.ttf"
-  desc "LXGW Neo ZhiSong"
-  desc "霞鹜新致宋"
   desc "Chinese serif font derived from IPAmj Mincho"
   homepage "https://github.com/lxgw/LxgwNeoZhiSong"
+  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v0.923/LXGWNeoZhiSong.ttf"
+  version "0.923"
+  sha256 "329d163177c5388d68cd4354936d89c30b4bdc44afe7536a606ace9e203c4fba"
+
   def install
     (share/"fonts").install Dir.glob("./**/LXGWNeoZhiSong.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
