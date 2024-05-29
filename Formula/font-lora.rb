3b343e7ce517b0518cd7b22bf76be8ebc850b221
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-lora updated

--- Formula/font-lora.rb
+++ Formula/font-lora.rb
@@ -1,13 +1,17 @@
 class FontLora < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/lora"
-  desc "Lora"
+  desc "Lora font"
   homepage "https://fonts.google.com/specimen/Lora"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/lora"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/lora/./**/Lora-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/lora/./**/Lora\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/lora/./**/Lora-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/lora/./**/Lora[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
