d0ea0152aa87b5be0434e8dffabca84875b1f65c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-diplomata-sc updated

--- Formula/font-diplomata-sc.rb
+++ Formula/font-diplomata-sc.rb
@@ -1,12 +1,14 @@
 class FontDiplomataSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/diplomatasc/DiplomataSC-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Diplomata SC"
+  desc "Diplomata sc font"
   homepage "https://fonts.google.com/specimen/Diplomata+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/diplomatasc/DiplomataSC-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DiplomataSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
