12a96972e4c6360a9c2b37aabf36fa83878c5e31
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:33 +0000

    font-unifrakturcook updated

--- Formula/font-unifrakturcook.rb
+++ Formula/font-unifrakturcook.rb
@@ -1,12 +1,14 @@
 class FontUnifrakturcook < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/unifrakturcook/UnifrakturCook-Bold.ttf", verified: "github.com/google/fonts/"
-  desc "UnifrakturCook"
+  desc "Unifrakturcook font"
   homepage "https://fonts.google.com/specimen/UnifrakturCook"
+  head "https://github.com/google/fonts/raw/main/ofl/unifrakturcook/UnifrakturCook-Bold.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/UnifrakturCook-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
