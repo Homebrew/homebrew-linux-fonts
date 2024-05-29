b3504e443f66c9bfd7a93fd94a3f11cf70d822e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-meetei-mayek updated

--- Formula/font-noto-sans-meetei-mayek.rb
+++ Formula/font-noto-sans-meetei-mayek.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMeeteiMayek < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmeeteimayek/NotoSansMeeteiMayek%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Meetei Mayek"
+  desc "Noto sans meetei mayek font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Meetei+Mayek"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmeeteimayek/NotoSansMeeteiMayek%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansMeeteiMayek\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansMeeteiMayek[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
