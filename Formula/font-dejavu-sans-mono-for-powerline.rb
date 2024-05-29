e0730f997c8f5fe52f5ffbec05afe51e8b9b758c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-dejavu-sans-mono-for-powerline updated

--- Formula/font-dejavu-sans-mono-for-powerline.rb
+++ Formula/font-dejavu-sans-mono-for-powerline.rb
@@ -1,15 +1,18 @@
 class FontDejavuSansMonoForPowerline < Formula
-  head "https://github.com/powerline/fonts.git", branch: "master", only_path: "DejaVuSansMono"
-  desc "DejaVu Sans Mono for Powerline"
+  desc "Dejavu sans mono for powerline font"
   homepage "https://github.com/powerline/fonts/tree/master/DejaVuSansMono"
+  head "https://github.com/powerline/fonts.git",
+       branch:    "master",
+       only_path: "DejaVuSansMono"
+
   def install
     (share/"fonts").install Dir.glob("DejaVuSansMono/./**/DejaVu Sans Mono for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("DejaVuSansMono/./**/DejaVu Sans Mono Bold for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("DejaVuSansMono/./**/DejaVu Sans Mono Oblique for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("DejaVuSansMono/./**/DejaVu Sans Mono Bold Oblique for Powerline.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
