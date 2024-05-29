7f93a437104fe99724603ad6cd721cd0d5aed23c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-east-sea-dokdo updated

--- Formula/font-east-sea-dokdo.rb
+++ Formula/font-east-sea-dokdo.rb
@@ -1,12 +1,14 @@
 class FontEastSeaDokdo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/eastseadokdo/EastSeaDokdo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "East Sea Dokdo"
+  desc "East sea dokdo font"
   homepage "https://fonts.google.com/specimen/East+Sea+Dokdo"
+  head "https://github.com/google/fonts/raw/main/ofl/eastseadokdo/EastSeaDokdo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/EastSeaDokdo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
