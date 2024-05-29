d20221d7ab7be083f3e4fae40674cbfef3bffebc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-salsa updated

--- Formula/font-salsa.rb
+++ Formula/font-salsa.rb
@@ -1,12 +1,14 @@
 class FontSalsa < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/salsa/Salsa-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Salsa"
+  desc "Salsa font"
   homepage "https://fonts.google.com/specimen/Salsa"
+  head "https://github.com/google/fonts/raw/main/ofl/salsa/Salsa-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Salsa-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
