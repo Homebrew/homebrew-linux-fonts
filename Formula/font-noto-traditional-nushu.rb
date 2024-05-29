b0359080a7b6d6bcc4aaf9186a9208aad31c617c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-noto-traditional-nushu updated

--- Formula/font-noto-traditional-nushu.rb
+++ Formula/font-noto-traditional-nushu.rb
@@ -1,12 +1,14 @@
 class FontNotoTraditionalNushu < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nototraditionalnushu/NotoTraditionalNushu%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Traditional Nushu"
+  desc "Noto traditional nushu font"
   homepage "https://fonts.google.com/specimen/Noto+Traditional+Nushu"
+  head "https://github.com/google/fonts/raw/main/ofl/nototraditionalnushu/NotoTraditionalNushu%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoTraditionalNushu\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoTraditionalNushu[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
