0a593820a96950287067312d7c8f983ea8a9ad23
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-rammetto-one updated

--- Formula/font-rammetto-one.rb
+++ Formula/font-rammetto-one.rb
@@ -1,12 +1,14 @@
 class FontRammettoOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rammettoone/RammettoOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rammetto One"
+  desc "Rammetto one font"
   homepage "https://fonts.google.com/specimen/Rammetto+One"
+  head "https://github.com/google/fonts/raw/main/ofl/rammettoone/RammettoOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RammettoOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
