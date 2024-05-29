18d18ed9ef27c4965ca71de63bfd9a85eacd6bed
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-rouge-script updated

--- Formula/font-rouge-script.rb
+++ Formula/font-rouge-script.rb
@@ -1,12 +1,14 @@
 class FontRougeScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rougescript/RougeScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rouge Script"
+  desc "Rouge script font"
   homepage "https://fonts.google.com/specimen/Rouge+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/rougescript/RougeScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RougeScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
