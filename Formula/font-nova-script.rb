3b4c358c9e44b93da85142092cb46c887766fbe4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-nova-script updated

--- Formula/font-nova-script.rb
+++ Formula/font-nova-script.rb
@@ -1,12 +1,14 @@
 class FontNovaScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/novascript/NovaScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Nova Script"
+  desc "Nova script font"
   homepage "https://fonts.google.com/specimen/Nova+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/novascript/NovaScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NovaScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
