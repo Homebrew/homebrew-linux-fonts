fcaef1f9aaa4573cdf9b9de5b8b854aded699bbd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-shippori-mincho-b1 updated

--- Formula/font-shippori-mincho-b1.rb
+++ Formula/font-shippori-mincho-b1.rb
@@ -1,8 +1,11 @@
 class FontShipporiMinchoB1 < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/shipporiminchob1"
-  desc "Shippori Mincho B1"
   desc "Based on the Tsukiji Typeface making facility of Tokyo"
   homepage "https://fonts.google.com/specimen/Shippori+Mincho+B1"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/shipporiminchob1"
+
   def install
     (share/"fonts").install Dir.glob("ofl/shipporiminchob1/./**/ShipporiMinchoB1-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/shipporiminchob1/./**/ShipporiMinchoB1-ExtraBold.ttf")[0]
@@ -10,8 +13,8 @@ class FontShipporiMinchoB1 < Formula
     (share/"fonts").install Dir.glob("ofl/shipporiminchob1/./**/ShipporiMinchoB1-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/shipporiminchob1/./**/ShipporiMinchoB1-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
