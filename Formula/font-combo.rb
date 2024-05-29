2f1a7217c10fa97cd5076d235d1ec9478feffb42
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-combo updated

--- Formula/font-combo.rb
+++ Formula/font-combo.rb
@@ -1,12 +1,14 @@
 class FontCombo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/combo/Combo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Combo"
+  desc "Combo font"
   homepage "https://fonts.google.com/specimen/Combo"
+  head "https://github.com/google/fonts/raw/main/ofl/combo/Combo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Combo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
