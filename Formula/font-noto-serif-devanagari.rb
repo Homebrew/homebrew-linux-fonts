47e3f0aa1f5b07d1f77355c6b62aac8c262e246c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-devanagari updated

--- Formula/font-noto-serif-devanagari.rb
+++ Formula/font-noto-serif-devanagari.rb
@@ -1,13 +1,15 @@
 class FontNotoSerifDevanagari < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifDevanagari-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Serif Devanagari"
+  desc "Noto serif devanagari font"
   homepage "https://www.google.com/get/noto/#serif-deva"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifDevanagari-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifDevanagari-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifDevanagari-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
