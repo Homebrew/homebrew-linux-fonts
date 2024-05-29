711217bc115e3ef1d84838d4d540414d8552a4a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-klee-one updated

--- Formula/font-klee-one.rb
+++ Formula/font-klee-one.rb
@@ -1,13 +1,17 @@
 class FontKleeOne < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kleeone"
-  desc "Klee One"
+  desc "Klee one font"
   homepage "https://fonts.google.com/specimen/Klee+One"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kleeone"
+
   def install
     (share/"fonts").install Dir.glob("ofl/kleeone/./**/KleeOne-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kleeone/./**/KleeOne-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
