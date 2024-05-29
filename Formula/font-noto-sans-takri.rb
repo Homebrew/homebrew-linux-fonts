9c4105f923f5575293fb74d674aaae779e031883
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-takri updated

--- Formula/font-noto-sans-takri.rb
+++ Formula/font-noto-sans-takri.rb
@@ -1,12 +1,14 @@
 class FontNotoSansTakri < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanstakri/NotoSansTakri-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Takri"
+  desc "Noto sans takri font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Takri"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanstakri/NotoSansTakri-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansTakri-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
