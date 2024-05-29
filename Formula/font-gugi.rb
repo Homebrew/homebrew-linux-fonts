fa9e985c970f348360c20d3f09a7ecd5e207c3af
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-gugi updated

--- Formula/font-gugi.rb
+++ Formula/font-gugi.rb
@@ -1,12 +1,14 @@
 class FontGugi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gugi/Gugi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gugi"
+  desc "Gugi font"
   homepage "https://fonts.google.com/specimen/Gugi"
+  head "https://github.com/google/fonts/raw/main/ofl/gugi/Gugi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Gugi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
