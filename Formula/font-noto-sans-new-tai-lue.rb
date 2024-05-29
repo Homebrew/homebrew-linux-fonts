130c9b646f2328bd41009e83ab58c88ab847d932
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:54 +0000

    font-noto-sans-new-tai-lue updated

--- Formula/font-noto-sans-new-tai-lue.rb
+++ Formula/font-noto-sans-new-tai-lue.rb
@@ -1,12 +1,14 @@
 class FontNotoSansNewTaiLue < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNewTaiLue-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans New Tai Lue"
+  desc "Noto sans new tai lue font"
   homepage "https://www.google.com/get/noto/#sans-talu"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNewTaiLue-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansNewTaiLue-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
