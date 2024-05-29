5150da0b33ab733d212f8f747531a497da18dac9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:50 +0000

    font-bungee-spice updated

--- Formula/font-bungee-spice.rb
+++ Formula/font-bungee-spice.rb
@@ -1,13 +1,14 @@
 class FontBungeeSpice < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bungeespice/BungeeSpice-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bungee Spice"
   desc "Released under the sil open font license"
   homepage "https://fonts.google.com/specimen/Bungee+Spice"
+  head "https://github.com/google/fonts/raw/main/ofl/bungeespice/BungeeSpice-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BungeeSpice-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
