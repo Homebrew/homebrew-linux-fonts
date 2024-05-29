eb927bcfc458bc08d6649698d100afd82dd7e3b6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-tex-gyre-pagella-math updated

--- Formula/font-tex-gyre-pagella-math.rb
+++ Formula/font-tex-gyre-pagella-math.rb
@@ -1,14 +1,15 @@
 class FontTexGyrePagellaMath < Formula
+  desc "Pagella math font"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tg-math"
+  url "https://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrepagella-math-1632.zip"
   version "1.632"
   sha256 "68a9c0ce195915334673960b13a281f24d31a8ae380454a0e35652dba506d474"
-  url "https://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrepagella-math-#{version.to_s.gsub(".", "")}.zip"
-  desc "Pagella Math"
-  homepage "https://www.gust.org.pl/projects/e-foundry/tg-math"
+
   def install
-    (share/"fonts").install Dir.glob("texgyrepagella-math-#{version.to_s.gsub(".", "")}/opentype/**/texgyrepagella-math.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyrepagella-math-1632/opentype/texgyrepagella-math.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
