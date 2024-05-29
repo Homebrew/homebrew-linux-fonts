dbc0da3c2b7fb192732db1f7bf183e4ce5ea93fb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-saira-extra-condensed updated

--- Formula/font-saira-extra-condensed.rb
+++ Formula/font-saira-extra-condensed.rb
@@ -1,9 +1,13 @@
 class FontSairaExtraCondensed < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sairaextracondensed"
-  desc "Saira Extra Condensed"
+  desc "Saira extra condensed font"
   homepage "https://fonts.google.com/specimen/Saira+Extra+Condensed"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sairaextracondensed"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-ExtraLight.ttf")[0]
@@ -11,10 +15,10 @@ class FontSairaExtraCondensed < Formula
     (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sairaextracondensed/./**/SairaExtraCondensed-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
