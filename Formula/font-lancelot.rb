06cfc7dce6c9031147261f3213f8e7d2def92d3a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-lancelot updated

--- Formula/font-lancelot.rb
+++ Formula/font-lancelot.rb
@@ -1,12 +1,14 @@
 class FontLancelot < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lancelot/Lancelot-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Lancelot"
+  desc "Lancelot font"
   homepage "https://fonts.google.com/specimen/Lancelot"
+  head "https://github.com/google/fonts/raw/main/ofl/lancelot/Lancelot-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Lancelot-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
