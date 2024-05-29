748e9d490a025818a6bfabfcec175364c0a5d59a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-kdam-thmor-pro updated

--- Formula/font-kdam-thmor-pro.rb
+++ Formula/font-kdam-thmor-pro.rb
@@ -1,13 +1,14 @@
 class FontKdamThmorPro < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kdamthmorpro/KdamThmorPro-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kdam Thmor Pro"
   desc "Used as the latin counterpart in the project"
   homepage "https://fonts.google.com/specimen/Kdam+Thmor+Pro"
+  head "https://github.com/google/fonts/raw/main/ofl/kdamthmorpro/KdamThmorPro-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/KdamThmorPro-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
