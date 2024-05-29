3d4cee1e22e117f356eae4c19635686dbcc896a5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:56 +0000

    font-noto-sans-tagalog updated

--- Formula/font-noto-sans-tagalog.rb
+++ Formula/font-noto-sans-tagalog.rb
@@ -1,12 +1,14 @@
 class FontNotoSansTagalog < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagalog-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Tagalog"
+  desc "Noto sans tagalog font"
   homepage "https://www.google.com/get/noto/#sans-tglg"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagalog-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansTagalog-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
