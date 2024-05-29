64e6b30e0755b7c5cff855f8f178b8ccb6b06314
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-lxgw-wenkai-lite updated

--- Formula/font-lxgw-wenkai-lite.rb
+++ Formula/font-lxgw-wenkai-lite.rb
@@ -1,21 +1,20 @@
 class FontLxgwWenkaiLite < Formula
-  version "1.330"
-  sha256 "10beb11701118e6f1b301cdc2b2a2e6177e0fe53671c5f802603a9e9bd9c168d"
-  url "https://github.com/lxgw/LxgwWenKai-Lite/releases/download/v#{version}/lxgw-wenkai-lite-v#{version}.zip"
-  desc "LXGW WenKai Lite"
-  desc "霞鹜文楷 轻便版"
   desc "Open-source Chinese font derived from Fontworks' Klee One"
   homepage "https://github.com/lxgw/LxgwWenKai-Lite"
+  url "https://github.com/lxgw/LxgwWenKai-Lite/releases/download/v1.330/lxgw-wenkai-lite-v1.330.zip"
+  version "1.330"
+  sha256 "10beb11701118e6f1b301cdc2b2a2e6177e0fe53671c5f802603a9e9bd9c168d"
+
   def install
-    (share/"fonts").install Dir.glob("lxgw-wenkai-lite-v#{version}/**/LXGWWenKaiLite-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-lite-v#{version}/**/LXGWWenKaiLite-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-lite-v#{version}/**/LXGWWenKaiLite-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-lite-v#{version}/**/LXGWWenKaiMonoLite-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-lite-v#{version}/**/LXGWWenKaiMonoLite-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("lxgw-wenkai-lite-v#{version}/**/LXGWWenKaiMonoLite-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-lite-v1.330/LXGWWenKaiLite-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-lite-v1.330/LXGWWenKaiLite-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-lite-v1.330/LXGWWenKaiLite-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-lite-v1.330/LXGWWenKaiMonoLite-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-lite-v1.330/LXGWWenKaiMonoLite-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-lite-v1.330/LXGWWenKaiMonoLite-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
