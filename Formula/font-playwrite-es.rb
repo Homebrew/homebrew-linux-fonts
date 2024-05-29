206e9a6db2059290b00c6cb10ae45d6a161b58f4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:07 +0000

    font-playwrite-es updated

--- Formula/font-playwrite-es.rb
+++ Formula/font-playwrite-es.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteEs < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwritees/PlaywriteES%5Bwght%5D.ttf"
-  desc "Playwrite ES"
+  desc "Playwrite es font"
   homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritees/PlaywriteES%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteES\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteES[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
