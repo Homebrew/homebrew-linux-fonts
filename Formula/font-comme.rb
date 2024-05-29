4028310b5acf87aff7fb34921ab0e420c1e41baf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-comme updated

--- Formula/font-comme.rb
+++ Formula/font-comme.rb
@@ -1,13 +1,14 @@
 class FontComme < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/comme/Comme%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Comme"
   desc "Variable, with a weight axis ranging from thin to black"
   homepage "https://fonts.google.com/specimen/Comme"
+  head "https://github.com/google/fonts/raw/main/ofl/comme/Comme%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Comme\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Comme[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
