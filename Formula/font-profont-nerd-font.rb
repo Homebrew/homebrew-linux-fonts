1c393f02dd3455aa93e2a7ce434c96c493cb0467
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:10 +0000

    font-profont-nerd-font updated

--- Formula/font-profont-nerd-font.rb
+++ Formula/font-profont-nerd-font.rb
@@ -1,10 +1,10 @@
 class FontProfontNerdFont < Formula
-  version "3.2.1"
-  sha256 "ae5898e790662f261471f3dd263f4357981b23bff68ee2ed1290a64901fa1d87"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ProFont.zip"
-  desc "ProFont Nerd Font families (ProFont)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/ProFont.zip"
+  version "3.2.1"
+  sha256 "ae5898e790662f261471f3dd263f4357981b23bff68ee2ed1290a64901fa1d87"
+
   def install
     (share/"fonts").install Dir.glob("./**/ProFontIIxNerdFont-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/ProFontIIxNerdFontMono-Regular.ttf")[0]
@@ -13,8 +13,8 @@ class FontProfontNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/ProFontWindowsNerdFontMono-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/ProFontWindowsNerdFontPropo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
