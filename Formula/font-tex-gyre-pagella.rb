b68a72108ea7360f067dbab892c906dc61f52e6b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-tex-gyre-pagella updated

--- Formula/font-tex-gyre-pagella.rb
+++ Formula/font-tex-gyre-pagella.rb
@@ -1,17 +1,18 @@
 class FontTexGyrePagella < Formula
+  desc "Tex gyre pagella font"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl2_501otf.zip"
   version "2.501"
   sha256 "e16078d19860d68a54bcaedc00e35c2a81b396cdc36842700f1d556f89cf8ef2"
-  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl#{version.to_s.gsub(".", "_")}otf.zip"
-  desc "TeX Gyre Pagella"
-  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella"
+
   def install
-    (share/"fonts").install Dir.glob("qpl#{version.to_s.gsub(".", "_")}otf/**/texgyrepagella-bold.otf")[0]
-    (share/"fonts").install Dir.glob("qpl#{version.to_s.gsub(".", "_")}otf/**/texgyrepagella-bolditalic.otf")[0]
-    (share/"fonts").install Dir.glob("qpl#{version.to_s.gsub(".", "_")}otf/**/texgyrepagella-italic.otf")[0]
-    (share/"fonts").install Dir.glob("qpl#{version.to_s.gsub(".", "_")}otf/**/texgyrepagella-regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/qpl2_501otf/texgyrepagella-bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/qpl2_501otf/texgyrepagella-bolditalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/qpl2_501otf/texgyrepagella-italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/qpl2_501otf/texgyrepagella-regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
