54b237dd476e85500aeffbfb602fb485699fb54f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:32 +0000

    font-lexend-tera updated

--- Formula/font-lexend-tera.rb
+++ Formula/font-lexend-tera.rb
@@ -1,12 +1,14 @@
 class FontLexendTera < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lexendtera/LexendTera%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Lexend Tera"
+  desc "Lexend tera font"
   homepage "https://fonts.google.com/specimen/Lexend+Tera"
+  head "https://github.com/google/fonts/raw/main/ofl/lexendtera/LexendTera%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/LexendTera\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LexendTera[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
