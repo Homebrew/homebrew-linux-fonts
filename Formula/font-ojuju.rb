b9252bd07f06bfb3b4231dc7c03445a8b75524d1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-ojuju updated

--- Formula/font-ojuju.rb
+++ Formula/font-ojuju.rb
@@ -1,13 +1,14 @@
 class FontOjuju < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ojuju/Ojuju%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Ojuju"
   desc "Distinct with varying apertures as it moves from extra-light to bold"
   homepage "https://fonts.google.com/specimen/Ojuju"
+  head "https://github.com/google/fonts/raw/main/ofl/ojuju/Ojuju%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Ojuju\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Ojuju[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
