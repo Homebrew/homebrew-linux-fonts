03dbcbfe480ba59a80e1647cb747cdd74fe3b911
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-vollkorn-sc updated

--- Formula/font-vollkorn-sc.rb
+++ Formula/font-vollkorn-sc.rb
@@ -1,15 +1,19 @@
 class FontVollkornSc < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/vollkornsc"
-  desc "Vollkorn SC"
+  desc "Vollkorn sc font"
   homepage "https://fonts.google.com/specimen/Vollkorn+SC"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/vollkornsc"
+
   def install
     (share/"fonts").install Dir.glob("ofl/vollkornsc/./**/VollkornSC-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/vollkornsc/./**/VollkornSC-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/vollkornsc/./**/VollkornSC-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/vollkornsc/./**/VollkornSC-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
