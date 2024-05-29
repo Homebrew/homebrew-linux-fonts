776d50ff459f6f85060b8dad838e57cb30d626eb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-cormorant-garamond updated

--- Formula/font-cormorant-garamond.rb
+++ Formula/font-cormorant-garamond.rb
@@ -1,9 +1,13 @@
 class FontCormorantGaramond < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cormorantgaramond"
-  desc "Cormorant Garamond"
+  desc "Cormorant garamond font"
   homepage "https://fonts.google.com/specimen/Cormorant+Garamond"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cormorantgaramond"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-Light.ttf")[0]
@@ -12,10 +16,10 @@ class FontCormorantGaramond < Formula
     (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/cormorantgaramond/./**/CormorantGaramond-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
