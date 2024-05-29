1051f4a7547c177a827b33106691c4d6f24dfba5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:18 +0000

    font-rye updated

--- Formula/font-rye.rb
+++ Formula/font-rye.rb
@@ -1,12 +1,14 @@
 class FontRye < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rye/Rye-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rye"
+  desc "Rye font"
   homepage "https://fonts.google.com/specimen/Rye"
+  head "https://github.com/google/fonts/raw/main/ofl/rye/Rye-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Rye-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
