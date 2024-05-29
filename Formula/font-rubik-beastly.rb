8bba6819062c683db1b828d495185fd9e1a4f7fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-rubik-beastly updated

--- Formula/font-rubik-beastly.rb
+++ Formula/font-rubik-beastly.rb
@@ -1,13 +1,14 @@
 class FontRubikBeastly < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikbeastly/RubikBeastly-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Beastly"
   desc "Based on the google fonts rubik by hubert and fischer, meir sadan and cyreal"
   homepage "https://fonts.google.com/specimen/Rubik+Beastly"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikbeastly/RubikBeastly-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikBeastly-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
