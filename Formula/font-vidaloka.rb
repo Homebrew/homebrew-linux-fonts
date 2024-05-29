1933bf11e8a58174e4a169e612b87f743d1d5fd9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-vidaloka updated

--- Formula/font-vidaloka.rb
+++ Formula/font-vidaloka.rb
@@ -1,12 +1,14 @@
 class FontVidaloka < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/vidaloka/Vidaloka-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Vidaloka"
+  desc "Vidaloka font"
   homepage "https://fonts.google.com/specimen/Vidaloka"
+  head "https://github.com/google/fonts/raw/main/ofl/vidaloka/Vidaloka-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Vidaloka-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
