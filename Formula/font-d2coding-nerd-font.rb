2f9287082e406c20e183e26ea977d0d79266cc7c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-d2coding-nerd-font updated

--- Formula/font-d2coding-nerd-font.rb
+++ Formula/font-d2coding-nerd-font.rb
@@ -1,10 +1,10 @@
 class FontD2codingNerdFont < Formula
-  version "3.2.1"
-  sha256 "d7c2ec38d316583c8af0319068ac05488f0c053cccea842aae6089f3d64d758f"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/D2Coding.zip"
-  desc "D2CodingLigature Nerd Font (D2Coding)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/D2Coding.zip"
+  version "3.2.1"
+  sha256 "d7c2ec38d316583c8af0319068ac05488f0c053cccea842aae6089f3d64d758f"
+
   def install
     (share/"fonts").install Dir.glob("./**/D2CodingLigatureNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/D2CodingLigatureNerdFont-Regular.ttf")[0]
@@ -13,8 +13,8 @@ class FontD2codingNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/D2CodingLigatureNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/D2CodingLigatureNerdFontPropo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
