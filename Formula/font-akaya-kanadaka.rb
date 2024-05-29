21856a8ea98689c31694180fdec5cce6ef21df43
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-akaya-kanadaka updated

--- Formula/font-akaya-kanadaka.rb
+++ Formula/font-akaya-kanadaka.rb
@@ -1,12 +1,14 @@
 class FontAkayaKanadaka < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/akayakanadaka/AkayaKanadaka-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Akaya Kanadaka"
+  desc "Akaya kanadaka font"
   homepage "https://fonts.google.com/specimen/Akaya+Kanadaka"
+  head "https://github.com/google/fonts/raw/main/ofl/akayakanadaka/AkayaKanadaka-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AkayaKanadaka-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
