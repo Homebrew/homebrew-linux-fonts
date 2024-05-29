d3819450fea58b25e2d7859ae033b374beef619e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-fraunces updated

--- Formula/font-fraunces.rb
+++ Formula/font-fraunces.rb
@@ -1,14 +1,17 @@
 class FontFraunces < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fraunces"
-  desc "Fraunces"
   desc "Variable font with four axes"
   homepage "https://fonts.google.com/specimen/Fraunces"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/fraunces"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/fraunces/./**/Fraunces-Italic\\[SOFT,WONK,opsz,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/fraunces/./**/Fraunces\\[SOFT,WONK,opsz,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/fraunces/./**/Fraunces-Italic[SOFT,WONK,opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/fraunces/./**/Fraunces[SOFT,WONK,opsz,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
