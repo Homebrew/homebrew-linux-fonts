7e9f817e0926fd04a51dc22a3cd2e16567e4174b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-red-hat-mono updated

--- Formula/font-red-hat-mono.rb
+++ Formula/font-red-hat-mono.rb
@@ -1,13 +1,17 @@
 class FontRedHatMono < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/redhatmono"
-  desc "Red Hat Mono"
+  desc "Red hat mono font"
   homepage "https://fonts.google.com/specimen/Red+Hat+Mono"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/redhatmono"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/redhatmono/./**/RedHatMono-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/redhatmono/./**/RedHatMono\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/redhatmono/./**/RedHatMono-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/redhatmono/./**/RedHatMono[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
