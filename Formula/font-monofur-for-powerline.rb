f6bd760d4dc056f991c677ebad26fcc6b6aa55fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:42 +0000

    font-monofur-for-powerline updated

--- Formula/font-monofur-for-powerline.rb
+++ Formula/font-monofur-for-powerline.rb
@@ -1,14 +1,17 @@
 class FontMonofurForPowerline < Formula
-  head "https://github.com/powerline/fonts.git", branch: "master", only_path: "Monofur"
-  desc "monofur for Powerline"
+  desc "Monofur for powerline font"
   homepage "https://github.com/powerline/fonts/tree/master/Monofur"
+  head "https://github.com/powerline/fonts.git",
+       branch:    "master",
+       only_path: "Monofur"
+
   def install
     (share/"fonts").install Dir.glob("Monofur/./**/Monofur Bold for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("Monofur/./**/Monofur Italic for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("Monofur/./**/Monofur for Powerline.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
