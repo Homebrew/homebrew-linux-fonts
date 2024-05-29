158ee9cccfc50427b399f150e1c8862c017aa75a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-im-fell-types updated

--- Formula/font-im-fell-types.rb
+++ Formula/font-im-fell-types.rb
@@ -1,10 +1,12 @@
 class FontImFellTypes < Formula
-  head "https://iginomarini.com/fell/wp-content/uploads/IMFellTypesClass.zip"
-  desc "IM Fell Types"
+  desc "Im fell types font"
   homepage "https://iginomarini.com/fell/"
-  disable! date: "2024-04-04", because: :no_longer_available
+  head "https://iginomarini.com/fell/wp-content/uploads/IMFellTypesClass.zip"
+
+  disable! "2024-04-04", because: :no_longer_available
+
   def install
-    (share/"fonts").install Dir.glob("./**/IMFeDPit29C.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IMFeTLrm29C.otf")[0]
     (share/"fonts").install Dir.glob("./**/IMFeDPrm29C.otf")[0]
     (share/"fonts").install Dir.glob("./**/IMFeENit29C.otf")[0]
     (share/"fonts").install Dir.glob("./**/IMFeENrm29C.otf")[0]
@@ -16,10 +18,10 @@ class FontImFellTypes < Formula
     (share/"fonts").install Dir.glob("./**/IMFeGPrm29C.otf")[0]
     (share/"fonts").install Dir.glob("./**/IMFePIit29C.otf")[0]
     (share/"fonts").install Dir.glob("./**/IMFePIrm29C.otf")[0]
-    (share/"fonts").install Dir.glob("./**/IMFeTLrm29C.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IMFeDPit29C.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
