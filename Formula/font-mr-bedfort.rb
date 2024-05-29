8d50ebe045c4208586b211a775497f24cdbfa743
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-mr-bedfort updated

--- Formula/font-mr-bedfort.rb
+++ Formula/font-mr-bedfort.rb
@@ -1,12 +1,14 @@
 class FontMrBedfort < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mrbedfort/MrBedfort-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mr Bedfort"
+  desc "Mr bedfort font"
   homepage "https://fonts.google.com/specimen/Mr+Bedfort"
+  head "https://github.com/google/fonts/raw/main/ofl/mrbedfort/MrBedfort-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MrBedfort-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
