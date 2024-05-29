d07bedf540c0573e21876f3abe2e3ed61a8c8015
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-sedan updated

--- Formula/font-sedan.rb
+++ Formula/font-sedan.rb
@@ -1,13 +1,17 @@
 class FontSedan < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sedan"
-  desc "Sedan"
+  desc "Sedan font"
   homepage "https://fonts.google.com/specimen/Sedan"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sedan"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sedan/./**/Sedan-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sedan/./**/Sedan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
