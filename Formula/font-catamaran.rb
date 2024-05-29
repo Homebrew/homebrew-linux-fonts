c0be4c5ce07c800d0dcfd5c4502431d496546436
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:52 +0000

    font-catamaran updated

--- Formula/font-catamaran.rb
+++ Formula/font-catamaran.rb
@@ -1,12 +1,14 @@
 class FontCatamaran < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/catamaran/Catamaran%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Catamaran"
+  desc "Catamaran font"
   homepage "https://fonts.google.com/specimen/Catamaran"
+  head "https://github.com/google/fonts/raw/main/ofl/catamaran/Catamaran%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Catamaran\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Catamaran[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
