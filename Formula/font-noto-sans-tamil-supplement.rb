e9ac8cba4331374d32d5efbc839a17dbebeb3651
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-tamil-supplement updated

--- Formula/font-noto-sans-tamil-supplement.rb
+++ Formula/font-noto-sans-tamil-supplement.rb
@@ -1,12 +1,14 @@
 class FontNotoSansTamilSupplement < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanstamilsupplement/NotoSansTamilSupplement-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Tamil Supplement"
+  desc "Noto sans tamil supplement font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Tamil+Supplement"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanstamilsupplement/NotoSansTamilSupplement-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansTamilSupplement-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
