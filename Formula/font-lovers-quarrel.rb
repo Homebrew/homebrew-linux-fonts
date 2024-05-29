086736a12dda58686ba1d650bd01495e344f89cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-lovers-quarrel updated

--- Formula/font-lovers-quarrel.rb
+++ Formula/font-lovers-quarrel.rb
@@ -1,12 +1,14 @@
 class FontLoversQuarrel < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/loversquarrel/LoversQuarrel-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Lovers Quarrel"
+  desc "Lovers quarrel font"
   homepage "https://fonts.google.com/specimen/Lovers+Quarrel"
+  head "https://github.com/google/fonts/raw/main/ofl/loversquarrel/LoversQuarrel-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LoversQuarrel-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
