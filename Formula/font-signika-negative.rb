6a34aafeaa229e90ef4372ba2e62e813cdbbc628
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-signika-negative updated

--- Formula/font-signika-negative.rb
+++ Formula/font-signika-negative.rb
@@ -1,12 +1,14 @@
 class FontSignikaNegative < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/signikanegative/SignikaNegative%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Signika Negative"
+  desc "Signika negative font"
   homepage "https://fonts.google.com/specimen/Signika+Negative"
+  head "https://github.com/google/fonts/raw/main/ofl/signikanegative/SignikaNegative%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/SignikaNegative\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/SignikaNegative[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
