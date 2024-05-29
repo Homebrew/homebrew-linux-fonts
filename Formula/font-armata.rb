c05d3e70da3c7c63ec423a245ad4dc762f80584e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-armata updated

--- Formula/font-armata.rb
+++ Formula/font-armata.rb
@@ -1,12 +1,14 @@
 class FontArmata < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/armata/Armata-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Armata"
+  desc "Armata font"
   homepage "https://fonts.google.com/specimen/Armata"
+  head "https://github.com/google/fonts/raw/main/ofl/armata/Armata-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Armata-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
