ba850e38e77af1492b7429a8f02deb346d5a1ee0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:04 +0000

    font-overlock-sc updated

--- Formula/font-overlock-sc.rb
+++ Formula/font-overlock-sc.rb
@@ -1,12 +1,14 @@
 class FontOverlockSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/overlocksc/OverlockSC-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Overlock SC"
+  desc "Overlock sc font"
   homepage "https://fonts.google.com/specimen/Overlock+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/overlocksc/OverlockSC-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/OverlockSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
