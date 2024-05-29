7a2bdfbe20eb13432fed1d746b5192d02447f503
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-jim-nightshade updated

--- Formula/font-jim-nightshade.rb
+++ Formula/font-jim-nightshade.rb
@@ -1,12 +1,14 @@
 class FontJimNightshade < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jimnightshade/JimNightshade-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jim Nightshade"
+  desc "Jim nightshade font"
   homepage "https://fonts.google.com/specimen/Jim+Nightshade"
+  head "https://github.com/google/fonts/raw/main/ofl/jimnightshade/JimNightshade-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JimNightshade-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
