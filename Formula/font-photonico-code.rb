48a24a547ac81eaaa6f6d0f377817bbb8ae0d465
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-photonico-code updated

--- Formula/font-photonico-code.rb
+++ Formula/font-photonico-code.rb
@@ -1,15 +1,15 @@
 class FontPhotonicoCode < Formula
-  version "1.4"
-  sha256 "a6919ca4e80089843027c0e17bee2ab1f7a8f022ad651425cabe8c07ef8e53a2"
-  url "https://github.com/Photonico/Photonico_Code/releases/download/#{version}/Photonico.#{version}.Regular.ttf"
-  desc "Photonico Code"
   desc "Free, monospace, slab-serif style font for coding"
   homepage "https://github.com/Photonico/Photonico_Code"
+  url "https://github.com/Photonico/Photonico_Code/releases/download/1.4/Photonico.1.4.Regular.ttf"
+  version "1.4"
+  sha256 "a6919ca4e80089843027c0e17bee2ab1f7a8f022ad651425cabe8c07ef8e53a2"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Photonico.#{version}.Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Photonico.1.4.Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
