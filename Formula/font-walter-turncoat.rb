7faf8e264d3b30f63d029659d7a06d42730a6d48
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-walter-turncoat updated

--- Formula/font-walter-turncoat.rb
+++ Formula/font-walter-turncoat.rb
@@ -1,12 +1,14 @@
 class FontWalterTurncoat < Formula
-  head "https://github.com/google/fonts/raw/main/apache/walterturncoat/WalterTurncoat-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Walter Turncoat"
+  desc "Walter turncoat font"
   homepage "https://fonts.google.com/specimen/Walter+Turncoat"
+  head "https://github.com/google/fonts/raw/main/apache/walterturncoat/WalterTurncoat-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/WalterTurncoat-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
