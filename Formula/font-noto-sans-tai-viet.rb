ea996acf9ec2956bad9d2eb9ad0cff9ead144f90
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-tai-viet updated

--- Formula/font-noto-sans-tai-viet.rb
+++ Formula/font-noto-sans-tai-viet.rb
@@ -1,12 +1,14 @@
 class FontNotoSansTaiViet < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiViet-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Tai Viet"
+  desc "Noto sans tai viet font"
   homepage "https://www.google.com/get/noto/#sans-tavt"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiViet-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansTaiViet-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
