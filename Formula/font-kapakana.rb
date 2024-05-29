7274959a606d3a64bbaff6b945aecf5a1e8996a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:27 +0000

    font-kapakana updated

--- Formula/font-kapakana.rb
+++ Formula/font-kapakana.rb
@@ -1,13 +1,14 @@
 class FontKapakana < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kapakana/Kapakana%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Kapakana"
   desc "Two weight font and available both as static instances and as a variable font"
   homepage "https://fonts.google.com/specimen/Kapakana"
+  head "https://github.com/google/fonts/raw/main/ofl/kapakana/Kapakana%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Kapakana\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Kapakana[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
