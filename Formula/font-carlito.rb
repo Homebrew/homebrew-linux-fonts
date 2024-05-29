cd36e75a9b1e71f0bf15476cd34eb11867c74982
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:52 +0000

    font-carlito updated

--- Formula/font-carlito.rb
+++ Formula/font-carlito.rb
@@ -1,16 +1,19 @@
 class FontCarlito < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/carlito"
-  desc "Carlito"
   desc "Metric-compatible with calibri"
   homepage "https://fonts.google.com/specimen/Carlito"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/carlito"
+
   def install
     (share/"fonts").install Dir.glob("ofl/carlito/./**/Carlito-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/carlito/./**/Carlito-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/carlito/./**/Carlito-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/carlito/./**/Carlito-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
