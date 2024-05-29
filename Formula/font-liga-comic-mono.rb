1bfbb63cb6394293c98b419c3abfd7f7cf80ceed
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:33 +0000

    font-liga-comic-mono updated

--- Formula/font-liga-comic-mono.rb
+++ Formula/font-liga-comic-mono.rb
@@ -1,14 +1,14 @@
 class FontLigaComicMono < Formula
-  head "https://codeload.github.com/wayou/comic-mono-font/zip/master"
-  desc "Liga Comic Mono"
   desc "Legible monospace font with programming ligatures"
   homepage "https://github.com/wayou/comic-mono-font"
+  head "https://codeload.github.com/wayou/comic-mono-font/zip/master"
+
   def install
-    (share/"fonts").install Dir.glob("comic-mono-font-master/**/LigaComicMono.ttf")[0]
-    (share/"fonts").install Dir.glob("comic-mono-font-master/**/LigaComicMono-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/comic-mono-font-master/LigaComicMono.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/comic-mono-font-master/LigaComicMono-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
