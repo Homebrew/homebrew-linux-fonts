72a89512577a00db769a7f4f965d0eb64a6b2304
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-urw-base35 updated

--- Formula/font-urw-base35.rb
+++ Formula/font-urw-base35.rb
@@ -1,50 +1,49 @@
 class FontUrwBase35 < Formula
-  # NOTE: "35" is not a version number, but an intrinsic part of the product name
-
+  desc "Urw++ base 35 font"
+  homepage "https://github.com/ArtifexSoftware/urw-base35-fonts"
+  url "https://github.com/ArtifexSoftware/urw-base35-fonts/archive/20200910.zip"
   version "20200910"
   sha256 "66eed7ca2dfbf44665aa34cb80559f4a90807d46858ccf76c34f9ac1701cfa27"
-  url "https://github.com/ArtifexSoftware/urw-base35-fonts/archive/#{version}.zip"
-  desc "URW++ base 35"
-  homepage "https://github.com/ArtifexSoftware/urw-base35-fonts"
+
   def install
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/C059-BdIta.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/C059-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/C059-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/C059-Roman.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/D050000L.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusMonoPS-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusMonoPS-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusMonoPS-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusMonoPS-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusRoman-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusRoman-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusRoman-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusRoman-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusSans-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusSans-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusSans-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusSans-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusSansNarrow-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusSansNarrow-BoldOblique.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusSansNarrow-Oblique.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/NimbusSansNarrow-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/P052-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/P052-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/P052-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/P052-Roman.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/StandardSymbolsPS.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/URWBookman-Demi.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/URWBookman-DemiItalic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/URWBookman-Light.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/URWBookman-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/URWGothic-Book.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/URWGothic-BookOblique.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/URWGothic-Demi.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/URWGothic-DemiOblique.otf")[0]
-    (share/"fonts").install Dir.glob("urw-base35-fonts-#{version}/fonts/**/Z003-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/Z003-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/C059-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/C059-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/C059-Roman.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/D050000L.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusMonoPS-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusMonoPS-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusMonoPS-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusMonoPS-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusRoman-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusRoman-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusRoman-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusRoman-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusSans-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusSans-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusSans-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusSans-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusSansNarrow-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusSansNarrow-BoldOblique.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusSansNarrow-Oblique.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/NimbusSansNarrow-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/P052-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/P052-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/P052-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/P052-Roman.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/StandardSymbolsPS.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/URWBookman-Demi.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/URWBookman-DemiItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/URWBookman-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/URWBookman-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/URWGothic-Book.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/URWGothic-BookOblique.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/URWGothic-Demi.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/URWGothic-DemiOblique.otf")[0]
+    (share/"fonts").install Dir.glob("./**/urw-base35-fonts-20200910/fonts/C059-BdIta.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
