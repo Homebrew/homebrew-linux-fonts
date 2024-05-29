242808501423bda6bed46008d079d3a9e37abdc1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-days-one updated

--- Formula/font-days-one.rb
+++ Formula/font-days-one.rb
@@ -1,12 +1,14 @@
 class FontDaysOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/daysone/DaysOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Days One"
+  desc "Days one font"
   homepage "https://fonts.google.com/specimen/Days+One"
+  head "https://github.com/google/fonts/raw/main/ofl/daysone/DaysOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DaysOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
