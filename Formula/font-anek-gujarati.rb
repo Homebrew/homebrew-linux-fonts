91c8ec4b4fc17782ae4d090fa787950dcb4fa1fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-anek-gujarati updated

--- Formula/font-anek-gujarati.rb
+++ Formula/font-anek-gujarati.rb
@@ -1,12 +1,14 @@
 class FontAnekGujarati < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anekgujarati/AnekGujarati%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Anek Gujarati"
+  desc "Anek gujarati font"
   homepage "https://fonts.google.com/specimen/Anek+Gujarati"
+  head "https://github.com/google/fonts/raw/main/ofl/anekgujarati/AnekGujarati%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/AnekGujarati\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekGujarati[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
