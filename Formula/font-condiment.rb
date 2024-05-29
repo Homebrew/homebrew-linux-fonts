1c2fa3c948cb3b4cd50176c7200c9bde0900957e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-condiment updated

--- Formula/font-condiment.rb
+++ Formula/font-condiment.rb
@@ -1,12 +1,14 @@
 class FontCondiment < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/condiment/Condiment-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Condiment"
+  desc "Condiment font"
   homepage "https://fonts.google.com/specimen/Condiment"
+  head "https://github.com/google/fonts/raw/main/ofl/condiment/Condiment-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Condiment-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
