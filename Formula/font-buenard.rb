ed97203362d10cc107d484ad72092f06deea3cdb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:50 +0000

    font-buenard updated

--- Formula/font-buenard.rb
+++ Formula/font-buenard.rb
@@ -1,13 +1,17 @@
 class FontBuenard < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/buenard"
-  desc "Buenard"
+  desc "Buenard font"
   homepage "https://fonts.google.com/specimen/Buenard"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/buenard"
+
   def install
     (share/"fonts").install Dir.glob("ofl/buenard/./**/Buenard-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/buenard/./**/Buenard-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
