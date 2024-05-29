42e9c72a9b580c58907d88ac884c03349f0a6c92
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:27 +0000

    font-kantumruy-pro updated

--- Formula/font-kantumruy-pro.rb
+++ Formula/font-kantumruy-pro.rb
@@ -1,14 +1,17 @@
 class FontKantumruyPro < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kantumruypro"
-  desc "Kantumruy Pro"
   desc "From work sans, with modified width and weight"
   homepage "https://fonts.google.com/specimen/Kantumruy+Pro"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kantumruypro"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/kantumruypro/./**/KantumruyPro-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/kantumruypro/./**/KantumruyPro\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kantumruypro/./**/KantumruyPro-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kantumruypro/./**/KantumruyPro[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
