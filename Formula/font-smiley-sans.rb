9639d82a446a71752f73965dd66e20719822e39d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-smiley-sans updated

--- Formula/font-smiley-sans.rb
+++ Formula/font-smiley-sans.rb
@@ -1,15 +1,16 @@
 class FontSmileySans < Formula
-  version "2.0.1"
-  sha256 "299c0be6c960ae37361762eca76f7d0cd516615435bb96c0d4b98a1e70178a07"
-  url "https://github.com/atelier-anchor/smiley-sans/releases/download/v#{version}/smiley-sans-v#{version}.zip", verified: "github.com/atelier-anchor/smiley-sans/"
-  desc "Smiley Sans"
   desc "Chinese typeface seeking a visual balance between the humanist and the geometric"
   homepage "https://atelier-anchor.com/typefaces/smiley-sans/"
+  url "https://github.com/atelier-anchor/smiley-sans/releases/download/v2.0.1/smiley-sans-v2.0.1.zip",
+       verified: "github.com/atelier-anchor/smiley-sans/"
+  version "2.0.1"
+  sha256 "299c0be6c960ae37361762eca76f7d0cd516615435bb96c0d4b98a1e70178a07"
+
   def install
     (share/"fonts").install Dir.glob("./**/SmileySans-Oblique.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
