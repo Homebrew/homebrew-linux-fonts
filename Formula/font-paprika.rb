c98c0ffe18f16b1c94ce73629ad28678f1a6b8dc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-paprika updated

--- Formula/font-paprika.rb
+++ Formula/font-paprika.rb
@@ -1,12 +1,14 @@
 class FontPaprika < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/paprika/Paprika-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Paprika"
+  desc "Paprika font"
   homepage "https://fonts.google.com/specimen/Paprika"
+  head "https://github.com/google/fonts/raw/main/ofl/paprika/Paprika-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Paprika-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
