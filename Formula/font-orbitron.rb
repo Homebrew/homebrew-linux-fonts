6b825d93adbfad1a7ad29f235224c221b28ae86b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-orbitron updated

--- Formula/font-orbitron.rb
+++ Formula/font-orbitron.rb
@@ -1,12 +1,14 @@
 class FontOrbitron < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/orbitron/Orbitron%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Orbitron"
+  desc "Orbitron font"
   homepage "https://fonts.google.com/specimen/Orbitron"
+  head "https://github.com/google/fonts/raw/main/ofl/orbitron/Orbitron%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Orbitron\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Orbitron[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
