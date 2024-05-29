6b3d16231621223dcd576516f2b8b755a29a6203
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-snippet updated

--- Formula/font-snippet.rb
+++ Formula/font-snippet.rb
@@ -1,12 +1,14 @@
 class FontSnippet < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/snippet/Snippet.ttf", verified: "github.com/google/fonts/"
-  desc "Snippet"
+  desc "Snippet font"
   homepage "https://fonts.google.com/specimen/Snippet"
+  head "https://github.com/google/fonts/raw/main/ofl/snippet/Snippet.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Snippet.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
