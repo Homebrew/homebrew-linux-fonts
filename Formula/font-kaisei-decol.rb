6e7e822463d39ab1c25d749d5487e724837e4d02
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-kaisei-decol updated

--- Formula/font-kaisei-decol.rb
+++ Formula/font-kaisei-decol.rb
@@ -1,15 +1,18 @@
 class FontKaiseiDecol < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kaiseidecol"
-  desc "Kaisei Decol"
   desc "Designed with the same element in kanji, the little dot at the end of the stroke"
   homepage "https://fonts.google.com/specimen/Kaisei+Decol"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kaiseidecol"
+
   def install
     (share/"fonts").install Dir.glob("ofl/kaiseidecol/./**/KaiseiDecol-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kaiseidecol/./**/KaiseiDecol-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kaiseidecol/./**/KaiseiDecol-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
