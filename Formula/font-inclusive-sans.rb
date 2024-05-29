a0b1c093daf252a4fb3525ba21a8c3224dc33fe5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:20 +0000

    font-inclusive-sans updated

--- Formula/font-inclusive-sans.rb
+++ Formula/font-inclusive-sans.rb
@@ -1,14 +1,17 @@
 class FontInclusiveSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/inclusivesans"
-  desc "Inclusive Sans"
   desc "Contemporary neo-grotesques"
   homepage "https://fonts.google.com/specimen/Inclusive+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/inclusivesans"
+
   def install
     (share/"fonts").install Dir.glob("ofl/inclusivesans/./**/InclusiveSans-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/inclusivesans/./**/InclusiveSans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
