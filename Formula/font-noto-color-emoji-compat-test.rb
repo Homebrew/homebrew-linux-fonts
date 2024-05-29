50d5ca1422fb292cd1f761e3a29d164c3e999d21
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-color-emoji-compat-test updated

--- Formula/font-noto-color-emoji-compat-test.rb
+++ Formula/font-noto-color-emoji-compat-test.rb
@@ -1,12 +1,14 @@
 class FontNotoColorEmojiCompatTest < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notocoloremojicompattest/NotoColorEmojiCompatTest-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Color Emoji Compat Test"
+  desc "Noto color emoji compat test font"
   homepage "https://fonts.google.com/specimen/Noto+Color+Emoji+Compat+Test"
+  head "https://github.com/google/fonts/raw/main/ofl/notocoloremojicompattest/NotoColorEmojiCompatTest-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoColorEmojiCompatTest-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
