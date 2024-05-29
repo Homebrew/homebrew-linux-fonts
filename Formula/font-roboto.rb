f0d66b4adde56575b0b5a0d7b31bf69cdb08c553
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:15 +0000

    font-roboto updated

--- Formula/font-roboto.rb
+++ Formula/font-roboto.rb
@@ -1,14 +1,17 @@
 class FontRoboto < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/roboto"
-  desc "Roboto"
   desc "Font with a mechanical skeleton and the forms are largely geometric"
   homepage "https://fonts.google.com/specimen/Roboto"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/roboto"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/roboto/./**/Roboto-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/roboto/./**/Roboto\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/roboto/./**/Roboto-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/roboto/./**/Roboto[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
