87a115fb3bef92f8baacaf9156bd3b9836390783
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-amiri updated

--- Formula/font-amiri.rb
+++ Formula/font-amiri.rb
@@ -1,21 +1,23 @@
 class FontAmiri < Formula
-  version "1.000"
-  sha256 "926fe1bd7dfde8e55178281f645258bfced6420c951c6f2fd532fd21691bca30"
-  url "https://github.com/alif-type/amiri/releases/download/#{version}/Amiri-#{version}.zip", verified: "github.com/alif-type/amiri/"
-  desc "Amiri"
   desc "Classical Arabic typeface in Naskh style"
   homepage "https://www.amirifont.org/"
-  deprecate! date: "2023-12-17", because: :unsupported
+  url "https://github.com/alif-type/amiri/releases/download/1.000/Amiri-1.000.zip",
+       verified: "github.com/alif-type/amiri/"
+  version "1.000"
+  sha256 "926fe1bd7dfde8e55178281f645258bfced6420c951c6f2fd532fd21691bca30"
+
+  deprecate! "2023-12-17", because: :discontinued
+
   def install
-    (share/"fonts").install Dir.glob("Amiri-#{version}/**/Amiri-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("Amiri-#{version}/**/Amiri-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("Amiri-#{version}/**/Amiri-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("Amiri-#{version}/**/Amiri-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("Amiri-#{version}/**/AmiriQuran.ttf")[0]
-    (share/"fonts").install Dir.glob("Amiri-#{version}/**/AmiriQuranColored.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Amiri-1.000/Amiri-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Amiri-1.000/Amiri-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Amiri-1.000/Amiri-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Amiri-1.000/Amiri-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Amiri-1.000/AmiriQuran.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Amiri-1.000/AmiriQuranColored.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
