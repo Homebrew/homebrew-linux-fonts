f7efbdf6496a63b63ab9cfb051cbad0589f28a26
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-domine updated

--- Formula/font-domine.rb
+++ Formula/font-domine.rb
@@ -1,12 +1,14 @@
 class FontDomine < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/domine/Domine%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Domine"
+  desc "Domine font"
   homepage "https://fonts.google.com/specimen/Domine"
+  head "https://github.com/google/fonts/raw/main/ofl/domine/Domine%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Domine\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Domine[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
