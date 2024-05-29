85ecab95c229f7a91590020ef85626ea121253b6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:10 +0000

    font-potta-one updated

--- Formula/font-potta-one.rb
+++ Formula/font-potta-one.rb
@@ -1,13 +1,14 @@
 class FontPottaOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/pottaone/PottaOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Potta One"
   desc "Single style font family featuring letterforms inspired by brush lettering"
   homepage "https://fonts.google.com/specimen/Potta+One"
+  head "https://github.com/google/fonts/raw/main/ofl/pottaone/PottaOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PottaOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
