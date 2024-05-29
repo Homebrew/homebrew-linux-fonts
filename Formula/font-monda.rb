6497633d5decfef7a968ab7a99691e3aabd0a9d1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:42 +0000

    font-monda updated

--- Formula/font-monda.rb
+++ Formula/font-monda.rb
@@ -1,12 +1,14 @@
 class FontMonda < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/monda/Monda%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Monda"
+  desc "Monda font"
   homepage "https://fonts.google.com/specimen/Monda"
+  head "https://github.com/google/fonts/raw/main/ofl/monda/Monda%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Monda\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Monda[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
