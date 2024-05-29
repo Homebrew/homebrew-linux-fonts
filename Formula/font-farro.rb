27752feb4a6cff57c98fdb2d6adfcc68a472f562
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:05 +0000

    font-farro updated

--- Formula/font-farro.rb
+++ Formula/font-farro.rb
@@ -1,15 +1,19 @@
 class FontFarro < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/farro"
-  desc "Farro"
+  desc "Farro font"
   homepage "https://fonts.google.com/specimen/Farro"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/farro"
+
   def install
     (share/"fonts").install Dir.glob("ofl/farro/./**/Farro-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/farro/./**/Farro-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/farro/./**/Farro-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/farro/./**/Farro-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
