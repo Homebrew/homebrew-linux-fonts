3bff9231896a3689726df07e4f6750abaac4443d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-imprima updated

--- Formula/font-imprima.rb
+++ Formula/font-imprima.rb
@@ -1,12 +1,14 @@
 class FontImprima < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/imprima/Imprima-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Imprima"
+  desc "Imprima font"
   homepage "https://fonts.google.com/specimen/Imprima"
+  head "https://github.com/google/fonts/raw/main/ofl/imprima/Imprima-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Imprima-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
