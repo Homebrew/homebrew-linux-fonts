954498e5e07038f32f8e42ee0538ac9c5f8110e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-linear-b updated

--- Formula/font-noto-sans-linear-b.rb
+++ Formula/font-noto-sans-linear-b.rb
@@ -1,12 +1,14 @@
 class FontNotoSansLinearB < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLinearB-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Linear B"
+  desc "Noto sans linear b font"
   homepage "https://www.google.com/get/noto/#sans-linb"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLinearB-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansLinearB-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
