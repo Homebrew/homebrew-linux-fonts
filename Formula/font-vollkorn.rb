a9133cb128ff5a2e4a6ea5a61c99011dc9dc6d6a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-vollkorn updated

--- Formula/font-vollkorn.rb
+++ Formula/font-vollkorn.rb
@@ -1,14 +1,17 @@
 class FontVollkorn < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/vollkorn"
-  desc "Vollkorn"
   desc "Quiet, modest and high quality serif typeface"
   homepage "https://fonts.google.com/specimen/Vollkorn"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/vollkorn"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/vollkorn/./**/Vollkorn-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/vollkorn/./**/Vollkorn\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/vollkorn/./**/Vollkorn-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/vollkorn/./**/Vollkorn[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
