79ae90c16788c275212200529136990a52f6ca57
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-cambo updated

--- Formula/font-cambo.rb
+++ Formula/font-cambo.rb
@@ -1,12 +1,14 @@
 class FontCambo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/cambo/Cambo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Cambo"
+  desc "Cambo font"
   homepage "https://fonts.google.com/specimen/Cambo"
+  head "https://github.com/google/fonts/raw/main/ofl/cambo/Cambo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Cambo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
