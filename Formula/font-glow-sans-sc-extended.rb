95af14aa441807a6c368af9caea137d553d4e017
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:12 +0000

    font-glow-sans-sc-extended updated

--- Formula/font-glow-sans-sc-extended.rb
+++ Formula/font-glow-sans-sc-extended.rb
@@ -1,12 +1,14 @@
 class FontGlowSansScExtended < Formula
+  desc "Glow sans sc extended font"
+  homepage "https://github.com/welai/glow-sans"
+  url "https://github.com/welai/glow-sans/releases/download/v0.93/GlowSansSC-Extended-v0.93.zip"
   version "0.93"
   sha256 "3bdeb1dae573c87ace447a4dbad4482dc516c5aeede4a7a8f21ca4b181456422"
-  url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansSC-Extended-v#{version}.zip"
-  desc "Glow Sans SC Extended"
-  homepage "https://github.com/welai/glow-sans"
-  deprecate! date: "2024-02-17", because: :unsupported
+
+  deprecate! "2024-02-17", because: :discontinued
+
   def install
-    (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-Thin.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-Book.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-ExtraBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-ExtraLight.otf")[0]
@@ -14,10 +16,10 @@ class FontGlowSansScExtended < Formula
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-Light.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansSC-Extended-Bold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
