e8a3be514a19939972cb0cbad1d00fa223543c48
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-kokoro updated

--- Formula/font-kokoro.rb
+++ Formula/font-kokoro.rb
@@ -1,12 +1,14 @@
 class FontKokoro < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kokoro/Kokoro-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kokoro"
+  desc "Kokoro font"
   homepage "https://fonts.google.com/specimen/Kokoro"
+  head "https://github.com/google/fonts/raw/main/ofl/kokoro/Kokoro-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Kokoro-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
