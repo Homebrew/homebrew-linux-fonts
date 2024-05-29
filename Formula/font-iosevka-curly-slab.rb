920f058be50aef86522d1e1c36fae9405ac4b839
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:21 +0000

    font-iosevka-curly-slab updated

--- Formula/font-iosevka-curly-slab.rb
+++ Formula/font-iosevka-curly-slab.rb
@@ -1,12 +1,12 @@
 class FontIosevkaCurlySlab < Formula
-  version "30.1.1"
-  sha256 "8b256495fb786a155d13a0c800dcb0bffcd776c077b4274721913779547552d1"
-  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaCurlySlab-#{version}.zip"
-  desc "Iosevka Curly Slab"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
+  url "https://github.com/be5invis/Iosevka/releases/download/v30.1.1/PkgTTC-IosevkaCurlySlab-30.1.1.zip"
+  version "30.1.1"
+  sha256 "8b256495fb786a155d13a0c800dcb0bffcd776c077b4274721913779547552d1"
+
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-Thin.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-ExtraBold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-ExtraLight.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-Heavy.ttc")[0]
@@ -14,10 +14,10 @@ class FontIosevkaCurlySlab < Formula
     (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-Medium.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-Regular.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab-Bold.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
