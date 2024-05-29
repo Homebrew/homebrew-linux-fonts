fadbfde3234053a4028cad49a0dc24de24ec88d7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:09 +0000

    font-podkova-vf-beta updated

--- Formula/font-podkova-vf-beta.rb
+++ Formula/font-podkova-vf-beta.rb
@@ -1,12 +1,14 @@
 class FontPodkovaVfBeta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/podkovavfbeta/PodkovaVFBeta.ttf", verified: "github.com/google/fonts/"
-  desc "Podkova VF Beta"
+  desc "Podkova vf beta font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts/raw/main/ofl/podkovavfbeta/PodkovaVFBeta.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PodkovaVFBeta.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
