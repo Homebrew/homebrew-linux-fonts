cd31892e27b23de3e724d0ee24bbea417df8630a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:57 +0000

    font-courier-prime-medium-and-semi-bold updated

--- Formula/font-courier-prime-medium-and-semi-bold.rb
+++ Formula/font-courier-prime-medium-and-semi-bold.rb
@@ -1,13 +1,14 @@
 class FontCourierPrimeMediumAndSemiBold < Formula
-  head "https://quoteunquoteapps.com/courierprime/downloads/courier-prime-medium-semi-bold.zip"
-  desc "Courier Prime Medium & Semi-Bold"
+  desc "Courier prime medium & semi-bold font"
   homepage "https://quoteunquoteapps.com/courierprime/#cyrillic-semi-bold"
+  head "https://quoteunquoteapps.com/courierprime/downloads/courier-prime-medium-semi-bold.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/CourierPrime-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/CourierPrime-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
