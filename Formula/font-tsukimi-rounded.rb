8555a07586e4c9f8c71c993fb24ba774f9454db4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:32 +0000

    font-tsukimi-rounded updated

--- Formula/font-tsukimi-rounded.rb
+++ Formula/font-tsukimi-rounded.rb
@@ -1,8 +1,11 @@
 class FontTsukimiRounded < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tsukimirounded"
-  desc "Tsukimi Rounded"
   desc "Sans-serif typeface with rounded terminals"
   homepage "https://fonts.google.com/specimen/Tsukimi+Rounded"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tsukimirounded"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tsukimirounded/./**/TsukimiRounded-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tsukimirounded/./**/TsukimiRounded-Light.ttf")[0]
@@ -10,8 +13,8 @@ class FontTsukimiRounded < Formula
     (share/"fonts").install Dir.glob("ofl/tsukimirounded/./**/TsukimiRounded-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tsukimirounded/./**/TsukimiRounded-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
