47d1257240eba5449d836cf3d33d9dac2e539af6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:33 +0000

    font-libre-caslon-text updated

--- Formula/font-libre-caslon-text.rb
+++ Formula/font-libre-caslon-text.rb
@@ -1,14 +1,15 @@
 class FontLibreCaslonText < Formula
-  head "https://github.com/impallari/Libre-Caslon-Text/archive/master.zip"
-  desc "Libre Caslon Text"
+  desc "Libre caslon text font"
   homepage "https://github.com/impallari/Libre-Caslon-Text/"
+  head "https://github.com/impallari/Libre-Caslon-Text/archive/master.zip"
+
   def install
-    (share/"fonts").install Dir.glob("Libre-Caslon-Text-master/fonts/OTF/**/LibreCaslonText-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("Libre-Caslon-Text-master/fonts/OTF/**/LibreCaslonText-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("Libre-Caslon-Text-master/fonts/OTF/**/LibreCaslonText-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
