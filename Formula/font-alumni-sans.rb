57908c7d45f02df724e72f0cc42a7f60e14fd344
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-alumni-sans updated

--- Formula/font-alumni-sans.rb
+++ Formula/font-alumni-sans.rb
@@ -1,13 +1,17 @@
 class FontAlumniSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/alumnisans"
-  desc "Alumni Sans"
+  desc "Alumni sans font"
   homepage "https://fonts.google.com/specimen/Alumni+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/alumnisans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/alumnisans/./**/AlumniSans-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/alumnisans/./**/AlumniSans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/alumnisans/./**/AlumniSans-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/alumnisans/./**/AlumniSans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
