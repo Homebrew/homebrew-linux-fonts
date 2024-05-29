51daf7fa36e9cb76e93650e2ac585519e0684eef
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-amatic-sc updated

--- Formula/font-amatic-sc.rb
+++ Formula/font-amatic-sc.rb
@@ -1,13 +1,17 @@
 class FontAmaticSc < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/amaticsc"
-  desc "Amatic SC"
+  desc "Amatic sc font"
   homepage "https://fonts.google.com/specimen/Amatic+SC"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/amaticsc"
+
   def install
     (share/"fonts").install Dir.glob("ofl/amaticsc/./**/AmaticSC-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/amaticsc/./**/AmaticSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
