19d06ef6524733f7cbb71ccbc590926c10743c8f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-masaram-gondi updated

--- Formula/font-noto-sans-masaram-gondi.rb
+++ Formula/font-noto-sans-masaram-gondi.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMasaramGondi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmasaramgondi/NotoSansMasaramGondi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Masaram Gondi"
+  desc "Noto sans masaram gondi font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Masaram+Gondi"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmasaramgondi/NotoSansMasaramGondi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMasaramGondi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
