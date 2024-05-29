7af643bb77cfc701f672f7a41dad0b6a647347d6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:42 +0000

    font-monofett updated

--- Formula/font-monofett.rb
+++ Formula/font-monofett.rb
@@ -1,12 +1,14 @@
 class FontMonofett < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/monofett/Monofett-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Monofett"
+  desc "Monofett font"
   homepage "https://fonts.google.com/specimen/Monofett"
+  head "https://github.com/google/fonts/raw/main/ofl/monofett/Monofett-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Monofett-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
