d1c0c67cb3296fb497f398b9b02461d74262ca12
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-pavanam updated

--- Formula/font-pavanam.rb
+++ Formula/font-pavanam.rb
@@ -1,12 +1,14 @@
 class FontPavanam < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/pavanam/Pavanam-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Pavanam"
+  desc "Pavanam font"
   homepage "https://fonts.google.com/specimen/Pavanam"
+  head "https://github.com/google/fonts/raw/main/ofl/pavanam/Pavanam-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Pavanam-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
