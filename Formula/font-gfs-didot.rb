4d85ccf48c869802bb7238ce0d3357682f5c07e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-gfs-didot updated

--- Formula/font-gfs-didot.rb
+++ Formula/font-gfs-didot.rb
@@ -1,12 +1,14 @@
 class FontGfsDidot < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gfsdidot/GFSDidot-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "GFS Didot"
+  desc "Gfs didot font"
   homepage "https://fonts.google.com/specimen/GFS+Didot"
+  head "https://github.com/google/fonts/raw/main/ofl/gfsdidot/GFSDidot-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GFSDidot-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
