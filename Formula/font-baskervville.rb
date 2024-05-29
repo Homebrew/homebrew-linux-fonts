43f092a89fe36d854f984a9feaff84f3b0f78986
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:45 +0000

    font-baskervville updated

--- Formula/font-baskervville.rb
+++ Formula/font-baskervville.rb
@@ -1,13 +1,17 @@
 class FontBaskervville < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/baskervville"
-  desc "Baskervville"
+  desc "Baskervville font"
   homepage "https://fonts.google.com/specimen/Baskervville"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/baskervville"
+
   def install
     (share/"fonts").install Dir.glob("ofl/baskervville/./**/Baskervville-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/baskervville/./**/Baskervville-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
