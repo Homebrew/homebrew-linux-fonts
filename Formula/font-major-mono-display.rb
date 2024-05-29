6ba3be30766495865912c40fcd6eafddd4d8cf43
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:36 +0000

    font-major-mono-display updated

--- Formula/font-major-mono-display.rb
+++ Formula/font-major-mono-display.rb
@@ -1,12 +1,14 @@
 class FontMajorMonoDisplay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/majormonodisplay/MajorMonoDisplay-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Major Mono Display"
+  desc "Major mono display font"
   homepage "https://fonts.google.com/specimen/Major+Mono+Display"
+  head "https://github.com/google/fonts/raw/main/ofl/majormonodisplay/MajorMonoDisplay-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MajorMonoDisplay-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
