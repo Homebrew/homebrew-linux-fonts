7682f2d93197877dbe5bc6145a13a4f7b7598599
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:18 +0000

    font-ruda updated

--- Formula/font-ruda.rb
+++ Formula/font-ruda.rb
@@ -1,12 +1,14 @@
 class FontRuda < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ruda/Ruda%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Ruda"
+  desc "Ruda font"
   homepage "https://fonts.google.com/specimen/Ruda"
+  head "https://github.com/google/fonts/raw/main/ofl/ruda/Ruda%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Ruda\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Ruda[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
