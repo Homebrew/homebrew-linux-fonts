0b4925d29ed1f41e588aa332df029e4634f9014a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-mingzat updated

--- Formula/font-mingzat.rb
+++ Formula/font-mingzat.rb
@@ -1,13 +1,14 @@
 class FontMingzat < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mingzat/Mingzat-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mingzat"
   desc "Unicode font based on jason glavy's jg lepcha custom-encoded font"
   homepage "https://fonts.google.com/specimen/Mingzat"
+  head "https://github.com/google/fonts/raw/main/ofl/mingzat/Mingzat-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Mingzat-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
