285f6db110e8c99fb8135a6a652ea4dd92c9ff65
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:40 +0000

    font-miama updated

--- Formula/font-miama.rb
+++ Formula/font-miama.rb
@@ -1,12 +1,14 @@
 class FontMiama < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/miama/Miama-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Miama"
+  desc "Miama font"
   homepage "https://fonts.google.com/specimen/Miama"
+  head "https://github.com/google/fonts/raw/main/ofl/miama/Miama-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Miama-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
