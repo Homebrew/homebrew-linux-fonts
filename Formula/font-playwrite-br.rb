68b23d40673ff6a91e7e2c1df7d0155291d3ee4b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:07 +0000

    font-playwrite-br updated

--- Formula/font-playwrite-br.rb
+++ Formula/font-playwrite-br.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteBr < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwritebr/PlaywriteBR%5Bwght%5D.ttf"
-  desc "Playwrite BR"
+  desc "Playwrite br font"
   homepage "https://github.com/TypeTogether/Playwrite/"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritebr/PlaywriteBR%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteBR\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteBR[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
