bec7ac119fa36fb9f91ec275a2c00b04097a257a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-adobe-blank updated

--- Formula/font-adobe-blank.rb
+++ Formula/font-adobe-blank.rb
@@ -1,12 +1,14 @@
 class FontAdobeBlank < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/adobeblank/AdobeBlank-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Adobe Blank"
+  desc "Adobe blank font"
   homepage "https://fonts.google.com/specimen/Adobe+Blank"
+  head "https://github.com/google/fonts/raw/main/ofl/adobeblank/AdobeBlank-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AdobeBlank-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
