c6bba773793492fa56b8fbcbc50dad25fad47311
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:10 +0000

    font-pridi updated

--- Formula/font-pridi.rb
+++ Formula/font-pridi.rb
@@ -1,7 +1,11 @@
 class FontPridi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/pridi"
-  desc "Pridi"
+  desc "Pridi font"
   homepage "https://fonts.google.com/specimen/Pridi"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/pridi"
+
   def install
     (share/"fonts").install Dir.glob("ofl/pridi/./**/Pridi-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/pridi/./**/Pridi-ExtraLight.ttf")[0]
@@ -10,8 +14,8 @@ class FontPridi < Formula
     (share/"fonts").install Dir.glob("ofl/pridi/./**/Pridi-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/pridi/./**/Pridi-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
