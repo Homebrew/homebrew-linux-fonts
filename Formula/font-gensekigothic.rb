0ef64f5bdede872a333d3cc5b4f1f6d86b4caaae
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-gensekigothic updated

--- Formula/font-gensekigothic.rb
+++ Formula/font-gensekigothic.rb
@@ -1,9 +1,10 @@
 class FontGensekigothic < Formula
+  desc "Gensekigothic font"
+  homepage "https://github.com/ButTaiwan/genseki-font"
+  url "https://github.com/ButTaiwan/genseki-font/releases/download/v1.501/GenSekiGothic.zip"
   version "1.501"
   sha256 "d4baad35c36428fe355648615c30bfb5f74bc1d4eca12e88af664a83fb7c7d6a"
-  url "https://github.com/ButTaiwan/genseki-font/releases/download/v#{version}/GenSekiGothic.zip"
-  desc "GenSekiGothic"
-  homepage "https://github.com/ButTaiwan/genseki-font"
+
   def install
     (share/"fonts").install Dir.glob("./**/GenSekiGothic-B.ttc")[0]
     (share/"fonts").install Dir.glob("./**/GenSekiGothic-H.ttc")[0]
@@ -11,8 +12,8 @@ class FontGensekigothic < Formula
     (share/"fonts").install Dir.glob("./**/GenSekiGothic-M.ttc")[0]
     (share/"fonts").install Dir.glob("./**/GenSekiGothic-R.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
