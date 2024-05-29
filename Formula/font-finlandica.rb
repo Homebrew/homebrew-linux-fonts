60f44592809097b587bcf8fa4fbc656d4cef6cbf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:06 +0000

    font-finlandica updated

--- Formula/font-finlandica.rb
+++ Formula/font-finlandica.rb
@@ -1,13 +1,17 @@
 class FontFinlandica < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/finlandica"
-  desc "Finlandica"
+  desc "Finlandica font"
   homepage "https://fonts.google.com/specimen/Finlandica"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/finlandica"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/finlandica/./**/Finlandica-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/finlandica/./**/Finlandica\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/finlandica/./**/Finlandica-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/finlandica/./**/Finlandica[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
