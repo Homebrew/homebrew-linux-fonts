cb4b7dd6228f2532286d0d18a3643f2f27d0cc23
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-comforter updated

--- Formula/font-comforter.rb
+++ Formula/font-comforter.rb
@@ -1,13 +1,14 @@
 class FontComforter < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/comforter/Comforter-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Comforter"
   desc "Appealing for many usages"
   homepage "https://fonts.google.com/specimen/Comforter"
+  head "https://github.com/google/fonts/raw/main/ofl/comforter/Comforter-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Comforter-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
