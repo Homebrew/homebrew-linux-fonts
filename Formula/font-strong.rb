e548e4fcf3f6dbc243a9cd71d4569b020b0d7f6d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-strong updated

--- Formula/font-strong.rb
+++ Formula/font-strong.rb
@@ -1,12 +1,14 @@
 class FontStrong < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/strong/Strong-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Strong"
+  desc "Strong font"
   homepage "https://fonts.google.com/specimen/Strong"
+  head "https://github.com/google/fonts/raw/main/ofl/strong/Strong-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Strong-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
