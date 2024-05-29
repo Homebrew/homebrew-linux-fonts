65e98ec88b435b8c9bb3bbf52ece196035ccf0e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-ravi-prakash updated

--- Formula/font-ravi-prakash.rb
+++ Formula/font-ravi-prakash.rb
@@ -1,12 +1,14 @@
 class FontRaviPrakash < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/raviprakash/RaviPrakash-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ravi Prakash"
+  desc "Ravi prakash font"
   homepage "https://fonts.google.com/specimen/Ravi+Prakash"
+  head "https://github.com/google/fonts/raw/main/ofl/raviprakash/RaviPrakash-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RaviPrakash-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
