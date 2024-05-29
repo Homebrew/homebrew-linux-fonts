3b8e19e9de37680d11d10c9b21c99b3aaf7cf1f2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-habibi updated

--- Formula/font-habibi.rb
+++ Formula/font-habibi.rb
@@ -1,12 +1,14 @@
 class FontHabibi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/habibi/Habibi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Habibi"
+  desc "Habibi font"
   homepage "https://fonts.google.com/specimen/Habibi"
+  head "https://github.com/google/fonts/raw/main/ofl/habibi/Habibi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Habibi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
