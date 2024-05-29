0f4d6b7179c3e074cc123a47e6b598cc6cdac632
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-ysabeau-office updated

--- Formula/font-ysabeau-office.rb
+++ Formula/font-ysabeau-office.rb
@@ -1,14 +1,17 @@
 class FontYsabeauOffice < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ysabeauoffice"
-  desc "Ysabeau Office"
   desc "Exercise in restraint"
   homepage "https://fonts.google.com/specimen/Ysabeau+Office"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ysabeauoffice"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/ysabeauoffice/./**/YsabeauOffice-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ysabeauoffice/./**/YsabeauOffice\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ysabeauoffice/./**/YsabeauOffice-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ysabeauoffice/./**/YsabeauOffice[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
