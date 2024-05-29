240f27d014f6a71650d9968952ee3a3a067fcf87
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-allerta-stencil updated

--- Formula/font-allerta-stencil.rb
+++ Formula/font-allerta-stencil.rb
@@ -1,12 +1,14 @@
 class FontAllertaStencil < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/allertastencil/AllertaStencil-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Allerta Stencil"
+  desc "Allerta stencil font"
   homepage "https://fonts.google.com/specimen/Allerta+Stencil"
+  head "https://github.com/google/fonts/raw/main/ofl/allertastencil/AllertaStencil-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AllertaStencil-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
