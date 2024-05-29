d9c0e43431d474a2785008e81feebee8831a73fd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:04 +0000

    font-oxanium updated

--- Formula/font-oxanium.rb
+++ Formula/font-oxanium.rb
@@ -1,12 +1,14 @@
 class FontOxanium < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/oxanium/Oxanium%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Oxanium"
+  desc "Oxanium font"
   homepage "https://fonts.google.com/specimen/Oxanium"
+  head "https://github.com/google/fonts/raw/main/ofl/oxanium/Oxanium%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Oxanium\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Oxanium[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
