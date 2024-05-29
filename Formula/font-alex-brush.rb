1f7c4a49e8394d376b5e33f145e463ab127f11af
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-alex-brush updated

--- Formula/font-alex-brush.rb
+++ Formula/font-alex-brush.rb
@@ -1,12 +1,14 @@
 class FontAlexBrush < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alexbrush/AlexBrush-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Alex Brush"
+  desc "Alex brush font"
   homepage "https://fonts.google.com/specimen/Alex+Brush"
+  head "https://github.com/google/fonts/raw/main/ofl/alexbrush/AlexBrush-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AlexBrush-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
