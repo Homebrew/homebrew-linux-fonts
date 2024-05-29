ee449c993d1585afff2b73ffcc5490d0bbae21bd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-ranga updated

--- Formula/font-ranga.rb
+++ Formula/font-ranga.rb
@@ -1,13 +1,17 @@
 class FontRanga < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ranga"
-  desc "Ranga"
+  desc "Ranga font"
   homepage "https://fonts.google.com/specimen/Ranga"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ranga"
+
   def install
     (share/"fonts").install Dir.glob("ofl/ranga/./**/Ranga-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ranga/./**/Ranga-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
