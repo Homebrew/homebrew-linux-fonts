5e086cb840d7b0eca6c52c1e53809e14616a4be0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-cambay updated

--- Formula/font-cambay.rb
+++ Formula/font-cambay.rb
@@ -1,15 +1,19 @@
 class FontCambay < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cambay"
-  desc "Cambay"
+  desc "Cambay font"
   homepage "https://fonts.google.com/specimen/Cambay"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cambay"
+
   def install
     (share/"fonts").install Dir.glob("ofl/cambay/./**/Cambay-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cambay/./**/Cambay-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cambay/./**/Cambay-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cambay/./**/Cambay-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
