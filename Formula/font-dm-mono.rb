8bead88ba26c88cc5af2fd0b898da3e462c94401
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-dm-mono updated

--- Formula/font-dm-mono.rb
+++ Formula/font-dm-mono.rb
@@ -1,7 +1,11 @@
 class FontDmMono < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/dmmono"
-  desc "DM Mono"
+  desc "Dm mono font"
   homepage "https://fonts.google.com/specimen/DM+Mono"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/dmmono"
+
   def install
     (share/"fonts").install Dir.glob("ofl/dmmono/./**/DMMono-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/dmmono/./**/DMMono-Light.ttf")[0]
@@ -10,8 +14,8 @@ class FontDmMono < Formula
     (share/"fonts").install Dir.glob("ofl/dmmono/./**/DMMono-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/dmmono/./**/DMMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
