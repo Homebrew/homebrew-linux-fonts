491947025cc651251ddff846a7aa907f524f25e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noto-music updated

--- Formula/font-noto-music.rb
+++ Formula/font-noto-music.rb
@@ -1,12 +1,14 @@
 class FontNotoMusic < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notomusic/NotoMusic-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Music"
+  desc "Noto music font"
   homepage "https://fonts.google.com/specimen/Noto+Music"
+  head "https://github.com/google/fonts/raw/main/ofl/notomusic/NotoMusic-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoMusic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
