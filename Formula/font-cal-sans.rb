e51b715b695d16f84f768b35a958a1d204dcfbfd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-cal-sans updated

--- Formula/font-cal-sans.rb
+++ Formula/font-cal-sans.rb
@@ -1,15 +1,15 @@
 class FontCalSans < Formula
-  version "1.0.0"
-  sha256 "888fd98adfb1e6cd5606dd85881163d11c9c4007eaf01d015262893a48983b01"
-  url "https://github.com/calcom/font/releases/download/v#{version}/CalSans_Semibold_v#{version}.zip"
-  desc "Cal Sans"
   desc "Geometric sans-serif typeface"
   homepage "https://github.com/calcom/font"
+  url "https://github.com/calcom/font/releases/download/v1.0.0/CalSans_Semibold_v1.0.0.zip"
+  version "1.0.0"
+  sha256 "888fd98adfb1e6cd5606dd85881163d11c9c4007eaf01d015262893a48983b01"
+
   def install
-    (share/"fonts").install Dir.glob("fonts/otf/**/CalSans-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/otf/CalSans-SemiBold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
