afc326df3e90b792791db7d86753243cd8c45ea8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:38 +0000

    font-zen-kurenaido updated

--- Formula/font-zen-kurenaido.rb
+++ Formula/font-zen-kurenaido.rb
@@ -1,13 +1,14 @@
 class FontZenKurenaido < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/zenkurenaido/ZenKurenaido-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Zen Kurenaido"
   desc "Japanese font with a handwritten feeling"
   homepage "https://fonts.google.com/specimen/Zen+Kurenaido"
+  head "https://github.com/google/fonts/raw/main/ofl/zenkurenaido/ZenKurenaido-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ZenKurenaido-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
