eea2edd3e4dedc59c628a4db339af5d353dd8d8c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-marvel updated

--- Formula/font-marvel.rb
+++ Formula/font-marvel.rb
@@ -1,15 +1,19 @@
 class FontMarvel < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/marvel"
-  desc "Marvel"
+  desc "Marvel font"
   homepage "https://fonts.google.com/specimen/Marvel"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/marvel"
+
   def install
     (share/"fonts").install Dir.glob("ofl/marvel/./**/Marvel-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/marvel/./**/Marvel-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/marvel/./**/Marvel-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/marvel/./**/Marvel-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
