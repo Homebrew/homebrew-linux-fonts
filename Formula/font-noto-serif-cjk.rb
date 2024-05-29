17cb0042ee4c8fe016ecbdb265b4f6c59c01f961
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-cjk updated

--- Formula/font-noto-serif-cjk.rb
+++ Formula/font-noto-serif-cjk.rb
@@ -1,15 +1,15 @@
 class FontNotoSerifCjk < Formula
-  version "2.002"
-  sha256 "e78b23ea9f803d8376eee66d330a556e1133b713ff97014a75991d5723324f85"
-  url "https://github.com/notofonts/noto-cjk/releases/download/Serif#{version}/01_NotoSerifCJK.ttc.zip"
-  desc "Noto Serif CJK"
   desc "Static Super OTC"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Serif"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Serif2.002/01_NotoSerifCJK.ttc.zip"
+  version "2.002"
+  sha256 "e78b23ea9f803d8376eee66d330a556e1133b713ff97014a75991d5723324f85"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifCJK.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
