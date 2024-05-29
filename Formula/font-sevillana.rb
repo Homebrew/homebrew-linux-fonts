f80399745d6ae1e828ed03905c5bb49a44558ef9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-sevillana updated

--- Formula/font-sevillana.rb
+++ Formula/font-sevillana.rb
@@ -1,12 +1,14 @@
 class FontSevillana < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sevillana/Sevillana-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sevillana"
+  desc "Sevillana font"
   homepage "https://fonts.google.com/specimen/Sevillana"
+  head "https://github.com/google/fonts/raw/main/ofl/sevillana/Sevillana-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Sevillana-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
