8f7aebb2b0a81354d8de36ae775bf6140a128d96
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-nosifer-caps updated

--- Formula/font-nosifer-caps.rb
+++ Formula/font-nosifer-caps.rb
@@ -1,12 +1,14 @@
 class FontNosiferCaps < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nosifercaps/NosiferCaps-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Nosifer Caps"
+  desc "Nosifer caps font"
   homepage "https://fonts.google.com/specimen/Nosifer+Caps"
+  head "https://github.com/google/fonts/raw/main/ofl/nosifercaps/NosiferCaps-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NosiferCaps-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
