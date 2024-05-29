e90765a93a9d9725270286c1b9a6fab2d3c26bf4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-nerko-one updated

--- Formula/font-nerko-one.rb
+++ Formula/font-nerko-one.rb
@@ -1,13 +1,14 @@
 class FontNerkoOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nerkoone/NerkoOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Nerko One"
   desc "Chunky ‘marker’ effect font"
   homepage "https://fonts.google.com/specimen/Nerko+One"
+  head "https://github.com/google/fonts/raw/main/ofl/nerkoone/NerkoOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NerkoOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
