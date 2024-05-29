436175957b74bde4409e5e1df5fdcb31bfb6ec80
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:01 +0000

    font-noto-serif-toto updated

--- Formula/font-noto-serif-toto.rb
+++ Formula/font-noto-serif-toto.rb
@@ -1,12 +1,14 @@
 class FontNotoSerifToto < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoseriftoto/NotoSerifToto%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Toto"
+  desc "Noto serif toto font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Toto"
+  head "https://github.com/google/fonts/raw/main/ofl/notoseriftoto/NotoSerifToto%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifToto\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifToto[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
