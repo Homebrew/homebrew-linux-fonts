cd743c4885fb6b77b785bfe75042cf75c0409918
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:36 +0000

    font-yaldevi updated

--- Formula/font-yaldevi.rb
+++ Formula/font-yaldevi.rb
@@ -1,13 +1,14 @@
 class FontYaldevi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yaldevi/Yaldevi%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Yaldevi"
   desc "Designed by sol matas"
   homepage "https://fonts.google.com/specimen/Yaldevi"
+  head "https://github.com/google/fonts/raw/main/ofl/yaldevi/Yaldevi%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Yaldevi\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Yaldevi[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
