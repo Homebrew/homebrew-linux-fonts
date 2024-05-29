be24512630170b3cb0dcfb23c6ed064809acdac9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-tangsa updated

--- Formula/font-noto-sans-tangsa.rb
+++ Formula/font-noto-sans-tangsa.rb
@@ -1,13 +1,14 @@
 class FontNotoSansTangsa < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanstangsa/NotoSansTangsa%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Tangsa"
   desc "Design for the indic tangsa script"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Tangsa"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanstangsa/NotoSansTangsa%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansTangsa\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansTangsa[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
