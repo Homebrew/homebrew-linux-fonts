f1351d0d2d7769831b4e0549197a478eecf08769
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:49 +0000

    font-noto-sans-duployan updated

--- Formula/font-noto-sans-duployan.rb
+++ Formula/font-noto-sans-duployan.rb
@@ -1,13 +1,17 @@
 class FontNotoSansDuployan < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosansduployan"
-  desc "Noto Sans Duployan"
+  desc "Noto sans duployan font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Duployan"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/notosansduployan"
+
   def install
     (share/"fonts").install Dir.glob("ofl/notosansduployan/./**/NotoSansDuployan-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansduployan/./**/NotoSansDuployan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
