f5d2293270a8271a8c2b22ca1130b36263efa11d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:01 +0000

    font-doppio-one updated

--- Formula/font-doppio-one.rb
+++ Formula/font-doppio-one.rb
@@ -1,12 +1,14 @@
 class FontDoppioOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/doppioone/DoppioOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Doppio One"
+  desc "Doppio one font"
   homepage "https://fonts.google.com/specimen/Doppio+One"
+  head "https://github.com/google/fonts/raw/main/ofl/doppioone/DoppioOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DoppioOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
