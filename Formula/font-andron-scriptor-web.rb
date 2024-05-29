97c84ef5b40e6e3a9cc6c9158969986637061030
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-andron-scriptor-web updated

--- Formula/font-andron-scriptor-web.rb
+++ Formula/font-andron-scriptor-web.rb
@@ -1,14 +1,15 @@
 class FontAndronScriptorWeb < Formula
+  desc "Andron scriptor web font"
+  homepage "https://folk.uib.no/hnooh/mufi/fonts/"
+  url "https://folk.uib.no/hnooh/mufi/fonts/Andron/AND_SCR_WEB_3.0.zip"
   version "3.0"
   sha256 "19071b0d58e0d53c852d3f8e3b056205046236ccc33d8c185a9d4a0044644930"
-  url "https://folk.uib.no/hnooh/mufi/fonts/Andron/AND_SCR_WEB_#{version}.zip"
-  desc "Andron Scriptor Web"
-  homepage "https://folk.uib.no/hnooh/mufi/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("AND_SCR_WEB_#{version}/**/Andron Scriptor Web.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AND_SCR_WEB_3.0/Andron Scriptor Web.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
