ec4b4f5727b61bdb45b3daf405b81173b5ce9eff
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-hind-siliguri updated

--- Formula/font-hind-siliguri.rb
+++ Formula/font-hind-siliguri.rb
@@ -1,7 +1,11 @@
 class FontHindSiliguri < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/hindsiliguri"
-  desc "Hind Siliguri"
+  desc "Hind siliguri font"
   homepage "https://fonts.google.com/specimen/Hind+Siliguri"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/hindsiliguri"
+
   def install
     (share/"fonts").install Dir.glob("ofl/hindsiliguri/./**/HindSiliguri-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hindsiliguri/./**/HindSiliguri-Light.ttf")[0]
@@ -9,8 +13,8 @@ class FontHindSiliguri < Formula
     (share/"fonts").install Dir.glob("ofl/hindsiliguri/./**/HindSiliguri-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/hindsiliguri/./**/HindSiliguri-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
