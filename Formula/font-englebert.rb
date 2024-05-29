f9dc70e12fb2cc327c2f7532c22aa9fb22e9a0ee
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-englebert updated

--- Formula/font-englebert.rb
+++ Formula/font-englebert.rb
@@ -1,12 +1,14 @@
 class FontEnglebert < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/englebert/Englebert-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Englebert"
+  desc "Englebert font"
   homepage "https://fonts.google.com/specimen/Englebert"
+  head "https://github.com/google/fonts/raw/main/ofl/englebert/Englebert-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Englebert-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
