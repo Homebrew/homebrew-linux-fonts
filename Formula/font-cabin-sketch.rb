0546369bec01857860753d26fcc34fccbb119994
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:50 +0000

    font-cabin-sketch updated

--- Formula/font-cabin-sketch.rb
+++ Formula/font-cabin-sketch.rb
@@ -1,13 +1,17 @@
 class FontCabinSketch < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/cabinsketch"
-  desc "Cabin Sketch"
+  desc "Cabin sketch font"
   homepage "https://fonts.google.com/specimen/Cabin+Sketch"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/cabinsketch"
+
   def install
     (share/"fonts").install Dir.glob("ofl/cabinsketch/./**/CabinSketch-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/cabinsketch/./**/CabinSketch-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
