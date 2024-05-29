a66059698fd82df08addc0c01e88f89f21721cb6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-nanum-brush-script updated

--- Formula/font-nanum-brush-script.rb
+++ Formula/font-nanum-brush-script.rb
@@ -1,12 +1,14 @@
 class FontNanumBrushScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nanumbrushscript/NanumBrushScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Nanum Brush Script"
+  desc "Nanum brush script font"
   homepage "https://fonts.google.com/specimen/Nanum+Brush+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/nanumbrushscript/NanumBrushScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NanumBrushScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
