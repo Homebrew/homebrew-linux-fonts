122f1174b236d2ae45f16756595347333f91492a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:24 +0000

    font-jersey-15-charted updated

--- Formula/font-jersey-15-charted.rb
+++ Formula/font-jersey-15-charted.rb
@@ -1,12 +1,14 @@
 class FontJersey15Charted < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jersey15charted/Jersey15Charted-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jersey 15 Charted"
+  desc "Jersey 15 charted font"
   homepage "https://fonts.google.com/specimen/Jersey+15+Charted"
+  head "https://github.com/google/fonts/raw/main/ofl/jersey15charted/Jersey15Charted-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Jersey15Charted-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
