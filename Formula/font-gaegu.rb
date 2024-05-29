fc9043926b5874545ef6c2ae72d35d5779295963
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-gaegu updated

--- Formula/font-gaegu.rb
+++ Formula/font-gaegu.rb
@@ -1,14 +1,18 @@
 class FontGaegu < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gaegu"
-  desc "Gaegu"
+  desc "Gaegu font"
   homepage "https://fonts.google.com/specimen/Gaegu"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gaegu"
+
   def install
     (share/"fonts").install Dir.glob("ofl/gaegu/./**/Gaegu-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gaegu/./**/Gaegu-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gaegu/./**/Gaegu-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
