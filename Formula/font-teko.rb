417c49732f3a19d85fce53fc0ee8e93715559bc4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-teko updated

--- Formula/font-teko.rb
+++ Formula/font-teko.rb
@@ -1,12 +1,14 @@
 class FontTeko < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/teko/Teko%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Teko"
+  desc "Teko font"
   homepage "https://fonts.google.com/specimen/Teko"
+  head "https://github.com/google/fonts/raw/main/ofl/teko/Teko%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Teko\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Teko[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
