7bf723a879e1a9b722a327fb950617320b054fe0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-kristi updated

--- Formula/font-kristi.rb
+++ Formula/font-kristi.rb
@@ -1,12 +1,14 @@
 class FontKristi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kristi/Kristi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kristi"
+  desc "Kristi font"
   homepage "https://fonts.google.com/specimen/Kristi"
+  head "https://github.com/google/fonts/raw/main/ofl/kristi/Kristi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Kristi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
