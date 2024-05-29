67ed5027725e50bef977bd102efee04f76f5e284
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-londrina-outline updated

--- Formula/font-londrina-outline.rb
+++ Formula/font-londrina-outline.rb
@@ -1,12 +1,14 @@
 class FontLondrinaOutline < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/londrinaoutline/LondrinaOutline-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Londrina Outline"
+  desc "Londrina outline font"
   homepage "https://fonts.google.com/specimen/Londrina+Outline"
+  head "https://github.com/google/fonts/raw/main/ofl/londrinaoutline/LondrinaOutline-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LondrinaOutline-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
