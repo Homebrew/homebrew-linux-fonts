e7ecdf0ec22650715fb5125f6d5dc5716a40e4b7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-ebh updated

--- Formula/font-ebh.rb
+++ Formula/font-ebh.rb
@@ -1,19 +1,18 @@
 class FontEbh < Formula
-  version "1.0.0"
-  sha256 "6787471d7766a8be31f2e601620e402ff43bafd183d626a8df75767e91052af3"
-  url "https://exeterbookhand.com/EBH-v#{version}.zip"
-  desc "EBH"
-  desc "Exeter Book Hand"
   desc "Vectorized font based on script in the Exeter Book"
   homepage "https://exeterbookhand.com/"
+  url "https://exeterbookhand.com/EBH-v1.0.0.zip"
+  version "1.0.0"
+  sha256 "6787471d7766a8be31f2e601620e402ff43bafd183d626a8df75767e91052af3"
+
   def install
-    (share/"fonts").install Dir.glob("EBH-v#{version}/**/EBH Alternates.otf")[0]
-    (share/"fonts").install Dir.glob("EBH-v#{version}/**/EBH Facsimile.otf")[0]
-    (share/"fonts").install Dir.glob("EBH-v#{version}/**/EBH Initials.otf")[0]
-    (share/"fonts").install Dir.glob("EBH-v#{version}/**/EBH Runes.otf")[0]
+    (share/"fonts").install Dir.glob("./**/EBH-v1.0.0/EBH Alternates.otf")[0]
+    (share/"fonts").install Dir.glob("./**/EBH-v1.0.0/EBH Facsimile.otf")[0]
+    (share/"fonts").install Dir.glob("./**/EBH-v1.0.0/EBH Initials.otf")[0]
+    (share/"fonts").install Dir.glob("./**/EBH-v1.0.0/EBH Runes.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
