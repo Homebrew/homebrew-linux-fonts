8fde7b65120009fe7515290dbbbb7e365daf33fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-im-fell-french-canon-sc updated

--- Formula/font-im-fell-french-canon-sc.rb
+++ Formula/font-im-fell-french-canon-sc.rb
@@ -1,12 +1,14 @@
 class FontImFellFrenchCanonSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/imfellfrenchcanonsc/IMFeFCsc28P.ttf", verified: "github.com/google/fonts/"
-  desc "IM Fell French Canon SC"
+  desc "Im fell french canon sc font"
   homepage "https://fonts.google.com/specimen/IM+Fell+French+Canon+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/imfellfrenchcanonsc/IMFeFCsc28P.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/IMFeFCsc28P.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
