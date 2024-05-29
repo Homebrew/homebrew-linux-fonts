fa04cae712c51b092cefa18904f5ba9702e79ca9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:09 +0000

    font-galada updated

--- Formula/font-galada.rb
+++ Formula/font-galada.rb
@@ -1,12 +1,14 @@
 class FontGalada < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/galada/Galada-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Galada"
+  desc "Galada font"
   homepage "https://fonts.google.com/specimen/Galada"
+  head "https://github.com/google/fonts/raw/main/ofl/galada/Galada-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Galada-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
