a8d69f6b03f4cf74895db764cf02abad6df1e734
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-gfs-neohellenic updated

--- Formula/font-gfs-neohellenic.rb
+++ Formula/font-gfs-neohellenic.rb
@@ -1,15 +1,19 @@
 class FontGfsNeohellenic < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gfsneohellenic"
-  desc "GFS Neohellenic"
+  desc "Gfs neohellenic font"
   homepage "https://fonts.google.com/specimen/GFS+Neohellenic"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gfsneohellenic"
+
   def install
     (share/"fonts").install Dir.glob("ofl/gfsneohellenic/./**/GFSNeohellenic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gfsneohellenic/./**/GFSNeohellenicBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gfsneohellenic/./**/GFSNeohellenicBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gfsneohellenic/./**/GFSNeohellenicItalic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
