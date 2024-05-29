2ea1c45c62e27d01c715a76ff0a054466f3a637e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:54 +0000

    font-noto-sans-newa updated

--- Formula/font-noto-sans-newa.rb
+++ Formula/font-noto-sans-newa.rb
@@ -1,12 +1,14 @@
 class FontNotoSansNewa < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansnewa/NotoSansNewa-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Newa"
+  desc "Noto sans newa font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Newa"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansnewa/NotoSansNewa-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansNewa-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
