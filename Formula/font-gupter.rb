7f4821e679d73dd6ca46b3848a97dda7b7754f96
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-gupter updated

--- Formula/font-gupter.rb
+++ Formula/font-gupter.rb
@@ -1,14 +1,18 @@
 class FontGupter < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/gupter"
-  desc "Gupter"
+  desc "Gupter font"
   homepage "https://fonts.google.com/specimen/Gupter"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/gupter"
+
   def install
     (share/"fonts").install Dir.glob("ofl/gupter/./**/Gupter-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gupter/./**/Gupter-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gupter/./**/Gupter-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
