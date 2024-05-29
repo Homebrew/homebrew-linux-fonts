f9af004441c2cdb6927165ca84bc28930b1e60e0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-handlee updated

--- Formula/font-handlee.rb
+++ Formula/font-handlee.rb
@@ -1,12 +1,14 @@
 class FontHandlee < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/handlee/Handlee-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Handlee"
+  desc "Handlee font"
   homepage "https://fonts.google.com/specimen/Handlee"
+  head "https://github.com/google/fonts/raw/main/ofl/handlee/Handlee-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Handlee-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
