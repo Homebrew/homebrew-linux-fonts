9ea6f485279957415d1a210dd3588836e7ed50c5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-reenie-beanie updated

--- Formula/font-reenie-beanie.rb
+++ Formula/font-reenie-beanie.rb
@@ -1,12 +1,14 @@
 class FontReenieBeanie < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/reeniebeanie/ReenieBeanie.ttf", verified: "github.com/google/fonts/"
-  desc "Reenie Beanie"
+  desc "Reenie beanie font"
   homepage "https://fonts.google.com/specimen/Reenie+Beanie"
+  head "https://github.com/google/fonts/raw/main/ofl/reeniebeanie/ReenieBeanie.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ReenieBeanie.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
