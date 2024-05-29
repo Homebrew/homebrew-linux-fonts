b0c5ddd67117edba50e26a7e21b0a66558114754
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-hanken-grotesk updated

--- Formula/font-hanken-grotesk.rb
+++ Formula/font-hanken-grotesk.rb
@@ -1,14 +1,17 @@
 class FontHankenGrotesk < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/hankengrotesk"
-  desc "Hanken Grotesk"
   desc "Led by alfredo marco pradil"
   homepage "https://fonts.google.com/specimen/Hanken+Grotesk"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/hankengrotesk"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/hankengrotesk/./**/HankenGrotesk-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/hankengrotesk/./**/HankenGrotesk\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/hankengrotesk/./**/HankenGrotesk-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/hankengrotesk/./**/HankenGrotesk[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
