1f176c954e6db5372362a3e04d080dd0db61da3e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:43 +0000

    font-azeret-mono updated

--- Formula/font-azeret-mono.rb
+++ Formula/font-azeret-mono.rb
@@ -1,14 +1,17 @@
 class FontAzeretMono < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/azeretmono"
-  desc "Azeret Mono"
   desc "Designed by martin vácha and daniel quisek"
   homepage "https://fonts.google.com/specimen/Azeret+Mono"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/azeretmono"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/azeretmono/./**/AzeretMono-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/azeretmono/./**/AzeretMono\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/azeretmono/./**/AzeretMono-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/azeretmono/./**/AzeretMono[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
