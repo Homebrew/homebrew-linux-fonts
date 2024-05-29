c266012019c5b4910843acd73692c0a94f28c362
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-dirt updated

--- Formula/font-rubik-dirt.rb
+++ Formula/font-rubik-dirt.rb
@@ -1,12 +1,14 @@
 class FontRubikDirt < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikdirt/RubikDirt-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Dirt"
+  desc "Rubik dirt font"
   homepage "https://fonts.google.com/specimen/Rubik+Dirt"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikdirt/RubikDirt-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikDirt-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
