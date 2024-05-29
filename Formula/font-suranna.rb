42d1c0d31ac5c870a395a19bc399bec0e54c0c33
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-suranna updated

--- Formula/font-suranna.rb
+++ Formula/font-suranna.rb
@@ -1,12 +1,14 @@
 class FontSuranna < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/suranna/Suranna-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Suranna"
+  desc "Suranna font"
   homepage "https://fonts.google.com/specimen/Suranna"
+  head "https://github.com/google/fonts/raw/main/ofl/suranna/Suranna-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Suranna-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
