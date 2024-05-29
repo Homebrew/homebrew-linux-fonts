6b393a7ab17596360bde4e29f9d19e2ef72758c5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-doulos-sil updated

--- Formula/font-doulos-sil.rb
+++ Formula/font-doulos-sil.rb
@@ -1,15 +1,15 @@
 class FontDoulosSil < Formula
-  version "6.200"
-  sha256 "a7ad76326c126b2748297b987a634a56f7e42cd45bc3ff2c90a7909cbb164223"
-  url "https://software.sil.org/downloads/r/doulos/DoulosSIL-#{version}.zip"
-  desc "Doulos SIL"
   desc "Unicode-based font family supporting languages using Latin and Cyrillic scripts"
   homepage "https://software.sil.org/doulos/"
+  url "https://software.sil.org/downloads/r/doulos/DoulosSIL-6.200.zip"
+  version "6.200"
+  sha256 "a7ad76326c126b2748297b987a634a56f7e42cd45bc3ff2c90a7909cbb164223"
+
   def install
-    (share/"fonts").install Dir.glob("DoulosSIL-#{version}/**/DoulosSIL-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DoulosSIL-6.200/DoulosSIL-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
