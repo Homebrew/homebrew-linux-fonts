1bb10f894cfb9bf4d49f7770527629c2a7d65697
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-sulphur-point updated

--- Formula/font-sulphur-point.rb
+++ Formula/font-sulphur-point.rb
@@ -1,14 +1,18 @@
 class FontSulphurPoint < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sulphurpoint"
-  desc "Sulphur Point"
+  desc "Sulphur point font"
   homepage "https://fonts.google.com/specimen/Sulphur+Point"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sulphurpoint"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sulphurpoint/./**/SulphurPoint-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sulphurpoint/./**/SulphurPoint-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sulphurpoint/./**/SulphurPoint-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
