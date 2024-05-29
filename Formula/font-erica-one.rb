6cad910264cdaefc8d81edb443e897d14378e0fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:04 +0000

    font-erica-one updated

--- Formula/font-erica-one.rb
+++ Formula/font-erica-one.rb
@@ -1,12 +1,14 @@
 class FontEricaOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ericaone/EricaOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Erica One"
+  desc "Erica one font"
   homepage "https://fonts.google.com/specimen/Erica+One"
+  head "https://github.com/google/fonts/raw/main/ofl/ericaone/EricaOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/EricaOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
