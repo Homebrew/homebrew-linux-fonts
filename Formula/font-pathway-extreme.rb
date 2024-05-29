c0d51d323784396b50d6a110ce7cf45450d59c7c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-pathway-extreme updated

--- Formula/font-pathway-extreme.rb
+++ Formula/font-pathway-extreme.rb
@@ -1,14 +1,17 @@
 class FontPathwayExtreme < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/pathwayextreme"
-  desc "Pathway Extreme"
   desc "Very popular historic typographic style"
   homepage "https://fonts.google.com/specimen/Pathway+Extreme"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/pathwayextreme"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/pathwayextreme/./**/PathwayExtreme-Italic\\[opsz,wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/pathwayextreme/./**/PathwayExtreme\\[opsz,wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/pathwayextreme/./**/PathwayExtreme-Italic[opsz,wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/pathwayextreme/./**/PathwayExtreme[opsz,wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
