4fb816de5226d33d86780d32d932df0ffb263a89
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-istok-web updated

--- Formula/font-istok-web.rb
+++ Formula/font-istok-web.rb
@@ -1,15 +1,19 @@
 class FontIstokWeb < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/istokweb"
-  desc "Istok Web"
+  desc "Istok web font"
   homepage "https://fonts.google.com/specimen/Istok+Web"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/istokweb"
+
   def install
     (share/"fonts").install Dir.glob("ofl/istokweb/./**/IstokWeb-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/istokweb/./**/IstokWeb-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/istokweb/./**/IstokWeb-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/istokweb/./**/IstokWeb-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
