1ee6f9c61882c92825a511c46520239950ee8c71
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-smythe updated

--- Formula/font-smythe.rb
+++ Formula/font-smythe.rb
@@ -1,12 +1,14 @@
 class FontSmythe < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/smythe/Smythe-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Smythe"
+  desc "Smythe font"
   homepage "https://fonts.google.com/specimen/Smythe"
+  head "https://github.com/google/fonts/raw/main/ofl/smythe/Smythe-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Smythe-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
