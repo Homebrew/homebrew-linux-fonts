2cce698e29b1fe43653bf623929531b07aa9cd5f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-langar updated

--- Formula/font-langar.rb
+++ Formula/font-langar.rb
@@ -1,13 +1,14 @@
 class FontLangar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/langar/Langar-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Langar"
   desc "One-weight latin/gurmukhi display font based on informal, playful letterforms"
   homepage "https://fonts.google.com/specimen/Langar"
+  head "https://github.com/google/fonts/raw/main/ofl/langar/Langar-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Langar-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
