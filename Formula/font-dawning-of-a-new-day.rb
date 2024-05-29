14b0bd9acfc10bd5298e9e6403bf94a42ba32d84
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-dawning-of-a-new-day updated

--- Formula/font-dawning-of-a-new-day.rb
+++ Formula/font-dawning-of-a-new-day.rb
@@ -1,12 +1,14 @@
 class FontDawningOfANewDay < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/dawningofanewday/DawningofaNewDay.ttf", verified: "github.com/google/fonts/"
-  desc "Dawning of a New Day"
+  desc "Dawning of a new day font"
   homepage "https://fonts.google.com/specimen/Dawning+of+a+New+Day"
+  head "https://github.com/google/fonts/raw/main/ofl/dawningofanewday/DawningofaNewDay.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DawningofaNewDay.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
