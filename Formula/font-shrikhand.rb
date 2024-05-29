7fd28d37d6d05aafa83bd8780fc6f4b0f5aad2ec
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-shrikhand updated

--- Formula/font-shrikhand.rb
+++ Formula/font-shrikhand.rb
@@ -1,12 +1,14 @@
 class FontShrikhand < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/shrikhand/Shrikhand-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Shrikhand"
+  desc "Shrikhand font"
   homepage "https://fonts.google.com/specimen/Shrikhand"
+  head "https://github.com/google/fonts/raw/main/ofl/shrikhand/Shrikhand-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Shrikhand-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
