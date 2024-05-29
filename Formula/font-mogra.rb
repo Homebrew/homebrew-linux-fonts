aedd11bad72e09a74380f26937d01b67fe738e7b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:41 +0000

    font-mogra updated

--- Formula/font-mogra.rb
+++ Formula/font-mogra.rb
@@ -1,12 +1,14 @@
 class FontMogra < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mogra/Mogra-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mogra"
+  desc "Mogra font"
   homepage "https://fonts.google.com/specimen/Mogra"
+  head "https://github.com/google/fonts/raw/main/ofl/mogra/Mogra-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Mogra-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
