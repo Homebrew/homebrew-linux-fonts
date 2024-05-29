62f024af47df61ab19ca21cb8962c58c43d7b19a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:47 +0000

    font-noticia-text updated

--- Formula/font-noticia-text.rb
+++ Formula/font-noticia-text.rb
@@ -1,15 +1,19 @@
 class FontNoticiaText < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/noticiatext"
-  desc "Noticia Text"
+  desc "Noticia text font"
   homepage "https://fonts.google.com/specimen/Noticia+Text"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/noticiatext"
+
   def install
     (share/"fonts").install Dir.glob("ofl/noticiatext/./**/NoticiaText-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/noticiatext/./**/NoticiaText-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/noticiatext/./**/NoticiaText-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/noticiatext/./**/NoticiaText-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
