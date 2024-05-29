689902d445e8da82c0fc852b5ee177032ac26f72
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-jp updated

--- Formula/font-noto-serif-jp.rb
+++ Formula/font-noto-serif-jp.rb
@@ -1,13 +1,14 @@
 class FontNotoSerifJp < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifjp/NotoSerifJP%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif JP"
   desc "Modulated (“serif”) design for the japanese language"
   homepage "https://fonts.google.com/specimen/Noto+Serif+JP"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifjp/NotoSerifJP%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifJP\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifJP[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
