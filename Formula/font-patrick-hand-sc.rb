44f19e30dad0c426c3e2f21c37b4ac91febede39
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-patrick-hand-sc updated

--- Formula/font-patrick-hand-sc.rb
+++ Formula/font-patrick-hand-sc.rb
@@ -1,12 +1,14 @@
 class FontPatrickHandSc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/patrickhandsc/PatrickHandSC-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Patrick Hand SC"
+  desc "Patrick hand sc font"
   homepage "https://fonts.google.com/specimen/Patrick+Hand+SC"
+  head "https://github.com/google/fonts/raw/main/ofl/patrickhandsc/PatrickHandSC-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PatrickHandSC-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
