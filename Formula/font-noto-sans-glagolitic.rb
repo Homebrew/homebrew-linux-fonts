9754951601d132a19bb0d09b6f3ce3e5c7c04f88
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:50 +0000

    font-noto-sans-glagolitic updated

--- Formula/font-noto-sans-glagolitic.rb
+++ Formula/font-noto-sans-glagolitic.rb
@@ -1,12 +1,14 @@
 class FontNotoSansGlagolitic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGlagolitic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Glagolitic"
+  desc "Noto sans glagolitic font"
   homepage "https://www.google.com/get/noto/#sans-glag"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGlagolitic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansGlagolitic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
