bfad1eff715341a3beb00615925c38a12fd7698a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-myanmar updated

--- Formula/font-noto-sans-myanmar.rb
+++ Formula/font-noto-sans-myanmar.rb
@@ -1,9 +1,11 @@
 class FontNotoSansMyanmar < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMyanmar-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Myanmar"
+  desc "Noto sans myanmar font"
   homepage "https://www.google.com/get/noto/#sans-mymr"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMyanmar-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansMyanmar-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansMyanmarUI-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansMyanmar-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansMyanmar-Condensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansMyanmar-CondensedBlack.ttf")[0]
@@ -74,10 +76,10 @@ class FontNotoSansMyanmar < Formula
     (share/"fonts").install Dir.glob("./**/NotoSansMyanmarUI-SemiCondensedMedium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansMyanmarUI-SemiCondensedSemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansMyanmarUI-SemiCondensedThin.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/NotoSansMyanmarUI-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansMyanmar-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
