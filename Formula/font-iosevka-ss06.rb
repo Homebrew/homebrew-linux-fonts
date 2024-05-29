6ae668bf44c11fdf55dc6fb49cea1f77d0d95ef6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:22 +0000

    font-iosevka-ss06 updated

--- Formula/font-iosevka-ss06.rb
+++ Formula/font-iosevka-ss06.rb
@@ -1,12 +1,12 @@
 class FontIosevkaSs06 < Formula
-  version "30.1.1"
-  sha256 "bc0550cdba9e63e46ceca1ffb7be29900645ed60c2a980591bac91f13dd361f2"
-  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaSS06-#{version}.zip"
-  desc "Iosevka SS06"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
+  url "https://github.com/be5invis/Iosevka/releases/download/v30.1.1/PkgTTC-IosevkaSS06-30.1.1.zip"
+  version "30.1.1"
+  sha256 "bc0550cdba9e63e46ceca1ffb7be29900645ed60c2a980591bac91f13dd361f2"
+
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaSS06-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS06-Thin.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-ExtraBold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-ExtraLight.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-Heavy.ttc")[0]
@@ -14,10 +14,10 @@ class FontIosevkaSs06 < Formula
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-Medium.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-Regular.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS06-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS06-Bold.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
