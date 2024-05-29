50933fde877fc1176015302eac5d9638c9d6d0a2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-aclonica updated

--- Formula/font-aclonica.rb
+++ Formula/font-aclonica.rb
@@ -1,12 +1,14 @@
 class FontAclonica < Formula
-  head "https://github.com/google/fonts/raw/main/apache/aclonica/Aclonica-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Aclonica"
+  desc "Aclonica font"
   homepage "https://fonts.google.com/specimen/Aclonica"
+  head "https://github.com/google/fonts/raw/main/apache/aclonica/Aclonica-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Aclonica-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
