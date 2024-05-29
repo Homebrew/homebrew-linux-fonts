91af34e66f71a081c94d27d2719788bae1c62e5e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-tagbanwa updated

--- Formula/font-noto-sans-tagbanwa.rb
+++ Formula/font-noto-sans-tagbanwa.rb
@@ -1,12 +1,14 @@
 class FontNotoSansTagbanwa < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagbanwa-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Tagbanwa"
+  desc "Noto sans tagbanwa font"
   homepage "https://www.google.com/get/noto/#sans-tagb"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagbanwa-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansTagbanwa-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
