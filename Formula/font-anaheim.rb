3c47b35a89a89b75e2e2000727bb249b9801fa90
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-anaheim updated

--- Formula/font-anaheim.rb
+++ Formula/font-anaheim.rb
@@ -1,12 +1,14 @@
 class FontAnaheim < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anaheim/Anaheim%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Anaheim"
+  desc "Anaheim font"
   homepage "https://fonts.google.com/specimen/Anaheim"
+  head "https://github.com/google/fonts/raw/main/ofl/anaheim/Anaheim%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Anaheim\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Anaheim[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
