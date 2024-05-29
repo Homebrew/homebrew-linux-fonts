03869664b4ec62fb4e220bf0ffa5c3407a096882
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-souliyo updated

--- Formula/font-souliyo.rb
+++ Formula/font-souliyo.rb
@@ -1,12 +1,14 @@
 class FontSouliyo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/souliyo/Souliyo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Souliyo"
+  desc "Souliyo font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts/raw/main/ofl/souliyo/Souliyo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Souliyo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
