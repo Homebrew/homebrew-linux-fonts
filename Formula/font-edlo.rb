e2f09592d5f6aa463e9c5fe8a2383dc3c008dcb3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:02 +0000

    font-edlo updated

--- Formula/font-edlo.rb
+++ Formula/font-edlo.rb
@@ -1,12 +1,14 @@
 class FontEdlo < Formula
-  head "https://github.com/ehamiter/Edlo/raw/master/edlo.ttf", verified: "github.com/ehamiter/Edlo/"
-  desc "Edlo"
+  desc "Edlo font"
   homepage "https://ehamiter.github.io/Edlo/"
+  head "https://github.com/ehamiter/Edlo/raw/master/edlo.ttf",
+       verified: "github.com/ehamiter/Edlo/"
+
   def install
     (share/"fonts").install Dir.glob("./**/edlo.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
