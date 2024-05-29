7769d20a37bdb8199b9cb8f997309ed60777b79c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-commit-mono-nerd-font updated

--- Formula/font-commit-mono-nerd-font.rb
+++ Formula/font-commit-mono-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontCommitMonoNerdFont < Formula
-  version "3.2.1"
-  sha256 "99e3e1b6126191cc0f7e49ede5dc20b65a6b8ca808da0139b5669e412632bf8d"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/CommitMono.zip"
-  desc "CommitMono Nerd Font (Commit Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CommitMono.zip"
+  version "3.2.1"
+  sha256 "99e3e1b6126191cc0f7e49ede5dc20b65a6b8ca808da0139b5669e412632bf8d"
+
   def install
-    (share/"fonts").install Dir.glob("./**/CommitMonoNerdFont-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CommitMonoNerdFontPropo-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/CommitMonoNerdFont-BoldItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/CommitMonoNerdFont-Italic.otf")[0]
     (share/"fonts").install Dir.glob("./**/CommitMonoNerdFont-Regular.otf")[0]
@@ -17,10 +17,10 @@ class FontCommitMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/CommitMonoNerdFontPropo-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/CommitMonoNerdFontPropo-BoldItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/CommitMonoNerdFontPropo-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CommitMonoNerdFontPropo-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CommitMonoNerdFont-Bold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
