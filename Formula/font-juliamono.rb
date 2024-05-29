114dca803215da0eb1642078ef28b6ac04bd9bc5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-juliamono updated

--- Formula/font-juliamono.rb
+++ Formula/font-juliamono.rb
@@ -1,12 +1,12 @@
 class FontJuliamono < Formula
-  version "0.055"
-  sha256 "027654737001a1f85cc1689faa3676fb42a06d5bf5aa6e83ff77eb1edb3a3602"
-  url "https://github.com/cormullion/juliamono/releases/download/v#{version}/JuliaMono.tar.gz"
-  desc "JuliaMono"
   desc "Monospaced typeface designed for programming in Julia"
   homepage "https://github.com/cormullion/juliamono"
+  url "https://github.com/cormullion/juliamono/releases/download/v0.055/JuliaMono.tar.gz"
+  version "0.055"
+  sha256 "027654737001a1f85cc1689faa3676fb42a06d5bf5aa6e83ff77eb1edb3a3602"
+
   def install
-    (share/"fonts").install Dir.glob("./**/JuliaMono-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/JuliaMono-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-BoldItalic.ttf")[0]
@@ -21,10 +21,10 @@ class FontJuliamono < Formula
     (share/"fonts").install Dir.glob("./**/JuliaMono-RegularItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-RegularLatin.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/JuliaMono-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/JuliaMono-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
