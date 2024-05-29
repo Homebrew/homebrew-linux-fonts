e5caeb7a1ad4cb4d3e28db6d2e9fc8d7654cfab4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:32 +0000

    font-twinkle-star updated

--- Formula/font-twinkle-star.rb
+++ Formula/font-twinkle-star.rb
@@ -1,13 +1,14 @@
 class FontTwinkleStar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/twinklestar/TwinkleStar-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Twinkle Star"
   desc "Cute and fun juvenile script"
   homepage "https://fonts.google.com/specimen/Twinkle+Star"
+  head "https://github.com/google/fonts/raw/main/ofl/twinklestar/TwinkleStar-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/TwinkleStar-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
