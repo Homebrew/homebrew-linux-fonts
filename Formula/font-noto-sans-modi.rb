4c426648ff9fea3555164167ad9e15992cd4dc92
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-modi updated

--- Formula/font-noto-sans-modi.rb
+++ Formula/font-noto-sans-modi.rb
@@ -1,12 +1,14 @@
 class FontNotoSansModi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmodi/NotoSansModi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Modi"
+  desc "Noto sans modi font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Modi"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmodi/NotoSansModi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansModi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
