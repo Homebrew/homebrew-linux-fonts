588568e32a1cda09a70b3aaa25cb0c596f8ac044
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:12 +0000

    font-qwitcher-grypen updated

--- Formula/font-qwitcher-grypen.rb
+++ Formula/font-qwitcher-grypen.rb
@@ -1,14 +1,17 @@
 class FontQwitcherGrypen < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/qwitchergrypen"
-  desc "Qwitcher Grypen"
   desc "Casual brush script with a bit of an edge"
   homepage "https://fonts.google.com/specimen/Qwitcher+Grypen"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/qwitchergrypen"
+
   def install
     (share/"fonts").install Dir.glob("ofl/qwitchergrypen/./**/QwitcherGrypen-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/qwitchergrypen/./**/QwitcherGrypen-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
