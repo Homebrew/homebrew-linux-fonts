edabbe2867f24918395a1829a7806de395aa8080
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:00 +0000

    font-devicons updated

--- Formula/font-devicons.rb
+++ Formula/font-devicons.rb
@@ -1,14 +1,16 @@
 class FontDevicons < Formula
+  desc "Devicons font"
+  homepage "https://vorillaz.github.io/devicons/"
+  url "https://github.com/vorillaz/devicons/archive/1.8.0.zip",
+       verified: "github.com/vorillaz/devicons/"
   version "1.8.0"
   sha256 "fc0baa260f54832c059d1d9eab3798ae758d1a1cf0c1695e9883aab85d9a4308"
-  url "https://github.com/vorillaz/devicons/archive/#{version}.zip", verified: "github.com/vorillaz/devicons/"
-  desc "Devicons"
-  homepage "https://vorillaz.github.io/devicons/"
+
   def install
-    (share/"fonts").install Dir.glob("devicons-#{version}/fonts/**/devicons.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/devicons-1.8.0/fonts/devicons.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
