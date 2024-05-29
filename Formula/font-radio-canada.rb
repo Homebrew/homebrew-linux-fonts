7ee2cbff048c892560b741cf64b54d0b8cc17a91
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:12 +0000

    font-radio-canada updated

--- Formula/font-radio-canada.rb
+++ Formula/font-radio-canada.rb
@@ -1,13 +1,17 @@
 class FontRadioCanada < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/radiocanada"
-  desc "Radio Canada"
+  desc "Radio canada font"
   homepage "https://fonts.google.com/specimen/Radio+Canada"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/radiocanada"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/radiocanada/./**/RadioCanada-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/radiocanada/./**/RadioCanada\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/radiocanada/./**/RadioCanada-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/radiocanada/./**/RadioCanada[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
