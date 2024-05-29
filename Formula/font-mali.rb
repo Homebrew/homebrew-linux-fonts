e11b1aa9a946eec13f158baa3d43f9683011101f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:37 +0000

    font-mali updated

--- Formula/font-mali.rb
+++ Formula/font-mali.rb
@@ -1,9 +1,13 @@
 class FontMali < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/mali"
-  desc "Mali"
+  desc "Mali font"
   homepage "https://fonts.google.com/specimen/Mali"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/mali"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-ExtraLightItalic.ttf")[0]
@@ -14,10 +18,10 @@ class FontMali < Formula
     (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/mali/./**/Mali-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
