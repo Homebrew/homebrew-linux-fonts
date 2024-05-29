1c2dc710c48cda1cfa180d2d33070ac08dbe19ef
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:36 +0000

    font-aguafina-script updated

--- Formula/font-aguafina-script.rb
+++ Formula/font-aguafina-script.rb
@@ -1,12 +1,14 @@
 class FontAguafinaScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/aguafinascript/AguafinaScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Aguafina Script"
+  desc "Aguafina script font"
   homepage "https://fonts.google.com/specimen/Aguafina+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/aguafinascript/AguafinaScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AguafinaScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
