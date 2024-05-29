cb6a41debe8bcba55249e460adef5ac05d83039b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:58 +0000

    font-noto-sans-vai updated

--- Formula/font-noto-sans-vai.rb
+++ Formula/font-noto-sans-vai.rb
@@ -1,12 +1,14 @@
 class FontNotoSansVai < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansVai-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Vai"
+  desc "Noto sans vai font"
   homepage "https://www.google.com/get/noto/#sans-vaii"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansVai-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansVai-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
