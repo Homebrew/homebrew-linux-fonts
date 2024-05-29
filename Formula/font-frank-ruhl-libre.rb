3d32df98414d32ee30427a7bc7876e091d16abef
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-frank-ruhl-libre updated

--- Formula/font-frank-ruhl-libre.rb
+++ Formula/font-frank-ruhl-libre.rb
@@ -1,12 +1,14 @@
 class FontFrankRuhlLibre < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/frankruhllibre/FrankRuhlLibre%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Frank Ruhl Libre"
+  desc "Frank ruhl libre font"
   homepage "https://fonts.google.com/specimen/Frank+Ruhl+Libre"
+  head "https://github.com/google/fonts/raw/main/ofl/frankruhllibre/FrankRuhlLibre%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/FrankRuhlLibre\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FrankRuhlLibre[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
