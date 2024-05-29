d4b1fdfaf6d6837ce044d2448acd38304243a8b6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:30 +0000

    font-kulim-park updated

--- Formula/font-kulim-park.rb
+++ Formula/font-kulim-park.rb
@@ -1,9 +1,13 @@
 class FontKulimPark < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kulimpark"
-  desc "Kulim Park"
+  desc "Kulim park font"
   homepage "https://fonts.google.com/specimen/Kulim+Park"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kulimpark"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-ExtraLightItalic.ttf")[0]
@@ -12,10 +16,10 @@ class FontKulimPark < Formula
     (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-LightItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kulimpark/./**/KulimPark-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
