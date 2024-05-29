ee02893cbc1adb06d815886b95792dbcc8ff913d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:11 +0000

    font-pushster updated

--- Formula/font-pushster.rb
+++ Formula/font-pushster.rb
@@ -1,12 +1,14 @@
 class FontPushster < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/pushster/Pushster-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Pushster"
+  desc "Pushster font"
   homepage "https://fonts.google.com/specimen/Lobster"
+  head "https://github.com/google/fonts/raw/main/ofl/pushster/Pushster-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Pushster-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
