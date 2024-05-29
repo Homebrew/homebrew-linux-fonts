aef8f063cf08590a20774cb3702ad4eb525309ae
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-black-han-sans updated

--- Formula/font-black-han-sans.rb
+++ Formula/font-black-han-sans.rb
@@ -1,12 +1,14 @@
 class FontBlackHanSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/blackhansans/BlackHanSans-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Black Han Sans"
+  desc "Black han sans font"
   homepage "https://fonts.google.com/specimen/Black+Han+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/blackhansans/BlackHanSans-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/BlackHanSans-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
