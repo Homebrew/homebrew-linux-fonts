1264e3d1dbcb62bfc0a8533da5599464adff3944
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:34 +0000

    font-sf-compact (new font)

---
+++ Formula/font-sf-compact.rb
@@ -0,0 +1,15 @@
+class FontSfCompact < Formula
+  desc "Compact variant of \"San Francisco\" by Apple"
+  homepage "https://developer.apple.com/fonts/"
+  head "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg"
+
+  depends_on :macos
+
+  def install
+    # nothing to install
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
