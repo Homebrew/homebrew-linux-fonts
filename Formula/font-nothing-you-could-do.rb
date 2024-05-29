8ff3b45c3d63a74d406c975f8e32a4020b63121e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:46 +0000

    font-nothing-you-could-do updated

--- Formula/font-nothing-you-could-do.rb
+++ Formula/font-nothing-you-could-do.rb
@@ -1,12 +1,14 @@
 class FontNothingYouCouldDo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/nothingyoucoulddo/NothingYouCouldDo.ttf", verified: "github.com/google/fonts/"
-  desc "Nothing You Could Do"
+  desc "Nothing you could do font"
   homepage "https://fonts.google.com/specimen/Nothing+You+Could+Do"
+  head "https://github.com/google/fonts/raw/main/ofl/nothingyoucoulddo/NothingYouCouldDo.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NothingYouCouldDo.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
