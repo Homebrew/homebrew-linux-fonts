cf3df9c8a160dc39dee155eebec232844dca1836
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-charter updated

--- Formula/font-charter.rb
+++ Formula/font-charter.rb
@@ -1,17 +1,18 @@
 class FontCharter < Formula
+  desc "Charter font"
+  homepage "https://practicaltypography.com/charter.html"
+  url "https://practicaltypography.com/fonts/Charter%20210112.zip"
   version "210112"
   sha256 "b40297f1a615f94594bdad0995848eb2223fb53ccb4ea197cabf24439bd811c9"
-  url "https://practicaltypography.com/fonts/Charter%20#{version}.zip"
-  desc "Charter"
-  homepage "https://practicaltypography.com/charter.html"
+
   def install
-    (share/"fonts").install Dir.glob("Charter #{version}/OTF format (best for Mac OS)/Charter/**/Charter Bold Italic.otf")[0]
-    (share/"fonts").install Dir.glob("Charter #{version}/OTF format (best for Mac OS)/Charter/**/Charter Bold.otf")[0]
-    (share/"fonts").install Dir.glob("Charter #{version}/OTF format (best for Mac OS)/Charter/**/Charter Italic.otf")[0]
-    (share/"fonts").install Dir.glob("Charter #{version}/OTF format (best for Mac OS)/Charter/**/Charter Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Charter 210112/OTF format (best for Mac OS)/Charter/Charter Bold Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Charter 210112/OTF format (best for Mac OS)/Charter/Charter Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Charter 210112/OTF format (best for Mac OS)/Charter/Charter Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Charter 210112/OTF format (best for Mac OS)/Charter/Charter Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
