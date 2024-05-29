e237f1237891e5dfb66dec351045a56837e69d41
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-pahawh-hmong updated

--- Formula/font-noto-sans-pahawh-hmong.rb
+++ Formula/font-noto-sans-pahawh-hmong.rb
@@ -1,12 +1,14 @@
 class FontNotoSansPahawhHmong < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanspahawhhmong/NotoSansPahawhHmong-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Pahawh Hmong"
+  desc "Noto sans pahawh hmong font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Pahawh+Hmong"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanspahawhhmong/NotoSansPahawhHmong-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansPahawhHmong-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
