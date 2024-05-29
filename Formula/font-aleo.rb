e6bced0bd4e40d717318cf5895d5fb86633ad931
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-aleo updated

--- Formula/font-aleo.rb
+++ Formula/font-aleo.rb
@@ -1,13 +1,17 @@
 class FontAleo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/aleo"
-  desc "Aleo"
+  desc "Aleo font"
   homepage "https://fonts.google.com/specimen/Aleo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/aleo"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/aleo/./**/Aleo-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/aleo/./**/Aleo\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/aleo/./**/Aleo-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/aleo/./**/Aleo[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
