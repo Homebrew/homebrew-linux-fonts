c47049b03e57ab55f4a551810835bd1d6eff3b6d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-jost updated

--- Formula/font-jost.rb
+++ Formula/font-jost.rb
@@ -1,13 +1,17 @@
 class FontJost < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/jost"
-  desc "Jost"
+  desc "Jost font"
   homepage "https://fonts.google.com/specimen/Jost"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/jost"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/jost/./**/Jost-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/jost/./**/Jost\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/jost/./**/Jost-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/jost/./**/Jost[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
