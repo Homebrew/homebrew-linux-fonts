c409651a260bde0a31bfa82b2cd650299b978373
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:27 +0000

    font-katibeh updated

--- Formula/font-katibeh.rb
+++ Formula/font-katibeh.rb
@@ -1,12 +1,14 @@
 class FontKatibeh < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/katibeh/Katibeh-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Katibeh"
+  desc "Katibeh font"
   homepage "https://fonts.google.com/specimen/Katibeh"
+  head "https://github.com/google/fonts/raw/main/ofl/katibeh/Katibeh-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Katibeh-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
