a1f83175afdeb001dc2612e3c5847e68d74bc83d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-oriya-ui updated

--- Formula/font-noto-sans-oriya-ui.rb
+++ Formula/font-noto-sans-oriya-ui.rb
@@ -1,15 +1,19 @@
 class FontNotoSansOriyaUi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosansoriyaui"
-  desc "Noto Sans Oriya UI"
+  desc "Noto sans oriya ui font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Oriya"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/notosansoriyaui"
+
   def install
     (share/"fonts").install Dir.glob("ofl/notosansoriyaui/./**/NotoSansOriyaUI-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansoriyaui/./**/NotoSansOriyaUI-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansoriyaui/./**/NotoSansOriyaUI-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansoriyaui/./**/NotoSansOriyaUI-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
