0920297828d37691ff7b893341712a7ec9e1422e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-bitstream-vera-sans-mono-nerd-font updated

--- Formula/font-bitstream-vera-sans-mono-nerd-font.rb
+++ Formula/font-bitstream-vera-sans-mono-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontBitstreamVeraSansMonoNerdFont < Formula
-  version "3.2.1"
-  sha256 "6a5d20c72f068a65ebf6e1cf1e168ced5d8c78d703d90b6041a5232db8111205"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BitstreamVeraSansMono.zip"
-  desc "BitstromWera Nerd Font (Bitstream Vera Sans Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/BitstreamVeraSansMono.zip"
+  version "3.2.1"
+  sha256 "6a5d20c72f068a65ebf6e1cf1e168ced5d8c78d703d90b6041a5232db8111205"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-Oblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-Regular.ttf")[0]
@@ -17,10 +17,10 @@ class FontBitstreamVeraSansMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-Oblique.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
