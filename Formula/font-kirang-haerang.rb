22f3ee22f23b99122ce72c4d820afb81711d56fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-kirang-haerang updated

--- Formula/font-kirang-haerang.rb
+++ Formula/font-kirang-haerang.rb
@@ -1,12 +1,14 @@
 class FontKirangHaerang < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kiranghaerang/KirangHaerang-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Kirang Haerang"
+  desc "Kirang haerang font"
   homepage "https://fonts.google.com/specimen/Kirang+Haerang"
+  head "https://github.com/google/fonts/raw/main/ofl/kiranghaerang/KirangHaerang-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/KirangHaerang-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
