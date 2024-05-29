c2f59828c6821616260de983bba7e0b1a49480d0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:36 +0000

    font-macondo updated

--- Formula/font-macondo.rb
+++ Formula/font-macondo.rb
@@ -1,12 +1,14 @@
 class FontMacondo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/macondo/Macondo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Macondo"
+  desc "Macondo font"
   homepage "https://fonts.google.com/specimen/Macondo"
+  head "https://github.com/google/fonts/raw/main/ofl/macondo/Macondo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Macondo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
