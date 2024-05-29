44857800194dd8c23bec8ee2b4d73d986dc61b0f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-foldit updated

--- Formula/font-foldit.rb
+++ Formula/font-foldit.rb
@@ -1,13 +1,14 @@
 class FontFoldit < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/foldit/Foldit%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Foldit"
   desc "Font which uses gradients to play with dimension and sense of space"
   homepage "https://fonts.google.com/specimen/Foldit"
+  head "https://github.com/google/fonts/raw/main/ofl/foldit/Foldit%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Foldit\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Foldit[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
