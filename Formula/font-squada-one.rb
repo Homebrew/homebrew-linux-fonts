fba8281b912f887a4c3b9e21b93143928b1315bc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:25 +0000

    font-squada-one updated

--- Formula/font-squada-one.rb
+++ Formula/font-squada-one.rb
@@ -1,12 +1,14 @@
 class FontSquadaOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/squadaone/SquadaOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Squada One"
+  desc "Squada one font"
   homepage "https://fonts.google.com/specimen/Squada+One"
+  head "https://github.com/google/fonts/raw/main/ofl/squadaone/SquadaOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SquadaOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
