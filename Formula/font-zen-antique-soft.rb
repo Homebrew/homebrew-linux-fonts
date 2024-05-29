0b362c228edc30fe49a33efc69741e482228f758
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:38 +0000

    font-zen-antique-soft updated

--- Formula/font-zen-antique-soft.rb
+++ Formula/font-zen-antique-soft.rb
@@ -1,12 +1,14 @@
 class FontZenAntiqueSoft < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/zenantiquesoft/ZenAntiqueSoft-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Zen Antique Soft"
+  desc "Zen antique soft font"
   homepage "https://fonts.google.com/specimen/Zen+Antique+Soft"
+  head "https://github.com/google/fonts/raw/main/ofl/zenantiquesoft/ZenAntiqueSoft-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ZenAntiqueSoft-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
