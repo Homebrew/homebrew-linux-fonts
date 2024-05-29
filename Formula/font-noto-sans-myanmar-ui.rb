64750577d1f01eb1ed796c18c587e398eb5e90de
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-myanmar-ui updated

--- Formula/font-noto-sans-myanmar-ui.rb
+++ Formula/font-noto-sans-myanmar-ui.rb
@@ -1,9 +1,13 @@
 class FontNotoSansMyanmarUi < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosansmyanmarui"
-  desc "Noto Sans Myanmar UI"
+  desc "Noto sans myanmar ui font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Myanmar"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/notosansmyanmarui"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-ExtraLight.ttf")[0]
@@ -11,10 +15,10 @@ class FontNotoSansMyanmarUi < Formula
     (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansmyanmarui/./**/NotoSansMyanmarUI-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
