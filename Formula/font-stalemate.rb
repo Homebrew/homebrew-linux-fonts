5d3ff8178d17777905cad763294aefa370cb81e1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-stalemate updated

--- Formula/font-stalemate.rb
+++ Formula/font-stalemate.rb
@@ -1,12 +1,14 @@
 class FontStalemate < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/stalemate/Stalemate-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Stalemate"
+  desc "Stalemate font"
   homepage "https://fonts.google.com/specimen/Stalemate"
+  head "https://github.com/google/fonts/raw/main/ofl/stalemate/Stalemate-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Stalemate-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
