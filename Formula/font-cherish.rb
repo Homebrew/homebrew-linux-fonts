8672dca9329cf2e0af8951e5e0a23fd3e5ffcbd2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:54 +0000

    font-cherish updated

--- Formula/font-cherish.rb
+++ Formula/font-cherish.rb
@@ -1,13 +1,14 @@
 class FontCherish < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/cherish/Cherish-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Cherish"
   desc "Dry brush style that adds expression and sophistication"
   homepage "https://fonts.google.com/specimen/Cherish"
+  head "https://github.com/google/fonts/raw/main/ofl/cherish/Cherish-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Cherish-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
