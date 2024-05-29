0d2c40d81e54c8d14910fa8c0ade59ab90ddbcfd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-mrs-sheppards updated

--- Formula/font-mrs-sheppards.rb
+++ Formula/font-mrs-sheppards.rb
@@ -1,12 +1,14 @@
 class FontMrsSheppards < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mrssheppards/MrsSheppards-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mrs Sheppards"
+  desc "Mrs sheppards font"
   homepage "https://fonts.google.com/specimen/Mrs+Sheppards"
+  head "https://github.com/google/fonts/raw/main/ofl/mrssheppards/MrsSheppards-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MrsSheppards-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
