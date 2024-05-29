5508f268d89895d9e512d1fb03b6082f5a4f82c1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:09 +0000

    font-poppins-latin updated

--- Formula/font-poppins-latin.rb
+++ Formula/font-poppins-latin.rb
@@ -1,11 +1,12 @@
 class FontPoppinsLatin < Formula
+  desc "Poppinslatin font"
+  homepage "https://github.com/itfoundry/poppins"
+  url "https://github.com/itfoundry/Poppins/raw/master/products/PoppinsLatin-5.001-Latin-OTF.zip"
   version "5.001"
   sha256 "cd4bdded93013fc9f123485b8fbc6113f62a7ff77492cc97cba6901cf6493738"
-  url "https://github.com/itfoundry/Poppins/raw/master/products/PoppinsLatin-#{version}-Latin-OTF.zip"
-  desc "PoppinsLatin"
-  homepage "https://github.com/itfoundry/poppins"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PoppinsLatin-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/PoppinsLatin-ThinItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/PoppinsLatin-BlackItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/PoppinsLatin-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/PoppinsLatin-BoldItalic.otf")[0]
@@ -22,10 +23,10 @@ class FontPoppinsLatin < Formula
     (share/"fonts").install Dir.glob("./**/PoppinsLatin-SemiBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/PoppinsLatin-SemiBoldItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/PoppinsLatin-Thin.otf")[0]
-    (share/"fonts").install Dir.glob("./**/PoppinsLatin-ThinItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/PoppinsLatin-Black.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
