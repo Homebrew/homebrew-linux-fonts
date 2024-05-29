b60438f444147382b3239b397530e8f009f0a288
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:06 +0000

    font-faune updated

--- Formula/font-faune.rb
+++ Formula/font-faune.rb
@@ -1,18 +1,20 @@
 class FontFaune < Formula
-  version "1.000"
-  url "http://www.cnap.graphismeenfrance.fr/faune/Faune_Fonts.zip"
-  desc "Faune"
+  desc "Faune font"
   homepage "http://www.cnap.graphismeenfrance.fr/faune/en.html"
+  url "http://www.cnap.graphismeenfrance.fr/faune/Faune_Fonts.zip"
+  version "1.000"
+  sha256 :no_check
+
   def install
-    (share/"fonts").install Dir.glob("Faune_Fonts/Otf/**/Faune-DisplayBlack.otf")[0]
-    (share/"fonts").install Dir.glob("Faune_Fonts/Otf/**/Faune-DisplayBoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Faune_Fonts/Otf/**/Faune-DisplayThin.otf")[0]
-    (share/"fonts").install Dir.glob("Faune_Fonts/Otf/**/Faune-TextBold.otf")[0]
-    (share/"fonts").install Dir.glob("Faune_Fonts/Otf/**/Faune-TextItalic.otf")[0]
-    (share/"fonts").install Dir.glob("Faune_Fonts/Otf/**/Faune-TextRegular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Faune_Fonts/Otf/Faune-DisplayBlack.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Faune_Fonts/Otf/Faune-DisplayBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Faune_Fonts/Otf/Faune-DisplayThin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Faune_Fonts/Otf/Faune-TextBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Faune_Fonts/Otf/Faune-TextItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Faune_Fonts/Otf/Faune-TextRegular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
