3bff270b97106038afe21ebaae11c97a11505026
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-moo-lah-lah updated

--- Formula/font-moo-lah-lah.rb
+++ Formula/font-moo-lah-lah.rb
@@ -1,12 +1,14 @@
 class FontMooLahLah < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/moolahlah/MooLahLah-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Moo Lah Lah"
+  desc "Moo lah lah font"
   homepage "https://fonts.google.com/specimen/Moo+Lah+Lah"
+  head "https://github.com/google/fonts/raw/main/ofl/moolahlah/MooLahLah-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MooLahLah-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
