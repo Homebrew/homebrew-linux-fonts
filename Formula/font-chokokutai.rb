5b0aedb8736184bb47776fbfa351e64fe5afd567
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:54 +0000

    font-chokokutai updated

--- Formula/font-chokokutai.rb
+++ Formula/font-chokokutai.rb
@@ -1,13 +1,14 @@
 class FontChokokutai < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/chokokutai/Chokokutai-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Chokokutai"
   desc "Japanese display font whose characters have a funky appearance"
   homepage "https://fonts.google.com/specimen/Chokokutai"
+  head "https://github.com/google/fonts/raw/main/ofl/chokokutai/Chokokutai-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Chokokutai-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
