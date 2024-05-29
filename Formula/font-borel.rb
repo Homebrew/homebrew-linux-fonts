77f761c9e797bd1a6f3bf9c7e9132e431c978f69
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:49 +0000

    font-borel updated

--- Formula/font-borel.rb
+++ Formula/font-borel.rb
@@ -1,13 +1,14 @@
 class FontBorel < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/borel/Borel-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Borel"
   desc "French cursive primer"
   homepage "https://fonts.google.com/specimen/Borel"
+  head "https://github.com/google/fonts/raw/main/ofl/borel/Borel-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Borel-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
