c283d8510836030f5e0bac3c686cfe4b55144f7d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-orelega-one updated

--- Formula/font-orelega-one.rb
+++ Formula/font-orelega-one.rb
@@ -1,12 +1,14 @@
 class FontOrelegaOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/orelegaone/OrelegaOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Orelega One"
+  desc "Orelega one font"
   homepage "https://fonts.google.com/specimen/Orelega+One"
+  head "https://github.com/google/fonts/raw/main/ofl/orelegaone/OrelegaOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/OrelegaOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
