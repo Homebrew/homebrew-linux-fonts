ece394b1f0a0c5f936d1e55b5ffbb835d9c27f18
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-oswald updated

--- Formula/font-oswald.rb
+++ Formula/font-oswald.rb
@@ -1,12 +1,14 @@
 class FontOswald < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/oswald/Oswald%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Oswald"
+  desc "Oswald font"
   homepage "https://fonts.google.com/specimen/Oswald"
+  head "https://github.com/google/fonts/raw/main/ofl/oswald/Oswald%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Oswald\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Oswald[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
