8b581e26404c6366a1d07918ab7604cfcf6cf4a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-sue-ellen-francisco updated

--- Formula/font-sue-ellen-francisco.rb
+++ Formula/font-sue-ellen-francisco.rb
@@ -1,12 +1,14 @@
 class FontSueEllenFrancisco < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sueellenfrancisco/SueEllenFrancisco-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sue Ellen Francisco"
+  desc "Sue ellen francisco font"
   homepage "https://fonts.google.com/specimen/Sue+Ellen+Francisco"
+  head "https://github.com/google/fonts/raw/main/ofl/sueellenfrancisco/SueEllenFrancisco-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SueEllenFrancisco-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
