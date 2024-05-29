9a0f53856195a312edced0be0eced89c44642459
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-solitreo updated

--- Formula/font-solitreo.rb
+++ Formula/font-solitreo.rb
@@ -1,13 +1,14 @@
 class FontSolitreo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/solitreo/Solitreo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Solitreo"
   desc "Relic of the past, as most writers of the language utilize roman characters"
   homepage "https://fonts.google.com/specimen/Solitreo"
+  head "https://github.com/google/fonts/raw/main/ofl/solitreo/Solitreo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Solitreo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
