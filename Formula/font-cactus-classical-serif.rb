795ff7e5aefcc57915b7c6dfd8e3037fbdf50c88
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:50 +0000

    font-cactus-classical-serif updated

--- Formula/font-cactus-classical-serif.rb
+++ Formula/font-cactus-classical-serif.rb
@@ -1,12 +1,13 @@
 class FontCactusClassicalSerif < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/cactusclassicalserif/CactusClassicalSerif-Regular.ttf"
-  desc "Cactus Classical Serif"
+  desc "Cactus classical serif font"
   homepage "https://github.com/aaronbell/CactusSerif"
+  head "https://github.com/google/fonts/raw/main/ofl/cactusclassicalserif/CactusClassicalSerif-Regular.ttf"
+
   def install
     (share/"fonts").install Dir.glob("./**/CactusClassicalSerif-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
