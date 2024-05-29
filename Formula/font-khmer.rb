38d3b3114115617058fdd38299f91b8b4da74962
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:28 +0000

    font-khmer updated

--- Formula/font-khmer.rb
+++ Formula/font-khmer.rb
@@ -1,12 +1,14 @@
 class FontKhmer < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/khmer/Khmer.ttf", verified: "github.com/google/fonts/"
-  desc "Khmer"
+  desc "Khmer font"
   homepage "https://fonts.google.com/specimen/Khmer"
+  head "https://github.com/google/fonts/raw/main/ofl/khmer/Khmer.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Khmer.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
