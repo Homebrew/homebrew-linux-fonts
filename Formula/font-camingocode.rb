5b1cb35cba3901cf219c24e8736987405c2b6fea
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:51 +0000

    font-camingocode updated

--- Formula/font-camingocode.rb
+++ Formula/font-camingocode.rb
@@ -1,16 +1,18 @@
 class FontCamingocode < Formula
-  version "1.0"
-  url "https://janfromm.de/typefaces/camingocode/camingocode.zip"
-  desc "CamingoCode"
+  desc "Camingocode font"
   homepage "https://www.janfromm.de/typefaces/camingomono/camingocode/"
+  url "https://janfromm.de/typefaces/camingocode/camingocode.zip"
+  version "1.0"
+  sha256 :no_check
+
   def install
-    (share/"fonts").install Dir.glob("TTF/**/CamingoCode-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("TTF/**/CamingoCode-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("TTF/**/CamingoCode-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("TTF/**/CamingoCode-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TTF/CamingoCode-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TTF/CamingoCode-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TTF/CamingoCode-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TTF/CamingoCode-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
