e529c690e48245f36198b87727c0637dff99b456
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:31 +0000

    font-tiro-tamil updated

--- Formula/font-tiro-tamil.rb
+++ Formula/font-tiro-tamil.rb
@@ -1,14 +1,17 @@
 class FontTiroTamil < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/tirotamil"
-  desc "Tiro Tamil"
   desc "Especially suited to traditional literary publishing"
   homepage "https://fonts.google.com/specimen/Tiro+Tamil"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/tirotamil"
+
   def install
     (share/"fonts").install Dir.glob("ofl/tirotamil/./**/TiroTamil-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/tirotamil/./**/TiroTamil-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
