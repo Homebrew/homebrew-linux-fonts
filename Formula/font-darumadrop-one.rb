d5847e47eec67090a33127f3d458d654caa97c3a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:59 +0000

    font-darumadrop-one updated

--- Formula/font-darumadrop-one.rb
+++ Formula/font-darumadrop-one.rb
@@ -1,12 +1,14 @@
 class FontDarumadropOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/darumadropone/DarumadropOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Darumadrop One"
+  desc "Darumadrop one font"
   homepage "https://fonts.google.com/specimen/Darumadrop+One"
+  head "https://github.com/google/fonts/raw/main/ofl/darumadropone/DarumadropOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DarumadropOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
