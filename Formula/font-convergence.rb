e907688c3b1e99160dc76895c80157f00df05ac1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-convergence updated

--- Formula/font-convergence.rb
+++ Formula/font-convergence.rb
@@ -1,12 +1,14 @@
 class FontConvergence < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/convergence/Convergence-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Convergence"
+  desc "Convergence font"
   homepage "https://fonts.google.com/specimen/Convergence"
+  head "https://github.com/google/fonts/raw/main/ofl/convergence/Convergence-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Convergence-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
