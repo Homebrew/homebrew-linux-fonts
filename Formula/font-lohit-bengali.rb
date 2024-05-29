dd46ec15e91ef94f2bdcf32c724353f05e7f815e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-lohit-bengali updated

--- Formula/font-lohit-bengali.rb
+++ Formula/font-lohit-bengali.rb
@@ -1,12 +1,14 @@
 class FontLohitBengali < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lohitbengali/Lohit-Bengali.ttf", verified: "github.com/google/fonts/"
-  desc "Lohit Bengali"
+  desc "Lohit bengali font"
   homepage "https://fonts.google.com/specimen/Lohit+Bengali"
+  head "https://github.com/google/fonts/raw/main/ofl/lohitbengali/Lohit-Bengali.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Lohit-Bengali.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
