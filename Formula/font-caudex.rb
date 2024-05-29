a95d96249ee8481e533c65673377690f5914299d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:52 +0000

    font-caudex updated

--- Formula/font-caudex.rb
+++ Formula/font-caudex.rb
@@ -1,15 +1,19 @@
 class FontCaudex < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/caudex"
-  desc "Caudex"
+  desc "Caudex font"
   homepage "https://fonts.google.com/specimen/Caudex"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/caudex"
+
   def install
     (share/"fonts").install Dir.glob("ofl/caudex/./**/Caudex-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/caudex/./**/Caudex-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/caudex/./**/Caudex-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/caudex/./**/Caudex-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
