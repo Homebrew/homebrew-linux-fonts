cb0a318fdf2ce3d2f62a012b7a04a4e47d8be1e4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:58 +0000

    font-crete-round updated

--- Formula/font-crete-round.rb
+++ Formula/font-crete-round.rb
@@ -1,13 +1,17 @@
 class FontCreteRound < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/creteround"
-  desc "Crete Round"
+  desc "Crete round font"
   homepage "https://fonts.google.com/specimen/Crete+Round"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/creteround"
+
   def install
     (share/"fonts").install Dir.glob("ofl/creteround/./**/CreteRound-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/creteround/./**/CreteRound-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
