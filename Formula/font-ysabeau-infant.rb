7f8d847ca06cb3d9aa1240173bf98c434c1a6271
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-ysabeau-infant updated

--- Formula/font-ysabeau-infant.rb
+++ Formula/font-ysabeau-infant.rb
@@ -1,13 +1,17 @@
 class FontYsabeauInfant < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/ysabeauinfant"
-  desc "Ysabeau Infant"
+  desc "Ysabeau infant font"
   homepage "https://fonts.google.com/specimen/Ysabeau+Infant"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/ysabeauinfant"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/ysabeauinfant/./**/YsabeauInfant-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ysabeauinfant/./**/YsabeauInfant\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ysabeauinfant/./**/YsabeauInfant-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ysabeauinfant/./**/YsabeauInfant[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
