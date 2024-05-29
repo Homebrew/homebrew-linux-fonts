b6a98f3591db52f7d8f2b2c3ba2a00fd9dedac43
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-albertella updated

--- Formula/font-albertella.rb
+++ Formula/font-albertella.rb
@@ -1,13 +1,13 @@
 class FontAlbertella < Formula
-  head "https://moji-waku.com/download/albertella.zip"
-  desc "Albertella"
   desc "Ancient biology styled font inspired by trilobite"
   homepage "https://moji-waku.com/albertella/index.html"
+  head "https://moji-waku.com/download/albertella.zip"
+
   def install
-    (share/"fonts").install Dir.glob("albertella/**/Albertella-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/albertella/Albertella-Light.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
