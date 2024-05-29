9d4929a9e625315b065430654ccd115c6a9095f2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-neucha updated

--- Formula/font-neucha.rb
+++ Formula/font-neucha.rb
@@ -1,12 +1,14 @@
 class FontNeucha < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/neucha/Neucha.ttf", verified: "github.com/google/fonts/"
-  desc "Neucha"
+  desc "Neucha font"
   homepage "https://fonts.google.com/specimen/Neucha"
+  head "https://github.com/google/fonts/raw/main/ofl/neucha/Neucha.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Neucha.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
