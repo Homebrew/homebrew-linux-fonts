58675098fc2acc6f21ecb02d2a80a20a1782eef2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-tc updated

--- Formula/font-noto-sans-tc.rb
+++ Formula/font-noto-sans-tc.rb
@@ -1,13 +1,14 @@
 class FontNotoSansTc < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanstc/NotoSansTC%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans TC"
   desc "Unmodulated (“sans serif”) design for languages in taiwan and macau"
   homepage "https://fonts.google.com/specimen/Noto+Sans+TC"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanstc/NotoSansTC%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansTC\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansTC[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
