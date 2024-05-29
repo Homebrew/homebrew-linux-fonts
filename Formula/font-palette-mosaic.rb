3de06ede21c19a2eacafcc6a95f1180d9dcbdf45
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:04 +0000

    font-palette-mosaic updated

--- Formula/font-palette-mosaic.rb
+++ Formula/font-palette-mosaic.rb
@@ -1,12 +1,14 @@
 class FontPaletteMosaic < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/palettemosaic/PaletteMosaic-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Palette Mosaic"
+  desc "Palette mosaic font"
   homepage "https://fonts.google.com/specimen/Palette+Mosaic"
+  head "https://github.com/google/fonts/raw/main/ofl/palettemosaic/PaletteMosaic-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/PaletteMosaic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
