aee7f42ea2c43b13808ac54ee94aba391e5c44d5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-gideon-roman updated

--- Formula/font-gideon-roman.rb
+++ Formula/font-gideon-roman.rb
@@ -1,12 +1,14 @@
 class FontGideonRoman < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/gideonroman/GideonRoman-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Gideon Roman"
+  desc "Gideon roman font"
   homepage "https://fonts.google.com/specimen/Gideon+Roman"
+  head "https://github.com/google/fonts/raw/main/ofl/gideonroman/GideonRoman-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/GideonRoman-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
