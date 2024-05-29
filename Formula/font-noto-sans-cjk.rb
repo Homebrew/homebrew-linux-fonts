2069a16eed9ee373d6a0fb352fb6be1541ae39e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:49 +0000

    font-noto-sans-cjk updated

--- Formula/font-noto-sans-cjk.rb
+++ Formula/font-noto-sans-cjk.rb
@@ -1,15 +1,15 @@
 class FontNotoSansCjk < Formula
-  version "2.004"
-  sha256 "a56077736038875f3a5b1e6715b2713a2bd049cc0bf0dfdf645927b070d38bfc"
-  url "https://github.com/notofonts/noto-cjk/releases/download/Sans#{version}/00_NotoSansCJK.ttc.zip"
-  desc "Noto Sans CJK"
   desc "Static Super OTC"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Sans2.004/00_NotoSansCJK.ttc.zip"
+  version "2.004"
+  sha256 "a56077736038875f3a5b1e6715b2713a2bd049cc0bf0dfdf645927b070d38bfc"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansCJK.ttc")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
