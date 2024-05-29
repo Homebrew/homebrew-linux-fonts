5c4b2803222b791dec95281eda267fd58b7d533a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-brahmi updated

--- Formula/font-noto-sans-brahmi.rb
+++ Formula/font-noto-sans-brahmi.rb
@@ -1,12 +1,14 @@
 class FontNotoSansBrahmi < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBrahmi-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Brahmi"
+  desc "Noto sans brahmi font"
   homepage "https://www.google.com/get/noto/#sans-brah"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBrahmi-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansBrahmi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
