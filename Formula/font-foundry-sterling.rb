fcb3c2ba6e33f1568e402e86d224c7756745c548
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-foundry-sterling updated

--- Formula/font-foundry-sterling.rb
+++ Formula/font-foundry-sterling.rb
@@ -1,10 +1,12 @@
 class FontFoundrySterling < Formula
-  version "001.005"
-  url "https://fontlot.com/wp-content/uploads/2017/09/foundrysterling.zip"
-  desc "Foundry Sterling"
+  desc "Foundry sterling font"
   homepage "https://fontlot.com/7134/foundry-sterling/"
+  url "https://fontlot.com/wp-content/uploads/2017/09/foundrysterling.zip"
+  version "001.005"
+  sha256 :no_check
+
   def install
-    (share/"fonts").install Dir.glob("./**/FoundrySterling-Book.otf")[0]
+    (share/"fonts").install Dir.glob("./**/zFoundrySterling-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/FoundrySterling-BookItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/FoundrySterling-Demi.otf")[0]
     (share/"fonts").install Dir.glob("./**/FoundrySterling-ExtraBold.otf")[0]
@@ -24,10 +26,10 @@ class FontFoundrySterling < Formula
     (share/"fonts").install Dir.glob("./**/FoundrySterlingOT3-ExtraBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/FoundrySterlingOT3-Light.otf")[0]
     (share/"fonts").install Dir.glob("./**/FoundrySterlingOT3-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zFoundrySterling-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/FoundrySterling-Book.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
