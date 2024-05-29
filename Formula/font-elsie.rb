467a0d0c3839bd8543e2d122b3fb9a9a321035ec
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:03 +0000

    font-elsie updated

--- Formula/font-elsie.rb
+++ Formula/font-elsie.rb
@@ -1,13 +1,17 @@
 class FontElsie < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/elsie"
-  desc "Elsie"
+  desc "Elsie font"
   homepage "https://fonts.google.com/specimen/Elsie"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/elsie"
+
   def install
     (share/"fonts").install Dir.glob("ofl/elsie/./**/Elsie-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/elsie/./**/Elsie-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
