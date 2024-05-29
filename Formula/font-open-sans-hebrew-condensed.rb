050bd13d428d3992b4769d44a0d1465a31750ee0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-open-sans-hebrew-condensed updated

--- Formula/font-open-sans-hebrew-condensed.rb
+++ Formula/font-open-sans-hebrew-condensed.rb
@@ -1,19 +1,23 @@
 class FontOpenSansHebrewCondensed < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "apache/opensanshebrewcondensed"
-  desc "Open Sans Hebrew Condensed"
+  desc "Open sans hebrew condensed font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "apache/opensanshebrewcondensed"
+
   def install
-    (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-ExtraBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-Light.ttf")[0]
     (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-LightItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("apache/opensanshebrewcondensed/./**/OpenSansHebrewCondensed-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
