046183fb2570650d6efcaa225654e833a7bd0a9b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:06 +0000

    font-federo updated

--- Formula/font-federo.rb
+++ Formula/font-federo.rb
@@ -1,12 +1,14 @@
 class FontFedero < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/federo/Federo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Federo"
+  desc "Federo font"
   homepage "https://fonts.google.com/specimen/Federo"
+  head "https://github.com/google/fonts/raw/main/ofl/federo/Federo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Federo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
