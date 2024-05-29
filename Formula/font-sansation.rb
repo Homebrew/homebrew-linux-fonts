eb554c790b66f655eb091b8933dc9a9f0afffaf1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-sansation updated

--- Formula/font-sansation.rb
+++ Formula/font-sansation.rb
@@ -1,7 +1,11 @@
 class FontSansation < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sansation"
-  desc "Sansation"
+  desc "Sansation font"
   homepage "https://www.fontspace.com/sansation-font-f20328"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sansation"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sansation/./**/Sansation-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sansation/./**/Sansation-BoldItalic.ttf")[0]
@@ -10,8 +14,8 @@ class FontSansation < Formula
     (share/"fonts").install Dir.glob("ofl/sansation/./**/Sansation-LightItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sansation/./**/Sansation-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
