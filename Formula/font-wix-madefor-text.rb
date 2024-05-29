c28c5406c6b537ecd0c05a84eae5b0a2b4206dcd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:36 +0000

    font-wix-madefor-text updated

--- Formula/font-wix-madefor-text.rb
+++ Formula/font-wix-madefor-text.rb
@@ -1,16 +1,17 @@
 class FontWixMadeforText < Formula
-  version "3.100"
-  sha256 "7fdbd012ca9e245d7c177a341bdbdf789521590e175322a9013c035981138f1c"
-  url "https://github.com/wix-incubator/wixmadefor/releases/download/#{version}/wixmadefor-fonts.zip", verified: "github.com/wix-incubator/wixmadefor/"
-  desc "Wix Madefor Text"
   desc "Typeface with wide geometric proportions, clean curves, and grotesk terminals"
   homepage "https://www.wix.com/typeface/madefor"
+  url "https://github.com/wix-incubator/wixmadefor/releases/download/3.100/wixmadefor-fonts.zip",
+       verified: "github.com/wix-incubator/wixmadefor/"
+  version "3.100"
+  sha256 "7fdbd012ca9e245d7c177a341bdbdf789521590e175322a9013c035981138f1c"
+
   def install
-    (share/"fonts").install Dir.glob("wixmadefor-fonts/fonts/variable/**/WixMadeforText\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("wixmadefor-fonts/fonts/variable/**/WixMadeforText-Italic\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/wixmadefor-fonts/fonts/variable/WixMadeforText[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/wixmadefor-fonts/fonts/variable/WixMadeforText-Italic[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
