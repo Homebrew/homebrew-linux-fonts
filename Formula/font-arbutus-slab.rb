b321d4b7339ed26307d7c1d741b041999817e0d1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-arbutus-slab updated

--- Formula/font-arbutus-slab.rb
+++ Formula/font-arbutus-slab.rb
@@ -1,12 +1,14 @@
 class FontArbutusSlab < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/arbutusslab/ArbutusSlab-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Arbutus Slab"
+  desc "Arbutus slab font"
   homepage "https://fonts.google.com/specimen/Arbutus+Slab"
+  head "https://github.com/google/fonts/raw/main/ofl/arbutusslab/ArbutusSlab-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ArbutusSlab-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
