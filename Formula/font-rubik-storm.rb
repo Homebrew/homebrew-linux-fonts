749ec1198e4c5e13ba509580af61fbcae0a66ec0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:18 +0000

    font-rubik-storm updated

--- Formula/font-rubik-storm.rb
+++ Formula/font-rubik-storm.rb
@@ -1,12 +1,14 @@
 class FontRubikStorm < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikstorm/RubikStorm-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik Storm"
+  desc "Rubik storm font"
   homepage "https://fonts.google.com/specimen/Rubik+Storm"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikstorm/RubikStorm-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikStorm-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
