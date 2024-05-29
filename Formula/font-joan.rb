6dbd075103d1b013890facb03d6f8122743243d2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-joan updated

--- Formula/font-joan.rb
+++ Formula/font-joan.rb
@@ -1,13 +1,14 @@
 class FontJoan < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/joan/Joan-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Joan"
   desc "Under development"
   homepage "https://fonts.google.com/specimen/Joan"
+  head "https://github.com/google/fonts/raw/main/ofl/joan/Joan-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Joan-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
