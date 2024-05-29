e6b167f3c133c9b9785028f1441ae244a3f2c048
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-thai-looped updated

--- Formula/font-noto-sans-thai-looped.rb
+++ Formula/font-noto-sans-thai-looped.rb
@@ -1,9 +1,13 @@
 class FontNotoSansThaiLooped < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/notosansthailooped"
-  desc "Noto Sans Thai Looped"
+  desc "Noto sans thai looped font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Thai+Looped"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/notosansthailooped"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-Thin.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-ExtraLight.ttf")[0]
@@ -11,10 +15,10 @@ class FontNotoSansThaiLooped < Formula
     (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansthailooped/./**/NotoSansThaiLooped-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
