5dedc9ae561d5ff973049fe89c4a1e31d7591b23
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-mahajani updated

--- Formula/font-noto-sans-mahajani.rb
+++ Formula/font-noto-sans-mahajani.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMahajani < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmahajani/NotoSansMahajani-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Mahajani"
+  desc "Noto sans mahajani font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Mahajani"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmahajani/NotoSansMahajani-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMahajani-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
