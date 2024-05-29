f6fb1e373fd13ce97c98425c37e84090922ae4b0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-merriweather-sans updated

--- Formula/font-merriweather-sans.rb
+++ Formula/font-merriweather-sans.rb
@@ -1,13 +1,17 @@
 class FontMerriweatherSans < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/merriweathersans"
-  desc "Merriweather Sans"
+  desc "Merriweather sans font"
   homepage "https://fonts.google.com/specimen/Merriweather+Sans"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/merriweathersans"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/merriweathersans/./**/MerriweatherSans-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/merriweathersans/./**/MerriweatherSans\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/merriweathersans/./**/MerriweatherSans-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/merriweathersans/./**/MerriweatherSans[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
