5da43a1b62f3ac62f876bf4da9eec7e825df4b88
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-sorts-mill-goudy updated

--- Formula/font-sorts-mill-goudy.rb
+++ Formula/font-sorts-mill-goudy.rb
@@ -1,13 +1,17 @@
 class FontSortsMillGoudy < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sortsmillgoudy"
-  desc "Sorts Mill Goudy"
+  desc "Sorts mill goudy font"
   homepage "https://fonts.google.com/specimen/Sorts+Mill+Goudy"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sortsmillgoudy"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sortsmillgoudy/./**/SortsMillGoudy-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sortsmillgoudy/./**/SortsMillGoudy-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
