c2681c695655220df22e4a615d6ccc5943317d42
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-lisu updated

--- Formula/font-noto-sans-lisu.rb
+++ Formula/font-noto-sans-lisu.rb
@@ -1,12 +1,14 @@
 class FontNotoSansLisu < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLisu-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Lisu"
+  desc "Noto sans lisu font"
   homepage "https://www.google.com/get/noto/#sans-lisu"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLisu-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansLisu-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
