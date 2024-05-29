ba1198e72c5efa861d98325fed48b114a29fa3c1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-suwannaphum updated

--- Formula/font-suwannaphum.rb
+++ Formula/font-suwannaphum.rb
@@ -1,7 +1,11 @@
 class FontSuwannaphum < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/suwannaphum"
-  desc "Suwannaphum"
+  desc "Suwannaphum font"
   homepage "https://fonts.google.com/specimen/Suwannaphum"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/suwannaphum"
+
   def install
     (share/"fonts").install Dir.glob("ofl/suwannaphum/./**/Suwannaphum-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/suwannaphum/./**/Suwannaphum-Bold.ttf")[0]
@@ -9,8 +13,8 @@ class FontSuwannaphum < Formula
     (share/"fonts").install Dir.glob("ofl/suwannaphum/./**/Suwannaphum-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/suwannaphum/./**/Suwannaphum-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
