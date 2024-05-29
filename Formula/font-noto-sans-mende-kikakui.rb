dc812e2b0ffa863ed97bc286eff87c343204d30e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-mende-kikakui updated

--- Formula/font-noto-sans-mende-kikakui.rb
+++ Formula/font-noto-sans-mende-kikakui.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMendeKikakui < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmendekikakui/NotoSansMendeKikakui-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Mende Kikakui"
+  desc "Noto sans mende kikakui font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Mende+Kikakui"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmendekikakui/NotoSansMendeKikakui-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMendeKikakui-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
