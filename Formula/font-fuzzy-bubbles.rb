47c67d3e7d8a6ca6359e89e2bdf05756b0b5e94d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:08 +0000

    font-fuzzy-bubbles updated

--- Formula/font-fuzzy-bubbles.rb
+++ Formula/font-fuzzy-bubbles.rb
@@ -1,14 +1,17 @@
 class FontFuzzyBubbles < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/fuzzybubbles"
-  desc "Fuzzy Bubbles"
   desc "Perfect for children's parties"
   homepage "https://fonts.google.com/specimen/Fuzzy+Bubbles"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/fuzzybubbles"
+
   def install
     (share/"fonts").install Dir.glob("ofl/fuzzybubbles/./**/FuzzyBubbles-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/fuzzybubbles/./**/FuzzyBubbles-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
