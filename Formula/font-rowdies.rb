20bab0050bdf8ffb68067b95beaf0dc36b20c844
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-rowdies updated

--- Formula/font-rowdies.rb
+++ Formula/font-rowdies.rb
@@ -1,14 +1,18 @@
 class FontRowdies < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rowdies"
-  desc "Rowdies"
+  desc "Rowdies font"
   homepage "https://fonts.google.com/specimen/Rowdies"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/rowdies"
+
   def install
     (share/"fonts").install Dir.glob("ofl/rowdies/./**/Rowdies-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/rowdies/./**/Rowdies-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/rowdies/./**/Rowdies-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
