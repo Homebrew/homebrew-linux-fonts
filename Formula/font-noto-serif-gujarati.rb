c598bdfce4b5df3382a758f80778dc3d9fed9bda
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-gujarati updated

--- Formula/font-noto-serif-gujarati.rb
+++ Formula/font-noto-serif-gujarati.rb
@@ -1,13 +1,15 @@
 class FontNotoSerifGujarati < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifGujarati-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Serif Gujarati"
+  desc "Noto serif gujarati font"
   homepage "https://www.google.com/get/noto/#serif-gujr"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifGujarati-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifGujarati-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifGujarati-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
