40af6b630f2f692fc6d554e6b380483bfaaa3135
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:20 +0000

    font-inconsolata-nerd-font updated

--- Formula/font-inconsolata-nerd-font.rb
+++ Formula/font-inconsolata-nerd-font.rb
@@ -1,10 +1,10 @@
 class FontInconsolataNerdFont < Formula
-  version "3.2.1"
-  sha256 "012aa0cac12137786832362022318556a155d2008bbfd198f72944b49cb8a6f8"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Inconsolata.zip"
-  desc "Inconsolata Nerd Font (Inconsolata)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Inconsolata.zip"
+  version "3.2.1"
+  sha256 "012aa0cac12137786832362022318556a155d2008bbfd198f72944b49cb8a6f8"
+
   def install
     (share/"fonts").install Dir.glob("./**/InconsolataNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/InconsolataNerdFont-Regular.ttf")[0]
@@ -13,8 +13,8 @@ class FontInconsolataNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/InconsolataNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/InconsolataNerdFontPropo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
