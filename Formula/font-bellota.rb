71c753cb65861e00c7f4f96a7de7f367959a8b11
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-bellota updated

--- Formula/font-bellota.rb
+++ Formula/font-bellota.rb
@@ -1,7 +1,11 @@
 class FontBellota < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/bellota"
-  desc "Bellota"
+  desc "Bellota font"
   homepage "https://fonts.google.com/specimen/Bellota"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/bellota"
+
   def install
     (share/"fonts").install Dir.glob("ofl/bellota/./**/Bellota-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/bellota/./**/Bellota-BoldItalic.ttf")[0]
@@ -10,8 +14,8 @@ class FontBellota < Formula
     (share/"fonts").install Dir.glob("ofl/bellota/./**/Bellota-LightItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/bellota/./**/Bellota-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
