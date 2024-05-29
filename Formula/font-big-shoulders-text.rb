c77afc303524e58b86fa7fb006edb502a2ce0006
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-big-shoulders-text updated

--- Formula/font-big-shoulders-text.rb
+++ Formula/font-big-shoulders-text.rb
@@ -1,12 +1,14 @@
 class FontBigShouldersText < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bigshoulderstext/BigShouldersText%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Big Shoulders Text"
+  desc "Big shoulders text font"
   homepage "https://fonts.google.com/specimen/Big+Shoulders+Text"
+  head "https://github.com/google/fonts/raw/main/ofl/bigshoulderstext/BigShouldersText%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BigShouldersText\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BigShouldersText[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
