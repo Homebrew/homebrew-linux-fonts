5f065954519a6bf6fb37fe21227218891478810b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:45 +0000

    font-balthazar updated

--- Formula/font-balthazar.rb
+++ Formula/font-balthazar.rb
@@ -1,12 +1,14 @@
 class FontBalthazar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/balthazar/Balthazar-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Balthazar"
+  desc "Balthazar font"
   homepage "https://fonts.google.com/specimen/Balthazar"
+  head "https://github.com/google/fonts/raw/main/ofl/balthazar/Balthazar-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Balthazar-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
