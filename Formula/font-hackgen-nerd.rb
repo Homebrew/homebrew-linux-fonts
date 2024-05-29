81a4d2c32262c8a5be83490577057e3d7dc9cdd3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-hackgen-nerd updated

--- Formula/font-hackgen-nerd.rb
+++ Formula/font-hackgen-nerd.rb
@@ -1,18 +1,18 @@
 class FontHackgenNerd < Formula
-  version "2.9.0"
-  sha256 "277cb874bbcf8a884e512bb2a01d62341d57286d8947057d652fab6488fc941c"
-  url "https://github.com/yuru7/HackGen/releases/download/v#{version}/HackGen_NF_v#{version}.zip"
-  desc "HackGenNerd"
   desc "Japanese programming font which is a composed of Hack and GenJyuu-Gothic"
   homepage "https://github.com/yuru7/HackGen"
+  url "https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip"
+  version "2.9.0"
+  sha256 "277cb874bbcf8a884e512bb2a01d62341d57286d8947057d652fab6488fc941c"
+
   def install
-    (share/"fonts").install Dir.glob("HackGen_NF_v#{version}/**/HackGen35ConsoleNF-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("HackGen_NF_v#{version}/**/HackGen35ConsoleNF-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("HackGen_NF_v#{version}/**/HackGenConsoleNF-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("HackGen_NF_v#{version}/**/HackGenConsoleNF-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/HackGen_NF_v2.9.0/HackGen35ConsoleNF-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/HackGen_NF_v2.9.0/HackGen35ConsoleNF-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/HackGen_NF_v2.9.0/HackGenConsoleNF-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/HackGen_NF_v2.9.0/HackGenConsoleNF-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
