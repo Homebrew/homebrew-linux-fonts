dfcec3710fb4ff5ca42c50a1f87fb445a021a055
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:40 +0000

    font-metamorphous updated

--- Formula/font-metamorphous.rb
+++ Formula/font-metamorphous.rb
@@ -1,12 +1,14 @@
 class FontMetamorphous < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/metamorphous/Metamorphous-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Metamorphous"
+  desc "Metamorphous font"
   homepage "https://fonts.google.com/specimen/Metamorphous"
+  head "https://github.com/google/fonts/raw/main/ofl/metamorphous/Metamorphous-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Metamorphous-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
