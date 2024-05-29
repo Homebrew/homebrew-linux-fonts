6a5dd78d1c35865c48603d98fc6e8c7ed6360b5f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-montserrat updated

--- Formula/font-montserrat.rb
+++ Formula/font-montserrat.rb
@@ -1,13 +1,17 @@
 class FontMontserrat < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/montserrat"
-  desc "Montserrat"
+  desc "Montserrat font"
   homepage "https://fonts.google.com/specimen/Montserrat"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/montserrat"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/montserrat/./**/Montserrat-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/montserrat/./**/Montserrat\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/montserrat/./**/Montserrat-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/montserrat/./**/Montserrat[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
