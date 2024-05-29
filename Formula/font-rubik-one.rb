c078e0a7db63cc5f4547911179fb8d527e518d53
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:17 +0000

    font-rubik-one updated

--- Formula/font-rubik-one.rb
+++ Formula/font-rubik-one.rb
@@ -1,12 +1,14 @@
 class FontRubikOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rubikone/RubikOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rubik One"
+  desc "Rubik one font"
   homepage "https://fonts.google.com/specimen/Rubik+One"
+  head "https://github.com/google/fonts/raw/main/ofl/rubikone/RubikOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RubikOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
