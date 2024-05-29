1a3d9785c79b9a4570c678e1c4f731b05ee8af14
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-symbols-only-nerd-font updated

--- Formula/font-symbols-only-nerd-font.rb
+++ Formula/font-symbols-only-nerd-font.rb
@@ -1,16 +1,16 @@
 class FontSymbolsOnlyNerdFont < Formula
-  version "3.2.1"
-  sha256 "bc59c2ea74d022a6262ff9e372fde5c36cd5ae3f82a567941489ecfab4f03d66"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/NerdFontsSymbolsOnly.zip"
-  desc "Symbols Nerd Font (Symbols Only)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/NerdFontsSymbolsOnly.zip"
+  version "3.2.1"
+  sha256 "bc59c2ea74d022a6262ff9e372fde5c36cd5ae3f82a567941489ecfab4f03d66"
+
   def install
     (share/"fonts").install Dir.glob("./**/SymbolsNerdFont-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/SymbolsNerdFontMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
