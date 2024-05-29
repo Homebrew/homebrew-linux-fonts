10899c36a3a0ee6029b115a3b7c5452feb3a1a7e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-dogra updated

--- Formula/font-noto-serif-dogra.rb
+++ Formula/font-noto-serif-dogra.rb
@@ -1,12 +1,14 @@
 class FontNotoSerifDogra < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifdogra/NotoSerifDogra-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Dogra"
+  desc "Noto serif dogra font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Dogra"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifdogra/NotoSerifDogra-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifDogra-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
