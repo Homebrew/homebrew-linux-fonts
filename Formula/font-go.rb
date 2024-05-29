de83cf0388781b0f66c00bf1da655fd415fbabbd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-go updated

--- Formula/font-go.rb
+++ Formula/font-go.rb
@@ -1,9 +1,10 @@
 class FontGo < Formula
-  head "https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz"
-  desc "Go"
+  desc "Go font"
   homepage "https://go.googlesource.com/image/+/master/font/gofont/ttfs/README"
+  head "https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Go-Bold-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Go-Smallcaps.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Medium-Italic.ttf")[0]
@@ -14,10 +15,10 @@ class FontGo < Formula
     (share/"fonts").install Dir.glob("./**/Go-Mono.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Smallcaps-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Go-Smallcaps.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Go-Bold-Italic.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
