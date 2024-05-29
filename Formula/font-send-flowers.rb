4b8b3f154c083a2446eaec688a8144b881c49436
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:21 +0000

    font-send-flowers updated

--- Formula/font-send-flowers.rb
+++ Formula/font-send-flowers.rb
@@ -1,13 +1,14 @@
 class FontSendFlowers < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sendflowers/SendFlowers-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Send Flowers"
   desc "Appropriate for an older audience"
   homepage "https://fonts.google.com/specimen/Send+Flowers"
+  head "https://github.com/google/fonts/raw/main/ofl/sendflowers/SendFlowers-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SendFlowers-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
