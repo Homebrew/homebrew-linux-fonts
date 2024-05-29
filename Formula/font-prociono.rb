30e69b1641813c1c8b4e984ac018baa1bdad6f46
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:10 +0000

    font-prociono updated

--- Formula/font-prociono.rb
+++ Formula/font-prociono.rb
@@ -1,12 +1,14 @@
 class FontProciono < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/prociono/Prociono-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Prociono"
+  desc "Prociono font"
   homepage "https://fonts.google.com/specimen/Prociono"
+  head "https://github.com/google/fonts/raw/main/ofl/prociono/Prociono-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Prociono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
