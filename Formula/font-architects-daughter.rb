0feb83f24d2e1aa2b85af98aa0ed48274043b49a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-architects-daughter updated

--- Formula/font-architects-daughter.rb
+++ Formula/font-architects-daughter.rb
@@ -1,12 +1,14 @@
 class FontArchitectsDaughter < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/architectsdaughter/ArchitectsDaughter-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Architects Daughter"
+  desc "Architects daughter font"
   homepage "https://fonts.google.com/specimen/Architects+Daughter"
+  head "https://github.com/google/fonts/raw/main/ofl/architectsdaughter/ArchitectsDaughter-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ArchitectsDaughter-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
