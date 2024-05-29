1dc589b8c0729d8f703d45768f81083e99e8e3cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-multani updated

--- Formula/font-noto-sans-multani.rb
+++ Formula/font-noto-sans-multani.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMultani < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmultani/NotoSansMultani-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Multani"
+  desc "Noto sans multani font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Multani"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmultani/NotoSansMultani-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMultani-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
