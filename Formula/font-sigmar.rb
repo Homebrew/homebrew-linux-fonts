5bcab9c1523770a4cb5d12c200304b5683d5ab54
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-sigmar updated

--- Formula/font-sigmar.rb
+++ Formula/font-sigmar.rb
@@ -1,12 +1,14 @@
 class FontSigmar < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sigmar/Sigmar-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sigmar"
+  desc "Sigmar font"
   homepage "https://fonts.google.com/specimen/Sigmar"
+  head "https://github.com/google/fonts/raw/main/ofl/sigmar/Sigmar-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Sigmar-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
