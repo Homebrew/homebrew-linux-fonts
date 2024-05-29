abe60d054387809cb46d767116cef17c0d92db23
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:47 +0000

    font-bitter-ht updated

--- Formula/font-bitter-ht.rb
+++ Formula/font-bitter-ht.rb
@@ -1,10 +1,10 @@
 class FontBitterHt < Formula
-  head "https://www.huertatipografica.com/free_download/144"
-  desc "Bitter ht"
   desc "Slab-serif typeface optimized for e-ink screens"
   homepage "https://www.huertatipografica.com/fonts/bitter-ht"
+  head "https://www.huertatipografica.com/free_download/144"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BitterPro-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BitterPro-ThinItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitterPro-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitterPro-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitterPro-BoldItalic.ttf")[0]
@@ -21,10 +21,10 @@ class FontBitterHt < Formula
     (share/"fonts").install Dir.glob("./**/BitterPro-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitterPro-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitterPro-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/BitterPro-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BitterPro-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
