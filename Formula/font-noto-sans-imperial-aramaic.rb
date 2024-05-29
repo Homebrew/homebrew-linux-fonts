02013560b518b509db107a4605720adfaf5870a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:51 +0000

    font-noto-sans-imperial-aramaic updated

--- Formula/font-noto-sans-imperial-aramaic.rb
+++ Formula/font-noto-sans-imperial-aramaic.rb
@@ -1,12 +1,14 @@
 class FontNotoSansImperialAramaic < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansImperialAramaic-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Imperial Aramaic"
+  desc "Noto sans imperial aramaic font"
   homepage "https://www.google.com/get/noto/#sans-armi"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansImperialAramaic-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansImperialAramaic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
