c6dc633211599719cfca9ad2576e7e14d7e3d727
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-optician-sans updated

--- Formula/font-optician-sans.rb
+++ Formula/font-optician-sans.rb
@@ -1,12 +1,14 @@
 class FontOpticianSans < Formula
-  head "https://github.com/anewtypeofinterference/Optician-Sans/raw/master/OpenType-PS/Optician-Sans.otf", verified: "github.com/anewtypeofinterference/Optician-Sans/"
-  desc "Optician Sans"
+  desc "Optician sans font"
   homepage "https://optician-sans.com/"
+  head "https://github.com/anewtypeofinterference/Optician-Sans/raw/master/OpenType-PS/Optician-Sans.otf",
+       verified: "github.com/anewtypeofinterference/Optician-Sans/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Optician-Sans.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
