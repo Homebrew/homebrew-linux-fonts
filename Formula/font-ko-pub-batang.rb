a15fce2c8cd5f222bfe262cae1829a0bbef7eeb7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-ko-pub-batang updated

--- Formula/font-ko-pub-batang.rb
+++ Formula/font-ko-pub-batang.rb
@@ -1,14 +1,18 @@
 class FontKoPubBatang < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kopubbatang"
-  desc "Ko Pub Batang"
+  desc "Ko pub batang font"
   homepage "https://fonts.google.com/earlyaccess"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kopubbatang"
+
   def install
     (share/"fonts").install Dir.glob("ofl/kopubbatang/./**/KoPubBatang-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kopubbatang/./**/KoPubBatang-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kopubbatang/./**/KoPubBatang-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
