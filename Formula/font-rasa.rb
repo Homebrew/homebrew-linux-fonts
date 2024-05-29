f480cb4c454c695cc5ba40af1546ed94e0418f19
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-rasa updated

--- Formula/font-rasa.rb
+++ Formula/font-rasa.rb
@@ -1,13 +1,17 @@
 class FontRasa < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/rasa"
-  desc "Rasa"
+  desc "Rasa font"
   homepage "https://fonts.google.com/specimen/Rasa"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/rasa"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/rasa/./**/Rasa-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/rasa/./**/Rasa\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/rasa/./**/Rasa-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/rasa/./**/Rasa[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
