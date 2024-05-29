3e663f69d9525e1166ef90b4dc0d6e7a92f5dc80
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:15 +0000

    font-righteous updated

--- Formula/font-righteous.rb
+++ Formula/font-righteous.rb
@@ -1,12 +1,14 @@
 class FontRighteous < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/righteous/Righteous-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Righteous"
+  desc "Righteous font"
   homepage "https://fonts.google.com/specimen/Righteous"
+  head "https://github.com/google/fonts/raw/main/ofl/righteous/Righteous-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Righteous-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
