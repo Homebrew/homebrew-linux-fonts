9b9fc0e26e164df76f2b58cb528b35a4922a7067
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-neonderthaw updated

--- Formula/font-neonderthaw.rb
+++ Formula/font-neonderthaw.rb
@@ -1,13 +1,14 @@
 class FontNeonderthaw < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/neonderthaw/Neonderthaw-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Neonderthaw"
   desc "Single weight script that simulates neon"
   homepage "https://fonts.google.com/specimen/Neonderthaw"
+  head "https://github.com/google/fonts/raw/main/ofl/neonderthaw/Neonderthaw-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Neonderthaw-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
