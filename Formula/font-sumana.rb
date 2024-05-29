3e0d8e185d2cb4e64bda46cba8cab347bb183d50
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-sumana updated

--- Formula/font-sumana.rb
+++ Formula/font-sumana.rb
@@ -1,13 +1,17 @@
 class FontSumana < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sumana"
-  desc "Sumana"
+  desc "Sumana font"
   homepage "https://fonts.google.com/specimen/Sumana"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sumana"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sumana/./**/Sumana-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sumana/./**/Sumana-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
