6a82e996cbe2c252e6f22357d07a2cd6cf0b7f55
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:39 +0000

    font-meslo-for-powerline updated

--- Formula/font-meslo-for-powerline.rb
+++ Formula/font-meslo-for-powerline.rb
@@ -1,19 +1,20 @@
 class FontMesloForPowerline < Formula
+  desc "Meslo for powerline font"
+  homepage "https://github.com/powerline/fonts"
+  url "https://github.com/powerline/fonts/archive/2015-12-04.zip"
   version "2015-12-04"
   sha256 "b6673e0e28441cc04065f8e5c79960aeaa220dae52351852b37e6cf3a03bb8af"
-  url "https://github.com/powerline/fonts/archive/#{version}.zip"
-  desc "Meslo for Powerline"
-  homepage "https://github.com/powerline/fonts"
+
   def install
-    (share/"fonts").install Dir.glob("fonts-#{version}/Meslo/**/Meslo LG L DZ Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("fonts-#{version}/Meslo/**/Meslo LG L Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("fonts-#{version}/Meslo/**/Meslo LG M DZ Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("fonts-#{version}/Meslo/**/Meslo LG M Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("fonts-#{version}/Meslo/**/Meslo LG S DZ Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("fonts-#{version}/Meslo/**/Meslo LG S Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG L DZ Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG L Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG M DZ Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG M Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG S DZ Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG S Regular for Powerline.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
