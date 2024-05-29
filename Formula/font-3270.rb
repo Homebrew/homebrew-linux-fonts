a2e522722378363af353bd7bc912d6c208a35211
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-3270 updated

--- Formula/font-3270.rb
+++ Formula/font-3270.rb
@@ -1,10 +1,10 @@
 class Font3270 < Formula
-  version "3.0.1,d916271"
-  sha256 "623fb815b16d6c4940b5014a21c5474ef6cddb02c325d03f153341b676b4cffa"
-  url "https://github.com/rbanffy/3270font/releases/download/v#{version.to_s.sub(/,.*/, "")}/3270_fonts_#{version.to_s.sub(/.*,/, "")}.zip"
-  desc "IBM 3270"
   desc "Derived from the x3270 font"
   homepage "https://github.com/rbanffy/3270font"
+  url "https://github.com/rbanffy/3270font/releases/download/v3.0.1/3270_fonts_d916271.zip"
+  version "3.0.1,d916271"
+  sha256 "623fb815b16d6c4940b5014a21c5474ef6cddb02c325d03f153341b676b4cffa"
+
   def install
     (share/"fonts").install Dir.glob("./**/3270-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/3270-Regular.ttf")[0]
@@ -13,8 +13,8 @@ class Font3270 < Formula
     (share/"fonts").install Dir.glob("./**/3270SemiCondensed-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/3270SemiCondensed-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
