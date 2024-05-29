f34cbf056a73b5fc709ddd857ec829be1c842948
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-pau-cin-hau updated

--- Formula/font-noto-sans-pau-cin-hau.rb
+++ Formula/font-noto-sans-pau-cin-hau.rb
@@ -1,12 +1,14 @@
 class FontNotoSansPauCinHau < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanspaucinhau/NotoSansPauCinHau-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Pau Cin Hau"
+  desc "Noto sans pau cin hau font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Pau+Cin+Hau"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanspaucinhau/NotoSansPauCinHau-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansPauCinHau-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
