2ed2df87e4eb27d110a64c5203827f38c9ae65f6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:59 +0000

    font-noto-serif-kannada updated

--- Formula/font-noto-serif-kannada.rb
+++ Formula/font-noto-serif-kannada.rb
@@ -1,13 +1,15 @@
 class FontNotoSerifKannada < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifKannada-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Serif Kannada"
+  desc "Noto serif kannada font"
   homepage "https://www.google.com/get/noto/#serif-knda"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifKannada-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifKannada-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSerifKannada-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
