68d5060a5bcf3ff7fc1ad4c3ec63e78fafd1fdb6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:05 +0000

    font-petemoss updated

--- Formula/font-petemoss.rb
+++ Formula/font-petemoss.rb
@@ -1,13 +1,14 @@
 class FontPetemoss < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/petemoss/Petemoss-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Petemoss"
   desc "Inspired by the forms created using a pentel color brush"
   homepage "https://fonts.google.com/specimen/Petemoss"
+  head "https://github.com/google/fonts/raw/main/ofl/petemoss/Petemoss-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Petemoss-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
