8b11c64e2812fe44dc6dd48867be7dfb1ae5224b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-gidole updated

--- Formula/font-gidole.rb
+++ Formula/font-gidole.rb
@@ -1,13 +1,15 @@
 class FontGidole < Formula
-  head "https://github.com/gidole/Gidole-Typefaces/raw/master/gidole.zip", verified: "github.com/gidole/"
-  desc "Gidole"
+  desc "Gidole font"
   homepage "https://gidole.github.io/"
+  head "https://github.com/gidole/Gidole-Typefaces/raw/master/gidole.zip",
+       verified: "github.com/gidole/"
+
   def install
-    (share/"fonts").install Dir.glob("GidoleFont/**/Gidole-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("GidoleFont/**/Gidolinya-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GidoleFont/Gidole-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GidoleFont/Gidolinya-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
