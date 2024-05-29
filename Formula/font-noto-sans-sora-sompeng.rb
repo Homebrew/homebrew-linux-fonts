bc520165e27a93ec57e80223758bf68217b34e69
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:56 +0000

    font-noto-sans-sora-sompeng updated

--- Formula/font-noto-sans-sora-sompeng.rb
+++ Formula/font-noto-sans-sora-sompeng.rb
@@ -1,12 +1,14 @@
 class FontNotoSansSoraSompeng < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanssorasompeng/NotoSansSoraSompeng%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Sora Sompeng"
+  desc "Noto sans sora sompeng font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Sora+Sompeng"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanssorasompeng/NotoSansSoraSompeng%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansSoraSompeng\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansSoraSompeng[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
