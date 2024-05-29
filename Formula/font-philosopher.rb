0b49eeb962b1f11dd3634616799c22056ee442c5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:06 +0000

    font-philosopher updated

--- Formula/font-philosopher.rb
+++ Formula/font-philosopher.rb
@@ -1,15 +1,19 @@
 class FontPhilosopher < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/philosopher"
-  desc "Philosopher"
+  desc "Philosopher font"
   homepage "https://fonts.google.com/specimen/Philosopher"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/philosopher"
+
   def install
     (share/"fonts").install Dir.glob("ofl/philosopher/./**/Philosopher-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/philosopher/./**/Philosopher-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/philosopher/./**/Philosopher-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/philosopher/./**/Philosopher-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
