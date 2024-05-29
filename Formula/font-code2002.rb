a49cd90896e0cd81863e2669046148e35306583c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-code2002 updated

--- Formula/font-code2002.rb
+++ Formula/font-code2002.rb
@@ -1,12 +1,13 @@
 class FontCode2002 < Formula
-  head "https://www.ffonts.net/Code2002.font.zip"
-  desc "Code2002"
+  desc "Code2002 font"
   homepage "https://www.ffonts.net/Code2002.font.download"
+  head "https://www.ffonts.net/Code2002.font.zip"
+
   def install
     (share/"fonts").install Dir.glob("./**/CODE2002.TTF")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
