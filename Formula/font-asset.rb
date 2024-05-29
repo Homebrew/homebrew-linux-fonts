72150a72d589c33e337431e54d2a3264bba8dc01
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:42 +0000

    font-asset updated

--- Formula/font-asset.rb
+++ Formula/font-asset.rb
@@ -1,12 +1,14 @@
 class FontAsset < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/asset/Asset-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Asset"
+  desc "Asset font"
   homepage "https://fonts.google.com/specimen/Asset"
+  head "https://github.com/google/fonts/raw/main/ofl/asset/Asset-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Asset-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
