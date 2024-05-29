76d1cc3a6c01b255cb83411966ac6dba29b3553c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:50 +0000

    font-noto-sans-gurmukhi updated

--- Formula/font-noto-sans-gurmukhi.rb
+++ Formula/font-noto-sans-gurmukhi.rb
@@ -1,13 +1,15 @@
 class FontNotoSansGurmukhi < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGurmukhi-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Gurmukhi"
+  desc "Noto sans gurmukhi font"
   homepage "https://www.google.com/get/noto/#sans-guru"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGurmukhi-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansGurmukhi-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansGurmukhi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
