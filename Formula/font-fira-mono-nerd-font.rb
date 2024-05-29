a9ebe261b0d9ae8b4385deae189cd0cec5b309e4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:06 +0000

    font-fira-mono-nerd-font updated

--- Formula/font-fira-mono-nerd-font.rb
+++ Formula/font-fira-mono-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontFiraMonoNerdFont < Formula
-  version "3.2.1"
-  sha256 "9f739a71d5b687325127b6076292bab1597efba188603b4f8cfcdfdf82e9e6fb"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraMono.zip"
-  desc "FiraMono Nerd Font (Fira)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraMono.zip"
+  version "3.2.1"
+  sha256 "9f739a71d5b687325127b6076292bab1597efba188603b4f8cfcdfdf82e9e6fb"
+
   def install
-    (share/"fonts").install Dir.glob("./**/FiraMonoNerdFont-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontPropo-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFont-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFont-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontMono-Bold.otf")[0]
@@ -14,10 +14,10 @@ class FontFiraMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontMono-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontPropo-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontPropo-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontPropo-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraMonoNerdFont-Bold.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
