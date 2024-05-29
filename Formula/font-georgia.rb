6e722c2fe3a1e4dadfa35981cb033fb3fe7def47
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:11 +0000

    font-georgia updated

--- Formula/font-georgia.rb
+++ Formula/font-georgia.rb
@@ -1,15 +1,16 @@
 class FontGeorgia < Formula
-  head "https://downloads.sourceforge.net/corefonts/georgi32.exe"
-  desc "Georgia"
+  desc "Georgia font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
+  head "https://downloads.sourceforge.net/corefonts/georgi32.exe"
+
   def install
     (share/"fonts").install Dir.glob("./**/Georgiaz.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Georgiab.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Georgiai.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Georgia.TTF")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
