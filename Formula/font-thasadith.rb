0f1fa51b4394b332f151ad83bb60eeb9b425144e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-thasadith updated

--- Formula/font-thasadith.rb
+++ Formula/font-thasadith.rb
@@ -1,15 +1,19 @@
 class FontThasadith < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/thasadith"
-  desc "Thasadith"
+  desc "Thasadith font"
   homepage "https://fonts.google.com/specimen/Thasadith"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/thasadith"
+
   def install
     (share/"fonts").install Dir.glob("ofl/thasadith/./**/Thasadith-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/thasadith/./**/Thasadith-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/thasadith/./**/Thasadith-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/thasadith/./**/Thasadith-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
