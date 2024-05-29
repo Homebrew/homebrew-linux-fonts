4738ece9773897e2a13b6c6bd130299774ad3069
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:42 +0000

    font-monoid updated

--- Formula/font-monoid.rb
+++ Formula/font-monoid.rb
@@ -1,22 +1,19 @@
 class FontMonoid < Formula
-  version "0.61"
-  url "https://github.com/larsenwork/monoid/blob/release/Monoid.zip?raw=true", verified: "github.com/larsenwork/monoid/"
-  desc "Monoid"
+  desc "Monoid font"
   homepage "https://larsenwork.com/monoid/"
+  url "https://github.com/larsenwork/monoid/blob/release/Monoid.zip?raw=true",
+       verified: "github.com/larsenwork/monoid/"
+  version "0.61"
+  sha256 :no_check
+
   def install
     (share/"fonts").install Dir.glob("./**/Monoid-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Monoid-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Monoid-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Monoid-Retina.ttf")[0]
   end
-  def creavat; <<~EOS
-    #{token} only installs the Normal Weight, Medium LineHeight, with Ligatures variant.
-    To get other styles, please tap the sscotth/homebrew-monoid repo
-      brew tap sscotth/monoid
-  EOS
-  end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
