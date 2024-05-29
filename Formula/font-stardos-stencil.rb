68843534111dc9ed3d93e4a9220ef64e608bc8e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:26 +0000

    font-stardos-stencil updated

--- Formula/font-stardos-stencil.rb
+++ Formula/font-stardos-stencil.rb
@@ -1,13 +1,17 @@
 class FontStardosStencil < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/stardosstencil"
-  desc "Stardos Stencil"
+  desc "Stardos stencil font"
   homepage "https://fonts.google.com/specimen/Stardos+Stencil"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/stardosstencil"
+
   def install
     (share/"fonts").install Dir.glob("ofl/stardosstencil/./**/StardosStencil-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/stardosstencil/./**/StardosStencil-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
