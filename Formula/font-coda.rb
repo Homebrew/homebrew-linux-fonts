ddb2109a1c3f54bad9f94e6540854aacdc9e564e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-coda updated

--- Formula/font-coda.rb
+++ Formula/font-coda.rb
@@ -1,13 +1,17 @@
 class FontCoda < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/coda"
-  desc "Coda"
+  desc "Coda font"
   homepage "https://fonts.google.com/specimen/Coda"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/coda"
+
   def install
     (share/"fonts").install Dir.glob("ofl/coda/./**/Coda-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/coda/./**/Coda-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
