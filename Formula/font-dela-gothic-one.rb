947611acd7ffead3d86e529a597adfd5cb7907ee
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:00 +0000

    font-dela-gothic-one updated

--- Formula/font-dela-gothic-one.rb
+++ Formula/font-dela-gothic-one.rb
@@ -1,12 +1,14 @@
 class FontDelaGothicOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/delagothicone/DelaGothicOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Dela Gothic One"
+  desc "Dela gothic one font"
   homepage "https://fonts.google.com/specimen/Dela+Gothic+One"
+  head "https://github.com/google/fonts/raw/main/ofl/delagothicone/DelaGothicOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/DelaGothicOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
