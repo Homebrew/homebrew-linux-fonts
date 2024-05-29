887dccdb0449ebeba86a769366839f178ca5ebe3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:13 +0000

    font-go-mono-nerd-font updated

--- Formula/font-go-mono-nerd-font.rb
+++ Formula/font-go-mono-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontGoMonoNerdFont < Formula
-  version "3.2.1"
-  sha256 "1440419676af8287b1e707ec1d2846880b6745524099f566b34ba7facd28d84a"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Go-Mono.zip"
-  desc "GoMono Nerd Font (Go Mono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Go-Mono.zip"
+  version "3.2.1"
+  sha256 "1440419676af8287b1e707ec1d2846880b6745524099f566b34ba7facd28d84a"
+
   def install
-    (share/"fonts").install Dir.glob("./**/GoMonoNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GoMonoNerdFontPropo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GoMonoNerdFont-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GoMonoNerdFont-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GoMonoNerdFont-Regular.ttf")[0]
@@ -17,10 +17,10 @@ class FontGoMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/GoMonoNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GoMonoNerdFontPropo-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GoMonoNerdFontPropo-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GoMonoNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GoMonoNerdFont-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
