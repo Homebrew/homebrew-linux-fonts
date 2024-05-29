e08724dbf3b9fd12881e0917d72e24eb0039ed1e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:34 +0000

    font-vampiro-one updated

--- Formula/font-vampiro-one.rb
+++ Formula/font-vampiro-one.rb
@@ -1,12 +1,14 @@
 class FontVampiroOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/vampiroone/VampiroOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Vampiro One"
+  desc "Vampiro one font"
   homepage "https://fonts.google.com/specimen/Vampiro+One"
+  head "https://github.com/google/fonts/raw/main/ofl/vampiroone/VampiroOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/VampiroOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
