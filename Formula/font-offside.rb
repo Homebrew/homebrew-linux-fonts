ebc1fac92ba260eba2cdceef5c8202fb24efa538
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-offside updated

--- Formula/font-offside.rb
+++ Formula/font-offside.rb
@@ -1,12 +1,14 @@
 class FontOffside < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/offside/Offside-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Offside"
+  desc "Offside font"
   homepage "https://fonts.google.com/specimen/Offside"
+  head "https://github.com/google/fonts/raw/main/ofl/offside/Offside-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Offside-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
