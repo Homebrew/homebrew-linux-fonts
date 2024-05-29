531d4ef7c49fc3c7d24449a4b2280a2d920e14a0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:19 +0000

    font-im-fell-french-canon updated

--- Formula/font-im-fell-french-canon.rb
+++ Formula/font-im-fell-french-canon.rb
@@ -1,13 +1,17 @@
 class FontImFellFrenchCanon < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/imfellfrenchcanon"
-  desc "IM Fell French Canon"
+  desc "Im fell french canon font"
   homepage "https://fonts.google.com/specimen/IM+Fell+French+Canon"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/imfellfrenchcanon"
+
   def install
     (share/"fonts").install Dir.glob("ofl/imfellfrenchcanon/./**/IMFeFCit28P.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/imfellfrenchcanon/./**/IMFeFCrm28P.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
