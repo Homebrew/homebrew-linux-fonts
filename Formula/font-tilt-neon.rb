3f841cd42682a39a768b3f782fb93a3befe36247
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:30 +0000

    font-tilt-neon updated

--- Formula/font-tilt-neon.rb
+++ Formula/font-tilt-neon.rb
@@ -1,12 +1,14 @@
 class FontTiltNeon < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/tiltneon/TiltNeon%5BXROT%2CYROT%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Tilt Neon"
+  desc "Tilt neon font"
   homepage "https://fonts.google.com/specimen/Tilt+Neon"
+  head "https://github.com/google/fonts/raw/main/ofl/tiltneon/TiltNeon%5BXROT%2CYROT%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/TiltNeon\\[XROT,YROT\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TiltNeon[XROT,YROT].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
