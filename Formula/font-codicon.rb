64f236af8034166f9ce6aba99510759a66bf2611
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:55 +0000

    font-codicon updated

--- Formula/font-codicon.rb
+++ Formula/font-codicon.rb
@@ -1,15 +1,15 @@
 class FontCodicon < Formula
-  version "0.0.36"
-  sha256 "360527575395ad70f940eff1e43eb7c2ec64f4e69f89dc5861b73420d2a578f0"
-  url "https://github.com/microsoft/vscode-codicons/releases/download/#{version}/codicon.ttf"
-  desc "Codicon"
   desc "Icon font for Visual Studio Code"
   homepage "https://github.com/microsoft/vscode-codicons"
+  url "https://github.com/microsoft/vscode-codicons/releases/download/0.0.36/codicon.ttf"
+  version "0.0.36"
+  sha256 "360527575395ad70f940eff1e43eb7c2ec64f4e69f89dc5861b73420d2a578f0"
+
   def install
     (share/"fonts").install Dir.glob("./**/codicon.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
