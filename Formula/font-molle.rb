8810b30e9c29938ebb4900e03fcbd32fd0555bdb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:42 +0000

    font-molle updated

--- Formula/font-molle.rb
+++ Formula/font-molle.rb
@@ -1,12 +1,14 @@
 class FontMolle < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/molle/Molle-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Molle"
+  desc "Molle font"
   homepage "https://fonts.google.com/specimen/Molle"
+  head "https://github.com/google/fonts/raw/main/ofl/molle/Molle-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Molle-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
