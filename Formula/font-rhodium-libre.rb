12386ef0c52bd4d332374667c5ca145c8e77d3b4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-rhodium-libre updated

--- Formula/font-rhodium-libre.rb
+++ Formula/font-rhodium-libre.rb
@@ -1,12 +1,14 @@
 class FontRhodiumLibre < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/rhodiumlibre/RhodiumLibre-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Rhodium Libre"
+  desc "Rhodium libre font"
   homepage "https://fonts.google.com/specimen/Rhodium+Libre"
+  head "https://github.com/google/fonts/raw/main/ofl/rhodiumlibre/RhodiumLibre-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/RhodiumLibre-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
