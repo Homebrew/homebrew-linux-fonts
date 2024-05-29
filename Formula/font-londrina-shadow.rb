cb09f7c1e6a030f105dd6220d39be1e37636b93c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-londrina-shadow updated

--- Formula/font-londrina-shadow.rb
+++ Formula/font-londrina-shadow.rb
@@ -1,12 +1,14 @@
 class FontLondrinaShadow < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/londrinashadow/LondrinaShadow-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Londrina Shadow"
+  desc "Londrina shadow font"
   homepage "https://fonts.google.com/specimen/Londrina+Shadow"
+  head "https://github.com/google/fonts/raw/main/ofl/londrinashadow/LondrinaShadow-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LondrinaShadow-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
