40815ecdb51c0f3cbf1f39406f82577d3289351d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:04 +0000

    font-oxygen updated

--- Formula/font-oxygen.rb
+++ Formula/font-oxygen.rb
@@ -1,14 +1,18 @@
 class FontOxygen < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/oxygen"
-  desc "Oxygen"
+  desc "Oxygen font"
   homepage "https://fonts.google.com/specimen/Oxygen"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/oxygen"
+
   def install
     (share/"fonts").install Dir.glob("ofl/oxygen/./**/Oxygen-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/oxygen/./**/Oxygen-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/oxygen/./**/Oxygen-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
