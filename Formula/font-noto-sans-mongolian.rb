30d1ca1c78f4226a30f478e0033f8b0aafd822de
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-mongolian updated

--- Formula/font-noto-sans-mongolian.rb
+++ Formula/font-noto-sans-mongolian.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMongolian < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMongolian-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Mongolian"
+  desc "Noto sans mongolian font"
   homepage "https://www.google.com/get/noto/#sans-mong"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMongolian-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMongolian-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
