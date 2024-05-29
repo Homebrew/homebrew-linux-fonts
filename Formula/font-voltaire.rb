8f7bce005712f4e0ea0cd1c208cfce319685ce51
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:35 +0000

    font-voltaire updated

--- Formula/font-voltaire.rb
+++ Formula/font-voltaire.rb
@@ -1,12 +1,14 @@
 class FontVoltaire < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/voltaire/Voltaire-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Voltaire"
+  desc "Voltaire font"
   homepage "https://fonts.google.com/specimen/Voltaire"
+  head "https://github.com/google/fonts/raw/main/ofl/voltaire/Voltaire-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Voltaire-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
