bed2e979a16d80b49b13288ce03258386994480f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-mulish updated

--- Formula/font-mulish.rb
+++ Formula/font-mulish.rb
@@ -1,14 +1,17 @@
 class FontMulish < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/mulish"
-  desc "Mulish"
   desc "Minimalist sans serif typeface, designed for both display and text typography"
   homepage "https://fonts.google.com/specimen/Mulish"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/mulish"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/mulish/./**/Mulish-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/mulish/./**/Mulish\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/mulish/./**/Mulish-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/mulish/./**/Mulish[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
