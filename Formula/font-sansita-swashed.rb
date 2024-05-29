da26ed7869e34c435368eb3d7fe4023a3b514919
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-sansita-swashed updated

--- Formula/font-sansita-swashed.rb
+++ Formula/font-sansita-swashed.rb
@@ -1,13 +1,14 @@
 class FontSansitaSwashed < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sansitaswashed/SansitaSwashed%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Sansita Swashed"
   desc "Beautiful display font in vintage form"
   homepage "https://fonts.google.com/specimen/Sansita+Swashed"
+  head "https://github.com/google/fonts/raw/main/ofl/sansitaswashed/SansitaSwashed%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/SansitaSwashed\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/SansitaSwashed[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
