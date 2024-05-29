f2d9ba63b927a150a259b00f52838410a7c5b711
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-alkalami updated

--- Formula/font-alkalami.rb
+++ Formula/font-alkalami.rb
@@ -1,13 +1,14 @@
 class FontAlkalami < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alkalami/Alkalami-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Alkalami"
   desc "Very black, solid rectangle"
   homepage "https://fonts.google.com/specimen/Alkalami"
+  head "https://github.com/google/fonts/raw/main/ofl/alkalami/Alkalami-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Alkalami-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
