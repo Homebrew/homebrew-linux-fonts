87cffb0c50431ef9eb56f340ca17ba9b9169b14d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-martian-mono-nerd-font updated

--- Formula/font-martian-mono-nerd-font.rb
+++ Formula/font-martian-mono-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontMartianMonoNerdFont < Formula
-  version "3.2.1"
-  sha256 "a0bb5ab838bb2dcbcc2a7824e6a73505e8dc8f95f48183e056d82790108c32ea"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/MartianMono.zip"
-  desc "MartianMono Nerd Font (MartianMono)"
   desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/MartianMono.zip"
+  version "3.2.1"
+  sha256 "a0bb5ab838bb2dcbcc2a7824e6a73505e8dc8f95f48183e056d82790108c32ea"
+
   def install
-    (share/"fonts").install Dir.glob("./**/MartianMonoNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MartianMonoNerdFontPropo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MartianMonoNerdFont-CondensedBold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MartianMonoNerdFont-CondensedMedium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MartianMonoNerdFont-CondensedRegular.ttf")[0]
@@ -23,10 +23,10 @@ class FontMartianMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/MartianMonoNerdFontPropo-CondensedMedium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MartianMonoNerdFontPropo-CondensedRegular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MartianMonoNerdFontPropo-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/MartianMonoNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MartianMonoNerdFont-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
