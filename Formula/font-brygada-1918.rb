d08ec29d81f42697c51e08ffc0384fa2205d60af
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:49 +0000

    font-brygada-1918 updated

--- Formula/font-brygada-1918.rb
+++ Formula/font-brygada-1918.rb
@@ -1,14 +1,17 @@
 class FontBrygada1918 < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/brygada1918"
-  desc "Brygada 1918"
   desc "Available on the project's website"
   homepage "https://fonts.google.com/specimen/Brygada+1918"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/brygada1918"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/brygada1918/./**/Brygada1918-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/brygada1918/./**/Brygada1918\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/brygada1918/./**/Brygada1918-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/brygada1918/./**/Brygada1918[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
