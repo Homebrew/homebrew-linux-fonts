ba3ed80a099b047b5c690d74a4bb13a5512f00af
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:23 +0000

    font-ipaexfont updated

--- Formula/font-ipaexfont.rb
+++ Formula/font-ipaexfont.rb
@@ -1,15 +1,16 @@
 class FontIpaexfont < Formula
+  desc "Ipaex fonts font"
+  homepage "https://moji.or.jp/ipafont/"
+  url "https://moji.or.jp/wp-content/ipafont/IPAexfont/IPAexfont00401.zip"
   version "004.01"
   sha256 "bcf8374ab3f9672c421120430dd19a51c99f5265cf06fc340d9a661ddfd7974b"
-  url "https://moji.or.jp/wp-content/ipafont/IPAexfont/IPAexfont#{version.to_s.gsub(".", "")}.zip"
-  desc "IPAex Fonts"
-  homepage "https://moji.or.jp/ipafont/"
+
   def install
-    (share/"fonts").install Dir.glob("IPAexfont#{version.to_s.gsub(".", "")}/**/ipaexg.ttf")[0]
-    (share/"fonts").install Dir.glob("IPAexfont#{version.to_s.gsub(".", "")}/**/ipaexm.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/IPAexfont00401/ipaexg.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/IPAexfont00401/ipaexm.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
