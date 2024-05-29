b9a4ff0793771f57c7677adf553fcc042c7966b1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:23 +0000

    font-snowburst-one updated

--- Formula/font-snowburst-one.rb
+++ Formula/font-snowburst-one.rb
@@ -1,12 +1,14 @@
 class FontSnowburstOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/snowburstone/SnowburstOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Snowburst One"
+  desc "Snowburst one font"
   homepage "https://fonts.google.com/specimen/Snowburst+One"
+  head "https://github.com/google/fonts/raw/main/ofl/snowburstone/SnowburstOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SnowburstOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
