edaff4f9e31cd95c9a827604349eec0437d3056a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:03 +0000

    font-orienta updated

--- Formula/font-orienta.rb
+++ Formula/font-orienta.rb
@@ -1,12 +1,14 @@
 class FontOrienta < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/orienta/Orienta-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Orienta"
+  desc "Orienta font"
   homepage "https://fonts.google.com/specimen/Orienta"
+  head "https://github.com/google/fonts/raw/main/ofl/orienta/Orienta-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Orienta-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
