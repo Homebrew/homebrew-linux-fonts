7116e433bdbf9aae5d841773a719a9237dee653c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:44 +0000

    font-ms-madi updated

--- Formula/font-ms-madi.rb
+++ Formula/font-ms-madi.rb
@@ -1,13 +1,14 @@
 class FontMsMadi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/msmadi/MsMadi-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ms Madi"
   desc "Never a good idea to use this font in all capital letters"
   homepage "https://fonts.google.com/specimen/Ms+Madi"
+  head "https://github.com/google/fonts/raw/main/ofl/msmadi/MsMadi-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/MsMadi-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
