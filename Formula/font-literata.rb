0e428a8e75a9441ee0d471b0c1870c19f856b38b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:34 +0000

    font-literata updated

--- Formula/font-literata.rb
+++ Formula/font-literata.rb
@@ -1,13 +1,17 @@
 class FontLiterata < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/literata"
-  desc "Literata"
+  desc "Literata font"
   homepage "https://fonts.google.com/specimen/Literata"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/literata"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/literata/./**/Literata-Italic\\[opsz,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/literata/./**/Literata\\[opsz,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/literata/./**/Literata-Italic[opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/literata/./**/Literata[opsz,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
