0a125b3f0bf4299e6bb0ca3f6481ca633454ac00
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-alexandria updated

--- Formula/font-alexandria.rb
+++ Formula/font-alexandria.rb
@@ -1,13 +1,14 @@
 class FontAlexandria < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alexandria/Alexandria%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Alexandria"
   desc "9 weights font family made in matching to the latin type Montserrat"
   homepage "https://fonts.google.com/specimen/Alexandria"
+  head "https://github.com/google/fonts/raw/main/ofl/alexandria/Alexandria%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Alexandria\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Alexandria[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
