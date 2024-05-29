3928599bcdc5444332b97f37156e68394ffc0258
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:24 +0000

    font-sofia-sans-extra-condensed updated

--- Formula/font-sofia-sans-extra-condensed.rb
+++ Formula/font-sofia-sans-extra-condensed.rb
@@ -1,13 +1,17 @@
 class FontSofiaSansExtraCondensed < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sofiasansextracondensed"
-  desc "Sofia Sans Extra Condensed"
+  desc "Sofia sans extra condensed font"
   homepage "https://fonts.google.com/specimen/Sofia+Sans+Extra+Condensed"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sofiasansextracondensed"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/sofiasansextracondensed/./**/SofiaSansExtraCondensed-Italic\\[wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/sofiasansextracondensed/./**/SofiaSansExtraCondensed\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sofiasansextracondensed/./**/SofiaSansExtraCondensed-Italic[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sofiasansextracondensed/./**/SofiaSansExtraCondensed[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
