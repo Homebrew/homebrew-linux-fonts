927ce663ad3d8888b68a54a8096270818991243f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-mooli updated

--- Formula/font-mooli.rb
+++ Formula/font-mooli.rb
@@ -1,13 +1,14 @@
 class FontMooli < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/mooli/Mooli-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Mooli"
   desc "Derived from the mulish font family"
   homepage "https://fonts.google.com/specimen/Mooli"
+  head "https://github.com/google/fonts/raw/main/ofl/mooli/Mooli-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Mooli-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
