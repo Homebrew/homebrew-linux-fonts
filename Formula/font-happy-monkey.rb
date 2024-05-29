498f3dfdb7250126780e45b66d2fcbc9a3a0c268
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-happy-monkey updated

--- Formula/font-happy-monkey.rb
+++ Formula/font-happy-monkey.rb
@@ -1,12 +1,14 @@
 class FontHappyMonkey < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/happymonkey/HappyMonkey-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Happy Monkey"
+  desc "Happy monkey font"
   homepage "https://fonts.google.com/specimen/Happy+Monkey"
+  head "https://github.com/google/fonts/raw/main/ofl/happymonkey/HappyMonkey-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/HappyMonkey-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
