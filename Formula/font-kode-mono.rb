af4b285915800ffe0936aded99dcaa59b9b6398b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-kode-mono updated

--- Formula/font-kode-mono.rb
+++ Formula/font-kode-mono.rb
@@ -1,12 +1,14 @@
 class FontKodeMono < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/kodemono/KodeMono%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Kode Mono"
+  desc "Kode mono font"
   homepage "https://fonts.google.com/specimen/Kode+Mono"
+  head "https://github.com/google/fonts/raw/main/ofl/kodemono/KodeMono%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/KodeMono\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/KodeMono[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
