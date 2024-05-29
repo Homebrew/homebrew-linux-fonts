1c8716c11c172126619322cbf840851dbb84a171
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:17 +0000

    font-honk updated

--- Formula/font-honk.rb
+++ Formula/font-honk.rb
@@ -1,13 +1,14 @@
 class FontHonk < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/honk/Honk%5BMORF%2CSHLN%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Honk"
   desc "Done by taresh vohra and team ek type"
   homepage "https://fonts.google.com/specimen/Honk"
+  head "https://github.com/google/fonts/raw/main/ofl/honk/Honk%5BMORF%2CSHLN%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Honk\\[MORF,SHLN\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Honk[MORF,SHLN].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
