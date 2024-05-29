829e43a6024ba43a679821e7e78fe81734848dfb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:08 +0000

    font-playwrite-nz updated

--- Formula/font-playwrite-nz.rb
+++ Formula/font-playwrite-nz.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteNz < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwritenz/PlaywriteNZ%5Bwght%5D.ttf"
-  desc "Playwrite NZ"
+  desc "Playwrite nz font"
   homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritenz/PlaywriteNZ%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteNZ\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteNZ[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
