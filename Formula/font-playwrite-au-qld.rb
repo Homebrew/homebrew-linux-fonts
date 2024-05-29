7dfa1015131e20bbe9eca37290d62d3e391b93a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:07 +0000

    font-playwrite-au-qld updated

--- Formula/font-playwrite-au-qld.rb
+++ Formula/font-playwrite-au-qld.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteAuQld < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteauqld/PlaywriteAUQLD%5Bwght%5D.ttf"
-  desc "Playwrite AU QLD"
+  desc "Playwrite au qld font"
   homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteauqld/PlaywriteAUQLD%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteAUQLD\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteAUQLD[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
