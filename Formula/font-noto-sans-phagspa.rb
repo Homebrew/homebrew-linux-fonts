025d396cc75b956bf4fea8d16aa1b76f55735475
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:55 +0000

    font-noto-sans-phagspa updated

--- Formula/font-noto-sans-phagspa.rb
+++ Formula/font-noto-sans-phagspa.rb
@@ -1,13 +1,13 @@
 class FontNotoSansPhagspa < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansphagspa/NotoSansPhagsPa-Regular.ttf"
-  desc "Noto Sans PhagsPa"
   desc "Sans-serif for texts in the historical central asian phags-pa script"
   homepage "https://github.com/notofonts/phags-pa.git"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansphagspa/NotoSansPhagsPa-Regular.ttf"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansPhagsPa-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
