d6dea6ce4d5f60333e75b19793c21a552e3c9419
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:08 +0000

    font-playwrite-it-moderna updated

--- Formula/font-playwrite-it-moderna.rb
+++ Formula/font-playwrite-it-moderna.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteItModerna < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteitmoderna/PlaywriteITModerna%5Bwght%5D.ttf"
-  desc "Playwrite IT Moderna"
+  desc "Playwrite it moderna font"
   homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteitmoderna/PlaywriteITModerna%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteITModerna\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteITModerna[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
