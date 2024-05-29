cbdd76ba1076e88b3147d48d1f63d3876c2faa41
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:02 +0000

    font-oldenburg updated

--- Formula/font-oldenburg.rb
+++ Formula/font-oldenburg.rb
@@ -1,12 +1,14 @@
 class FontOldenburg < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/oldenburg/Oldenburg-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Oldenburg"
+  desc "Oldenburg font"
   homepage "https://fonts.google.com/specimen/Oldenburg"
+  head "https://github.com/google/fonts/raw/main/ofl/oldenburg/Oldenburg-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Oldenburg-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
