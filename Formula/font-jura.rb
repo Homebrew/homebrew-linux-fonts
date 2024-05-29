02141ccb295a1003baddf8ed08ec5cd1ae17a282
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:26 +0000

    font-jura updated

--- Formula/font-jura.rb
+++ Formula/font-jura.rb
@@ -1,12 +1,14 @@
 class FontJura < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jura/Jura%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Jura"
+  desc "Jura font"
   homepage "https://fonts.google.com/specimen/Jura"
+  head "https://github.com/google/fonts/raw/main/ofl/jura/Jura%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/Jura\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Jura[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
