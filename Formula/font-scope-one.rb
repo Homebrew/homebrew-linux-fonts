d37bc860dd6f018a15a1eade22ad2f2d61ff791d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-scope-one updated

--- Formula/font-scope-one.rb
+++ Formula/font-scope-one.rb
@@ -1,12 +1,14 @@
 class FontScopeOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/scopeone/ScopeOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Scope One"
+  desc "Scope one font"
   homepage "https://fonts.google.com/specimen/Scope+One"
+  head "https://github.com/google/fonts/raw/main/ofl/scopeone/ScopeOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ScopeOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
