71f0f5334a4c6d49cd74f3297ba494220c76f826
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-baloo-tamma-2 updated

--- Formula/font-baloo-tamma-2.rb
+++ Formula/font-baloo-tamma-2.rb
@@ -1,12 +1,14 @@
 class FontBalooTamma2 < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/balootamma2/BalooTamma2%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Baloo Tamma 2"
+  desc "Baloo tamma 2 font"
   homepage "https://fonts.google.com/specimen/Baloo+Tamma+2"
+  head "https://github.com/google/fonts/raw/main/ofl/balootamma2/BalooTamma2%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/BalooTamma2\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooTamma2[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
