a940987ec7df32d9d7ce51841ca48e618588f80b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:38 +0000

    font-amiri-quran updated

--- Formula/font-amiri-quran.rb
+++ Formula/font-amiri-quran.rb
@@ -1,12 +1,14 @@
 class FontAmiriQuran < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/amiriquran/AmiriQuran-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Amiri Quran"
+  desc "Amiri quran font"
   homepage "https://fonts.google.com/specimen/Amiri+Quran"
+  head "https://github.com/google/fonts/raw/main/ofl/amiriquran/AmiriQuran-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AmiriQuran-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
