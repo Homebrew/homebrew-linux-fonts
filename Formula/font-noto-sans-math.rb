13968de91d4be376f3183a5c869e3869010e9d7e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-math updated

--- Formula/font-noto-sans-math.rb
+++ Formula/font-noto-sans-math.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMath < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmath/NotoSansMath-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Math"
+  desc "Noto sans math font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Math"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmath/NotoSansMath-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMath-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
