04127e1674ff330afee193a6381dccc79d7ac904
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:39 +0000

    font-anek-odia updated

--- Formula/font-anek-odia.rb
+++ Formula/font-anek-odia.rb
@@ -1,12 +1,14 @@
 class FontAnekOdia < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/anekodia/AnekOdia%5Bwdth%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Anek Odia"
+  desc "Anek odia font"
   homepage "https://fonts.google.com/specimen/Anek+Odia"
+  head "https://github.com/google/fonts/raw/main/ofl/anekodia/AnekOdia%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/AnekOdia\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekOdia[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
