afebe6ecbde1ad49a459db744fe2b2259072f244
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-architype-renner updated

--- Formula/font-architype-renner.rb
+++ Formula/font-architype-renner.rb
@@ -1,16 +1,18 @@
 class FontArchitypeRenner < Formula
-  version "001.000"
-  url "https://fontlot.com/wp-content/uploads/2017/08/archityperenner.zip"
-  desc "Architype Renner"
+  desc "Architype renner font"
   homepage "https://fontlot.com/4598/architype-renner/"
+  url "https://fontlot.com/wp-content/uploads/2017/08/archityperenner.zip"
+  version "001.000"
+  sha256 :no_check
+
   def install
     (share/"fonts").install Dir.glob("./**/ArchitypeRenner-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/ArchitypeRenner-Demi.otf")[0]
     (share/"fonts").install Dir.glob("./**/ArchitypeRenner-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/ArchitypeRenner-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
