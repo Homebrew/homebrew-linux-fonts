0193f06769ba3a56e728294d2df516a1562b4e0f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:14 +0000

    font-reem-kufi updated

--- Formula/font-reem-kufi.rb
+++ Formula/font-reem-kufi.rb
@@ -1,12 +1,14 @@
 class FontReemKufi < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/reemkufi/ReemKufi%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Reem Kufi"
+  desc "Reem kufi font"
   homepage "https://fonts.google.com/specimen/Reem+Kufi"
+  head "https://github.com/google/fonts/raw/main/ofl/reemkufi/ReemKufi%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/ReemKufi\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ReemKufi[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
