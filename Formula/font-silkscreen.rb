176d79196f5f688f153980ea7a3c12fc33062cbb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-silkscreen updated

--- Formula/font-silkscreen.rb
+++ Formula/font-silkscreen.rb
@@ -1,14 +1,17 @@
 class FontSilkscreen < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/silkscreen"
-  desc "Silkscreen"
   desc "Typeface for your web graphics"
   homepage "https://fonts.google.com/specimen/Silkscreen"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/silkscreen"
+
   def install
     (share/"fonts").install Dir.glob("ofl/silkscreen/./**/Silkscreen-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/silkscreen/./**/Silkscreen-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
