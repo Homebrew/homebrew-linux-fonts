7f11e64cc2c53ae39e6e8de5444e0d97db451919
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:32 +0000

    font-turret-road updated

--- Formula/font-turret-road.rb
+++ Formula/font-turret-road.rb
@@ -1,7 +1,11 @@
 class FontTurretRoad < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/turretroad"
-  desc "Turret Road"
+  desc "Turret road font"
   homepage "https://fonts.google.com/specimen/Turret+Road"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/turretroad"
+
   def install
     (share/"fonts").install Dir.glob("ofl/turretroad/./**/TurretRoad-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/turretroad/./**/TurretRoad-ExtraBold.ttf")[0]
@@ -10,8 +14,8 @@ class FontTurretRoad < Formula
     (share/"fonts").install Dir.glob("ofl/turretroad/./**/TurretRoad-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/turretroad/./**/TurretRoad-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
