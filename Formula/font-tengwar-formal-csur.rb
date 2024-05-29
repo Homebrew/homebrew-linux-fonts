599e165f415dda361484c0514eab31f1654d06fb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:28 +0000

    font-tengwar-formal-csur updated

--- Formula/font-tengwar-formal-csur.rb
+++ Formula/font-tengwar-formal-csur.rb
@@ -1,15 +1,15 @@
 class FontTengwarFormalCsur < Formula
-  version "11"
-  sha256 "924c282f7f967ecf6ca81003d95d4355dcccda9cf3fb7afdc746cb9d8f8250c7"
-  url "https://downloads.sourceforge.net/freetengwar/TengwarFormalCSUR#{version}.zip"
-  desc "Tengwar Formal CSUR"
   desc "Tengwar Unicode font compliant with CSUR"
   homepage "http://freetengwar.sourceforge.net/formal.html"
+  url "https://downloads.sourceforge.net/freetengwar/TengwarFormalCSUR11.zip"
+  version "11"
+  sha256 "924c282f7f967ecf6ca81003d95d4355dcccda9cf3fb7afdc746cb9d8f8250c7"
+
   def install
-    (share/"fonts").install Dir.glob("TengwarFormalCSUR#{version}/**/TengwarFormalCSUR.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TengwarFormalCSUR11/TengwarFormalCSUR.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
