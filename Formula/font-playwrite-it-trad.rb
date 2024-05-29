0728a216963d360459d5f28031fd27655b9d2226
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:08 +0000

    font-playwrite-it-trad updated

--- Formula/font-playwrite-it-trad.rb
+++ Formula/font-playwrite-it-trad.rb
@@ -1,12 +1,13 @@
 class FontPlaywriteItTrad < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteittrad/PlaywriteITTrad%5Bwght%5D.ttf"
-  desc "Playwrite IT Trad"
+  desc "Playwrite it trad font"
   homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteittrad/PlaywriteITTrad%5Bwght%5D.ttf"
+
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteITTrad\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteITTrad[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
