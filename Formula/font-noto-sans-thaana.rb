127b9853385b1542ea99fc44abcda880166c12fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:57 +0000

    font-noto-sans-thaana updated

--- Formula/font-noto-sans-thaana.rb
+++ Formula/font-noto-sans-thaana.rb
@@ -1,13 +1,15 @@
 class FontNotoSansThaana < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansThaana-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Thaana"
+  desc "Noto sans thaana font"
   homepage "https://www.google.com/get/noto/#sans-thaa"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansThaana-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansThaana-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansThaana-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
