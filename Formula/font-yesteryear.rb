5e3af6f9df99e81bb85e9a7e02cfb2d45712570c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:37 +0000

    font-yesteryear updated

--- Formula/font-yesteryear.rb
+++ Formula/font-yesteryear.rb
@@ -1,12 +1,14 @@
 class FontYesteryear < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/yesteryear/Yesteryear-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Yesteryear"
+  desc "Yesteryear font"
   homepage "https://fonts.google.com/specimen/Yesteryear"
+  head "https://github.com/google/fonts/raw/main/ofl/yesteryear/Yesteryear-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Yesteryear-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
