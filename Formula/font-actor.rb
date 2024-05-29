f2140aa70a37c644a5dc743a564dde4254270dff
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:35 +0000

    font-actor updated

--- Formula/font-actor.rb
+++ Formula/font-actor.rb
@@ -1,12 +1,14 @@
 class FontActor < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/actor/Actor-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Actor"
+  desc "Actor font"
   homepage "https://fonts.google.com/specimen/Actor"
+  head "https://github.com/google/fonts/raw/main/ofl/actor/Actor-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Actor-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
