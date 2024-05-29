88c68d1c096d06aec139d6a1fd463013c4a723d8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:37 +0000

    font-alike-angular updated

--- Formula/font-alike-angular.rb
+++ Formula/font-alike-angular.rb
@@ -1,12 +1,14 @@
 class FontAlikeAngular < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/alikeangular/AlikeAngular-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Alike Angular"
+  desc "Alike angular font"
   homepage "https://fonts.google.com/specimen/Alike+Angular"
+  head "https://github.com/google/fonts/raw/main/ofl/alikeangular/AlikeAngular-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/AlikeAngular-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
