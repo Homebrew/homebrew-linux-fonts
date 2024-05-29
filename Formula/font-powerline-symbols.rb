b5d1837e2fcd9909e815ae1ca0e6e84b3a262edf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:10 +0000

    font-powerline-symbols updated

--- Formula/font-powerline-symbols.rb
+++ Formula/font-powerline-symbols.rb
@@ -1,12 +1,13 @@
 class FontPowerlineSymbols < Formula
-  head "https://github.com/powerline/powerline/raw/master/font/PowerlineSymbols.otf"
-  desc "Powerline Symbols"
+  desc "Powerline symbols font"
   homepage "https://github.com/powerline/powerline"
+  head "https://github.com/powerline/powerline/raw/master/font/PowerlineSymbols.otf"
+
   def install
     (share/"fonts").install Dir.glob("./**/PowerlineSymbols.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
