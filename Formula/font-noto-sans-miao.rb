be3bbaa32e5ddae0c390fb35092328136507717a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:53 +0000

    font-noto-sans-miao updated

--- Formula/font-noto-sans-miao.rb
+++ Formula/font-noto-sans-miao.rb
@@ -1,12 +1,14 @@
 class FontNotoSansMiao < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosansmiao/NotoSansMiao-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans Miao"
+  desc "Noto sans miao font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Miao"
+  head "https://github.com/google/fonts/raw/main/ofl/notosansmiao/NotoSansMiao-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMiao-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
