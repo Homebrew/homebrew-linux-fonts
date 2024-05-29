bd6e3dd7d46ae86f5022a38946f8fe1a42ba4796
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:14 +0000

    font-grenze updated

--- Formula/font-grenze.rb
+++ Formula/font-grenze.rb
@@ -1,9 +1,13 @@
 class FontGrenze < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/grenze"
-  desc "Grenze"
+  desc "Grenze font"
   homepage "https://fonts.google.com/specimen/Grenze"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/grenze"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-ThinItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-BoldItalic.ttf")[0]
@@ -20,10 +24,10 @@ class FontGrenze < Formula
     (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/grenze/./**/Grenze-Black.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
