1f5f62ff2e717bcf23f9adeb6d4e9a87790ef132
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:20 +0000

    font-sawarabi-gothic updated

--- Formula/font-sawarabi-gothic.rb
+++ Formula/font-sawarabi-gothic.rb
@@ -1,12 +1,14 @@
 class FontSawarabiGothic < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/sawarabigothic/SawarabiGothic-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Sawarabi Gothic"
+  desc "Sawarabi gothic font"
   homepage "https://fonts.google.com/specimen/Sawarabi+Gothic"
+  head "https://github.com/google/fonts/raw/main/ofl/sawarabigothic/SawarabiGothic-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SawarabiGothic-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
