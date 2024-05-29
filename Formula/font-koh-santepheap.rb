78aee07cc93b389c3944a38f13a6d3cfd2658df7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:29 +0000

    font-koh-santepheap updated

--- Formula/font-koh-santepheap.rb
+++ Formula/font-koh-santepheap.rb
@@ -1,8 +1,11 @@
 class FontKohSantepheap < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/kohsantepheap"
-  desc "Koh Santepheap"
   desc "Khmer font for body text"
   homepage "https://fonts.google.com/specimen/Koh+Santepheap"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/kohsantepheap"
+
   def install
     (share/"fonts").install Dir.glob("ofl/kohsantepheap/./**/KohSantepheap-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kohsantepheap/./**/KohSantepheap-Bold.ttf")[0]
@@ -10,8 +13,8 @@ class FontKohSantepheap < Formula
     (share/"fonts").install Dir.glob("ofl/kohsantepheap/./**/KohSantepheap-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/kohsantepheap/./**/KohSantepheap-Thin.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
