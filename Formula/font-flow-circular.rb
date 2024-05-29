49acb52f2743c6a3109262feb1778a264bbecad6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:07 +0000

    font-flow-circular updated

--- Formula/font-flow-circular.rb
+++ Formula/font-flow-circular.rb
@@ -1,12 +1,14 @@
 class FontFlowCircular < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/flowcircular/FlowCircular-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Flow Circular"
+  desc "Flow circular font"
   homepage "https://fonts.google.com/specimen/Flow+Circular"
+  head "https://github.com/google/fonts/raw/main/ofl/flowcircular/FlowCircular-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/FlowCircular-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
