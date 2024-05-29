eeff75fa8b7993ae2baddb436da88c02593c7470
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-sintony updated

--- Formula/font-sintony.rb
+++ Formula/font-sintony.rb
@@ -1,13 +1,17 @@
 class FontSintony < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/sintony"
-  desc "Sintony"
+  desc "Sintony font"
   homepage "https://fonts.google.com/specimen/Sintony"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/sintony"
+
   def install
     (share/"fonts").install Dir.glob("ofl/sintony/./**/Sintony-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sintony/./**/Sintony-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
