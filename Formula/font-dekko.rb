6af0b70dd4bb29ca8127dc926d8e0eb213e796a6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:00 +0000

    font-dekko updated

--- Formula/font-dekko.rb
+++ Formula/font-dekko.rb
@@ -1,12 +1,14 @@
 class FontDekko < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/dekko/Dekko-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Dekko"
+  desc "Dekko font"
   homepage "https://fonts.google.com/specimen/Dekko"
+  head "https://github.com/google/fonts/raw/main/ofl/dekko/Dekko-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Dekko-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
