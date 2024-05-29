f0f4e781b76bf81d19fdce1654e8a7ff182217bd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-simonetta updated

--- Formula/font-simonetta.rb
+++ Formula/font-simonetta.rb
@@ -1,15 +1,19 @@
 class FontSimonetta < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/simonetta"
-  desc "Simonetta"
+  desc "Simonetta font"
   homepage "https://fonts.google.com/specimen/Simonetta"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/simonetta"
+
   def install
     (share/"fonts").install Dir.glob("ofl/simonetta/./**/Simonetta-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/simonetta/./**/Simonetta-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/simonetta/./**/Simonetta-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/simonetta/./**/Simonetta-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
