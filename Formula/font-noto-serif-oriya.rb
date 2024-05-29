1352d9121537ce86356a368ef897d3fe45316621
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:00 +0000

    font-noto-serif-oriya updated

--- Formula/font-noto-serif-oriya.rb
+++ Formula/font-noto-serif-oriya.rb
@@ -1,13 +1,14 @@
 class FontNotoSerifOriya < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notoseriforiya/NotoSerifOriya%5Bwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Serif Oriya"
   desc "Modulated (“serif”) design for texts in the indic odia (oriya) script"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Oriya"
+  head "https://github.com/google/fonts/raw/main/ofl/notoseriforiya/NotoSerifOriya%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifOriya\\[wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifOriya[wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
