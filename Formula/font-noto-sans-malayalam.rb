61e7721d67a926e3480e9cc170109504e1ead0c3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:52 +0000

    font-noto-sans-malayalam updated

--- Formula/font-noto-sans-malayalam.rb
+++ Formula/font-noto-sans-malayalam.rb
@@ -1,13 +1,15 @@
 class FontNotoSansMalayalam < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMalayalam-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Malayalam"
+  desc "Noto sans malayalam font"
   homepage "https://www.google.com/get/noto/#sans-mlym"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMalayalam-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansMalayalam-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/NotoSansMalayalam-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
