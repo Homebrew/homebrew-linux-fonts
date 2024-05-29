de8e00b717133250d144392471196948d4e1f100
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:42 +0000

    font-monoisome updated

--- Formula/font-monoisome.rb
+++ Formula/font-monoisome.rb
@@ -1,13 +1,16 @@
 class FontMonoisome < Formula
-  version "0.61"
-  url "https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true", verified: "github.com/larsenwork/monoid/"
-  desc "Monoisome"
+  desc "Monoisome font"
   homepage "https://larsenwork.com/monoid/"
+  url "https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true",
+       verified: "github.com/larsenwork/monoid/"
+  version "0.61"
+  sha256 :no_check
+
   def install
     (share/"fonts").install Dir.glob("./**/Monoisome-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
