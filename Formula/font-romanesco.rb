1582370d3547454069233acab20fd9d49f366ac0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:16 +0000

    font-romanesco updated

--- Formula/font-romanesco.rb
+++ Formula/font-romanesco.rb
@@ -1,12 +1,14 @@
 class FontRomanesco < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/romanesco/Romanesco-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Romanesco"
+  desc "Romanesco font"
   homepage "https://fonts.google.com/specimen/Romanesco"
+  head "https://github.com/google/fonts/raw/main/ofl/romanesco/Romanesco-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Romanesco-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
