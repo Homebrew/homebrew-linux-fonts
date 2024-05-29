8fabc17629dbe7f747882414de06449044099f07
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:44 +0000

    font-bahianita updated

--- Formula/font-bahianita.rb
+++ Formula/font-bahianita.rb
@@ -1,12 +1,14 @@
 class FontBahianita < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/bahianita/Bahianita-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Bahianita"
+  desc "Bahianita font"
   homepage "https://fonts.google.com/specimen/Bahianita"
+  head "https://github.com/google/fonts/raw/main/ofl/bahianita/Bahianita-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Bahianita-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
