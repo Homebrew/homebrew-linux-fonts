2850f37d5cac4bd236dc6bb4747c8da9caee127d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-archivo updated

--- Formula/font-archivo.rb
+++ Formula/font-archivo.rb
@@ -1,13 +1,17 @@
 class FontArchivo < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/archivo"
-  desc "Archivo"
+  desc "Archivo font"
   homepage "https://fonts.google.com/specimen/Archivo"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/archivo"
+
   def install
-    (share/"fonts").install Dir.glob("ofl/archivo/./**/Archivo-Italic\\[wdth,wght\\].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/archivo/./**/Archivo\\[wdth,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/archivo/./**/Archivo-Italic[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/archivo/./**/Archivo[wdth,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
