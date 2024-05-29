e2cf5dc132631dec90aedfc58a8e9a84950c1d5c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:15 +0000

    font-hannari updated

--- Formula/font-hannari.rb
+++ Formula/font-hannari.rb
@@ -1,12 +1,14 @@
 class FontHannari < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/hannari/Hannari-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Hannari"
+  desc "Hannari font"
   homepage "https://fonts.google.com/specimen/Hannari"
+  head "https://github.com/google/fonts/raw/main/ofl/hannari/Hannari-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Hannari-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
