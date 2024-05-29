7666137db6083ffd6bd52a54aaf2a773ff5e1b0b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:35 +0000

    font-lugrasimo updated

--- Formula/font-lugrasimo.rb
+++ Formula/font-lugrasimo.rb
@@ -1,13 +1,14 @@
 class FontLugrasimo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/lugrasimo/Lugrasimo-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Lugrasimo"
   desc "Typeface in calligraphic lettering style"
   homepage "https://fonts.google.com/specimen/Lugrasimo"
+  head "https://github.com/google/fonts/raw/main/ofl/lugrasimo/Lugrasimo-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Lugrasimo-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
