24184ba8d94720d0020efc35f1a755575807a2dd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:41 +0000

    font-ar-one-sans updated

--- Formula/font-ar-one-sans.rb
+++ Formula/font-ar-one-sans.rb
@@ -1,13 +1,14 @@
 class FontArOneSans < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/aronesans/AROneSans%5BARRR%2Cwght%5D.ttf", verified: "github.com/google/fonts/"
-  desc "AR One Sans"
   desc "Type family is for use in augmented reality environments and user interfaces"
   homepage "https://fonts.google.com/specimen/AR+One+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/aronesans/AROneSans%5BARRR%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/AROneSans\\[ARRR,wght\\].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AROneSans[ARRR,wght].ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
