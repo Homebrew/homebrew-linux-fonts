86fc011f26a45f3816b04d02d02e08de2a8c7e1e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:13 +0000

    font-ranchers updated

--- Formula/font-ranchers.rb
+++ Formula/font-ranchers.rb
@@ -1,12 +1,14 @@
 class FontRanchers < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ranchers/Ranchers-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ranchers"
+  desc "Ranchers font"
   homepage "https://fonts.google.com/specimen/Ranchers"
+  head "https://github.com/google/fonts/raw/main/ofl/ranchers/Ranchers-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Ranchers-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
