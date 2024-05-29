718677efc5cd98e9086cd89292f0bb677776fff8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-montez updated

--- Formula/font-montez.rb
+++ Formula/font-montez.rb
@@ -1,12 +1,14 @@
 class FontMontez < Formula
-  head "https://github.com/google/fonts/raw/main/apache/montez/Montez-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Montez"
+  desc "Montez font"
   homepage "https://fonts.google.com/specimen/Montez"
+  head "https://github.com/google/fonts/raw/main/apache/montez/Montez-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Montez-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
