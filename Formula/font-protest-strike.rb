b822732b732101ec4a449c181f60ae276dc8e5f2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:11 +0000

    font-protest-strike updated

--- Formula/font-protest-strike.rb
+++ Formula/font-protest-strike.rb
@@ -1,13 +1,14 @@
 class FontProtestStrike < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/proteststrike/ProtestStrike-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Protest Strike"
   desc "Solid but peaceful sans serif typeface"
   homepage "https://fonts.google.com/specimen/Protest+Strike"
+  head "https://github.com/google/fonts/raw/main/ofl/proteststrike/ProtestStrike-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/ProtestStrike-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
