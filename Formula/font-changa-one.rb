266966d5bb51681b8ad8ecb1d2636a2ed71f31bf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:53 +0000

    font-changa-one updated

--- Formula/font-changa-one.rb
+++ Formula/font-changa-one.rb
@@ -1,13 +1,17 @@
 class FontChangaOne < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/changaone"
-  desc "Changa One"
+  desc "Changa one font"
   homepage "https://fonts.google.com/specimen/Changa+One"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/changaone"
+
   def install
     (share/"fonts").install Dir.glob("ofl/changaone/./**/ChangaOne-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/changaone/./**/ChangaOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
