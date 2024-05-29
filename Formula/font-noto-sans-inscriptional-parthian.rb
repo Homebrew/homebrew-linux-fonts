01339a1579ef778bfc7f885f61e1c29a7a294874
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:51 +0000

    font-noto-sans-inscriptional-parthian updated

--- Formula/font-noto-sans-inscriptional-parthian.rb
+++ Formula/font-noto-sans-inscriptional-parthian.rb
@@ -1,12 +1,14 @@
 class FontNotoSansInscriptionalParthian < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansInscriptionalParthian-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Inscriptional Parthian"
+  desc "Noto sans inscriptional parthian font"
   homepage "https://www.google.com/get/noto/#sans-prti"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansInscriptionalParthian-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansInscriptionalParthian-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
