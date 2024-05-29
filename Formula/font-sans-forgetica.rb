06a6beb1de271d0e6acb799a2b29be4755b6b0d9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:19 +0000

    font-sans-forgetica updated

--- Formula/font-sans-forgetica.rb
+++ Formula/font-sans-forgetica.rb
@@ -1,12 +1,13 @@
 class FontSansForgetica < Formula
-  head "https://sansforgetica.rmit.edu.au/Common/Zips/Sans%20Forgetica.zip"
-  desc "Sans Forgetica"
+  desc "Sans forgetica font"
   homepage "https://sansforgetica.rmit.edu.au/"
+  head "https://sansforgetica.rmit.edu.au/Common/Zips/Sans%20Forgetica.zip"
+
   def install
-    (share/"fonts").install Dir.glob("Sans Forgetica/**/SansForgetica-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Sans Forgetica/SansForgetica-Regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
