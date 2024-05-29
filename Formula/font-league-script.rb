91da1bb58443168b89348259cc51a09166e642d2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:31 +0000

    font-league-script updated

--- Formula/font-league-script.rb
+++ Formula/font-league-script.rb
@@ -1,12 +1,14 @@
 class FontLeagueScript < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/leaguescript/LeagueScript-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "League Script"
+  desc "League script font"
   homepage "https://fonts.google.com/specimen/League+Script"
+  head "https://github.com/google/fonts/raw/main/ofl/leaguescript/LeagueScript-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/LeagueScript-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
