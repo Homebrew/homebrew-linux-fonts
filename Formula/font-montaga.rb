420f7714b6aac90f6d3739c4801a170a370448a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:43 +0000

    font-montaga updated

--- Formula/font-montaga.rb
+++ Formula/font-montaga.rb
@@ -1,12 +1,14 @@
 class FontMontaga < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/montaga/Montaga-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Montaga"
+  desc "Montaga font"
   homepage "https://fonts.google.com/specimen/Montaga"
+  head "https://github.com/google/fonts/raw/main/ofl/montaga/Montaga-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Montaga-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
