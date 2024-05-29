4f60693f563a8c07b42951c76c48e36a7148a7b2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:56 +0000

    font-noto-sans-signwriting updated

--- Formula/font-noto-sans-signwriting.rb
+++ Formula/font-noto-sans-signwriting.rb
@@ -1,13 +1,14 @@
 class FontNotoSansSignwriting < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/notosanssignwriting/NotoSansSignWriting-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Noto Sans SignWriting"
   desc "Design for the sign-language signwriting script"
   homepage "https://fonts.google.com/specimen/Noto+Sans+SignWriting"
+  head "https://github.com/google/fonts/raw/main/ofl/notosanssignwriting/NotoSansSignWriting-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansSignWriting-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
