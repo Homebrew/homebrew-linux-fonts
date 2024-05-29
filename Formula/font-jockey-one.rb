852dc22122924f24ba4ee2fb855e0ba557e45703
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-jockey-one updated

--- Formula/font-jockey-one.rb
+++ Formula/font-jockey-one.rb
@@ -1,12 +1,14 @@
 class FontJockeyOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jockeyone/JockeyOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Jockey One"
+  desc "Jockey one font"
   homepage "https://fonts.google.com/specimen/Jockey+One"
+  head "https://github.com/google/fonts/raw/main/ofl/jockeyone/JockeyOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JockeyOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
