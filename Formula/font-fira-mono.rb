ba146bd7cc5a228d816df7e9490d48941416ec94
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:06 +0000

    font-fira-mono updated

--- Formula/font-fira-mono.rb
+++ Formula/font-fira-mono.rb
@@ -1,14 +1,18 @@
 class FontFiraMono < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/firamono"
-  desc "Fira Mono"
+  desc "Fira mono font"
   homepage "https://fonts.google.com/specimen/Fira+Mono"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/firamono"
+
   def install
     (share/"fonts").install Dir.glob("ofl/firamono/./**/FiraMono-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/firamono/./**/FiraMono-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/firamono/./**/FiraMono-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
