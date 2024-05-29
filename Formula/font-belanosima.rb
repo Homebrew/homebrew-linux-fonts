a24d1f3dcac45f98b335ac907ee1c3c56c86e229
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:46 +0000

    font-belanosima updated

--- Formula/font-belanosima.rb
+++ Formula/font-belanosima.rb
@@ -1,15 +1,18 @@
 class FontBelanosima < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/belanosima"
-  desc "Belanosima"
   desc "Inspired by geometric sans serif designs from the 1920s"
   homepage "https://fonts.google.com/specimen/Belanosima"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/belanosima"
+
   def install
     (share/"fonts").install Dir.glob("ofl/belanosima/./**/Belanosima-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/belanosima/./**/Belanosima-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/belanosima/./**/Belanosima-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
