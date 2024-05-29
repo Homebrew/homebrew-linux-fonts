38978ed397ed4f5920063d8cf365ff9ec2925aab
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:48 +0000

    font-noto-sans-buhid updated

--- Formula/font-noto-sans-buhid.rb
+++ Formula/font-noto-sans-buhid.rb
@@ -1,12 +1,14 @@
 class FontNotoSansBuhid < Formula
-  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBuhid-unhinted.zip", verified: "noto-website-2.storage.googleapis.com/"
-  desc "Noto Sans Buhid"
+  desc "Noto sans buhid font"
   homepage "https://www.google.com/get/noto/#sans-buhd"
+  head "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBuhid-unhinted.zip",
+       verified: "noto-website-2.storage.googleapis.com/"
+
   def install
     (share/"fonts").install Dir.glob("./**/NotoSansBuhid-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
