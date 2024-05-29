f7aa38cc3372d53d40c90c67593a629d48abd350
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:29 +0000

    font-tex-gyre-bonum updated

--- Formula/font-tex-gyre-bonum.rb
+++ Formula/font-tex-gyre-bonum.rb
@@ -1,17 +1,18 @@
 class FontTexGyreBonum < Formula
+  desc "Tex gyre bonum font"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/bonum"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/bonum/qbk2.004otf.zip"
   version "2.004"
   sha256 "30e7e9bf24b73709edff9916c95214a66b7fac38f78a11a19e4bc18ab019d398"
-  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/bonum/qbk#{version}otf.zip"
-  desc "TeX Gyre Bonum"
-  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/bonum"
+
   def install
     (share/"fonts").install Dir.glob("./**/texgyrebonum-bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/texgyrebonum-bolditalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/texgyrebonum-italic.otf")[0]
     (share/"fonts").install Dir.glob("./**/texgyrebonum-regular.otf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
