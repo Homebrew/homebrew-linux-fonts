474565e1e80409cef08a8fc36a802ddcd03f0a8f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-swanky-and-moo-moo updated

--- Formula/font-swanky-and-moo-moo.rb
+++ Formula/font-swanky-and-moo-moo.rb
@@ -1,12 +1,14 @@
 class FontSwankyAndMooMoo < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/swankyandmoomoo/SwankyandMooMoo.ttf", verified: "github.com/google/fonts/"
-  desc "Swanky and Moo Moo"
+  desc "Swanky and moo moo font"
   homepage "https://fonts.google.com/specimen/Swanky+and+Moo+Moo"
+  head "https://github.com/google/fonts/raw/main/ofl/swankyandmoomoo/SwankyandMooMoo.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/SwankyandMooMoo.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
