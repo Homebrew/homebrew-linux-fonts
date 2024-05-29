3646c0b60c27cd270ee1225bd05e8687ae9ff54a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:27 +0000

    font-syncopate updated

--- Formula/font-syncopate.rb
+++ Formula/font-syncopate.rb
@@ -1,13 +1,17 @@
 class FontSyncopate < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "apache/syncopate"
-  desc "Syncopate"
+  desc "Syncopate font"
   homepage "https://fonts.google.com/specimen/Syncopate"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "apache/syncopate"
+
   def install
     (share/"fonts").install Dir.glob("apache/syncopate/./**/Syncopate-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("apache/syncopate/./**/Syncopate-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
