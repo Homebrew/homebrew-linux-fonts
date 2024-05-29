4861aaf760a167fff26d83329c5b0dbc590b90b2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:20 +0000

    font-inknut-antiqua updated

--- Formula/font-inknut-antiqua.rb
+++ Formula/font-inknut-antiqua.rb
@@ -1,7 +1,11 @@
 class FontInknutAntiqua < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/inknutantiqua"
-  desc "Inknut Antiqua"
+  desc "Inknut antiqua font"
   homepage "https://fonts.google.com/specimen/Inknut+Antiqua"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/inknutantiqua"
+
   def install
     (share/"fonts").install Dir.glob("ofl/inknutantiqua/./**/InknutAntiqua-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/inknutantiqua/./**/InknutAntiqua-Bold.ttf")[0]
@@ -11,8 +15,8 @@ class FontInknutAntiqua < Formula
     (share/"fonts").install Dir.glob("ofl/inknutantiqua/./**/InknutAntiqua-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/inknutantiqua/./**/InknutAntiqua-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
