9d1fc1a7f0ea73df2e05c8e615a271b6fa08a0a9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:31 +0000

    font-ledger updated

--- Formula/font-ledger.rb
+++ Formula/font-ledger.rb
@@ -1,12 +1,14 @@
 class FontLedger < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/ledger/Ledger-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Ledger"
+  desc "Ledger font"
   homepage "https://fonts.google.com/specimen/Ledger"
+  head "https://github.com/google/fonts/raw/main/ofl/ledger/Ledger-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/Ledger-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
