54f79099475298f4b652cbc7de281cbc121ecb13
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:56 +0000

    font-content updated

--- Formula/font-content.rb
+++ Formula/font-content.rb
@@ -1,13 +1,17 @@
 class FontContent < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/content"
-  desc "Content"
+  desc "Content font"
   homepage "https://fonts.google.com/specimen/Content"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/content"
+
   def install
     (share/"fonts").install Dir.glob("ofl/content/./**/Content-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/content/./**/Content-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
