908375a79dea0d4ecedf6e1819b4901e2fd847e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:25 +0000

    font-joti-one updated

--- Formula/font-joti-one.rb
+++ Formula/font-joti-one.rb
@@ -1,12 +1,14 @@
 class FontJotiOne < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/jotione/JotiOne-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Joti One"
+  desc "Joti one font"
   homepage "https://fonts.google.com/specimen/Joti+One"
+  head "https://github.com/google/fonts/raw/main/ofl/jotione/JotiOne-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/JotiOne-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
