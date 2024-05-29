9d180ca37f17dfd18b84415f9a52f003079b7e06
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:10 +0000

    font-genshingothic updated

--- Formula/font-genshingothic.rb
+++ Formula/font-genshingothic.rb
@@ -1,11 +1,13 @@
 class FontGenshingothic < Formula
+  desc "Gen shin gothic font"
+  homepage "http://jikasei.me/font/genshin/"
+  url "https://osdn.dl.osdn.jp/users/8/8637/genshingothic-20150607.zip",
+       verified: "osdn.jp/"
   version "20150607,8.637"
   sha256 "b8e00f00a6e2517bfe75ceb2a732b596fe002457b89c05c181d6b71373aada58"
-  url "https://osdn.dl.osdn.jp/users/#{version.to_s.sub(/.*,/, "").sub(/\..*/, "")}/#{version.to_s.sub(/.*,/, "").gsub(".", "")}/genshingothic-#{version.to_s.sub(/,.*/, "")}.zip", verified: "osdn.jp/"
-  desc "Gen Shin Gothic"
-  homepage "http://jikasei.me/font/genshin/"
+
   def install
-    (share/"fonts").install Dir.glob("./**/GenShinGothic-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenShinGothic-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenShinGothic-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenShinGothic-Heavy.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenShinGothic-Light.ttf")[0]
@@ -25,10 +27,10 @@ class FontGenshingothic < Formula
     (share/"fonts").install Dir.glob("./**/GenShinGothic-P-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenShinGothic-P-Normal.ttf")[0]
     (share/"fonts").install Dir.glob("./**/GenShinGothic-P-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GenShinGothic-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenShinGothic-Bold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
