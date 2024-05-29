ce5eb22f77611de1a98bd62edce9119123bf62b9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:45 +0000

    font-n-gage updated

--- Formula/font-n-gage.rb
+++ Formula/font-n-gage.rb
@@ -1,12 +1,13 @@
 class FontNGage < Formula
-  head "https://dl.dafont.com/dl/?f=n_gage"
-  desc "N-Gage"
+  desc "N-gage font"
   homepage "https://www.dafont.com/n-gage.font"
+  head "https://dl.dafont.com/dl/?f=n_gage"
+
   def install
     (share/"fonts").install Dir.glob("./**/N-Gage.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
