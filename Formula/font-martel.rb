c6c0ec51413766c8e85289422d0956cbda64526f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:21:38 +0000

    font-martel updated

--- Formula/font-martel.rb
+++ Formula/font-martel.rb
@@ -1,7 +1,11 @@
 class FontMartel < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/martel"
-  desc "Martel"
+  desc "Martel font"
   homepage "https://fonts.google.com/specimen/Martel"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/martel"
+
   def install
     (share/"fonts").install Dir.glob("ofl/martel/./**/Martel-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/martel/./**/Martel-DemiBold.ttf")[0]
@@ -11,8 +15,8 @@ class FontMartel < Formula
     (share/"fonts").install Dir.glob("ofl/martel/./**/Martel-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/martel/./**/Martel-UltraLight.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
