532b05f04bb4f2ae3fdfd802fe679cf32e16e774
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:22:22 +0000

    font-signika-negative-sc updated

--- Formula/font-signika-negative-sc.rb
+++ Formula/font-signika-negative-sc.rb
@@ -1,16 +1,19 @@
 class FontSignikaNegativeSc < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/signikanegativesc"
-  desc "Signika Negative SC"
   desc "Alternative version of the Signika SC font"
   homepage "https://fonts.google.com/specimen/Signika+Negative"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/signikanegativesc"
+
   def install
     (share/"fonts").install Dir.glob("ofl/signikanegativesc/./**/SignikaNegativeSC-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/signikanegativesc/./**/SignikaNegativeSC-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/signikanegativesc/./**/SignikaNegativeSC-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/signikanegativesc/./**/SignikaNegativeSC-SemiBold.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
