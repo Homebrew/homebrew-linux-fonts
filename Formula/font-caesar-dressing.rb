8d020955acfd4dd6956b3dbc955c6cebebd40e74
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:50 +0000

    font-caesar-dressing updated

--- Formula/font-caesar-dressing.rb
+++ Formula/font-caesar-dressing.rb
@@ -1,12 +1,14 @@
 class FontCaesarDressing < Formula
-  head "https://github.com/google/fonts/raw/main/ofl/caesardressing/CaesarDressing-Regular.ttf", verified: "github.com/google/fonts/"
-  desc "Caesar Dressing"
+  desc "Caesar dressing font"
   homepage "https://fonts.google.com/specimen/Caesar+Dressing"
+  head "https://github.com/google/fonts/raw/main/ofl/caesardressing/CaesarDressing-Regular.ttf",
+       verified: "github.com/google/fonts/"
+
   def install
     (share/"fonts").install Dir.glob("./**/CaesarDressing-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
