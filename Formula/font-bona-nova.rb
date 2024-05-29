31b653bdf4e2402fb8201a11e6f78d998695bd31
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-05-29 00:20:48 +0000

    font-bona-nova updated

--- Formula/font-bona-nova.rb
+++ Formula/font-bona-nova.rb
@@ -1,15 +1,18 @@
 class FontBonaNova < Formula
-  head "https://github.com/google/fonts.git", verified: "github.com/google/fonts", branch: "main", only_path: "ofl/bonanova"
-  desc "Bona Nova"
   desc "Cursive typeface"
   homepage "https://fonts.google.com/specimen/Bona+Nova"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/bonanova"
+
   def install
     (share/"fonts").install Dir.glob("ofl/bonanova/./**/BonaNova-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/bonanova/./**/BonaNova-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/bonanova/./**/BonaNova-Regular.ttf")[0]
   end
-  # No zap stanza required
 
   test do
+    assert_path_exists share/"fonts"
   end
 end
