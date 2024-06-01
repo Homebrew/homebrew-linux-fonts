9fc8df9d24e0e98a817b399b03ebcd93d48bbee6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-01 00:21:46 +0000

    font-bungee updated

--- Formula/font-bungee.rb
+++ Formula/font-bungee.rb
@@ -1,32 +1,26 @@
 class FontBungee < Formula
   desc "Fonts for vertical and multicolor typography"
   homepage "https://djr.com/bungee/"
-  url "https://github.com/djrrb/bungee/releases/download/v1.2.1/Bungee-fonts.zip",
+  url "https://github.com/djrrb/bungee/releases/download/v2.000/Bungee-fonts.zip",
        verified: "github.com/djrrb/bungee/"
-  version "1.2.1"
-  sha256 "b6ff37d099140cb82c143622a097b95154265931735e84ef2105957976bb1bd0"
+  version "2.000"
+  sha256 "475c443ada44d5530debcf49703ef6871b968fbc0967676b3e1b0852b812b750"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Shade.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Rotated/BungeeRotatedShade-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeHairline-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeInline-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeOutline-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeShade-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/Bungee/Bungee-Hairline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/Bungee/Bungee-Inline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/Bungee/Bungee-Outline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/Bungee/Bungee-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/Bungee/Bungee-Shade.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeColor/BungeeColor-Regular-COLRv0.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeColor/BungeeColor-Regular-SVG.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeColor/BungeeSpice-Regular-COLRv1.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Inline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Outline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Shade.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Inline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Outline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Color/BungeeColor-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Color/BungeeSpice-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayers-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayersInline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayersOutline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayersShade-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Rotated/BungeeRotated-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Rotated/BungeeRotatedInline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Rotated/BungeeRotatedOutline-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/Bungee-Regular.ttf")[0]
   end
 
