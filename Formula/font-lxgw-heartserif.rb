6e38b222c7b8979e5479256ba2157e4db3afe49e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-02 00:21:51 +0000

    font-lxgw-heartserif updated

--- Formula/font-lxgw-heartserif.rb
+++ Formula/font-lxgw-heartserif.rb
@@ -1,9 +1,9 @@
 class FontLxgwHeartserif < Formula
   desc "Chinese serif font derived from IPAmj Mincho"
   homepage "https://github.com/lxgw/LxgwNeoZhiSong"
-  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v0.923/LXGWHeartSerif.ttf"
-  version "0.923"
-  sha256 "926b9d8465f276fffb6590dd38674bc60ff7560842536743972605a2884e2bde"
+  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v0.923.1/LXGWHeartSerif.ttf"
+  version "0.923.1"
+  sha256 "d9599d890d853250ef22edd10ec8915cedb38e80bb9b775d213e374f4ed72715"
 
   def install
     (share/"fonts").install Dir.glob("./**/LXGWHeartSerif.ttf")[0]
