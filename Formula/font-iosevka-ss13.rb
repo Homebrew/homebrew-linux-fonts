8de0191da4a00c5e77fc03639841321312a17f7c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-02 00:21:50 +0000

    font-iosevka-ss13 updated

--- Formula/font-iosevka-ss13.rb
+++ Formula/font-iosevka-ss13.rb
@@ -1,9 +1,9 @@
 class FontIosevkaSs13 < Formula
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v30.1.1/PkgTTC-IosevkaSS13-30.1.1.zip"
-  version "30.1.1"
-  sha256 "ebc604b0b2598bc1cb02b31bd28082c212836aa29842de0bb67d7d829ff428d1"
+  url "https://github.com/be5invis/Iosevka/releases/download/v30.1.2/PkgTTC-IosevkaSS13-30.1.2.zip"
+  version "30.1.2"
+  sha256 "5ba4e698e05740db159a08e7403a914b91edc04ef4698a39741e32e63c0433f6"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaSS13-Thin.ttc")[0]
