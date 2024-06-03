b5b0f075f7537d3263e9e5dfac696adb8d786cbc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-03 00:20:46 +0000

    font-baskervville-sc (new font)

---
+++ Formula/font-baskervville-sc.rb
@@ -0,0 +1,13 @@
+class FontBaskervvilleSc < Formula
+  desc "The small caps version of baskervville"
+  homepage "https://github.com/anrt-type/ANRT-Baskervville"
+  head "https://github.com/google/fonts/raw/main/ofl/baskervvillesc/BaskervvilleSC-Regular.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/BaskervvilleSC-Regular.ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
