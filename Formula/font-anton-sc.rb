e9d750fb952106107c86a7dc005dc5976ca292da
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-03 00:20:46 +0000

    font-anton-sc (new font)

---
+++ Formula/font-anton-sc.rb
@@ -0,0 +1,13 @@
+class FontAntonSc < Formula
+  desc "The small caps version of anton"
+  homepage "https://github.com/googlefonts/AntonFont"
+  head "https://github.com/google/fonts/raw/main/ofl/antonsc/AntonSC-Regular.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/AntonSC-Regular.ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
