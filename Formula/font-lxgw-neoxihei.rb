c184ddaceb60145ffcd675086e3f686af4e18c42
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-06 00:20:02 +0000

    font-lxgw-neoxihei updated

--- Formula/font-lxgw-neoxihei.rb
+++ Formula/font-lxgw-neoxihei.rb
@@ -1,9 +1,9 @@
 class FontLxgwNeoxihei < Formula
   desc "Chinese sans-serif font derived from IPAex Gothic"
   homepage "https://github.com/lxgw/LxgwNeoXiHei"
-  url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v1.123.1/LXGWNeoXiHei.ttf"
-  version "1.123.1"
-  sha256 "b5586b9569ae0e8822fdd8c721443eca202ccc682dc51e10b6441caa88bc25ba"
+  url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v1.123.2/LXGWNeoXiHei.ttf"
+  version "1.123.2"
+  sha256 "b285453dea14e11a2f938c695ddf740a3cd0d72d6bc75393ff878a0993537784"
 
   def install
     (share/"fonts").install Dir.glob("./**/LXGWNeoXiHei.ttf")[0]
