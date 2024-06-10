f784c7fc54eb3602622d4fba17de56188d1d1c7b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-10 00:21:24 +0000

    font-playwrite-cu (new font)

---
+++ Formula/font-playwrite-cu.rb
@@ -0,0 +1,13 @@
+class FontPlaywriteCu < Formula
+  desc "Playwrite cu font"
+  homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritecu/PlaywriteCU%5Bwght%5D.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/PlaywriteCU[wght].ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
