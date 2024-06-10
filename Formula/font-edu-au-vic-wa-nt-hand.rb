7a9b5e8dfc4e778199d9458f1599f3eadaddb20e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-10 00:21:23 +0000

    font-edu-au-vic-wa-nt-hand (new font)

---
+++ Formula/font-edu-au-vic-wa-nt-hand.rb
@@ -0,0 +1,13 @@
+class FontEduAuVicWaNtHand < Formula
+  desc "Edu au vic wa nt hand font"
+  homepage "https://github.com/SorkinType/VICWANTSchoolhandAustralia"
+  head "https://github.com/google/fonts/raw/main/ofl/eduauvicwanthand/EduAUVICWANTHand%5Bwght%5D.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/EduAUVICWANTHand[wght].ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
