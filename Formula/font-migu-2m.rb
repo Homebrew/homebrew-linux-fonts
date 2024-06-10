ec415c756f7000661bd353621199867bab21111b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-10 00:21:25 +0000

    font-migu-2m updated

--- Formula/font-migu-2m.rb
+++ Formula/font-migu-2m.rb
@@ -1,13 +1,14 @@
 class FontMigu2m < Formula
   desc "Migu 2m font"
-  homepage "https://mix-mplus-ipa.osdn.jp/migu/#migu2m"
-  url "https://osdn.dl.osdn.jp/mix-mplus-ipa/63545/migu-2m-20150712.zip"
-  version "20150712"
-  sha256 "659a6a121dadb6eac78369b9d129e2ec77a09fa292ca20932e42a5c753874297"
+  homepage "https://itouhiro.github.io/mixfont-mplus-ipa/migu/"
+  url "https://github.com/itouhiro/mixfont-mplus-ipa/releases/download/v2023.1123/migu-2m-20231123.zip",
+       verified: "github.com/itouhiro/mixfont-mplus-ipa/"
+  version "2023.1123"
+  sha256 "e7845f148772c984064c325eba70ed4dfb9a27084c2011a3a1b6194be6e439e5"
 
   def install
-    (share/"fonts").install Dir.glob("./**/migu-2m-20150712/migu-2m-bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/migu-2m-20150712/migu-2m-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-2m-20231123/migu-2m-bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-2m-20231123/migu-2m-regular.ttf")[0]
   end
 
   test do
