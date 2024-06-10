87f931ccec5e3884222769ebb8a7cf908f930007
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-10 00:21:25 +0000

    font-migu-1c updated

--- Formula/font-migu-1c.rb
+++ Formula/font-migu-1c.rb
@@ -1,13 +1,14 @@
 class FontMigu1c < Formula
   desc "Migu 1c font"
-  homepage "https://mix-mplus-ipa.osdn.jp/migu/#migu1c"
-  url "https://osdn.dl.osdn.jp/mix-mplus-ipa/63545/migu-1c-20150712.zip"
-  version "20150712"
-  sha256 "62aba11af4e5343b5437c866e3747366d084b63885539c92636222d2978999f1"
+  homepage "https://itouhiro.github.io/mixfont-mplus-ipa/migu/"
+  url "https://github.com/itouhiro/mixfont-mplus-ipa/releases/download/v2020.0307/migu-1c-20200307.zip",
+       verified: "github.com/itouhiro/mixfont-mplus-ipa/"
+  version "2020.0307"
+  sha256 "de18e4558495ab2860e01a218e43274c49660ab882085f4b803bfd9f0ccde02b"
 
   def install
-    (share/"fonts").install Dir.glob("./**/migu-1c-20150712/migu-1c-bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/migu-1c-20150712/migu-1c-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-1c-20200307/migu-1c-bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-1c-20200307/migu-1c-regular.ttf")[0]
   end
 
   test do
