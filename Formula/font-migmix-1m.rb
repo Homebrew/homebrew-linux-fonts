8c3ae122fecb9ebe3ffbffe5859a6d89cecd6203
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-10 00:21:25 +0000

    font-migmix-1m updated

--- Formula/font-migmix-1m.rb
+++ Formula/font-migmix-1m.rb
@@ -1,13 +1,14 @@
 class FontMigmix1m < Formula
   desc "Migmix 1m font"
-  homepage "https://mix-mplus-ipa.osdn.jp/migmix/#migmix1m"
-  url "https://osdn.dl.osdn.jp/mix-mplus-ipa/63544/migmix-1m-20150712.zip"
-  version "20150712"
-  sha256 "ac91394f3687315fb2727f8ee2b8ef70c6801d0b674dfc991912400eb3e7a344"
+  homepage "https://itouhiro.github.io/mixfont-mplus-ipa/migmix/"
+  url "https://github.com/itouhiro/mixfont-mplus-ipa/releases/download/v2020.0307/migmix-1m-20200307.zip",
+       verified: "github.com/itouhiro/mixfont-mplus-ipa/"
+  version "2020.0307"
+  sha256 "5b662021765d5a091cdbe6b09dd464710fbc42fb20c544d28795b3e0580a216e"
 
   def install
-    (share/"fonts").install Dir.glob("./**/migmix-1m-20150712/migmix-1m-bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/migmix-1m-20150712/migmix-1m-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migmix-1m-20200307/migmix-1m-bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migmix-1m-20200307/migmix-1m-regular.ttf")[0]
   end
 
   test do
