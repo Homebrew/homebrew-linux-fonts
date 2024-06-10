0048c01145100d7fb2478261e1c6a8ba16c37244
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-10 00:21:24 +0000

    font-playwrite-pe (new font)

---
+++ Formula/font-playwrite-pe.rb
@@ -0,0 +1,13 @@
+class FontPlaywritePe < Formula
+  desc "Playwrite pe font"
+  homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritepe/PlaywritePE%5Bwght%5D.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/PlaywritePE[wght].ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
