074b285a11ea9cf0d714c9b8be08f0515757317b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-10 00:21:25 +0000

    font-tengwar-formal-csur updated

--- Formula/font-tengwar-formal-csur.rb
+++ Formula/font-tengwar-formal-csur.rb
@@ -1,6 +1,6 @@
 class FontTengwarFormalCsur < Formula
   desc "Tengwar Unicode font compliant with CSUR"
-  homepage "http://freetengwar.sourceforge.net/formal.html"
+  homepage "https://freetengwar.sourceforge.net/formal.html"
   url "https://downloads.sourceforge.net/freetengwar/TengwarFormalCSUR11.zip"
   version "11"
   sha256 "924c282f7f967ecf6ca81003d95d4355dcccda9cf3fb7afdc746cb9d8f8250c7"
