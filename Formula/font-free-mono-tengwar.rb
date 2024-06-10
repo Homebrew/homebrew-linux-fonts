dc0b3cc377d40f3175ef55007ef23fa4d6f46d2b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-10 00:21:24 +0000

    font-free-mono-tengwar updated

--- Formula/font-free-mono-tengwar.rb
+++ Formula/font-free-mono-tengwar.rb
@@ -1,6 +1,6 @@
 class FontFreeMonoTengwar < Formula
   desc "Unicode tengwar monospaced font"
-  homepage "http://freetengwar.sourceforge.net/freemonotengwar.html"
+  homepage "https://freetengwar.sourceforge.net/freemonotengwar.html"
   url "https://downloads.sourceforge.net/freetengwar/FreeMonoTengwar.2013-07-21.zip"
   version "2013-07-21"
   sha256 "11a33522a6a7aa43120f0e9f98862d922a89228ed762738809132e3b26784907"
