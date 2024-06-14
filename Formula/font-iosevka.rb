4a2f45c2b949f4ff202025023cee5d69ea3dbd5c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:26 +0000

    font-iosevka updated

--- Formula/font-iosevka.rb
+++ Formula/font-iosevka.rb
@@ -1,5 +1,5 @@
 class FontIosevka < Formula
-  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  desc "Iosevka font"
   homepage "https://github.com/be5invis/Iosevka/"
   url "https://github.com/be5invis/Iosevka/releases/download/v30.1.2/PkgTTC-Iosevka-30.1.2.zip"
   version "30.1.2"
