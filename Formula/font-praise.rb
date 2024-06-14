db93b7a1b95d7cb642af4bed5531aa6c0bc8c5f7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:36 +0000

    font-praise updated

--- Formula/font-praise.rb
+++ Formula/font-praise.rb
@@ -1,5 +1,5 @@
 class FontPraise < Formula
-  desc "Versatile script with variations from casual (non-connecting) to formal appeal"
+  desc "Praise font"
   homepage "https://fonts.google.com/specimen/Praise"
   head "https://github.com/google/fonts/raw/main/ofl/praise/Praise-Regular.ttf",
        verified: "github.com/google/fonts/"
