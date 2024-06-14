90830c53ebc07845f7f5654598628553990adb52
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:44 +0000

    font-zen-dots updated

--- Formula/font-zen-dots.rb
+++ Formula/font-zen-dots.rb
@@ -1,5 +1,5 @@
 class FontZenDots < Formula
-  desc "One of three latin fonts part of the zen fonts collection"
+  desc "Zen dots font"
   homepage "https://fonts.google.com/specimen/Zen+Dots"
   head "https://github.com/google/fonts/raw/main/ofl/zendots/ZenDots-Regular.ttf",
        verified: "github.com/google/fonts/"
