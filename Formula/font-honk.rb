5d70afe46d15b21c9a00d764d63ee3ed9b8e4d80
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:23 +0000

    font-honk updated

--- Formula/font-honk.rb
+++ Formula/font-honk.rb
@@ -1,5 +1,5 @@
 class FontHonk < Formula
-  desc "Done by taresh vohra and team ek type"
+  desc "Honk font"
   homepage "https://fonts.google.com/specimen/Honk"
   head "https://github.com/google/fonts/raw/main/ofl/honk/Honk%5BMORF%2CSHLN%5D.ttf",
        verified: "github.com/google/fonts/"
