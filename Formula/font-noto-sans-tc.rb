c059d9330fbd6e33890ddce2508861b676cec757
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:34 +0000

    font-noto-sans-tc updated

--- Formula/font-noto-sans-tc.rb
+++ Formula/font-noto-sans-tc.rb
@@ -1,5 +1,5 @@
 class FontNotoSansTc < Formula
-  desc "Unmodulated (“sans serif”) design for languages in taiwan and macau"
+  desc "Noto sans tc font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+TC"
   head "https://github.com/google/fonts/raw/main/ofl/notosanstc/NotoSansTC%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
