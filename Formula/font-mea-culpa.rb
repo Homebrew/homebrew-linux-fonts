492d6b87131cd87d332821b2b83f557cb224698c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:30 +0000

    font-mea-culpa updated

--- Formula/font-mea-culpa.rb
+++ Formula/font-mea-culpa.rb
@@ -1,5 +1,5 @@
 class FontMeaCulpa < Formula
-  desc "Beautiful formal script with flourished capitals"
+  desc "Mea culpa font"
   homepage "https://fonts.google.com/specimen/Mea+Culpa"
   head "https://github.com/google/fonts/raw/main/ofl/meaculpa/MeaCulpa-Regular.ttf",
        verified: "github.com/google/fonts/"
