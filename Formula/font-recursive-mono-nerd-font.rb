c2b5bcd9811d6b0f9af6989c87a83004199a748e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:37 +0000

    font-recursive-mono-nerd-font updated

--- Formula/font-recursive-mono-nerd-font.rb
+++ Formula/font-recursive-mono-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontRecursiveMonoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Recmono nerd font families (recursive mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Recursive.zip"
   version "3.2.1"
