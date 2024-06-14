45ced43dce936d76858cc7dd4a623b95946b29e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:16 +0000

    font-bigblue-terminal-nerd-font updated

--- Formula/font-bigblue-terminal-nerd-font.rb
+++ Formula/font-bigblue-terminal-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontBigblueTerminalNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Bigblueterm nerd font families (bigblue terminal) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/BigBlueTerminal.zip"
   version "3.2.1"
