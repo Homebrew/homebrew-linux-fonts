fc6174897c9c7e689e1eca8323e04787af0d65ba
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:30 +0000

    font-meslo-lg-nerd-font updated

--- Formula/font-meslo-lg-nerd-font.rb
+++ Formula/font-meslo-lg-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontMesloLgNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Meslolg nerd font families (meslo lg) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip"
   version "3.2.1"
