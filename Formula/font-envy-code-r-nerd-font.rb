7893d9eb231c3d891b0f247d9dffd65382aafd15
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:20 +0000

    font-envy-code-r-nerd-font updated

--- Formula/font-envy-code-r-nerd-font.rb
+++ Formula/font-envy-code-r-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontEnvyCodeRNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Envycoder nerd font (envy code r) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/EnvyCodeR.zip"
   version "3.2.1"
