10b9c85eb002b6b618811f2021f1cfad29ca2c3b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:19 +0000

    font-daddy-time-mono-nerd-font updated

--- Formula/font-daddy-time-mono-nerd-font.rb
+++ Formula/font-daddy-time-mono-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontDaddyTimeMonoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Daddytimemono nerd font (daddytimemono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/DaddyTimeMono.zip"
   version "3.2.1"
