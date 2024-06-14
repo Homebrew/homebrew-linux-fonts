97e01e785399143276741926f1a796795a68ee56
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:24 +0000

    font-hurmit-nerd-font updated

--- Formula/font-hurmit-nerd-font.rb
+++ Formula/font-hurmit-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontHurmitNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Hurmit nerd font (hermit) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hermit.zip"
   version "3.2.1"
