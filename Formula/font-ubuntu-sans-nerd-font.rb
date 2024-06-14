d37406cb65285e2abcc41ae025582f464ff48724
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:42 +0000

    font-ubuntu-sans-nerd-font updated

--- Formula/font-ubuntu-sans-nerd-font.rb
+++ Formula/font-ubuntu-sans-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontUbuntuSansNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Ubuntusans nerd font families (ubuntu sans) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/UbuntuSans.zip"
   version "3.2.1"
