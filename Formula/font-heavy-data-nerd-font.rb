151d90ae2064a7265c87f58a9e00753a38bbb20f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:23 +0000

    font-heavy-data-nerd-font updated

--- Formula/font-heavy-data-nerd-font.rb
+++ Formula/font-heavy-data-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontHeavyDataNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Heavydata nerd font (heavy data) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/HeavyData.zip"
   version "3.2.1"
