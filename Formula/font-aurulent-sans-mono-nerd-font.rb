bf61b166e7abf506e2cde3bce047f0e942f41f03
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:15 +0000

    font-aurulent-sans-mono-nerd-font updated

--- Formula/font-aurulent-sans-mono-nerd-font.rb
+++ Formula/font-aurulent-sans-mono-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontAurulentSansMonoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Aurulentsansm nerd font (aurulent sans mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/AurulentSansMono.zip"
   version "3.2.1"
