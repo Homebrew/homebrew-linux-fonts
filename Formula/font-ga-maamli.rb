365b3f84c529ff9ce1b255358899005f340304da
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:22 +0000

    font-ga-maamli updated

--- Formula/font-ga-maamli.rb
+++ Formula/font-ga-maamli.rb
@@ -1,5 +1,5 @@
 class FontGaMaamli < Formula
-  desc "All caps font inspired by painted event posters in Accra, Ghana"
+  desc "Ga maamli font"
   homepage "https://github.com/SorkinType/GaMaamli"
   head "https://github.com/google/fonts/raw/main/ofl/gamaamli/GaMaamli-Regular.ttf"
 
