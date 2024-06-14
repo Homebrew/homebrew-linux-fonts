38cf0b202cc28c7ad1440b65bd2b493786a22566
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:29 +0000

    font-lxgw-neozhisong updated

--- Formula/font-lxgw-neozhisong.rb
+++ Formula/font-lxgw-neozhisong.rb
@@ -1,5 +1,5 @@
 class FontLxgwNeozhisong < Formula
-  desc "Chinese serif font derived from IPAmj Mincho"
+  desc "Lxgw neo zhisong font"
   homepage "https://github.com/lxgw/LxgwNeoZhiSong"
   url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v0.923.3/LXGWNeoZhiSong.ttf"
   version "0.923.3"
