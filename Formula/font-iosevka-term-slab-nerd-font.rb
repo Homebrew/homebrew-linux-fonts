f95e43aae2a54f9f6fa9cdb10eb2f9e43e7fa5f4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:26 +0000

    font-iosevka-term-slab-nerd-font updated

--- Formula/font-iosevka-term-slab-nerd-font.rb
+++ Formula/font-iosevka-term-slab-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontIosevkaTermSlabNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Iosevkatermslab nerd font (iosevka term slab) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IosevkaTermSlab.zip"
   version "3.2.1"
