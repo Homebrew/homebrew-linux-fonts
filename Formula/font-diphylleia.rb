25d1b6e947f138be81d62fda2a882b869616181f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:20 +0000

    font-diphylleia updated

--- Formula/font-diphylleia.rb
+++ Formula/font-diphylleia.rb
@@ -1,5 +1,5 @@
 class FontDiphylleia < Formula
-  desc "Mysterious flower that gets transparent when the water touches its petals"
+  desc "Diphylleia font"
   homepage "https://fonts.google.com/specimen/Diphylleia"
   head "https://github.com/google/fonts/raw/main/ofl/diphylleia/Diphylleia-Regular.ttf",
        verified: "github.com/google/fonts/"
