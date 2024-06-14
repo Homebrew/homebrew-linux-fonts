ef15aa6fca643d33ad10949b784727aa748f4146
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:24 +0000

    font-intone-mono-nerd-font updated

--- Formula/font-intone-mono-nerd-font.rb
+++ Formula/font-intone-mono-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontIntoneMonoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Intonemono nerd font (intel one mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IntelOneMono.zip"
   version "3.2.1"
