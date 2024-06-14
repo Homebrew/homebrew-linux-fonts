342566fb8fd1cad7832ade98fdee0407caead72d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:31 +0000

    font-montagu-slab updated

--- Formula/font-montagu-slab.rb
+++ Formula/font-montagu-slab.rb
@@ -1,5 +1,5 @@
 class FontMontaguSlab < Formula
-  desc "Available as a variable font with weight and optical size axes"
+  desc "Montagu slab font"
   homepage "https://fonts.google.com/specimen/Montagu+Slab"
   head "https://github.com/google/fonts/raw/main/ofl/montaguslab/MontaguSlab%5Bopsz%2Cwght%5D.ttf",
        verified: "github.com/google/fonts/"
