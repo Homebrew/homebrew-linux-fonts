524a1803f978a0c657e214cdb9cb26bc739d01d2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:23 +0000

    font-handjet updated

--- Formula/font-handjet.rb
+++ Formula/font-handjet.rb
@@ -1,5 +1,5 @@
 class FontHandjet < Formula
-  desc "Font tailored to work well with handjet printers"
+  desc "Handjet font"
   homepage "https://fonts.google.com/specimen/Handjet"
   head "https://github.com/google/fonts/raw/main/ofl/handjet/Handjet%5BELGR%2CELSH%2Cwght%5D.ttf",
        verified: "github.com/google/fonts/"
