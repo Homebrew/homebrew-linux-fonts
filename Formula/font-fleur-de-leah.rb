02c3eb4038a8e8cd0fffe56f7632937cf4c7194a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:21 +0000

    font-fleur-de-leah updated

--- Formula/font-fleur-de-leah.rb
+++ Formula/font-fleur-de-leah.rb
@@ -1,5 +1,5 @@
 class FontFleurDeLeah < Formula
-  desc "Formal script with a floral flavour"
+  desc "Fleur de leah font"
   homepage "https://fonts.google.com/specimen/Fleur+De+Leah"
   head "https://github.com/google/fonts/raw/main/ofl/fleurdeleah/FleurDeLeah-Regular.ttf",
        verified: "github.com/google/fonts/"
