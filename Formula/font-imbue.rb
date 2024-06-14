3ef40b13a6c1276482cfee89130417e4b4cbb432
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:24 +0000

    font-imbue updated

--- Formula/font-imbue.rb
+++ Formula/font-imbue.rb
@@ -1,5 +1,5 @@
 class FontImbue < Formula
-  desc "Variable condensed Didone font"
+  desc "Imbue font"
   homepage "https://fonts.google.com/specimen/Imbue"
   head "https://github.com/google/fonts/raw/main/ofl/imbue/Imbue%5Bopsz%2Cwght%5D.ttf",
        verified: "github.com/google/fonts/"
