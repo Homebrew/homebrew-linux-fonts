f8216bef47b1774cf86421320af78d66c7333565
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:21 +0000

    font-fantasque-sans-mono-nerd-font updated

--- Formula/font-fantasque-sans-mono-nerd-font.rb
+++ Formula/font-fantasque-sans-mono-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontFantasqueSansMonoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Fantasquesansm nerd font (fantasque sans mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FantasqueSansMono.zip"
   version "3.2.1"
