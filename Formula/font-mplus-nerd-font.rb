0741b98ab8b7058ff0f3f7cd7f3a8aabcffb888a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:31 +0000

    font-mplus-nerd-font updated

--- Formula/font-mplus-nerd-font.rb
+++ Formula/font-mplus-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontMplusNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "M+ nerd font families (mplus) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/MPlus.zip"
   version "3.2.1"
