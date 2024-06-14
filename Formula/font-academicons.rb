0e03464617d7026cc56f21cec103af60e64f910f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:13 +0000

    font-academicons updated

--- Formula/font-academicons.rb
+++ Formula/font-academicons.rb
@@ -1,5 +1,5 @@
 class FontAcademicons < Formula
-  desc "Specialist icon font for academics"
+  desc "Academicons font"
   homepage "https://github.com/jpswalsh/academicons"
   url "https://github.com/jpswalsh/academicons/archive/v1.9.4.zip"
   version "1.9.4"
