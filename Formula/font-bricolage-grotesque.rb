eaaa60b458b2eb833383376092d4397b2a1449d3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:17 +0000

    font-bricolage-grotesque updated

--- Formula/font-bricolage-grotesque.rb
+++ Formula/font-bricolage-grotesque.rb
@@ -1,5 +1,5 @@
 class FontBricolageGrotesque < Formula
-  desc "Collage of historical sources, technical decisions and personal feelings"
+  desc "Bricolage grotesque font"
   homepage "https://fonts.google.com/specimen/Bricolage+Grotesque"
   head "https://github.com/google/fonts/raw/main/ofl/bricolagegrotesque/BricolageGrotesque%5Bopsz%2Cwdth%2Cwght%5D.ttf",
        verified: "github.com/google/fonts/"
