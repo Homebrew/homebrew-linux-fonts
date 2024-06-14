30af809daf1f72b04915608aa0f1aeb6baf981cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:16 +0000

    font-blex-mono-nerd-font updated

--- Formula/font-blex-mono-nerd-font.rb
+++ Formula/font-blex-mono-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontBlexMonoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Blexmono nerd font (ibm plex mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IBMPlexMono.zip"
   version "3.2.1"
