2a0e88403d6821064e060fb758ea2af9b114b069
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:25 +0000

    font-iosevka-ss07 updated

--- Formula/font-iosevka-ss07.rb
+++ Formula/font-iosevka-ss07.rb
@@ -1,5 +1,5 @@
 class FontIosevkaSs07 < Formula
-  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  desc "Iosevka ss07 font"
   homepage "https://github.com/be5invis/Iosevka/"
   url "https://github.com/be5invis/Iosevka/releases/download/v30.1.2/PkgTTC-IosevkaSS07-30.1.2.zip"
   version "30.1.2"
