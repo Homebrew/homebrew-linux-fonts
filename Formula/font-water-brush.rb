554847c6e647a4d2405d17dd13570ef138e765de
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:43 +0000

    font-water-brush updated

--- Formula/font-water-brush.rb
+++ Formula/font-water-brush.rb
@@ -1,5 +1,5 @@
 class FontWaterBrush < Formula
-  desc "Camel hair brush filled with color on a watercolor textured paper"
+  desc "Water brush font"
   homepage "https://fonts.google.com/specimen/Water+Brush"
   head "https://github.com/google/fonts/raw/main/ofl/waterbrush/WaterBrush-Regular.ttf",
        verified: "github.com/google/fonts/"
