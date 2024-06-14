486c6a2df84c39e25bb39cc74ccbc3610967507d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:33 +0000

    font-noto-sans-nko updated

--- Formula/font-noto-sans-nko.rb
+++ Formula/font-noto-sans-nko.rb
@@ -1,5 +1,5 @@
 class FontNotoSansNko < Formula
-  desc "Unmodulated design for texts in the African N’Ko script"
+  desc "Noto sans nko font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+NKo"
   head "https://github.com/google/fonts/raw/main/ofl/notosansnko/NotoSansNKo-Regular.ttf",
        verified: "github.com/google/fonts/"
