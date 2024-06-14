8998bdb8dda9decc132d5f781e3ac03948aeccfd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:14 +0000

    font-ark-pixel-16px-monospaced updated

--- Formula/font-ark-pixel-16px-monospaced.rb
+++ Formula/font-ark-pixel-16px-monospaced.rb
@@ -1,5 +1,5 @@
 class FontArkPixel16pxMonospaced < Formula
-  desc "Open source Pan-CJK pixel font"
+  desc "Ark pixel 16px monospaced font"
   homepage "https://github.com/TakWolf/ark-pixel-font"
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/2024.05.12/ark-pixel-font-16px-monospaced-otf-v2024.05.12.zip"
   version "2024.05.12"
