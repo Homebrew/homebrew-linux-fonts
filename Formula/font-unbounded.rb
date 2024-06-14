1cad0a9e71b85d4ee3d6b8d604af6979b77637c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:43 +0000

    font-unbounded updated

--- Formula/font-unbounded.rb
+++ Formula/font-unbounded.rb
@@ -1,5 +1,5 @@
 class FontUnbounded < Formula
-  desc "Possibly the first on-chain funded font in the world"
+  desc "Unbounded font"
   homepage "https://fonts.google.com/specimen/Unbounded"
   head "https://github.com/google/fonts/raw/main/ofl/unbounded/Unbounded%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
