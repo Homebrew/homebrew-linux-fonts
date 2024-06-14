6964b666e51e0c2c9170e1cab4b83f4a5c59f845
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:40 +0000

    font-stick updated

--- Formula/font-stick.rb
+++ Formula/font-stick.rb
@@ -1,5 +1,5 @@
 class FontStick < Formula
-  desc "Designed with straight lines that create a cute and playful feel"
+  desc "Stick font"
   homepage "https://fonts.google.com/specimen/Stick"
   head "https://github.com/google/fonts/raw/main/ofl/stick/Stick-Regular.ttf",
        verified: "github.com/google/fonts/"
