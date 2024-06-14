f231103ec9aad7919a67e5ae76ebc63b7296128c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:23 +0000

    font-hasklug-nerd-font updated

--- Formula/font-hasklug-nerd-font.rb
+++ Formula/font-hasklug-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontHasklugNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Hasklug nerd font (hasklig) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hasklig.zip"
   version "3.2.1"
