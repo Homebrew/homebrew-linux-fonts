0ee253a0719f4355c6dc2855a8d8852e014ac369
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:32 +0000

    font-noto-nerd-font updated

--- Formula/font-noto-nerd-font.rb
+++ Formula/font-noto-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontNotoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Noto nerd font families (noto) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Noto.zip"
   version "3.2.1"
