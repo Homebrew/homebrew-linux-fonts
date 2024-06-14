59faa222017ce50bfddf4c74cccb2dfc74840697
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:15 +0000

    font-big-shoulders-inline-display updated

--- Formula/font-big-shoulders-inline-display.rb
+++ Formula/font-big-shoulders-inline-display.rb
@@ -1,5 +1,5 @@
 class FontBigShouldersInlineDisplay < Formula
-  desc "Superfamily of condensed American Gothic typefaces"
+  desc "Big shoulders inline display font"
   homepage "https://fonts.google.com/specimen/Big+Shoulders+Inline+Display"
   head "https://github.com/google/fonts/raw/main/ofl/bigshouldersinlinedisplay/BigShouldersInlineDisplay%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
