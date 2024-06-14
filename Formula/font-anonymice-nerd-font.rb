3a4d2f3d30efc28911a38d23be708a4490ac00c9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:14 +0000

    font-anonymice-nerd-font updated

--- Formula/font-anonymice-nerd-font.rb
+++ Formula/font-anonymice-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontAnonymiceNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Anonymicepro nerd font (anonymous pro) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/AnonymousPro.zip"
   version "3.2.1"
