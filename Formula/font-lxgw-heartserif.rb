9c3eae3ca90dc1ef883bfb240aa2f6bd68db3ef2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:29 +0000

    font-lxgw-heartserif updated

--- Formula/font-lxgw-heartserif.rb
+++ Formula/font-lxgw-heartserif.rb
@@ -1,5 +1,5 @@
 class FontLxgwHeartserif < Formula
-  desc "Chinese serif font derived from IPAmj Mincho"
+  desc "Lxgw heart serif font"
   homepage "https://github.com/lxgw/LxgwNeoZhiSong"
   url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v0.923.3/LXGWHeartSerif.ttf"
   version "0.923.3"
