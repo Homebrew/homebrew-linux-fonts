13afb2c062f712d3fe53840faef666ad8f58c717
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:38 +0000

    font-sansita-swashed updated

--- Formula/font-sansita-swashed.rb
+++ Formula/font-sansita-swashed.rb
@@ -1,5 +1,5 @@
 class FontSansitaSwashed < Formula
-  desc "Beautiful display font in vintage form"
+  desc "Sansita swashed font"
   homepage "https://fonts.google.com/specimen/Sansita+Swashed"
   head "https://github.com/google/fonts/raw/main/ofl/sansitaswashed/SansitaSwashed%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
