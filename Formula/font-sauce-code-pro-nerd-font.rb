784ea103ca840c5029dd94b3ea3b9a38d0566d7a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:38 +0000

    font-sauce-code-pro-nerd-font updated

--- Formula/font-sauce-code-pro-nerd-font.rb
+++ Formula/font-sauce-code-pro-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontSauceCodeProNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Saucecodepro nerd font (source code pro) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/SourceCodePro.zip"
   version "3.2.1"
