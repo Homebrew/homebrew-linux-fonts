911c5af26c1d69e17657101c4a8b26c3838533a0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:27 +0000

    font-kings updated

--- Formula/font-kings.rb
+++ Formula/font-kings.rb
@@ -1,5 +1,5 @@
 class FontKings < Formula
-  desc "Based on the three set font family (kings honor, kings quest and kings dominion)"
+  desc "Kings font"
   homepage "https://fonts.google.com/specimen/Kings"
   head "https://github.com/google/fonts/raw/main/ofl/kings/Kings-Regular.ttf",
        verified: "github.com/google/fonts/"
