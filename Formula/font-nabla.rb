1894182bebaf62e2519ade80997d86a3e8499227
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:32 +0000

    font-nabla updated

--- Formula/font-nabla.rb
+++ Formula/font-nabla.rb
@@ -1,5 +1,5 @@
 class FontNabla < Formula
-  desc "Color font inspired by isometric computer games, built using the colrv1 format"
+  desc "Nabla font"
   homepage "https://fonts.google.com/specimen/Nabla"
   head "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf",
        verified: "github.com/google/fonts/"
