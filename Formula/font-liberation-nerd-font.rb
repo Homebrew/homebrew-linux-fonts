668b3359328f9b3051defa6bab300a54c77ff38b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:28 +0000

    font-liberation-nerd-font updated

--- Formula/font-liberation-nerd-font.rb
+++ Formula/font-liberation-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontLiberationNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Literation nerd font families (liberation mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/LiberationMono.zip"
   version "3.2.1"
