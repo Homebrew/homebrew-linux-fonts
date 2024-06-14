2eda7a17494a111b06554c99f427d724bd60dace
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:35 +0000

    font-noto-serif-oriya updated

--- Formula/font-noto-serif-oriya.rb
+++ Formula/font-noto-serif-oriya.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifOriya < Formula
-  desc "Modulated (“serif”) design for texts in the indic odia (oriya) script"
+  desc "Noto serif oriya font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Oriya"
   head "https://github.com/google/fonts/raw/main/ofl/notoseriforiya/NotoSerifOriya%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
