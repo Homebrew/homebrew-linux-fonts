f4536f8a6791c5d9689cec2a5442b51e55739634
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:25 +0000

    font-iosevka-aile updated

--- Formula/font-iosevka-aile.rb
+++ Formula/font-iosevka-aile.rb
@@ -1,5 +1,5 @@
 class FontIosevkaAile < Formula
-  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  desc "Iosevka aile font"
   homepage "https://github.com/be5invis/Iosevka/"
   url "https://github.com/be5invis/Iosevka/releases/download/v30.1.2/PkgTTC-IosevkaAile-30.1.2.zip"
   version "30.1.2"
