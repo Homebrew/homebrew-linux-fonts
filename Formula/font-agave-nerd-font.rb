2bdd40bebae2f7ddcc3363cd608ae8ee359407f4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:13 +0000

    font-agave-nerd-font updated

--- Formula/font-agave-nerd-font.rb
+++ Formula/font-agave-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontAgaveNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Agave nerd font (agave) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Agave.zip"
   version "3.2.1"
