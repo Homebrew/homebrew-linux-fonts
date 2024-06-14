59b0f622620ce850eb263bc9d01aca6a35349b57
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:13 +0000

    font-alkatra updated

--- Formula/font-alkatra.rb
+++ Formula/font-alkatra.rb
@@ -1,5 +1,5 @@
 class FontAlkatra < Formula
-  desc "Display typeface family"
+  desc "Alkatra font"
   homepage "https://fonts.google.com/specimen/Alkatra"
   head "https://github.com/google/fonts/raw/main/ofl/alkatra/Alkatra%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
