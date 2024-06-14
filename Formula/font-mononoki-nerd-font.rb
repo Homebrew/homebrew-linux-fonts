afb69037e7cd8555232ca05f3a43436243ddcb82
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:31 +0000

    font-mononoki-nerd-font updated

--- Formula/font-mononoki-nerd-font.rb
+++ Formula/font-mononoki-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontMononokiNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Mononoki nerd font (mononoki) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Mononoki.zip"
   version "3.2.1"
