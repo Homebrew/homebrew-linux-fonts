7ff23598c36272314e90675081756ca395224e6e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:28 +0000

    font-lekton-nerd-font updated

--- Formula/font-lekton-nerd-font.rb
+++ Formula/font-lekton-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontLektonNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Lekton nerd font (lekton) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Lekton.zip"
   version "3.2.1"
