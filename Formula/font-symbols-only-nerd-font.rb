e52c40ae3fb4b80dcd4ee595a0374ab53af8e507
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:40 +0000

    font-symbols-only-nerd-font updated

--- Formula/font-symbols-only-nerd-font.rb
+++ Formula/font-symbols-only-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontSymbolsOnlyNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Symbols nerd font (symbols only) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/NerdFontsSymbolsOnly.zip"
   version "3.2.1"
