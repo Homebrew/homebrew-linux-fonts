89150125d35833065bde23e4d804f23fbca0155d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:24 +0000

    font-inconsolata-nerd-font updated

--- Formula/font-inconsolata-nerd-font.rb
+++ Formula/font-inconsolata-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontInconsolataNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Inconsolata nerd font (inconsolata) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Inconsolata.zip"
   version "3.2.1"
