7ac0f9b470c3db069e37e5f52198dbbacc4be5ff
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:26 +0000

    font-jetbrains-mono-nerd-font updated

--- Formula/font-jetbrains-mono-nerd-font.rb
+++ Formula/font-jetbrains-mono-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontJetbrainsMonoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Jetbrainsmono nerd font families (jetbrains mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip"
   version "3.2.1"
