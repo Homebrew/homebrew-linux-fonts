b78759deeaa18f2df154c5a3f176b08ef8188460
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:23 +0000

    font-hack-nerd-font updated

--- Formula/font-hack-nerd-font.rb
+++ Formula/font-hack-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontHackNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Hack nerd font (hack) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip"
   version "3.2.1"
