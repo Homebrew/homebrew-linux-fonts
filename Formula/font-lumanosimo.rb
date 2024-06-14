ecb30103066fa75c25c309b9e535d3ceea6603b1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:29 +0000

    font-lumanosimo updated

--- Formula/font-lumanosimo.rb
+++ Formula/font-lumanosimo.rb
@@ -1,5 +1,5 @@
 class FontLumanosimo < Formula
-  desc "Expressive typeface"
+  desc "Lumanosimo font"
   homepage "https://fonts.google.com/specimen/Lumanosimo"
   head "https://github.com/google/fonts/raw/main/ofl/lumanosimo/Lumanosimo-Regular.ttf",
        verified: "github.com/google/fonts/"
