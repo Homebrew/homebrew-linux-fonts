8af3dc2797f243b24d66b1489485315468f76fd3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:31 +0000

    font-monofur-nerd-font updated

--- Formula/font-monofur-nerd-font.rb
+++ Formula/font-monofur-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontMonofurNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Monofur nerd font (monofur) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Monofur.zip"
   version "3.2.1"
