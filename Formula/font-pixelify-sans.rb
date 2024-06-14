c3705440fd8f0b971f1ff2bb26573bf13006dc6a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:36 +0000

    font-pixelify-sans updated

--- Formula/font-pixelify-sans.rb
+++ Formula/font-pixelify-sans.rb
@@ -1,5 +1,5 @@
 class FontPixelifySans < Formula
-  desc "Achieved by using a grid of small, square pixels to create each letterform"
+  desc "Pixelify sans font"
   homepage "https://fonts.google.com/specimen/Pixelify+Sans"
   head "https://github.com/google/fonts/raw/main/ofl/pixelifysans/PixelifySans%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
