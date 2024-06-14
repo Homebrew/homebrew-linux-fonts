a3b58eac1ebd320b77df230daf744753eca69a0a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:30 +0000

    font-markazi-text updated

--- Formula/font-markazi-text.rb
+++ Formula/font-markazi-text.rb
@@ -1,5 +1,5 @@
 class FontMarkaziText < Formula
-  desc "Contemporary and highly readable typeface"
+  desc "Markazi text font"
   homepage "https://fonts.google.com/specimen/Markazi+Text"
   head "https://github.com/google/fonts/raw/main/ofl/markazitext/MarkaziText%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
