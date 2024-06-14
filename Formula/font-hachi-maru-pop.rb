d74280b6a91c1cf8efe9f90374f4896220bc2f51
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:23 +0000

    font-hachi-maru-pop updated

--- Formula/font-hachi-maru-pop.rb
+++ Formula/font-hachi-maru-pop.rb
@@ -1,5 +1,5 @@
 class FontHachiMaruPop < Formula
-  desc "Handwritten font"
+  desc "Hachi maru pop font"
   homepage "https://fonts.google.com/specimen/Hachi+Maru+Pop"
   head "https://github.com/google/fonts/raw/main/ofl/hachimarupop/HachiMaruPop-Regular.ttf",
        verified: "github.com/google/fonts/"
