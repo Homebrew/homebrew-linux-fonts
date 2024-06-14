43bff8d28785192d26006e90d425a7e8a835efd1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:16 +0000

    font-birthstone updated

--- Formula/font-birthstone.rb
+++ Formula/font-birthstone.rb
@@ -1,5 +1,5 @@
 class FontBirthstone < Formula
-  desc "Sibling birthstone bounce that includes caps and ending swashed forms"
+  desc "Birthstone font"
   homepage "https://fonts.google.com/specimen/Birthstone"
   head "https://github.com/google/fonts/raw/main/ofl/birthstone/Birthstone-Regular.ttf",
        verified: "github.com/google/fonts/"
