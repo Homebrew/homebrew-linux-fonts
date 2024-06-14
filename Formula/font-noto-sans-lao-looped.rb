86acb3fa8136c61a72ade01936ae0e44f7eb852d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:33 +0000

    font-noto-sans-lao-looped updated

--- Formula/font-noto-sans-lao-looped.rb
+++ Formula/font-noto-sans-lao-looped.rb
@@ -1,5 +1,5 @@
 class FontNotoSansLaoLooped < Formula
-  desc "Looped variant of the southeast asian lao script"
+  desc "Noto sans lao looped font"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Lao+Looped"
   head "https://github.com/google/fonts/raw/main/ofl/notosanslaolooped/NotoSansLaoLooped%5Bwdth%2Cwght%5D.ttf",
        verified: "github.com/google/fonts/"
