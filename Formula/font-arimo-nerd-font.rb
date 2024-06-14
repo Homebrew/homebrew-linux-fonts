8e94e5bf431a086037748b8a25ff8c0300d8063c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:14 +0000

    font-arimo-nerd-font updated

--- Formula/font-arimo-nerd-font.rb
+++ Formula/font-arimo-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontArimoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Arimo nerd font (arimo) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Arimo.zip"
   version "3.2.1"
