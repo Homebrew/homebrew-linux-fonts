6c8f1cd7eafd2fa27d6d512b42e6e71654399556
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:34 +0000

    font-noto-serif-khojki updated

--- Formula/font-noto-serif-khojki.rb
+++ Formula/font-noto-serif-khojki.rb
@@ -1,5 +1,5 @@
 class FontNotoSerifKhojki < Formula
-  desc "Modulated (“serif”) design for texts in the indic khojki script"
+  desc "Noto serif khojki font"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Khojki"
   head "https://github.com/google/fonts/raw/main/ofl/notoserifkhojki/NotoSerifKhojki%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
