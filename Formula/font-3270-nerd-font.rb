22d3b550ea0f57740f6bc238f06a76fa758f9d9e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:12 +0000

    font-3270-nerd-font updated

--- Formula/font-3270-nerd-font.rb
+++ Formula/font-3270-nerd-font.rb
@@ -1,5 +1,5 @@
 class Font3270NerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "3270 nerd font (ibm 3270) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/3270.zip"
   version "3.2.1"
