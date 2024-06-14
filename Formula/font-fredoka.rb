20a75634945db3701f4b5b750cc15d5ed6e49972
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:21 +0000

    font-fredoka updated

--- Formula/font-fredoka.rb
+++ Formula/font-fredoka.rb
@@ -1,5 +1,5 @@
 class FontFredoka < Formula
-  desc "Led by ben nathan, a typeer design foundry based in israel"
+  desc "Fredoka font"
   homepage "https://fonts.google.com/specimen/Fredoka"
   head "https://github.com/google/fonts/raw/main/ofl/fredoka/Fredoka%5Bwdth%2Cwght%5D.ttf",
        verified: "github.com/google/fonts/"
