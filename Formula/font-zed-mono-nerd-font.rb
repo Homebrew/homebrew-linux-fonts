b6b066d2e55c215d7a7cd9872473e434a78ea70d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:44 +0000

    font-zed-mono-nerd-font updated

--- Formula/font-zed-mono-nerd-font.rb
+++ Formula/font-zed-mono-nerd-font.rb
@@ -1,5 +1,5 @@
 class FontZedMonoNerdFont < Formula
-  desc "Developer targeted fonts with a high number of glyphs"
+  desc "Zedmono nerd font (zed mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/ZedMono.zip"
   version "3.2.1"
