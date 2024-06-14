2b2c8a2e7c7ca809f01e38280a489fc65679761b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-14 00:20:26 +0000

    font-iosevka-ss12 updated

--- Formula/font-iosevka-ss12.rb
+++ Formula/font-iosevka-ss12.rb
@@ -1,5 +1,5 @@
 class FontIosevkaSs12 < Formula
-  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  desc "Iosevka ss12 font"
   homepage "https://github.com/be5invis/Iosevka/"
   url "https://github.com/be5invis/Iosevka/releases/download/v30.1.2/PkgTTC-IosevkaSS12-30.1.2.zip"
   version "30.1.2"
