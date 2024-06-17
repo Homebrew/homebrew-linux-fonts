61c6bb9a6b5a9d258a962f1fb834566681f88867
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:32 +0000

    font-playwrite-id updated

--- Formula/font-playwrite-id.rb
+++ Formula/font-playwrite-id.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteId < Formula
   desc "Playwrite id font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteid/PlaywriteID%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+ID"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteid/PlaywriteID%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteID[wght].ttf")[0]
