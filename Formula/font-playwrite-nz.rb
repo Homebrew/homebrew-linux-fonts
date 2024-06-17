1c31c833a3098d8e2eae6382586d3f69b6562504
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:32 +0000

    font-playwrite-nz updated

--- Formula/font-playwrite-nz.rb
+++ Formula/font-playwrite-nz.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteNz < Formula
   desc "Playwrite nz font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritenz/PlaywriteNZ%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+NZ"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritenz/PlaywriteNZ%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteNZ[wght].ttf")[0]
