79841fb611607be7a3db099380faf906ea6732f3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:32 +0000

    font-playwrite-no updated

--- Formula/font-playwrite-no.rb
+++ Formula/font-playwrite-no.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteNo < Formula
   desc "Playwrite no font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteno/PlaywriteNO%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+NO"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteno/PlaywriteNO%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteNO[wght].ttf")[0]
