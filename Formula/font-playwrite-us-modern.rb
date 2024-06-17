8b122b537bf8d2d49800a47a62110a16168d121e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:33 +0000

    font-playwrite-us-modern updated

--- Formula/font-playwrite-us-modern.rb
+++ Formula/font-playwrite-us-modern.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteUsModern < Formula
   desc "Playwrite us modern font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteusmodern/PlaywriteUSModern%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+US+Modern"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteusmodern/PlaywriteUSModern%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteUSModern[wght].ttf")[0]
