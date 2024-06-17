ef55e70b7cbae9b844511993ccc1d4217fec57c5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:31 +0000

    font-playwrite-au-nsw updated

--- Formula/font-playwrite-au-nsw.rb
+++ Formula/font-playwrite-au-nsw.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteAuNsw < Formula
   desc "Playwrite au nsw font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteaunsw/PlaywriteAUNSW%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+AU+NSW"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteaunsw/PlaywriteAUNSW%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteAUNSW[wght].ttf")[0]
