c87cb3f669770b71a7b4ac41030356afc78b5fe3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:31 +0000

    font-playwrite-au-tas updated

--- Formula/font-playwrite-au-tas.rb
+++ Formula/font-playwrite-au-tas.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteAuTas < Formula
   desc "Playwrite au tas font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteautas/PlaywriteAUTAS%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+AU+TAS"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteautas/PlaywriteAUTAS%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteAUTAS[wght].ttf")[0]
