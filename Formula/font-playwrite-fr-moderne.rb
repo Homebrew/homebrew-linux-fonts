6c5251427b80e560131467d72ad6e5fab36056c0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:32 +0000

    font-playwrite-fr-moderne updated

--- Formula/font-playwrite-fr-moderne.rb
+++ Formula/font-playwrite-fr-moderne.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteFrModerne < Formula
   desc "Playwrite fr moderne font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritefrmoderne/PlaywriteFRModerne%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+FR+Moderne"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritefrmoderne/PlaywriteFRModerne%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteFRModerne[wght].ttf")[0]
