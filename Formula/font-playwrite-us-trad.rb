f87bd314b15d92593662f94359691b3763064b96
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:33 +0000

    font-playwrite-us-trad updated

--- Formula/font-playwrite-us-trad.rb
+++ Formula/font-playwrite-us-trad.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteUsTrad < Formula
   desc "Playwrite us trad font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteustrad/PlaywriteUSTrad%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+US+Trad"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteustrad/PlaywriteUSTrad%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteUSTrad[wght].ttf")[0]
