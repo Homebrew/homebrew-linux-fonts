3c2733c4d23f1c840899afa7cc1068946cd42960
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:32 +0000

    font-playwrite-fr-trad updated

--- Formula/font-playwrite-fr-trad.rb
+++ Formula/font-playwrite-fr-trad.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteFrTrad < Formula
   desc "Playwrite fr trad font"
-  homepage "https://github.com/TypeTogether/Playwrite/"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritefrtrad/PlaywriteFRTrad%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+FR+Trad"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritefrtrad/PlaywriteFRTrad%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteFRTrad[wght].ttf")[0]
