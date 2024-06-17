04cc1e6d2e51680ad2ba41e0b3213c32548250f9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:32 +0000

    font-playwrite-mx updated

--- Formula/font-playwrite-mx.rb
+++ Formula/font-playwrite-mx.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteMx < Formula
   desc "Playwrite mx font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritemx/PlaywriteMX%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+MX"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritemx/PlaywriteMX%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteMX[wght].ttf")[0]
