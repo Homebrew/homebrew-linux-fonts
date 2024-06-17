f5dad8691a0d21c868af051e6ee33154cb59cadd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:31 +0000

    font-playwrite-co updated

--- Formula/font-playwrite-co.rb
+++ Formula/font-playwrite-co.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteCo < Formula
   desc "Playwrite co font"
-  homepage "https://github.com/TypeTogether/Playwrite/"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteco/PlaywriteCO%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+CO"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteco/PlaywriteCO%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteCO[wght].ttf")[0]
