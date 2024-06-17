20e071c3576e607a22f90da010459fed80f8af32
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:31 +0000

    font-playwrite-de-va updated

--- Formula/font-playwrite-de-va.rb
+++ Formula/font-playwrite-de-va.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteDeVa < Formula
   desc "Playwrite de va font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritedeva/PlaywriteDEVA%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+DE+VA"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritedeva/PlaywriteDEVA%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteDEVA[wght].ttf")[0]
