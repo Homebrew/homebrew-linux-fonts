c89b6585bf1f15fad44104a66cdaf945cb0fb38e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:32 +0000

    font-playwrite-in updated

--- Formula/font-playwrite-in.rb
+++ Formula/font-playwrite-in.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteIn < Formula
   desc "Playwrite in font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritein/PlaywriteIN%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+IN"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritein/PlaywriteIN%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteIN[wght].ttf")[0]
