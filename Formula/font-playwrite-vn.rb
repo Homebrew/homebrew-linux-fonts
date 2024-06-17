8e3523bea1bd415e55660ad14d8d0b5cc7a5b6e0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:33 +0000

    font-playwrite-vn updated

--- Formula/font-playwrite-vn.rb
+++ Formula/font-playwrite-vn.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteVn < Formula
   desc "Playwrite vn font"
-  homepage "https://github.com/vv-monsalve/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritevn/PlaywriteVN%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+VN"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritevn/PlaywriteVN%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteVN[wght].ttf")[0]
