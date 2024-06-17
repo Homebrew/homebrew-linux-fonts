7119fc89a45bf6b979d8926c89a620efa7064883
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:32 +0000

    font-playwrite-ro updated

--- Formula/font-playwrite-ro.rb
+++ Formula/font-playwrite-ro.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteRo < Formula
   desc "Playwrite ro font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritero/PlaywriteRO%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+RO"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritero/PlaywriteRO%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteRO[wght].ttf")[0]
