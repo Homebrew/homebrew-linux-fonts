239de70221de5b5aecfdb2ca0dead5db00d9b45d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:33 +0000

    font-playwrite-za updated

--- Formula/font-playwrite-za.rb
+++ Formula/font-playwrite-za.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteZa < Formula
   desc "Playwrite za font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteza/PlaywriteZA%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+ZA"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteza/PlaywriteZA%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteZA[wght].ttf")[0]
