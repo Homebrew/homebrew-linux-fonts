594645c1fe71933fbb06a32182584aaee21fa459
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:32 +0000

    font-playwrite-pt updated

--- Formula/font-playwrite-pt.rb
+++ Formula/font-playwrite-pt.rb
@@ -1,7 +1,8 @@
 class FontPlaywritePt < Formula
   desc "Playwrite pt font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritept/PlaywritePT%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+PT"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritept/PlaywritePT%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywritePT[wght].ttf")[0]
