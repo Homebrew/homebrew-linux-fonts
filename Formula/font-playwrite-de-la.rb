775131e9d7b69a746e828075230526dadfd08e2d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-17 00:21:31 +0000

    font-playwrite-de-la updated

--- Formula/font-playwrite-de-la.rb
+++ Formula/font-playwrite-de-la.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteDeLa < Formula
   desc "Playwrite de la font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritedela/PlaywriteDELA%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+DE+LA"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritedela/PlaywriteDELA%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteDELA[wght].ttf")[0]
