19d72ed7f6d1a1dfe95df1dfa64b2ed9c4c434e0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-24 00:21:55 +0000

    font-miriam-libre updated

--- Formula/font-miriam-libre.rb
+++ Formula/font-miriam-libre.rb
@@ -1,14 +1,11 @@
 class FontMiriamLibre < Formula
   desc "Miriam libre font"
   homepage "https://fonts.google.com/specimen/Miriam+Libre"
-  head "https://github.com/google/fonts.git",
-       verified:  "github.com/google/fonts",
-       branch:    "main",
-       only_path: "ofl/miriamlibre"
+  head "https://github.com/google/fonts/raw/main/ofl/miriamlibre/MiriamLibre%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/miriamlibre/./**/MiriamLibre-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/miriamlibre/./**/MiriamLibre-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MiriamLibre[wght].ttf")[0]
   end
 
   test do
