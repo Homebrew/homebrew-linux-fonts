8f8099c8d9d07b8054a734aaeb231eb460431f28
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:15 +0000

    font-gnu-unifont updated

--- Formula/font-gnu-unifont.rb
+++ Formula/font-gnu-unifont.rb
@@ -1,8 +1,8 @@
 class FontGnuUnifont < Formula
   desc "Gnu unifont font"
   homepage "https://unifoundry.com/unifont.html"
-  url "https://ftpmirror.gnu.org/unifont/unifont-15.1.05/unifont-15.1.05.tar.gz",
-       verified: "ftpmirror.gnu.org/unifont/"
+  url "https://ftp.gnu.org/gnu/unifont/unifont-15.1.05/unifont-15.1.05.tar.gz",
+       verified: "ftp.gnu.org/gnu/unifont/"
   version "15.1.05"
   sha256 "d275f55f4358750e0f86305b92e87b88eb330aa46c15f553d2edf047fb1c23fa"
 
