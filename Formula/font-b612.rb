9b513831c70e66f036b3140bcad0adb3160ac513
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:15 +0000

    font-b612 updated

--- Formula/font-b612.rb
+++ Formula/font-b612.rb
@@ -1,10 +1,10 @@
 class FontB612 < Formula
   desc "B612 font"
   homepage "https://b612-font.com/"
-  url "https://github.com/polarsys/b612/archive/refs/tags/1.008.zip",
+  url "https://github.com/polarsys/b612/archive/refs/tags/1.008.tar.gz",
        verified: "github.com/polarsys/b612/"
   version "1.008"
-  sha256 "727cb91e47d65ac49c2d97d7b1c36d9891b885d9ddf06e15ad3d23d22bdad9cf"
+  sha256 "bfe4827931a22ecde5576c2bd06b462d467b89fa580b6ded651346f94eba8dd6"
 
   def install
     (share/"fonts").install Dir.glob("./**/b612-1.008/fonts/ttf/B612-Bold.ttf")[0]
