4507927f0b61bb00f86302a70a146222606aefc4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:16 +0000

    font-ia-writer-mono updated

--- Formula/font-ia-writer-mono.rb
+++ Formula/font-ia-writer-mono.rb
@@ -1,7 +1,7 @@
 class FontIaWriterMono < Formula
   desc "Ia writer mono font"
   homepage "https://github.com/iaolo/iA-Fonts"
-  head "https://codeload.github.com/iaolo/iA-Fonts/zip/master"
+  head "https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.tar.gz"
 
   def install
     (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Bold.ttf")[0]
