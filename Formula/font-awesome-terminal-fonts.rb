7446225c626c324bba05507fa38fd6fc87c43a84
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:15 +0000

    font-awesome-terminal-fonts updated

--- Formula/font-awesome-terminal-fonts.rb
+++ Formula/font-awesome-terminal-fonts.rb
@@ -1,7 +1,7 @@
 class FontAwesomeTerminalFonts < Formula
   desc "Awesome terminal fonts font"
   homepage "https://github.com/gabrielelana/awesome-terminal-fonts"
-  head "https://github.com/gabrielelana/awesome-terminal-fonts/archive/patching-strategy.tar.gz"
+  head "https://github.com/gabrielelana/awesome-terminal-fonts/archive/refs/heads/patching-strategy.tar.gz"
 
   def install
     (share/"fonts").install Dir.glob("./**/awesome-terminal-fonts-patching-strategy/patched/Droid+Sans+Mono+Awesome.ttf")[0]
