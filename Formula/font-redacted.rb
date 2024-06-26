9d97830da60079a4495dd369ff5a750ff05418d9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:17 +0000

    font-redacted updated

--- Formula/font-redacted.rb
+++ Formula/font-redacted.rb
@@ -1,13 +1,13 @@
 class FontRedacted < Formula
   desc "Redacted font"
   homepage "https://github.com/christiannaths/Redacted-Font"
-  head "https://github.com/christiannaths/Redacted-Font/archive/old-sources.zip"
+  head "https://github.com/christiannaths/Redacted-Font/archive/refs/heads/master.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Redacted-Font-old-sources/fonts/redacted-regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Redacted-Font-old-sources/fonts/redacted-script-bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Redacted-Font-old-sources/fonts/redacted-script-light.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Redacted-Font-old-sources/fonts/redacted-script-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/redacted-font-master/Redacted/fonts/ttf/Redacted-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/redacted-font-master/RedactedScript/fonts/ttf/RedactedScript-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/redacted-font-master/RedactedScript/fonts/ttf/RedactedScript-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/redacted-font-master/RedactedScript/fonts/ttf/RedactedScript-Regular.ttf")[0]
   end
 
   test do
