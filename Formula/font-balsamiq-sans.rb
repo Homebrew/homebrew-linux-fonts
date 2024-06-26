7c3e12ce701027af01fe4f326d78e8f84e4c2fbd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:15 +0000

    font-balsamiq-sans updated

--- Formula/font-balsamiq-sans.rb
+++ Formula/font-balsamiq-sans.rb
@@ -1,14 +1,16 @@
 class FontBalsamiqSans < Formula
   desc "Balsamiq sans font"
   homepage "https://balsamiq.com/givingback/opensource/font/"
-  head "https://github.com/balsamiq/balsamiqsans/archive/master.zip",
-       verified: "github.com/balsamiq/balsamiqsans/"
+  head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
+       branch:    "main",
+       only_path: "ofl/balsamiqsans"
 
   def install
-    (share/"fonts").install Dir.glob("./**/balsamiqsans-master/fonts/ttf/BalsamiqSans-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/balsamiqsans-master/fonts/ttf/BalsamiqSans-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/balsamiqsans-master/fonts/ttf/BalsamiqSans-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/balsamiqsans-master/fonts/ttf/BalsamiqSans-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/balsamiqsans/./**/BalsamiqSans-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/balsamiqsans/./**/BalsamiqSans-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/balsamiqsans/./**/BalsamiqSans-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/balsamiqsans/./**/BalsamiqSans-Regular.ttf")[0]
   end
 
   test do
