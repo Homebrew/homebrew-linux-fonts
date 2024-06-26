12af132c00c4073d76f40f0a2e505419ac6b921c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:16 +0000

    font-icomoon updated

--- Formula/font-icomoon.rb
+++ Formula/font-icomoon.rb
@@ -1,7 +1,7 @@
 class FontIcomoon < Formula
   desc "Icomoon font"
   homepage "https://icomoon.io/"
-  head "https://github.com/Keyamoon/IcoMoon-Free/archive/master.zip",
+  head "https://github.com/Keyamoon/IcoMoon-Free/archive/refs/heads/master.tar.gz",
        verified: "github.com/Keyamoon/IcoMoon-Free/"
 
   def install
