5b2ce19ae934191af3c41f561765a8c1fdb34d14
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:15 +0000

    font-devicons updated

--- Formula/font-devicons.rb
+++ Formula/font-devicons.rb
@@ -1,10 +1,10 @@
 class FontDevicons < Formula
   desc "Devicons font"
   homepage "https://vorillaz.github.io/devicons/"
-  url "https://github.com/vorillaz/devicons/archive/1.8.0.zip",
+  url "https://github.com/vorillaz/devicons/archive/refs/tags/1.8.0.tar.gz",
        verified: "github.com/vorillaz/devicons/"
   version "1.8.0"
-  sha256 "fc0baa260f54832c059d1d9eab3798ae758d1a1cf0c1695e9883aab85d9a4308"
+  sha256 "d8d2dc243ca42897a082ffe32a22cab53cdd148cf87b24162cf450ccfc12fece"
 
   def install
     (share/"fonts").install Dir.glob("./**/devicons-1.8.0/fonts/devicons.ttf")[0]
