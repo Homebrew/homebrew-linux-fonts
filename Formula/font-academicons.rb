23aa044618618ef71b22a91018ba3fbc1870e598
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:15 +0000

    font-academicons updated

--- Formula/font-academicons.rb
+++ Formula/font-academicons.rb
@@ -1,9 +1,9 @@
 class FontAcademicons < Formula
   desc "Academicons font"
   homepage "https://github.com/jpswalsh/academicons"
-  url "https://github.com/jpswalsh/academicons/archive/v1.9.4.zip"
+  url "https://github.com/jpswalsh/academicons/archive/refs/tags/v1.9.4.tar.gz"
   version "1.9.4"
-  sha256 "6f27234f174e9a8872296b1778083393127773ddcb19f0b3cc5ab3bfa42c37e9"
+  sha256 "5b6aeecaaf2a63d23b07b722f3e1be2ae1a12f433677704e73a6b8f564943784"
 
   def install
     (share/"fonts").install Dir.glob("./**/academicons-1.9.4/fonts/academicons.ttf")[0]
