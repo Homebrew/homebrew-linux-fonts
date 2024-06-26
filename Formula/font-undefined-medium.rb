30f82273f53156a4bb17d14c839e4b0f907d02a5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-26 00:20:17 +0000

    font-undefined-medium updated

--- Formula/font-undefined-medium.rb
+++ Formula/font-undefined-medium.rb
@@ -1,9 +1,9 @@
 class FontUndefinedMedium < Formula
   desc "Undefined medium font"
   homepage "https://github.com/andirueckel/undefined-medium"
-  url "https://github.com/andirueckel/undefined-medium/archive/refs/tags/v1.3.zip"
+  url "https://github.com/andirueckel/undefined-medium/archive/refs/tags/v1.3.tar.gz"
   version "1.3"
-  sha256 "a8e7d738177af224c0cd5f659a9504c2976ab0194c91be3777d01e80d0f121f8"
+  sha256 "a5e6682e165e70e10b575a468c00013038ec60bad4f1daa9cfb8415d3ce4d84d"
 
   def install
     (share/"fonts").install Dir.glob("./**/undefined-medium-1.3/fonts/otf/undefined-medium.otf")[0]
