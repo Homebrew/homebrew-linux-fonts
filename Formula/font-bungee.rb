faa688636ddd16c86c21b2c2a33a0e13d9bbbe63
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-06-30 00:22:43 +0000

    font-bungee updated

--- Formula/font-bungee.rb
+++ Formula/font-bungee.rb
@@ -1,10 +1,10 @@
 class FontBungee < Formula
   desc "Bungee font"
   homepage "https://djr.com/bungee/"
-  url "https://github.com/djrrb/bungee/releases/download/v2.000/Bungee-fonts.zip",
+  url "https://github.com/djrrb/bungee/releases/download/v2.001/Bungee-fonts.zip",
        verified: "github.com/djrrb/bungee/"
-  version "2.000"
-  sha256 "475c443ada44d5530debcf49703ef6871b968fbc0967676b3e1b0852b812b750"
+  version "2.001"
+  sha256 "df293931cb01e8a8737defbcee3eb853db2af67271005d65e4508d541b6f592c"
 
   def install
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Rotated/BungeeRotatedShade-Regular.ttf")[0]
@@ -12,8 +12,8 @@ class FontBungee < Formula
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeInline-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeOutline-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeShade-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Color/BungeeColor-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Color/BungeeSpice-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Color/BungeeTint-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayers-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayersInline-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayersOutline-Regular.ttf")[0]
