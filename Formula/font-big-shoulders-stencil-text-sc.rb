8e764934d305d8ceed0947a8dc90686e3c44ea11
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-01 00:24:07 +0000

    font-big-shoulders-stencil-text-sc (new font)

---
+++ Formula/font-big-shoulders-stencil-text-sc.rb
@@ -0,0 +1,13 @@
+class FontBigShouldersStencilTextSc < Formula
+  desc "Big shoulders stencil text sc font"
+  homepage "https://github.com/xotypeco/big_shoulders"
+  head "https://github.com/google/fonts/raw/main/ofl/bigshouldersstenciltextsc/BigShouldersStencilTextSC%5Bwght%5D.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/BigShouldersStencilTextSC[wght].ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
