f06ae0843a81a616e9c38355565b5fb437c00954
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-01 00:24:07 +0000

    font-big-shoulders-text-sc (new font)

---
+++ Formula/font-big-shoulders-text-sc.rb
@@ -0,0 +1,13 @@
+class FontBigShouldersTextSc < Formula
+  desc "Big shoulders text sc font"
+  homepage "https://github.com/xotypeco/big_shoulders"
+  head "https://github.com/google/fonts/raw/main/ofl/bigshoulderstextsc/BigShouldersTextSC%5Bwght%5D.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/BigShouldersTextSC[wght].ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
