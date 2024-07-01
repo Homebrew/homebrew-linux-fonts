3fb759111e214f975fdb18c8f9af7e0a69db80a2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-01 00:24:07 +0000

    font-big-shoulders-stencil-display-sc (new font)

---
+++ Formula/font-big-shoulders-stencil-display-sc.rb
@@ -0,0 +1,13 @@
+class FontBigShouldersStencilDisplaySc < Formula
+  desc "Big shoulders stencil display sc font"
+  homepage "https://github.com/xotypeco/big_shoulders"
+  head "https://github.com/google/fonts/raw/main/ofl/bigshouldersstencildisplaysc/BigShouldersStencilDisplaySC%5Bwght%5D.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/BigShouldersStencilDisplaySC[wght].ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
