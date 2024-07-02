8fdfbcd59fd95f377556f85c8f592e61e10a3a39
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-02 00:20:37 +0000

    font-iosevka-etoile updated

--- Formula/font-iosevka-etoile.rb
+++ Formula/font-iosevka-etoile.rb
@@ -1,9 +1,9 @@
 class FontIosevkaEtoile < Formula
   desc "Iosevka etoile font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v30.3.0/PkgTTC-IosevkaEtoile-30.3.0.zip"
-  version "30.3.0"
-  sha256 "a4e0b8f101deee3e9b1bb2f0d9dc92b8c79650d276d134f7efd255c769548c81"
+  url "https://github.com/be5invis/Iosevka/releases/download/v30.3.1/PkgTTC-IosevkaEtoile-30.3.1.zip"
+  version "30.3.1"
+  sha256 "07272e67a63331fc9ba71c7714888bd5d2c39a938f2a4ab823118e8aea90cafc"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Thin.ttc")[0]
