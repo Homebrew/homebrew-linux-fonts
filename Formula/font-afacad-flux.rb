aaaefe664405e302a08e4fd59f18f54d19c38d45
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-08 00:21:48 +0000

    font-afacad-flux (new font)

---
+++ Formula/font-afacad-flux.rb
@@ -0,0 +1,13 @@
+class FontAfacadFlux < Formula
+  desc "Afacad flux font"
+  homepage "https://github.com/Dicotype/Afacad"
+  head "https://github.com/google/fonts/raw/main/ofl/afacadflux/AfacadFlux%5Bslnt%2Cwght%5D.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/AfacadFlux[slnt,wght].ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
