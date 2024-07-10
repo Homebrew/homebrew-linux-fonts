71bdf1e87873fb4bd0c365e1406ce7d59e93ce60
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-10 00:21:09 +0000

    font-lxgw-heartserif updated

--- Formula/font-lxgw-heartserif.rb
+++ Formula/font-lxgw-heartserif.rb
@@ -1,9 +1,9 @@
 class FontLxgwHeartserif < Formula
   desc "Lxgw heart serif font"
   homepage "https://github.com/lxgw/LxgwNeoZhiSong"
-  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v0.924/LXGWHeartSerif.ttf"
-  version "0.924"
-  sha256 "0d4d3e6bbb6630af5142413405c38ae7bc4cd219348d1b8122d22903758891cc"
+  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v0.924.1/LXGWHeartSerif.ttf"
+  version "0.924.1"
+  sha256 "ad5808fcef71962064804e01024b637bdea44e8b57a482d2048cc02e805d1182"
 
   def install
     (share/"fonts").install Dir.glob("./**/LXGWHeartSerif.ttf")[0]
