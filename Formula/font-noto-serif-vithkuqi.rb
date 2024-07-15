2a9103192d3d01fd910918317eaaf894f57e45a1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:30 +0000

    font-noto-serif-vithkuqi updated

--- Formula/font-noto-serif-vithkuqi.rb
+++ Formula/font-noto-serif-vithkuqi.rb
@@ -5,7 +5,7 @@ class FontNotoSerifVithkuqi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifVithkuqi[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifVithkuqi?wght?.ttf")[0]
   end
 
   test do
