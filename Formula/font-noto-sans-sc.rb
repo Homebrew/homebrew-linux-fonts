86d20984f6c442b971631e667012929967b1b73a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:27 +0000

    font-noto-sans-sc updated

--- Formula/font-noto-sans-sc.rb
+++ Formula/font-noto-sans-sc.rb
@@ -5,7 +5,7 @@ class FontNotoSansSc < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansSC[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansSC?wght?.ttf")[0]
   end
 
   test do
