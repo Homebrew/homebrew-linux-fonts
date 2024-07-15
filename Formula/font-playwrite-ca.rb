bd34b38f294931c1608e1cfd83fad898dccb6bd6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:32 +0000

    font-playwrite-ca updated

--- Formula/font-playwrite-ca.rb
+++ Formula/font-playwrite-ca.rb
@@ -5,7 +5,7 @@ class FontPlaywriteCa < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteCA[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteCA?wght?.ttf")[0]
   end
 
   test do
