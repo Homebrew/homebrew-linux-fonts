4c460de10d7f222f2d970cf854c4fbc279c90fe8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:02 +0000

    font-baloo-tamma-2 updated

--- Formula/font-baloo-tamma-2.rb
+++ Formula/font-baloo-tamma-2.rb
@@ -5,7 +5,7 @@ class FontBalooTamma2 < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/BalooTamma2[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooTamma2?wght?.ttf")[0]
   end
 
   test do
