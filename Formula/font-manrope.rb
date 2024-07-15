8e5a5f0261649ae95e3bc251d1cccb9c91c8d047
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:22 +0000

    font-manrope updated

--- Formula/font-manrope.rb
+++ Formula/font-manrope.rb
@@ -5,7 +5,7 @@ class FontManrope < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Manrope[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Manrope?wght?.ttf")[0]
   end
 
   test do
