90ed7c5bdc5719138290997480749460cddff735
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:35 +0000

    font-quicksand updated

--- Formula/font-quicksand.rb
+++ Formula/font-quicksand.rb
@@ -5,7 +5,7 @@ class FontQuicksand < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Quicksand[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Quicksand?wght?.ttf")[0]
   end
 
   test do
