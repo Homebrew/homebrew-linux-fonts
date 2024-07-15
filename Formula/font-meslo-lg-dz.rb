2c15661d6186b42ad1afcadd8031a2f0eaee5203
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:23 +0000

    font-meslo-lg-dz updated

--- Formula/font-meslo-lg-dz.rb
+++ Formula/font-meslo-lg-dz.rb
@@ -6,18 +6,18 @@ class FontMesloLgDz < Formula
   sha256 "e5201a6816281872c23a2b2f43ac91dc8c1f9d91e9f4b82d64e9bfcb769ecb52"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGSDZ-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGLDZ-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGLDZ-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGLDZ-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGMDZ-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGMDZ-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGMDZ-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGMDZ-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGSDZ-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGSDZ-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGSDZ-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Meslo LG DZ v1.2.1/MesloLGLDZ-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGLDZ-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGLDZ-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGLDZ-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGLDZ-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGMDZ-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGMDZ-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGMDZ-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGMDZ-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGSDZ-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGSDZ-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGSDZ-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MesloLGSDZ-Regular.ttf")[0]
   end
 
   test do
