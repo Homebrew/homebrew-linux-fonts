3208f7006d728931a46c7dfa9793d769423df04c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:06 +0000

    font-commit-mono updated

--- Formula/font-commit-mono.rb
+++ Formula/font-commit-mono.rb
@@ -7,10 +7,10 @@ class FontCommitMono < Formula
   sha256 "f7d1f26a7c7554800a996f76f5d706bf0648b936ca2a66b5bc4d46e3a2c49ed0"
 
   def install
-    (share/"fonts").install Dir.glob("./**/CommitMono-1.143/CommitMono-400-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CommitMono-1.143/CommitMono-400-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CommitMono-1.143/CommitMono-700-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CommitMono-1.143/CommitMono-700-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CommitMono-400-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CommitMono-400-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CommitMono-700-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CommitMono-700-Regular.otf")[0]
   end
 
   test do
