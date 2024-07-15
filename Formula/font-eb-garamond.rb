47574012f50bf22f10686ffe23b20338b61b9f88
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:08 +0000

    font-eb-garamond updated

--- Formula/font-eb-garamond.rb
+++ Formula/font-eb-garamond.rb
@@ -7,16 +7,16 @@ class FontEbGaramond < Formula
   sha256 "a0b77b405f55c10cff078787ef9d2389a9638e7604d53aa80207fe62e104c378"
 
   def install
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramondSC12-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramond-InitialsF1.otf")[0]
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramond-InitialsF2.otf")[0]
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramond08-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramond08-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramond12-AllSC.otf")[0]
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramond12-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramond12-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramondSC08-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/EBGaramond-0.016/otf/EBGaramond-Initials.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramond-Initials.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramond-InitialsF1.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramond-InitialsF2.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramond08-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramond08-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramond12-AllSC.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramond12-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramond12-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramondSC08-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/EBGaramondSC12-Regular.otf")[0]
   end
 
   test do
