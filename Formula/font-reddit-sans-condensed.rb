e686697e4b1859b589e64295529bac31c70e0c3c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:36 +0000

    font-reddit-sans-condensed updated

--- Formula/font-reddit-sans-condensed.rb
+++ Formula/font-reddit-sans-condensed.rb
@@ -5,7 +5,7 @@ class FontRedditSansCondensed < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/RedditSansCondensed[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/RedditSansCondensed?wght?.ttf")[0]
   end
 
   test do
