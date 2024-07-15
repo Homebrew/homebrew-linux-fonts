c05cec2b6f53d43c48098ea507c6263fb9ada4c0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:18 +0000

    font-jaro updated

--- Formula/font-jaro.rb
+++ Formula/font-jaro.rb
@@ -5,7 +5,7 @@ class FontJaro < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Jaro[opsz].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Jaro?opsz?.ttf")[0]
   end
 
   test do
