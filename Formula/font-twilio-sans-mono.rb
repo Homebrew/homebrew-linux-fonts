d8bee1f9ec6223058b5188fb2ea3a5e5b0fb653c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:42 +0000

    font-twilio-sans-mono updated

--- Formula/font-twilio-sans-mono.rb
+++ Formula/font-twilio-sans-mono.rb
@@ -4,22 +4,22 @@ class FontTwilioSansMono < Formula
   head "https://github.com/twilio/twilio-sans-mono/raw/main/Twilio-Sans-Mono.zip"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-SemiboldItl.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-BoldItl.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-Extrabold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-ExtraboldItl.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-Heavy.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-HeavyItl.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-LightItl.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-MediumItl.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-RegularItl.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-Retina.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-RetinaItl.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-Semibold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Twilio-Sans-Mono/OTF/TwilioSansMono-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-BoldItl.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-Extrabold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-ExtraboldItl.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-Heavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-HeavyItl.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-LightItl.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-MediumItl.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-RegularItl.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-Retina.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-RetinaItl.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-Semibold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/OTF/TwilioSansMono-SemiboldItl.otf")[0]
   end
 
   test do
