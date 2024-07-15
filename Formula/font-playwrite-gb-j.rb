31bded10c00e58bd378f82f643b9671b398b91fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:33 +0000

    font-playwrite-gb-j updated

--- Formula/font-playwrite-gb-j.rb
+++ Formula/font-playwrite-gb-j.rb
@@ -7,8 +7,8 @@ class FontPlaywriteGbJ < Formula
        only_path: "ofl/playwritegbj"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/playwritegbj/./**/PlaywriteGBJ-Italic[wght].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/playwritegbj/./**/PlaywriteGBJ[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playwritegbj/./**/PlaywriteGBJ-Italic?wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playwritegbj/./**/PlaywriteGBJ?wght?.ttf")[0]
   end
 
   test do
