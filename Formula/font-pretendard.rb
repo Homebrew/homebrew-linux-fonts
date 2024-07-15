573c4c248a5921ff975754a6be78be459e352094
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:35 +0000

    font-pretendard updated

--- Formula/font-pretendard.rb
+++ Formula/font-pretendard.rb
@@ -6,15 +6,15 @@ class FontPretendard < Formula
   sha256 "04be351a74d6bf7d60c480a3087e51d185485d35a52023142af1df19eb8c428a"
 
   def install
-    (share/"fonts").install Dir.glob("./**/public/static/Pretendard-Thin.otf")[0]
-    (share/"fonts").install Dir.glob("./**/public/static/Pretendard-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/public/static/Pretendard-ExtraBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/public/static/Pretendard-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("./**/public/static/Pretendard-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/public/static/Pretendard-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/public/static/Pretendard-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/public/static/Pretendard-SemiBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/public/static/Pretendard-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/Pretendard-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/Pretendard-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/Pretendard-ExtraBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/Pretendard-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/Pretendard-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/Pretendard-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/Pretendard-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/Pretendard-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/Pretendard-Thin.otf")[0]
   end
 
   test do
