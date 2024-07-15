5e503bf8e2319af00d2bfc6748d62282bbb32d06
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:10 +0000

    font-firgenerd updated

--- Formula/font-firgenerd.rb
+++ Formula/font-firgenerd.rb
@@ -6,10 +6,10 @@ class FontFirgenerd < Formula
   sha256 "54cd76378fbc5025f42d441d95ca6ec1d3ecc4270e6107558840fed7c04cfe4f"
 
   def install
-    (share/"fonts").install Dir.glob("./**/FirgeNerd_v0.3.0/Firge35NerdConsole-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/FirgeNerd_v0.3.0/Firge35NerdConsole-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/FirgeNerd_v0.3.0/FirgeNerdConsole-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/FirgeNerd_v0.3.0/FirgeNerdConsole-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Firge35NerdConsole-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Firge35NerdConsole-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FirgeNerdConsole-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FirgeNerdConsole-Regular.ttf")[0]
   end
 
   test do
