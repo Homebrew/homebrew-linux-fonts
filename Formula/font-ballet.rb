57a7d87f716daff27bfeb556bf48c0740ba15135
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:01 +0000

    font-ballet updated

--- Formula/font-ballet.rb
+++ Formula/font-ballet.rb
@@ -5,7 +5,7 @@ class FontBallet < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Ballet[opsz].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Ballet?opsz?.ttf")[0]
   end
 
   test do
