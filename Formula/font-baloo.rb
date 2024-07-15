45121d4a634bf24dd146b74ba9cc16f1f12469c8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:02 +0000

    font-baloo updated

--- Formula/font-baloo.rb
+++ Formula/font-baloo.rb
@@ -6,16 +6,16 @@ class FontBaloo < Formula
   sha256 "86db29e7463475fd0ce93376fff7eefb3a787525b8113bca9662b9cdc1c3a139"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Fonts/BalooThambi-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Fonts/BalooBhai-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Fonts/BalooBhaijaan-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Fonts/BalooBhaina-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Fonts/BalooChettan-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Fonts/BalooDa-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Fonts/BalooPaaji-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Fonts/BalooTamma-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Fonts/BalooTammudu-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Fonts/Baloo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Baloo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooBhai-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooBhaijaan-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooBhaina-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooChettan-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooDa-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooPaaji-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooTamma-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooTammudu-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooThambi-Regular.ttf")[0]
   end
 
   test do
