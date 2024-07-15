dd466eb82ba64ae4ab2f33c4f13e5d848a85a9f9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:18 +0000

    font-kablammo updated

--- Formula/font-kablammo.rb
+++ Formula/font-kablammo.rb
@@ -5,7 +5,7 @@ class FontKablammo < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Kablammo[MORF].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Kablammo?MORF?.ttf")[0]
   end
 
   test do
