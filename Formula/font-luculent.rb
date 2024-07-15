2101e1133d998a757dcddb6b87090645036af9ae
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:21 +0000

    font-luculent updated

--- Formula/font-luculent.rb
+++ Formula/font-luculent.rb
@@ -4,10 +4,10 @@ class FontLuculent < Formula
   head "http://eastfarthing.com/luculent/luculent.tar.xz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/luculent/luculent.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/luculent/luculentb.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/luculent/luculentbi.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/luculent/luculenti.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/luculent.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/luculentb.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/luculentbi.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/luculenti.ttf")[0]
   end
 
   test do
