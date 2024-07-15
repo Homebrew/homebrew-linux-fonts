705c29153fd0698c7c20d60ea3e81a8e384dccde
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:04 +0000

    font-bungee updated

--- Formula/font-bungee.rb
+++ Formula/font-bungee.rb
@@ -7,21 +7,21 @@ class FontBungee < Formula
   sha256 "df293931cb01e8a8737defbcee3eb853db2af67271005d65e4508d541b6f592c"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Rotated/BungeeRotatedShade-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeHairline-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeInline-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeOutline-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/BungeeShade-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Color/BungeeSpice-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Color/BungeeTint-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayers-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayersInline-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayersOutline-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Layers/BungeeLayersShade-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Rotated/BungeeRotated-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Rotated/BungeeRotatedInline-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Rotated/BungeeRotatedOutline-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bungee-fonts/Bungee_Basic/Bungee-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Basic/Bungee-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Basic/BungeeHairline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Basic/BungeeInline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Basic/BungeeOutline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Basic/BungeeShade-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Color/BungeeSpice-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Color/BungeeTint-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Layers/BungeeLayers-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Layers/BungeeLayersInline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Layers/BungeeLayersOutline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Layers/BungeeLayersShade-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Rotated/BungeeRotated-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Rotated/BungeeRotatedInline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Rotated/BungeeRotatedOutline-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Bungee_Rotated/BungeeRotatedShade-Regular.ttf")[0]
   end
 
   test do
