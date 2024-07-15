4a6680138ab3c8c9bc1c7169b8eecbcd23eb8c8a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:19 +0000

    font-kameron updated

--- Formula/font-kameron.rb
+++ Formula/font-kameron.rb
@@ -5,7 +5,7 @@ class FontKameron < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Kameron[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Kameron?wght?.ttf")[0]
   end
 
   test do
