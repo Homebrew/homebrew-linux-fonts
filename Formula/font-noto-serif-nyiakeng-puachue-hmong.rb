f7357735c6ee19d5e77e2e65ec1202f4b5d71ab3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:29 +0000

    font-noto-serif-nyiakeng-puachue-hmong updated

--- Formula/font-noto-serif-nyiakeng-puachue-hmong.rb
+++ Formula/font-noto-serif-nyiakeng-puachue-hmong.rb
@@ -5,7 +5,7 @@ class FontNotoSerifNyiakengPuachueHmong < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifNyiakengPuachueHmong[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifNyiakengPuachueHmong?wght?.ttf")[0]
   end
 
   test do
