0ceaa1cdcfe16811960a5d7dc4dde5a34912d510
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:21 +0000

    font-lxgw-wenkai-tc updated

--- Formula/font-lxgw-wenkai-tc.rb
+++ Formula/font-lxgw-wenkai-tc.rb
@@ -6,12 +6,12 @@ class FontLxgwWenkaiTc < Formula
   sha256 "cbccbb37676487c3fddafb7da190bfab30e26ae57049478a3e474123d2cc47a3"
 
   def install
-    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-tc-v1.330/LXGWWenKaiMonoTC-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-tc-v1.330/LXGWWenKaiMonoTC-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-tc-v1.330/LXGWWenKaiMonoTC-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-tc-v1.330/LXGWWenKaiTC-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-tc-v1.330/LXGWWenKaiTC-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/lxgw-wenkai-tc-v1.330/LXGWWenKaiTC-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWWenKaiMonoTC-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWWenKaiMonoTC-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWWenKaiMonoTC-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWWenKaiTC-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWWenKaiTC-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWWenKaiTC-Regular.ttf")[0]
   end
 
   test do
