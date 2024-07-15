7a509a0a6bb757f332519c70b0bf197d8d557b12
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:30 +0000

    font-noto-serif updated

--- Formula/font-noto-serif.rb
+++ Formula/font-noto-serif.rb
@@ -7,8 +7,8 @@ class FontNotoSerif < Formula
   sha256 "fb4c6c75f10365f63b5c8ad5a1864ebe46dd0c70c40d0461cb0dc1b1b7c13a35"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerif/unhinted/variable/NotoSerif-Italic[wdth,wght].ttf")[0]
-    (share/"fonts").install Dir.glob("./**/NotoSerif/unhinted/variable/NotoSerif[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/unhinted/variable/NotoSerif-Italic?wdth,wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/unhinted/variable/NotoSerif?wdth,wght?.ttf")[0]
   end
 
   test do
