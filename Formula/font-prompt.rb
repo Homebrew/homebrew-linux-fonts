11613c7129ee436f6d4d7c97cf960ccb8b7cd316
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:35 +0000

    font-prompt updated

--- Formula/font-prompt.rb
+++ Formula/font-prompt.rb
@@ -7,7 +7,7 @@ class FontPrompt < Formula
        only_path: "ofl/prompt"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-BoldItalic.ttf")[0]
@@ -24,7 +24,7 @@ class FontPrompt < Formula
     (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/prompt/./**/Prompt-ThinItalic.ttf")[0]
   end
 
   test do
