371b7753596e903a125d3be8ff349d42abf515ab
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:06 +0000

    font-courier-prime updated

--- Formula/font-courier-prime.rb
+++ Formula/font-courier-prime.rb
@@ -4,10 +4,10 @@ class FontCourierPrime < Formula
   head "https://quoteunquoteapps.com/courierprime/downloads/courier-prime.zip"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Courier Prime/Courier Prime.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Courier Prime/Courier Prime Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Courier Prime/Courier Prime Bold Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Courier Prime/Courier Prime Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Courier Prime Bold Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Courier Prime Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Courier Prime Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Courier Prime.ttf")[0]
   end
 
   test do
