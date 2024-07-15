e36eb400104e87ed57d836dff2f7a140de49ca67
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:43 +0000

    font-workbench updated

--- Formula/font-workbench.rb
+++ Formula/font-workbench.rb
@@ -5,7 +5,7 @@ class FontWorkbench < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Workbench[BLED,SCAN].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Workbench?BLED,SCAN?.ttf")[0]
   end
 
   test do
