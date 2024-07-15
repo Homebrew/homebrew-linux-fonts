2a27d4c71042df3c06fe30bb055257ef7432c4e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:21:59 +0000

    font-anek-bangla updated

--- Formula/font-anek-bangla.rb
+++ Formula/font-anek-bangla.rb
@@ -5,7 +5,7 @@ class FontAnekBangla < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/AnekBangla[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekBangla?wdth,wght?.ttf")[0]
   end
 
   test do
