2ead8f53fbde35e854af71928e85be7e7000ae83
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:01 +0000

    font-baloo-bhai-2 updated

--- Formula/font-baloo-bhai-2.rb
+++ Formula/font-baloo-bhai-2.rb
@@ -5,7 +5,7 @@ class FontBalooBhai2 < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/BalooBhai2[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooBhai2?wght?.ttf")[0]
   end
 
   test do
