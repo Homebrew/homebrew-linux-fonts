4cabca8442f9d426b7e0cb9820f3bbb95561870f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:32 +0000

    font-playwrite-cz updated

--- Formula/font-playwrite-cz.rb
+++ Formula/font-playwrite-cz.rb
@@ -1,10 +1,11 @@
 class FontPlaywriteCz < Formula
   desc "Playwrite cz font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritecz/PlaywriteCZ%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+CZ"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritecz/PlaywriteCZ%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteCZ[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteCZ?wght?.ttf")[0]
   end
 
   test do
