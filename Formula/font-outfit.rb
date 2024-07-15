9f182c9173bc72782ef01fc5d16615ba294dfb78
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:31 +0000

    font-outfit updated

--- Formula/font-outfit.rb
+++ Formula/font-outfit.rb
@@ -5,7 +5,7 @@ class FontOutfit < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Outfit[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Outfit?wght?.ttf")[0]
   end
 
   test do
