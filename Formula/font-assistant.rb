dd4eb06e8b18e3ce68c1086b41412993c0ad9066
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:01 +0000

    font-assistant updated

--- Formula/font-assistant.rb
+++ Formula/font-assistant.rb
@@ -5,7 +5,7 @@ class FontAssistant < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Assistant[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Assistant?wght?.ttf")[0]
   end
 
   test do
