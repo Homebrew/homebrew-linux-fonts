cc6540a8f24602500e6061852b44aaccbbcf1705
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:20 +0000

    font-league-gothic updated

--- Formula/font-league-gothic.rb
+++ Formula/font-league-gothic.rb
@@ -7,10 +7,10 @@ class FontLeagueGothic < Formula
   sha256 "a1d9d3aaba2abda2791eaad03d51dbfd97aa6f6f2ea23c3b2b2b31f90d8cbeb5"
 
   def install
-    (share/"fonts").install Dir.glob("./**/LeagueGothic-1.601/static/OTF/LeagueGothic-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueGothic-1.601/static/OTF/LeagueGothic-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueGothic-1.601/static/OTF/LeagueGothic-Condensed.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueGothic-1.601/static/OTF/LeagueGothic-CondensedItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueGothic-Condensed.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueGothic-CondensedItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueGothic-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueGothic-Regular.otf")[0]
   end
 
   test do
