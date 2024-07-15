d5acdabcd1026691c3ef4e9d715734c1b813bd59
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:01 +0000

    font-awesome-terminal-fonts updated

--- Formula/font-awesome-terminal-fonts.rb
+++ Formula/font-awesome-terminal-fonts.rb
@@ -4,9 +4,9 @@ class FontAwesomeTerminalFonts < Formula
   head "https://github.com/gabrielelana/awesome-terminal-fonts/archive/refs/heads/patching-strategy.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/awesome-terminal-fonts-patching-strategy/patched/Droid+Sans+Mono+Awesome.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/awesome-terminal-fonts-patching-strategy/patched/Inconsolata+Awesome.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/awesome-terminal-fonts-patching-strategy/patched/SourceCodePro+Powerline+Awesome+Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/patched/Droid+Sans+Mono+Awesome.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/patched/Inconsolata+Awesome.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/patched/SourceCodePro+Powerline+Awesome+Regular.ttf")[0]
   end
 
   test do
