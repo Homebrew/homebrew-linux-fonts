5dfe41fe45684f232a35641edc3b0f6e388f682e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:34 +0000

    font-playwrite-ng-modern updated

--- Formula/font-playwrite-ng-modern.rb
+++ Formula/font-playwrite-ng-modern.rb
@@ -5,7 +5,7 @@ class FontPlaywriteNgModern < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteNGModern[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteNGModern?wght?.ttf")[0]
   end
 
   test do
