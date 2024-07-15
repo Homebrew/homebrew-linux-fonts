817aeeefc75c19f11448e73d8511184d4acfcccd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:26 +0000

    font-noto-sans-khmer-ui updated

--- Formula/font-noto-sans-khmer-ui.rb
+++ Formula/font-noto-sans-khmer-ui.rb
@@ -5,7 +5,7 @@ class FontNotoSansKhmerUi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansKhmerUI[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansKhmerUI?wdth,wght?.ttf")[0]
   end
 
   test do
