dd8c2dc2f0d81d9da54c135dcf48d4d826c0c55e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:40 +0000

    font-tex-gyre-adventor updated

--- Formula/font-tex-gyre-adventor.rb
+++ Formula/font-tex-gyre-adventor.rb
@@ -6,10 +6,10 @@ class FontTexGyreAdventor < Formula
   sha256 "9e619eb1c8af5cb55240f8bb865453562a2efd9059dee39d085fb71f7a00f7a2"
 
   def install
-    (share/"fonts").install Dir.glob("./**/qag2_501otf/texgyreadventor-bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/qag2_501otf/texgyreadventor-bolditalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/qag2_501otf/texgyreadventor-italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/qag2_501otf/texgyreadventor-regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyreadventor-bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyreadventor-bolditalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyreadventor-italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyreadventor-regular.otf")[0]
   end
 
   test do
