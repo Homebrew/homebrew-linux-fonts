686dd9dae2a416897c069aced017f192a44ec5db
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:11 +0000

    font-fontawesome updated

--- Formula/font-fontawesome.rb
+++ Formula/font-fontawesome.rb
@@ -7,9 +7,9 @@ class FontFontawesome < Formula
   sha256 "6392bc956eb3d391c9d7a14e891ce8010226ffc0c75f1338db126f13cb9cb8f4"
 
   def install
-    (share/"fonts").install Dir.glob("./**/fontawesome-free-6.5.2-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fontawesome-free-6.5.2-desktop/otfs/Font Awesome 6 Free-Regular-400.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fontawesome-free-6.5.2-desktop/otfs/Font Awesome 6 Free-Solid-900.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otfs/Font Awesome 6 Brands-Regular-400.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otfs/Font Awesome 6 Free-Regular-400.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otfs/Font Awesome 6 Free-Solid-900.otf")[0]
   end
 
   test do
