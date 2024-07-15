a6b45c80178276bb435c61ffbd4a88f115d82069
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:33 +0000

    font-playwrite-dk-loopet updated

--- Formula/font-playwrite-dk-loopet.rb
+++ Formula/font-playwrite-dk-loopet.rb
@@ -1,10 +1,11 @@
 class FontPlaywriteDkLoopet < Formula
   desc "Playwrite dk loopet font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritedkloopet/PlaywriteDKLoopet%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+DK+Loopet"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritedkloopet/PlaywriteDKLoopet%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteDKLoopet[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteDKLoopet?wght?.ttf")[0]
   end
 
   test do
