b8c8582a70aed056919e62d125f411cf36f9e6f3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:22 +0000

    font-m-plus-2 updated

--- Formula/font-m-plus-2.rb
+++ Formula/font-m-plus-2.rb
@@ -5,7 +5,7 @@ class FontMPlus2 < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/MPLUS2[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MPLUS2?wght?.ttf")[0]
   end
 
   test do
