397ce305584f7b13f85e53f39d3dc23389df0a6a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:33 +0000

    font-playwrite-is updated

--- Formula/font-playwrite-is.rb
+++ Formula/font-playwrite-is.rb
@@ -5,7 +5,7 @@ class FontPlaywriteIs < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteIS[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteIS?wght?.ttf")[0]
   end
 
   test do
