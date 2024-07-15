c032187f08a0e55d058bc9ee5592a45dff9db439
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:21:59 +0000

    font-alkatra updated

--- Formula/font-alkatra.rb
+++ Formula/font-alkatra.rb
@@ -5,7 +5,7 @@ class FontAlkatra < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Alkatra[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Alkatra?wght?.ttf")[0]
   end
 
   test do
