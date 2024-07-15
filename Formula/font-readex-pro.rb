59e9ce9e6a5ba00f3a7b6876a7808e5bbc5655b5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:36 +0000

    font-readex-pro updated

--- Formula/font-readex-pro.rb
+++ Formula/font-readex-pro.rb
@@ -5,7 +5,7 @@ class FontReadexPro < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/ReadexPro[HEXP,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ReadexPro?HEXP,wght?.ttf")[0]
   end
 
   test do
