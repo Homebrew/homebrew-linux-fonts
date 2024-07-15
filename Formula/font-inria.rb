e1a55f184abf885de1131c9c7de363983560220f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:16 +0000

    font-inria updated

--- Formula/font-inria.rb
+++ Formula/font-inria.rb
@@ -4,18 +4,18 @@ class FontInria < Formula
   head "https://github.com/BlackFoundry/InriaFonts/archive/refs/heads/master.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSans/OTF/InriaSans-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSans/OTF/InriaSans-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSerif/OTF/InriaSerif-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/InriaFonts-master/fonts/InriaSans/OTF/InriaSans-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSans/OTF/InriaSans-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSans/OTF/InriaSans-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSans/OTF/InriaSans-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSans/OTF/InriaSans-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSans/OTF/InriaSans-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSans/OTF/InriaSans-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSerif/OTF/InriaSerif-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSerif/OTF/InriaSerif-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSerif/OTF/InriaSerif-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSerif/OTF/InriaSerif-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSerif/OTF/InriaSerif-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/InriaSerif/OTF/InriaSerif-Regular.otf")[0]
   end
 
   test do
