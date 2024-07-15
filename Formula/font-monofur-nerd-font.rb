fdc27a536199ef052fc60d362134de7334301c58
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:24 +0000

    font-monofur-nerd-font updated

--- Formula/font-monofur-nerd-font.rb
+++ Formula/font-monofur-nerd-font.rb
@@ -6,7 +6,7 @@ class FontMonofurNerdFont < Formula
   sha256 "c63786a1ef1a5b2b0bd5cdd80722d9c8ebe81f3830a5a492b54c07db9022fec6"
 
   def install
-    (share/"fonts").install Dir.glob("./**/MonofurNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MonofurNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MonofurNerdFont-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MonofurNerdFont-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MonofurNerdFontMono-Bold.ttf")[0]
@@ -14,7 +14,7 @@ class FontMonofurNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/MonofurNerdFontMono-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MonofurNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MonofurNerdFontPropo-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/MonofurNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MonofurNerdFontPropo-Regular.ttf")[0]
   end
 
   test do
