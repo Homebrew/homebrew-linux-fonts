c8202300ce2b68c05e3b795638d1a7122edc2db1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:15 +0000

    font-iming updated

--- Formula/font-iming.rb
+++ Formula/font-iming.rb
@@ -6,12 +6,12 @@ class FontIming < Formula
   sha256 "ed6bd2853c89d07670859149116ffdbc832b1d66756b3edb95676b9b1c84f1a2"
 
   def install
-    (share/"fonts").install Dir.glob("./**/I.Ming-8.10/8.10/I.Ming-8.10.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/I.Ming-8.10/8.10/I.MingCP-8.10.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/I.Ming-8.10/8.10/I.MingVar-8.10.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/I.Ming-8.10/8.10/I.MingVarCP-8.10.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/I.Ming-8.10/8.10/PMingI.U-8.10.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/I.Ming-8.10/8.10/PMingI.UVar-8.10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/8.10/I.Ming-8.10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/8.10/I.MingCP-8.10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/8.10/I.MingVar-8.10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/8.10/I.MingVarCP-8.10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/8.10/PMingI.U-8.10.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/8.10/PMingI.UVar-8.10.ttf")[0]
   end
 
   test do
