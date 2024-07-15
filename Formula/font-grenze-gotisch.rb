0d43846aea8b0800c71b3bb82093aebd10ddf20c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:13 +0000

    font-grenze-gotisch updated

--- Formula/font-grenze-gotisch.rb
+++ Formula/font-grenze-gotisch.rb
@@ -5,7 +5,7 @@ class FontGrenzeGotisch < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/GrenzeGotisch[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GrenzeGotisch?wght?.ttf")[0]
   end
 
   test do
