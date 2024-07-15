ab0ba5acb718b768661f16b6989c0943103b1eac
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:28 +0000

    font-noto-sans-tc updated

--- Formula/font-noto-sans-tc.rb
+++ Formula/font-noto-sans-tc.rb
@@ -5,7 +5,7 @@ class FontNotoSansTc < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansTC[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansTC?wght?.ttf")[0]
   end
 
   test do
