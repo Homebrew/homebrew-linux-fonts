f7c974471c6b9bcd7b94891268892f8b4fc94388
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:21 +0000

    font-literata updated

--- Formula/font-literata.rb
+++ Formula/font-literata.rb
@@ -7,8 +7,8 @@ class FontLiterata < Formula
        only_path: "ofl/literata"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/literata/./**/Literata-Italic[opsz,wght].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/literata/./**/Literata[opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/literata/./**/Literata-Italic?opsz,wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/literata/./**/Literata?opsz,wght?.ttf")[0]
   end
 
   test do
