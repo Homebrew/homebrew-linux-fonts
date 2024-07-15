93a808a21ecdb0ace547833bea0f621cbc87d382
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:33 +0000

    font-playwrite-hr updated

--- Formula/font-playwrite-hr.rb
+++ Formula/font-playwrite-hr.rb
@@ -1,10 +1,11 @@
 class FontPlaywriteHr < Formula
   desc "Playwrite hr font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritehr/PlaywriteHR%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+HR"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritehr/PlaywriteHR%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteHR[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteHR?wght?.ttf")[0]
   end
 
   test do
