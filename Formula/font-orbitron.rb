e4a590e2c861952e11d8dbb7f0c4e6321881a9b3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:31 +0000

    font-orbitron updated

--- Formula/font-orbitron.rb
+++ Formula/font-orbitron.rb
@@ -5,7 +5,7 @@ class FontOrbitron < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Orbitron[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Orbitron?wght?.ttf")[0]
   end
 
   test do
