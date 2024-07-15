ecbc5a865882fe1fe64a1d18089e2343db56a4ec
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:00 +0000

    font-antonio updated

--- Formula/font-antonio.rb
+++ Formula/font-antonio.rb
@@ -5,7 +5,7 @@ class FontAntonio < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Antonio[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Antonio?wght?.ttf")[0]
   end
 
   test do
