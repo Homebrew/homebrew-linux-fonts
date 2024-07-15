e7a53d08a0d724f155d14ca3ca8b8e20ed9db147
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:00 +0000

    font-anonymous-pro updated

--- Formula/font-anonymous-pro.rb
+++ Formula/font-anonymous-pro.rb
@@ -6,10 +6,10 @@ class FontAnonymousPro < Formula
   sha256 "6d165fd08fe2827f38656c26f644eb1f5d6a0f1cb067cb758c7f295a333eb112"
 
   def install
-    (share/"fonts").install Dir.glob("./**/AnonymousPro-1.002.001/Anonymous Pro B.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/AnonymousPro-1.002.001/Anonymous Pro BI.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/AnonymousPro-1.002.001/Anonymous Pro I.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/AnonymousPro-1.002.001/Anonymous Pro.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Anonymous Pro B.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Anonymous Pro BI.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Anonymous Pro I.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Anonymous Pro.ttf")[0]
   end
 
   test do
