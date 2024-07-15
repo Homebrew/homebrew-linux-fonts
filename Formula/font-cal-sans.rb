72f890c96e3f3d36007d874b52ba109a781c4c5c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:04 +0000

    font-cal-sans updated

--- Formula/font-cal-sans.rb
+++ Formula/font-cal-sans.rb
@@ -6,7 +6,7 @@ class FontCalSans < Formula
   sha256 "888fd98adfb1e6cd5606dd85881163d11c9c4007eaf01d015262893a48983b01"
 
   def install
-    (share/"fonts").install Dir.glob("./**/fonts/otf/CalSans-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/CalSans-SemiBold.otf")[0]
   end
 
   test do
