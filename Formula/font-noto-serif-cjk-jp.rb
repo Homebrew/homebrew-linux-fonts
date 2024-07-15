7d25a25ab7e3e5904ff7d86cdc4e19f0f72090e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:28 +0000

    font-noto-serif-cjk-jp updated

--- Formula/font-noto-serif-cjk-jp.rb
+++ Formula/font-noto-serif-cjk-jp.rb
@@ -6,13 +6,13 @@ class FontNotoSerifCjkJp < Formula
   sha256 "166a03cc7725f4d52a0533f4137991089e55d1d417b9d7e15197a9d483b41de3"
 
   def install
-    (share/"fonts").install Dir.glob("./**/OTF/Japanese/NotoSerifCJKjp-Black.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Japanese/NotoSerifCJKjp-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Japanese/NotoSerifCJKjp-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Japanese/NotoSerifCJKjp-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Japanese/NotoSerifCJKjp-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Japanese/NotoSerifCJKjp-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Japanese/NotoSerifCJKjp-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Japanese/NotoSerifCJKjp-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Japanese/NotoSerifCJKjp-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Japanese/NotoSerifCJKjp-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Japanese/NotoSerifCJKjp-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Japanese/NotoSerifCJKjp-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Japanese/NotoSerifCJKjp-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Japanese/NotoSerifCJKjp-SemiBold.otf")[0]
   end
 
   test do
