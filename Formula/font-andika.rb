c30e7454fffe237cf450bad0e86e4fa475643df9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:21:59 +0000

    font-andika updated

--- Formula/font-andika.rb
+++ Formula/font-andika.rb
@@ -6,10 +6,10 @@ class FontAndika < Formula
   sha256 "cc9a7523fcdecf5d2e1858f8ed43bca8f1e1be1ad5f20ac81475d816a9dcf87c"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Andika-6.200/Andika-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Andika-6.200/Andika-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Andika-6.200/Andika-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Andika-6.200/Andika-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Andika-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Andika-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Andika-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Andika-Regular.ttf")[0]
   end
 
   test do
