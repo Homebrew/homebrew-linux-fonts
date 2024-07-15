357118d52f6f319a47e84130275165df3af101d4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:39 +0000

    font-sn-pro updated

--- Formula/font-sn-pro.rb
+++ Formula/font-sn-pro.rb
@@ -7,22 +7,22 @@ class FontSnPro < Formula
   sha256 "b8953185881a741e62912033d0fdc0ad99f31e470c6a51c07859f883ec26ee19"
 
   def install
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-ThinItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-BlackItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-Heavy.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-HeavyItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-RegularItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-Semibold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-SemiboldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-Thin.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SNPro/SNPro-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-BlackItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-Heavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-HeavyItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-RegularItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-Semibold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-SemiboldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SNPro-ThinItalic.otf")[0]
   end
 
   test do
