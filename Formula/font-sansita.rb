948fc0f55ae17d3d5f6a2468cb519c1fac093ee3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:38 +0000

    font-sansita updated

--- Formula/font-sansita.rb
+++ Formula/font-sansita.rb
@@ -7,14 +7,14 @@ class FontSansita < Formula
        only_path: "ofl/sansita"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-ExtraBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/sansita/./**/Sansita-Regular.ttf")[0]
   end
 
   test do
