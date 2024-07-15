dd92907bf9d37c49a3727d235a00b8c9786b28db
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:21:58 +0000

    font-3270-nerd-font updated

--- Formula/font-3270-nerd-font.rb
+++ Formula/font-3270-nerd-font.rb
@@ -6,7 +6,7 @@ class Font3270NerdFont < Formula
   sha256 "0e7cffe10575de3d80df920065ea6f320e86cd515ddd16beed817d284ba44afc"
 
   def install
-    (share/"fonts").install Dir.glob("./**/3270NerdFontPropo-SemiCondensed.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/3270NerdFont-Condensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFont-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFont-SemiCondensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFontMono-Condensed.ttf")[0]
@@ -14,7 +14,7 @@ class Font3270NerdFont < Formula
     (share/"fonts").install Dir.glob("./**/3270NerdFontMono-SemiCondensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFontPropo-Condensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/3270NerdFontPropo-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/3270NerdFont-Condensed.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/3270NerdFontPropo-SemiCondensed.ttf")[0]
   end
 
   test do
