f14e39247431ae540c54bc36c05716e4f8635789
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:23 +0000

    font-migu-2m updated

--- Formula/font-migu-2m.rb
+++ Formula/font-migu-2m.rb
@@ -7,8 +7,8 @@ class FontMigu2m < Formula
   sha256 "e7845f148772c984064c325eba70ed4dfb9a27084c2011a3a1b6194be6e439e5"
 
   def install
-    (share/"fonts").install Dir.glob("./**/migu-2m-20231123/migu-2m-bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/migu-2m-20231123/migu-2m-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-2m-bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-2m-regular.ttf")[0]
   end
 
   test do
