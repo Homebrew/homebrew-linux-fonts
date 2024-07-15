29d1227d95b11bd7ec932ad21d2d6bd1bc90a8a2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:21:59 +0000

    font-anaheim updated

--- Formula/font-anaheim.rb
+++ Formula/font-anaheim.rb
@@ -5,7 +5,7 @@ class FontAnaheim < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Anaheim[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Anaheim?wght?.ttf")[0]
   end
 
   test do
