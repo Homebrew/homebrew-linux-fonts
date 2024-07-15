4be5ca1e20f4a34c7bd1b3180d29eefd29888523
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:32 +0000

    font-playwrite-br updated

--- Formula/font-playwrite-br.rb
+++ Formula/font-playwrite-br.rb
@@ -5,7 +5,7 @@ class FontPlaywriteBr < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteBR[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteBR?wght?.ttf")[0]
   end
 
   test do
