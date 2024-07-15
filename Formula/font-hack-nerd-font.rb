08d8e60aa4140dee8e6f362aada27b88f79afe2f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:14 +0000

    font-hack-nerd-font updated

--- Formula/font-hack-nerd-font.rb
+++ Formula/font-hack-nerd-font.rb
@@ -6,7 +6,7 @@ class FontHackNerdFont < Formula
   sha256 "70b2c3e2375f99e9d81b1f01c7ad90efc9d72177ea975fc62f2310fea21e2f18"
 
   def install
-    (share/"fonts").install Dir.glob("./**/HackNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/HackNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/HackNerdFont-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/HackNerdFont-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/HackNerdFont-Regular.ttf")[0]
@@ -17,7 +17,7 @@ class FontHackNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/HackNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/HackNerdFontPropo-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/HackNerdFontPropo-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/HackNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/HackNerdFontPropo-Regular.ttf")[0]
   end
 
   test do
