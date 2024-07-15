293424a83293798cd74c0422cacd5ed60ce1fb00
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:42 +0000

    font-twitter-color-emoji updated

--- Formula/font-twitter-color-emoji.rb
+++ Formula/font-twitter-color-emoji.rb
@@ -6,7 +6,7 @@ class FontTwitterColorEmoji < Formula
   sha256 "8e2c6cb768b5a578b1dacb8f70d3b91e782a8098821484af77cf322ac499f28a"
 
   def install
-    (share/"fonts").install Dir.glob("./**/TwitterColorEmoji-SVGinOT-14.0.2/TwitterColorEmoji-SVGinOT.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TwitterColorEmoji-SVGinOT.ttf")[0]
   end
 
   test do
