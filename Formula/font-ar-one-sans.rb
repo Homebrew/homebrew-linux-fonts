aaca65ac1e6be519cde15afddf9564393cac3e47
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:00 +0000

    font-ar-one-sans updated

--- Formula/font-ar-one-sans.rb
+++ Formula/font-ar-one-sans.rb
@@ -5,7 +5,7 @@ class FontArOneSans < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/AROneSans[ARRR,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AROneSans?ARRR,wght?.ttf")[0]
   end
 
   test do
