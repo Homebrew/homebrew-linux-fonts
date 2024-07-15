eb500f03c6ba359d27782dd9e5836eabec8df29b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:15 +0000

    font-ibm-plex-mono updated

--- Formula/font-ibm-plex-mono.rb
+++ Formula/font-ibm-plex-mono.rb
@@ -7,7 +7,7 @@ class FontIbmPlexMono < Formula
        only_path: "ofl/ibmplexmono"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-ExtraLightItalic.ttf")[0]
@@ -20,7 +20,7 @@ class FontIbmPlexMono < Formula
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-SemiBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ibmplexmono/./**/IBMPlexMono-ThinItalic.ttf")[0]
   end
 
   test do
