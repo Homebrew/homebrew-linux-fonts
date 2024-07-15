a2a718aaf3a6b6784d9588c540e88b4cb1c708a0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:36 +0000

    font-redacted updated

--- Formula/font-redacted.rb
+++ Formula/font-redacted.rb
@@ -4,10 +4,10 @@ class FontRedacted < Formula
   head "https://github.com/christiannaths/Redacted-Font/archive/refs/heads/master.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/redacted-font-master/Redacted/fonts/ttf/Redacted-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/redacted-font-master/RedactedScript/fonts/ttf/RedactedScript-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/redacted-font-master/RedactedScript/fonts/ttf/RedactedScript-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/redacted-font-master/RedactedScript/fonts/ttf/RedactedScript-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Redacted/fonts/ttf/Redacted-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/RedactedScript/fonts/ttf/RedactedScript-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/RedactedScript/fonts/ttf/RedactedScript-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/RedactedScript/fonts/ttf/RedactedScript-Regular.ttf")[0]
   end
 
   test do
