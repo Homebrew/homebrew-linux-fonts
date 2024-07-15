7f216db3ba86895b45be59bb03ecf80e7ef3ddeb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:03 +0000

    font-bodoni-moda updated

--- Formula/font-bodoni-moda.rb
+++ Formula/font-bodoni-moda.rb
@@ -7,8 +7,8 @@ class FontBodoniModa < Formula
   sha256 "787426889302f357b1e108fd5284fbe9d40063cb0c994d936c7b6a99816f8ccc"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Bodoni-master/fonts/variable/Bodoni-Italic-VF.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Bodoni-master/fonts/variable/Bodoni-VF.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/variable/Bodoni-Italic-VF.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/variable/Bodoni-VF.ttf")[0]
   end
 
   test do
