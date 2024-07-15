c18eca14a62f1d3903fd24a38520a17acf05d0e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:30 +0000

    font-noto-serif-tibetan updated

--- Formula/font-noto-serif-tibetan.rb
+++ Formula/font-noto-serif-tibetan.rb
@@ -5,7 +5,7 @@ class FontNotoSerifTibetan < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifTibetan[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifTibetan?wght?.ttf")[0]
   end
 
   test do
