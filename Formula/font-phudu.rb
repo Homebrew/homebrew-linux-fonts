e0a657c5d297e7ec6b9f330719fb5ddd4febf71b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:31 +0000

    font-phudu updated

--- Formula/font-phudu.rb
+++ Formula/font-phudu.rb
@@ -5,7 +5,7 @@ class FontPhudu < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Phudu[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Phudu?wght?.ttf")[0]
   end
 
   test do
