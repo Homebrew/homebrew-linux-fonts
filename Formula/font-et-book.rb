807c71511719308229a92b15dcc10acabfad54b0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:09 +0000

    font-et-book updated

--- Formula/font-et-book.rb
+++ Formula/font-et-book.rb
@@ -5,11 +5,11 @@ class FontEtBook < Formula
        verified: "github.com/edwardtufte/et-book/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/et-book-gh-pages/et-book/et-book-bold-line-figures/et-book-bold-line-figures.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/et-book-gh-pages/et-book/et-book-display-italic-old-style-figures/et-book-display-italic-old-style-figures.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/et-book-gh-pages/et-book/et-book-roman-line-figures/et-book-roman-line-figures.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/et-book-gh-pages/et-book/et-book-roman-old-style-figures/et-book-roman-old-style-figures.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/et-book-gh-pages/et-book/et-book-semi-bold-old-style-figures/et-book-semi-bold-old-style-figures.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/et-book/et-book-bold-line-figures/et-book-bold-line-figures.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/et-book/et-book-display-italic-old-style-figures/et-book-display-italic-old-style-figures.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/et-book/et-book-roman-line-figures/et-book-roman-line-figures.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/et-book/et-book-roman-old-style-figures/et-book-roman-old-style-figures.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/et-book/et-book-semi-bold-old-style-figures/et-book-semi-bold-old-style-figures.ttf")[0]
   end
 
   test do
