5dffcdd3f59fde01c8c258ba541abe06988c2b4b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:40 +0000

    font-stix updated

--- Formula/font-stix.rb
+++ Formula/font-stix.rb
@@ -7,15 +7,15 @@ class FontStix < Formula
   sha256 "c4671ec4a116d887c8ebf91b2706301bebc43e776ac00f549c36ad1f34964c98"
 
   def install
-    (share/"fonts").install Dir.glob("./**/stixfonts-2.13/fonts/static_otf/STIXTwoText-SemiBoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/stixfonts-2.13/fonts/static_otf/STIXTwoText-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/stixfonts-2.13/fonts/static_otf/STIXTwoText-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/stixfonts-2.13/fonts/static_otf/STIXTwoText-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/stixfonts-2.13/fonts/static_otf/STIXTwoText-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/stixfonts-2.13/fonts/static_otf/STIXTwoText-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/stixfonts-2.13/fonts/static_otf/STIXTwoText-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/stixfonts-2.13/fonts/static_otf/STIXTwoText-SemiBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/stixfonts-2.13/fonts/static_otf/STIXTwoMath-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoMath-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-SemiBoldItalic.otf")[0]
   end
 
   test do
