2d974a07ec50a39f4e863e3fe3af34b8af4f9775
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:03 +0000

    font-bricolage-grotesque updated

--- Formula/font-bricolage-grotesque.rb
+++ Formula/font-bricolage-grotesque.rb
@@ -5,7 +5,7 @@ class FontBricolageGrotesque < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/BricolageGrotesque[opsz,wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BricolageGrotesque?opsz,wdth,wght?.ttf")[0]
   end
 
   test do
