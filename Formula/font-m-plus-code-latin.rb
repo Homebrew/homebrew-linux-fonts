f7562b7aebae8978b7b6384828e8240b597d2025
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:22 +0000

    font-m-plus-code-latin updated

--- Formula/font-m-plus-code-latin.rb
+++ Formula/font-m-plus-code-latin.rb
@@ -5,7 +5,7 @@ class FontMPlusCodeLatin < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/MPLUSCodeLatin[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MPLUSCodeLatin?wdth,wght?.ttf")[0]
   end
 
   test do
