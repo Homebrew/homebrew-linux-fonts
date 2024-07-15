7ed7492ea696f85b616e6110dddb4673743e7710
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:32 +0000

    font-playwrite-de-grund updated

--- Formula/font-playwrite-de-grund.rb
+++ Formula/font-playwrite-de-grund.rb
@@ -5,7 +5,7 @@ class FontPlaywriteDeGrund < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteDEGrund[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteDEGrund?wght?.ttf")[0]
   end
 
   test do
