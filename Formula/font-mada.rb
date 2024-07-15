581c5a3d1106e13d51c78ab759055459922d985f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:22 +0000

    font-mada updated

--- Formula/font-mada.rb
+++ Formula/font-mada.rb
@@ -5,7 +5,7 @@ class FontMada < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Mada[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Mada?wght?.ttf")[0]
   end
 
   test do
