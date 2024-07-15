5a5f3d8c9a909d332c091769cc44dd895c72dcf3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:32 +0000

    font-playwrite-at updated

--- Formula/font-playwrite-at.rb
+++ Formula/font-playwrite-at.rb
@@ -1,13 +1,14 @@
 class FontPlaywriteAt < Formula
   desc "Playwrite at font"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+AT"
   head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
        branch:    "main",
        only_path: "ofl/playwriteat"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/playwriteat/./**/PlaywriteAT-Italic[wght].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/playwriteat/./**/PlaywriteAT[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playwriteat/./**/PlaywriteAT-Italic?wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/playwriteat/./**/PlaywriteAT?wght?.ttf")[0]
   end
 
   test do
