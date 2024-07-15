246a12f8a695b6fcb775bc9c98c421663ac2b142
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:27 +0000

    font-noto-sans-nko-unjoined updated

--- Formula/font-noto-sans-nko-unjoined.rb
+++ Formula/font-noto-sans-nko-unjoined.rb
@@ -5,7 +5,7 @@ class FontNotoSansNkoUnjoined < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansNKoUnjoined[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansNKoUnjoined?wght?.ttf")[0]
   end
 
   test do
