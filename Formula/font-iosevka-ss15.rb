19871ec14401245187ca4bea4f4ad5cf4a4854de
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:17 +0000

    font-iosevka-ss15 updated

--- Formula/font-iosevka-ss15.rb
+++ Formula/font-iosevka-ss15.rb
@@ -6,7 +6,7 @@ class FontIosevkaSs15 < Formula
   sha256 "520c5d675c1a66658932235a2146787214996404a0e5d385d5da9f93a4fae722"
 
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaSS15-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS15-Bold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS15-ExtraBold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS15-ExtraLight.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS15-Heavy.ttc")[0]
@@ -14,7 +14,7 @@ class FontIosevkaSs15 < Formula
     (share/"fonts").install Dir.glob("./**/IosevkaSS15-Medium.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS15-Regular.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS15-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS15-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS15-Thin.ttc")[0]
   end
 
   test do
