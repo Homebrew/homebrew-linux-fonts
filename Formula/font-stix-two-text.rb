43c31691491fed8cd91a5caa49f39d52d325e549
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:40 +0000

    font-stix-two-text updated

--- Formula/font-stix-two-text.rb
+++ Formula/font-stix-two-text.rb
@@ -7,8 +7,8 @@ class FontStixTwoText < Formula
        only_path: "ofl/stixtwotext"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/stixtwotext/./**/STIXTwoText-Italic[wght].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/stixtwotext/./**/STIXTwoText[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/stixtwotext/./**/STIXTwoText-Italic?wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/stixtwotext/./**/STIXTwoText?wght?.ttf")[0]
   end
 
   test do
