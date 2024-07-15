d1450a0572721bdea2baeb9acbea8c9c0765c898
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:17 +0000

    font-iosevka-ss10 updated

--- Formula/font-iosevka-ss10.rb
+++ Formula/font-iosevka-ss10.rb
@@ -6,7 +6,7 @@ class FontIosevkaSs10 < Formula
   sha256 "09aca94493106a0e345f0eacc6ca25b6632c6608cba72c6e3eeeaa958f7b24ed"
 
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaSS10-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS10-Bold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS10-ExtraBold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS10-ExtraLight.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS10-Heavy.ttc")[0]
@@ -14,7 +14,7 @@ class FontIosevkaSs10 < Formula
     (share/"fonts").install Dir.glob("./**/IosevkaSS10-Medium.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS10-Regular.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS10-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS10-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS10-Thin.ttc")[0]
   end
 
   test do
