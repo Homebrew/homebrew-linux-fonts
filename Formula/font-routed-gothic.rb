ce91df4847f504cb9b59c4163968e090b1f93707
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:37 +0000

    font-routed-gothic updated

--- Formula/font-routed-gothic.rb
+++ Formula/font-routed-gothic.rb
@@ -6,15 +6,15 @@ class FontRoutedGothic < Formula
   sha256 "3ecffba109bf2619057815411253e5c6f47f82fedba38ade6c951e13a8b2ad5e"
 
   def install
-    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-narrow-half-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-narrow-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-narrow.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-wide-half-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-wide-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-wide.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-ttf-v1.0.0/routed-gothic-half-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-half-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-narrow-half-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-narrow-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-narrow.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-wide-half-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-wide-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic-wide.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/routed-gothic.ttf")[0]
   end
 
   test do
