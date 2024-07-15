23f11480485851fb30fb06f7ca1367dd9011e939
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:41 +0000

    font-trebuchet-ms updated

--- Formula/font-trebuchet-ms.rb
+++ Formula/font-trebuchet-ms.rb
@@ -4,8 +4,8 @@ class FontTrebuchetMs < Formula
   head "https://downloads.sourceforge.net/corefonts/trebuc32.exe"
 
   def install
-    (share/"fonts").install Dir.glob("./**/trebuc.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Trebucbd.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/trebuc.ttf")[0]
     (share/"fonts").install Dir.glob("./**/trebucbi.ttf")[0]
     (share/"fonts").install Dir.glob("./**/trebucit.ttf")[0]
   end
