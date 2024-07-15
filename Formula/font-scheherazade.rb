dd26b8b8f6e5dad7fd1eee78b08026380631b4cf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:38 +0000

    font-scheherazade updated

--- Formula/font-scheherazade.rb
+++ Formula/font-scheherazade.rb
@@ -6,7 +6,7 @@ class FontScheherazade < Formula
   sha256 "d8bb12997507937347efba4e59550a9655350563d1df662f0651e04ba916aff1"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Scheherazade-2.020/Scheherazade-R.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Scheherazade-R.ttf")[0]
   end
 
   test do
