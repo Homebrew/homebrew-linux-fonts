133f9e252b545d3a039d36d4d9e471acf8cf71fb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:29 +0000

    font-noto-serif-kr updated

--- Formula/font-noto-serif-kr.rb
+++ Formula/font-noto-serif-kr.rb
@@ -5,7 +5,7 @@ class FontNotoSerifKr < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifKR[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifKR?wght?.ttf")[0]
   end
 
   test do
