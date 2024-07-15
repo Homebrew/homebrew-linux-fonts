d0c25d6a7598c04db2d90f8f9e676a1871977113
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:42 +0000

    font-undefined-medium updated

--- Formula/font-undefined-medium.rb
+++ Formula/font-undefined-medium.rb
@@ -6,7 +6,7 @@ class FontUndefinedMedium < Formula
   sha256 "a5e6682e165e70e10b575a468c00013038ec60bad4f1daa9cfb8415d3ce4d84d"
 
   def install
-    (share/"fonts").install Dir.glob("./**/undefined-medium-1.3/fonts/otf/undefined-medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/otf/undefined-medium.otf")[0]
   end
 
   test do
