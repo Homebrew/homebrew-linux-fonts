496ad2137634492c87a7a7376f1eee9a35e505f3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:33 +0000

    font-playwrite-hr-lijeva updated

--- Formula/font-playwrite-hr-lijeva.rb
+++ Formula/font-playwrite-hr-lijeva.rb
@@ -1,10 +1,11 @@
 class FontPlaywriteHrLijeva < Formula
   desc "Playwrite hr lijeva font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritehrlijeva/PlaywriteHRLijeva%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+HR+Lijeva"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritehrlijeva/PlaywriteHRLijeva%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteHRLijeva[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteHRLijeva?wght?.ttf")[0]
   end
 
   test do
