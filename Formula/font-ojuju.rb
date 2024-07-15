96ea0f34c2325ea74822d8e5d03457c838debf9e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:30 +0000

    font-ojuju updated

--- Formula/font-ojuju.rb
+++ Formula/font-ojuju.rb
@@ -5,7 +5,7 @@ class FontOjuju < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Ojuju[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Ojuju?wght?.ttf")[0]
   end
 
   test do
