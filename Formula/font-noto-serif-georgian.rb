f799b8b3b366c7b7116707c6f2f2cbe152421cd2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:28 +0000

    font-noto-serif-georgian updated

--- Formula/font-noto-serif-georgian.rb
+++ Formula/font-noto-serif-georgian.rb
@@ -5,7 +5,7 @@ class FontNotoSerifGeorgian < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifGeorgian[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifGeorgian?wdth,wght?.ttf")[0]
   end
 
   test do
