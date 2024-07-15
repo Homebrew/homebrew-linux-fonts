78cd6908ff75684334b803762a879eb02a04bf80
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:13 +0000

    font-golos-text updated

--- Formula/font-golos-text.rb
+++ Formula/font-golos-text.rb
@@ -5,7 +5,7 @@ class FontGolosText < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/GolosText[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GolosText?wght?.ttf")[0]
   end
 
   test do
