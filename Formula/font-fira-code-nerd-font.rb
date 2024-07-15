a22b21b2ff46ce94b37271ba3b11736833992504
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:10 +0000

    font-fira-code-nerd-font updated

--- Formula/font-fira-code-nerd-font.rb
+++ Formula/font-fira-code-nerd-font.rb
@@ -6,7 +6,7 @@ class FontFiraCodeNerdFont < Formula
   sha256 "4ee8fbafecfc90460399b9828270b8ece30ccbf60b3ab875d64ff77696c6e262"
 
   def install
-    (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-SemiBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Light.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Regular.ttf")[0]
@@ -23,7 +23,7 @@ class FontFiraCodeNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-Retina.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/FiraCodeNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCodeNerdFontPropo-SemiBold.ttf")[0]
   end
 
   test do
