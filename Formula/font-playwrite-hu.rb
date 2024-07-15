bd51d3e8b5a51ab16d9f98241c0cdbe065717401
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:33 +0000

    font-playwrite-hu updated

--- Formula/font-playwrite-hu.rb
+++ Formula/font-playwrite-hu.rb
@@ -1,10 +1,11 @@
 class FontPlaywriteHu < Formula
   desc "Playwrite hu font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritehu/PlaywriteHU%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+HU"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritehu/PlaywriteHU%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteHU[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteHU?wght?.ttf")[0]
   end
 
   test do
