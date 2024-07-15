855b01aca294dfe53f434c1ba5454cf01e6f0fbb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:15 +0000

    font-infini updated

--- Formula/font-infini.rb
+++ Formula/font-infini.rb
@@ -6,10 +6,10 @@ class FontInfini < Formula
   sha256 :no_check
 
   def install
-    (share/"fonts").install Dir.glob("./**/Infini-cnap-engl_fonts-specimen-v2/01_Fonts/01_Regular/infini-regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Infini-cnap-engl_fonts-specimen-v2/01_Fonts/02_Italic/infini-italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Infini-cnap-engl_fonts-specimen-v2/01_Fonts/03_Bold/infini-bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Infini-cnap-engl_fonts-specimen-v2/01_Fonts/04_Pictos/infini-picto.otf")[0]
+    (share/"fonts").install Dir.glob("./**/01_Fonts/01_Regular/infini-regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/01_Fonts/02_Italic/infini-italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/01_Fonts/03_Bold/infini-bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/01_Fonts/04_Pictos/infini-picto.otf")[0]
   end
 
   test do
