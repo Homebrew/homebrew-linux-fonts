858fceeed4a73462373b11ef3ebdcec8177061a5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:37 +0000

    font-ricty-diminished updated

--- Formula/font-ricty-diminished.rb
+++ Formula/font-ricty-diminished.rb
@@ -6,14 +6,14 @@ class FontRictyDiminished < Formula
   sha256 "09d6cf8eaf3402205dd8a0fa35469d41833b4e85bedddfac235255d78747766b"
 
   def install
-    (share/"fonts").install Dir.glob("./**/RictyDiminishedDiscord-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/RictyDiminished-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/RictyDiminished-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/RictyDiminished-Oblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/RictyDiminished-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/RictyDiminishedDiscord-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/RictyDiminishedDiscord-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/RictyDiminishedDiscord-Oblique.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/RictyDiminished-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/RictyDiminishedDiscord-Regular.ttf")[0]
   end
 
   test do
