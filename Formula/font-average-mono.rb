826384778b1b029699c06e8ce049c2b698a2827d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:01 +0000

    font-average-mono updated

--- Formula/font-average-mono.rb
+++ Formula/font-average-mono.rb
@@ -4,10 +4,10 @@ class FontAverageMono < Formula
   head "https://fontlibrary.org/assets/downloads/average-mono/7e3ab639a72edd95bd08537df255440d/average-mono.zip"
 
   def install
-    (share/"fonts").install Dir.glob("./**/AverageMono/AverageMono.otf")[0]
-    (share/"fonts").install Dir.glob("./**/AverageMono/AverageMonoBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/AverageMono/AverageMonoItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/AverageMono/AverageMonoBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/AverageMono.otf")[0]
+    (share/"fonts").install Dir.glob("./**/AverageMonoBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/AverageMonoBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/AverageMonoItalic.otf")[0]
   end
 
   test do
