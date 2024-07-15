a74d858abe96cc8de2853d011feea61ef56828b3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:40 +0000

    font-tex-gyre-heros updated

--- Formula/font-tex-gyre-heros.rb
+++ Formula/font-tex-gyre-heros.rb
@@ -6,14 +6,14 @@ class FontTexGyreHeros < Formula
   sha256 "755954b7349b803fc1c3d82fe9d9c4f7cf66467af718eaaf4f78ae1a09bf265d"
 
   def install
-    (share/"fonts").install Dir.glob("./**/texgyreheroscn-regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyreheros-bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/texgyreheros-bolditalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/texgyreheros-italic.otf")[0]
     (share/"fonts").install Dir.glob("./**/texgyreheros-regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/texgyreheroscn-bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/texgyreheroscn-bolditalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/texgyreheroscn-italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/texgyreheros-bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyreheroscn-regular.otf")[0]
   end
 
   test do
