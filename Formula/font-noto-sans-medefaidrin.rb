85c3636033b032473d595792a60fde833ff5245e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:27 +0000

    font-noto-sans-medefaidrin updated

--- Formula/font-noto-sans-medefaidrin.rb
+++ Formula/font-noto-sans-medefaidrin.rb
@@ -5,7 +5,7 @@ class FontNotoSansMedefaidrin < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansMedefaidrin[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansMedefaidrin?wght?.ttf")[0]
   end
 
   test do
