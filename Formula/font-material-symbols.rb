630733e43c3723fa575eb0aa0532cde1c635a5e7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:22 +0000

    font-material-symbols updated

--- Formula/font-material-symbols.rb
+++ Formula/font-material-symbols.rb
@@ -7,9 +7,9 @@ class FontMaterialSymbols < Formula
        only_path: "variablefont"
 
   def install
-    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].ttf")[0]
-    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsRounded[FILL,GRAD,opsz,wght].ttf")[0]
-    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsSharp[FILL,GRAD,opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsOutlined?FILL,GRAD,opsz,wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsRounded?FILL,GRAD,opsz,wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("variablefont/./**/MaterialSymbolsSharp?FILL,GRAD,opsz,wght?.ttf")[0]
   end
 
   test do
