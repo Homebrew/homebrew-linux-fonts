c4c5e4518fc93719be82691a55d73ea58bd53328
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:29 +0000

    font-noto-serif-gurmukhi updated

--- Formula/font-noto-serif-gurmukhi.rb
+++ Formula/font-noto-serif-gurmukhi.rb
@@ -5,7 +5,7 @@ class FontNotoSerifGurmukhi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifGurmukhi[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifGurmukhi?wght?.ttf")[0]
   end
 
   test do
