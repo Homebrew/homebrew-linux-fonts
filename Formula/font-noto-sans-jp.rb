8a128c19c029d12c80fd7cf285de260a91efa097
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:26 +0000

    font-noto-sans-jp updated

--- Formula/font-noto-sans-jp.rb
+++ Formula/font-noto-sans-jp.rb
@@ -5,7 +5,7 @@ class FontNotoSansJp < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansJP[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansJP?wght?.ttf")[0]
   end
 
   test do
