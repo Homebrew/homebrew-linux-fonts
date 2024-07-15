6fd400a6ce54a856ffcf5c314c9a80fc80d21c47
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:27 +0000

    font-noto-sans-sinhala-ui updated

--- Formula/font-noto-sans-sinhala-ui.rb
+++ Formula/font-noto-sans-sinhala-ui.rb
@@ -5,7 +5,7 @@ class FontNotoSansSinhalaUi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansSinhalaUI[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansSinhalaUI?wdth,wght?.ttf")[0]
   end
 
   test do
