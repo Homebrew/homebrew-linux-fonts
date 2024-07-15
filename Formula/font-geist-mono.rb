31ce610dd5c793edf9f726c7e868d238aa5666cf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:11 +0000

    font-geist-mono updated

--- Formula/font-geist-mono.rb
+++ Formula/font-geist-mono.rb
@@ -7,16 +7,16 @@ class FontGeistMono < Formula
   sha256 "d6d19b0fc600110cb1595152c803a1f08e258f76637d1509baf76e2e0b9708d5"
 
   def install
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/variable-ttf/GeistMonoVF.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/statics-otf/GeistMono-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/statics-otf/GeistMono-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/statics-otf/GeistMono-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/statics-otf/GeistMono-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/statics-otf/GeistMono-SemiBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/statics-otf/GeistMono-Thin.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/statics-otf/GeistMono-UltraBlack.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/statics-otf/GeistMono-UltraLight.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GeistMono-1.3.0/statics-otf/GeistMono-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/statics-otf/GeistMono-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/statics-otf/GeistMono-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/statics-otf/GeistMono-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/statics-otf/GeistMono-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/statics-otf/GeistMono-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/statics-otf/GeistMono-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/statics-otf/GeistMono-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/statics-otf/GeistMono-UltraBlack.otf")[0]
+    (share/"fonts").install Dir.glob("./**/statics-otf/GeistMono-UltraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/variable-ttf/GeistMonoVF.ttf")[0]
   end
 
   test do
