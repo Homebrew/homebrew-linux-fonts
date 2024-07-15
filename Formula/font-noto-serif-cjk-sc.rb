66161230061861ee3e025762f1aa47ededb01603
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:28 +0000

    font-noto-serif-cjk-sc updated

--- Formula/font-noto-serif-cjk-sc.rb
+++ Formula/font-noto-serif-cjk-sc.rb
@@ -6,13 +6,13 @@ class FontNotoSerifCjkSc < Formula
   sha256 "eeede72f5b88655a3630f18661155028578afc88aa9e67e55db45a8b5be46789"
 
   def install
-    (share/"fonts").install Dir.glob("./**/OTF/SimplifiedChinese/NotoSerifCJKsc-Black.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/SimplifiedChinese/NotoSerifCJKsc-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/SimplifiedChinese/NotoSerifCJKsc-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/SimplifiedChinese/NotoSerifCJKsc-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/SimplifiedChinese/NotoSerifCJKsc-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/SimplifiedChinese/NotoSerifCJKsc-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/SimplifiedChinese/NotoSerifCJKsc-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SimplifiedChinese/NotoSerifCJKsc-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SimplifiedChinese/NotoSerifCJKsc-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SimplifiedChinese/NotoSerifCJKsc-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SimplifiedChinese/NotoSerifCJKsc-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SimplifiedChinese/NotoSerifCJKsc-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SimplifiedChinese/NotoSerifCJKsc-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SimplifiedChinese/NotoSerifCJKsc-SemiBold.otf")[0]
   end
 
   test do
