a22f113ab51c990bc1525eb5601773e22fc5a4f9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:39 +0000

    font-sora updated

--- Formula/font-sora.rb
+++ Formula/font-sora.rb
@@ -5,7 +5,7 @@ class FontSora < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Sora[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Sora?wght?.ttf")[0]
   end
 
   test do
