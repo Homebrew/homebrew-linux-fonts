3eb4379f09f79b7384ca5a921c084bebdd4b614e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:14 +0000

    font-hermit updated

--- Formula/font-hermit.rb
+++ Formula/font-hermit.rb
@@ -7,11 +7,11 @@ class FontHermit < Formula
 
   def install
     (share/"fonts").install Dir.glob("./**/Hermit-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Hermit-RegularItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Hermit-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Hermit-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Hermit-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/Hermit-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hermit-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hermit-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hermit-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hermit-RegularItalic.otf")[0]
   end
 
   test do
