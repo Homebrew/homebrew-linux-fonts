7ff5f19d6b28dcd333dca6eadc09780d0775f2b3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:18 +0000

    font-jaapokki updated

--- Formula/font-jaapokki.rb
+++ Formula/font-jaapokki.rb
@@ -6,9 +6,9 @@ class FontJaapokki < Formula
   sha256 "aa5f38cbb682df8732d406996e37349b83e47d3c14bae4af2125f2a7784430b7"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Jaapokki-font-package-1.0/Regular/Jaapokki-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Jaapokki-font-package-1.0/enchance/Jaapokkienchance-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Jaapokki-font-package-1.0/subtract/Jaapokkisubtract-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Regular/Jaapokki-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/enchance/Jaapokkienchance-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/subtract/Jaapokkisubtract-Regular.otf")[0]
   end
 
   test do
