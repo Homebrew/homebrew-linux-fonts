85349ba7843891d4a72928e6e507e1f3f69f45c7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:04 +0000

    font-cairo updated

--- Formula/font-cairo.rb
+++ Formula/font-cairo.rb
@@ -5,7 +5,7 @@ class FontCairo < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Cairo[slnt,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Cairo?slnt,wght?.ttf")[0]
   end
 
   test do
