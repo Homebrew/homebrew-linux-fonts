f08a1a8b5c0578976974ae43f7bdf3102926eb75
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:07 +0000

    font-delugia-mono-complete updated

--- Formula/font-delugia-mono-complete.rb
+++ Formula/font-delugia-mono-complete.rb
@@ -6,12 +6,12 @@ class FontDelugiaMonoComplete < Formula
   sha256 "c731d71580731cf15fe1ff6f742c17b1cfa6a41f44f003b699d39c2b10779add"
 
   def install
-    (share/"fonts").install Dir.glob("./**/delugia-mono-complete/DelugiaMonoComplete-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-mono-complete/DelugiaMonoComplete-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-mono-complete/DelugiaMonoComplete-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-mono-complete/DelugiaMonoComplete.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-mono-complete/DelugiaMonoCompleteLight-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-mono-complete/DelugiaMonoCompleteLight.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaMonoComplete-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaMonoComplete-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaMonoComplete-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaMonoComplete.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaMonoCompleteLight-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaMonoCompleteLight.ttf")[0]
   end
 
   test do
