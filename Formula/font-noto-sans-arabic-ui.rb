79f7a89c263bdcac05372cca0f1be8ea5de9de24
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:25 +0000

    font-noto-sans-arabic-ui updated

--- Formula/font-noto-sans-arabic-ui.rb
+++ Formula/font-noto-sans-arabic-ui.rb
@@ -5,7 +5,7 @@ class FontNotoSansArabicUi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansArabicUI[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansArabicUI?wdth,wght?.ttf")[0]
   end
 
   test do
