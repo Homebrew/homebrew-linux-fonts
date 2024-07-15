b327d65e9ff46fffe547b5f702e36d840f445be0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:38 +0000

    font-sf-mono-for-powerline updated

--- Formula/font-sf-mono-for-powerline.rb
+++ Formula/font-sf-mono-for-powerline.rb
@@ -4,18 +4,18 @@ class FontSfMonoForPowerline < Formula
   head "https://github.com/Twixes/SF-Mono-Powerline/archive/refs/heads/master.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-SemiboldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-Heavy.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-HeavyItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-RegularItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-Semibold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-master/SF-Mono-Powerline-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-Heavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-HeavyItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-RegularItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-Semibold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/SF-Mono-Powerline-SemiboldItalic.otf")[0]
   end
 
   test do
