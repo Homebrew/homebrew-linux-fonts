e9a010893582026161f34ed6a7077d62c0374cf8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:02 +0000

    font-baloo-paaji-2 updated

--- Formula/font-baloo-paaji-2.rb
+++ Formula/font-baloo-paaji-2.rb
@@ -5,7 +5,7 @@ class FontBalooPaaji2 < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/BalooPaaji2[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BalooPaaji2?wght?.ttf")[0]
   end
 
   test do
