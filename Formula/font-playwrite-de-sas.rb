544f17325e951d589aeedb36fc14356a4fee8d38
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:33 +0000

    font-playwrite-de-sas updated

--- Formula/font-playwrite-de-sas.rb
+++ Formula/font-playwrite-de-sas.rb
@@ -5,7 +5,7 @@ class FontPlaywriteDeSas < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteDESAS[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteDESAS?wght?.ttf")[0]
   end
 
   test do
