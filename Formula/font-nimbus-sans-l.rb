d19dba65092c69f043c4a85572e721fc13ba712c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:25 +0000

    font-nimbus-sans-l updated

--- Formula/font-nimbus-sans-l.rb
+++ Formula/font-nimbus-sans-l.rb
@@ -4,14 +4,14 @@ class FontNimbusSansL < Formula
   head "https://fontlibrary.org/assets/downloads/nimbus-sans-l/2c1b22cb6708de61d2051f12c90a024e/nimbus-sans-l.zip"
 
   def install
-    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-ReguItal.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-BoldCond.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-BoldCondItal.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-BoldItal.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-Regu.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-ReguCond.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-ReguCondItal.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/nimbus-sans-l/NimbusSanL-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NimbusSanL-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NimbusSanL-BoldCond.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NimbusSanL-BoldCondItal.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NimbusSanL-BoldItal.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NimbusSanL-Regu.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NimbusSanL-ReguCond.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NimbusSanL-ReguCondItal.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NimbusSanL-ReguItal.ttf")[0]
   end
 
   test do
