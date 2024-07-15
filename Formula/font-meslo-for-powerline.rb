6fafb85be55e810a3d04b34af0e6132ccb13a169
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:23 +0000

    font-meslo-for-powerline updated

--- Formula/font-meslo-for-powerline.rb
+++ Formula/font-meslo-for-powerline.rb
@@ -6,12 +6,12 @@ class FontMesloForPowerline < Formula
   sha256 "3a0b73abca6334b5e6bddefab67f6eb1b2fac1231817d95fc79126c8998c4844"
 
   def install
-    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG L DZ Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG L Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG M DZ Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG M Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG S DZ Regular for Powerline.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts-2015-12-04/Meslo/Meslo LG S Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo/Meslo LG L DZ Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo/Meslo LG L Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo/Meslo LG M DZ Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo/Meslo LG M Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo/Meslo LG S DZ Regular for Powerline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Meslo/Meslo LG S Regular for Powerline.otf")[0]
   end
 
   test do
