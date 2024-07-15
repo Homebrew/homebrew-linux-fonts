921247c0b7072e8448f6ca091a2f53f0f3d7c5ed
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:19 +0000

    font-labrada updated

--- Formula/font-labrada.rb
+++ Formula/font-labrada.rb
@@ -7,8 +7,8 @@ class FontLabrada < Formula
        only_path: "ofl/labrada"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/labrada/./**/Labrada-Italic[wght].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/labrada/./**/Labrada[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/labrada/./**/Labrada-Italic?wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/labrada/./**/Labrada?wght?.ttf")[0]
   end
 
   test do
