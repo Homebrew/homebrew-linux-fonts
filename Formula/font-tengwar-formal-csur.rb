c8f5bfc3187653e33c12a0aee50606b8fd1f9fa6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:40 +0000

    font-tengwar-formal-csur updated

--- Formula/font-tengwar-formal-csur.rb
+++ Formula/font-tengwar-formal-csur.rb
@@ -6,7 +6,7 @@ class FontTengwarFormalCsur < Formula
   sha256 "924c282f7f967ecf6ca81003d95d4355dcccda9cf3fb7afdc746cb9d8f8250c7"
 
   def install
-    (share/"fonts").install Dir.glob("./**/TengwarFormalCSUR11/TengwarFormalCSUR.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TengwarFormalCSUR.ttf")[0]
   end
 
   test do
