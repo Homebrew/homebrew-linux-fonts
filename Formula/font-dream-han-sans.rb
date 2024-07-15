697bd6a4c322da28f488ed5d6e4c53dbd4dd3807
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:08 +0000

    font-dream-han-sans updated

--- Formula/font-dream-han-sans.rb
+++ Formula/font-dream-han-sans.rb
@@ -6,15 +6,7 @@ class FontDreamHanSans < Formula
   sha256 "7555b909965c4f32d9d757dfd7397365a4d7b2e8c149de6649c9d34a7bf8ed23"
 
   def install
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W27.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W2.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W3.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W4.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W5.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W6.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W7.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W8.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W9.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W1.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W10.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W11.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W12.ttc")[0]
@@ -25,6 +17,7 @@ class FontDreamHanSans < Formula
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W17.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W18.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W19.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W2.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W20.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W21.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W22.ttc")[0]
@@ -32,7 +25,14 @@ class FontDreamHanSans < Formula
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W24.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W25.ttc")[0]
     (share/"fonts").install Dir.glob("./**/DreamHanSans-W26.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/DreamHanSans-W1.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W27.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W3.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W4.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W5.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W6.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W7.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W8.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/DreamHanSans-W9.ttc")[0]
   end
 
   test do
