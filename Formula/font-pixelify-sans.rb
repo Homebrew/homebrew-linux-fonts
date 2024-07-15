a6f9ec699ac1f88d09b500d65b8f745de3924199
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:31 +0000

    font-pixelify-sans updated

--- Formula/font-pixelify-sans.rb
+++ Formula/font-pixelify-sans.rb
@@ -5,7 +5,7 @@ class FontPixelifySans < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PixelifySans[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PixelifySans?wght?.ttf")[0]
   end
 
   test do
