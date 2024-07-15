a21d7fc0095334a975770eb65ed99f836960d313
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:41 +0000

    font-tex-gyre-pagella updated

--- Formula/font-tex-gyre-pagella.rb
+++ Formula/font-tex-gyre-pagella.rb
@@ -6,10 +6,10 @@ class FontTexGyrePagella < Formula
   sha256 "e16078d19860d68a54bcaedc00e35c2a81b396cdc36842700f1d556f89cf8ef2"
 
   def install
-    (share/"fonts").install Dir.glob("./**/qpl2_501otf/texgyrepagella-bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/qpl2_501otf/texgyrepagella-bolditalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/qpl2_501otf/texgyrepagella-italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/qpl2_501otf/texgyrepagella-regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyrepagella-bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyrepagella-bolditalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyrepagella-italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/texgyrepagella-regular.otf")[0]
   end
 
   test do
