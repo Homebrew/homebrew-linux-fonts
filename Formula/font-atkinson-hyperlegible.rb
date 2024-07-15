c9bddd976ec40ac137a820984f4f00db9ef3d5a2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:01 +0000

    font-atkinson-hyperlegible updated

--- Formula/font-atkinson-hyperlegible.rb
+++ Formula/font-atkinson-hyperlegible.rb
@@ -6,10 +6,10 @@ class FontAtkinsonHyperlegible < Formula
   sha256 "9e1a65d53ddadc8253791e958a07bf5aba210ef6155ea9c91b2b4c46bbce53e3"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Print Fonts/Atkinson-Hyperlegible-Bold-102.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Print Fonts/Atkinson-Hyperlegible-BoldItalic-102.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Print Fonts/Atkinson-Hyperlegible-Italic-102.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Print Fonts/Atkinson-Hyperlegible-Regular-102.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Print Fonts/Atkinson-Hyperlegible-Bold-102.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Print Fonts/Atkinson-Hyperlegible-BoldItalic-102.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Print Fonts/Atkinson-Hyperlegible-Italic-102.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Print Fonts/Atkinson-Hyperlegible-Regular-102.otf")[0]
   end
 
   test do
