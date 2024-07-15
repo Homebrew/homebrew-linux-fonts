eb249be7de5c19f970b729c3c200c76e6b49cbd7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:42 +0000

    font-unbounded updated

--- Formula/font-unbounded.rb
+++ Formula/font-unbounded.rb
@@ -5,7 +5,7 @@ class FontUnbounded < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Unbounded[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Unbounded?wght?.ttf")[0]
   end
 
   test do
