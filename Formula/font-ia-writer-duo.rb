d6c8f66a2609431b6decf1795a027f4a6f6819ce
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:15 +0000

    font-ia-writer-duo updated

--- Formula/font-ia-writer-duo.rb
+++ Formula/font-ia-writer-duo.rb
@@ -4,12 +4,12 @@ class FontIaWriterDuo < Formula
   head "https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Duo/Variable/iAWriterDuoV-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Duo/Variable/iAWriterDuoV.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Duo/Static/iAWriterDuoS-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Duo/Static/iAWriterDuoS-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Duo/Static/iAWriterDuoS-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Duo/Static/iAWriterDuoS-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Duo/Variable/iAWriterDuoV-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Duo/Variable/iAWriterDuoV.ttf")[0]
   end
 
   test do
