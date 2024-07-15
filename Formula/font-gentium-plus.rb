17cdd45420abdf7cd7f5014f3ec5d14cb1130f33
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:12 +0000

    font-gentium-plus updated

--- Formula/font-gentium-plus.rb
+++ Formula/font-gentium-plus.rb
@@ -7,14 +7,14 @@ class FontGentiumPlus < Formula
   sha256 "9b21103b79961149b6508791572acb3b2fe7eb621474c57d5e4ee37e76d7b073"
 
   def install
-    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumPlus-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumBookPlus-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumBookPlus-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumBookPlus-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumPlus-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumPlus-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumPlus-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumPlus-6.200/GentiumBookPlus-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumBookPlus-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumBookPlus-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumBookPlus-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumBookPlus-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GentiumPlus-Regular.ttf")[0]
   end
 
   test do
