1e37c880da1fc2a453251338a8d2777388d70d55
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:09 +0000

    font-encode-sans updated

--- Formula/font-encode-sans.rb
+++ Formula/font-encode-sans.rb
@@ -5,7 +5,7 @@ class FontEncodeSans < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/EncodeSans[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/EncodeSans?wdth,wght?.ttf")[0]
   end
 
   test do
