c92582bbb4f8634925313cfe2b6d1b48a425cf8d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:24 +0000

    font-monda updated

--- Formula/font-monda.rb
+++ Formula/font-monda.rb
@@ -5,7 +5,7 @@ class FontMonda < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Monda[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Monda?wght?.ttf")[0]
   end
 
   test do
