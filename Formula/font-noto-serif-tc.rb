f3fa10b047ddff60ff6da9546d2deb8997414730
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:29 +0000

    font-noto-serif-tc updated

--- Formula/font-noto-serif-tc.rb
+++ Formula/font-noto-serif-tc.rb
@@ -5,7 +5,7 @@ class FontNotoSerifTc < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifTC[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifTC?wght?.ttf")[0]
   end
 
   test do
