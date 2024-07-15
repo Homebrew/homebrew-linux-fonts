66147cfe4bb67a38fa11b462a75f4824e4a3b28f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:43 +0000

    font-ysabeau-sc updated

--- Formula/font-ysabeau-sc.rb
+++ Formula/font-ysabeau-sc.rb
@@ -5,7 +5,7 @@ class FontYsabeauSc < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/YsabeauSC[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/YsabeauSC?wght?.ttf")[0]
   end
 
   test do
