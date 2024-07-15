3abe9735e3d58310f70a39901f5fa92bc90a50ed
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:20 +0000

    font-lexend-mega updated

--- Formula/font-lexend-mega.rb
+++ Formula/font-lexend-mega.rb
@@ -5,7 +5,7 @@ class FontLexendMega < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/LexendMega[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LexendMega?wght?.ttf")[0]
   end
 
   test do
