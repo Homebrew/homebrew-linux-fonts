638cbd1262bd036b45cb664f423eff1c07d13766
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:32 +0000

    font-playpen-sans updated

--- Formula/font-playpen-sans.rb
+++ Formula/font-playpen-sans.rb
@@ -5,7 +5,7 @@ class FontPlaypenSans < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaypenSans[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaypenSans?wght?.ttf")[0]
   end
 
   test do
