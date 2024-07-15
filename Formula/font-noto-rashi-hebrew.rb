581ab38068e5677f5dc80944fb3b8591b648fd40
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:25 +0000

    font-noto-rashi-hebrew updated

--- Formula/font-noto-rashi-hebrew.rb
+++ Formula/font-noto-rashi-hebrew.rb
@@ -5,7 +5,7 @@ class FontNotoRashiHebrew < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoRashiHebrew[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoRashiHebrew?wght?.ttf")[0]
   end
 
   test do
