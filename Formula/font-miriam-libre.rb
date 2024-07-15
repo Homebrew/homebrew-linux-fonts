33699381205a5e16bbf3f49fb7137e3032d57620
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:24 +0000

    font-miriam-libre updated

--- Formula/font-miriam-libre.rb
+++ Formula/font-miriam-libre.rb
@@ -5,7 +5,7 @@ class FontMiriamLibre < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/MiriamLibre[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MiriamLibre?wght?.ttf")[0]
   end
 
   test do
