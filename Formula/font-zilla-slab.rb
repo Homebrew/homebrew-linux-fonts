694cc64c436c8dcb2112e47ac7e6b6270464997c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:44 +0000

    font-zilla-slab updated

--- Formula/font-zilla-slab.rb
+++ Formula/font-zilla-slab.rb
@@ -6,18 +6,18 @@ class FontZillaSlab < Formula
   sha256 "62490dc19cd17e2951fe88ba3e662089ca14077634cacf1f12926374281dcf42"
 
   def install
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlabHighlight-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-SemiBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-SemiBoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlabHighlight-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/zilla-slab/otf/ZillaSlab-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlab-SemiBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlabHighlight-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/ZillaSlabHighlight-Regular.otf")[0]
   end
 
   test do
