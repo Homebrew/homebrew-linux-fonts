ae79cfb355c43b85511a87b062c2788153834593
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:41 +0000

    font-truculenta updated

--- Formula/font-truculenta.rb
+++ Formula/font-truculenta.rb
@@ -5,7 +5,7 @@ class FontTruculenta < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Truculenta[opsz,wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Truculenta?opsz,wdth,wght?.ttf")[0]
   end
 
   test do
