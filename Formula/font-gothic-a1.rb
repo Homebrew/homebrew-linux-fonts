5d120efca7406de41779f80bb2338e2231c94a5c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:13 +0000

    font-gothic-a1 updated

--- Formula/font-gothic-a1.rb
+++ Formula/font-gothic-a1.rb
@@ -7,7 +7,7 @@ class FontGothicA1 < Formula
        only_path: "ofl/gothica1"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-ExtraLight.ttf")[0]
@@ -15,7 +15,7 @@ class FontGothicA1 < Formula
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/gothica1/./**/GothicA1-Thin.ttf")[0]
   end
 
   test do
