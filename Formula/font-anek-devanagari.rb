6524964bb619d178ed2a091b697357613858bd0b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:21:59 +0000

    font-anek-devanagari updated

--- Formula/font-anek-devanagari.rb
+++ Formula/font-anek-devanagari.rb
@@ -5,7 +5,7 @@ class FontAnekDevanagari < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/AnekDevanagari[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekDevanagari?wdth,wght?.ttf")[0]
   end
 
   test do
