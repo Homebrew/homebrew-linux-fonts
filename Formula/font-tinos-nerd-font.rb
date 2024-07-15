6601e6ecb82c5cb18c84e323ba1510ae734f1c75
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:41 +0000

    font-tinos-nerd-font updated

--- Formula/font-tinos-nerd-font.rb
+++ Formula/font-tinos-nerd-font.rb
@@ -6,14 +6,14 @@ class FontTinosNerdFont < Formula
   sha256 "15c26edd632ee7794149006fbc32759cdca103ed0504d6fcc9bba0cd92a8b2e4"
 
   def install
-    (share/"fonts").install Dir.glob("./**/TinosNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TinosNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/TinosNerdFont-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/TinosNerdFont-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/TinosNerdFont-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/TinosNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/TinosNerdFontPropo-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/TinosNerdFontPropo-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/TinosNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/TinosNerdFontPropo-Regular.ttf")[0]
   end
 
   test do
