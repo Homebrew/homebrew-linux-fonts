fc775dddf07dc9582c1f0918dbf072a00d468805
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:10 +0000

    font-fira-mono-nerd-font updated

--- Formula/font-fira-mono-nerd-font.rb
+++ Formula/font-fira-mono-nerd-font.rb
@@ -6,7 +6,7 @@ class FontFiraMonoNerdFont < Formula
   sha256 "9f739a71d5b687325127b6076292bab1597efba188603b4f8cfcdfdf82e9e6fb"
 
   def install
-    (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontPropo-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraMonoNerdFont-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFont-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFont-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontMono-Bold.otf")[0]
@@ -14,7 +14,7 @@ class FontFiraMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontMono-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontPropo-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontPropo-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/FiraMonoNerdFont-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraMonoNerdFontPropo-Regular.otf")[0]
   end
 
   test do
