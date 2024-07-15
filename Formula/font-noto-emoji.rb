8bd87e969e36c2f9896574cd934b3bd99bb3c344
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:25 +0000

    font-noto-emoji updated

--- Formula/font-noto-emoji.rb
+++ Formula/font-noto-emoji.rb
@@ -5,7 +5,7 @@ class FontNotoEmoji < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoEmoji[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoEmoji?wght?.ttf")[0]
   end
 
   test do
