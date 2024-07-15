84dd043f767ab9fa04ed6c3381d111fbc06da58a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:03 +0000

    font-bitstream-vera updated

--- Formula/font-bitstream-vera.rb
+++ Formula/font-bitstream-vera.rb
@@ -6,16 +6,16 @@ class FontBitstreamVera < Formula
   sha256 "1b0ba0f7af2e1d05f64e259d351965a2cb2673104a057ce715a06969c478f6cc"
 
   def install
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/VeraSeBd.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/VeraBI.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/VeraBd.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/VeraIt.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/VeraMoBI.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/VeraMoBd.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/VeraMoIt.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/VeraMono.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/VeraSe.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf-bitstream-vera-1.10/Vera.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Vera.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/VeraBI.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/VeraBd.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/VeraIt.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/VeraMoBI.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/VeraMoBd.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/VeraMoIt.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/VeraMono.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/VeraSe.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/VeraSeBd.ttf")[0]
   end
 
   test do
