c4e5e3da3f86f997c93e5d4e9457f25f2aced311
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:13 +0000

    font-go updated

--- Formula/font-go.rb
+++ Formula/font-go.rb
@@ -4,7 +4,7 @@ class FontGo < Formula
   head "https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Go-Smallcaps.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Go-Bold-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Medium-Italic.ttf")[0]
@@ -15,7 +15,7 @@ class FontGo < Formula
     (share/"fonts").install Dir.glob("./**/Go-Mono.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Go-Smallcaps-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Go-Bold-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Go-Smallcaps.ttf")[0]
   end
 
   test do
