515578dabec6db6439a81a85caed41b018df3115
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:22 +0000

    font-maven-pro updated

--- Formula/font-maven-pro.rb
+++ Formula/font-maven-pro.rb
@@ -5,7 +5,7 @@ class FontMavenPro < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/MavenPro[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MavenPro?wght?.ttf")[0]
   end
 
   test do
