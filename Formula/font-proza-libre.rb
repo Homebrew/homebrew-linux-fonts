92d736eabbf41eb1cfd6225d7cc0bea9ef603eb0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:35 +0000

    font-proza-libre updated

--- Formula/font-proza-libre.rb
+++ Formula/font-proza-libre.rb
@@ -6,16 +6,16 @@ class FontProzaLibre < Formula
   sha256 "c6d975321ef7853769a68fbb290c3f0b0b72ca175a2bbd2a33b1bdf2e1db5490"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-SemiBoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-ExtraBold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-ExtraBoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-MediumItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Proza-Libre-1.0/Fonts/ProzaLibre-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-ExtraBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-ExtraBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-Medium.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-MediumItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-SemiBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Fonts/ProzaLibre-SemiBoldItalic.ttf")[0]
   end
 
   test do
