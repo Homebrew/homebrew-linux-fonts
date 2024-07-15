d1654817f61722ea45d2db9a8e07e3ed789980ca
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:12 +0000

    font-glow-sans-sc-compressed updated

--- Formula/font-glow-sans-sc-compressed.rb
+++ Formula/font-glow-sans-sc-compressed.rb
@@ -8,14 +8,14 @@ class FontGlowSansScCompressed < Formula
   deprecate! "2024-02-17", because: :discontinued
 
   def install
-    (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-Book.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-ExtraBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-ExtraLight.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-Light.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GlowSansSC-Compressed-Thin.otf")[0]
   end
 
   test do
