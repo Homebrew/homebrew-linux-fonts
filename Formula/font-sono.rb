c15684544811f13db0326a820527b2f4bb906d48
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:39 +0000

    font-sono updated

--- Formula/font-sono.rb
+++ Formula/font-sono.rb
@@ -5,7 +5,7 @@ class FontSono < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Sono[MONO,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Sono?MONO,wght?.ttf")[0]
   end
 
   test do
