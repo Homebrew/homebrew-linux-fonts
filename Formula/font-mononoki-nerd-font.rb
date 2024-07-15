03ff8e288c4a0d5c2abe4ff34c1fcf262f2ab0e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:24 +0000

    font-mononoki-nerd-font updated

--- Formula/font-mononoki-nerd-font.rb
+++ Formula/font-mononoki-nerd-font.rb
@@ -6,7 +6,7 @@ class FontMononokiNerdFont < Formula
   sha256 "3a6cd53ec11108090d9271331ad6b02f7dce8dff368068bdeb25affaaca8ce4a"
 
   def install
-    (share/"fonts").install Dir.glob("./**/MononokiNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MononokiNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MononokiNerdFont-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MononokiNerdFont-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MononokiNerdFont-Regular.ttf")[0]
@@ -17,7 +17,7 @@ class FontMononokiNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/MononokiNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MononokiNerdFontPropo-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MononokiNerdFontPropo-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/MononokiNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MononokiNerdFontPropo-Regular.ttf")[0]
   end
 
   test do
