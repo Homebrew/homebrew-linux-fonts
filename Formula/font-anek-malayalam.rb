eaf797ba864018e0c5e732bff281bfc5617d7b25
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:00 +0000

    font-anek-malayalam updated

--- Formula/font-anek-malayalam.rb
+++ Formula/font-anek-malayalam.rb
@@ -5,7 +5,7 @@ class FontAnekMalayalam < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/AnekMalayalam[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekMalayalam?wdth,wght?.ttf")[0]
   end
 
   test do
