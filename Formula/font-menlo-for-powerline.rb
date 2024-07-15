03662575b79030fc656d7a1c1e611f610ffc8e50
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:23 +0000

    font-menlo-for-powerline updated

--- Formula/font-menlo-for-powerline.rb
+++ Formula/font-menlo-for-powerline.rb
@@ -4,10 +4,10 @@ class FontMenloForPowerline < Formula
   head "https://github.com/abertsch/Menlo-for-Powerline/archive/refs/heads/master.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Menlo-for-Powerline-master/Menlo for Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Menlo-for-Powerline-master/Menlo Bold for Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Menlo-for-Powerline-master/Menlo Italic for Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Menlo-for-Powerline-master/Menlo Bold Italic for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Menlo Bold Italic for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Menlo Bold for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Menlo Italic for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Menlo for Powerline.ttf")[0]
   end
 
   test do
