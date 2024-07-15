128eb0de70a6be805764b2736759449aea3e9d6b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:44 +0000

    font-zhudou-sans updated

--- Formula/font-zhudou-sans.rb
+++ Formula/font-zhudou-sans.rb
@@ -6,13 +6,13 @@ class FontZhudouSans < Formula
   sha256 "1a2718aa52c98d1ac7e18d60e0f1d61057b18e558e8196a3a770104855a6fc69"
 
   def install
-    (share/"fonts").install Dir.glob("./**/otf/ZhudouSans-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/ZhudouSans-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/ZhudouSans-Heavy.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/ZhudouSans-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/ZhudouSans-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/ZhudouSans-Normal.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/ZhudouSans-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ZhudouSans-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ZhudouSans-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ZhudouSans-Heavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ZhudouSans-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ZhudouSans-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ZhudouSans-Normal.otf")[0]
+    (share/"fonts").install Dir.glob("./**/ZhudouSans-Regular.otf")[0]
   end
 
   test do
