968d6f514f992dd74f1a56a4a2a0d21ce3c2c1b7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:16 +0000

    font-intel-one-mono updated

--- Formula/font-intel-one-mono.rb
+++ Formula/font-intel-one-mono.rb
@@ -6,14 +6,14 @@ class FontIntelOneMono < Formula
   sha256 "89921f9171fe1a9955c044b82da40121a096b3b38a984b68f49d92a73bda812b"
 
   def install
-    (share/"fonts").install Dir.glob("./**/otf/IntelOneMono-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/IntelOneMono-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/IntelOneMono-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/IntelOneMono-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/IntelOneMono-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/IntelOneMono-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/IntelOneMono-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/IntelOneMono-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IntelOneMono-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IntelOneMono-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IntelOneMono-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IntelOneMono-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IntelOneMono-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IntelOneMono-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IntelOneMono-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/IntelOneMono-Regular.otf")[0]
   end
 
   test do
