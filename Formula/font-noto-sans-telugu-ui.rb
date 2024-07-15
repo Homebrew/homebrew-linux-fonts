727b6a86be8f17ce993c6a537f828acd923d8943
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:28 +0000

    font-noto-sans-telugu-ui updated

--- Formula/font-noto-sans-telugu-ui.rb
+++ Formula/font-noto-sans-telugu-ui.rb
@@ -5,7 +5,7 @@ class FontNotoSansTeluguUi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansTeluguUI[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansTeluguUI?wdth,wght?.ttf")[0]
   end
 
   test do
