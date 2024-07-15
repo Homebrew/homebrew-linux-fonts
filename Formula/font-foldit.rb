7056834b8014b57868fac982cc4f7965a0f559dc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:10 +0000

    font-foldit updated

--- Formula/font-foldit.rb
+++ Formula/font-foldit.rb
@@ -5,7 +5,7 @@ class FontFoldit < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Foldit[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Foldit?wght?.ttf")[0]
   end
 
   test do
