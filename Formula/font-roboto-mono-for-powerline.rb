aa31a20034ec18623d8cb5ff2921efe51255bd0a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:37 +0000

    font-roboto-mono-for-powerline updated

--- Formula/font-roboto-mono-for-powerline.rb
+++ Formula/font-roboto-mono-for-powerline.rb
@@ -6,7 +6,7 @@ class FontRobotoMonoForPowerline < Formula
        only_path: "RobotoMono"
 
   def install
-    (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Bold Italic for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Bold for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Italic for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Light Italic for Powerline.ttf")[0]
@@ -15,7 +15,7 @@ class FontRobotoMonoForPowerline < Formula
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Medium for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Thin Italic for Powerline.ttf")[0]
     (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Thin for Powerline.ttf")[0]
-    (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono Bold Italic for Powerline.ttf")[0]
+    (share/"fonts").install Dir.glob("RobotoMono/./**/Roboto Mono for Powerline.ttf")[0]
   end
 
   test do
