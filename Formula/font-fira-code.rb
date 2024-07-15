2c09e0885310d151ee1b34f071698b357495b365
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:10 +0000

    font-fira-code updated

--- Formula/font-fira-code.rb
+++ Formula/font-fira-code.rb
@@ -6,12 +6,12 @@ class FontFiraCode < Formula
   sha256 "0949915ba8eb24d89fd93d10a7ff623f42830d7c5ffc3ecbf960e4ecad3e3e79"
 
   def install
-    (share/"fonts").install Dir.glob("./**/ttf/FiraCode-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/FiraCode-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/FiraCode-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/FiraCode-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/FiraCode-Retina.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/FiraCode-SemiBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCode-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCode-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCode-Medium.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCode-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCode-Retina.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FiraCode-SemiBold.ttf")[0]
   end
 
   test do
