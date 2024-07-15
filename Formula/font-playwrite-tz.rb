e37167e33da80413fa1d643e0106244152c81dfe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:34 +0000

    font-playwrite-tz updated

--- Formula/font-playwrite-tz.rb
+++ Formula/font-playwrite-tz.rb
@@ -5,7 +5,7 @@ class FontPlaywriteTz < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteTZ[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteTZ?wght?.ttf")[0]
   end
 
   test do
