7ef4b00b6e8effb66c817ce6abbb2999a62a5dc1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:26 +0000

    font-noto-sans-hk updated

--- Formula/font-noto-sans-hk.rb
+++ Formula/font-noto-sans-hk.rb
@@ -5,7 +5,7 @@ class FontNotoSansHk < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansHK[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansHK?wght?.ttf")[0]
   end
 
   test do
