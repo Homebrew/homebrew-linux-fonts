71a81864694434efa0e0a051dd1a15638f068267
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:04 +0000

    font-cairo-play updated

--- Formula/font-cairo-play.rb
+++ Formula/font-cairo-play.rb
@@ -5,7 +5,7 @@ class FontCairoPlay < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/CairoPlay[slnt,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CairoPlay?slnt,wght?.ttf")[0]
   end
 
   test do
