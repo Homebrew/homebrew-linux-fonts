b8e3a78b473b1e139c6e63ad3cf522a6a66d2ad1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:14 +0000

    font-hasklig updated

--- Formula/font-hasklig.rb
+++ Formula/font-hasklig.rb
@@ -6,20 +6,20 @@ class FontHasklig < Formula
   sha256 "9cd35a7449b220dc84f9516c57817e147003fc905a477f1ec727816d9d8a81d4"
 
   def install
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-SemiboldIt.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-BlackIt.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-BoldIt.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-ExtraLightIt.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-It.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-LightIt.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-MediumIt.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-Semibold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/Hasklig-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-BlackIt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-BoldIt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-ExtraLightIt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-It.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-LightIt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-MediumIt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-Semibold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Hasklig-SemiboldIt.otf")[0]
   end
 
   test do
