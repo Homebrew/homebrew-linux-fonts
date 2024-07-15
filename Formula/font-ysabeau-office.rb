2e465016ba72d7022426222eea879243b419bee6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:43 +0000

    font-ysabeau-office updated

--- Formula/font-ysabeau-office.rb
+++ Formula/font-ysabeau-office.rb
@@ -7,8 +7,8 @@ class FontYsabeauOffice < Formula
        only_path: "ofl/ysabeauoffice"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ysabeauoffice/./**/YsabeauOffice-Italic[wght].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ysabeauoffice/./**/YsabeauOffice[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ysabeauoffice/./**/YsabeauOffice-Italic?wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/ysabeauoffice/./**/YsabeauOffice?wght?.ttf")[0]
   end
 
   test do
