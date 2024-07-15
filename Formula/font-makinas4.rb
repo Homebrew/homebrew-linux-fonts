d6a5009602c92b4c3a82a46b0f1a8f770e570cfe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:22 +0000

    font-makinas4 updated

--- Formula/font-makinas4.rb
+++ Formula/font-makinas4.rb
@@ -4,8 +4,8 @@ class FontMakinas4 < Formula
   head "https://moji-waku.com/download/makinas4.zip"
 
   def install
-    (share/"fonts").install Dir.glob("./**/makinas4/Makinas-4-Flat.otf")[0]
-    (share/"fonts").install Dir.glob("./**/makinas4/Makinas-4-Square.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Makinas-4-Flat.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Makinas-4-Square.otf")[0]
   end
 
   test do
