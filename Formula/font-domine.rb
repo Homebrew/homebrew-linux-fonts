5af5c9fab431036faf519f3e625e3c58622f711b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:08 +0000

    font-domine updated

--- Formula/font-domine.rb
+++ Formula/font-domine.rb
@@ -5,7 +5,7 @@ class FontDomine < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Domine[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Domine?wght?.ttf")[0]
   end
 
   test do
