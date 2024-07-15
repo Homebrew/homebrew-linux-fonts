9ee03aaf3d52335b08508d38b8890361c74f51ce
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:29 +0000

    font-noto-serif-khojki updated

--- Formula/font-noto-serif-khojki.rb
+++ Formula/font-noto-serif-khojki.rb
@@ -5,7 +5,7 @@ class FontNotoSerifKhojki < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifKhojki[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifKhojki?wght?.ttf")[0]
   end
 
   test do
