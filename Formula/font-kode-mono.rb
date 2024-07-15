fb552088af4ea81906220e70e36f2d7056f19fcf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:19 +0000

    font-kode-mono updated

--- Formula/font-kode-mono.rb
+++ Formula/font-kode-mono.rb
@@ -5,7 +5,7 @@ class FontKodeMono < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/KodeMono[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/KodeMono?wght?.ttf")[0]
   end
 
   test do
