54f6a1c3c56a95ee2c5819c6d84f222b9077c16d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:09 +0000

    font-familjen-grotesk updated

--- Formula/font-familjen-grotesk.rb
+++ Formula/font-familjen-grotesk.rb
@@ -7,8 +7,8 @@ class FontFamiljenGrotesk < Formula
        only_path: "ofl/familjengrotesk"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/familjengrotesk/./**/FamiljenGrotesk-Italic[wght].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/familjengrotesk/./**/FamiljenGrotesk[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/familjengrotesk/./**/FamiljenGrotesk-Italic?wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/familjengrotesk/./**/FamiljenGrotesk?wght?.ttf")[0]
   end
 
   test do
