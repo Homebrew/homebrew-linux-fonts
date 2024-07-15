8194399d43b9e28b1de6e473c772f69ed837dfa6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:23 +0000

    font-migu-1m updated

--- Formula/font-migu-1m.rb
+++ Formula/font-migu-1m.rb
@@ -7,8 +7,8 @@ class FontMigu1m < Formula
   sha256 "e4806d297e59a7f9c235b0079b2819f44b8620d4365a8955cb612c9ff5809321"
 
   def install
-    (share/"fonts").install Dir.glob("./**/migu-1m-20200307/migu-1m-bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/migu-1m-20200307/migu-1m-regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-1m-bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/migu-1m-regular.ttf")[0]
   end
 
   test do
