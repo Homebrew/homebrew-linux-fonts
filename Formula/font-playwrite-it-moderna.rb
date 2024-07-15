6c3b458228598edb66837960d4a3fe02697d5ee7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:33 +0000

    font-playwrite-it-moderna updated

--- Formula/font-playwrite-it-moderna.rb
+++ Formula/font-playwrite-it-moderna.rb
@@ -5,7 +5,7 @@ class FontPlaywriteItModerna < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteITModerna[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteITModerna?wght?.ttf")[0]
   end
 
   test do
