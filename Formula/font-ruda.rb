dd475798f0d97ae13c4eb5af4c5e8a579f6fe048
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:37 +0000

    font-ruda updated

--- Formula/font-ruda.rb
+++ Formula/font-ruda.rb
@@ -5,7 +5,7 @@ class FontRuda < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Ruda[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Ruda?wght?.ttf")[0]
   end
 
   test do
