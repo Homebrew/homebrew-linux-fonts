5c7844eb8d005012d81416d5ed55019ce0d70000
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:11 +0000

    font-gandhi-sans updated

--- Formula/font-gandhi-sans.rb
+++ Formula/font-gandhi-sans.rb
@@ -4,10 +4,10 @@ class FontGandhiSans < Formula
   head "https://www.fontsquirrel.com/fonts/download/gandhi-sans"
 
   def install
-    (share/"fonts").install Dir.glob("./**/GandhiSans-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/GandhiSans-Italic.otf")[0]
     (share/"fonts").install Dir.glob("./**/GandhiSans-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/GandhiSans-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GandhiSans-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/GandhiSans-Regular.otf")[0]
   end
 
   test do
