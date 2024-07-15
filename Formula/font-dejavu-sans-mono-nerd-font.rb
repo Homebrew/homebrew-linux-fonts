363d018e42ffac824677e5fac0a8ee42bfdeb8c0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:07 +0000

    font-dejavu-sans-mono-nerd-font updated

--- Formula/font-dejavu-sans-mono-nerd-font.rb
+++ Formula/font-dejavu-sans-mono-nerd-font.rb
@@ -6,7 +6,7 @@ class FontDejavuSansMonoNerdFont < Formula
   sha256 "cf0f11c7e2e35df79c5a2f3b42073e8becd3597f23cbd4487de89c3546c17e70"
 
   def install
-    (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFont-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFont-Oblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFont-Regular.ttf")[0]
@@ -17,7 +17,7 @@ class FontDejavuSansMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFontPropo-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFontPropo-Oblique.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DejaVuSansMNerdFontPropo-Regular.ttf")[0]
   end
 
   test do
