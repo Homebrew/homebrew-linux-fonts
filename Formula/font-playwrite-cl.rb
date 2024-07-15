277bbe91e04538fbeb7c7f6d3279ba6cbc796ae4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:32 +0000

    font-playwrite-cl updated

--- Formula/font-playwrite-cl.rb
+++ Formula/font-playwrite-cl.rb
@@ -1,10 +1,11 @@
 class FontPlaywriteCl < Formula
   desc "Playwrite cl font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritecl/PlaywriteCL%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+CL"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritecl/PlaywriteCL%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteCL[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteCL?wght?.ttf")[0]
   end
 
   test do
