1146f608e5c34f0b451ea815dcea4a17176d17f4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:38 +0000

    font-signika-negative updated

--- Formula/font-signika-negative.rb
+++ Formula/font-signika-negative.rb
@@ -5,7 +5,7 @@ class FontSignikaNegative < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/SignikaNegative[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/SignikaNegative?wght?.ttf")[0]
   end
 
   test do
