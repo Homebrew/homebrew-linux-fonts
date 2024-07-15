ce104cb4cf5d33441971b3ef10819ac83a086f53
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:05 +0000

    font-chapbook updated

--- Formula/font-chapbook.rb
+++ Formula/font-chapbook.rb
@@ -4,9 +4,9 @@ class FontChapbook < Formula
   head "https://www.feorag.com/freestuff/archives/Chapbook-OTF.zip"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Chapbook OTF/Chapbook-Dingbats.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Chapbook OTF/Chapbook-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/Chapbook OTF/Chapbook-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Chapbook-Dingbats.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Chapbook-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Chapbook-Regular.otf")[0]
   end
 
   test do
