c2e2f3aad5a8fcf7bd0c57939d42e86b62aca677
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:05 +0000

    font-climate-crisis updated

--- Formula/font-climate-crisis.rb
+++ Formula/font-climate-crisis.rb
@@ -5,7 +5,7 @@ class FontClimateCrisis < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/ClimateCrisis[YEAR].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/ClimateCrisis?YEAR?.ttf")[0]
   end
 
   test do
