03369c1e830129fece1b886e22ee8872a893fde2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:10 +0000

    font-firge updated

--- Formula/font-firge.rb
+++ Formula/font-firge.rb
@@ -6,14 +6,14 @@ class FontFirge < Formula
   sha256 "c986f621000a6598064c7e2078322ed02040af4f7804d258033df949b35b0527"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Firge_v0.3.0/FirgeConsole-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Firge_v0.3.0/Firge-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Firge_v0.3.0/Firge35-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Firge_v0.3.0/Firge35-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Firge_v0.3.0/Firge35Console-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Firge_v0.3.0/Firge35Console-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Firge_v0.3.0/FirgeConsole-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Firge_v0.3.0/Firge-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Firge-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Firge-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Firge35-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Firge35-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Firge35Console-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Firge35Console-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FirgeConsole-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/FirgeConsole-Regular.ttf")[0]
   end
 
   test do
