b0a0b9a8e053c9985cefa73f99dc605b0d3dea50
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:14 +0000

    font-heebo updated

--- Formula/font-heebo.rb
+++ Formula/font-heebo.rb
@@ -5,7 +5,7 @@ class FontHeebo < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Heebo[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Heebo?wght?.ttf")[0]
   end
 
   test do
