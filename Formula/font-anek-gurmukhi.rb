f55c454118b8938a9e000d1bdbf053e6bac8b09d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:00 +0000

    font-anek-gurmukhi updated

--- Formula/font-anek-gurmukhi.rb
+++ Formula/font-anek-gurmukhi.rb
@@ -5,7 +5,7 @@ class FontAnekGurmukhi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/AnekGurmukhi[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AnekGurmukhi?wdth,wght?.ttf")[0]
   end
 
   test do
