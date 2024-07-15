e1fb0460021da5be37b1235eb2663ef41d3775f7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:42 +0000

    font-udev-gothic-nf updated

--- Formula/font-udev-gothic-nf.rb
+++ Formula/font-udev-gothic-nf.rb
@@ -6,22 +6,22 @@ class FontUdevGothicNf < Formula
   sha256 "84004a3038bdf528286a113b4db076d8412bb4ca6771d02a240318473f9b9fce"
 
   def install
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothicNFLG-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothic35NF-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothic35NF-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothic35NF-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothic35NFLG-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothic35NFLG-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothic35NFLG-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothic35NFLG-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothicNF-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothicNF-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothicNF-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothicNF-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothicNFLG-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothicNFLG-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothicNFLG-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UDEVGothic_NF_v1.3.1/UDEVGothic35NF-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothic35NF-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothic35NF-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothic35NF-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothic35NF-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothic35NFLG-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothic35NFLG-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothic35NFLG-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothic35NFLG-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothicNF-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothicNF-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothicNF-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothicNF-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothicNFLG-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothicNFLG-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothicNFLG-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UDEVGothicNFLG-Regular.ttf")[0]
   end
 
   test do
