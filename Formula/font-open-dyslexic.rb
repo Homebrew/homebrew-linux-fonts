49241eccd01954c9b820fa9caaa6c559c26994f7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:30 +0000

    font-open-dyslexic updated

--- Formula/font-open-dyslexic.rb
+++ Formula/font-open-dyslexic.rb
@@ -6,15 +6,15 @@ class FontOpenDyslexic < Formula
   sha256 "a44fde7d5fcf1e3825d00de20f7d71fb7b339a7e71067cd9566e8ab16692802a"
 
   def install
-    (share/"fonts").install Dir.glob("./**/open-dyslexic-20160623-Stable/otf/OpenDyslexicMono-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/open-dyslexic-20160623-Stable/otf/OpenDyslexic-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/open-dyslexic-20160623-Stable/otf/OpenDyslexic-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/open-dyslexic-20160623-Stable/otf/OpenDyslexic-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/open-dyslexic-20160623-Stable/otf/OpenDyslexicAlta-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/open-dyslexic-20160623-Stable/otf/OpenDyslexicAlta-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/open-dyslexic-20160623-Stable/otf/OpenDyslexicAlta-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/open-dyslexic-20160623-Stable/otf/OpenDyslexicAlta-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/open-dyslexic-20160623-Stable/otf/OpenDyslexic-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/OpenDyslexic-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/OpenDyslexic-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/OpenDyslexic-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/OpenDyslexic-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/OpenDyslexicAlta-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/OpenDyslexicAlta-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/OpenDyslexicAlta-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/OpenDyslexicAlta-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/OpenDyslexicMono-Regular.otf")[0]
   end
 
   test do
