5e3f0d2d3752b3885f5f387fc5dd62f7a68b8d18
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:10 +0000

    font-fantasque-sans-mono updated

--- Formula/font-fantasque-sans-mono.rb
+++ Formula/font-fantasque-sans-mono.rb
@@ -6,10 +6,10 @@ class FontFantasqueSansMono < Formula
   sha256 "84be689e231ff773ed9d352e83dccd8151d9e445f1cb0b88cb0e9330fc4d9cfc"
 
   def install
-    (share/"fonts").install Dir.glob("./**/OTF/FantasqueSansMono-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/FantasqueSansMono-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/FantasqueSansMono-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/OTF/FantasqueSansMono-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/FantasqueSansMono-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/FantasqueSansMono-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/FantasqueSansMono-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/FantasqueSansMono-Regular.otf")[0]
   end
 
   test do
