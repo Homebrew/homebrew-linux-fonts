de5be0e05c10a259b4ad049fc29ae551083f78fc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:05 +0000

    font-clear-sans updated

--- Formula/font-clear-sans.rb
+++ Formula/font-clear-sans.rb
@@ -8,14 +8,14 @@ class FontClearSans < Formula
   deprecate! "2023-12-17", because: :discontinued
 
   def install
-    (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Light.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("TTF/./**/ClearSans-Thin.ttf")[0]
   end
 
   test do
