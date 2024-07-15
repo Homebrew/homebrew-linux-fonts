0b185d0a685a2a166405c0d231518d99a119ce69
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:05 +0000

    font-code-new-roman-nerd-font updated

--- Formula/font-code-new-roman-nerd-font.rb
+++ Formula/font-code-new-roman-nerd-font.rb
@@ -6,7 +6,7 @@ class FontCodeNewRomanNerdFont < Formula
   sha256 "859a283172d71538634104b87b48cea4e36f1bb0824defe7d99a0092d6f5e52d"
 
   def install
-    (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFontPropo-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFont-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFont-Italic.otf")[0]
     (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFont-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFontMono-Bold.otf")[0]
@@ -14,7 +14,7 @@ class FontCodeNewRomanNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFontMono-Regular.otf")[0]
     (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFontPropo-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFontPropo-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFont-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CodeNewRomanNerdFontPropo-Regular.otf")[0]
   end
 
   test do
