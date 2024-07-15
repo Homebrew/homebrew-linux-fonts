0158b6bad36cc80eb2c2c0e473cf5d97b901493a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:05 +0000

    font-caveat updated

--- Formula/font-caveat.rb
+++ Formula/font-caveat.rb
@@ -5,7 +5,7 @@ class FontCaveat < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Caveat[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Caveat?wght?.ttf")[0]
   end
 
   test do
