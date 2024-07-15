9928740d739b8806af397136fe963fcc309a9288
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:16 +0000

    font-iosevka-aile updated

--- Formula/font-iosevka-aile.rb
+++ Formula/font-iosevka-aile.rb
@@ -6,7 +6,7 @@ class FontIosevkaAile < Formula
   sha256 "9bdc938612239b476a1db15b77fa7a88a7eaeb8994bc08508eebe720142e80f2"
 
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaAile-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaAile-Bold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-ExtraBold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-ExtraLight.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-Heavy.ttc")[0]
@@ -14,7 +14,7 @@ class FontIosevkaAile < Formula
     (share/"fonts").install Dir.glob("./**/IosevkaAile-Medium.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-Regular.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaAile-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaAile-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaAile-Thin.ttc")[0]
   end
 
   test do
