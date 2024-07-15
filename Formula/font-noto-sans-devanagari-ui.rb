6ff29115c095fdcbeb502c4ff16244c8bda8a13d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:26 +0000

    font-noto-sans-devanagari-ui updated

--- Formula/font-noto-sans-devanagari-ui.rb
+++ Formula/font-noto-sans-devanagari-ui.rb
@@ -7,7 +7,7 @@ class FontNotoSansDevanagariUi < Formula
        only_path: "ofl/notosansdevanagariui"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-ExtraLight.ttf")[0]
@@ -15,7 +15,7 @@ class FontNotoSansDevanagariUi < Formula
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/notosansdevanagariui/./**/NotoSansDevanagariUI-Thin.ttf")[0]
   end
 
   test do
