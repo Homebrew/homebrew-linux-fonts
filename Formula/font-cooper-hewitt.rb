76e37d13ff36ca09c957e94af99aa5656e9d6a28
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:06 +0000

    font-cooper-hewitt updated

--- Formula/font-cooper-hewitt.rb
+++ Formula/font-cooper-hewitt.rb
@@ -4,20 +4,20 @@ class FontCooperHewitt < Formula
   head "https://www.cooperhewitt.org/wp-content/uploads/fonts/CooperHewitt-OTF-public.zip"
 
   def install
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-ThinItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-Book.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-BookItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-Heavy.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-HeavyItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-Semibold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-SemiboldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-Thin.otf")[0]
-    (share/"fonts").install Dir.glob("./**/CooperHewitt-OTF-public/CooperHewitt-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-Book.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-BookItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-Heavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-HeavyItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-Semibold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-SemiboldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CooperHewitt-ThinItalic.otf")[0]
   end
 
   test do
