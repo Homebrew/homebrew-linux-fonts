2009145e339439aa448eade5a99f6a5a060c75db
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:25 +0000

    font-niramit updated

--- Formula/font-niramit.rb
+++ Formula/font-niramit.rb
@@ -7,7 +7,7 @@ class FontNiramit < Formula
        only_path: "ofl/niramit"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-ExtraLightItalic.ttf")[0]
@@ -18,7 +18,7 @@ class FontNiramit < Formula
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/niramit/./**/Niramit-SemiBoldItalic.ttf")[0]
   end
 
   test do
