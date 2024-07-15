08a57d6ee3499064880d6300458abc22651e2e88
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:28 +0000

    font-noto-sans-thai-ui updated

--- Formula/font-noto-sans-thai-ui.rb
+++ Formula/font-noto-sans-thai-ui.rb
@@ -5,7 +5,7 @@ class FontNotoSansThaiUi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansThaiUI[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansThaiUI?wdth,wght?.ttf")[0]
   end
 
   test do
