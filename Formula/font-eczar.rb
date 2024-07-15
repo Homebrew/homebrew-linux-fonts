661416f8631e9ed0e5e7f46bc2417b20e50c04e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:08 +0000

    font-eczar updated

--- Formula/font-eczar.rb
+++ Formula/font-eczar.rb
@@ -5,7 +5,7 @@ class FontEczar < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Eczar[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Eczar?wght?.ttf")[0]
   end
 
   test do
