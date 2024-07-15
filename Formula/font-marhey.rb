d88c237d6802a45790c3e4e7580f7af7b17236d7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:22 +0000

    font-marhey updated

--- Formula/font-marhey.rb
+++ Formula/font-marhey.rb
@@ -5,7 +5,7 @@ class FontMarhey < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Marhey[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Marhey?wght?.ttf")[0]
   end
 
   test do
