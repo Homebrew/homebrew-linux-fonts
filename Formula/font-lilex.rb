0e3f961810046c789de43df03ec8ea70b093467e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:21 +0000

    font-lilex updated

--- Formula/font-lilex.rb
+++ Formula/font-lilex.rb
@@ -6,13 +6,13 @@ class FontLilex < Formula
   sha256 "2fd9cf5d12ca390bba8c487f189f78e5900e7f6f7d6a702d6ed5074c26260537"
 
   def install
-    (share/"fonts").install Dir.glob("./**/ttf/Lilex-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/Lilex-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/Lilex-ExtraThick.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/Lilex-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/Lilex-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ttf/Lilex-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/variable/Lilex-VF.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-ExtraLight.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-ExtraThick.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-Medium.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-VF.ttf")[0]
   end
 
   test do
