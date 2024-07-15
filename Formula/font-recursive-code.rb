5fd63a10bc9fd263b78bb7db71d97169482006cf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:36 +0000

    font-recursive-code updated

--- Formula/font-recursive-code.rb
+++ Formula/font-recursive-code.rb
@@ -6,22 +6,22 @@ class FontRecursiveCode < Formula
   sha256 "cbcbdf7a0e181d284a9235e09ed5f3873e527bc5dd1d977df71cdc1ff937da02"
 
   def install
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoLinear/RecMonoLinear-Regular-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoSemicasual/RecMonoSemicasual-Bold-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoSemicasual/RecMonoSemicasual-Italic-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoSemicasual/RecMonoSemicasual-Regular-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoDuotone/RecMonoDuotone-BoldItalic-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoDuotone/RecMonoDuotone-Bold-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoDuotone/RecMonoDuotone-Italic-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoDuotone/RecMonoDuotone-Regular-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoCasual/RecMonoCasual-BoldItalic-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoCasual/RecMonoCasual-Bold-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoCasual/RecMonoCasual-Italic-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoCasual/RecMonoCasual-Regular-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoLinear/RecMonoLinear-BoldItalic-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoLinear/RecMonoLinear-Bold-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoLinear/RecMonoLinear-Italic-1.085.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/ArrowType-Recursive-1.085/Recursive_Code/RecMonoSemicasual/RecMonoSemicasual-BoldItalic-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoCasual/RecMonoCasual-Bold-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoCasual/RecMonoCasual-BoldItalic-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoCasual/RecMonoCasual-Italic-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoCasual/RecMonoCasual-Regular-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoDuotone/RecMonoDuotone-Bold-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoDuotone/RecMonoDuotone-BoldItalic-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoDuotone/RecMonoDuotone-Italic-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoDuotone/RecMonoDuotone-Regular-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoLinear/RecMonoLinear-Bold-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoLinear/RecMonoLinear-BoldItalic-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoLinear/RecMonoLinear-Italic-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoLinear/RecMonoLinear-Regular-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoSemicasual/RecMonoSemicasual-Bold-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoSemicasual/RecMonoSemicasual-BoldItalic-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoSemicasual/RecMonoSemicasual-Italic-1.085.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Recursive_Code/RecMonoSemicasual/RecMonoSemicasual-Regular-1.085.ttf")[0]
   end
 
   test do
