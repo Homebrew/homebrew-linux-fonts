e9669783150f8fa496c67657009ca17b65879bcf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:33 +0000

    font-playwrite-dk-uloopet updated

--- Formula/font-playwrite-dk-uloopet.rb
+++ Formula/font-playwrite-dk-uloopet.rb
@@ -1,10 +1,11 @@
 class FontPlaywriteDkUloopet < Formula
   desc "Playwrite dk uloopet font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritedkuloopet/PlaywriteDKUloopet%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+DK+Uloopet"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritedkuloopet/PlaywriteDKUloopet%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteDKUloopet[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteDKUloopet?wght?.ttf")[0]
   end
 
   test do
