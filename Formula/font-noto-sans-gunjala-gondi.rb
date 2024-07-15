2215d1c03ae2072532cb5e78ba9e7d0dd0f0a984
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:26 +0000

    font-noto-sans-gunjala-gondi updated

--- Formula/font-noto-sans-gunjala-gondi.rb
+++ Formula/font-noto-sans-gunjala-gondi.rb
@@ -5,7 +5,7 @@ class FontNotoSansGunjalaGondi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansGunjalaGondi[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansGunjalaGondi?wght?.ttf")[0]
   end
 
   test do
