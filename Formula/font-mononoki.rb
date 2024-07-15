2b2b02cee797a2950cbf8264438d5b293fbc190b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:24 +0000

    font-mononoki updated

--- Formula/font-mononoki.rb
+++ Formula/font-mononoki.rb
@@ -7,14 +7,14 @@ class FontMononoki < Formula
   sha256 "86549faebfd9231c3bd0942038cb1f717f5186729c9a54d0045e36b996c1a355"
 
   def install
-    (share/"fonts").install Dir.glob("./**/mononoki-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/mononoki-Bold.otf")[0]
     (share/"fonts").install Dir.glob("./**/mononoki-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/mononoki-BoldItalic.otf")[0]
     (share/"fonts").install Dir.glob("./**/mononoki-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/mononoki-Italic.otf")[0]
     (share/"fonts").install Dir.glob("./**/mononoki-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/mononoki-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/mononoki-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/mononoki-Regular.ttf")[0]
   end
 
   test do
