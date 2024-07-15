f61f83dab764a7648306a16fba049c8211dda663
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:09 +0000

    font-encode-sans-expanded updated

--- Formula/font-encode-sans-expanded.rb
+++ Formula/font-encode-sans-expanded.rb
@@ -7,7 +7,7 @@ class FontEncodeSansExpanded < Formula
        only_path: "ofl/encodesansexpanded"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-ExtraLight.ttf")[0]
@@ -15,7 +15,7 @@ class FontEncodeSansExpanded < Formula
     (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/encodesansexpanded/./**/EncodeSansExpanded-Thin.ttf")[0]
   end
 
   test do
