f0656e119a2734a0438ebd8afe8bb2a9dac1a56c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:12 +0000

    font-gentium-basic updated

--- Formula/font-gentium-basic.rb
+++ Formula/font-gentium-basic.rb
@@ -6,10 +6,10 @@ class FontGentiumBasic < Formula
   sha256 "2f1a2c5491d7305dffd3520c6375d2f3e14931ee35c6d8ae1e8f098bf1a7b3cc"
 
   def install
-    (share/"fonts").install Dir.glob("./**/GentiumBasic_1102/GenBasB.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumBasic_1102/GenBasBI.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumBasic_1102/GenBasI.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/GentiumBasic_1102/GenBasR.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenBasB.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenBasBI.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenBasI.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/GenBasR.ttf")[0]
   end
 
   test do
