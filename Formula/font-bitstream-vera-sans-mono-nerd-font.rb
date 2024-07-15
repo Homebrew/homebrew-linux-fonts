f48318db70b2655e992c7aa0d394d94d2cc5c503
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:03 +0000

    font-bitstream-vera-sans-mono-nerd-font updated

--- Formula/font-bitstream-vera-sans-mono-nerd-font.rb
+++ Formula/font-bitstream-vera-sans-mono-nerd-font.rb
@@ -6,7 +6,7 @@ class FontBitstreamVeraSansMonoNerdFont < Formula
   sha256 "6a5d20c72f068a65ebf6e1cf1e168ced5d8c78d703d90b6041a5232db8111205"
 
   def install
-    (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-Oblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-Regular.ttf")[0]
@@ -17,7 +17,7 @@ class FontBitstreamVeraSansMonoNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-BoldOblique.ttf")[0]
     (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-Oblique.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/BitstromWeraNerdFontPropo-Regular.ttf")[0]
   end
 
   test do
