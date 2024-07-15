ffe9ab337f7f38dd8f7dd296c77d3302211ffccd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:19 +0000

    font-kantumruy-pro updated

--- Formula/font-kantumruy-pro.rb
+++ Formula/font-kantumruy-pro.rb
@@ -7,8 +7,8 @@ class FontKantumruyPro < Formula
        only_path: "ofl/kantumruypro"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/kantumruypro/./**/KantumruyPro-Italic[wght].ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/kantumruypro/./**/KantumruyPro[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kantumruypro/./**/KantumruyPro-Italic?wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/kantumruypro/./**/KantumruyPro?wght?.ttf")[0]
   end
 
   test do
