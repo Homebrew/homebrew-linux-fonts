03c14ad7e0997aa377bce6abaa5b61d7875d9dd4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:42 +0000

    font-ubuntu-nerd-font updated

--- Formula/font-ubuntu-nerd-font.rb
+++ Formula/font-ubuntu-nerd-font.rb
@@ -6,7 +6,7 @@ class FontUbuntuNerdFont < Formula
   sha256 "e6029f1439ef78ed40dcd26a17fac43a69e493a9ac13616aa9b677db8a35c960"
 
   def install
-    (share/"fonts").install Dir.glob("./**/UbuntuNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UbuntuNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/UbuntuNerdFont-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/UbuntuNerdFont-Condensed.ttf")[0]
     (share/"fonts").install Dir.glob("./**/UbuntuNerdFont-Italic.ttf")[0]
@@ -23,7 +23,7 @@ class FontUbuntuNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/UbuntuNerdFontPropo-LightItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/UbuntuNerdFontPropo-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/UbuntuNerdFontPropo-MediumItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/UbuntuNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/UbuntuNerdFontPropo-Regular.ttf")[0]
   end
 
   test do
