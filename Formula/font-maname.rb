160c2f2b5a846ec825541e52a9580449bcf807f2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:22 +0000

    font-maname updated

--- Formula/font-maname.rb
+++ Formula/font-maname.rb
@@ -1,7 +1,8 @@
 class FontManame < Formula
   desc "Maname font"
-  homepage "https://github.com/mooniak/maname-font"
-  head "https://github.com/google/fonts/raw/main/ofl/maname/Maname-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Maname"
+  head "https://github.com/google/fonts/raw/main/ofl/maname/Maname-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/Maname-Regular.ttf")[0]
