0a8af969afa59f5b3223f9a368935bfc0db42a34
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:29 +0000

    font-noto-serif-hk updated

--- Formula/font-noto-serif-hk.rb
+++ Formula/font-noto-serif-hk.rb
@@ -5,7 +5,7 @@ class FontNotoSerifHk < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSerifHK[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSerifHK?wght?.ttf")[0]
   end
 
   test do
