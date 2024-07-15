a9b8b102cba24fb48a65c81455b8cd833f38886d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:20 +0000

    font-lekton-nerd-font updated

--- Formula/font-lekton-nerd-font.rb
+++ Formula/font-lekton-nerd-font.rb
@@ -6,7 +6,7 @@ class FontLektonNerdFont < Formula
   sha256 "76685975a18c6047e391141ca4a49d8e0ff549b02f69aa77dd3afc1e1e98cc4e"
 
   def install
-    (share/"fonts").install Dir.glob("./**/LektonNerdFontPropo-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LektonNerdFont-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LektonNerdFont-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LektonNerdFont-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LektonNerdFontMono-Bold.ttf")[0]
@@ -14,7 +14,7 @@ class FontLektonNerdFont < Formula
     (share/"fonts").install Dir.glob("./**/LektonNerdFontMono-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LektonNerdFontPropo-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LektonNerdFontPropo-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/LektonNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LektonNerdFontPropo-Regular.ttf")[0]
   end
 
   test do
