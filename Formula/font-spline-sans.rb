d9d72bf2d32a044793b740815dccbcfb22ab5749
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:40 +0000

    font-spline-sans updated

--- Formula/font-spline-sans.rb
+++ Formula/font-spline-sans.rb
@@ -5,7 +5,7 @@ class FontSplineSans < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/SplineSans[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/SplineSans?wght?.ttf")[0]
   end
 
   test do
