2215fcb1ec15c729f11c691fd45d6bbaffaf46eb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:13 +0000

    font-gluten updated

--- Formula/font-gluten.rb
+++ Formula/font-gluten.rb
@@ -5,7 +5,7 @@ class FontGluten < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Gluten[slnt,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Gluten?slnt,wght?.ttf")[0]
   end
 
   test do
