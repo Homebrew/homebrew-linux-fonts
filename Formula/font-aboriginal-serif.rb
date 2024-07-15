6617fe6da78cf996bab6309bb06ad4ada4ad5e8e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:21:58 +0000

    font-aboriginal-serif updated

--- Formula/font-aboriginal-serif.rb
+++ Formula/font-aboriginal-serif.rb
@@ -4,10 +4,10 @@ class FontAboriginalSerif < Formula
   head "https://www.languagegeek.com/font/AboriginalSerif.zip"
 
   def install
-    (share/"fonts").install Dir.glob("./**/AboriginalSerifREGULAR.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AboriginalSerifBOLD.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AboriginalSerifBOLDITALIC.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AboriginalSerifITALIC.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AboriginalSerifREGULAR.ttf")[0]
   end
 
   test do
