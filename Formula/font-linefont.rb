55e1106fddd5eafcf3d39553ce7274788e7b2a47
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:21 +0000

    font-linefont updated

--- Formula/font-linefont.rb
+++ Formula/font-linefont.rb
@@ -5,7 +5,7 @@ class FontLinefont < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Linefont[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Linefont?wdth,wght?.ttf")[0]
   end
 
   test do
