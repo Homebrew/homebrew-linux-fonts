17b6cf09feefe759af30df83a0177665563621fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:08 +0000

    font-edu-nsw-act-foundation updated

--- Formula/font-edu-nsw-act-foundation.rb
+++ Formula/font-edu-nsw-act-foundation.rb
@@ -5,7 +5,7 @@ class FontEduNswActFoundation < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/EduNSWACTFoundation[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/EduNSWACTFoundation?wght?.ttf")[0]
   end
 
   test do
