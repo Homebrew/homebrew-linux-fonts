de91b36b0d1c39306d9851f5504573776afffbab
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:31 +0000

    font-overpass-mono updated

--- Formula/font-overpass-mono.rb
+++ Formula/font-overpass-mono.rb
@@ -5,7 +5,7 @@ class FontOverpassMono < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/OverpassMono[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/OverpassMono?wght?.ttf")[0]
   end
 
   test do
