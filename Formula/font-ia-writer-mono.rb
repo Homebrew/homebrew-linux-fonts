7fdeed60fdde504b4158ce9b87937cbb51db76f0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:15 +0000

    font-ia-writer-mono updated

--- Formula/font-ia-writer-mono.rb
+++ Formula/font-ia-writer-mono.rb
@@ -4,12 +4,12 @@ class FontIaWriterMono < Formula
   head "https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Mono/Variable/iAWriterMonoV-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/iA-Fonts-master/iA Writer Mono/Variable/iAWriterMonoV.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Mono/Static/iAWriterMonoS-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Mono/Static/iAWriterMonoS-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Mono/Static/iAWriterMonoS-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Mono/Static/iAWriterMonoS-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Mono/Variable/iAWriterMonoV-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/iA Writer Mono/Variable/iAWriterMonoV.ttf")[0]
   end
 
   test do
