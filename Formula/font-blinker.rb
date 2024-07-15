0dc2b3757823df7286e859c26e3d513d7cd4ab66
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:03 +0000

    font-blinker updated

--- Formula/font-blinker.rb
+++ Formula/font-blinker.rb
@@ -7,14 +7,14 @@ class FontBlinker < Formula
        only_path: "ofl/blinker"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Black.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-ExtraLight.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Light.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Black.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/blinker/./**/Blinker-Thin.ttf")[0]
   end
 
   test do
