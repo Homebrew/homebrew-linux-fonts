c8bfce676eef9cc70fa001237c5381c0093d2e46
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:40 +0000

    font-spleen updated

--- Formula/font-spleen.rb
+++ Formula/font-spleen.rb
@@ -6,11 +6,11 @@ class FontSpleen < Formula
   sha256 "8b47c56f1a6eb858fbcf9e34530557404b02fbb3455e38e64fb84473fd0c372f"
 
   def install
-    (share/"fonts").install Dir.glob("./**/spleen-2.1.0/spleen-6x12.otf")[0]
-    (share/"fonts").install Dir.glob("./**/spleen-2.1.0/spleen-8x16.otf")[0]
-    (share/"fonts").install Dir.glob("./**/spleen-2.1.0/spleen-12x24.otf")[0]
-    (share/"fonts").install Dir.glob("./**/spleen-2.1.0/spleen-16x32.otf")[0]
-    (share/"fonts").install Dir.glob("./**/spleen-2.1.0/spleen-32x64.otf")[0]
+    (share/"fonts").install Dir.glob("./**/spleen-12x24.otf")[0]
+    (share/"fonts").install Dir.glob("./**/spleen-16x32.otf")[0]
+    (share/"fonts").install Dir.glob("./**/spleen-32x64.otf")[0]
+    (share/"fonts").install Dir.glob("./**/spleen-6x12.otf")[0]
+    (share/"fonts").install Dir.glob("./**/spleen-8x16.otf")[0]
   end
 
   test do
