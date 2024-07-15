d55103dc7f42111de3bfbf7354cb6dd209175a6d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:21:58 +0000

    font-aboriginal-sans updated

--- Formula/font-aboriginal-sans.rb
+++ Formula/font-aboriginal-sans.rb
@@ -4,10 +4,10 @@ class FontAboriginalSans < Formula
   head "https://www.languagegeek.com/font/AboriginalSans.zip"
 
   def install
-    (share/"fonts").install Dir.glob("./**/AboriginalSansREGULAR.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AboriginalSansBOLD.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AboriginalSansBOLDITALIC.ttf")[0]
     (share/"fonts").install Dir.glob("./**/AboriginalSansITALIC.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/AboriginalSansREGULAR.ttf")[0]
   end
 
   test do
