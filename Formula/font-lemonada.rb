93fc6c7bf7070ed24143da4ebd2a20bf9dae31dd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:20 +0000

    font-lemonada updated

--- Formula/font-lemonada.rb
+++ Formula/font-lemonada.rb
@@ -5,7 +5,7 @@ class FontLemonada < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Lemonada[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lemonada?wght?.ttf")[0]
   end
 
   test do
