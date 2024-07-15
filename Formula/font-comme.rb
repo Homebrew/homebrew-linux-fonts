74524792d993e5a0f6bcb90ca700d64871cb24ff
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:06 +0000

    font-comme updated

--- Formula/font-comme.rb
+++ Formula/font-comme.rb
@@ -5,7 +5,7 @@ class FontComme < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Comme[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Comme?wght?.ttf")[0]
   end
 
   test do
