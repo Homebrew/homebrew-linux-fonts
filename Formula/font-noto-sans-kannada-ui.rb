b46725b1d13bd88727b7c13bee5efe2c37021765
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:26 +0000

    font-noto-sans-kannada-ui updated

--- Formula/font-noto-sans-kannada-ui.rb
+++ Formula/font-noto-sans-kannada-ui.rb
@@ -5,7 +5,7 @@ class FontNotoSansKannadaUi < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NotoSansKannadaUI[wdth,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/NotoSansKannadaUI?wdth,wght?.ttf")[0]
   end
 
   test do
