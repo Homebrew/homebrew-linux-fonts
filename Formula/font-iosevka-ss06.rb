28d833d34c95003d1e5334e1c6ec774920162b45
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:17 +0000

    font-iosevka-ss06 updated

--- Formula/font-iosevka-ss06.rb
+++ Formula/font-iosevka-ss06.rb
@@ -6,7 +6,7 @@ class FontIosevkaSs06 < Formula
   sha256 "9b645f28d9df6d835c23f49ba245d31f458d0111ca7730b0fdbfb84a20c3908f"
 
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaSS06-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS06-Bold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-ExtraBold.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-ExtraLight.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-Heavy.ttc")[0]
@@ -14,7 +14,7 @@ class FontIosevkaSs06 < Formula
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-Medium.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-Regular.ttc")[0]
     (share/"fonts").install Dir.glob("./**/IosevkaSS06-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS06-Bold.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS06-Thin.ttc")[0]
   end
 
   test do
