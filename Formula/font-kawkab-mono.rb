59f71ba186078af7b822f2c0381f92f3ff82e1a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:19 +0000

    font-kawkab-mono updated

--- Formula/font-kawkab-mono.rb
+++ Formula/font-kawkab-mono.rb
@@ -7,9 +7,9 @@ class FontKawkabMono < Formula
   sha256 "11c06f57dddefaf0166d74caaa072865ab6ff8d34076e7ec5d2c20edda145666"
 
   def install
-    (share/"fonts").install Dir.glob("./**/kawkab-mono-0.501/KawkabMono-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/kawkab-mono-0.501/KawkabMono-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/kawkab-mono-0.501/KawkabMono-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/KawkabMono-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/KawkabMono-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/KawkabMono-Regular.otf")[0]
   end
 
   test do
