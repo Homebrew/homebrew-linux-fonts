b7947980f1d14b422d4aa93931eb9f68437e5a9c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:07 +0000

    font-dancing-script updated

--- Formula/font-dancing-script.rb
+++ Formula/font-dancing-script.rb
@@ -5,7 +5,7 @@ class FontDancingScript < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/DancingScript[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DancingScript?wght?.ttf")[0]
   end
 
   test do
