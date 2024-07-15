f0e46c3328e8ffb0203bc69b41f176e6576d1f64
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:05 +0000

    font-charis-sil updated

--- Formula/font-charis-sil.rb
+++ Formula/font-charis-sil.rb
@@ -6,10 +6,10 @@ class FontCharisSil < Formula
   sha256 "4b09aa75760b8aa697b762c34afb995dde0754c8f09256cb912dbfc478c97ade"
 
   def install
-    (share/"fonts").install Dir.glob("./**/CharisSIL-6.200/CharisSIL-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/CharisSIL-6.200/CharisSIL-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/CharisSIL-6.200/CharisSIL-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/CharisSIL-6.200/CharisSIL-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CharisSIL-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CharisSIL-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CharisSIL-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/CharisSIL-Regular.ttf")[0]
   end
 
   test do
