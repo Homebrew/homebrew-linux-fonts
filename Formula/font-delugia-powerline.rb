ed318f08ca689eef6a38aab5fa4b0d8e7ce3752d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:07 +0000

    font-delugia-powerline updated

--- Formula/font-delugia-powerline.rb
+++ Formula/font-delugia-powerline.rb
@@ -6,12 +6,12 @@ class FontDelugiaPowerline < Formula
   sha256 "4baf2f2a151eda7559f7cac7c378768ca5a80ce7b79b0fb3f93a87ff9f5b79ca"
 
   def install
-    (share/"fonts").install Dir.glob("./**/delugia-powerline/DelugiaPL-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-powerline/DelugiaPL-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-powerline/DelugiaPL-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-powerline/DelugiaPL.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-powerline/DelugiaPLLight-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/delugia-powerline/DelugiaPLLight.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaPL-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaPL-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaPL-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaPL.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaPLLight-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/DelugiaPLLight.ttf")[0]
   end
 
   test do
