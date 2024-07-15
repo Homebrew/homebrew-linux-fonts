74446fe6e98f41019068d07bd6d02dc712d07dd2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:20 +0000

    font-league-spartan updated

--- Formula/font-league-spartan.rb
+++ Formula/font-league-spartan.rb
@@ -7,15 +7,15 @@ class FontLeagueSpartan < Formula
   sha256 "141a96e215554871504dca16be159901bbb0d56f3a84224f39fe472c7ab4ef47"
 
   def install
-    (share/"fonts").install Dir.glob("./**/LeagueSpartan-2.220/variable/TTF/LeagueSpartan-VF.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueSpartan-2.220/static/OTF/LeagueSpartan-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueSpartan-2.220/static/OTF/LeagueSpartan-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueSpartan-2.220/static/OTF/LeagueSpartan-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueSpartan-2.220/static/OTF/LeagueSpartan-SemiBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueSpartan-2.220/static/OTF/LeagueSpartan-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueSpartan-2.220/static/OTF/LeagueSpartan-ExtraBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueSpartan-2.220/static/OTF/LeagueSpartan-Black.otf")[0]
-    (share/"fonts").install Dir.glob("./**/LeagueSpartan-2.220/static/OTF/LeagueSpartan-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueSpartan-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueSpartan-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueSpartan-ExtraBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueSpartan-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueSpartan-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueSpartan-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueSpartan-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/static/OTF/LeagueSpartan-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/variable/TTF/LeagueSpartan-VF.ttf")[0]
   end
 
   test do
