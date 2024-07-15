895e7376b1041a4071d3560ca6ac0521822c4491
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:32 +0000

    font-playwrite-ar updated

--- Formula/font-playwrite-ar.rb
+++ Formula/font-playwrite-ar.rb
@@ -1,10 +1,11 @@
 class FontPlaywriteAr < Formula
   desc "Playwrite ar font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritear/PlaywriteAR%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+AR"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritear/PlaywriteAR%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlaywriteAR[wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlaywriteAR?wght?.ttf")[0]
   end
 
   test do
