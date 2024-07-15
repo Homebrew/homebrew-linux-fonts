fca55c1a102869f1ab9eb628e70d91342b625284
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-15 00:22:15 +0000

    font-imbue updated

--- Formula/font-imbue.rb
+++ Formula/font-imbue.rb
@@ -5,7 +5,7 @@ class FontImbue < Formula
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Imbue[opsz,wght].ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Imbue?opsz,wght?.ttf")[0]
   end
 
   test do
