38cac322f2ce411570702b919f24fd539e4ebe25
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-19 00:20:48 +0000

    font-plangothic updated

--- Formula/font-plangothic.rb
+++ Formula/font-plangothic.rb
@@ -1,13 +1,13 @@
 class FontPlangothic < Formula
   desc "Plangothic font"
   homepage "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic-Project"
-  url "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic-Project/archive/refs/tags/V1.8.5760.tar.gz"
-  version "1.8.5760"
-  sha256 "aa5f27a28a275743ee7a667c7f6cee14702c9ed5439ffc0f04c9d7da0384c34c"
+  url "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic-Project/archive/refs/tags/V1.8.5764.tar.gz"
+  version "1.8.5764"
+  sha256 "5d4e3f113e1aaf9f95f2733e3ebd677a5853d17426f649058bd637487af27a85"
 
   def install
-    (share/"fonts").install Dir.glob("./**/PlangothicP1-Regular (allideo).ttf")[0]
-    (share/"fonts").install Dir.glob("./**/PlangothicP2-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/PlangothicP1-Regular (allideo).otf")[0]
+    (share/"fonts").install Dir.glob("./**/PlangothicP2-Regular.otf")[0]
   end
 
   test do
