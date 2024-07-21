3f68ad71f6902ad94bddc1b47725df9b005fcaec
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-21 00:23:29 +0000

    font-lilex updated

--- Formula/font-lilex.rb
+++ Formula/font-lilex.rb
@@ -1,18 +1,17 @@
 class FontLilex < Formula
   desc "Lilex font"
   homepage "https://github.com/mishamyrt/Lilex"
-  url "https://github.com/mishamyrt/Lilex/releases/download/2.510/Lilex.zip"
-  version "2.510"
-  sha256 "2fd9cf5d12ca390bba8c487f189f78e5900e7f6f7d6a702d6ed5074c26260537"
+  url "https://github.com/mishamyrt/Lilex/releases/download/2.520/Lilex.zip"
+  version "2.520"
+  sha256 "c19cac5d41d763f6edd92fed1799e2522ee994452ab6ce979bbcedcef187c46d"
 
   def install
     (share/"fonts").install Dir.glob("./**/Lilex-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Lilex-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Lilex-ExtraThick.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Lilex-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Lilex-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Lilex-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/Lilex-VF.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex?wght?.ttf")[0]
   end
 
   test do
