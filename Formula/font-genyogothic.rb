6d7431c98d14440a9db943764ce805e9f52886d2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-26 00:20:51 +0000

    font-genyogothic updated

--- Formula/font-genyogothic.rb
+++ Formula/font-genyogothic.rb
@@ -1,18 +1,18 @@
 class FontGenyogothic < Formula
   desc "Genyogothic font"
   homepage "https://github.com/ButTaiwan/genyog-font"
-  url "https://github.com/ButTaiwan/genyog-font/releases/download/v1.501/GenYoGothic.zip"
-  version "1.501"
-  sha256 "d90705ecb3ab25a9b98972fcb244323b6a0c76c4320bb7e0ab1b82815f727086"
+  url "https://github.com/ButTaiwan/genyog-font/releases/download/v2.000/GenYoGothic2-ttc.zip"
+  version "2.000"
+  sha256 "e570102fd1c9abb67897b215e6b7f2a0666610f9bb1e2af5fbb3a2db8f67fa93"
 
   def install
-    (share/"fonts").install Dir.glob("./**/GenYoGothic-B.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenYoGothic-EL.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenYoGothic-H.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenYoGothic-L.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenYoGothic-M.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenYoGothic-N.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenYoGothic-R.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenYoGothic2-B.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenYoGothic2-EL.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenYoGothic2-H.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenYoGothic2-L.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenYoGothic2-M.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenYoGothic2-N.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenYoGothic2-R.ttc")[0]
   end
 
   test do
