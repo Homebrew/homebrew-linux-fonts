d6933d909c98c5a5100d1a51ff5d78e74a57f4e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-27 00:20:37 +0000

    font-gensenrounded updated

--- Formula/font-gensenrounded.rb
+++ Formula/font-gensenrounded.rb
@@ -1,17 +1,17 @@
 class FontGensenrounded < Formula
   desc "Gensenrounded font"
   homepage "https://github.com/ButTaiwan/gensen-font"
-  url "https://github.com/ButTaiwan/gensen-font/releases/download/v1.501/GenSenRounded.zip"
-  version "1.501"
-  sha256 "13fffdee6e3b3358164f136ecee1d8f9fbe3a3346a2b2e61767556b472fc962f"
+  url "https://github.com/ButTaiwan/gensen-font/releases/download/v2.000/GenSenRounded2-ttc.zip"
+  version "2.000"
+  sha256 "82c439a688a7083c641164e09515c11dbe3ca23f8d9d4a8874f6bb06a8d028c0"
 
   def install
-    (share/"fonts").install Dir.glob("./**/GenSenRounded-B.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenSenRounded-EL.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenSenRounded-H.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenSenRounded-L.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenSenRounded-M.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenSenRounded-R.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenSenRounded2-B.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenSenRounded2-EL.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenSenRounded2-H.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenSenRounded2-L.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenSenRounded2-M.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenSenRounded2-R.ttc")[0]
   end
 
   test do
