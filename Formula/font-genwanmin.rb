43da8dbe2019d3ba446d274ced395adc523293cf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-27 00:20:37 +0000

    font-genwanmin updated

--- Formula/font-genwanmin.rb
+++ Formula/font-genwanmin.rb
@@ -1,16 +1,16 @@
 class FontGenwanmin < Formula
   desc "Genwanmin font"
   homepage "https://github.com/ButTaiwan/genwan-font"
-  url "https://github.com/ButTaiwan/genwan-font/releases/download/v1.501/GenWanMin.zip"
-  version "1.501"
-  sha256 "8eb2b17f7c4d3fc1bd16a51f2e70e55a51f1ea39afc967640fa35af1102da1d6"
+  url "https://github.com/ButTaiwan/genwan-font/releases/download/v2.000/GenWanMin2-ttc.zip"
+  version "2.000"
+  sha256 "15ef26d0184849a450afba405704c09a60591ddbdebee2fa8e2496a96fc351f7"
 
   def install
-    (share/"fonts").install Dir.glob("./**/GenWanMin-EL.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenWanMin-L.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenWanMin-M.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenWanMin-R.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/GenWanMin-SB.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenWanMin2-EL.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenWanMin2-L.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenWanMin2-M.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenWanMin2-R.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/GenWanMin2-SB.ttc")[0]
   end
 
   test do
