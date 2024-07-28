c00bfdfb973fb9dcf2330534862947629c9ca9bf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-28 00:23:49 +0000

    font-cozette updated

--- Formula/font-cozette.rb
+++ Formula/font-cozette.rb
@@ -1,9 +1,9 @@
 class FontCozette < Formula
   desc "Cozette font"
   homepage "https://github.com/slavfox/Cozette"
-  url "https://github.com/slavfox/Cozette/releases/download/v.1.25.0/CozetteVector.dfont"
-  version "1.25.0"
-  sha256 "bd9e08d76b66968a3a2236503165f7ed16f9e65bcf77f24787c6f18a751c2d24"
+  url "https://github.com/slavfox/Cozette/releases/download/v.1.25.1/CozetteVector.dfont"
+  version "1.25.1"
+  sha256 "bb7c7b3d28ebfd5b87fdb09e929be72f2e905342cc2bc1aecd84b2c4b36d9e12"
 
   def install
     (share/"fonts").install Dir.glob("./**/CozetteVector.dfont")[0]
