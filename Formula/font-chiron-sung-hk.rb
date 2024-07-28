57b937be1c7f62f574f4d8f2cc1700177ed8c491
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-28 00:23:49 +0000

    font-chiron-sung-hk updated

--- Formula/font-chiron-sung-hk.rb
+++ Formula/font-chiron-sung-hk.rb
@@ -1,9 +1,9 @@
 class FontChironSungHk < Formula
   desc "Chiron sung hk font"
   homepage "https://github.com/chiron-fonts/chiron-sung-hk"
-  url "https://github.com/chiron-fonts/chiron-sung-hk/archive/refs/tags/v1.012.tar.gz"
-  version "1.012"
-  sha256 "554404b43f2eab11182220964d959dfaa609638cec8fb950604cc6ed80b5a373"
+  url "https://github.com/chiron-fonts/chiron-sung-hk/archive/refs/tags/v1.013.tar.gz"
+  version "1.013"
+  sha256 "0da92f806a130338de304d17a4bcd0614e394ef22adf2716373264b2d0401b50"
 
   def install
     (share/"fonts").install Dir.glob("./**/VAR/ChironSungHKItVF.otf")[0]
