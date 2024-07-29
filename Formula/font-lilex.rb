7d98f9bcf472dd65f94f8d8e760aae4f1f565ad5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-29 00:22:27 +0000

    font-lilex updated

--- Formula/font-lilex.rb
+++ Formula/font-lilex.rb
@@ -1,9 +1,9 @@
 class FontLilex < Formula
   desc "Lilex font"
   homepage "https://github.com/mishamyrt/Lilex"
-  url "https://github.com/mishamyrt/Lilex/releases/download/2.520/Lilex.zip"
-  version "2.520"
-  sha256 "c19cac5d41d763f6edd92fed1799e2522ee994452ab6ce979bbcedcef187c46d"
+  url "https://github.com/mishamyrt/Lilex/releases/download/2.530/Lilex.zip"
+  version "2.530"
+  sha256 "b019fc0da5e3ece5c735aa040218d3b8c9945546d2d333598b587b1237a66c4a"
 
   def install
     (share/"fonts").install Dir.glob("./**/Lilex-Bold.ttf")[0]
