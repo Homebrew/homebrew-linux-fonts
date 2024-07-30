bb1e6df427c5687f52488d8be01587c915a52c65
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-30 00:21:26 +0000

    font-national-park updated

--- Formula/font-national-park.rb
+++ Formula/font-national-park.rb
@@ -1,16 +1,19 @@
 class FontNationalPark < Formula
   desc "National park font"
   homepage "https://nationalparktypeface.com/"
-  url "https://files.cargocollective.com/c206464/NationalPark.zip",
+  url "https://files.cargocollective.com/c1415345/NationalPark-otf.zip",
        verified: "cargocollective.com/"
-  version "1.000"
+  version "1.005"
   sha256 :no_check
 
   def install
-    (share/"fonts").install Dir.glob("./**/NationalPark-Heavy.otf")[0]
-    (share/"fonts").install Dir.glob("./**/NationalPark-Outline.otf")[0]
+    (share/"fonts").install Dir.glob("./**/NationalPark-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/NationalPark-ExtraBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/NationalPark-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/NationalPark-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/NationalPark-Medium.otf")[0]
     (share/"fonts").install Dir.glob("./**/NationalPark-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/NationalPark-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/NationalPark-SemiBold.otf")[0]
   end
 
   test do
