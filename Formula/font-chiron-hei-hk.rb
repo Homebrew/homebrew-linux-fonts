adb896c87dd80e35565f9c598ca926c19e6a1ed6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-30 00:21:26 +0000

    font-chiron-hei-hk updated

--- Formula/font-chiron-hei-hk.rb
+++ Formula/font-chiron-hei-hk.rb
@@ -1,9 +1,9 @@
 class FontChironHeiHk < Formula
   desc "Chiron hei hk font"
   homepage "https://github.com/chiron-fonts/chiron-hei-hk"
-  url "https://github.com/chiron-fonts/chiron-hei-hk/archive/refs/tags/v2.513.tar.gz"
-  version "2.513"
-  sha256 "15fe06ac6d420ee150c038afb4bd45ee5e3629da12a72dd22ae6e95d3d8c4651"
+  url "https://github.com/chiron-fonts/chiron-hei-hk/archive/refs/tags/v2.514.tar.gz"
+  version "2.514"
+  sha256 "bec3536907115e6b1e97fba94e6d44aa1106a62329e048a5f47f5ac3e9d4811e"
 
   def install
     (share/"fonts").install Dir.glob("./**/VAR/ChironHeiHKItVF.otf")[0]
