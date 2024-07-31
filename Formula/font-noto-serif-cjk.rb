065391bc2ec544b38d1e48b8dafc3de8f0318146
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-31 00:17:54 +0000

    font-noto-serif-cjk updated

--- Formula/font-noto-serif-cjk.rb
+++ Formula/font-noto-serif-cjk.rb
@@ -1,9 +1,9 @@
 class FontNotoSerifCjk < Formula
   desc "Noto serif cjk font"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Serif"
-  url "https://github.com/notofonts/noto-cjk/releases/download/Serif2.002/01_NotoSerifCJK.ttc.zip"
-  version "2.002"
-  sha256 "e78b23ea9f803d8376eee66d330a556e1133b713ff97014a75991d5723324f85"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Serif2.003/01_NotoSerifCJK.ttc.zip"
+  version "2.003"
+  sha256 "5009285197f068fe9a58f2cf8d6d312f4bf185887305751aae2608db4e02e48f"
 
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifCJK.ttc")[0]
