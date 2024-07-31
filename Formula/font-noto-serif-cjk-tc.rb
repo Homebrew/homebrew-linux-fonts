5de5c95319a1bbd05e01e567ddd71556aa48d55d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-07-31 00:17:54 +0000

    font-noto-serif-cjk-tc updated

--- Formula/font-noto-serif-cjk-tc.rb
+++ Formula/font-noto-serif-cjk-tc.rb
@@ -1,9 +1,9 @@
 class FontNotoSerifCjkTc < Formula
   desc "Noto serif cjk tc font"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Serif"
-  url "https://github.com/notofonts/noto-cjk/releases/download/Serif2.002/10_NotoSerifCJKtc.zip"
-  version "2.002"
-  sha256 "bd14b808431c5058c69f999c3c6d898b9f37d96962ec26f7212fb34c2d49ea5c"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Serif2.003/10_NotoSerifCJKtc.zip"
+  version "2.003"
+  sha256 "b4aa07b217532c5859b3674d53588671e7e4f340054fc30e9bf417ee3b1aa4d4"
 
   def install
     (share/"fonts").install Dir.glob("./**/TraditionalChinese/NotoSerifCJKtc-Black.otf")[0]
