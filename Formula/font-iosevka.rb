a1cddb2d6dab0474c5ed265778abe0d6e2a4c186
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-05 00:22:32 +0000

    font-iosevka updated

--- Formula/font-iosevka.rb
+++ Formula/font-iosevka.rb
@@ -1,20 +1,12 @@
 class FontIosevka < Formula
   desc "Iosevka font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v31.1.0/PkgTTC-Iosevka-31.1.0.zip"
+  url "https://github.com/be5invis/Iosevka/releases/download/v31.1.0/SuperTTC-Iosevka-31.1.0.zip"
   version "31.1.0"
-  sha256 "dc8a6521d01c258e3520464701d05aade52d84e3f8352d8b1128f945f9c948b0"
+  sha256 "781116f7a77de48e286b18292b2a746fda10aef5a7de6428a96fc7681c6ba0c3"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Iosevka-Bold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/Iosevka-ExtraBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/Iosevka-ExtraLight.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/Iosevka-Heavy.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/Iosevka-Light.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/Iosevka-Medium.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/Iosevka-Regular.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/Iosevka-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/Iosevka-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/Iosevka.ttc")[0]
   end
 
   test do
