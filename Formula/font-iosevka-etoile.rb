d61afb086be4dd0c2d910d9299f4f18f14c5426c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-05 00:22:31 +0000

    font-iosevka-etoile updated

--- Formula/font-iosevka-etoile.rb
+++ Formula/font-iosevka-etoile.rb
@@ -1,20 +1,12 @@
 class FontIosevkaEtoile < Formula
   desc "Iosevka etoile font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v31.1.0/PkgTTC-IosevkaEtoile-31.1.0.zip"
+  url "https://github.com/be5invis/Iosevka/releases/download/v31.1.0/SuperTTC-IosevkaEtoile-31.1.0.zip"
   version "31.1.0"
-  sha256 "f823426e323d922602e57e03449178d91786d3eca4b475d3864e288c2af1b62e"
+  sha256 "893ddf25070e016f7e61177bcce8218dbb4f184e32f7c93eba95c8adc08bdc34"
 
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Bold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-ExtraBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-ExtraLight.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Heavy.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Light.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Medium.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Regular.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaEtoile-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaEtoile.ttc")[0]
   end
 
   test do
