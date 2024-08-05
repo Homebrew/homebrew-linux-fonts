d0eb246550799a44d6ccb6f1816d49c55a9995f9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-05 00:22:31 +0000

    font-iosevka-ss14 updated

--- Formula/font-iosevka-ss14.rb
+++ Formula/font-iosevka-ss14.rb
@@ -1,20 +1,12 @@
 class FontIosevkaSs14 < Formula
   desc "Iosevka ss14 font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v31.1.0/PkgTTC-IosevkaSS14-31.1.0.zip"
+  url "https://github.com/be5invis/Iosevka/releases/download/v31.1.0/SuperTTC-IosevkaSS14-31.1.0.zip"
   version "31.1.0"
-  sha256 "a37a9c88b511a14af944254730650f791b77ba04c1a814f2d777ee42ec154199"
+  sha256 "2785b8e8d6b64c3309c094cee5b9dac9a2190d7435edf91f4884dc14829af7c2"
 
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaSS14-Bold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS14-ExtraBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS14-ExtraLight.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS14-Heavy.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS14-Light.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS14-Medium.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS14-Regular.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS14-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaSS14-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaSS14.ttc")[0]
   end
 
   test do
