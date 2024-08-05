6567941e5398294ab0e95d211a2524ac3c2a74b7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-05 00:22:31 +0000

    font-iosevka-curly updated

--- Formula/font-iosevka-curly.rb
+++ Formula/font-iosevka-curly.rb
@@ -1,20 +1,12 @@
 class FontIosevkaCurly < Formula
   desc "Iosevka curly font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v31.1.0/PkgTTC-IosevkaCurly-31.1.0.zip"
+  url "https://github.com/be5invis/Iosevka/releases/download/v31.1.0/SuperTTC-IosevkaCurly-31.1.0.zip"
   version "31.1.0"
-  sha256 "46d2aad84a105670c21cf6ae9b0b0efadda38d44e683d72c4ead7167cf1d8de4"
+  sha256 "c28513e30aa4687d5d3eff8126f3294c43645618a0fb1a8a433bcce6af3fc4da"
 
   def install
-    (share/"fonts").install Dir.glob("./**/IosevkaCurly-Bold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaCurly-ExtraBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaCurly-ExtraLight.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaCurly-Heavy.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaCurly-Light.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaCurly-Medium.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaCurly-Regular.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaCurly-SemiBold.ttc")[0]
-    (share/"fonts").install Dir.glob("./**/IosevkaCurly-Thin.ttc")[0]
+    (share/"fonts").install Dir.glob("./**/IosevkaCurly.ttc")[0]
   end
 
   test do
