ad8a4269f610e06195dad8e07911c60a828d3ac4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-11 00:24:01 +0000

    font-monocraft updated

--- Formula/font-monocraft.rb
+++ Formula/font-monocraft.rb
@@ -1,12 +1,12 @@
 class FontMonocraft < Formula
   desc "Monocraft font"
   homepage "https://github.com/IdreesInc/Monocraft"
-  url "https://github.com/IdreesInc/Monocraft/releases/download/v3.0/Monocraft.ttf"
-  version "3.0"
-  sha256 "b61ee3256f449e96140f54515819540840ef0cade82711eba359a5864c779076"
+  url "https://github.com/IdreesInc/Monocraft/releases/download/v4.0/Monocraft.ttc"
+  version "4.0"
+  sha256 "481ce5fd7d8f40eab5718e1d96a3bcf410f00ab3fefcb63067d5f6e29d8b0c2b"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Monocraft.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Monocraft.ttc")[0]
   end
 
   test do
