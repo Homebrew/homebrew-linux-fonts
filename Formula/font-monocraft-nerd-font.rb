f1a1359f3680dd8668083d341788cd610882b0b0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-11 00:24:01 +0000

    font-monocraft-nerd-font updated

--- Formula/font-monocraft-nerd-font.rb
+++ Formula/font-monocraft-nerd-font.rb
@@ -1,12 +1,12 @@
 class FontMonocraftNerdFont < Formula
   desc "Monocraft with nerd glyphs font"
   homepage "https://github.com/IdreesInc/Monocraft"
-  url "https://github.com/IdreesInc/Monocraft/releases/download/v3.0/Monocraft-nerd-fonts-patched.ttf"
-  version "3.0"
-  sha256 "431329f14c1c4635b248d1d6a0d797dac5fdb5e678fad0858fe0c6e356b3b17c"
+  url "https://github.com/IdreesInc/Monocraft/releases/download/v4.0/Monocraft-nerd-fonts-patched.ttc"
+  version "4.0"
+  sha256 "95801bf21826bf8572af3787af82e77ee48df4bfb87e90c4317fcffbe7eaf037"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Monocraft-nerd-fonts-patched.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Monocraft-nerd-fonts-patched.ttc")[0]
   end
 
   test do
