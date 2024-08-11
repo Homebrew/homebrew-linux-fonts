7f59c770c6cc2618c3d18aa34b5ac8205b72deb4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-11 00:24:00 +0000

    font-iosevka-curly-slab updated

--- Formula/font-iosevka-curly-slab.rb
+++ Formula/font-iosevka-curly-slab.rb
@@ -1,9 +1,9 @@
 class FontIosevkaCurlySlab < Formula
   desc "Iosevka curly slab font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v31.1.0/SuperTTC-IosevkaCurlySlab-31.1.0.zip"
-  version "31.1.0"
-  sha256 "0cbc4cc7ade277c277f361999dd4602d33cc2f990f417b6d7719ef97c65b8dc3"
+  url "https://github.com/be5invis/Iosevka/releases/download/v31.2.0/SuperTTC-IosevkaCurlySlab-31.2.0.zip"
+  version "31.2.0"
+  sha256 "ed00b50ecac1ac401c0bdd845e9f21995e69614da116b4b394233d223f878a4b"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaCurlySlab.ttc")[0]
