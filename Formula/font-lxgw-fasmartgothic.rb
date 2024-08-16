748401e8b815ab8912b798b69e7e0cde11cf2056
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-16 00:21:33 +0000

    font-lxgw-fasmartgothic updated

--- Formula/font-lxgw-fasmartgothic.rb
+++ Formula/font-lxgw-fasmartgothic.rb
@@ -1,9 +1,9 @@
 class FontLxgwFasmartgothic < Formula
   desc "Lxgw fasmartgothic font"
   homepage "https://github.com/lxgw/LxgwNeoXiHei"
-  url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v1.124/LXGWFasmartGothic.ttf"
-  version "1.124"
-  sha256 "fdd76652cda628b3b7fe9e6dacde8a5c45ff0808712ebc512bc9287a56a22cc1"
+  url "https://github.com/lxgw/LxgwFasmartGothic/releases/download/v1.200/LXGWFasmartGothic.ttf"
+  version "1.200"
+  sha256 "46c06f85c75aa55b4450fa02c37a35f0f8a4efe8058ffcb6e48b9e2eccd764eb"
 
   def install
     (share/"fonts").install Dir.glob("./**/LXGWFasmartGothic.ttf")[0]
