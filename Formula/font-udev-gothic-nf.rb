a96ab6f1456f16da0f6021701fd6d130abc9c624
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-18 00:23:31 +0000

    font-udev-gothic-nf updated

--- Formula/font-udev-gothic-nf.rb
+++ Formula/font-udev-gothic-nf.rb
@@ -1,9 +1,9 @@
 class FontUdevGothicNf < Formula
   desc "Udev gothic nf font"
   homepage "https://github.com/yuru7/udev-gothic"
-  url "https://github.com/yuru7/udev-gothic/releases/download/v1.3.1/UDEVGothic_NF_v1.3.1.zip"
-  version "1.3.1"
-  sha256 "84004a3038bdf528286a113b4db076d8412bb4ca6771d02a240318473f9b9fce"
+  url "https://github.com/yuru7/udev-gothic/releases/download/v2.0.0/UDEVGothic_NF_v2.0.0.zip"
+  version "2.0.0"
+  sha256 "551f1e89861f71cdf0c714cea42e503dbec5a6064f07af1711f1731003433039"
 
   def install
     (share/"fonts").install Dir.glob("./**/UDEVGothic35NF-Bold.ttf")[0]
