6e096d3983063922a48b448131edd0827af9cded
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-20 00:21:43 +0000

    font-sarasa-gothic updated

--- Formula/font-sarasa-gothic.rb
+++ Formula/font-sarasa-gothic.rb
@@ -1,9 +1,9 @@
 class FontSarasaGothic < Formula
   desc "Sarasa gothic font"
   homepage "https://github.com/be5invis/Sarasa-Gothic"
-  url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v1.0.18/Sarasa-TTC-1.0.18.7z"
-  version "1.0.18"
-  sha256 "678cd391c20365d1e60ab0cc2e5c93200c9aaec3e341e17c3db9b56915f5abe2"
+  url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v1.0.19/Sarasa-TTC-1.0.19.7z"
+  version "1.0.19"
+  sha256 "663c32a7f86fa6dc1078ae2a6341386a8823e217e25d5d3ce414b96e480a486d"
 
   def install
     (share/"fonts").install Dir.glob("./**/Sarasa-Bold.ttc")[0]
