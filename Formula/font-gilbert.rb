d0799a5c656ff6ffc1001740b2f66620336ea843
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-20 00:21:43 +0000

    font-gilbert updated

--- Formula/font-gilbert.rb
+++ Formula/font-gilbert.rb
@@ -1,8 +1,7 @@
 class FontGilbert < Formula
   desc "Gilbert font"
-  homepage "https://typewithpride.com/"
-  url "https://github.com/Fontself/TypeWithPride/releases/download/1.005/Gilbert_1.005_alpha.zip",
-       verified: "github.com/Fontself/TypeWithPride/"
+  homepage "https://github.com/Fontself/TypeWithPride"
+  url "https://github.com/Fontself/TypeWithPride/releases/download/1.005/Gilbert_1.005_alpha.zip"
   version "1.005,alpha"
   sha256 "d3ac3075efe00bf4302264b2e626f548e3549740d359a43991605b2a180d8cbe"
 
