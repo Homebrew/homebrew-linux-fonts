9663744958a87d7e02caf011441720531aa05922
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-21 00:21:30 +0000

    font-african-sans updated

--- Formula/font-african-sans.rb
+++ Formula/font-african-sans.rb
@@ -3,7 +3,6 @@ class FontAfricanSans < Formula
   homepage "https://www.languagegeek.com/font/fontdownload.html"
   url "https://www.languagegeek.com/font/AfricanSans.zip"
   version "9.380"
-  sha256 :no_check
 
   def install
     (share/"fonts").install Dir.glob("./**/African Sans BOLD 938.ttf")[0]
