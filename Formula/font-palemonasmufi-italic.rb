5407dbf35f429a752b5b6b4e7c6db9cc12e27523
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-21 00:21:31 +0000

    font-palemonasmufi-italic updated

--- Formula/font-palemonasmufi-italic.rb
+++ Formula/font-palemonasmufi-italic.rb
@@ -3,7 +3,6 @@ class FontPalemonasmufiItalic < Formula
   homepage "https://mufi.info/q.php?p=mufi/fonts/fontfamily/6"
   url "https://mufi.info/q.php?o=mufifontfiledownload&name=PalemMUFI-it.ttf"
   version "2.0"
-  sha256 :no_check
 
   def install
     (share/"fonts").install Dir.glob("./**/PalemMUFI-it.ttf")[0]
