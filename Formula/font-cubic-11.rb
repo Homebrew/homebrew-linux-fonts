625cda7f3e1d834e62ede22e1ea86df7cf12bee2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-23 00:21:35 +0000

    font-cubic-11 updated

--- Formula/font-cubic-11.rb
+++ Formula/font-cubic-11.rb
@@ -1,12 +1,12 @@
 class FontCubic11 < Formula
   desc "Cubic 11 font"
   homepage "https://github.com/ACh-K/Cubic-11"
-  url "https://github.com/ACh-K/Cubic-11/archive/refs/tags/v1.300.tar.gz"
-  version "1.300"
-  sha256 "1db4f204bbac140a386216ec7762eb616eb2cb67f8dc56d8de3f14de0d28b21a"
+  url "https://github.com/ACh-K/Cubic-11/archive/refs/tags/v1.400.tar.gz"
+  version "1.400"
+  sha256 "662749dc12204c76832883f7edf8523aeeda0a6f59b0b8ceb91d5c67cfa903a5"
 
   def install
-    (share/"fonts").install Dir.glob("./**/fonts/ttf/Cubic_11_1.300_R.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/ttf/Cubic_11.ttf")[0]
   end
 
   test do
