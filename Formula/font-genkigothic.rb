aa9b56d769ec48d9324e8a42e0024a8267a8b784
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-24 00:20:47 +0000

    font-genkigothic updated

--- Formula/font-genkigothic.rb
+++ Formula/font-genkigothic.rb
@@ -1,9 +1,9 @@
 class FontGenkigothic < Formula
   desc "Genkigothic font"
   homepage "https://github.com/ButTaiwan/genyog-font"
-  url "https://github.com/ButTaiwan/genyog-font/releases/download/v2.000/GenKiGothic2-ttc.zip"
-  version "2.000"
-  sha256 "e2aab3e38f6a5e0b6b40f0d8fff93c1f48a250f51e913f738d1247edda96974b"
+  url "https://github.com/ButTaiwan/genyog-font/releases/download/v2.100/GenKiGothic2-ttc.zip"
+  version "2.100"
+  sha256 "0104ba39ff119ed691e10b0da28684226928e1862058fa6096ac87e125fe31f0"
 
   def install
     (share/"fonts").install Dir.glob("./**/GenKiGothic2-B.ttc")[0]
