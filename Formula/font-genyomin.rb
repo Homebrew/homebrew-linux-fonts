fe243b479bca1f6aaafd4ee43671a9d4ff402278
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-24 00:20:47 +0000

    font-genyomin updated

--- Formula/font-genyomin.rb
+++ Formula/font-genyomin.rb
@@ -1,9 +1,9 @@
 class FontGenyomin < Formula
   desc "Genyomin font"
   homepage "https://github.com/ButTaiwan/genyo-font"
-  url "https://github.com/ButTaiwan/genyo-font/releases/download/v2.000/GenYoMin2-ttc.zip"
-  version "2.000"
-  sha256 "555d78284473dc73e22e45f9f727ef8cd794b096b41eada80afd508f5d406bfb"
+  url "https://github.com/ButTaiwan/genyo-font/releases/download/v2.100/GenYoMin2-ttc.zip"
+  version "2.100"
+  sha256 "292b3af4232d070483e2fa6f09b56a328a4d47da04b4e9d354a6d6d15f2c1f73"
 
   def install
     (share/"fonts").install Dir.glob("./**/GenYoMin2-B.ttc")[0]
