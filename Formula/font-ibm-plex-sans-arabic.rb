bb3799ce213a366e4892dc0443615f6cfac74078
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-03 00:22:18 +0000

    font-ibm-plex-sans-arabic updated

--- Formula/font-ibm-plex-sans-arabic.rb
+++ Formula/font-ibm-plex-sans-arabic.rb
@@ -1,19 +1,19 @@
 class FontIbmPlexSansArabic < Formula
   desc "Ibm plex sans arabic font"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+Arabic"
-  head "https://github.com/google/fonts.git",
-       verified:  "github.com/google/fonts",
-       branch:    "main",
-       only_path: "ofl/ibmplexsansarabic"
+  homepage "https://github.com/IBM/plex"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-arabic%401.0.0/ibm-plex-sans-arabic.zip"
+  version "1.0.0"
+  sha256 "759e7a77c4dbae38371e5d3b1016e2ee5f75a164e708a960bb69d8949f0c3593"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansarabic/./**/IBMPlexSansArabic-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansarabic/./**/IBMPlexSansArabic-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansarabic/./**/IBMPlexSansArabic-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansarabic/./**/IBMPlexSansArabic-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansarabic/./**/IBMPlexSansArabic-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansarabic/./**/IBMPlexSansArabic-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansarabic/./**/IBMPlexSansArabic-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansArabic-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansArabic-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansArabic-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansArabic-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansArabic-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansArabic-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansArabic-Text.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansArabic-Thin.otf")[0]
   end
 
   test do
