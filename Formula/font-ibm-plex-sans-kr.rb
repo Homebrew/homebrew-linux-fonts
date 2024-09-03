99331ccd2023c62eacc63fa0b5e5c8a0b924cd65
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-03 00:22:18 +0000

    font-ibm-plex-sans-kr updated

--- Formula/font-ibm-plex-sans-kr.rb
+++ Formula/font-ibm-plex-sans-kr.rb
@@ -1,19 +1,19 @@
 class FontIbmPlexSansKr < Formula
   desc "Ibm plex sans kr font"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+KR"
-  head "https://github.com/google/fonts.git",
-       verified:  "github.com/google/fonts",
-       branch:    "main",
-       only_path: "ofl/ibmplexsanskr"
+  homepage "https://github.com/IBM/plex"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-kr%401.0.0/ibm-plex-sans-kr.zip"
+  version "1.0.0"
+  sha256 "b977effaabce2328f888db8590f0989c162cb9a9aad9d70949f3910552e6652b"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanskr/./**/IBMPlexSansKR-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanskr/./**/IBMPlexSansKR-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanskr/./**/IBMPlexSansKR-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanskr/./**/IBMPlexSansKR-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanskr/./**/IBMPlexSansKR-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanskr/./**/IBMPlexSansKR-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanskr/./**/IBMPlexSansKR-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansKR-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansKR-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansKR-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansKR-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansKR-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansKR-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansKR-Text.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansKR-Thin.otf")[0]
   end
 
   test do
