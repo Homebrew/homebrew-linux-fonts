4879cf4df4daa810f953680621247326a35a63d3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-03 00:22:18 +0000

    font-ibm-plex-sans-thai updated

--- Formula/font-ibm-plex-sans-thai.rb
+++ Formula/font-ibm-plex-sans-thai.rb
@@ -1,19 +1,19 @@
 class FontIbmPlexSansThai < Formula
   desc "Ibm plex sans thai font"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+Thai"
-  head "https://github.com/google/fonts.git",
-       verified:  "github.com/google/fonts",
-       branch:    "main",
-       only_path: "ofl/ibmplexsansthai"
+  homepage "https://github.com/IBM/plex"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-thai%401.0.0/ibm-plex-sans-thai.zip"
+  version "1.0.0"
+  sha256 "1a39b37654916260791073046aa811450fd9bea63c2acb4413546636939c43a1"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthai/./**/IBMPlexSansThai-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThai-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThai-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThai-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThai-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThai-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThai-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThai-Text.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThai-Thin.otf")[0]
   end
 
   test do
