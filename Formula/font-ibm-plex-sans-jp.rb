c7ee1adca4303ed656bf493ac4f50336a556d2d1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-03 00:22:18 +0000

    font-ibm-plex-sans-jp updated

--- Formula/font-ibm-plex-sans-jp.rb
+++ Formula/font-ibm-plex-sans-jp.rb
@@ -1,19 +1,19 @@
 class FontIbmPlexSansJp < Formula
   desc "Ibm plex sans jp font"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+JP"
-  head "https://github.com/google/fonts.git",
-       verified:  "github.com/google/fonts",
-       branch:    "main",
-       only_path: "ofl/ibmplexsansjp"
+  homepage "https://github.com/IBM/plex"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-jp%401.0.0/ibm-plex-sans-jp.zip"
+  version "1.0.0"
+  sha256 "6946948f66b511560786fd0ec7d561de985a1f6d8692b0ed44fbf7d880a994fa"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansjp/./**/IBMPlexSansJP-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/hinted/IBMPlexSansJP-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/hinted/IBMPlexSansJP-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/hinted/IBMPlexSansJP-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/hinted/IBMPlexSansJP-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/hinted/IBMPlexSansJP-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/hinted/IBMPlexSansJP-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/hinted/IBMPlexSansJP-Text.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/hinted/IBMPlexSansJP-Thin.otf")[0]
   end
 
   test do
