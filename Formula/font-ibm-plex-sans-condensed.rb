ed8fbeb046c15d8a4f080e55ccecfa8fb0aff28e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-03 00:22:18 +0000

    font-ibm-plex-sans-condensed updated

--- Formula/font-ibm-plex-sans-condensed.rb
+++ Formula/font-ibm-plex-sans-condensed.rb
@@ -1,26 +1,27 @@
 class FontIbmPlexSansCondensed < Formula
   desc "Ibm plex sans condensed font"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+Condensed"
-  head "https://github.com/google/fonts.git",
-       verified:  "github.com/google/fonts",
-       branch:    "main",
-       only_path: "ofl/ibmplexsanscondensed"
+  homepage "https://github.com/IBM/plex"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-condensed%401.0.0/ibm-plex-sans-condensed.zip"
+  version "1.0.0"
+  sha256 "4c3298d50688e08ebbc9f238a1f1dfe907f9bcdabaf0c2c3aab0fbeaa3e1d38d"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-ExtraLightItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-LightItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-MediumItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-SemiBoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanscondensed/./**/IBMPlexSansCondensed-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-ExtraLightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-SemiBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-Text.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-TextItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansCondensed-ThinItalic.otf")[0]
   end
 
   test do
