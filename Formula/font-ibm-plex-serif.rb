87366c36b43b0250ca56934e41ed3f0881aadeef
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-03 00:22:19 +0000

    font-ibm-plex-serif updated

--- Formula/font-ibm-plex-serif.rb
+++ Formula/font-ibm-plex-serif.rb
@@ -1,26 +1,27 @@
 class FontIbmPlexSerif < Formula
   desc "Ibm plex serif font"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Serif"
-  head "https://github.com/google/fonts.git",
-       verified:  "github.com/google/fonts",
-       branch:    "main",
-       only_path: "ofl/ibmplexserif"
+  homepage "https://github.com/IBM/plex"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-serif%401.0.0/ibm-plex-serif.zip"
+  version "1.0.0"
+  sha256 "bb11f1cfe119d93e4a8d915d2d27e98c57a18f538e6fa88c5e31ea852a41233f"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-ExtraLightItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-LightItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-MediumItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-SemiBoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-Thin.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexserif/./**/IBMPlexSerif-ThinItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-ExtraLightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-LightItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-SemiBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-Text.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-TextItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSerif-ThinItalic.otf")[0]
   end
 
   test do
