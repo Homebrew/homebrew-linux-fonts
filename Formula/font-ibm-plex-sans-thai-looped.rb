3c135f8354360421cb864d0d3980c5432807e089
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-03 00:22:18 +0000

    font-ibm-plex-sans-thai-looped updated

--- Formula/font-ibm-plex-sans-thai-looped.rb
+++ Formula/font-ibm-plex-sans-thai-looped.rb
@@ -1,19 +1,19 @@
 class FontIbmPlexSansThaiLooped < Formula
   desc "Ibm plex sans thai looped font"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+Thai+Looped"
-  head "https://github.com/google/fonts.git",
-       verified:  "github.com/google/fonts",
-       branch:    "main",
-       only_path: "ofl/ibmplexsansthailooped"
+  homepage "https://github.com/IBM/plex"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-thai-looped%401.0.0/ibm-plex-sans-thai-looped.zip"
+  version "1.0.0"
+  sha256 "734b854b745c715123713b283a077d587800c1de0e2d6c84e524862d58a649e6"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthailooped/./**/IBMPlexSansThaiLooped-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthailooped/./**/IBMPlexSansThaiLooped-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthailooped/./**/IBMPlexSansThaiLooped-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthailooped/./**/IBMPlexSansThaiLooped-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthailooped/./**/IBMPlexSansThaiLooped-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthailooped/./**/IBMPlexSansThaiLooped-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsansthailooped/./**/IBMPlexSansThaiLooped-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThaiLooped-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThaiLooped-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThaiLooped-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThaiLooped-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThaiLooped-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThaiLooped-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThaiLooped-Text.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansThaiLooped-Thin.otf")[0]
   end
 
   test do
