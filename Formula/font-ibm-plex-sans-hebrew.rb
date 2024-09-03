ceb001c00de94bc8f201b440d62415512ae98299
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-03 00:22:18 +0000

    font-ibm-plex-sans-hebrew updated

--- Formula/font-ibm-plex-sans-hebrew.rb
+++ Formula/font-ibm-plex-sans-hebrew.rb
@@ -1,19 +1,19 @@
 class FontIbmPlexSansHebrew < Formula
   desc "Ibm plex sans hebrew font"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+Hebrew"
-  head "https://github.com/google/fonts.git",
-       verified:  "github.com/google/fonts",
-       branch:    "main",
-       only_path: "ofl/ibmplexsanshebrew"
+  homepage "https://github.com/IBM/plex"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-hebrew%401.0.0/ibm-plex-sans-hebrew.zip"
+  version "1.0.0"
+  sha256 "06ad64c91a9e485a80b2c16f38e60659f0e75c0cfe3ceae23dd7830adde667c2"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanshebrew/./**/IBMPlexSansHebrew-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanshebrew/./**/IBMPlexSansHebrew-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanshebrew/./**/IBMPlexSansHebrew-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanshebrew/./**/IBMPlexSansHebrew-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanshebrew/./**/IBMPlexSansHebrew-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanshebrew/./**/IBMPlexSansHebrew-SemiBold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/ibmplexsanshebrew/./**/IBMPlexSansHebrew-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansHebrew-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansHebrew-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansHebrew-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansHebrew-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansHebrew-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansHebrew-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansHebrew-Text.otf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansHebrew-Thin.otf")[0]
   end
 
   test do
