e46f8c64ec7349b9a3fb8d6adcd60ff947df6cac
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-04 00:22:46 +0000

    font-sankofa-display updated

--- Formula/font-sankofa-display.rb
+++ Formula/font-sankofa-display.rb
@@ -1,7 +1,8 @@
 class FontSankofaDisplay < Formula
   desc "Sankofa display font"
-  homepage "https://github.com/batsimadz/Sankofa-Display"
-  head "https://github.com/google/fonts/raw/main/ofl/sankofadisplay/SankofaDisplay-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Sankofa+Display"
+  head "https://github.com/google/fonts/raw/main/ofl/sankofadisplay/SankofaDisplay-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/SankofaDisplay-Regular.ttf")[0]
