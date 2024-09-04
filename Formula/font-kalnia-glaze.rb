56519effbc58d68103037dad52337830fdc82706
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-04 00:22:46 +0000

    font-kalnia-glaze updated

--- Formula/font-kalnia-glaze.rb
+++ Formula/font-kalnia-glaze.rb
@@ -1,7 +1,8 @@
 class FontKalniaGlaze < Formula
   desc "Kalnia glaze font"
-  homepage "https://github.com/fridamedrano/Kalnia-Glaze"
-  head "https://github.com/google/fonts/raw/main/ofl/kalniaglaze/KalniaGlaze%5Bwdth%2Cwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Kalnia+Glaze"
+  head "https://github.com/google/fonts/raw/main/ofl/kalniaglaze/KalniaGlaze%5Bwdth%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/KalniaGlaze?wdth,wght?.ttf")[0]
