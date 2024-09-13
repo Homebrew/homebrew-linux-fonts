3de08a079abe18aa1da60abde2be031d52e40c75
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-13 00:23:01 +0000

    font-lxgw-simzhisong updated

--- Formula/font-lxgw-simzhisong.rb
+++ Formula/font-lxgw-simzhisong.rb
@@ -1,9 +1,9 @@
 class FontLxgwSimzhisong < Formula
   desc "Lxgw simzhisong font"
   homepage "https://github.com/lxgw/SimXiZhi"
-  url "https://github.com/lxgw/SimXiZhi/releases/download/v1.004/SimZhiSong.ttf"
-  version "1.004"
-  sha256 "665a295c3bf5c28c7df7f1b57db441ff5ae03d16b434763064970506aff3429b"
+  url "https://github.com/lxgw/SimXiZhi/releases/download/v1.005/SimZhiSong.ttf"
+  version "1.005"
+  sha256 "fd2d6263e5154015162eef4a156637c725882104a36edf5f756bdef7a57febd6"
 
   def install
     (share/"fonts").install Dir.glob("./**/SimZhiSong.ttf")[0]
