7d05c469fb704caf32febba4dcc9c892018d0ae5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-14 00:22:34 +0000

    font-last-resort updated

--- Formula/font-last-resort.rb
+++ Formula/font-last-resort.rb
@@ -1,9 +1,9 @@
 class FontLastResort < Formula
   desc "Last resort font"
   homepage "https://github.com/unicode-org/last-resort-font"
-  url "https://github.com/unicode-org/last-resort-font/releases/download/15.100/LastResort-Regular.ttf"
-  version "15.100"
-  sha256 "2c25999fae0a09f4cf5d1c084729b43e81296f497c59e5891a710a7744c724ff"
+  url "https://github.com/unicode-org/last-resort-font/releases/download/16.000/LastResort-Regular.ttf"
+  version "16.000"
+  sha256 "f7b2a5ddecf37e3c9d5a4388eb291430f3382b304470b48677f3d40f93d29166"
 
   def install
     (share/"fonts").install Dir.glob("./**/LastResort-Regular.ttf")[0]
