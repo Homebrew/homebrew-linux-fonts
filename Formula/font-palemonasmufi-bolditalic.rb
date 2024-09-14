ab24e0b254b396f12a7427ddf7694ab4efe0326a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-14 00:22:34 +0000

    font-palemonasmufi-bolditalic updated

--- Formula/font-palemonasmufi-bolditalic.rb
+++ Formula/font-palemonasmufi-bolditalic.rb
@@ -2,7 +2,7 @@ class FontPalemonasmufiBolditalic < Formula
   desc "Palemonasmufi bold italic font"
   homepage "https://mufi.info/q.php?p=mufi/fonts/fontfamily/6"
   url "https://mufi.info/q.php?o=mufifontfiledownload&name=PalemMUFI-bi.ttf"
-  version "2.0"
+  version "2.000"
 
   def install
     (share/"fonts").install Dir.glob("./**/PalemMUFI-bi.ttf")[0]
