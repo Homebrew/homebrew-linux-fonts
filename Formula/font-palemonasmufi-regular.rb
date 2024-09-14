ef5c790572796e4b74f2f07c02c96354c779519e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-14 00:22:34 +0000

    font-palemonasmufi-regular updated

--- Formula/font-palemonasmufi-regular.rb
+++ Formula/font-palemonasmufi-regular.rb
@@ -2,7 +2,7 @@ class FontPalemonasmufiRegular < Formula
   desc "Palemonasmufi regular font"
   homepage "https://mufi.info/q.php?p=mufi/fonts/fontfamily/6"
   url "https://mufi.info/q.php?o=mufifontfiledownload&name=PalemMUFI-reg.ttf"
-  version "2.0"
+  version "2.000"
 
   def install
     (share/"fonts").install Dir.glob("./**/PalemMUFI-reg.ttf")[0]
