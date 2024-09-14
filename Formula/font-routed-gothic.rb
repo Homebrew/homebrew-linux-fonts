75596755e3c21a3d1f676420b32ed1613bc18ba1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-14 00:22:34 +0000

    font-routed-gothic updated

--- Formula/font-routed-gothic.rb
+++ Formula/font-routed-gothic.rb
@@ -1,20 +1,21 @@
 class FontRoutedGothic < Formula
   desc "Routed gothic font"
   homepage "https://webonastick.com/fonts/routed-gothic/"
-  url "https://webonastick.com/fonts/routed-gothic/download/routed-gothic-ttf-v1.0.0.zip"
+  url "https://github.com/dse/routed-gothic/archive/refs/tags/v1.0.0.tar.gz",
+       verified: "github.com/dse/routed-gothic/"
   version "1.0.0"
-  sha256 "3ecffba109bf2619057815411253e5c6f47f82fedba38ade6c951e13a8b2ad5e"
+  sha256 "e0079b81fa068a4672f02585f7bc2910bf1535d8cd73b04d4a023bd2cbca361d"
 
   def install
-    (share/"fonts").install Dir.glob("./**/routed-gothic-half-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-narrow-half-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-narrow-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-narrow.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-wide-half-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-wide-italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic-wide.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/routed-gothic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/dist/ttf/routed-gothic-half-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/dist/ttf/routed-gothic-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/dist/ttf/routed-gothic-narrow-half-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/dist/ttf/routed-gothic-narrow-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/dist/ttf/routed-gothic-narrow.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/dist/ttf/routed-gothic-wide-half-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/dist/ttf/routed-gothic-wide-italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/dist/ttf/routed-gothic-wide.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/dist/ttf/routed-gothic.ttf")[0]
   end
 
   test do
