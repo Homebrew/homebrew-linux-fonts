fa616aecbd32d7b0e71f21e71cadd2d9c4885a2d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-14 00:22:34 +0000

    font-last-resort-high-efficiency updated

--- Formula/font-last-resort-high-efficiency.rb
+++ Formula/font-last-resort-high-efficiency.rb
@@ -1,9 +1,9 @@
 class FontLastResortHighEfficiency < Formula
   desc "Last resort high-efficiency font"
   homepage "https://github.com/unicode-org/last-resort-font"
-  url "https://github.com/unicode-org/last-resort-font/releases/download/15.100/LastResortHE-Regular.ttf"
-  version "15.100"
-  sha256 "74f93a8fb3ab8756e0e86cb3c2c0783fda10bdea6f979d5817869ba7284e56ca"
+  url "https://github.com/unicode-org/last-resort-font/releases/download/16.000/LastResortHE-Regular.ttf"
+  version "16.000"
+  sha256 "60c48abfc05e9f2ba33599c4dcba40105d1ece71d3761f0b26f75a38b5b17895"
 
   def install
     (share/"fonts").install Dir.glob("./**/LastResortHE-Regular.ttf")[0]
