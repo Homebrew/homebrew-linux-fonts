d6d1d1b35bfdf26ca928cb025d019631aea00c2a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-14 00:22:34 +0000

    font-gnu-unifont updated

--- Formula/font-gnu-unifont.rb
+++ Formula/font-gnu-unifont.rb
@@ -1,17 +1,17 @@
 class FontGnuUnifont < Formula
   desc "Gnu unifont font"
   homepage "https://unifoundry.com/unifont.html"
-  url "https://ftp.gnu.org/gnu/unifont/unifont-15.1.05/unifont-15.1.05.tar.gz",
+  url "https://ftp.gnu.org/gnu/unifont/unifont-16.0.01/unifont-16.0.01.tar.gz",
        verified: "ftp.gnu.org/gnu/unifont/"
-  version "15.1.05"
-  sha256 "d275f55f4358750e0f86305b92e87b88eb330aa46c15f553d2edf047fb1c23fa"
+  version "16.0.01"
+  sha256 "5d6180b8cf53238c8354d42ffc422dac65ac6bcbe28d27646be058a045d87a50"
 
   def install
-    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont-15.1.05.otf")[0]
-    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont_csur-15.1.05.otf")[0]
-    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont_jp-15.1.05.otf")[0]
-    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont_sample-15.1.05.otf")[0]
-    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont_upper-15.1.05.otf")[0]
+    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont-16.0.01.otf")[0]
+    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont_csur-16.0.01.otf")[0]
+    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont_jp-16.0.01.otf")[0]
+    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont_sample-16.0.01.otf")[0]
+    (share/"fonts").install Dir.glob("./**/font/precompiled/unifont_upper-16.0.01.otf")[0]
   end
 
   test do
