f2d87e471f653df067339cfac9237006f05d7fc9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-15 00:26:02 +0000

    font-chenyuluoyan updated

--- Formula/font-chenyuluoyan.rb
+++ Formula/font-chenyuluoyan.rb
@@ -1,8 +1,8 @@
 class FontChenyuluoyan < Formula
   desc "Chenyuluoyan font"
   homepage "https://github.com/Chenyu-otf/chenyuluoyan_thin"
-  url "https://github.com/Chenyu-otf/chenyuluoyan_thin/releases/download/v1.0-motospaced/ChenYuluoyan-Thin-Monospaced.ttf"
-  version "1.0-motospaced"
+  url "https://github.com/Chenyu-otf/chenyuluoyan_thin/releases/download/v1.0-monospaced/ChenYuluoyan-Thin-Monospaced.ttf"
+  version "1.0"
   sha256 "d8ecd0598634c92f0b29f90aabef8f7916f17aef19b0350883ca7b46979a5373"
 
   def install
