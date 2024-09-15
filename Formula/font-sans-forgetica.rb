fb02147748c6bf6c751842e5ec0b4a2a4e13469a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-15 00:26:02 +0000

    font-sans-forgetica updated

--- Formula/font-sans-forgetica.rb
+++ Formula/font-sans-forgetica.rb
@@ -3,6 +3,8 @@ class FontSansForgetica < Formula
   homepage "https://sansforgetica.rmit.edu.au/"
   head "https://sansforgetica.rmit.edu.au/Common/Zips/Sans%20Forgetica.zip"
 
+  disable! "2024-09-13", because: :no_longer_available
+
   def install
     (share/"fonts").install Dir.glob("./**/SansForgetica-Regular.otf")[0]
   end
