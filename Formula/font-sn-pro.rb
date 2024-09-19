e24a282aafe6aede0554df84eb546cb4ac5fe1e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-19 00:23:14 +0000

    font-sn-pro updated

--- Formula/font-sn-pro.rb
+++ Formula/font-sn-pro.rb
@@ -1,10 +1,10 @@
 class FontSnPro < Formula
   desc "Sn pro font family font"
   homepage "https://supernotes.app/open-source/sn-pro"
-  url "https://github.com/supernotes/sn-pro/releases/download/1.1.0/SN-Pro.zip",
+  url "https://github.com/supernotes/sn-pro/releases/download/1.2.0/SN-Pro.zip",
        verified: "github.com/supernotes/sn-pro/"
-  version "1.1.0"
-  sha256 "b8953185881a741e62912033d0fdc0ad99f31e470c6a51c07859f883ec26ee19"
+  version "1.2.0"
+  sha256 "1bd89ccc333df57ca959207b83282fcd6b9806d71070244394a892ea280ac792"
 
   def install
     (share/"fonts").install Dir.glob("./**/SNPro-Black.otf")[0]
