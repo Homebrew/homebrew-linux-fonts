1e260c75a312755f61a0916f4f20595094b627b3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-20 00:23:33 +0000

    font-jf-open-huninn updated

--- Formula/font-jf-open-huninn.rb
+++ Formula/font-jf-open-huninn.rb
@@ -1,13 +1,13 @@
 class FontJfOpenHuninn < Formula
   desc "Jf open huninn font"
   homepage "https://justfont.com/huninn/"
-  url "https://github.com/justfont/open-huninn-font/releases/download/v2.0/jf-openhuninn-2.0.ttf",
+  url "https://github.com/justfont/open-huninn-font/releases/download/v2.1/jf-openhuninn-2.1.ttf",
        verified: "github.com/justfont/open-huninn-font/"
-  version "2.0"
-  sha256 "eec8b0b68c34b9166ae37bed839b6126116b225d97f21a5954bb542b9fd1e68c"
+  version "2.1"
+  sha256 "9d5bf4932d31fe94c18cd8cfddc98bc1b14ce10f4e354c682179db290a99c825"
 
   def install
-    (share/"fonts").install Dir.glob("./**/jf-openhuninn-2.0.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/jf-openhuninn-2.1.ttf")[0]
   end
 
   test do
