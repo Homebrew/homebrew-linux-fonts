99e8938aad244fb252ac3cc018f458d91b3812de
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-27 00:23:59 +0000

    font-lilex updated

--- Formula/font-lilex.rb
+++ Formula/font-lilex.rb
@@ -1,16 +1,22 @@
 class FontLilex < Formula
   desc "Lilex font"
   homepage "https://github.com/mishamyrt/Lilex"
-  url "https://github.com/mishamyrt/Lilex/releases/download/2.530/Lilex.zip"
-  version "2.530"
-  sha256 "b019fc0da5e3ece5c735aa040218d3b8c9945546d2d333598b587b1237a66c4a"
+  url "https://github.com/mishamyrt/Lilex/releases/download/2.600/Lilex.zip"
+  version "2.600"
+  sha256 "1bcce6df9692897ae71a061e3d2c0b301cf04a777d99f0a29c76521b5a811f4c"
 
   def install
     (share/"fonts").install Dir.glob("./**/Lilex-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-BoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Lilex-ExtraLight.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-ExtraLightItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-Italic?wght?.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Lilex-Medium.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Lilex-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Lilex-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Lilex-ThinItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/Lilex?wght?.ttf")[0]
   end
 
