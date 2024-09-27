b35d4d7cfc7b78fd19e82416d516b7ac2f2effda
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-27 00:23:59 +0000

    font-geist updated

--- Formula/font-geist.rb
+++ Formula/font-geist.rb
@@ -1,22 +1,22 @@
 class FontGeist < Formula
   desc "Geist font"
   homepage "https://vercel.com/font/sans"
-  url "https://github.com/vercel/geist-font/releases/download/1.3.0/Geist-1.3.0.zip",
+  url "https://github.com/vercel/geist-font/releases/download/1.4.0/Geist-1.4.0.zip",
        verified: "github.com/vercel/geist-font/"
-  version "1.3.0"
-  sha256 "6a656e5efc991a0b465bc288b5455eebd7219e0668a936f8705a2e9d3a2a62c9"
+  version "1.4.0"
+  sha256 "1bd906111a8853f0720831d08a363077358afc755acf893c6d4ed29529bef139"
 
   def install
-    (share/"fonts").install Dir.glob("./**/statics-otf/Geist-Black.otf")[0]
-    (share/"fonts").install Dir.glob("./**/statics-otf/Geist-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/statics-otf/Geist-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/statics-otf/Geist-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/statics-otf/Geist-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/statics-otf/Geist-SemiBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/statics-otf/Geist-Thin.otf")[0]
-    (share/"fonts").install Dir.glob("./**/statics-otf/Geist-UltraBlack.otf")[0]
-    (share/"fonts").install Dir.glob("./**/statics-otf/Geist-UltraLight.otf")[0]
-    (share/"fonts").install Dir.glob("./**/variable-ttf/GeistVF.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/Geist-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/Geist-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/Geist-ExtraBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/Geist-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/Geist-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/Geist-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/Geist-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/Geist-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/Geist-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/variable/Geist?wght?.ttf")[0]
   end
 
   test do
