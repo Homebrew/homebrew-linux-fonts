197ade7ea441a97d2b932379a96f21cc91e05c98
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-03 00:23:54 +0000

    font-sudo updated

--- Formula/font-sudo.rb
+++ Formula/font-sudo.rb
@@ -1,11 +1,12 @@
 class FontSudo < Formula
   desc "Sudo font"
   homepage "https://github.com/jenskutilek/sudo-font/"
-  url "https://github.com/jenskutilek/sudo-font/releases/download/v1.4/sudo.zip"
-  version "1.4"
-  sha256 "cbb84bbfd5394734800f6156baf5253c03609ba871d971d4f7bd74f24effc9d5"
+  url "https://github.com/jenskutilek/sudo-font/releases/download/v2.0.0/sudo.zip"
+  version "2.0.0"
+  sha256 "525fecc3b57428e9f996e21a5cbbcb80e23b1c7198c7692b2f75b2046effc081"
 
   def install
+    (share/"fonts").install Dir.glob("./**/SudoUIVariable.ttf")[0]
     (share/"fonts").install Dir.glob("./**/SudoVariable.ttf")[0]
   end
 
