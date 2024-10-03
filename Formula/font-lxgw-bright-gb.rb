ab03cdf5641d9a4323ed2d839d9971b64f96cb58
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-03 00:23:54 +0000

    font-lxgw-bright-gb updated

--- Formula/font-lxgw-bright-gb.rb
+++ Formula/font-lxgw-bright-gb.rb
@@ -1,17 +1,17 @@
 class FontLxgwBrightGb < Formula
   desc "Lxgw bright gb font"
   homepage "https://github.com/lxgw/LxgwBright"
-  url "https://github.com/lxgw/LxgwBright/releases/download/v5.330/LXGWBrightGB.7z"
-  version "5.330"
-  sha256 "f9378f55d7c77216be7b430ec5b9a834d89e92a16fd640eb5a32d73b43adc3a0"
+  url "https://github.com/lxgw/LxgwBright/releases/download/v5.500/LXGWBrightGB.7z"
+  version "5.500"
+  sha256 "8083e54f6e0576191738c3c7cb8e56a05594bf788feb870d5aed6e2a9214fdd0"
 
   def install
     (share/"fonts").install Dir.glob("./**/LXGWBrightGB-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWBrightGB-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWBrightGB-LightItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LXGWBrightGB-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LXGWBrightGB-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LXGWBrightGB-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/LXGWBrightGB-SemiLight.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/LXGWBrightGB-SemiLightItalic.ttf")[0]
   end
 
   test do
