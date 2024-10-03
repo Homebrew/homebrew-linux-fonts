be849a5e1c1249b5821f679836ac7130d4bb06ff
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-03 00:23:54 +0000

    font-lxgw-wenkai-lite updated

--- Formula/font-lxgw-wenkai-lite.rb
+++ Formula/font-lxgw-wenkai-lite.rb
@@ -1,16 +1,16 @@
 class FontLxgwWenkaiLite < Formula
   desc "Lxgw wenkai lite font"
   homepage "https://github.com/lxgw/LxgwWenKai-Lite"
-  url "https://github.com/lxgw/LxgwWenKai-Lite/releases/download/v1.330/lxgw-wenkai-lite-v1.330.zip"
-  version "1.330"
-  sha256 "10beb11701118e6f1b301cdc2b2a2e6177e0fe53671c5f802603a9e9bd9c168d"
+  url "https://github.com/lxgw/LxgwWenKai-Lite/releases/download/v1.500/lxgw-wenkai-lite-v1.500.zip"
+  version "1.500"
+  sha256 "8705f7931b75ce33e65079af3756e256a0fbe4b28aa3b74156d339fb51e9c543"
 
   def install
-    (share/"fonts").install Dir.glob("./**/LXGWWenKaiLite-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LXGWWenKaiLite-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWWenKaiLite-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LXGWWenKaiLite-Regular.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/LXGWWenKaiMonoLite-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LXGWWenKaiMonoLite-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/LXGWWenKaiMonoLite-Medium.ttf")[0]
     (share/"fonts").install Dir.glob("./**/LXGWWenKaiMonoLite-Regular.ttf")[0]
   end
 
