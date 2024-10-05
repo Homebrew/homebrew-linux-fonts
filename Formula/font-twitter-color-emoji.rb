c4ae64eb58d27d7af507483fc04f8bd93cb7afea
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-05 00:23:44 +0000

    font-twitter-color-emoji updated

--- Formula/font-twitter-color-emoji.rb
+++ Formula/font-twitter-color-emoji.rb
@@ -1,9 +1,9 @@
 class FontTwitterColorEmoji < Formula
   desc "Twitter color emoji font"
   homepage "https://github.com/eosrei/twemoji-color-font/"
-  url "https://github.com/eosrei/twemoji-color-font/releases/download/v14.0.2/TwitterColorEmoji-SVGinOT-14.0.2.zip"
-  version "14.0.2"
-  sha256 "8e2c6cb768b5a578b1dacb8f70d3b91e782a8098821484af77cf322ac499f28a"
+  url "https://github.com/eosrei/twemoji-color-font/releases/download/v15.0.3/TwitterColorEmoji-SVGinOT-15.0.3.zip"
+  version "15.0.3"
+  sha256 "f96d2c9c54b7dc868749e90a2dcb0b060c592de9388763a18ff0a95bee9bbf8c"
 
   def install
     (share/"fonts").install Dir.glob("./**/TwitterColorEmoji-SVGinOT.ttf")[0]
