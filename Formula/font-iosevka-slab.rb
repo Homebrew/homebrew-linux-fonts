4c7aaba54c5b9cfd2888706658ff9c218fa846b6
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-06 00:26:19 +0000

    font-iosevka-slab updated

--- Formula/font-iosevka-slab.rb
+++ Formula/font-iosevka-slab.rb
@@ -1,9 +1,9 @@
 class FontIosevkaSlab < Formula
   desc "Iosevka slab font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v31.7.1/SuperTTC-IosevkaSlab-31.7.1.zip"
-  version "31.7.1"
-  sha256 "7738ba3952e1a43d94827f9ef88c1123350c61dc957cf62edf36edbe34056e95"
+  url "https://github.com/be5invis/Iosevka/releases/download/v31.8.0/SuperTTC-IosevkaSlab-31.8.0.zip"
+  version "31.8.0"
+  sha256 "dcb3f5766cb5e71017417e35541172dc260a9ece9e533f7e8d234ee16c708b2d"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaSlab.ttc")[0]
