785f9594fafde5c9c7704de8d8b19baf0c8db478
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-06 00:26:18 +0000

    font-geist updated

--- Formula/font-geist.rb
+++ Formula/font-geist.rb
@@ -4,7 +4,7 @@ class FontGeist < Formula
   url "https://github.com/vercel/geist-font/releases/download/1.4.0/Geist-v1.4.0.zip",
        verified: "github.com/vercel/geist-font/"
   version "1.4.0"
-  sha256 "b2c99487cd205def10ce8ab4b2ca045426c5c0e60f9707ec53c31146c63eb7b2"
+  sha256 "fafb2a4ce068d293bd53c29cef517597cef6437dc5f6eb5ecca8bc40337ec179"
 
   def install
     (share/"fonts").install Dir.glob("./**/otf/Geist-Black.otf")[0]
