3455f26beda1b160e2ffedf231e5bd0e17397d6a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-16 00:24:22 +0000

    font-ahuramzda updated

--- Formula/font-ahuramzda.rb
+++ Formula/font-ahuramzda.rb
@@ -2,7 +2,7 @@ class FontAhuramzda < Formula
   desc "Ahuramzda font"
   homepage "https://fontlibrary.org/font/ahuramazda/"
   url "https://fontlibrary.org/assets/downloads/ahuramazda/b2c0eeb9186f389749746f075b5a1abf/ahuramazda.zip"
-  version "1.000"
+  version "1.0"
 
   def install
     (share/"fonts").install Dir.glob("./**/ahuramazda.ttf")[0]
