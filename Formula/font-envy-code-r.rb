efe8c0a5e355e0b74c4ab42840e0ee57f1bc03e3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-16 00:24:22 +0000

    font-envy-code-r updated

--- Formula/font-envy-code-r.rb
+++ Formula/font-envy-code-r.rb
@@ -1,6 +1,6 @@
 class FontEnvyCodeR < Formula
   desc "Envy code r font"
-  homepage "https://damieng.com/blog/tag/envy-code/"
+  homepage "https://damieng.com/blog/2008/05/26/envy-code-r-preview-7-coding-font-released"
   url "https://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip"
   version "PR7"
   sha256 "9f7e9703aaf21110b4e1a54d954d57d4092727546348598a5a8e8101e4597aff"
