a4208ce1187ea0f0aae848fcf4694ab55f8d6819
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-16 00:24:22 +0000

    font-national-park updated

--- Formula/font-national-park.rb
+++ Formula/font-national-park.rb
@@ -1,18 +1,19 @@
 class FontNationalPark < Formula
   desc "National park font"
   homepage "https://nationalparktypeface.com/"
-  url "https://files.cargocollective.com/c1415345/NationalPark-otf.zip",
+  url "https://files.cargocollective.com/c1415345/NationalPark-All-Fonts.zip",
        verified: "cargocollective.com/"
   version "1.005"
 
   def install
-    (share/"fonts").install Dir.glob("./**/NationalPark-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/NationalPark-ExtraBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/NationalPark-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("./**/NationalPark-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/NationalPark-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/NationalPark-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/NationalPark-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/NationalPark-VariableVF.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/NationalPark-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/NationalPark-ExtraBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/NationalPark-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/NationalPark-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/NationalPark-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/NationalPark-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/otf/NationalPark-SemiBold.otf")[0]
   end
 
   test do
