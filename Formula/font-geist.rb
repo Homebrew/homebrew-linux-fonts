fb4da47eee3f245d82f0ae68eb151d06c9086420
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-18 00:24:01 +0000

    font-geist updated

--- Formula/font-geist.rb
+++ Formula/font-geist.rb
@@ -1,22 +1,22 @@
 class FontGeist < Formula
   desc "Geist font"
   homepage "https://vercel.com/font/sans"
-  url "https://github.com/vercel/geist-font/releases/download/1.4.0/Geist-v1.4.0.zip",
+  url "https://github.com/vercel/geist-font/releases/download/1.4.01/Geist-v1.4.01.zip",
        verified: "github.com/vercel/geist-font/"
-  version "1.4.0"
-  sha256 "fafb2a4ce068d293bd53c29cef517597cef6437dc5f6eb5ecca8bc40337ec179"
+  version "1.4.01"
+  sha256 "b013dce423d16c03238593f6b8e0a281322bafdc2a75b376a91de8721cf18315"
 
   def install
-    (share/"fonts").install Dir.glob("./**/otf/Geist-Black.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/Geist-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/Geist-ExtraBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/Geist-ExtraLight.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/Geist-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/Geist-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/Geist-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/Geist-SemiBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/otf/Geist-Thin.otf")[0]
-    (share/"fonts").install Dir.glob("./**/variable/Geist?wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist-Black.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist-ExtraBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist-ExtraLight.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist-Light.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist-Thin.otf")[0]
+    (share/"fonts").install Dir.glob("./**/Geist?wght?.ttf")[0]
   end
 
   test do
