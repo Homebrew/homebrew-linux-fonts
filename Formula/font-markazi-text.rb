a6fbec34a03df021ad80c3a473a4ec18f8dfae36
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-21 00:25:09 +0000

    font-markazi-text updated

--- Formula/font-markazi-text.rb
+++ Formula/font-markazi-text.rb
@@ -1,11 +1,11 @@
 class FontMarkaziText < Formula
   desc "Markazi text font"
   homepage "https://fonts.google.com/specimen/Markazi+Text"
-  head "https://github.com/google/fonts/raw/main/ofl/markazitextvfbeta/MarkaziText-VF.ttf",
+  head "https://github.com/google/fonts/raw/main/ofl/markazitext/MarkaziText%5Bwght%5D.ttf",
        verified: "github.com/google/fonts/"
 
   def install
-    (share/"fonts").install Dir.glob("./**/MarkaziText-VF.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MarkaziText?wght?.ttf")[0]
   end
 
   test do
