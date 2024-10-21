a915e7b2db43b0172fe9460258d8f393d3dc9c27
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-21 00:25:09 +0000

    font-juliamono updated

--- Formula/font-juliamono.rb
+++ Formula/font-juliamono.rb
@@ -1,16 +1,15 @@
 class FontJuliamono < Formula
   desc "Juliamono font"
   homepage "https://github.com/cormullion/juliamono"
-  url "https://github.com/cormullion/juliamono/releases/download/v0.057/JuliaMono-ttf.tar.gz"
-  version "0.057"
-  sha256 "e397ca1c6b84e6140d2e025fdc310544a519775b98d8f756ef0f5c42aefd4cc0"
+  url "https://github.com/cormullion/juliamono/releases/download/v0.058/JuliaMono-ttf.tar.gz"
+  version "0.058"
+  sha256 "513b9851d980cf4abc58ffee7c32819b0c7c995e9d8557fddc1d37016432726b"
 
   def install
     (share/"fonts").install Dir.glob("./**/JuliaMono-Black.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-BlackItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/JuliaMono-BoldLatin.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-ExtraBold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-ExtraBoldItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-Light.ttf")[0]
@@ -19,7 +18,6 @@ class FontJuliamono < Formula
     (share/"fonts").install Dir.glob("./**/JuliaMono-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-Regular.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-RegularItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/JuliaMono-RegularLatin.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-SemiBold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/JuliaMono-SemiBoldItalic.ttf")[0]
   end
