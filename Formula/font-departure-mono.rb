d3709a8395545eec35a91d4cf481058433d7dbbd
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-23 00:24:08 +0000

    font-departure-mono updated

--- Formula/font-departure-mono.rb
+++ Formula/font-departure-mono.rb
@@ -1,9 +1,9 @@
 class FontDepartureMono < Formula
   desc "Departure mono font"
   homepage "https://github.com/rektdeckard/departure-mono"
-  url "https://github.com/rektdeckard/departure-mono/releases/download/v1.420/DepartureMono-1.420.zip"
-  version "1.420"
-  sha256 "95481ab2f4fe7ee79b200971b63158741b8cfe228076499cb8abff46ef1fac6d"
+  url "https://github.com/rektdeckard/departure-mono/releases/download/v1.422/DepartureMono-1.422.zip"
+  version "1.422"
+  sha256 "fae4949a63893d265c79b7dd9903e63c5a3db52d07d4f29aa604390663067bdb"
 
   def install
     (share/"fonts").install Dir.glob("./**/DepartureMono-Regular.otf")[0]
