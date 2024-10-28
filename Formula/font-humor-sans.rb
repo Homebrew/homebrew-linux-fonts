27ac66929c4d8e9530bfc9222d4b4710c0800fcc
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-28 00:25:55 +0000

    font-humor-sans updated

--- Formula/font-humor-sans.rb
+++ Formula/font-humor-sans.rb
@@ -1,7 +1,7 @@
 class FontHumorSans < Formula
   desc "Humor sans font"
   homepage "https://xkcdsucks.blogspot.com.au/2009/03/xkcdsucks-is-proud-to-present-humor.html"
-  url "https://antiyawn.com/uploads/Humor-Sans-1.0.ttf",
+  url "http://antiyawn.com/uploads/Humor-Sans-1.0.ttf",
        verified: "antiyawn.com/"
   version "1.0"
   sha256 "2ded6a27448c9ed30aaff177744e2bcf1e52e0aab018b2a8be64565df633318f"
