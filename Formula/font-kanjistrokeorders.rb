9b418dde20a3ca8d7ce3947bc17a9a6912c2742f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-29 00:24:57 +0000

    font-kanjistrokeorders updated

--- Formula/font-kanjistrokeorders.rb
+++ Formula/font-kanjistrokeorders.rb
@@ -1,9 +1,9 @@
 class FontKanjistrokeorders < Formula
   desc "Kanjistrokeorders font"
   homepage "https://sites.google.com/site/nihilistorguk/"
-  url "https://drive.google.com/uc?export=download&id=1oyQoTB531tbhlYaOW7ugvutXZ7HSlJfW"
-  version "4.004,1oyQoTB531tbhlYaOW7ugvutXZ7HSlJfW"
-  sha256 "3374338e6d72c703ac2d70f69d8822463e8e285f32accc497e8ec8fe2ddbec2d"
+  url "https://drive.google.com/uc?export=download&id=1snpD-IQmT6fGGQjEePHdDzE2aiwuKrz4"
+  version "4.004,1snpD-IQmT6fGGQjEePHdDzE2aiwuKrz4"
+  sha256 "53061d36e2c7400b2b941bd6328c40d386586d5c5a3f89c43825a2d6979d5eee"
 
   def install
     (share/"fonts").install Dir.glob("./**/KanjiStrokeOrders_v4.004.ttf")[0]
