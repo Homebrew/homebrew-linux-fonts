bf6f7677e0ec31615021ad7945202d69a5253cdb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-05 00:24:09 +0000

    font-ark-pixel-16px-proportional updated

--- Formula/font-ark-pixel-16px-proportional.rb
+++ Formula/font-ark-pixel-16px-proportional.rb
@@ -1,9 +1,9 @@
 class FontArkPixel16pxProportional < Formula
   desc "Ark pixel 16px proportional font"
   homepage "https://github.com/TakWolf/ark-pixel-font"
-  url "https://github.com/TakWolf/ark-pixel-font/releases/download/2024.05.12/ark-pixel-font-16px-proportional-otf-v2024.05.12.zip"
-  version "2024.05.12"
-  sha256 "c02c9ab969edd153600af854f3f271800ee07d5bd854c29f1596aeca96fabea9"
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/2024.11.04/ark-pixel-font-16px-proportional-otf-v2024.11.04.zip"
+  version "2024.11.04"
+  sha256 "5de9f4611eabfea7906f485fea4790ec86b905daf1126a2e52d9e3b898847ce4"
 
   def install
     (share/"fonts").install Dir.glob("./**/ark-pixel-16px-proportional-ja.otf")[0]
