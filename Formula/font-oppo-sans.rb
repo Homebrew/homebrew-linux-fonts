7e0f2fabbefc24fb723dc676a826ae0acf9b8099
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-06 00:23:56 +0000

    font-oppo-sans updated

--- Formula/font-oppo-sans.rb
+++ Formula/font-oppo-sans.rb
@@ -1,14 +1,12 @@
 class FontOppoSans < Formula
   desc "Oppo sans font"
-  homepage "https://www.coloros.com/index/newsDetail?id=72"
-  head "https://static01.coloros.com/www/public/img/topic7/font-opposans.zip"
+  homepage "https://open.oppomobile.com/new/developmentDoc/info?id=13223"
+  url "https://openfs.oppomobile.com/open/oop/202410/18/62d51f494591f1a9040d83b597745911.zip"
+  version "4.0,202410,18,62d51f494591f1a9040d83b597745911"
+  sha256 "f8bef382e209a4965fe589efdb72fd6bef45a9b4fdb494e20ffa16b31cd364d5"
 
   def install
-    (share/"fonts").install Dir.glob("./**/OPlusSans3-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/OPlusSans3-ExtraLight.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/OPlusSans3-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/OPlusSans3-Medium.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/OPlusSans3-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/OPPO Sans 4.0.ttf")[0]
   end
 
   test do
