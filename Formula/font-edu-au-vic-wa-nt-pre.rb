f06741b7dcd40c947ab19b08a3f0700c6384c77c
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-11 00:25:03 +0000

    font-edu-au-vic-wa-nt-pre updated

--- Formula/font-edu-au-vic-wa-nt-pre.rb
+++ Formula/font-edu-au-vic-wa-nt-pre.rb
@@ -1,7 +1,8 @@
 class FontEduAuVicWaNtPre < Formula
   desc "Edu au vic wa nt pre font"
-  homepage "https://github.com/SorkinType/VICWANTSchoolhandAustralia"
-  head "https://github.com/google/fonts/raw/main/ofl/eduauvicwantpre/EduAUVICWANTPre%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Edu+AU+VIC+WA+NT+Pre"
+  head "https://github.com/google/fonts/raw/main/ofl/eduauvicwantpre/EduAUVICWANTPre%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/EduAUVICWANTPre?wght?.ttf")[0]
