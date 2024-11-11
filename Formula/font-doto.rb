9f53a4d9b0d045e4b40e5208d3715b76cf5e2292
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-11 00:25:03 +0000

    font-doto updated

--- Formula/font-doto.rb
+++ Formula/font-doto.rb
@@ -1,7 +1,8 @@
 class FontDoto < Formula
   desc "Doto font"
-  homepage "https://github.com/oliverlalan/Doto"
-  head "https://github.com/google/fonts/raw/main/ofl/doto/Doto%5BROND%2Cwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Doto"
+  head "https://github.com/google/fonts/raw/main/ofl/doto/Doto%5BROND%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/Doto?ROND,wght?.ttf")[0]
