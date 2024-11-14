24b1a4c6217006e26b090c312e66e4a27708721b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-14 00:24:10 +0000

    font-ibm-plex-sans-kr updated

--- Formula/font-ibm-plex-sans-kr.rb
+++ Formula/font-ibm-plex-sans-kr.rb
@@ -1,9 +1,9 @@
 class FontIbmPlexSansKr < Formula
   desc "Ibm plex sans kr font"
   homepage "https://github.com/IBM/plex"
-  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-kr%401.0.0/ibm-plex-sans-kr.zip"
-  version "1.0.0"
-  sha256 "b977effaabce2328f888db8590f0989c162cb9a9aad9d70949f3910552e6652b"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-kr%401.1.0/ibm-plex-sans-kr.zip"
+  version "1.1.0"
+  sha256 "9837800c8e5aedf4123775e1d767afa482c983321bd2fc606c985f405d24562e"
 
   def install
     (share/"fonts").install Dir.glob("./**/fonts/complete/otf/IBMPlexSansKR-Bold.otf")[0]
