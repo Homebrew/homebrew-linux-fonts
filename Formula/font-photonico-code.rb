eca8a69e4324a30994f350f2dffcd80138ba8a71
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-14 00:24:10 +0000

    font-photonico-code updated

--- Formula/font-photonico-code.rb
+++ Formula/font-photonico-code.rb
@@ -1,12 +1,12 @@
 class FontPhotonicoCode < Formula
   desc "Photonico code font"
   homepage "https://github.com/Photonico/Photonico_Code"
-  url "https://github.com/Photonico/Photonico_Code/releases/download/1.4/Photonico.1.4.Regular.ttf"
-  version "1.4"
-  sha256 "a6919ca4e80089843027c0e17bee2ab1f7a8f022ad651425cabe8c07ef8e53a2"
+  url "https://github.com/Photonico/Photonico_Code/releases/download/1.5/Photonico.1.5.Regular.ttf"
+  version "1.5"
+  sha256 "7d553f8f28a8a35b5a1a820bdcd68b42a6f1b54c820a25c76ec9e41e2bc807c1"
 
   def install
-    (share/"fonts").install Dir.glob("./**/Photonico.1.4.Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/Photonico.1.5.Regular.ttf")[0]
   end
 
   test do
