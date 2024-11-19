491ca1c21a51180429328d658db1022bcf363b22
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-19 00:25:33 +0000

    font-bigblue-terminal-nerd-font updated

--- Formula/font-bigblue-terminal-nerd-font.rb
+++ Formula/font-bigblue-terminal-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontBigblueTerminalNerdFont < Formula
   desc "Bigblueterm nerd font families (bigblue terminal) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/BigBlueTerminal.zip"
-  version "3.2.1"
-  sha256 "abc64063c7d13d87a3cfacc91912e3f647bb8b0fbae38a62b23d86548dc72b0a"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/BigBlueTerminal.zip"
+  version "3.3.0"
+  sha256 "1bf7384fa7649153ffa2666a71948f48ec19db5cf6753fa9eae5736e280f1fc1"
 
   def install
     (share/"fonts").install Dir.glob("./**/BigBlueTerm437NerdFont-Regular.ttf")[0]
