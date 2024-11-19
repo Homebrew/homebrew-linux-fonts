1087a7a98651aef00444b210f15b9e565e43c7e0
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-19 00:25:35 +0000

    font-profont-nerd-font updated

--- Formula/font-profont-nerd-font.rb
+++ Formula/font-profont-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontProfontNerdFont < Formula
   desc "Profont nerd font families (profont) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/ProFont.zip"
-  version "3.2.1"
-  sha256 "ae5898e790662f261471f3dd263f4357981b23bff68ee2ed1290a64901fa1d87"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/ProFont.zip"
+  version "3.3.0"
+  sha256 "74ea9be9a856489367dbd2f38abc61b0086a57ad3bbb780d9efc1ecdd256dedd"
 
   def install
     (share/"fonts").install Dir.glob("./**/ProFontIIxNerdFont-Regular.ttf")[0]
