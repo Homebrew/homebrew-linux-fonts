99be7f6759f01c57ab982b7beab0417585dcccdf
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-19 00:25:34 +0000

    font-fantasque-sans-mono-nerd-font updated

--- Formula/font-fantasque-sans-mono-nerd-font.rb
+++ Formula/font-fantasque-sans-mono-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontFantasqueSansMonoNerdFont < Formula
   desc "Fantasquesansm nerd font (fantasque sans mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FantasqueSansMono.zip"
-  version "3.2.1"
-  sha256 "8ea52f3ee930a64a086b0ca95f1cc1553034de7431f4c1813d239bf6b65532df"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FantasqueSansMono.zip"
+  version "3.3.0"
+  sha256 "849a660ce68cfba5b63dd8ee9cf90ed572f50579b8a098c2a2218559e6293fe5"
 
   def install
     (share/"fonts").install Dir.glob("./**/FantasqueSansMNerdFont-Bold.ttf")[0]
