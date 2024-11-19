22eff0808d7b46663e63cbb0efece16859b400b4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-19 00:25:34 +0000

    font-iosevka-nerd-font updated

--- Formula/font-iosevka-nerd-font.rb
+++ Formula/font-iosevka-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontIosevkaNerdFont < Formula
   desc "Iosevka nerd font (iosevka) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Iosevka.zip"
-  version "3.2.1"
-  sha256 "40e4256822d648bcd147be0f9afbe89e9eb611b59edf3d757768d51ad593963d"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Iosevka.zip"
+  version "3.3.0"
+  sha256 "a55e4e6d1ef065d0d09d5d6c6c6f3b9dd547d734db5850e107993a92e3258283"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaNerdFont-Bold.ttf")[0]
