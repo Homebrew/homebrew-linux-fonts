4693765c5438e6cce7d72a554adb4712a9de5c97
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-19 00:25:34 +0000

    font-go-mono-nerd-font updated

--- Formula/font-go-mono-nerd-font.rb
+++ Formula/font-go-mono-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontGoMonoNerdFont < Formula
   desc "Gomono nerd font (go mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Go-Mono.zip"
-  version "3.2.1"
-  sha256 "1440419676af8287b1e707ec1d2846880b6745524099f566b34ba7facd28d84a"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Go-Mono.zip"
+  version "3.3.0"
+  sha256 "bba4b48d0dfbff9da4c60a69f8280e7071566562185487aebdbf655cfaebd793"
 
   def install
     (share/"fonts").install Dir.glob("./**/GoMonoNerdFont-Bold.ttf")[0]
