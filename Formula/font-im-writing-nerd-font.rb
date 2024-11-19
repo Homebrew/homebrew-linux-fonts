9ed08c5d5a7fa0b0b3dc3369fa86dfb1534b29f1
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-19 00:25:34 +0000

    font-im-writing-nerd-font updated

--- Formula/font-im-writing-nerd-font.rb
+++ Formula/font-im-writing-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontImWritingNerdFont < Formula
   desc "Imwriting nerd font families (ia writer) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/iA-Writer.zip"
-  version "3.2.1"
-  sha256 "6d70576c6cb4b7a8b1ceb66c185dc01b355d269a1ec4e9a2b3bdb40e7cfa9293"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/iA-Writer.zip"
+  version "3.3.0"
+  sha256 "e1005ad388cf4125b62ab52cf22465b8ba22bc7e5805c0d287401cc9a0ec45a1"
 
   def install
     (share/"fonts").install Dir.glob("./**/iMWritingDuoNerdFont-Bold.ttf")[0]
