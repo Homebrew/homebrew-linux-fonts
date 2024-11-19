ae700e4611d7af89301a57717fa7317004feaa5d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-19 00:25:34 +0000

    font-gohufont-nerd-font updated

--- Formula/font-gohufont-nerd-font.rb
+++ Formula/font-gohufont-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontGohufontNerdFont < Formula
   desc "Gohufont  nerd font families (gohu) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Gohu.zip"
-  version "3.2.1"
-  sha256 "c223119a6ff3e222f266c2c977b3bf22ad27cf55c9d088b21305edfd0aaa0430"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Gohu.zip"
+  version "3.3.0"
+  sha256 "e3af23eb736c224b4a0aaf952c41158e70c423a74cf0958582fda5ce869d3e79"
 
   def install
     (share/"fonts").install Dir.glob("./**/GohuFont11NerdFont-Regular.ttf")[0]
