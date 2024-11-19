3bcf9b21c4c17a198389d39e7013d6785ddf63d9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-19 00:25:33 +0000

    font-0xproto-nerd-font updated

--- Formula/font-0xproto-nerd-font.rb
+++ Formula/font-0xproto-nerd-font.rb
@@ -1,13 +1,19 @@
 class Font0xprotoNerdFont < Formula
   desc "0xproto nerd font (0xproto) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip"
-  version "3.2.1"
-  sha256 "103f8dff0b6c06687a08cbe91bd446e03935ac6dbc0ba6f426967b45e3edd3b6"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/0xProto.zip"
+  version "3.3.0"
+  sha256 "99deb8d6b26e8946792f75107761bb46c4b927b5d21e4f9b2e163eb6efd6487b"
 
   def install
+    (share/"fonts").install Dir.glob("./**/0xProtoNerdFont-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/0xProtoNerdFont-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/0xProtoNerdFont-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/0xProtoNerdFontMono-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/0xProtoNerdFontMono-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/0xProtoNerdFontMono-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/0xProtoNerdFontPropo-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/0xProtoNerdFontPropo-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/0xProtoNerdFontPropo-Regular.ttf")[0]
   end
 
