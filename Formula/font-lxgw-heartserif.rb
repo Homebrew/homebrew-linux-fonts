dd18dd1a65ee40a3509fd2be4ca9614f58b0638d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-23 00:25:49 +0000

    font-lxgw-heartserif updated

--- Formula/font-lxgw-heartserif.rb
+++ Formula/font-lxgw-heartserif.rb
@@ -5,6 +5,8 @@ class FontLxgwHeartserif < Formula
   version "1.011"
   sha256 "c31ad15b1b3eee633ff74ac3d7b6440a73be7286a508ad190ffbb54b3cd61c31"
 
+  deprecate! "2024-11-22", because: :discontinued
+
   def install
     (share/"fonts").install Dir.glob("./**/LXGWHeartSerif.ttf")[0]
   end
