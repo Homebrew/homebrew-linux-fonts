d9951c977aa67470b43b53748b17c40d6240c210
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-02 00:27:30 +0000

    font-playwrite-sk-guides (new font)

---
+++ Formula/font-playwrite-sk-guides.rb
@@ -0,0 +1,13 @@
+class FontPlaywriteSkGuides < Formula
+  desc "Playwrite sk guides font"
+  homepage "https://github.com/TypeTogether/Playwrite"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteskguides/PlaywriteSKGuides-Regular.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/PlaywriteSKGuides-Regular.ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
