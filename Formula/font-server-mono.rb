a34f90e0cb1c1e4eff3404591c2e1718bd1e0951
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-03 00:27:33 +0000

    font-server-mono updated

--- Formula/font-server-mono.rb
+++ Formula/font-server-mono.rb
@@ -1,10 +1,10 @@
 class FontServerMono < Formula
   desc "Server mono font"
   homepage "https://servermono.com/"
-  url "https://github.com/internet-development/www-server-mono/archive/refs/tags/0.0.4.tar.gz",
+  url "https://github.com/internet-development/www-server-mono/archive/refs/tags/0.0.5.tar.gz",
        verified: "github.com/internet-development/www-server-mono/"
-  version "0.0.4"
-  sha256 "677169c9ed971ccafea0d55f4f34f3784e61a3b848784e12247608b344f477c4"
+  version "0.0.5"
+  sha256 "ba94c13607a98a8bf303e785c5cdf03d90d77089469c68ccbb4f4f4564f21719"
 
   def install
     (share/"fonts").install Dir.glob("./**/fonts/ServerMono-Regular-Italic.otf")[0]
