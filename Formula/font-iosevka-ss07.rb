5ae55e0ad5e519007a94733f9fd8dec09d187abe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-08 00:29:04 +0000

    font-iosevka-ss07 updated

--- Formula/font-iosevka-ss07.rb
+++ Formula/font-iosevka-ss07.rb
@@ -1,9 +1,9 @@
 class FontIosevkaSs07 < Formula
   desc "Iosevka ss07 font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v32.2.0/SuperTTC-IosevkaSS07-32.2.0.zip"
-  version "32.2.0"
-  sha256 "cff79f1a93745743fa0346e4a52fe3b237318825d233c963a158d7a528bcb9d6"
+  url "https://github.com/be5invis/Iosevka/releases/download/v32.2.1/SuperTTC-IosevkaSS07-32.2.1.zip"
+  version "32.2.1"
+  sha256 "000dc74ed94f41847fde532e699421a7da169e5c90f813b865f4764024a05d57"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaSS07.ttc")[0]
