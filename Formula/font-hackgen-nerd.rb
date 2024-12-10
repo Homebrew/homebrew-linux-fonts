25bc515cdd228c95a24c213b7e0c2f3b4e86267f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-10 00:26:59 +0000

    font-hackgen-nerd updated

--- Formula/font-hackgen-nerd.rb
+++ Formula/font-hackgen-nerd.rb
@@ -1,9 +1,9 @@
 class FontHackgenNerd < Formula
   desc "Hackgennerd font"
   homepage "https://github.com/yuru7/HackGen"
-  url "https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip"
-  version "2.9.0"
-  sha256 "277cb874bbcf8a884e512bb2a01d62341d57286d8947057d652fab6488fc941c"
+  url "https://github.com/yuru7/HackGen/releases/download/v2.9.1/HackGen_NF_v2.9.1.zip"
+  version "2.9.1"
+  sha256 "1fd174866c7bcbe1cc09ebef74f9246a21664fedbb9c9ced349a328ab6357f0d"
 
   def install
     (share/"fonts").install Dir.glob("./**/HackGen35ConsoleNF-Bold.ttf")[0]
