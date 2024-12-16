36d6a92f1e9cd7cc9493782be9df7ccb2c404f4d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:42 +0000

    font-playwrite-pe-guides updated

--- Formula/font-playwrite-pe-guides.rb
+++ Formula/font-playwrite-pe-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywritePeGuides < Formula
   desc "Playwrite pe guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritepeguides/PlaywritePEGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+PE+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritepeguides/PlaywritePEGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywritePEGuides-Regular.ttf")[0]
