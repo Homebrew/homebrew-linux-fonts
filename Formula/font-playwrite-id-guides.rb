2ce4ae11603be0dce2448bf2da9bd3b6dd006410
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:42 +0000

    font-playwrite-id-guides updated

--- Formula/font-playwrite-id-guides.rb
+++ Formula/font-playwrite-id-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteIdGuides < Formula
   desc "Playwrite id guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteidguides/PlaywriteIDGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+ID+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteidguides/PlaywriteIDGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteIDGuides-Regular.ttf")[0]
