43bc456c3d67338a5b11ef221ddaad472c66898a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:42 +0000

    font-playwrite-mx-guides updated

--- Formula/font-playwrite-mx-guides.rb
+++ Formula/font-playwrite-mx-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteMxGuides < Formula
   desc "Playwrite mx guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritemxguides/PlaywriteMXGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+MX+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritemxguides/PlaywriteMXGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteMXGuides-Regular.ttf")[0]
