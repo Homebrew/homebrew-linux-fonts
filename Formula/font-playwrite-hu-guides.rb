ed69fe07d9f012f32fcdae370ec22a823829f455
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:42 +0000

    font-playwrite-hu-guides updated

--- Formula/font-playwrite-hu-guides.rb
+++ Formula/font-playwrite-hu-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteHuGuides < Formula
   desc "Playwrite hu guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritehuguides/PlaywriteHUGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+HU+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritehuguides/PlaywriteHUGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteHUGuides-Regular.ttf")[0]
