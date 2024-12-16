223e00d6df79095d58cfc3b37145aa9ebe96dc3d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:41 +0000

    font-playwrite-es-deco-guides updated

--- Formula/font-playwrite-es-deco-guides.rb
+++ Formula/font-playwrite-es-deco-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteEsDecoGuides < Formula
   desc "Playwrite es deco guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteesdecoguides/PlaywriteESDecoGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+ES+Deco+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteesdecoguides/PlaywriteESDecoGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteESDecoGuides-Regular.ttf")[0]
