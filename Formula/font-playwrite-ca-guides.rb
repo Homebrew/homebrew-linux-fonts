daeb0886ee8d254069f9f90d8faebccdf2be8349
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:41 +0000

    font-playwrite-ca-guides updated

--- Formula/font-playwrite-ca-guides.rb
+++ Formula/font-playwrite-ca-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteCaGuides < Formula
   desc "Playwrite ca guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritecaguides/PlaywriteCAGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+CA+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritecaguides/PlaywriteCAGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteCAGuides-Regular.ttf")[0]
