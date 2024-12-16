b71b88f5623ba3cbfed3f7b8c2b75669ba802281
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:41 +0000

    font-playwrite-au-vic-guides updated

--- Formula/font-playwrite-au-vic-guides.rb
+++ Formula/font-playwrite-au-vic-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteAuVicGuides < Formula
   desc "Playwrite au vic guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteauvicguides/PlaywriteAUVICGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+AU+VIC+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteauvicguides/PlaywriteAUVICGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteAUVICGuides-Regular.ttf")[0]
