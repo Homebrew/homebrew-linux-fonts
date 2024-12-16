e7b757b61e5da587ce8dc09f4f6c21f4edea380e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:42 +0000

    font-playwrite-in-guides updated

--- Formula/font-playwrite-in-guides.rb
+++ Formula/font-playwrite-in-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteInGuides < Formula
   desc "Playwrite in guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwriteinguides/PlaywriteINGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+IN+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwriteinguides/PlaywriteINGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteINGuides-Regular.ttf")[0]
