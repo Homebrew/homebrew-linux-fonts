40d6096733b5b0e38cbc23f7435ea5a490410cb3
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:42 +0000

    font-playwrite-ng-modern-guides updated

--- Formula/font-playwrite-ng-modern-guides.rb
+++ Formula/font-playwrite-ng-modern-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteNgModernGuides < Formula
   desc "Playwrite ng modern guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritengmodernguides/PlaywriteNGModernGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+NG+Modern+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritengmodernguides/PlaywriteNGModernGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteNGModernGuides-Regular.ttf")[0]
