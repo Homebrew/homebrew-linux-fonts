e2e160ef216ffc6add5357d1387d04202407190d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:41 +0000

    font-playwrite-co-guides updated

--- Formula/font-playwrite-co-guides.rb
+++ Formula/font-playwrite-co-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteCoGuides < Formula
   desc "Playwrite co guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritecoguides/PlaywriteCOGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+CO+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritecoguides/PlaywriteCOGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteCOGuides-Regular.ttf")[0]
