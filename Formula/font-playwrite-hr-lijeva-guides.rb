83845a10fbc759bf9e88432d226a49e213e256d4
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:42 +0000

    font-playwrite-hr-lijeva-guides updated

--- Formula/font-playwrite-hr-lijeva-guides.rb
+++ Formula/font-playwrite-hr-lijeva-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteHrLijevaGuides < Formula
   desc "Playwrite hr lijeva guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritehrlijevaguides/PlaywriteHRLijevaGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+HR+Lijeva+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritehrlijevaguides/PlaywriteHRLijevaGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteHRLijevaGuides-Regular.ttf")[0]
