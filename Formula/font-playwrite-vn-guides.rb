3c3f686c0651c9321ef418c43fbaf30d450a84eb
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:42 +0000

    font-playwrite-vn-guides updated

--- Formula/font-playwrite-vn-guides.rb
+++ Formula/font-playwrite-vn-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteVnGuides < Formula
   desc "Playwrite vn guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritevnguides/PlaywriteVNGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+VN+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritevnguides/PlaywriteVNGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteVNGuides-Regular.ttf")[0]
