3ac7d69ee1e33280dbeac9f54d7c3058d448d8d9
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:41 +0000

    font-playwrite-de-grund-guides updated

--- Formula/font-playwrite-de-grund-guides.rb
+++ Formula/font-playwrite-de-grund-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteDeGrundGuides < Formula
   desc "Playwrite de grund guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritedegrundguides/PlaywriteDEGrundGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+DE+Grund+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritedegrundguides/PlaywriteDEGrundGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteDEGrundGuides-Regular.ttf")[0]
