e8823994edef29b4ad358ac13e1cc91ef51ae848
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-16 00:27:41 +0000

    font-playwrite-br-guides updated

--- Formula/font-playwrite-br-guides.rb
+++ Formula/font-playwrite-br-guides.rb
@@ -1,7 +1,8 @@
 class FontPlaywriteBrGuides < Formula
   desc "Playwrite br guides font"
-  homepage "https://github.com/TypeTogether/Playwrite"
-  head "https://github.com/google/fonts/raw/main/ofl/playwritebrguides/PlaywriteBRGuides-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Playwrite+BR+Guides"
+  head "https://github.com/google/fonts/raw/main/ofl/playwritebrguides/PlaywriteBRGuides-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/PlaywriteBRGuides-Regular.ttf")[0]
