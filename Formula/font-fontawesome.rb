fa990adfaa16126d87544775b087da0398f972e5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-18 00:25:31 +0000

    font-fontawesome updated

--- Formula/font-fontawesome.rb
+++ Formula/font-fontawesome.rb
@@ -1,10 +1,10 @@
 class FontFontawesome < Formula
   desc "Font awesome font"
   homepage "https://fontawesome.com/"
-  url "https://github.com/FortAwesome/Font-Awesome/releases/download/6.7.1/fontawesome-free-6.7.1-desktop.zip",
+  url "https://github.com/FortAwesome/Font-Awesome/releases/download/6.7.2/fontawesome-free-6.7.2-desktop.zip",
        verified: "github.com/FortAwesome/Font-Awesome/"
-  version "6.7.1"
-  sha256 "3118838d8d0aa88b8c9a5e132f8a195a3f1b23895ae66c61dc6746f9ceef80da"
+  version "6.7.2"
+  sha256 "22ff7898b429b997a45e1cf89bb869ed3abcc65333d90289181ba5363c8fd19b"
 
   def install
     (share/"fonts").install Dir.glob("./**/otfs/Font Awesome 6 Brands-Regular-400.otf")[0]
