09fd6824a469c1782952b851f11b8473321c35a7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-18 00:25:31 +0000

    font-ebh updated

--- Formula/font-ebh.rb
+++ Formula/font-ebh.rb
@@ -1,7 +1,8 @@
 class FontEbh < Formula
   desc "Ebh font"
   homepage "https://exeterbookhand.com/"
-  url "https://exeterbookhand.com/EBH-v1.0.0.zip"
+  url "https://github.com/rubywku/exeterbookhand/releases/download/v1.0.0/EBH-v1.0.0.zip",
+       verified: "github.com/rubywku/exeterbookhand/"
   version "1.0.0"
   sha256 "6787471d7766a8be31f2e601620e402ff43bafd183d626a8df75767e91052af3"
 
