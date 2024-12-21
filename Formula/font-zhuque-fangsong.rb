054cbd82c3287a0425db04ebcb67b5fc7fd4c9a7
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-21 00:24:00 +0000

    font-zhuque-fangsong updated

--- Formula/font-zhuque-fangsong.rb
+++ Formula/font-zhuque-fangsong.rb
@@ -1,9 +1,9 @@
 class FontZhuqueFangsong < Formula
   desc "Zhuque fangsong font"
   homepage "https://github.com/TrionesType/zhuque"
-  url "https://github.com/TrionesType/zhuque/releases/download/v0.210/ZhuqueFangsong-v0.210.zip"
-  version "0.210"
-  sha256 "17afd2e7a196ec4a3139f2fea0a174285aac148c76f5a9bc7eb7d72039a9b87a"
+  url "https://github.com/TrionesType/zhuque/releases/download/v0.211/ZhuqueFangsong-Regular-v0.211.zip"
+  version "0.211"
+  sha256 "4141782e9f36bc669d042422505f5c506df47930ee7bfa82342d8cec631f8f10"
 
   def install
     (share/"fonts").install Dir.glob("./**/ZhuqueFangsong-Regular.ttf")[0]
