26885f1b0cffd96507d087a019b4fc0958432776
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-22 00:27:03 +0000

    font-iosevka-aile updated

--- Formula/font-iosevka-aile.rb
+++ Formula/font-iosevka-aile.rb
@@ -1,9 +1,9 @@
 class FontIosevkaAile < Formula
   desc "Iosevka aile font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v32.2.1/SuperTTC-IosevkaAile-32.2.1.zip"
-  version "32.2.1"
-  sha256 "fb0d3f56b5524107f7c5f1d03eaedf0083644756783300b88a811f3977820ce4"
+  url "https://github.com/be5invis/Iosevka/releases/download/v32.3.0/SuperTTC-IosevkaAile-32.3.0.zip"
+  version "32.3.0"
+  sha256 "2c62f85d1caa185c38091fddb0ebc1afe68ba3b2caffc8e71067000f5abf2df7"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaAile.ttc")[0]
