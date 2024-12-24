00d06ff2a2173c6179bdbac2bbd309eee5f75f9b
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-24 00:24:27 +0000

    font-edlo updated

--- Formula/font-edlo.rb
+++ Formula/font-edlo.rb
@@ -1,10 +1,11 @@
 class FontEdlo < Formula
   desc "Edlo font"
   homepage "https://ehamiter.github.io/Edlo/"
-  head "https://github.com/ehamiter/Edlo/raw/master/edlo.ttf",
+  head "https://github.com/ehamiter/Edlo/archive/refs/heads/master.tar.gz",
        verified: "github.com/ehamiter/Edlo/"
 
   def install
+    (share/"fonts").install Dir.glob("./**/edlo-nerd-font.ttf")[0]
     (share/"fonts").install Dir.glob("./**/edlo.ttf")[0]
   end
 
