9395fb1bd53e388aef8d9011332cd923e3dae57a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-24 00:24:27 +0000

    font-coelacanth updated

--- Formula/font-coelacanth.rb
+++ Formula/font-coelacanth.rb
@@ -1,20 +1,26 @@
 class FontCoelacanth < Formula
   desc "Coelacanth font"
-  homepage "https://github.com/Fuzzypeg/Coelacanth"
-  url "https://github.com/Fuzzypeg/Coelacanth/raw/master/release/coelacanth_v0.008.zip"
-  version "0.008"
-  sha256 "77c0d55970b6275a7249bd73a9907b22e6613c1bf3a162a74a41656b1ff81139"
+  homepage "https://gitlab.com/Fuzzypeg/coelacanth"
+  url "https://gitlab.com/Fuzzypeg/coelacanth/-/raw/master/release/coelacanth_v0.1.0.zip"
+  version "0.1.0"
+  sha256 "dc4efc80aa92eaf1987ae4fe26cd4e455b52eedf791386e12d739e0d31935250"
 
   def install
     (share/"fonts").install Dir.glob("./**/Coelacanth.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthBoldIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthCaption.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthCaptionBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthCaptionBoldIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthCaptionExtraLt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthCaptionExtraLtIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthCaptionHeavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthCaptionHeavyIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthCaptionIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthCaptionLt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthCaptionLtIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthCaptionSemibd.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthCaptionSemibdIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthDisplay.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthDisplayBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthDisplayExtraLt.otf")[0]
@@ -22,24 +28,38 @@ class FontCoelacanth < Formula
     (share/"fonts").install Dir.glob("./**/CoelacanthDisplayLt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthDisplaySemibd.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthExtraLt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthExtraLtIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthHeavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthHeavyIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthLt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthLtIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthPearl.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthPearlBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthPearlBoldIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthPearlExtraLt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthPearlExtraLtIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthPearlHeavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthPearlHeavyIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthPearlIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthPearlLt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthPearlLtIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthPearlSemibd.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthPearlSemibdIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSemibd.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthSemibdIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubcapt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptBoldIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptExtraLt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptExtraLtIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptHeavy.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptHeavyIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptLt.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptLtIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptSemibd.otf")[0]
+    (share/"fonts").install Dir.glob("./**/CoelacanthSubcaptSemibdIt.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubhd.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubhdBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/CoelacanthSubhdExtraLt.otf")[0]
