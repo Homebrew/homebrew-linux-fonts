15c5d14d41d6758e8379eafda3e4c3a13a4d279a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-24 00:24:27 +0000

    font-constructium updated

--- Formula/font-constructium.rb
+++ Formula/font-constructium.rb
@@ -1,8 +1,10 @@
 class FontConstructium < Formula
   desc "Constructium font"
-  homepage "https://www.kreativekorp.com/software/fonts/constructium.shtml"
-  head "https://www.kreativekorp.com/swdownload/fonts/xlang/constructium.zip",
-       user_agent: ":fake"
+  homepage "https://www.kreativekorp.com/software/fonts/constructium/"
+  url "https://github.com/kreativekorp/open-relay/releases/download/2024-06-01/Constructium.zip",
+       verified: "github.com/kreativekorp/open-relay/"
+  version "2024-06-01"
+  sha256 "85cf9d728b143169ddf4f9c00a53025dce533a748646d1d7afbe24c18d6dbe2e"
 
   def install
     (share/"fonts").install Dir.glob("./**/Constructium.ttf")[0]
