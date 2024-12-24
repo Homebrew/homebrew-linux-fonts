9ec26eae311451264be807f1564da0358ae37f5d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-12-24 00:24:27 +0000

    font-ligature-symbols updated

--- Formula/font-ligature-symbols.rb
+++ Formula/font-ligature-symbols.rb
@@ -1,7 +1,8 @@
 class FontLigatureSymbols < Formula
   desc "Ligature symbols font"
   homepage "https://kudakurage.com/ligature_symbols/"
-  head "https://kudakurage.com/ligature_symbols/LigatureSymbols.zip"
+  url "https://kudakurage.com/ligature_symbols/LigatureSymbols.zip"
+  version "2.11"
 
   def install
     (share/"fonts").install Dir.glob("./**/LigatureSymbols-2.11.otf")[0]
