11adffbaa4f3d8a5277730c5c9e9c453fa062b2b
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-05 00:27:04 +0000

    font-adinatha-tamil-brahmi updated

--- Formula/font-adinatha-tamil-brahmi.rb
+++ Formula/font-adinatha-tamil-brahmi.rb
@@ -1,7 +1,7 @@
 class FontAdinathaTamilBrahmi < Formula
   desc "Adinatha tamil brahmi font"
-  homepage "http://www.virtualvinodh.com/wp/tamil-brahmi-font/"
-  head "http://www.virtualvinodh.com/download/Adinatha-Tamil-Brahmi.zip"
+  homepage "https://virtualvinodh.com/projects/adinatha"
+  head "https://www.virtualvinodh.com/downloads/Adinatha-Tamil-Brahmi.zip"
 
   def install
     (share/"fonts").install Dir.glob("./**/Adinatha-Tamil-Brahmi.otf")[0]
