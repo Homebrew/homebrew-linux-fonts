7fe8e473cd9516916e19f32747ae0a31d37df0bf
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-08 00:24:40 +0000

    font-ark-pixel-16px-monospaced updated

--- Formula/font-ark-pixel-16px-monospaced.rb
+++ Formula/font-ark-pixel-16px-monospaced.rb
@@ -1,9 +1,9 @@
 class FontArkPixel16pxMonospaced < Formula
   desc "Ark pixel 16px monospaced font"
   homepage "https://github.com/TakWolf/ark-pixel-font"
-  url "https://github.com/TakWolf/ark-pixel-font/releases/download/2024.11.04/ark-pixel-font-16px-monospaced-otf-v2024.11.04.zip"
-  version "2024.11.04"
-  sha256 "d851ea65d1c216c226af7b0051e5aed027156626e7dc70593807d4ee66218201"
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/2025.01.06/ark-pixel-font-16px-monospaced-otf-v2025.01.06.zip"
+  version "2025.01.06"
+  sha256 "c4e07f45e37ef1ebaf4ebd4e64c48a10faa49e4d3b5bc4b4df892185ba780e05"
 
   def install
     (share/"fonts").install Dir.glob("./**/ark-pixel-16px-monospaced-ja.otf")[0]
