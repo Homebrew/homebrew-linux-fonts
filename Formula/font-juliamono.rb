43862b82771cff8980c35f1cfd5976a1e7eb2cff
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-10 00:25:18 +0000

    font-juliamono updated

--- Formula/font-juliamono.rb
+++ Formula/font-juliamono.rb
@@ -1,9 +1,10 @@
 class FontJuliamono < Formula
   desc "Juliamono font"
-  homepage "https://github.com/cormullion/juliamono"
-  url "https://github.com/cormullion/juliamono/releases/download/v0.058/JuliaMono-ttf.tar.gz"
-  version "0.058"
-  sha256 "513b9851d980cf4abc58ffee7c32819b0c7c995e9d8557fddc1d37016432726b"
+  homepage "https://juliamono.netlify.app/"
+  url "https://github.com/cormullion/juliamono/releases/download/v0.059/JuliaMono-ttf.tar.gz",
+       verified: "github.com/cormullion/juliamono/"
+  version "0.059"
+  sha256 "e072777a94437519d9bda7eb5cf21b570a090802266d09827ab8b6bc84d13097"
 
   def install
     (share/"fonts").install Dir.glob("./**/JuliaMono-Black.ttf")[0]
