f08ada64d2c3be44df442787e595457762d41ed4
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-15 00:24:16 +0000

    font-ibm-plex-sans-tc updated

--- Formula/font-ibm-plex-sans-tc.rb
+++ Formula/font-ibm-plex-sans-tc.rb
@@ -1,8 +1,8 @@
 class FontIbmPlexSansTc < Formula
   desc "Ibm plex sans tc font"
   homepage "https://github.com/IBM/plex"
-  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-tc%401.1.0/ibm-plex-sans-tc.zip"
-  version "1.1.0"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-tc%401.1.1/ibm-plex-sans-tc.zip"
+  version "1.1.1"
   sha256 "b79600d3c9155fa05c9024eb81cfe2fbdd8cd068503dfafd79405ea577aa9fec"
 
   def install
