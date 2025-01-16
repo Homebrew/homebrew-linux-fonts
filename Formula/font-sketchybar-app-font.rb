515a5f936da058dbbbcacfdcd98b5d1d6dfe9521
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-16 00:24:06 +0000

    font-sketchybar-app-font updated

--- Formula/font-sketchybar-app-font.rb
+++ Formula/font-sketchybar-app-font.rb
@@ -1,9 +1,9 @@
 class FontSketchybarAppFont < Formula
   desc "Sketchybar-app-font font"
   homepage "https://github.com/kvndrsslr/sketchybar-app-font"
-  url "https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.29/sketchybar-app-font.ttf"
-  version "2.0.29"
-  sha256 "0e39ca14d7f305f675b7424acf2d175517992dc7f1468deca9cd979ec0b8804a"
+  url "https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.30/sketchybar-app-font.ttf"
+  version "2.0.30"
+  sha256 "d8ef40fb5683433584648df453a9c20896c2f0362bc2dc3465ed104d42b8ee9d"
 
   def install
     (share/"fonts").install Dir.glob("./**/sketchybar-app-font.ttf")[0]
