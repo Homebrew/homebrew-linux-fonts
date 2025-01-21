bb099fa8823856c946c9d7ca40314e71a8e69b04
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-21 00:23:52 +0000

    font-iosevka-curly updated

--- Formula/font-iosevka-curly.rb
+++ Formula/font-iosevka-curly.rb
@@ -1,9 +1,9 @@
 class FontIosevkaCurly < Formula
   desc "Iosevka curly font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v32.3.1/SuperTTC-IosevkaCurly-32.3.1.zip"
-  version "32.3.1"
-  sha256 "f0ac3331797740456982bb2ace7943ec7d6d2a61fa934005cd870b0a1451b789"
+  url "https://github.com/be5invis/Iosevka/releases/download/v32.4.0/SuperTTC-IosevkaCurly-32.4.0.zip"
+  version "32.4.0"
+  sha256 "bbc8ad8c2a4eda5547908770e1580f92d0dce7351f97c6806964315ae165d49b"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaCurly.ttc")[0]
