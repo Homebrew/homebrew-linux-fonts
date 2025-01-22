1fbd03d3dac0047ce80f0ed52cf5f9eb01468e2f
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-22 00:24:22 +0000

    font-lxgw-simxihei updated

--- Formula/font-lxgw-simxihei.rb
+++ Formula/font-lxgw-simxihei.rb
@@ -1,8 +1,8 @@
 class FontLxgwSimxihei < Formula
   desc "Lxgw simxihei font"
   homepage "https://github.com/lxgw/SimXiZhi"
-  url "https://github.com/lxgw/SimXiZhi/releases/download/v1.008/SimXiHei.ttf"
-  version "1.008"
+  url "https://github.com/lxgw/SimXiZhi/releases/download/v1.008.1/SimXiHei.ttf"
+  version "1.008.1"
   sha256 "73a0d405c71eb1cde0292fd796fc1827c618388c244c007b1582a8ec1e9a77de"
 
   def install
