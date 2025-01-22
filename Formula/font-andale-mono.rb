bd704e6e770afa2f74f5a76263cb459a9b4b0b95
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-22 00:24:22 +0000

    font-andale-mono updated

--- Formula/font-andale-mono.rb
+++ Formula/font-andale-mono.rb
@@ -2,8 +2,10 @@ class FontAndaleMono < Formula
   desc "Andale mono font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
   head "https://downloads.sourceforge.net/corefonts/andale32.exe"
+  depends_on "cabextract" => :build
 
   def install
+    system "cabextract", "andale32.exe"
     (share/"fonts").install Dir.glob("./**/AndaleMo.TTF")[0]
   end
 
