3351da7faf85baef94e5d6904c82da5c55b64fae
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-22 00:24:22 +0000

    font-courier-new updated

--- Formula/font-courier-new.rb
+++ Formula/font-courier-new.rb
@@ -2,8 +2,10 @@ class FontCourierNew < Formula
   desc "Courier new font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
   head "https://downloads.sourceforge.net/corefonts/courie32.exe"
+  depends_on "cabextract" => :build
 
   def install
+    system "cabextract", "courie32.exe"
     (share/"fonts").install Dir.glob("./**/cour.ttf")[0]
     (share/"fonts").install Dir.glob("./**/courbd.ttf")[0]
     (share/"fonts").install Dir.glob("./**/courbi.ttf")[0]
