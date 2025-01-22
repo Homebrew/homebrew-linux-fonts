e583bd9726236a8cbcc51ef8c61c028a794681d3
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-22 00:24:22 +0000

    font-arial updated

--- Formula/font-arial.rb
+++ Formula/font-arial.rb
@@ -2,8 +2,10 @@ class FontArial < Formula
   desc "Arial font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
   head "https://downloads.sourceforge.net/corefonts/arial32.exe"
+  depends_on "cabextract" => :build
 
   def install
+    system "cabextract", "arial32.exe"
     (share/"fonts").install Dir.glob("./**/Arial.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Arialbd.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Arialbi.TTF")[0]
