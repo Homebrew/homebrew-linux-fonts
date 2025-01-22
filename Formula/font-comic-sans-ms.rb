7bbcff515c12d51f0d51e2f1713309a77e3a8ee4
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-22 00:24:22 +0000

    font-comic-sans-ms updated

--- Formula/font-comic-sans-ms.rb
+++ Formula/font-comic-sans-ms.rb
@@ -2,8 +2,10 @@ class FontComicSansMs < Formula
   desc "Comic sans font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
   head "https://downloads.sourceforge.net/corefonts/comic32.exe"
+  depends_on "cabextract" => :build
 
   def install
+    system "cabextract", "comic32.exe"
     (share/"fonts").install Dir.glob("./**/Comic.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Comicbd.TTF")[0]
   end
