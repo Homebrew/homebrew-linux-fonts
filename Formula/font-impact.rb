97654256c46abda75ed2f8d7bd48eba219d68de8
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-22 00:24:22 +0000

    font-impact updated

--- Formula/font-impact.rb
+++ Formula/font-impact.rb
@@ -2,8 +2,10 @@ class FontImpact < Formula
   desc "Impact font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
   head "https://downloads.sourceforge.net/corefonts/impact32.exe"
+  depends_on "cabextract" => :build
 
   def install
+    system "cabextract", "impact32.exe"
     (share/"fonts").install Dir.glob("./**/Impact.TTF")[0]
   end
 
