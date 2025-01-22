6cfe76f90d20e63babca863b390ac6dfdc096ac5
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-22 00:24:22 +0000

    font-times-new-roman updated

--- Formula/font-times-new-roman.rb
+++ Formula/font-times-new-roman.rb
@@ -2,8 +2,10 @@ class FontTimesNewRoman < Formula
   desc "Times new roman font"
   homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
   head "https://downloads.sourceforge.net/corefonts/times32.exe"
+  depends_on "cabextract" => :build
 
   def install
+    system "cabextract", "times32.exe"
     (share/"fonts").install Dir.glob("./**/Times.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Timesbd.TTF")[0]
     (share/"fonts").install Dir.glob("./**/Timesbi.TTF")[0]
