09a1f6f4bd7befdae31c5091b3b0e3f537af096a
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-27 00:25:00 +0000

    font-bitcount-single-ink (new font)

---
+++ Formula/font-bitcount-single-ink.rb
@@ -0,0 +1,13 @@
+class FontBitcountSingleInk < Formula
+  desc "Bitcount single ink font"
+  homepage "https://github.com/petrvanblokland/TYPETR-Bitcount"
+  head "https://github.com/google/fonts/raw/main/ofl/bitcountsingleink/BitcountSingleInk%5BCRSV%2CELSH%2CELXP%2CSZP1%2CSZP2%2CXPN1%2CXPN2%2CYPN1%2CYPN2%2Cslnt%2Cwght%5D.ttf"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/BitcountSingleInk?CRSV,ELSH,ELXP,SZP1,SZP2,XPN1,XPN2,YPN1,YPN2,slnt,wght?.ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
