4a2a5873dcb8fa81a2acfb26608649bf3514e900
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-28 00:24:25 +0000

    font-maple-mono updated

--- Formula/font-maple-mono.rb
+++ Formula/font-maple-mono.rb
@@ -1,17 +1,27 @@
 class FontMapleMono < Formula
   desc "Maple mono font"
   homepage "https://github.com/subframe7536/Maple-font"
-  url "https://github.com/subframe7536/Maple-font/releases/download/v6.4/MapleMono-otf.zip"
-  version "6.4"
-  sha256 "7f07c594d6da5971428dc9e8ecd2b7759a80cec1cb87dadd1a44aadbac5cf6ac"
+  url "https://github.com/subframe7536/Maple-font/releases/download/v7.0-beta36/MapleMono-ttf.zip"
+  version "7.0-beta36"
+  sha256 "2774a3ac6ab66e46bfaaea98d3cd71bff696eabcb8c103eab7fc7bae461a534c"
 
   def install
-    (share/"fonts").install Dir.glob("./**/MapleMono-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/MapleMono-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/MapleMono-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/MapleMono-Light.otf")[0]
-    (share/"fonts").install Dir.glob("./**/MapleMono-LightItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/MapleMono-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-ExtraBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-ExtraBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-ExtraLight.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-ExtraLightItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-LightItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-Medium.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-MediumItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-SemiBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-ThinItalic.ttf")[0]
   end
 
   test do
