cabab08179aae968280afc19e9217a009f36e8b6
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-01-28 00:24:25 +0000

    font-maple-mono-nf updated

--- Formula/font-maple-mono-nf.rb
+++ Formula/font-maple-mono-nf.rb
@@ -1,17 +1,27 @@
 class FontMapleMonoNf < Formula
   desc "Maple mono nf font"
   homepage "https://github.com/subframe7536/Maple-font"
-  url "https://github.com/subframe7536/Maple-font/releases/download/v6.4/MapleMono-NF.zip"
-  version "6.4"
-  sha256 "7f2fa17546190d6e6790c562ae1926cacded22459eccf0eb9693719d1325e165"
+  url "https://github.com/subframe7536/Maple-font/releases/download/v7.0-beta36/MapleMono-NF-unhinted.zip"
+  version "7.0-beta36"
+  sha256 "d4dc9980d35b1af1511f42a953ab14b16f5843d8dbe9a74564fa2d91a4796857"
 
   def install
     (share/"fonts").install Dir.glob("./**/MapleMono-NF-Bold.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MapleMono-NF-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-ExtraBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-ExtraBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-ExtraLight.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-ExtraLightItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MapleMono-NF-Italic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MapleMono-NF-Light.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MapleMono-NF-LightItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-Medium.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-MediumItalic.ttf")[0]
     (share/"fonts").install Dir.glob("./**/MapleMono-NF-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-SemiBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMono-NF-ThinItalic.ttf")[0]
   end
 
   test do
