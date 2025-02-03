9b2e5ed990ababaddf49aadc99be543a25859fbf
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-03 00:24:57 +0000

    font-42dot-sans updated

--- Formula/font-42dot-sans.rb
+++ Formula/font-42dot-sans.rb
@@ -1,7 +1,8 @@
 class Font42dotSans < Formula
   desc "42dot sans font"
-  homepage "https://github.com/42dot/42dot-Sans"
-  head "https://github.com/google/fonts/raw/main/ofl/42dotsans/42dotSans%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/42dot+Sans"
+  head "https://github.com/google/fonts/raw/main/ofl/42dotsans/42dotSans%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/42dotSans?wght?.ttf")[0]
