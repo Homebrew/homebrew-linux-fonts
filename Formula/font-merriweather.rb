f5a0b96fe988ad488b19b60911ab25dfde7a1c9f
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-03 00:24:57 +0000

    font-merriweather updated

--- Formula/font-merriweather.rb
+++ Formula/font-merriweather.rb
@@ -7,14 +7,8 @@ class FontMerriweather < Formula
        only_path: "ofl/merriweather"
 
   def install
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Black.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-BlackItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Light.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-LightItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather-Italic?opsz,wdth,wght?.ttf")[0]
+    (share/"fonts").install Dir.glob("ofl/merriweather/./**/Merriweather?opsz,wdth,wght?.ttf")[0]
   end
 
   test do
