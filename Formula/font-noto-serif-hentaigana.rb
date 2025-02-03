8269fa4f71bab6d80b9a63df8fabbbe3b9413839
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-03 00:24:57 +0000

    font-noto-serif-hentaigana updated

--- Formula/font-noto-serif-hentaigana.rb
+++ Formula/font-noto-serif-hentaigana.rb
@@ -1,7 +1,8 @@
 class FontNotoSerifHentaigana < Formula
   desc "Noto serif hentaigana font"
-  homepage "https://github.com/notofonts/hentaigana"
-  head "https://github.com/google/fonts/raw/main/ofl/notoserifhentaigana/NotoSerifHentaigana%5Bwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Noto+Serif+Hentaigana"
+  head "https://github.com/google/fonts/raw/main/ofl/notoserifhentaigana/NotoSerifHentaigana%5Bwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/NotoSerifHentaigana?wght?.ttf")[0]
