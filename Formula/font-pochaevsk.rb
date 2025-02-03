532e28409a96b4aae870ff356f06963ee740766e
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-03 00:24:57 +0000

    font-pochaevsk updated

--- Formula/font-pochaevsk.rb
+++ Formula/font-pochaevsk.rb
@@ -1,7 +1,8 @@
 class FontPochaevsk < Formula
   desc "Pochaevsk font"
-  homepage "https://github.com/slavonic/pochaevsk"
-  head "https://github.com/google/fonts/raw/main/ofl/pochaevsk/Pochaevsk-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Pochaevsk"
+  head "https://github.com/google/fonts/raw/main/ofl/pochaevsk/Pochaevsk-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/Pochaevsk-Regular.ttf")[0]
