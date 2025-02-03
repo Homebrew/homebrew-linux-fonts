d519cb713e7a74e557b76dade117bb9165580dc9
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-03 00:24:57 +0000

    font-liter updated

--- Formula/font-liter.rb
+++ Formula/font-liter.rb
@@ -1,7 +1,8 @@
 class FontLiter < Formula
   desc "Liter font"
-  homepage "https://github.com/skugiz/liter"
-  head "https://github.com/google/fonts/raw/main/ofl/liter/Liter-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Liter"
+  head "https://github.com/google/fonts/raw/main/ofl/liter/Liter-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/Liter-Regular.ttf")[0]
