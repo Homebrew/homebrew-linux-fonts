dae49cc97ab259387e116a29b55027e0d5562a92
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-05 00:24:26 +0000

    font-space-mono updated

--- Formula/font-space-mono.rb
+++ Formula/font-space-mono.rb
@@ -4,10 +4,10 @@ class FontSpaceMono < Formula
   head "https://github.com/googlefonts/spacemono/archive/refs/heads/master.tar.gz"
 
   def install
-    (share/"fonts").install Dir.glob("./**/fonts/SpaceMono-Bold.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/SpaceMono-BoldItalic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/SpaceMono-Italic.ttf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/SpaceMono-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/ttf/SpaceMono-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/ttf/SpaceMono-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/ttf/SpaceMono-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/fonts/ttf/SpaceMono-Regular.ttf")[0]
   end
 
   test do
