2084e98ae80b4fd4b7b3f6e15c0d3e849aec8d70
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-10 00:25:46 +0000

    font-brill updated

--- Formula/font-brill.rb
+++ Formula/font-brill.rb
@@ -1,6 +1,6 @@
 class FontBrill < Formula
   desc "Brill font"
-  homepage "https://brill.com/page/BrillFont/"
+  homepage "https://brill.com/page/resources_brilltypeface"
   url "https://brill.com/fileasset/The_Brill_Typeface_Package_v_4_0.zip"
   version "4.0"
   sha256 "5575fbca12d5f8f50b8613bf01ae373f4237fcb2fdf5a379e7378f477e54352e"
