c65f6455b043976fa5f9b426fd7f3635b9ae50e9
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-16 00:27:06 +0000

    font-brass-mono updated

--- Formula/font-brass-mono.rb
+++ Formula/font-brass-mono.rb
@@ -1,9 +1,9 @@
 class FontBrassMono < Formula
   desc "Brass mono font"
   homepage "https://github.com/fonsecapeter/brass_mono"
-  url "https://github.com/fonsecapeter/brass_mono/releases/download/v1.100/BrassMono.zip"
-  version "1.100"
-  sha256 "8aa2761baf1003c293b5619cf15e0a23ff86cd3cd56b60df6819b70093cd4d18"
+  url "https://github.com/fonsecapeter/brass_mono/releases/download/v1.101/BrassMono.zip"
+  version "1.101"
+  sha256 "674285de45647fd35f9378f358cc85ca405026be67df991b26c35292ffd71459"
 
   def install
     (share/"fonts").install Dir.glob("./**/BrassMono-Bold.ttf")[0]
