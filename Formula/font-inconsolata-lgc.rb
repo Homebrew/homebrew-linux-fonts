29315f99fc4be074ea8d8d35e76de357710b2b21
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-17 00:26:22 +0000

    font-inconsolata-lgc updated

--- Formula/font-inconsolata-lgc.rb
+++ Formula/font-inconsolata-lgc.rb
@@ -1,9 +1,9 @@
 class FontInconsolataLgc < Formula
   desc "Inconsolata lgc font"
   homepage "https://github.com/MihailJP/Inconsolata-LGC"
-  url "https://github.com/MihailJP/Inconsolata-LGC/releases/download/LGC-1.9.0/InconsolataLGC-1.9.0.tar.xz"
-  version "1.9.0"
-  sha256 "009464109b37d32dd568056aafba7369dc4fc84bd91f8645b3184ca3608e30ed"
+  url "https://github.com/MihailJP/Inconsolata-LGC/releases/download/LGC-1.10.0/InconsolataLGC-1.10.0.tar.xz"
+  version "1.10.0"
+  sha256 "f649b0ac61c01ccb4604413b30aeaab7c7965f8ee3bff47a7a0b88bed7d62637"
 
   def install
     (share/"fonts").install Dir.glob("./**/Inconsolata-LGC-Bold.ttf")[0]
