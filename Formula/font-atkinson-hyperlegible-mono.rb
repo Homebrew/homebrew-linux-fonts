4a85259bc09389050ecd49f5fa858f08dc67cf1d
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-17 00:26:22 +0000

    font-atkinson-hyperlegible-mono updated

--- Formula/font-atkinson-hyperlegible-mono.rb
+++ Formula/font-atkinson-hyperlegible-mono.rb
@@ -1,7 +1,8 @@
 class FontAtkinsonHyperlegibleMono < Formula
   desc "Atkinson hyperlegible mono font"
-  homepage "https://github.com/googlefonts/atkinson-hyperlegible-next-mono"
+  homepage "https://fonts.google.com/specimen/Atkinson+Hyperlegible+Mono"
   head "https://github.com/google/fonts.git",
+       verified:  "github.com/google/fonts",
        branch:    "main",
        only_path: "ofl/atkinsonhyperlegiblemono"
 
