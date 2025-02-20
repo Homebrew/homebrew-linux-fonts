54aedf053d5c98c9157b94267108653c95a1cc26
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-20 00:24:46 +0000

    font-glow-sans-j-normal updated

--- Formula/font-glow-sans-j-normal.rb
+++ Formula/font-glow-sans-j-normal.rb
@@ -5,7 +5,7 @@ class FontGlowSansJNormal < Formula
   version "0.93"
   sha256 "b4a84f6d277d3c0ec6e833ac0fe25b40545ededc19df5fb4643731eef0f730a2"
 
-  deprecate! "2024-02-17", because: :discontinued
+  disable! "2025-02-19", because: :discontinued
 
   def install
     (share/"fonts").install Dir.glob("./**/GlowSansJ-Normal-Bold.otf")[0]
