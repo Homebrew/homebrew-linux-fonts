103dc240ec78e818b11b9e4ea498e909df6ef28f
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-22 00:23:49 +0000

    font-0xproto updated

--- Formula/font-0xproto.rb
+++ Formula/font-0xproto.rb
@@ -1,9 +1,9 @@
 class Font0xproto < Formula
   desc "0xproto font"
   homepage "https://github.com/0xType/0xProto"
-  url "https://github.com/0xType/0xProto/releases/download/2.201/0xProto_2_201.zip"
-  version "2.201"
-  sha256 "cc383e1562085438d026ef6c3ad678ca0a12d8349013485adce9791890bf8821"
+  url "https://github.com/0xType/0xProto/releases/download/2.202/0xProto_2_202.zip"
+  version "2.202"
+  sha256 "4247363c00fafc5343d47e3a8a45e6dac7416019dabbf614b6aa24a5af056c1d"
 
   def install
     (share/"fonts").install Dir.glob("./**/0xProto-Bold.otf")[0]
