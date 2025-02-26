35437ef92550012d5949b5fdb1f426ea9f1c7db6
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-02-26 00:25:05 +0000

    font-lxgw-neozhisong updated

--- Formula/font-lxgw-neozhisong.rb
+++ Formula/font-lxgw-neozhisong.rb
@@ -1,9 +1,9 @@
 class FontLxgwNeozhisong < Formula
   desc "Lxgw neo zhisong font"
   homepage "https://github.com/lxgw/LxgwNeoZhiSong"
-  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v1.024/LXGWNeoZhiSong.ttf"
-  version "1.024"
-  sha256 "d193fe3bbb807342433b46d7ca992da0a4b5b049d352bc62ea7dab235b699b08"
+  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v1.025/LXGWNeoZhiSong.ttf"
+  version "1.025"
+  sha256 "09f00750c7a3eaaed8485663c58b07e15578e270f2a666846c8ed89da3bf167f"
 
   def install
     (share/"fonts").install Dir.glob("./**/LXGWNeoZhiSong.ttf")[0]
