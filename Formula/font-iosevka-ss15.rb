924b7243a1ce5ed8fcf7089a2e5b297a3b4118fb
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-03 00:26:50 +0000

    font-iosevka-ss15 updated

--- Formula/font-iosevka-ss15.rb
+++ Formula/font-iosevka-ss15.rb
@@ -1,9 +1,9 @@
 class FontIosevkaSs15 < Formula
   desc "Iosevka ss15 font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v33.0.0/SuperTTC-IosevkaSS15-33.0.0.zip"
-  version "33.0.0"
-  sha256 "d20a67bf2bc7bfa495845195acb1fd99da05cf9743be730efd2d26ad9981a443"
+  url "https://github.com/be5invis/Iosevka/releases/download/v33.0.1/SuperTTC-IosevkaSS15-33.0.1.zip"
+  version "33.0.1"
+  sha256 "85c2aafcb5e8392275eb4799a4afbb648f16d60de2bdafadcea10cf5235fa5a2"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaSS15.ttc")[0]
