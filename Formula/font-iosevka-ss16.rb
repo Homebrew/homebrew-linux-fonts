038562ef1cc01b78667eac375cea103f2d488fa9
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-03 00:26:50 +0000

    font-iosevka-ss16 updated

--- Formula/font-iosevka-ss16.rb
+++ Formula/font-iosevka-ss16.rb
@@ -1,9 +1,9 @@
 class FontIosevkaSs16 < Formula
   desc "Iosevka ss16 font"
   homepage "https://github.com/be5invis/Iosevka/"
-  url "https://github.com/be5invis/Iosevka/releases/download/v33.0.0/SuperTTC-IosevkaSS16-33.0.0.zip"
-  version "33.0.0"
-  sha256 "019db485c527c7117c316b3fef968d133dd3197f6ffe96dd624c8ed3e887d728"
+  url "https://github.com/be5invis/Iosevka/releases/download/v33.0.1/SuperTTC-IosevkaSS16-33.0.1.zip"
+  version "33.0.1"
+  sha256 "7002335750191826e48355f72a689dda834f6a401b42ff8561aacd3f9f3a0ff5"
 
   def install
     (share/"fonts").install Dir.glob("./**/IosevkaSS16.ttc")[0]
