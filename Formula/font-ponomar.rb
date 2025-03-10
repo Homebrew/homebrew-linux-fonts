df7c298ab333a4a2f478fcb47db02b545b225842
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-10 00:22:35 +0000

    font-ponomar updated

--- Formula/font-ponomar.rb
+++ Formula/font-ponomar.rb
@@ -1,7 +1,8 @@
 class FontPonomar < Formula
   desc "Ponomar font"
-  homepage "https://github.com/slavonic/Ponomar"
-  head "https://github.com/google/fonts/raw/main/ofl/ponomar/Ponomar-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Ponomar"
+  head "https://github.com/google/fonts/raw/main/ofl/ponomar/Ponomar-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/Ponomar-Regular.ttf")[0]
