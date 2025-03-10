f8907510df8d02cc1797a79a3d2dff392f966e8e
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-10 00:22:34 +0000

    font-big-shoulders updated

--- Formula/font-big-shoulders.rb
+++ Formula/font-big-shoulders.rb
@@ -1,7 +1,8 @@
 class FontBigShoulders < Formula
   desc "Big shoulders font"
-  homepage "https://github.com/xotypeco/big_shoulders"
-  head "https://github.com/google/fonts/raw/main/ofl/bigshoulders/BigShoulders%5Bopsz%2Cwght%5D.ttf"
+  homepage "https://fonts.google.com/specimen/Big+Shoulders"
+  head "https://github.com/google/fonts/raw/main/ofl/bigshoulders/BigShoulders%5Bopsz%2Cwght%5D.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/BigShoulders?opsz,wght?.ttf")[0]
