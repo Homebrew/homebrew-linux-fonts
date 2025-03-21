3b558ae998f4a5f8637e1dd048f0b1503648ce24
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-21 00:26:08 +0000

    font-maple-mono-normal (new font)

---
+++ Formula/font-maple-mono-normal.rb
@@ -0,0 +1,30 @@
+class FontMapleMonoNormal < Formula
+  desc "Maple mono normal font"
+  homepage "https://github.com/subframe7536/Maple-font"
+  url "https://github.com/subframe7536/Maple-font/releases/download/v7.0/MapleMonoNormal-TTF.zip"
+  version "7.0"
+  sha256 "bcbe9f47010fd8648e441ace1af120480f9fc63ad9c989a96cd7ae20ca55bd83"
+
+  def install
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-Bold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-BoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-ExtraBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-ExtraBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-ExtraLight.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-ExtraLightItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-Italic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-Light.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-LightItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-Medium.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-MediumItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-Regular.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-SemiBold.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-SemiBoldItalic.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-Thin.ttf")[0]
+    (share/"fonts").install Dir.glob("./**/MapleMonoNormal-ThinItalic.ttf")[0]
+  end
+
+  test do
+    assert_path_exists share/"fonts"
+  end
+end
