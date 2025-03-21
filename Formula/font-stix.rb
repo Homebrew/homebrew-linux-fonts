f1c092728f4270a243bffe4d5b7bf96e4d5e4aed
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-21 00:26:08 +0000

    font-stix updated

--- Formula/font-stix.rb
+++ Formula/font-stix.rb
@@ -1,21 +1,21 @@
 class FontStix < Formula
   desc "Stix font"
   homepage "https://stixfonts.org/"
-  url "https://github.com/stipub/stixfonts/archive/refs/tags/v2.13.tar.gz",
+  url "https://github.com/stipub/stixfonts/releases/download/v2.14/fonts.zip",
        verified: "github.com/stipub/stixfonts/"
-  version "2.13"
-  sha256 "c4671ec4a116d887c8ebf91b2706301bebc43e776ac00f549c36ad1f34964c98"
+  version "2.14"
+  sha256 "b9ce7effe9cf97185bc3bfd9b3c5e79e0928a500127d1f55d0a704e04d274420"
 
   def install
-    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoMath-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-Bold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-BoldItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-Italic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-Medium.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-MediumItalic.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-Regular.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-SemiBold.otf")[0]
-    (share/"fonts").install Dir.glob("./**/fonts/static_otf/STIXTwoText-SemiBoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/STIXTwoMath/OTF/STIXTwoMath.otf")[0]
+    (share/"fonts").install Dir.glob("./**/STIXTwoText/OTF/STIXTwoText-Bold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/STIXTwoText/OTF/STIXTwoText-BoldItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/STIXTwoText/OTF/STIXTwoText-Italic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/STIXTwoText/OTF/STIXTwoText-Medium.otf")[0]
+    (share/"fonts").install Dir.glob("./**/STIXTwoText/OTF/STIXTwoText-MediumItalic.otf")[0]
+    (share/"fonts").install Dir.glob("./**/STIXTwoText/OTF/STIXTwoText-Regular.otf")[0]
+    (share/"fonts").install Dir.glob("./**/STIXTwoText/OTF/STIXTwoText-SemiBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/STIXTwoText/OTF/STIXTwoText-SemiBoldItalic.otf")[0]
   end
 
   test do
