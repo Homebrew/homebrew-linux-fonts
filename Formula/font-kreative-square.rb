6240b77847babd085a466e731490bf7bffcb6d8d
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-22 00:25:31 +0000

    font-kreative-square updated

--- Formula/font-kreative-square.rb
+++ Formula/font-kreative-square.rb
@@ -1,10 +1,10 @@
 class FontKreativeSquare < Formula
   desc "Kreative square font"
   homepage "https://www.kreativekorp.com/software/fonts/ksquare/"
-  url "https://github.com/kreativekorp/open-relay/releases/download/2024-06-01/KreativeSquare.zip",
+  url "https://github.com/kreativekorp/open-relay/releases/download/2025-03-20/KreativeSquare.zip",
        verified: "github.com/kreativekorp/open-relay/"
-  version "2024-06-01"
-  sha256 "d824b150dddfcb6215eb07fe890bc72721ff02dd03e8941b10e87deb25177e32"
+  version "2025-03-20"
+  sha256 "31fca4cd1f17ddc778377a47bf06a3e221b850b482bf6019251f30e4f8296a33"
 
   def install
     (share/"fonts").install Dir.glob("./**/KreativeSquare.ttf")[0]
