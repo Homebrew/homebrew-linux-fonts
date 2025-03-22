e33502197ae15006d52a50062f2b17afa7c77cc4
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-22 00:25:31 +0000

    font-fairfax updated

--- Formula/font-fairfax.rb
+++ Formula/font-fairfax.rb
@@ -1,10 +1,10 @@
 class FontFairfax < Formula
   desc "Fairfax font"
   homepage "https://www.kreativekorp.com/software/fonts/fairfax/"
-  url "https://github.com/kreativekorp/open-relay/releases/download/2024-06-01/Fairfax.zip",
+  url "https://github.com/kreativekorp/open-relay/releases/download/2025-03-20/Fairfax.zip",
        verified: "github.com/kreativekorp/open-relay/"
-  version "2024-06-01"
-  sha256 "eaac64504ec44232a90677670e234c6905c05819448847fc1fa0ea44ca24a805"
+  version "2025-03-20"
+  sha256 "3060579c3853acd08d781c2c1c5479908d32652bb84646e10edd6435bc88628e"
 
   def install
     (share/"fonts").install Dir.glob("./**/Fairfax.ttf")[0]
