c61a1bcac1f021afdead7448d0ee6bbbb4143dfd
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-27 00:26:02 +0000

    font-cozette updated

--- Formula/font-cozette.rb
+++ Formula/font-cozette.rb
@@ -1,15 +1,17 @@
 class FontCozette < Formula
   desc "Cozette font"
   homepage "https://github.com/slavfox/Cozette"
-  url "https://github.com/slavfox/Cozette/releases/download/v.1.27.0/CozetteFonts-v-1-27-0.zip"
-  version "1.27.0"
-  sha256 "ce3435f89bad4e6fcc1faf45fb9cfcb62473a07b6b8f5f709973f9cb4df643d0"
+  url "https://github.com/slavfox/Cozette/releases/download/v.1.28.0/CozetteFonts-v-1-28-0.zip"
+  version "1.28.0"
+  sha256 "18edb5af56a38b4a4fcc5772ccab38069a669682d28c45bedd899eda30853d72"
 
   def install
     (share/"fonts").install Dir.glob("./**/CozetteCrossedSevenVector.otf")[0]
     (share/"fonts").install Dir.glob("./**/CozetteCrossedSevenVectorBold.otf")[0]
     (share/"fonts").install Dir.glob("./**/CozetteVector.otf")[0]
     (share/"fonts").install Dir.glob("./**/CozetteVectorBold.otf")[0]
+    (share/"fonts").install Dir.glob("./**/cozette.dfont")[0]
+    (share/"fonts").install Dir.glob("./**/cozette_hidpi.dfont")[0]
   end
 
   test do
