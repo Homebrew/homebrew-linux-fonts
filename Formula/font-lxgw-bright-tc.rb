32c6aa01d49920e6c49d97d5f69986501370a609
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-29 00:26:05 +0000

    font-lxgw-bright-tc updated

--- Formula/font-lxgw-bright-tc.rb
+++ Formula/font-lxgw-bright-tc.rb
@@ -1,9 +1,9 @@
 class FontLxgwBrightTc < Formula
   desc "Lxgw bright tc font"
   homepage "https://github.com/lxgw/LxgwBright"
-  url "https://github.com/lxgw/LxgwBright/releases/download/v5.510/LXGWBrightTC.7z"
-  version "5.510"
-  sha256 "405d888610f23eedf84fbb1b0827f09415b483602e787d1efe95ec8430376be3"
+  url "https://github.com/lxgw/LxgwBright/releases/download/v5.517/LXGWBrightTC.7z"
+  version "5.517"
+  sha256 "43ed03a42f2ca8004fa6c4647e1f85a9f08380f58365b185852f14d3d2f81651"
 
   def install
     (share/"fonts").install Dir.glob("./**/LXGWBrightTC-Italic.ttf")[0]
