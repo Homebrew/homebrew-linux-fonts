7104e31e194d74605ee360a271193ca25319760a
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-03-29 00:26:05 +0000

    font-lxgw-wenkai-tc updated

--- Formula/font-lxgw-wenkai-tc.rb
+++ Formula/font-lxgw-wenkai-tc.rb
@@ -1,9 +1,9 @@
 class FontLxgwWenkaiTc < Formula
   desc "Lxgw wenkai tc font"
   homepage "https://github.com/lxgw/LxgwWenkaiTC"
-  url "https://github.com/lxgw/LxgwWenkaiTC/releases/download/v1.510/lxgw-wenkai-tc-v1.510.zip"
-  version "1.510"
-  sha256 "0363daf5ec7797453e28c83409421bebc7bb294c81ce30f4ef46e9dc585c5f70"
+  url "https://github.com/lxgw/LxgwWenkaiTC/releases/download/v1.511/lxgw-wenkai-tc-v1.511.zip"
+  version "1.511"
+  sha256 "76dc9e3da57a301d36d8aca6d3fc1d1f36c2b10904da5aa6707130f4f608819b"
 
   def install
     (share/"fonts").install Dir.glob("./**/LXGWWenKaiMonoTC-Light.ttf")[0]
