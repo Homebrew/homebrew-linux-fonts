ceb5b210c61d0aaa4265af4c52577a708e459c10
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-10 00:26:36 +0000

    font-special-gothic updated

--- Formula/font-special-gothic.rb
+++ Formula/font-special-gothic.rb
@@ -1,7 +1,8 @@
 class FontSpecialGothic < Formula
   desc "Special gothic font"
-  homepage "https://github.com/AlistairMcCready/Special-Gothic"
-  head "https://github.com/google/fonts/raw/main/ofl/specialgothic/SpecialGothic-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Special+Gothic"
+  head "https://github.com/google/fonts/raw/main/ofl/specialgothic/SpecialGothic-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/SpecialGothic-Regular.ttf")[0]
