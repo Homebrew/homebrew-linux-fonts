106ff01d3cbe14eb747d6fae7b9690dac2c48cca
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:40 +0000

    font-inconsolata-lgc-nerd-font updated

--- Formula/font-inconsolata-lgc-nerd-font.rb
+++ Formula/font-inconsolata-lgc-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontInconsolataLgcNerdFont < Formula
   desc "Inconsolata lgc nerd font (inconsolata lgc) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/InconsolataLGC.zip"
-  version "3.3.0"
-  sha256 "12c30458e9d6fe14b6fe8f86a21af5709a217262288b1e7a1050cc2de534a134"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/InconsolataLGC.zip"
+  version "3.4.0"
+  sha256 "49d865259e5a85620c1805810ab40bd366d199519117649651a010b3b2f0660f"
 
   def install
     (share/"fonts").install Dir.glob("./**/InconsolataLGCNerdFont-Bold.ttf")[0]
