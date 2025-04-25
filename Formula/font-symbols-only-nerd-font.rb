2502518333fdb5daf96e189d29c4cb26534745cb
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:42 +0000

    font-symbols-only-nerd-font updated

--- Formula/font-symbols-only-nerd-font.rb
+++ Formula/font-symbols-only-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontSymbolsOnlyNerdFont < Formula
   desc "Symbols nerd font (symbols only) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/NerdFontsSymbolsOnly.zip"
-  version "3.3.0"
-  sha256 "2078603c1e7a2fc2fa9e625ba1c30264d5d7c39907813d89beaa373f73a3a340"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/NerdFontsSymbolsOnly.zip"
+  version "3.4.0"
+  sha256 "8e617904b980fe3648a4b116808788fe50c99d2d495376cb7c0badbd8a564c47"
 
   def install
     (share/"fonts").install Dir.glob("./**/SymbolsNerdFont-Regular.ttf")[0]
