a5126e0ae7201bddba5dfffa6987bd649d9a73ef
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:41 +0000

    font-monofur-nerd-font updated

--- Formula/font-monofur-nerd-font.rb
+++ Formula/font-monofur-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontMonofurNerdFont < Formula
   desc "Monofur nerd font (monofur) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Monofur.zip"
-  version "3.3.0"
-  sha256 "d397ff7d7c9ca3472afeded1c9318221bd1dfad574d4d223bdbe8fc75eaa49c0"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Monofur.zip"
+  version "3.4.0"
+  sha256 "3a1e6a3b85821a61c8d42d98bc42f181cf1fa7994cdba7068bcf3f6dec249995"
 
   def install
     (share/"fonts").install Dir.glob("./**/MonofurNerdFont-Bold.ttf")[0]
