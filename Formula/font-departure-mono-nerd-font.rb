3a6b542f94afec27d19e8762607228c9573c0600
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:39 +0000

    font-departure-mono-nerd-font updated

--- Formula/font-departure-mono-nerd-font.rb
+++ Formula/font-departure-mono-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontDepartureMonoNerdFont < Formula
   desc "Departuremono nerd font (departure mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/DepartureMono.zip"
-  version "3.3.0"
-  sha256 "4721f307cd4a6f3632cfec4aafe68f7459002e4650bb4e5c5906a1d087636aff"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/DepartureMono.zip"
+  version "3.4.0"
+  sha256 "e186890efcae31b72e9f2abc860360123ec1b3ae03bc79bb2a9fec388ebade8a"
 
   def install
     (share/"fonts").install Dir.glob("./**/DepartureMonoNerdFont-Regular.otf")[0]
