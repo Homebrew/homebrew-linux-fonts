6f6ea441a515b14dfe3b5eea624bf30cd42f0866
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:41 +0000

    font-mononoki-nerd-font updated

--- Formula/font-mononoki-nerd-font.rb
+++ Formula/font-mononoki-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontMononokiNerdFont < Formula
   desc "Mononoki nerd font (mononoki) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Mononoki.zip"
-  version "3.3.0"
-  sha256 "b7f2419d6c83b68699ef7adc80d64fc6e384ca563bac0f0b6527deda55879d8f"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Mononoki.zip"
+  version "3.4.0"
+  sha256 "6c02113312b8f9be9888affc2dc13ed5f118a28786bb8b9c54306169ceda967d"
 
   def install
     (share/"fonts").install Dir.glob("./**/MononokiNerdFont-Bold.ttf")[0]
