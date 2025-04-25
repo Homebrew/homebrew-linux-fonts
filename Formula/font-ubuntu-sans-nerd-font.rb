8ddbae0dd55201ec45e7627ecf9325a0d9956ccd
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:42 +0000

    font-ubuntu-sans-nerd-font updated

--- Formula/font-ubuntu-sans-nerd-font.rb
+++ Formula/font-ubuntu-sans-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontUbuntuSansNerdFont < Formula
   desc "Ubuntusans nerd font families (ubuntu sans) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/UbuntuSans.zip"
-  version "3.3.0"
-  sha256 "65d74bd6d3b5d4447fb6abe8da009fcc133a888a29b27be46baec98872f5dee9"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/UbuntuSans.zip"
+  version "3.4.0"
+  sha256 "1166e2666da3ee4f044e32dd1dfc08da7f9ddaa6989fc1b0cbc18a78479434d7"
 
   def install
     (share/"fonts").install Dir.glob("./**/UbuntuSansMonoNerdFont-Bold.ttf")[0]
