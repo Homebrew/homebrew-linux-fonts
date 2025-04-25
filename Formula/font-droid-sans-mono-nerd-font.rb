793c1db2148f8fc7351f700732e7582970c34165
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:40 +0000

    font-droid-sans-mono-nerd-font updated

--- Formula/font-droid-sans-mono-nerd-font.rb
+++ Formula/font-droid-sans-mono-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontDroidSansMonoNerdFont < Formula
   desc "Droidsansm nerd font (droid sans mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/DroidSansMono.zip"
-  version "3.3.0"
-  sha256 "60a93ad45d38a1ee901413aa93dabcf9ed9ea8d954d6f9481e1f2d8733c40ea2"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/DroidSansMono.zip"
+  version "3.4.0"
+  sha256 "030229341907c833862f00682f0f4bce65d0dc8d281fc9a40d80b7ee9d306e88"
 
   def install
     (share/"fonts").install Dir.glob("./**/DroidSansMNerdFont-Regular.otf")[0]
