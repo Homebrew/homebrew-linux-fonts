38856756a39638c4b75c7dca9d707e2b5e2137a8
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:42 +0000

    font-space-mono-nerd-font updated

--- Formula/font-space-mono-nerd-font.rb
+++ Formula/font-space-mono-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontSpaceMonoNerdFont < Formula
   desc "Spacemono nerd font (space mono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/SpaceMono.zip"
-  version "3.3.0"
-  sha256 "1bad6d29b3a4a955f01a3e2cb2fdf92ad11d817914c05fdbfce70126770ebc25"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/SpaceMono.zip"
+  version "3.4.0"
+  sha256 "5edd68ef794c54cda235eb88b350e75f86b459bf8d499c698ea715cb121045e1"
 
   def install
     (share/"fonts").install Dir.glob("./**/SpaceMonoNerdFont-Bold.ttf")[0]
