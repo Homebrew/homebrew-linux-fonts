7cd8855ac559f75e551a399ab2fd931368c07603
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:39 +0000

    font-daddy-time-mono-nerd-font updated

--- Formula/font-daddy-time-mono-nerd-font.rb
+++ Formula/font-daddy-time-mono-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontDaddyTimeMonoNerdFont < Formula
   desc "Daddytimemono nerd font (daddytimemono) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/DaddyTimeMono.zip"
-  version "3.3.0"
-  sha256 "61504f83ba5e4dc5b8982bf0973db286ce66e795fd36f7075b825aa009a50a3a"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/DaddyTimeMono.zip"
+  version "3.4.0"
+  sha256 "3350e2309dce64b1a7b4221795e43b331499f13c3efb02d904ca157755fcf413"
 
   def install
     (share/"fonts").install Dir.glob("./**/DaddyTimeMonoNerdFont-Regular.ttf")[0]
