9a0379662806dfe9ca8cd668b43ecf4e91f809d0
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:39 +0000

    font-d2coding-nerd-font updated

--- Formula/font-d2coding-nerd-font.rb
+++ Formula/font-d2coding-nerd-font.rb
@@ -1,9 +1,9 @@
 class FontD2codingNerdFont < Formula
   desc "D2codingligature nerd font (d2coding) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/D2Coding.zip"
-  version "3.3.0"
-  sha256 "48c4270ffe60e86b236cfa3e7a03facea67f67d0b30440980f0e2be2e6a5c0f4"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/D2Coding.zip"
+  version "3.4.0"
+  sha256 "dd9aaf8c3b01a16e4e454e3dbc0542a8679c80c53299ea5a23f0ce7f53561345"
 
   def install
     (share/"fonts").install Dir.glob("./**/D2CodingLigatureNerdFont-Bold.ttf")[0]
