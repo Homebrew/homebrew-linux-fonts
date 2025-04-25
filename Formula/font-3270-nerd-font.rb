67c18de30f13e8575664ee80740564c233e1ceb5
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-25 00:27:39 +0000

    font-3270-nerd-font updated

--- Formula/font-3270-nerd-font.rb
+++ Formula/font-3270-nerd-font.rb
@@ -1,9 +1,9 @@
 class Font3270NerdFont < Formula
   desc "3270 nerd font (ibm 3270) font"
   homepage "https://github.com/ryanoasis/nerd-fonts"
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/3270.zip"
-  version "3.3.0"
-  sha256 "59b0701d5eef862a01f8a013b5a0c2055872642678d0694c4d0409053280ee93"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/3270.zip"
+  version "3.4.0"
+  sha256 "455574baeafb90558302c798198c34a7c91e70f2d9e88664df7738b1a310b22e"
 
   def install
     (share/"fonts").install Dir.glob("./**/3270NerdFont-Condensed.ttf")[0]
