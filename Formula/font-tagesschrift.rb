a303cd1bc1576ad68352bc4f2e7be9e22d429e07
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-28 00:28:46 +0000

    font-tagesschrift updated

--- Formula/font-tagesschrift.rb
+++ Formula/font-tagesschrift.rb
@@ -1,7 +1,8 @@
 class FontTagesschrift < Formula
   desc "Tagesschrift font"
-  homepage "https://github.com/yanone/tagesschrift"
-  head "https://github.com/google/fonts/raw/main/ofl/tagesschrift/Tagesschrift-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Tagesschrift"
+  head "https://github.com/google/fonts/raw/main/ofl/tagesschrift/Tagesschrift-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/Tagesschrift-Regular.ttf")[0]
