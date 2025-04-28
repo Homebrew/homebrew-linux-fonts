0164aa38e01c5c5579235e9a1301e1c5c799fa53
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-28 00:28:46 +0000

    font-coral-pixels updated

--- Formula/font-coral-pixels.rb
+++ Formula/font-coral-pixels.rb
@@ -1,7 +1,8 @@
 class FontCoralPixels < Formula
   desc "Coral pixels font"
-  homepage "https://github.com/tanukifont/Coral-Pixels"
-  head "https://github.com/google/fonts/raw/main/ofl/coralpixels/CoralPixels-Regular.ttf"
+  homepage "https://fonts.google.com/specimen/Coral+Pixels"
+  head "https://github.com/google/fonts/raw/main/ofl/coralpixels/CoralPixels-Regular.ttf",
+       verified: "github.com/google/fonts/"
 
   def install
     (share/"fonts").install Dir.glob("./**/CoralPixels-Regular.ttf")[0]
